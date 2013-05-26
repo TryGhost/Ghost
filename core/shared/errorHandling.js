ee610c6fc66bb46527265cd0d23eb6e0a457560d
Jacob Gable <jacob.gable@gmail.com>
2013-05-26 13:51:58 -0500

    Error handlers

---
+++ core/shared/errorHandling.js
@@ -0,0 +1,59 @@
+(function() {
+    "use strict";
+
+    var _ = require('underscore'),
+        errors;
+
+    /**
+     * Basic error handling helpers
+     */
+    errors = {
+        throwError: function(err) {
+            if (!err) {
+                return;
+            }
+
+            if (_.isString(err)) {
+                throw new Error(err);
+            }
+
+            throw err;
+        },
+
+        logError: function (err) {
+            // TODO: Logging framework hookup
+            console.log("Error occurred: ", err.message || err);
+        },
+
+        logAndThrowError: function (err) {
+            this.logError(err);
+
+            this.throwError(err);
+        },
+
+        logErrorWithMessage: function (msg) {
+            var self = this;
+
+            return function () {
+                self.logError(msg);
+            };
+        },
+
+        logErrorWithRedirect: function (msg, redirectTo, req, res) {
+            var self = this;
+
+            return function () {
+                self.logError(msg);
+
+                if (_.isFunction(res.redirect)) {
+                    res.redirect(redirectTo);
+                }
+            };
+        }
+    };
+
+    // Ensure our 'this' context in the functions
+    _.bindAll(errors, "throwError", "logError", "logAndThrowError", "logErrorWithMessage", "logErrorWithRedirect");
+
+    module.exports = errors;
+}());
