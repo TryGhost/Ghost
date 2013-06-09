40d8b0321c54771f5aab419e185533a86c4c082e
cobbspur <cobbspur@hotmail.com>
2013-06-09 19:04:34 +0100

    implements #38 html and scss for uploader
    - Fixed broken html
    - Added file input to html
    - Styling for hidden file input button
    - Styling for progress bar and loading
    - Added javascript classes for hooking file upload
    - Added loading image (cat)

--- core/admin/assets/lib/showdown/extensions/ghostdown.js
+++ core/admin/assets/lib/showdown/extensions/ghostdown.js
@@ -5,8 +5,15 @@
             {
                 type: 'lang',
                 filter: function (source) {
-                    return source.replace(/\n+!image\[([\d\w\s]*)\]/gi, function (match, alt, a) {
-                        return '<section class="image-uploader"><span class="media"><span class="hidden">Image Upload</span></span><div class="description">Add image of <strong>' + alt + '</strong></div><a class="image-url" title="Add image from URL"><span class="hidden">URL</a><a class="image-webcam" title="Add image from webcam"><span class="hidden">Webcam</a></section>';
+                    return source.replace(/\n?!image\[([\d\w\s]*)\]/gi, function (match, alt, a) {
+                        return '<section  class="js-drop-zone image-uploader">' +
+                            '<span class="media"><span class="hidden">Image Upload</span></span>' +
+                            '<div class="description">Add image of <strong>' + alt + '</strong></div>' +
+                            '<input data-url="upload" class="js-fileupload fileupload" type="file" name="uploadimage">' +
+                            '<a class="image-url" title="Add image from URL"><span class="hidden">URL</span></a>' +
+                            '<a class="image-webcam" title="Add image from webcam">' +
+                            '<span class="hidden">Webcam</span></a>' +
+                            '</section>';
                     });
                 }
             }
