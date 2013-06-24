0479d6cf5af1b8620533a4d74c9c051fd2b2b0cf
Matthew Harrison-Jones <matthojo@gmail.com>
2013-06-24 11:37:57 +0100

    Updated documentation generation to use Docco
    Also removed generated documentation from main repo as it is generated on Grunt init. Split docs into `application` and `test` for easy folder separation. Now includes all `.js` applicable files present in the repo.
    
    Note: more than one `index.js` is present so Docco overwrites the `index.html` file for each.

--- Gruntfile.js
+++ Gruntfile.js
@@ -169,10 +169,36 @@
                 },
 
                 docco: {
-                    debug: {
-                        src: ["README.md", "config.js", "app.js", "core/ghost.js", "core/admin/assets/js/*.js", "core/frontend/helpers/index.js", "core/lang/i18n.js"],
+                    application: {
+                        src: [
+                            "README.md",
+                            "config.js",
+                            "app.js",
+                            "core/ghost.js",
+                            "core/admin/assets/js/*.js",
+                            "core/admin/assets/js/models/*.js",
+                            "core/admin/assets/js/views/*.js",
+                            "core/admin/controllers/*.js",
+                            "core/frontend/controllers/*.js",
+                            "core/frontend/helpers/*.js",
+                            "core/frontend/filters/*.js",
+                            "core/lang/i18n.js",
+                            "core/shared/*.js",
+                            "core/shared/models/*.js",
+                            "core/shared/permissions/*.js"
+                        ],
                         options: {
                             output: './docs',
+                            css: './docs/ghost.css'
+                        }
+                    },
+                    test: {
+                        src: [
+                            "core/test/ghost.js",
+                            "core/test/ghost/*.js"
+                        ],
+                        options: {
+                            output: './docs/test',
                             css: './docs/ghost.css'
                         }
                     }
@@ -200,7 +226,7 @@
 
             // Prepare the project for development
             // TODO: Git submodule init/update (https://github.com/jaubourg/grunt-update-submodules)?
-            grunt.registerTask("init", ["shell:bourbon", "sass:admin", 'handlebars']);
+            grunt.registerTask("init", ["shell:bourbon", "sass:admin", 'handlebars', 'docco']);
 
             // Run API tests only
             grunt.registerTask("test-api", ["mochaTest:api"]);
@@ -209,7 +235,9 @@
             grunt.registerTask("test-p", ["mochaTest:perm"]);
 
             // Run tests and lint code
-            grunt.registerTask("validate", ["jslint", "mochaTest:all"]);
+            grunt.registerTask("validate", ["jslint", "mochaTest:all", 'docco:test']);
+
+            grunt.registerTask("docs", ["docco"]);
 
             /* Nightly builds
              * - Bump patch version in package.json,
@@ -247,7 +275,7 @@
             ]);
 
             // When you just say "grunt"
-            grunt.registerTask("default", ['sass:admin', 'handlebars', 'docco']);
+            grunt.registerTask("default", ['sass:admin', 'handlebars', 'docco:application']);
         };
 
     module.exports = configureGrunt;
