e32aad1960a042e393c4b830fb236be3749affb0
Hannah Wolfe <erisds@gmail.com>
2013-06-30 21:58:12 +0100

    issue #186 - load plugins
    - Adding activePlugins array to config.js
    - Adding a loadPlugins function to ghost.js
    - Tweaking fancyFirstChar.js so that it works again & getting rid of the function wrapper

--- content/plugins/fancyFirstChar.js
+++ content/plugins/fancyFirstChar.js
@@ -1,48 +1,39 @@
-/*globals exports */
-(function () {
-    "use strict";
+var FancyFirstChar;
 
-    var FancyFirstChar;
-
-    FancyFirstChar = function (ghost) {
-        this.ghost = function () {
-            return ghost;
-        };
+FancyFirstChar = function (ghost) {
+    this.ghost = function () {
+        return ghost;
     };
+};
 
-    FancyFirstChar.prototype.init = function () {
-        this.ghost().registerFilter('prePostsRender', function (posts) {
-            var post,
-                originalContent,
-                newContent,
-                firstCharIndex = 0;
+FancyFirstChar.prototype.init = function () {
+    this.ghost().registerFilter('prePostsRender', function (posts) {
+        var post,
+            originalContent,
+            newContent,
+            firstCharIndex = 0;
 
-
-
-            for (post in posts) {
-                if (posts.hasOwnProperty(post)) {
-                    originalContent = posts[post].content;
-                    if (originalContent.substr(0, 1) === '<') {
-                        firstCharIndex = originalContent.indexOf('>') + 1;
-                    }
-
-                    newContent = originalContent.substr(0, firstCharIndex);
-                    newContent += '<span class="fancyChar">';
-                    newContent += originalContent.substr(firstCharIndex, 1);
-                    newContent += '</span>';
-                    newContent += originalContent.substr(firstCharIndex + 1, originalContent.length - firstCharIndex - 1);
-
-                    posts[post].content = newContent;
+        for (post in posts) {
+            if (posts.hasOwnProperty(post)) {
+                originalContent = posts[post].content_html;
+                if (originalContent.substr(0, 1) === '<') {
+                    firstCharIndex = originalContent.indexOf('>') + 1;
                 }
+
+                newContent = originalContent.substr(0, firstCharIndex);
+                newContent += '<span class="fancyChar">';
+                newContent += originalContent.substr(firstCharIndex, 1);
+                newContent += '</span>';
+                newContent += originalContent.substr(firstCharIndex + 1, originalContent.length - firstCharIndex - 1);
+
+                posts[post].content_html = newContent;
             }
-            return posts;
-        });
-    };
+        }
+        return posts;
+    });
+};
 
-    FancyFirstChar.prototype.activate = function () {};
-    FancyFirstChar.prototype.deactivate = function () {};
+FancyFirstChar.prototype.activate = function () {};
+FancyFirstChar.prototype.deactivate = function () {};
 
-
-
-    module.exports = FancyFirstChar;
-}());
+module.exports = FancyFirstChar;
