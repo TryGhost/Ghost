8bbacd9ec9c90d3bd8e3e61c78f2e0cbe5a099a2
cobbspur <cobbspur@hotmail.com>
2013-07-04 19:42:49 +0100

    closes #195 posts date on content page
    - adds dateFormat handlebars helper for client side with extra option to format in time since style
    - adds this extra dateFormat option to existing server side helper.
    - adds scss for draft and scheduled status
    - adds true/false values to post for draft  and published to validate in handlebars
    - changes admin>content post collection query to order posts by updated_at values in router.js
    - adds minified moment.js and links to moment.js and helper.js for clientside

---
+++ core/admin/assets/js/helpers/index.js
@@ -0,0 +1,16 @@
+/*globals Handlebars, moment
+*/
+(function () {
+    "use strict";
+    Handlebars.registerHelper('dateFormat', function (context, block) {
+        var f = block.hash.format || "MMM Do, YYYY",
+            timeago = block.hash.timeago,
+            date;
+        if (timeago) {
+            date = moment(context).fromNow();
+        } else {
+            date = moment(context).format(f);
+        }
+        return date;
+    });
+}());
