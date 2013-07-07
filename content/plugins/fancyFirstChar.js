7acd165d7a851f1c5b72b0f087eee625abc9b601
Hannah Wolfe <erisds@gmail.com>
2013-07-07 18:54:50 +0100

    Bug fix - unable to view single page
    - The fancyFirstChar plugin expected to always get an array of posts, and therefore broke on the single post pages
    - Changed the plugin to cope with single objects as well as arrays

--- content/plugins/fancyFirstChar.js
+++ content/plugins/fancyFirstChar.js
@@ -1,29 +1,36 @@
+var _ = require('underscore');
+
 var fancyFirstChar;
+
+function fancify(originalContent) {
+    var newContent,
+        firstCharIndex = 0;
+
+    if (originalContent.substr(0, 1) === '<') {
+        firstCharIndex = originalContent.indexOf('>') + 1;
+    }
+
+    newContent = originalContent.substr(0, firstCharIndex);
+    newContent += '<span class="fancyChar">';
+    newContent += originalContent.substr(firstCharIndex, 1);
+    newContent += '</span>';
+    newContent += originalContent.substr(firstCharIndex + 1, originalContent.length - firstCharIndex - 1);
+
+    return newContent;
+}
+
 
 fancyFirstChar = {
     init: function (ghost) {
         ghost.registerFilter('prePostsRender', function (posts) {
-            var post,
-                originalContent,
-                newContent,
-                firstCharIndex = 0;
-
-            for (post in posts) {
-                if (posts.hasOwnProperty(post)) {
-                    originalContent = posts[post].content_html;
-                    if (originalContent.substr(0, 1) === '<') {
-                        firstCharIndex = originalContent.indexOf('>') + 1;
-                    }
-
-                    newContent = originalContent.substr(0, firstCharIndex);
-                    newContent += '<span class="fancyChar">';
-                    newContent += originalContent.substr(firstCharIndex, 1);
-                    newContent += '</span>';
-                    newContent += originalContent.substr(firstCharIndex + 1, originalContent.length - firstCharIndex - 1);
-
-                    posts[post].content_html = newContent;
-                }
+            if (_.isArray(posts)) {
+                _.each(posts, function (post) {
+                    post.content_html = fancify(post.content_html);
+                });
+            } else if (posts.hasOwnProperty('content_html')) {
+                posts.content_html = fancify(posts.content_html);
             }
+
             return posts;
         });
     },
