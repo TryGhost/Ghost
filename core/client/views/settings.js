52dc22c95249950c05ee5ae62daa08e58dc3f1e0
Hannah Wolfe <erisds@gmail.com>
2013-08-05 18:26:44 +0100

    Editable user profiles in settings screen
    closes #276
    
     - settings screen now loads a model when a pane is requested, rather than when the whole screen is requested
     - added browse, read and edit methods and routes for users to the API
     - added user model & template to client and wired everything up.
     - provided default images for cover and profile picture

--- core/client/views/settings.js
+++ core/client/views/settings.js
@@ -30,6 +30,8 @@
             this.showContent(options.pane || 'general');
         },
 
+        models: {},
+
         events: {
             'click .settings-menu li' : 'switchPane'
         },
@@ -42,13 +44,30 @@
         },
 
         showContent: function (id) {
+            var self = this,
+                model;
+
             Backbone.history.navigate('/settings/' + id);
             if (this.pane && id === this.pane.el.id) {
                 return;
             }
             _.result(this.pane, 'destroy');
             this.setActive(id);
-            this.pane = new Settings[id]({ el: '.settings-content', model: this.model });
+            this.pane = new Settings[id]({ el: '.settings-content'});
+
+            if (!this.models.hasOwnProperty(this.pane.options.modelType)) {
+                model = this.models[this.pane.options.modelType] = new Ghost.Models[this.pane.options.modelType]();
+                model.fetch().then(function () {
+                    self.renderPane(model);
+                });
+            } else {
+                model = this.models[this.pane.options.modelType];
+                self.renderPane(model);
+            }
+        },
+
+        renderPane: function (model) {
+            this.pane.model = model;
             this.pane.render();
         },
 
@@ -63,6 +82,9 @@
     // Content panes
     // --------------
     Settings.Pane = Ghost.View.extend({
+        options: {
+            modelType: 'Settings'
+        },
         destroy: function () {
             this.$el.removeClass('active');
             this.undelegateEvents();
@@ -71,6 +93,25 @@
         afterRender: function () {
             this.$el.attr('id', this.id);
             this.$el.addClass('active');
+        },
+        saveSuccess: function () {
+            this.addSubview(new Ghost.Views.NotificationCollection({
+                model: [{
+                    type: 'success',
+                    message: 'Saved',
+                    status: 'passive'
+                }]
+            }));
+
+        },
+        saveError: function () {
+            this.addSubview(new Ghost.Views.NotificationCollection({
+                model: [{
+                    type: 'error',
+                    message: 'Something went wrong, not saved :(',
+                    status: 'passive'
+                }]
+            }));
         }
     });
 
@@ -85,29 +126,12 @@
         },
 
         saveSettings: function () {
-            var self = this;
             this.model.save({
                 title: this.$('#blog-title').val(),
                 email: this.$('#email-address').val()
             }, {
-                success: function () {
-                    self.addSubview(new Ghost.Views.NotificationCollection({
-                        model: [{
-                            type: 'success',
-                            message: 'Saved',
-                            status: 'passive'
-                        }]
-                    }));
-                },
-                error: function () {
-                    self.addSubview(new Ghost.Views.NotificationCollection({
-                        model: [{
-                            type: 'error',
-                            message: 'Something went wrong, not saved :(',
-                            status: 'passive'
-                        }]
-                    }));
-                }
+                success: this.saveSuccess,
+                error: this.saveError
             });
         },
 
@@ -127,29 +151,11 @@
             'click .button-save': 'saveSettings'
         },
         saveSettings: function () {
-            var self = this;
             this.model.save({
                 description: this.$('#blog-description').val()
             }, {
-                success: function () {
-                    self.addSubview(new Ghost.Views.NotificationCollection({
-                        model: [{
-                            type: 'success',
-                            message: 'Saved',
-                            status: 'passive'
-                        }]
-                    }));
-
-                },
-                error: function () {
-                    self.addSubview(new Ghost.Views.NotificationCollection({
-                        model: [{
-                            type: 'error',
-                            message: 'Something went wrong, not saved :(',
-                            status: 'passive'
-                        }]
-                    }));
-                }
+                success: this.saveSuccess,
+                error: this.saveError
             });
         },
 
@@ -161,30 +167,71 @@
         }
     });
 
+     // ### User profile
+    Settings.user = Settings.Pane.extend({
+        id: 'user',
+
+        options: {
+            modelType: 'User'
+        },
+
+        events: {
+            'click .button-save': 'saveUser'
+        },
+
+        saveUser: function () {
+            this.model.save({
+                'full_name':        this.$('#user-name').val(),
+                'email_address':    this.$('#user-email').val(),
+                'location':         this.$('#user-location').val(),
+                'url':              this.$('#user-website').val(),
+                'bio':              this.$('#user-bio').val(),
+                'profile_picture':  this.$('#user-profile-picture').attr('src'),
+                'cover_picture':    this.$('#user-cover-picture').attr('src')
+            }, {
+                success: this.saveSuccess,
+                error: this.saveError
+            });
+        },
+
+        templateName: 'settings/user-profile',
+
+        beforeRender: function () {
+            var user = this.model.toJSON();
+            this.$('#user-name').val(user.full_name);
+            this.$('#user-email').val(user.email_address);
+            this.$('#user-location').val(user.location);
+            this.$('#user-website').val(user.url);
+            this.$('#user-bio').val(user.bio);
+            this.$('#user-profile-picture').attr('src', user.profile_picture);
+            this.$('#user-cover-picture').attr('src', user.cover_picture);
+        }
+    });
+
     // ### User settings
     Settings.users = Settings.Pane.extend({
-        el: '#users',
+        id: 'users',
         events: {
         }
     });
 
     // ### Appearance settings
     Settings.appearance = Settings.Pane.extend({
-        el: '#appearance',
+        id: 'appearance',
         events: {
         }
     });
 
     // ### Services settings
     Settings.services = Settings.Pane.extend({
-        el: '#services',
+        id: 'services',
         events: {
         }
     });
 
     // ### Plugins settings
     Settings.plugins = Settings.Pane.extend({
-        el: '#plugins',
+        id: 'plugins',
         events: {
         }
     });
