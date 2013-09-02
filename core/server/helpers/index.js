552a25018fc4468bd7bd8fd422ad3f8e926d4887
Hannah Wolfe <erisds@gmail.com>
2013-09-02 22:06:16 +0100

    Absolute option for {{url}} helper
    - pass absolute to {{url}} to get an absolute url rather than a relative one

--- core/server/helpers/index.js
+++ core/server/helpers/index.js
@@ -44,11 +44,25 @@ coreHelpers = function (ghost) {
         return date;
     });
 
+    // ### URL helper
+    //
+    // *Usage example:*
+    // `{{url}}`
+    // `{{url absolute}}`
+    //
+    // Returns the URL for the current object context
+    // i.e. If inside a post context will return post permalink
+    // absolute flag outputs absolute URL, else URL is relative
     ghost.registerThemeHelper('url', function (context, options) {
-        if (models.isPost(this)) {
-            return "/" + this.slug;
+        var output = '';
+
+        if (options && options.absolute) {
+            output += ghost.config().env[process.NODE_ENV].url;
         }
-        return '';
+        if (models.isPost(this)) {
+            output += "/" + this.slug;
+        }
+        return output;
     });
 
     // ### Author Helper
