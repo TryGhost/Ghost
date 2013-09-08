e51a14ee90e6f1145c1900aa0ec1c5fddff4bde3
Sebastian Gierlinger <s.gierlinger@me.com>
2013-09-08 22:11:52 +0200

    Clean up settings/general screen
    Closes #345
    - added blog description to general screen
    - removed content screen from sidebar
    - removed beforeRender from settings front end
    - removed content screen from tests

--- core/client/models/settings.js
+++ core/client/models/settings.js
@@ -2,15 +2,8 @@
 (function () {
     "use strict";
 
-    // Set the url manually and id to '0' to force PUT requests
     Ghost.Models.Settings = Backbone.Model.extend({
-        url: Ghost.settings.apiRoot + '/settings',
-        id: "0",
-        defaults: {
-            title: 'My Blog',
-            description: '',
-            email: 'admin@tryghost.org'
-        }
+        url: Ghost.settings.apiRoot + '/settings'
     });
 
 }());
