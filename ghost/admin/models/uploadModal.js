5041a5677c8b70a1db901c44ba267167572be86d
cobbspur <cobbspur@hotmail.com>
2013-09-08 17:17:16 +0100

    User Profile Image Upload
    closes #280
    
    - adds image uploader to user profile page.
    - click on cover picture or change cover button to open file upload modal.
    - created new upload modal that extends model to reduce some code duplication

---
+++ ghost/admin/models/uploadModal.js
@@ -0,0 +1,36 @@
+/*global Ghost, Backbone */
+(function () {
+    "use strict";
+    Ghost.Models.uploadModal = Backbone.Model.extend({
+
+        options: {
+            close: false,
+            type: "action",
+            style: "wide",
+            animation: 'fadeIn',
+            afterRender: function () {
+                this.$('.js-drop-zone').upload();
+            },
+            confirm: {
+                reject: {
+                    func: function () { // The function called on rejection
+                        return true;
+                    },
+                    buttonClass: true,
+                    text: "Cancel" // The reject button text
+                }
+            }
+        },
+        content: {
+            template: 'uploadImage'
+        },
+
+        initialize: function (options) {
+            this.options.id = options.id;
+            this.options.key = options.key;
+            this.options.src = options.src;
+            this.options.confirm.accept = options.accept;
+        }
+    });
+
+}());
