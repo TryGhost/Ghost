01f6551bf2c2745e7eadd7c0f17f1185d07c666d
Hannah Wolfe <erisds@gmail.com>
2013-09-13 15:06:17 +0100

    Small model update for tags and users
    - tags are now created with uuid & timestamps
    - user role is no longer a model, just a join done with attach

--- core/server/models/tag.js
+++ core/server/models/tag.js
@@ -1,10 +1,19 @@
 var Tag,
     Tags,
+    uuid = require('node-uuid'),
     Posts = require('./post').Posts,
     GhostBookshelf = require('./base');
 
 Tag = GhostBookshelf.Model.extend({
     tableName: 'tags',
+
+    hasTimestamps: true,
+
+    defaults: function () {
+        return {
+            uuid: uuid.v4()
+        };
+    },
 
     posts: function () {
         return this.belongsToMany(Posts);
