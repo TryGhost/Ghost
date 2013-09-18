eb6856dac327685949889221a399cf70cb2b5826
Hannah Wolfe <erisds@gmail.com>
2013-09-18 23:24:26 +0100

    Removing Temporary importer

--- core/server/data/import/index.js
+++ core/server/data/import/index.js
@@ -13,19 +13,5 @@ module.exports = function (version, data) {
         return when.reject("No importer found");
     }
 
-    return importer.importData(data).otherwise(function (err) {
-        if (err === "Unsupported version of data: 001" || err === "Unsupported version of data: 002") {
-            try {
-                importer = require("./temp.js");
-            } catch (ignore) {
-                // Zero effs given
-            }
-
-            if (!importer) {
-                return when.reject("No importer found");
-            }
-
-            return importer.importData(data);
-        }
-    });
+    return importer.importData(data);
 };
