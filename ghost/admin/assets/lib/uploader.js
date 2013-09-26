8eefbf9cc70a718a096c20455e7a88904d244fb3
Hannah Wolfe <erisds@gmail.com>
2013-09-26 12:34:35 +0100

    Image upload start event fired earlier
    issue #882

--- ghost/admin/assets/lib/uploader.js
+++ ghost/admin/assets/lib/uploader.js
@@ -22,7 +22,6 @@
         $.extend(this, {
             complete: function (result) {
                 var self = this;
-                $dropzone.trigger('uploadstart', [$dropzone.attr('id')]);
 
                 function showImage(width, height) {
                     $dropzone.find('img.js-upload-target').attr({"width": width, "height": height}).css({"display": "block"});
@@ -162,6 +161,7 @@
                 $dropzone.find('div.description').before($url);
 
                 $dropzone.find('.js-button-accept').on('click', function () {
+                    $dropzone.trigger('uploadstart', [$dropzone.attr('id')]);
                     $dropzone.find('div.description').hide();
                     val = $('#uploadurl').val();
                     $dropzone.find('.js-fileupload').removeClass('right');
