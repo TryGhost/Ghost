c732cd2ccbe0f52567fe98309aea519c8ceda6a4
Hannah Wolfe <erisds@gmail.com>
2013-10-10 16:43:25 +0100

    Adding xss unit test
    issue #938

--- core/test/unit/model_posts_spec.js
+++ core/test/unit/model_posts_spec.js
@@ -361,4 +361,13 @@ describe('Post Model', function () {
             done();
         }).then(null, done);
     });
+
+    it('should santize the title', function (done) {
+        new PostModel().fetch().then(function (model) {
+            return model.set({'title': "</title></head><body><script>alert('blogtitle');</script>"}).save();
+        }).then(function (saved) {
+                saved.get('title').should.eql("&lt;/title&gt;&lt;/head>&lt;body&gt;[removed]alert&#40;'blogtitle'&#41;;[removed]");
+                done();
+            }).otherwise(done);
+    });
 });
