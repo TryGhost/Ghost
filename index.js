b4cf4247c0e41fc9dd2faa700ff59455c4809528
germanrcuriel <germix@germix.net>
2013-11-03 00:40:43 +0100

    Add Coverage report for Unit tests
    issue #361
    - Added shell:jscoverage task to convert library folder to generate coverage report with mocha
    - Added shell:mocha_unit_coverage task to generate unit test coverage report
    - Added shell:mocha_integration_coverage task to generate integration test coverage report
    - Added clean:jscoverage task to remove converted folder when finished
    - Registered test-coverage Grunt task to include all the above
    - Added coverage files to .gitignore

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 // # Ghost bootloader
 // Orchestrates the loading of Ghost
 
-var configLoader = require('./core/config-loader.js'),
-    error        = require('./core/server/errorHandling');
+var core_path = process.env.APP_COVERAGE
+    ? './core-cov'
+    : './core',
+    configLoader = require(core_path + '/config-loader.js'),
+    error        = require(core_path + '/server/errorHandling');
 
 // If no env is set, default to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 configLoader.loadConfig().then(function () {
     // The server and its dependencies require a populated config
-    require('./core/server');
+    require(core_path + '/server');
 }).otherwise(error.logAndThrowError);
