0faabe9132f0d79770b909ca2eef99a9e176b4df
Fabian Becker <halfdan@xnorfz.de>
2013-11-10 23:46:09 +0000

    Fix page errors during builds.
    fixes #1434

--- core/client/assets/vendor/shortcuts.js
+++ core/client/assets/vendor/shortcuts.js
@@ -43,6 +43,7 @@ shortcut = {
             //Find Which key is pressed
             if (e.keyCode) code = e.keyCode;
             else if (e.which) code = e.which;
+            else return;
             var character = String.fromCharCode(code).toLowerCase();
             
             if(code == 188) character=","; //If the user presses , when the type is onkeydown
