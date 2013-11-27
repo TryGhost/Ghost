62f656cc531d7a8bb27ddcd95caac8a44ce6563e
Fabian Becker <halfdan@xnorfz.de>
2013-11-27 02:00:55 +0000

    Subdir support for admin interface
    refs #527

--- ghost/admin/models/widget.js
+++ ghost/admin/models/widget.js
@@ -36,7 +36,7 @@
     });
 
     Ghost.Collections.Widgets = Ghost.ProgressCollection.extend({
-        // url: Ghost.settings.apiRoot + '/widgets/', // What will this be?
+        // url: Ghost.paths.apiRoot + '/widgets/', // What will this be?
         model: Ghost.Models.Widget
     });
 
