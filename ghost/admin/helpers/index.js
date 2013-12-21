9c38d1a2d3d708800505ac6a410b1df2978d52d2
Hannah Wolfe <erisds@gmail.com>
2013-12-21 21:43:38 +0000

    Date helper fix, moment breaks with null values
    fixes #1730

--- ghost/admin/helpers/index.js
+++ ghost/admin/helpers/index.js
@@ -2,10 +2,26 @@
 */
 (function () {
     'use strict';
-    Handlebars.registerHelper('date', function (context, block) {
-        var f = block.hash.format || 'MMM Do, YYYY',
-            timeago = block.hash.timeago,
+    Handlebars.registerHelper('date', function (context, options) {
+        if (!options && context.hasOwnProperty('hash')) {
+            options = context;
+            context = undefined;
+
+            // set to published_at by default, if it's available
+            // otherwise, this will print the current date
+            if (this.published_at) {
+                context = this.published_at;
+            }
+        }
+
+        // ensure that context is undefined, not null, as that can cause errors
+        context = context === null ? undefined : context;
+
+        var f = options.hash.format || 'MMM Do, YYYY',
+            timeago = options.hash.timeago,
             date;
+
+
         if (timeago) {
             date = moment(context).fromNow();
         } else {
