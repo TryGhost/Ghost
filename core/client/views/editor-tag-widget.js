56e9c70547cf561d0dece19899158a6452f061a9
Matt Hughes <hughesm02@gmail.com>
2013-12-22 19:24:21 -0500

    Improved comma tag completion on international keyboard layouts
    Reported in issue #1475
    - `String.localeCompare` can be more reliable for keys that do not relate to cursor movement
    - Adds a third key handler (`keypress`) that contains the character code rather than physical key
    - `COMMA` key constant no longer required (unless `,` char should be a constant?)

--- core/client/views/editor-tag-widget.js
+++ core/client/views/editor-tag-widget.js
@@ -10,6 +10,7 @@
         events: {
             'keyup [data-input-behaviour="tag"]': 'handleKeyup',
             'keydown [data-input-behaviour="tag"]': 'handleKeydown',
+            'keypress [data-input-behaviour="tag"]': 'handleKeypress',
             'click ul.suggestions li': 'handleSuggestionClick',
             'click .tags .tag': 'handleTagClick',
             'click .tag-label': 'mobileTags'
@@ -20,7 +21,6 @@
             DOWN: 40,
             ESC: 27,
             ENTER: 13,
-            COMMA: 188,
             BACKSPACE: 8
         },
 
@@ -130,9 +130,7 @@
 
         handleKeyup: function (e) {
             var $target = $(e.currentTarget),
-                searchTerm = $.trim($target.val()),
-                tag,
-                $selectedSuggestion;
+                searchTerm = $.trim($target.val());
 
             if (e.keyCode === this.keys.UP) {
                 e.preventDefault();
@@ -153,28 +151,6 @@
                     }
                 }
             } else if (e.keyCode === this.keys.ESC) {
-                this.$suggestions.hide();
-            } else if ((e.keyCode === this.keys.ENTER || e.keyCode === this.keys.COMMA) && searchTerm) {
-                // Submit tag using enter or comma key
-                e.preventDefault();
-
-                $selectedSuggestion = this.$suggestions.children(".selected");
-                if (this.$suggestions.is(":visible") && $selectedSuggestion.length !== 0) {
-
-                    if ($('.tag:containsExact("' + $selectedSuggestion.data('tag-name') + '")').length === 0) {
-                        tag = {id: $selectedSuggestion.data('tag-id'), name: $selectedSuggestion.data('tag-name')};
-                        this.addTag(tag);
-                    }
-                } else {
-                    if (e.keyCode === this.keys.COMMA) {
-                        searchTerm = searchTerm.replace(/,/g, "");
-                    }  // Remove comma from string if comma is used to submit.
-                    if ($('.tag:containsExact("' + searchTerm + '")').length === 0) {
-                        this.addTag({id: null, name: searchTerm});
-                    }
-                }
-                $target.val('').focus();
-                searchTerm = ""; // Used to reset search term
                 this.$suggestions.hide();
             }
 
@@ -199,6 +175,39 @@
                 lastBlock.remove();
                 tag = {id: lastBlock.data('tag-id'), name: lastBlock.text()};
                 this.model.removeTag(tag);
+            }
+        },
+
+        handleKeypress: function (e) {
+            var $target = $(e.currentTarget),
+                searchTerm = $.trim($target.val()),
+                tag,
+                $selectedSuggestion,
+                isComma = ",".localeCompare(String.fromCharCode(e.keyCode || e.charCode)) === 0;
+
+            // use localeCompare in case of international keyboard layout
+            if ((e.keyCode === this.keys.ENTER || isComma) && searchTerm) {
+                // Submit tag using enter or comma key
+                e.preventDefault();
+
+                $selectedSuggestion = this.$suggestions.children(".selected");
+                if (this.$suggestions.is(":visible") && $selectedSuggestion.length !== 0) {
+
+                    if ($('.tag:containsExact("' + $selectedSuggestion.data('tag-name') + '")').length === 0) {
+                        tag = {id: $selectedSuggestion.data('tag-id'), name: $selectedSuggestion.data('tag-name')};
+                        this.addTag(tag);
+                    }
+                } else {
+                    if (isComma) {
+                        searchTerm = searchTerm.replace(/,/g, "");
+                    }  // Remove comma from string if comma is used to submit.
+                    if ($('.tag:containsExact("' + searchTerm + '")').length === 0) {
+                        this.addTag({id: null, name: searchTerm});
+                    }
+                }
+                $target.val('').focus();
+                searchTerm = ""; // Used to reset search term
+                this.$suggestions.hide();
             }
         },
 
