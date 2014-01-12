3dea6431df899b75197fd44a2106a60b1c71f675
Hannah Wolfe <erisds@gmail.com>
2014-01-12 17:08:12 +0000

    Ensure cookies are only ever set for admin
    fixes #1901
    
    - Adds a trailing slash to the cookie path
    - Resolves random log-outs
    - Adds a test which proves the case

--- core/test/functional/routes/frontend_test.js
+++ core/test/functional/routes/frontend_test.js
@@ -249,6 +249,17 @@ describe('Frontend Routing', function () {
         });
     });
 
+    describe('Post with Ghost in the url', function () {
+        // All of Ghost's admin depends on the /ghost/ in the url to work properly
+        // Badly formed regexs can cause breakage if a post slug starts with the 5 letters ghost
+        it('should retrieve a blog post with ghost at the start of the url', function (done) {
+            request.get('/ghostly-kitchen-sink/')
+                .expect('Cache-Control', cacheRules['public'])
+                .expect(200)
+                .end(doEnd(done));
+        });
+    });
+
     describe('Static assets', function () {
         it('should retrieve shared assets', function (done) {
             request.get('/shared/img/usr-image.png')
