63f6aba0bc0870b302e62ab37fee84585a2873c8
Rafał Gutkowski <goodkowski@gmail.com>
2014-01-16 20:25:19 +0100

    Add name of used template to locales

--- core/server/controllers/frontend.js
+++ core/server/controllers/frontend.js
@@ -88,7 +88,7 @@ frontendControllers = {
                     api.settings.read('activeTheme').then(function (activeTheme) {
                         var paths = config.paths().availableThemes[activeTheme.value],
                             view = post.page && paths.hasOwnProperty('page') ? 'page' : 'post';
-                        res.render(view, {post: post});
+                        res.render(view, {template: view, post: post});
                     });
                 });
             }
