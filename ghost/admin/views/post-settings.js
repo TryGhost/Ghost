197b479394d1942ff4d7230ee7bd6073226f170f
Brad Dougherty <me@brad.is>
2014-02-22 11:53:07 -0500

    Update delete post button labels
    Closes #2027

--- ghost/admin/views/post-settings.js
+++ ghost/admin/views/post-settings.js
@@ -330,13 +330,15 @@
                                         });
                                     });
                                 },
-                                text: "Yes"
+                                text: "Delete",
+                                buttonClass: "button-delete"
                             },
                             reject: {
                                 func: function () {
                                     return true;
                                 },
-                                text: "No"
+                                text: "Cancel",
+                                buttonClass: "button"
                             }
                         },
                         type: "action",
