667888aeb359920cd4937567da20d87dcf3267da
Gabor Javorszky <gabor@javorszky.co.uk>
2014-02-23 12:32:35 +0000

    Implements Initial lifecycle and App UI start
    Closes #2083
    
    * Added hbs template for apps listing
    * Added settings to read the activeApps
    * Added viewcontrol to activate / deactivate apps
    * Added API handler to store activeApps (by `name` in the `package.json` file)
    * On button click it turns the button into "Working" and changes class to `button` (grey one)
    * On success, rerenders the pane, adds success notification about apps being saved
    * On error, rerenders the pane, adds error notification with error message
    
    Missing:
    * tests: couldn't figure out how to add mock apps with mock package.json data
    * actually registering, etc, re #2140
    * icon from the sidebar

--- core/client/models/settings.js
+++ core/client/models/settings.js
@@ -3,7 +3,7 @@
     'use strict';
     //id:0 is used to issue PUT requests
     Ghost.Models.Settings = Ghost.ProgressModel.extend({
-        url: Ghost.paths.apiRoot + '/settings/?type=blog,theme',
+        url: Ghost.paths.apiRoot + '/settings/?type=blog,theme,app',
         id: '0'
     });
 
--- core/server/controllers/admin.js
+++ core/server/controllers/admin.js
@@ -89,7 +89,7 @@ adminControllers = {
     // Method: GET
     'settings': function (req, res, next) {
         // TODO: Centralise list/enumeration of settings panes, so we don't run into trouble in future.
-        var allowedSections = ['', 'general', 'user'],
+        var allowedSections = ['', 'general', 'user', 'app'],
             section = req.url.replace(/(^\/ghost\/settings[\/]*|\/$)/ig, '');
 
         if (allowedSections.indexOf(section) < 0) {
