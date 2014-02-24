aa557427cbe8d4712b70a8402b1d90f73a346bf0
Harry Wolff <hswolff@gmail.com>
2014-02-24 11:02:30 -0500

    Add inline documentation

--- core/client/views/angular-demo.js
+++ core/client/views/angular-demo.js
@@ -2,8 +2,28 @@
 (function () {
     "use strict";
 
+    // Here we're defining an Angular module.  This is a namespace under which
+    // all relevant code resides.
+    //
+    // Because Angular uses dependency injection as its way of instantiating
+    // code every available dependency must be registered in a module.
+    //
+    // The syntax for defining a module is
+    // angular.module('moduleName', ['array', 'of', 'module', 'dependencies']);
+    //
+    // The syntax for getting a module is angular.module('moduleName') - no second argument.
     angular.module('ghost', []);
 
+    // On an angular module you can define all code that resides there.
+    // For a little more thorough introduction please read my blog
+    // post I wrote about every type of code you can use in an
+    // Angular application: http://blog.harrywolff.com/what-is-angularjs-and-why-is-it-awesome/
+
+    // Here we're defining an Angular Service.  A service is a singleton object
+    // that is instantiated when your Angular application starts.
+    // Everything defined on the function in the 2nd argument is exposed.
+    // This also gives you the ability to easily create private variables
+    // as you can see with my usage of `var activePost;`
     angular.module('ghost').service('BackboneData', function() {
 
         var activePost;
@@ -17,29 +37,75 @@
         };
     });
 
+    // Here we're defining an Angular Directive.
+    // The important concept to understand about a directive is Angular's HTML Compiler.
+    // When an AnuglarJS application starts up it compiles all HTML that it is given.
+    // The Angular HTML Compiler can be programmed to do whatever you want.
+    // An Angular Directive is Angular's way of letting you program Angular's HTML Compiler.
+    //
+    // Here we're defining a 'postSettingsMenu' element.  This definition is given
+    // to Angular's HTML Compiler.  So now when it encounters a <post-settings-menu>
+    // element it knows how it should behave.
+    //
+    // The sole aim of a directive is to define how how the HTML Compiler should
+    // be programmed.
     angular.module('ghost').directive('postSettingsMenu', function() {
         var parseDateFormats = ["DD MMM YY HH:mm", "DD MMM YYYY HH:mm", "DD/MM/YY HH:mm", "DD/MM/YYYY HH:mm",
                 "DD-MM-YY HH:mm", "DD-MM-YYYY HH:mm", "YYYY-MM-DD HH:mm"],
             displayDateFormat = 'DD MMM YY @ HH:mm';
 
         return {
+            // Restricting the HTML Compiler to only recgonize postSettingsMenu
+            // directives as an element.
+            // Were we to include 'A' the HTML Compiler would also recognize
+            // if postSettingsMenu was used as an attribute, like so:
+            // <div class="blah" post-settings-menu>.
             restrict: 'E',
             templateUrl: '/angular/templates/post-settings-menu.html',
+            // This tells the HTML Compiler to replace the entire element
+            // with the template contents.
             replace: true,
+            // This scope decleration is making use of the way
+            // Angular handles scope throughout an Angular application.
+            // Here we tell the HTML Compiler that we want to create
+            // an isolated scope for this directive, i.e.
+            // that no properties that exist 'above' this element
+            // should be accessible to this directive.
             scope: {
                 klasses: '@class'
             },
+            // An AngularJS controller is where you define what data
+            // is accessible in the HTML.
+            // Everything you define as an argument of the controller is inflated
+            // on instantiated of the controller, due to AngularJS dependency injection
+            // i.e. when this controller is created Angular fetches an instance of $scope
+            // and the BackboneData service and passes it into the controller for you.
+            //
+            // The $scope object is special in that everything that is defined on that object
+            // is accessible in the HTML.
             controller: function($scope, BackboneData) {
 
                 var post;
 
                 $scope.state = {};
 
-                /*
-                    We see when the post has been set,
-                    and when it is we assign it to our $scope object
-                    which is accessible in the template.
-                 */
+                // The $scope object has a few special functions
+                // available on it, one of which is $watch.
+                // This function allows you to register an event listener.
+                //
+                // During every cycle of an Angular application
+                // all $watch functions are run.
+                //
+                // The first argument is a function where you define
+                // what property you're looking to see if it changed.
+                // In this case we want to know when the `activePost`
+                // is changed.
+                //
+                // When it changes we are passed the new value in the 2nd
+                // function where we can do whatever we want with it.
+                //
+                // In this case we check to see if a new activePost has been set
+                // and if so then we attach it to the $scope so the HTML can access it.
                 $scope.$watch(BackboneData.getPost, function(p) {
                     post = p;
 
@@ -63,13 +129,9 @@
                 });
 
 
-                /*
-                    Save Post
-
-                    Called by all input fields when their data changes.
-
-                    It's properly debounced.
-                 */
+                // Here we define a function on the $scope object
+                // which is accessible in the HTML template.
+                // We wrap it in a debounce function for better behavior.
                 $scope.savePost = _.debounce(function() {
                     console.log('Properly debounced');
 
@@ -99,9 +161,7 @@
 
                 }, 300);
 
-                /*
-                    Delete post
-                 */
+                // Another function exposed to the HTML template.
                 $scope.deletePost = function() {
                     alert('Delete post');
                 };
@@ -111,15 +171,25 @@
 
 
 
-    // Helpers to bridge angular and Backbone
-
+    // This is not specific to Angular, it's a bridge I created between the
+    // Backbone world and the new Angular world.
+    // It uses some advanced Angular concepts.
     angular.ghost = {
         injector: function() {
+            // Here we're accessing the Angular application from the outside.
             return angular.element(document.getElementsByTagName('html')[0]).injector();
         },
         setPost: function (post) {
+            // The invoke() function is how AngularJS' dependency injection works.
+            // You pass it in a function with the dependencies defined as arguments
+            // and Angular's invoke function will hand that function those dependencies.
             angular.ghost.injector().invoke(function(BackboneData, $rootScope) {
               BackboneData.setPost(post);
+              // Because we are modifying the Angular application externally
+              // we need to explicitly tell it that changes have occured
+              // and that it should commence with a new loop of its run time.
+              // $digest() is what lets Angular know that it should run
+              // all $watch functions.
               $rootScope.$digest();
             });
         }
