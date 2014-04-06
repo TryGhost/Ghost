76ad6cb9bc1b6de3afb1c93b5b89a104db0d40ec
Harry Wolff <hswolff@gmail.com>
2014-04-06 18:11:22 -0400

    Create GhostPopover component
    issue #2418
    
    - create ghost-popover component and template
    - use it for the navigation popover
    - still needs to handle global click event on the document
    to close the popover

--- ghost/admin/controllers/application.js
+++ ghost/admin/controllers/application.js
@@ -1,5 +1,10 @@
 var ApplicationController = Ember.Controller.extend({
-    isLoggedOut: Ember.computed.match('currentPath', /(signin|signup|forgotten|reset)/)
+    isLoggedOut: Ember.computed.match('currentPath', /(signin|signup|forgotten|reset)/),
+    actions: {
+        toggleMenu: function () {
+            this.toggleProperty('showMenu');
+        }
+    }
 });
 
 export default ApplicationController;
