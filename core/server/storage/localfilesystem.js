e3056990cd403e94d5b36fdf585fc37c5e99c556
Jason Williams <jaswilli@gmail.com>
2014-04-06 23:13:29 +0000

    Build correct path on image upload when config has custom content path
    closes #2563
    - build path relative to imagesPath (which contains the custom content path)
      instead of appRoot
    - added test for custom content path
    - added logic to tests for Windows url building to handle cases where Windows
      functionality is being tested on a unix operating system

--- core/server/storage/localfilesystem.js
+++ core/server/storage/localfilesystem.js
@@ -33,7 +33,7 @@ localFileStore = _.extend(baseStore, {
         }).then(function () {
             // The src for the image must be in URI format, not a file system path, which in Windows uses \
             // For local file system storage can use relative path so add a slash
-            var fullUrl = (config().paths.subdir + '/' + path.relative(config().paths.appRoot, targetFilename)).replace(new RegExp('\\' + path.sep, 'g'), '/');
+            var fullUrl = (config().paths.subdir + '/' + config().paths.imagesRelPath + '/' + path.relative(config().paths.imagesPath, targetFilename)).replace(new RegExp('\\' + path.sep, 'g'), '/');
             return saved.resolve(fullUrl);
         }).otherwise(function (e) {
             errors.logError(e);
