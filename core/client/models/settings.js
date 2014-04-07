266aed8d4c76b683c40c32b5952f03d5626ef332
Peter Szel <peter_szel@epam.com>
2014-04-08 00:01:46 +0200

    Ported logic to settings: Debug Tab closes #2424
    - Added file upload component
    - Added import client logic
    - Added e-mail sending client logic
    - Added settings model

--- core/client/models/settings.js
+++ core/client/models/settings.js
@@ -2,7 +2,7 @@ var validator = window.validator;
 
 import BaseModel from 'ghost/models/base';
 
-export default BaseModel.extend({
+var SettingsModel = BaseModel.extend({
     url: BaseModel.apiRoot + '/settings/?type=blog,theme,app',
 
     title: null,
@@ -46,5 +46,31 @@ export default BaseModel.extend({
         }
 
         return validationErrors;
+    },
+    exportPath: BaseModel.adminRoot + '/export/',
+    importFrom: function (file) {
+        var formData = new FormData();
+        formData.append('importfile', file);
+        return ic.ajax.request(BaseModel.apiRoot + '/db/', {
+            headers: {
+                'X-CSRF-Token': $('meta[name="csrf-param"]').attr('content')
+            },
+            type: 'POST',
+            data: formData,
+            dataType: 'json',
+            cache: false,
+            contentType: false,
+            processData: false
+        });
+    },
+    sendTestEmail: function () {
+        return ic.ajax.request(BaseModel.apiRoot + '/mail/test/', {
+            type: 'POST',
+            headers: {
+                'X-CSRF-Token': $('meta[name="csrf-param"]').attr('content')
+            }
+        });
     }
 });
+
+export default SettingsModel;
