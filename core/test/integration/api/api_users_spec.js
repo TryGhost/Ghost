d9d01e4971b44429fd8f4e13f74d6e0fe4a785d8
Sebastian Gierlinger <s.gierlinger@me.com>
2014-04-10 17:56:56 +0200

    Added and fixed permission tests
    - fixed test for db functions
    - added tests for different users

--- core/test/integration/api/api_users_spec.js
+++ core/test/integration/api/api_users_spec.js
@@ -16,13 +16,15 @@ describe('Users API', function () {
     });
 
     beforeEach(function (done) {
-        testUtils.initData()
-            .then(function () {
-                return testUtils.insertDefaultFixtures();
-            })
-            .then(function () {
-                done();
-            }, done);
+        testUtils.initData().then(function () {
+            return testUtils.insertDefaultFixtures();
+        }).then(function () {
+            return testUtils.insertEditorUser();
+        }).then(function () {
+            return testUtils.insertAuthorUser();
+        }).then(function () {
+            done();
+        }, done);
     });
 
     afterEach(function (done) {
@@ -31,16 +33,77 @@ describe('Users API', function () {
         }, done);
     });
 
-    it('can browse', function (done) {
+    it('browse', function (done) {
         permissions.init().then(function () {
-            return UsersAPI.browse.call({user:1})
+            return UsersAPI.browse.call({user: 1});
+        }).then(function (results) {
+            should.exist(results);
+            results.length.should.be.above(0);
+            testUtils.API.checkResponse(results[0], 'user');
+        }, function (error) {
+            done(new Error(JSON.stringify(error)));
+        }).then(function () {
+            return UsersAPI.browse.call({user: 2});
+        }).then(function (results) {
+            should.exist(results);
+            results.length.should.be.above(0);
+            testUtils.API.checkResponse(results[0], 'user');
+        }, function (error) {
+            done(new Error(JSON.stringify(error)));
+        }).then(function () {
+            return UsersAPI.browse.call({user: 3});
         }).then(function (results) {
             should.exist(results);
             results.length.should.be.above(0);
             testUtils.API.checkResponse(results[0], 'user');
             done();
-        }).otherwise(function () {
+        }, function (error) {
+            done(new Error(JSON.stringify(error)));
+        })
+    });
+    it('browse denied', function (done) {
+        permissions.init().then(function () {
+            return UsersAPI.browse();
+        }).then(function (results) {
+            done(new Error("Browse user is not denied without authentication."));
+        }, function () {
             done();
+        });
+    });
+    it('read', function (done) {
+        permissions.init().then(function () {
+            return UsersAPI.read.call({user: 1}, {id: 1});
+        }).then(function (result) {
+            should.exist(result);
+            result.id.should.eql(1);
+            testUtils.API.checkResponse(result, 'user');
+        }, function (error) {
+            done(new Error(JSON.stringify(error)));
+        }).then(function () {
+            return UsersAPI.read.call({user: 2}, {id: 1});
+        }).then(function (result) {
+            should.exist(result);
+            result.id.should.eql(1);
+            testUtils.API.checkResponse(result, 'user');
+        }, function (error) {
+            done(new Error(JSON.stringify(error)));
+        }).then(function () {
+            return UsersAPI.read.call({user: 3}, {id: 1});
+        }).then(function (result) {
+            should.exist(result);
+            result.id.should.eql(1);
+            testUtils.API.checkResponse(result, 'user');
+        }, function (error) {
+            done(new Error(JSON.stringify(error)));
+        }).then(function () {
+            return UsersAPI.read({id: 1});
+        }).then(function (result) {
+            should.exist(result);
+            result.id.should.eql(1);
+            testUtils.API.checkResponse(result, 'user');
+            done();
+        }, function (error) {
+            done(new Error(JSON.stringify(error)));
         });
     });
 });
