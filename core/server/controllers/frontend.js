7a57a98b1017951bc32d729b3d3e6f297a95b340
Steve <steve@shindakun.net>
2014-04-19 21:48:14 -0700

    Do not redirect to editor if parameter does not equal edit.
    Closes #2619
    - If edit parameter is 'edit' redirect to editor.
    - If edit parameter is anything other then undefined redirect to 404.
    - Create edit post tests.
    - Test redirect without trailing slash.
    - Test redirect to editor.
    - Test redirect to 404.

--- core/server/controllers/frontend.js
+++ core/server/controllers/frontend.js
@@ -183,8 +183,11 @@ frontendControllers = {
 
             function render() {
                 // If we're ready to render the page but the last param is 'edit' then we'll send you to the edit page.
-                if (params.edit !== undefined) {
+                if (params.edit === 'edit') {
                     return res.redirect(config().paths.subdir + '/ghost/editor/' + post.id + '/');
+                } else if (params.edit !== undefined) {
+                    // Use throw 'no match' to show 404.
+                    throw new Error('no match');
                 }
                 filters.doFilter('prePostsRender', post).then(function (post) {
                     api.settings.read('activeTheme').then(function (activeTheme) {
