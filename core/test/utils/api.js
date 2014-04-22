4e64344988331147e7ac5e4445c573d5b43023c9
Alan Richards <alarobric@gmail.com>
2014-04-21 18:08:11 -0700

    Post API response to use author not author_id
    Closes #2608
    - added toJSON method override for post model
    - in the event no expanded author relation is present the id will be used
    - removed author_id from expected response JSON for posts.
    - updated integration tests to check for existence or not of author and author_id

--- core/test/utils/api.js
+++ core/test/utils/api.js
@@ -8,7 +8,7 @@ var _ = require('lodash'),
         posts: ['posts', 'meta'],
         pagination: ['page', 'limit', 'pages', 'total', 'next', 'prev'],
         post: ['id', 'uuid', 'title', 'slug', 'markdown', 'html', 'meta_title', 'meta_description',
-            'featured', 'image', 'status', 'language', 'author_id', 'created_at', 'created_by', 'updated_at',
+            'featured', 'image', 'status', 'language', 'created_at', 'created_by', 'updated_at',
             'updated_by', 'published_at', 'published_by', 'page', 'author', 'tags', 'fields'],
         // TODO: remove databaseVersion, dbHash
         settings: ['databaseVersion', 'dbHash', 'title', 'description', 'email', 'logo', 'cover', 'defaultLang',
