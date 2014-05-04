4d41e12c02b5cc1aa48949519581a2fd0c21ce56
Jason Williams <jaswilli@gmail.com>
2014-05-04 01:30:30 +0000

    Adjust update check to handle changes in API calls
    closes #2681
    - fixed api calls and response parsing
    - added a test to for update check
    - adjusted gruntfile.js to run new update check integration test

---
+++ core/test/integration/update_check_spec.js
@@ -0,0 +1,70 @@
+var should = require('should');
+var when = require('when');
+var rewire = require('rewire');
+var packageInfo = require('../../../package');
+var ghost = require('../../../core');
+var permissions = require('../../server/permissions');
+var testUtils = require('../utils');
+var updateCheck = rewire('../../server/update-check');
+
+describe('Update Check', function () {
+    var environmentsOrig;
+
+    before(function (done) {
+        environmentsOrig = updateCheck.__get__('allowedCheckEnvironments');
+        updateCheck.__set__('allowedCheckEnvironments', ['development', 'production', 'testing']);
+
+        ghost().then(function () {
+            return testUtils.clearData();
+        }).then(function () {
+            done();
+        }, done);
+    });
+
+    after(function () {
+        updateCheck.__set__('allowedCheckEnvironments', environmentsOrig);
+    });
+
+    beforeEach(function (done) {
+        testUtils.initData().then(function () {
+            return testUtils.insertDefaultFixtures();
+        }).then(function () {
+            return testUtils.insertEditorUser();
+        }).then(function () {
+            return testUtils.insertAuthorUser();
+        }).then(function () {
+            return permissions.init();
+        }).then(function () {
+            done();
+        }, done);
+    });
+
+    afterEach(function (done) {
+        testUtils.clearData().then(function () {
+            done();
+        }, done);
+    });
+
+    it('should report the correct data', function (done) {
+        var updateCheckData = updateCheck.__get__('updateCheckData');
+
+        updateCheckData().then(function (data) {
+            data.should.exist;
+            data.ghost_version.should.equal(packageInfo.version);
+            data.node_version.should.equal(process.versions.node);
+            data.env.should.equal(process.env.NODE_ENV);
+            data.database_type.should.match(/sqlite3|pg|mysql/);
+            data.blog_id.should.be.a.string;
+            data.blog_id.should.not.be.empty;
+            data.theme.should.be.equal('casper');
+            data.apps.should.be.a.string;
+            data.blog_created_at.should.be.a.number;
+            data.user_count.should.be.above(0);
+            data.post_count.should.be.above(0);
+            data.npm_version.should.be.a.string;
+            data.npm_version.should.not.be.empty;
+
+            done();
+        }).otherwise(done);
+    });
+});
