9d0f875fe23f19ad8676dfaac4d0d61d37115a17
fabfou <fabrice.clementz@neuf.fr>
2014-05-14 22:47:05 +0200

    Move debug page from /settings/debug to /debug
    closes #2743
    - remove settings/debug route and associated files
    - reimplementation at debug route

--- core/client/routes/debug.js
+++ core/client/routes/debug.js
@@ -1,6 +1,12 @@
-var DebugRoute = Ember.Route.extend({
-    beforeModel: function () {
-        this.transitionTo('settings.debug');
+import styleBody from 'ghost/mixins/style-body';
+import SettingsModel from 'ghost/models/settings';
+
+var settingsModel = SettingsModel.create();
+
+var DebugRoute = Ember.Route.extend(styleBody, {
+    classNames: ['settings'],
+    model: function () {
+        return settingsModel;
     }
 });
 
