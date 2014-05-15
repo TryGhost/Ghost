0229a127fea80b4bb9b1d8345bfab0767d277437
Jacob Gable <jacob.gable@gmail.com>
2014-05-14 20:24:49 -0500

    Implement signup in Ember
    Closes #2410
    
    - Add signup action that posts to signup endpoint
    - Fix nav bar showing on signup page
    - Fix image link when a user hasn't set their image yet
    - Redirect to the ember/signin page if requesting an ember page

--- ghost/admin/controllers/application.js
+++ ghost/admin/controllers/application.js
@@ -1,5 +1,6 @@
 var ApplicationController = Ember.Controller.extend({
     isSignedIn: Ember.computed.bool('user.isSignedIn'),
+    hideNav: Ember.computed.match('currentPath', /(signin|signup|forgotten|reset)/),
 
     actions: {
         toggleMenu: function () {
