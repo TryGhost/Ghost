1947d79e417e8982579fe5ca66cb53d7ba471929
Lucas Holmquist <lholmqui@redhat.com>
2014-06-04 11:37:18 -0400

    [WIP] - better upload component?

--- core/client/components/gh-file-upload.js
+++ core/client/components/gh-file-upload.js
@@ -2,16 +2,45 @@ var FileUpload = Ember.Component.extend({
     _file: null,
     uploadButtonText: 'Text',
     uploadButtonDisabled: true,
-    change: function (event) {
-        this.set('uploadButtonDisabled', false);
-        this.sendAction('onAdd');
-        this._file = event.target.files[0];
+    autoUpload: true,
+    acceptEncoding: 'image/*',
+    didInsertElement: function () {
+        var self = this;
+
+        this.$('input').fileupload({
+            url: '/ghost/upload/',
+            headers: {
+                'X-CSRF-Token': $('meta[name=\'csrf-param\']').attr('content')
+            },
+            add: function (e, data) {
+                data.submit();
+            },
+            progresAll: function (e, data) {
+                console.log('progressAll', e, data);
+            },
+            fail: function (e, data) {
+                self.sendAction('onUploadError', data.errorThrown);
+            },
+            done: function (e, data) {
+                self.sendAction('onUploadSuccess', data);
+            }
+        });
     },
+    // change: function (event) {
+    //     this.set('uploadButtonDisabled', false);
+    //     this._file = event.target.files[0];
+
+    //     if (this.autoUpload) {
+    //         this.send('upload');
+    //     }
+    // },
     actions: {
         upload: function () {
-            var self = this;
-            if (!this.uploadButtonDisabled && self._file) {
-                self.sendAction('onUpload', self._file);
+            //@TODO: add progress bar back in
+            //@TODO: add in drag and drop
+
+            if (!this.uploadButtonDisabled && this._file) {
+                this.sendAction('onUpload', this._file);
             }
 
             // Prevent double post by disabling the button.
