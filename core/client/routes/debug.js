1ccdce45f36c3705c2de7be10c7dcfa496d1deb8
Maurice Williams <morficus@gmail.com>
2014-06-04 23:18:23 -0400

    Re-implementing the loading indicator for the Ember admin
    closes #2855 , closes #2848
    - New  mixin that utilizes NProgress for displaying a loading indictor for all routes who's model issue a "loading" event (aka: when requesting data from the server during a route change).
    - Also removing (the now unnecessary) "loading" template.

--- core/client/routes/debug.js
+++ core/client/routes/debug.js
@@ -1,8 +1,9 @@
 import styleBody from 'ghost/mixins/style-body';
 import AuthenticatedRoute from 'ghost/routes/authenticated';
 import SettingsModel from 'ghost/models/settings';
+import loadingIndicator from 'ghost/mixins/loading-indicator';
 
-export default AuthenticatedRoute.extend(styleBody, {
+export default AuthenticatedRoute.extend(styleBody, loadingIndicator, {
     classNames: ['settings'],
 
     model: function () {
--- core/client/routes/settings/general.js
+++ core/client/routes/settings/general.js
@@ -1,6 +1,7 @@
 import AuthenticatedRoute from 'ghost/routes/authenticated';
+import loadingIndicator from 'ghost/mixins/loading-indicator';
 
-var SettingsGeneralRoute = AuthenticatedRoute.extend({
+var SettingsGeneralRoute = AuthenticatedRoute.extend(loadingIndicator, {
     model: function () {
         return this.store.find('setting', { type: 'blog,theme' }).then(function (records) {
             return records.get('firstObject');
