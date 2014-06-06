ccc015036901867675df593ac54d0d8094d72e31
Lucas Holmquist <lholmqui@redhat.com>
2014-06-06 10:44:09 -0400

    Ember Admin - Upload Modal
    Issue #2547

--- core/client/routes/application.js
+++ core/client/routes/application.js
@@ -24,12 +24,14 @@ var ApplicationRoute = Ember.Route.extend({
             this.set('controller.user', null);
         },
 
-        openModal: function (modalName, model) {
+        openModal: function (modalName, model, type) {
             modalName = 'modals/' + modalName;
             // We don't always require a modal to have a controller
             // so we're skipping asserting if one exists
             if (this.controllerFor(modalName, true)) {
                 this.controllerFor(modalName).set('model', model);
+                this.controllerFor(modalName).set('imageType', type);
+                this.controllerFor(modalName).set('src', model.get(type));
             }
             return this.render(modalName, {
                 into: 'application',
