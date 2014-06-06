caf910dea0fe9fc66d383616f7081bf5f91ce306
Lucas Holmquist <lholmqui@redhat.com>
2014-06-06 10:44:09 -0400

    Ember Admin - Upload Modal
    Issue #2547

--- core/client/components/gh-upload-modal.js
+++ core/client/components/gh-upload-modal.js
@@ -1,17 +1,36 @@
-/*global console */
-
 import ModalDialog from 'ghost/components/gh-modal-dialog';
+import upload from 'ghost/assets/lib/uploader';
 
 var UploadModal = ModalDialog.extend({
     layoutName: 'components/gh-modal-dialog',
 
     didInsertElement: function () {
         this._super();
+        var filestorage = $('#general').data('filestorage');
+        upload.call(this.$('.js-drop-zone'), {fileStorage: filestorage});
+    },
+    confirm: {
+        reject: {
+            func: function () { // The function called on rejection
+                return true;
+            },
+            buttonClass: true,
+            text: 'Cancel' // The reject button text
+        },
+        accept: {
+            buttonClass: 'button-save right',
+            text: 'Save', // The accept button texttext: 'Save'
+            func: function () {
+                var imageType = 'model.' + this.get('imageType');
 
-        // @TODO: get this real
-        console.log('UploadController:afterRender');
-        // var filestorage = $('#' + this.options.model.id).data('filestorage');
-        // this.$('.js-drop-zone').upload({fileStorage: filestorage});
+                if (this.$('.js-upload-url').val()) {
+                    this.set(imageType, this.$('.js-upload-url').val());
+                } else {
+                    this.set(imageType, this.$('.js-upload-target').attr('src'));
+                }
+                return true;
+            }
+        }
     },
 
     actions: {
@@ -21,12 +40,11 @@ var UploadModal = ModalDialog.extend({
         confirm: function (type) {
             var func = this.get('confirm.' + type + '.func');
             if (typeof func === 'function') {
-                func();
+                func.apply(this);
             }
             this.sendAction();
         }
-    },
-
+    }
 });
 
 export default UploadModal;
