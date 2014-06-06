b518e9e7a39073843935725f24a023ec9278774b
Jason Williams <jaswilli@gmail.com>
2014-06-06 04:04:49 +0000

    Close post settings popover menu after delete post
    No issue
    -inject popover:service into modal component delete post
     controller so popover close can be triggered as part of
     the delete action
    -remove unnecessary 'needs' from the delete post controller

--- core/client/controllers/modals/delete-post.js
+++ core/client/controllers/modals/delete-post.js
@@ -1,10 +1,10 @@
 var DeletePostController = Ember.Controller.extend({
-    needs: 'posts/post',
     actions: {
         confirmAccept: function () {
             var self = this;
 
             this.get('model').destroyRecord().then(function () {
+                self.get('popover').closePopovers();
                 self.notifications.showSuccess('Your post has been deleted.');
                 self.transitionToRoute('posts.index');
             }, function () {
