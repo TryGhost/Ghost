7c6b67f3f0688e9ddfad1d7cf240c49c4ff1d213
Jason Williams <jaswilli@gmail.com>
2014-06-16 22:54:21 +0000

    Show notification on successful sign out
    Closes #2977

--- core/client/routes/signout.js
+++ core/client/routes/signout.js
@@ -15,7 +15,7 @@ var SignoutRoute = AuthenticatedRoute.extend(styleBody, {
                 'X-CSRF-Token': this.get('csrf')
             }
         }).then(function () {
-
+            self.notifications.showSuccess('You were successfully signed out.');
             // @TODO: new CSRF token to enable logging back in w/o refreshing - see issue #2861 for details
             self.transitionTo('signin');
         }, function (resp) {
