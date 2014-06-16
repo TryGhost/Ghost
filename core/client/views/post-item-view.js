c649d239368cc2e64182f60a1784f09eedab91ec
Fabian Becker <halfdan@xnorfz.de>
2014-06-16 05:44:07 +0000

    Content screen: Featured posts
    fixes #2748
    - Properly handle marking post as featured/non-featured
    - Show correct notifications

--- core/client/views/post-item-view.js
+++ core/client/views/post-item-view.js
@@ -1,6 +1,14 @@
 import itemView from 'ghost/views/item-view';
 
 var PostItemView = itemView.extend({
+    classNameBindings: ['isFeatured'],
+
+    isFeatured: function () {
+        if (this.get('controller.model.featured')) {
+            return 'featured';
+        }
+    }.property('controller.model.featured'),
+
     openEditor: function () {
         this.get('controller').send('openEditor', this.get('controller.model'));  // send action to handle transition to editor route
     }.on('doubleClick')
