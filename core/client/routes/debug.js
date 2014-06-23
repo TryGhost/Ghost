b66aa46db976546cd16956c84497c2a66bfffb7a
Jason Williams <jaswilli@gmail.com>
2014-06-23 14:24:37 +0000

    Finish Debug screen for Ember admin
    Closes #2847

--- core/client/routes/debug.js
+++ core/client/routes/debug.js
@@ -1,12 +1,15 @@
 import styleBody from 'ghost/mixins/style-body';
 import AuthenticatedRoute from 'ghost/routes/authenticated';
-import SettingsModel from 'ghost/models/settings';
 import loadingIndicator from 'ghost/mixins/loading-indicator';
 
-export default AuthenticatedRoute.extend(styleBody, loadingIndicator, {
+var DebugRoute = AuthenticatedRoute.extend(styleBody, loadingIndicator, {
     classNames: ['settings'],
 
     model: function () {
-        return SettingsModel.create();
+        return this.store.find('setting', { type: 'blog,theme' }).then(function (records) {
+            return records.get('firstObject');
+        });
     }
 });
+
+export default DebugRoute;
