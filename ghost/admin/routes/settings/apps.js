725e00442d6a2f3821aa50cc59b549ab092a808c
Hannah Wolfe <erisds@gmail.com>
2014-06-23 17:01:43 +0100

    Hide settings/apps unless apps:true is in config.js
    fixes #3031
    
    - Adds an initializer for passing config to the frontend, it's not pretty but it works
    - Forwards the apps route and hides the apps menu item if apps:true is not present in config.js

--- ghost/admin/routes/settings/apps.js
+++ ghost/admin/routes/settings/apps.js
@@ -1,6 +1,11 @@
 import AuthenticatedRoute from 'ghost/routes/authenticated';
 
 var AppsRoute = AuthenticatedRoute.extend({
+    beforeModel: function () {
+        if (!this.get('config.apps')) {
+            this.transitionTo('settings.general');
+        }
+    },
     model: function () {
         return this.store.find('app');
     }
