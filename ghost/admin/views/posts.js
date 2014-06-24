54eda5c03ce6015a1d5f04b76439d0bc7ddefdfc
David Arvelo <david@davidarvelo.com>
2014-06-24 02:19:20 -0400

    Mobile Interactions
    closes #3018
    - split clientold mobile interactions into their respective Ember routes/views
    - create PostsView and SettingsView with mobile interactions
    - place interactions for the publish bar into ApplicationRoute on init

---
+++ ghost/admin/views/posts.js
@@ -0,0 +1,30 @@
+import mobileUtils from 'ghost/utils/mobile-utils';
+
+var PostsView = Ember.View.extend({
+    classNames: ['content-view-container'],
+    tagName: 'section',
+
+    mobileInteractions: function () {
+        var responsiveAction = mobileUtils.responsiveAction;
+
+        Ember.run.scheduleOnce('afterRender', this, function () {
+            // ### Show content preview when swiping left on content list
+            $('.manage').on('click', '.content-list ol li', function (event) {
+                responsiveAction(event, '(max-width: 800px)', function () {
+                    $('.content-list').animate({right: '100%', left: '-100%', 'margin-right': '15px'}, 300);
+                    $('.content-preview').animate({right: '0', left: '0', 'margin-left': '0'}, 300);
+                });
+            });
+
+            // ### Hide content preview
+            $('.manage').on('click', '.content-preview .button-back', function (event) {
+                responsiveAction(event, '(max-width: 800px)', function () {
+                    $('.content-list').animate({right: '0', left: '0', 'margin-right': '0'}, 300);
+                    $('.content-preview').animate({right: '-100%', left: '100%', 'margin-left': '15px'}, 300);
+                });
+            });
+        });
+    }.on('didInsertElement'),
+});
+
+export default PostsView;
