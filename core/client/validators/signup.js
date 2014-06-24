c577376717b4ab13de59f03e6ddc4f34f48c2d06
Jacob Gable <jacob.gable@gmail.com>
2014-06-23 19:47:51 -0500

    Add validation for signin/signup
    Closes #2976, Closes #3017
    
    - Move logic to signup controller
      - Add ValidationEngine mixin to signup controller
      - Add signup validator with code from clientold login view
    - Add signin validator and integrate into signin controller
    - Add validation to forgotten controller
      - Switch to button action to support hitting enter in text field to submit
    - Clear all notifications in notifications.closeAll
    - Modify ValidationEngine.validate to not format errors based on option
    - Update casper test for signin to wait for notification before trying
    to do another signin

---
+++ core/client/validators/signup.js
@@ -0,0 +1,28 @@
+var SignupValidator = Ember.Object.create({
+    validate: function (model) {
+        var data = model.getProperties('name', 'email', 'password'),
+            validationErrors = [];
+        
+        if (!validator.isLength(data.name || '', 1)) {
+            validationErrors.push({
+                message: 'Please enter a name.'
+            });
+        }
+
+        if (!validator.isEmail(data.email)) {
+            validationErrors.push({
+                message: 'Invalid Email.'
+            });
+        }
+
+        if (!validator.isLength(data.password || '', 1)) {
+            validationErrors.push({
+                message: 'Please enter a password.'
+            });
+        }
+
+        return validationErrors;
+    }
+});
+
+export default SignupValidator;
