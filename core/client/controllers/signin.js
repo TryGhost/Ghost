a7f1173f932bfc0f388de654e9d1122566aeee8c
David Arvelo <david@davidarvelo.com>
2014-06-30 16:07:30 -0400

    Signin Validation
    closes #3120
    - create `validateAndAuthenticate` action to validate, send `authenticate` action on success, notifications on error
    - signin template uses `validateAndAuthenticate` action instead of `authenticate` action

--- core/client/controllers/signin.js
+++ core/client/controllers/signin.js
@@ -1,5 +1,21 @@
-var SigninController = Ember.Controller.extend(Ember.SimpleAuth.LoginControllerMixin, {
+import ValidationEngine from 'ghost/mixins/validation-engine';
+
+var SigninController = Ember.Controller.extend(Ember.SimpleAuth.LoginControllerMixin, ValidationEngine, {
     authenticatorFactory: 'ember-simple-auth-authenticator:oauth2-password-grant',
+
+    validationType: 'signin',
+
+    actions: {
+        validateAndAuthenticate: function () {
+            var self = this;
+
+            this.validate({ format: false }).then(function () {
+                self.send('authenticate');
+            }).catch(function (errors) {
+                self.notifications.showErrors(errors);
+            });
+        }
+    }
 });
 
 export default SigninController;
