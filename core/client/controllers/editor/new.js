c6d9fdfe0629a468554d72de121249ed13d6a26b
Jason Williams <jaswilli@gmail.com>
2014-07-02 03:34:46 +0000

    Prevent stacking notifications during rapid toggle
    No issue
    -Call notifications.closePassive after the resolution of the
     promise that generates the notifications. Otherwise multiple
     promises can stack up after notifications have been cleared,
     which results in a bunch of stacked notifications.
    -Remove some unnecessary propagation of rejected promises from
     action handlers that can result in unhandled reject errors.

--- core/client/controllers/editor/new.js
+++ core/client/controllers/editor/new.js
@@ -11,7 +11,6 @@ var EditorNewController = Ember.ObjectController.extend(EditorControllerMixin, {
                 if (model.get('id')) {
                     self.transitionToRoute('editor.edit', model);
                 }
-                return model;
             });
         }
     }
