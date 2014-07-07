6d9298dbf76d42333537c105e38d8479135072ab
Matt Enlow <matt@novaugust.net>
2014-07-07 08:55:22 -0600

    Fix mobile sidebar menu
    Closes #3110
    - Created `ApplicationView`
    - Added `js-close-sidebar` classes to navbar navigation links
    - Clicking on a navigation link in the sidebar will close it

---
+++ core/client/views/application.js
@@ -0,0 +1,20 @@
+import {responsiveAction} from 'ghost/utils/mobile-utils';
+
+var ApplicationView = Ember.View.extend({
+
+    mobileInteractions: function () {
+        var body = $('body');
+        // ### Toggle the mobile sidebar menu
+        $('[data-off-canvas]').on('click', function (event) {
+            responsiveAction(event, '(max-width: 650px)', function () {
+                body.toggleClass('off-canvas');
+            });
+        });
+        // #### Navigating within the sidebar closes it.
+        $('.js-close-sidebar').on('click', function () {
+            body.removeClass('off-canvas');
+        });
+    }.on('didInsertElement')
+});
+
+export default ApplicationView;
