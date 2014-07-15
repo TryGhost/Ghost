b41b22530798af0f2efb37f766af90cac263e85e
Matt Enlow <matt@novaugust.net>
2014-07-14 22:02:34 -0600

    Remove minor notifications; Close persistent notifications even on error
    Closes #3105, Closes #3175
    
    - Removed notification on successful post's `page` status change
    - Removed notification on successful post `featured` status change
    - Added `closePassive()` notifications on error in the post-settings-menu
    - Persistent notifications will close whether their `DELETE` request was
      successful or not.
    
     #### Misc
    - Added `name` attribute to `post-setting-menu.hbs` inputs to facilitate testing
    - Removed `return <Promise>` from action in `PostSettingsMenuController`. Actions should only return `true`
    - Toggling `post.featured` won't fire NProgress.

--- core/client/controllers/posts/post.js
+++ core/client/controllers/posts/post.js
@@ -4,13 +4,11 @@ var PostController = Ember.ObjectController.extend({
 
     actions: {
         toggleFeatured: function () {
-            var featured = this.toggleProperty('featured'),
+            var options = {disableNProgress: true},
                 self = this;
 
-            this.get('model').save().then(function () {
-                self.notifications.closePassive();
-                self.notifications.showSuccess('Post successfully marked as ' + (featured ? 'featured' : 'not featured') + '.');
-            }).catch(function (errors) {
+            this.toggleProperty('featured');
+            this.get('model').save(options).catch(function (errors) {
                 self.notifications.closePassive();
                 self.notifications.showErrors(errors);
             });
