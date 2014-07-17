ddcb441268a409381373fe48a8d36d7125d2b6ce
Jason Williams <jaswilli@gmail.com>
2014-07-17 22:34:21 +0000

    Get Ember Admin ready for production
    Closes #3161
    - Add a config.js file for the client which is used to configure
      Ember.Application during runtime. The correct version of config.js
      is copied into place by grunt via the copy:(dev|prod) task from
      either config-dev.js or config-prod.js.
    - Serve minified and production versions of libraries where applicable
      including handlebars-runtime and ember-prod.
    - Bundle third party libraries into vendor.min.js.
    - Bundle Ghost's Ember app and templates into ghost.min.js
    - Remove all fixture data and code from the client.

--- ghost/admin/app.js
+++ ghost/admin/app.js
@@ -1,25 +1,18 @@
 import Resolver from 'ember/resolver';
-import initFixtures from 'ghost/fixtures/init';
 import loadInitializers from 'ember/load-initializers';
 import 'ghost/utils/link-view';
 import 'ghost/utils/text-field';
+import configureApp from 'ghost/config';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
-    /**
-     * These are debugging flags, they are useful during development
-     */
-    LOG_ACTIVE_GENERATION: true,
-    LOG_MODULE_RESOLVER: true,
-    LOG_TRANSITIONS: true,
-    LOG_TRANSITIONS_INTERNAL: true,
-    LOG_VIEW_LOOKUPS: true,
     modulePrefix: 'ghost',
     Resolver: Resolver['default']
 });
 
-initFixtures();
+// Runtime configuration of Ember.Application
+configureApp(App);
 
 loadInitializers(App, 'ghost');
 
