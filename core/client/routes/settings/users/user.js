d874e79dbdd30781998ce245f831a36a40fba295
Jacob Gable <jacob.gable@gmail.com>
2014-07-16 21:16:31 -0500

    Fetch actual user on settings/users/user
    Closes #3279
    
    - Switch from this.session.get('user') to this.store.find('user') and
    some further limiting until a custom user adapter is created
    - Switch the deactivate logic to rollback the used model
    - Pass the user as the model in the link-to in user list template

--- core/client/routes/settings/users/user.js
+++ core/client/routes/settings/users/user.js
@@ -1,20 +1,23 @@
 var SettingsUserRoute = Ember.Route.extend({
-    model: function () {
-        return this.session.get('user').then(function (user) {
-            user.reload();
-            return user;
+    model: function (params) {
+        // TODO: Make custom user adapter that uses /api/users/:slug endpoint
+        // return this.store.find('user', { slug: params.slug });
+
+        // Instead, get all the users and then find by slug
+        return this.store.find('user').then(function (result) {
+            return result.findBy('slug', params.slug);
         });
     },
 
     deactivate: function () {
-        this._super();
+        var model = this.modelFor('settings.users.user');
 
         // we want to revert any unsaved changes on exit
-        this.session.get('user').then(function (user) {
-            if (user.get('isDirty')) {
-                user.rollback();
-            }
-        });
+        if (model && model.get('isDirty')) {
+            model.rollback();
+        }
+
+        this._super();
     }
 });
 
