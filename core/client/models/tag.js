69e61013bc154444469e52e16bd1c4f12797f27f
Matt Enlow <matt@novaugust.net>
2014-07-18 17:52:56 -0600

    Add relationships to client models
    No issue
    - Removed tabs from tag.js (why didn't jshint catch this?)
    - Removed superfluous `activate` in SettingsIndexRoute
    - updated `UserModel` and `TagModel` to have `created_by, updated_by` be references to `user` objects.
    - updated `UserModel` to use `moment-date` instead of `date`

--- core/client/models/tag.js
+++ core/client/models/tag.js
@@ -1,15 +1,15 @@
 var Tag = DS.Model.extend({
-	uuid: DS.attr('string'),
-	name: DS.attr('string'),
-	slug: DS.attr('string'),
-	description: DS.attr('string'),
-	parent_id: DS.attr('number'),
-	meta_title: DS.attr('string'),
-	meta_description: DS.attr('string'),
-	created_at: DS.attr('date'),
-	created_by: DS.attr('number'),
-	updated_at: DS.attr('date'),
-	updated_by: DS.attr('number')
+    uuid: DS.attr('string'),
+    name: DS.attr('string'),
+    slug: DS.attr('string'),
+    description: DS.attr('string'),
+    parent_id: DS.attr('number'),
+    meta_title: DS.attr('string'),
+    meta_description: DS.attr('string'),
+    created_at: DS.attr('moment-date'),
+    created_by: DS.belongsTo('user', {async: true}),
+    updated_at: DS.attr('moment-date'),
+    updated_by: DS.belongsTo('user', {async: true})
 });
 
 export default Tag;
