d3491c53b98f6177ed59184ef1090a3f39990273
Hannah Wolfe <erisds@gmail.com>
2014-07-19 01:58:27 +0100

    Adding helper for invite status
    closes #3309, refs #3229
    
    - adds different message depending on status
    - doesn't delete the new user if the problem was an email error
    - filters the 2 lists based on all statuses

--- ghost/admin/controllers/settings/users/index.js
+++ ghost/admin/controllers/settings/users/index.js
@@ -1,9 +1,9 @@
 var UsersIndexController = Ember.ArrayController.extend({
     users: Ember.computed.alias('model'),
 
-    activeUsers: Ember.computed.filterBy('users', 'status', 'active'),
+    activeUsers: Ember.computed.filterBy('users', 'active', true).property('users'),
 
-    invitedUsers: Ember.computed.filterBy('users', 'status', 'invited')
+    invitedUsers: Ember.computed.filterBy('users', 'invited', true).property('users')
 });
 
 export default UsersIndexController;
