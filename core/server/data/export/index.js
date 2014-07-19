5df1057a843d533451c5e2bb1df5a32a86c78aef
Hannah Wolfe <erisds@gmail.com>
2014-07-19 13:20:16 +0100

    Export 003
    closes #3284
    
    - ensure token tables aren't exported
    - cleanup filename
    - failed export throws internal server error

--- core/server/data/export/index.js
+++ core/server/data/export/index.js
@@ -1,11 +1,31 @@
-var _          = require('lodash'),
-    when       = require('when'),
-    versioning = require('../versioning'),
-    config     = require('../../config'),
-    utils      = require('../utils'),
+var _           = require('lodash'),
+    when        = require('when'),
 
-    excludedTables = [],
-    exporter;
+    versioning  = require('../versioning'),
+    config      = require('../../config'),
+    utils       = require('../utils'),
+    serverUtils = require('../../utils'),
+    errors      = require('../../errors'),
+    settings    = require('../../api/settings'),
+
+    excludedTables = ['accesstokens', 'refreshtokens', 'clients'],
+    exporter,
+    exportFileName;
+
+exportFileName = function () {
+    var datetime = (new Date()).toJSON().substring(0, 10),
+        title = '';
+
+    return settings.read({key: 'title', context: {internal: true}}).then(function (result) {
+        if (result) {
+            title = serverUtils.safeString(result.settings[0].value) + '.';
+        }
+        return title + 'ghost.' + datetime + '.json';
+    }).catch(function (err) {
+        errors.logError(err);
+        return 'ghost.' + datetime + '.json';
+    });
+};
 
 exporter = function () {
     return when.join(versioning.getDatabaseVersion(), utils.getTables()).then(function (results) {
@@ -34,9 +54,10 @@ exporter = function () {
 
             return when.resolve(exportData);
         }).catch(function (err) {
-            console.log('Error exporting data: ' + err);
+            errors.logAndThrowError(err, 'Error exporting data', '');
         });
     });
 };
 
 module.exports = exporter;
+module.exports.fileName = exportFileName;
