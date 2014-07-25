503f359aabbb6ba6ba89486c57400e0be22329ee
Paul Adam Davis <pauladamdavis@gmail.com>
2014-07-25 16:31:18 +0100

    Add plural handlebars helper
    Closes #3414
    
    - Adds a helper which shows a select string deopending on the number of items provided
    
    ```
    {{plural pagination.total empty='No posts' singular='% post' plural='% posts'}}
    ```
    If `pagination.total` == 0, output 'No Posts'
    If `pagination.total` == 1, outputs `1 Post`
    If `pagination.total` ==  2 or more, outputs `2 Posts`
    
    Credit to @sebgie for making this work.

--- core/test/unit/server_helpers_index_spec.js
+++ core/test/unit/server_helpers_index_spec.js
@@ -215,6 +215,59 @@ describe('Core Helpers', function () {
         });
     });
 
+    
+
+    describe('Plural Helper', function () {
+
+       it('has loaded plural helper', function () {
+           should.exist(handlebars.helpers.plural);
+       });
+
+       it('will show no-value string', function () {
+           var expected = 'No Posts',
+               rendered = helpers.plural.call({}, 0, {
+                   'hash': {
+                       'empty': 'No Posts',
+                       'singular': '% Post',
+                       'plural': '% Posts'
+                   }
+               });
+
+           should.exist(rendered);
+           rendered.string.should.equal(expected);
+       });
+
+       it('will show singular string', function () {
+           var expected = '1 Post',
+               rendered = helpers.plural.call({}, 1, {
+                   'hash': {
+                       'empty': 'No Posts',
+                       'singular': '% Post',
+                       'plural': '% Posts'
+                   }
+               });
+
+           should.exist(rendered);
+           rendered.string.should.equal(expected);
+       });
+
+       it('will show plural string', function () {
+           var expected = '2 Posts',
+               rendered = helpers.plural.call({}, 2, {
+                   'hash': {
+                       'empty': 'No Posts',
+                       'singular': '% Post',
+                       'plural': '% Posts'
+                   }
+               });
+
+           should.exist(rendered);
+           rendered.string.should.equal(expected);
+       });
+
+   });
+
+
     describe('Excerpt Helper', function () {
 
         it('has loaded excerpt helper', function () {
