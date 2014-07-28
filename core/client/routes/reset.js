b3112a4349ca5dc6179ba795874ec36e23a2163b
Sebastian Gierlinger <s.gierlinger@me.com>
2014-07-28 18:00:08 +0200

    Reset/Signin while signed in
    no issue
    - added redirect and notification to reset route
    - added notification to signup route

--- core/client/routes/reset.js
+++ core/client/routes/reset.js
@@ -3,6 +3,12 @@ import loadingIndicator from 'ghost/mixins/loading-indicator';
 
 var ResetRoute = Ember.Route.extend(styleBody, loadingIndicator, {
     classNames: ['ghost-reset'],
+    beforeModel: function () {
+        if (this.get('session').isAuthenticated) {
+            this.notifications.showWarn('You can\'t reset your password while you\'re signed in.', true);
+            this.transitionTo(SimpleAuth.Configuration.routeAfterAuthentication);
+        }
+    },
     setupController: function (controller, params) {
         controller.token = params.token;
     }
