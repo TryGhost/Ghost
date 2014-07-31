94dee5a57d88415a84698966e298373195bcc4ba
Hannah Wolfe <erisds@gmail.com>
2014-07-31 09:29:05 +0100

    Completed post & user list filer by role
    closes #3446, closes #3086
    
    - Authors can only ever get to their own posts
    - Editors only ever see authors in the user list

--- core/client/routes/posts/post.js
+++ core/client/routes/posts/post.js
@@ -35,7 +35,7 @@ var PostsPostRoute = Ember.Route.extend(SimpleAuth.AuthenticatedRouteMixin, load
             return self.store.find('post', paginationSettings).then(function (records) {
                 var post = records.get('firstObject');
 
-                if (user.get('isAuthor') && user.get('id') !== post.get('author_id')) {
+                if (user.get('isAuthor') && post.isAuthoredByUser(user)) {
                     // do not show the post if they are an author but not this posts author
                     post = null;
                 }
@@ -50,10 +50,10 @@ var PostsPostRoute = Ember.Route.extend(SimpleAuth.AuthenticatedRouteMixin, load
     },
     setupController: function (controller, model) {
         this._super(controller, model);
-        
+
         this.controllerFor('posts').set('currentPost', model);
     },
-    
+
     shortcuts: {
         'enter': 'openEditor'
     },
