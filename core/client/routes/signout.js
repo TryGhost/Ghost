da96df8f7eab6274bbb9db9cdac73b4fb04f5a33
Jason Williams <jaswilli@gmail.com>
2014-07-31 22:41:35 +0000

    Remove unnecessary hard refresh on signout.
    Refs #3488
    - Hard refresh handled by ember-simple-auth.

--- core/client/routes/signout.js
+++ core/client/routes/signout.js
@@ -1,6 +1,5 @@
 import styleBody from 'ghost/mixins/style-body';
 import loadingIndicator from 'ghost/mixins/loading-indicator';
-import ghostPaths from 'ghost/utils/ghost-paths';
 
 var SignoutRoute = Ember.Route.extend(SimpleAuth.AuthenticatedRouteMixin, styleBody, loadingIndicator, {
     classNames: ['ghost-signout'],
@@ -13,13 +12,7 @@ var SignoutRoute = Ember.Route.extend(SimpleAuth.AuthenticatedRouteMixin, styleB
         } else {
             this.send('invalidateSession');
         }
-
-        this.hardRefresh();
     },
-
-    hardRefresh: function () {
-        window.location = ghostPaths().adminRoot + '/signin/';
-    }
 });
 
 export default SignoutRoute;
