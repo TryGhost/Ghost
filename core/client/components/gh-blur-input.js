1d95e6bdcec8f553952474852f637cc7e3d20055
Maurice Williams <morficus@gmail.com>
2014-08-02 23:17:25 -0400

    Stop event propagation when hitting "enter" in the gh-blur-input component
    fixes #3516
    - new behavior is disabled by default
    - added new stopEnterKeyDownPropagation property enable new behavior

--- core/client/components/gh-blur-input.js
+++ core/client/components/gh-blur-input.js
@@ -1,5 +1,6 @@
 var BlurInput = Ember.TextField.extend({
     selectOnClick: false,
+    stopEnterKeyDownPropagation: false,
     click: function (event) {
         if (this.get('selectOnClick')) {
             event.currentTarget.select();
@@ -7,6 +8,15 @@ var BlurInput = Ember.TextField.extend({
     },
     focusOut: function () {
         this.sendAction('action', this.get('value'));
+    },
+    keyDown: function (event) {
+        // stop event propagation when pressing "enter"
+        // most useful in the case when undesired (global) keyboard shortcuts are getting triggered while interacting
+        // with this particular input element.
+        if (this.get('stopEnterKeyDownPropagation') && event.keyCode === 13) {
+            event.stopPropagation();
+            return true;
+        }
     }
 });
 
