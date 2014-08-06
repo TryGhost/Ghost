1f608cf5ab862581a95b6f9b899e45ae40863968
Matt Enlow <matt@novaugust.net>
2014-08-06 08:32:10 -0600

    Add open class to popover buttons
    Ref#3603
    - Popover Buttons now have an `open` class applied to them when their popover is open
    - The open class is removed from the popover button at the start of the popover's fadeout
    - Consolidated common code into popover-mixin

--- core/client/mixins/popover-mixin.js
+++ core/client/mixins/popover-mixin.js
@@ -2,6 +2,8 @@
   Popovers and their buttons are evented and do not propagate clicks.
 */
 var PopoverMixin = Ember.Mixin.create(Ember.Evented, {
+    classNameBindings: ['isOpen:open'],
+    isOpen: false,
     click: function (event) {
         this._super(event);
         return event.stopPropagation();
