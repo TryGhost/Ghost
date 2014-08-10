e97a2406f942221d2a010dc1f25a64439e54506a
Hannah Wolfe <erisds@gmail.com>
2014-08-10 18:57:47 +0100

    Adding author pages to cache invalidation

--- core/server/api/index.js
+++ core/server/api/index.js
@@ -80,7 +80,7 @@ cacheInvalidationHeader = function (req, result) {
 
             // Don't set x-cache-invalidate header for drafts
             if (hasStatusChanged || wasDeleted || wasPublishedUpdated) {
-                cacheInvalidate = '/, /page/*, /rss/, /rss/*, /tag/*';
+                cacheInvalidate = '/, /page/*, /rss/, /rss/*, /tag/*, /author/*';
                 if (id && post.slug) {
                     return config.urlForPost(settings, post).then(function (postUrl) {
                         return cacheInvalidate + ', ' + postUrl;
