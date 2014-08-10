d61e37e7990c5d0b3a30ce6becafff5a83d3190f
Matt Enlow <matt@novaugust.net>
2014-08-10 14:50:25 -0600

    Fix -navbar events attachment
    Closes #3623
    - Move hamburger logic to action with terrible name, "toggleSidebarOrGoHome"
    - Move ".js-close-sidebar" events to a document.on(event, *selector*, f) to make sure they attach even when the js-close-sidebars aren't on page (ie, hidenav)

--- core/client/views/application.js
+++ core/client/views/application.js
@@ -1,21 +1,23 @@
-import {responsiveAction} from 'ghost/utils/mobile';
-
 var ApplicationView = Ember.View.extend({
-
-    mobileInteractions: function () {
-        var body = $('body');
-        // ### Toggle the mobile sidebar menu
-        $('[data-off-canvas]').on('click', function (event) {
-            responsiveAction(event, '(max-width: 650px)', function () {
-                body.toggleClass('off-canvas');
-            });
-        });
-        $('[data-off-canvas]').attr('href', this.get('controller.ghostPaths.blogRoot'));
+    setupCloseSidebar: function () {
         // #### Navigating within the sidebar closes it.
-        $('.js-close-sidebar').on('click', function () {
-            body.removeClass('off-canvas');
+        $(document).on('click', '.js-close-sidebar', function () {
+            $('body').removeClass('off-canvas');
         });
-    }.on('didInsertElement')
+    }.on('didInsertElement'),
+    
+    actions: {
+        //Sends the user to the front if they're not on mobile,
+        //otherwise toggles the sidebar.
+        toggleSidebarOrGoHome: function () {
+            if (window.matchMedia('(max-width: 650px)').matches) {
+                $('body').toggleClass('off-canvas');
+            }
+            else {
+                window.location = this.get('controller.ghostPaths').blogRoot;
+            }
+        }
+    }
 });
 
 export default ApplicationView;
