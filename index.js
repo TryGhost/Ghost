4fc36f2b024de9a2ec2664884d84f88ca7ea0513
Harry Wolff <hswolff@gmail.com>
2014-08-23 16:42:44 -0400

    Makes the Ghost application more express middleware friendly.
    refs #827
    
    - Moves ./index to use Ghost in a similar manner to how someone uses
    Ghost as an npm module.
    - Allows Ghost to be cleanly mounted on another express application
    on any arbitrary endpoint, all you need to customize is the mount path.

--- index.js
+++ index.js
@@ -2,11 +2,19 @@
 // Orchestrates the loading of Ghost
 // When run from command line.
 
-var ghost = require('./core'),
-    errors = require('./core/server/errors');
+var express = require('express'),
+    ghost   = require('./core'),
+    errors  = require('./core/server/errors');
 
-ghost().then(function (app) {
-    app.start();
+// Create our parent express app instance.
+var server = express();
+
+ghost().then(function (instance) {
+    // Mount our ghost instance on our desired subdirectory path if it exists.
+    server.use(instance.config.paths.subdir, instance.app);
+
+    // Let ghost handle starting our server instance.
+    instance.start(server);
 }).catch(function (err) {
     errors.logErrorAndExit(err, err.context, err.help);
 });
