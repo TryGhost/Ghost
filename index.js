8383f977c18a5f598f96f2b9072ca5298692cc7f
Jason Williams <jaswilli@gmail.com>
2014-09-01 19:46:37 +0000

    Add checks for packages and contentPath to startup
    Refs #3864
    - Make sure that require() is able to resolve all dependencies
      listed in package.json.  If packages are missing halt the
      bootstrap process and display an error and help message for user.
    - Check that contentPath and its subdirectories exist with the correct
      permissions.
    - Check sqlite3 database file is set for read/write access.

--- index.js
+++ index.js
@@ -2,11 +2,21 @@
 // Orchestrates the loading of Ghost
 // When run from command line.
 
-var express = require('express'),
-    ghost   = require('./core'),
-    errors  = require('./core/server/errors'),
-    // Create our parent express app instance.
-    parentApp = express();
+var express,
+    ghost,
+    parentApp,
+    errors;
+
+// Make sure dependencies are installed and file system permissions are correct.
+require('./core/server/utils/startup-check').check();
+
+// Proceed with startup
+express = require('express');
+ghost = require('./core');
+errors = require('./core/server/errors');
+
+// Create our parent express app instance.
+parentApp = express();
 
 ghost().then(function (ghostServer) {
     // Mount our ghost instance on our desired subdirectory path if it exists.
