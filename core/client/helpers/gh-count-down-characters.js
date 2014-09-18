e0d8d7731600c351a614b3e7d2ac7a1065e90885
Fabian Becker <halfdan@xnorfz.de>
2014-09-18 23:42:07 +0000

    Meta data screen
    closes #3939
    - Add Seo Tab component to PSM
    - Add new gh-blur-textarea component
    - Refactor blur-input to use new text-input mixin

---
+++ core/client/helpers/gh-count-down-characters.js
@@ -0,0 +1,17 @@
+var countDownCharacters = Ember.Handlebars.makeBoundHelper(function (content, maxCharacters) {
+    var el = document.createElement('span'),
+        length = content ? content.length : 0;
+
+    el.className = 'word-count';
+    if (length > maxCharacters) {
+        el.style.color = '#E25440';
+    } else {
+        el.style.color = '#9E9D95';
+    }
+
+    el.innerHTML = length;
+
+    return new Ember.Handlebars.SafeString(el.outerHTML);
+});
+
+export default countDownCharacters;
