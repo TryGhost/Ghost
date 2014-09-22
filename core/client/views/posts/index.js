d197538d8182fd40ffcc3226887ad4bc404a754f
Matt Enlow <matt@novaugust.net>
2014-09-22 17:42:56 -0600

    Fix uncentered no posts view
    Closes #4097

--- core/client/views/posts/index.js
+++ core/client/views/posts/index.js
@@ -1,5 +1,7 @@
 import MobileIndexView from 'ghost/views/mobile/index-view';
 
-var PostsIndexView = MobileIndexView.extend();
+var PostsIndexView = MobileIndexView.extend({
+    classNames: ['no-posts-box']
+});
 
 export default PostsIndexView;
