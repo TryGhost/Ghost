6bf4f33744293d67b4ac70a4ccfa1551a91604f2
Ashish Dixit <tundal45@gmail.com>
2014-09-24 20:06:42 -0500

    Rename helpers/ghostPaths.js -> helpers/ghost-paths.js
    Refs #4144

--- core/client/app.js
+++ core/client/app.js
@@ -3,7 +3,7 @@ import loadInitializers from 'ember/load-initializers';
 import 'ghost/utils/link-view';
 import 'ghost/utils/text-field';
 import configureApp from 'ghost/config';
-import ghostPathsHelper from 'ghost/helpers/ghostPaths';
+import ghostPathsHelper from 'ghost/helpers/ghost-paths';
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
