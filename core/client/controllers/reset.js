ff1e1bee516916063c9763c250c190058f41ca93
Matt Enlow <matt@novaugust.net>
2014-10-02 09:12:54 -0600

    Reset password signs the user in
    Closes #4196
    - Clear confidential info on leaving reset route
    - Remove nested password access, because gross
    - Also cleaned up some .then(f, h) to .then(f).catch(h) in setup controller

--- core/client/controllers/reset.js
+++ core/client/controllers/reset.js
@@ -4,19 +4,32 @@ import ajax from 'ghost/utils/ajax';
 import ValidationEngine from 'ghost/mixins/validation-engine';
 
 var ResetController = Ember.Controller.extend(ValidationEngine, {
-    passwords: {
-        newPassword: '',
-        ne2Password: ''
-    },
+    newPassword: '',
+    ne2Password: '',
     token: '',
     submitButtonDisabled: false,
 
     validationType: 'reset',
 
+    email: Ember.computed('token', function () {
+        // The token base64 encodes the email (and some other stuff),
+        // each section is divided by a '|'. Email comes second.
+        return atob(this.get('token')).split('|')[1];
+    }),
+
+    // Used to clear sensitive information
+    clearData: function () {
+        this.setProperties({
+            newPassword: '',
+            ne2Password: '',
+            token: ''
+        });
+    },
+
     actions: {
         submit: function () {
-            var self = this,
-                data = self.getProperties('passwords', 'token');
+            var credentials = this.getProperties('newPassword', 'ne2Password', 'token'),
+                self = this;
 
             this.toggleProperty('submitting');
             this.validate({format: false}).then(function () {
@@ -24,16 +37,15 @@ var ResetController = Ember.Controller.extend(ValidationEngine, {
                     url: self.get('ghostPaths.url').api('authentication', 'passwordreset'),
                     type: 'PUT',
                     data: {
-                        passwordreset: [{
-                            newPassword: data.passwords.newPassword,
-                            ne2Password: data.passwords.ne2Password,
-                            token: data.token
-                        }]
+                        passwordreset: [credentials]
                     }
                 }).then(function (resp) {
                     self.toggleProperty('submitting');
                     self.notifications.showSuccess(resp.passwordreset[0].message, true);
-                    self.transitionToRoute('signin');
+                    self.get('session').authenticate('simple-auth-authenticator:oauth2-password-grant', {
+                        identification: self.get('email'),
+                        password: credentials.newPassword
+                    });
                 }).catch(function (response) {
                     self.notifications.showAPIError(response);
                     self.toggleProperty('submitting');
