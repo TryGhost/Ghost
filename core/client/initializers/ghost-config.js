51c6b129b581aa1c7dc37716ac91d4966d52e7b4
cobbspur <cobbspur@hotmail.com>
2014-10-13 17:41:03 +0200

    Create Tags Management Stubs for Ember
    closes #4260
    
    - Adds "tags" route inside of the settings routes.
    - Adds this route to router.js
    - Links the route from the settings.hbs template
    - Adds demo tags html in tag.hbs template
    - Adds flag for tagsUI

--- core/client/initializers/ghost-config.js
+++ core/client/initializers/ghost-config.js
@@ -3,11 +3,12 @@ var ConfigInitializer = {
 
     initialize: function (container, application) {
         var apps = $('body').data('apps'),
+            tagsUI = $('body').data('tagsui'),
             fileStorage = $('body').data('filestorage'),
             blogUrl = $('body').data('blogurl');
 
         application.register(
-            'ghost:config', {apps: apps, fileStorage: fileStorage, blogUrl: blogUrl}, {instantiate: false}
+            'ghost:config', {apps: apps, fileStorage: fileStorage, blogUrl: blogUrl, tagsUI: tagsUI}, {instantiate: false}
         );
 
         application.inject('route', 'config', 'ghost:config');
