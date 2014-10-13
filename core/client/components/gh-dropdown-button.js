0afcc27bd462024124979f5de0fefc1b94ae1a83
Paul Adam Davis <pauladamdavis@gmail.com>
2014-10-13 12:09:56 +0100

    Open user menu on hover

--- core/client/components/gh-dropdown-button.js
+++ core/client/components/gh-dropdown-button.js
@@ -5,9 +5,24 @@ var DropdownButton = Ember.Component.extend(DropdownMixin, {
     /*matches with the dropdown this button toggles*/
     dropdownName: null,
     /*Notify dropdown service this dropdown should be toggled*/
+
+    openOnHover: false,
+
     click: function (event) {
         this._super(event);
         this.get('dropdown').toggleDropdown(this.get('dropdownName'), this);
+    },
+    mouseEnter: function(event){
+        this._super(event);
+        if (this.openOnHover) {
+            this.get('dropdown').toggleDropdown(this.get('dropdownName'), this);
+        }
+    },
+    mouseLeave: function(event){
+        this._super(event);
+        if (this.openOnHover) {
+            this.get('dropdown').toggleDropdown(this.get('dropdownName'), this);
+        }
     }
 });
 
