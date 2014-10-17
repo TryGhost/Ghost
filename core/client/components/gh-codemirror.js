af4e1809e8c18c4a40416bce1c10046d7a34c34c
Hannah Wolfe <erisds@gmail.com>
2014-10-17 18:29:25 +0200

    Autosave additions
    issue #4259, issue #1413
    
    - decrease timeout by 1 sec
    - really actually turn off nprogress

--- core/client/components/gh-codemirror.js
+++ core/client/components/gh-codemirror.js
@@ -22,7 +22,7 @@ var onChangeHandler = function (cm, changeObj) {
     cm.component.set('value', cm.getValue());
 
     // Send an action notifying a 5 second pause in typing/changes.
-    Ember.run.debounce(component, 'sendAction', 'typingPause', 5000);
+    Ember.run.debounce(component, 'sendAction', 'typingPause', 3000);
 };
 
 var onScrollHandler = function (cm) {
