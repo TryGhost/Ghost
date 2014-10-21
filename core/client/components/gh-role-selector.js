40bef7df4084d25e90dd5c2dfcb1150c610457c2
imShadow <shadow.iqka@qq.com>
2014-10-21 17:54:10 +0800

    汉化后台角色呈现名称

--- core/client/components/gh-role-selector.js
+++ core/client/components/gh-role-selector.js
@@ -5,6 +5,16 @@ var RolesSelector = GhostSelect.extend({
     options: Ember.computed(function () {
         var rolesPromise = this.store.find('role', { permissions: 'assign' });
 
+        //汉化修改后台角色呈现名称
+        this.store.find('role', { permissions: 'assign' }).then(function(item){
+            item.forEach( function( i ){ 
+                var n = i.get('description');
+                i.set('name', n);
+            });
+        });
+        var rolesPromise =  this.store.find('role', { permissions: 'assign' });
+        
+
         return Ember.ArrayProxy.extend(Ember.PromiseProxyMixin)
             .create({promise: rolesPromise});
     })
