cc6f698053f1da7a0da32cfcf4474b3ee914e89d
Paul Adam Davis <pauladamdavis@gmail.com>
2014-12-14 17:56:04 +0000

    Start of labs
    issue #4409

--- core/client/models/setting.js
+++ core/client/models/setting.js
@@ -16,7 +16,8 @@ var Setting = DS.Model.extend(NProgressSaveMixin, ValidationEngine, {
     activeTheme: DS.attr('string'),
     availableThemes: DS.attr(),
     ghost_head: DS.attr('string'),
-    ghost_foot: DS.attr('string')
+    ghost_foot: DS.attr('string'),
+    labs: DS.attr('string')
 });
 
 export default Setting;
