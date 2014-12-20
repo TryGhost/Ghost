28c754d56e85a16cba9a926f472a859c8cd8fb42
cobbspur <cobbspur@hotmail.com>
2014-12-20 13:35:02 +0000

    Adds structured data to first index/tag/author page
    closes #4677
    
    - Tests if page is first page or paginated
    - Adds relevant structured data to index/tag/author page
    - Does not add structured data on paginated pages
    - For author structured data, cover image overrides image
    - blog cover image is made absolute by image helper
    - Tests updated and additional tests for tag and author pages

--- core/test/unit/server_helpers/ghost_head_spec.js
+++ core/test/unit/server_helpers/ghost_head_spec.js
@@ -20,7 +20,9 @@ describe('{{ghost_head}} helper', function () {
         utils.overrideConfig({
             url: 'http://testurl.com/',
             theme: {
-                title: 'Ghost'
+                title: 'Ghost',
+                description: 'blog description',
+                cover: '/content/images/blog-cover.png'
             }
         });
     });
@@ -49,10 +51,159 @@ describe('{{ghost_head}} helper', function () {
             should.exist(handlebars.helpers.ghost_head);
         });
 
-        it('returns meta tag string', function (done) {
-            helpers.ghost_head.call({version: '0.3.0', post: false}).then(function (rendered) {
+        it('returns meta tag string on paginated index page', function (done) {
+            helpers.ghost_head.call({version: '0.3.0', relativeUrl: '/page/2/', context: ['paged', 'index']}).then(function (rendered) {
                 should.exist(rendered);
-                rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/" />\n' +
+                rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/page/2/" />\n' +
+                    '    <meta name="generator" content="Ghost 0.3" />\n' +
+                    '    <link rel="alternate" type="application/rss+xml" title="Ghost" href="http://testurl.com/rss/" />');
+
+                done();
+            }).catch(done);
+        });
+
+        it('returns structured data on first index page', function (done) {
+            helpers.ghost_head.call({version: '0.3.0', relativeUrl: '/', context: ['home', 'index']}).then(function (rendered) {
+                should.exist(rendered);
+                rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/" />\n    \n' +
+                    '    <meta property="og:site_name" content="Ghost" />\n' +
+                    '    <meta property="og:type" content="website" />\n' +
+                    '    <meta property="og:title" content="Ghost" />\n' +
+                    '    <meta property="og:description" content="blog description..." />\n' +
+                    '    <meta property="og:url" content="http://testurl.com/" />\n' +
+                    '    <meta property="og:image" content="http://testurl.com/content/images/blog-cover.png" />\n    \n' +
+                    '    <meta name="twitter:card" content="summary_large_image" />\n' +
+                    '    <meta name="twitter:title" content="Ghost" />\n' +
+                    '    <meta name="twitter:description" content="blog description..." />\n' +
+                    '    <meta name="twitter:url" content="http://testurl.com/" />\n' +
+                    '    <meta name="twitter:image:src" content="http://testurl.com/content/images/blog-cover.png" />\n    \n' +
+                    '    <meta name="generator" content="Ghost 0.3" />\n' +
+                    '    <link rel="alternate" type="application/rss+xml" title="Ghost" href="http://testurl.com/rss/" />');
+
+                done();
+            }).catch(done);
+        });
+
+        it('returns structured data on first tag page without schema', function (done) {
+            var tag = {
+                meta_description: 'tag meta description',
+                title: 'tagtitle',
+                meta_title: 'tag meta title',
+                image: '/content/images/tag-image.png'
+            };
+
+            helpers.ghost_head.call({version: '0.3.0', relativeUrl: '/tag/tagtitle/', tag: tag, context: ['tag']}).then(function (rendered) {
+                should.exist(rendered);
+                rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/tag/tagtitle/" />\n    \n' +
+                    '    <meta property="og:site_name" content="Ghost" />\n' +
+                    '    <meta property="og:type" content="website" />\n' +
+                    '    <meta property="og:title" content="tag meta title" />\n' +
+                    '    <meta property="og:description" content="tag meta description..." />\n' +
+                    '    <meta property="og:url" content="http://testurl.com/tag/tagtitle/" />\n' +
+                    '    <meta property="og:image" content="http://testurl.com/content/images/tag-image.png" />\n    \n' +
+                    '    <meta name="twitter:card" content="summary_large_image" />\n' +
+                    '    <meta name="twitter:title" content="tag meta title" />\n' +
+                    '    <meta name="twitter:description" content="tag meta description..." />\n' +
+                    '    <meta name="twitter:url" content="http://testurl.com/tag/tagtitle/" />\n' +
+                    '    <meta name="twitter:image:src" content="http://testurl.com/content/images/tag-image.png" />\n    \n' +
+                    '    <meta name="generator" content="Ghost 0.3" />\n' +
+                    '    <link rel="alternate" type="application/rss+xml" title="Ghost" href="http://testurl.com/rss/" />');
+
+                done();
+            }).catch(done);
+        });
+
+        it('does not contain description meta tags if blank', function (done) {
+            var tag = {
+                meta_description: '',
+                title: 'tagtitle',
+                meta_title: 'tag meta title',
+                image: '/content/images/tag-image.png'
+            };
+
+            helpers.ghost_head.call({version: '0.3.0', relativeUrl: '/tag/tagtitle/', tag: tag, context: ['tag']}).then(function (rendered) {
+                should.exist(rendered);
+                rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/tag/tagtitle/" />\n    \n' +
+                    '    <meta property="og:site_name" content="Ghost" />\n' +
+                    '    <meta property="og:type" content="website" />\n' +
+                    '    <meta property="og:title" content="tag meta title" />\n' +
+                    '    <meta property="og:url" content="http://testurl.com/tag/tagtitle/" />\n' +
+                    '    <meta property="og:image" content="http://testurl.com/content/images/tag-image.png" />\n    \n' +
+                    '    <meta name="twitter:card" content="summary_large_image" />\n' +
+                    '    <meta name="twitter:title" content="tag meta title" />\n' +
+                    '    <meta name="twitter:url" content="http://testurl.com/tag/tagtitle/" />\n' +
+                    '    <meta name="twitter:image:src" content="http://testurl.com/content/images/tag-image.png" />\n    \n' +
+                    '    <meta name="generator" content="Ghost 0.3" />\n' +
+                    '    <link rel="alternate" type="application/rss+xml" title="Ghost" href="http://testurl.com/rss/" />');
+
+                done();
+            }).catch(done);
+        });
+
+        it('does not return structured data on paginated tag pages', function (done) {
+            var tag = {
+                meta_description: 'tag meta description',
+                title: 'tagtitle',
+                meta_title: 'tag meta title',
+                image: '/content/images/tag-image.png'
+            };
+
+            helpers.ghost_head.call({version: '0.3.0', relativeUrl: '/tag/tagtitle/page/2/', tag: tag, context: ['paged', 'tag']}).then(function (rendered) {
+                should.exist(rendered);
+                rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/tag/tagtitle/page/2/" />\n' +
+                    '    <meta name="generator" content="Ghost 0.3" />\n' +
+                    '    <link rel="alternate" type="application/rss+xml" title="Ghost" href="http://testurl.com/rss/" />');
+
+                done();
+            }).catch(done);
+        });
+
+        it('returns structured data on first author page with cover image, without schema', function (done) {
+            var author = {
+                    name: 'Author name',
+                    url: 'http//:testauthorurl.com',
+                    slug: 'AuthorName',
+                    bio: 'Author bio',
+                    image: '/content/images/test-author-image.png',
+                    cover: '/content/images/author-cover-image.png',
+                    website: 'http://authorwebsite.com'
+            };
+
+            helpers.ghost_head.call({version: '0.3.0', relativeUrl: '/author/AuthorName/', author: author, context: ['author']}).then(function (rendered) {
+                should.exist(rendered);
+                rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/author/AuthorName/" />\n    \n' +
+                    '    <meta property="og:site_name" content="Ghost" />\n' +
+                    '    <meta property="og:type" content="profile" />\n' +
+                    '    <meta property="og:title" content="Author name - Ghost" />\n' +
+                    '    <meta property="og:description" content="Author bio..." />\n' +
+                    '    <meta property="og:url" content="http://testurl.com/author/AuthorName/" />\n' +
+                    '    <meta property="og:image" content="http://testurl.com/content/images/author-cover-image.png" />\n    \n' +
+                    '    <meta name="twitter:card" content="summary_large_image" />\n' +
+                    '    <meta name="twitter:title" content="Author name - Ghost" />\n' +
+                    '    <meta name="twitter:description" content="Author bio..." />\n' +
+                    '    <meta name="twitter:url" content="http://testurl.com/author/AuthorName/" />\n' +
+                    '    <meta name="twitter:image:src" content="http://testurl.com/content/images/author-cover-image.png" />\n    \n' +
+                    '    <meta name="generator" content="Ghost 0.3" />\n' +
+                    '    <link rel="alternate" type="application/rss+xml" title="Ghost" href="http://testurl.com/rss/" />');
+
+                done();
+            }).catch(done);
+        });
+
+        it('does not return structured data on paginated author pages', function (done) {
+            var author = {
+                name: 'Author name',
+                url: 'http//:testauthorurl.com',
+                slug: 'AuthorName',
+                bio: 'Author bio',
+                image: '/content/images/test-author-image.png',
+                cover: '/content/images/author-cover-image.png',
+                website: 'http://authorwebsite.com'
+            };
+
+            helpers.ghost_head.call({version: '0.3.0', relativeUrl: '/author/AuthorName/page/2/', author: author, context: ['paged', 'author']}).then(function (rendered) {
+                should.exist(rendered);
+                rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/author/AuthorName/page/2/" />\n' +
                     '    <meta name="generator" content="Ghost 0.3" />\n' +
                     '    <link rel="alternate" type="application/rss+xml" title="Ghost" href="http://testurl.com/rss/" />');
 
@@ -61,9 +212,9 @@ describe('{{ghost_head}} helper', function () {
         });
 
         it('returns meta tag string even if version is invalid', function (done) {
-            helpers.ghost_head.call({version: '0.9'}).then(function (rendered) {
+            helpers.ghost_head.call({version: '0.9', relativeUrl: '/page/2/', context: ['paged', 'index']}).then(function (rendered) {
                 should.exist(rendered);
-                rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/" />\n' +
+                rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/page/2/" />\n' +
                     '    <meta name="generator" content="Ghost 0.9" />\n' +
                     '    <link rel="alternate" type="application/rss+xml" title="Ghost" href="http://testurl.com/rss/" />');
 
@@ -88,7 +239,7 @@ describe('{{ghost_head}} helper', function () {
                 }
             };
 
-            helpers.ghost_head.call({relativeUrl: '/post/', version: '0.3.0', post: post}).then(function (rendered) {
+            helpers.ghost_head.call({relativeUrl: '/post/', version: '0.3.0', context: ['post'], post: post}).then(function (rendered) {
                 should.exist(rendered);
                 rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/post/" />\n    \n' +
                     '    <meta property="og:site_name" content="Ghost" />\n' +
@@ -141,7 +292,7 @@ describe('{{ghost_head}} helper', function () {
                 }
             };
 
-            helpers.ghost_head.call({relativeUrl: '/post/', version: '0.3.0', post: post}).then(function (rendered) {
+            helpers.ghost_head.call({relativeUrl: '/post/', version: '0.3.0', context: ['post'], post: post}).then(function (rendered) {
                 should.exist(rendered);
                 rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/post/" />\n    \n' +
                     '    <meta property="og:site_name" content="Ghost" />\n' +
@@ -193,7 +344,7 @@ describe('{{ghost_head}} helper', function () {
                 }
             };
 
-            helpers.ghost_head.call({relativeUrl: '/post/', version: '0.3.0', post: post}).then(function (rendered) {
+            helpers.ghost_head.call({relativeUrl: '/post/', version: '0.3.0', context: ['post'], post: post}).then(function (rendered) {
                 should.exist(rendered);
                 rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/post/" />\n    \n' +
                     '    <meta property="og:site_name" content="Ghost" />\n' +
@@ -242,7 +393,7 @@ describe('{{ghost_head}} helper', function () {
                 }
             };
 
-            helpers.ghost_head.call({relativeUrl: '/post/', version: '0.3.0', post: post}).then(function (rendered) {
+            helpers.ghost_head.call({relativeUrl: '/post/', version: '0.3.0', context: ['post'], post: post}).then(function (rendered) {
                 should.exist(rendered);
                 rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/post/" />\n    \n' +
                     '    <meta property="og:site_name" content="Ghost" />\n' +
@@ -296,7 +447,7 @@ describe('{{ghost_head}} helper', function () {
                 }
             };
 
-            helpers.ghost_head.call({relativeUrl: '/post/', version: '0.3.0', post: post}).then(function (rendered) {
+            helpers.ghost_head.call({relativeUrl: '/post/', version: '0.3.0', context: ['post'], post: post}).then(function (rendered) {
                 should.exist(rendered);
                 rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/post/" />\n' +
                     '    <meta name="generator" content="Ghost 0.3" />\n' +
@@ -307,7 +458,7 @@ describe('{{ghost_head}} helper', function () {
         });
 
         it('returns canonical URL', function (done) {
-            helpers.ghost_head.call({version: '0.3.0', relativeUrl: '/about/'}).then(function (rendered) {
+            helpers.ghost_head.call({version: '0.3.0', relativeUrl: '/about/', context: ['home', 'index']}).then(function (rendered) {
                 should.exist(rendered);
                 rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/about/" />\n' +
                     '    <meta name="generator" content="Ghost 0.3" />\n' +
@@ -318,7 +469,7 @@ describe('{{ghost_head}} helper', function () {
         });
 
         it('returns next & prev URL correctly for middle page', function (done) {
-            helpers.ghost_head.call({version: '0.3.0', relativeUrl: '/page/3/', pagination: {next: '4', prev: '2'}}).then(function (rendered) {
+            helpers.ghost_head.call({version: '0.3.0', relativeUrl: '/page/3/', context: ['paged', 'index'], pagination: {next: '4', prev: '2'}}).then(function (rendered) {
                 should.exist(rendered);
                 rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/page/3/" />\n' +
                     '    <link rel="prev" href="http://testurl.com/page/2/" />\n' +
@@ -330,7 +481,7 @@ describe('{{ghost_head}} helper', function () {
         });
 
         it('returns next & prev URL correctly for second page', function (done) {
-            helpers.ghost_head.call({version: '0.3.0', relativeUrl: '/page/2/', pagination: {next: '3', prev: '1'}}).then(function (rendered) {
+            helpers.ghost_head.call({version: '0.3.0', relativeUrl: '/page/2/', context: ['paged', 'index'], pagination: {next: '3', prev: '1'}}).then(function (rendered) {
                 should.exist(rendered);
                 rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/page/2/" />\n' +
                     '    <link rel="prev" href="http://testurl.com/" />\n' +
@@ -346,7 +497,9 @@ describe('{{ghost_head}} helper', function () {
                 utils.overrideConfig({
                     url: 'http://testurl.com/blog/',
                     theme: {
-                        title: 'Ghost'
+                        title: 'Ghost',
+                        description: 'blog description',
+                        cover: '/content/images/blog-cover.png'
                     }
                 });
             });
@@ -356,7 +509,7 @@ describe('{{ghost_head}} helper', function () {
             });
 
             it('returns correct rss url with subdirectory', function (done) {
-                helpers.ghost_head.call({version: '0.3.0'}).then(function (rendered) {
+                helpers.ghost_head.call({version: '0.3.0', context: ['paged', 'index']}).then(function (rendered) {
                     should.exist(rendered);
                     rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/blog/" />\n' +
                         '    <meta name="generator" content="Ghost 0.3" />\n' +
@@ -380,7 +533,9 @@ describe('{{ghost_head}} helper', function () {
             utils.overrideConfig({
                 url: 'http://testurl.com/',
                 theme: {
-                    title: 'Ghost'
+                    title: 'Ghost',
+                    description: 'blog description',
+                    cover: '/content/images/blog-cover.png'
                 }
             });
         });
@@ -391,7 +546,7 @@ describe('{{ghost_head}} helper', function () {
         });
 
         it('returns meta tag plus injected code', function (done) {
-            helpers.ghost_head.call({version: '0.3.0', post: false}).then(function (rendered) {
+            helpers.ghost_head.call({version: '0.3.0', context: ['paged', 'index'], post: false}).then(function (rendered) {
                 should.exist(rendered);
                 rendered.string.should.equal('<link rel="canonical" href="http://testurl.com/" />\n' +
                     '    <meta name="generator" content="Ghost 0.3" />\n' +
