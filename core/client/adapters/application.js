03ea6bdfb0584bbd3c2a74d71ffdcb5e76f860e9
Jason Williams <jaswilli@gmail.com>
2014-12-26 00:01:02 +0000

    Make embedded-relation-adapter the default
    No Issue
    - Generalize embedded-relation-adapter so it can be used for both fetching
      and saving records.
    - Change Adapter inheritance hierarchy to
      RESTAdapter => BaseAdapter => EmbeddedRelationAdapter. ApplicationAdapter
      now extends EmbeddedRelationAdapter.
    - Cleanup or remove adapters that existed just to extend directly
      from EmbeddedRelationAdapter.

--- core/client/adapters/application.js
+++ core/client/adapters/application.js
@@ -1,44 +1,5 @@
-import ghostPaths from 'ghost/utils/ghost-paths';
+import EmbeddedRelationAdapter from 'ghost/adapters/embedded-relation-adapter';
 
-var ApplicationAdapter = DS.RESTAdapter.extend({
-    host: window.location.origin,
-    namespace: ghostPaths().apiRoot.slice(1),
-
-    findQuery: function (store, type, query) {
-        var id;
-
-        if (query.id) {
-            id = query.id;
-            delete query.id;
-        }
-
-        return this.ajax(this.buildURL(type.typeKey, id), 'GET', {data: query});
-    },
-
-    buildURL: function (type, id) {
-        // Ensure trailing slashes
-        var url = this._super(type, id);
-
-        if (url.slice(-1) !== '/') {
-            url += '/';
-        }
-
-        return url;
-    },
-
-    // Override deleteRecord to disregard the response body on 2xx responses.
-    // This is currently needed because the API is returning status 200 along
-    // with the JSON object for the deleted entity and Ember expects an empty
-    // response body for successful DELETEs.
-    // Non-2xx (failure) responses will still work correctly as Ember will turn
-    // them into rejected promises.
-    deleteRecord: function () {
-        var response = this._super.apply(this, arguments);
-
-        return response.then(function () {
-            return null;
-        });
-    }
-});
+var ApplicationAdapter = EmbeddedRelationAdapter.extend();
 
 export default ApplicationAdapter;
