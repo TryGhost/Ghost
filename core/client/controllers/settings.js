c0fc7c5bcd0c27c70a27be3eb36243f436a27681
Hannah Wolfe <erisds@gmail.com>
2015-01-01 19:09:55 +0000

    Adds global feature manager
    closes #4409

--- core/client/controllers/settings.js
+++ core/client/controllers/settings.js
@@ -5,18 +5,15 @@ var SettingsController = Ember.Controller.extend({
     showUsers: Ember.computed('session.user.name', function () {
         return this.get('session.user.isAuthor') ? false : true;
     }),
-    showTags: Ember.computed('session.user.name', 'config.tagsUI', function () {
-        return this.get('session.user.isAuthor') || !this.get('config.tagsUI') ? false : true;
+    showTags: Ember.computed('session.user.name', 'feature.tagsUI', function () {
+        return this.get('session.user.isAuthor') || !this.get('feature.tagsUI') ? false : true;
     }),
-
-    showCodeInjection: Ember.computed('session.user.name', 'config.codeInjectionUI', function () {
-        return this.get('session.user.isAuthor') || this.get('session.user.isEditor') || !this.get('config.codeInjectionUI') ? false : true;
+    showCodeInjection: Ember.computed('session.user.name', 'feature.codeInjectionUI', function () {
+        return this.get('session.user.isAuthor') || this.get('session.user.isEditor') || !this.get('feature.codeInjectionUI') ? false : true;
     }),
-
     showLabs: Ember.computed('session.user.name', function () {
         return this.get('session.user.isAuthor')  || this.get('session.user.isEditor') ? false : true;
     }),
-
     showAbout: Ember.computed('session.user.name', function () {
         return this.get('session.user.isAuthor') ? false : true;
     })
