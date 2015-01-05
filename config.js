076a7aa2d6dc2579113a4577e4658cd089fa38c4
Wyndham Hudson <whudson05@gmail.com>
2015-01-05 19:34:10 +0000

    Added basic config file

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ config = {
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
         // Change this to your Ghost blogs published URL.
-        url: 'http://localhost:2368',
+        url: 'http://wyndham-blog.azurewebsites.net',
 
         // Example mail config
         // Visit http://support.ghost.org/mail for instructions
@@ -60,7 +60,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+            port: process.env.PORT
         },
         paths: {
             contentPath: path.join(__dirname, '/content/')
