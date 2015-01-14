d87994aa35870e7b3247086e0e94b1a24ccd7d3a
Jason Williams <jaswilli@gmail.com>
2015-01-14 14:46:29 +0000

    Connect Navigation page to live data
    Closes #4539

--- ghost/admin/controllers/settings/navigation.js
+++ ghost/admin/controllers/settings/navigation.js
@@ -1,23 +1,114 @@
-var NavigationController = Ember.Controller.extend(Ember.Evented, {
+var NavigationController,
+    NavItem;
 
-    navigationJSON: Ember.computed('model.navigation', function () {
-        var navJSON = JSON.parse(this.get('model.navigation') || {}),
-            lastNavItem = navJSON[navJSON.length - 1];
-        lastNavItem.last = true; // Set a 'last' property on the last nav item, only used in the template
-        return navJSON;
+NavItem = Ember.Object.extend({
+    label: '',
+    url: '',
+
+    isBlank: Ember.computed('label', 'url', function () {
+        return Ember.isBlank(this.get('label')) && Ember.isBlank(this.get('url'));
+    })
+});
+
+NavigationController = Ember.Controller.extend({
+     navigationItems: Ember.computed('model.navigation', function () {
+        var navItems,
+            lastItem;
+
+        try {
+            navItems = JSON.parse(this.get('model.navigation') || [{}]);
+        } catch (e) {
+            navItems = [{}];
+        }
+
+        navItems = navItems.map(function (item) {
+            return NavItem.create(item);
+        });
+
+        lastItem = navItems.get('lastObject');
+        if (!lastItem || !lastItem.get('isBlank')) {
+            navItems.addObject(NavItem.create());
+        }
+
+        return navItems;
+    }),
+
+    navigationItemsObserver: Ember.observer('navigationItems.[]', function () {
+        var navItems = this.get('navigationItems');
+
+        navItems.forEach(function (item, index, items) {
+            if (index === (items.length - 1)) {
+                item.set('last', true);
+            } else {
+                item.set('last', false);
+            }
+        });
     }),
 
     actions: {
         addItem: function () {
-            // Add a new item
+            var navItems = this.get('navigationItems'),
+                lastItem = navItems.get('lastObject');
+
+            if (lastItem && !lastItem.get('isBlank')) {
+                navItems.addObject(NavItem.create());
+            }
         },
 
-        deleteItem: function () {
-            // Delete navItem which should be a function param like: `deleteItem: function(navItem) {`
+        deleteItem: function (item) {
+            if (!item) {
+                return;
+            }
+
+            this.get('navigationItems').removeObject(item);
         },
 
         save: function () {
-            // Save everything
+            var self = this,
+                navSetting,
+                blogUrl = this.get('config').blogUrl,
+                blogUrlRegex = new RegExp('^' + blogUrl + '(.*)', 'i'),
+                match;
+
+            navSetting = this.get('navigationItems').map(function (item) {
+                var label,
+                    url;
+
+                if (!item || item.get('isBlank')) {
+                    return;
+                }
+
+                label = item.get('label').trim();
+                url = item.get('url').trim();
+
+                match = url.match(blogUrlRegex);
+
+                if (match) {
+                    if (match[1] === '') {
+                        url = '/';
+                    } else {
+                        url = match[1];
+                    }
+                } else if (!validator.isURL(url) && url !== '' && url[0] !== '/') {
+                    url = '/' + url;
+                }
+
+                return {label: label, url: url};
+            }).compact();
+
+            this.set('model.navigation', JSON.stringify(navSetting));
+
+            // trigger change event because even if the final JSON is unchanged
+            // we need to have navigationItems recomputed.
+            this.get('model').notifyPropertyChange('navigation');
+
+            this.notifications.closePassive();
+
+            this.get('model').save().then(function () {
+                self.notifications.showSuccess('Navigation items saved.');
+            }).catch(function (err) {
+                self.notifications.showErrors(err);
+            });
         }
     }
 });
