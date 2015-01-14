b5caeb6ce934ded8fb959bfb17fba2f47ed466e4
Jason Williams <jaswilli@gmail.com>
2015-01-14 14:46:29 +0000

    Connect Navigation page to live data
    Closes #4539

--- core/client/views/settings/navigation.js
+++ core/client/views/settings/navigation.js
@@ -1,5 +1,13 @@
 import BaseView from 'ghost/views/settings/content-base';
 
-var SettingsNavigationView = BaseView.extend();
+var SettingsNavigationView = BaseView.extend({
+    keyPress: function (event) {
+        // + character
+        if (event.keyCode === 43) {
+            event.preventDefault();
+            this.get('controller').send('addItem');
+        }
+    }
+});
 
 export default SettingsNavigationView;
