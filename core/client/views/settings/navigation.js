d446ad84ea28d9726166f48276df9a0a10b9677b
Jason Williams <jaswilli@gmail.com>
2015-01-18 00:16:54 +0000

    Implement Navigation item input behaviors
    Closes #4807

--- core/client/views/settings/navigation.js
+++ core/client/views/settings/navigation.js
@@ -1,13 +1,6 @@
 import BaseView from 'ghost/views/settings/content-base';
 
 var SettingsNavigationView = BaseView.extend({
-    keyPress: function (event) {
-        // + character
-        if (event.keyCode === 43) {
-            event.preventDefault();
-            this.get('controller').send('addItem');
-        }
-    }
 });
 
 export default SettingsNavigationView;
