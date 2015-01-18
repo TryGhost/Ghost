aa09046580519c4c06a3758d88f21536d323cbdc
Mark Stosberg <mark@rideamigos.com>
2015-01-18 13:44:50 -0500

    Refactor: Make checkSSL unit-testable and add unit tests for it.
    - Code was moved to core/server/middleware/middleware.js, which is the
      home for unit-testable middleware.
    
    - Functional code coverage for this code also exists at:
      test/functional/routes/admin_test.js

--- core/server/middleware/middleware.js
+++ core/server/middleware/middleware.js
@@ -10,6 +10,7 @@ var _           = require('lodash'),
     api         = require('../api'),
     passport    = require('passport'),
     errors      = require('../errors'),
+    url         = require('url'),
     utils       = require('../utils'),
 
     middleware,
@@ -30,6 +31,49 @@ function cacheBlogApp(app) {
 
 function cacheOauthServer(server) {
     oauthServer = server;
+}
+
+function isSSLrequired(isAdmin, configUrl, forceAdminSSL) {
+    var forceSSL = url.parse(configUrl).protocol === 'https:' ? true : false;
+    if (forceSSL || (isAdmin && forceAdminSSL)) {
+        return true;
+    }
+    return false;
+}
+
+// The guts of checkSSL. Indicate forbidden or redirect according to configuration.
+// Required args: forceAdminSSL, url and urlSSL should be passed from config. reqURL from req.url
+function sslForbiddenOrRedirect(opt) {
+    var forceAdminSSL = opt.forceAdminSSL,
+        reqUrl        = opt.reqUrl, // expected to be relative-to-root
+        baseUrl       = url.parse(opt.configUrlSSL || opt.configUrl),
+        response = {
+        // Check if forceAdminSSL: { redirect: false } is set, which means
+        // we should just deny non-SSL access rather than redirect
+        isForbidden: (forceAdminSSL && forceAdminSSL.redirect !== undefined && !forceAdminSSL.redirect),
+
+        // Append the request path to the base configuration path, trimming out a double "//"
+        redirectPathname: function () {
+            var pathname  = baseUrl.path;
+            if (reqUrl[0] === '/' && pathname[pathname.length - 1] === '/') {
+                pathname += reqUrl.slice(1);
+            } else {
+                pathname += reqUrl;
+            }
+            return pathname;
+        },
+        redirectUrl: function (query) {
+            return url.format({
+                protocol: 'https:',
+                hostname: baseUrl.hostname,
+                port: baseUrl.port,
+                pathname: this.redirectPathname(),
+                query: query
+            });
+        }
+    };
+
+    return response;
 }
 
 middleware = {
@@ -259,9 +303,35 @@ middleware = {
         return oauthServer.token()(req, res, next);
     },
 
+    // Check to see if we should use SSL
+    // and redirect if needed
+    checkSSL: function (req, res, next) {
+        if (isSSLrequired(res.isAdmin, config.url, config.forceAdminSSL)) {
+            if (!req.secure) {
+                var response = sslForbiddenOrRedirect({
+                    forceAdminSSL: config.forceAdminSSL,
+                    configUrlSSL: config.urlSSL,
+                    configUrl: config.url,
+                    reqUrl: req.url
+                });
+
+                if (response.isForbidden) {
+                    return res.sendStatus(403);
+                } else {
+                    return res.redirect(301, response.redirectUrl(req.query));
+                }
+            }
+        }
+        next();
+    },
+
     busboy: busboy
 };
 
 module.exports = middleware;
 module.exports.cacheBlogApp = cacheBlogApp;
 module.exports.cacheOauthServer = cacheOauthServer;
+
+// SSL helper functions are exported primarily for unity testing.
+module.exports.isSSLrequired = isSSLrequired;
+module.exports.sslForbiddenOrRedirect = sslForbiddenOrRedirect;
--- core/test/unit/middleware_spec.js
+++ core/test/unit/middleware_spec.js
@@ -170,4 +170,70 @@ describe('Middleware', function () {
             });
         });
     });
+
+    describe('isSSLRequired', function () {
+        var isSSLrequired = middleware.isSSLrequired;
+
+        it('SSL is required if config.url starts with https', function () {
+            isSSLrequired(undefined, 'https://example.com', undefined).should.be.true;
+        });
+
+        it('SSL is required if isAdmin and config.forceAdminSSL is set', function () {
+            isSSLrequired(true, 'http://example.com', true).should.be.true;
+        });
+
+        it('SSL is not required if config.url starts with "http:/" and forceAdminSSL is not set', function () {
+            isSSLrequired(false, 'http://example.com', false).should.be.false;
+        });
+    });
+
+    describe('sslForbiddenOrRedirect', function () {
+        var sslForbiddenOrRedirect = middleware.sslForbiddenOrRedirect;
+        it('Return forbidden if config forces admin SSL for AdminSSL redirect is false.', function () {
+            var response = sslForbiddenOrRedirect({
+                forceAdminSSL: {redirect: false},
+                configUrl: 'http://example.com'
+            });
+            response.isForbidden.should.be.true;
+        });
+
+        it('If not forbidden, should produce SSL to redirect to when config.url ends with no slash', function () {
+            var response = sslForbiddenOrRedirect({
+                forceAdminSSL: {redirect: true},
+                configUrl: 'http://example.com/config/path',
+                reqUrl: '/req/path'
+            });
+            response.isForbidden.should.be.false;
+            response.redirectUrl({}).should.equal('https://example.com/config/path/req/path');
+        });
+
+        it('If config ends is slash, potential double-slash in resulting URL is removed', function () {
+            var response = sslForbiddenOrRedirect({
+                forceAdminSSL: {redirect: true},
+                configUrl: 'http://example.com/config/path/',
+                reqUrl: '/req/path'
+            });
+            response.redirectUrl({}).should.equal('https://example.com/config/path/req/path');
+        });
+
+        it('If config.urlSSL is provided it is preferred over config.url', function () {
+            var response = sslForbiddenOrRedirect({
+                forceAdminSSL: {redirect: true},
+                configUrl: 'http://example.com/config/path/',
+                configUrlSSL: 'https://example.com/ssl/config/path/',
+                reqUrl: '/req/path'
+            });
+            response.redirectUrl({}).should.equal('https://example.com/ssl/config/path/req/path');
+        });
+
+        it('query string in request is preserved in redirect URL', function () {
+            var response = sslForbiddenOrRedirect({
+                forceAdminSSL: {redirect: true},
+                configUrl: 'http://example.com/config/path/',
+                configUrlSSL: 'https://example.com/ssl/config/path/',
+                reqUrl: '/req/path'
+            });
+            response.redirectUrl({a: 'b'}).should.equal('https://example.com/ssl/config/path/req/path?a=b');
+        });
+    });
 });
