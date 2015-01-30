aacf216ef782087415907cb352038034f67f5610
Jason Williams <jaswilli@gmail.com>
2015-01-30 22:18:26 +0000

    Do not add autofocus attribute unless set to focus
    No Issue
    - Don't add the "autofocus" attribute to the input element unless
      the "focus" property is set to true.

--- core/test/client/unit/components/gh-trim-focus-input_test.js
+++ core/test/client/unit/components/gh-trim-focus-input_test.js
@@ -15,4 +15,26 @@ describeComponent('gh-trim-focus-input', function () {
         component.$().focusout();
         expect(component.$().val()).to.equal('some random stuff');
     });
+
+    it('does not have the autofocus attribute if not set to focus', function () {
+        var component = this.subject({
+            value: 'some text',
+            focus: false
+        });
+
+        this.render();
+
+        expect(component.$().attr('autofocus')).to.not.be.ok;
+    });
+
+    it('has the autofocus attribute if set to focus', function () {
+        var component = this.subject({
+            value: 'some text',
+            focus: true
+        });
+
+        this.render();
+
+        expect(component.$().attr('autofocus')).to.be.ok;
+    });
 });
