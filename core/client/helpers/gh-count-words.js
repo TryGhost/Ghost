2a91a1071e4d4d7a5a4bfea971a77b6eed6a9349
Jason Williams <jaswilli@gmail.com>
2015-02-09 15:57:50 +0000

    Update Ember to 1.10.0
    No issue.
    - Ember@1.10.0
    - Update grunt-ember-templates to version that supports HTMLBars.
    - Update Gruntfile.js to compile templates with HTMLBars.
    - Convert Handlebars code to its HTMLBars equivalent.

--- core/client/helpers/gh-count-words.js
+++ core/client/helpers/gh-count-words.js
@@ -1,11 +1,20 @@
 import counter from 'ghost/utils/word-count';
 
-var countWords = Ember.Handlebars.makeBoundHelper(function (markdown) {
+var countWords = Ember.HTMLBars.makeBoundHelper(function (arr /* hashParams */) {
+    if (!arr || !arr.length) {
+        return;
+    }
+
+    var markdown,
+        count;
+
+    markdown = arr[0] || '';
+
     if (/^\s*$/.test(markdown)) {
         return '0 words';
     }
 
-    var count = counter(markdown || '');
+    count = counter(markdown);
 
     return count + (count === 1 ? ' word' : ' words');
 });
