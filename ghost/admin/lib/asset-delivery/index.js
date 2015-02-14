0bc879edbeff51c7efbcee8851862df4af5d02c0
Matt Enlow <matt@novaugust.net>
2015-02-13 21:03:16 -0700

    Build assets with ember, and serve with ghost

---
+++ ghost/admin/lib/asset-delivery/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'asset-delivery',
+    postBuild: function (results) {
+      var fs = this.project.require('fs-extra');
+
+      fs.copySync(results.directory + '/index.html', '../server/views/default.hbs');
+    }
+};
