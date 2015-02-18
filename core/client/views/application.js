ecb6d4066beddb0a32a8be0bbba61b988932f847
Jason Williams <jaswilli@gmail.com>
2015-02-18 17:11:02 +0000

    Run listener callbacks in Ember run-loops
    Closes #4871

--- core/client/views/application.js
+++ core/client/views/application.js
@@ -1,37 +1,53 @@
 import mobileQuery from 'ghost/utils/mobile';
-import bind from 'ghost/utils/bind';
 
 var ApplicationView = Ember.View.extend({
     elementId: 'container',
 
-    setupGlobalMobileNav: function () {
+    didInsertElement: function () {
         // #### Navigating within the sidebar closes it.
         var self = this;
+
         $('body').on('click tap', '.js-nav-item', function () {
-            if (mobileQuery.matches) {
-                self.set('controller.showGlobalMobileNav', false);
-            }
+            Ember.run(function () {
+                if (mobileQuery.matches) {
+                    self.set('controller.showGlobalMobileNav', false);
+                }
+            });
         });
 
         // #### Close the nav if mobile and clicking outside of the nav or not the burger toggle
         $('.js-nav-cover').on('click tap', function () {
-            var isOpen = self.get('controller.showGlobalMobileNav');
-            if (isOpen) {
-                self.set('controller.showGlobalMobileNav', false);
-            }
+            Ember.run(function () {
+                var isOpen = self.get('controller.showGlobalMobileNav');
+
+                if (isOpen) {
+                    self.set('controller.showGlobalMobileNav', false);
+                }
+            });
         });
 
-        // #### Listen to the viewport and change user-menu dropdown triangle classes accordingly
-        mobileQuery.addListener(this.swapUserMenuDropdownTriangleClasses);
-        this.swapUserMenuDropdownTriangleClasses(mobileQuery);
-    }.on('didInsertElement'),
-
-    swapUserMenuDropdownTriangleClasses: function (mq) {
-        if (mq.matches) {
-            $('.js-user-menu-dropdown-menu').removeClass('dropdown-triangle-top-right ').addClass('dropdown-triangle-bottom');
-        } else {
-            $('.js-user-menu-dropdown-menu').removeClass('dropdown-triangle-bottom').addClass('dropdown-triangle-top-right');
+        function swapUserMenuDropdownTriangleClasses(mq) {
+            if (mq.matches) {
+                $('.js-user-menu-dropdown-menu').removeClass('dropdown-triangle-top-right ').addClass('dropdown-triangle-bottom');
+            } else {
+                $('.js-user-menu-dropdown-menu').removeClass('dropdown-triangle-bottom').addClass('dropdown-triangle-top-right');
+            }
         }
+
+        // #### Listen to the viewport and change user-menu dropdown triangle classes accordingly
+        this.set('swapUserMenuDropdownTriangleClasses', Ember.run.bind(this, swapUserMenuDropdownTriangleClasses));
+
+        mobileQuery.addListener(this.get('swapUserMenuDropdownTriangleClasses'));
+        swapUserMenuDropdownTriangleClasses(mobileQuery);
+
+        this.set('closeGlobalMobileNavOnDesktop', Ember.run.bind(this, function closeGlobalMobileNavOnDesktop(mq) {
+            if (!mq.matches) {
+                // Is desktop sized
+                this.set('controller.showGlobalMobileNav', false);
+            }
+        }));
+
+        mobileQuery.addListener(this.get('closeGlobalMobileNavOnDesktop'));
     },
 
     showGlobalMobileNavObserver: function () {
@@ -42,20 +58,10 @@ var ApplicationView = Ember.View.extend({
         }
     }.observes('controller.showGlobalMobileNav'),
 
-    setupCloseNavOnDesktop: function () {
-        this.set('closeGlobalMobileNavOnDesktop', bind(function closeGlobalMobileNavOnDesktop(mq) {
-            if (!mq.matches) {
-                // Is desktop sized
-                this.set('controller.showGlobalMobileNav', false);
-            }
-        }, this));
-
-        mobileQuery.addListener(this.closeGlobalMobileNavOnDesktop);
-    }.on('didInsertElement'),
-
-    removeCloseNavOnDesktop: function () {
-        mobileQuery.removeListener(this.closeGlobalMobileNavOnDesktop);
-    }.on('willDestroyElement'),
+    willDestroyElement: function () {
+        mobileQuery.removeListener(this.get('closeGlobalMobileNavOnDesktop'));
+        mobileQuery.removeListener(this.get('swapUserMenuDropdownTriangleClasses'));
+    },
 
     toggleSettingsMenuBodyClass: function () {
         $('body').toggleClass('settings-menu-expanded', this.get('controller.showSettingsMenu'));
