40958e13a8dbededefb42a1652cd805ef6771bd5
Jason Williams <jaswilli@gmail.com>
2015-02-18 18:11:42 -0600

    Fix up ember-cli generated files and misc jscs/jshint errors

--- core/client/lib/asset-delivery/index.js
+++ core/client/lib/asset-delivery/index.js
@@ -1,8 +1,8 @@
 module.exports = {
     name: 'asset-delivery',
     postBuild: function (results) {
-      var fs = this.project.require('fs-extra');
+        var fs = this.project.require('fs-extra');
 
-      fs.copySync(results.directory + '/index.html', '../server/views/default.hbs');
+        fs.copySync(results.directory + '/index.html', '../server/views/default.hbs');
     }
 };
