2450f181702c1c8f7896e76e8c23a31f68c1146e
Hannah Wolfe <erisds@gmail.com>
2015-02-28 12:53:00 +0000

    Make the {{navigation}} helper global
    refs #4535
    
    - Rather than storing navigation data as a top level key, store it as @blog.navigation
    - Reference the global data from the helper

--- core/test/unit/server_helpers/navigation_spec.js
+++ core/test/unit/server_helpers/navigation_spec.js
@@ -1,4 +1,4 @@
-/*globals describe, before, it*/
+/*globals describe, before, beforeEach, it*/
 /*jshint expr:true*/
 var should         = require('should'),
     hbs            = require('express-hbs'),
@@ -9,6 +9,13 @@ var should         = require('should'),
     helpers        = require('../../../server/helpers');
 
 describe('{{navigation}} helper', function () {
+    var runHelper = function (data) {
+            return function () {
+                helpers.navigation(data);
+            };
+        },
+        optionsData;
+
     before(function (done) {
         utils.loadHelpers();
         hbs.express3({partialsDir: [utils.config.paths.helperTemplates]});
@@ -17,27 +24,43 @@ describe('{{navigation}} helper', function () {
         });
     });
 
+    beforeEach(function () {
+        optionsData = {
+            data: {
+                blog: {
+                    navigation: []
+                },
+                root: {
+                    relativeUrl: ''
+                }
+            }
+        };
+    });
+
     it('has loaded navigation helper', function () {
         should.exist(handlebars.helpers.navigation);
     });
 
     it('should throw errors on invalid data', function () {
-        var runHelper = function (data) {
-            return function () {
-                helpers.navigation.call(data);
-            };
-        };
+        // Test 1: navigation = string
+        optionsData.data.blog.navigation = 'not an object';
+        runHelper(optionsData).should.throwError('navigation data is not an object or is a function');
 
-        runHelper('not an object').should.throwError('navigation data is not an object or is a function');
-        runHelper(function () {}).should.throwError('navigation data is not an object or is a function');
+        // Test 2: navigation = function
+        optionsData.data.blog.navigation = function () {};
+        runHelper(optionsData).should.throwError('navigation data is not an object or is a function');
 
-        runHelper({navigation: [{label: 1, url: 'bar'}]}).should.throwError('Invalid value, Url and Label must be strings');
-        runHelper({navigation: [{label: 'foo', url: 1}]}).should.throwError('Invalid value, Url and Label must be strings');
+        // Test 3: invalid label
+        optionsData.data.blog.navigation = [{label: 1, url: 'bar'}];
+        runHelper(optionsData).should.throwError('Invalid value, Url and Label must be strings');
+
+        // Test 4: invalid url
+        optionsData.data.blog.navigation = [{label: 'foo', url: 1}];
+        runHelper(optionsData).should.throwError('Invalid value, Url and Label must be strings');
     });
 
     it('can render empty nav', function () {
-        var navigation = {navigation:[]},
-            rendered = helpers.navigation.call(navigation);
+        var rendered = helpers.navigation(optionsData);
 
         should.exist(rendered);
         rendered.string.should.be.equal('');
@@ -45,9 +68,11 @@ describe('{{navigation}} helper', function () {
 
     it('can render one item', function () {
         var singleItem = {label: 'Foo', url: '/foo'},
-            navigation = {navigation: [singleItem]},
-            rendered = helpers.navigation.call(navigation),
-            testUrl = 'href="' + utils.config.url + '/foo"';
+            testUrl = 'href="' + utils.config.url + '/foo"',
+            rendered;
+
+        optionsData.data.blog.navigation = [singleItem];
+        rendered = helpers.navigation(optionsData);
 
         should.exist(rendered);
         rendered.string.should.containEql('li');
@@ -58,10 +83,12 @@ describe('{{navigation}} helper', function () {
     it('can render multiple items', function () {
         var firstItem = {label: 'Foo', url: '/foo'},
             secondItem = {label: 'Bar Baz Qux', url: '/qux'},
-            navigation = {navigation: [firstItem, secondItem]},
-            rendered = helpers.navigation.call(navigation),
             testUrl = 'href="' + utils.config.url + '/foo"',
-            testUrl2 = 'href="' + utils.config.url + '/qux"';
+            testUrl2 = 'href="' + utils.config.url + '/qux"',
+            rendered;
+
+        optionsData.data.blog.navigation = [firstItem, secondItem];
+        rendered = helpers.navigation(optionsData);
 
         should.exist(rendered);
         rendered.string.should.containEql('nav-foo');
@@ -73,11 +100,11 @@ describe('{{navigation}} helper', function () {
     it('can annotate the current url', function () {
         var firstItem = {label: 'Foo', url: '/foo'},
             secondItem = {label: 'Bar', url: '/qux'},
-            navigation = {
-                relativeUrl: '/foo',
-                navigation: [firstItem, secondItem]
-            },
-            rendered = helpers.navigation.call(navigation);
+            rendered;
+
+        optionsData.data.blog.navigation = [firstItem, secondItem];
+        optionsData.data.root.relativeUrl = '/foo';
+        rendered = helpers.navigation(optionsData);
 
         should.exist(rendered);
         rendered.string.should.containEql('nav-foo');
