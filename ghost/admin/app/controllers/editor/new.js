8f354ddbc264663341bbe705409ae21918362fba
Hannah Wolfe <erisds@gmail.com>
2015-04-11 16:44:25 +0100

    Don't do autosave on new post
    closes #5130
    
    - rather than checking for a model change, noop autosave on new controller

--- ghost/admin/app/controllers/editor/new.js
+++ ghost/admin/app/controllers/editor/new.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 import EditorControllerMixin from 'ghost/mixins/editor-base-controller';
 
 var EditorNewController = Ember.Controller.extend(EditorControllerMixin, {
+    // Overriding autoSave on the base controller, as the new controller shouldn't be autosaving
+    autoSave: Ember.K,
     actions: {
         /**
           * Redirect to editor after the first save
