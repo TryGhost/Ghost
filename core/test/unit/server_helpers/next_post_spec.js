433956c1022812bc06f679c0b5666d7391921f23
Matt Enlow <matt@novaugust.net>
2015-04-16 13:40:32 -0600

    Add post preview via uuid (/p/:uuid) Refs #5097
    - All drafts will show a preview link (this needs real css)
    - Published posts will redirect
    - prev/next post helpers only activate on published posts
    - Powered by ~10 pints between the two of us (@ErisDS, @novaugust)

--- core/test/unit/server_helpers/next_post_spec.js
+++ core/test/unit/server_helpers/next_post_spec.js
@@ -40,6 +40,7 @@ describe('{{next_post}} helper', function () {
                 optionsData = {name: 'next_post', fn: fn, inverse: inverse};
 
             helpers.prev_post.call({html: 'content',
+                status: 'published',
                 markdown: 'ff',
                 title: 'post2',
                 slug: 'current',
@@ -117,6 +118,47 @@ describe('{{next_post}} helper', function () {
             helpers.prev_post.call({}, optionsData).then(function () {
                 fn.called.should.be.false;
                 inverse.called.should.be.true;
+                done();
+            }).catch(function (err) {
+                done(err);
+            });
+        });
+    });
+
+    describe('for unpublished post', function () {
+        var sandbox;
+
+        beforeEach(function () {
+            sandbox = sinon.sandbox.create();
+            utils.loadHelpers();
+            sandbox.stub(api.posts, 'read', function (options) {
+                if (options.include === 'next') {
+                    return Promise.resolve({
+                        posts: [{slug: '/current/', title: 'post 2', next: {slug: '/next/', title: 'post 3'}}]
+                    });
+                }
+            });
+        });
+
+        afterEach(function () {
+            sandbox.restore();
+        });
+
+        it('shows \'else\' template', function (done) {
+            var fn = sinon.spy(),
+                inverse = sinon.spy(),
+                optionsData = {name: 'next_post', fn: fn, inverse: inverse};
+
+            helpers.prev_post.call({html: 'content',
+                status: 'published',
+                markdown: 'ff',
+                title: 'post2',
+                slug: 'current',
+                created_at: new Date(0),
+                url: '/current/'}, optionsData)
+            .then(function () {
+                fn.called.should.be.true;
+                inverse.called.should.be.false;
                 done();
             }).catch(function (err) {
                 done(err);
