075c4fbf4632896bc099d1bcfb5e6bd938500155
Artyom Fedenko <artyomfedenko@hotmail.com>
2015-05-02 22:56:33 +0300

    Added support for all letters in word-count (not only ASCII) and added xregexp bower module to a project

--- core/client/app/utils/word-count.js
+++ core/client/app/utils/word-count.js
@@ -1,7 +1,12 @@
 // jscs: disable
+/* global XRegExp */
+
 function wordCount(s) {
+
+    var nonANumLetters = new XRegExp("[^\\s\\d\\p{L}]", "g"); // all non-alphanumeric letters regexp
+
     s = s.replace(/<(.|\n)*?>/g, ' '); // strip tags
-    s = s.replace(/[^\w\s]/g, ''); // ignore non-alphanumeric letters
+    s = s.replace(nonANumLetters, ''); // ignore non-alphanumeric letters
     s = s.replace(/(^\s*)|(\s*$)/gi, ''); // exclude starting and ending white-space
     s = s.replace(/\n /gi, ' '); // convert newlines to spaces
     s = s.replace(/\n+/gi, ' ');
