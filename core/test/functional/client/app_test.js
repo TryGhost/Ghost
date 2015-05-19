1cecd79ccd502398f0aa92326604cf7dca24ff45
cobbspur <cobbspur@hotmail.com>
2015-05-19 14:21:39 +0100

    Add UI link classes
    No Issue
    
    - Adds contextual classnames to Nav Bar links
    - Various test updates
    
    TODO
    - Increase test coverage for new admin UI
    - Fix up Caspertests for new Admin UI

--- core/test/functional/client/app_test.js
+++ core/test/functional/client/app_test.js
@@ -10,47 +10,47 @@ CasperTest.begin('Admin navigation bar is correct', 45, function suite(test) {
     });
 
     casper.then(function testNavItems() {
-        var logoHref = this.getElementAttribute('.ghost-logo', 'href'),
-            contentHref = this.getElementAttribute('.nav-content', 'href'),
-            editorHref = this.getElementAttribute('.nav-new', 'href'),
-            settingsHref = this.getElementAttribute('.nav-settings', 'href');
+        var logoHref = this.getElementAttribute('.gh-nav-footer-sitelink', 'href'),
+            contentHref = this.getElementAttribute('.gh-nav-main-content', 'href'),
+            editorHref = this.getElementAttribute('.gh-nav-main-editor', 'href'),
+            settingsGeneralHref = this.getElementAttribute('.gh-nav-settings-general', 'href');
 
         // Logo
-        test.assertExists('.ghost-logo', 'Ghost logo home page link exists');
+        test.assertExists('.gh-nav-footer-sitelink', 'Ghost logo home page link exists');
         test.assertEquals(logoHref, 'http://127.0.0.1:2369/', 'Ghost logo link href is correct');
 
         // Content
-        test.assertExists('.nav-content', 'Content nav item exists');
-        test.assertSelectorHasText('.nav-content', 'Content', 'Content nav item has correct text');
+        test.assertExists('.gh-nav-main-content', 'Content nav item exists');
+        test.assertSelectorHasText('.gh-nav-main-content', 'Content', 'Content nav item has correct text');
         test.assertEquals(contentHref, '/ghost/', 'Content href is correct');
-        test.assertExists('.nav-content.active', 'Content nav item is not marked active');
+        test.assertExists('.gh-nav-main-content.active', 'Content nav item is not marked active');
 
         // Editor
-        test.assertExists('.nav-new', 'Editor nav item exists');
-        test.assertSelectorHasText('.nav-new', 'New Post', 'Editor nav item has correct text');
+        test.assertExists('.gh-nav-main-editor', 'Editor nav item exists');
+        test.assertSelectorHasText('.gh-nav-main-editor', 'New Post', 'Editor nav item has correct text');
         test.assertEquals(editorHref, '/ghost/editor/', 'Editor href is correct');
-        test.assertDoesntExist('.nav-new.active', 'Editor nav item is not marked active');
+        test.assertDoesntExist('.gh-nav-main-editor.active', 'Editor nav item is not marked active');
 
         // Settings
-        test.assertExists('.nav-settings', 'Settings nav item exists');
-        test.assertSelectorHasText('.nav-settings', 'Settings', 'Settings nav item has correct text');
-        test.assertEquals(settingsHref, '/ghost/settings/', 'Settings href is correct');
-        test.assertDoesntExist('.nav-settings.active', 'Settings nav item is marked active');
+        test.assertExists('.gh-nav-settings-general', 'Settings nav item exists');
+        test.assertSelectorHasText('.gh-nav-settings-general', 'General', 'Settings nav item has correct text');
+        test.assertEquals(settingsGeneralHref, '/ghost/settings/general/', 'Settings href is correct');
+        test.assertDoesntExist('.gh-nav-settings-general.active', 'Settings nav item is marked active');
     });
 
     casper.then(function testHelpMenuNotVisible() {
-        test.assertExists('.help-menu', 'Help menu nav item exists');
-        test.assertNotExists('.help-menu .dropdown.open', 'Help menu should not be visible');
+        test.assertExists('.gh-help-button', 'Help menu nav item exists');
+        test.assertNotExists('.gh-help-button .dropdown.open', 'Help menu should not be visible');
     });
 
-    casper.thenClick('.help-menu .nav-label');
-    casper.waitForSelector('.help-menu .dropdown.open', function then() {
+    casper.thenClick('.gh-help-button');
+    casper.waitForSelector('.dropdown', function then() {
         var supportHref = this.getElementAttribute('.help-menu-support', 'href'),
             tweetHref = this.getElementAttribute('.help-menu-tweet', 'href'),
             howtoHref = this.getElementAttribute('.help-menu-how-to', 'href'),
             wishlistHref = this.getElementAttribute('.help-menu-wishlist', 'href');
 
-        test.assertVisible('.help-menu .dropdown-menu', 'Help menu should be visible');
+        test.assertVisible('.dropdown-menu', 'Help menu should be visible');
 
         test.assertExists('.help-menu-support', 'Support menu item exists');
         test.assertSelectorHasText('.help-menu-support', 'Support Center', 'Support menu item has correct text');
@@ -87,22 +87,22 @@ CasperTest.begin('Admin navigation bar is correct', 45, function suite(test) {
         casper.waitWhileVisible('.modal-container', function onSuccess() {
             test.assert(true, 'clicking close should remove the markdown help modal');
         });
-    }, casper.failOnTimeout(test, 'WaitForSelector .help-menu .dropdown failed'));
+    }, casper.failOnTimeout(test, 'WaitForSelector .gh-help-menu .ember-view.open failed'));
 
     casper.then(function testUserMenuNotVisible() {
-        test.assertExists('.user-menu', 'User menu nav item exists');
-        test.assertNotExists('.user-menu .dropdown.open', 'User menu should not be visible');
+        test.assertExists('.gh-nav-menu .gh-nav-menu-details-user', 'User menu nav item exists');
+        test.assertNotExists('.gh-nav-menu-details-user .user-menu-profile', 'User menu should not be visible');
     });
 
-    casper.thenClick('.user-menu .nav-label');
-    casper.waitForSelector('.user-menu .dropdown.open', function then() {
+    casper.thenClick('.gh-nav-menu');
+    casper.waitForSelector('.dropdown', function then() {
         var profileHref = this.getElementAttribute('.user-menu-profile', 'href'),
             signoutHref = this.getElementAttribute('.user-menu-signout', 'href');
 
-        test.assertVisible('.user-menu .dropdown-menu', 'User menu should be visible');
+        test.assertVisible('.dropdown-item.user-menu-profile', 'User menu should be visible');
 
-        test.assertExists('.user-menu-profile', 'Profile menu item exists');
-        test.assertSelectorHasText('.user-menu-profile', 'Your Profile',
+        test.assertExists('.dropdown-item.user-menu-profile', 'Profile menu item exists');
+        test.assertSelectorHasText('.dropdown-item.user-menu-profile', 'Your Profile',
             'Profile menu item has correct text');
         test.assertEquals(profileHref, '/ghost/settings/users/' + newUser.slug + '/', 'Profile href is correct');
 
