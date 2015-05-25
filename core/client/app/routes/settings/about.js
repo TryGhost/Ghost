c3bb7cb396bdaa546eed3b502a11c9b48d21995d
cobbspur <cobbspur@hotmail.com>
2015-05-25 17:00:42 +0100

    Fix route for About page
    No Issue
    
    - Routes,  views and controllers updated for about page
    - fix up tests and add redirect check

--- core/client/app/routes/settings/about.js
+++ core/client/app/routes/settings/about.js
@@ -3,32 +3,8 @@ import loadingIndicator from 'ghost/mixins/loading-indicator';
 import styleBody from 'ghost/mixins/style-body';
 
 var SettingsAboutRoute = AuthenticatedRoute.extend(styleBody, loadingIndicator, {
-    titleToken: 'About',
-
-    classNames: ['settings-view-about'],
-
-    cachedConfig: false,
-    model: function () {
-        var cachedConfig = this.get('cachedConfig'),
-            self = this;
-        if (cachedConfig) {
-            return cachedConfig;
-        }
-
-        return ic.ajax.request(this.get('ghostPaths.url').api('configuration'))
-            .then(function (configurationResponse) {
-                var configKeyValues = configurationResponse.configuration;
-                cachedConfig = {};
-                configKeyValues.forEach(function (configKeyValue) {
-                    cachedConfig[configKeyValue.key] = configKeyValue.value;
-                });
-                self.set('cachedConfig', cachedConfig);
-                return cachedConfig;
-            });
-    },
-
-    renderTemplate: function () {
-        this.render('settings/about', {into: 'application'});
+    beforeModel: function () {
+        this.transitionTo('about');
     }
 });
 
