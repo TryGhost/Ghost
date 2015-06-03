43922564b68bb3cd1261d7add7d8684663d32093
Austin Burdine <acburdine@gmail.com>
2015-06-02 20:56:42 -0600

    update Ember to 1.12.1/ember-data to 1.0.0-beta.18
    No issue
    - fixed "{{#each}}" helper in templates to use block syntax
    - fixed deprecated ember.controller getter/setter function to use new syntax
    - removed unnecessary pass-protect route view

--- core/client/app/mixins/settings-menu-controller.js
+++ core/client/app/mixins/settings-menu-controller.js
@@ -3,16 +3,17 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
     application: Ember.inject.controller(),
 
-    isViewingSubview: Ember.computed('application.showSettingsMenu', function (key, value) {
-        // Not viewing a subview if we can't even see the PSM
-        if (!this.get('application.showSettingsMenu')) {
+    isViewingSubview: Ember.computed('application.showSettingsMenu', {
+        get: function () {
             return false;
-        }
-        if (arguments.length > 1) {
+        },
+        set: function (key, value) {
+            // Not viewing a subview if we can't even see the PSM
+            if (!this.get('application.showSettingsMenu')) {
+                return false;
+            }
             return value;
         }
-
-        return false;
     }),
 
     actions: {
