d37a51fb9003d6e0f5249c410e63822c2e2aadbd
Matt Enlow <matt@novaugust.net>
2015-06-09 09:25:45 -0700

    Zelda menu z-index and viewport transition finishes
    - [x] Mobilemenu button is missing from `content` and `editor` views
    - [x] Mobilemenu pane slides entire content over, should expand over-top-of-content
    - [x] Mobilemenu can't be closed
    - [x] gh-view-title no longer generates an extra div; it is the h2.
    - [x] gh-autonav-toggle closes the mobile menu on mobile. renamed `gh-menu-toggle`
    - [ ] There is weird behaviour with mobile menu when changing from big=>small=>big viewport sizes
    - ~~[ ] (Future issue) Ghost should remember (localstorage?) whether desktop menu is expanded or collapsed~~

--- ghost/admin/app/components/gh-view-title.js
+++ ghost/admin/app/components/gh-view-title.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    tagName: 'h2',
+    classNames: ['view-title'],
     actions: {
         openMobileMenu () {
             this.sendAction('openMobileMenu');
