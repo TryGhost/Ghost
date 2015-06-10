e5718cfb38f74d337dcf1631d198b86bbb75d8b2
Alex Kleissner <alex@keen.io>
2015-06-10 12:18:31 -0700

    Move uncapitalize into its own file.
    refs #5286
    - Moved the logic into its own file
    - Added unit tests

--- core/server/middleware/index.js
+++ core/server/middleware/index.js
@@ -24,6 +24,7 @@ var api            = require('../api'),
     utils          = require('../utils'),
     sitemapHandler = require('../data/xml/sitemap/handler'),
     decideIsAdmin  = require('./decide-is-admin'),
+    uncapitalise   = require('./uncapitalise'),
 
     blogApp,
     setupMiddleware;
@@ -150,29 +151,6 @@ function redirectToSetup(req, res, next) {
     }).catch(function handleError(err) {
         return next(new Error(err));
     });
-}
-
-// Detect uppercase in req.path
-function uncapitalise(req, res, next) {
-    var pathToTest = req.path,
-        isSignupOrReset = req.path.match(/(\/ghost\/(signup|reset)\/)/i),
-        isAPI = req.path.match(/(\/ghost\/api\/v[\d\.]+\/.*?\/)/i);
-
-    if (isSignupOrReset) {
-        pathToTest = isSignupOrReset[1];
-    }
-
-    // Do not lowercase anything after /api/v0.1/ to protect :key/:slug
-    if (isAPI) {
-        pathToTest = isAPI[1];
-    }
-
-    if (/[A-Z]/.test(pathToTest)) {
-        res.set('Cache-Control', 'public, max-age=' + utils.ONE_YEAR_S);
-        res.redirect(301, req.url.replace(pathToTest, pathToTest.toLowerCase()));
-    } else {
-        next();
-    }
 }
 
 // ### ServeSharedFile Middleware
