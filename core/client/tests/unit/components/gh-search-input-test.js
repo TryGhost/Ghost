8ec01e68ffb0f2f9d89f5f0248973e8fa2fa9ace
Kevin Ansfield <kevin@lookingsideways.co.uk>
2015-07-06 11:00:54 +0100

    WIP: autocomplete search input

---
+++ core/client/tests/unit/components/gh-search-input-test.js
@@ -0,0 +1,26 @@
+/* jshint expr:true */
+import { expect } from 'chai';
+import {
+  describeComponent,
+  it
+} from 'ember-mocha';
+
+describeComponent(
+  'gh-search-input',
+  'GhSearchInputComponent',
+  {
+    // specify the other units that are required for this test
+    // needs: ['component:foo', 'helper:bar']
+  },
+  function() {
+    it('renders', function() {
+      // creates the component instance
+      var component = this.subject();
+      expect(component._state).to.equal('preRender');
+
+      // renders the component on the page
+      this.render();
+      expect(component._state).to.equal('inDOM');
+    });
+  }
+);
