7d75129dfbb83a994e8c86d4e88b8ef8042d6a1c
Fernando Mendes <devfrmendes@gmail.com>
2015-07-21 15:12:32 +0100

    Adds gulpfile and updates gitignore

---
+++ koenig/kg-simplemde/gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+
+gulp.task('scripts', function() {
+  var js_files = ['./src/js/codemirror/*.js',
+    './src/js/typo.js',
+    './src/js/spell-checker.js',
+    './src/js/marked.js',
+    './src/js/simplemde.js'];
+
+  return gulp.src(js_files)
+    .pipe(concat('simplemde.min.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('styles', function() {
+  return gulp.src('./src/css/*.css')
+    .pipe(concat('simplemde.min.css'))
+    .pipe(gulp.dest('dist'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['scripts', 'styles']);
+
