bd78ff41ddf455ed1f6d64de07d5f4cf618bc053
Valentin Vago <zeropaper@irata.ch>
2015-07-28 11:42:59 +0200

    Allow use of round and append downsize options for excerpt helper

--- core/server/helpers/excerpt.js
+++ core/server/helpers/excerpt.js
@@ -14,28 +14,38 @@ excerpt = function (options) {
     var truncateOptions = (options || {}).hash || {},
         excerpt;
 
-    truncateOptions = _.pick(truncateOptions, ['words', 'characters']);
-    _.keys(truncateOptions).map(function (key) {
+    truncateOptions = _.pick(truncateOptions, ['words', 'characters', 'round', 'append']);
+    ['words', 'characters'].map(function (key) {
+        if (!truncateOptions[key]) { return; }
         truncateOptions[key] = parseInt(truncateOptions[key], 10);
     });
+    truncateOptions.round = !!truncateOptions.round;
+
+    if (!truncateOptions.words && !truncateOptions.characters) {
+        truncateOptions.words = 50;
+    }
 
     /*jslint regexp:true */
     excerpt = String(this.html);
     // Strip inline and bottom footnotes
     excerpt = excerpt.replace(/<a href="#fn.*?rel="footnote">.*?<\/a>/gi, '');
     excerpt = excerpt.replace(/<div class="footnotes"><ol>.*?<\/ol><\/div>/, '');
+
     // Strip other html
-    excerpt = excerpt.replace(/<\/?[^>]+>/gi, '');
-    excerpt = excerpt.replace(/(\r\n|\n|\r)+/gm, ' ');
+    if (!truncateOptions.round) {
+        excerpt = excerpt.replace(/<\/?[^>]+>/gi, '');
+        excerpt = excerpt.replace(/(\r\n|\n|\r)+/gm, ' ');
+        excerpt = downsize(excerpt, truncateOptions);
+    }
+    else {
+        excerpt = downsize(excerpt, truncateOptions);
+        // Strip other html
+        excerpt = excerpt.replace(/<\/?[^>]+>/gi, '');
+        excerpt = excerpt.replace(/(\r\n|\n|\r)+/gm, ' ');
+    }
     /*jslint regexp:false */
 
-    if (!truncateOptions.words && !truncateOptions.characters) {
-        truncateOptions.words = 50;
-    }
-
-    return new hbs.handlebars.SafeString(
-        downsize(excerpt, truncateOptions)
-    );
+    return new hbs.handlebars.SafeString(excerpt);
 };
 
 module.exports = excerpt;
