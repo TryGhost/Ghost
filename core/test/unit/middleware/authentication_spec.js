5070e1cdf6c19212ab9e894452d4615e89e0bd1d
Sebastian Gierlinger <s.gierlinger@me.com>
2015-08-09 12:50:05 +0200

    Auth tests
    - added tests for authentication middleware
    - changed use of auth strategies

--- core/test/unit/middleware/authentication_spec.js
+++ core/test/unit/middleware/authentication_spec.js
@@ -1,57 +1,145 @@
 /*globals describe, it, beforeEach, afterEach */
 /*jshint expr:true*/
-var sinon        = require('sinon'),
-    should       = require('should'),
-    passport     = require('passport'),
-    authenticate = require('../../../server/middleware/authenticate');
+var sinon           = require('sinon'),
+    should          = require('should'),
+    passport        = require('passport'),
+    authenticate    = require('../../../server/middleware/authenticate'),
+    BearerStrategy  = require('passport-http-bearer').Strategy,
+    user            = {id: 1},
+    info            = {scope: '*'},
+    token           = 'test_token';
 
 should.equal(true, true);
+
+function registerSuccessfulBearerStrategy() {
+    // register fake BearerStrategy which always authenticates
+    passport.use(new BearerStrategy(
+        function strategy(accessToken, done) {
+            accessToken.should.eql(token);
+            return done(null, user, info);
+        }
+    ));
+}
+
+function registerUnsuccessfulBearerStrategy() {
+    // register fake BearerStrategy which always authenticates
+    passport.use(new BearerStrategy(
+        function strategy(accessToken, done) {
+            accessToken.should.eql(token);
+            return done(null, false);
+        }
+    ));
+}
+
+function registerSuccessfulBearerStrategy() {
+    // register fake BearerStrategy which always authenticates
+    passport.use(new BearerStrategy(
+        function strategy(accessToken, done) {
+            accessToken.should.eql(token);
+            return done(null, user, info);
+        }
+    ));
+}
 
 describe('authenticate', function () {
     var res, req, next, sandbox;
 
     beforeEach(function () {
         sandbox = sinon.sandbox.create();
-
-        res = sinon.spy();
-        req = sinon.spy();
-        next = sinon.spy();
+        req = {};
+        res = {};
+        next = sandbox.spy();
     });
 
     afterEach(function () {
         sandbox.restore();
     });
 
-    it('should skip authentication if not hitting /ghost', function () {
-        req.path = '/tag/foo';
+    it('should skip authentication if not hitting /ghost', function (done) {
+        req.path = '/tag/foo/';
+        req.method = 'GET';
 
+        registerSuccessfulBearerStrategy();
         authenticate(req, res, next);
 
         next.called.should.be.true;
+        next.calledWith().should.be.true;
+        done();
     });
 
-    it('should authenticate if hitting /ghost/ auth endpoint', function (done) {
+    it('should skip authentication if hitting /ghost/api/v0.1/authenticaton/', function (done) {
         req.path = '/ghost/api/v0.1/authentication/';
+        req.method = 'GET';
+
+        registerSuccessfulBearerStrategy();
+        authenticate(req, res, next);
+
+        next.called.should.be.true;
+        next.calledWith().should.be.true;
+        done();
+    });
+
+    it('should skip authentication if hitting GET /ghost/api/v0.1/authenticaton/setup/', function (done) {
+        req.path = '/ghost/api/v0.1/authentication/setup/';
+        req.method = 'GET';
+
+        registerSuccessfulBearerStrategy();
+        authenticate(req, res, next);
+
+        next.called.should.be.true;
+        next.calledWith().should.be.true;
+        done();
+    });
+
+    it('should authentication if hitting PUT /ghost/api/v0.1/authenticaton/setup/', function (done) {
+        req.path = '/ghost/api/v0.1/authentication/setup/';
         req.method = 'PUT';
+        req.headers = {};
+        req.headers.authorization = 'Bearer ' + token;
 
-        sandbox.stub(passport, 'authenticate', function (method, options, cb) {
-            var user = {slug: 'user'},
-                info = {info: 'some'};
+        registerSuccessfulBearerStrategy();
+        authenticate(req, res, next);
 
-            method.should.equal('bearer');
-            options.session.should.be.false;
-            options.failWithError.should.be.true;
+        next.called.should.be.true;
+        next.calledWith(null, user, info).should.be.true;
+        done();
+    });
 
-            return function (req, res, next) {
-                cb(null, user, info);
-                next.called.should.be.true;
-                next.calledWith(null, user, info).should.be.true;
-                req.authInfo.should.equal(info);
-                req.user.should.equal(user);
-                done();
+    it('should authenticate if hitting /ghost/api/ endpoint', function (done) {
+        req.path = '/ghost/api/v0.1/test/';
+        req.method = 'PUT';
+        req.headers = {};
+        req.headers.authorization = 'Bearer ' + token;
+
+        registerSuccessfulBearerStrategy();
+        authenticate(req, res, next);
+
+        next.called.should.be.true;
+        next.calledWith(null, user, info).should.be.true;
+        done();
+    });
+
+    it('shouldn\'t authenticate if hitting /ghost/ auth endpoint with invalid credentials', function (done) {
+        res.status = {};
+        req.path = '/ghost/api/v0.1/test/';
+        req.method = 'PUT';
+        req.headers = {};
+        req.headers.authorization = 'Bearer ' + token;
+
+        registerUnsuccessfulBearerStrategy();
+
+        // stub res.status for error handling
+        sandbox.stub(res, 'status', function (statusCode) {
+            statusCode.should.eql(401);
+            return {
+                json: function (err) {
+                    err.errors[0].errorType.should.eql('NoPermissionError');
+                }
             };
         });
 
         authenticate(req, res, next);
+        next.called.should.be.false;
+        done();
     });
 });
