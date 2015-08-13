0009f55613b98ccb7d7c5768f9086618082bc9b3
Sergey Lukin <sergey@investing.com>
2015-08-13 23:19:37 +0300

    Add scheduled posts publisher
    - Introducing a scheduled task which runs every minute at **:01 and publishes
      draft posts with expired publish date
    
    P.S. This commit introduces 2 new npm dependencies: `moment` and `cron`
    
    `cron` is used to periodically run `./core/server/publisher.js` which
    publishes posts with expired publish date (a.k.a scheduled posts)
    
    `moment` is used to convert and format publish and current datetimes
    so that they could be numerically compared in order to detect posts with
    expired publish dates

---
+++ core/server/publisher.js
@@ -0,0 +1,49 @@
+var Promise      = require('bluebird'),
+    api          = require('./api'),
+    CronJob      = require('cron').CronJob,
+    moment       = require('moment');
+
+function GhostPublisher() {}
+
+// *This promise should always resolve to avoid halting Ghost::init*.
+GhostPublisher.prototype.init = function () {
+    // Set a cronjob task which publishes posts (currently with status = "draft")
+    // with expired publish datetime.
+    // Because publish datetime is usually set per minutes (with seconds being set
+    // to "00") we will cover good auto-publish timing by running the task
+    // on second "02" of every minute.
+    var job = new CronJob({
+        cronTime: '01 * * * * *',
+        onTick: function () {
+            // Note context: {intenal: true} there in both `.browse` and `.edit` calls?
+            // It's done so that to avoid authorization check
+            api.posts.browse({status: 'draft', context: {internal: true}}).then(function (response) {
+                response.posts.forEach(function (post) {
+                    // Normalize both post publish datetime and current datetime to
+                    // Universal Time in Unix Timestamp format so that they could be
+                    // numerically compared
+                    var publishedTimestamp = moment(post.published_at).utc().format('X'),
+                        currentTimestamp = moment().utc().format('X');
+
+                    if (publishedTimestamp < currentTimestamp) {
+                        console.log('Publishing post #' + post.id);
+
+                        api.posts.edit({
+                            posts: [{status: 'published'}]
+                        }, {
+                            id: post.id,
+                            context: {internal: true}
+                        });
+                    }
+                });
+            });
+        },
+        start: false,
+        timeZone: 'GMT'
+    });
+    job.start();
+
+    return Promise.resolve();
+};
+
+module.exports = new GhostPublisher();
