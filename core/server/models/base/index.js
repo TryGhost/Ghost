bd857fba433b3d23cdb6f11b85edebd8deca3f41
rfpe <dodjocata@yahoo.com.br>
2015-08-18 00:19:07 -0300

    Completed harvesting /server/models/ strings #5617

--- core/server/models/base/index.js
+++ core/server/models/base/index.js
@@ -19,6 +19,7 @@ var _          = require('lodash'),
     validation = require('../../data/validation'),
     baseUtils  = require('./utils'),
     pagination = require('./pagination'),
+    i18n       = require('../../i18n'),
 
     ghostBookshelf;
 
@@ -127,7 +128,7 @@ ghostBookshelf.Model = ghostBookshelf.Model.extend({
         } else if (options.context && options.context.internal) {
             return 1;
         } else {
-            errors.logAndThrowError(new Error('missing context'));
+            errors.logAndThrowError(new Error(i18n.t('errors.models.base.index.missingContext')));
         }
     },
 
