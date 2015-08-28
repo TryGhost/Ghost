750497b63b542cf9eac73e7484e112ae269e0974
Kevin Ansfield <kevin@lookingsideways.co.uk>
2015-08-28 11:52:38 +0100

    Fix broken content mgmt -> editor links on mobile
    refs #5652
    - add `ember-resize` addon that registers a single resize event handler and exposes it as a service and mixin
    - add a component that wraps the posts list and content preview and exposes a `previewIsHidden` property
    - use the `previewIsHidden` property in `gh-posts-list-item` to switch the item's link between the editor and the preview
    - add `display: none` to the preview pane when in mobile so that we can test it's visibility

---
+++ core/client/app/components/gh-content-view-container.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    tagName: 'section',
+    classNames: ['gh-view', 'content-view-container'],
+
+    previewIsHidden: false,
+
+    resizeService: Ember.inject.service(),
+
+    calculatePreviewIsHidden: function () {
+        this.set('previewIsHidden', !this.$('.content-preview').is(':visible'));
+    },
+
+    didInsertElement: function () {
+        this._super(...arguments);
+        this.calculatePreviewIsHidden();
+        this.get('resizeService').on('debouncedDidResize',
+            Ember.run.bind(this, this.calculatePreviewIsHidden));
+    }
+});
