c6534b0c8854a0868098fafc4d1b3845f9077a49
vdemedes <vdemedes@gmail.com>
2015-10-06 15:36:56 +0200

    Refactor require-tree and split it into models
    closes #5492
    - remove core/server/require-tree.js and split it into modules
    - add read-directory module to recursively read directories
    - add validate-themes module to scan themes and return errors/warnings
    - add parse-package-json module to parse json and validate requirements
    - rewrite core/server/models/index.js to manually require models

--- core/server/models/index.js
+++ core/server/models/index.js
@@ -1,61 +1,69 @@
-var _       = require('lodash'),
-    Promise = require('bluebird'),
-    requireTree   = require('../require-tree'),
+/**
+ * Dependencies
+ */
+
+var Promise = require('bluebird'),
+    _ = require('lodash'),
+
+    exports,
     models;
 
-models = {
-    excludeFiles: ['_messages', 'base', 'index.js'],
+/**
+ * Expose all models
+ */
 
-    // ### init
-    // Scan all files in this directory and then require each one and cache
-    // the objects exported onto this `models` object so that every other
-    // module can safely access models without fear of introducing circular
-    // dependency issues.
-    // @returns {Promise}
-    init: function init() {
-        var self = this;
+exports = module.exports;
 
-        // One off inclusion of Base file.
-        self.Base = require('./base');
+models = [
+    'accesstoken',
+    'app-field',
+    'app-setting',
+    'app',
+    'client-trusted-domain',
+    'client',
+    'permission',
+    'post',
+    'refreshtoken',
+    'role',
+    'settings',
+    'tag',
+    'user'
+];
 
-        // Require all files in this directory
-        return requireTree.readAll(__dirname, {followSymlinks: false}).then(function then(modelFiles) {
-            // For each found file, excluding those we don't want,
-            // we will require it and cache it here.
-            _.each(modelFiles, function each(path, fileName) {
-                // Return early if this fileName is one of the ones we want
-                // to exclude.
-                if (_.contains(self.excludeFiles, fileName)) {
-                    return;
-                }
+function init() {
+    exports.Base = require('./base');
 
-                // Require the file.
-                var file = require(path);
+    models.forEach(function (name) {
+        _.extend(exports, require('./' + name));
+    });
 
-                // Cache its `export` object onto this object.
-                _.extend(self, file);
-            });
+    return Promise.resolve();
+}
 
-            return;
-        });
-    },
-    // ### deleteAllContent
-    // Delete all content from the database (posts, tags, tags_posts)
-    deleteAllContent: function deleteAllContent() {
-        var self = this;
+/**
+ * TODO: move to some other place
+ */
 
-        return self.Post.findAll().then(function then(posts) {
-            return Promise.all(_.map(posts.toJSON(), function mapper(post) {
-                return self.Post.destroy({id: post.id});
+// ### deleteAllContent
+// Delete all content from the database (posts, tags, tags_posts)
+exports.deleteAllContent = function deleteAllContent() {
+    var self = this;
+
+    return self.Post.findAll().then(function then(posts) {
+        return Promise.all(_.map(posts.toJSON(), function mapper(post) {
+            return self.Post.destroy({id: post.id});
+        }));
+    }).then(function () {
+        return self.Tag.findAll().then(function then(tags) {
+            return Promise.all(_.map(tags.toJSON(), function mapper(tag) {
+                return self.Tag.destroy({id: tag.id});
             }));
-        }).then(function () {
-            return self.Tag.findAll().then(function then(tags) {
-                return Promise.all(_.map(tags.toJSON(), function mapper(tag) {
-                    return self.Tag.destroy({id: tag.id});
-                }));
-            });
         });
-    }
+    });
 };
 
-module.exports = models;
+/**
+ * Expose `init`
+ */
+
+exports.init = init;
