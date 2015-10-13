6398bba29f582efeda9b64b3dc338c81ccd05054
Kevin Ansfield <kevin@lookingsideways.co.uk>
2015-10-13 14:52:41 +0100

    Set up ember-cli-mirage and update existing acceptance tests
    refs #6039
    - adds ember-cli-mirage dependency
    - sets up mirage to match our API endpoints and responses
    - adds fixture data for settings that are always present for all blogs
    - converts existing acceptance tests to use mirage

--- core/client/config/environment.js
+++ core/client/config/environment.js
@@ -39,6 +39,9 @@ module.exports = function (environment) {
         ENV.APP.LOG_TRANSITIONS = true;
         ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
         ENV.APP.LOG_VIEW_LOOKUPS = true;
+        ENV['ember-cli-mirage'] = {
+            enabled: false
+        };
     }
 
     if (environment === 'test') {
