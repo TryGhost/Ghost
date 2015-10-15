015e6cdf13ddebda6a70c2b1fd575d245e2c32ad
Kevin Ansfield <kevin@lookingsideways.co.uk>
2015-10-15 12:43:04 +0100

    Fix "revoke invite" feature on /team page
    refs #5947
    - override the `findRecord` method of the user adapter to always include "status=all" when querying by ID

--- core/client/app/adapters/user.js
+++ core/client/app/adapters/user.js
@@ -5,6 +5,17 @@ export default ApplicationAdapter.extend({
         return this.findQuery(store, type, {id: id, status: 'all'});
     },
 
+    // TODO: This is needed because the API currently expects you to know the
+    // status of the record before retrieving by ID. Quick fix is to always
+    // include status=all in the query
+    findRecord: function (store, type, id, snapshot) {
+        let url = this.buildIncludeURL(store, type.modelName, id, snapshot, 'findRecord');
+
+        url += '&status=all';
+
+        return this.ajax(url, 'GET');
+    },
+
     findAll: function (store, type, id) {
         return this.query(store, type, {id: id, status: 'all'});
     }
