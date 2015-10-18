87e54c0d54696af6361b8704de1049298f2ff935
Austin Burdine <acburdine@gmail.com>
2015-10-18 13:17:02 -0500

    deps: ember-simple-auth@1.0.0
    closes #5951
    - update to esa 1.0

--- core/client/app/authenticators/oauth2.js
+++ core/client/app/authenticators/oauth2.js
@@ -1,8 +1,18 @@
 import Ember from 'ember';
-import Authenticator from 'simple-auth-oauth2/authenticators/oauth2';
+import Authenticator from 'ember-simple-auth/authenticators/oauth2-password-grant';
 
 export default Authenticator.extend({
     config: Ember.inject.service(),
+    ghostPaths: Ember.inject.service('ghost-paths'),
+
+    serverTokenEndpoint: Ember.computed('ghostPaths.apiRoot', function () {
+        return this.get('ghostPaths.apiRoot') + '/authentication/token';
+    }),
+
+    serverTokenRevocationEndpoint: Ember.computed('ghostPaths.apiRoot', function () {
+        return this.get('ghostPaths.apiRoot') + '/authentication/revoke';
+    }),
+
     makeRequest: function (url, data) {
         data.client_id = this.get('config.clientId');
         data.client_secret = this.get('config.clientSecret');
---
+++ core/client/app/authorizers/oauth2.js
@@ -0,0 +1,3 @@
+import Oauth2Bearer from 'ember-simple-auth/authorizers/oauth2-bearer';
+
+export default Oauth2Bearer;
