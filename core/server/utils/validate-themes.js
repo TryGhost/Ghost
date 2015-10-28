9a635ebd154cb779da23a8e451290186e8c31e48
vdemedes <vdemedes@gmail.com>
2015-10-28 15:13:54 +0100

    Make read-directory ignore invalid package.json files
    refs #5940, #5923
    - make read-directory ignore invalid package.json files
    - display a warning about invalid package.json files on startup
    - add tests to ensure read-directory continues, even with invalid package.json files

--- core/server/utils/validate-themes.js
+++ core/server/utils/validate-themes.js
@@ -23,13 +23,25 @@ function validateThemes(dir) {
             _.each(themes, function (theme, name) {
                 var hasPackageJson, warning;
 
-                hasPackageJson = !!theme['package.json'];
+                hasPackageJson = theme['package.json'] !== undefined;
 
                 if (!hasPackageJson) {
                     warning = {
                         message: 'Found a theme with no package.json file',
                         context: 'Theme name: ' + name,
                         help: 'This will be required in future. Please see http://docs.ghost.org/themes/'
+                    };
+
+                    result.warnings.push(warning);
+                }
+
+                // if package.json is `null`, it means that it exists
+                // but JSON.parse failed (invalid json syntax)
+                if (hasPackageJson && theme['package.json'] === null) {
+                    warning = {
+                        message: 'Found a malformed package.json',
+                        context: 'Theme name: ' + name,
+                        help: 'Valid package.json will be required in future. Please see http://docs.ghost.org/themes/'
                     };
 
                     result.warnings.push(warning);
