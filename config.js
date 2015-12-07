1dc7903793193a0644e4824590cb5571a065b1eb
Tommy Rochette <tommy@julo.ca>
2015-12-06 19:16:19 -0800

    Changing to public config

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://blog.minutesdepot.com',
+        url: 'http://minutesdepot-blog.herokuapp.com',
         mail: {  
             transport: 'SMTP',
             host: 'smtp.mandrillapp.com',
