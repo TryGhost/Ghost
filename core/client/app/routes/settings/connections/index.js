45450482365a09131b63a53b227f46c4120115ae
zinyando <brizdigital@gmail.com>
2015-12-17 17:48:56 +0200

    Add client route

--- core/client/app/routes/settings/connections/index.js
+++ core/client/app/routes/settings/connections/index.js
@@ -4,7 +4,13 @@ import AuthenticatedRoute from 'ghost/routes/authenticated';
 const {inject} = Ember;
 
 export default AuthenticatedRoute.extend({
+  beforeModel() {
+      this._super(...arguments);
+  },
+
   model() {
+    this.store.unloadAll('client');
+
     return this.store.findAll('client');
   }
 });
