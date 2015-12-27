1c03da81de6f762467f465188c979249d0c28e90
Cole Lyman <cole@colelyman.com>
2015-12-27 11:03:14 -0500

    Basic message for route

--- core/cole/index.js
+++ core/cole/index.js
@@ -1,7 +1,12 @@
-var router = express.Router(),
+var express = require('express'),
   coleRoutes;
 
 coleRoutes = function coleRoutes(middleware) {
+  var router = express.Router();
+
+  router.get(/^\/(ht|hometeaching)\/$/, function(req, res) {
+    res.send('Welcome to hometeaching!!');
+  });
 
 };
 
