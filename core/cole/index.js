a67250a3fbebe787c5725065f396c7f05ccd363c
Cole Lyman <cole@colelyman.com>
2015-12-27 11:09:31 -0500

    Added debug statment

--- core/cole/index.js
+++ core/cole/index.js
@@ -5,6 +5,7 @@ coleRoutes = function coleRoutes(middleware) {
   var router = express.Router();
 
   router.get(/^\/(ht|hometeaching)\/$/, function(req, res) {
+    console.log('IN HOMETEACHING');
     res.send('Welcome to hometeaching!!');
   });
 
