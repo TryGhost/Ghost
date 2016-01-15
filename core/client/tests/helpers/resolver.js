40375e7a2c1ca6e7cbf630270482d8c9f4b22929
Austin Burdine <acburdine@gmail.com>
2016-01-15 10:21:19 -0600

    [WIP] deps: ember-cli@2.2.0-beta.6 [ci skip]

--- core/client/tests/helpers/resolver.js
+++ core/client/tests/helpers/resolver.js
@@ -1,7 +1,7 @@
 import Resolver from 'ember-resolver';
 import config from '../../config/environment';
 
-let resolver = Resolver.create();
+const resolver = Resolver.create();
 
 resolver.namespace = {
     modulePrefix: config.modulePrefix,
