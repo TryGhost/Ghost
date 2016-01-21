103455f1bb440085dc2fa6ef9413a62c86132d29
zinyando <brizdigital@gmail.com>
2016-01-21 21:26:05 +0200

    Minor css tweaks

--- core/client/app/adapters/client.js
+++ core/client/app/adapters/client.js
@@ -1,10 +1,4 @@
 import ApplicationAdapter from 'ghost/adapters/application';
 import SlugUrl from 'ghost/mixins/slug-url';
 
-export default ApplicationAdapter.extend(SlugUrl, {
-  // urlForUpdateRecord(id, modelName, snapshot) {
-  //   let namespace = this.get('namespace');
-  //   let { id } = snapshot.attributes();
-  //   return `${namespace}/clients/${id}`;
-  // }
-});
+export default ApplicationAdapter.extend(SlugUrl);
