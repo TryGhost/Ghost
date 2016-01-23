45a71db1657edf979eb482c05bc16e9c47f80b44
trwired <igor.kalat@gmail.com>
2016-01-23 23:40:31 +0100

    fixes TryGhost/Koenig#196

--- koenig/kg-simplemde/src/js/simplemde.js
+++ koenig/kg-simplemde/src/js/simplemde.js
@@ -456,7 +456,7 @@ function togglePreview(editor) {
 	var cm = editor.codemirror;
 	var wrapper = cm.getWrapperElement();
 	var toolbar_div = wrapper.previousSibling;
-	var toolbar = editor.toolbarElements.preview;
+	var toolbar = editor.options.toolbar ? editor.toolbarElements.preview : false;
 	var preview = wrapper.lastChild;
 	if(!preview || !/editor-preview/.test(preview.className)) {
 		preview = document.createElement("div");
@@ -467,8 +467,10 @@ function togglePreview(editor) {
 		preview.className = preview.className.replace(
 			/\s*editor-preview-active\s*/g, ""
 		);
-		toolbar.className = toolbar.className.replace(/\s*active\s*/g, "");
-		toolbar_div.className = toolbar_div.className.replace(/\s*disabled-for-preview*/g, "");
+		if(toolbar) {
+			toolbar.className = toolbar.className.replace(/\s*active\s*/g, "");
+			toolbar_div.className = toolbar_div.className.replace(/\s*disabled-for-preview*/g, "");
+		}
 	} else {
 		// When the preview button is clicked for the first time,
 		// give some time for the transition from editor.css to fire and the view to slide from right to left,
@@ -476,8 +478,10 @@ function togglePreview(editor) {
 		setTimeout(function() {
 			preview.className += " editor-preview-active";
 		}, 1);
-		toolbar.className += " active";
-		toolbar_div.className += " disabled-for-preview";
+		if(toolbar) {
+			toolbar.className += " active";
+			toolbar_div.className += " disabled-for-preview";
+		}
 	}
 	preview.innerHTML = editor.options.previewRender(editor.value(), preview);
 
