d0a3e22d0990b3b23d202ae925cf9feb4700d795
zinyando <brizdigital@gmail.com>
2016-01-25 19:50:08 +0200

    Make special Ghost clients not editable

--- core/client/app/components/gh-client-list-item.js
+++ core/client/app/components/gh-client-list-item.js
@@ -1,8 +1,15 @@
 import Ember from 'ember';
 
-const {Component, computed, inject} = Ember;
+const {Component, computed} = Ember;
 
 export default Ember.Component.extend({
+
+    isSpecialClient: Ember.computed('name', function () {
+        let name = this.get('name');
+
+        return name.startsWith('Ghost');
+    }),
+
   actions: {
     deleteClient(id){
       this.sendAction('deleteClient', id);
