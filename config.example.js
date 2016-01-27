4ba053bde829a65003914ae822fdf2123275b30f
PMas551 <petermason@fastmail.fm>
2016-01-27 14:58:18 +1100

    Remove quotes from port variable suggestion for use with IISNode
    Closes #6402

--- config.example.js
+++ config.example.js
@@ -62,7 +62,7 @@ config = {
         server: {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to process.env.PORT
             port: '2368'
         },
         // #### Paths
