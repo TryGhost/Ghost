e0dc4daecc8444e09490cf0ea0844f6b4fdc7f90
zinyando <brizdigital@gmail.com>
2016-01-28 21:06:54 +0200

    Use factories instead of fixtures in mirage

--- core/client/app/mirage/config.js
+++ core/client/app/mirage/config.js
@@ -329,123 +329,10 @@ export default function () {
         };
     });
 
-    this.get('/clients', function (db) {
-      return {
-          clients: [{
-              "slug": "ghost-admin",
-              "type": "ua",
-              "id": 1,
-              "uuid": "c712cbcd-326d-4f85-a8e0-15d74188c56f",
-              "name": "Ghost Admin",
-              "secret": "bf9b141079f9",
-              "redirection_uri": null,
-              "logo": null,
-              "status": "enabled",
-              "description": null,
-              "created_at": "2015-12-07T17:55:06.861Z",
-              "created_by": 1,
-              "updated_at": "2015-12-07T17:55:06.861Z",
-              "updated_by": 1
-              },
-              {
-              "slug":"ghost-frontend",
-              "type":"ua",
-              "id":2,
-              "uuid":"0be92c46-452e-4d2c-8e05-4c6972a57ab4",
-              "name":"Ghost Frontend",
-              "secret":"2f5c4f62913e",
-              "redirection_uri":null,
-              "logo":null,
-              "status":"enabled",
-              "description":null,
-              "created_at":"2015-12-07T17:55:06.861Z",
-              "created_by":1,
-              "updated_at":"2015-12-07T17:55:06.861Z",
-              "updated_by":1
-            },
-            {
-              "slug":"test-client",
-              "type":"ua",
-              "id":3,
-              "uuid":"0be92c46-452e-4d2c-8e05-4c6972a57cv8",
-              "name":"Test Client",
-              "secret":"2f5c4f65764k",
-              "redirection_uri":null,
-              "logo":null,
-              "status":"enabled",
-              "description":null,
-              "created_at":"2015-12-07T17:55:06.861Z",
-              "created_by":1,
-              "updated_at":"2015-12-07T17:55:06.861Z",
-              "updated_by":1
-          }
-          ]};
-    });
-
-    this.get('/clients/slug/ghost-admin/', function (db) {
-        return {
-            clients: [{
-                "slug": "ghost-admin",
-                "type": "ua",
-                "id": 1,
-                "uuid": "c712cbcd-326d-4f85-a8e0-15d74188c56f",
-                "name": "Ghost Admin",
-                "secret": "bf9b141079f9",
-                "redirection_uri": null,
-                "logo": null,
-                "status": "enabled",
-                "description": null,
-                "created_at": "2015-12-07T17:55:06.861Z",
-                "created_by": 1,
-                "updated_at": "2015-12-07T17:55:06.861Z",
-                "updated_by": 1
-                }]
-        };
-    });
-
-    this.get('/clients/slug/ghost-frontend/', function (db) {
-        return {
-            clients: [{
-                      "slug":"ghost-frontend",
-                      "type":"ua",
-                      "id":2,
-                      "uuid":"0be92c46-452e-4d2c-8e05-4c6972a57ab4",
-                      "name":"Ghost Frontend",
-                      "secret":"2f5c4f62913e",
-                      "redirection_uri":null,
-                      "logo":null,
-                      "status":"enabled",
-                      "description":null,
-                      "created_at":"2015-12-07T17:55:06.861Z",
-                      "created_by":1,
-                      "updated_at":"2015-12-07T17:55:06.861Z",
-                      "updated_by":1
-                    }]
-        };
-    });
-
-    this.get('/clients/slug/test-client/', function (db) {
-        return {
-            clients: [{
-              "slug":"test-client",
-              "type":"ua",
-              "id":3,
-              "uuid":"0be92c46-452e-4d2c-8e05-4c6972a57cv8",
-              "name":"Test Client",
-              "secret":"2f5c4f65764k",
-              "redirection_uri":null,
-              "logo":null,
-              "status":"enabled",
-              "description":null,
-              "created_at":"2015-12-07T17:55:06.861Z",
-              "created_by":1,
-              "updated_at":"2015-12-07T17:55:06.861Z",
-              "updated_by":1
-          }]
-        };
-    });
+    this.get('/clients', 'client');
 
     this.post('/clients', 'client');
+    
     this.del('/clients/:id', 'client');
 
     this.put('/clients/:id', function (db, request) {
@@ -456,6 +343,12 @@ export default function () {
 
         return {
             clients: [client]
+        };
+    });
+
+    this.get('/clients/slug/:slug/', function (db, request) {
+        return {
+            clients: db.clients.where({slug: request.params.slug})
         };
     });
 
