8357b53ce2e5d3c7e4868e3800565ce79061bc67
Hannah Wolfe <erisds@gmail.com>
2016-02-09 14:14:24 +0000

    Make frontend routing dynamic & driven by channels
    refs #5091
    
    - Move renderChannel to own file
    - Update channel config to have get/list methods
    - Move main routes to be generated based on the list of channels
    - Move RSS routes to be subroutes of channels
    - Move redirect301 to be a shared util
    - Add full test coverage
    - Split frontend route tests into frontend & channels

--- core/server/routes/frontend.js
+++ core/server/routes/frontend.js
@@ -1,6 +1,6 @@
 var frontend    = require('../controllers/frontend'),
+    channels    = require('../controllers/frontend/channels'),
     config      = require('../config'),
-    errors      = require('../errors'),
     express     = require('express'),
     utils       = require('../utils'),
 
@@ -10,52 +10,19 @@ frontendRoutes = function frontendRoutes(middleware) {
     var router = express.Router(),
         subdir = config.paths.subdir,
         routeKeywords = config.routeKeywords,
-        indexRouter = express.Router(),
-        tagRouter = express.Router({mergeParams: true}),
-        authorRouter = express.Router({mergeParams: true}),
-        rssRouter = express.Router({mergeParams: true}),
         privateRouter = express.Router();
-
-    function redirect301(res, path) {
-        /*jslint unparam:true*/
-        res.set({'Cache-Control': 'public, max-age=' + utils.ONE_YEAR_S});
-        res.redirect(301, path);
-    }
-
-    function handlePageParam(req, res, next, page) {
-        var pageRegex = new RegExp('/' + routeKeywords.page + '/(.*)?/'),
-            rssRegex = new RegExp('/rss/(.*)?/');
-
-        page = parseInt(page, 10);
-
-        if (page === 1) {
-            // Page 1 is an alias, do a permanent 301 redirect
-            if (rssRegex.test(req.url)) {
-                return redirect301(res, req.originalUrl.replace(rssRegex, '/rss/'));
-            } else {
-                return redirect301(res, req.originalUrl.replace(pageRegex, '/'));
-            }
-        } else if (page < 1 || isNaN(page)) {
-            // Nothing less than 1 is a valid page number, go straight to a 404
-            return next(new errors.NotFoundError());
-        } else {
-            // Set req.params.page to the already parsed number, and continue
-            req.params.page = page;
-            return next();
-        }
-    }
 
     // ### Admin routes
     router.get(/^\/(logout|signout)\/$/, function redirectToSignout(req, res) {
-        redirect301(res, subdir + '/ghost/signout/');
+        utils.redirect301(res, subdir + '/ghost/signout/');
     });
     router.get(/^\/signup\/$/, function redirectToSignup(req, res) {
-        redirect301(res, subdir + '/ghost/signup/');
+        utils.redirect301(res, subdir + '/ghost/signup/');
     });
 
     // redirect to /ghost and let that do the authentication to prevent redirects to /ghost//admin etc.
     router.get(/^\/((ghost-admin|admin|wp-admin|dashboard|signin|login)\/?)$/, function redirectToAdmin(req, res) {
-        redirect301(res, subdir + '/ghost/');
+        utils.redirect301(res, subdir + '/ghost/');
     });
 
     // password-protected frontend route
@@ -71,45 +38,14 @@ frontendRoutes = function frontendRoutes(middleware) {
             frontend.private
         );
 
-    rssRouter.route('/rss/').get(frontend.rss);
-    rssRouter.route('/rss/:page/').get(frontend.rss);
-    rssRouter.route('/feed/').get(function redirect(req, res) {
-        redirect301(res, subdir + '/rss/');
-    });
-    rssRouter.param('page', handlePageParam);
-
-    // Index
-    indexRouter.route('/').get(frontend.index);
-    indexRouter.route('/' + routeKeywords.page + '/:page/').get(frontend.index);
-    indexRouter.param('page', handlePageParam);
-    indexRouter.use(rssRouter);
-
-    // Tags
-    tagRouter.route('/').get(frontend.tag);
-    tagRouter.route('/' + routeKeywords.page + '/:page/').get(frontend.tag);
-    tagRouter.route('/edit?').get(function redirect(req, res) {
-        res.redirect(subdir + '/ghost/settings/tags/' + req.params.slug + '/');
-    });
-    tagRouter.param('page', handlePageParam);
-    tagRouter.use(rssRouter);
-
-    // Authors
-    authorRouter.route('/').get(frontend.author);
-    authorRouter.route('/edit?').get(function redirect(req, res) {
-        res.redirect(subdir + '/ghost/team/' + req.params.slug + '/');
-    });
-    authorRouter.route('/' + routeKeywords.page + '/:page/').get(frontend.author);
-    authorRouter.param('page', handlePageParam);
-    authorRouter.use(rssRouter);
-
-    // Mount the Routers
-    router.use('/' + routeKeywords.private + '/', privateRouter);
-    router.use('/' + routeKeywords.author + '/:slug/', authorRouter);
-    router.use('/' + routeKeywords.tag + '/:slug/', tagRouter);
-    router.use('/', indexRouter);
-
     // Post Live Preview
     router.get('/' + routeKeywords.preview + '/:uuid', frontend.preview);
+
+    // Private
+    router.use('/' + routeKeywords.private + '/', privateRouter);
+
+    // Channels
+    router.use(channels.router());
 
     // Default
     router.get('*', frontend.single);
