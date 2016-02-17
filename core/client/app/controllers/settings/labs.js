7bfaa0a91dd7f774023ed04ebccaf8e9d077acd8
Austin Burdine <acburdine@gmail.com>
2016-02-17 14:50:40 -0600

    inject ajax service in settings/labs
    closes #6530
    - turns out one must inject a service in order to use it

--- core/client/app/controllers/settings/labs.js
+++ core/client/app/controllers/settings/labs.js
@@ -16,6 +16,7 @@ export default Controller.extend({
     ghostPaths: service(),
     notifications: service(),
     session: service(),
+    ajax: service(),
 
     actions: {
         onUpload(file) {
