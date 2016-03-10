1c488f598d1ef44f5330c461a4f453942137f123
Alex <calidadis@gmail.com>
2016-03-10 16:47:10 -0500

    Added the ability to import theme as an experimental feature under labs page

--- core/server/config/index.js
+++ core/server/config/index.js
@@ -167,6 +167,7 @@ ConfigManager.prototype.set = function (config) {
 
             contentPath:      contentPath,
             themePath:        path.resolve(contentPath, 'themes'),
+            themeRelPath:     'content/themes',
             appPath:          path.resolve(contentPath, 'apps'),
             imagesPath:       path.resolve(contentPath, 'images'),
             imagesRelPath:    'content/images',
