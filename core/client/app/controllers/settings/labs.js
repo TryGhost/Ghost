2811954c79d246a61f6e20f9e2b3d33dc0582074
Alex <calidadis@gmail.com>
2016-03-12 09:17:00 -0500

    Fix stylistics errors

--- core/client/app/controllers/settings/labs.js
+++ core/client/app/controllers/settings/labs.js
@@ -25,7 +25,7 @@ export default Controller.extend({
             let currentUserId = this.get('session.user.id');
             let dbUrl = this.get('ghostPaths.url').api('themes');
 
-            this.set('uploadButtonText', 'Importing');  
+            this.set('uploadButtonText', 'Importing');
             this.set('importErrors', '');
 
             formData.append('importfile', file);
