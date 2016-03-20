a47da3e56b51febc6626821e553f9679a70e326a
Kasparas Galdikas <foreachas@gmail.com>
2016-03-20 14:55:19 +0000

    Removed console.log left overs

--- koenig/kg-simplemde/src/js/simplemde.js
+++ koenig/kg-simplemde/src/js/simplemde.js
@@ -1672,9 +1672,7 @@ SimpleMDE.prototype.createToolbar = function(items) {
 			var nonSeparatorIconsFollow = false;
 
 			for(var x = (i + 1); x < items.length; x++) {
-				console.log(x);
 				if(items[x] !== "|" && (!self.options.hideIcons || self.options.hideIcons.indexOf(items[x].name) == -1)) {
-					console.log(items[x]);
 					nonSeparatorIconsFollow = true;
 				}
 			}
