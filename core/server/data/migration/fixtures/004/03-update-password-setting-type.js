666215a38a5147cc440e44a0cd7fa75d68155f06
Hannah Wolfe <erisds@gmail.com>
2016-03-21 12:44:23 +0000

    Migration messaging improvements
    refs #6301
    
    - fix messages that joined with comma and therefore missed outputting version no
    - change `logInfo` to `logger` that has both an info and a warn method
    - add new warn method to errors
    - add a warn message everytime a migration (data or fixture) gets skipped over
    - update logger everywhere, including tests
    - update tests to check logger.warn gets called

--- core/server/data/migration/fixtures/004/03-update-password-setting-type.js
+++ core/server/data/migration/fixtures/004/03-update-password-setting-type.js
@@ -1,12 +1,16 @@
 // Update the `password` setting, so that it has a type of `private` rather than `blog`
 var models  = require('../../../../models'),
-    Promise = require('bluebird');
+    Promise = require('bluebird'),
 
-module.exports = function updatePasswordSetting(options, logInfo) {
+    message = 'Update password setting';
+
+module.exports = function updatePasswordSetting(options, logger) {
     return models.Settings.findOne('password').then(function (setting) {
         if (setting && setting.get('type') !== 'private') {
-            logInfo('Update password setting');
+            logger.info(message);
             return models.Settings.edit({key: 'password', type: 'private'}, options);
+        } else {
+            logger.warn(message);
         }
         return Promise.resolve();
     });
