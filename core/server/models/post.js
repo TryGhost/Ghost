cb51fa7fc746731d3b1c829cd4664a9b1517ca20
Joerg Henning <henning.joerg@gmail.com>
2016-03-27 18:19:32 +0800

    deps: validator@5.1.0
    closes #6462
    
    - monkey-patch validator.extends() since it was dropped by validator @5.0.0
    - coerce input to string prior to validation (custom toString func)
    - need to handle boolean validation based on column type not isIn()
    - use `lodash.tostring` to convert input values to strings

--- core/server/models/post.js
+++ core/server/models/post.js
@@ -12,6 +12,7 @@ var _              = require('lodash'),
     config         = require('../config'),
     baseUtils      = require('./base/utils'),
     i18n           = require('../i18n'),
+    toString       = require('lodash.tostring'),
     Post,
     Posts;
 
@@ -178,11 +179,11 @@ Post = ghostBookshelf.Model.extend({
 
         ghostBookshelf.Model.prototype.saving.call(this, model, attr, options);
 
-        this.set('html', converter.makeHtml(this.get('markdown')));
+        this.set('html', converter.makeHtml(toString(this.get('markdown'))));
 
         // disabling sanitization until we can implement a better version
         title = this.get('title') || i18n.t('errors.models.post.untitled');
-        this.set('title', title.trim());
+        this.set('title', toString(title).trim());
 
         // ### Business logic for published_at and published_by
         // If the current status is 'published' and published_at is not set, set it to now
