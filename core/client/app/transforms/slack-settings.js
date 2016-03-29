35ecaee6d3fc77aea1f13e1184991bb90b64ef79
Aileen Nowak <AileenCGN@gmail.com>
2016-03-29 10:40:44 +0200

    Slack integration
    closes #6584
    - Frontend Changes:
    	- adds 'Apps' to Navigation Menu
    	- adds 'Slack' as nested page to Apps
    	- adds `apps.css`
    	- adds `slack-integration` model and uses `slack-settings` custom transform to parse JSON file
    	- adds validation for `slack` model
    	- adds fixtures and `slack/test` API endpoint to Mirage
    	- adds acceptance tests for `apps-test` and `slack-test`
    	- adds unit tests for `slack-settings` and `slack-integration`
    - Backend Changes:
    	- adds API endpoint `slack/test` to send Test Notification
    	- adds default-values for slack model
    	- sends payload to slack:
    		- text: the url of the blogpost / test message
    		- icon_url: url to ghost logo
    		- username: Ghost
    	- adds `slack/index.js` to send webhook to slack if
    		- a new post is published (if slack webhook url is saved in settings)
    		- user clicks on 'Send Test Notification' in UI
    	- adds `slack.init()` to `server.index.js` to add event listener
    	- adds unit test for `slack/index`

---
+++ core/client/app/transforms/slack-settings.js
@@ -0,0 +1,37 @@
+/* jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
+import Ember from 'ember';
+import Transform from 'ember-data/transform';
+import SlackObject from 'ghost/models/slack-integration';
+
+const {isArray} = Ember;
+const emberA = Ember.A;
+
+export default Transform.extend({
+    deserialize(serialized) {
+        let slackObj, settingsArray;
+        try {
+            settingsArray = JSON.parse(serialized) || [];
+        } catch (e) {
+            settingsArray = [];
+        }
+
+        slackObj = settingsArray.map((itemDetails) => {
+            return SlackObject.create(itemDetails);
+        });
+        return emberA(slackObj);
+    },
+
+    serialize(deserialized) {
+        let settingsArray;
+        if (isArray(deserialized)) {
+            settingsArray = deserialized.map((item) => {
+                let url = (item.get('url') || '').trim();
+
+                return {url};
+            }).compact();
+        } else {
+            settingsArray = [];
+        }
+        return JSON.stringify(settingsArray);
+    }
+});
