c58b1f2aff4bbfc969abd7be270a7f46aef88104
zinyando <brizdigital@gmail.com>
2016-04-06 01:47:10 +0200

    Minor fixes and tweaks

--- core/client/app/components/gh-client-list-item.js
+++ core/client/app/components/gh-client-list-item.js
@@ -1,35 +1,38 @@
 import Ember from 'ember';
 
 const {Component,
-     computed,
-      inject: {service}
-        } = Ember;
+ computed,
+  inject: {service}
+    } = Ember;
 
 export default Ember.Component.extend({
-    ghostPaths: service(),
+ghostPaths: service(),
 
-    userDefault: computed('ghostPaths', function () {
-        return `${this.get('ghostPaths.subdir')}/ghost/img/user-image.png`;
-    }),
+userDefault: computed('ghostPaths', function () {
+    return `${this.get('ghostPaths.subdir')}/ghost/img/user-image.png`;
+}),
 
-    clientImageBackground: computed('logo', 'userDefault', function () {
-        let url = this.get('logo') || this.get('userDefault');
+clientImageBackground: computed('logo', 'userDefault', function () {
+    let url = this.get('logo') || this.get('userDefault');
 
-        return Ember.String.htmlSafe(`background-image: url(${url})`);
-    }),
+    return Ember.String.htmlSafe(`background-image: url(${url})`);
+}),
 
-    isSpecialClient: Ember.computed('name', function () {
-        let slug = this.get('slug');
+isSpecialClient: Ember.computed('slug', function () {
+    let slug = this.get('slug');
 
-        return slug.startsWith('ghost-');
-    }),
-
-  actions: {
-    deleteClient(id){
-      const deleteClient = this.get('deleteClient');
-      return deleteClient(id).catch(err => {
-          this.set('formError', err);
-      });
+    if (slug === 'ghost-admin' || slug === 'ghost-frontend'){
+        return true;
     }
-  }
+    return false;
+}),
+
+actions: {
+deleteClient(id){
+  const deleteClient = this.get('deleteClient');
+  return deleteClient(id).catch(err => {
+      this.set('formError', err);
+  });
+}
+}
 });
