29cc46c8f0aa987618409d024ae5579d46dd198f
Hannah Wolfe <erisds@gmail.com>
2016-04-14 18:33:22 +0100

    Adding subscribe app + tpl + helper
    - added new internal app "subscribers"
    - app has a template "subscribe.hbs"
    - adds a new helper called "form_subscribe"

---
+++ core/server/apps/subscribers/index.js
@@ -0,0 +1,23 @@
+var _          = require('lodash'),
+    path       = require('path'),
+    config     = require('../../config'),
+    router     = require('./lib/router'),
+
+    // Dirty require
+    template   = require('../../helpers/template');
+
+module.exports = {
+    activate: function activate(ghost) {
+        // Correct way to register a helper from an app
+        ghost.helpers.register('form_subscribe', function formSubscribeHelper(options) {
+            var data = _.merge({}, options.hash, {
+                action: path.join(config.paths.subdir, config.routeKeywords.subscribe) + '/'
+            });
+            return template.execute('form_subscribe', data, options);
+        });
+    },
+
+    setupRoutes: function setupRoutes(blogRouter) {
+        blogRouter.use('/' + config.routeKeywords.subscribe + '/', router);
+    }
+};
