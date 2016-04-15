83c9270682ab1ac76ae7bdc0b843c6a047d8b2c6
Kevin Ansfield <kevin@lookingsideways.co.uk>
2016-04-15 15:45:50 +0100

    Subscribers: Admin User Interface v1
    Initial Subscribers screen
    - set up mocked api endpoints
    - basic subscribers screen with data loading, infinite scroll
    
    "Add Subscriber" screen
    - uses modal to display a new subscriber form
    - validates subscriber e-mail address
    - moves pagination from route into controller to use filtered/sorted CPs on top of a live-query so that new subscribers are added to the list and the total can be properly managed
    
    TODO:
    - there is currently a pretty serious performance issue where the whole table is re-rendered when the live-query is updated. `ember-light-table` doesn't allow for live-binding and has no options to easily manipulate it's rows using an external interface - it's possible to move the page loading into the component so we only render new rows but that leaves it difficult to react to new subscribers being added through the UI. I believe the number of components used within the table is also adding to the performance problems.
      - most likely solution is to drop `ember-light-table` in favour of rendering the table directly - glimmer should do a good job of fast updates even though the underlying array will be completely swapped out
    
    "Import subscribers" screen
    - uses modal to display an import subscribers CSV file upload form
    - displays upload progress
    - displays import stats and reloads subscribers table once import has completed
    - adds `gh-file-uploader` component (NB. pared down copy of `gh-image-uploader`, ripe for some refactoring)
    - fixes subscribers acceptance test failing because fixtures did not have the labs flag enabled
    
    Unfortunately this doesn't have 100% test coverage as we're limited in how we can simulate file uploads 😞
    
    Fix performance issues with subscribers table
    - moves the table definition from the component up to the controller
    - switches back to manually manipulating table rows instead of using a live-query
    
    This is a quick-fix in that it allows us to continue using the `ember-light-table` component but it does mean that we lose some flexibility that the live-query gave us. For now it's not much of an issue and it allows us to defer deeper performance/flexibility work until we have a concrete need and requirements.
    
    Hook up Export CSV button
    - use a hidden iFrame to trigger the browser to hit the CSV export endpoint and download the file
    
    Re-order subscribers table by clicking column headers
    - displays currently sorted column and sort direction
    - clicking a column header re-fetches the data from the server with the appropriate query params
    
    Fix scroll triggers for infinite pagination + icon change
    - adds a debounce as well as the throttle so that we always get a final scroll trigger once scrolling has stopped
    - changes the subscribers icon from the temporary team icon to the mail icon

---
+++ ghost/admin/tests/integration/components/gh-subscribers-table-test.js
@@ -0,0 +1,25 @@
+/* jshint expr:true */
+import { expect } from 'chai';
+import {
+    describeComponent,
+    it
+} from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+import Table from 'ember-light-table';
+
+describeComponent(
+    'gh-subscribers-table',
+    'Integration: Component: gh-subscribers-table',
+    {
+        integration: true
+    },
+    function() {
+        it('renders', function() {
+            this.set('table', new Table([], []));
+            this.set('sortByColumn', function () {});
+
+            this.render(hbs`{{gh-subscribers-table table=table sortByColumn=(action sortByColumn)}}`);
+            expect(this.$()).to.have.length(1);
+        });
+    }
+);
