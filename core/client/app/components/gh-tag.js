74a8e5d2a5a813b353915e8fd7ac38fbe7d4aeea
Austin Burdine <acburdine@gmail.com>
2016-04-26 16:32:17 -0500

    deps: ember-invoke-action@1.3.0
    no issue
    - replace instances of `this.get('action')()` with ember-invoke-action syntax

--- core/client/app/components/gh-tag.js
+++ core/client/app/components/gh-tag.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
+import {invokeAction} from 'ember-invoke-action';
 
 export default Ember.Component.extend({
     willDestroyElement() {
         this._super(...arguments);
 
         if (this.get('tag.isDeleted') && this.get('onDelete')) {
-            this.get('onDelete')();
+            invokeAction(this, 'onDelete');
         }
     }
 });
