73373022332cec7ffc9193c277e425a851827cde
Kevin Ansfield <kevin@lookingsideways.co.uk>
2016-04-28 17:21:03 +0100

    update client Subscriber model properties and test factory to match schema

--- core/client/app/models/subscriber.js
+++ core/client/app/models/subscriber.js
@@ -1,17 +1,23 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import {belongsTo} from 'ember-data/relationships';
 import ValidationEngine from 'ghost/mixins/validation-engine';
 
 export default Model.extend(ValidationEngine, {
     validationType: 'subscriber',
 
+    uuid: attr('string'),
+    name: attr('string'),
     email: attr('string'),
+    status: attr('string'),
+    subscribedUrl: attr('string'),
+    subscribedReferrer: attr('string'),
+    unsubscribedUrl: attr('string'),
+    unsubscribedAt: attr('moment-date'),
     createdAt: attr('moment-date'),
     updatedAt: attr('moment-date'),
     createdBy: attr('number'),
     updatedBy: attr('number'),
-    unsubscribedAt: attr('moment-date'),
 
-    // TODO: Replace once the API has different states
-    status: 'Confirmed'
+    post: belongsTo('post')
 });
