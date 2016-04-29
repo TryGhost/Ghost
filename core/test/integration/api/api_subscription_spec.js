5b716923d9d57e5e0ee9ba67b27287109787df0e
kirrg001 <katharina.irrgang@googlemail.com>
2016-04-29 12:58:40 +0200

    finish permission handling
    no issue
    - add some more tests, optimise tests and finish tests
    - subscriber model checks external context permissions in permissible fn
    - add missing permissions for subscriber csv

--- core/test/integration/api/api_subscription_spec.js
+++ core/test/integration/api/api_subscription_spec.js
@@ -3,16 +3,15 @@ var testUtils   = require('../../utils'),
     should      = require('should'),
     Promise     = require('bluebird'),
     _           = require('lodash'),
-    // Stuff we are testing
     context     = testUtils.context,
-
+    errors      = require('../../../server/errors'),
     SubscribersAPI      = require('../../../server/api/subscribers');
 
 describe('Subscribers API', function () {
     // Keep the DB clean
     before(testUtils.teardown);
     afterEach(testUtils.teardown);
-    beforeEach(testUtils.setup('users:roles', 'permission', 'perms:init', 'subscriber'));
+    beforeEach(testUtils.setup('users:roles', 'perms:subscriber', 'perms:init', 'subscriber'));
 
     should.exist(SubscribersAPI);
 
@@ -34,6 +33,26 @@ describe('Subscribers API', function () {
                 }).catch(done);
         });
 
+        it('can add a subscriber (editor)', function (done) {
+            SubscribersAPI.add({subscribers: [newSubscriber]}, testUtils.context.editor)
+                .then(function (results) {
+                    should.exist(results);
+                    should.exist(results.subscribers);
+                    results.subscribers.length.should.be.above(0);
+                    done();
+                }).catch(done);
+        });
+
+        it('can add a subscriber (author)', function (done) {
+            SubscribersAPI.add({subscribers: [newSubscriber]}, testUtils.context.author)
+                .then(function (results) {
+                    should.exist(results);
+                    should.exist(results.subscribers);
+                    results.subscribers.length.should.be.above(0);
+                    done();
+                }).catch(done);
+        });
+
         it('can add a subscriber (external)', function (done) {
             SubscribersAPI.add({subscribers: [newSubscriber]}, testUtils.context.external)
                 .then(function (results) {
@@ -45,11 +64,14 @@ describe('Subscribers API', function () {
         });
 
         it('CANNOT add subscriber without context', function (done) {
-            SubscribersAPI.add({subscribers: [newSubscriber]}).then(function () {
-                done(new Error('Add subscriber is not denied without authentication.'));
-            }, function () {
-                done();
-            }).catch(done);
+            SubscribersAPI.add({subscribers: [newSubscriber]})
+                .then(function () {
+                    done(new Error('Add subscriber without context should have no access.'));
+                })
+                .catch(function (err) {
+                    (err instanceof errors.NoPermissionError).should.eql(true);
+                    done();
+                });
         });
     });
 
@@ -67,8 +89,8 @@ describe('Subscribers API', function () {
                 }).catch(done);
         });
 
-        it('can edit a subscriber (editor)', function (done) {
-            SubscribersAPI.edit({subscribers: [{email: newSubscriberEmail}]}, _.extend({}, context.editor, {id: firstSubscriber}))
+        it('can edit subscriber (external)', function (done) {
+            SubscribersAPI.edit({subscribers: [{email: newSubscriberEmail}]}, _.extend({}, context.external, {id: firstSubscriber}))
                 .then(function (results) {
                     should.exist(results);
                     should.exist(results.subscribers);
@@ -77,35 +99,59 @@ describe('Subscribers API', function () {
                 }).catch(done);
         });
 
-        // needs permissions to work properly
-        it.skip('CANNOT edit subscriber (external)', function (done) {
-            SubscribersAPI.edit({subscribers: [{email: newSubscriberEmail}]}, _.extend({}, context.external, {id: firstSubscriber}))
-            .then(function () {
-                done(new Error('Edit subscriber is not denied with external context.'));
-            }, function () {
-                done();
-            }).catch(done);
+        it('CANNOT edit a subscriber (editor)', function (done) {
+            SubscribersAPI.edit({subscribers: [{email: newSubscriberEmail}]}, _.extend({}, context.editor, {id: firstSubscriber}))
+                .then(function () {
+                    done(new Error('Edit subscriber as author should have no access.'));
+                })
+                .catch(function (err) {
+                    (err instanceof errors.NoPermissionError).should.eql(true);
+                    done();
+                });
+        });
+
+        it('CANNOT edit subscriber (author)', function (done) {
+            SubscribersAPI.edit({subscribers: [{email: newSubscriberEmail}]}, _.extend({}, context.author, {id: firstSubscriber}))
+                .then(function () {
+                    done(new Error('Edit subscriber as author should have no access.'));
+                })
+                .catch(function (err) {
+                    (err instanceof errors.NoPermissionError).should.eql(true);
+                    done();
+                });
         });
 
         it('CANNOT edit subscriber that doesn\'t exit', function (done) {
             SubscribersAPI.edit({subscribers: [{email: newSubscriberEmail}]}, _.extend({}, context.internal, {id: 999}))
-            .then(function () {
-                done(new Error('Edit non-existent subscriber is possible.'));
-            }, function (err) {
-                should.exist(err);
-                err.message.should.eql('Subscriber not found.');
-                done();
-            }).catch(done);
+                .then(function () {
+                    done(new Error('Edit non-existent subscriber is possible.'));
+                }, function (err) {
+                    should.exist(err);
+                    (err instanceof errors.NotFoundError).should.eql(true);
+                    done();
+                }).catch(done);
         });
     });
 
     describe('Destroy', function () {
         var firstSubscriber = 1;
-        it('can destroy subscriber', function (done) {
+
+        it('can destroy subscriber as admin', function (done) {
             SubscribersAPI.destroy(_.extend({}, testUtils.context.admin, {id: firstSubscriber}))
                 .then(function (results) {
                     should.not.exist(results);
 
+                    done();
+                }).catch(done);
+        });
+
+        it('CANNOT destroy subscriber', function (done) {
+            SubscribersAPI.destroy(_.extend({}, testUtils.context.editor, {id: firstSubscriber}))
+                .then(function () {
+                    done(new Error('Destroy subscriber should not be possible as editor.'));
+                }, function (err) {
+                    should.exist(err);
+                    (err instanceof errors.NoPermissionError).should.eql(true);
                     done();
                 }).catch(done);
         });
@@ -131,13 +177,15 @@ describe('Subscribers API', function () {
             }).catch(done);
         });
 
-        // needs permissions to work properly
-        it.skip('CANNOT browse subscriber (external)', function (done) {
-            SubscribersAPI.browse(testUtils.context.external).then(function () {
-                done(new Error('Browse subscriber is not denied with external context.'));
-            }, function () {
-                done();
-            }).catch(done);
+        it('CANNOT browse subscriber (external)', function (done) {
+            SubscribersAPI.browse(testUtils.context.external)
+                .then(function () {
+                    done(new Error('Browse subscriber should be denied with external context.'));
+                })
+                .catch(function (err) {
+                    (err instanceof errors.NoPermissionError).should.eql(true);
+                    done();
+                });
         });
     });
 
@@ -163,12 +211,12 @@ describe('Subscribers API', function () {
             }).catch(done);
         });
 
-        it('cannot fetch a subscriber which doesn\'t exist', function (done) {
+        it('CANNOT fetch a subscriber which doesn\'t exist', function (done) {
             SubscribersAPI.read({context: {user: 1}, id: 999}).then(function () {
                 done(new Error('Should not return a result'));
             }).catch(function (err) {
                 should.exist(err);
-                err.message.should.eql('Subscriber not found.');
+                (err instanceof errors.NotFoundError).should.eql(true);
                 done();
             });
         });
