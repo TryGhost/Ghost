b07b3acedd7562bb746207676fe027c1ed1be6fe
Jochem Maas <jochem@iamjochem.com>
2016-05-06 15:37:06 +0200

    use `_.defaultsDeep()` rather than `_.merge()` so that automatically-generated config object structure does not over-write explicitly given values

--- core/server/config/index.js
+++ core/server/config/index.js
@@ -154,7 +154,7 @@ ConfigManager.prototype.set = function (config) {
         storagePath = path.join(contentPath, 'storage');
     }
 
-    _.merge(this._config, {
+    _.defaultsDeep(this._config, {
         ghostVersion: packageInfo.version,
         paths: {
             appRoot:          appRoot,
