ff132fd934a06d510624f71018468e1feea5f53b
kirrg001 <katharina.irrgang@googlemail.com>
2016-05-18 16:27:54 +0200

    timezones: support permalinks based on current timezone
    issue #6406
    - redirect date permalink if timezone changed
    - create permalinks based on blog TZ
    - post-lookup fn is now more generic

--- core/server/controllers/frontend/post-lookup.js
+++ core/server/controllers/frontend/post-lookup.js
@@ -16,28 +16,32 @@ function postLookup(postUrl) {
         postPermalink = config.theme.permalinks,
         pagePermalink = '/:slug/',
         isEditURL = false,
-        matchFunc,
+        matchFuncPost,
+        matchFuncPage,
+        postParams,
+        pageParams,
         params;
 
     // Convert saved permalink into a path-match function
-    matchFunc = routeMatch(getEditFormat(postPermalink));
-    params = matchFunc(postPath);
+    matchFuncPost = routeMatch(getEditFormat(postPermalink));
+    postParams = matchFuncPost(postPath);
 
     // Check if the path matches the permalink structure.
     // If there are no matches found, test to see if this is a page instead
-    if (params === false) {
-        matchFunc = routeMatch(getEditFormat(pagePermalink));
-        params = matchFunc(postPath);
+    if (postParams === false) {
+        matchFuncPage = routeMatch(getEditFormat(pagePermalink));
+        pageParams = matchFuncPage(postPath);
     }
 
     // If there are still no matches then return empty.
-    if (params === false) {
+    if (pageParams === false) {
         return Promise.resolve();
     }
 
+    params = postParams || pageParams;
+
     // If params contains edit, and it is equal to 'edit' this is an edit URL
     if (params.edit && params.edit.toLowerCase() === 'edit') {
-        postPath = postPath.replace(params.edit + '/', '');
         isEditURL = true;
     } else if (params.edit !== undefined) {
         // Unknown string in URL, return empty
@@ -53,9 +57,12 @@ function postLookup(postUrl) {
     return api.posts.read(params).then(function then(result) {
         var post = result.posts[0];
 
-        // If there is no post, or the post has no URL, or it isn't a match for our original lookup, return empty
-        // This also catches the case where we use the pagePermalink but the post is not a page
-        if (!post || !post.url || post.url !== postPath) {
+        if (!post) {
+            return Promise.resolve();
+        }
+
+        // CASE: we originally couldn't match the post based on date permalink and we tried to check if its a page
+        if (!post.page && !postParams) {
             return Promise.resolve();
         }
 
