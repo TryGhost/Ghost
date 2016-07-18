c3f1df8ca11033b6ed2fd45aec23464ab0f02b8b
Slav Dachev <dachev@enhancv.com>
2016-07-18 19:27:32 +0300

    Enhancv Bastard Bootstrap Theme initial commit

---
+++ content/themes/enhancv/assets/js/bastard.js
@@ -0,0 +1,91 @@
+/**
+ *  Bastard - Main JavaScript file
+ */
+/**
+ * Main JS file for Bastard theme, behaviours
+ * Based on Casper theme js - /content/themes/casper/assets/js
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function() {
+        /* Higlight.js */
+        hljs.initHighlightingOnLoad();
+        /* Scroll to Top */
+        $("#scroll-to-top").click(function() {
+          $('body').animate({ scrollTop:$('#top').position().top }, 'slow');
+          return false;
+        });
+        /* Custom JavaScript for the Slide Menu */
+        $("#slidemenu-close").click(function(e) {
+            e.preventDefault();
+            $("#slidemenu-wrapper").toggleClass("active");
+        });
+        $("#slidemenu-toggle").click(function(e) {
+            e.preventDefault();
+            $("#slidemenu-wrapper").toggleClass("active");
+        });
+        /* FitVids */
+        $(".post-content").fitVids();
+        /* Cover Parralax effect */
+        $(".site-head").parallax("70%", 0.2);
+
+        $('.page-scroll a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1600, 'easeInOutExpo');
+            event.preventDefault();
+        });
+
+        function bastardFullImg() {
+            $("img").each( function() {
+                var contentWidth = $(".post-content").outerWidth(); // Width of the content
+                var imageWidth = $(this)[0].naturalWidth; // Original image resolution
+
+                if (imageWidth >= contentWidth) {
+                    $(this).addClass('full-img');
+                } else {
+                    $(this).removeClass('full-img');
+                }
+            });
+        }
+
+        bastardFullImg();
+        $(window).smartresize(bastardFullImg);
+
+    });
+
+}(jQuery));
+
+(function($,sr) {
+
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap) {
+                  func.apply(obj, args);
+              }
+              timeout = null;
+          }
+
+          if (timeout) {
+              clearTimeout(timeout);
+          } else if (execAsap) {
+              func.apply(obj, args);
+          }
+
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  };
+  // smartresize
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
---
+++ content/themes/enhancv/assets/js/jquery.parallax-1.1.3.js
@@ -0,0 +1,69 @@
+/*
+Plugin: jQuery Parallax
+Version 1.1.3
+Author: Ian Lunn
+Twitter: @IanLunn
+Author URL: http://www.ianlunn.co.uk/
+Plugin URL: http://www.ianlunn.co.uk/plugins/jquery-parallax/
+
+Dual licensed under the MIT and GPL licenses:
+http://www.opensource.org/licenses/mit-license.php
+http://www.gnu.org/licenses/gpl.html
+*/
+
+(function( $ ){
+	var $window = $(window);
+	var windowHeight = $window.height();
+
+	$window.resize(function () {
+		windowHeight = $window.height();
+	});
+
+	$.fn.parallax = function(xpos, speedFactor, outerHeight) {
+		var $this = $(this);
+		var getHeight;
+		var firstTop;
+		var paddingTop = 0;
+		
+		//get the starting position of each element to have parallax applied to it		
+		$this.each(function(){
+		    firstTop = $this.offset().top;
+		});
+
+		if (outerHeight) {
+			getHeight = function(jqo) {
+				return jqo.outerHeight(true);
+			};
+		} else {
+			getHeight = function(jqo) {
+				return jqo.height();
+			};
+		}
+			
+		// setup defaults if arguments aren't specified
+		if (arguments.length < 1 || xpos === null) xpos = "50%";
+		if (arguments.length < 2 || speedFactor === null) speedFactor = 0.1;
+		if (arguments.length < 3 || outerHeight === null) outerHeight = true;
+		
+		// function to be called whenever the window is scrolled or resized
+		function update(){
+			var pos = $window.scrollTop();				
+
+			$this.each(function(){
+				var $element = $(this);
+				var top = $element.offset().top;
+				var height = getHeight($element);
+
+				// Check if totally above or totally below viewport
+				if (top + height < pos || top > pos + windowHeight) {
+					return;
+				}
+
+				$this.css('backgroundPosition', xpos + " " + Math.round((firstTop - pos) * speedFactor) + "px");
+			});
+		}		
+
+		$window.bind('scroll', update).resize(update);
+		update();
+	};
+})(jQuery);
