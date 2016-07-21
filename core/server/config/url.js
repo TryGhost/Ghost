6197b82f4ac4ac9f69a97e25cfaa25e8bd8e8e4d
Lukas Strassel <lukasstrassel@googlemail.com>
2016-07-21 15:05:13 +0200

    Allow usage of blogurl:port inside of navigation (#6998)
    closes #6893
    
    - modified logic to allow urls in form of domain:port to be referenced in navigation
    - added a test a domain:port link

--- core/server/config/url.js
+++ core/server/config/url.js
@@ -204,11 +204,13 @@ function urlFor(context, data, absolute) {
             baseUrl = getBaseUrl(secure);
             hostname = baseUrl.split('//')[1] + ghostConfig.paths.subdir;
             if (urlPath.indexOf(hostname) > -1
-                && !urlPath.split(hostname)[0].match(/\.|mailto:/)) {
+                && !urlPath.split(hostname)[0].match(/\.|mailto:/)
+                && urlPath.split(hostname)[1].substring(0,1) !== ':') {
                 // make link relative to account for possible
                 // mismatch in http/https etc, force absolute
                 // do not do so if link is a subdomain of blog url
                 // or if hostname is inside of the slug
+                // or if slug is a port
                 urlPath = urlPath.split(hostname)[1];
                 if (urlPath.substring(0, 1) !== '/') {
                     urlPath = '/' + urlPath;
