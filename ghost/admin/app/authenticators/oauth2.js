94cf89dc75c7e2d0b1464ee83d80f5ab54363c9a
Vijay Kandy <vkandy@users.noreply.github.com>
2016-07-20 22:52:15 -0600

    authentication fix for intermittent internet connection (#131)
    closes TryGhost/Ghost#6868
    
    - token is refreshed when internet connectivity is established provided the session is still active
    - Added navigator.online handler which listens for browser's online status.
    - _refreshAccessTokens() is explicitly called when browser becomes online

--- ghost/admin/app/authenticators/oauth2.js
+++ ghost/admin/app/authenticators/oauth2.js
@@ -1,10 +1,21 @@
 import computed from 'ember-computed';
 import injectService from 'ember-service/inject';
 import Authenticator from 'ember-simple-auth/authenticators/oauth2-password-grant';
+import run from 'ember-runloop';
 
 export default Authenticator.extend({
+    session: injectService(),
     config: injectService(),
     ghostPaths: injectService(),
+
+    init() {
+        this._super(...arguments);
+
+        let handler = run.bind(this, () => {
+            this.onOnline();
+        });
+        window.addEventListener('online', handler);
+    },
 
     serverTokenEndpoint: computed('ghostPaths.apiRoot', function () {
         return `${this.get('ghostPaths.apiRoot')}/authentication/token`;
@@ -20,5 +31,21 @@ export default Authenticator.extend({
         data.client_secret = this.get('config.clientSecret');
         /* jscs:enable requireCamelCaseOrUpperCaseIdentifiers */
         return this._super(url, data);
+    },
+
+    /**
+     * Invoked when "navigator.online" event is trigerred.
+     * This is a helper function to handle intermittent internet connectivity. Token is refreshed
+     * when browser status becomes "online".
+     */
+    onOnline() {
+        if (this.get('session.isAuthenticated')) {
+            let autoRefresh = this.get('refreshAccessTokens');
+            if (autoRefresh) {
+                let expiresIn = this.get('session.data.authenticated.expires_in');
+                let token = this.get('session.data.authenticated.refresh_token');
+                return this._refreshAccessToken(expiresIn, token);
+            }
+        }
     }
 });
