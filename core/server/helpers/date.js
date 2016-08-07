16f1cfece6160caef1d59a4d155a6832d2fa26d8
Saulo Toledo <st.public@live.com>
2016-08-07 15:07:41 -0300

    Proper usage of Moment.js module in date helper
    - creates just one Moment.js instance per helper call
    - previously it was creating a Moment.js instance only to get a time string for this.published_at
    - code refactoring should improve performance a bit

--- core/server/helpers/date.js
+++ core/server/helpers/date.js
@@ -3,37 +3,28 @@
 //
 // Formats a date using moment-timezone.js. Formats published_at by default but will also take a date as a parameter
 
-var moment          = require('moment-timezone'),
-    date,
-    timezone;
+var moment = require('moment-timezone'),
+    date;
 
 date = function (date, options) {
     if (!options && date.hasOwnProperty('hash')) {
         options = date;
-        date = undefined;
-        timezone = options.data.blog.timezone;
-
-        // set to published_at by default, if it's available
-        // otherwise, this will print the current date
-        if (this.published_at) {
-            date = moment(this.published_at).tz(timezone).format();
-        }
-    }
-
-    // ensure that context is undefined, not null, as that can cause errors
-    date = date === null ? undefined : date;
-
-    var f = options.hash.format || 'MMM DD, YYYY',
-        timeago = options.hash.timeago,
-        timeNow = moment().tz(timezone);
-
-    if (timeago) {
-        date = timezone ?  moment(date).tz(timezone).from(timeNow) : moment(date).fromNow();
+        date = moment(this.published_at || new Date());
     } else {
-        date = timezone ? moment(date).tz(timezone).format(f) : moment(date).format(f);
+        date = moment(date);
     }
 
-    return date;
+    var timezone = options.data.blog.timezone;
+
+    if (timezone) {
+        date = date.tz(timezone);
+    }
+
+    if (options.hash.timeago) {
+        return timezone ? date.from(moment.tz(timezone)) : date.fromNow();
+    }
+
+    return date.format(options.hash.format || 'MMM DD, YYYY');
 };
 
 module.exports = date;
