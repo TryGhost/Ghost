3d446b5ba05afa54b1d6e81fc423b8a368a0b31d
Aileen Nowak <AileenCGN@gmail.com>
2016-08-16 08:52:54 +0200

    🎨 Use `context` in meta as array [AMP preparation]
    refs #6588
    
    Instead of reading the first value of the context array, we're checking if it includes certain context values.
    This is a preparation change for AMP, where the context will be delivered as `['amp', 'post']`.

--- core/server/data/meta/author_image.js
+++ core/server/data/meta/author_image.js
@@ -1,11 +1,12 @@
 var config           = require('../../config'),
-    getContextObject = require('./context_object.js');
+    getContextObject = require('./context_object.js'),
+    _                = require('lodash');
 
 function getAuthorImage(data, absolute) {
-    var context = data.context ? data.context[0] : null,
+    var context = data.context ? data.context : null,
         contextObject = getContextObject(data, context);
 
-    if ((context === 'post' || context === 'page') && contextObject.author && contextObject.author.image) {
+    if ((_.includes(context, 'post') || _.includes(context, 'page')) && contextObject.author && contextObject.author.image) {
         return config.urlFor('image', {image: contextObject.author.image}, absolute);
     }
     return null;
