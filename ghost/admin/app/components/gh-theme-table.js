990889f3cecbd6e8b6a6535b61eb88b68c10a4ed
Kevin Ansfield <kevin@lookingsideways.co.uk>
2016-08-23 16:55:23 +0100

    ✨ indicate folder names to tell duplicate themes apart
    refs https://github.com/TryGhost/Ghost-Admin/pull/210
    - removes unused `activeTheme` property on `gh-theme-table`
    - updates label generation in `gh-theme-table` to add folder names when there are duplicate package.json name+version combos

--- ghost/admin/app/components/gh-theme-table.js
+++ ghost/admin/app/components/gh-theme-table.js
@@ -4,21 +4,58 @@ import computed from 'ember-computed';
 export default Component.extend({
 
     availableThemes: null,
-    activeTheme: null,
 
-    themes: computed('availableThemes', 'activeTheme', function () {
-        return this.get('availableThemes').map((t) => {
+    themes: computed('availableThemes', function () {
+        let themes = this.get('availableThemes').map((t) => {
             let theme = {};
 
             theme.name = t.name;
             theme.label = t.package ? `${t.package.name} - ${t.package.version}` : t.name;
             theme.package = t.package;
             theme.active = !!t.active;
-            theme.isDefault = t.name === 'casper';
-            theme.isDeletable = !theme.active && !theme.isDefault;
+            theme.isDeletable = !theme.active;
 
             return theme;
-        }).sortBy('label');
+        });
+        let duplicateThemes = [];
+
+        themes.forEach((theme) => {
+            let duplicateLabels = themes.filterBy('label', theme.label);
+
+            if (duplicateLabels.length > 1) {
+                duplicateThemes.pushObject(theme);
+            }
+        });
+
+        duplicateThemes.forEach((theme) => {
+            if (theme.name !== 'casper') {
+                theme.label = `${theme.label} (${theme.name})`;
+            }
+        });
+
+        // "(default)" needs to be added to casper manually as it's always
+        // displayed and would mess up the duplicate checking if added earlier
+        let casper = themes.findBy('name', 'casper');
+        if (casper) {
+            casper.label = `${casper.label} (default)`;
+            casper.isDefault = true;
+            casper.isDeletable = false;
+        }
+
+        // sorting manually because .sortBy('label') has a different sorting
+        // algorithm to [...strings].sort()
+        return themes.sort((themeA, themeB) => {
+            let a = themeA.label.toLowerCase();
+            let b = themeB.label.toLowerCase();
+
+            if (a < b) {
+                return -1;
+            }
+            if (a > b) {
+                return 1;
+            }
+            return 0;
+        });
     }).readOnly()
 
 });
