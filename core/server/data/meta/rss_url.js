67f3f4b27a21bfa19f69ac6cc70bf77552a318e2
Katharina Irrgang <katharina.irrgang@googlemail.com>
2016-09-09 12:23:47 +0200

    :art: source out url utils from ConfigManager (#7347)
    refs #6982

--- core/server/data/meta/rss_url.js
+++ core/server/data/meta/rss_url.js
@@ -1,7 +1,7 @@
-var config = require('../../config');
+var utils = require('../../utils');
 
 function getRssUrl(data, absolute) {
-    return config.urlFor('rss', {secure: data.secure}, absolute);
+    return utils.url.urlFor('rss', {secure: data.secure}, absolute);
 }
 
 module.exports = getRssUrl;
