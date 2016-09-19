bc8c860604bbc061eb88f7e8f888e5edccc607a1
kirrg001 <katharina.irrgang@googlemail.com>
2016-09-19 18:18:52 +0200

    🛠  update lodash,knex and bookshelf to latest versions
    refs #7189
    - we had a memory leak after upgrading to knex 0.11.x
    - knex has published a new version 0.12.x
    - the memory leak does not longer exists
    - knex has reverted their pool logic, see https://github.com/tgriesser/knex/pull/1665

--- core/server/overrides.js
+++ core/server/overrides.js
@@ -8,24 +8,3 @@ var moment = require('moment-timezone');
  *   - be careful when you work with date operations, therefor always wrap a date into moment
  */
 moment.tz.setDefault('UTC');
-
-// jscs:disable
-var _ = require('lodash'),
-    toPairs = require('lodash.topairs'),
-    fromPairs = require('lodash.frompairs'),
-    toString = require('lodash.tostring'),
-    pickBy = require('lodash.pickby'),
-    uniqBy = require('lodash.uniqby'),
-    orderBy = require('lodash.orderby'),
-    omitBy = require('lodash.omitby');
-
-/**
- * lodash 4.x functions we use
- */
-_.toPairs = toPairs;
-_.fromPairs = fromPairs;
-_.toString = toString;
-_.omitBy = omitBy;
-_.uniqBy = uniqBy;
-_.pickBy = pickBy;
-_.orderBy = orderBy;
