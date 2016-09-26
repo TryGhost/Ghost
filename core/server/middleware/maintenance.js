39377afe88767d823ed9966f17a34abda6b35f4d
Hannah Wolfe <github.erisds@gmail.com>
2016-09-26 10:10:44 +0100

    💄 Ensure all middleware use named functions (#7434)
    no issue
    
    - anonymous functions are hard to debug in memory traces etc
    - having anonymous middleware functions makes it hard to inspect or debug the middleware stack (something I like to do)
    - these 2 are the only ones atm, including all 3rd party middleware

--- core/server/middleware/maintenance.js
+++ core/server/middleware/maintenance.js
@@ -2,7 +2,7 @@ var config = require('../config'),
     i18n   = require('../i18n'),
     errors = require('../errors');
 
-module.exports = function (req, res, next) {
+module.exports = function maintenance(req, res, next) {
     if (config.get('maintenance').enabled) {
         return next(new errors.Maintenance(
             i18n.t('errors.general.maintenance')
