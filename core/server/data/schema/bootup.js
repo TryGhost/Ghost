885bb25af1a9ac329373bbfb0646e2cb87774b94
kirrg001 <katharina.irrgang@googlemail.com>
2016-10-05 23:34:01 +0200

    🎨  change bootUp file
    - add big picture description
    - version error get's trigger from versioning module
    
    [ci skip]

--- core/server/data/schema/bootup.js
+++ core/server/data/schema/bootup.js
@@ -4,20 +4,29 @@ var Promise = require('bluebird'),
     errors = require('./../../errors');
 
 module.exports = function bootUp() {
+    /**
+     * @TODO:
+     * - 1. call is check if tables are populated
+     * - 2. call is check if db is seeded
+     *
+     * These are the calls Ghost will make to find out if the db is in OK state!
+     * These check's will have nothing to do with the migration module!
+     * Ghost will not touch the migration module at all.
+     *
+     * Example code:
+     * models.Settings.findOne({key: 'databasePopulated'})
+     * If not, throw error and tell user what to do (ghost db-init)!
+     *
+     * versioning.getDatabaseVersion() - not sure about that yet.
+     * This will read the database version of the settings table!
+     * If not, throw error and tell user what to do (ghost db-seed)!
+     *
+     * @TODO:
+     * - remove return populate() -> belongs to db init
+     */
     return versioning
         .getDatabaseVersion()
-        .then(function successHandler(result) {
-            if (!/^alpha/.test(result)) {
-                // This database was not created with Ghost alpha, and is not compatible
-                throw new errors.DatabaseVersionError({
-                    message: 'Your database version is not compatible with Ghost 1.0.0 Alpha (master branch)',
-                    context: 'Want to keep your DB? Use Ghost < 1.0.0 or the "stable" branch. Otherwise please delete your DB and restart Ghost',
-                    help: 'More information on the Ghost 1.0.0 Alpha at https://support.ghost.org/v1-0-alpha'
-                });
-            }
-        },
-        // We don't use .catch here, as it would catch the error from the successHandler
-        function errorHandler(err) {
+        .catch(function errorHandler(err) {
             if (err instanceof errors.DatabaseNotPopulatedError) {
                 return populate();
             }
