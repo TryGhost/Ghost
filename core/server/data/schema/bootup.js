8fbde2f94d02c84b2d45656c290a72c1d6b3612b
kirrg001 <katharina.irrgang@googlemail.com>
2016-10-06 16:05:16 +0200

    🎨  use sephiroth in bootUp script
    - do not populate the database
    - ask sephiroth for database state
    - do seeding manually (this will be removed in next seeding PR)
    [ci skip]

--- core/server/data/schema/bootup.js
+++ core/server/data/schema/bootup.js
@@ -1,35 +1,47 @@
 var Promise = require('bluebird'),
-    versioning = require('./versioning'),
-    populate = require('../migration/populate'),
-    errors = require('./../../errors');
+    Sephiroth = require('../sephiroth'),
+    db = require('../db'),
+    config = require('./../../config'),
+    errors = require('./../../errors'),
+    models = require('./../../models'),
+    versioning = require('./../../data/schema/versioning'),
+    logging = require('./../../logging'),
+    fixtures = require('../migration/fixtures'),
+    i18n = require('./../../i18n'),
+    sephiroth = new Sephiroth({database: config.get('database')});
 
+/**
+ * @TODO:
+ * - move this file out of schema folder
+ * - this file right now takes over seeding, which get's fixed in one of the next PR's
+ * - remove fixtures.populate
+ * - remove versioning.setDatabaseVersion(transaction);
+ * - remove models.Settings.populateDefaults(_.merge({}, {transacting: transaction}, modelOptions));
+ * - key: migrations-kate
+ */
 module.exports = function bootUp() {
-    /**
-     * @TODO:
-     * - 1. call is check if tables are populated
-     * - 2. call is check if db is seeded
-     *
-     * These are the calls Ghost will make to find out if the db is in OK state!
-     * These check's will have nothing to do with the migration module!
-     * Ghost will not touch the migration module at all.
-     *
-     * Example code:
-     * models.Settings.findOne({key: 'databasePopulated'})
-     * If not, throw error and tell user what to do (ghost db-init)!
-     *
-     * versioning.getDatabaseVersion() - not sure about that yet.
-     * This will read the database version of the settings table!
-     * If not, throw error and tell user what to do (ghost db-seed)!
-     *
-     * @TODO:
-     * - remove return populate() -> belongs to db init
-     * - key: migrations-kate
-     */
-    return versioning
-        .getDatabaseVersion()
-        .catch(function errorHandler(err) {
-            if (err instanceof errors.DatabaseNotPopulatedError) {
-                return populate();
+    var modelOptions = {
+        context: {
+            internal: true
+        }
+    };
+
+    return sephiroth.utils.isDatabaseOK()
+        .then(function () {
+            return models.Settings.populateDefaults(modelOptions);
+        })
+        .then(function () {
+            return versioning.setDatabaseVersion(db.knex);
+        })
+        .then(function () {
+            return fixtures.populate(logging, modelOptions);
+        })
+        .catch(function (err) {
+            if ([sephiroth.utils.errors.dbInitMissing, sephiroth.utils.errors.migrationsTableMissing].indexOf(err.code) !== -1) {
+                return Promise.reject(new errors.DatabaseNotPopulated(
+                    i18n.t('errors.data.versioning.index.databaseNotInitialised'),
+                    i18n.t('errors.data.versioning.index.databaseNotInitialisedContext')
+                ));
             }
 
             return Promise.reject(err);
