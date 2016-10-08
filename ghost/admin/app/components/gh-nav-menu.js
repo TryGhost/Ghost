aea6c24c9df480e3bdc87ccd443dad43b70b5142
Austin Burdine <acburdine@users.noreply.github.com>
2016-10-08 06:16:45 -0500

    :art: fix path of nav menu ghost icon (#317)
    refs TryGhost/Ghost#7503

--- ghost/admin/app/components/gh-nav-menu.js
+++ ghost/admin/app/components/gh-nav-menu.js
@@ -11,7 +11,7 @@ export default Component.extend({
     open: false,
 
     navMenuIcon: computed('ghostPaths.subdir', function () {
-        let url = `${this.get('ghostPaths.subdir')}/ghost/img/ghosticon.jpg`;
+        let url = `${this.get('ghostPaths.assetRoot')}/img/ghosticon.jpg`;
 
         return htmlSafe(`background-image: url(${url})`);
     }),
