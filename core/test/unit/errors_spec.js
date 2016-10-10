f570aaef3cdfa9be9338ff88f4b6420760f839e6
Katharina Irrgang <katharina.irrgang@googlemail.com>
2016-10-10 19:30:30 +0200

    🎨  optimise error to inherit from in GhostError prototype (#7529)
    refs #7116
    - add errors_spec
    - inherit all given attribute values

---
+++ core/test/unit/errors_spec.js
@@ -0,0 +1,66 @@
+var errors = require('../../server/errors'),
+    should = require('should');
+
+should.equal(true, true);
+
+describe('Errors', function () {
+    describe('Inherite from other error', function () {
+        it('default', function () {
+            var someError = new Error(), ghostError;
+
+            someError.message = 'test';
+            someError.context = 'test';
+            someError.help = 'test';
+
+            ghostError = new errors.GhostError({err: someError});
+            ghostError.message.should.eql(someError.message);
+            ghostError.context.should.eql(someError.context);
+            ghostError.help.should.eql(someError.help);
+        });
+
+        it('has nested object', function () {
+            var someError = new Error(), ghostError;
+
+            someError.message = 'test';
+            someError.obj = {
+                a: 'b'
+            };
+
+            ghostError = new errors.GhostError({
+                err: someError
+            });
+
+            ghostError.message.should.eql(someError.message);
+            ghostError.obj.should.eql(someError.obj);
+        });
+
+        it('with custom attribute', function () {
+            var someError = new Error(), ghostError;
+
+            someError.message = 'test';
+            someError.context = 'test';
+
+            ghostError = new errors.GhostError({
+                err: someError,
+                context: 'context'
+            });
+
+            ghostError.message.should.eql(someError.message);
+            ghostError.context.should.eql('test');
+        });
+
+        it('with custom attribute', function () {
+            var someError = new Error(), ghostError;
+
+            someError.message = 'test';
+
+            ghostError = new errors.GhostError({
+                err: someError,
+                context: 'context'
+            });
+
+            ghostError.message.should.eql(someError.message);
+            ghostError.context.should.eql('context');
+        });
+    });
+});
