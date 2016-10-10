505e75e8658321198c9ec280d08edbb1be26a80a
Katharina Irrgang <katharina.irrgang@googlemail.com>
2016-10-10 14:27:31 +0200

    ✨  Migration runner - first iteration (#7501)
    refs #7489
    - add independent migratio runner
    - add init script
    - this is not connected to Ghost yet, but next PR will

---
+++ core/server/data/sephiroth/lib/database.js
@@ -0,0 +1,19 @@
+var knex = require('knex');
+
+/**
+ * we only support knex
+ */
+exports.connect = function connect(options) {
+    var client = options.client;
+
+    if (client === 'sqlite3') {
+        options.useNullAsDefault = options.useNullAsDefault || false;
+    }
+
+    if (client === 'mysql') {
+        options.connection.timezone = 'UTC';
+        options.connection.charset = 'utf8mb4';
+    }
+
+    exports.knex = knex(options);
+};
