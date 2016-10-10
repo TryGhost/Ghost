3510b730455c434f9d863628ce7bee68fc3f3209
Jeff Jewiss <jeffjewiss@gmail.com>
2016-10-10 03:07:39 +0000

    Updates utils unit tests to use promises

--- core/test/unit/utils/cached-image-size-from-url_spec.js
+++ core/test/unit/utils/cached-image-size-from-url_spec.js
@@ -21,7 +21,7 @@ describe('getCachedImageSizeFromUrl', function () {
         getCachedImageSizeFromUrl.__set__('imageSizeCache', {});
     });
 
-    it('should read from cache, if dimensions for image are fetched already', function (done) {
+    it('should read from cache, if dimensions for image are fetched already', function () {
         var url = 'http://mysite.com/content/image/mypostcoverimage.jpg';
 
         sizeOfStub.returns(new Promise.resolve({
@@ -32,7 +32,7 @@ describe('getCachedImageSizeFromUrl', function () {
 
         getCachedImageSizeFromUrl.__set__('getImageSizeFromUrl', sizeOfStub);
 
-        getCachedImageSizeFromUrl(url).then(function () {
+        return getCachedImageSizeFromUrl(url).then(function () {
             // first call to get result from `getImageSizeFromUrl`
             cachedImagedSize = getCachedImageSizeFromUrl.__get__('imageSizeCache');
             should.exist(cachedImagedSize);
@@ -50,37 +50,32 @@ describe('getCachedImageSizeFromUrl', function () {
                 cachedImagedSize[url].width.should.be.equal(50);
                 should.exist(cachedImagedSize[url].height);
                 cachedImagedSize[url].height.should.be.equal(50);
-
-                done();
             });
-        }).catch(done);
+        });
     });
 
-    it('can handle image-size errors', function (done) {
+    it('can handle image-size errors', function () {
         var url = 'http://mysite.com/content/image/mypostcoverimage.jpg';
 
         sizeOfStub.returns(new Promise.reject('error'));
 
         getCachedImageSizeFromUrl.__set__('getImageSizeFromUrl', sizeOfStub);
 
-        getCachedImageSizeFromUrl(url)
-        .then(function () {
+        return getCachedImageSizeFromUrl(url).then(function () {
             cachedImagedSize = getCachedImageSizeFromUrl.__get__('imageSizeCache');
             should.exist(cachedImagedSize);
             cachedImagedSize.should.have.property(url);
             should.not.exist(cachedImagedSize[url].width);
             should.not.exist(cachedImagedSize[url].height);
-            done();
-        }).catch(done);
+        });
     });
 
-    it('should return null if url is undefined', function (done) {
+    it('should return null if url is undefined', function () {
         var url = null,
             result;
 
         result = getCachedImageSizeFromUrl(url);
 
         should.not.exist(result);
-        done();
     });
 });
