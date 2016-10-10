c4fa34224f7e677e9784d297ca0c8db96b4cb8f7
Katharina Irrgang <katharina.irrgang@googlemail.com>
2016-10-10 14:27:31 +0200

    ✨  Migration runner - first iteration (#7501)
    refs #7489
    - add independent migratio runner
    - add init script
    - this is not connected to Ghost yet, but next PR will

---
+++ core/server/data/sephiroth/lib/commands/init.js
@@ -0,0 +1,49 @@
+var Promise = require('bluebird'),
+    path = require('path'),
+    utils = require('../utils'),
+    logging = require('../../../../logging');
+
+/**
+ * @TODO:
+ * - better error handling
+ * - prettier code please
+ * - dirty requires
+ */
+module.exports = function init(options) {
+    options = options || {};
+
+    var migrationFolder = options.migrationFolder || path.join(__dirname, '../../../migrations'),
+        dbInitTasks = utils.readTasks(migrationFolder + '/init');
+
+    return utils.createTransaction(function executeTasks(transaction) {
+        return Promise.each(dbInitTasks, function executeInitTask(task) {
+            return utils.preTask({
+                database: transaction,
+                task: task.name,
+                type: 'init'
+            }).then(function () {
+                logging.info('Running: ' + task.name);
+
+                return task.execute({
+                    database: transaction
+                });
+            }).then(function () {
+                return utils.postTask({
+                    database: transaction,
+                    task: task.name,
+                    type: 'init'
+                });
+            }).catch(function (err) {
+                if (err.code === utils.errors.taskFound) {
+                    logging.warn('Skipping:' + task.name);
+                    return Promise.resolve();
+                }
+
+                throw err;
+            });
+        });
+    }).catch(function (err) {
+        logging.warn('rolling back...');
+        return Promise.reject(err);
+    });
+};
