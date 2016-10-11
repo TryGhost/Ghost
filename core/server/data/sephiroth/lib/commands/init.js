18b3ac7028ff7fca4d7dc52bc80446b2c4106c73
kirrg001 <katharina.irrgang@googlemail.com>
2016-10-11 17:05:30 +0200

    ✨  sephiroth: allow params for init command
    - param: skip (do not run this script)
    - param: only (only run this script)
    - very simple way
    
    [ci skip]

--- core/server/data/sephiroth/lib/commands/init.js
+++ core/server/data/sephiroth/lib/commands/init.js
@@ -1,5 +1,4 @@
 var Promise = require('bluebird'),
-    path = require('path'),
     utils = require('../utils'),
     errors = require('../errors'),
     logging = require('../../../../logging');
@@ -9,12 +8,24 @@ var Promise = require('bluebird'),
  * - better error handling
  * - prettier code please
  * - dirty requires
+ *
+ * sephiroth init --only 1 (execute first script only)
+ * sephiroth init --skip 2 (execute all except of 2)
+ * sephiroth migrate --version 1.0 --only 1
  */
 module.exports = function init(options) {
     options = options || {};
 
     var initPath = utils.getPath({type: 'init'}),
-        dbInitTasks = utils.readTasks(initPath);
+        dbInitTasks = utils.readTasks(initPath),
+        skip = options.skip || null,
+        only = options.only || null;
+
+    if (only !== null) {
+        dbInitTasks = [dbInitTasks[only - 1]];
+    } else if (skip !== null) {
+        dbInitTasks.splice(skip - 1, 1);
+    }
 
     return utils.createTransaction(function executeTasks(transacting) {
         return Promise.each(dbInitTasks, function executeInitTask(task) {
