03af2ea0cfdbd4f135e4c015c64f13288299b5ab
kirrg001 <katharina.irrgang@googlemail.com>
2016-10-13 23:56:56 +0200

    ✨  knex migrator
    refs #7489
    - remove sephiroth
    - use knex migrator npm
    - goodbye bootup script

--- core/server/data/schema/index.js
+++ core/server/data/schema/index.js
@@ -3,4 +3,3 @@ module.exports.checks = require('./checks');
 module.exports.commands = require('./commands');
 module.exports.versioning = require('./versioning');
 module.exports.defaultSettings = require('./default-settings');
-module.exports.bootUp = require('./bootup');
