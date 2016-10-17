a4be73930281bc2b0077c93f8ac72cf81077690b
Kevin Ansfield <kevin@lookingsideways.co.uk>
2016-10-17 14:32:22 +0100

    attempt expired session restore on network request (#327)
    refs TryGhost/Ghost#5202
    
    We can get into a situation where the app is left open without a
    network connection and the token subsequently expires, this will
    result in the next network request returning a 401 and killing the
    session. This is an attempt to detect that and restore the session
    using the stored refresh token before continuing with the request
    
    - wrap ajax requests in a session restore request if we detect an expired `access_token`

--- ghost/admin/tests/integration/services/ajax-test.js
+++ ghost/admin/tests/integration/services/ajax-test.js
@@ -14,6 +14,8 @@ import {
     isUnsupportedMediaTypeError
 } from 'ghost-admin/services/ajax';
 import config from 'ghost-admin/config/environment';
+import Service from 'ember-service';
+import RSVP from 'rsvp';
 
 function stubAjaxEndpoint(server, response = {}, code = 200) {
     server.get('/test/', function () {
@@ -174,6 +176,98 @@ describeModule(
             }).catch((error) => {
                 expect(isUnsupportedMediaTypeError(error)).to.be.true;
                 done();
+            });
+        });
+
+        /* jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
+        describe('session handling', function () {
+            let successfulRequest = false;
+
+            let sessionStub = Service.extend({
+                isAuthenticated: true,
+                restoreCalled: false,
+                authenticated: null,
+
+                init() {
+                    this.authenticated = {
+                        expires_at: (new Date()).getTime() - 10000,
+                        refresh_token: 'RefreshMe123'
+                    };
+                },
+
+                restore() {
+                    this.restoreCalled = true;
+                    this.authenticated.expires_at = (new Date()).getTime() + 10000;
+                    return RSVP.resolve();
+                },
+
+                authorize() {
+
+                }
+            });
+
+            beforeEach(function () {
+                server.get('/ghost/api/v0.1/test/', function () {
+                    return [
+                        200,
+                        {'Content-Type': 'application/json'},
+                        JSON.stringify({
+                            success: true
+                        })
+                    ];
+                });
+
+                server.post('/ghost/api/v0.1/authentication/token', function () {
+                    return [
+                        401,
+                        {'Content-Type': 'application/json'},
+                        JSON.stringify({})
+                    ];
+                });
+            });
+
+            it('can restore an expired session', function (done) {
+                let ajax = this.subject();
+                ajax.set('session', sessionStub.create());
+
+                ajax.request('/ghost/api/v0.1/test/');
+
+                ajax.request('/ghost/api/v0.1/test/').then((result) => {
+                    expect(ajax.get('session.restoreCalled'), 'restoreCalled').to.be.true;
+                    expect(result.success, 'result.success').to.be.true;
+                    done();
+                }).catch((error) => {
+                    expect(true, 'request failed').to.be.false;
+                    done();
+                });
+            });
+
+            it('errors correctly when session restoration fails', function (done) {
+                let ajax = this.subject();
+                let invalidateCalled = false;
+
+                ajax.set('session', sessionStub.create());
+                ajax.set('session.restore', function () {
+                    this.set('restoreCalled', true);
+                    return ajax.post('/ghost/api/v0.1/authentication/token');
+                });
+                ajax.set('session.invalidate', function () {
+                    invalidateCalled = true;
+                });
+
+                stubAjaxEndpoint(server, {}, 401);
+
+                ajax.request('/ghost/api/v0.1/test/').then(() => {
+                    expect(true, 'request was successful').to.be.false;
+                    done();
+                }).catch((error) => {
+                    // TODO: fix the error return when a session restore fails
+                    // expect(isUnauthorizedError(error)).to.be.true;
+                    expect(ajax.get('session.restoreCalled'), 'restoreCalled').to.be.true;
+                    expect(successfulRequest, 'successfulRequest').to.be.false;
+                    expect(invalidateCalled, 'invalidateCalled').to.be.true;
+                    done();
+                });
             });
         });
     }
