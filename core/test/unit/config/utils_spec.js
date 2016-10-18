fd0a08ae8cb7e863067f6469019a7ff70443f857
Katharina Irrgang <katharina.irrgang@googlemail.com>
2016-10-18 10:04:44 +0200

    🎨 make sqlite filename absolute (#7585)
    no issue
    
    - add tests for makePathsAbsolute
    - add support for windows paths
    
    When Ghost-CLI inits the database of the current GhostVersion (in /current), then it uses knex-migrator to do that.
    Knex migrator is reading the .knex-migrator file of the current Ghost version. This returns a relative path to the database location.
    The problem: knex-migrator will init the database in the root folder of Ghost-CLI /content/data instead of /current/content . And when you start Ghost (ghost start), it always complains that
    that database is not initialised, because it expects the database in /current/content...
    
    * 🎨  move config_spec to config/index_spec
    - add one more test case

---
+++ core/test/unit/config/utils_spec.js
@@ -0,0 +1,83 @@
+var configUtils = require('../../../server/config/utils'),
+    should = require('should');
+
+should.equal(true, true);
+
+describe('UNIT: Config utils', function () {
+    describe('makePathsAbsolute', function () {
+        it('ensure we change paths only', function () {
+            var changedKey = [],
+                obj = {
+                    database: {
+                        client: 'mysql',
+                        connection: {
+                            filename: 'content/data/ghost.db'
+                        }
+                    }
+                };
+
+            this.set = function (key, value) {
+                changedKey.push([key, value]);
+            };
+
+            configUtils.makePathsAbsolute.bind(this)(obj.database, 'database');
+
+            changedKey.length.should.eql(1);
+            changedKey[0][0].should.eql('database:connection:filename');
+            changedKey[0][1].should.not.eql('content/data/ghost.db');
+        });
+
+        it('ensure it skips non strings', function () {
+            var changedKey = [],
+                obj = {
+                    database: {
+                        test: 10
+                    }
+                };
+
+            this.set = function (key, value) {
+                changedKey.push([key, value]);
+            };
+
+            configUtils.makePathsAbsolute.bind(this)(obj.database, 'database');
+            changedKey.length.should.eql(0);
+        });
+
+        it('ensure we don\' change absolute paths', function () {
+            var changedKey = [],
+                obj = {
+                    database: {
+                        client: 'mysql',
+                        connection: {
+                            filename: '/content/data/ghost.db'
+                        }
+                    }
+                };
+
+            this.set = function (key, value) {
+                changedKey.push([key, value]);
+            };
+
+            configUtils.makePathsAbsolute.bind(this)(obj.database, 'database');
+            changedKey.length.should.eql(0);
+        });
+
+        it('match paths on windows', function () {
+            var changedKey = [],
+                obj = {
+                    database: {
+                        filename: 'content\\data\\ghost.db'
+                    }
+                };
+
+            this.set = function (key, value) {
+                changedKey.push([key, value]);
+            };
+
+            configUtils.makePathsAbsolute.bind(this)(obj.database, 'database');
+            changedKey.length.should.eql(1);
+            changedKey[0][0].should.eql('database:filename');
+            changedKey[0][1].should.not.eql('content\\data\\ghost.db');
+        });
+    });
+});
