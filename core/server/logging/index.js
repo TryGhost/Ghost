08c4bd0690b4dbb3a5255dffc13e98e0b7a8ee72
kirrg001 <katharina.irrgang@googlemail.com>
2016-10-19 19:19:46 +0200

    ✨  shortcuts for mode and level
    [ci skip]

--- core/server/logging/index.js
+++ core/server/logging/index.js
@@ -3,8 +3,8 @@ var config = require('../config'),
     adapter = new GhostLogger({
         domain: config.get('url').replace(/[^\w]/gi, '_'),
         env: config.get('env'),
-        mode: process.env.NODE_MODE || config.get('logging:mode'),
-        level: process.env.NODE_LEVEL || config.get('logging:level'),
+        mode: process.env.NODE_MODE || process.env.MODE || config.get('logging:mode'),
+        level: process.env.NODE_LEVEL || process.env.LEVEL || config.get('logging:level'),
         transports: config.get('logging:transports'),
         rotation: config.get('logging:rotation'),
         path: config.get('logging:path') || config.getContentPath('logs')
