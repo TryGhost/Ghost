6de0bc62c0392405d744c201d4e02d7dfe85ccb9
kirrg001 <katharina.irrgang@googlemail.com>
2016-10-21 19:56:53 +0200

    🕵🏻 do not drop the database connection when running tests
    - please read the comments in the commit

--- core/server/data/migrations/hooks/migrate/after.js
+++ core/server/data/migrations/hooks/migrate/after.js
@@ -1,4 +1,5 @@
 var errors = require('../../../../errors'),
+    config = require('../../../../config'),
     versioning = require('../../../schema/versioning'),
     database = require('../../../db');
 
@@ -12,6 +13,18 @@ module.exports = function after(options) {
             throw err;
         })
         .finally(function destroyConnection() {
+            // do not close database connection in test mode, because all tests are executed one after another
+            // this check is not nice, but there is only one other solution i can think of:
+            // forward a custom object to knex-migrator, which get's forwarded to the hooks
+            if (config.get('env').match(/testing/g)) {
+                return;
+            }
+
+            // we need to close the database connection
+            // the after hook signals the last step of a knex-migrator command
+            // Example:
+            // Ghost-CLI calls knexMigrator.init and afterwards it starts Ghost, but Ghost-CLI can't shutdown
+            // if Ghost keeps a connection alive
             return database.knex.destroy();
         });
 };
