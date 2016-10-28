ecb68c0cb9af64d22aea65e052f19b18ca0028b4
Katharina Irrgang <katharina.irrgang@googlemail.com>
2016-10-28 15:07:46 +0200

    🎨 public config endpoint (#7631)
    closes #7628
    
    With this PR we expose a public configuration endpoint.
    When /ghost is requested, we don't load and render the configurations into the template anymore. Instead, Ghost-Admin can request the public configuration endpoint.
    
    * 🎨  make configuration endpoint public
    * 🔥  remove loading configurations in admin app
    - do not render them into the default html page
    * ✨  load client credentials in configuration endpoint
    - this is not a security issue, because we have exposed this information anyway before (by rendering them into the requested html page)
    * 🎨  extend existing configuration integration test
    * ✨  tests: add ghost-auth to data generator
    * ✨  add functional test
    * 🔥  remove type/value pattern
    * 🎨  do not return stringified JSON objects

--- core/test/integration/api/api_configuration_spec.js
+++ core/test/integration/api/api_configuration_spec.js
@@ -1,18 +1,25 @@
-var testUtils         = require('../../utils'),
-    should            = require('should'),
-    rewire            = require('rewire'),
+var testUtils = require('../../utils'),
+    configUtils = require('../../utils/configUtils'),
+    should = require('should'),
+    rewire = require('rewire'),
 
     // Stuff we are testing
-    ConfigurationAPI  = rewire('../../../server/api/configuration');
+    ConfigurationAPI = rewire('../../../server/api/configuration');
 
 describe('Configuration API', function () {
     // Keep the DB clean
     before(testUtils.teardown);
-    afterEach(testUtils.teardown);
+    beforeEach(testUtils.setup('clients'));
+    afterEach(function () {
+        configUtils.restore();
+        return testUtils.teardown();
+    });
 
     should.exist(ConfigurationAPI);
 
     it('can read basic config and get all expected properties', function (done) {
+        configUtils.set('auth:type', 'ghost');
+
         ConfigurationAPI.read().then(function (response) {
             var props;
 
@@ -21,17 +28,29 @@ describe('Configuration API', function () {
             response.configuration.should.be.an.Array().with.lengthOf(1);
             props = response.configuration[0];
 
-            // Check the structure
-            props.should.have.property('blogUrl').which.is.an.Object().with.properties('type', 'value');
-            props.should.have.property('blogTitle').which.is.an.Object().with.properties('type', 'value');
-            props.should.have.property('routeKeywords').which.is.an.Object().with.properties('type', 'value');
-            props.should.have.property('fileStorage').which.is.an.Object().with.properties('type', 'value');
-            props.should.have.property('useGravatar').which.is.an.Object().with.properties('type', 'value');
-            props.should.have.property('publicAPI').which.is.an.Object().with.properties('type', 'value');
+            props.blogUrl.should.eql('http://127.0.0.1:2369');
+            props.routeKeywords.should.eql({
+                tag: 'tag',
+                author: 'author',
+                page: 'page',
+                preview: 'p',
+                private: 'private',
+                subscribe: 'subscribe',
+                amp: 'amp'
+            });
 
-            // Check a few values
-            props.blogUrl.should.have.property('value', 'http://127.0.0.1:2369');
-            props.fileStorage.should.have.property('value', true);
+            props.fileStorage.should.eql(true);
+            props.useGravatar.should.eql(true);
+            props.publicAPI.should.eql(false);
+            props.clientId.should.eql('ghost-admin');
+            props.clientSecret.should.eql('not_available');
+            props.ghostAuthUrl.should.eql('http://devauth.ghost.org:8080');
+
+            // value not available, because settings API was not called yet
+            props.hasOwnProperty('blogTitle').should.eql(true);
+
+            // uuid
+            props.hasOwnProperty('ghostAuthId').should.eql(true);
 
             done();
         }).catch(done);
