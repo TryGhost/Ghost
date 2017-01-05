07a85de1bd277a6156ef1f8420c546565c4cd52f
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-01-05 15:36:50 +0000

    add manual pretender shutdown to fix acceptance tests (#342)
    no issue
    - a [recent update](https://github.com/pretenderjs/pretender/pull/178) to Pretender contained a breaking change that throws an error when multiple pretender instances exist
    - using mirage in acceptance tests was [triggering the error](https://github.com/samselikoff/ember-cli-mirage/issues/915)

--- ghost/admin/tests/helpers/destroy-app.js
+++ ghost/admin/tests/helpers/destroy-app.js
@@ -1,5 +1,10 @@
 import run from 'ember-runloop';
 
 export default function destroyApp(application) {
+    // this is required to fix "second Pretender instance" warnings
+    if (server) {
+        server.shutdown();
+    }
+
     run(application, 'destroy');
 }
