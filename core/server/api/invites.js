042750f4cf09dfe84d815fe37998908bb8fdcf30
Katharina Irrgang <katharina.irrgang@googlemail.com>
2017-01-25 13:07:31 +0100

    🐛  fix invite permissions for editor (#7889)
    refs #7724
    
    - we already fixed the permissions for the editor
    - see https://github.com/TryGhost/Ghost/commit/3d3101ad0ec5b2a72e6a99c2e6d37e36104751b7
    - but as we are inside of a refactoring process, we had two fixtures.json files
    - we fixed the fixtures.json in the wrong place
    - now that the permissions are used, we can see failing tests
    - i have added the correct permissions handling

--- core/server/api/invites.js
+++ core/server/api/invites.js
@@ -91,15 +91,7 @@ invites = {
         function addInvite(options) {
             var data = options.data;
 
-            return dataProvider.User.findOne({id: loggedInUser}, options)
-                .then(function (user) {
-                    if (!user) {
-                        return Promise.reject(new errors.NotFoundError({message: i18n.t('errors.api.users.userNotFound')}));
-                    }
-
-                    loggedInUser = user;
-                    return dataProvider.Invite.add(data.invites[0], _.omit(options, 'data'));
-                })
+            return dataProvider.Invite.add(data.invites[0], _.omit(options, 'data'))
                 .then(function (_invite) {
                     invite = _invite;
 
@@ -177,25 +169,56 @@ invites = {
                 return Promise.reject(new errors.ValidationError({message: i18n.t('errors.api.invites.roleIsRequired')}));
             }
 
-            // @TODO move this logic to permissible
+            // @TODO remove when we have a new permission unit
             // Make sure user is allowed to add a user with this role
-            return dataProvider.Role.findOne({id: options.data.invites[0].role_id}).then(function (role) {
-                if (!role) {
+            // We cannot use permissible because we don't have access to the role_id!!!
+            // Adding a permissible function to the invite model, doesn't give us much context of the invite we would like to add
+            // As we are looking forward to replace the permission system completely, we do not add a hack here
+            return dataProvider.Role.findOne({id: options.data.invites[0].role_id}).then(function (roleToInvite) {
+                if (!roleToInvite) {
                     return Promise.reject(new errors.NotFoundError({message: i18n.t('errors.api.invites.roleNotFound')}));
                 }
 
-                if (role.get('name') === 'Owner') {
+                if (roleToInvite.get('name') === 'Owner') {
                     return Promise.reject(new errors.NoPermissionError({message: i18n.t('errors.api.invites.notAllowedToInviteOwner')}));
+                }
+
+                var loggedInUserRole = loggedInUser.related('roles').models[0].get('name'),
+                    allowed = [];
+
+                if (loggedInUserRole === 'Owner' || loggedInUserRole === 'Administrator') {
+                    allowed = ['Administrator', 'Editor', 'Author'];
+                } else if (loggedInUserRole === 'Editor') {
+                    allowed = ['Author'];
+                }
+
+                if (allowed.indexOf(roleToInvite.get('name')) === -1) {
+                    return Promise.reject(new errors.NoPermissionError({
+                        message: i18n.t('errors.api.invites.notAllowedToInvite')
+                    }));
                 }
             }).then(function () {
                 return options;
             });
         }
 
+        function fetchLoggedInUser(options) {
+            return dataProvider.User.findOne({id: loggedInUser}, _.merge({}, options, {include: ['roles']}))
+                .then(function (user) {
+                    if (!user) {
+                        return Promise.reject(new errors.NotFoundError({message: i18n.t('errors.api.users.userNotFound')}));
+                    }
+
+                    loggedInUser = user;
+                    return options;
+                });
+        }
+
         tasks = [
             utils.validate(docName, {opts: ['email']}),
             utils.handlePermissions(docName, 'add'),
             utils.convertOptions(allowedIncludes),
+            fetchLoggedInUser,
             validation,
             destroyOldInvite,
             addInvite
