0a5cff9e16e2814f5fb1933deba964112cebf5e8
Katharina Irrgang <katharina.irrgang@googlemail.com>
2017-01-26 13:12:00 +0100

    🔥  remove database version (#7894)
    refs #7489
    
    - as we are now using a different migration approach (knex-migrator), we don't need to remember the database version anymore
    - it was once used to check the state of a database and based on it we decided to migrate or not
    - with knex-migrator everything depends on the migration table entries and the current ghost version you are on
    - on current master the leftover usage is to add the db version when exporting the database, which can be replaced by reading the ghost version
    - removing this solves also an interesting migration case with knex-migrator:
      - you are on 1.0
      - you update to 1.1, but 1.1 has no migrations
      - the db version would remain in 1.0
      - because the db version was only updated when knex migrator executed a migration

--- core/server/data/migrations/hooks/init/after.js
+++ core/server/data/migrations/hooks/init/after.js
@@ -1,30 +1,18 @@
-var errors = require('../../../../errors'),
-    config = require('../../../../config'),
-    versioning = require('../../../schema/versioning'),
+var config = require('../../../../config'),
     database = require('../../../db');
 
-module.exports = function after(options) {
-    return versioning.getDatabaseVersion(options)
-        .catch(function (err) {
-            if (err instanceof errors.DatabaseVersionError && err.code === 'VERSION_DOES_NOT_EXIST') {
-                return versioning.setDatabaseVersion(options);
-            }
+module.exports = function after() {
+    // do not close database connection in test mode, because all tests are executed one after another
+    // this check is not nice, but there is only one other solution i can think of:
+    // forward a custom object to knex-migrator, which get's forwarded to the hooks
+    if (config.get('env').match(/testing/g)) {
+        return;
+    }
 
-            throw err;
-        })
-        .finally(function destroyConnection() {
-            // do not close database connection in test mode, because all tests are executed one after another
-            // this check is not nice, but there is only one other solution i can think of:
-            // forward a custom object to knex-migrator, which get's forwarded to the hooks
-            if (config.get('env').match(/testing/g)) {
-                return;
-            }
-
-            // we need to close the database connection
-            // the after hook signals the last step of a knex-migrator command
-            // Example:
-            // Ghost-CLI calls knexMigrator.init and afterwards it starts Ghost, but Ghost-CLI can't shutdown
-            // if Ghost keeps a connection alive
-            return database.knex.destroy();
-        });
+    // we need to close the database connection
+    // the after hook signals the last step of a knex-migrator command
+    // Example:
+    // Ghost-CLI calls knexMigrator.init and afterwards it starts Ghost, but Ghost-CLI can't shutdown
+    // if Ghost keeps a connection alive
+    return database.knex.destroy();
 };
