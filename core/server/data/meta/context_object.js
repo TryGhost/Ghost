431ef29d9698b6445181edacca5d10d3c2f78c19
kirrg001 <katharina.irrgang@googlemail.com>
2017-01-31 13:18:40 +0100

    🎨  context object helper
    - replace config.get('theme') by settings cache
    
    IMPORTANT
    I've added a TODO because the context object helper does not make any sense to me.
    For example data/meta/author_image. This meta helper uses the context object and expects to receive `contextObject.author.image`.
    So in case the context object itself has no `author.image`, we use the blog configuration. But there is no `author.image` property?

--- core/server/data/meta/context_object.js
+++ core/server/data/meta/context_object.js
@@ -1,8 +1,16 @@
-var config = require('../../config'),
-    _      = require('lodash');
+var settingsCache = require('../../api/settings').cache,
+    _ = require('lodash');
 
 function getContextObject(data, context) {
-    var blog = config.get('theme'),
+    /**
+     * @TODO:
+     *   - the only valid property is cover
+     *   - there was no image property in config.get('theme')
+     *   - there was no author property in config.get('theme')
+     */
+    var blog = {
+            cover: settingsCache.get('cover')
+        },
         contextObject;
 
     context = _.includes(context, 'page') || _.includes(context, 'amp') ? 'post' : context;
