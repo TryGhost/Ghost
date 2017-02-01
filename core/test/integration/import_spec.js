2887b017947bc6c888f9f83d7ff8559904acd227
kirrg001 <katharina.irrgang@googlemail.com>
2017-02-01 19:24:37 +0100

    🚨  database: change hard limits and field types
    refs #7432
    
    - we went over all schema fields and decided to decrease/increase the hard limits
    - the core goal is to have more flexibility in the future
    - we reconsidered string vs. text
    
    There are 5 groups:
    
    - small strings: 50 characters
        - static strings
        - status, visibility, language, role name, permission name, client name etc.
    - medium strings: 191 characters
        - all unique fields or fields which can be unique in the future
        - slug, tokens, user name, password, tag name, email
    - large strings: 1000-2000 characters
        - these fields need to be very flexible
        - these fields get a soft limit attached (in a different PR)
        - post title, meta title, meta description, urls
    - medium text: 64kb characters
        - bio, settings, location, tour
    - long text: 1000000000 chars
        - html, amp, mobiledoc, markdown

--- core/test/integration/import_spec.js
+++ core/test/integration/import_spec.js
@@ -226,14 +226,14 @@ describe('Import', function () {
             testUtils.fixtures.loadExportFixture('export-001').then(function (exported) {
                 exportData = exported;
 
-                // change title to 151 characters
-                exportData.data.posts[0].title = new Array(152).join('a');
+                // change title to 1001 characters
+                exportData.data.posts[0].title = new Array(1002).join('a');
                 exportData.data.posts[0].tags = 'Tag';
                 return importer.doImport(exportData);
             }).then(function () {
                 (1).should.eql(0, 'Data import should not resolve promise.');
             }, function (error) {
-                error[0].message.should.eql('Value in [posts.title] exceeds maximum length of 150 characters.');
+                error[0].message.should.eql('Value in [posts.title] exceeds maximum length of 1000 characters.');
                 error[0].errorType.should.eql('ValidationError');
 
                 Promise.all([
@@ -374,14 +374,14 @@ describe('Import', function () {
             testUtils.fixtures.loadExportFixture('export-002').then(function (exported) {
                 exportData = exported;
 
-                // change title to 151 characters
-                exportData.data.posts[0].title = new Array(152).join('a');
+                // change title to 1001 characters
+                exportData.data.posts[0].title = new Array(1002).join('a');
                 exportData.data.posts[0].tags = 'Tag';
                 return importer.doImport(exportData);
             }).then(function () {
                 (1).should.eql(0, 'Data import should not resolve promise.');
             }, function (error) {
-                error[0].message.should.eql('Value in [posts.title] exceeds maximum length of 150 characters.');
+                error[0].message.should.eql('Value in [posts.title] exceeds maximum length of 1000 characters.');
                 error[0].errorType.should.eql('ValidationError');
 
                 Promise.all([
--- core/test/unit/migration_spec.js
+++ core/test/unit/migration_spec.js
@@ -20,7 +20,7 @@ should.equal(true, true);
 // both of which are required for migrations to work properly.
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    var currentSchemaHash = 'fa72ab0ca7ce8ee20bafb6e73b61a324',
+    var currentSchemaHash = '34844d053e5f9a698985bbd8c48a5db6',
         currentFixturesHash = 'b9e684a87353c592df9b23948e364c05';
 
     // If this test is failing, then it is likely a change has been made that requires a DB version bump,
