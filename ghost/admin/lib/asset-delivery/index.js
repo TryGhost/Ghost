53e722d41685256acfdd4cc5671fe19108f72db1
Felix Rieseberg <felix@felixrieseberg.com>
2017-02-11 06:42:46 -0800

    :rocket: Offline Support via ServiceWorker (#523)
    This commit adds support for service workers, mostly by including packages that do the heavy lifting. Those are in detail:
    
    * ember-service-worker (SW infrastructure)
    * ember-service-worker-ghost-cache (Caches static assets and the main application)
    * ember-service-worker-cache-fallback (Caches API requests and falls back to the cache if the server can’t be reached)
    * ember-service-worker-index (Caches main html)

--- ghost/admin/lib/asset-delivery/index.js
+++ ghost/admin/lib/asset-delivery/index.js
@@ -18,5 +18,9 @@ module.exports = {
 
             fs.copySync(assetsIn + '/' + relativePath, assetsOut + '/' + relativePath, {overwrite: true});
         });
+
+        // Copy service workers
+        fs.copySync(results.directory + '/sw-registration.js', assetsOut + '/sw-registration.js');
+        fs.copySync(results.directory + '/sw.js', assetsOut + '/sw.js');
     }
 };
