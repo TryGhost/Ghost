27f7b722feca3fc420e0bb0e6355bd1d5f0bd834
Katharina Irrgang <katharina.irrgang@googlemail.com>
2017-02-17 15:29:25 +0100

    🎨  remove domain string replacement for logging (#8011)
    no issue

--- core/server/logging.js
+++ core/server/logging.js
@@ -4,7 +4,7 @@ var config = require('./config'),
 module.exports = logging({
     env: config.get('env'),
     path: config.get('logging:path') || config.getContentPath('logs'),
-    domain: config.get('url').replace(/[^\w]/gi, '_'),
+    domain: config.get('url'),
     mode: config.get('logging:mode'),
     level: config.get('logging:level'),
     transports: config.get('logging:transports'),
