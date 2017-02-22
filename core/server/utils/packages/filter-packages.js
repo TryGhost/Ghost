8ac452a5450adf75e6edb0c7af3defbd3c371d29
Hannah Wolfe <erisds@gmail.com>
2017-02-22 15:42:09 +0000

    🎨 Simplify filterPackages code

--- core/server/utils/packages/filter-packages.js
+++ core/server/utils/packages/filter-packages.js
@@ -1,49 +1,41 @@
 var _ = require('lodash'),
+    notAPackageRegex = /^\.|_messages|README.md/i,
     filterPackages;
 
 /**
- * ### Filter Paths
- * Normalizes paths read by require-tree so that the apps and themes modules can use them. Creates an empty
- * array (res), and populates it with useful info about the read packages like name, whether they're active
- * (comparison with the second argument), and if they have a package.json, that, otherwise false
- * @private
- * @param   {object}            packages       as returned by read-packages
+ * ### Filter Packages
+ * Normalizes paths read by read-packages so that the apps and themes modules can use them.
+ * Iterates over each package and return an array of objects which are simplified representations of the package
+ * with 3 properties:
+ * package name as `name`, the package.json as `package` and an active field set to true if this package is active
+ *
+ * @param   {object}            packages    as returned by read-packages
  * @param   {array/string}      active      as read from the settings object
  * @returns {Array}                         of objects with useful info about apps / themes
  */
 filterPackages = function filterPackages(packages, active) {
-    var pathKeys = Object.keys(packages),
-        res = [],
-        item;
-
     // turn active into an array (so themes and apps can be checked the same)
     if (!Array.isArray(active)) {
         active = [active];
     }
 
-    _.each(pathKeys, function (key) {
-        // do not include hidden files or _messages
-        if (key.indexOf('.') !== 0 &&
-            key !== '_messages' &&
-            key !== 'README.md'
-        ) {
+    return _.reduce(packages, function (result, pkg, key) {
+        var item = {};
+        if (!key.match(notAPackageRegex)) {
             item = {
-                name: key
+                name: key,
+                package: pkg['package.json'] || false
             };
-            if (packages[key].hasOwnProperty('package.json')) {
-                item.package = packages[key]['package.json'];
-            } else {
-                item.package = false;
-            }
 
             if (_.indexOf(active, key) !== -1) {
                 item.active = true;
             }
-            res.push(item);
-        }
-    });
 
-    return res;
+            result.push(item);
+        }
+
+        return result;
+    }, []);
 };
 
 module.exports = filterPackages;
