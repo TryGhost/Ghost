d95d34da93eb5be91f94de86af7c0aeb2178ea99
Hannah Wolfe <erisds@gmail.com>
2017-02-28 17:43:55 +0000

    🎨 Better error handling

--- core/server/themes/read.js
+++ core/server/themes/read.js
@@ -4,12 +4,20 @@
  * Util that wraps packages.read
  */
 var packages = require('../utils/packages'),
+    logging = require('../logging'),
+    i18n = require('../i18n'),
 
     readOneTheme,
     readAllThemes;
 
 readOneTheme = function readOneTheme(dir, name) {
-    return packages.read.one(dir, name);
+    return packages
+        .read.one(dir, name)
+        .catch(function () {
+            // For now we return an empty object as this is not fatal unless the frontend of the blog is requested
+            logging.warn(i18n.t('errors.middleware.themehandler.missingTheme', {theme: name}));
+            return {};
+        });
 };
 
 readAllThemes = function readAllThemes(dir) {
