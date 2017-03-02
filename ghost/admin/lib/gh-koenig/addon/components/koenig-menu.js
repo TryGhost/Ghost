2db84d6e4451c6772213d6b0e04913455c11d86d
Ryan McCarvill <ryan@disordinary.com>
2017-03-02 15:09:57 +1300

    fixed the menu in perperation to the + button

--- ghost/admin/lib/gh-koenig/addon/components/koenig-menu.js
+++ ghost/admin/lib/gh-koenig/addon/components/koenig-menu.js
@@ -4,13 +4,13 @@ import layout from '../templates/components/koenig-menu';
 
 export default Ember.Component.extend({
     layout,
-    classNames: ['koenig-menu'],
-    classNameBindings: ['isVisible'],
     range: null,
     menuSelectedItem: 0,
     toolsLength:0,
     selectedTool:null,
-    isVisible:false,
+    isActive: false,
+    isInputting: false,
+    isSetup: false,
     toolbar: Ember.computed(function () {
         let tools = [ ];
         let match = (this.query || "").trim().toLowerCase();
@@ -43,7 +43,7 @@ export default Ember.Component.extend({
 
         if(tools.length <  1) {
             this.isActive = false;
-            this.set('isVisible', false);
+            this.$('.koenig-menu').hide();
         }
         return tools;
     }),
@@ -63,17 +63,16 @@ export default Ember.Component.extend({
                 self.open(editor);
             }
         });
-
-
     },
     willDestroy() {
         this.editor.destroy();
     },
     cursorChange() {
-        if(this.isActive) {
-            if(!this.editor.range.isCollapsed || this.editor.range.head.section !== this._node || this.editor.range.head.offset < 1 || !this.editor.range.head.section) {
-               this.close();
-            }
+        if(!this.editor.range.isCollapsed || this.editor.range.head.section !== this._node || this.editor.range.head.offset < 1 || !this.editor.range.head.section) {
+            this.close();
+        }
+        
+        if(this.isActive && this.isInputting) {
             this.query = this.editor.range.head.section.text.substring(this._offset, this.editor.range.head.offset);
             this.set('range', {
                 section: this._node,
@@ -85,21 +84,34 @@ export default Ember.Component.extend({
 
 
     },
-    open(editor) {
+    didRender( ) {
+        if(!this.isSetup) {
+            this.$('.koenig-menu-button').onClick = () => {alert("CLICK")};
+            this.isSetup = true;
+        }
+    },
+    /**
+     * 
+     * @param {*} editor 
+     * @param {*} notInputting is true if the user isn't typing to filter, this occurs
+     * if the menu is oppened via pressing + rather than typing in /
+     */
+    open(editor, notInputting) {
         let self = this;
-        let $this = this.$();
+        let $this = this.$('.koenig-menu');
         let $editor = Ember.$('.gh-editor-container');
 
         this._node = editor.range.head.section;
         this._offset = editor.range.head.offset;
         this.isActive = true;
+        this.isInputting = !notInputting;
         this.cursorChange();
         let range = window.getSelection().getRangeAt(0); // get the actual range within the DOM.
 
         let position =  range.getBoundingClientRect();
         let edOffset = $editor.offset();
 
-        this.set('isVisible', true);
+        $this.show();
 
         Ember.run.schedule('afterRender', this,
             () => {
@@ -164,8 +176,8 @@ export default Ember.Component.extend({
     },
     close() {
         this.isActive = false;
-
-        this.set('isVisible', false);
+        this.isInputting = false;
+        this.$('.koenig-menu').hide();
         // note: below is using a mobiledoc Private API.
         // there is no way to unregister a keycommand when it's registered so we have to remove it ourselves.
         for( let i = this.editor._keyCommands.length-1; i > -1; i--) {
