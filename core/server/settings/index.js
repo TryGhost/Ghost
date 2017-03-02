f26d159223d1b0063c2a91f77819ccd2845214d5
Hannah Wolfe <erisds@gmail.com>
2017-03-02 15:16:30 +0000

    🎨 Restructure init & finish up settingsCache
    - move initialisation into settingsCache.init AT LAST
    - change settingCache to use cloneDeep, so that the object can't be modified outside of the functions
    - add lots of docs to settings cache

--- core/server/settings/index.js
+++ core/server/settings/index.js
@@ -3,27 +3,17 @@
  * A collection of utilities for handling settings including a cache
  */
 
-var _ = require('lodash'),
-    SettingsModel = require('../models/settings').Settings,
+var SettingsModel = require('../models/settings').Settings,
     SettingsCache = require('./cache');
 
 module.exports = {
     init: function init() {
         // Bind to events
-        SettingsCache.init();
+
         // Update the defaults
         return SettingsModel.populateDefaults()
             .then(function (settingsCollection) {
-                // Reset the cache
-                // PopulateDefaults returns us a bookshelf Collection of Settings Models.
-                // We want to iterate over the models, and for each model:
-                // Get the key, and the JSON version of the model, and call settingsCache.set()
-                // This is identical to the updateSettingFromModel code inside of settings/cache.init()
-                _.each(settingsCollection.models, function updateSettingFromModel(settingModel) {
-                    SettingsCache.set(settingModel.get('key'), settingModel.toJSON());
-                });
-
-                return SettingsCache.getAll();
+                SettingsCache.init(settingsCollection);
             });
     }
 };
