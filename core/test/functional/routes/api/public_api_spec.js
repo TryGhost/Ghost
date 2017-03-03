4367a337dc8b08ebbf8f0c0451f30822a780e4a5
Katharina Irrgang <katharina.irrgang@googlemail.com>
2017-03-03 17:41:20 +0100

    🐛  fix cors middleware (#8094)
    no issue
    
    - same fix as https://github.com/TryGhost/Ghost/pull/8066
    - just for master

--- core/test/functional/routes/api/public_api_spec.js
+++ core/test/functional/routes/api/public_api_spec.js
@@ -22,7 +22,7 @@ describe('Public API', function () {
         }).then(function () {
             request = supertest.agent(config.get('url'));
         }).then(function () {
-            return testUtils.doAuth(request, 'posts', 'tags');
+            return testUtils.doAuth(request, 'posts', 'tags', 'client:trusted-domain');
         }).then(function (token) {
             // enable public API
             request.put(testUtils.API.getApiQuery('settings/'))
@@ -58,7 +58,37 @@ describe('Public API', function () {
                     return done(err);
                 }
 
+                res.headers.vary.should.eql('Origin, Accept-Encoding');
+                should.exist(res.headers['access-control-allow-origin']);
                 should.not.exist(res.headers['x-cache-invalidate']);
+
+                var jsonResponse = res.body;
+                should.exist(jsonResponse.posts);
+                testUtils.API.checkResponse(jsonResponse, 'posts');
+                jsonResponse.posts.should.have.length(5);
+                testUtils.API.checkResponse(jsonResponse.posts[0], 'post');
+                testUtils.API.checkResponse(jsonResponse.meta.pagination, 'pagination');
+                _.isBoolean(jsonResponse.posts[0].featured).should.eql(true);
+                _.isBoolean(jsonResponse.posts[0].page).should.eql(true);
+                done();
+            });
+    });
+
+    it('browse posts from different origin', function (done) {
+        request.get(testUtils.API.getApiQuery('posts/?client_id=ghost-test&client_secret=not_available'))
+            .set('Origin', 'https://example.com')
+            .expect('Content-Type', /json/)
+            .expect('Cache-Control', testUtils.cacheRules.private)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+
+                res.headers.vary.should.eql('Origin, Accept-Encoding');
+                should.exist(res.headers['access-control-allow-origin']);
+                should.not.exist(res.headers['x-cache-invalidate']);
+
                 var jsonResponse = res.body;
                 should.exist(jsonResponse.posts);
                 testUtils.API.checkResponse(jsonResponse, 'posts');
