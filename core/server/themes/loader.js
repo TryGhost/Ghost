0c7f0fa59931df093630f25c7627a23d31c7a697
Hannah Wolfe <erisds@gmail.com>
2017-03-08 12:53:02 +0000

    🎨 simplify loader - use loadOneTheme for init
    - use loadOneTheme for init
    - move updateThemeList to the one place that it is used
    - this just reduces the surface area of the loader

--- core/server/themes/loader.js
+++ core/server/themes/loader.js
@@ -4,33 +4,33 @@ var debug = require('debug')('ghost:themes:loader'),
     themeList = require('./list'),
     read = require('./read'),
     settingsCache = require('../settings/cache'),
-    updateThemeList,
     loadAllThemes,
     loadOneTheme,
     initThemes;
 
-updateThemeList = function updateThemeList(themes) {
-    debug('loading themes', Object.keys(themes));
-    themeList.init(themes);
-};
-
 loadAllThemes = function loadAllThemes() {
     return read
         .all(config.getContentPath('themes'))
-        .then(updateThemeList);
+        .then(function updateThemeList(themes) {
+            debug('loading themes', Object.keys(themes));
+
+            themeList.init(themes);
+        });
 };
 
 loadOneTheme = function loadOneTheme(themeName) {
     return read
         .one(config.getContentPath('themes'), themeName)
         .then(function (readThemes) {
+            debug('loaded one theme', themeName);
             // @TODO change read one to not return a keyed object
             return themeList.set(themeName, readThemes[themeName]);
         });
 };
 
 initThemes = function initThemes() {
-    debug('init themes', settingsCache.get('activeTheme'));
+    var activeThemeName = settingsCache.get('activeTheme');
+    debug('init themes', activeThemeName);
 
     // Register a listener for server-start to load all themes
     events.on('server:start', function readAllThemesOnServerStart() {
@@ -38,9 +38,7 @@ initThemes = function initThemes() {
     });
 
     // Just read the active theme for now
-    return read
-        .one(config.getContentPath('themes'), settingsCache.get('activeTheme'))
-        .then(updateThemeList);
+    return loadOneTheme(activeThemeName);
 };
 
 module.exports = {
