b2f1d0559bcec61e1f06edabc09ded12d4403a97
Hannah Wolfe <github.erisds@gmail.com>
2017-03-13 11:44:44 +0000

    ✨ Themes API activation permissions & validation (#8104)
    refs #8093
    
    ✨ Add activate theme permission
    - add permission to activate themes
    - update tests
    - also: update tests for invites
    TODO: change how the active theme setting is updated to reduce extra permissions
    
    ✨ Move theme validation to gscan
    - add a new gscan validation method and use it for upload
    - update activate endpoint to do validation also using gscan
    - change to using SettingsModel instead of API so that we don't call validation or permissions on the settings API
    - remove validation from the settings model
    - remove the old validation function
    - add new invalid theme message to translations & remove a bunch of theme validation related unused keys
    
    📖  Planned changes
    
    🚨 Tests for theme activation API endpoint
    🐛 Don't allow deleting the active theme
    
    🚫 Prevent activeTheme being set via settings API
    - We want to control how this happens in future.
    - We still want to store the information in settings, via the model.
    - We just don't want to be able to change this info via the settings edit endpoint
    
    🐛 ✨ Fix warnings for uploads & add for activations
    - warnings for uploads were broken in f8b498d
    - fix the response + adds tests to cover that warnings are correctly returned
    - add the same response to activations + more tests
    - activations now return a single theme object - the theme that was activated + any warnings
    
    🎨 Improve how we generate theme API responses
    - remove the requirement to pass in the active theme!
    - move this to a specialist function, away from the list
    
    🎨 Do not load gscan on boot

---
+++ core/server/themes/to-json.js
@@ -0,0 +1,33 @@
+var _ = require('lodash'),
+    themeList = require('./list'),
+    packages = require('../utils/packages'),
+    settingsCache = require('../settings/cache');
+
+/**
+ * Provides a JSON object which can be returned via the API
+ *
+ * @param {string} [name] - the theme to output
+ * @param {object} [checkedTheme] - a theme result from gscan
+ * @return {*}
+ */
+module.exports = function toJSON(name, checkedTheme) {
+    var themeResult, toFilter;
+
+    if (!name) {
+        toFilter = themeList.getAll();
+        // Default to returning the full list
+        themeResult = packages.filterPackages(toFilter, settingsCache.get('activeTheme'));
+    } else {
+        // If we pass in a gscan result, convert this instead
+        toFilter = {};
+        toFilter[name] = themeList.get(name);
+
+        themeResult = packages.filterPackages(toFilter, settingsCache.get('activeTheme'));
+
+        if (checkedTheme && checkedTheme.results.warning.length > 0) {
+            themeResult[0].warnings = _.cloneDeep(checkedTheme.results.warning);
+        }
+    }
+
+    return {themes: themeResult};
+};
