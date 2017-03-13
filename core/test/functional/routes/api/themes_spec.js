TryGhost/Ghost

commit f8b498d6e78779da000fb6b2c4dd3f59d8dbe735

🔥✨ No more availableThemes (#8085)
no issue

🎨 Switch themes API to use config.availableThemes
- this gets rid of the only places where settings.availableThemes are used

🔥 Get rid of settings.availableThemes
- this is no longer used anywhere
- also get rid of every related call to updateSettingsCache

🔥 Replace config.availableThemes with theme cache
- Creates a tailor-made in-memory cache for themes inside the theme module
- Add methods for getting & setting items on the cache
- Move all references to config.availableThemes to use the new cache
- This can be abstracted later to support other kinds of caches?

🎨 Start improving theme lib's API
Still TODO: simplifying/clarifying:
- what is the structure of the internal list
- what is the difference between a package list, and a theme list?
- what is the difference between reading a theme and loading it?
- how do we update the theme list (add/remove)
- how do we refresh the theme list? (hot reload?!)
- how do we get from an internal list, to one that is sent as part of the API?
- how are we going to handle theme storage: read/write, such that the path is configurable

🎨 Use themeList consistently
🎨 Update list after storage

core/server/api/themes.js
@@ -1,6 +1,7 @@
 // # Themes API
 // RESTful API for Themes
-var Promise = require('bluebird'),
+var debug = require('debug')('ghost:api:themes'),
+    Promise = require('bluebird'),
     _ = require('lodash'),
     gscan = require('gscan'),
     fs = require('fs-extra'),
@@ -9,12 +10,12 @@ var Promise = require('bluebird'),
     events = require('../events'),
     logging = require('../logging'),
     storage = require('../storage'),
-    settings = require('./settings'),
-    settingsCache = require('../settings/cache'),
     apiUtils = require('./utils'),
     utils = require('./../utils'),
     i18n = require('../i18n'),
     themeUtils = require('../themes'),
+    themeList = themeUtils.list,
+    packageUtils = require('../utils/packages'),
     themes;
 
 /**
@@ -24,7 +25,10 @@ var Promise = require('bluebird'),
  */
 themes = {
     browse: function browse() {
-        return Promise.resolve({themes: settingsCache.get('availableThemes')});
+        debug('browsing');
+        var result = packageUtils.filterPackages(themeList.getAll());
+        debug('got result');
+        return Promise.resolve({themes: result});
     },
 
     upload: function upload(options) {
@@ -80,23 +84,18 @@ themes = {
                 }, config.getContentPath('themes'));
             })
             .then(function () {
-                // force reload of availableThemes
-                // right now the logic is in the ConfigManager
-                // if we create a theme collection, we don't have to read them from disk
-                return themeUtils.load();
+                return themeUtils.loadOne(zip.shortName);
             })
-            .then(function () {
-                // the settings endpoint is used to fetch the availableThemes
-                // so we have to force updating the in process cache
-                return settings.updateSettingsCache();
-            })
-            .then(function (settings) {
+            .then(function (themeObject) {
+                // @TODO fix this craziness
+                var toFilter = {};
+                toFilter[zip.shortName] = themeObject;
+                themeObject = packageUtils.filterPackages(toFilter);
                 // gscan theme structure !== ghost theme structure
-                var themeObject = _.find(settings.availableThemes.value, {name: zip.shortName}) || {};
                 if (theme.results.warning.length > 0) {
                     themeObject.warnings = _.cloneDeep(theme.results.warning);
                 }
-                return {themes: [themeObject]};
+                return {themes: themeObject};
             })
             .finally(function () {
                 // remove zip upload from multer
@@ -119,7 +118,7 @@ themes = {
 
     download: function download(options) {
         var themeName = options.name,
-            theme = config.get('paths').availableThemes[themeName],
+            theme = themeList.get(themeName),
             storageAdapter = storage.getStorage('themes');
 
         if (!theme) {
@@ -148,20 +147,17 @@ themes = {
                     throw new errors.ValidationError({message: i18n.t('errors.api.themes.destroyCasper')});
                 }
 
-                theme = config.get('paths').availableThemes[name];
+                theme = themeList.get(name);
 
                 if (!theme) {
                     throw new errors.NotFoundError({message: i18n.t('errors.api.themes.themeDoesNotExist')});
                 }
 
-                events.emit('theme.deleted', name);
                 return storageAdapter.delete(name, config.getContentPath('themes'));
             })
             .then(function () {
-                return themeUtils.load();
-            })
-            .then(function () {
-                return settings.updateSettingsCache();
+                themeList.del(name);
+                events.emit('theme.deleted', name);
             });
     }
 };

core/test/functional/routes/api/themes_spec.js
@@ -65,17 +65,18 @@ describe('Themes API', function () {
     });
 
     describe('success cases', function () {
-        it('get all available themes', function (done) {
-            request.get(testUtils.API.getApiQuery('settings/'))
+        it('get all themes', function (done) {
+            request.get(testUtils.API.getApiQuery('themes/'))
                 .set('Authorization', 'Bearer ' + scope.ownerAccessToken)
                 .end(function (err, res) {
                     if (err) {
                         return done(err);
                     }
 
-                    var availableThemes = _.find(res.body.settings, {key: 'availableThemes'});
-                    should.exist(availableThemes);
-                    availableThemes.value.length.should.be.above(0);
+                    var jsonResponse = res.body;
+                    should.exist(jsonResponse.themes);
+                    testUtils.API.checkResponse(jsonResponse, 'themes');
+                    jsonResponse.themes.length.should.be.above(0);
                     done();
                 });
         });
@@ -108,19 +109,21 @@ describe('Themes API', function () {
                 });
         });
 
-        it('get all available themes + new theme', function (done) {
-            request.get(testUtils.API.getApiQuery('settings/'))
+        it('get all themes + new theme', function (done) {
+            request.get(testUtils.API.getApiQuery('themes/'))
                 .set('Authorization', 'Bearer ' + scope.ownerAccessToken)
                 .end(function (err, res) {
                     if (err) {
                         return done(err);
                     }
 
-                    var availableThemes = _.find(res.body.settings, {key: 'availableThemes'});
-                    should.exist(availableThemes);
+                    var jsonResponse = res.body;
+                    should.exist(jsonResponse.themes);
+                    testUtils.API.checkResponse(jsonResponse, 'themes');
+                    jsonResponse.themes.length.should.be.above(0);
 
                     // ensure the new 'valid' theme is available
-                    should.exist(_.find(availableThemes.value, {name: 'valid'}));
+                    should.exist(_.find(jsonResponse.themes, {name: 'valid'}));
                     done();
                 });
         });

core/test/unit/themes_spec.js
@@ -1,13 +1,22 @@
 var should          = require('should'),
+    sinon           = require('sinon'),
+    _               = require('lodash'),
     fs              = require('fs'),
     tmp             = require('tmp'),
     join            = require('path').join,
-    readThemes      = require('../../server/themes/read');
+    themeList      = require('../../server/themes').list,
+    readThemes      = require('../../server/themes/read'),
+
+    sandbox = sinon.sandbox.create();
 
 // To stop jshint complaining
 should.equal(true, true);
 
 describe('Themes', function () {
+    afterEach(function () {
+        sandbox.restore();
+    });
+
     describe('Read All', function () {
         it('should read directory and include only folders', function (done) {
             var themePath = tmp.dirSync({unsafeCleanup: true});
@@ -124,6 +133,75 @@ describe('Themes', function () {
                 })
                 .catch(done)
                 .finally(themePath.removeCallback);
+        });
+    });
+
+    describe('List', function () {
+        beforeEach(function () {
+            themeList.init({
+                casper: {foo: 'bar'},
+                'not-casper': {bar: 'baz'}
+            });
+        });
+
+        it('get() allows getting a single theme', function () {
+            themeList.get('casper').should.eql({foo: 'bar'});
+        });
+
+        it('get() with no args should do nothing', function () {
+            should.not.exist(themeList.get());
+        });
+
+        it('getAll() returns all themes', function () {
+            themeList.getAll().should.be.an.Object().with.properties('casper', 'not-casper');
+            Object.keys(themeList.getAll()).should.have.length(2);
+        });
+
+        it('set() updates an existing theme', function () {
+            var origCasper = _.cloneDeep(themeList.get('casper'));
+            themeList.set('casper', {magic: 'update'});
+
+            themeList.get('casper').should.not.eql(origCasper);
+            themeList.get('casper').should.eql({magic: 'update'});
+        });
+
+        it('set() can add a new theme', function () {
+            themeList.set('rasper', {color: 'red'});
+            themeList.get('rasper').should.eql({color: 'red'});
+        });
+
+        it('del() removes a key from the list', function () {
+            should.exist(themeList.get('casper'));
+            should.exist(themeList.get('not-casper'));
+            themeList.del('casper');
+            should.not.exist(themeList.get('casper'));
+            should.exist(themeList.get('not-casper'));
+        });
+
+        it('del() with no argument does nothing', function () {
+            should.exist(themeList.get('casper'));
+            should.exist(themeList.get('not-casper'));
+            themeList.del();
+            should.exist(themeList.get('casper'));
+            should.exist(themeList.get('not-casper'));
+        });
+
+        it('init() calls set for each theme', function () {
+            var setSpy = sandbox.spy(themeList, 'set');
+
+            themeList.init({test: {a: 'b'}, casper: {c: 'd'}});
+            setSpy.calledTwice.should.be.true();
+            setSpy.firstCall.calledWith('test', {a: 'b'}).should.be.true();
+            setSpy.secondCall.calledWith('casper', {c: 'd'}).should.be.true();
+        });
+
+        it('init() with empty object resets the list', function () {
+            themeList.init();
+            var result = themeList.getAll();
+            should.exist(result);
+            result.should.be.an.Object();
+            result.should.eql({});
+            Object.keys(result).should.have.length(0);
         });
     });
 });

core/server/api/settings.js
@@ -3,12 +3,10 @@
 var _            = require('lodash'),
     dataProvider = require('../models'),
     Promise      = require('bluebird'),
-    config       = require('../config'),
     canThis      = require('../permissions').canThis,
     errors       = require('../errors'),
     utils        = require('./utils'),
     i18n         = require('../i18n'),
-    filterPackages = require('../utils/packages').filterPackages,
 
     docName      = 'settings',
     settings,
@@ -68,6 +66,17 @@ settingsFilter = function (settings, filter) {
 
 /**
  * ### Read Settings Result
+ *
+ * Converts the models to keyed JSON
+ * E.g.
+ * dbHash: {
+ *   id: '123abc',
+ *   key: 'dbash',
+ *   value: 'xxxx',
+ *   type: 'core',
+ *   timestamps
+ *  }
+ *
  * @private
  * @param {Array} settingsModels
  * @returns {Settings}
@@ -79,20 +88,7 @@ readSettingsResult = function (settingsModels) {
             }
 
             return memo;
-        }, {}),
-        themes = config.get('paths').availableThemes,
-        res;
-
-    // @TODO: remove availableThemes from settings cache and create an endpoint to fetch themes
-    if (settings.activeTheme && themes) {
-        res = filterPackages(themes, settings.activeTheme.value);
-
-        settings.availableThemes = {
-            key: 'availableThemes',
-            value: res,
-            type: 'theme'
-        };
-    }
+        }, {});
 
     return settings;
 };
@@ -255,7 +251,7 @@ settings = {
         }
 
         object.settings = _.reject(object.settings, function (setting) {
-            return setting.key === 'type' || setting.key === 'availableThemes';
+            return setting.key === 'type';
         });
 
         return canEditAllSettings(object.settings, options).then(function () {

core/server/apps/amp/tests/router_spec.js
@@ -7,6 +7,7 @@ var rewire            = require('rewire'),
     errors            = require('../../../errors'),
     should            = require('should'),
     configUtils       = require('../../../../test/utils/configUtils'),
+    themeList        = require('../../../themes').list,
     sandbox           = sinon.sandbox.create();
 
 // Helper function to prevent unit tests
@@ -53,10 +54,11 @@ describe('AMP Controller', function () {
     afterEach(function () {
         sandbox.restore();
         configUtils.restore();
+        themeList.init();
     });
 
     it('should render default amp page when theme has no amp template', function (done) {
-        configUtils.set({paths: {availableThemes: {casper: {}}}});
+        themeList.init({casper: {}});
 
         setResponseContextStub = sandbox.stub();
         ampController.__set__('setResponseContext', setResponseContextStub);
@@ -70,9 +72,9 @@ describe('AMP Controller', function () {
     });
 
     it('should render theme amp page when theme has amp template', function (done) {
-        configUtils.set({paths: {availableThemes: {casper: {
+        themeList.init({casper: {
             'amp.hbs': '/content/themes/casper/amp.hbs'
-        }}}});
+        }});
 
         setResponseContextStub = sandbox.stub();
         ampController.__set__('setResponseContext', setResponseContextStub);
@@ -86,7 +88,7 @@ describe('AMP Controller', function () {
     });
 
     it('should render with error when error is passed in', function (done) {
-        configUtils.set({paths: {availableThemes: {casper: {}}}});
+        themeList.init({casper: {}});
         res.error = 'Test Error';
 
         setResponseContextStub = sandbox.stub();
@@ -103,7 +105,7 @@ describe('AMP Controller', function () {
 
     it('does not render amp page when amp context is missing', function (done) {
         var renderSpy;
-        configUtils.set({paths: {availableThemes: {casper: {}}}});
+        themeList.init({casper: {}});
 
         setResponseContextStub = sandbox.stub();
         ampController.__set__('setResponseContext', setResponseContextStub);
@@ -121,7 +123,7 @@ describe('AMP Controller', function () {
 
     it('does not render amp page when context is other than amp and post', function (done) {
         var renderSpy;
-        configUtils.set({paths: {availableThemes: {casper: {}}}});
+        themeList.init({casper: {}});
 
         setResponseContextStub = sandbox.stub();
         ampController.__set__('setResponseContext', setResponseContextStub);

core/server/apps/private-blogging/tests/controller_spec.js
@@ -3,6 +3,7 @@ var privateController = require('../lib/router').controller,
     path              = require('path'),
     sinon             = require('sinon'),
     configUtils       = require('../../../../test/utils/configUtils'),
+    themeList        = require('../../../themes').list,
     sandbox = sinon.sandbox.create();
 
 describe('Private Controller', function () {
@@ -42,10 +43,11 @@ describe('Private Controller', function () {
     afterEach(function () {
         sandbox.restore();
         configUtils.restore();
+        themeList.init();
     });
 
     it('Should render default password page when theme has no password template', function (done) {
-        configUtils.set({paths: {availableThemes: {casper: {}}}});
+        themeList.init({casper: {}});
 
         res.render = function (view) {
             view.should.eql(defaultPath);
@@ -56,9 +58,9 @@ describe('Private Controller', function () {
     });
 
     it('Should render theme password page when it exists', function (done) {
-        configUtils.set({paths: {availableThemes: {casper: {
+        themeList.init({casper: {
             'private.hbs': '/content/themes/casper/private.hbs'
-        }}}});
+        }});
 
         res.render = function (view) {
             view.should.eql('private');
@@ -69,7 +71,7 @@ describe('Private Controller', function () {
     });
 
     it('Should render with error when error is passed in', function (done) {
-        configUtils.set({paths: {availableThemes: {casper: {}}}});
+        themeList.init({casper: {}});
         res.error = 'Test Error';
 
         res.render = function (view, context) {

core/server/controllers/frontend/templates.js
@@ -2,11 +2,11 @@
 //
 // Figure out which template should be used to render a request
 // based on the templates which are allowed, and what is available in the theme
-var _      = require('lodash'),
-    config = require('../../config');
+var _ = require('lodash'),
+    themeList = require('../../themes').list;
 
 function getActiveThemePaths(activeTheme) {
-    return config.get('paths').availableThemes[activeTheme];
+    return themeList.get(activeTheme);
 }
 
 /**

core/server/middleware/theme-handler.js
@@ -9,6 +9,7 @@ var _      = require('lodash'),
     logging = require('../logging'),
     errors = require('../errors'),
     i18n = require('../i18n'),
+    themeList = require('../themes').list,
     themeHandler;
 
 themeHandler = {
@@ -95,7 +96,7 @@ themeHandler = {
             // Check if the theme changed
             if (activeTheme.value !== blogApp.get('activeTheme')) {
                 // Change theme
-                if (!config.get('paths').availableThemes.hasOwnProperty(activeTheme.value)) {
+                if (!themeList.get(activeTheme.value)) {
                     if (!res.isAdmin) {
                         return next(new errors.NotFoundError({
                             message: i18n.t('errors.middleware.themehandler.missingTheme', {theme: activeTheme.value})

commit 6c58398e99be6f8ffaf04e16f59e62e7b50304f4

🐛 ✨ Fix warnings for uploads & add for activations
- warnings for uploads were broken in f8b498d
- fix the response + adds tests to cover that warnings are correctly returned
- add the same response to activations + more tests
- activations now return a single theme object - the theme that was activated + any warnings

core/server/api/themes.js
@@ -36,30 +36,40 @@ themes = {
             newSettings = [{
                 key: 'activeTheme',
                 value: themeName
-            }];
+            }],
+            loadedTheme,
+            checkedTheme;
 
         return apiUtils
             .handlePermissions('themes', 'activate')(options)
             .then(function activateTheme() {
-                var theme = themeList.get(themeName);
+                loadedTheme = themeList.get(themeName);
 
-                if (!theme) {
+                if (!loadedTheme) {
                     return Promise.reject(new errors.ValidationError({
                         message: i18n.t('notices.data.validation.index.themeCannotBeActivated', {themeName: themeName}),
                         context: 'activeTheme'
                     }));
                 }
 
-                return themeUtils.validate.check(theme);
+                return themeUtils.validate.check(loadedTheme);
             })
-            .then(function haveValidTheme(/* checkedTheme */) {
+            .then(function haveValidTheme(_checkedTheme) {
+                checkedTheme = _checkedTheme;
                 // We use the model, not the API here, as we don't want to trigger permissions
                 return settingsModel.edit(newSettings, options);
             })
             .then(function hasEditedSetting() {
-                var result = themeList.toAPI(themeList.getAll(), themeName);
                 // @TODO actually do things to activate the theme, other than just the setting?
-                return Promise.resolve({themes: result});
+
+                var themeResult = themeList.toAPI(loadedTheme, settingsCache.get('activeTheme'));
+                // gscan theme structure !== ghost theme structure
+                // @TODO consider a different way to build this result from the validations
+                if (checkedTheme.results.warning.length > 0) {
+                    themeResult[0].warnings = _.cloneDeep(checkedTheme.results.warning);
+                }
+
+                return {themes: themeResult};
             });
     },
 
@@ -75,7 +85,7 @@ themes = {
                 name: options.originalname,
                 shortName: storageAdapter.getSanitizedFileName(options.originalname.split('.zip')[0])
             },
-            theme;
+            checkedTheme;
 
         // check if zip name is casper.zip
         if (zip.name === 'casper.zip') {
@@ -86,8 +96,8 @@ themes = {
             .then(function validateTheme() {
                 return themeUtils.validate.check(zip, true);
             })
-            .then(function checkExists(checkedTheme) {
-                theme = checkedTheme;
+            .then(function checkExists(_checkedTheme) {
+                checkedTheme = _checkedTheme;
 
                 return storageAdapter.exists(utils.url.urlJoin(config.getContentPath('themes'), zip.shortName));
             })
@@ -102,21 +112,21 @@ themes = {
                 // store extracted theme
                 return storageAdapter.save({
                     name: zip.shortName,
-                    path: theme.path
+                    path: checkedTheme.path
                 }, config.getContentPath('themes'));
             })
             .then(function () {
                 return themeUtils.loadOne(zip.shortName);
             })
-            .then(function (themeObject) {
-                themeObject = themeList.toAPI(themeObject, settingsCache.get('activeTheme'));
+            .then(function (loadedTheme) {
+                var themeResult = themeList.toAPI(loadedTheme, settingsCache.get('activeTheme'));
                 // gscan theme structure !== ghost theme structure
                 // @TODO consider a different way to build this result from the validations
-                if (theme.results.warning.length > 0) {
-                    themeObject.warnings = _.cloneDeep(theme.results.warning);
+                if (checkedTheme.results.warning.length > 0) {
+                    themeResult[0].warnings = _.cloneDeep(checkedTheme.results.warning);
                 }
 
-                return {themes: themeObject};
+                return {themes: themeResult};
             })
             .finally(function () {
                 // @TODO we should probably do this as part of saving the theme
@@ -130,8 +140,8 @@ themes = {
                 // @TODO we should probably do this as part of saving the theme
                 // remove extracted dir from gscan
                 // happens in background
-                if (theme) {
-                    Promise.promisify(fs.removeSync)(theme.path)
+                if (checkedTheme) {
+                    Promise.promisify(fs.removeSync)(checkedTheme.path)
                         .catch(function (err) {
                             logging.error(new errors.GhostError({err: err}));
                         });

core/test/functional/routes/api/themes_spec.js
@@ -27,8 +27,9 @@ describe('Themes API (Forked)', function () {
 
     /**
      * Create a temporary folder that contains:
-     * - 2 valid themes: casper & test-theme
-     * - 1 invalid theme:  broken-theme
+     * - 1 valid theme: casper
+     * - 1 valid theme that has warnings: test-theme
+     * - 1 invalid theme: broken-theme
      */
     function setupThemesFolder() {
         tmpContentPath = tmp.dirSync({unsafeCleanup: true});
@@ -45,7 +46,7 @@ describe('Themes API (Forked)', function () {
         fs.mkdirSync(join(tmpContentPath.name, 'themes', 'test-theme'));
         fs.writeFileSync(
             join(tmpContentPath.name, 'themes', 'test-theme', 'package.json'),
-            JSON.stringify({name: 'test-theme', version: '0.5.7'})
+            JSON.stringify({name: 'test-theme', version: '0.5'})
         );
         fs.writeFileSync(join(tmpContentPath.name, 'themes', 'test-theme', 'index.hbs'));
         fs.writeFileSync(join(tmpContentPath.name, 'themes', 'test-theme', 'post.hbs'));
@@ -287,7 +288,39 @@ describe('Themes API (Forked)', function () {
                 });
         });
 
-        it('activate "test-theme" valid theme', function (done) {
+        it('upload new "warnings" theme that has validation warnings', function (done) {
+            var jsonResponse;
+
+            scope.uploadTheme({themePath: join(__dirname, '/../../../utils/fixtures/themes/warnings.zip')})
+                .end(function (err, res) {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    jsonResponse = res.body;
+
+                    should.exist(jsonResponse.themes);
+                    testUtils.API.checkResponse(jsonResponse, 'themes');
+                    jsonResponse.themes.length.should.eql(1);
+                    testUtils.API.checkResponse(jsonResponse.themes[0], 'theme', ['warnings']);
+                    jsonResponse.themes[0].name.should.eql('warnings');
+                    jsonResponse.themes[0].active.should.be.false();
+                    jsonResponse.themes[0].warnings.should.be.an.Array();
+
+                    // Delete the theme to clean up after the test
+                    request.del(testUtils.API.getApiQuery('themes/warnings'))
+                        .set('Authorization', 'Bearer ' + scope.ownerAccessToken)
+                        .expect(204)
+                        .end(function (err) {
+                            if (err) {
+                                return done(err);
+                            }
+                            done();
+                        });
+                });
+        });
+
+        it('activate "test-theme" valid theme that has warnings', function (done) {
             var jsonResponse, casperTheme, testTheme;
 
             // First check the browse response to see that casper is the active theme
@@ -328,17 +361,16 @@ describe('Themes API (Forked)', function () {
 
                             should.exist(jsonResponse.themes);
                             testUtils.API.checkResponse(jsonResponse, 'themes');
-                            jsonResponse.themes.length.should.eql(3);
+                            jsonResponse.themes.length.should.eql(1);
 
                             casperTheme = _.find(jsonResponse.themes, {name: 'casper'});
-                            should.exist(casperTheme);
-                            testUtils.API.checkResponse(casperTheme, 'theme');
-                            casperTheme.active.should.be.false();
+                            should.not.exist(casperTheme);
 
                             testTheme = _.find(jsonResponse.themes, {name: 'test-theme'});
                             should.exist(testTheme);
-                            testUtils.API.checkResponse(testTheme, 'theme');
+                            testUtils.API.checkResponse(testTheme, 'theme', ['warnings']);
                             testTheme.active.should.be.true();
+                            testTheme.warnings.should.be.an.Array();
 
                             done();
                         });
@@ -460,8 +492,9 @@ describe('Themes API (Forked)', function () {
 
                     testTheme = _.find(jsonResponse.themes, {name: 'test-theme'});
                     should.exist(testTheme);
-                    testUtils.API.checkResponse(testTheme, 'theme');
+                    testUtils.API.checkResponse(testTheme, 'theme', ['warnings']);
                     testTheme.active.should.be.true();
+                    testTheme.warnings.should.be.an.Array();
 
                     request.del(testUtils.API.getApiQuery('themes/test-theme'))
                         .set('Authorization', 'Bearer ' + scope.ownerAccessToken)