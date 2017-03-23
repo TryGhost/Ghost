0cd1fd6ea80e168b4fc07c4e7cac376596e27841
kirrg001 <katharina.irrgang@googlemail.com>
2017-03-23 22:31:43 +0100

    Revert LocalFileStorage
    - revert serve
    - revert delete

--- core/server/storage/LocalFileStorage.js
+++ core/server/storage/LocalFileStorage.js
@@ -58,69 +58,37 @@ class LocalFileStore extends StorageBase {
         });
     }
 
-    serve(options) {
-        options = options || {};
+    /**
+     * For some reason send divides the max age number by 1000
+     * Fallthrough: false ensures that if an image isn't found, it automatically 404s
+     * Wrap server static errors
+     *
+     * @returns {serveStaticContent}
+     */
+    serve() {
+        var self = this;
 
-        // CASE: serve themes
-        // serveStatic can't be used to serve themes, because
-        // download files depending on the route (see `send` npm module)
-        if (options.isTheme) {
-            return function downloadTheme(req, res, next) {
-                var themeName = options.name,
-                    themePath = path.join(config.getContentPath('themes'), themeName),
-                    zipName = themeName + '.zip',
-                    // store this in a unique temporary folder
-                    zipBasePath = path.join(os.tmpdir(), utils.uid(10)),
-                    zipPath = path.join(zipBasePath, zipName),
-                    stream;
-
-                Promise.promisify(fs.ensureDir)(zipBasePath)
-                    .then(function () {
-                        return Promise.promisify(utils.zipFolder)(themePath, zipPath);
-                    })
-                    .then(function (length) {
-                        res.set({
-                            'Content-disposition': 'attachment; filename={themeName}.zip'.replace('{themeName}', themeName),
-                            'Content-Type': 'application/zip',
-                            'Content-Length': length
-                        });
-
-                        stream = fs.createReadStream(zipPath);
-                        stream.pipe(res);
-                    })
-                    .catch(function (err) {
-                        next(err);
-                    })
-                    .finally(function () {
-                        remove(zipBasePath);
-                    });
-            };
-        } else {
-            // CASE: serve images
-            // For some reason send divides the max age number by 1000
-            // Fallthrough: false ensures that if an image isn't found, it automatically 404s
-            // Wrap server static errors
-            return function serveStaticContent(req, res, next) {
-                return serveStatic(config.getContentPath('images'), {maxAge: utils.ONE_YEAR_MS, fallthrough: false})(req, res, function (err) {
-                    if (err) {
-                        if (err.statusCode === 404) {
-                            return next(new errors.NotFoundError({message: i18n.t('errors.errors.pageNotFound')}));
-                        }
-
-                        return next(new errors.GhostError({err: err}));
+        return function serveStaticContent(req, res, next) {
+            return serveStatic(config.getContentPath('images'), {maxAge: utils.ONE_YEAR_MS, fallthrough: false})(req, res, function (err) {
+                if (err) {
+                    if (err.statusCode === 404) {
+                        return next(new errors.NotFoundError({message: i18n.t('errors.errors.pageNotFound')}));
                     }
 
-                    next();
-                });
-            };
-        }
+                    return next(new errors.GhostError({err: err}));
+                }
+
+                next();
+            });
+        };
     }
 
-    delete(fileName, targetDir) {
-        targetDir = targetDir || this.getTargetDir(config.getContentPath('images'));
-
-        var pathToDelete = path.join(targetDir, fileName);
-        return remove(pathToDelete);
+    /**
+     * Not implemented.
+     * @returns {Promise.<*>}
+     */
+    delete() {
+        return Promise.reject('not implemented');
     }
 
     /**
