6c9e752d19c4a435eb5c258ddef647ee6ebeefae
Katharina Irrgang <katharina.irrgang@googlemail.com>
2017-04-03 17:29:36 +0200

    🎨  🚑  do not use testurl.com in tests (#8254)
    no issue
    
    - if we are using any random domain in our tests, it could be that the test behaviour changes over time
    - in this case: the ghost_head_spec triggers a request for image diminsions (cover image, author image, blog logo)
    - if the target blog url/domain can't be reached (connecting....), the timeout would be reached - but the test is not setup to handle longer waiting times
    - even with https://github.com/TryGhost/Ghost/pull/8210/files, the default timeout would be 5s, which is not acceptable for a unit test
    - that's why, we need to choose a blog url, which definitely is "down" - any long localhost port

--- core/test/unit/server_helpers/url_spec.js
+++ core/test/unit/server_helpers/url_spec.js
@@ -13,7 +13,7 @@ describe('{{url}} helper', function () {
     var rendered;
 
     before(function () {
-        configUtils.set({url: 'http://testurl.com/'});
+        configUtils.set({url: 'http://localhost:82832/'});
     });
 
     beforeEach(function () {
@@ -52,7 +52,7 @@ describe('{{url}} helper', function () {
         );
 
         should.exist(rendered);
-        rendered.string.should.equal('http://testurl.com/slug/');
+        rendered.string.should.equal('http://localhost:82832/slug/');
     });
 
     it('should output an absolute URL with https if the option is present and secure', function () {
@@ -65,7 +65,7 @@ describe('{{url}} helper', function () {
         );
 
         should.exist(rendered);
-        rendered.string.should.equal('https://testurl.com/slug/');
+        rendered.string.should.equal('https://localhost:82832/slug/');
     });
 
     it('should output an absolute URL with https if secure', function () {
@@ -78,7 +78,7 @@ describe('{{url}} helper', function () {
         );
 
         should.exist(rendered);
-        rendered.string.should.equal('https://testurl.com/slug/');
+        rendered.string.should.equal('https://localhost:82832/slug/');
     });
 
     it('should return the slug with a prefixed /tag/ if the context is a tag', function () {
@@ -123,7 +123,7 @@ describe('{{url}} helper', function () {
             {url: '/bar', label: 'Bar', slug: 'bar', current: true},
             {hash: {absolute: 'true'}});
         should.exist(rendered);
-        rendered.string.should.equal('http://testurl.com/bar');
+        rendered.string.should.equal('http://localhost:82832/bar');
     });
 
     it('should return an absolute url with https if context is secure', function () {
@@ -131,7 +131,7 @@ describe('{{url}} helper', function () {
             {url: '/bar', label: 'Bar', slug: 'bar', current: true, secure: true},
             {hash: {absolute: 'true'}});
         should.exist(rendered);
-        rendered.string.should.equal('https://testurl.com/bar');
+        rendered.string.should.equal('https://localhost:82832/bar');
     });
 
     it('external urls should be retained in a nav context', function () {
@@ -144,40 +144,40 @@ describe('{{url}} helper', function () {
 
     it('should handle hosted urls in a nav context', function () {
         rendered = helpers.url.call(
-            {url: 'http://testurl.com/qux', label: 'Qux', slug: 'qux', current: true},
+            {url: 'http://localhost:82832/qux', label: 'Qux', slug: 'qux', current: true},
             {hash: {absolute: 'true'}});
         should.exist(rendered);
-        rendered.string.should.equal('http://testurl.com/qux');
+        rendered.string.should.equal('http://localhost:82832/qux');
     });
 
     it('should handle hosted urls in a nav context with secure', function () {
         rendered = helpers.url.call(
             {
-                url: 'http://testurl.com/qux', label: 'Qux', slug: 'qux', current: true,
+                url: 'http://localhost:82832/qux', label: 'Qux', slug: 'qux', current: true,
                 secure: true
             },
             {hash: {absolute: 'true'}});
         should.exist(rendered);
-        rendered.string.should.equal('https://testurl.com/qux');
+        rendered.string.should.equal('https://localhost:82832/qux');
     });
 
     it('should handle hosted https urls in a nav context with secure', function () {
         rendered = helpers.url.call(
             {
-                url: 'https://testurl.com/qux', label: 'Qux', slug: 'qux', current: true,
+                url: 'https://localhost:82832/qux', label: 'Qux', slug: 'qux', current: true,
                 secure: true
             },
             {hash: {absolute: 'true'}});
         should.exist(rendered);
-        rendered.string.should.equal('https://testurl.com/qux');
+        rendered.string.should.equal('https://localhost:82832/qux');
     });
 
     it('should handle hosted urls with the wrong protocol in a nav context', function () {
         rendered = helpers.url.call(
-            {url: 'https://testurl.com/quux', label: 'Quux', slug: 'quux', current: true},
+            {url: 'https://localhost:82832/quux', label: 'Quux', slug: 'quux', current: true},
             {hash: {absolute: 'true'}});
         should.exist(rendered);
-        rendered.string.should.equal('http://testurl.com/quux');
+        rendered.string.should.equal('http://localhost:82832/quux');
     });
 
     it('should pass through protocol-less URLs regardless of absolute setting', function () {
@@ -257,7 +257,7 @@ describe('{{url}} helper', function () {
 
     describe('with subdir', function () {
         it('external urls should be retained in a nav context with subdir', function () {
-            configUtils.set({url: 'http://testurl.com/blog'});
+            configUtils.set({url: 'http://localhost:82832/blog'});
             rendered = helpers.url.call(
                 {url: 'http://casper.website/baz', label: 'Baz', slug: 'baz', current: true},
                 {hash: {absolute: 'true'}});
@@ -266,13 +266,13 @@ describe('{{url}} helper', function () {
         });
 
         it('should handle subdir being set in nav context', function () {
-            configUtils.set({url: 'http://testurl.com/blog'});
+            configUtils.set({url: 'http://localhost:82832/blog'});
 
             rendered = helpers.url.call(
                 {url: '/xyzzy', label: 'xyzzy', slug: 'xyzzy', current: true},
                 {hash: {absolute: 'true'}});
             should.exist(rendered);
-            rendered.string.should.equal('http://testurl.com/blog/xyzzy');
+            rendered.string.should.equal('http://localhost:82832/blog/xyzzy');
         });
     });
 });
