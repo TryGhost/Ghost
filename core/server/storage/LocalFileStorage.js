817b8d09cafe4d22d8371844379eb61315babe10
Katharina Irrgang <katharina.irrgang@googlemail.com>
2017-04-05 16:10:34 +0200

    😱 🎨 Refactor storage adapter (#8229)
    refs #7687
    
    There are four main changes in this PR:
    
    we have outsourced the base storage adapter to npm, because for storage developers it's annoying to inherit from a script within Ghost
    we hacked theme storage handling into the default local storage adapter - this was reverted, instead we have added a static theme storage here
    use classes instead of prototyping
    optimise the storage adapter in general - everything is explained in each commit
    
    ----
    
    * rename local-file-store to LocalFileStorage
    
    I would like to keep the name pattern i have used for scheduling.
    If a file is a class, the file name reflects the class name.
    We can discuss this, if concerns are raised.
    
    * Transform LocalFileStorage to class and inherit from new base
    
    - inherit from npm ghost-storage-base
    - rewrite to class
    - no further refactoring, happens later
    
    * Rename core/test/unit/storage/local-file-store_spec.js -> core/test/unit/storage/LocalFileStorage_spec.js
    
    * Fix wrong require in core/test/unit/storage/LocalFileStorage_spec.js
    
    * remove base storage and test
    
    - see https://github.com/kirrg001/Ghost-Storage-Base
    - the test has moved to this repo as well
    
    * Use npm ghost-storage-base in storage/index.js
    
    * remove the concept of getStorage('themes')
    
    This concept was added when we added themes as a feature.
    Back then, we have changed the local storage adapter to support images and themes.
    This has added some hacks into the local storage adapters.
    We want to revert this change and add a simple static theme storage.
    
    Will adapt the api/themes layer in the next commits.
    
    * Revert LocalFileStorage
    
    - revert serve
    - revert delete
    
    * add storagePath as property to LocalFileStorage
    
    - define one property which holds the storage path
    - could be considered to pass from outside, but found that not helpful, as other storage adapters do not need this property
    - IMPORTANT: save has no longer a targetDir option, because this was used to pass the alternative theme storage path
    - IMPORTANT: exists has now an alternative targetDir, this makes sense, because
      - you can either ask the storage exists('my-file') and it will look in the base storage path
      - or you pass a specific path where to look exists('my-file', /path/to/dir)
    
    * LocalFileStorage: get rid of store pattern
    
    - getUniqueFileName(THIS)
    - this doesn't make sense, instances always have access to this by default
    
    * Add static theme storage
    
    - inherits from the local file storage, because they both operate on the file system
    - IMPORTANT: added a TODO to consider a merge of themes/loader and themes/storage
    - but will be definitely not part of this PR
    
    * Use new static theme storage in api/themes
    
    - storage functions are simplified!
    
    * Add https://github.com/kirrg001/Ghost-Storage-Base as dependency
    
    - tarball for now, as i am still testing
    - will release if PR review get's accepted
    
    * Adapt tests and jscs/jshint
    
    * 🐛  fix storage.read in favicon utility
    
    - wrong implementation of error handling
    
    * 🎨  optimise error messages for custom storage adapter errors
    
    * little renaming in the storage utlity
    
    - purpose is to have access to the custom storage instance and to the custom storage class
    - see next commit why
    
    * optimise instanceof base storage
    
    - instanceof is always tricky in javascript
    - if multiple modules exist, it can happen that instanceof is false
    
    * fix getTargetDir
    
    - the importer uses the `targetDir` option to ensure that images land in the correct folder
    
    * ghost-storage-base@0.0.1 package.json dependency

---
+++ core/server/storage/LocalFileStorage.js
@@ -0,0 +1,135 @@
+// jscs:disable requireMultipleVarDecl
+
+'use strict';
+
+// # Local File System Image Storage module
+// The (default) module for storing images, using the local file system
+
+var serveStatic = require('express').static,
+    fs = require('fs-extra'),
+    path = require('path'),
+    Promise = require('bluebird'),
+    config = require('../config'),
+    errors = require('../errors'),
+    i18n = require('../i18n'),
+    utils = require('../utils'),
+    StorageBase = require('ghost-storage-base');
+
+class LocalFileStore extends StorageBase {
+
+    constructor() {
+        super();
+
+        this.storagePath = config.getContentPath('images');
+    }
+
+    /**
+     * Saves the image to storage (the file system)
+     * - image is the express image object
+     * - returns a promise which ultimately returns the full url to the uploaded image
+     *
+     * @param image
+     * @param targetDir
+     * @returns {*}
+     */
+    save(image, targetDir) {
+        var targetFilename,
+            self = this;
+
+        // NOTE: the base implementation of `getTargetDir` returns the format this.storagePath/YYYY/MM
+        targetDir = targetDir || this.getTargetDir(this.storagePath);
+
+        return this.getUniqueFileName(image, targetDir).then(function (filename) {
+            targetFilename = filename;
+            return Promise.promisify(fs.mkdirs)(targetDir);
+        }).then(function () {
+            return Promise.promisify(fs.copy)(image.path, targetFilename);
+        }).then(function () {
+            // The src for the image must be in URI format, not a file system path, which in Windows uses \
+            // For local file system storage can use relative path so add a slash
+            var fullUrl = (
+                utils.url.urlJoin('/', utils.url.getSubdir(),
+                    utils.url.STATIC_IMAGE_URL_PREFIX,
+                    path.relative(self.storagePath, targetFilename))
+            ).replace(new RegExp('\\' + path.sep, 'g'), '/');
+
+            return fullUrl;
+        }).catch(function (e) {
+            return Promise.reject(e);
+        });
+    }
+
+    exists(fileName, targetDir) {
+        var filePath = path.join(targetDir || this.storagePath, fileName);
+
+        return new Promise(function (resolve) {
+            fs.stat(filePath, function (err) {
+                var exists = !err;
+                resolve(exists);
+            });
+        });
+    }
+
+    /**
+     * For some reason send divides the max age number by 1000
+     * Fallthrough: false ensures that if an image isn't found, it automatically 404s
+     * Wrap server static errors
+     *
+     * @returns {serveStaticContent}
+     */
+    serve() {
+        var self = this;
+
+        return function serveStaticContent(req, res, next) {
+            return serveStatic(self.storagePath, {maxAge: utils.ONE_YEAR_MS, fallthrough: false})(req, res, function (err) {
+                if (err) {
+                    if (err.statusCode === 404) {
+                        return next(new errors.NotFoundError({message: i18n.t('errors.errors.pageNotFound')}));
+                    }
+
+                    return next(new errors.GhostError({err: err}));
+                }
+
+                next();
+            });
+        };
+    }
+
+    /**
+     * Not implemented.
+     * @returns {Promise.<*>}
+     */
+    delete() {
+        return Promise.reject('not implemented');
+    }
+
+    /**
+     * Reads bytes from disk for a target image
+     * - path of target image (without content path!)
+     *
+     * @param options
+     */
+    read(options) {
+        options = options || {};
+
+        // remove trailing slashes
+        options.path = (options.path || '').replace(/\/$|\\$/, '');
+
+        var targetPath = path.join(this.storagePath, options.path);
+
+        return new Promise(function (resolve, reject) {
+            fs.readFile(targetPath, function (err, bytes) {
+                if (err) {
+                    return reject(new errors.GhostError({
+                        err: err,
+                        message: 'Could not read image: ' + targetPath
+                    }));
+                }
+
+                resolve(bytes);
+            });
+        });
+    }
+}
+
+module.exports = LocalFileStore;
