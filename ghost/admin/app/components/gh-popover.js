27ef26a991a9f2113fbd3c2456846e1f8a407622
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-04-05 14:08:29 +0100

    🔥 remove unused popover components (#620)
    no issue
    
    - `gh-popover` and `gh-popover-button` are no longer used in the codebase
    - removing them seems prudent as we are moving towards a different approach for dropdowns/popovers/tethers etc

--- ghost/admin/app/components/gh-popover.js
+++
@@ -1,7 +0,0 @@
-import injectService from 'ember-service/inject';
-import GhostDropdown from 'ghost-admin/components/gh-dropdown';
-
-export default GhostDropdown.extend({
-    classNames: 'ghost-popover',
-    dropdown: injectService()
-});
