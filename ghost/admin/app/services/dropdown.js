400150e67bf9a08ff3871065a93bf9802175ce61
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-04-05 14:08:29 +0100

    🔥 remove unused popover components (#620)
    no issue
    
    - `gh-popover` and `gh-popover-button` are no longer used in the codebase
    - removing them seems prudent as we are moving towards a different approach for dropdowns/popovers/tethers etc

--- ghost/admin/app/services/dropdown.js
+++ ghost/admin/app/services/dropdown.js
@@ -1,6 +1,6 @@
 import Service from 'ember-service';
 import Evented from 'ember-evented';
-// This is used by the dropdown initializer (and subsequently popovers) to manage closing & toggling
+// This is used by the dropdown initializer to manage closing & toggling
 import BodyEventListener from 'ghost-admin/mixins/body-event-listener';
 
 export default Service.extend(Evented, BodyEventListener, {
