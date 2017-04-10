4d927f31a61459c19ad0081feceebbc60f2113cd
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-04-10 10:53:27 +0100

    add power-select/trigger template override

---
+++ ghost/admin/app/components/power-select/trigger.js
@@ -0,0 +1,6 @@
+import layout from '../../templates/components/power-select/trigger';
+import trigger from 'ember-power-select/components/power-select/trigger';
+
+export default trigger.extend({
+    layout
+});
