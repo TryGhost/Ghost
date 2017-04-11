ae5b27a8e81a889db79d26349a3839b152e44fd0
Aileen Nowak <AileenCGN@gmail.com>
2017-04-11 19:28:02 +0700

    use urlJoin() and first try on test

--- core/test/unit/middleware/serve-favicon_spec.js
+++ core/test/unit/middleware/serve-favicon_spec.js
@@ -127,6 +127,93 @@ describe('Serve Favicon', function () {
             });
         });
 
+        describe.skip('serves with subdirectory setup', function () {
+            beforeEach(function () {
+                sandbox
+                    .stub(storage.getStorage(), 'read')
+                    .returns(new Promise.resolve('<Buffer 2c be a4 40 f7 87 73 1e 57 2c c1 e4 0d 79 03 95 42 f0 42 2e 41 95 27 c9 5c 35 a7 71 2c 09 5a 57 d3 04 1e 83 03 28 07 96 b0 c8 88 65 07 7a d1 d6 63 50>'));
+            });
+
+            it('custom uploaded favicon.png', function (done) {
+                var middleware = serveFavicon();
+                req.path = '/favicon.png';
+
+                configUtils.set('url', 'https://my-blog.com/blog/');
+
+                localSettingsCache.icon = 'favicon.png';
+
+                res = {
+                    writeHead: function (statusCode) {
+                        statusCode.should.eql(200);
+                    },
+                    end: function (body) {
+                        body.length.should.eql(6792);
+                        done();
+                    }
+                };
+
+                middleware(req, res, next);
+            });
+
+            it('custom uploaded favicon.ico', function (done) {
+                var middleware = serveFavicon();
+                req.path = '/favicon.ico';
+
+                storage.getStorage().storagePath = path.join(__dirname, '../../../test/utils/fixtures/images/');
+                localSettingsCache.icon = 'favicon.ico';
+
+                res = {
+                    writeHead: function (statusCode) {
+                        statusCode.should.eql(200);
+                    },
+                    end: function (body) {
+                        body.length.should.eql(15086);
+                        done();
+                    }
+                };
+
+                middleware(req, res, next);
+            });
+
+            it('custom uploaded myicon.ico', function (done) {
+                var middleware = serveFavicon();
+                req.path = '/favicon.ico';
+
+                storage.getStorage().storagePath = path.join(__dirname, '../../../test/utils/fixtures/images/');
+                localSettingsCache.icon = 'myicon.ico';
+
+                res = {
+                    writeHead: function (statusCode) {
+                        statusCode.should.eql(200);
+                    },
+                    end: function (body) {
+                        body.length.should.eql(15086);
+                        done();
+                    }
+                };
+
+                middleware(req, res, next);
+            });
+
+            it('default favicon.ico', function (done) {
+                var middleware = serveFavicon();
+                req.path = '/favicon.ico';
+                localSettingsCache.icon = '';
+
+                res = {
+                    writeHead: function (statusCode) {
+                        statusCode.should.eql(200);
+                    },
+                    end: function (body) {
+                        body.length.should.eql(15086);
+                        done();
+                    }
+                };
+
+                middleware(req, res, next);
+            });
+        });
+
         describe('redirects', function () {
             it('to custom favicon.ico when favicon.png is requested', function (done) {
                 var middleware = serveFavicon();
