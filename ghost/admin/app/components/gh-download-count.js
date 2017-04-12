1c6776d108d9ae2326e6680a41a9c173d76bb428
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-04-12 12:34:59 +0100

    💄 replace DownloadCountPoller with gh-download-count
    closes https://github.com/TryGhost/Ghost/issues/8321
    - adds `gh-download-count` component that uses ember-concurrency to poll the count endpoint
    - removes the no-longer-needed `setup/one` route as ember-concurrency now handles the setInterval bookkeeping for us

---
+++ ghost/admin/app/components/gh-download-count.js
@@ -0,0 +1,41 @@
+import Ember from 'ember';
+import Component from 'ember-component';
+import injectService from 'ember-service/inject';
+import {task, timeout} from 'ember-concurrency';
+
+const {testing} = Ember;
+const INTERVAL = testing ? 20 : 2000;
+
+export default Component.extend({
+    ajax: injectService(),
+    ghostPaths: injectService(),
+
+    count: '',
+
+    _poll: task(function* () {
+        let url = this.get('ghostPaths.count');
+        let pattern = /(-?\d+)(\d{3})/;
+
+        try {
+            let data = yield this.get('ajax').request(url);
+            let count = data.count.toString();
+
+            while (pattern.test(count)) {
+                count = count.replace(pattern, '$1,$2');
+            }
+
+            this.set('count', count);
+
+            if (!testing) {
+                yield timeout(INTERVAL);
+                this.get('_poll').perform();
+            }
+        } catch (e) {
+            // no-op - we don't want to create noise for a failed download count
+        }
+    }),
+
+    didInsertElement() {
+        this.get('_poll').perform();
+    }
+});
---
+++ ghost/admin/tests/integration/components/gh-download-count-test.js
@@ -0,0 +1,45 @@
+import {expect} from 'chai';
+import {describe, it} from 'mocha';
+import {setupComponentTest} from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+import Pretender from 'pretender';
+import wait from 'ember-test-helpers/wait';
+
+describe('Integration: Component: gh-download-count', function() {
+    setupComponentTest('gh-download-count', {
+        integration: true
+    });
+
+    let server;
+
+    beforeEach(function () {
+        server = new Pretender();
+        server.get('https://count.ghost.org/', function () {
+            return [200, {}, JSON.stringify({count: 42})];
+        });
+    });
+
+    afterEach(function () {
+        server.shutdown();
+    });
+
+    it('hits count endpoint and renders', function () {
+        this.render(hbs`{{gh-download-count}}`);
+
+        return wait().then(() => {
+            expect(this.$().text().trim()).to.equal('42');
+        });
+    });
+
+    it('renders with a block', function () {
+        this.render(hbs`
+            {{#gh-download-count as |count|}}
+                {{count}} downloads
+            {{/gh-download-count}}
+        `);
+
+        return wait().then(() => {
+            expect(this.$().text().trim()).to.equal('42 downloads');
+        });
+    });
+});
