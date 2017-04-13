fd50af79b6948099bcd6232f4772cb7e91857dd1
Hannah Wolfe <github.erisds@gmail.com>
2017-04-13 08:26:48 +0100

    ✨ Added pre-commit hook to handle submodules (#8302)
    refs #8235
    
    Usage:
    - for existing development setups: `grunt symlink` (will create the pre-commit symlink)
    - for fresh development setups: `npm run init` (symlinking happens as part of the typical set up)
    
    - ✨ Added pre-commit hook to handle submodules
      - Checks to see if there are any submodules about to be committed
      - Output matches closely to `git st` to make it easy to read
      - Requires interaction from the committer to accept that this really should be committed
    - ✨ Use grunt symlink to register githooks
      - Grunt symlink will make a link to the pre-commit hook
      - It ONLY does this if there isn't already a pre-commit hook, so won't overwrite anything
      - It does this as part of npm run init, not grunt init, because a release repo would NEVER want this
      - This is a dev tool, that configures the repo for development

--- Gruntfile.js
+++ Gruntfile.js
@@ -370,6 +370,22 @@ var overrides      = require('./core/server/overrides'),
                         'core/client': ['shell:ember:watch', '--live-reload-base-url="' + utils.url.getSubdir() + '/ghost/"']
                     }
                 }
+            },
+
+            // ### grunt-contrib-symlink
+            // Create symlink for git hooks
+            symlink: {
+                githooks: {
+                    // Enable overwrite to delete symlinks before recreating them
+                    overwrite: false,
+                    // Enable force to overwrite symlinks outside the current working directory
+                    force: false,
+                    // Expand to all files in /hooks
+                    expand: true,
+                    cwd: '.github/hooks',
+                    src: ['*'],
+                    dest: '.git/hooks'
+                }
             }
         };
 
