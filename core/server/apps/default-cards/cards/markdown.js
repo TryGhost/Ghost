3a8f553e3f8446fa07524f80087c97b75e5e860c
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-05-12 08:47:10 +0100

    add duplicate header ID handling

--- core/server/apps/default-cards/cards/markdown.js
+++ core/server/apps/default-cards/cards/markdown.js
@@ -14,9 +14,13 @@ converter = new MarkdownIt({
 .use(require('markdown-it-mark'))
 .use(require('markdown-it-named-headers'), {
     // match legacy Showdown IDs otherwise default is github style dasherized
-    // NOTE: this doesn't add a count suffix to duplicate titles
-    slugify: function (input_string) {
-        return input_string.replace(/[^\w]/g, '').toLowerCase();
+    slugify: function (input_string, used_headers) {
+        var slug = input_string.replace(/[^\w]/g, '').toLowerCase();
+        if (used_headers[slug]) {
+            used_headers[slug]++;
+            slug += used_headers[slug];
+        }
+        return slug;
     }
 });
 
--- core/test/unit/markdown_client_integrated_spec.js
+++ core/test/unit/markdown_client_integrated_spec.js
@@ -18,9 +18,13 @@ var should = require('should'), // jshint ignore:line
     .use(require('markdown-it-mark'))
     .use(require('markdown-it-named-headers'), {
         // match legacy Showdown IDs otherwise default is github style dasherized
-        // NOTE: this doesn't add a count suffix to duplicate titles
-        slugify: function (input_string) {
-            return input_string.replace(/[^\w]/g, '').toLowerCase();
+        slugify: function (input_string, used_headers) {
+            var slug = input_string.replace(/[^\w]/g, '').toLowerCase();
+            if (used_headers[slug]) {
+                used_headers[slug]++;
+                slug += used_headers[slug];
+            }
+            return slug;
         }
     });
 
