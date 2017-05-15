8cfd205371431be068e16a6a8cf8f575a13d5d3e
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-05-15 17:51:19 +0100

    ✨ use markdown-it for markdown previews (#690)
    ✨ use markdown-it for markdown previews
    
    no issue
    
    - replaces SimpleMDE's default `marked` rendering with `markdown-it`
    - add ember-browserify and markdown-it plugins

--- ghost/admin/app/utils/caja-sanitizers.js
+++ ghost/admin/app/utils/caja-sanitizers.js
@@ -4,6 +4,7 @@
 /**
  * Check if URL is allowed
  * URLs are allowed if they start with http://, https://, or /.
+ * NOTE: # urls are not allowed as clicking them will break the editor when clicked
  */
 let url = function (url) {
     url = url.toString().replace(/['"]+/g, '');
