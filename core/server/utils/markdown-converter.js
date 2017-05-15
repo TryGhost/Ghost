5b57d1226479515086f55a8be6a98a9aba415938
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-05-15 13:02:01 +0100

    fix shameful lint errors 🙈 🔔 🔔 🔔

--- core/server/utils/markdown-converter.js
+++ core/server/utils/markdown-converter.js
@@ -1,27 +1,26 @@
-var MarkdownIt  = require('markdown-it')
-
-converter = new MarkdownIt({
-    html: true,
-    breaks: true,
-    linkify: true
-})
-.use(require('markdown-it-footnote'))
-.use(require('markdown-it-lazy-headers'))
-.use(require('markdown-it-mark'))
-.use(require('markdown-it-named-headers'), {
-    // match legacy Showdown IDs otherwise default is github style dasherized
-    slugify: function (input_string, used_headers) {
-        var slug = input_string.replace(/[^\w]/g, '').toLowerCase();
-        if (used_headers[slug]) {
-            used_headers[slug]++;
-            slug += used_headers[slug];
+var MarkdownIt  = require('markdown-it'),
+    converter = new MarkdownIt({
+        html: true,
+        breaks: true,
+        linkify: true
+    })
+    .use(require('markdown-it-footnote'))
+    .use(require('markdown-it-lazy-headers'))
+    .use(require('markdown-it-mark'))
+    .use(require('markdown-it-named-headers'), {
+        // match legacy Showdown IDs otherwise default is github style dasherized
+        slugify: function (inputString, usedHeaders) {
+            var slug = inputString.replace(/[^\w]/g, '').toLowerCase();
+            if (usedHeaders[slug]) {
+                usedHeaders[slug] += 1;
+                slug += usedHeaders[slug];
+            }
+            return slug;
         }
-        return slug;
-    }
-});
+    });
 
 module.exports = {
     render: function (markdown) {
         return converter.render(markdown);
     }
-}
+};
