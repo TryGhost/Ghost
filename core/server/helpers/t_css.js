2d2c8369d0e3cecc6bcc82d3c8b8f42989cfd10a
juan-g <juan-g@users.noreply.github.com>
2017-05-19 10:08:49 +0200

    [i18n] Moved .css translation files for themes
    pull request #8437
    
    - to facilitate translators' work, moved .css translation files for
    themes from assets/css/en.css, etc. to
    assets/translations/mytheme_en.css, etc. (replacing "mytheme" with the
    active theme, such as casper, etc.)
    - adjusted {{t_css}} helper and its test accordingly
    - there is a complementary commit for Casper default theme's PR
    TryGhost/Casper#306

--- core/server/helpers/t_css.js
+++ core/server/helpers/t_css.js
@@ -1,10 +1,10 @@
 // # t_css helper
-// i18n: Helper enabling a stylesheet file such as /assets/css/es.css
+// i18n: Helper enabling a stylesheet file such as /assets/translations/mytheme_es.css
 // to translate css content by overriding.
 // This is used for example in Casper's default.hbs theme template file:
 // <link rel="stylesheet" type="text/css" href="{{t_css}}" />
 // which results in something like:
-// <link rel="stylesheet" type="text/css" href="/assets/css/es.css?v=15890d2423" />
+// <link rel="stylesheet" type="text/css" href="/assets/translations/casper_es.css?v=15890d2423" />
 //
 // We use the name t_css to match the helper for consistency:
 // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
@@ -13,10 +13,12 @@ var proxy = require('./proxy'),
     i18n = proxy.i18n,
     SafeString = proxy.SafeString,
     getAssetUrl = proxy.metaData.getAssetUrl,
+    settingsCache = proxy.settingsCache,
+    theme = settingsCache.get('active_theme'),
     locale = i18n.locale(),
     tCss;
 
 module.exports = function t_css() {
-    tCss = new SafeString(getAssetUrl('css/' + locale + '.css'));
+    tCss = new SafeString(getAssetUrl('translations/' + theme + '_' + locale + '.css'));
     return tCss;
 };
--- core/test/unit/server_helpers/t_css_spec.js
+++ core/test/unit/server_helpers/t_css_spec.js
@@ -1,6 +1,6 @@
 // Test: {{t_css}} translation helper for css accepts no parameters,
 // and returns URI for translation stylesheet such as for example:
-// "/assets/css/en.css?v=4999b1c396"
+// "/assets/translations/mytheme_es.css?v=15890d2423"
 
 var should = require('should'),
     helpers = require('../../../server/helpers');
@@ -9,6 +9,6 @@ describe('{{t_css}} helper', function () {
     it('returns correct URI for translation stylesheet', function () {
         var rendered = helpers.t_css.call();
         should.exist(rendered);
-        rendered.string.should.match(/^\/assets\/css\/.+\?v\=[0-9a-f]{10}$/);
+        rendered.string.should.match(/^\/assets\/translations\/.+\_.+\.css\?v\=[0-9a-f]{10}$/);
     });
 });
