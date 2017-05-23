5f01e8cc8935ef1503747cdeb5f752f0a74ffd19
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-05-23 17:45:25 +0100

    🐛 fix flickering buttons during modal transitions (#710)
    no issue
    
    - removes transition on `all` properties in favour of an explicit list, fixes the buttons flickering after the modal appears and the outline/text being visible when the modal disappears
    - simplifies the liquid-fire transition so that elements being removed at slightly different times doesn't cause flickering when the modal is removed

--- ghost/admin/app/transitions.js
+++ ghost/admin/app/transitions.js
@@ -3,12 +3,7 @@ export default function () {
         this.hasClass('fullscreen-modal-container'),
         this.toValue(true),
         this.use('fade', {duration: 150}),
-        this.reverse('explode', {
-            pick: '.fullscreen-modal',
-            use: ['fade', {duration: 80}]
-        }, {
-            use: ['fade', {duration: 150}]
-        })
+        this.reverse('fade', {duration: 150})
     );
 
     this.transition(
