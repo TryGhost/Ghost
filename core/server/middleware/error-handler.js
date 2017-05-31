813d288eb2c054ba1ea7e0ee7af114ce41ca8264
kirrg001 <katharina.irrgang@googlemail.com>
2017-05-31 18:42:42 +0200

    🎨  gscan 1.1.0 & optimisations
    refs #8222
    
    - differentiate between errors and fatal errors
    - use gscan errors in theme middleware
    - Adds a new `error()` method to `currentActiveTheme` constructor which will return the errors we receive from gscan
    - In middleware, if a theme couldn't be activated because it's invalid, we'll fetch the erros and send them to our error handler. We also use a new property `hideStack` to control, if the stack (in dev mode and if available) should be shown or the gscan errors (in prod mode, or in dev if no stack error)
    - In our error handler we use this conditional to send a new property `gscan` to our error theme
    - In `error.hbs` we'll iterate through possible `gscan` error objects and render them.
    - remove stack printing
    - stack for theme developers in development mode doesn't make sense
    - stack in production doesn't make sense
    - the stack is usually hard to read
    - if you are developer you can read the error stack on the server log
    - utils.packages: transform native error into Ghost error
    - use `onlyFatalErrors` for gscan format and differeniate fatal errors vo.2
    - optimise bootstrap error handling
    - transform theme is missing into an error
    - add new translation key
    - show html tags for error.hbs template: rule

--- core/server/middleware/error-handler.js
+++ core/server/middleware/error-handler.js
@@ -1,6 +1,5 @@
 var _ = require('lodash'),
     hbs = require('express-hbs'),
-    config = require('../config'),
     errors = require('../errors'),
     i18n = require('../i18n'),
     templates = require('../controllers/frontend/templates'),
@@ -17,47 +16,6 @@ _private.createHbsEngine = function createHbsEngine() {
     engine.registerHelper('asset', require('../helpers/asset'));
 
     return engine.express4();
-};
-
-/**
- * This function splits the stack into pieces, that are then rendered using the following handlebars code:
- * ```
- * {{#each stack}}
- *   <li>
- *   at
- *   {{#if function}}<em class="error-stack-function">{{function}}</em>{{/if}}
- *   <span class="error-stack-file">({{at}})</span>
- *   </li>
- * {{/each}}
- * ```
- * @TODO revisit whether this is useful as part of #7491
- */
-_private.parseStack = function parseStack(stack) {
-    if (!_.isString(stack)) {
-        return stack;
-    }
-
-    var stackRegex = /\s*at\s*(\w+)?\s*\(([^\)]+)\)\s*/i;
-
-    return (
-        stack
-            .split(/[\r\n]+/)
-            .slice(1)
-            .map(function (line) {
-                var parts = line.match(stackRegex);
-                if (!parts) {
-                    return null;
-                }
-
-                return {
-                    function: parts[1],
-                    at: parts[2]
-                };
-            })
-            .filter(function (line) {
-                return !!line;
-            })
-    );
 };
 
 /**
@@ -114,13 +72,10 @@ _private.JSONErrorRenderer = function JSONErrorRenderer(err, req, res, /*jshint 
 
 _private.HTMLErrorRenderer = function HTMLErrorRender(err, req, res, /*jshint unused:false */ next) {
     var templateData = {
-            message: err.message,
-            code: err.statusCode
-        };
-
-    if (err.statusCode === 500 && config.get('printErrorStack')) {
-        templateData.stack = err.stack;
-    }
+        message: err.message,
+        code: err.statusCode,
+        errorDetails: err.errorDetails || []
+    };
 
     // It can be that something went wrong with the theme or otherwise loading handlebars
     // This ensures that no matter what res.render will work here
