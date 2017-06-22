94c4ff7f84c1f9c2a0db835713a37bb09acb847b
Hannah Wolfe <erisds@gmail.com>
2017-06-22 17:11:33 +0100

    ✨ Pass clientExtensions to Ghost Admin
    - pass through this additional config if it exists

--- core/server/api/configuration.js
+++ core/server/api/configuration.js
@@ -30,7 +30,8 @@ function getBaseConfig() {
         publicAPI:      config.get('publicAPI') === true,
         blogUrl:        utils.url.urlFor('home', true),
         blogTitle:      settingsCache.get('title'),
-        routeKeywords:  config.get('routeKeywords')
+        routeKeywords:  config.get('routeKeywords'),
+        clientExtensions: config.get('clientExtensions')
     };
 }
 
