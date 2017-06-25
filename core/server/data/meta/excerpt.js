2fbf6434c5545e6a81c398f121b5d8f646379e83
HIGAN <higan@live.cn>
2017-06-25 19:08:00 +0800

    Full support of 'Downsize' for excerpts

--- core/server/data/meta/excerpt.js
+++ core/server/data/meta/excerpt.js
@@ -2,19 +2,22 @@ var downsize = require('downsize');
 
 function getExcerpt(html, truncateOptions) {
     truncateOptions = truncateOptions || {};
+
+    if (!truncateOptions.words && !truncateOptions.characters) {
+        truncateOptions.words = 50;
+    }
+
+    var excerpt = downsize(html, truncateOptions);
+
     // Strip inline and bottom footnotes
-    var excerpt = html.replace(/<a href="#fn.*?rel="footnote">.*?<\/a>/gi, '');
+    excerpt = excerpt.replace(/<a href="#fn.*?rel="footnote">.*?<\/a>/gi, '');
     excerpt = excerpt.replace(/<div class="footnotes"><ol>.*?<\/ol><\/div>/, '');
     // Strip other html
     excerpt = excerpt.replace(/<\/?[^>]+>/gi, '');
     excerpt = excerpt.replace(/(\r\n|\n|\r)+/gm, ' ');
     /*jslint regexp:false */
 
-    if (!truncateOptions.words && !truncateOptions.characters) {
-        truncateOptions.words = 50;
-    }
-
-    return downsize(excerpt, truncateOptions);
+    return excerpt;
 }
 
 module.exports = getExcerpt;
