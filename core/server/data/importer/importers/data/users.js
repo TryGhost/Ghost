0e23f203915bf31606e6c9fb4ed15426da5c708b
Sam Wilskey <swilskey41@gmail.com>
2017-07-21 21:32:11 -0700

    Keeps user inactive during user import
    closes #8651
    - added a check to see if user is inactive in import
    - passes all tests

--- core/server/data/importer/importers/data/users.js
+++ core/server/data/importer/importers/data/users.js
@@ -41,7 +41,9 @@ class UsersImporter extends BaseImporter {
 
         _.each(this.dataToImport, function (model) {
             model.password = globalUtils.uid(50);
-            model.status = 'locked';
+            if (model.status !== 'inactive') {
+                model.status = 'locked';
+            }
         });
 
         _.each(this.roles_users, function (attachedRole) {
