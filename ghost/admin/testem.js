c489f4474b7b5fe802a0e846396356e92001b372
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-07-31 13:38:46 +0400

    ⬆️ bump core ember dependencies (#804)
    no issue
    
    - bump core `ember`, `ember-data` and `ember-cli` related dependencies
    - remove deprecated `MODEL_FACTORY_INJECTIONS` flag
    - update files to better match `ember-cli new` output for easier diffs during upgrades

--- ghost/admin/testem.js
+++ ghost/admin/testem.js
@@ -1,13 +1,14 @@
 /* eslint-env node */
+/* eslint-disable camelcase */
 module.exports = {
-    'framework': 'mocha',
-    'test_page': 'tests/index.html?hidepassed',
-    'disable_watching': true,
-    'launch_in_ci': [
+    framework: 'mocha',
+    test_page: 'tests/index.html?hidepassed',
+    disable_watching: true,
+    launch_in_ci: [
         'Chrome',
         'Firefox'
     ],
-    'launch_in_dev': [
+    launch_in_dev: [
         'Chrome',
         'Firefox'
     ]
