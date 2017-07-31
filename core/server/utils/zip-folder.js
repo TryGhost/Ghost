9ff937828bf9d90382c823abd25a2de30d914505
Austin Burdine <acburdine@gmail.com>
2017-07-31 03:48:00 -0400

    🐛 🔗 resolve symlinks before building zip (#8780)
    closes #8778
    
    - if folderToZip is a symlink, find the target using fs.realPathSync so we zip the right thing
    - add a test

--- core/server/utils/zip-folder.js
+++ core/server/utils/zip-folder.js
@@ -5,6 +5,12 @@ module.exports = function zipFolder(folderToZip, destination, callback) {
         output = fs.createWriteStream(destination),
         archive = archiver.create('zip', {});
 
+    // If folder to zip is a symlink, we want to get the target
+    // of the link and zip that instead of zipping the symlink
+    if (fs.lstatSync(folderToZip).isSymbolicLink()) {
+        folderToZip = fs.realpathSync(folderToZip);
+    }
+
     output.on('close', function () {
         callback(null, archive.pointer());
     });
