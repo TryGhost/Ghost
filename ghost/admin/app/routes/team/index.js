982e52389b94f66ef6e730e43c2653577c2d3d97
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-07-31 17:10:52 +0400

    🐛 fixed active users list not showing more than 20 users (#806)
    closes https://github.com/TryGhost/Ghost/issues/8773
    - swap old custom `PaginationMixin` for `ember-infinity`

--- ghost/admin/app/routes/team/index.js
+++ ghost/admin/app/routes/team/index.js
@@ -1,24 +1,31 @@
 import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
 import CurrentUserSettings from 'ghost-admin/mixins/current-user-settings';
-import PaginationMixin from 'ghost-admin/mixins/pagination';
+import InfinityRoute from 'ember-infinity/mixins/route';
 import RSVP from 'rsvp';
 import styleBody from 'ghost-admin/mixins/style-body';
 
-export default AuthenticatedRoute.extend(styleBody, CurrentUserSettings, PaginationMixin, {
+export default AuthenticatedRoute.extend(styleBody, CurrentUserSettings, InfinityRoute, {
     titleToken: 'Team',
 
     classNames: ['view-team'],
 
-    paginationModel: 'user',
-    paginationSettings: {
-        filter: 'status:-inactive',
-        limit: 20
-    },
+    modelPath: 'controller.activeUsers',
+    perPage: 15,
+    perPageParam: 'limit',
+    totalPagesParam: 'meta.pagination.pages',
 
     model() {
         return this.get('session.user').then((user) => {
+            let modelPath = this.get('modelPath');
+            let perPage = this.get('perPage');
+
             let modelPromises = {
-                activeUsers: this.loadFirstPage()
+                activeUsers: this.infinityModel('user', {
+                    modelPath,
+                    perPage,
+                    filter: 'status:-inactive',
+                    startingPage: 1
+                })
             };
 
             // authors do not have permission to hit the invites or suspended users endpoint
