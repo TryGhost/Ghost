f53fbc2e9e2991a4cab8128bf08cd0be3f913c99
kirrg001 <katharina.irrgang@googlemail.com>
2017-08-02 11:34:48 +0400

    ✨  Code Injection per Post feature
    no issue
    
    - add 1.4 database migration to add two new fields to the database (use type text, because of max row size)
    - handle global code injection vs. post code injection
    - add tests

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -23,7 +23,9 @@ module.exports = {
         updated_by: {type: 'string', maxlength: 24, nullable: true},
         published_at: {type: 'dateTime', nullable: true},
         published_by: {type: 'string', maxlength: 24, nullable: true},
-        custom_excerpt: {type: 'string', maxlength: 2000, nullable: true, validations: {isLength: {max: 300}}}
+        custom_excerpt: {type: 'string', maxlength: 2000, nullable: true, validations: {isLength: {max: 300}}},
+        codeinjection_head: {type: 'text', maxlength: 65535, nullable: true},
+        codeinjection_foot: {type: 'text', maxlength: 65535, nullable: true}
     },
     users: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
--- core/test/unit/server_helpers/ghost_head_spec.js
+++ core/test/unit/server_helpers/ghost_head_spec.js
@@ -1189,6 +1189,45 @@ describe('{{ghost_head}} helper', function () {
             }).catch(done);
         });
 
+        it('outputs post codeinjection', function (done) {
+            helpers.ghost_head.call(
+                {safeVersion: '0.3', context: ['paged', 'index']},
+                {data: {root: {context: [], post: {codeinjection_head: 'post-codeinjection'}}}}
+            ).then(function (rendered) {
+                should.exist(rendered);
+                rendered.string.should.not.match(/<style>body {background: red;}<\/style>/);
+                rendered.string.should.match(/post-codeinjection/);
+
+                done();
+            }).catch(done);
+        });
+
+        it('handles post codeinjection being empty', function (done) {
+            helpers.ghost_head.call(
+                {safeVersion: '0.3', context: ['paged', 'index']},
+                {data: {root: {context: [], post: {codeinjection_head: ''}}}}
+            ).then(function (rendered) {
+                should.exist(rendered);
+                rendered.string.should.match(/<style>body {background: red;}<\/style>/);
+                rendered.string.should.not.match(/post-codeinjection/);
+
+                done();
+            }).catch(done);
+        });
+
+        it('handles post codeinjection being null', function (done) {
+            helpers.ghost_head.call(
+                {safeVersion: '0.3', context: ['paged', 'index']},
+                {data: {root: {context: [], post: {codeinjection_head: null}}}}
+            ).then(function (rendered) {
+                should.exist(rendered);
+                rendered.string.should.match(/<style>body {background: red;}<\/style>/);
+                rendered.string.should.not.match(/post-codeinjection/);
+
+                done();
+            }).catch(done);
+        });
+
         it('returns meta tag without injected code for amp context', function (done) {
             var post = {
                 meta_description: 'blog description',
