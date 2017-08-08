b2336209de2641170fb2efc3f593da0140ff2086
Hannah Wolfe <erisds@gmail.com>
2017-08-08 11:47:56 +0100

    🦄  Channels: make RSS & Pagination configurable
    refs #5091
    
    - occurred to me whilst documenting the custom homepage config, that RSS and pagination
    need to be optional
    - added a very quick if statement & tests
    - needs further refactoring & test improvements
    - this will not disable the RSS url output in meta data yet 😔

--- core/test/unit/controllers/frontend/channel-config_spec.js
+++ core/test/unit/controllers/frontend/channel-config_spec.js
@@ -1,22 +1,22 @@
 /*jshint expr:true*/
 var should = require('should'),
-    channelConfig = require('../../../../server/controllers/frontend/channel-config').get;
+    channelConfig = require('../../../../server/controllers/frontend/channel-config');
 
 describe('Channel Config', function () {
     it('should get the index config', function () {
-        var result = channelConfig('index');
+        var result = channelConfig.get('index');
         should.exist(result);
         result.name.should.eql('index');
     });
 
     it('should get the author config', function () {
-        var result = channelConfig('author');
+        var result = channelConfig.get('author');
         should.exist(result);
         result.name.should.eql('author');
     });
 
     it('should get the tag config', function () {
-        var result = channelConfig('tag');
+        var result = channelConfig.get('tag');
         should.exist(result);
         result.name.should.eql('tag');
     });
