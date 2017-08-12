94a49c480732d0a37a12bdc1509fc1ebab97540a
Hannah Wolfe <erisds@gmail.com>
2017-08-12 16:43:08 +0100

    Split routes out from the API app 🎨
    no issue
    
    - Use the same pattern as the blog app
    - General cleanup/unification across all of the `app.js` files

--- core/server/admin/app.js
+++ core/server/admin/app.js
@@ -1,17 +1,21 @@
 var debug = require('ghost-ignition').debug('admin'),
-    config = require('../config'),
     express = require('express'),
+
+    // App requires
+    config = require('../config'),
+    utils = require('../utils'),
+
+    // Middleware
     // Admin only middleware
     adminMiddleware = require('./middleware'),
+    serveStatic = require('express').static,
 
-    // Global/shared middleware?
+    // Global/shared middleware
     cacheControl = require('../middleware/cache-control'),
     urlRedirects = require('../middleware/url-redirects'),
     errorHandler = require('../middleware/error-handler'),
     maintenance = require('../middleware/maintenance'),
-    prettyURLs = require('../middleware/pretty-urls'),
-    serveStatic = require('express').static,
-    utils = require('../utils');
+    prettyURLs = require('../middleware/pretty-urls');
 
 module.exports = function setupAdminApp() {
     debug('Admin setup start');
