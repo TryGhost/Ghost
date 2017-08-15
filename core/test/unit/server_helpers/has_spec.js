b7366beb81a04cce00731e4ccfabb392b82f3ada
Hannah Wolfe <erisds@gmail.com>
2017-08-15 12:26:20 +0100

    🛠 Simplified {{#has}} helper tests

--- core/test/unit/server_helpers/has_spec.js
+++ core/test/unit/server_helpers/has_spec.js
@@ -7,176 +7,153 @@ var should = require('should'), // jshint ignore:line
     sandbox = sinon.sandbox.create();
 
 describe('{{#has}} helper', function () {
+    var fn, inverse, thisCtx, handlebarsOptions;
+
     afterEach(function () {
         sandbox.restore();
     });
 
-    it('should handle tag list that validates true', function () {
-        var fn = sandbox.spy(),
-            inverse = sandbox.spy();
+    beforeEach(function () {
+        fn = sandbox.spy();
+        inverse = sandbox.spy();
 
-        helpers.has.call(
-            {tags: [{name: 'foo'}, {name: 'bar'}, {name: 'baz'}]},
-            {hash: {tag: 'invalid, bar, wat'}, fn: fn, inverse: inverse}
-        );
+        thisCtx = {};
 
-        fn.called.should.be.true();
-        inverse.called.should.be.false();
+        // This object mocks out the object that handlebars helpers get passed
+        handlebarsOptions = {
+            hash: {},
+            data: {},
+            fn: fn,
+            inverse: inverse
+        };
     });
 
-    it('should handle tags with case-insensitivity', function () {
-        var fn = sandbox.spy(),
-            inverse = sandbox.spy();
+    function callHasHelper(thisCtx, hash) {
+        // Hash is the options passed in
+        handlebarsOptions.hash = hash;
+        return helpers.has.call(thisCtx, handlebarsOptions);
+    }
 
-        helpers.has.call(
-            {tags: [{name: 'ghost'}]},
-            {hash: {tag: 'GhoSt'}, fn: fn, inverse: inverse}
-        );
+    describe('tag match', function () {
+        it('should handle tag list that validates true', function () {
+            thisCtx = {tags: [{name: 'foo'}, {name: 'bar'}, {name: 'baz'}]};
 
-        fn.called.should.be.true();
-        inverse.called.should.be.false();
+            // {{#has tag="invalid, bar, wat"}}
+            callHasHelper(thisCtx, {tag: 'invalid, bar, wat'});
+
+            fn.called.should.be.true();
+            inverse.called.should.be.false();
+        });
+
+        it('should handle tags with case-insensitivity', function () {
+            thisCtx = {tags: [{name: 'ghost'}]};
+
+            // {{#has tag="GhoSt"}}
+            callHasHelper(thisCtx, {tag: 'GhoSt'});
+
+            fn.called.should.be.true();
+            inverse.called.should.be.false();
+        });
+
+        it('should match exact tags, not superstrings', function () {
+            thisCtx = {tags: [{name: 'magical'}]};
+
+            callHasHelper(thisCtx, {tag: 'magic'});
+
+            fn.called.should.be.false();
+            inverse.called.should.be.true();
+        });
+
+        it('should match exact tags, not substrings', function () {
+            thisCtx = {tags: [{name: 'magic'}]};
+
+            callHasHelper(thisCtx, {tag: 'magical'});
+
+            fn.called.should.be.false();
+            inverse.called.should.be.true();
+        });
+
+        it('should handle tag list that validates false', function () {
+            thisCtx = {tags: [{name: 'foo'}, {name: 'bar'}, {name: 'baz'}]};
+
+            callHasHelper(thisCtx, {tag: 'much, such, wow'});
+
+            fn.called.should.be.false();
+            inverse.called.should.be.true();
+        });
+
+        it('should not do anything if there are no attributes', function () {
+            thisCtx = {tags: [{name: 'foo'}, {name: 'bar'}, {name: 'baz'}]};
+
+            callHasHelper(thisCtx);
+
+            fn.called.should.be.false();
+            inverse.called.should.be.false();
+        });
+
+        it('should not do anything when an invalid attribute is given', function () {
+            thisCtx = {tags: [{name: 'foo'}, {name: 'bar'}, {name: 'baz'}]};
+
+            callHasHelper(thisCtx, {invalid: 'nonsense'});
+
+            fn.called.should.be.false();
+            inverse.called.should.be.false();
+        });
     });
 
-    it('should match exact tags, not superstrings', function () {
-        var fn = sandbox.spy(),
-            inverse = sandbox.spy();
+    describe('author match', function () {
+        it('should handle author list that evaluates to true', function () {
+            thisCtx = {author: {name: 'sam'}};
 
-        helpers.has.call(
-            {tags: [{name: 'magical'}]},
-            {hash: {tag: 'magic'}, fn: fn, inverse: inverse}
-        );
+            callHasHelper(thisCtx, {author: 'joe, sam, pat'});
 
-        fn.called.should.be.false();
-        inverse.called.should.be.true();
-    });
+            fn.called.should.be.true();
+            inverse.called.should.be.false();
+        });
 
-    it('should match exact tags, not substrings', function () {
-        var fn = sandbox.spy(),
-            inverse = sandbox.spy();
+        it('should handle author list that evaluates to false', function () {
+            thisCtx = {author: {name: 'jamie'}};
 
-        helpers.has.call(
-            {tags: [{name: 'magic'}]},
-            {hash: {tag: 'magical'}, fn: fn, inverse: inverse}
-        );
+            callHasHelper(thisCtx, {author: 'joe, sam, pat'});
 
-        fn.called.should.be.false();
-        inverse.called.should.be.true();
-    });
+            fn.called.should.be.false();
+            inverse.called.should.be.true();
+        });
 
-    it('should handle tag list that validates false', function () {
-        var fn = sandbox.spy(),
-            inverse = sandbox.spy();
+        it('should handle authors with case-insensitivity', function () {
+            thisCtx = {author: {name: 'Sam'}};
 
-        helpers.has.call(
-            {tags: [{name: 'foo'}, {name: 'bar'}, {name: 'baz'}]},
-            {hash: {tag: 'much, such, wow'}, fn: fn, inverse: inverse}
-        );
+            callHasHelper(thisCtx, {author: 'joe, sAm, pat'});
 
-        fn.called.should.be.false();
-        inverse.called.should.be.true();
-    });
+            fn.called.should.be.true();
+            inverse.called.should.be.false();
+        });
 
-    it('should not do anything if there are no attributes', function () {
-        var fn = sandbox.spy(),
-            inverse = sandbox.spy();
+        it('should handle tags and authors like an OR query (pass)', function () {
+            thisCtx = {author: {name: 'sam'}, tags: [{name: 'foo'}, {name: 'bar'}, {name: 'baz'}]};
 
-        helpers.has.call(
-            {tags: [{name: 'foo'}, {name: 'bar'}, {name: 'baz'}]},
-            {fn: fn, inverse: inverse}
-        );
+            callHasHelper(thisCtx, {author: 'joe, sam, pat', tag: 'much, such, wow'});
 
-        fn.called.should.be.false();
-        inverse.called.should.be.false();
-    });
+            fn.called.should.be.true();
+            inverse.called.should.be.false();
+        });
 
-    it('should not do anything when an invalid attribute is given', function () {
-        var fn = sandbox.spy(),
-            inverse = sandbox.spy();
+        it('should handle tags and authors like an OR query (pass)', function () {
+            thisCtx = {author: {name: 'sam'}, tags: [{name: 'much'}, {name: 'bar'}, {name: 'baz'}]};
 
-        helpers.has.call(
-            {tags: [{name: 'foo'}, {name: 'bar'}, {name: 'baz'}]},
-            {hash: {invalid: 'nonsense'}, fn: fn, inverse: inverse}
-        );
+            callHasHelper(thisCtx, {author: 'joe, sam, pat', tag: 'much, such, wow'});
 
-        fn.called.should.be.false();
-        inverse.called.should.be.false();
-    });
+            fn.called.should.be.true();
+            inverse.called.should.be.false();
+        });
 
-    it('should handle author list that evaluates to true', function () {
-        var fn = sandbox.spy(),
-            inverse = sandbox.spy();
+        it('should handle tags and authors like an OR query (fail)', function () {
+            thisCtx = {author: {name: 'fred'}, tags: [{name: 'foo'}, {name: 'bar'}, {name: 'baz'}]};
 
-        helpers.has.call(
-            {author: {name: 'sam'}},
-            {hash: {author: 'joe, sam, pat'}, fn: fn, inverse: inverse}
-        );
+            callHasHelper(thisCtx, {author: 'joe, sam, pat', tag: 'much, such, wow'});
 
-        fn.called.should.be.true();
-        inverse.called.should.be.false();
-    });
-
-    it('should handle author list that evaluates to false', function () {
-        var fn = sandbox.spy(),
-            inverse = sandbox.spy();
-
-        helpers.has.call(
-            {author: {name: 'jamie'}},
-            {hash: {author: 'joe, sam, pat'}, fn: fn, inverse: inverse}
-        );
-
-        fn.called.should.be.false();
-        inverse.called.should.be.true();
-    });
-
-    it('should handle authors with case-insensitivity', function () {
-        var fn = sandbox.spy(),
-            inverse = sandbox.spy();
-
-        helpers.has.call(
-            {author: {name: 'Sam'}},
-            {hash: {author: 'joe, sAm, pat'}, fn: fn, inverse: inverse}
-        );
-
-        fn.called.should.be.true();
-        inverse.called.should.be.false();
-    });
-
-    it('should handle tags and authors like an OR query (pass)', function () {
-        var fn = sandbox.spy(),
-            inverse = sandbox.spy();
-
-        helpers.has.call(
-            {author: {name: 'sam'}, tags: [{name: 'foo'}, {name: 'bar'}, {name: 'baz'}]},
-            {hash: {author: 'joe, sam, pat', tag: 'much, such, wow'}, fn: fn, inverse: inverse}
-        );
-
-        fn.called.should.be.true();
-        inverse.called.should.be.false();
-    });
-
-    it('should handle tags and authors like an OR query (pass)', function () {
-        var fn = sandbox.spy(),
-            inverse = sandbox.spy();
-
-        helpers.has.call(
-            {author: {name: 'sam'}, tags: [{name: 'much'}, {name: 'bar'}, {name: 'baz'}]},
-            {hash: {author: 'joe, sam, pat', tag: 'much, such, wow'}, fn: fn, inverse: inverse}
-        );
-
-        fn.called.should.be.true();
-        inverse.called.should.be.false();
-    });
-
-    it('should handle tags and authors like an OR query (fail)', function () {
-        var fn = sandbox.spy(),
-            inverse = sandbox.spy();
-
-        helpers.has.call(
-            {author: {name: 'fred'}, tags: [{name: 'foo'}, {name: 'bar'}, {name: 'baz'}]},
-            {hash: {author: 'joe, sam, pat', tag: 'much, such, wow'}, fn: fn, inverse: inverse}
-        );
-
-        fn.called.should.be.false();
-        inverse.called.should.be.true();
+            fn.called.should.be.false();
+            inverse.called.should.be.true();
+        });
     });
 });
