3c60d4f60234d8984d17220ec29d1017ed7ab914
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-08-17 21:11:34 +0100

    return ValidationError for /mailchimp/lists if api key is invalid

--- core/server/api/mailchimp.js
+++ core/server/api/mailchimp.js
@@ -1,5 +1,6 @@
 var Mailchimp = require('mailchimp-api-v3'),
     settingsCache = require('../settings/cache'),
+    errors = require('../errors'),
     pipeline = require('../utils/pipeline'),
     _ = require('lodash'),
     mailchimp;
@@ -21,8 +22,15 @@ mailchimp = {
          * @return {[type]}         [description]
          */
         function doQuery(options) {
-            let apiKey = options.apiKey || settingsCache.get('mailchimp').apiKey;
-            let mailchimp = new Mailchimp(apiKey);
+            var apiKey, mailchimp;
+
+            if (options.apiKey !== null && options.apiKey !== undefined) {
+                apiKey = options.apiKey;
+            } else {
+                apiKey = settingsCache.get('mailchimp').apiKey;
+            }
+
+            mailchimp = new Mailchimp(apiKey);
 
             return mailchimp.get({
                 path: '/lists'
@@ -37,7 +45,7 @@ mailchimp = {
 
         // Pipeline calls each task passing the result of one to be the arguments for the next
         return pipeline(tasks, options).then(function formatResponse(result) {
-            // TODO: error handling
+            // remove details that the client doesn't need
             var lists = _.map(result.lists, function (list) {
                 return {
                     id: list.id,
@@ -48,6 +56,32 @@ mailchimp = {
             return {
                 lists: lists
             };
+        }).catch(function handleMailchimpError(error) {
+            // TODO: return a custom error type here?
+            if (error.title === 'API Key Invalid' || error.message.indexOf('invalid api key') > -1) {
+                throw new errors.ValidationError({
+                    code: 'MAILCHIMP',
+                    message: error.title || error.message,
+                    statusCode: 422,
+                    context: error.detail && error.detail + ' (' + error.type + ')'
+                });
+            }
+
+            // general catch-all for Mailchimp API errors
+            if (error.title) {
+                throw new errors.InternalServerError({
+                    code: 'MAILCHIMP',
+                    message: error.title,
+                    statusCode: error.status || 500,
+                    context: error.detail + ' (' + error.type + ')'
+                });
+            }
+
+            // TODO: keep this or just `throw error`?
+            throw new errors.InternalServerError({
+                code: 'MAILCHIMP',
+                message: error.message
+            });
         });
     }
 };
