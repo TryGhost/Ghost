e25543695bcd1528a27d91c458b84d121d35317e
Hannah Wolfe <erisds@gmail.com>
2017-08-27 19:06:53 +0200

    🐛 Downgraded errors to warnings for img_url
    refs #8703
    
    - Instead of throwing errors, throw warnings for incorrect usage of the img_url helper
    - Differentiate between no attribute passed, and attribute evaluating to undefined

--- core/test/unit/server_helpers/img_url_spec.js
+++ core/test/unit/server_helpers/img_url_spec.js
@@ -4,13 +4,19 @@ var should = require('should'), // jshint ignore:line
 
     // Stuff we are testing
     helpers = require('../../../server/helpers'),
-    errors = require('../../../server/errors'),
+    logging = require('../../../server/logging'),
 
     sandbox = sinon.sandbox.create();
 
 describe('{{image}} helper', function () {
+    var logWarnStub;
+
     before(function () {
         configUtils.set({url: 'http://localhost:82832/'});
+    });
+
+    beforeEach(function () {
+        logWarnStub = sandbox.stub(logging, 'warn');
     });
 
     afterEach(function () {
@@ -25,33 +31,39 @@ describe('{{image}} helper', function () {
         var rendered = helpers.img_url('/content/images/image-relative-url.png', {});
         should.exist(rendered);
         rendered.should.equal('/content/images/image-relative-url.png');
+        logWarnStub.called.should.be.false();
     });
 
     it('should output absolute url of image if the option is present ', function () {
         var rendered = helpers.img_url('/content/images/image-relative-url.png', {hash: {absolute: 'true'}});
         should.exist(rendered);
         rendered.should.equal('http://localhost:82832/content/images/image-relative-url.png');
+        logWarnStub.called.should.be.false();
     });
 
     it('should output author url', function () {
         var rendered = helpers.img_url('/content/images/author-image-relative-url.png', {});
         should.exist(rendered);
         rendered.should.equal('/content/images/author-image-relative-url.png');
+        logWarnStub.called.should.be.false();
     });
 
-    it('should have no output if there is no image ', function (done) {
-        try {
-            helpers.img_url(undefined, {hash: {absolute: 'true'}});
-            done(new Error('we expect an error from img_url'));
-        } catch (err) {
-            (err instanceof errors.IncorrectUsageError).should.eql(true);
-            done();
-        }
+    it('should have no output if the image attributeis not provided (with warning)', function () {
+        var rendered = helpers.img_url({hash: {absolute: 'true'}});
+        should.not.exist(rendered);
+        logWarnStub.calledOnce.should.be.true();
     });
 
-    it('should have no output if there is no image ', function () {
+    it('should have no output if the image attribute evaluates to undefined (with warning)', function () {
+        var rendered = helpers.img_url(undefined, {hash: {absolute: 'true'}});
+        should.not.exist(rendered);
+        logWarnStub.calledOnce.should.be.true();
+    });
+
+    it('should have no output if the image attribute evaluates to null (no waring)', function () {
         var rendered = helpers.img_url(null, {hash: {absolute: 'true'}});
         should.not.exist(rendered);
+        logWarnStub.calledOnce.should.be.false();
     });
 
     describe('with sub-directory', function () {
