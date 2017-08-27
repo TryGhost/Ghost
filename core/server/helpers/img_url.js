da9d4f9f2efacf0dc2c4f00aff9ff0f5e4b96771
Hannah Wolfe <erisds@gmail.com>
2017-08-27 19:06:53 +0200

    🐛 Downgraded errors to warnings for img_url
    refs #8703
    
    - Instead of throwing errors, throw warnings for incorrect usage of the img_url helper
    - Differentiate between no attribute passed, and attribute evaluating to undefined

--- core/server/helpers/img_url.js
+++ core/server/helpers/img_url.js
@@ -1,39 +1,39 @@
 
 // Usage:
-// `{{img_url}}` - does not work, argument is required
 // `{{img_url feature_image}}`
 // `{{img_url profile_image absolute="true"}}`
+// Note:
+// `{{img_url}}` - does not work, argument is required
 //
 // Returns the URL for the current object scope i.e. If inside a post scope will return image permalink
 // `absolute` flag outputs absolute URL, else URL is relative.
 
 var proxy = require('./proxy'),
-    errors = require('../errors'),
+    logging = require('../logging'),
     i18n = require('../i18n'),
     url = proxy.url;
 
 module.exports = function imgUrl(attr, options) {
-    var absolute;
-
-    // CASE: if you pass e.g. cover_image, but it is not set, then attr is null!
-    //       in this case we don't throw an error
-    if (!options) {
-        attr = undefined;
-        options = attr;
-    }
-
-    absolute = options && options.hash && options.hash.absolute;
-
-    if (attr === undefined) {
-        throw new errors.IncorrectUsageError({
-            message: i18n.t('warnings.helpers.img_url.attrIsRequired')
-        });
-    }
-
-    // CASE: property is not set in the model e.g. cover_image
-    if (attr === null) {
+    // CASE: if no attribute is passed, e.g. `{{img_url}}` we show a warning
+    if (arguments.length < 2) {
+        logging.warn(i18n.t('warnings.helpers.img_url.attrIsRequired'));
         return;
     }
 
-    return url.urlFor('image', {image: attr}, absolute);
+    var absolute = options && options.hash && options.hash.absolute;
+
+    // CASE: if attribute is passed, but it is undefined, then the attribute was
+    // an unknown value, e.g. {{img_url feature_img}} and we also show a warning
+    if (attr === undefined) {
+        logging.warn(i18n.t('warnings.helpers.img_url.attrIsRequired'));
+        return;
+    }
+
+    if (attr) {
+        return url.urlFor('image', {image: attr}, absolute);
+    } else {
+        // CASE: if you pass e.g. cover_image, but it is not set, then attr is null!
+        // in this case we don't show a warning
+        return;
+    }
 };
