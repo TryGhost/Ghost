23b48a9438fca264047a091bf3b359e4a25f32ad
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-08-30 13:31:04 +0100

    🐛 Fixed "active" state of apps on apps index screen (#843)
    no issue
    
    On the apps index screen there were conditionals for each app so that active apps show "Active" instead of "Configure" when they are activated - the conditionals weren't working because the properties they check for weren't available in the template's context.
    
    - add a new `settings/apps/index` controller that imports the `settings` service
    - updates template conditionals to check for properties on the `settings` service

--- ghost/admin/tests/acceptance/settings/apps-test.js
+++ ghost/admin/tests/acceptance/settings/apps-test.js
@@ -56,35 +56,53 @@ describe('Acceptance: Settings - Apps', function () {
             return authenticateSession(application);
         });
 
+        it('renders correctly', async function () {
+            await visit('/settings/apps');
+
+            // slack is not configured in the fixtures
+            expect(
+                find('[data-test-app="slack"] [data-test-app-status]').text().trim(),
+                'slack app status'
+            ).to.equal('Configure');
+
+            // amp is enabled in the fixtures
+            expect(
+                find('[data-test-app="amp"] [data-test-app-status]').text().trim(),
+                'amp app status'
+            ).to.equal('Active');
+        });
+
         it('it redirects to Slack when clicking on the grid', async function () {
             await visit('/settings/apps');
 
             // has correct url
             expect(currentURL(), 'currentURL').to.equal('/settings/apps');
 
-            await click('#slack-link');
+            await click('[data-test-link="slack"]');
 
             // has correct url
             expect(currentURL(), 'currentURL').to.equal('/settings/apps/slack');
         });
+
         it('it redirects to AMP when clicking on the grid', async function () {
             await visit('/settings/apps');
 
             // has correct url
             expect(currentURL(), 'currentURL').to.equal('/settings/apps');
 
-            await click('#amp-link');
+            await click('[data-test-link="amp"]');
 
             // has correct url
             expect(currentURL(), 'currentURL').to.equal('/settings/apps/amp');
         });
+
         it('it redirects to Unsplash when clicking on the grid', async function () {
             await visit('/settings/apps');
 
             // has correct url
             expect(currentURL(), 'currentURL').to.equal('/settings/apps');
 
-            await click('#unsplash-link');
+            await click('[data-test-link="unsplash"]');
 
             // has correct url
             expect(currentURL(), 'currentURL').to.equal('/settings/apps/unsplash');
