7d9c8e865673cfedf1adf65685affd36384e6ac8
Aileen Nowak <aileen@ghost.org>
2017-08-31 16:39:37 +0700

    Used `got` to handle requests for image-size (#8892)
    refs #8589, refs #8868
    
    - swap `request` with `got` in `getImageSizeFromUrl` util
    - less handling for request cases e.g. timeouts, follow redirects

--- core/server/utils/image-size-from-url.js
+++ core/server/utils/image-size-from-url.js
@@ -14,17 +14,14 @@
 // we add the protocol to the incomplete one and use urlFor() to get the absolute URL.
 // If the request fails or image-size is not able to read the file, we reject with error.
 
-var sizeOf       = require('image-size'),
-    url          = require('url'),
-    Promise      = require('bluebird'),
-    http         = require('http'),
-    https        = require('https'),
-    config       = require('../config'),
-    utils        = require('../utils'),
-    errors       = require('../errors'),
-    dimensions,
-    request,
-    requestHandler;
+var sizeOf = require('image-size'),
+    url = require('url'),
+    Promise = require('bluebird'),
+    got = require('got'),
+    config = require('../config'),
+    utils = require('../utils'),
+    errors = require('../errors'),
+    dimensions;
 
 /**
  * @description read image dimensions from URL
@@ -32,90 +29,75 @@ var sizeOf       = require('image-size'),
  * @returns {Promise<Object>} imageObject or error
  */
 module.exports.getImageSizeFromUrl = function getImageSizeFromUrl(imagePath) {
-    return new Promise(function imageSizeRequest(resolve, reject) {
-        var imageObject = {},
-            options,
-            timeout = config.get('times:getImageSizeTimeoutInMS') || 10000;
+    var imageObject = {},
+        requestOptions,
+        timeout = config.get('times:getImageSizeTimeoutInMS') || 10000;
 
-        imageObject.url = imagePath;
+    imageObject.url = imagePath;
 
-        // check if we got an url without any protocol
-        if (imagePath.indexOf('http') === -1) {
-            // our gravatar urls start with '//' in that case add 'http:'
-            if (imagePath.indexOf('//') === 0) {
-                // it's a gravatar url
-                imagePath = 'http:' + imagePath;
-            } else {
-                // get absolute url for image
-                imagePath = utils.url.urlFor('image', {image: imagePath}, true);
-            }
+    // check if we got an url without any protocol
+    if (imagePath.indexOf('http') === -1) {
+        // our gravatar urls start with '//' in that case add 'http:'
+        if (imagePath.indexOf('//') === 0) {
+            // it's a gravatar url
+            imagePath = 'http:' + imagePath;
+        } else {
+            // get absolute url for image
+            imagePath = utils.url.urlFor('image', {image: imagePath}, true);
         }
+    }
 
-        options = url.parse(imagePath);
+    imagePath = url.parse(imagePath);
+    requestOptions = {
+        headers: {
+            'User-Agent': 'Mozilla/5.0'
+        },
+        timeout: timeout,
+        encoding: null
+    };
 
-        requestHandler = imagePath.indexOf('https') === 0 ? https : http;
-        options.headers = {'User-Agent': 'Mozilla/5.0'};
+    return got(
+        imagePath,
+        requestOptions
+    ).then(function (response) {
+        try {
+            // response.body contains the Buffer. Using the Buffer rather than an URL
+            // requires to use sizeOf synchronously. See https://github.com/image-size/image-size#asynchronous
+            dimensions = sizeOf(response.body);
 
-        request = requestHandler.get(options, function (res) {
-            var chunks = [];
+            imageObject.width = dimensions.width;
+            imageObject.height = dimensions.height;
 
-            res.on('data', function (chunk) {
-                chunks.push(chunk);
-            });
-
-            res.on('end', function () {
-                if (res.statusCode === 200) {
-                    try {
-                        dimensions = sizeOf(Buffer.concat(chunks));
-
-                        imageObject.width = dimensions.width;
-                        imageObject.height = dimensions.height;
-
-                        return resolve(imageObject);
-                    } catch (err) {
-                        return reject(new errors.InternalServerError({
-                            code: 'IMAGE_SIZE',
-                            err: err,
-                            context: imagePath
-                        }));
-                    }
-                } else if (res.statusCode === 404) {
-                    return reject(new errors.NotFoundError({
-                        message: 'Image not found.',
-                        code: 'IMAGE_SIZE',
-                        statusCode: res.statusCode,
-                        context: imagePath
-                    }));
-                } else {
-                    return reject(new errors.InternalServerError({
-                        message: 'Unknown Request error.',
-                        code: 'IMAGE_SIZE',
-                        statusCode: res.statusCode,
-                        context: imagePath
-                    }));
-                }
-            });
-        }).on('socket', function (socket) {
-            if (timeout) {
-                socket.setTimeout(timeout);
-
-                /**
-                 * https://nodejs.org/api/http.html
-                 * "...if a callback is assigned to the Server's 'timeout' event, timeouts must be handled explicitly"
-                 *
-                 * socket.destroy will jump to the error listener
-                 */
-                socket.on('timeout', function () {
-                    request.abort();
-                    socket.destroy(new Error('Request timed out.'));
-                });
-            }
-        }).on('error', function (err) {
-            return reject(new errors.InternalServerError({
+            return Promise.resolve(imageObject);
+        } catch (err) {
+            return Promise.reject(new errors.InternalServerError({
                 code: 'IMAGE_SIZE',
                 err: err,
-                context: imagePath
+                context: imagePath.href
             }));
-        });
+        }
+    }).catch(function (err) {
+        if (err.statusCode === 404) {
+            return Promise.reject(new errors.NotFoundError({
+                message: 'Image not found.',
+                code: 'IMAGE_SIZE',
+                statusCode: err.statusCode,
+                context: err.url || imagePath.href || imagePath
+            }));
+        } else if (err.code === 'ETIMEDOUT') {
+            return Promise.reject(new errors.InternalServerError({
+                message: 'Request timed out.',
+                code: 'IMAGE_SIZE',
+                statusCode: err.statusCode,
+                context: err.url || imagePath.href || imagePath
+            }));
+        } else {
+            return Promise.reject(new errors.InternalServerError({
+                message: 'Unknown Request error.',
+                code: 'IMAGE_SIZE',
+                statusCode: err.statusCode,
+                context: err.url || imagePath.href || imagePath
+            }));
+        }
     });
 };
--- core/test/unit/utils/image-size-from-url_spec.js
+++ core/test/unit/utils/image-size-from-url_spec.js
@@ -1,6 +1,5 @@
 var should = require('should'),
     sinon = require('sinon'),
-    Promise = require('bluebird'),
     rewire = require('rewire'),
     nock = require('nock'),
     configUtils = require('../../utils/configUtils'),
@@ -14,7 +13,8 @@ var should = require('should'),
 describe('Image Size', function () {
     var sizeOfStub,
         result,
-        requestMock;
+        requestMock,
+        secondRequestMock;
 
     beforeEach(function () {
         sizeOfStub = sandbox.stub();
@@ -23,6 +23,7 @@ describe('Image Size', function () {
     afterEach(function () {
         sandbox.restore();
         configUtils.restore();
+        imageSize = rewire('../../../server/utils/image-size-from-url');
     });
 
     it('should have an image size function', function () {
@@ -45,7 +46,7 @@ describe('Image Size', function () {
         sizeOfStub.returns({width: 50, height: 50, type: 'jpg'});
         imageSize.__set__('sizeOf', sizeOfStub);
 
-        result = Promise.resolve(imageSize.getImageSizeFromUrl(url)).then(function (res) {
+        result = imageSize.getImageSizeFromUrl(url).then(function (res) {
             requestMock.isDone().should.be.true();
             should.exist(res);
             should.exist(res.width);
@@ -69,13 +70,13 @@ describe('Image Size', function () {
         requestMock = nock('https://static.wixstatic.com')
             .get('/media/355241_d31358572a2542c5a44738ddcb59e7ea.jpg_256')
             .reply(200, {
-                data: '<Buffer 2c be a4 40 f7 87 73 1e 57 2c c1 e4 0d 79 03 95 42 f0 42 2e 41 95 27 c9 5c 35 a7 71 2c 09 5a 57 d3 04 1e 83 03 28 07 96 b0 c8 88 65 07 7a d1 d6 63 50>'
+                body: '<Buffer 2c be a4 40 f7 87 73 1e 57 2c c1 e4 0d 79 03 95 42 f0 42 2e 41 95 27 c9 5c 35 a7 71 2c 09 5a 57 d3 04 1e 83 03 28 07 96 b0 c8 88 65 07 7a d1 d6 63 50>'
             });
 
         sizeOfStub.returns({width: 256, height: 256, type: 'png'});
         imageSize.__set__('sizeOf', sizeOfStub);
 
-        result = Promise.resolve(imageSize.getImageSizeFromUrl(url)).then(function (res) {
+        result = imageSize.getImageSizeFromUrl(url).then(function (res) {
             requestMock.isDone().should.be.true();
             should.exist(res);
             should.exist(res.width);
@@ -99,13 +100,13 @@ describe('Image Size', function () {
         requestMock = nock('http://www.gravatar.com')
             .get('/avatar/ef6dcde5c99bb8f685dd451ccc3e050a?s=250&d=mm&r=x')
             .reply(200, {
-                data: '<Buffer 2c be a4 40 f7 87 73 1e 57 2c c1 e4 0d 79 03 95 42 f0 42 2e 41 95 27 c9 5c 35 a7 71 2c 09 5a 57 d3 04 1e 83 03 28 07 96 b0 c8 88 65 07 7a d1 d6 63 50>'
+                body: '<Buffer 2c be a4 40 f7 87 73 1e 57 2c c1 e4 0d 79 03 95 42 f0 42 2e 41 95 27 c9 5c 35 a7 71 2c 09 5a 57 d3 04 1e 83 03 28 07 96 b0 c8 88 65 07 7a d1 d6 63 50>'
             });
 
         sizeOfStub.returns({width: 250, height: 250, type: 'jpg'});
         imageSize.__set__('sizeOf', sizeOfStub);
 
-        result = Promise.resolve(imageSize.getImageSizeFromUrl(url)).then(function (res) {
+        result = imageSize.getImageSizeFromUrl(url).then(function (res) {
             requestMock.isDone().should.be.true();
             should.exist(res);
             should.exist(res.width);
@@ -134,13 +135,13 @@ describe('Image Size', function () {
         requestMock = nock('http://myblog.com')
             .get('/content/images/cat.jpg')
             .reply(200, {
-                data: '<Buffer 2c be a4 40 f7 87 73 1e 57 2c c1 e4 0d 79 03 95 42 f0 42 2e 41 95 27 c9 5c 35 a7 71 2c 09 5a 57 d3 04 1e 83 03 28 07 96 b0 c8 88 65 07 7a d1 d6 63 50>'
+                body: '<Buffer 2c be a4 40 f7 87 73 1e 57 2c c1 e4 0d 79 03 95 42 f0 42 2e 41 95 27 c9 5c 35 a7 71 2c 09 5a 57 d3 04 1e 83 03 28 07 96 b0 c8 88 65 07 7a d1 d6 63 50>'
             });
 
         sizeOfStub.returns({width: 100, height: 100, type: 'jpg'});
         imageSize.__set__('sizeOf', sizeOfStub);
 
-        result = Promise.resolve(imageSize.getImageSizeFromUrl(url)).then(function (res) {
+        result = imageSize.getImageSizeFromUrl(url).then(function (res) {
             requestMock.isDone().should.be.true();
             should.exist(res);
             should.exist(res.width);
@@ -160,12 +161,53 @@ describe('Image Size', function () {
             .get('/files/f/feedough/x/11/1540353_20925115.jpg')
             .reply(404);
 
-        result = Promise.resolve(imageSize.getImageSizeFromUrl(url))
+        result = imageSize.getImageSizeFromUrl(url)
             .catch(function (err) {
                 requestMock.isDone().should.be.true();
                 should.exist(err);
                 done();
             });
+    });
+
+    it('can handle redirect', function (done) {
+        var url = 'http://noimagehere.com/files/f/feedough/x/11/1540353_20925115.jpg',
+            expectedImageObject =
+            {
+                height: 100,
+                url: 'http://noimagehere.com/files/f/feedough/x/11/1540353_20925115.jpg',
+                width: 100
+            };
+
+        requestMock = nock('http://noimagehere.com')
+            .get('/files/f/feedough/x/11/1540353_20925115.jpg')
+            .reply(301, {
+                body: '<Buffer 2c be a4 40 f7 87 73 1e 57 2c c1 e4 0d 79 03 95 42 f0 42 2e 41 95 27 c9 5c 35 a7 71 2c 09 5a 57 d3 04 1e 83 03 28 07 96 b0 c8 88 65 07 7a d1 d6 63 50>'
+            },
+            {
+                location: 'http://someredirectedurl.com/files/f/feedough/x/11/1540353_20925115.jpg'
+            });
+
+        secondRequestMock = nock('http://someredirectedurl.com')
+            .get('/files/f/feedough/x/11/1540353_20925115.jpg')
+            .reply(200, {
+                body: '<Buffer 2c be a4 40 f7 87 73 1e 57 2c c1 e4 0d 79 03 95 42 f0 42 2e 41 95 27 c9 5c 35 a7 71 2c 09 5a 57 d3 04 1e 83 03 28 07 96 b0 c8 88 65 07 7a d1 d6 63 50>'
+            });
+
+        sizeOfStub.returns({width: 100, height: 100, type: 'jpg'});
+        imageSize.__set__('sizeOf', sizeOfStub);
+
+        result = imageSize.getImageSizeFromUrl(url).then(function (res) {
+            requestMock.isDone().should.be.true();
+            secondRequestMock.isDone().should.be.true();
+            should.exist(res);
+            should.exist(res.width);
+            res.width.should.be.equal(expectedImageObject.width);
+            should.exist(res.height);
+            res.height.should.be.equal(expectedImageObject.height);
+            should.exist(res.url);
+            res.url.should.be.equal(expectedImageObject.url);
+            done();
+        }).catch(done);
     });
 
     it('will timeout', function (done) {
@@ -176,7 +218,7 @@ describe('Image Size', function () {
             .reply(408);
 
         configUtils.set('times:getImageSizeTimeoutInMS', 10);
-        result = Promise.resolve(imageSize.getImageSizeFromUrl(url))
+        result = imageSize.getImageSizeFromUrl(url)
             .catch(function (err) {
                 requestMock.isDone().should.be.true();
                 should.exist(err);
@@ -190,13 +232,13 @@ describe('Image Size', function () {
             requestMock = nock('https://static.wixstatic.com')
                 .get('/media/355241_d31358572a2542c5a44738ddcb59e7ea.jpg_256')
                 .reply(200, {
-                    data: '<Buffer 2c be a4 40 f7 87 73 1e 57 2c c1 e4 0d 79 03 95 42 f0 42 2e 41 95 27 c9 5c 35 a7 71 2c 09 5a 57 d3 04 1e 83 03 28 07 96 b0 c8 88 65 07 7a d1 d6 63 50>'
+                    body: '<Buffer 2c be a4 40 f7 87 73 1e 57 2c c1 e4 0d 79 03 95 42 f0 42 2e 41 95 27 c9 5c 35 a7 71 2c 09 5a 57 d3 04 1e 83 03 28 07 96 b0 c8 88 65 07 7a d1 d6 63 50>'
                 });
 
         sizeOfStub.throws({error: 'image-size could not find dimensions'});
         imageSize.__set__('sizeOf', sizeOfStub);
 
-        result = Promise.resolve(imageSize.getImageSizeFromUrl(url))
+        result = imageSize.getImageSizeFromUrl(url)
             .catch(function (err) {
                 requestMock.isDone().should.be.true();
                 should.exist(err);
@@ -211,7 +253,9 @@ describe('Image Size', function () {
                 .get('/images/notapicture.jpg')
                 .replyWithError({message: 'something awful happened', code: 'AWFUL_ERROR'});
 
-        result = Promise.resolve(imageSize.getImageSizeFromUrl(url))
+        configUtils.set('times:getImageSizeTimeoutInMS', 5);
+
+        result = imageSize.getImageSizeFromUrl(url)
             .catch(function (err) {
                 requestMock.isDone().should.be.true();
                 should.exist(err);
