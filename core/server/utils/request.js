d37b3dc351560a58e00f9bc47958426fc00b0650
Aileen Nowak <aileen@ghost.org>
2017-09-07 18:17:24 +0700

    ✨  Request util to wrap `got` library (#8980)
    no issue
    
    This PR includes a new util which wraps the `got` library. It is not used in the codebase yet, but tested with `image-size` util:
    - wraps `got` request library in its own `request.js` util that returns bluebird promises and validates URL before starting a request
    - adds tests

---
+++ core/server/utils/request.js
@@ -0,0 +1,24 @@
+var got = require('got'),
+    Promise = require('bluebird'),
+    validator = require('../data/validation').validator,
+    errors = require('../errors'),
+    _  = require('lodash');
+
+module.exports = function request(url, options) {
+    if (_.isEmpty(url) || !validator.isURL(url)) {
+        return Promise.reject(new errors.InternalServerError({
+            message: 'URL empty or invalid.',
+            code: 'URL_MISSING_INVALID',
+            context: url
+        }));
+    }
+
+    return got(
+        url,
+        options
+    ).then(function (response) {
+        return Promise.resolve(response);
+    }).catch(function (err) {
+        return Promise.reject(err);
+    });
+};
