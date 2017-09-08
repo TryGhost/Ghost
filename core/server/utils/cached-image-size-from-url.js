299f65738a8c0e5ec5520f399b5f366f4c5183c2
Aileen Nowak <AileenCGN@gmail.com>
2017-09-08 11:52:23 +0700

    🔥  Remove not needed `Promise.resolve()`
    We're always resolving the result in `getCachedImageSizeFromUrl`, so there's no need to return the values with a `Promise.resolve()`. The caller fn uses waits for the Promises to be fulfilled.

--- core/server/utils/cached-image-size-from-url.js
+++ core/server/utils/cached-image-size-from-url.js
@@ -1,5 +1,4 @@
 var debug = require('ghost-ignition').debug('utils:image-size-cache'),
-    Promise = require('bluebird'),
     imageSize = require('./image-size'),
     logging = require('../logging'),
     errors = require('../errors'),
@@ -25,22 +24,26 @@ function getCachedImageSizeFromUrl(url) {
 
             debug('Cached image:', url);
 
-            return Promise.resolve(imageSizeCache[url]);
-        }).catch(errors.NotFoundError, {code: 'ENOENT'}, function () {
+            return imageSizeCache[url];
+        }).catch(errors.NotFoundError, function () {
             debug('Cached image (not found):', url);
             // in case of error we just attach the url
-            return Promise.resolve(imageSizeCache[url] = url);
+            imageSizeCache[url] = url;
+
+            return imageSizeCache[url];
         }).catch(function (err) {
             debug('Cached image (error):', url);
             logging.error(err);
 
             // in case of error we just attach the url
-            return Promise.resolve(imageSizeCache[url] = url);
+            imageSizeCache[url] = url;
+
+            return imageSizeCache[url];
         });
     }
     debug('Read image from cache:', url);
     // returns image size from cache
-    return Promise.resolve(imageSizeCache[url]);
+    return imageSizeCache[url];
 }
 
 module.exports = getCachedImageSizeFromUrl;
