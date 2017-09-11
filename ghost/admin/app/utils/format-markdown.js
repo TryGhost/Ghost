8362f0cdf0a9f225947346e4c54805aac13e6e37
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-09-11 04:39:22 +0100

    Remove unwanted String.prototype extensions (#856)
    closes https://github.com/TryGhost/Ghost/issues/8958
    
    - `markdown-it-named-headers` pulled in `String.js` despite it not being used which added a lot of String.prototype extensions and caused Ember deprecation notices
    - moves the short `markdown-it-named-headers` functionality directly into our app code without using `String.js`

--- ghost/admin/app/utils/format-markdown.js
+++ ghost/admin/app/utils/format-markdown.js
@@ -4,7 +4,41 @@ import markdownit from 'npm:markdown-it';
 import markdownitFootnote from 'npm:markdown-it-footnote';
 import markdownitLazyHeaders from 'npm:markdown-it-lazy-headers';
 import markdownitMark from 'npm:markdown-it-mark';
-import markdownitNamedHeaders from 'npm:markdown-it-named-headers';
+
+let slugify = function slugify(inputString, usedHeaders) {
+    let slug = inputString.replace(/[^\w]/g, '').toLowerCase();
+    if (usedHeaders[slug]) {
+        usedHeaders[slug]++;
+        slug += usedHeaders[slug];
+    }
+    return slug;
+};
+
+// originally from https://github.com/leff/markdown-it-named-headers
+// moved here to avoid pulling in http://stringjs.com dependency
+let markdownitNamedHeaders = function markdownitNamedHeaders(md) {
+    let originalHeadingOpen = md.renderer.rules.heading_open;
+
+    // eslint-disable-next-line camelcase
+    md.renderer.rules.heading_open = function (tokens, idx, options, env, self) {
+        let usedHeaders = {};
+
+        tokens[idx].attrs = tokens[idx].attrs || [];
+
+        let title = tokens[idx + 1].children.reduce(function (acc, t) {
+            return acc + t.content;
+        }, '');
+
+        let slug = slugify(title, usedHeaders);
+        tokens[idx].attrs.push(['id', slug]);
+
+        if (originalHeadingOpen) {
+            return originalHeadingOpen.apply(this, arguments);
+        } else {
+            return self.renderToken(...arguments);
+        }
+    };
+};
 
 // eslint-disable-next-line new-cap
 let md = markdownit({
@@ -15,17 +49,7 @@ let md = markdownit({
 .use(markdownitFootnote)
 .use(markdownitLazyHeaders)
 .use(markdownitMark)
-.use(markdownitNamedHeaders, {
-    // match legacy Showdown IDs otherwise default is github style dasherized
-    slugify(inputString, usedHeaders) {
-        let slug = inputString.replace(/[^\w]/g, '').toLowerCase();
-        if (usedHeaders[slug]) {
-            usedHeaders[slug]++;
-            slug += usedHeaders[slug];
-        }
-        return slug;
-    }
-});
+.use(markdownitNamedHeaders);
 
 export default function formatMarkdown(_markdown, replaceJS = true) {
     let markdown = _markdown || '';
