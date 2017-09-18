333eddc5570a6c1f9144786d4567cd2ad1c3ad09
Katharina Irrgang <katharina.irrgang@googlemail.com>
2017-09-18 14:01:58 +0200

    Removed defunct Ghost OAuth code (#9014)
    closes #8342
    
    - no need to add a migration, because when we'released 1.0, OAuth was never an option
    - it was disabled in April, 1.0-beta was released in June
    - remove all remote authentication code

--- core/server/auth/index.js
+++ core/server/auth/index.js
@@ -1,23 +1,13 @@
 var passport = require('./passport'),
     authorize = require('./authorize'),
     authenticate = require('./authenticate'),
-    sync = require('./sync'),
-    oauth = require('./oauth'),
-    validation = require('./validation'),
-    ghostAuth = require('./ghost-auth');
+    oauth = require('./oauth');
 
 exports.init = function (options) {
     oauth.init(options);
-    sync.init(options);
-
-    return passport.init(options)
-        .then(function (response) {
-            return {auth: response.passport};
-        });
+    return passport.init(options);
 };
 
-exports.validation = validation;
 exports.oauth = oauth;
 exports.authorize = authorize;
 exports.authenticate = authenticate;
-exports.ghostAuth = ghostAuth;
