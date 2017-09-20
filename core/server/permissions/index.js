6a497e2c9618252a2d5bc48f3b348d589ea60d17
Hannah Wolfe <erisds@gmail.com>
2017-09-20 15:41:16 +0100

    Split public funtions out to own file
    - Making tests easier and coverage clearer

--- core/server/permissions/index.js
+++ core/server/permissions/index.js
@@ -23,61 +23,6 @@ function hasActionsMap() {
     });
 }
 
-function applyStatusRules(docName, method, opts) {
-    var err = new errors.NoPermissionError({message: i18n.t('errors.permissions.applyStatusRules.error', {docName: docName})});
-
-    // Enforce status 'active' for users
-    if (docName === 'users') {
-        if (!opts.status) {
-            return 'all';
-        }
-    }
-
-    // Enforce status 'published' for posts
-    if (docName === 'posts') {
-        if (!opts.status) {
-            return 'published';
-        } else if (
-            method === 'read'
-            && (opts.status === 'draft' || opts.status === 'all')
-            && _.isString(opts.uuid) && _.isUndefined(opts.id) && _.isUndefined(opts.slug)
-        ) {
-            // public read requests can retrieve a draft, but only by UUID
-            return opts.status;
-        } else if (opts.status !== 'published') {
-            // any other parameter would make this a permissions error
-            throw err;
-        }
-    }
-
-    return opts.status;
-}
-
-/**
- * API Public Permission Rules
- * This method enforces the rules for public requests
- * @param {String} docName
- * @param {String} method (read || browse)
- * @param {Object} options
- * @returns {Object} options
- */
-function applyPublicRules(docName, method, options) {
-    try {
-        // If this is a public context
-        if (parseContext(options.context).public === true) {
-            if (method === 'browse') {
-                options.status = applyStatusRules(docName, method, options);
-            } else if (method === 'read') {
-                options.data.status = applyStatusRules(docName, method, options.data);
-            }
-        }
-
-        return Promise.resolve(options);
-    } catch (err) {
-        return Promise.reject(err);
-    }
-}
-
 // Base class for canThis call results
 CanThisResult = function () {
     return;
@@ -283,6 +228,6 @@ module.exports = exported = {
     canThis: canThis,
     // @TODO: Make it so that we don't need to export these
     parseContext: parseContext,
-    applyPublicRules: applyPublicRules,
+    applyPublicRules: require('./public'),
     actionsMap: {}
 };
