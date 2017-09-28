bf978566c4c40975b6abf93044d04af40e98c66b
kirrg001 <katharina.irrgang@googlemail.com>
2017-09-28 11:32:34 +0200

    Reduce length of custom template to 100
    A file which is 100 characters long, looks like
    
    custom-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -32,7 +32,7 @@ module.exports = {
         twitter_image: {type: 'string', maxlength: 2000, nullable: true},
         twitter_title: {type: 'string', maxlength: 300, nullable: true},
         twitter_description: {type: 'string', maxlength: 500, nullable: true},
-        custom_template: {type: 'string', maxlength: 150, nullable: true}
+        custom_template: {type: 'string', maxlength: 100, nullable: true}
     },
     users: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
