83050d768979edeee0ca47f7b7a92a42c60131ed
Kevin Ansfield <kevin@lookingsideways.co.uk>
2017-10-03 05:54:24 +0100

    🐛 Fixed error when navigating to tag from admin search box (#881)
    closes https://github.com/TryGhost/Ghost/issues/9074
    
    - remove unused pagination route attrs that were interfering with routing service - these were a pre-1.0 hangover from when the tags screen had infinite scroll
    - change `Posts` to `Stories` in the search dropdown for consistency
    - add general fix for `adapter returned an array for the primary data of a 'queryRecord' response` deprecations by ensuring that `normalizeResponse` in our application serializer transforms the array we get from the server into a "single" JSON-API response
      - this deprecation was being triggered when loading a tag route directly
    - fix `defaultValue` complex object deprecations raised from `Setting.unsplash` attr

--- ghost/admin/app/serializers/tag.js
+++ ghost/admin/app/serializers/tag.js
@@ -20,5 +20,20 @@ export default ApplicationSerializer.extend({
         delete data.count;
 
         hash[root] = [data];
+    },
+
+    // if we use `queryRecord` ensure we grab the first record to avoid
+    // DS.SERIALIZER.REST.QUERYRECORD-ARRAY-RESPONSE deprecations
+    normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+        if (requestType === 'queryRecord') {
+            let singular = primaryModelClass.modelName;
+            let plural = pluralize(singular);
+
+            if (payload[plural]) {
+                payload[singular] = payload[plural][0];
+                delete payload[plural];
+            }
+        }
+        return this._super(...arguments);
     }
 });
