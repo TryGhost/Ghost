f16d43d52c5a1a34061e120cef16a0e668e68f9c
Katharina Irrgang <katharina.irrgang@googlemail.com>
2017-10-10 14:36:35 +0200

    ✨  Custom post templates (#9073)
    closes #9060
    
    - Update `gscan` - it now extracts custom templates and exposes them to Ghost
    - Add `custom_template` field to post schema w/ 1.13 migration
    - Return `templates` array for the active theme in `/themes/` requests
    - Users with Author/Editor roles can now request `/themes/`
    - Front-end will render `custom_template` for posts if it exists, template priority is now:
      1. `post/page-{{slug}}.hbs`
      2. `{{custom_template}}.hbs`
      3. `post/page.hbs`

--- core/test/unit/migration_spec.js
+++ core/test/unit/migration_spec.js
@@ -19,8 +19,8 @@ var should = require('should'), // jshint ignore:line
 // both of which are required for migrations to work properly.
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    var currentSchemaHash = 'af4028653a7c0804f6bf7b98c50db5dc',
-        currentFixturesHash = '00e9b37f49b8eed5591ec2d381afb9e3';
+    var currentSchemaHash = '0de1eaa8bc79046a9f43927917c294c3',
+        currentFixturesHash = '7c8b21872959945c22ed5780f513db67';
 
     // If this test is failing, then it is likely a change has been made that requires a DB version bump,
     // and the values above will need updating as confirmation
