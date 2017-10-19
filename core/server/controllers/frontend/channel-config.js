2c249b2d93c72f272e037eb4aba968f109f84359
Hannah Wolfe <erisds@gmail.com>
2017-10-19 15:02:14 +0100

    Move channel config to be JSON based

--- core/server/controllers/frontend/channel-config.js
+++ core/server/controllers/frontend/channel-config.js
@@ -1,54 +1,12 @@
 var _ = require('lodash'),
-    channelConfig;
+    channelConfig = require('./config.channels.json');
 
-channelConfig = function channelConfig() {
-    var defaults = {
-        index: {
-            name: 'index',
-            route: '/',
-            frontPageTemplate: 'home'
-        },
-        tag: {
-            name: 'tag',
-            route: '/:rkw-tag/:slug/',
-            postOptions: {
-                filter: 'tags:\'%s\'+tags.visibility:public'
-            },
-            data: {
-                tag: {
-                    type: 'read',
-                    resource: 'tags',
-                    options: {slug: '%s', visibility: 'public'}
-                }
-            },
-            slugTemplate: true,
-            editRedirect: '#/settings/tags/:slug/'
-        },
-        author: {
-            name: 'author',
-            route: '/:rkw-author/:slug/',
-            postOptions: {
-                filter: 'author:\'%s\''
-            },
-            data: {
-                author: {
-                    type: 'read',
-                    resource: 'users',
-                    options: {slug: '%s'}
-                }
-            },
-            slugTemplate: true,
-            editRedirect: '#/team/:slug/'
-        }
-    };
-
-    return defaults;
-};
-
+// @TODO expand this to support loading channels from different places?
 module.exports.list = function list() {
-    return channelConfig();
+    return channelConfig;
 };
 
+// @TODO refactor this away, it is current only used by tests
 module.exports.get = function get(name) {
-    return _.cloneDeep(channelConfig()[name]);
+    return _.cloneDeep(channelConfig[name]);
 };
