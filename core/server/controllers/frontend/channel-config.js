08f2793f3af4f1d627cb1b81cefc9c5e3df4cd33
Hannah Wolfe <erisds@gmail.com>
2017-10-22 14:59:52 +0100

    channelConfig.get can be removed now

--- core/server/controllers/frontend/channel-config.js
+++ core/server/controllers/frontend/channel-config.js
@@ -13,8 +13,3 @@ module.exports.list = function list() {
 
     return channelConfig;
 };
-
-// @TODO refactor this away, it is current only used by tests
-module.exports.get = function get(name) {
-    return _.cloneDeep(channelConfig[name]);
-};
