f3f544a6513ed40053be3aa2a44cc362c7900f6b
Hannah Wolfe <erisds@gmail.com>
2017-10-29 16:26:27 +0000

    Switch api.read usage for settingsCache

--- core/server/services/apps/index.js
+++ core/server/services/apps/index.js
@@ -5,95 +5,70 @@ var _ = require('lodash'),
     api = require('../../api'),
     i18n = require('../../i18n'),
     config = require('../../config'),
+    settingsCache = require('../../settings/cache'),
     loader = require('./loader'),
     // Holds the available apps
     availableApps = {};
 
 function getInstalledApps() {
-    return api.settings.read({context: {internal: true}, key: 'installed_apps'}).then(function (response) {
-        var installed = response.settings[0];
+    var installedApps = settingsCache.get('installed_apps'),
+        internalApps = config.get('apps:internal');
 
-        installed.value = installed.value || '[]';
-
-        try {
-            installed = JSON.parse(installed.value);
-        } catch (e) {
-            return Promise.reject(e);
-        }
-
-        return installed.concat(config.get('apps:internal'));
-    });
+    return installedApps.concat(internalApps);
 }
 
 function saveInstalledApps(installedApps) {
-    return getInstalledApps().then(function (currentInstalledApps) {
-        var updatedAppsInstalled = _.difference(_.uniq(installedApps.concat(currentInstalledApps)), config.get('apps:internal'));
+    var currentInstalledApps = getInstalledApps(),
+        updatedAppsInstalled = _.difference(_.uniq(installedApps.concat(currentInstalledApps)), config.get('apps:internal'));
 
-        return api.settings.edit({settings: [{key: 'installed_apps', value: updatedAppsInstalled}]}, {context: {internal: true}});
-    });
+    return api.settings.edit({settings: [{key: 'installed_apps', value: updatedAppsInstalled}]}, {context: {internal: true}});
 }
 
 module.exports = {
     init: function () {
-        var appsToLoad;
+        var activeApps = settingsCache.get('active_apps'),
+            installedApps = getInstalledApps(),
+            internalApps = config.get('apps:internal'),
+            appsToLoad = activeApps.concat(internalApps),
+            loadedApps = {},
+            loadPromises;
 
-        try {
-            // We have to parse the value because it's a string
-            api.settings.read({context: {internal: true}, key: 'active_apps'}).then(function (response) {
-                var aApps = response.settings[0];
+        function recordLoadedApp(name, loadedApp) {
+            // After loading the app, add it to our hash of loaded apps
+            loadedApps[name] = loadedApp;
 
-                appsToLoad = JSON.parse(aApps.value) || [];
-
-                appsToLoad = appsToLoad.concat(config.get('apps:internal'));
-            });
-        } catch (err) {
-            logging.error(new errors.GhostError({
-                err: err,
-                context: i18n.t('errors.apps.failedToParseActiveAppsSettings.context'),
-                help: i18n.t('errors.apps.failedToParseActiveAppsSettings.help')
-            }));
-
-            return Promise.resolve();
+            return Promise.resolve(loadedApp);
         }
 
         // Grab all installed apps, install any not already installed that are in appsToLoad.
-        return getInstalledApps().then(function (installedApps) {
-            var loadedApps = {},
-                recordLoadedApp = function (name, loadedApp) {
-                    // After loading the app, add it to our hash of loaded apps
-                    loadedApps[name] = loadedApp;
-
-                    return Promise.resolve(loadedApp);
-                },
-                loadPromises = _.map(appsToLoad, function (app) {
-                    // If already installed, just activate the app
-                    if (_.includes(installedApps, app)) {
-                        return loader.activateAppByName(app).then(function (loadedApp) {
-                            return recordLoadedApp(app, loadedApp);
-                        });
-                    }
-
-                    // Install, then activate the app
-                    return loader.installAppByName(app).then(function () {
-                        return loader.activateAppByName(app);
-                    }).then(function (loadedApp) {
-                        return recordLoadedApp(app, loadedApp);
-                    });
+        loadPromises = _.map(appsToLoad, function (app) {
+            // If already installed, just activate the app
+            if (_.includes(installedApps, app)) {
+                return loader.activateAppByName(app).then(function (loadedApp) {
+                    return recordLoadedApp(app, loadedApp);
                 });
+            }
 
-            return Promise.all(loadPromises).then(function () {
-                // Save our installed apps to settings
-                return saveInstalledApps(_.keys(loadedApps));
-            }).then(function () {
-                // Extend the loadedApps onto the available apps
-                _.extend(availableApps, loadedApps);
-            }).catch(function (err) {
-                logging.error(new errors.GhostError({
-                    err: err,
-                    context: i18n.t('errors.apps.appWillNotBeLoaded.error'),
-                    help: i18n.t('errors.apps.appWillNotBeLoaded.help')
-                }));
+            // Install, then activate the app
+            return loader.installAppByName(app).then(function () {
+                return loader.activateAppByName(app);
+            }).then(function (loadedApp) {
+                return recordLoadedApp(app, loadedApp);
             });
+        });
+
+        return Promise.all(loadPromises).then(function () {
+            // Save our installed apps to settings
+            return saveInstalledApps(_.keys(loadedApps));
+        }).then(function () {
+            // Extend the loadedApps onto the available apps
+            _.extend(availableApps, loadedApps);
+        }).catch(function (err) {
+            logging.error(new errors.GhostError({
+                err: err,
+                context: i18n.t('errors.apps.appWillNotBeLoaded.error'),
+                help: i18n.t('errors.apps.appWillNotBeLoaded.help')
+            }));
         });
     },
     availableApps: availableApps
