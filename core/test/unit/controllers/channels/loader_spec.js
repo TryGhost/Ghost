4c5ef16bc33eab49c4631ea8937b22e9e6357337
Hannah Wolfe <github.erisds@gmail.com>
2017-11-05 10:04:59 +0000

    Represent channels as class instances (#9209)
    refs #9192, refs #5091
    
    - Using a class allows for easy shared logic
    - Loading is designed to work from config right now, but could be DB driven, etc
    - Provided configuration can be simplified and extended in the constructor / class methods
    - Update tests, move custom assertions to utils

--- core/test/unit/controllers/channels/loader_spec.js
+++ core/test/unit/controllers/channels/loader_spec.js
@@ -2,7 +2,8 @@ var should = require('should'),  // jshint ignore:line
     _ = require('lodash'),
     rewire = require('rewire'),
     channelUtils = require('../../../utils/channelUtils'),
-    channelLoader = rewire('../../../../server/controllers/channels/loader');
+    channelLoader = rewire('../../../../server/controllers/channels/loader'),
+    Channel = channelUtils.Channel;
 
 describe('Channel Config', function () {
     var channelReset;
@@ -19,8 +20,12 @@ describe('Channel Config', function () {
 
     it('should build a list of channels', function () {
         var channels = channelLoader.list();
-        channels.should.be.an.Object();
+        channels.should.be.an.Array().with.lengthOf(3);
 
         _.map(channels, 'name').should.eql(['index', 'tag', 'author']);
+
+        _.each(channels, function (channel) {
+            channel.should.be.a.Channel();
+        });
     });
 });
