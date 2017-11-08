2f019bb7fd7703802f9d6c29ff55740848f810db
Hannah Wolfe <erisds@gmail.com>
2017-11-08 09:14:46 +0000

    Move admin redirects out of site router
    refs #9192
    
    - Admin redirects should really happen first, up with custom redirects
    - Later we can package this up, maybe
    - For now, let's focus the site router on site-related things

--- core/server/site/routes.js
+++ core/server/site/routes.js
@@ -9,13 +9,6 @@ module.exports = function siteRouter() {
     var router = express.Router(),
         routeKeywords = config.get('routeKeywords');
 
-    // Admin redirects - register redirect as route
-    // TODO: this should be middleware!
-    router.get(/^\/(logout|signout)\/$/, function (req, res) { return utils.url.redirectToAdmin(301, res, '#/signout/'); });
-    router.get(/^\/signup\/$/, function (req, res) { return utils.url.redirectToAdmin(301, res, '#/signup/'); });
-    // redirect to /ghost and let that do the authentication to prevent redirects to /ghost//admin etc.
-    router.get(/^\/((ghost-admin|admin|wp-admin|dashboard|signin|login)\/?)$/, function (req, res) { return utils.url.redirectToAdmin(301, res, '/'); });
-
     // Preview - register controller as route
     router.get(utils.url.urlJoin('/', routeKeywords.preview, ':uuid', ':options?'), controllers.preview);
 
