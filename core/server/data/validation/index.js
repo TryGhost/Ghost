6c7d4439002984fbe3c1772040af53f1a82abdfd
Aileen Nowak <aileen@ghost.org>
2017-11-09 21:22:20 +0700

    Added more database soft limits (#9225)
    refs #8143
    
    Sets soft limits for certain db fields:
    
    - `posts`:
    	- `title`: 255 chars (current hard limit: 2,000 chars)
    	- `meta_title`: 300 chars (current hard limit: 2,000 chars)
    	- `meta_description`: 500 chars (current hard limit: 2,000 chars)
    - `users`:
    	- `bio`: 200 chars (current hard limit: 65,535 chars)
    	- `location`: 150 chars (current hard limit: 65,535 chars)
    	- `meta_description`: 500 chars (current hard limit: 2,000 chars)
    	- `meta_title`: 300 chars (current hard limit: 2,000 chars)
    - `tags`:
    	- `description`: 500 chars (current hard limit: 65,535 chars)
    	- `meta_title`: 300 chars (current hard limit: 2,000 chars)
    	- `meta_description`: 500 chars (current hard limit: 2,000 chars)
    
    - same error message for isLength validator as for hard limits (more improvements are comming with https://github.com/TryGhost/Ghost/issues/6050)
    - added more tests for importer
    - added dynamic translation key handling for validators errors (isLength is only supported atm)

--- core/server/data/validation/index.js
+++ core/server/data/validation/index.js
@@ -208,7 +208,7 @@ validateSchema = function validateSchema(tableName, model) {
 
             // check validations objects
             if (schema[tableName][columnKey].hasOwnProperty('validations')) {
-                validationErrors = validationErrors.concat(validate(strVal, columnKey, schema[tableName][columnKey].validations));
+                validationErrors = validationErrors.concat(validate(strVal, columnKey, schema[tableName][columnKey].validations, tableName));
             }
 
             // check type
@@ -243,7 +243,7 @@ validateSettings = function validateSettings(defaultSettings, model) {
         matchingDefault = defaultSettings[values.key];
 
     if (matchingDefault && matchingDefault.validations) {
-        validationErrors = validationErrors.concat(validate(values.value, values.key, matchingDefault.validations));
+        validationErrors = validationErrors.concat(validate(values.value, values.key, matchingDefault.validations, 'settings'));
     }
 
     if (validationErrors.length !== 0) {
@@ -253,25 +253,29 @@ validateSettings = function validateSettings(defaultSettings, model) {
     return Promise.resolve();
 };
 
-// Validate default settings using the validator module.
-// Each validation's key is a method name and its value is an array of options
-//
-// eg:
-//      validations: { isURL: true, isLength: [20, 40] }
-//
-// will validate that a setting's length is a URL between 20 and 40 chars.
-//
-// If you pass a boolean as the value, it will specify the "good" result. By default
-// the "good" result is assumed to be true.
-//
-// eg:
-//      validations: { isNull: false }  // means the "good" result would
-//                                      // fail the `isNull` check, so
-//                                      // not null.
-//
-// available validators: https://github.com/chriso/validator.js#validators
-validate = function validate(value, key, validations) {
-    var validationErrors = [];
+/**
+ * Validate keys using the validator module.
+ * Each validation's key is a method name and its value is an array of options
+ * eg:
+ *       validations: { isURL: true, isLength: [20, 40] }
+ * will validate that a values's length is a URL between 20 and 40 chars.
+ *
+ * If you pass a boolean as the value, it will specify the "good" result. By default
+ * the "good" result is assumed to be true.
+ * eg:
+ *       validations: { isNull: false } // means the "good" result would
+ *                                      // fail the `isNull` check, so
+ *                                      // not null.
+ *
+ * available validators: https://github.com/chriso/validator.js#validators
+ * @param {String} value the value to validate.
+ * @param {String} key the db column key of the value to validate.
+ * @param {Object} validations the validations object as described above.
+ * @param {String} tableName (optional) the db table of the value to validate, used for error message.
+ * @return {Array} returns an Array including the found validation errors (empty if none found);
+ */
+validate = function validate(value, key, validations, tableName) {
+    var validationErrors = [], translation;
     value = _.toString(value);
 
     _.each(validations, function each(validationOptions, validationName) {
@@ -288,8 +292,19 @@ validate = function validate(value, key, validations) {
 
         // equivalent of validator.isSomething(option1, option2)
         if (validator[validationName].apply(validator, validationOptions) !== goodResult) {
+            // CASE: You can define specific translations for validators e.g. isLength
+            if (i18n.doesTranslationKeyExist('notices.data.validation.index.validationFailedTypes.' + validationName)) {
+                translation = i18n.t('notices.data.validation.index.validationFailedTypes.' + validationName, _.merge({
+                    validationName: validationName,
+                    key: key,
+                    tableName: tableName
+                }, validationOptions[1]));
+            } else {
+                translation = i18n.t('notices.data.validation.index.validationFailed', {validationName: validationName, key: key});
+            }
+
             validationErrors.push(new errors.ValidationError({
-                message: i18n.t('notices.data.validation.index.validationFailed', {validationName: validationName, key: key})
+                message: translation
             }));
         }
 
