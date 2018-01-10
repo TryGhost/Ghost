05e0dd9f690767827c759ade6413c426a1fa9d47
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-01-10 12:40:07 +0000

    Remove unused "copy-html" modal component

--- ghost/admin/app/components/modal-copy-html.js
+++
@@ -1,6 +0,0 @@
-import ModalComponent from 'ghost-admin/components/modal-base';
-import {alias} from '@ember/object/computed';
-
-export default ModalComponent.extend({
-    generatedHtml: alias('model')
-});
