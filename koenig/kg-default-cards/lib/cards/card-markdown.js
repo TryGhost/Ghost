TryGhost/Ghost

commit 5d7af6c586c93112c995813d3169468e38a71f56

Koenig - Rename server-side cards
refs https://github.com/TryGhost/Ghost/issues/9311
- match card names to the new generic Koenig card names introduced in https://github.com/TryGhost/Ghost-Admin/commit/95a068615db20664dab578da2e71499ce8b9c50d

koenig/kg-default-cards/lib/cards/card-markdown.js
@@ -1,0 +1,4 @@
+// this card is just an alias of the `markdown` card which is necessary because
+// our markdown-only editor was using the `card-markdown` card name
+let markdownCard = require('./markdown');
+module.exports = markdownCard;

koenig/kg-default-cards/lib/cards/hr.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'card-hr',
+    name: 'hr',
     type: 'dom',
     render(opts) {
         return opts.env.dom.createElement('hr');

koenig/kg-default-cards/lib/cards/html.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    name: 'card-html',
+    name: 'html',
     type: 'dom',
     render(opts) {
         let html = `<div class="kg-card-html">${opts.payload.html}</div>`;

koenig/kg-default-cards/lib/cards/image.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'card-image',
+    name: 'image',
     type: 'dom',
     render(opts) {
         var img = opts.env.dom.createElement('img');

koenig/kg-default-cards/lib/cards/markdown.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'card-markdown',
+    name: 'markdown',
     type: 'dom',
     render: function (opts) {
         var converters = require('../converters'),

commit 1b5be541417804aabc0ab46fafe14c32a35d3ba4

Fixed missing export of `card-markdown` card and broken tests
no issue
- fixes rendering bug introduced in https://github.com/TryGhost/Ghost/commit/5d7af6c586c93112c995813d3169468e38a71f56
- updates test generators/fixtures to use new card names

koenig/kg-default-cards/lib/cards/card-markdown.js
@@ -4,4 +4,4 @@
 // our markdown-only editor was using the `card-markdown` card name
 const markdownCard = require('./markdown');
 
-module.exports = Object.assign(markdownCard, {name: 'card-markdown'});
+module.exports = Object.assign({}, markdownCard, {name: 'card-markdown'});