5f4759913fd58a5dcd7c36fb197498f8000e789e
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-02-01 15:50:43 +0100

    Fixed missing export of `card-markdown` card
    no issue
    - fixes the bug introduced in https://github.com/TryGhost/Ghost/commit/5d7af6c586c93112c995813d3169468e38a71f56

--- koenig/kg-default-cards/lib/cards/card-markdown.js
+++ koenig/kg-default-cards/lib/cards/card-markdown.js
@@ -1,4 +1,7 @@
+'use strict';
+
 // this card is just an alias of the `markdown` card which is necessary because
 // our markdown-only editor was using the `card-markdown` card name
-let markdownCard = require('./markdown');
-module.exports = markdownCard;
+const markdownCard = require('./markdown');
+
+module.exports = Object.assign(markdownCard, {name: 'card-markdown'});
