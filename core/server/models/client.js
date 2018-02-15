616d45c1e9542054d8865b41e6d1d5a339e15a2c
kirrg001 <katharina.irrgang@googlemail.com>
2018-02-15 22:51:03 +0100

    revert reorder

--- core/server/models/client.js
+++ core/server/models/client.js
@@ -16,9 +16,9 @@ Client = ghostBookshelf.Model.extend({
 
         return {
             uuid: uuid.v4(),
+            secret: secret,
             status: 'development',
-            type: 'ua',
-            secret: secret
+            type: 'ua'
         };
     },
 
