be05ad6dc70906029987ba92a2fc47946c81be97
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-02-28 14:53:34 +0000

    Only transpile for IE11 target in CI & production builds
    no issue
    - makes debugging during development easier because there's a lot less indirection added to work around missing ES6 support in IE11

--- ghost/admin/config/targets.js
+++ ghost/admin/config/targets.js
@@ -1,11 +1,19 @@
 /* eslint-env node */
 
+const browsers = [
+    'last 2 Chrome versions',
+    'last 2 Firefox versions',
+    'last 2 Safari versions',
+    'last 2 Edge versions'
+];
+
+const isCI = !!process.env.CI;
+const isProduction = process.env.EMBER_ENV === 'production';
+
+if (isCI || isProduction) {
+    browsers.push('ie 11');
+}
+
 module.exports = {
-    browsers: [
-        'ie 11',
-        'last 2 Chrome versions',
-        'last 2 Firefox versions',
-        'last 2 Safari versions',
-        'last 2 Edge versions'
-    ]
+    browsers
 };
