edb79cec0f7b5e54c65f5c1af3674501dd7d7192
Mante Bridts <mantebridts@gmail.com>
2018-03-04 14:07:31 +0100

    pass complete options object, fixed conflicts

--- core/server/data/meta/title.js
+++ core/server/data/meta/title.js
@@ -12,7 +12,7 @@ function getTitle(data, root, options) {
     options = options ? options : {};
 
     if (pagination && pagination.total > 1) {
-        pageString = _.has(options, 'page') ? options.page.replace('%', pagination.page) : ' (Page ' + pagination.page + ')';
+        pageString = _.has(options.hash, 'page') ? options.hash.page.replace('%', pagination.page) : ' (Page ' + pagination.page + ')';
     }
 
     // If there's a specific meta title
--- core/server/helpers/meta_title.js
+++ core/server/helpers/meta_title.js
@@ -8,5 +8,5 @@ var proxy = require('./proxy'),
 
 // We use the name meta_title to match the helper for consistency:
 module.exports = function meta_title(options) { // eslint-disable-line camelcase
-    return getMetaDataTitle(this, options.data.root, options.hash);
+    return getMetaDataTitle(this, options.data.root, options);
 };
