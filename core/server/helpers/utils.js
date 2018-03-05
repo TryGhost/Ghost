301e1b2419ec25573e53a9313e03f2af02ba4960
Rosco Kalis <roscokalis@gmail.com>
2018-03-05 09:30:15 +0100

    🎨Improved image counting for the {{reading_time}} helper (#9366)
    refs #9200
    
    - We have not yet counted the images within your html, this commit counts images based on the this algorithm: https://blog.medium.com/read-time-and-you-bc2048ab620c
    - Added imageCount utility, which counts images using an img-tag regex, amended from the general tag-regex found in wordCount
    - Added this imageCount to the {{reading_time}} helper, adding 12 seconds to the reading time for every image
    - The feature image is still counted as before
    - The first image adds 12 seconds, the second 11, the third 10, and so on
    - Images from the tenth onwards add 3 seconds to the reading time

--- core/server/helpers/utils.js
+++ core/server/helpers/utils.js
@@ -23,6 +23,16 @@ module.exports.wordCount = function wordCount(html) {
     return html.split(' ').length;
 };
 
+/**
+ * Image count Utility
+ * @param {string} html
+ * @returns {integer} image count
+ * @description Takes an HTML string and returns the number of images
+ **/
+module.exports.imageCount = function wordCount(html) {
+    return (html.match(/<img(.|\n)*?>/g) || []).length;
+};
+
 module.exports.findKey = function findKey(key /* ...objects... */) {
     let objects = Array.prototype.slice.call(arguments, 1);
 
