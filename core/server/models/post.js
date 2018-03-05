1da2eec91519fd9975f1c6bff7f6dfba1978d3da
Aileen Nowak <aileen@ghost.org>
2018-03-05 15:10:27 +0700

    🐛Fixed image properties to be reset to null after removal (#9432)
    closes #9085
    
    Fixes an issue, where the client sets image properties to `""` after deleting the image. This causes problems with the query filter (see https://github.com/TryGhost/GQL/issues/24), as they have to be `null`.
    
    Added a check in the model layer saving method to set value  to `null`, when the property is empty.
    
    Affected models and properties:
    - `posts`:
    	- `feature_image`
    	- `og_image`
    	- `twitter_image`
    - `users`:
    	- `profile_image`
    	- `cover_image`
    - `tags`:
    	- `feature_image`

--- core/server/models/post.js
+++ core/server/models/post.js
@@ -302,6 +302,13 @@ Post = ghostBookshelf.Model.extend({
         return sequence(ops);
     },
 
+    emptyStringProperties: function emptyStringProperties() {
+        // CASE: the client might send empty image properties with "" instead of setting them to null.
+        // This can cause GQL to fail. We therefore enforce 'null' for empty image properties.
+        // See https://github.com/TryGhost/GQL/issues/24
+        return ['feature_image', 'og_image', 'twitter_image'];
+    },
+
     onCreating: function onCreating(model, attr, options) {
         options = options || {};
 
---
+++ core/test/unit/models/tag_spec.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var should = require('should'), // jshint ignore:line
+    sinon = require('sinon'),
+    models = require('../../../server/models'),
+    testUtils = require('../../utils'),
+    sandbox = sinon.sandbox.create();
+
+describe('Unit: models/tags', function () {
+    before(function () {
+        models.init();
+    });
+
+    after(function () {
+        sandbox.restore();
+    });
+
+    describe('Edit', function () {
+        let knexMock;
+
+        before(function () {
+            knexMock = new testUtils.mocks.knex();
+            knexMock.mock();
+        });
+
+        after(function () {
+            knexMock.unmock();
+        });
+
+        it('resets given empty value to null', function () {
+            return models.Tag.findOne({slug: 'kitchen-sink'})
+                .then(function (tag) {
+                    tag.get('slug').should.eql('kitchen-sink');
+                    tag.get('feature_image').should.eql('https://example.com/super_photo.jpg');
+                    tag.set('feature_image', '');
+                    tag.set('description', '');
+                    return tag.save();
+                })
+                .then(function (tag) {
+                    should(tag.get('feature_image')).be.null();
+                    tag.get('description').should.eql('');
+                });
+        });
+    });
+});
