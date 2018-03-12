b535d583866e6a379fa7831a4d1ee4b04c7e0387
Rosco Kalis <roscokalis@gmail.com>
2018-03-12 20:31:43 +0100

    Redirect amp links to their non-amp counterpart when amp is disabled closes #9495
    - Added a clause for amp being disabled
    - In this clause, we strip the final 'amp/' part of the url, and redirect
    - Changed corresponding test in frontend_spec.js

--- core/server/apps/amp/index.js
+++ core/server/apps/amp/index.js
@@ -5,17 +5,20 @@ var router           = require('./lib/router'),
     config = require('../../config'),
     settingsCache = require('../../services/settings/cache');
 
+function ampRouter(req, res) {
+    if (settingsCache.get('amp') === true) {
+        return router.apply(this, arguments);
+    } else {
+        var redirectUrl = req.originalUrl.replace(/amp\/$/, '');
+        res.redirect(301, redirectUrl);
+    }
+}
+
 module.exports = {
     activate: function activate(ghost) {
         var ampRoute = '*/' + config.get('routeKeywords').amp + '/';
 
-        ghost.routeService.registerRouter(ampRoute, function settingsEnabledRouter(req, res, next) {
-            if (settingsCache.get('amp') === true) {
-                return router.apply(this, arguments);
-            }
-
-            next();
-        });
+        ghost.routeService.registerRouter(ampRoute, ampRouter);
 
         registerHelpers(ghost);
     }
