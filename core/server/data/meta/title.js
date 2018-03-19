7ed822cc0c6c774f0b34d990e7f4ddb6dec18d72
Mante Bridts <mantebridts@users.noreply.github.com>
2018-03-19 09:48:07 +0100

    🐛Fixed i18n for the `{{meta_title}}` helper (#9468)
    closes #9466
    
    - make 'page' in the meta title translatable through subexpression in the meta title helper
    - e.g. `{{meta_title page=(t....)}}`

--- core/server/data/meta/title.js
+++ core/server/data/meta/title.js
@@ -12,7 +12,7 @@ function getTitle(data, root, options) {
     options = options ? options : {};
 
     if (pagination && pagination.total > 1) {
-        pageString = ' (Page ' + pagination.page + ')';
+        pageString = _.has(options.hash, 'page') ? options.hash.page.replace('%', pagination.page) : ' (Page ' + pagination.page + ')';
     }
 
     // If there's a specific meta title
