0b527ea2c29111329a889de0653ce44a1e1c380f
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-04-05 11:54:36 +0100

    Don't send `created_by` and `updated_by` attrs in API requests
    refs https://github.com/TryGhost/Ghost/issues/9548
    - refactor serialisers to use `serialize` rather than `serializeToHash` to avoid code duplication
    - strip `created_by` and `updated_by` attrs when serializing - Ghost will set these automatically based on the currently logged in user

--- ghost/admin/app/serializers/application.js
+++ ghost/admin/app/serializers/application.js
@@ -3,6 +3,16 @@ import {decamelize} from '@ember/string';
 import {pluralize} from 'ember-inflector';
 
 export default RESTSerializer.extend({
+    serialize(/*snapshot, options*/) {
+        let json = this._super(...arguments);
+
+        // don't send attributes that are updated automatically on the server
+        delete json.created_by;
+        delete json.updated_by;
+
+        return json;
+    },
+
     serializeIntoHash(hash, type, record, options) {
         // Our API expects an id on the posted object
         options = options || {};
