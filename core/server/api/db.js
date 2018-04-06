ca83c01477c1cf7071bd596a74638203f90485a7
kirrg001 <katharina.irrgang@googlemail.com>
2018-04-06 14:34:07 +0200

    Fixed `post.unpublished` when deleting all content
    no issue
    
    - if you delete all content, we expect two events
      - `post.deleted` and `post.unpublished`
    - `post.unpublished` was never triggered, because the api implementation made use of `collection.invoke(`destroy`)`
    - what happened?
      - you fetch all posts (columns:id)
      - you destroy the post (only id column is available)
      - the model events are triggered
      - but you have no access to a default set of data
      - the result is that the event handler can't even tell if this is a post or a page
    - added a proper test to ensure which events are triggered

--- core/server/api/db.js
+++ core/server/api/db.js
@@ -1,6 +1,9 @@
+'use strict';
+
 // # DB API
 // API for DB operations
 var Promise = require('bluebird'),
+    _ = require('lodash'),
     pipeline = require('../lib/promise/pipeline'),
     localUtils = require('./utils'),
     exporter = require('../data/export'),
@@ -110,18 +113,39 @@ db = {
 
         options = options || {};
 
+        /**
+         * @NOTE:
+         * We fetch all posts with `columns:id` to increase the speed of this endpoint.
+         * And if you trigger `post.destroy(..)`, this will trigger bookshelf and model events.
+         * But we only have to `id` available in the model. This won't work, because:
+         *   - model layer can't trigger event e.g. `post.page` to trigger `post|page.unpublished`.
+         *   - `onDestroyed` or `onDestroying` can contain custom logic
+         */
         function deleteContent() {
-            var collections = [
-                models.Post.findAll(queryOpts),
-                models.Tag.findAll(queryOpts)
-            ];
+            return models.Base.transaction(function (transacting) {
+                queryOpts.transacting = transacting;
 
-            return Promise.each(collections, function then(Collection) {
-                return Collection.invokeThen('destroy', queryOpts);
-            }).return({db: []})
-                .catch(function (err) {
-                    throw new common.errors.GhostError({err: err});
-                });
+                return models.Post.findAll(queryOpts)
+                    .then((response) => {
+                        return Promise.map(response.models, (post) => {
+                            return models.Post.destroy(_.merge({id: post.id}, queryOpts));
+                        }, {concurrency: 100});
+                    })
+                    .then(() => {
+                        return models.Tag.findAll(queryOpts);
+                    })
+                    .then((response) => {
+                        return Promise.map(response.models, (tag) => {
+                            return models.Tag.destroy(_.merge({id: tag.id}, queryOpts));
+                        }, {concurrency: 100});
+                    })
+                    .return({db: []})
+                    .catch((err) => {
+                        throw new common.errors.GhostError({
+                            err: err
+                        });
+                    });
+            });
         }
 
         tasks = [
