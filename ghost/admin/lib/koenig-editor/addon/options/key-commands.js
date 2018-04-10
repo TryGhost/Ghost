d87787c47c793648828b938549c134ee0af5f194
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-04-10 15:34:12 +0100

    Koenig - Fix error on SHIFT+ENTER whilst a card is selected
    refs https://github.com/TryGhost/Ghost/issues/9505
    - a card section isn't markerable so a guard is needed to prevent inserting the soft-break atom

--- ghost/admin/lib/koenig-editor/addon/options/key-commands.js
+++ ghost/admin/lib/koenig-editor/addon/options/key-commands.js
@@ -6,6 +6,10 @@ export default function (editor) {
         str: 'SHIFT+ENTER',
 
         run(editor) {
+            if (!editor.range.headSection.isMarkerable) {
+                return;
+            }
+
             editor.run((postEditor) => {
                 let softReturn = postEditor.builder.createAtom('soft-return');
                 postEditor.insertMarkers(editor.range.head, [softReturn]);
