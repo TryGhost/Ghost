15ddb3735b87aacc986fcacf813fffdfde2c202f
kirrg001 <katharina.irrgang@googlemail.com>
2018-04-18 17:17:12 +0200

    Render 503 if url service did not finish
    https://github.com/TryGhost/Team/issues/65
    
    - return 503 if the url service has not finished generating the resource urls

--- core/server/web/middleware/maintenance.js
+++ core/server/web/middleware/maintenance.js
@@ -1,10 +1,17 @@
 var config = require('../../config'),
-    common = require('../../lib/common');
+    common = require('../../lib/common'),
+    urlService = require('../../services/url');
 
 module.exports = function maintenance(req, res, next) {
     if (config.get('maintenance').enabled) {
         return next(new common.errors.MaintenanceError({
             message: common.i18n.t('errors.general.maintenance')
+        }));
+    }
+
+    if (!urlService.hasFinished()) {
+        return next(new common.errors.MaintenanceError({
+            message: common.i18n.t('errors.general.maintenanceUrlService')
         }));
     }
 
