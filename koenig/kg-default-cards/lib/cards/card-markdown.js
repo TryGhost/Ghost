25066c13b07512ff96e00684150329175318a184
kirrg001 <katharina.irrgang@googlemail.com>
2018-04-30 12:33:36 +0200

    🔥  Drop Node v4 Support
    no issue
    
    - support ends today
    - see https://github.com/nodejs/Release
    - removed `use strict`

--- koenig/kg-default-cards/lib/cards/card-markdown.js
+++ koenig/kg-default-cards/lib/cards/card-markdown.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // this card is just an alias of the `markdown` card which is necessary because
 // our markdown-only editor was using the `card-markdown` card name
 const markdownCard = require('./markdown');
