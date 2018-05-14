5b4335011206a104fe60385ed70c735886d36c9c
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-05-14 13:04:53 +0100

    Add {{background-image-style}} helper
    no issue
    - cleans up multiple CPs that were only outputting a background image style
    - moves URL decoding/encoding and `htmlSafe` output into one place

--- ghost/admin/app/components/gh-user-active.js
+++ ghost/admin/app/components/gh-user-active.js
@@ -1,17 +1,11 @@
 import Component from '@ember/component';
 import moment from 'moment';
 import {computed} from '@ember/object';
-import {htmlSafe} from '@ember/string';
 
 export default Component.extend({
     tagName: '',
 
     user: null,
-
-    userImageBackground: computed('user.profileImageUrl', function () {
-        let url = encodeURI(decodeURI(this.user.get('profileImageUrl')));
-        return htmlSafe(`background-image: url(${url})`);
-    }),
 
     lastLoginUTC: computed('user.lastLoginUTC', function () {
         let lastLoginUTC = this.get('user.lastLoginUTC');
