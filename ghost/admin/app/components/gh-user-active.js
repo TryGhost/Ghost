5ce5dd04ab1d8bbfa32526930eaa1663535eb204
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-05-14 12:15:08 +0100

    🐛 Fixed low-resolution gravatar images on Team/User screens
    closes https://github.com/TryGhost/Ghost/issues/9563
    - fixes incorrect gravatar URLs by removing incorrect HTML encoding of the URL and enforcing URL encoding
    - centralise image URL generation in the User model so that default image logic isn't spread across the app
    - remove unnecessary proxy mixin from `ghost-paths` service (allows property access without `.get()`)

--- ghost/admin/app/components/gh-user-active.js
+++ ghost/admin/app/components/gh-user-active.js
@@ -1,29 +1,16 @@
 import Component from '@ember/component';
-import Ember from 'ember';
 import moment from 'moment';
 import {computed} from '@ember/object';
 import {htmlSafe} from '@ember/string';
-import {inject as service} from '@ember/service';
-
-const {Handlebars} = Ember;
 
 export default Component.extend({
-    ghostPaths: service(),
-
     tagName: '',
 
     user: null,
 
-    userDefault: computed('ghostPaths', function () {
-        let image = '/img/user-image.png';
-        return `${this.get('ghostPaths.assetRoot')}${image}`;
-    }),
-
-    userImageBackground: computed('user.profileImage', 'userDefault', function () {
-        let url = this.get('user.profileImage') || this.get('userDefault');
-        let safeUrl = Handlebars.Utils.escapeExpression(url);
-
-        return htmlSafe(`background-image: url(${safeUrl})`);
+    userImageBackground: computed('user.profileImageUrl', function () {
+        let url = encodeURI(decodeURI(this.user.get('profileImageUrl')));
+        return htmlSafe(`background-image: url(${url})`);
     }),
 
     lastLoginUTC: computed('user.lastLoginUTC', function () {
--- ghost/admin/app/services/ghost-paths.js
+++ ghost/admin/app/services/ghost-paths.js
@@ -1,10 +1,4 @@
-import Ember from 'ember';
 import Service from '@ember/service';
 import ghostPaths from 'ghost-admin/utils/ghost-paths';
 
-// ember-cli-shims doesn't export _ProxyMixin
-const {_ProxyMixin} = Ember;
-
-export default Service.extend(_ProxyMixin, {
-    content: ghostPaths()
-});
+export default Service.extend(ghostPaths());
