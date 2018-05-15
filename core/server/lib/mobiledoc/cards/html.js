978817a768c5b0fae142252b9088589b56f7daf1
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-05-15 10:09:52 +0100

    Koenig - Fixed empty HTML card rendering `undefined`
    refs https://github.com/TryGhost/Ghost/issues/9623
    - add tests for undefined payloads in container cards
    - add guard for undefined payload in html card

--- core/server/lib/mobiledoc/cards/html.js
+++ core/server/lib/mobiledoc/cards/html.js
@@ -2,6 +2,10 @@ module.exports = {
     name: 'html',
     type: 'dom',
     render(opts) {
+        if (!opts.payload.html) {
+            return '';
+        }
+
         // use the SimpleDOM document to create a raw HTML section.
         // avoids parsing/rendering of potentially broken or unsupported HTML
         return opts.env.dom.createRawHTMLSection(opts.payload.html);
