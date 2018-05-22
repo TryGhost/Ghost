634542ab5dd508b65994fed6ddd602494e8e92ec
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-05-22 09:43:53 +0100

    Koenig - Fix broken image card tests
    refs https://github.com/TryGhost/Ghost/issues/9505
    - update tests to match loose BEM image style classes

--- core/test/unit/lib/mobiledoc/converters/mobiledoc-converter_spec.js
+++ core/test/unit/lib/mobiledoc/converters/mobiledoc-converter_spec.js
@@ -72,7 +72,7 @@ describe('Mobiledoc converter', function () {
                 ]
             };
 
-            converter.render(mobiledoc, 2).should.eql('<div class="kg-post">\n<p>One<br>Two</p><h1 id="markdowncard">Markdown card</h1>\n<p>Some markdown</p>\n<p>Three</p><hr><figure class="kg-image-card"><img src="/content/images/2018/04/NatGeo06.jpg" class="kg-image kg-image--wide"><figcaption>Birdies</figcaption></figure><p>Four</p><h2>HTML card</h2>\n<div><p>Some HTML</p></div>\n</div>');
+            converter.render(mobiledoc, 2).should.eql('<div class="kg-post">\n<p>One<br>Two</p><h1 id="markdowncard">Markdown card</h1>\n<p>Some markdown</p>\n<p>Three</p><hr><figure class="kg-image-card"><img src="/content/images/2018/04/NatGeo06.jpg" class="kg-image kg-image-wide"><figcaption>Birdies</figcaption></figure><p>Four</p><h2>HTML card</h2>\n<div><p>Some HTML</p></div>\n</div>');
         });
 
         it('wraps output with a .kg-post div', function () {
