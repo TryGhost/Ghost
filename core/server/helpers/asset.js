8ecc09231c3829a0ea5694f7a0e3f80784bdbd02
hkd987 <lundinmatthews@me.com>
2018-06-04 07:38:13 -0500

    CHORE: ES6 Language Update
    updaed the file to use const where approiate and using an anonymous arrow function on the exports.

--- core/server/helpers/asset.js
+++ core/server/helpers/asset.js
@@ -2,13 +2,13 @@
 // Usage: `{{asset "css/screen.css"}}`, `{{asset "css/screen.css" ghost="true"}}`
 //
 // Returns the path to the specified asset. The ghost flag outputs the asset path for the Ghost admin
-var proxy = require('./proxy'),
+const proxy = require('./proxy'),
     _ = require('lodash'),
     getAssetUrl = proxy.metaData.getAssetUrl,
     SafeString = proxy.SafeString;
 
-module.exports = function asset(path, options) {
-    var hasMinFile = _.get(options, 'hash.hasMinFile');
+module.exports = (path, options) => {
+    const hasMinFile = _.get(options, 'hash.hasMinFile');
 
     return new SafeString(
         getAssetUrl(path, hasMinFile)
