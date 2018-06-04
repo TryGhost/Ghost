a0ceb1e46c16ae27a75078c8276c1595b3827cb5
hkd987 <lundinmatthews@me.com>
2018-06-04 13:06:10 -0500

    CLEANED: Removed linter errors
    Linter does not want spaces after descontruction.

--- core/server/helpers/author.js
+++ core/server/helpers/author.js
@@ -10,28 +10,27 @@
 // Block helper: `{{#author}}{{/author}}`
 // This is the default handlebars behaviour of dropping into the author object scope
 
-const { SafeString, templates, url, hbs: { handlebars } } = require('./proxy'),
+const {SafeString, templates, url, hbs: {handlebars}} = require('./proxy'),
     isString = require('lodash/isString'),
     ESCAPE = require('lodash/escape'); // must be capital to avoid naming collision
 
 /**
  * @deprecated: will be removed in Ghost 2.0
  */
-
 module.exports = function author(options) {
-  const { fn, hash: { autolink } } = options;
-  const { author } = this;
+  const {fn, hash: {autolink}} = options;
+  const {author} = this;
     if (fn) {
         return handlebars.helpers.with.call(this, author, options);
     }
 
-    const autolink_check = isString(autolink) && autolink === 'false' ? false : true;
+    const autolinkCheck = isString(autolink) && autolink === 'false' ? false : true;
     let output = '';
 
     if (author && author.name) {
-        if (autolink_check) {
+        if (autolinkCheck) {
             output = templates.link({
-                url: url.urlFor('author', { author }),
+                url: url.urlFor('author', {author}),
                 text: ESCAPE(author.name)
             });
         } else {
