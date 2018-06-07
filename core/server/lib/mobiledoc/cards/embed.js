fe8c07333d516873df5562f655e63da92bd53351
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-06-07 12:35:38 +0100

    Koenig - Embed card renderer
    refs https://github.com/TryGhost/Ghost/issues/9623
    - add `embed` card renderer

---
+++ core/server/lib/mobiledoc/cards/embed.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: 'embed',
+    type: 'dom',
+    render(opts) {
+        if (!opts.payload.html) {
+            return '';
+        }
+
+        let {payload, env: {dom}} = opts;
+
+        let figure = dom.createElement('figure');
+        figure.setAttribute('class', 'kg-embed-card');
+
+        let html = dom.createRawHTMLSection(payload.html);
+        figure.appendChild(html);
+
+        if (payload.caption) {
+            let figcaption = dom.createElement('figcaption');
+            figcaption.appendChild(dom.createTextNode(payload.caption));
+            figure.appendChild(figcaption);
+        }
+
+        return figure;
+    }
+};
