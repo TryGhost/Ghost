TryGhost/Ghost

commit b7bb1916208df7a1e72046b75f0ad0a96c3f0d42

✨ Koenig - Toggle list types using MD list syntax
refs https://github.com/TryGhost/Ghost/issues/9623
- toggle the list type (unordered vs ordered) when typing `- `, `* `, `1 `, or `1. ` at the beginning of a list item

ghost/admin/lib/koenig-editor/addon/options/text-expansions.js
@@ -8,15 +8,40 @@ export function replaceWithListSection(editor, matches, listTagName) {
     let {range, range: {head, head: {section}}} = editor;
     let text = section.textUntil(head);
 
-    if (section.isListItem) {
-        return;
-    }
-
     // we don't want to convert to a heading if the user has not just
     // finished typing the markdown (eg, they've made a previous
     // heading expansion then Cmd-Z'ed it to get the text back then
     // starts typing at the end of the heading)
     if (text !== matches[0]) {
+        return;
+    }
+
+    if (section.isListItem) {
+        if (section.parent.tagName === listTagName) {
+            return;
+        }
+
+        // toggle all list items to the new list type
+        editor.run((postEditor) => {
+            let listRange = section.parent.toRange();
+            let {post} = editor;
+
+            range = range.extend(-(matches[0].length));
+            postEditor.deleteRange(range);
+
+            let cursorListItem;
+            post.walkMarkerableSections(listRange, (listItem) => {
+                let isCursorListItem = listItem === section;
+                let changedListItem = postEditor.changeSectionTagName(listItem, listTagName);
+
+                if (isCursorListItem) {
+                    cursorListItem = changedListItem;
+                }
+            });
+
+            postEditor.setRange(cursorListItem.headPosition().toRange());
+        });
+
         return;
     }
 

commit 3a836e8f3824de788e645b957bbf978cecfbd221

Koenig - Fixed dash text expansions
refs https://github.com/TryGhost/Ghost/issues/9623
- https://github.com/TryGhost/Ghost-Admin/commit/b7bb1916208df7a1e72046b75f0ad0a96c3f0d42 broke dash expansions because the list toggle was matching the `- ` before the dash text expansion could be triggered
- resolved by adding the missing beginning-of-string `^`

ghost/admin/lib/koenig-editor/addon/options/text-expansions.js
@@ -88,7 +88,7 @@ export default function (editor, koenig) {
     editor.unregisterTextInputHandler('ul');
     editor.onTextInput({
         name: 'md_ul',
-        match: /^\* |- /,
+        match: /^\* |^- /,
         run(editor, matches) {
             replaceWithListSection(editor, matches, 'ul');
         }