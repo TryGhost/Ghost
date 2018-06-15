ea2a6a09dcba1fe9edf6139d0d1cb88035f3b7ae
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-06-15 14:00:00 +0100

    ✨ Koenig - Toggle list types using MD list syntax
    refs https://github.com/TryGhost/Ghost/issues/9623
    - toggle the list type (unordered vs ordered) when typing `- `, `* `, `1 `, or `1. ` at the beginning of a list item

--- ghost/admin/lib/koenig-editor/addon/options/text-expansions.js
+++ ghost/admin/lib/koenig-editor/addon/options/text-expansions.js
@@ -8,15 +8,40 @@ export function replaceWithListSection(editor, matches, listTagName) {
     let {range, range: {head, head: {section}}} = editor;
     let text = section.textUntil(head);
 
-    if (section.isListItem) {
-        return;
-    }
-
     // we don't want to convert to a heading if the user has not just
     // finished typing the markdown (eg, they've made a previous
     // heading expansion then Cmd-Z'ed it to get the text back then
     // starts typing at the end of the heading)
     if (text !== matches[0]) {
+        return;
+    }
+
+    if (section.isListItem) {
+        if (section.parent.tagName === listTagName) {
+            return;
+        }
+
+        // toggle all list items to the new list type
+        editor.run((postEditor) => {
+            let listRange = section.parent.toRange();
+            let {post} = editor;
+
+            range = range.extend(-(matches[0].length));
+            postEditor.deleteRange(range);
+
+            let cursorListItem;
+            post.walkMarkerableSections(listRange, (listItem) => {
+                let isCursorListItem = listItem === section;
+                let changedListItem = postEditor.changeSectionTagName(listItem, listTagName);
+
+                if (isCursorListItem) {
+                    cursorListItem = changedListItem;
+                }
+            });
+
+            postEditor.setRange(cursorListItem.headPosition().toRange());
+        });
+
         return;
     }
 
