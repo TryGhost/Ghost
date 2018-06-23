d1b7163fabad6ec617a7f4383a44667b05fb3170
kirrg001 <katharina.irrgang@googlemail.com>
2018-06-24 00:32:01 +0200

    Dynamic Routing Beta: Added resource config
    refs #9601
    
    - outsource resource config
    - we will re-use it in the yaml validator

---
+++ core/server/services/routing/assets/resource-config.js
@@ -0,0 +1,53 @@
+/* eslint-disable */
+module.exports.QUERY = {
+    tag: {
+        alias: 'tags',
+        type: 'read',
+        resource: 'tags',
+        options: {
+            slug: '%s',
+            visibility: 'public'
+        }
+    },
+    author: {
+        alias: 'users',
+        type: 'read',
+        resource: 'users',
+        options: {
+            slug: '%s',
+            visibility: 'public'
+        }
+    },
+    post: {
+        alias: 'posts',
+        type: 'read',
+        resource: 'posts',
+        options: {
+            slug: '%s',
+            status: 'published',
+            page: 0
+        }
+    },
+    page: {
+        alias: 'pages',
+        type: 'read',
+        resource: 'posts',
+        options: {
+            slug: '%s',
+            status: 'published',
+            page: 1
+        }
+    }
+};
+
+module.exports.TAXONOMIES = {
+    tag: {
+        filter: 'tags:\'%s\'+tags.visibility:public',
+        editRedirect: '#/settings/tags/:slug/'
+    },
+    author: {
+        filter: 'authors:\'%s\'',
+        editRedirect: '#/team/:slug/'
+    }
+};
+/* eslint-enable */
