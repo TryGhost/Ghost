51e62869248452b7da818828f6119f4f703a06e9
kirrg001 <katharina.irrgang@googlemail.com>
2018-06-24 00:32:05 +0200

    Dynamic Routing Beta: Added redirect middleware to ParentRouter
    refs #9601
    
    - middleware to control dominant router redirects

--- core/test/unit/services/routing/ParentRouter_spec.js
+++ core/test/unit/services/routing/ParentRouter_spec.js
@@ -1,7 +1,9 @@
 const should = require('should'),
     sinon = require('sinon'),
+    configUtils = require('../../../utils/configUtils'),
     settingsCache = require('../../../../server/services/settings/cache'),
     common = require('../../../../server/lib/common'),
+    urlService = require('../../../../server/services/url'),
     ParentRouter = require('../../../../server/services/routing/ParentRouter'),
     sandbox = sinon.sandbox.create();
 
@@ -14,7 +16,15 @@ describe('UNIT - services/routing/ParentRouter', function () {
         sandbox.stub(common.events, 'emit');
         sandbox.stub(common.events, 'on');
 
+        sandbox.stub(urlService.utils, 'redirect301');
+
         req = sandbox.stub();
+        req.app = {
+            _router: {
+                stack: []
+            }
+        };
+
         res = sandbox.stub();
         next = sandbox.stub();
 
@@ -23,7 +33,228 @@ describe('UNIT - services/routing/ParentRouter', function () {
 
     afterEach(function () {
         sandbox.restore();
+        configUtils.restore();
     });
+
+    describe('fn: _getSiteRouter', function () {
+        it('find site router', function () {
+            const parentRouter = new ParentRouter();
+
+            req.app = {
+                _router: {
+                    stack: [{
+                        name: 'SiteRouter'
+                    }]
+                }
+            };
+
+            should.exist(parentRouter._getSiteRouter(req));
+        });
+    });
+
+    describe('fn: _respectDominantRouter', function () {
+        it('redirect', function () {
+            const parentRouter = new ParentRouter();
+            parentRouter.getType = sandbox.stub().returns('tags');
+            parentRouter.permalinks = {
+                getValue: sandbox.stub().returns('/tag/:slug/')
+            };
+
+            req.url = '/tag/bacon/';
+            req.originalUrl = '/tag/bacon/';
+
+            req.app._router.stack = [{
+                name: 'SiteRouter',
+                handle: {
+                    stack: [{
+                        name: 'StaticRoutesRouter',
+                        handle: {
+                            parent: {
+                                isRedirectEnabled: sandbox.stub().returns(true),
+                                getRoute: sandbox.stub().returns('/channel/')
+                            }
+                        }
+                    }]
+                }
+            }];
+
+            parentRouter._respectDominantRouter(req, res, next, 'bacon');
+            next.called.should.eql(false);
+            urlService.utils.redirect301.withArgs(res, '/channel/').calledOnce.should.be.true();
+        });
+
+        it('redirect with query params', function () {
+            const parentRouter = new ParentRouter('tag', '/tag/:slug/');
+            parentRouter.getType = sandbox.stub().returns('tags');
+            parentRouter.permalinks = {
+                getValue: sandbox.stub().returns('/tag/:slug/')
+            };
+
+            req.url = '/tag/bacon/';
+            req.originalUrl = '/tag/bacon/?a=b';
+
+            req.app._router.stack = [{
+                name: 'SiteRouter',
+                handle: {
+                    stack: [{
+                        name: 'StaticRoutesRouter',
+                        handle: {
+                            parent: {
+                                isRedirectEnabled: sandbox.stub().returns(true),
+                                getRoute: sandbox.stub().returns('/channel/')
+                            }
+                        }
+                    }]
+                }
+            }];
+
+            parentRouter._respectDominantRouter(req, res, next, 'bacon');
+            next.called.should.eql(false);
+            urlService.utils.redirect301.withArgs(res, '/channel/?a=b').calledOnce.should.be.true();
+        });
+
+        it('redirect rss', function () {
+            const parentRouter = new ParentRouter('tag', '/tag/:slug/');
+            parentRouter.getType = sandbox.stub().returns('tags');
+            parentRouter.permalinks = {
+                getValue: sandbox.stub().returns('/tag/:slug/')
+            };
+
+            req.url = '/tag/bacon/rss/';
+            req.originalUrl = '/tag/bacon/rss/';
+
+            req.app._router.stack = [{
+                name: 'SiteRouter',
+                handle: {
+                    stack: [{
+                        name: 'StaticRoutesRouter',
+                        handle: {
+                            parent: {
+                                isRedirectEnabled: sandbox.stub().returns(true),
+                                getRoute: sandbox.stub().returns('/channel/')
+                            }
+                        }
+                    }]
+                }
+            }];
+
+            parentRouter._respectDominantRouter(req, res, next, 'bacon');
+            next.called.should.eql(false);
+            urlService.utils.redirect301.withArgs(res, '/channel/rss/').calledOnce.should.be.true();
+        });
+
+        it('redirect pagination', function () {
+            const parentRouter = new ParentRouter('tag', '/tag/:slug/');
+            parentRouter.getType = sandbox.stub().returns('tags');
+            parentRouter.permalinks = {
+                getValue: sandbox.stub().returns('/tag/:slug/')
+            };
+
+            req.url = '/tag/bacon/page/2/';
+            req.originalUrl = '/tag/bacon/page/2/';
+
+            req.app._router.stack = [{
+                name: 'SiteRouter',
+                handle: {
+                    stack: [{
+                        name: 'StaticRoutesRouter',
+                        handle: {
+                            parent: {
+                                isRedirectEnabled: sandbox.stub().returns(true),
+                                getRoute: sandbox.stub().returns('/channel/')
+                            }
+                        }
+                    }]
+                }
+            }];
+
+            parentRouter._respectDominantRouter(req, res, next, 'bacon');
+            next.called.should.eql(false);
+            urlService.utils.redirect301.withArgs(res, '/channel/page/2/').calledOnce.should.be.true();
+        });
+
+        it('redirect correctly with subdirectory', function () {
+            configUtils.set('url', 'http://localhost:7777/blog/');
+
+            const parentRouter = new ParentRouter('tag', '/tag/:slug/');
+            parentRouter.getType = sandbox.stub().returns('tags');
+            parentRouter.permalinks = {
+                getValue: sandbox.stub().returns('/tag/:slug/')
+            };
+
+            req.url = '/tag/bacon/';
+            req.originalUrl = '/blog/tag/bacon/';
+
+            req.app._router.stack = [{
+                name: 'SiteRouter',
+                handle: {
+                    stack: [{
+                        name: 'StaticRoutesRouter',
+                        handle: {
+                            parent: {
+                                isRedirectEnabled: sandbox.stub().returns(true),
+                                getRoute: sandbox.stub().returns('/channel/')
+                            }
+                        }
+                    }]
+                }
+            }];
+
+            parentRouter._respectDominantRouter(req, res, next, 'bacon');
+            next.called.should.eql(false);
+            urlService.utils.redirect301.withArgs(res, '/blog/channel/').calledOnce.should.be.true();
+        });
+
+        it('no redirect: different data key', function () {
+            const parentRouter = new ParentRouter('tag', '/tag/:slug/');
+            parentRouter.getType = sandbox.stub().returns('tags');
+            parentRouter.permalinks = {
+                getValue: sandbox.stub().returns('/tag/:slug/')
+            };
+
+            req.app._router.stack = [{
+                name: 'SiteRouter',
+                handle: {
+                    stack: [{
+                        name: 'StaticRoutesRouter',
+                        handle: {
+                            parent: {
+                                isRedirectEnabled: sandbox.stub().returns(false),
+                                getRoute: sandbox.stub().returns('/channel/')
+                            }
+                        }
+                    }]
+                }
+            }];
+
+            parentRouter._respectDominantRouter(req, res, next, 'bacon');
+            next.called.should.eql(true);
+            urlService.utils.redirect301.called.should.be.false();
+        });
+
+        it('no redirect: no channel defined', function () {
+            const parentRouter = new ParentRouter('tag', '/tag/:slug/');
+            parentRouter.getType = sandbox.stub().returns('tags');
+            parentRouter.permalinks = {
+                getValue: sandbox.stub().returns('/tag/:slug/')
+            };
+
+            req.app._router.stack = [{
+                name: 'SiteRouter',
+                handle: {
+                    stack: [{
+                        name: 'StaticPagesRouter',
+                        handle: {}
+                    }]
+                }
+            }];
+
+            parentRouter._respectDominantRouter(req, res, next, 'bacon');
+            next.called.should.eql(true);
+            urlService.utils.redirect301.called.should.be.false();
+        });
+    });
+
     describe('fn: isRedirectEnabled', function () {
         it('no data key defined', function () {
             const parentRouter = new ParentRouter();
