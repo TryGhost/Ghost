7b599ceacd1ea52f9a62df69dcf49df59d1a1154
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-07-02 14:57:33 +0100

    Handle empty array of slack setting objects
    no issue
    - ensure at least one SlackIntegration object exists in the slack setting when deserialising
    - remove blank SlackIntegration objects when serialising

--- ghost/admin/app/transforms/slack-settings.js
+++ ghost/admin/app/transforms/slack-settings.js
@@ -1,19 +1,25 @@
 /* eslint-disable camelcase */
 import SlackObject from 'ghost-admin/models/slack-integration';
 import Transform from 'ember-data/transform';
-import {A as emberA, isArray as isEmberArray} from '@ember/array';
+import {isArray as isEmberArray} from '@ember/array';
+import {isEmpty} from '@ember/utils';
 
 export default Transform.extend({
     deserialize(serialized) {
-        let slackObj, settingsArray;
+        let settingsArray;
         try {
             settingsArray = JSON.parse(serialized) || [];
         } catch (e) {
             settingsArray = [];
         }
 
-        slackObj = settingsArray.map(itemDetails => SlackObject.create(itemDetails));
-        return emberA(slackObj);
+        if (isEmpty(settingsArray)) {
+            settingsArray.push({url: ''});
+        }
+
+        let slackObjs = settingsArray.map(itemDetails => SlackObject.create(itemDetails));
+
+        return slackObjs;
     },
 
     serialize(deserialized) {
@@ -21,8 +27,9 @@ export default Transform.extend({
         if (isEmberArray(deserialized)) {
             settingsArray = deserialized.map((item) => {
                 let url = (item.get('url') || '').trim();
-
-                return {url};
+                if (url) {
+                    return {url};
+                }
             }).compact();
         } else {
             settingsArray = [];
