4349d7d64238065c18f5f859d8257dce53d98891
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-07-10 23:03:25 +0100

    🎨 Koenig - Added ID attributes to heading tags when rendering (#9720)
    refs https://github.com/TryGhost/Ghost/issues/9623
    
    - added `DomModifier` class to walk a SimpleDom document and modify as needed
      - adds `id` attributes to `h1`, `h2`, etc heading tags
        - converts H* tag content to a dasherized string for the id attribute (dasherized id's are different to the smushed ids that are generated by our markdown converted but there are no backwards-compatibility concerns here)
        - if a duplicate id is detected then add a `-1`, `-2`, etc suffix to the id
    - use `DomModifier` after converting mobiledoc to SimpleDom but before serialising to html
    - switched top-level var declarations to es6

--- core/test/unit/lib/mobiledoc/converters/mobiledoc-converter_spec.js
+++ core/test/unit/lib/mobiledoc/converters/mobiledoc-converter_spec.js
@@ -109,5 +109,94 @@ describe('Mobiledoc converter', function () {
 
             converter.render(mobiledoc, 2).should.eql('<div class="kg-post">\n<p>Test</p>\n</div>');
         });
+
+        it('adds id attributes to headings', function () {
+            let mobiledoc = {
+                version: '0.3.1',
+                atoms: [],
+                cards: [],
+                markups: [
+                    ['a', ['href', 'http://example.com']]
+                ],
+                sections: [
+                    [1, 'h1', [
+                        [0, [], 0, 'Heading One']
+                    ]],
+                    [1, 'h2', [
+                        [0, [], 0, 'Heading Two']
+                    ]],
+                    [1, 'h3', [
+                        [0, [], 0, 'Heading Three']
+                    ]],
+                    [1, 'h4', [
+                        [0, [], 0, 'Heading Four']
+                    ]],
+                    [1, 'h5', [
+                        [0, [], 0, 'Heading Five']
+                    ]],
+                    [1, 'h6', [
+                        [0, [], 0, 'Heading Six']
+                    ]],
+                    // duplicate text
+                    [1, 'h1', [
+                        [0, [], 0, 'Heading One']
+                    ]],
+                    [1, 'h3', [
+                        [0, [], 0, 'Heading One']
+                    ]],
+                    // invalid attr chars
+                    [1, 'h1', [
+                        [0, [], 0, '< left < arrow <']
+                    ]],
+                    [1, 'h1', [
+                        [0, [], 0, '> right > arrow >']
+                    ]],
+                    [1, 'h1', [
+                        [0, [], 0, '"quote" "test"']
+                    ]],
+                    [1, 'h1', [
+                        [0, [], 0, '? question?']
+                    ]],
+                    [1, 'h1', [
+                        [0, [], 0, '& ampersand&']
+                    ]],
+                    // trailing link
+                    [1, 'h1', [
+                        [0, [], 0, 'trailing '],
+                        [0, [0], 1, 'link']
+                    ]],
+                    // preceding link
+                    [1, 'h1', [
+                        [0, [0], 1, 'preceding'],
+                        [0, [], 0, ' link']
+                    ]]
+                ]
+            };
+
+            let output = converter.render(mobiledoc, 2);
+
+            // normal headings
+            output.should.match(/<h1 id="heading-one">Heading One<\/h1>/);
+            output.should.match(/<h2 id="heading-two">Heading Two<\/h2>/);
+            output.should.match(/<h3 id="heading-three">Heading Three<\/h3>/);
+            output.should.match(/<h4 id="heading-four">Heading Four<\/h4>/);
+            output.should.match(/<h5 id="heading-five">Heading Five<\/h5>/);
+            output.should.match(/<h6 id="heading-six">Heading Six<\/h6>/);
+
+            // duplicate heading text
+            output.should.match(/<h1 id="heading-one-1">Heading One<\/h1>/);
+            output.should.match(/<h3 id="heading-one-2">Heading One<\/h3>/);
+
+            // invalid ID/hash-url chars
+            output.should.match(/<h1 id="left-arrow">&lt; left &lt; arrow &lt;<\/h1>/);
+            output.should.match(/<h1 id="right-arrow">&gt; right &gt; arrow &gt;<\/h1>/);
+            output.should.match(/<h1 id="quote-test">"quote" "test"<\/h1>/);
+            output.should.match(/<h1 id="question">\? question\?<\/h1>/);
+            output.should.match(/<h1 id="ampersand">&amp; ampersand&amp;<\/h1>/);
+
+            // heading with link
+            output.should.match(/<h1 id="trailing-link">trailing <a href="http:\/\/example\.com">link<\/a><\/h1>/);
+            output.should.match(/<h1 id="preceding-link"><a href="http:\/\/example\.com">preceding<\/a> link<\/h1>/);
+        });
     });
 });
