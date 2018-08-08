022c9f0a00460c551be43409a0354584ce27b9d2
Katharina Irrgang <katharina.irrgang@googlemail.com>
2018-08-08 14:24:56 +0200

    🔥 Removed demo post (#9769)
    refs #9742
    
    - removed demo post fixture for fresh blogs
    - added migration to remove the demo post from the database
    - on rollback: bring back demo post

--- core/test/functional/routes/api/posts_spec.js
+++ core/test/functional/routes/api/posts_spec.js
@@ -291,7 +291,7 @@ describe('Post API', function () {
                         var jsonResponse = res.body;
                         should.exist(jsonResponse.posts);
                         testUtils.API.checkResponse(jsonResponse, 'posts');
-                        jsonResponse.posts.should.have.length(2);
+                        jsonResponse.posts.should.have.length(1);
                         testUtils.API.checkResponse(jsonResponse.posts[0], 'post');
                         testUtils.API.checkResponse(jsonResponse.meta.pagination, 'pagination');
                         done();
