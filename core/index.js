72cb6c048c3f37df094f2875250b6693819b9127
AnanT <freezboltz@gmail.com>
2018-08-08 14:54:06 +0530

    changed let to const line-3 core/index.js

--- core/index.js
+++ core/index.js
@@ -1,6 +1,6 @@
 // ## Server Loader
 // Passes options through the boot process to get a server instance back
-let server = require('./server');
+const server = require('./server');
 
 // Set the default environment to be `development`
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
