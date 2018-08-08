34fcf34e7c52499b293c5ca5ff1869f08e06f427
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-08-08 17:32:36 +0100

    Dropped IE11 browser target for transpilation

--- ghost/admin/config/targets.js
+++ ghost/admin/config/targets.js
@@ -7,13 +7,6 @@ const browsers = [
     'last 2 Edge versions'
 ];
 
-const isCI = !!process.env.CI;
-const isProduction = process.env.EMBER_ENV === 'production';
-
-if (isCI || isProduction) {
-    browsers.push('ie 11');
-}
-
 module.exports = {
     browsers
 };
