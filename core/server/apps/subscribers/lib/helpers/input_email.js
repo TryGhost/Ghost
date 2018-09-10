1302935471b57ac317a2f2f7a187f702f43a8cfc
Tien Do <tiendq@gmail.com>
2018-09-10 16:06:41 +0700

    ES6 migration: server/apps/subscribers (#9688)
    refs #9589
    
    - use const, let, arrow function, string template

--- core/server/apps/subscribers/lib/helpers/input_email.js
+++ core/server/apps/subscribers/lib/helpers/input_email.js
@@ -4,39 +4,38 @@
 // Used by `{{subscribe_form}}`
 
 // (less) dirty requires
-var proxy = require('../../../../helpers/proxy'),
-    SafeString = proxy.SafeString,
-    templates = proxy.templates;
+const proxy = require('../../../../helpers/proxy');
+const SafeString = proxy.SafeString;
+const templates = proxy.templates;
 
 // We use the name input_email to match the helper for consistency:
 module.exports = function input_email(options) { // eslint-disable-line camelcase
     options = options || {};
     options.hash = options.hash || {};
 
-    var className = (options.hash.class) ? options.hash.class : 'subscribe-email',
-        extras = '',
-        output;
+    const className = (options.hash.class) ? options.hash.class : 'subscribe-email';
+    let extras = '';
 
     if (options.hash.autofocus) {
         extras += 'autofocus="autofocus"';
     }
 
     if (options.hash.placeholder) {
-        extras += ' placeholder="' + options.hash.placeholder + '"';
+        extras += ` placeholder="${options.hash.placeholder}"`;
     }
 
     if (options.hash.value) {
-        extras += ' value="' + options.hash.value + '"';
+        extras += ` value="${options.hash.value}"`;
     }
 
     if (options.hash.id) {
         extras += ' id="' + options.hash.id + '"';
     }
 
-    output = templates.input({
+    const output = templates.input({
         type: 'email',
         name: 'email',
-        className: className,
+        className,
         extras: (extras ? extras.trim() : '')
     });
 
