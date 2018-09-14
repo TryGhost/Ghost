7f054585a5dc403f91383bc1286364b91c15d4f3
Kevin Ansfield <kevin@lookingsideways.co.uk>
2018-09-14 13:06:14 +0100

    enforce Administrator/null roles for admin/content api keys

--- core/server/models/api-key.js
+++ core/server/models/api-key.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto');
 const ghostBookshelf = require('./base');
+const {Role} = require('./role');
 
 const ApiKey = ghostBookshelf.Model.extend({
     tableName: 'api_keys',
@@ -19,9 +20,37 @@ const ApiKey = ghostBookshelf.Model.extend({
 
     integration() {
         return this.belongsTo('Integration');
+    },
+
+    onSaving(model, attrs, options) {
+        let tasks = [];
+
+        ghostBookshelf.Model.prototype.onSaving.apply(this, arguments);
+
+        // enforce roles which are currently hardcoded
+        // - admin key = Adminstrator role
+        // - content key = no role
+        if (this.hasChanged('type')) {
+            if (this.get('type') === 'admin') {
+                tasks.setAdminRole = Role.findOne({name: 'Administrator'}, {columns: ['id']})
+                    .then((role) => {
+                        this.set('role_id', role.get('id'));
+                    });
+            }
+
+            if (this.get('type') === 'content') {
+                this.set('role_id', null);
+            }
+        }
+
+        return Promise.props(tasks);
     }
 }, {
+    add(data, unfilteredOptions) {
+        const options = ApiKey.filterOptions(unfilteredOptions, 'add');
 
+        return ghostBookshelf.Model.add.call(this, data, options);
+    }
 });
 
 const ApiKeys = ghostBookshelf.Collection.extend({
--- core/test/unit/models/api_key_spec.js
+++ core/test/unit/models/api_key_spec.js
@@ -33,5 +33,32 @@ describe('Unit: models/api_key', function () {
                     });
             });
         });
+
+        it('sets hardcoded role for key type', function () {
+            let role_id = testUtils.DataGenerator.forKnex.roles[0].id;
+
+            let adminCheck = models.ApiKey.add({type: 'admin'}).then((api_key) => {
+                return models.ApiKey.where({id: api_key.id}).fetch({withRelated: ['role']})
+                    .then((api_key) => {
+                        api_key.get('type').should.eql('admin');
+
+                        // defaults
+                        api_key.related('role').should.exist;
+                        api_key.related('role').get('id').should.eql(role_id);
+                    });
+            });
+
+            let contentCheck = models.ApiKey.add({type: 'content'}).then((api_key) => {
+                return models.ApiKey.where({id: api_key.id}).fetch({withRelated: ['role']})
+                    .then((api_key) => {
+                        api_key.get('type').should.eql('content');
+
+                        // defaults
+                        api_key.related('role').should.not.exist;
+                    });
+            });
+
+            return Promise.all([adminCheck, contentCheck]);
+        });
     });
 });
