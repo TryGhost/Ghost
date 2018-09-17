3c3f52913679c9a082f6d1ad7de9918c246f1bc5
Nazar Gargol <nazargargol@gmail.com>
2018-09-17 09:06:07 +0200

    Removed version matching middleware from use in Content API

--- core/server/web/api/v2/content/app.js
+++ core/server/web/api/v2/content/app.js
@@ -8,9 +8,6 @@ const debug = require('ghost-ignition').debug('api'),
 
     // Include the middleware
 
-    // API specific
-    versionMatch = require('../../../middleware/api/version-match'), // global
-
     // Shared
     cacheControl = require('../../../middleware/cache-control'), // global, shared
     maintenance = require('../../../middleware/maintenance'), // global, shared
@@ -34,10 +31,6 @@ module.exports = function setupApiApp() {
 
     // send 503 json response in case of maintenance
     apiApp.use(maintenance);
-
-    // Check version matches for API requests, depends on res.locals.safeVersion being set
-    // Therefore must come after themeHandler.ghostLocals, for now
-    apiApp.use(versionMatch);
 
     // API shouldn't be cached
     apiApp.use(cacheControl('private'));
