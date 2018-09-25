fb18fe50129c756b64917e3200829b818d89ed97
Fabien O'Carroll <fabien@allou.is>
2018-09-25 17:30:09 +0700

    Created Session Model
    refs #9865
    
    - Created session model for session table
    - Added relation to User model

---
+++ core/test/unit/models/session_spec.js
@@ -0,0 +1,54 @@
+const should = require('should');
+const models = require('../../../server/models');
+
+describe('Unit: models/session', function () {
+    before(function () {
+        models.init();
+    });
+
+    describe('parse', function () {
+        const parse = function parse(attrs) {
+            return new models.Session().parse(attrs);
+        };
+
+        it('converts session_data to an object', function () {
+            const attrs = {
+                id: 'something',
+                session_data: JSON.stringify({
+                    some: 'data'
+                })
+            };
+            const parsed = parse(attrs);
+            should.equal(typeof parsed.session_data, 'object');
+            should.equal(parsed.session_data.some, 'data');
+        });
+    });
+
+    describe('format', function () {
+        const format = function format(attrs) {
+            return new models.Session().format(attrs);
+        };
+
+        it('converts session_data to a string', function () {
+            const attrs = {
+                id: 'something',
+                session_data: {
+                    some: 'data'
+                }
+            };
+            const formatted = format(attrs);
+            should.equal(typeof formatted.session_data, 'string');
+            should.equal(formatted.session_data, JSON.stringify({
+                some: 'data'
+            }));
+        });
+
+        it('does not add session_data key if missing', function () {
+            const attrs = {
+                id: 'something'
+            };
+            const formatted = format(attrs);
+            should.equal(formatted.session_data, undefined);
+        });
+    });
+});
