762b53224c4927587590330805d74aa15010d6ed
Fabien O'Carroll <fabien@allou.is>
2018-09-25 16:20:40 +0700

    Created session table migration
    refs #9865
    
    This table will be used for storing user sessions in

--- core/test/unit/data/schema/integrity_spec.js
+++ core/test/unit/data/schema/integrity_spec.js
@@ -19,8 +19,8 @@ var should = require('should'),
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    var currentSchemaHash = '22d24b1de23d118b90e9547fefae5ad7',
-        currentFixturesHash = 'eab42b1e9cd754e76600f1c57c4a7af8';
+    const currentSchemaHash = 'be8d6ff382ae07b238c60d5b453a2944';
+    const currentFixturesHash = 'eab42b1e9cd754e76600f1c57c4a7af8';
 
     // If this test is failing, then it is likely a change has been made that requires a DB version bump,
     // and the values above will need updating as confirmation
