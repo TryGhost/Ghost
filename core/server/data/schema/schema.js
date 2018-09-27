d9f25c32de793fa88e82d2414c94ac61c5151050
Fabien O'Carroll <fabien@allou.is>
2018-09-27 14:51:46 +0700

    Created session table migration (#9908)
    refs #9865
    
    - This table will be used for storing user sessions in

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -300,5 +300,13 @@ module.exports = {
         created_by: {type: 'string', maxlength: 24, nullable: false},
         updated_at: {type: 'dateTime', nullable: true},
         updated_by: {type: 'string', maxlength: 24, nullable: true}
+    },
+    sessions: {
+        id: {type: 'string', maxlength: 24, nullable: false, primary: true},
+        session_id: {type: 'string', maxlength: 32, nullable: false, unique: true},
+        user_id: {type: 'string', maxlength: 24, nullable: false},
+        session_data: {type: 'string', maxlength: 2000, nullable: false},
+        created_at: {type: 'dateTime', nullable: false},
+        updated_at: {type: 'dateTime', nullable: true}
     }
 };
