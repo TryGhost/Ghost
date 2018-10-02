c4a4db7c27b150ce43220b153036f32266231600
kirrg001 <katharina.irrgang@googlemail.com>
2018-10-02 18:36:38 +0200

    Added v2/index.js exports

--- core/server/api/v2/index.js
+++ core/server/api/v2/index.js
@@ -1,4 +1,10 @@
+const shared = require('../shared');
+
 module.exports = {
+    get http() {
+        return shared.http;
+    },
+
     get session() {
         return require('./session');
     }
