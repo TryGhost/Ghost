91f8e032449ebdd5058ecc7140397a5922319cb9
Rish <zrishabhgarg@gmail.com>
2018-10-02 22:19:01 +0530

    Added mobiledoc_revisions schema, migration script and updated tests

--- core/test/functional/routes/api/db_spec.js
+++ core/test/functional/routes/api/db_spec.js
@@ -72,7 +72,7 @@ describe('DB API', function () {
                 var jsonResponse = res.body;
                 should.exist(jsonResponse.db);
                 jsonResponse.db.should.have.length(1);
-                Object.keys(jsonResponse.db[0].data).length.should.eql(24);
+                Object.keys(jsonResponse.db[0].data).length.should.eql(25);
                 done();
             });
     });
@@ -90,7 +90,7 @@ describe('DB API', function () {
                 const jsonResponse = res.body;
                 should.exist(jsonResponse.db);
                 jsonResponse.db.should.have.length(1);
-                Object.keys(jsonResponse.db[0].data).length.should.eql(26);
+                Object.keys(jsonResponse.db[0].data).length.should.eql(27);
                 done();
             });
     });
