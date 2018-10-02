a49bbfb9d61a9cf22421b01d30515ec4ea50867f
Rish <zrishabhgarg@gmail.com>
2018-10-02 16:21:44 +0530

    Updated method `getApiPath` to take options as param
    Refs #9936
    
    - Updated method to take single options param with version and admin instead of separate values
    - Updated urlFor method to use the updated syntax
    - Updated parent-app to use updated syntax

--- core/server/web/parent-app.js
+++ core/server/web/parent-app.js
@@ -39,9 +39,9 @@ module.exports = function setupParentApp(options = {}) {
     // API
     // @TODO: finish refactoring the API app
     // @TODO: decide what to do with these paths - config defaults? config overrides?
-    parentApp.use(urlUtils.getApiPath('deprecated'), require('./api/v0.1/app')());
-    parentApp.use(urlUtils.getApiPath('active'), require('./api/v2/content/app')());
-    parentApp.use(urlUtils.getApiPath('active', true), require('./api/v2/admin/app')());
+    parentApp.use(urlUtils.getApiPath({version: 'deprecated', type: 'content'}), require('./api/v0.1/app')());
+    parentApp.use(urlUtils.getApiPath({version: 'active', type: 'content'}), require('./api/v2/content/app')());
+    parentApp.use(urlUtils.getApiPath({version: 'active', type: 'admin'}), require('./api/v2/admin/app')());
 
     // ADMIN
     parentApp.use('/ghost', require('./admin')());
