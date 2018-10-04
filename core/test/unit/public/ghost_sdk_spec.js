f78d9d39146190a8236368a394a024947065fb9a
Rishabh Garg <zrishabhgarg@gmail.com>
2018-10-04 21:43:12 +0530

    Refactored hardcoded v0.1 url unit tests to support multiple versions (#9945)
    refs #9866
    
    - Added test util method for api path based on version
    - Updated all hardcoded v0.1 tests strings to use dynamic string from util method
    - Updated hardcoded v0.1 tests using regex match to use string equal with new util method

--- core/test/unit/public/ghost_sdk_spec.js
+++ core/test/unit/public/ghost_sdk_spec.js
@@ -1,7 +1,8 @@
-var should = require('should'),
-    ghostSdk = require('../../../server/public/ghost-sdk'),
-    configUtils = require('../../utils/configUtils'),
-    urlService = require('../../../server/services/url');
+const should = require('should');
+const ghostSdk = require('../../../server/public/ghost-sdk');
+const configUtils = require('../../utils/configUtils');
+const urlService = require('../../../server/services/url');
+const testUtils = require('../../utils');
 
 describe('Ghost Ajax Helper', function () {
     beforeEach(function () {
@@ -23,82 +24,123 @@ describe('Ghost Ajax Helper', function () {
         ghostSdk.url.api().should.equal('');
     });
 
-    it('renders basic url correctly when no arguments are presented', function () {
-        ghostSdk.init({
-            clientId: '',
-            clientSecret: '',
-            url: urlService.utils.urlFor('api', {cors: true, version: 'deprecated', versionType: 'content'}, true)
+    ['deprecated', 'active'].forEach((apiVersion) => {
+        describe(`for api version: ${apiVersion}`, function () {
+            it('renders basic url correctly when no arguments are presented', function () {
+                ghostSdk.init({
+                    clientId: '',
+                    clientSecret: '',
+                    url: urlService.utils.urlFor('api', {cors: true, version: apiVersion, versionType: 'content'}, true)
+                });
+
+                ghostSdk.url.api().should.equal(`//testblog.com${testUtils.API.getApiPath({version: apiVersion})}`);
+            });
+
+            it('blog url is https', function () {
+                configUtils.set({
+                    url: 'https://testblog.com/'
+                });
+
+                ghostSdk.init({
+                    clientId: '',
+                    clientSecret: '',
+                    url: urlService.utils.urlFor('api', {cors: true, version: apiVersion, versionType: 'content'}, true)
+                });
+
+                ghostSdk.url.api().should.equal(`https://testblog.com${testUtils.API.getApiPath({version: apiVersion})}`);
+            });
+
+            it('admin url is https', function () {
+                configUtils.set({
+                    url: 'http://testblog.com/',
+                    admin: {
+                        url: 'https://admin.testblog.com'
+                    }
+                });
+
+                ghostSdk.init({
+                    clientId: '',
+                    clientSecret: '',
+                    url: urlService.utils.urlFor('api', {cors: true, version: apiVersion, versionType: 'content'}, true)
+                });
+
+                ghostSdk.url.api().should.equal(`https://admin.testblog.com${testUtils.API.getApiPath({version: apiVersion})}`);
+            });
+
+            it('strips arguments of forward and trailing slashes correctly', function () {
+                ghostSdk.init({
+                    clientId: '',
+                    clientSecret: '',
+                    url: urlService.utils.urlFor('api', {cors: true, version: apiVersion, versionType: 'content'}, true)
+                });
+
+                ghostSdk.url.api('a/', '/b', '/c/').should.equal(`//testblog.com${testUtils.API.getApiPath({version: apiVersion})}a/b/c/`);
+            });
+
+            it('appends client_id & client_secret to query string automatically', function () {
+                ghostSdk.init({
+                    clientId: 'ghost-frontend',
+                    clientSecret: 'notasecret',
+                    url: urlService.utils.urlFor('api', {cors: true, version: apiVersion, versionType: 'content'}, true)
+                });
+
+                ghostSdk.url.api().should.equal(`//testblog.com${testUtils.API.getApiPath({version: apiVersion})}?client_id=ghost-frontend&client_secret=notasecret`);
+            });
+
+            it('generates query parameters correctly', function () {
+                ghostSdk.init({
+                    clientId: 'ghost-frontend',
+                    clientSecret: 'notasecret',
+                    url: urlService.utils.urlFor('api', {cors: true, version: apiVersion, versionType: 'content'}, true)
+                });
+
+                var rendered = ghostSdk.url.api({a: 'string', b: 5, c: 'en coded'});
+                rendered.should.equal(`//testblog.com${testUtils.API.getApiPath({version: apiVersion, versionType: 'content'})}?a=string&b=5&c=en%20coded&client_id=ghost-frontend&client_secret=notasecret`);
+            });
+
+            it('generates complex query correctly', function () {
+                ghostSdk.init({
+                    clientId: 'ghost-frontend',
+                    clientSecret: 'notasecret',
+                    url: urlService.utils.urlFor('api', {cors: true, version: apiVersion, versionType: 'content'}, true)
+                });
+
+                var rendered = ghostSdk.url.api('posts/', '/tags/', '/count', {include: 'tags,tests', page: 2});
+
+                rendered.should.equal(`//testblog.com${testUtils.API.getApiPath({version: apiVersion, versionType: 'content'})}posts/tags/count/?include=tags%2Ctests&page=2&client_id=ghost-frontend&client_secret=notasecret`);
+            });
+
+            it('works with an https config', function () {
+                configUtils.set({
+                    url: 'https://testblog.com/'
+                });
+
+                ghostSdk.init({
+                    clientId: 'ghost-frontend',
+                    clientSecret: 'notasecret',
+                    url: urlService.utils.urlFor('api', {version: apiVersion, versionType: 'content'}, true)
+                });
+
+                var rendered = ghostSdk.url.api('posts/', '/tags/', '/count', {include: 'tags,tests', page: 2});
+
+                rendered.should.equal(`https://testblog.com${testUtils.API.getApiPath({version: apiVersion, versionType: 'content'})}posts/tags/count/?include=tags%2Ctests&page=2&client_id=ghost-frontend&client_secret=notasecret`);
+            });
+
+            it('works with an https config and subdirectory', function () {
+                configUtils.set({
+                    url: 'https://testblog.com/blog/'
+                });
+                ghostSdk.init({
+                    clientId: 'ghost-frontend',
+                    clientSecret: 'notasecret',
+                    url: urlService.utils.urlFor('api', {version: apiVersion, versionType: 'content'}, true)
+                });
+
+                var rendered = ghostSdk.url.api('posts/', '/tags/', '/count', {include: 'tags,tests', page: 2});
+
+                rendered.should.equal(`https://testblog.com/blog${testUtils.API.getApiPath({version: apiVersion, versionType: 'content'})}posts/tags/count/?include=tags%2Ctests&page=2&client_id=ghost-frontend&client_secret=notasecret`);
+            });
         });
-
-        ghostSdk.url.api().should.equal('//testblog.com/ghost/api/v0.1/');
-    });
-
-    it('blog url is https', function () {
-        configUtils.set({
-            url: 'https://testblog.com/'
-        });
-
-        ghostSdk.init({
-            clientId: '',
-            clientSecret: '',
-            url: urlService.utils.urlFor('api', {cors: true, version: 'deprecated', versionType: 'content'}, true)
-        });
-
-        ghostSdk.url.api().should.equal('https://testblog.com/ghost/api/v0.1/');
-    });
-
-    it('admin url is https', function () {
-        configUtils.set({
-            url: 'http://testblog.com/',
-            admin: {
-                url: 'https://admin.testblog.com'
-            }
-        });
-
-        ghostSdk.init({
-            clientId: '',
-            clientSecret: '',
-            url: urlService.utils.urlFor('api', {cors: true, version: 'deprecated', versionType: 'content'}, true)
-        });
-
-        ghostSdk.url.api().should.equal('https://admin.testblog.com/ghost/api/v0.1/');
-    });
-
-    it('strips arguments of forward and trailing slashes correctly', function () {
-        ghostSdk.init({
-            clientId: '',
-            clientSecret: '',
-            url: urlService.utils.urlFor('api', {cors: true, version: 'deprecated', versionType: 'content'}, true)
-        });
-
-        ghostSdk.url.api('a/', '/b', '/c/').should.equal('//testblog.com/ghost/api/v0.1/a/b/c/');
-    });
-
-    it('appends client_id & client_secret to query string automatically', function () {
-        ghostSdk.init({
-            clientId: 'ghost-frontend',
-            clientSecret: 'notasecret',
-            url: urlService.utils.urlFor('api', {cors: true, version: 'deprecated', versionType: 'content'}, true)
-        });
-
-        ghostSdk.url.api().should.equal('//testblog.com/ghost/api/v0.1/?client_id=ghost-frontend&client_secret=notasecret');
-    });
-
-    it('generates query parameters correctly', function () {
-        ghostSdk.init({
-            clientId: 'ghost-frontend',
-            clientSecret: 'notasecret',
-            url: urlService.utils.urlFor('api', {cors: true, version: 'deprecated', versionType: 'content'}, true)
-        });
-
-        var rendered = ghostSdk.url.api({a: 'string', b: 5, c: 'en coded'});
-
-        rendered.should.match(/\/\/testblog\.com\/ghost\/api\/v0\.1\/\?/);
-        rendered.should.match(/client_id=ghost-frontend/);
-        rendered.should.match(/client_secret=notasecret/);
-        rendered.should.match(/a/);
-        rendered.should.match(/b=5/);
-        rendered.should.match(/c=en\%20coded/);
     });
 
     it('handles null/undefined queryOptions correctly', function () {
@@ -120,61 +162,6 @@ describe('Ghost Ajax Helper', function () {
         rendered2.should.match(/test/);
         rendered2.should.match(/client_id=ghost-frontend/);
         rendered2.should.match(/client_secret=notasecret/);
-    });
-
-    it('generates complex query correctly', function () {
-        ghostSdk.init({
-            clientId: 'ghost-frontend',
-            clientSecret: 'notasecret',
-            url: urlService.utils.urlFor('api', {cors: true, version: 'deprecated', versionType: 'content'}, true)
-        });
-
-        var rendered = ghostSdk.url.api('posts/', '/tags/', '/count', {include: 'tags,tests', page: 2});
-
-        rendered.should.match(/\/\/testblog\.com\/ghost\/api\/v0\.1\/posts\/tags\/count\/\?/);
-        rendered.should.match(/client_id=ghost-frontend/);
-        rendered.should.match(/client_secret=notasecret/);
-        rendered.should.match(/include=tags%2Ctests/);
-        rendered.should.match(/page=2/);
-    });
-
-    it('works with an https config', function () {
-        configUtils.set({
-            url: 'https://testblog.com/'
-        });
-
-        ghostSdk.init({
-            clientId: 'ghost-frontend',
-            clientSecret: 'notasecret',
-            url: urlService.utils.urlFor('api', {version: 'deprecated', versionType: 'content'}, true)
-        });
-
-        var rendered = ghostSdk.url.api('posts/', '/tags/', '/count', {include: 'tags,tests', page: 2});
-
-        rendered.should.match(/https:\/\/testblog\.com\/ghost\/api\/v0\.1\/posts\/tags\/count\/\?/);
-        rendered.should.match(/client_id=ghost-frontend/);
-        rendered.should.match(/client_secret=notasecret/);
-        rendered.should.match(/include=tags%2Ctests/);
-        rendered.should.match(/page=2/);
-    });
-
-    it('works with an https config and subdirectory', function () {
-        configUtils.set({
-            url: 'https://testblog.com/blog/'
-        });
-        ghostSdk.init({
-            clientId: 'ghost-frontend',
-            clientSecret: 'notasecret',
-            url: urlService.utils.urlFor('api', {version: 'deprecated', versionType: 'content'}, true)
-        });
-
-        var rendered = ghostSdk.url.api('posts/', '/tags/', '/count', {include: 'tags,tests', page: 2});
-
-        rendered.should.match(/https:\/\/testblog\.com\/blog\/ghost\/api\/v0\.1\/posts\/tags\/count\/\?/);
-        rendered.should.match(/client_id=ghost-frontend/);
-        rendered.should.match(/client_secret=notasecret/);
-        rendered.should.match(/include=tags%2Ctests/);
-        rendered.should.match(/page=2/);
     });
 
     it('should be idempotent', function () {
