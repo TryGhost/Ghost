084f593dd11356de49d76708be094adc27b2a2d4
Fabien O'Carroll <fabien@allou.is>
2018-10-09 11:04:51 +0700

    Added basic doAuth to v2 admin test utils
    refs #9866
    
    This will allow functional tests to use session based authentication.

--- core/test/functional/api/v2/admin/utils.js
+++ core/test/functional/api/v2/admin/utils.js
@@ -1,5 +1,6 @@
 const url = require('url');
 const _ = require('lodash');
+const testUtils = require('../../../../utils');
 const API_URL = '/ghost/api/v2/admin/';
 
 module.exports = {
@@ -7,5 +8,9 @@ module.exports = {
         getApiQuery(route) {
             return url.resolve(API_URL, route);
         }
+    },
+
+    doAuth(...args) {
+        return testUtils.API.doAuth(`${API_URL}session/`, ...args);
     }
 };
