b9f31131c4c165f55052f0f0b2656791621ad7b9
Hannah Wolfe <erisds@gmail.com>
2018-10-09 11:28:13 -0400

    ✨ Wired up parser plugins in html-to-mobiledoc

--- koenig/html-to-mobiledoc/lib/converter.js
+++ koenig/html-to-mobiledoc/lib/converter.js
@@ -1,7 +1,7 @@
 const DOMParser = require('@tryghost/mobiledoc-kit/dist/commonjs/mobiledoc-kit/parsers/dom').default;
 const Builder = require('@tryghost/mobiledoc-kit/dist/commonjs/mobiledoc-kit/models/post-node-builder').default;
 const mobiledocRenderer = require('@tryghost/mobiledoc-kit/dist/commonjs/mobiledoc-kit/renderers/mobiledoc').default;
-
+const parserPlugins = require('@tryghost/kg-parser-plugins');
 const {JSDOM} = require('jsdom');
 
 module.exports.toMobiledoc = (html) => {
@@ -11,7 +11,8 @@ module.exports.toMobiledoc = (html) => {
 
     // 2. Do something vaguely like loadPost
     // https://github.com/ErisDS/mobiledoc-kit/blob/master/src/js/editor/editor.js#L193
-    let parser = new DOMParser(new Builder());
+    // @TODO: make parser plugins extensible
+    let parser = new DOMParser(new Builder(), {plugins: parserPlugins});
     let dom = new JSDOM(sanitizedHTML);
     let post = parser.parse(dom.window.document.body);
 
