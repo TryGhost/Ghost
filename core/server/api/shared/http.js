7e2e41e1de700bad886e5fdbb80c1edc96346ffb
Fabien O'Carroll <fabien@allou.is>
2018-10-09 11:53:40 +0700

    Removed client and client_id from frame.context
    refs #9865
    
    This is unused as we only support oauth on v0.1 API.

--- core/server/api/shared/http.js
+++ core/server/api/shared/http.js
@@ -15,9 +15,7 @@ const http = (apiImpl) => {
             user: req.user,
             context: {
                 api_key: (req.api_key && req.api_key.id) ? req.api_key.id : null,
-                user: ((req.user && req.user.id) || (req.user && models.User.isExternalUser(req.user.id))) ? req.user.id : null,
-                client: (req.client && req.client.slug) ? req.client.slug : null,
-                client_id: (req.client && req.client.id) ? req.client.id : null
+                user: ((req.user && req.user.id) || (req.user && models.User.isExternalUser(req.user.id))) ? req.user.id : null
             }
         });
 
