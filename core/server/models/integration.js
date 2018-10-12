1ba35a8a0d71387adf533f4c894e9b313cc714ae
Fabien O'Carroll <fabien@allou.is>
2018-10-12 16:57:46 +0700

    Updated Integration model to use bookshelf relations (#9995)
    refs #9865 
    
    We use bookshelf relations so that we can create api_key relations easily.

--- core/server/models/integration.js
+++ core/server/models/integration.js
@@ -3,8 +3,18 @@ const ghostBookshelf = require('./base');
 const Integration = ghostBookshelf.Model.extend({
     tableName: 'integrations',
 
+    relationships: ['api_keys'],
+
+    relationshipBelongsTo: {
+        api_keys: 'api_keys'
+    },
+
+    permittedAttributes(...args) {
+        return ghostBookshelf.Model.prototype.permittedAttributes.apply(this, args).concat(this.relationships);
+    },
+
     api_keys: function apiKeys() {
-        return this.hasMany('ApiKey');
+        return this.hasMany('ApiKey', 'integration_id');
     }
 });
 
