1453f130ff0c025f4a64bb9601187f64b0573a55
Fabien O'Carroll <fabien@allou.is>
2018-10-16 12:25:54 +0700

    Updated Integration model to use generateSlug (#10009)
    no-issue
    
    This ensures that slugs are stripped of illegal characters, and that we
    do not create duplicates.

--- core/server/models/integration.js
+++ core/server/models/integration.js
@@ -9,6 +9,17 @@ const Integration = ghostBookshelf.Model.extend({
         api_keys: 'api_keys'
     },
 
+    onSaving(newIntegration, attr, options) {
+        if (this.hasChanged('slug') || !this.get('slug')) {
+            // Pass the new slug through the generator to strip illegal characters, detect duplicates
+            return ghostBookshelf.Model.generateSlug(Integration, this.get('slug') || this.get('name'),
+                {transacting: options.transacting})
+                .then((slug) => {
+                    this.set({slug});
+                });
+        }
+    },
+
     permittedAttributes(...args) {
         return ghostBookshelf.Model.prototype.permittedAttributes.apply(this, args).concat(this.relationships);
     },
