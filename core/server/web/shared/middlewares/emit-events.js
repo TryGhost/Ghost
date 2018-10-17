f5a713e07828ceb9058adc94979507c7851234b5
Rish <zrishabhgarg@gmail.com>
2018-10-17 16:56:15 +0530

    Refactored webhooks service
    - added new trigger service, moved listen service to its own file
    - started listening to new site.changed event
    - cleaned up trigger service to work with new webhook fields
    - cleaned up tests
    - removed redundant trigger method in v0.1 controller

--- core/server/web/shared/middlewares/emit-events.js
+++ core/server/web/shared/middlewares/emit-events.js
@@ -1,12 +1,12 @@
 const common = require('../../../lib/common');
 
 module.exports = function emitEvents(req, res, next) {
-    res.on('finish', function triggerWebhookEvents() {
+    res.on('finish', function triggerEvents() {
         if (res.get('X-Cache-Invalidate') === '/*') {
             common.events.emit('site.changed');
         }
 
-        res.removeListener('finish', triggerWebhookEvents);
+        res.removeListener('finish', triggerEvents);
     });
     next();
 };
