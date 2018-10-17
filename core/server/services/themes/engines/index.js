7a26f6b5f872edee7ba45e99886cefe1ffa62f32
kirrg001 <katharina.irrgang@googlemail.com>
2018-10-17 09:23:56 +0200

    Supported reading ghost api engine
    refs #9866
    
    - we fallback to v0.1 by default
    - we support different formats
    - this opens the box to switch the ghost api version for the whole blog site
    - i had to add a different notation for overrides.json, because the structure is not optimal (i only want the versions, not the shortcuts)

---
+++ core/server/services/themes/engines/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    get create() {
+        return require('./create');
+    }
+};
---
+++ core/test/unit/services/themes/engines/create_spec.js
@@ -0,0 +1,103 @@
+const should = require('should');
+const sinon = require('sinon');
+const themeEngines = require('../../../../../server/services/themes/engines');
+const sandbox = sinon.sandbox.create();
+
+describe('Themes: engines', function () {
+    afterEach(function () {
+        sandbox.restore();
+    });
+
+    it('no engines', function () {
+        const engines = themeEngines.create();
+        engines.should.eql({
+            'ghost-api': 'v0.1'
+        });
+    });
+
+    describe('ghost-api', function () {
+        it('v2', function () {
+            const engines = themeEngines.create({
+                engines: {
+                    'ghost-api': 'v2'
+                }
+            });
+
+            engines.should.eql({
+                'ghost-api': 'v2'
+            });
+        });
+
+        it('v10', function () {
+            const engines = themeEngines.create({
+                engines: {
+                    'ghost-api': 'v10'
+                }
+            });
+
+            engines.should.eql({
+                'ghost-api': 'v0.1'
+            });
+        });
+
+        it('^0.1', function () {
+            const engines = themeEngines.create({
+                engines: {
+                    'ghost-api': '^0.1'
+                }
+            });
+
+            engines.should.eql({
+                'ghost-api': 'v0.1'
+            });
+        });
+
+        it('^2', function () {
+            const engines = themeEngines.create({
+                engines: {
+                    'ghost-api': '^2'
+                }
+            });
+
+            engines.should.eql({
+                'ghost-api': 'v2'
+            });
+        });
+
+        it('2.0.0', function () {
+            const engines = themeEngines.create({
+                engines: {
+                    'ghost-api': '2.0.0'
+                }
+            });
+
+            engines.should.eql({
+                'ghost-api': 'v2'
+            });
+        });
+
+        it('2.17.0', function () {
+            const engines = themeEngines.create({
+                engines: {
+                    'ghost-api': '2.17.0'
+                }
+            });
+
+            engines.should.eql({
+                'ghost-api': 'v2'
+            });
+        });
+
+        it('3', function () {
+            const engines = themeEngines.create({
+                engines: {
+                    'ghost-api': '3'
+                }
+            });
+
+            engines.should.eql({
+                'ghost-api': 'v0.1'
+            });
+        });
+    });
+});
