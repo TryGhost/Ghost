fd958addb64167241f549329fc31231d7383422b
Nazar Gargol <nazargargol@gmail.com>
2018-10-17 13:45:46 +0200

    Migrated update check to use api v2 refs #9866
    - Switched update checker to api v2
    - Updated and cleaned up the corresponding test suite
    - Updated the frame pipeline to respect context passed in with Frame instance
    - Exposed 'active' verison from api index module

--- core/server/api/index.js
+++ core/server/api/index.js
@@ -1,3 +1,5 @@
+const config = require('../config');
 module.exports = require('./v0.1');
 module.exports['v0.1'] = require('./v0.1');
 module.exports.v2 = require('./v2');
+module.exports.active = require(`./${config.get('api:versions:active')}`);
