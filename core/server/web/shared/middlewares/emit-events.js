4010218885bf9f076053d323dc630bde7425688a
Rish <zrishabhgarg@gmail.com>
2018-10-18 22:50:54 +0530

    Updated to use named variable for invalidate all

--- core/server/web/shared/middlewares/emit-events.js
+++ core/server/web/shared/middlewares/emit-events.js
@@ -1,8 +1,9 @@
 const common = require('../../../lib/common');
+const INVALIDATE_ALL = '/*';
 
 module.exports = function emitEvents(req, res, next) {
     res.on('finish', function triggerEvents() {
-        if (res.get('X-Cache-Invalidate') === '/*') {
+        if (res.get('X-Cache-Invalidate') === INVALIDATE_ALL) {
             common.events.emit('site.changed');
         }
 
