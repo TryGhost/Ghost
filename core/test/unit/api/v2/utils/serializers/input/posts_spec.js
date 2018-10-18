915d5612a1691121f6485bc64beb2eb62e5995ff
Rishabh Garg <zrishabhgarg@gmail.com>
2018-10-18 16:48:47 +0530

    🐛 Fixed relative image URLs becoming absolute URLs on save (#10025)
    closes #10024
    
    - Updated input serializers for posts/tags/users to handle absolute urls conversion
    
    -------
    1. Ghost stores relative images urls
    2. API V2 returns images with absolute urls
    3. Ghost-Admin sends absolute urls back on any save e.g. update user
    
    **Current behavior**: This will override the relative image path in db to absolute, which in turn won't get updated in future if domain or protocol changes for e.g.
    **Fix**: On save/update, input serializers converts any absolute image url paths back to relative if the base URL from image fields matches the configured URL

--- core/test/unit/api/v2/utils/serializers/input/posts_spec.js
+++ core/test/unit/api/v2/utils/serializers/input/posts_spec.js
@@ -1,5 +1,6 @@
 const should = require('should');
 const serializers = require('../../../../../../../server/api/v2/utils/serializers');
+const configUtils = require('../../../../../../utils/configUtils');
 
 describe('Unit: v2/utils/serializers/input/posts', function () {
     it('default', function () {
@@ -74,5 +75,91 @@ describe('Unit: v2/utils/serializers/input/posts', function () {
 
         serializers.input.posts.all(apiConfig, frame);
         frame.options.filter.should.eql('page:false');
+    });
+
+    describe('Ensure relative urls are returned for standard image urls', function () {
+        after(function () {
+            configUtils.restore();
+        });
+
+        it('when blog url is without subdir', function () {
+            configUtils.set({url: 'https://mysite.com'});
+            const apiConfig = {};
+            const frame = {
+                options: {
+                    context: {
+                        user: 0,
+                        api_key_id: 1
+                    },
+                    withRelated: ['tags', 'authors']
+                },
+                data: {
+                    posts: [
+                        {
+                            id: 'id1',
+                            feature_image: 'https://mysite.com/content/images/image.jpg',
+                            og_image: 'https://mysite.com/mycustomstorage/images/image.jpg',
+                            twitter_image: 'https://mysite.com/blog/content/images/image.jpg',
+                            tags: [{
+                                id: 'id3',
+                                feature_image: 'http://mysite.com/content/images/image.jpg'
+                            }],
+                            authors: [{
+                                id: 'id4',
+                                name: 'Ghosty',
+                                profile_image: 'https://somestorage.com/blog/images/image.jpg'
+                            }]
+                        }
+                    ]
+                }
+            };
+            serializers.input.posts.edit(apiConfig, frame);
+            let postData = frame.data.posts[0];
+            postData.feature_image.should.eql('/content/images/image.jpg');
+            postData.og_image.should.eql('https://mysite.com/mycustomstorage/images/image.jpg');
+            postData.twitter_image.should.eql('https://mysite.com/blog/content/images/image.jpg');
+            postData.tags[0].feature_image.should.eql('/content/images/image.jpg');
+            postData.authors[0].profile_image.should.eql('https://somestorage.com/blog/images/image.jpg');
+        });
+
+        it('when blog url is with subdir', function () {
+            configUtils.set({url: 'https://mysite.com/blog'});
+            const apiConfig = {};
+            const frame = {
+                options: {
+                    context: {
+                        user: 0,
+                        api_key_id: 1
+                    },
+                    withRelated: ['tags', 'authors']
+                },
+                data: {
+                    posts: [
+                        {
+                            id: 'id1',
+                            feature_image: 'https://mysite.com/blog/content/images/image.jpg',
+                            og_image: 'https://mysite.com/content/images/image.jpg',
+                            twitter_image: 'https://mysite.com/mycustomstorage/images/image.jpg',
+                            tags: [{
+                                id: 'id3',
+                                feature_image: 'http://mysite.com/blog/mycustomstorage/content/images/image.jpg'
+                            }],
+                            authors: [{
+                                id: 'id4',
+                                name: 'Ghosty',
+                                profile_image: 'https://somestorage.com/blog/content/images/image.jpg'
+                            }]
+                        }
+                    ]
+                }
+            };
+            serializers.input.posts.edit(apiConfig, frame);
+            let postData = frame.data.posts[0];
+            postData.feature_image.should.eql('/blog/content/images/image.jpg');
+            postData.og_image.should.eql('https://mysite.com/content/images/image.jpg');
+            postData.twitter_image.should.eql('https://mysite.com/mycustomstorage/images/image.jpg');
+            postData.tags[0].feature_image.should.eql('http://mysite.com/blog/mycustomstorage/content/images/image.jpg');
+            postData.authors[0].profile_image.should.eql('https://somestorage.com/blog/content/images/image.jpg');
+        });
     });
 });
