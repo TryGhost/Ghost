f2f45faad9e888578cdedab7b17397e6a468abfe
Nazar Gargol <nazargargol@gmail.com>
2018-11-05 19:44:21 +0100

    Extracted relationship handling logic to mapper

--- core/server/api/v2/utils/serializers/output/utils/mapper.js
+++ core/server/api/v2/utils/serializers/output/utils/mapper.js
@@ -4,10 +4,30 @@ const date = require('./date');
 
 const mapPost = (model, frame) => {
     const jsonModel = model.toJSON(frame.options);
+
     url.forPost(model.id, jsonModel, frame.options);
 
     if (utils.isContentAPI(frame)) {
         date.forPost(jsonModel);
+    }
+
+    if (frame.options && frame.options.withRelated) {
+        frame.options.withRelated.forEach((relation) => {
+            // @NOTE: this block also decorates primary_tag/primary_author objects as they
+            // are being passed by reference in tags/authors. Might be refactored into more explicit call
+            // in the future, but is good enough for current use-case
+            if (relation === 'tags' && jsonModel.tags) {
+                jsonModel.tags = jsonModel.tags.map(tag => url.forTag(tag.id, tag));
+            }
+
+            if (relation === 'author' && jsonModel.author) {
+                jsonModel.author = url.forUser(jsonModel.author.id, jsonModel.author, frame.options);
+            }
+
+            if (relation === 'authors' && jsonModel.authors) {
+                jsonModel.authors = jsonModel.authors.map(author => url.forUser(author.id, author, frame.options));
+            }
+        });
     }
 
     return jsonModel;
