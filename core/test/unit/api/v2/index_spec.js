c0aa897e5e2b8e8e1403edbf5d1e226237e7bcd1
Rish <zrishabhgarg@gmail.com>
2018-11-06 16:54:47 +0530

    Fixed spacing

--- core/test/unit/api/v2/index_spec.js
+++ core/test/unit/api/v2/index_spec.js
@@ -2,11 +2,12 @@ const should = require('should');
 const sinon = require('sinon');
 const utils = require('../../../../server/api/v2/utils');
 const sandbox = sinon.sandbox.create();
- describe('Unit: v2/utils/index', function () {
+
+describe('Unit: v2/utils/index', function () {
     afterEach(function () {
         sandbox.restore();
     });
-     describe('isContentAPI', function () {
+    describe('isContentAPI', function () {
         it('is truthy when having api key and no user', function () {
             const frame = {
                 options: {
@@ -15,9 +16,9 @@ const sandbox = sinon.sandbox.create();
                     }
                 }
             };
-             should(utils.isContentAPI(frame)).equal(true);
+            should(utils.isContentAPI(frame)).equal(true);
         });
-         it('is falsy when having api key and a user', function () {
+        it('is falsy when having api key and a user', function () {
             const frame = {
                 options: {
                     context: {
@@ -26,16 +27,16 @@ const sandbox = sinon.sandbox.create();
                     }
                 }
             };
-             should(utils.isContentAPI(frame)).equal(false);
+            should(utils.isContentAPI(frame)).equal(false);
         });
-         it('is truthy when context is empty', function () {
+        it('is truthy when context is empty', function () {
             const frame = {
                 options: {
                     context: {
                     }
                 }
             };
-             should(utils.isContentAPI(frame)).equal(true);
+            should(utils.isContentAPI(frame)).equal(true);
         });
     });
 });
