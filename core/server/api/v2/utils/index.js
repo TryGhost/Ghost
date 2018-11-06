ef7109e593ba80ac0aa5052ec07a0f6f2e682a9a
Rishabh Garg <zrishabhgarg@gmail.com>
2018-11-06 17:36:22 +0530

    Added helper to check for content API (#10104)
    no issue
    
    * Added helper to check if API called is content API

--- core/server/api/v2/utils/index.js
+++ core/server/api/v2/utils/index.js
@@ -9,5 +9,16 @@ module.exports = {
 
     get validators() {
         return require('./validators');
+    },
+
+    /**
+     *  TODO: We need to check for public context as permission stage overrides
+     * the whole context object currently: https://github.com/TryGhost/Ghost/issues/10099
+     */
+    isContentAPI: (frame) => {
+        return !!(Object.keys(frame.options.context).length === 0 ||
+                    (!frame.options.context.user && frame.options.context.api_key_id) ||
+                    frame.options.context.public
+        );
     }
 };
