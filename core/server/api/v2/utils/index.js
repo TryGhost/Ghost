a1175c989a1938004242265400a4182ee92fc0d8
Rish <zrishabhgarg@gmail.com>
2018-11-06 17:10:35 +0530

    Added comment to refer permissions issue

--- core/server/api/v2/utils/index.js
+++ core/server/api/v2/utils/index.js
@@ -11,6 +11,10 @@ module.exports = {
         return require('./validators');
     },
 
+    /**
+     *  TODO: We need to check for public context as permission stage overrides
+     * the whole context object currently: https://github.com/TryGhost/Ghost/issues/10099
+     */
     isContentAPI: (frame) => {
         return !!(Object.keys(frame.options.context).length === 0 ||
                     (!frame.options.context.user && frame.options.context.api_key_id) ||
