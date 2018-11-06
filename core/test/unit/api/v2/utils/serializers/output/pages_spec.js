92239850169d8da57e7e497821059703cfb51731
Nazar Gargol <nazargargol@gmail.com>
2018-11-06 15:20:20 +0100

    Refactored pages serializer unit test

--- core/test/unit/api/v2/utils/serializers/output/pages_spec.js
+++ core/test/unit/api/v2/utils/serializers/output/pages_spec.js
@@ -2,7 +2,7 @@ const should = require('should');
 const sinon = require('sinon');
 const testUtils = require('../../../../../../utils');
 const urlService = require('../../../../../../../server/services/url');
-const dateUtil = require('../../../../../../../server/api/v2/utils/serializers/output/utils/date');
+const mapper = require('../../../../../../../server/api/v2/utils/serializers/output/utils/mapper');
 const serializers = require('../../../../../../../server/api/v2/utils/serializers');
 
 const sandbox = sinon.sandbox.create();
@@ -15,21 +15,22 @@ describe('Unit: v2/utils/serializers/output/pages', function () {
             return Object.assign(data, {toJSON: sandbox.stub().returns(data)});
         };
 
-        sandbox.stub(urlService, 'getUrlByResourceId').returns('getUrlByResourceId');
-        sandbox.stub(urlService.utils, 'urlFor').returns('urlFor');
-        sandbox.stub(urlService.utils, 'makeAbsoluteUrls').returns({html: sandbox.stub()});
+        sandbox.stub(mapper, 'mapPost').returns({});
     });
 
     afterEach(function () {
         sandbox.restore();
     });
 
-    describe('Ensure absolute urls are returned by default', function () {
-        it('meta & models & relations', function () {
+    describe('Ensure mapper is being called for each model', function () {
+        it('calls the mapper', function () {
             const apiConfig = {};
             const frame = {
                 options: {
-                    withRelated: ['tags', 'authors']
+                    withRelated: ['tags', 'authors'],
+                    context: {
+                        private: false
+                    }
                 }
             };
 
@@ -38,15 +39,6 @@ describe('Unit: v2/utils/serializers/output/pages', function () {
                     pageModel(testUtils.DataGenerator.forKnex.createPost({
                         id: 'id1',
                         feature_image: 'value',
-                        page: true,
-                        tags: [{
-                            id: 'id3',
-                            feature_image: 'value'
-                        }],
-                        authors: [{
-                            id: 'id4',
-                            name: 'Ghosty'
-                        }]
                     })),
                     pageModel(testUtils.DataGenerator.forKnex.createPost({
                         id: 'id2',
@@ -59,115 +51,8 @@ describe('Unit: v2/utils/serializers/output/pages', function () {
 
             serializers.output.pages.all(ctrlResponse, apiConfig, frame);
 
-            frame.response.pages[0].hasOwnProperty('url').should.be.true();
-            frame.response.pages[0].tags[0].hasOwnProperty('url').should.be.true();
-            frame.response.pages[0].authors[0].hasOwnProperty('url').should.be.true();
-            frame.response.pages[1].hasOwnProperty('url').should.be.true();
-
-            urlService.utils.urlFor.callCount.should.eql(4);
-            urlService.utils.urlFor.getCall(0).args.should.eql(['image', {image: 'value'}, true]);
-            urlService.utils.urlFor.getCall(1).args.should.eql(['home', true]);
-            urlService.utils.urlFor.getCall(2).args.should.eql(['image', {image: 'value'}, true]);
-            urlService.utils.urlFor.getCall(3).args.should.eql(['home', true]);
-
-            urlService.utils.makeAbsoluteUrls.callCount.should.eql(2);
-            urlService.utils.makeAbsoluteUrls.getCall(0).args.should.eql([
-                '## markdown',
-                'urlFor',
-                'getUrlByResourceId',
-                {assetsOnly: true}
-            ]);
-
-            urlService.utils.makeAbsoluteUrls.getCall(1).args.should.eql([
-                '<img href=/content/test.jpf',
-                'urlFor',
-                'getUrlByResourceId',
-                {assetsOnly: true}
-            ]);
-
-            urlService.getUrlByResourceId.callCount.should.eql(4);
-            urlService.getUrlByResourceId.getCall(0).args.should.eql(['id1', {absolute: true}]);
-            urlService.getUrlByResourceId.getCall(1).args.should.eql(['id3', {absolute: true}]);
-            urlService.getUrlByResourceId.getCall(2).args.should.eql(['id4', {absolute: true}]);
-            urlService.getUrlByResourceId.getCall(3).args.should.eql(['id2', {absolute: true}]);
-        });
-    });
-
-    describe('Ensure date fields are being processed with date formatter', function () {
-        let dateStub;
-
-        beforeEach(() => {
-            dateStub = sandbox.stub(dateUtil, 'format');
-        });
-
-        it('date formatter is being called for single item in public context', function () {
-            const apiConfig = {};
-            const frame = {
-                options: {
-                    context: {
-                        public: true
-                    }
-                }
-            };
-
-            const ctrlResponse = pageModel(testUtils.DataGenerator.forKnex.createPost({
-                page: true
-            }));
-
-            serializers.output.pages.all(ctrlResponse, apiConfig, frame);
-
-            dateStub.callCount.should.equal(3);
-        });
-
-        it('date formatter is being called in public context', function () {
-            const apiConfig = {};
-            const frame = {
-                options: {
-                    context: {
-                        public: true
-                    }
-                }
-            };
-
-            const ctrlResponse = {
-                data: [
-                    pageModel(testUtils.DataGenerator.forKnex.createPost({
-                        page: true
-                    })),
-                    pageModel(testUtils.DataGenerator.forKnex.createPost({
-                        page: true
-                    }))
-                ],
-                meta: {}
-            };
-
-            serializers.output.pages.all(ctrlResponse, apiConfig, frame);
-
-            dateStub.callCount.should.equal(6);
-        });
-
-        it('date formatter is not being called in private context', function () {
-            const apiConfig = {};
-            const frame = {
-                options: {
-                    context: {
-                        public: false
-                    }
-                }
-            };
-
-            const ctrlResponse = {
-                data: [
-                    pageModel(testUtils.DataGenerator.forKnex.createPost({
-                        page: true
-                    }))
-                ],
-                meta: {}
-            };
-
-            serializers.output.pages.all(ctrlResponse, apiConfig, frame);
-
-            dateStub.called.should.be.false;
+            mapper.mapPost.callCount.should.equal(2);
+            mapper.mapPost.getCall(0).args.should.eql([ctrlResponse.data[0], frame]);
         });
     });
 });
