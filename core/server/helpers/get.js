59c2052a17c30b42399d50d0a835b9b2ac8cc02a
kirrg001 <katharina.irrgang@googlemail.com>
2018-11-06 14:08:13 +0100

    Extended resource lookup in {{get}} helper
    refs #10061
    
    - prep task to add authors for content API

--- core/server/helpers/get.js
+++ core/server/helpers/get.js
@@ -12,12 +12,33 @@ var proxy = require('./proxy'),
 
     api = proxy.api,
     labs = proxy.labs,
-    resources,
     pathAliases,
     get;
 
-// Endpoints that the helper is able to access
-resources = ['posts', 'tags', 'users', 'pages'];
+/**
+ * v0.1: users, posts, tags
+ * v2: authors, pages, posts, tags
+ *
+ * @NOTE: if you use "users" in v2, we should fallback to authors
+ */
+const RESOURCES = {
+    posts: {
+        alias: 'posts',
+        resource: 'posts'
+    },
+    tags: {
+        alias: 'tags',
+        resource: 'tags'
+    },
+    users: {
+        alias: 'authors',
+        resource: 'users'
+    },
+    pages: {
+        alias: 'pages',
+        resource: 'posts'
+    }
+};
 
 // Short forms of paths which we should understand
 pathAliases = {
@@ -32,7 +53,7 @@ pathAliases = {
  * @param {Object} options
  * @returns {boolean}
  */
-function isBrowse(resource, options) {
+function isBrowse(options) {
     var browse = true;
 
     if (options.id || options.slug) {
@@ -108,7 +129,6 @@ get = function get(resource, options) {
     const data = createFrame(options.data);
     const apiVersion = data.root._locals.apiVersion;
     let apiOptions = options.hash;
-    let apiMethod;
 
     if (!options.fn) {
         data.error = i18n.t('warnings.helpers.mustBeCalledAsBlock', {helperName: 'get'});
@@ -116,18 +136,19 @@ get = function get(resource, options) {
         return Promise.resolve();
     }
 
-    if (!_.includes(resources, resource)) {
+    if (!RESOURCES[resource]) {
         data.error = i18n.t('warnings.helpers.get.invalidResource');
         logging.warn(data.error);
         return Promise.resolve(options.inverse(self, {data: data}));
     }
 
-    // Determine if this is a read or browse
-    apiMethod = isBrowse(resource, apiOptions) ? api[apiVersion][resource].browse : api[apiVersion][resource].read;
+    const controller = api[apiVersion][RESOURCES[resource].alias] ? RESOURCES[resource].alias : RESOURCES[resource].resource;
+    const action = isBrowse(apiOptions) ? 'browse' : 'read';
+
     // Parse the options we're going to pass to the API
     apiOptions = parseOptions(this, apiOptions);
 
-    return apiMethod(apiOptions).then(function success(result) {
+    return api[apiVersion][controller][action](apiOptions).then(function success(result) {
         var blockParams;
 
         // block params allows the theme developer to name the data using something like
