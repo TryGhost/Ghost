c91a66a940792cd1ce9f497d169e10a60178ec92
Fabien O'Carroll <fabien@allou.is>
2018-11-07 17:41:49 +0700

    🚧 Authorized Content API requests with req.member
    closes #10111
    
    The members labs setting is required to be set for req.member to be
    considered valid authorization

--- core/server/services/auth/authorize.js
+++ core/server/services/auth/authorize.js
@@ -40,11 +40,14 @@ const authorize = {
     authorizeAdminAPI: [session.ensureUser],
     authorizeContentApi(req, res, next) {
         const hasApiKey = req.api_key && req.api_key.id;
+        const hasMember = req.member;
         if (hasApiKey) {
             return next();
-        } else {
-            return next(new common.errors.NoPermissionError({message: common.i18n.t('errors.middleware.auth.pleaseSignInOrAuthenticate')}));
         }
+        if (labs.isSet('members') && hasMember) {
+            return next();
+        }
+        return next(new common.errors.NoPermissionError({message: common.i18n.t('errors.middleware.auth.pleaseSignInOrAuthenticate')}));
     },
 
     requiresAuthorizedUserOrApiKey(req, res, next) {
