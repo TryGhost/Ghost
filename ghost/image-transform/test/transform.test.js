29991c67184408d4c04786f711d40e2fef474013
Nazar Gargol <nazargargol@gmail.com>
2018-11-12 18:52:36 +0100

    🐛  Fixed image optimisation for input image being smaller than optimized one
    closes #10144
    
    - When the input image is well optimized and has smaller byte size than the processed one it's still being used
    - Bumped sharp version to have access to `size` property

--- ghost/image-transform/test/transform.test.js
+++ ghost/image-transform/test/transform.test.js
@@ -1,5 +1,6 @@
 const should = require('should');
 const sinon = require('sinon');
+const fs = require('fs-extra');
 const common = require('../../../../server/lib/common');
 const manipulator = require('../../../../server/lib/image/manipulator');
 const testUtils = require('../../../utils');
@@ -15,11 +16,14 @@ describe('lib/image: manipulator', function () {
         let sharp, sharpInstance;
 
         beforeEach(function () {
+            sandbox.stub(fs, 'readFile').resolves('original');
+            sandbox.stub(fs, 'writeFile').resolves();
+
             sharpInstance = {
                 metadata: sandbox.stub(),
                 resize: sandbox.stub(),
                 rotate: sandbox.stub(),
-                toFile: sandbox.stub()
+                toBuffer: sandbox.stub(),
             };
 
             sharp = sandbox.stub().callsFake(() => {
@@ -32,35 +36,82 @@ describe('lib/image: manipulator', function () {
 
         it('resize image', function () {
             sharpInstance.metadata.resolves({width: 2000, height: 2000});
-            sharpInstance.toFile.resolves();
+            sharpInstance.toBuffer.resolves({
+                data: 'manipulated',
+                info: {
+                    size: 42
+                }
+            });
 
             return manipulator.process({width: 1000})
                 .then(() => {
                     sharp.cache.calledOnce.should.be.true();
                     sharpInstance.metadata.calledOnce.should.be.true();
-                    sharpInstance.toFile.calledOnce.should.be.true();
                     sharpInstance.resize.calledOnce.should.be.true();
                     sharpInstance.rotate.calledOnce.should.be.true();
+
+                    fs.writeFile.calledOnce.should.be.true();
+                    fs.writeFile.calledWith('manipulated');
                 });
         });
 
         it('skip resizing if image is too small', function () {
             sharpInstance.metadata.resolves({width: 50, height: 50});
-            sharpInstance.toFile.resolves();
+            sharpInstance.toBuffer.resolves({
+                data: 'manipulated',
+                info: {
+                    size: 42
+                }
+            });
 
             return manipulator.process({width: 1000})
                 .then(() => {
                     sharp.cache.calledOnce.should.be.true();
                     sharpInstance.metadata.calledOnce.should.be.true();
-                    sharpInstance.toFile.calledOnce.should.be.true();
                     sharpInstance.resize.calledOnce.should.be.false();
                     sharpInstance.rotate.calledOnce.should.be.true();
+
+                    fs.writeFile.calledOnce.should.be.true();
+                    fs.writeFile.calledWith('manipulated');
+                });
+        });
+
+        it('uses original image as an output when the size (bytes) is bigger after manipulation', function () {
+            sharpInstance.metadata.resolves({
+                width: 2000,
+                size: 10
+            });
+
+            sharpInstance.toBuffer.resolves({
+                data: 'manipulated',
+                info: {
+                    size: 42
+                }
+            });
+
+            return manipulator.process({width: 1000})
+                .then(() => {
+                    sharp.cache.calledOnce.should.be.true();
+                    sharpInstance.metadata.calledOnce.should.be.true();
+                    sharpInstance.resize.calledOnce.should.be.true();
+                    sharpInstance.rotate.calledOnce.should.be.true();
+                    sharpInstance.toBuffer.calledOnce.should.be.true();
+
+                    fs.writeFile.calledOnce.should.be.true();
+                    fs.writeFile.calledWith('original');
                 });
         });
 
         it('sharp throws error during processing', function () {
             sharpInstance.metadata.resolves({width: 500, height: 500});
-            sharpInstance.toFile.rejects(new Error('whoops'));
+            sharpInstance.toBuffer.resolves({
+                data: 'manipulated',
+                info: {
+                    size: 42
+                }
+            });
+
+            fs.writeFile.rejects(new Error('whoops'));
 
             return manipulator.process({width: 2000})
                 .then(() => {
@@ -71,9 +122,10 @@ describe('lib/image: manipulator', function () {
                     err.code.should.eql('IMAGE_PROCESSING');
                     sharp.cache.calledOnce.should.be.true;
                     sharpInstance.metadata.calledOnce.should.be.true();
-                    sharpInstance.toFile.calledOnce.should.be.true();
                     sharpInstance.resize.calledOnce.should.be.false();
                     sharpInstance.rotate.calledOnce.should.be.true();
+
+                    fs.writeFile.calledOnce.should.be.true();
                 });
         });
     });
