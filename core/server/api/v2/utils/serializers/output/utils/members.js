423c2015dc8e7c1274ffc126f72dfa8fba4dd254
Rish <zrishabhgarg@gmail.com>
2018-11-13 17:22:18 +0530

    Cleaned up members content check

--- core/server/api/v2/utils/serializers/output/utils/members.js
+++ core/server/api/v2/utils/serializers/output/utils/members.js
@@ -1,7 +1,18 @@
 const labsUtil = require('../../../../../../services/labs');
+const MEMBER_TAG = '#members';
+
+function hideMembersOnlyContent(attrs, frame) {
+    let hasMemberTag = false;
+    if (labsUtil.isSet('members') && !frame.original.context.member && attrs.tags) {
+        hasMemberTag = attrs.tags.find((tag) => {
+            return (tag.name === MEMBER_TAG);
+        }).length > 0;
+    }
+    return hasMemberTag;
+}
 
 const forPost = (attrs, frame) => {
-    const hideMemberOnlyContent = labsUtil.isSet('members') && !frame.original.context.member;
+    const hideMemberOnlyContent = hideMembersOnlyContent(attrs, frame);
     if (hideMemberOnlyContent) {
         ['plaintext', 'html'].forEach((field) => {
             attrs[field] = '';
