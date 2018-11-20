4611a0276d1fa5804cf10759fa78bb4d7e52dd67
Fabien O'Carroll <fabien@allou.is>
2018-11-20 15:57:14 +0700

    Added /secure/token route to members api lib
    closes #10189

--- core/test/unit/lib/members_spec.js
+++ core/test/unit/lib/members_spec.js
@@ -1,3 +1,6 @@
+const http = require('http');
+const got = require('got');
+const jwt = require('jsonwebtoken');
 const should = require('should');
 const MembersApi = require('../../../server/lib/members');
 
@@ -9,6 +12,30 @@ describe('MembersApi lib', function () {
     describe('MembersApi()', function () {
         it('returns a function', function () {
             should.equal(typeof MembersApi(), 'function');
+        });
+
+        describe('HTTP requests', function () {
+            let apiUrl;
+            before(function (done) {
+                const api = MembersApi();
+                const server = http.createServer(api);
+                server.listen(0, '0.0.0.0', () => {
+                    const {address, port} = server.address();
+                    apiUrl = `http://${address}:${port}`;
+                    done();
+                });
+                after(function (done) {
+                    server.close(() => done());
+                });
+            });
+
+            it('handles POST /secure/token and returns jwt', function () {
+                return got(`${apiUrl}/secure/token`, {method: 'post'})
+                    .then(response => {
+                        should.exist(response.body);
+                        should.exist(jwt.decode(response.body));
+                    });
+            });
         });
     });
 });
