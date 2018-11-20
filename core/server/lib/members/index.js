b9a6393005f4a962a38dbc89b3330241af4b3d50
Fabien O'Carroll <fabien@allou.is>
2018-11-20 15:57:14 +0700

    Added /secure/token route to members api lib
    closes #10189

--- core/server/lib/members/index.js
+++ core/server/lib/members/index.js
@@ -1,5 +1,15 @@
+const {Router} = require('express');
+const jwt = require('jsonwebtoken');
+
 module.exports = function MembersApi() {
-    return function httpHandler(req, res) {
-        res.end();
+    const router = Router();
+
+    router.post('/secure/token', (req, res) => {
+        const token = jwt.sign({}, null, {algorithm: 'none'});
+        return res.end(token);
+    });
+
+    return function httpHandler(req, res, next) {
+        return router.handle(req, res, next);
     };
 };
