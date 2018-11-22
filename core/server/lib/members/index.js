ba39c3c3881470c3998361e7f9d6b2a19f07e0cc
Fabien O'Carroll <fabien@allou.is>
2018-11-22 18:45:09 +0700

    Refactored members api to have two routers

--- core/server/lib/members/index.js
+++ core/server/lib/members/index.js
@@ -1,15 +1,29 @@
-const {Router} = require('express');
+const {Router, static} = require('express');
 const jwt = require('jsonwebtoken');
 
 module.exports = function MembersApi() {
     const router = Router();
 
-    router.post('/secure/token', (req, res) => {
+    const apiRouter = Router();
+
+    apiRouter.post('/token', (req, res) => {
         const token = jwt.sign({}, null, {algorithm: 'none'});
         return res.end(token);
     });
 
-    return function httpHandler(req, res, next) {
+    const staticRouter = Router();
+
+    staticRouter.use(static(require('path').join(__dirname, './static')));
+
+    router.use('/api', apiRouter);
+    router.use('/static', staticRouter);
+
+    function httpHandler(req, res, next) {
         return router.handle(req, res, next);
-    };
+    }
+
+    httpHandler.staticRouter = staticRouter;
+    httpHandler.apiRouter = apiRouter;
+
+    return httpHandler;
 };
