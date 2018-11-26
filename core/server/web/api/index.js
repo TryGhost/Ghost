1f64d49b5d5608ac2d831c1ebdf6aac41cab146d
anaplian <ahuckerby@hotmail.co.uk>
2018-11-26 09:35:38 +0000

    🐛 Fixed missing 404 for unknown API routes (#10070)
    closes #10020
    
    * Append trailing slash to version path

---
+++ core/server/web/api/index.js
@@ -0,0 +1,21 @@
+const debug = require('ghost-ignition').debug('web:api:default:app');
+const express = require('express');
+const urlUtils = require('../../services/url/utils');
+const errorHandler = require('../shared/middlewares/error-handler');
+
+module.exports = function setupApiApp() {
+    debug('Parent API setup start');
+    const apiApp = express();
+
+    // Mount different API versions
+    apiApp.use(urlUtils.getVersionPath({version: 'v0.1'}), require('./v0.1/app')());
+    apiApp.use(urlUtils.getVersionPath({version: 'v2', type: 'content'}), require('./v2/content/app')());
+    apiApp.use(urlUtils.getVersionPath({version: 'v2', type: 'admin'}), require('./v2/admin/app')());
+
+    // Error handling for requests to non-existent API versions
+    apiApp.use(errorHandler.resourceNotFound);
+    apiApp.use(errorHandler.handleJSONResponse);
+
+    debug('Parent API setup end');
+    return apiApp;
+};
