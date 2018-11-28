bc389fa8332d9cf9579ee2456de3737e8e9ff8eb
Fabien O'Carroll <fabien@allou.is>
2018-11-28 16:19:23 +0700

    Added signup form to auth page

--- core/server/lib/members/static/auth/bundle.js
+++ core/server/lib/members/static/auth/bundle.js
@@ -23,10 +23,35 @@
         });
     }
 
-    document.querySelector('form').addEventListener('submit', function (event) {
+    document.querySelector('form.signin').addEventListener('submit', function (event) {
         event.preventDefault();
         const email = event.target.elements.email.value;
         const password = event.target.elements.password.value;
         signin(email, password);
+    });
+
+    function signup(name, email, password) {
+        fetch(`${membersApi}/signup`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name, email, password
+            })
+        }).then((res) => {
+            if (!res.ok) {
+                throw new Error(res.statusCode);
+            }
+            storage.setItem('signedin', true);
+        });
+    }
+
+    document.querySelector('form.signup').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const name = event.target.elements.name.value;
+        const email = event.target.elements.email.value;
+        const password = event.target.elements.password.value;
+        signup(name, email, password);
     });
 })();
