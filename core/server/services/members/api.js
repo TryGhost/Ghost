ac03379e2aea5c2a106017fae217c80a79e3d042
Fabien O'Carroll <fabien@allou.is>
2018-11-28 14:02:59 +0700

    Created createMember function

--- core/server/services/members/api.js
+++ core/server/services/members/api.js
@@ -1,5 +1,14 @@
 const MembersApi = require('../../lib/members');
+const models = require('../../models');
 
-const api = MembersApi();
+function createMember(email, password) {
+    return models.Member.add({email, password: {
+        secret: password
+    }}, {});
+}
+
+const api = MembersApi({
+    createMember
+});
 
 module.exports = api;
