23c4348140d99632bd5679e53a1d1a01257e26e7
Fabien O'Carroll <fabien@allou.is>
2018-11-29 15:29:52 +0700

    Added id to fake member object

--- core/server/services/members/api.js
+++ core/server/services/members/api.js
@@ -9,6 +9,7 @@ const tokenIdx = {};
 
 const db = {
     'id-0': {
+        id: 'id-0',
         email: 'member@member.com',
         name: 'John Member',
         password: 'hunter2'
@@ -20,7 +21,7 @@ function createMember({name, email, password}) {
         return Promise.reject(new Error('Email already exists'));
     }
     const id = 'id-' + Object.keys(db).length;
-    db[id] = {name, email, password};
+    db[id] = {name, email, password, id};
     emailIdx[email] = id;
     return Promise.resolve(db[id]);
 }
@@ -49,6 +50,7 @@ function validateMember({email, password, token}) {
 }
 
 function updateMember({email}, data) {
+    delete data.id;
     const id = emailIdx[email];
     if (!id) {
         return Promise.reject('Could not find user');
