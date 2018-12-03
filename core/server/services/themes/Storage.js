515e23ff1ec2d3ade7b66edd998b44a3cb598aae
Fabien O'Carroll <fabien@allou.is>
2018-12-03 14:08:49 +0700

    Revert "Removed delete method from theme Storage class"
    This reverts commit b45fdb405a05faeaf4bd87e977c4ac64ff96b057.

--- core/server/services/themes/Storage.js
+++ core/server/services/themes/Storage.js
@@ -55,6 +55,10 @@ class ThemeStorage extends LocalFileStorage {
                 });
         };
     }
+
+    delete(fileName) {
+        return fs.remove(path.join(this.storagePath, fileName));
+    }
 }
 
 module.exports = ThemeStorage;
