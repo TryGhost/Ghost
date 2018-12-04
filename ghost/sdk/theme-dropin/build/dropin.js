483d6b4aaa333f537692b695f2c56f913be52c6d
Zimo <peter.zimon@gmail.com>
2018-12-04 19:42:47 +0100

    Update position of auth iFrame

--- ghost/sdk/theme-dropin/build/dropin.js
+++ ghost/sdk/theme-dropin/build/dropin.js
@@ -159,6 +159,7 @@ function create(options) {
     frame.style.width = '100%';
     frame.style.height = '100%';
     frame.style.background = 'transparent';
+    frame.style.top = '0';
     frame.style['z-index'] = '9999';
     frame.src = "".concat(options.blogUrl, "/members/auth");
 
