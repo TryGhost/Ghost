1bf1043d6a8a3f6946391f5b3e3f4542476020ed
Rish <zrishabhgarg@gmail.com>
2018-12-07 13:15:45 +0530

    Shorter regex

--- core/server/lib/members/static/preact/index.js
+++ core/server/lib/members/static/preact/index.js
@@ -7,7 +7,7 @@ const storage = window.localStorage;
 var layer0 = require('./layer0');
 
 function getFreshState() {
-    const [hash, formType, query] = window.location.hash.match(/^#(\w[\w']*(?:-\w+)*)\??(.*)$/) || ['#signin?', 'signin', ''];
+    const [hash, formType, query] = window.location.hash.match(/^#([^?]+)\??(.*)$/) || ['#signin?', 'signin', ''];
     console.log({hash, formType, query})
     return {
         formData: {},
