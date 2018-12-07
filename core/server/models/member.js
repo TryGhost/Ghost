c1423da8f089d653cb177b4b7fe067b5be663441
Fabien O'Carroll <fabien@allou.is>
2018-12-07 18:26:01 +0700

    Moved password field to member table

--- core/server/models/member.js
+++ core/server/models/member.js
@@ -1,86 +1,32 @@
 const ghostBookshelf = require('./base');
+const security = require('../lib/security');
 
 const Member = ghostBookshelf.Model.extend({
     tableName: 'members',
 
-    relationships: ['password'],
+    onSaving() {
+        ghostBookshelf.Model.prototype.onSaving.apply(this, arguments);
 
-    relationshipBelongsTo: {
-        password: 'member_passwords'
+        if (this.hasChanged('password')) {
+            return security.password.hash(String(this.get('password')))
+                .then((hash) => {
+                    this.set('password', hash);
+                });
+        }
     },
 
-    emitChange: function emitChange(event, options) {
-        const eventToTrigger = 'member' + '.' + event;
-        ghostBookshelf.Model.prototype.emitChange.bind(this)(this, eventToTrigger, options);
+    comparePassword(rawPassword) {
+        return security.password.compare(rawPassword, this.get('password'));
     },
 
-    onCreated: function onCreated(model, attrs, options) {
-        model.emitChange('added', options);
-    },
-
-    onUpdated: function onUpdated(model, attrs, options) {
-        model.emitChange('edited', options);
-    },
-
-    onDestroyed: function onDestroyed(model, options) {
-        model.emitChange('deleted', options);
-    },
-
-    toJSON: function toJSON(unfilteredOptions) {
+    toJSON(unfilteredOptions) {
         var options = Member.filterOptions(unfilteredOptions, 'toJSON'),
             attrs = ghostBookshelf.Model.prototype.toJSON.call(this, options);
 
         // remove password hash and tokens for security reasons
         delete attrs.password;
-        delete attrs.tokens;
 
         return attrs;
-    },
-
-    add(data, options) {
-        const addMember = () => {
-            return ghostBookshelf.Model.add.call(this, data, options)
-                .then(({id}) => {
-                    return this.findOne({id}, options);
-                });
-        };
-
-        if (!options.transacting) {
-            return ghostBookshelf.transaction((transacting) => {
-                options.transacting = transacting;
-
-                return addMember();
-            });
-        }
-
-        return addMember();
-    },
-
-    edit(data, options) {
-        const editMember = () => {
-            return ghostBookshelf.Model.edit.call(this, data, options)
-                .then(({id}) => {
-                    return this.findOne({id}, options);
-                });
-        };
-
-        if (!options.transacting) {
-            return ghostBookshelf.transaction((transacting) => {
-                options.transacting = transacting;
-
-                return editMember();
-            });
-        }
-
-        return editMember();
-    },
-
-    password: function password() {
-        return this.hasOne('MemberPassword', 'member_id');
-    },
-
-    permittedAttributes(...args) {
-        return ghostBookshelf.Model.prototype.permittedAttributes.apply(this, args).concat(this.relationships);
     }
 });
 
