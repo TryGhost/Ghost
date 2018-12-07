9b105e7590cebaad8098fdafefd9cde328363429
Fabien O'Carroll <fabien@allou.is>
2018-12-07 16:02:23 +0700

    Members update (#24)
    * Ignored build
    
    * Deleted build dir
    
    * Updated layer1 to interface with simplified gateway
    
    * Updated layer1 to only handle gateway
    
    * Updated layer2 to handle gateway & auth, but no DOM
    
    * Updated theme dropin to handle DOM
    
    * Updated layer2;
    
    * Added password-reset flow to the member dropin script
    
    * Reload page after password reset

--- ghost/sdk/layer2/index.js
+++ ghost/sdk/layer2/index.js
@@ -1,64 +1,85 @@
-var Members = require('@tryghost/members-layer1');
+var layer1 = require('@tryghost/members-layer1');
 
-function show (el) {
-    el.style.display = 'block';
-}
-function hide (el) {
-    el.style.display = 'none';
-}
+module.exports = function layer2(options) {
+    var authUrl = `${options.membersUrl}/auth`;
+    var gatewayUrl = `${options.membersUrl}/gateway`;
 
-module.exports = {
-    init: function init(options) {
-        var members = Members.create({
-            blogUrl: options.blogUrl
+    var members = layer1({
+        gatewayUrl
+    });
+
+    var loadAuth = loadFrame(authUrl).then(function (frame) {
+        frame.style.position = 'fixed';
+        frame.style.width = '100%';
+        frame.style.height = '100%';
+        frame.style.background = 'transparent';
+        frame.style.top = '0';
+        frame.style['z-index'] = '9999';
+        return frame;
+    });
+
+    function closeAuth() {
+        return loadAuth.then(function (frame) {
+            frame.style.display = 'none';
+            return frame;
         });
-
-        var signin = document.querySelector('[data-members-signin]');
-        var signinCta = document.querySelector('[data-members-signin-cta]');
-        var signout = document.querySelector('[data-members-signout]');
-
-        function render (token) {
-            if (token) {
-                show(signout);
-                hide(signin);
-            } else {
-                show(signin);
-                hide(signout);
-            }
-            return token;
-        }
-
-        function reload () {
-            if (options.reload) {
-                window.location.reload()
-            }
-        }
-
-        function onSigninEvent (event) {
-            event.preventDefault();
-            members.login()
-                .then(members.getToken)
-                .then(render)
-                .then(reload);
-        }
-
-        signin.addEventListener('click', onSigninEvent);
-        signinCta && signinCta.addEventListener('click', onSigninEvent);
-
-        signout.addEventListener('click', function (event) {
-            event.preventDefault();
-            members.logout()
-                .then(members.getToken)
-                .then(render)
-                .then(reload);
-        });
-
-        return members.getToken()
-            .then(render)
-            .then(function () {
-                return {
-                    getToken: members.getToken
-                };
-            });
     }
+
+    function openAuth(hash, query = '') {
+        return loadAuth.then(function (frame) {
+            frame.src = `${authUrl}#${hash}?${query}`;
+            frame.style.display = 'block';
+            return frame;
+        });
+    }
+
+    function resetPassword({token}) {
+        const query = `token=${token}`;
+        return openAuth('reset-password', query).then(function () {
+            return new Promise(function (resolve) {
+                members.bus.on('signedin', function self() {
+                    members.bus.off('signedin', self);
+                    resolve(true);
+                });
+            });
+        });
+    }
+
+    function signin() {
+        return openAuth('signin').then(function () {
+            return new Promise(function (resolve) {
+                members.bus.on('signedin', function self() {
+                    members.bus.off('signedin', self);
+                    resolve(true);
+                });
+            });
+        });
+    }
+
+    function getToken({audience}) {
+        return members.getToken({audience});
+    }
+
+    function signout() {
+        return members.signout();
+    }
+
+    return Object.assign(members.bus, {
+        getToken,
+        signout,
+        signin,
+        resetPassword
+    });
 };
+
+function loadFrame(src, container = document.body) {
+    return new Promise(function (resolve) {
+        const frame = document.createElement('iframe');
+        frame.style.display = 'none';
+        frame.src = src;
+        frame.onload = function () {
+            resolve(frame);
+        };
+        container.appendChild(frame);
+    });
+}
