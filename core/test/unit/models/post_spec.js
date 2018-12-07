180fdd4e181a40716183455255fbce7d3b4896d5
dexterleng <dexterlengcs@gmail.com>
2018-12-08 01:11:50 +0800

    Strip invisible characters from slug

--- core/test/unit/models/post_spec.js
+++ core/test/unit/models/post_spec.js
@@ -354,6 +354,14 @@ describe('Unit: models/post', function () {
             filter.should.equal('page:false+status:published');
         });
     });
+
+    describe('removeInvisibleUnicode', function () {
+        it('removes unicode backspace from string', function () {
+            const stringWithBackspace = 'abc\u0008';
+            const string = models.Post.removeInvisibleUnicode(stringWithBackspace);
+            should(string).eql('abc');
+        });
+    });
 });
 
 describe('Unit: models/post: uses database (@TODO: fix me)', function () {
