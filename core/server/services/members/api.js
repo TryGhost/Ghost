68fc4620d9791d96228a5634e3178acbf1713bb1
Rish <zrishabhgarg@gmail.com>
2018-12-08 10:54:38 +0530

    Added GhostMailer to Members service, sends reset password emails

--- core/server/services/members/api.js
+++ core/server/services/members/api.js
@@ -3,6 +3,7 @@ const config = require('../../config');
 const MembersApi = require('../../lib/members');
 const models = require('../../models');
 const URL = require('url').URL;
+const mail = require('../mail');
 
 function createMember({name, email, password}) {
     return models.Member.add({
@@ -60,23 +61,41 @@ const sessionSecret = settingsCache.get('members_session_secret');
 const passwordResetUrl = config.get('url');
 const issuer = config.get('url');
 const ssoOrigin = new URL(config.get('url')).origin;
+let mailer;
 
 function sendEmail(member, {token}) {
+    if (!(mailer instanceof mail.GhostMailer)) {
+        mailer = new mail.GhostMailer();
+    }
+    const message = {
+        to: member.email,
+        subject: member.password ? 'Password reset' : 'Confirm email address',
+        html: `
+        Hi ${member.name},
+
+        The link to reset your password is:
+
+            ${passwordResetUrl}#reset-password?token=${token}
+        `
+    };
+
     /* eslint-disable */
     console.log(`
-  From: members@ghost.com
-  To: ${member.email}
+    From: members@ghost.com
+    To: ${member.email}
 
-  Subject: ${member.password ? 'Password reset' : 'Confirm email address'}
+    Subject: ${member.password ? 'Password reset' : 'Confirm email address'}
 
-  Hi ${member.name},
+    Hi ${member.name},
 
-  The link to reset your password is:
+    The link to reset your password is:
 
-      ${passwordResetUrl}#reset-password?token=${token}
-`);
+        ${passwordResetUrl}#reset-password?token=${token}
+  `);
     /* eslint-enable */
-    return Promise.resolve();
+    return mailer.send(message).catch((err) => {
+        return Promise.reject(err);
+    });
 }
 
 const api = MembersApi({
