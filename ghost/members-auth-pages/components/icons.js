2b85a0165079f9063dc280708dc8ccdfa6193151
Fabien O'Carroll <fabien@allou.is>
2018-12-11 13:47:44 +0700

    Added members lib module (#10260)
    * Added members library inc. gateway
    
    refs #10213
    
    * Added the auth pages and build steps for them
    
    refs #10213
    
    * Cleaned up logs
    
    * Updated gruntfile to run yarn for member auth
    
    * Design refinements on members popups
    
    * UI refinements
    
    * Updated backend call to trigger only if frontend validation passes
    
    * Design refinements for error messages
    
    * Added error message for email failure
    
    * Updated request-password-reset to not attempt to send headers twice
    
    * Updated preact publicPath to relative path
    
    * Build auth pages on init

---
+++ ghost/members-auth-pages/components/icons.js
@@ -0,0 +1,20 @@
+export const IconEmail = (
+    <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><title>icon-email</title><g fill="none" fill-rule="evenodd"><path d="M15.619 12.53c0 .646-.38.97-1.143.97H1.524c-.762 0-1.143-.324-1.143-.97V3.47c0-.267.112-.496.335-.686.223-.19.492-.284.808-.284h12.952c.762 0 1.143.324 1.143.97v9.06z" stroke="#B2C2C9" stroke-linecap="round" stroke-linejoin="round" /><path stroke="#B2C2C9" stroke-linecap="round" stroke-linejoin="round" d="M15.238 3L8 10 .762 3" /></g></svg>
+);
+
+export const IconLock = (
+    <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><title>icon-lock</title><g fill="none" fill-rule="evenodd"><path d="M8.75 10.25c0 .5-.25.75-.75.75s-.75-.25-.75-.75.25-.75.75-.75.75.25.75.75zM8 11v2.25" stroke="#B2C2C9" stroke-linecap="round" stroke-linejoin="round" /><path d="M2.706 6.5h10.588c.47 0 .706.214.706.643v7.714c0 .429-.235.643-.706.643H2.706c-.47 0-.706-.214-.706-.643V7.143c0-.429.235-.643.706-.643zM3.875 4.817c0-1.645.687-2.878 2.063-3.7 1.375-.823 2.75-.823 4.125 0 1.374.822 2.062 2.055 2.062 3.7V6.5h-8.25V4.817z" stroke="#B2C2C9" stroke-linecap="round" stroke-linejoin="round" /></g></svg>
+);
+
+export const IconName = (
+    <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><title>icon-name</title><g fill="none" fill-rule="evenodd"><path stroke-opacity=".012" stroke="#000" stroke-width="0" d="M.5.5h15v15H.5z" /><g stroke="#B2C2C9" stroke-linecap="round" stroke-linejoin="round"><path d="M15.187 8c0 1.985-.701 3.679-2.105 5.082C11.68 14.486 9.985 15.187 8 15.187c-1.985 0-3.679-.701-5.082-2.105C1.514 11.68.812 9.985.812 8c0-1.985.702-3.679 2.106-5.082C4.32 1.514 6.015.812 8 .812c1.985 0 3.679.702 5.082 2.106C14.486 4.32 15.187 6.015 15.187 8z" /><path d="M2.974 13.138c1.071-.62 2.199-1.11 3.383-1.47.524-.193.58-1.393.205-1.805-.54-.596-1-1.294-1-2.98-.066-.711.145-1.328.633-1.85.489-.522 1.09-.773 1.805-.754.715-.02 1.316.232 1.805.754.488.522.7 1.139.632 1.85 0 1.688-.458 2.384-1 2.98-.375.412-.318 1.612.205 1.805 1.185.36 2.313.85 3.384 1.47" /></g></g></svg>
+);
+
+export const IconClose = (
+    <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><title>icon-close</title><g fill="none" fill-rule="evenodd"><path d="M2.25 2.25l11.5 11.5M13.75 2.25l-11.5 11.5" stroke="#9BAEB8" stroke-linecap="round" stroke-linejoin="round" /></g></svg>
+);
+
+
+export const IconError = (
+    <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><title>icon-error</title><g fill="none" fill-rule="evenodd"><path d="M15 7.88c.005 1.944-.674 3.61-2.038 4.997C11.6 14.263 9.945 14.97 8 14.999c-1.921.029-3.567-.63-4.937-1.976S1.005 10.043 1 8.123c-.005-1.946.674-3.612 2.037-4.999C4.401 1.737 6.055 1.029 8 1.001c1.921-.029 3.567.63 4.938 1.977 1.37 1.347 2.057 2.98 2.062 4.902zM7.933 9.337V4.67" stroke="#F05230" stroke-linecap="round" stroke-linejoin="round" /><path d="M7.927 11.67c-.046 0-.084.018-.116.051-.031.033-.046.073-.044.119.004.109.06.163.168.163.046 0 .085-.018.116-.051.032-.033.047-.073.045-.119-.003-.105-.057-.16-.163-.163H7.93" stroke="#F05230" stroke-linecap="round" stroke-linejoin="round" /></g></svg>
+);
