4ea111d6c283cfb9f3d1db947640c129765b79d3
Fabien O'Carroll <fabien@allou.is>
2018-12-13 20:25:24 +0700

    ✨ Supported dynamic image resizing for LocalFileStorage(#10184)
    refs #10181 
    
    * Added initial handleImageSizes middleware
    
    * Implemented saveRaw method on local file storage
    
    * Wired up handleImageSizes middleware
    
    * Implemented delete for LocalFileStorage
    
    * Removed delete method from theme Storage class
    
    * Deleted sizes directory when theme is activated
    
    * Ensured that smaller images are not enlarged
    
    * Renamed sizes -> size
    
    * Exited middleware as early as possible
    
    * Called getStorage as late as possible
    
    * Updated image sizes middleware to handle dimension paths
    
    * Revert "Deleted sizes directory when theme is activated"
    
    This reverts commit 9204dfcc73a6a79d597dbf23651817bcbfc59991.
    
    * Revert "Removed delete method from theme Storage class"
    
    This reverts commit b45fdb405a05faeaf4bd87e977c4ac64ff96b057.
    
    * Revert "Implemented delete for LocalFileStorage"
    
    This reverts commit a587cd6bae45b68a293b2d5cfd9b7705a29e7bfa.
    
    * Fixed typo
    
    Co-Authored-By: allouis <fabien@allou.is>
    
    * Redirected to original image if no image_sizes config
    
    * Refactored redirection because rule of three
    
    * Updated comments
    
    * Added rubbish tests
    
    * Added @TODO comment for handleImageSizes tests
    
    * Added safeResizeImage method to image manipulator
    
    * Used image manipulator lib in image_size middleware

---
+++ core/server/web/shared/middlewares/image/handle-image-sizes.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const image = require('../../../../lib/image');
+const storage = require('../../../../adapters/storage');
+const activeTheme = require('../../../../services/themes/active');
+
+const SIZE_PATH_REGEX = /^\/size\/([^/]+)\//;
+
+module.exports = function (req, res, next) {
+    if (!SIZE_PATH_REGEX.test(req.url)) {
+        return next();
+    }
+
+    const [sizeImageDir, requestedDimension] = req.url.match(SIZE_PATH_REGEX);
+    const redirectToOriginal = () => {
+        const url = req.originalUrl.replace(`/size/${requestedDimension}`, '');
+        return res.redirect(url);
+    };
+
+    const imageSizes = activeTheme.get().config('image_sizes');
+    // CASE: no image_sizes config
+    if (!imageSizes) {
+        return redirectToOriginal();
+    }
+
+    const imageDimensions = Object.keys(imageSizes).reduce((dimensions, size) => {
+        const {width, height} = imageSizes[size];
+        const dimension = (width ? 'w' + width : '') + (height ? 'h' + height : '');
+        return Object.assign({
+            [dimension]: imageSizes[size]
+        }, dimensions);
+    }, {});
+
+    const imageDimensionConfig = imageDimensions[requestedDimension];
+    // CASE: unknown dimension
+    if (!imageDimensionConfig || (!imageDimensionConfig.width && !imageDimensionConfig.height)) {
+        return redirectToOriginal();
+    }
+
+    const storageInstance = storage.getStorage();
+    // CASE: unsupported storage adapter but theme is using custom image_sizes
+    if (typeof storageInstance.saveRaw !== 'function') {
+        return redirectToOriginal();
+    }
+
+    storageInstance.exists(req.url).then((exists) => {
+        if (exists) {
+            return;
+        }
+
+        const originalImagePath = path.relative(sizeImageDir, req.url);
+
+        return storageInstance.read({path: originalImagePath})
+            .then((originalImageBuffer) => {
+                return image.manipulator.safeResizeImage(originalImageBuffer, imageDimensionConfig);
+            })
+            .then((resizedImageBuffer) => {
+                return storageInstance.saveRaw(resizedImageBuffer, req.url);
+            });
+    }).then(() => {
+        next();
+    }).catch(next);
+};
