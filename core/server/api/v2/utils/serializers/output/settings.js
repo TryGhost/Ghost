80f9765a35e7f8f79f30a4c1bae4997e7d7945b9
Hannah Wolfe <erisds@gmail.com>
2019-01-02 19:18:19 +0000

    Removed deprecated settings
    refs #10318
    
    - cheap and dirty way of removing settings we don't use
    - rewritten the settingsFilter function as that was unnecessarily complex
    - aslo fixed the require of default-settings in the importer

--- core/server/api/v2/utils/serializers/output/settings.js
+++ core/server/api/v2/utils/serializers/output/settings.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const _private = {};
+const deprecatedSettings = ['force_i18n', 'permalinks'];
 
 /**
  * ### Settings Filter
@@ -10,14 +11,14 @@ const _private = {};
  * @returns {*}
  */
 _private.settingsFilter = (settings, filter) => {
-    return _.fromPairs(_.toPairs(settings).filter((setting) => {
-        if (filter) {
-            return _.some(filter.split(','), (f) => {
-                return setting[1].type === f;
-            });
+    let filteredTypes = filter ? filter.split(',') : false;
+    return _.filter(settings, (setting) => {
+        if (filteredTypes) {
+            return _.includes(filteredTypes, setting.type) && !_.includes(deprecatedSettings, setting.key);
         }
-        return true;
-    }));
+
+        return !_.includes(deprecatedSettings, setting.key);
+    });
 };
 
 module.exports = {
