9929752731c454a27abe1dfd8aa5fcd192e72680
kirrg001 <katharina.irrgang@googlemail.com>
2019-01-04 18:43:37 +0100

    added edge case

--- core/server/api/v2/utils/serializers/output/utils/extra-attrs.js
+++ core/server/api/v2/utils/serializers/output/utils/extra-attrs.js
@@ -1,7 +1,8 @@
 module.exports.forPost = (frame, model, attrs) => {
     const _ = require('lodash');
 
-    if (!frame.options.hasOwnProperty('columns')) {
+    if (!frame.options.hasOwnProperty('columns') ||
+        (frame.options.columns.includes('excerpt') && frame.options.formats && frame.options.formats.includes('plaintext'))) {
         if (_.isEmpty(attrs.custom_excerpt)) {
             const plaintext = model.get('plaintext');
             attrs.excerpt = plaintext.substring(0, 500);
