bce16b30fd2a3556519cd99f0a2f78063ef3f4e0
Nazar Gargol <nazargargol@gmail.com>
2019-01-07 15:59:51 +0000

    Increased timeouts even more

--- core/test/functional/dynamic_routing_spec.js
+++ core/test/functional/dynamic_routing_spec.js
@@ -147,7 +147,7 @@ describe('Dynamic Routing', function () {
 
         describe('Paged', function () {
             // Inserting 25 posts takes a bit longer on Travis for MySQL suite
-            this.timeout(5000);
+            this.timeout(10000);
 
             // Add enough posts to trigger pages for both the index (25 pp) and rss (15 pp)
             before(function (done) {
@@ -388,7 +388,7 @@ describe('Dynamic Routing', function () {
 
         describe('Paged', function () {
             // Inserting 25 posts takes a bit longer on Travis for MySQL suite
-            this.timeout(5000);
+            this.timeout(10000);
 
             before(testUtils.teardown);
 
