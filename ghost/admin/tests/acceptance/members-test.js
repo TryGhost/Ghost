44c5e27a0d2349e4fbadbdbe96f7fb3270a2120d
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-01-22 16:23:26 +0000

    Added empty member management routes

---
+++ ghost/admin/tests/acceptance/members-test.js
@@ -0,0 +1,68 @@
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import {authenticateSession, invalidateSession} from 'ember-simple-auth/test-support';
+import {beforeEach, describe, it} from 'mocha';
+import {click, currentRouteName, currentURL, find} from '@ember/test-helpers';
+import {expect} from 'chai';
+import {setupApplicationTest} from 'ember-mocha';
+import {visit} from '../helpers/visit';
+
+describe('Acceptance: Members', function () {
+    let hooks = setupApplicationTest();
+    setupMirage(hooks);
+
+    it('redirects to signin when not authenticated', async function () {
+        await invalidateSession();
+        await visit('/members');
+
+        expect(currentURL()).to.equal('/signin');
+    });
+
+    it('redirects non-admins to posts', async function () {
+        let role = this.server.create('role', {name: 'Editor'});
+        this.server.create('user', {roles: [role]});
+
+        await authenticateSession();
+        await visit('/members');
+
+        expect(currentURL()).to.equal('/');
+        expect(find('[data-test-nav="members"]'), 'sidebar link')
+            .to.not.exist;
+    });
+
+    describe('as admin', function () {
+        beforeEach(async function () {
+            this.server.loadFixtures('configurations');
+            let config = this.server.schema.configurations.first();
+            config.update({enableDeveloperExperiments: true});
+
+            let role = this.server.create('role', {name: 'Administrator'});
+            this.server.create('user', {roles: [role]});
+
+            return await authenticateSession();
+        });
+
+        it('redirects to posts if developer experiments is disabled', async function () {
+            let config = this.server.schema.configurations.first();
+            config.update({enableDeveloperExperiments: false});
+
+            await visit('/members');
+
+            expect(currentURL()).to.equal('/');
+            expect(find('[data-test-nav="members"]'), 'sidebar link')
+                .to.not.exist;
+        });
+
+        it('shows sidebar link which navigates to members list', async function () {
+            await visit('/');
+
+            expect(find('[data-test-nav="members"]'), 'sidebar link')
+                .to.exist;
+
+            await click('[data-test-nav="members"]');
+
+            expect(currentURL()).to.equal('/members');
+            expect(currentRouteName()).to.equal('members.index');
+            expect(find('[data-test-screen-title]')).to.have.text('Members');
+        });
+    });
+});
