75fbd272c994eaa90614586a53a0db3f564b42ea
Katharina Irrgang <katharina.irrgang@googlemail.com>
2019-01-22 17:54:50 +0100

    Separated test env into: acceptance, regression and unit tests (#10411)
    refs #9178
    
    `yarn test` only runs acceptance and unit tests.
    We will setup a cronjob in Travis and run the regression tests once per day.
    You can manually run them with `yarn test:regression`
    
    This separation is just a first step into the right direction.
    Travis will no longer run for 10-13minutes.
    The goal is to run common API use cases and unit tests in Travis and locally by default.
    
    ## After this separation we still need to:
    
    - re-work our test utility
    - remove some tests
    - define which tests are our common API use cases
    - rewrite some tests
    - make testing easier (starting/stopping Ghost, fixtures and resetting services or event listeners, it's a pain and takes sometimes ages to fix tests)
    
    
    ---
    
    **Acceptance:**
    - common/basic API use cases against the current **stable** API
    
    **Unit:**
    - all unit tests (no database access)
    - proper mocking
    
    **Regression:**
    - packages we don't want to run for each PR or commit
    - tests which protect Ghost from breaking components and behaviour
    - it is wishful that regression tests are using Ghost's API's (frontend, apps, core)
    
    ---
    
    **This PR requires an update to our docs.**

---
+++ core/test/regression/api/v0.1/roles_spec.js
@@ -0,0 +1,84 @@
+const should = require('should');
+const supertest = require('supertest');
+const testUtils = require('../../../utils/index');
+const localUtils = require('./utils');
+const config = require('../../../../server/config/index');
+const ghost = testUtils.startGhost;
+let request;
+
+describe('Roles API', function () {
+    var accesstoken = '', ghostServer;
+
+    before(function () {
+        return ghost()
+            .then(function (_ghostServer) {
+                ghostServer = _ghostServer;
+                request = supertest.agent(config.get('url'));
+            })
+            .then(function () {
+                return localUtils.doAuth(request, 'posts');
+            })
+            .then(function (token) {
+                accesstoken = token;
+            });
+    });
+
+    describe('browse', function () {
+        it('default', function (done) {
+            request.get(localUtils.API.getApiQuery('roles/'))
+                .set('Authorization', 'Bearer ' + accesstoken)
+                .expect('Content-Type', /json/)
+                .expect('Cache-Control', testUtils.cacheRules.private)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    should.not.exist(res.headers['x-cache-invalidate']);
+                    const response = res.body;
+                    should.exist(response);
+                    should.exist(response.roles);
+                    localUtils.API.checkResponse(response, 'roles');
+                    response.roles.should.have.length(6);
+                    localUtils.API.checkResponse(response.roles[0], 'role');
+                    localUtils.API.checkResponse(response.roles[1], 'role');
+                    localUtils.API.checkResponse(response.roles[2], 'role');
+                    localUtils.API.checkResponse(response.roles[3], 'role');
+                    localUtils.API.checkResponse(response.roles[4], 'role');
+                    localUtils.API.checkResponse(response.roles[5], 'role');
+
+                    done();
+                });
+        });
+
+        it('permissions=assign', function (done) {
+            request.get(localUtils.API.getApiQuery('roles/?permissions=assign'))
+                .set('Authorization', 'Bearer ' + accesstoken)
+                .expect('Content-Type', /json/)
+                .expect('Cache-Control', testUtils.cacheRules.private)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) {
+                        return done(err);
+                    }
+
+                    should.not.exist(res.headers['x-cache-invalidate']);
+                    const response = res.body;
+                    should.exist(response.roles);
+                    localUtils.API.checkResponse(response, 'roles');
+                    response.roles.should.have.length(4);
+                    localUtils.API.checkResponse(response.roles[0], 'role');
+                    localUtils.API.checkResponse(response.roles[1], 'role');
+                    localUtils.API.checkResponse(response.roles[2], 'role');
+                    localUtils.API.checkResponse(response.roles[3], 'role');
+                    response.roles[0].name.should.equal('Administrator');
+                    response.roles[1].name.should.equal('Editor');
+                    response.roles[2].name.should.equal('Author');
+                    response.roles[3].name.should.equal('Contributor');
+
+                    done();
+                });
+        });
+    });
+});
