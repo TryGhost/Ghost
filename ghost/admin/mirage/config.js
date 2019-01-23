76a1a98b48ff60bf9e030bb375b84b8301a4ce12
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-01-23 14:24:29 +0000

    Re-enable mirage in development with config to allow asset passthrough

--- ghost/admin/mirage/config.js
+++ ghost/admin/mirage/config.js
@@ -18,6 +18,10 @@ import mockWebhooks from './config/webhooks';
 // import {versionMismatchResponse} from 'utils';
 
 export default function () {
+    // allow any local requests outside of the namespace (configured below) to hit the real server
+    // _must_ be called before the namespace property is set
+    this.passthrough('/ghost/assets/**');
+
     // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
     this.namespace = '/ghost/api/v2/admin'; // make this `api`, for example, if your API is namespaced
     this.timing = 400; // delay for each request, automatically set to 0 during testing
@@ -37,6 +41,7 @@ export default function () {
     // add any external domains to make sure those get passed through too
     this.passthrough('https://count.ghost.org/');
     this.passthrough('http://www.gravatar.com/**');
+    this.passthrough('https://cdn.jsdelivr.net/**');
 }
 
 // Mock all endpoints here as there is no real API during testing
