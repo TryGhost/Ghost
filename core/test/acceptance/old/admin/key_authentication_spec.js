73d8f764b211e8dc72286d31bec737beb1a59d1d
Nazar Gargol <nazargargol@gmail.com>
2019-01-23 12:22:47 +0000

    Added audience check in Admin API key authentication
    refs #9865
    
    - Extracted tests related to Admin API key authenticatoin into separate
    acceptance test suite

---
+++ core/test/acceptance/old/admin/key_authentication_spec.js
@@ -0,0 +1,97 @@
+const should = require('should');
+const supertest = require('supertest');
+const _ = require('lodash');
+const testUtils = require('../../../utils');
+const localUtils = require('./utils');
+const config = require('../../../../server/config');
+
+const ghost = testUtils.startGhost;
+
+// TODO: remove this suite once Admin API key auth is enabled
+describe('Admin API V2 key authentication', function () {
+    let request;
+
+    before(function () {
+        return ghost()
+            .then(function (_ghostServer) {
+                request = supertest.agent(config.get('url'));
+            })
+            .then(function () {
+                return testUtils.initFixtures('api_keys');
+            });
+    });
+
+    it('browse with correct GET endpoint token', function () {
+        return request.get(localUtils.API.getApiQuery('posts/'))
+            .set('Authorization', `Ghost ${localUtils.getValidAdminToken(localUtils.API.getApiQuery('posts/'))}`)
+            .expect('Content-Type', /json/)
+            .expect('Cache-Control', testUtils.cacheRules.private)
+            .expect(403);
+    });
+});
+
+// TODO: enable this suite once Admin API key auth is enabled
+describe.skip('Admin API V2 key authentication', function () {
+    let request;
+
+    before(function () {
+        return ghost()
+            .then(function (_ghostServer) {
+                request = supertest.agent(config.get('url'));
+            })
+            .then(function () {
+                return testUtils.initFixtures('api_keys');
+            });
+    });
+
+    it('do not authenticate without token header', function () {
+        return request.get(localUtils.API.getApiQuery('posts/'))
+            .set('Authorization', `Ghost`)
+            .expect('Content-Type', /json/)
+            .expect('Cache-Control', testUtils.cacheRules.private)
+            .expect(401);
+    });
+
+    it('do not authenticate with wrong endpoint token', function () {
+        return request.get(localUtils.API.getApiQuery('posts/'))
+            .set('Authorization', `Ghost ${localUtils.getValidAdminToken('https://wrong.com')}`)
+            .expect('Content-Type', /json/)
+            .expect('Cache-Control', testUtils.cacheRules.private)
+            .expect(401);
+    });
+
+    it('browse with no endpoint token', function () {
+        return request.get(localUtils.API.getApiQuery('posts/'))
+            .set('Authorization', `Ghost ${localUtils.getValidAdminToken('')}`)
+            .expect('Content-Type', /json/)
+            .expect('Cache-Control', testUtils.cacheRules.private)
+            .expect(401);
+    });
+
+    it('browse with correct GET endpoint token', function () {
+        return request.get(localUtils.API.getApiQuery('posts/'))
+            .set('Authorization', `Ghost ${localUtils.getValidAdminToken(localUtils.API.getApiQuery('posts/'))}`)
+            .expect('Content-Type', /json/)
+            .expect('Cache-Control', testUtils.cacheRules.private)
+            .expect(200);
+    });
+
+    it('browse with correct POST endpoint token', function () {
+        const post = {
+            // @TODO: required for now, needs proper validation
+            author_id: '1',
+            title: 'Post created with api_key'
+        };
+
+        return request
+            .post(localUtils.API.getApiQuery('posts'))
+            .set('Origin', config.get('url'))
+            .set('Authorization', `Ghost ${localUtils.getValidAdminToken(localUtils.API.getApiQuery('posts'))}`)
+            .send({
+                posts: [post]
+            })
+            .expect('Content-Type', /json/)
+            .expect('Cache-Control', testUtils.cacheRules.private)
+            .expect(201);
+    });
+});
