bdc2fce492ceb8763d77b83aca13037935a9377f
Nazar Gargol <nazargargol@gmail.com>
2019-01-28 13:37:34 +0000

    Removed redundant 'status=all'  in user queries
    closes https://github.com/TryGhost/Ghost/issues/5947
    
    - Removed status=all parameter assignment as they are handled on the server side using enforcedFilters/defaultFilters (https://github.com/TryGhost/Ghost/blob/6c7119071f9cc45a5a70cf5f9cd3611a97a0b20f/core/server/models/user.js#L293)

--- ghost/admin/app/adapters/user.js
+++ ghost/admin/app/adapters/user.js
@@ -2,25 +2,6 @@ import ApplicationAdapter from 'ghost-admin/adapters/application';
 import SlugUrl from 'ghost-admin/mixins/slug-url';
 
 export default ApplicationAdapter.extend(SlugUrl, {
-    find(store, type, id) {
-        return this.findQuery(store, type, {id, status: 'all'});
-    },
-
-    // TODO: This is needed because the API currently expects you to know the
-    // status of the record before retrieving by ID. Quick fix is to always
-    // include status=all in the query
-    findRecord(store, type, id, snapshot) {
-        let url = this.buildIncludeURL(store, type.modelName, id, snapshot, 'findRecord');
-
-        url += '&status=all';
-
-        return this.ajax(url, 'GET');
-    },
-
-    findAll(store, type, id) {
-        return this.query(store, type, {id, status: 'all'});
-    },
-
     queryRecord(store, type, query) {
         if (!query || query.id !== 'me') {
             return this._super(...arguments);
