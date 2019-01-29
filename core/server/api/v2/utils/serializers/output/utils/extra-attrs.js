58d6d7eea12dbdbf606a7e3d95b4bd8e969f4e27
Nazar Gargol <nazargargol@gmail.com>
2019-01-29 12:12:10 +0000

    🐛 Fixed plaintext attribute error when it is empty
    refs #10388
    
    - Issue was caused by migration of empty strings to null https://github.com/TryGhost/Ghost/commit/a4c1f87545a04abef268f82a2ef3045d33946e92

--- core/server/api/v2/utils/serializers/output/utils/extra-attrs.js
+++ core/server/api/v2/utils/serializers/output/utils/extra-attrs.js
@@ -5,7 +5,10 @@ module.exports.forPost = (frame, model, attrs) => {
         (frame.options.columns.includes('excerpt') && frame.options.formats && frame.options.formats.includes('plaintext'))) {
         if (_.isEmpty(attrs.custom_excerpt)) {
             const plaintext = model.get('plaintext');
-            attrs.excerpt = plaintext.substring(0, 500);
+
+            if (plaintext) {
+                attrs.excerpt = plaintext.substring(0, 500);
+            }
         } else {
             attrs.excerpt = attrs.custom_excerpt;
         }
