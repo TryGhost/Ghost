87bb3052a8b326014bb2236a00c1c0f6100ad778
kirrg001 <katharina.irrgang@googlemail.com>
2019-01-29 22:57:11 +0100

    Moved allowed `filter` option to base model
    no issue
    
    - no need to define the filter option in each model

--- core/server/models/subscriber.js
+++ core/server/models/subscriber.js
@@ -42,7 +42,7 @@ Subscriber = ghostBookshelf.Model.extend({
             // whitelists for the `options` hash argument on methods, by method name.
             // these are the only options that can be passed to Bookshelf / Knex.
             validOptions = {
-                findPage: ['page', 'limit', 'columns', 'filter', 'order'],
+                findPage: ['page', 'limit', 'columns', 'order'],
                 findAll: ['columns']
             };
 
--- core/server/models/tag.js
+++ core/server/models/tag.js
@@ -74,7 +74,7 @@ Tag = ghostBookshelf.Model.extend({
             // whitelists for the `options` hash argument on methods, by method name.
             // these are the only options that can be passed to Bookshelf / Knex.
             validOptions = {
-                findPage: ['page', 'limit', 'columns', 'filter', 'order'],
+                findPage: ['page', 'limit', 'columns', 'order'],
                 findAll: ['columns'],
                 findOne: ['visibility'],
                 destroy: ['destroyAll']
