d4279491362bcefdc4c61001f34a04ab23bf26cd
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-01-31 12:41:57 +0000

    Registered test waiter for ember-raf-scheduler
    no issue
    - recommended as per `vertical-collection` docs: https://github.com/html-next/vertical-collection#testing
    - ensures Ember's `wait` helper is aware of any raf-scheduler loops

--- ghost/admin/tests/test-helper.js
+++ ghost/admin/tests/test-helper.js
@@ -1,8 +1,11 @@
 import Application from '../app';
 import config from '../config/environment';
+import registerWaiter from 'ember-raf-scheduler/test-support/register-waiter';
 import {setApplication} from '@ember/test-helpers';
 
 setApplication(Application.create(config.APP));
+
+registerWaiter();
 
 mocha.setup({
     timeout: 15000,
