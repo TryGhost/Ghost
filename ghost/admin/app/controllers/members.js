3fe476b7fb84e66e01ae0a26ebc0012d83bf43c1
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-01-31 14:13:35 +0000

    Only fetch new/modified members in background refresh

--- ghost/admin/app/controllers/members.js
+++ ghost/admin/app/controllers/members.js
@@ -1,17 +1,38 @@
 import Controller from '@ember/controller';
+import {inject as service} from '@ember/service';
 import {task} from 'ember-concurrency';
 
 /* eslint-disable ghost/ember/alias-model-in-controller */
 export default Controller.extend({
+    store: service(),
+
     meta: null,
     members: null,
 
+    init() {
+        this._super(...arguments);
+        this.set('members', this.store.peekAll('member'));
+    },
+
     fetchMembers: task(function* () {
-        let results = yield this.store.query('member', {
-            limit: 'all'
-        });
+        let newFetchDate = new Date();
+        let results;
+
+        if (this._hasFetchedAll) {
+            // fetch any records modified since last fetch
+            results = yield this.store.query('member', {
+                limit: 'all',
+                filter: `updated_at:>='${this._lastFetchDate.toISOString()}'`
+            });
+        } else {
+            // fetch all records
+            results = yield this.store.query('member', {
+                limit: 'all'
+            });
+            this._hasFetchedAll = true;
+        }
 
         this.set('meta', results.meta);
-        this.set('members', results);
+        this._lastFetchDate = newFetchDate;
     })
 });
