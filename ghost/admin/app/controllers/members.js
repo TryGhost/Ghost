e57bb177b780afcc5cde74cf0d95e0a4491fd6b7
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-01-31 12:58:15 +0000

    Load all members on members screen, use occlusion scrolling for perf

--- ghost/admin/app/controllers/members.js
+++ ghost/admin/app/controllers/members.js
@@ -1,28 +1,14 @@
 import Controller from '@ember/controller';
-import {computed} from '@ember/object';
 import {task} from 'ember-concurrency';
 
 /* eslint-disable ghost/ember/alias-model-in-controller */
 export default Controller.extend({
-    queryParams: ['page'],
-
     meta: null,
     members: null,
 
-    page: computed('meta.pagination.page', function () {
-        let page = this.get('meta.pagination.page');
-
-        if (!page || page === 1) {
-            return null;
-        }
-
-        return page;
-    }),
-
     fetchMembers: task(function* () {
         let results = yield this.store.query('member', {
-            page: this.page || 1,
-            limit: 15
+            limit: 'all'
         });
 
         this.set('meta', results.meta);
