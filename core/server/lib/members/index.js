8c7989c1bdae679c3abbd5a2fcedb9f00102c43c
Fabien O'Carroll <fabien@allou.is>
2019-02-06 14:53:20 +0100

    Added create subscription endpoint

--- core/server/lib/members/index.js
+++ core/server/lib/members/index.js
@@ -84,6 +84,38 @@ module.exports = function MembersApi({
         next();
     }
 
+    /* subscriptions */
+    apiRouter.post('/subscription', getData('adapter', 'plan', 'stripeToken'), ssoOriginCheck, (req, res) => {
+        const {signedin} = getCookie(req);
+        if (!signedin) {
+            res.writeHead(401, {
+                'Set-Cookie': removeCookie()
+            });
+            return res.end();
+        }
+
+        const {plan, adapter, stripeToken} = req.data;
+
+        subscriptions.getAdapters()
+            .then((adapters) => {
+                if (!adapters.includes(adapter)) {
+                    throw new Error('Invalid adapter');
+                }
+            })
+            .then(() => users.get({id: signedin}))
+            .then((member) => {
+                return subscriptions.createSubscription(member, {
+                    adapter,
+                    plan,
+                    stripeToken
+                });
+            })
+            .then(() => {
+                res.end();
+            })
+            .catch(handleError(500, res));
+    });
+
     /* users, token, emails */
     apiRouter.post('/request-password-reset', getData('email'), ssoOriginCheck, (req, res) => {
         const {email} = req.data;
