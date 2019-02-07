071c7bb100c90dd8f437a2dbf5df623e1347731e
John O'Nolan <john@onolan.org>
2019-02-07 17:59:35 +0700

    Update admin redirects
    Refs https://github.com/TryGhost/Ghost/pull/10079

--- core/server/web/shared/middlewares/admin-redirects.js
+++ core/server/web/shared/middlewares/admin-redirects.js
@@ -14,7 +14,7 @@ module.exports = function adminRedirects() {
     router.get(/^\/(logout|signout)\/$/, adminRedirect('#/signout/'));
     router.get(/^\/signup\/$/, adminRedirect('#/signup/'));
     // redirect to /ghost and let that do the authentication to prevent redirects to /ghost//admin etc.
-    router.get(/^\/((ghost-admin|admin|wp-admin|dashboard|signin|login)\/?)$/, adminRedirect('/'));
+    router.get(/^\/((ghost-admin|admin|dashboard|signin|login)\/?)$/, adminRedirect('/'));
 
     return router;
 };
