e7c3b1b0e3ac2b33edb03190165ec8631e9be97f
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-02-11 21:07:51 +0000

    🐛 Fixed Code Injection input fields not being clickable
    no issue
    - lazy loaded scripts such as the CodeMirror asset used on the Code Injection screen could throw errors such as `TypeError: Cannot set property 'modeOption' of undefined`
    - this was caused by "loading" promise returned from the `lazyLoader` service returning as soon as the network request finished which can be before the loaded script has been parsed and run meaning any processing occurring after the promise returns could be depending on unloaded code
    - switched the lazyLoader service's loading mechanism from an ajax fetch to insertion of a `<script>` tag which can have `load` event attached which _will_ return after parsing/loading has completed

--- ghost/admin/tests/integration/services/lazy-loader-test.js
+++ ghost/admin/tests/integration/services/lazy-loader-test.js
@@ -6,6 +6,7 @@ import {setupTest} from 'ember-mocha';
 
 describe('Integration: Service: lazy-loader', function () {
     setupTest('service:lazy-loader', {integration: true});
+
     let server;
     let ghostPaths = {
         adminRoot: '/assets/'
@@ -19,28 +20,32 @@ describe('Integration: Service: lazy-loader', function () {
         server.shutdown();
     });
 
-    it('loads a script correctly and only once', function () {
+    it('loads a script correctly and only once', async function () {
         let subject = this.subject({
             ghostPaths,
             scriptPromises: {},
             testing: false
         });
 
-        server.get('/assets/test.js', function ({requestHeaders}) {
-            expect(requestHeaders.Accept).to.match(/text\/javascript/);
+        // first load should add script element
+        await subject.loadScript('test', 'lazy-test.js')
+            .then(() => {})
+            .catch(() => {});
 
-            return [200, {'Content-Type': 'text/javascript'}, 'window.testLoadScript = \'testvalue\''];
-        });
+        expect(
+            document.querySelectorAll('script[src="/assets/lazy-test.js"]').length,
+            'no of script tags on first load'
+        ).to.equal(1);
 
-        return subject.loadScript('test-script', 'test.js').then(() => {
-            expect(subject.get('scriptPromises.test-script')).to.exist;
-            expect(window.testLoadScript).to.equal('testvalue');
-            expect(server.handlers[0].numberOfCalls).to.equal(1);
+        // second load should not add another script element
+        await subject.loadScript('test', '/assets/lazy-test.js')
+            .then(() => { })
+            .catch(() => { });
 
-            return subject.loadScript('test-script', 'test.js');
-        }).then(() => {
-            expect(server.handlers[0].numberOfCalls).to.equal(1);
-        });
+        expect(
+            document.querySelectorAll('script[src="/assets/lazy-test.js"]').length,
+            'no of script tags on second load'
+        ).to.equal(1);
     });
 
     it('loads styles correctly', function () {
