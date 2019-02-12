db148e653fd876fd5c63e9a0728c23bd1be747ac
kirrg001 <katharina.irrgang@googlemail.com>
2019-02-12 19:26:31 +0100

    Removed `tag.parent` from Admin API v2 response
    refs #10438
    
    - this is an unused field
    - no need to expose this field
    - if we start working on nested tags, this field might become interesting/used

--- core/test/acceptance/old/admin/utils.js
+++ core/test/acceptance/old/admin/utils.js
@@ -35,8 +35,8 @@ const expectedProperties = {
     ,
     tag: _(schema.tags)
         .keys()
-        // Tag API swaps parent_id to parent
-        .without('parent_id').concat('parent')
+        // unused field
+        .without('parent_id')
     ,
     setting: _(schema.settings)
         .keys()
