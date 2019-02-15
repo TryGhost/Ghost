ebaae9eadd6f38dd96a17211b45f7e82faab0249
Zimo <peter.zimon@gmail.com>
2019-02-15 18:16:30 +0100

    Applying basic styles to members popups
    no issue

--- ghost/members-auth-pages/pages/RequestPasswordResetPage.js
+++ ghost/members-auth-pages/pages/RequestPasswordResetPage.js
@@ -5,7 +5,7 @@ import FormSubmit from '../components/FormSubmit';
 import Form from '../components/Form';
 
 export default ({ error, handleClose, handleSubmit }) => (
-    <div>
+    <div className="gm-modal-form">
         <FormHeader title="Reset password" error={error} errorText="Unable to send email" />
         <Form bindTo="request-password-reset" onSubmit={handleSubmit}>
             <EmailInput bindTo="email" />
