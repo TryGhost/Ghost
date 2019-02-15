9bd5c62a1bd300dab4f2d01e4aade9ecf4d29cda
Zimo <peter.zimon@gmail.com>
2019-02-15 18:16:30 +0100

    Applying basic styles to members popups
    no issue

--- core/server/lib/members/static/auth/components/FormHeaderCTA.js
+++ core/server/lib/members/static/auth/components/FormHeaderCTA.js
@@ -1,8 +1,11 @@
+import { IconRightArrow } from './icons';
+
 export default ({title, label, hash}) => (
     <div className="flex items-baseline mt2">
         <h4>{ title }</h4>
         <a href={hash}>
             { label }
+            { IconRightArrow }
         </a>
     </div>
 );
