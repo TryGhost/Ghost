c4a89bff8dc39c4cd262fd5d47d15e9cbfd18015
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-02-15 17:44:26 +0000

    add test for Integration.findOne allowing options.filter

--- core/test/unit/models/integration_spec.js
+++ core/test/unit/models/integration_spec.js
@@ -34,4 +34,38 @@ describe('Unit: models/integration', function () {
             should.deepEqual(returnedOptions, basePermittedOptionsReturnVal.concat('filter'));
         });
     });
+
+    describe('findOne', function () {
+        const mockDb = require('mock-knex');
+        let tracker;
+
+        before(function () {
+            mockDb.mock(knex);
+            tracker = mockDb.getTracker();
+        });
+
+        after(function () {
+            mockDb.unmock(knex);
+        });
+
+        it('generates correct query (allows use of options.filter)', function () {
+            const queries = [];
+            tracker.install();
+
+            tracker.on('query', (query) => {
+                queries.push(query);
+                query.response([]);
+            });
+
+            return models.Integration.findOne({
+                id: '123'
+            }, {
+                filter: 'type:[custom,builtin]'
+            }).then(() => {
+                queries.length.should.eql(1);
+                queries[0].sql.should.eql('select `integrations`.* from `integrations` where `integrations`.`type` in (?, ?) and `integrations`.`id` = ? limit ?');
+                queries[0].bindings.should.eql(['custom', 'builtin', '123', 1]);
+            });
+        });
+    });
 });
