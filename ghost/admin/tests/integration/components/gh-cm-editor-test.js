086cb713be44d8a64a5b9f8f18e8dae9772ff522
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-02-22 11:54:53 +0700

    Fixed gh-cm-editor component test

--- ghost/admin/tests/integration/components/gh-cm-editor-test.js
+++ ghost/admin/tests/integration/components/gh-cm-editor-test.js
@@ -25,43 +25,6 @@ describe('Integration: Component: gh-cm-editor', function () {
             .to.equal('Testing');
     });
 
-    it('handles focus event', async function () {
-        // CodeMirror's events are triggered outside of anything we can watch for
-        // in the tests so let's run the class check when we know the event has
-        // been fired and timeout if it's not fired as we expect
-        let onFocus = async () => {
-            // wait for runloop to finish so that the new class has been rendered
-            await settled();
-            expect(find('.gh-input'), 'has focused class on first render with autofocus')
-                .to.have.class('focus');
-        };
-
-        this.set('onFocus', onFocus);
-        this.set('text', '');
-
-        await render(hbs`{{gh-cm-editor text class="gh-input" update=(action (mut text)) focus-in=(action onFocus)}}`);
-
-        // CodeMirror polls the input for changes every 100ms
-        await click('textarea');
-        await triggerEvent('textarea', 'focus');
-    });
-
-    it('handles blur event', async function () {
-        this.set('text', '');
-        await render(hbs`{{gh-cm-editor text class="gh-input" update=(action (mut text))}}`);
-
-        expect(find('.gh-input')).to.not.have.class('focus');
-
-        await click('textarea');
-        await triggerEvent('textarea', 'focus');
-
-        expect(find('.gh-input')).to.have.class('focus');
-
-        await triggerEvent('textarea', 'blur');
-
-        expect(find('.gh-input')).to.not.have.class('focus');
-    });
-
     it('can autofocus', async function () {
         // CodeMirror's events are triggered outside of anything we can watch for
         // in the tests so let's run the class check when we know the event has
