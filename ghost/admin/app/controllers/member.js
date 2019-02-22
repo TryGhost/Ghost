2d1223028fa5506700a6aa266686fd5814236544
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-02-22 18:31:45 +0700

    Implemented first pass of member details screen

--- ghost/admin/app/controllers/member.js
+++ ghost/admin/app/controllers/member.js
@@ -1,6 +1,22 @@
 import Controller from '@ember/controller';
 import {alias} from '@ember/object/computed';
+import {inject as controller} from '@ember/controller';
+import {inject as service} from '@ember/service';
 
 export default Controller.extend({
-    member: alias('model')
+    members: controller(),
+    router: service(),
+
+    member: alias('model'),
+
+    actions: {
+        finaliseDeletion() {
+            // decrememnt the total member count manually so there's no flash
+            // when transitioning back to the members list
+            if (this.members.meta) {
+                this.members.decrementProperty('meta.pagination.total');
+            }
+            this.router.transitionTo('members');
+        }
+    }
 });
--- ghost/admin/mirage/config/members.js
+++ ghost/admin/mirage/config/members.js
@@ -2,4 +2,18 @@ import {paginatedResponse} from '../utils';
 
 export default function mockMembers(server) {
     server.get('/members/', paginatedResponse('members'));
+
+    server.get('/members/:id/', function ({members}, {params}) {
+        let {id} = params;
+        let member = members.find(id);
+
+        return member || new Response(404, {}, {
+            errors: [{
+                errorType: 'NotFoundError',
+                message: 'Member not found.'
+            }]
+        });
+    });
+
+    server.del('/members/:id/');
 }
