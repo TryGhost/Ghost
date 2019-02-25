b9cc2ff97b8f0833da8298ba5624a090b173a57f
kirrg001 <katharina.irrgang@googlemail.com>
2019-02-25 15:39:47 +0100

    Added "useGravatar" to config endpoint in Admin API v2
    no issue

--- core/test/acceptance/old/admin/utils.js
+++ core/test/acceptance/old/admin/utils.js
@@ -22,7 +22,7 @@ const expectedProperties = {
 
     action: ['id', 'resource_type', 'actor_type', 'event', 'created_at', 'actor'],
 
-    config: ['version', 'environment', 'database', 'mail', 'labs', 'clientExtensions', 'enableDeveloperExperiments'],
+    config: ['version', 'environment', 'database', 'mail', 'labs', 'clientExtensions', 'enableDeveloperExperiments', 'useGravatar'],
 
     post: _(schema.posts)
         .keys()
