138e64c89745e43c0cfc5a515b72525bb8ef32ed
Rish <zrishabhgarg@gmail.com>
2019-02-25 14:52:01 +0700

    Fix test with members enabled

--- core/server/services/members/index.js
+++ core/server/services/members/index.js
@@ -1,5 +1,10 @@
+const config = require('../../config/index.js');
+
 module.exports = {
     get api() {
+        if (!config.get('enableDeveloperExperiments')) {
+            return {};
+        }
         return require('./api');
     }
 };
