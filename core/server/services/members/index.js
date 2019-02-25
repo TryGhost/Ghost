3b5e1923f9ce50bc040554323d8e84ebb8f632ca
Rish <zrishabhgarg@gmail.com>
2019-02-25 15:46:36 +0700

    Fixed tests for members

--- core/server/services/members/index.js
+++ core/server/services/members/index.js
@@ -1,9 +1,17 @@
 const config = require('../../config/index.js');
+const common = require('../../lib/common');
 
 module.exports = {
     get api() {
         if (!config.get('enableDeveloperExperiments')) {
-            return {};
+            return {
+                apiRouter: function (req, res, next) {
+                    return next(new common.errors.NotFoundError());
+                },
+                staticRouter: function (req, res, next) {
+                    return next(new common.errors.NotFoundError());
+                }
+            };
         }
         return require('./api');
     }
