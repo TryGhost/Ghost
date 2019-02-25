4b903c7e80e5f0625ce7a1148d1d66e21feadbb6
Rish <zrishabhgarg@gmail.com>
2019-02-25 11:14:41 +0700

    Updated members service to work with correct routing

--- core/server/services/members/index.js
+++ core/server/services/members/index.js
@@ -1,9 +1,5 @@
-const labs = require('../labs');
 module.exports = {
     get api() {
-        if (!labs.isSet('members')) {
-            return {};
-        }
         return require('./api');
     }
 };
