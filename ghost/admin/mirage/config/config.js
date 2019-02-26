738823d8f83631b7a251b1e202de8112f32dc153
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-02-26 10:38:00 +0700

    Fixed "Authorization Failed" error screens when not logged in
    no issue
    - `/config/` can only be requested when authenticated
    - updated `/config/` mock to look for an Authentication header and return a 403 if it's missing
    - updated `ajax` service to add an `Authentication` header when authenticated in testing env (cookies are not present when testing)
    - updated `config` service to add `fetchUnauthenticated()` and `fetchAuthenticated()` methods in addition to `.fetch()`
    - updated `application` route to only fetch authenticated config when authenticated
    - updated `signin` controller to correctly fetch config after sign-in

--- ghost/admin/mirage/config/config.js
+++ ghost/admin/mirage/config/config.js
@@ -1,7 +1,18 @@
+import {Response} from 'ember-cli-mirage';
 import {isEmpty} from '@ember/utils';
 
 export default function mockConfig(server) {
-    server.get('/config/', function ({db}) {
+    server.get('/config/', function ({db}, request) {
+        if (!request.requestHeaders.Authorization) {
+            return new Response(403, {}, {
+                errors: [{
+                    type: 'NoPermissionError',
+                    message: 'Authorization failed',
+                    context: 'Unable to determine the authenticated user or integration. Check that cookies are being passed through if using session authentication.'
+                }]
+            });
+        }
+
         if (isEmpty(db.configs)) {
             server.loadFixtures('configs');
         }
