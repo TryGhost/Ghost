e8be3cc8a4b84fb8d15eee344597d828e689d0e3
Rish <zrishabhgarg@gmail.com>
2019-02-26 10:09:16 +0700

    Refactored members auth flow with dynamic settings
    no issue
    
    - Updated members auth flow UI
    - Updated members settings and routing to be dynamic

--- ghost/members-auth-pages/pages/RequestPasswordResetPage.js
+++ ghost/members-auth-pages/pages/RequestPasswordResetPage.js
@@ -5,11 +5,14 @@ import FormSubmit from '../components/FormSubmit';
 import Form from '../components/Form';
 
 export default ({ error, handleClose, handleSubmit }) => (
-    <div className="gm-modal-form">
+    <div className="gm-modal-form gm-reset-pwd-page">
         <FormHeader title="Reset password" error={error} errorText="Unable to send email" />
         <Form bindTo="request-password-reset" onSubmit={handleSubmit}>
-            <EmailInput bindTo="email" />
+            <EmailInput bindTo="email" className="single" />
             <FormSubmit label="Send reset password instructions" />
+            <div class="flex justify-center mt5">
+                <a href="#signin">Cancel</a>
+            </div>
         </Form>
     </div>
 );
--- ghost/members-auth-pages/pages/SignupPage.js
+++ ghost/members-auth-pages/pages/SignupPage.js
@@ -9,14 +9,17 @@ import PasswordInput from '../components/PasswordInput';
 import { IconClose } from '../components/icons';
 
 export default ({ error, handleClose, handleSubmit }) => (
-    <div className="gm-modal-form">
-        <FormHeader title="Sign up" error={error} errorText="Email already registered">
-            <FormHeaderCTA title="Already a member?" label="Log in" hash="#signin" />
+    <div className="gm-modal-form gm-signup-page">
+        <FormHeader title="" error={error} errorText="Email already registered">
+            <div className="flex justify-between items-baseline">
+                <h1>Sign up</h1>
+                <FormHeaderCTA title="Already a member?" label="Log in" hash="#signin" />
+            </div>
         </FormHeader>
         <Form onSubmit={handleSubmit}>
-            <NameInput bindTo="name" />
+            <NameInput bindTo="name" className="first" />
             <EmailInput bindTo="email" />
-            <PasswordInput bindTo="password" />
+            <PasswordInput bindTo="password" className="last" />
             <FormSubmit label="Sign up" />
         </Form>
     </div>
