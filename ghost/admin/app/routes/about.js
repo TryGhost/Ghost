cbfdd526d6b9b8130e4916f96dce205e07dac417
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-03-04 09:27:39 +0000

    Fixed linting

--- ghost/admin/app/routes/about.js
+++ ghost/admin/app/routes/about.js
@@ -1,6 +1,5 @@
 import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
 import styleBody from 'ghost-admin/mixins/style-body';
-import {inject as service} from '@ember/service';
 
 export default AuthenticatedRoute.extend(styleBody, {
     titleToken: 'About',
