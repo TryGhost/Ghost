5b73e8238e4c9f02ef83099dca93d7e073bda953
kirrg001 <katharina.irrgang@googlemail.com>
2019-03-06 10:17:41 +0100

    🐛 Fixed email address not being returned with admin api key authentication
    no issue
    
    - bug in model layer
    - we have to drop these context checks when dropping v0.1

--- core/server/models/user.js
+++ core/server/models/user.js
@@ -219,7 +219,8 @@ User = ghostBookshelf.Model.extend({
 
         // NOTE: We don't expose the email address for for external, app and public context.
         // @TODO: Why? External+Public is actually the same context? Was also mentioned here https://github.com/TryGhost/Ghost/issues/9043
-        if (!options || !options.context || (!options.context.user && !options.context.internal)) {
+        // @TODO: move to api serialization when we drop v0.1
+        if (!options || !options.context || (!options.context.user && !options.context.internal && (!options.context.api_key || options.context.api_key.type === 'content'))) {
             delete attrs.email;
         }
 
