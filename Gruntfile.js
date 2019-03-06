e7f2c95cd9d2489a941d3a67d9ce45a1dade494b
Fabien O'Carroll <fabien@allou.is>
2019-03-06 09:03:01 +0100

    Fixed grunt master to work with submodules correctly (#10566)
    refs #9441
    
    `grunt master` should only error if there are changes to tracked files which have not been committed - this ensures no work is lost.

--- Gruntfile.js
+++ Gruntfile.js
@@ -215,7 +215,10 @@ const configureGrunt = function (grunt) {
                     var upstream = grunt.option('upstream') || process.env.GHOST_UPSTREAM || 'upstream';
                     grunt.log.writeln('Pulling down the latest master from ' + upstream);
                     return `
-                        if ! git diff --exit-code --quiet; then
+                        git submodule sync
+                        git submodule update
+
+                        if ! git diff --exit-code --quiet --ignore-submodules=untracked; then
                             echo "Working directory is not clean, do you have uncommited changes? Please commit, stash or discard changes to continue."
                             exit 1
                         fi
