14d824ed9ce92911404f665b771bb24f7e8dc096
kirrg001 <katharina.irrgang@googlemail.com>
2019-03-07 12:23:26 +0100

    Removed `force_i18n` in API v2 settings serializer
    refs #10582
    
    - deprecated
    - won't serve
    - won't save

--- core/server/api/v2/utils/serializers/input/settings.js
+++ core/server/api/v2/utils/serializers/input/settings.js
@@ -48,5 +48,12 @@ module.exports = {
                 setting = url.forSetting(setting);
             }
         });
+
+        // CASE: deprecated, won't accept
+        const index = _.findIndex(frame.data.settings, {key: 'force_i18n'});
+
+        if (index !== -1) {
+            frame.data.settings.splice(index, 1);
+        }
     }
 };
