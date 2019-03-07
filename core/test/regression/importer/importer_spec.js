346ed1af75997b327bb965355dd300d1067618e2
kirrg001 <katharina.irrgang@googlemail.com>
2019-03-07 12:59:50 +0100

    Added proper importer regression tests
    refs #10582

--- core/test/regression/importer/importer_spec.js
+++ core/test/regression/importer/importer_spec.js
@@ -869,6 +869,63 @@ describe('Integration: Importer', function () {
                 });
         });
 
+        it('does import settings with string booleans', function () {
+            const exportData = exportedLatestBody().db[0];
+
+            exportData.data.settings[0] = testUtils.DataGenerator.forKnex.createSetting({
+                key: 'amp',
+                value: 'true'
+            });
+
+            exportData.data.settings[1] = testUtils.DataGenerator.forKnex.createSetting({
+                key: 'is_private',
+                value: '0'
+            });
+
+            exportData.data.settings[2] = testUtils.DataGenerator.forKnex.createSetting({
+                key: 'force_i18n',
+                value: false
+            });
+
+            return dataImporter.doImport(exportData, importOptions)
+                .then(function (imported) {
+                    imported.problems.length.should.eql(0);
+                    return models.Settings.findOne(_.merge({key: 'amp'}, testUtils.context.internal));
+                })
+                .then(function (result) {
+                    result.attributes.value.should.eql(true);
+                    return models.Settings.findOne(_.merge({key: 'is_private'}, testUtils.context.internal));
+                })
+                .then((result) => {
+                    result.attributes.value.should.eql(false);
+                    return models.Settings.findOne(_.merge({key: 'force_i18n'}, testUtils.context.internal));
+                })
+                .then((result) => {
+                    result.attributes.value.should.eql(false);
+
+                    return db
+                        .knex('settings')
+                        .where('key', 'amp');
+                })
+                .then((result) => {
+                    result[0].value.should.eql('true');
+
+                    return db
+                        .knex('settings')
+                        .where('key', 'is_private');
+                })
+                .then((result) => {
+                    result[0].value.should.eql('false');
+
+                    return db
+                        .knex('settings')
+                        .where('key', 'force_i18n');
+                })
+                .then((result) => {
+                    result[0].value.should.eql('false');
+                });
+        });
+
         it('import comment_id', function () {
             const exportData = exportedLatestBody().db[0];
 
