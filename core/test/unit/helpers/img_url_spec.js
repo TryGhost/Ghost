3ae2ed4651a2ae5ba780620b01147d02ead0a956
Fabien O'Carroll <fabien@allou.is>
2019-03-11 12:33:03 +0100

    Updated existing test to breaking test for img-url
    refs #10595
    
    Had made a dumb assumption when building images sizes, this updates the
    test to fail so we can verify the fix

--- core/test/unit/helpers/img_url_spec.js
+++ core/test/unit/helpers/img_url_spec.js
@@ -127,7 +127,7 @@ describe('{{image}} helper', function () {
                 }
             });
             should.exist(rendered);
-            rendered.should.equal('http://localhost:82832/content/images/size/w400/my-coole-img.jpg');
+            rendered.should.equal('/content/images/size/w400/my-coole-img.jpg');
         });
         it('should output the correct url for protocol relative urls', function () {
             var rendered = helpers.img_url('//website.com/whatever/my-coole-img.jpg', {
