b1f4f342db8d23be32fe4ff564b96bd5cbe27327
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-03-11 18:16:01 +0000

    Used ember-exam to split tests and run in parallel on Travis (#1112)
    no issue
    - improves test run times by splitting the tests into multiple groups and running each in their own browser instance
      - settled on 2 browser instances as that seems to best fit within Travis' memory and CPU constraints
    - updated ember-cli-code-coverage config to work with parallel builds

--- ghost/admin/tests/test-helper.js
+++ ghost/admin/tests/test-helper.js
@@ -1,7 +1,10 @@
 import Application from '../app';
 import config from '../config/environment';
+import loadEmberExam from 'ember-exam/test-support/load';
 import registerWaiter from 'ember-raf-scheduler/test-support/register-waiter';
 import {setApplication} from '@ember/test-helpers';
+
+loadEmberExam();
 
 setApplication(Application.create(config.APP));
 
