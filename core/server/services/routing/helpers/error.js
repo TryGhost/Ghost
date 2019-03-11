fca05453fe9a760edfbb8c78574c02ff5f83ed7a
kirrg001 <katharina.irrgang@googlemail.com>
2019-03-11 19:40:51 +0100

    🐛 Fixed site requests showing 422 if path is not a valid slug
    no issue
    
    - e.g. /feed.xml/ was showing a 422
    - any other none slug site requests showed a 422
    - should be a 404
    - context: site is talking to Content API
      - it can happen that the API returns a 422
      - the routing layer needs to handle this and always show a 404
    - catched error in routing error handling
    - need to see if there are more cases

--- core/server/services/routing/helpers/error.js
+++ core/server/services/routing/helpers/error.js
@@ -1,7 +1,19 @@
+const common = require('../../../lib/common');
+
 function handleError(next) {
     return function handleError(err) {
         // If we've thrown an error message of type: 'NotFound' then we found no path match.
         if (err.errorType === 'NotFoundError') {
+            return next();
+        }
+
+        // CASE: the site should not output validation errors e.g. you ask for /feed.xml/ and it tries to fetch
+        //       this post from Content API (by slug), but this is not a valid slug. With dynamic routing we cannot
+        //       add a regex to the target express route, because we don't know if people use /:slug/ or not. It's dynamic.
+        if (err.errorType === 'ValidationError') {
+            common.logging.error(err);
+
+            // @NOTE: Just try next router, it will end in a 404 if no router can resolve the request.
             return next();
         }
 
