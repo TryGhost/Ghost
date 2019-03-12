82ff34bc6ef3cc6bc3a99bb15b61a5600cf3c5c6
kirrg001 <katharina.irrgang@googlemail.com>
2019-03-12 20:20:46 +0100

    Added regression test for editing a scheduled post
    refs #10600
    
    - ensure it does not happen again
    - we only have some regression model tests, which edit scheduled posts

--- core/test/regression/api/v2/admin/posts_spec.js
+++ core/test/regression/api/v2/admin/posts_spec.js
@@ -210,6 +210,32 @@ describe('Posts API', function () {
                     model.get('updated_at').toISOString().should.not.eql(post.updated_at);
                 });
         });
+
+        it('Can change scheduled post', function () {
+            return request
+                .get(localUtils.API.getApiQuery(`posts/${testUtils.DataGenerator.Content.posts[7].id}/`))
+                .set('Origin', config.get('url'))
+                .expect(200)
+                .then((res) => {
+                    res.body.posts[0].status.should.eql('scheduled');
+
+                    return request
+                        .put(localUtils.API.getApiQuery('posts/' + testUtils.DataGenerator.Content.posts[7].id + '/'))
+                        .set('Origin', config.get('url'))
+                        .send({
+                            posts: [{
+                                title: 'change scheduled post',
+                                updated_at: res.body.posts[0].updated_at
+                            }]
+                        })
+                        .expect('Content-Type', /json/)
+                        .expect('Cache-Control', testUtils.cacheRules.private)
+                        .expect(200);
+                })
+                .then((res) => {
+                    should.exist(res.headers['x-cache-invalidate']);
+                });
+        });
     });
 
     describe('destroy', function () {
