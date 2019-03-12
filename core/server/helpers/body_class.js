c6b644dc43990da44a007f5b56349b9432c62509
kirrg001 <katharina.irrgang@googlemail.com>
2019-03-12 01:18:07 +0100

    🐛 Fixed {{body_class}} helper when using `data: page.{slug}` in routes
    refs #10082
    
    - throwed a 500 because this.page was not handled
    - v2 differentiates between page and post

--- core/server/helpers/body_class.js
+++ core/server/helpers/body_class.js
@@ -8,19 +8,19 @@ var proxy = require('./proxy'),
 
 // We use the name body_class to match the helper for consistency:
 module.exports = function body_class(options) { // eslint-disable-line camelcase
-    var classes = [],
-        context = options.data.root.context,
-        post = this.post,
-        tags = this.post && this.post.tags ? this.post.tags : this.tags || [],
-        page = this.post && this.post.page ? this.post.page : this.page || false;
+    let classes = [];
+    const context = options.data.root.context;
+    const obj = this.post || this.page;
+    const tags = obj && obj.tags ? obj.tags : [];
+    const isPage = !!(obj && obj.page);
 
     if (_.includes(context, 'home')) {
         classes.push('home-template');
-    } else if (_.includes(context, 'post') && post) {
+    } else if (_.includes(context, 'post') && obj) {
         classes.push('post-template');
-    } else if (_.includes(context, 'page') && page) {
+    } else if (_.includes(context, 'page') && obj && isPage) {
         classes.push('page-template');
-        classes.push('page-' + this.post.slug);
+        classes.push('page-' + obj.slug);
     } else if (_.includes(context, 'tag') && this.tag) {
         classes.push('tag-template');
         classes.push('tag-' + this.tag.slug);
@@ -33,7 +33,7 @@ module.exports = function body_class(options) { // eslint-disable-line camelcase
 
     if (tags) {
         classes = classes.concat(tags.map(function (tag) {
-            return 'tag-' + tag.slug; 
+            return 'tag-' + tag.slug;
         }));
     }
 
@@ -42,8 +42,9 @@ module.exports = function body_class(options) { // eslint-disable-line camelcase
     }
 
     classes = _.reduce(classes, function (memo, item) {
-        return memo + ' ' + item; 
+        return memo + ' ' + item;
     }, '');
+
     return new SafeString(classes.trim());
 };
 
--- core/test/unit/helpers/body_class_spec.js
+++ core/test/unit/helpers/body_class_spec.js
@@ -133,10 +133,19 @@ describe('{{body_class}} helper', function () {
             rendered.string.should.equal('post-template tag-foo tag-bar');
         });
 
-        it('a static page', function () {
+        it('v0.1: a static page', function () {
             var rendered = callBodyClassWithContext(
                 ['page'],
                 {relativeUrl: '/about', post: {page: true, slug: 'about'}}
+            );
+
+            rendered.string.should.equal('page-template page-about');
+        });
+
+        it('v2: a static page', function () {
+            var rendered = callBodyClassWithContext(
+                ['page'],
+                {relativeUrl: '/about', page: {page: true, slug: 'about'}}
             );
 
             rendered.string.should.equal('page-template page-about');
