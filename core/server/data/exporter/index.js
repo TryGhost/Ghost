3cee65537c556a4d6b6f8d3c3dbc673f71e50825
Fabien O'Carroll <fabien@allou.is>
2019-03-13 19:30:04 +0100

    Used moment for datetime

--- core/server/data/exporter/index.js
+++ core/server/data/exporter/index.js
@@ -25,7 +25,7 @@ var _ = require('lodash'),
     exportFileName;
 
 exportFileName = function exportFileName(options) {
-    var datetime = (new Date()).toJSON(),
+    var datetime = require('moment')().format('YYYY-MM-DD-HH-mm-ss'),
         title = '';
 
     options = options || {};
