65e3e426be80103227b0c30c90538b41e6f59cb5
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-03-21 17:55:58 +0000

    Added reset-to-homepage behaviour when clicking "view site" link
    no issue
    - return a basic "guid" from the site's `model` hook so that we have some data which changes on each refresh
    - add an action to the wrapper element of the "view site" link which will cause the route to refresh when clicked if we're already on the route
    - move the site iframe into a component so that it can watch an @uuid property and force a reset of the iframe's `src` when it detects a change

--- ghost/admin/app/routes/site.js
+++ ghost/admin/app/routes/site.js
@@ -4,5 +4,9 @@ import styleBody from 'ghost-admin/mixins/style-body';
 export default AuthenticatedRoute.extend(styleBody, {
     titleToken: 'Site',
 
-    classNames: ['view-site']
+    classNames: ['view-site'],
+
+    model() {
+        return (new Date()).valueOf();
+    }
 });
