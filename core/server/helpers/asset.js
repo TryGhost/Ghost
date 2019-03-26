502d5c22b26f16fdc638eecc0f422064fe75e443
Vikas Potluri <vikaspotluri123.github@gmail.com>
2019-03-25 23:36:41 -0500

    Migrated asset and author helpers to es6 (#10611)
    refs #9589
    
    * update asset helper to use newer code standards
    
    * update authors helper to use newer code standards

--- core/server/helpers/asset.js
+++ core/server/helpers/asset.js
@@ -1,11 +1,11 @@
 // # Asset helper
-// Usage: `{{asset "css/screen.css"}}`, `{{asset "css/screen.css" ghost="true"}}`
+// Usage: `{{asset "css/screen.css"}}`
 //
-// Returns the path to the specified asset. The ghost flag outputs the asset path for the Ghost admin
-const proxy = require('./proxy'),
-    get = require('lodash/get'),
-    getAssetUrl = proxy.metaData.getAssetUrl,
-    SafeString = proxy.SafeString;
+// Returns the path to the specified asset.
+const proxy = require('./proxy');
+const get = require('lodash/get');
+const {SafeString} = proxy;
+const {getAssetUrl} = proxy.metaData;
 
 module.exports = function asset(path, options) {
     const hasMinFile = get(options, 'hash.hasMinFile');
