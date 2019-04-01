TryGhost/Ghost

commit 94eea9122cef6e55558fa486063188b38c89095a

🐛  Fixed custom redirects with query/search params (#8998)
closes #8997

- improved the logic for custom redirects
- added more tests

core/server/middleware/custom-redirects.js
@@ -1,5 +1,6 @@
 var fs = require('fs-extra'),
     _ = require('lodash'),
+    url = require('url'),
     debug = require('ghost-ignition').debug('custom-redirects'),
     config = require('../config'),
     errors = require('../errors'),
@@ -43,13 +44,17 @@ module.exports = function redirects(blogApp) {
             }
 
             blogApp.get(new RegExp(redirect.from), function (req, res) {
-                var maxAge = redirect.permanent ? config.get('caching:customRedirects:maxAge') : 0;
+                var maxAge = redirect.permanent ? config.get('caching:customRedirects:maxAge') : 0,
+                    parsedUrl = url.parse(req.originalUrl);
 
                 res.set({
                     'Cache-Control': 'public, max-age=' + maxAge
                 });
 
-                res.redirect(redirect.permanent ? 301 : 302, req.originalUrl.replace(new RegExp(redirect.from), redirect.to));
+                res.redirect(redirect.permanent ? 301 : 302, url.format({
+                    pathname: parsedUrl.pathname.replace(new RegExp(redirect.from), redirect.to),
+                    search: parsedUrl.search
+                }));
             });
         });
     } catch (err) {

core/test/functional/routes/frontend_spec.js
@@ -786,6 +786,26 @@ describe('Frontend Routing', function () {
                         });
                 });
 
+                it('with query params', function (done) {
+                    request.get('/topic?something=good')
+                        .expect(302)
+                        .expect('Cache-Control', testUtils.cacheRules.public)
+                        .end(function (err, res) {
+                            res.headers.location.should.eql('/?something=good');
+                            doEnd(done)(err, res);
+                        });
+                });
+
+                it('with query params', function (done) {
+                    request.get('/post/10/a-nice-blog-post?a=b')
+                        .expect(302)
+                        .expect('Cache-Control', testUtils.cacheRules.public)
+                        .end(function (err, res) {
+                            res.headers.location.should.eql('/a-nice-blog-post?a=b');
+                            doEnd(done)(err, res);
+                        });
+                });
+
                 it('should not redirect', function (done) {
                     request.get('/post/a-nice-blog-post/')
                         .end(function (err, res) {

commit 9fe212fb362f325fdf48f897f9fafee45a787064

🐛 Fixed redirects to external URL
closes #10623

- The ability to redirect to external URLs was broken with https://github.com/TryGhost/Ghost/commit/94eea9122cef6e55558fa486063188b38c89095a
- Added test coverage for external URL case

core/server/web/shared/middlewares/custom-redirects.js
@@ -23,13 +23,6 @@ _private.registerRoutes = () => {
 
         redirects.forEach((redirect) => {
             /**
-             * Extract target info, such as hash.
-             * 
-             * Required to re-formulate the correct endpoint.
-             */
-            const parsedTo = url.parse(redirect.to);
-
-            /**
              * Detect case insensitive modifier when regex is enclosed by
              * / ... /i
              */
@@ -56,17 +49,18 @@ _private.registerRoutes = () => {
 
             debug('register', redirect.from);
             customRedirectsRouter.get(new RegExp(redirect.from, options), function (req, res) {
-                const maxAge = redirect.permanent ? config.get('caching:customRedirects:maxAge') : 0,
-                    parsedUrl = url.parse(req.originalUrl);
+                const maxAge = redirect.permanent ? config.get('caching:customRedirects:maxAge') : 0;
+                const fromURL = url.parse(req.originalUrl);
+                const toURL = url.parse(redirect.to);
+
+                toURL.pathname = fromURL.pathname.replace(new RegExp(redirect.from, options), toURL.pathname),
+                toURL.search = fromURL.search;
 
                 res.set({
                     'Cache-Control': `public, max-age=${maxAge}`
                 });
-                res.redirect(redirect.permanent ? 301 : 302, url.format({
-                    pathname: parsedUrl.pathname.replace(new RegExp(redirect.from, options), parsedTo.pathname),
-                    search: parsedUrl.search,
-                    hash: parsedTo.hash
-                }));
+
+                res.redirect(redirect.permanent ? 301 : 302, url.format(toURL));
             });
         });
     } catch (err) {

core/test/regression/site/frontend_spec.js
@@ -673,6 +673,7 @@ describe('Frontend Routing', function () {
         });
     });
 
+    // TODO: convert to unit tests
     describe('Redirects (use redirects.json from test/utils/fixtures/data)', function () {
         var ghostServer;
 
@@ -919,6 +920,28 @@ describe('Frontend Routing', function () {
                     .expect('Cache-Control', testUtils.cacheRules.public)
                     .end(function (err, res) {
                         res.headers.location.should.eql('/blog/expect-redirect');
+                        doEnd(done)(err, res);
+                    });
+            });
+        });
+
+        describe('external url redirect', function () {
+            it('with trailing slash', function (done) {
+                request.get('/external-url/')
+                    .expect(302)
+                    .expect('Cache-Control', testUtils.cacheRules.public)
+                    .end(function (err, res) {
+                        res.headers.location.should.eql('https://ghost.org/');
+                        doEnd(done)(err, res);
+                    });
+            });
+
+            it('without trailing slash', function (done) {
+                request.get('/external-url')
+                    .expect(302)
+                    .expect('Cache-Control', testUtils.cacheRules.public)
+                    .end(function (err, res) {
+                        res.headers.location.should.eql('https://ghost.org/');
                         doEnd(done)(err, res);
                     });
             });