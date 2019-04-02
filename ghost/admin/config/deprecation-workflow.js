984b658ff60562cd89412e0a91277a00f06f851b
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-04-02 10:53:20 +0100

    Added ember-cli-deprecation-workflow
    no issue
    - silences current deprecations so that Travis doesn't fail due to test logs being too long

---
+++ ghost/admin/config/deprecation-workflow.js
@@ -0,0 +1,15 @@
+self.deprecationWorkflow = self.deprecationWorkflow || {};
+self.deprecationWorkflow.config = {
+    workflow: [
+        {handler: 'silence', matchId: 'ember-views.curly-components.jquery-element'},
+        {handler: 'silence', matchId: 'computed-property.volatile'},
+        {handler: 'silence', matchId: 'computed-property.override'},
+        {handler: 'silence', matchId: 'application-controller.router-properties'},
+        {handler: 'silence', matchId: 'events.remove-all-listeners'},
+        {handler: 'silence', matchId: 'object.new-constructor'},
+        {handler: 'silence', matchId: 'ember-polyfills.deprecate-merge'},
+        {handler: 'silence', matchId: 'events.inherited-function-listeners'},
+        {handler: 'silence', matchId: 'ember-component.send-action'},
+        {handler: 'silence', matchId: 'transition-state'}
+    ]
+};
