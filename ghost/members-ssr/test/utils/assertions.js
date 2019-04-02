803b09664cc61b223824c3ebb281fd9db84d4f2d
Fabien O'Carroll <fabien@allou.is>
2019-04-02 10:20:43 +0200

    Ran `slimer new members-ssr`

---
+++ ghost/members-ssr/test/utils/assertions.js
@@ -0,0 +1,11 @@
+/**
+ * Custom Should Assertions
+ *
+ * Add any custom assertions to this file.
+ */
+
+// Example Assertion
+// should.Assertion.add('ExampleAssertion', function () {
+//     this.params = {operator: 'to be a valid Example Assertion'};
+//     this.obj.should.be.an.Object;
+// });
