f0b4a130be08946c45db313e3c15bc324b735e7b
Rishabh Garg <zrishabhgarg@gmail.com>
2019-04-13 10:38:56 +0530

    Added Admin API for deleting members (#10673)
    no issue
    
    - Added new API to delete members
    - Added methods to handle e2e member deletion
    - Deleting member via Admin leads to
      - Removal of member from payment processor and cancelling all active subscriptions immediately
      - Removal of member information from DB

--- core/server/api/v2/members.js
+++ core/server/api/v2/members.js
@@ -29,6 +29,26 @@ const members = {
         query(frame) {
             return memberUserObject.get(frame.data, frame.options);
         }
+    },
+
+    destroy: {
+        statusCode: 204,
+        headers: {},
+        options: [
+            'id'
+        ],
+        validation: {
+            options: {
+                id: {
+                    required: true
+                }
+            }
+        },
+        permissions: true,
+        query(frame) {
+            frame.options.require = true;
+            return memberUserObject.destroy(frame.options).return(null);
+        }
     }
 };
 
