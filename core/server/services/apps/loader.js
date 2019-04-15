9cccae70ab18272cf00322d5a6617c09ead04021
Fabien O'Carroll <fabien@allou.is>
2019-04-15 12:15:48 +0200

    Removed installAppByName method
    no-issue
    
    Only external apps needed the install step, we can safely remove this now.

--- core/server/services/apps/loader.js
+++ core/server/services/apps/loader.js
@@ -5,7 +5,6 @@ const config = require('../../config');
 const common = require('../../lib/common');
 const AppProxy = require('./proxy');
 const Sandbox = require('./sandbox');
-const AppDependencies = require('./dependencies');
 const AppPermissions = require('./permissions');
 
 // Get the full path to an app by name
@@ -49,40 +48,6 @@ function getAppByName(name, permissions) {
 }
 
 module.exports = {
-    // Load a app and return the instantiated app
-    installAppByName: function (name) {
-        // Install the apps dependencies first
-        const appPath = getAppAbsolutePath(name);
-        const deps = new AppDependencies(appPath);
-
-        return deps.install()
-            .then(function () {
-                // Load app permissions
-                const perms = new AppPermissions(appPath);
-
-                return perms.read().catch(function (err) {
-                    // Provide a helpful error about which app
-                    return Promise.reject(new Error(common.i18n.t('errors.apps.permissionsErrorLoadingApp.error', {
-                        name: name,
-                        message: err.message
-                    })));
-                });
-            })
-            .then(function (appPerms) {
-                const {app, proxy} = getAppByName(name, appPerms);
-
-                // Check for an install() method on the app.
-                if (!_.isFunction(app.install)) {
-                    return Promise.reject(new Error(common.i18n.t('errors.apps.noInstallMethodLoadingApp.error', {name: name})));
-                }
-
-                // Run the app.install() method
-                // Wrapping the install() with a when because it's possible
-                // to not return a promise from it.
-                return Promise.resolve(app.install(proxy)).return(app);
-            });
-    },
-
     // Activate a app and return it
     activateAppByName: function (name) {
         const perms = new AppPermissions(getAppAbsolutePath(name));
