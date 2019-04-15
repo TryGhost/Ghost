ca2dc1889af73a3112628173998fde97ac2c44c8
Fabien O'Carroll <fabien@allou.is>
2019-04-15 15:06:17 +0200

    Removed unused filters object from apps proxy
    no-issue
    
    This is not used by any current apps, and apps are considered
    deprecated.

--- core/test/unit/services/apps/proxy_spec.js
+++ core/test/unit/services/apps/proxy_spec.js
@@ -1,7 +1,6 @@
 const should = require('should'),
     sinon = require('sinon'),
     helpers = require('../../../../server/helpers/register'),
-    filters = require('../../../../server/filters'),
     AppProxy = require('../../../../server/services/apps/proxy'),
     routing = require('../../../../server/services/routing');
 
@@ -28,56 +27,9 @@ describe('Apps', function () {
         it('creates a ghost proxy', function () {
             var appProxy = AppProxy.getInstance('TestApp');
 
-            should.exist(appProxy.filters);
-            should.exist(appProxy.filters.register);
-            should.exist(appProxy.filters.deregister);
-
             should.exist(appProxy.helpers);
             should.exist(appProxy.helpers.register);
             should.exist(appProxy.helpers.registerAsync);
-        });
-
-        it('allows filter registration', function (done) {
-            var registerSpy = sinon.spy(filters, 'registerFilter'),
-                appProxy = AppProxy.getInstance('TestApp'),
-                fakePosts = [{id: 0}, {id: 1}],
-                filterStub = sinon.spy(function (val) {
-                    return val;
-                });
-
-            appProxy.filters.register('testFilter', 5, filterStub);
-
-            registerSpy.called.should.equal(true);
-
-            filterStub.called.should.equal(false);
-
-            filters.doFilter('testFilter', fakePosts)
-                .then(function () {
-                    filterStub.called.should.equal(true);
-                    appProxy.filters.deregister('testFilter', 5, filterStub);
-                    done();
-                })
-                .catch(done);
-        });
-
-        it('allows filter deregistration', function (done) {
-            var registerSpy = sinon.spy(filters, 'deregisterFilter'),
-                appProxy = AppProxy.getInstance('TestApp'),
-                fakePosts = [{id: 0}, {id: 1}],
-                filterStub = sinon.stub().returns(fakePosts);
-
-            appProxy.filters.deregister('prePostsRender', 5, filterStub);
-
-            registerSpy.called.should.equal(true);
-
-            filterStub.called.should.equal(false);
-
-            filters.doFilter('prePostsRender', fakePosts)
-                .then(function () {
-                    filterStub.called.should.equal(false);
-                    done();
-                })
-                .catch(done);
         });
 
         it('allows helper registration', function () {
