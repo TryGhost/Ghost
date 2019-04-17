64d1964bc094eea6c8915a4366262d27721c5582
Zimo <peter.zimon@gmail.com>
2019-04-17 10:34:19 +0200

    Members: disabled signup button during signup

--- core/server/lib/members/static/auth/components/FormSubmit.js
+++ core/server/lib/members/static/auth/components/FormSubmit.js
@@ -1,7 +1,10 @@
-export default ({onClick, label}) => (
+export default ({onClick, buttonClass, showSpinner, disabled, label}) => (
     <div className="mt7">
-        <button type="submit" className="gm-btn-blue" onClick={onClick}>
-            { label }
+        <button type="submit" className={ (buttonClass ? buttonClass : "gm-btn-blue") } onClick={onClick} disabled={ ((disabled || showSpinner) ? "disabled" : "") }>
+            <span>
+                { (showSpinner ? <span className="gm-spinner"></span> : "")}
+                { label }
+            </span>
         </button>
     </div>
 );
