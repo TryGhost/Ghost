32266fddfd4b0a805ab5fb299f82f639ba2d00c8
Nazar Gargol <nazargargol@gmail.com>
2019-04-17 19:18:44 +0200

    Added notification access persmission for users of Editor role
    closes #9546
    
    - Updated permissions  fixture for Editor role
    - Added migration for permissions for Editor users
    - Updated tests

--- core/test/unit/data/schema/integrity_spec.js
+++ core/test/unit/data/schema/integrity_spec.js
@@ -20,7 +20,7 @@ var should = require('should'),
 describe('DB version integrity', function () {
     // Only these variables should need updating
     const currentSchemaHash = 'fda0398e93a74b2dc435cb4c026679ba';
-    const currentFixturesHash = '42e15796b3c9bdcf0d0ec7eb66a1abf5';
+    const currentFixturesHash = '6be3e640cb0f757d472c140e59418f54';
 
     // If this test is failing, then it is likely a change has been made that requires a DB version bump,
     // and the values above will need updating as confirmation
