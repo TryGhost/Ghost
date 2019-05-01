4940b2279c1ab1ebffb30a9388fcc33aa11832c1
kirrg001 <katharina.irrgang@googlemail.com>
2019-05-01 22:05:42 +0200

    Added comments for post scheduling
    no issue
    
    - jsdoc
    - inline comments

--- core/server/adapters/scheduling/index.js
+++ core/server/adapters/scheduling/index.js
@@ -1,8 +1,20 @@
 const postScheduling = require(__dirname + '/post-scheduling');
 
 /**
- * scheduling modules:
- *   - post scheduling: publish posts/pages when scheduled
+ * @description Initialise all scheduler modules.
+ *
+ * We currently only support post-scheduling: publish posts/pages when scheduled.
+ *
+ * @param {Object} options
+ *                 {
+ *                      schedulerUrl: [String] Remote scheduler domain.
+ *                      active:       [String] Name of the custom scheduler.
+ *                      apiUrl:       [String] Target Ghost API url.
+ *                      internalPath: [String] Folder path where to find the default scheduler.
+ *                      contentPath:  [String] Folder path where to find custom schedulers.
+ *                  }
+ *
+ * @TODO: Simplify the passed in options.
  */
 exports.init = function init(options) {
     options = options || {};
