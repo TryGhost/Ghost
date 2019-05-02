d412e624d5c7f109cc674ee8b6c8904724625b51
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-05-02 17:14:00 +0100

    Use extracted @tryghost/kg-clean-basic-html package
    no issue
    - `cleanBasicHtml` has been extracted to an external package so that it can be used inside the extracted `@tryghost/kg-parser-plugins` package server-side

--- ghost/admin/lib/koenig-editor/addon/helpers/clean-basic-html.js
+++ ghost/admin/lib/koenig-editor/addon/helpers/clean-basic-html.js
@@ -1,38 +1,13 @@
+import cleanBasicHtml from '@tryghost/kg-clean-basic-html';
 import {helper} from '@ember/component/helper';
 import {isArray} from '@ember/array';
 
-export function cleanBasicHtml(html = '') {
+export function cleanBasicHtmlHelper(html = '') {
     if (isArray(html)) {
         html = html[0] || '';
     }
 
-    let cleanHtml = html
-        .replace(/<br>/g, ' ')
-        .replace(/(\s|&nbsp;){2,}/g, ' ')
-        .trim()
-        .replace(/^&nbsp;|&nbsp$/g, '')
-        .trim();
-
-    // remove any elements that have a blank textContent
-    if (cleanHtml) {
-        let doc = new DOMParser().parseFromString(cleanHtml, 'text/html');
-
-        doc.body.querySelectorAll('*').forEach((element) => {
-            if (!element.textContent.trim()) {
-                if (element.textContent.length > 0) {
-                    // keep a single space to avoid collapsing spaces
-                    let space = document.createTextNode(' ');
-                    element.replaceWith(space);
-                } else {
-                    element.remove();
-                }
-            }
-        });
-
-        cleanHtml = doc.body.innerHTML;
-    }
-
-    return cleanHtml;
+    return cleanBasicHtml(html);
 }
 
-export default helper(cleanBasicHtml);
+export default helper(cleanBasicHtmlHelper);
