2b1afa26ae564dc57895951d6ad8d377216bd94d
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-05-02 14:21:07 +0100

    Added ES module build to kg-parser-plugins
    no issue
    - this package will be used inside Ghost-Admin (and maybe other client-side projects) where it will be bundled directly with no transpilation, as such we need to transpile it here
    - target browsers are set to the same as those supported in Ghost-Admin
    - change the `module` configuration to point to the build rather than source files

--- koenig/kg-parser-plugins/rollup.config.js
+++ koenig/kg-parser-plugins/rollup.config.js
@@ -1,5 +1,6 @@
 /* eslint-env node */
 import pkg from './package.json';
+import babel from 'rollup-plugin-babel';
 
 export default [
     // Node build.
@@ -11,5 +12,31 @@ export default [
             format: 'cjs',
             interop: false
         }
+    },
+
+    // ES module build
+    // Transpiles to target browser support for use in client apps
+    {
+        input: pkg.source,
+        output: {
+            file: pkg.module,
+            format: 'es',
+            sourcemap: true
+        },
+        plugins: [
+            babel({
+                presets: [
+                    ['@babel/preset-env', {
+                        modules: false,
+                        targets: [
+                            'last 2 Chrome versions',
+                            'last 2 Firefox versions',
+                            'last 2 Safari versions'
+                        ].join(', ')
+                    }]
+                ],
+                exclude: ['node_modues/**', '../../node_modules/**']
+            })
+        ]
     }
 ];
