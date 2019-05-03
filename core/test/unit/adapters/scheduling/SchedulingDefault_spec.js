TryGhost/Ghost

commit 4acc375fb600bf9b46041e26e3d06b71a5b141b1

Bumped knex & bookshelf dependencies (#10404)
refs #9389, refs #9248

- https://github.com/bookshelf/bookshelf/releases/tag/0.14.0
- Bookshelf has fixed it's previous attr handling, see https://github.com/bookshelf/bookshelf/pull/1848
- SQlite3 double slashes was merged into knex and released 👻tgriesser/knex@c746dea

core/server/adapters/scheduling/post-scheduling/index.js
@@ -14,7 +14,7 @@ _private.normalize = function normalize(options) {
         url: `${urlService.utils.urlJoin(apiUrl, 'schedules', 'posts', object.get('id'))}?client_id=${client.get('slug')}&client_secret=${client.get('secret')}`,
         extra: {
             httpMethod: 'PUT',
-            oldTime: object.updated('published_at') ? moment(object.updated('published_at')).valueOf() : null
+            oldTime: object.previous('published_at') ? moment(object.previous('published_at')).valueOf() : null
         }
     };
 };

core/server/models/base/index.js
@@ -117,8 +117,6 @@ ghostBookshelf.Model = ghostBookshelf.Model.extend({
     emitChange: function (model, event, options) {
         debug(model.tableName, event);
 
-        const previousAttributes = model._previousAttributes;
-
         if (!options.transacting) {
             return common.events.emit(event, model, options);
         }
@@ -139,7 +137,6 @@ ghostBookshelf.Model = ghostBookshelf.Model.extend({
                 }
 
                 _.each(this.ghostEvents, (ghostEvent) => {
-                    model._previousAttributes = previousAttributes;
                     common.events.emit(ghostEvent, model, _.omit(options, 'transacting'));
                 });
 
@@ -227,11 +224,9 @@ ghostBookshelf.Model = ghostBookshelf.Model.extend({
         }
     },
 
-    onSaving: function onSaving(newObj) {
+    onSaving: function onSaving() {
         // Remove any properties which don't belong on the model
         this.attributes = this.pick(this.permittedAttributes());
-        // Store the previous attributes so we can tell what was updated later
-        this._updatedAttributes = newObj.previousAttributes();
     },
 
     /**
@@ -471,27 +466,8 @@ ghostBookshelf.Model = ghostBookshelf.Model.extend({
         return proto.toJSON.call(this, options);
     },
 
-    // Get attributes that have been updated (values before a .save() call)
-    updatedAttributes: function updatedAttributes() {
-        return this._updatedAttributes || {};
-    },
-
-    // Get a specific updated attribute value
-    updated: function updated(attr) {
-        return this.updatedAttributes()[attr];
-    },
-
-    /**
-     * There is difference between `updated` and `previous`:
-     * Depending on the hook (before or after writing into the db), both fields have a different meaning.
-     * e.g. onSaving  -> before db write (has to use previous)
-     *      onUpdated -> after db write  (has to use updated)
-     *
-     * hasDateChanged('attr', {beforeWrite: true})
-     */
-    hasDateChanged: function (attr, options) {
-        options = options || {};
-        return moment(this.get(attr)).diff(moment(options.beforeWrite ? this.previous(attr) : this.updated(attr))) !== 0;
+    hasDateChanged: function (attr) {
+        return moment(this.get(attr)).diff(moment(this.previous(attr))) !== 0;
     },
 
     /**
@@ -574,7 +550,7 @@ ghostBookshelf.Model = ghostBookshelf.Model.extend({
      * - Bookshelf updates the model with the new client data via the `set` function
      * - Bookshelf uses a simple `isEqual` function from lodash to detect real changes
      * - .previous(attr) and .get(attr) returns false obviously
-     * - internally we use our `hasDateChanged` if we have to compare previous/updated dates
+     * - internally we use our `hasDateChanged` if we have to compare previous dates
      * - but Bookshelf is not in our control for this case
      *
      * @IMPORTANT

core/server/api/v0.1/posts.js
@@ -161,7 +161,7 @@ posts = {
                     // If previously was not published and now is (or vice versa), signal the change
                     // @TODO: `statusChanged` get's added for the API headers only. Reconsider this.
                     post.statusChanged = false;
-                    if (model.updated('status') !== model.get('status')) {
+                    if (model.previous('status') !== model.get('status')) {
                         post.statusChanged = true;
                     }
 

core/server/api/v2/posts.js
@@ -132,9 +132,9 @@ module.exports = {
             return models.Post.edit(frame.data.posts[0], frame.options)
                 .then((model) => {
                     if (model.get('status') === 'published' ||
-                        model.get('status') === 'draft' && model.updated('status') === 'published') {
+                        model.get('status') === 'draft' && model.previous('status') === 'published') {
                         this.headers.cacheInvalidate = true;
-                    } else if (model.get('status') === 'draft' && model.updated('status') !== 'published') {
+                    } else if (model.get('status') === 'draft' && model.previous('status') !== 'published') {
                         this.headers.cacheInvalidate = {
                             value: urlService.utils.urlFor({
                                 relativeUrl: urlService.utils.urlJoin('/p', model.get('uuid'), '/')

core/server/models/base/listeners.js
@@ -57,7 +57,7 @@ common.events.on('settings.active_timezone.edited', function (settingModel, opti
     options = _.merge({}, options, {context: {internal: true}});
 
     var newTimezone = settingModel.attributes.value,
-        previousTimezone = settingModel._updatedAttributes.value,
+        previousTimezone = settingModel._previousAttributes.value,
         timezoneOffsetDiff = moment.tz(previousTimezone).utcOffset() - moment.tz(newTimezone).utcOffset();
 
     // CASE: TZ was updated, but did not change

core/server/models/post.js
@@ -72,14 +72,11 @@ Post = ghostBookshelf.Model.extend({
         return filteredKeys;
     },
 
-    emitChange: function emitChange(event, options) {
+    emitChange: function emitChange(event, options = {}) {
         let eventToTrigger;
+        let resourceType = this.get('page') ? 'page' : 'post';
 
-        var resourceType = this.get('page') ? 'page' : 'post';
-
-        if (options.useUpdatedAttribute) {
-            resourceType = this.updated('page') ? 'page' : 'post';
-        } else if (options.usePreviousAttribute) {
+        if (options.usePreviousAttribute) {
             resourceType = this.previous('page') ? 'page' : 'post';
         }
 
@@ -112,26 +109,26 @@ Post = ghostBookshelf.Model.extend({
     },
 
     onUpdated: function onUpdated(model, attrs, options) {
-        model.statusChanging = model.get('status') !== model.updated('status');
+        model.statusChanging = model.get('status') !== model.previous('status');
         model.isPublished = model.get('status') === 'published';
         model.isScheduled = model.get('status') === 'scheduled';
-        model.wasPublished = model.updated('status') === 'published';
-        model.wasScheduled = model.updated('status') === 'scheduled';
-        model.resourceTypeChanging = model.get('page') !== model.updated('page');
+        model.wasPublished = model.previous('status') === 'published';
+        model.wasScheduled = model.previous('status') === 'scheduled';
+        model.resourceTypeChanging = model.get('page') !== model.previous('page');
         model.publishedAtHasChanged = model.hasDateChanged('published_at');
         model.needsReschedule = model.publishedAtHasChanged && model.isScheduled;
 
         // Handle added and deleted for post -> page or page -> post
         if (model.resourceTypeChanging) {
             if (model.wasPublished) {
-                model.emitChange('unpublished', Object.assign({useUpdatedAttribute: true}, options));
+                model.emitChange('unpublished', Object.assign({usePreviousAttribute: true}, options));
             }
 
             if (model.wasScheduled) {
-                model.emitChange('unscheduled', Object.assign({useUpdatedAttribute: true}, options));
+                model.emitChange('unscheduled', Object.assign({usePreviousAttribute: true}, options));
             }
 
-            model.emitChange('deleted', Object.assign({useUpdatedAttribute: true}, options));
+            model.emitChange('deleted', Object.assign({usePreviousAttribute: true}, options));
             model.emitChange('added', options);
 
             if (model.isPublished) {
@@ -733,8 +730,6 @@ Post = ghostBookshelf.Model.extend({
     /**
      * ### Edit
      * Fetches and saves to Post. See model.Base.edit
-     *
-     * @extends ghostBookshelf.Model.edit to handle returning the full object and manage _updatedAttributes
      * **See:** [ghostBookshelf.Model.edit](base.js.html#edit)
      */
     edit: function edit(data, unfilteredOptions) {
@@ -752,7 +747,7 @@ Post = ghostBookshelf.Model.extend({
                         .then((found) => {
                             if (found) {
                                 // Pass along the updated attributes for checking status changes
-                                found._updatedAttributes = post._updatedAttributes;
+                                found._previousAttributes = post._previousAttributes;
                                 return found;
                             }
                         });

core/server/models/user.js
@@ -67,7 +67,7 @@ User = ghostBookshelf.Model.extend({
     },
 
     onUpdated: function onUpdated(model, response, options) {
-        model.statusChanging = model.get('status') !== model.updated('status');
+        model.statusChanging = model.get('status') !== model.previous('status');
         model.isActive = _.includes(activeStates, model.get('status'));
 
         if (model.statusChanging) {
@@ -953,6 +953,7 @@ User = ghostBookshelf.Model.extend({
             var userWithEmail = users.find(function findUser(user) {
                 return user.get('email').toLowerCase() === email.toLowerCase();
             });
+
             if (userWithEmail) {
                 return userWithEmail;
             }

core/server/services/routing/CollectionRouter.js
@@ -128,7 +128,7 @@ class CollectionRouter extends ParentRouter {
 
     _onTimezoneEdited(settingModel) {
         const newTimezone = settingModel.attributes.value,
-            previousTimezone = settingModel._updatedAttributes.value;
+            previousTimezone = settingModel._previousAttributes.value;
 
         if (newTimezone === previousTimezone) {
             return;

commit 3663afa9727919839fffd4ee00687fa7fd047dab

🐛 Fixed post scheduling on restart
no issue

- might fix https://forum.ghost.org/t/scheduled-posts-never-publish/6873/10
- CASES:
  - You restart Ghost and you have a post to schedule
  - I am not aware of any other use cases right now
- caused by https://github.com/TryGhost/Ghost/commit/4acc375fb600bf9b46041e26e3d06b71a5b141b1#diff-4726ce3c4d18d41afad4b46cb0aa7dd3
  - the bug exists since 2.12
  - Bookshelf added support (or better said fixed a bug) for accessing previous attributes
  - `object.updated('published_at')` always returned "undefined", because the self-implementation < 2.12 only remembered previous attributes after update (see https://github.com/TryGhost/Ghost/blob/2.11.0/core/server/models/base/index.js#L234)
  - but `object.previous('published_at')` returns the current value (object.get('published_at') === object.previous('published_at') -> and that's why rescheduling on bootstrap never worked

core/server/adapters/scheduling/SchedulingDefault.js
@@ -74,9 +74,24 @@ SchedulingDefault.prototype.schedule = function (object) {
  *                              oldTime:    [Number] The previous published time.
  *                          }
  *                       }
+ * @param {Object} options
+ *                      {
+ *                          bootstrap: [Boolean]
+ *                      }
  */
-SchedulingDefault.prototype.reschedule = function (object) {
-    this._deleteJob({time: object.extra.oldTime, url: object.url});
+SchedulingDefault.prototype.reschedule = function (object, options = {bootstrap: false}) {
+    /**
+     * CASE:
+     * The post scheduling unit calls "reschedule" on bootstrap, because other custom scheduling implementations
+     * could use a database and we need to give the chance to update the job (delete + re-add).
+     *
+     * We receive a "bootstrap" variable to ensure that jobs are scheduled correctly for this scheduler implementation,
+     * because "object.extra.oldTime" === "object.time". If we mark the job as deleted, it won't get scheduled.
+     */
+    if (!options.bootstrap) {
+        this._deleteJob({time: object.extra.oldTime, url: object.url});
+    }
+
     this._addJob(object);
 };
 

core/server/adapters/scheduling/post-scheduling/index.js
@@ -86,7 +86,9 @@ exports.init = function init(options = {}) {
             }
 
             scheduledPosts.forEach((model) => {
-                adapter.reschedule(_private.normalize({model, apiUrl, client}));
+                // NOTE: We are using reschedule, because custom scheduling adapter could use a database, which needs to be updated
+                //       and not an in-process implementation!
+                adapter.reschedule(_private.normalize({model, apiUrl, client}), {bootstrap: true});
             });
         })
         .then(() => {

core/test/unit/adapters/scheduling/SchedulingDefault_spec.js
@@ -60,6 +60,55 @@ describe('Scheduling Default Adapter', function () {
             ]);
         });
 
+        it('reschedule: default', function (done) {
+            sinon.stub(scope.adapter, '_pingUrl');
+
+            const time = moment().add(200, 'milliseconds').valueOf();
+
+            scope.adapter.schedule({
+                time: time,
+                url: 'something',
+                extra: {
+                    oldTime: null,
+                    method: 'PUT'
+                }
+            });
+
+            scope.adapter.reschedule({
+                time: time,
+                url: 'something',
+                extra: {
+                    oldTime: time,
+                    method: 'PUT'
+                }
+            });
+
+            setTimeout(() => {
+                scope.adapter._pingUrl.calledOnce.should.eql(true);
+                done();
+            }, 250);
+        });
+
+        it('reschedule: simulate restart', function (done) {
+            sinon.stub(scope.adapter, '_pingUrl');
+
+            const time = moment().add(100, 'milliseconds').valueOf();
+
+            scope.adapter.reschedule({
+               time: time,
+                url: 'something',
+                extra: {
+                   oldTime: time,
+                    method: 'PUT'
+                }
+            }, {bootstrap: true});
+
+            setTimeout(() => {
+                scope.adapter._pingUrl.calledOnce.should.eql(true);
+                done();
+            }, 150);
+        });
+
         it('run', function (done) {
             // 1000 jobs, but only the number x are under 1 minute
             var timestamps = _.map(_.range(1000), function (i) {