b2ed906773dc9d615c9d25cf8312e6995b538021
Aileen Nowak <aileen@ghost.org>
2019-05-06 21:27:54 +0800

    🎨 Allowed protocol overwrite for canonical URLs (#10729)
    closes #10709
    
    - Only transform a canonical URL that is identical with the Blog URL to a relative URL when the protocol matches as well
    - Leave the canonical URL absolute for all other cases
    - Use case for this is e. g. when users want to port over their Facebook comments/shares/likes after a move from `http` to `https`

---
+++ core/test/unit/api/shared/serializers/input/utils/url_spec.js
@@ -0,0 +1,57 @@
+const should = require('should');
+const sinon = require('sinon');
+const utils = require('../../../../../../../server/services/url/utils');
+const url = require('../../../../../../../server/api/v2/utils/serializers/input/utils/url');
+
+describe('Unit: v2/utils/serializers/input/utils/url', function () {
+    describe('forPost', function () {
+        beforeEach(function () {
+            sinon.stub(utils, 'getBlogUrl')
+                .returns('https://blogurl.com');
+        });
+
+        afterEach(function () {
+            sinon.restore();
+        });
+
+        it('should transform canonical_url when protocol and domain match', function () {
+            const attrs = {
+                canonical_url: 'https://blogurl.com/hello-world'
+            };
+
+            url.forPost(attrs, {});
+
+            should.equal(attrs.canonical_url, '/hello-world');
+        });
+
+        it('should transform canonical_url when protocol and domain match with backslash in the end', function () {
+            const attrs = {
+                canonical_url: 'https://blogurl.com/hello-world/'
+            };
+
+            url.forPost(attrs, {});
+
+            should.equal(attrs.canonical_url, '/hello-world/');
+        });
+
+        it('should not transform canonical_url when different domains', function () {
+            const attrs = {
+                canonical_url: 'http://ghost.org/no-transform'
+            };
+
+            url.forPost(attrs, {});
+
+            should.equal(attrs.canonical_url, 'http://ghost.org/no-transform');
+        });
+
+        it('should not transform canonical_url when different protocols', function () {
+            const attrs = {
+                canonical_url: 'http://blogurl.com/no-transform'
+            };
+
+            url.forPost(attrs, {});
+
+            should.equal(attrs.canonical_url, 'http://blogurl.com/no-transform');
+        });
+    });
+});
