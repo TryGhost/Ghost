f8ccb81dfc15ef833ad08525883ce89ade693a6d
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-05-06 14:51:23 +0100

    Fixed deprecated access of `controller.{currentPath,currentRouteName}`
    no issue
    
    - replaced access of `controller.{currentPath,currentRouteName}` with `router.currentRouteName`
    - https://deprecations.emberjs.com/v3.x/#toc_application-controller-router-properties

--- ghost/admin/app/controllers/setup.js
+++ ghost/admin/app/controllers/setup.js
@@ -1,17 +1,17 @@
 /* eslint-disable ghost/ember/alias-model-in-controller */
-import Controller, {inject as controller} from '@ember/controller';
+import Controller from '@ember/controller';
 import {computed} from '@ember/object';
 import {match} from '@ember/object/computed';
 import {inject as service} from '@ember/service';
 
 export default Controller.extend({
-    appController: controller('application'),
     ghostPaths: service(),
+    router: service(),
 
-    showBackLink: match('appController.currentRouteName', /^setup\.(two|three)$/),
+    showBackLink: match('router.currentRouteName', /^setup\.(two|three)$/),
 
-    backRoute: computed('appController.currentRouteName', function () {
-        let currentRoute = this.get('appController.currentRouteName');
+    backRoute: computed('router.currentRouteName', function () {
+        let currentRoute = this.router.currentRouteName;
 
         return currentRoute === 'setup.two' ? 'setup.one' : 'setup.two';
     })
