106b67cbbbf3abfe7c37d8e6064c752d03c6c386
Rish <zrishabhgarg@gmail.com>
2019-05-08 09:48:42 +0530

    Cleaned up helper to combine both block and simple helper

--- core/server/helpers/member.js
+++ core/server/helpers/member.js
@@ -1,54 +1,36 @@
-// # Members block helper
+// # Members helper
 // Usage: `{{#member}} {{/member}}`
+// OR `{{member 'property'}}`
 //
 const  proxy = require('./proxy');
 const labs = proxy.labs;
 const SafeString = proxy.SafeString;
 
 // Block helper designed for members context
-// module.exports = function member(options) {
-//     let ret = '';
-//     if (labs.isSet('members')) {
-//         let context = {
-//             loggedIn: !!options.data.member,
-//             memberName: (options.data.member && options.data.member.name) || '',
-//             memberEmail: (options.data.email && options.data.member.email) || '',
-//         };
-//         if (options.data.root.context.includes('post')) {
-//             context.restrictedPost = !options.data.root.post.html;
-//         }
-//         ret = options.fn(Object.assign({}, this, context));
-//     }
-//     return ret;
-// };
-
-// Block helper designed for members context
 module.exports = function member(...args) {
-    let options = {};
-    let prop = '';
-    if (args.length === 1) {
-        options = args[0];
-    } else if (args.length === 2) {
-        prop = args[0];
-        options = args[1];
+    if (args.length < 1) {
+        return null;
     }
+    let options = args[args.length - 1];
+    let prop = '';
     let context = {};
-    let ret = '';
+    if (args.length === 2) {
+        prop = args[0];
+    }
     if (labs.isSet('members')) {
         context = {
-            loggedIn: !!options.data.member,
+            isLoggedIn: !!options.data.member,
             name: (options.data.member && options.data.member.name) || '',
             email: (options.data.email && options.data.member.email) || '',
         };
         if (options.data.root.context.includes('post')) {
-            context.restrictedPost = !options.data.root.post.html;
+            context.isRestrictedPost = !options.data.root.post.html;
         }
     }
     if (options.fn) {
-        ret = options.fn(Object.assign({}, this, {
+        return options.fn(Object.assign({}, this, {
             member: context
         }));
-        return ret;
     }
     return context[prop] || '';
 };
