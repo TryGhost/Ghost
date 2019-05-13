e82f625a154641ce09e0eaaf68ad1e4b98320be0
Fabien O'Carroll <fabien@allou.is>
2019-05-13 10:11:29 +0200

    Removed members references in Gruntfile
    refs #10739
    
    These leftover references were causing issues when running grunt
    commands, now that the files are no longer there.

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,7 +78,6 @@ const configureGrunt = function (grunt) {
                 files: [
                     'core/ghost-server.js',
                     'core/server/**/*.js',
-                    '!core/server/lib/members/static/auth/**/*.js',
                     'config.*.json',
                     '!config.testing.json'
                 ],
@@ -284,8 +283,7 @@ const configureGrunt = function (grunt) {
                     npmInstall: true
                 },
                 projects: {
-                    'core/client': 'init',
-                    'core/server/lib/members/static/auth': 'init'
+                    'core/client': 'init'
                 }
             },
 
@@ -294,14 +292,12 @@ const configureGrunt = function (grunt) {
             },
 
             prod: {
-                'core/client': 'shell:ember:prod',
-                'core/server/lib/members/static/auth': 'shell:preact:prod'
+                'core/client': 'shell:ember:prod'
             },
 
             watch: {
                 projects: {
-                    'core/client': ['shell:ember:watch', '--live-reload-base-url="' + urlService.utils.getSubdir() + '/ghost/"'],
-                    'core/server/lib/members/static/auth': ['shell:preact:dev']
+                    'core/client': ['shell:ember:watch', '--live-reload-base-url="' + urlService.utils.getSubdir() + '/ghost/"']
                 }
             }
         },
