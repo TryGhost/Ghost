bc675c593e0e626e3a50f1bc591b0627156857df
Nazar Gargol <nazargargol@gmail.com>
2019-06-11 13:42:44 +0200

    Renamed urlUtils to urlService to follow project wise convention

--- core/server/web/api/index.js
+++ core/server/web/api/index.js
@@ -1,6 +1,6 @@
 const debug = require('ghost-ignition').debug('web:api:default:app');
 const express = require('express');
-const urlUtils = require('../../services/url/utils');
+const urlService = require('../../services/url');
 const errorHandler = require('../shared/middlewares/error-handler');
 const membersService = require('../../services/members');
 
@@ -11,10 +11,10 @@ module.exports = function setupApiApp() {
     const apiApp = express();
 
     // Mount different API versions
-    apiApp.use(urlUtils.getVersionPath({version: 'v0.1'}), require('./v0.1/app')());
-    apiApp.use(urlUtils.getVersionPath({version: 'v2', type: 'content'}), require('./v2/content/app')());
-    apiApp.use(urlUtils.getVersionPath({version: 'v2', type: 'admin'}), require('./v2/admin/app')());
-    apiApp.use(urlUtils.getVersionPath({version: 'v2', type: 'members'}), labs.members, membersService.api.apiRouter);
+    apiApp.use(urlService.utils.getVersionPath({version: 'v0.1'}), require('./v0.1/app')());
+    apiApp.use(urlService.utils.getVersionPath({version: 'v2', type: 'content'}), require('./v2/content/app')());
+    apiApp.use(urlService.utils.getVersionPath({version: 'v2', type: 'admin'}), require('./v2/admin/app')());
+    apiApp.use(urlService.utils.getVersionPath({version: 'v2', type: 'members'}), labs.members, membersService.api.apiRouter);
 
     // Error handling for requests to non-existent API versions
     apiApp.use(errorHandler.resourceNotFound);
