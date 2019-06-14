cc9c3361986839cfdd284c4e69ebb37d97b9ac47
Nazar Gargol <nazargargol@gmail.com>
2019-06-14 14:51:24 +0200

    Added url-utils stubbing utility for regression tests

--- core/test/utils/urlUtils.js
+++ core/test/utils/urlUtils.js
@@ -1,27 +1,53 @@
 const _ = require('lodash');
+const sinon = require('sinon');
+const config = require('../../server/config');
 const UrlUtils = require('@tryghost/url-utils');
 const urlUtils = require('../../server/lib/url-utils');
 
-const getInstance = (config) => {
-    const params = {
-        url: config.url,
-        adminUrl: config.adminUrl,
-        apiVersions: config.apiVersions,
-        slugs: config.slugs,
-        redirectCacheMaxAge: config.cachingMaxAge,
+let sandbox;
+
+const getInstance = (options) => {
+    const opts = {
+        url: options.url,
+        adminUrl: options.adminUrl,
+        apiVersions: options.apiVersions,
+        slugs: options.slugs,
+        redirectCacheMaxAge: options.redirectCacheMaxAge,
         baseApiPath: '/ghost/api'
     };
 
-    return new UrlUtils(params);
+    return new UrlUtils(opts);
 };
 
-const stubUrlUtils = (config, sandbox) => {
-    const stubInstance = getInstance(config);
+const stubUrlUtils = (options, sandbox) => {
+    const stubInstance = getInstance(options);
 
     Object.keys(urlUtils).forEach((key) => {
         sandbox.stub(urlUtils, key).callsFake(stubInstance[key]);
     });
 };
 
+// Method for regressions tests must be used with restore method
+const stubUrlUtilsFromConfig = () => {
+    sandbox = sinon.createSandbox();
+    const options = {
+        url: config.get('url'),
+        adminUrl: config.get('admin:url'),
+        apiVersions: config.get('api:versions'),
+        slugs: config.get('slugs').protected,
+        redirectCacheMaxAge: config.get('caching:301:maxAge'),
+        baseApiPath: '/ghost/api'
+    };
+    stubUrlUtils(options, sandbox);
+};
+
+const restore = () => {
+    if (sandbox) {
+        sandbox.restore();
+    }
+};
+
 module.exports.stubUrlUtils = stubUrlUtils;
+module.exports.stubUrlUtilsFromConfig = stubUrlUtilsFromConfig;
+module.exports.restore = restore;
 module.exports.getInstance = getInstance;
