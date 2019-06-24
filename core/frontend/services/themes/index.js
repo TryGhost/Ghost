TryGhost/Ghost

commit f8be43524ffa2d75428a7ecbee0c361e3533106e

Extracted frontend folder (#10780)
refs #10790

- Moved /core/apps into core/frontend
- Moved /core/server/helpers to /core/frontend/helpers along with /core/server/services/themes
- Changed helper location in overrides
- Moved /core/server/services/routing to /core/frontend/services
- Moved /core/server/services/url to /core/frontend/services
- Moved /core/server/data/meta to /core/frontend/meta
- Moved /core/server/services/rss to /core/frontend/services
- Moved /core/server/data/xml to /core/frontend/services

core/frontend/services/themes/index.js
@@ -1,0 +1,128 @@
+const _ = require('lodash');
+const debug = require('ghost-ignition').debug('themes');
+const common = require('../../../server/lib/common');
+const themeLoader = require('./loader');
+const active = require('./active');
+const validate = require('./validate');
+const Storage = require('./Storage');
+const settingsCache = require('../../../server/services/settings/cache');
+const engineDefaults = require('./engines/defaults');
+
+let themeStorage;
+
+// @TODO: reduce the amount of things we expose to the outside world
+// Make this a nice clean sensible API we can all understand!
+module.exports = {
+    // Init themes module
+    // TODO: move this once we're clear what needs to happen here
+    init: function initThemes() {
+        var activeThemeName = settingsCache.get('active_theme'),
+            self = this;
+
+        debug('init themes', activeThemeName);
+
+        // Register a listener for server-start to load all themes
+        common.events.on('server.start', function readAllThemesOnServerStart() {
+            themeLoader.loadAllThemes();
+        });
+
+        // Just read the active theme for now
+        return themeLoader
+            .loadOneTheme(activeThemeName)
+            .then(function activeThemeHasLoaded(theme) {
+                // Validate
+                return validate
+                    .check(theme)
+                    .then(function validationSuccess(checkedTheme) {
+                        if (!validate.canActivate(checkedTheme)) {
+                            const checkError = new common.errors.ThemeValidationError({
+                                message: common.i18n.t('errors.middleware.themehandler.invalidTheme', {theme: activeThemeName}),
+                                errorDetails: Object.assign(
+                                    _.pick(checkedTheme, ['checkedVersion', 'name', 'path', 'version']), {
+                                        errors: checkedTheme.results.error
+                                    }
+                                )
+                            });
+
+                            common.logging.error(checkError);
+
+                            self.activate(theme, checkedTheme, checkError);
+                        } else {
+                            // CASE: inform that the theme has errors, but not fatal (theme still works)
+                            if (checkedTheme.results.error.length) {
+                                common.logging.warn(new common.errors.ThemeValidationError({
+                                    errorType: 'ThemeWorksButHasErrors',
+                                    message: common.i18n.t('errors.middleware.themehandler.themeHasErrors', {theme: activeThemeName}),
+                                    errorDetails: Object.assign(
+                                        _.pick(checkedTheme, ['checkedVersion', 'name', 'path', 'version']), {
+                                            errors: checkedTheme.results.error
+                                        }
+                                    )
+                                }));
+                            }
+
+                            debug('Activating theme (method A on boot)', activeThemeName);
+
+                            self.activate(theme, checkedTheme);
+                        }
+                    });
+            })
+            .catch(common.errors.NotFoundError, function (err) {
+                // CASE: active theme is missing, we don't want to exit because the admin panel will still work
+                err.message = common.i18n.t('errors.middleware.themehandler.missingTheme', {theme: activeThemeName});
+                common.logging.error(err);
+            })
+            .catch(function (err) {
+                // CASE: theme threw an odd error, we don't want to exit because the admin panel will still work
+                // This is the absolute catch-all, at this point, we do not know what went wrong!
+                common.logging.error(err);
+            });
+    },
+    // Load themes, soon to be removed and exposed via specific function.
+    loadAll: themeLoader.loadAllThemes,
+    loadOne: themeLoader.loadOneTheme,
+    get storage() {
+        themeStorage = themeStorage || new Storage();
+
+        return themeStorage;
+    },
+    list: require('./list'),
+    validate: validate,
+    toJSON: require('./to-json'),
+    getActive: active.get,
+    getApiVersion: function getApiVersion() {
+        if (this.getActive()) {
+            return this.getActive().engine('ghost-api');
+        } else {
+            return engineDefaults['ghost-api'];
+        }
+    },
+    activate: function activate(loadedTheme, checkedTheme, error) {
+        // no need to check the score, activation should be used in combination with validate.check
+        // Use the two theme objects to set the current active theme
+        try {
+            let previousGhostAPI;
+
+            if (this.getActive()) {
+                previousGhostAPI = this.getApiVersion();
+            }
+
+            active.set(loadedTheme, checkedTheme, error);
+            const currentGhostAPI = this.getApiVersion();
+
+            common.events.emit('services.themes.activated');
+
+            if (previousGhostAPI !== undefined && (previousGhostAPI !== currentGhostAPI)) {
+                common.events.emit('services.themes.api.changed');
+                const siteApp = require('../../web/site/app');
+                siteApp.reload();
+            }
+        } catch (err) {
+            common.logging.error(new common.errors.InternalServerError({
+                message: common.i18n.t('errors.middleware.themehandler.activateFailed', {theme: loadedTheme.name}),
+                err: err
+            }));
+        }
+    },
+    middleware: require('./middleware')
+};

core/frontend/apps/amp/index.js
@@ -1,0 +1,27 @@
+const router = require('./lib/router'),
+    registerHelpers = require('./lib/helpers'),
+    urlUtils = require('../../../server/lib/url-utils'),
+
+    // Dirty requires
+    settingsCache = require('../../../server/services/settings/cache');
+
+function ampRouter(req, res) {
+    if (settingsCache.get('amp') === true) {
+        return router.apply(this, arguments);
+    } else {
+        // routeKeywords.amp: 'amp'
+        let redirectUrl = req.originalUrl.replace(/amp\/$/, '');
+        urlUtils.redirect301(res, redirectUrl);
+    }
+}
+
+module.exports = {
+    activate: function activate(ghost) {
+        // routeKeywords.amp: 'amp'
+        let ampRoute = '*/amp/';
+
+        ghost.routeService.registerRouter(ampRoute, ampRouter);
+
+        registerHelpers(ghost);
+    }
+};

core/frontend/apps/private-blogging/index.js
@@ -1,0 +1,43 @@
+const urlUtils = require('../../../server/lib/url-utils'),
+    common = require('../../../server/lib/common'),
+    middleware = require('./lib/middleware'),
+    router = require('./lib/router'),
+    registerHelpers = require('./lib/helpers'),
+    // routeKeywords.private: 'private'
+    PRIVATE_KEYWORD = 'private';
+
+let checkSubdir = function checkSubdir() {
+    let paths = '';
+
+    if (urlUtils.getSubdir()) {
+        paths = urlUtils.getSubdir().split('/');
+
+        if (paths.pop() === PRIVATE_KEYWORD) {
+            common.logging.error(new common.errors.GhostError({
+                message: common.i18n.t('errors.config.urlCannotContainPrivateSubdir.error'),
+                context: common.i18n.t('errors.config.urlCannotContainPrivateSubdir.description'),
+                help: common.i18n.t('errors.config.urlCannotContainPrivateSubdir.help')
+            }));
+
+            // @TODO: why
+            process.exit(0);
+        }
+    }
+};
+
+module.exports = {
+    activate: function activate(ghost) {
+        let privateRoute = `/${PRIVATE_KEYWORD}/`;
+
+        checkSubdir();
+
+        ghost.routeService.registerRouter(privateRoute, router);
+
+        registerHelpers(ghost);
+    },
+
+    setupMiddleware: function setupMiddleware(siteApp) {
+        siteApp.use(middleware.checkIsPrivate);
+        siteApp.use(middleware.filterPrivateRoutes);
+    }
+};

core/frontend/apps/subscribers/index.js
@@ -1,0 +1,21 @@
+const router = require('./lib/router'),
+    registerHelpers = require('./lib/helpers'),
+    // Dirty requires
+    labs = require('../../../server/services/labs');
+
+module.exports = {
+    activate(ghost) {
+        // routeKeywords.subscribe: 'subscribe'
+        const subscribeRoute = '/subscribe/';
+        // TODO, how to do all this only if the Subscribers flag is set?!
+        registerHelpers(ghost);
+
+        ghost.routeService.registerRouter(subscribeRoute, function labsEnabledRouter(req, res, next) {
+            if (labs.isSet('subscribers')) {
+                return router.apply(this, arguments);
+            }
+
+            next();
+        });
+    }
+};

core/frontend/apps/subscribers/lib/helpers/index.js
@@ -1,0 +1,19 @@
+// Dirty requires!
+const labs = require('../../../../../server/services/labs');
+
+module.exports = function registerHelpers(ghost) {
+    ghost.helpers.register('input_email', require('./input_email'));
+
+    ghost.helpers.register('subscribe_form', function labsEnabledHelper() {
+        let self = this, args = arguments;
+
+        return labs.enabledHelper({
+            flagKey: 'subscribers',
+            flagName: 'Subscribers',
+            helperName: 'subscribe_form',
+            helpUrl: 'https://docs.ghost.org/faq/enable-subscribers-feature/'
+        }, () => {
+            return require('./subscribe_form').apply(self, args);
+        });
+    });
+};

commit 71abf83e28e9acb50eecadaf451388d1ef462d25

Fixed a crash when theme is changed
refs #10790

- The error was happening due to wrong reference to the site app from themes service
- The issue was introduced in f8be43524ffa2d75428a7ecbee0c361e3533106e

core/frontend/services/themes/index.js
@@ -114,7 +114,7 @@ module.exports = {
 
             if (previousGhostAPI !== undefined && (previousGhostAPI !== currentGhostAPI)) {
                 common.events.emit('services.themes.api.changed');
-                const siteApp = require('../../web/site/app');
+                const siteApp = require('../../../server/web/site/app');
                 siteApp.reload();
             }
         } catch (err) {