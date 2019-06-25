6eaea7e699d9dadb3148710f2369dfc7046f92f4
Naz Gargol <nazargargol@gmail.com>
2019-06-25 10:19:35 +0200

    Updated Node v6 and v10 support (#10810)
    no issue
    
    - Updated travis.yml to xenial build
    - Fixes failing tests which require mysql
    - Reason for above: by default, Travis is now running all instances with xenial distro
    - Fixed unit test for amp-img
    - The unit test was doing HTTP request which failed because resources
    were migrated from previously used location
    - Added node v10 support
    - Updated amperize to run under Node v10
    - Droped Node v6 support

--- core/test/unit/apps/amp/amp_content_spec.js
+++ core/test/unit/apps/amp/amp_content_spec.js
@@ -1,9 +1,10 @@
-var should = require('should'),
-    rewire = require('rewire'),
-    configUtils = require('../../../../test/utils/configUtils'),
+const should = require('should');
+const rewire = require('rewire');
+const configUtils = require('../../../../test/utils/configUtils');
+const nock = require('nock');
 
 // Stuff we are testing
-    ampContentHelper = rewire('../../../../server/apps/amp/lib/helpers/amp_content');
+const ampContentHelper = rewire('../../../../server/apps/amp/lib/helpers/amp_content');
 
 // TODO: Amperize really needs to get stubbed, so we can test returning errors
 // properly and make this test faster!
@@ -111,7 +112,7 @@ describe('{{amp_content}} helper', function () {
 
     describe('Transforms and sanitizes HTML', function () {
         beforeEach(function () {
-            configUtils.set({url: 'https://blog.ghost.org/'});
+            configUtils.set({url: 'https://blog.ghost.org/content'});
         });
 
         afterEach(function () {
@@ -120,12 +121,18 @@ describe('{{amp_content}} helper', function () {
         });
 
         it('can transform img tags to amp-img', function (done) {
+            const GIF1x1 = Buffer.from('R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==', 'base64');
+
+            nock('https://blog.ghost.org')
+                .get('/content/images/2019/06/test.jpg')
+                .reply(200, GIF1x1);
+
             var testData = {
-                    html: '<img src="/content/images/2016/08/scheduled2-1.jpg" alt="The Ghost Logo" />',
+                    html: '<img src="/content/images/2019/06/test.jpg" alt="The Ghost Logo" />',
                     updated_at: 'Wed Jul 27 2016 18:17:22 GMT+0200 (CEST)',
                     id: 1
                 },
-                expectedResult = '<amp-img src="https://blog.ghost.org/content/images/2016/08/scheduled2-1.jpg" alt="The Ghost Logo" width="1000" height="281" layout="responsive"></amp-img>',
+                expectedResult = '<amp-img src="https://blog.ghost.org/content/images/2019/06/test.jpg" alt="The Ghost Logo" width="1" height="1" layout="fixed"></amp-img>',
                 ampResult = ampContentHelper.call(testData);
 
             ampResult.then(function (rendered) {
