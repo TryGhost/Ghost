91296261ee82fd61b513d96b5ab2b06405eeb1d9
Fabien O'Carroll <fabien@allou.is>
2019-06-25 14:30:02 +0700

    Updated members-theme-bindings & public/members.js
    no-issue
    
    This updates Ghost to inject the exact urls we want to use for both the
    static members pages and the ssr endpoints we've configured for the
    frontend. This allows us to changes these without having to update the
    members repository, and gives a cleaner split between the two.

--- core/server/public/members-theme-bindings.js
+++ core/server/public/members-theme-bindings.js
@@ -778,8 +778,10 @@ var each = require('lodash/each');
 var browserAuth = require('@tryghost/members-browser-auth');
 module.exports.init = init;
 function init($__0) {
-  var siteUrl = $__0.siteUrl;
-  var auth = browserAuth({membersUrl: siteUrl + '/members'});
+  var $__1 = $__0,
+      membersUrl = $__1.membersUrl,
+      ssrUrl = $__1.ssrUrl;
+  var auth = browserAuth({membersUrl: membersUrl});
   var $__2 = window.location.hash.match(/^#([^?]+)\??(.*)$/) || [],
       hashMatch = $__2[0],
       hash = $__2[1],
@@ -797,7 +799,7 @@ function init($__0) {
   }
   auth.on('signedin', function() {
     auth.getSSRToken({fresh: true}).then(function(token) {
-      createSession(token);
+      createSession(token, ssrUrl);
     });
   });
   auth.on('signedout', function() {
@@ -805,13 +807,13 @@ function init($__0) {
   });
   function signout() {
     auth.signout().then((function() {
-      return destroySession();
+      return destroySession(ssrUrl);
     })).then(reload);
   }
   function signin() {
     auth.signin().then((function() {
       return auth.getSSRToken({fresh: true}).then(function(token) {
-        return createSession(token);
+        return createSession(token, ssrUrl);
       });
     })).then(reload);
   }
@@ -821,14 +823,14 @@ function init($__0) {
         coupon = ($__6 = $__5.coupon) === void 0 ? '' : $__6;
     auth.signup({coupon: coupon}).then((function() {
       return auth.getSSRToken({fresh: true}).then(function(token) {
-        return createSession(token);
+        return createSession(token, ssrUrl);
       });
     })).then(reload);
   }
   function upgrade() {
     auth.upgrade().then((function() {
       return auth.getSSRToken({fresh: true}).then(function(token) {
-        return createSession(token);
+        return createSession(token, ssrUrl);
       });
     })).then(reload);
   }
@@ -867,8 +869,8 @@ function reload(success) {
     window.location.reload();
   }
 }
-function createSession(token) {
-  return fetch('/members/ssr', {
+function createSession(token, ssrUrl) {
+  return fetch(ssrUrl, {
     method: 'post',
     credentials: 'include',
     body: token
@@ -876,8 +878,8 @@ function createSession(token) {
     return !!res.ok;
   });
 }
-function destroySession() {
-  return fetch('/members/ssr', {method: 'delete'}).then(function(res) {
+function destroySession(ssrUrl) {
+  return fetch(ssrUrl, {method: 'delete'}).then(function(res) {
     return !!res.ok;
   });
 }
