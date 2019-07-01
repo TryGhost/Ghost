7d95ec3279f1e4e256209ae31ac1bf4eecd1549d
Hannah Wolfe <erisds@gmail.com>
2019-07-01 10:14:03 +0100

    ✨ Updated theme activation API to experimental
    - allow both uploading and activating themes as experiemental API features
    - previously only uploading was allowed, I believe purely due to an oversight

--- core/server/web/api/v2/admin/middleware.js
+++ core/server/web/api/v2/admin/middleware.js
@@ -18,7 +18,7 @@ const notImplemented = function (req, res, next) {
         // @NOTE: experimental
         tags: ['GET', 'PUT', 'DELETE', 'POST'],
         users: ['GET'],
-        themes: ['POST'],
+        themes: ['POST', 'PUT'],
         subscribers: ['GET', 'PUT', 'DELETE', 'POST'],
         config: ['GET'],
         webhooks: ['POST', 'DELETE']
