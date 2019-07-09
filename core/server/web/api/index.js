4f9b18540afadf1de1c3aa98fdfe74b233a6f5bd
Fabien O'Carroll <fabien@allou.is>
2019-07-09 15:41:58 +0800

    Mounted the members api app to the api route
    closes #10886
    
    This successfully mounts the api and the static pages to the
    /api/v2/members/ URL.

--- core/server/web/api/index.js
+++ core/server/web/api/index.js
@@ -14,7 +14,7 @@ module.exports = function setupApiApp() {
     apiApp.use(urlUtils.getVersionPath({version: 'v0.1'}), require('./v0.1/app')());
     apiApp.use(urlUtils.getVersionPath({version: 'v2', type: 'content'}), require('./v2/content/app')());
     apiApp.use(urlUtils.getVersionPath({version: 'v2', type: 'admin'}), require('./v2/admin/app')());
-    apiApp.use(urlUtils.getVersionPath({version: 'v2', type: 'members'}), labs.members, membersService.api.apiRouter);
+    apiApp.use(urlUtils.getVersionPath({version: 'v2', type: 'members'}), require('./v2/members/app')());
 
     // Error handling for requests to non-existent API versions
     apiApp.use(errorHandler.resourceNotFound);
