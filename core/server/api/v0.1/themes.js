f3ec2fb2f758ac80d9de18271aefd0398563fd63
Naz Gargol <nazargargol@gmail.com>
2019-07-09 16:35:18 +0200

    Cleaned up theme service (#10884)
    refs #10790
    
    - Following TODO in theme index file was waiting for 2 years, and today is the day to cross it out:
    - "Reduced the amount of things we expose to the outside world"
    - "Made this a nice clean sensible API we can all understand!" - by @ErisDS
    - Cleaned exposed methods from themes module
    - Removed unused storage getter
    - Removed list method
    - Removed validate method
    - Renamed Storage to ThemeStorage
      - Named the file the same way the class defined inside of it is named
      - Naming was conflicting with coming rename of  `settings` -> `storage`
    - Renamed theme settings to storage

--- core/server/api/v0.1/themes.js
+++ core/server/api/v0.1/themes.js
@@ -26,7 +26,7 @@ themes = {
             // Main action
             .then(() => {
                 // Return JSON result
-                return themeService.settings.get();
+                return themeService.getJSON();
             });
     },
 
@@ -52,7 +52,7 @@ themes = {
             })
             .then((checkedTheme) => {
                 // Return JSON result
-                return themeService.settings.get(themeName, checkedTheme);
+                return themeService.getJSON(themeName, checkedTheme);
             });
     },
 
@@ -72,7 +72,7 @@ themes = {
             .handlePermissions('themes', 'add')(options)
             // Validation
             .then(() => {
-                return themeService.settings.setFromZip(zip);
+                return themeService.storage.setFromZip(zip);
             })
             .then((theme) => {
                 common.events.emit('theme.uploaded');
@@ -87,7 +87,7 @@ themes = {
         // Permissions
             .handlePermissions('themes', 'read')(options)
             .then(() => {
-                return themeService.settings.getZip(themeName);
+                return themeService.storage.getZip(themeName);
             });
     },
 
@@ -103,7 +103,7 @@ themes = {
             .handlePermissions('themes', 'destroy')(options)
             // Validation
             .then(() => {
-                return themeService.settings.destroy(themeName);
+                return themeService.storage.destroy(themeName);
             });
     }
 };
