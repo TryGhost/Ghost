f3e33d0ddf67107cd280781c63bafe1dc3ec6412
Nazar Gargol <nazargargol@gmail.com>
2019-07-17 11:59:46 +0200

    Migrated invitations accept module to async

--- core/server/services/invitations/accept.js
+++ core/server/services/invitations/accept.js
@@ -2,35 +2,29 @@ const common = require('../../lib/common');
 const models = require('../../models');
 const security = require('../../lib/security');
 
-function accept(invitation) {
+async function accept(invitation) {
     const data = invitation.invitation[0];
     const inviteToken = security.url.decodeBase64(data.token);
     const options = {context: {internal: true}};
 
-    let invite;
+    let invite = await models.Invite.findOne({token: inviteToken, status: 'sent'}, options);
 
-    return models.Invite.findOne({token: inviteToken, status: 'sent'}, options)
-        .then((_invite) => {
-            invite = _invite;
+    if (!invite) {
+        throw new common.errors.NotFoundError({message: common.i18n.t('errors.api.invites.inviteNotFound')});
+    }
 
-            if (!invite) {
-                throw new common.errors.NotFoundError({message: common.i18n.t('errors.api.invites.inviteNotFound')});
-            }
+    if (invite.get('expires') < Date.now()) {
+        throw new common.errors.NotFoundError({message: common.i18n.t('errors.api.invites.inviteExpired')});
+    }
 
-            if (invite.get('expires') < Date.now()) {
-                throw new common.errors.NotFoundError({message: common.i18n.t('errors.api.invites.inviteExpired')});
-            }
+    await models.User.add({
+        email: data.email,
+        name: data.name,
+        password: data.password,
+        roles: [invite.toJSON().role_id]
+    }, options);
 
-            return models.User.add({
-                email: data.email,
-                name: data.name,
-                password: data.password,
-                roles: [invite.toJSON().role_id]
-            }, options);
-        })
-        .then(() => {
-            return invite.destroy(options);
-        });
+    return invite.destroy(options);
 }
 
 module.exports = accept;
