ed4994ae81029a552292c571655e176cd93b45de
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-07-22 11:05:52 +0100

    Remove unused files
    no issue
    
    - `app/helpers/gh-format-html` is duplicated in `lib/koenig-editor/addon/helpers/sanitize-html` and is not used anywhere else
    - `app/helpers/gh-path` is not used anywhere - the API should be returning absolute URLs everywhere so path generation is no longer as necessary within templates
    - `app/helpers/is-equal` replaced with `{{eq}}` from `ember-truth-helpers`
    - `app/helpers/is-not` replaced with `{{not}}` from `ember-truth-helpers`
    - `app/utils/isFinite` is not used anywhere
    - `app/utils/titleize` is not used anywhere

--- ghost/admin/app/helpers/is-not.js
+++
@@ -1,9 +0,0 @@
-import {helper} from '@ember/component/helper';
-
-export function isNot(params) {
-    return !params;
-}
-
-export default helper(function (params) {
-    return isNot(params);
-});
