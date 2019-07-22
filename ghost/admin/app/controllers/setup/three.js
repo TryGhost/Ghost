4d96f231ca94c6c5bb6e90ca8a9368a96c6463bc
Aileen Nowak <AileenCGN@gmail.com>
2019-07-22 16:53:24 +0800

    Updated links to docs (#1276)
    no issue

--- ghost/admin/app/controllers/setup/three.js
+++ ghost/admin/app/controllers/setup/three.js
@@ -230,7 +230,7 @@ export default Controller.extend({
             invitationsString = erroredEmails.length > 1 ? ' invitations: ' : ' invitation: ';
             message = `Failed to send ${erroredEmails.length} ${invitationsString}`;
             message += Ember.Handlebars.Utils.escapeExpression(erroredEmails.join(', '));
-            message += '. Please check your email configuration, see <a href=\'https://docs.ghost.org/mail/\' target=\'_blank\'>https://docs.ghost.org/mail/</a> for instructions';
+            message += '. Please check your email configuration, see <a href=\'https://ghost.org/docs/concepts/config/#mail\' target=\'_blank\'>https://ghost.org/docs/concepts/config/#mail</a> for instructions';
 
             message = htmlSafe(message);
             notifications.showAlert(message, {type: 'error', delayed: successCount > 0, key: 'signup.send-invitations.failed'});
--- ghost/admin/app/services/tour.js
+++ ghost/admin/app/services/tour.js
@@ -60,7 +60,7 @@ export default Service.extend(Evented, {
         }, {
             id: 'upload-a-theme',
             title: 'Customising your publication',
-            message: 'Using custom themes you can completely control the look and feel of your site to suit your brand. Here\'s a full guide to help: <strong><a href="https://docs.ghost.org/api/handlebars-themes/" target="_blank">https://docs.ghost.org/api/handlebars-themes/</a></strong>'
+            message: 'Using custom themes you can completely control the look and feel of your site to suit your brand. Here\'s a full guide to help: <strong><a href="https://ghost.org/docs/api/v2/handlebars-themes/" target="_blank">https://ghost.org/docs/api/v2/handlebars-themes/</a></strong>'
         }];
     },
 
