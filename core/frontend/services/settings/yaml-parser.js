101eab27f065e74be44a16153b72291553a55b0c
Hannah Wolfe <erisds@gmail.com>
2019-07-24 17:41:55 +0100

    Updated yaml parser error to be InvalidUsageError
    - a yaml parser error can only be fixed by the user uploading a file, therefore it should be a 4xx, not a 5xx error
    - an amp parser error indicates the amperize module is unable to handle a genuine case, and needs to be fixed at the code level

--- core/frontend/services/settings/yaml-parser.js
+++ core/frontend/services/settings/yaml-parser.js
@@ -18,8 +18,10 @@ module.exports = function parseYaml(file, fileName) {
     } catch (error) {
         // CASE: parsing failed, `js-yaml` tells us exactly what and where in the
         // `reason` property as well as in the message.
-        throw new common.errors.GhostError({
+        // As the file uploaded is invalid, the person uploading must fix this - it's a 4xx error
+        throw new common.errors.IncorrectUsageError({
             message: common.i18n.t('errors.services.settings.yaml.error', {file: fileName, context: error.reason}),
+            code: 'YAML_PARSER_ERROR',
             context: error.message,
             err: error,
             help: common.i18n.t('errors.services.settings.yaml.help', {file: fileName})
