07e09e3845d5cedb79a1b8aeb0a3ffdb6ef0a14e
Aileen Nowak <AileenCGN@gmail.com>
2019-07-25 15:37:33 +0800

    Updated fixtures hash for tests

--- core/test/unit/data/schema/integrity_spec.js
+++ core/test/unit/data/schema/integrity_spec.js
@@ -20,7 +20,7 @@ var should = require('should'),
 describe('DB version integrity', function () {
     // Only these variables should need updating
     const currentSchemaHash = 'fda0398e93a74b2dc435cb4c026679ba';
-    const currentFixturesHash = '40e10c96ee9a08164a637dc6b37d7b6b';
+    const currentFixturesHash = '28e4057d63ce22613a2d03f520872548';
 
     // If this test is failing, then it is likely a change has been made that requires a DB version bump,
     // and the values above will need updating as confirmation
