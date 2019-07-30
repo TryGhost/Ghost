8503bdceb888f1c86a748adc4a4e8959e26be380
Nazar Gargol <nazargargol@gmail.com>
2019-07-30 16:13:53 +0200

    Refactored sendNotification method to just use email address as parameter

--- core/server/services/auth/setup.js
+++ core/server/services/auth/setup.js
@@ -82,16 +82,16 @@ async function doSettings(data, settingsAPI) {
     return user;
 }
 
-function sendNotification(setupUser, mailAPI) {
-    const data = {
-        ownerEmail: setupUser.email
-    };
-
+function sendNotification(email, mailAPI) {
     if (config.get('sendWelcomeEmail')) {
+        const data = {
+            ownerEmail: email
+        };
+
         return mail.utils.generateContent({data: data, template: 'welcome'})
             .then((content) => {
                 const message = {
-                        to: setupUser.email,
+                        to: email,
                         subject: common.i18n.t('common.api.authentication.mail.yourNewGhostBlog'),
                         html: content.html,
                         text: content.text
@@ -108,11 +108,8 @@ function sendNotification(setupUser, mailAPI) {
                         err.context = common.i18n.t('errors.api.authentication.unableToSendWelcomeEmail');
                         common.logging.error(err);
                     });
-            })
-            .return(setupUser);
+            });
     }
-
-    return setupUser;
 }
 
 module.exports = {
