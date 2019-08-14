6ccfbdadcc20086cfd2af488a91c0fca9102b97e
Fabien O'Carroll <fabien@allou.is>
2019-08-14 13:27:08 +0800

    Updated canary actions endpoint
    closes #11015
    
    This simplifies the actions endpoint and allows for filtering and
    querying of all types of actions across separate resources and actors

--- core/server/api/canary/actions.js
+++ core/server/api/canary/actions.js
@@ -7,31 +7,12 @@ module.exports = {
         options: [
             'page',
             'limit',
-            'fields'
+            'fields',
+            'include',
+            'filter'
         ],
-        data: [
-            'id',
-            'type'
-        ],
-        validation: {
-            id: {
-                required: true
-            },
-            type: {
-                required: true,
-                values: ['resource', 'actor']
-            }
-        },
         permissions: true,
         query(frame) {
-            if (frame.data.type === 'resource') {
-                frame.options.withRelated = ['actor'];
-                frame.options.filter = `resource_id:${frame.data.id}`;
-            } else {
-                frame.options.withRelated = ['resource'];
-                frame.options.filter = `actor_id:${frame.data.id}`;
-            }
-
             return models.Action.findPage(frame.options);
         }
     }
