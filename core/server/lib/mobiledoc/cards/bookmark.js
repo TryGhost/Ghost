5f2d2d9685bedb2b93293db371d91979342cf6d5
Rish <zrishabhgarg@gmail.com>
2019-08-25 16:47:29 +0530

    Added first version for bookmark theme render

--- core/server/lib/mobiledoc/cards/bookmark.js
+++ core/server/lib/mobiledoc/cards/bookmark.js
@@ -1,20 +1,81 @@
+/**
+<figure class="kg-card kg-bookmark-card">
+  <div class="kg-bookmark-container">
+    <a href="[URL]">
+      <div class="kg-bookmark-content">
+        <div class="kg-bookmark-title">[TITLE]</div>
+        <div class="kg-bookmark-description">[DESCRIPTION]</div>
+        <div class="kg-bookmark-metadata">
+          <img src="[LOGO]" class="kg-bookmark-logo">
+          <span class="kg-bookmark-author">[AUTHOR]</span>
+          <span class="kg-bookmark-url">[URL/DOMAIN]</span>
+        </div>
+      </div>
+      <div class="kg-bookmark-thumbnail">
+        [THUMBNAIL]
+      </div>
+    </a>
+</figure> 
+ */
+
 const createCard = require('../create-card');
+
+function createElement(dom, elem, classNames = '', attributes = [], innerHtml) {
+    let element = dom.createElement(elem);
+    if (classNames) {
+        element.setAttribute('class', classNames)
+    }
+    attributes.forEach((attr) => {
+        element.setAttribute(attr.key, attr.value);
+    })
+    if (innerHtml) {
+        element.innerHTML = innerHtml;
+    }
+    return element;
+}
 
 module.exports = createCard({
     name: 'bookmark',
     type: 'dom',
     render(opts) {
-        if (!opts.payload.html) {
+        if (!opts.payload.metadata) {
             return '';
         }
 
         let {payload, env: {dom}} = opts;
 
-        let figure = dom.createElement('figure');
-        figure.setAttribute('class', 'kg-card kg-embed-card');
-
-        let html = dom.createRawHTMLSection(payload.html);
-        figure.appendChild(html);
+        let figure = createElement(dom, 'figure', 'kg-card kg-bookmark-card');
+        let containerDiv = createElement(dom, 'div', 'kg-bookmark-container');
+        let linkTag = createElement(dom, 'a', '', [{
+            key: 'href', 
+            value: payload.metadata.url
+        }]);
+        let contentDiv = createElement(dom, 'div', 'kg-bookmark-content');
+        let titleDiv = createElement(dom, 'div', 'kg-bookmark-title', [] , payload.metadata.title);
+        let descriptionDiv = createElement(dom, 'div', 'kg-bookmark-description', [] , payload.metadata.description);
+        let metadataDiv = createElement(dom, 'div', 'kg-bookmark-metadata');
+        let imgLogo = createElement(dom, 'img', 'kg-bookmark-logo', [{
+            key: 'src',
+            value: payload.metadata.logo
+        }]);
+        let authorSpan = createElement(dom, 'span', 'kg-bookmark-author', [] , payload.metadata.author);
+        let publisherSpan = createElement(dom, 'span', 'kg-bookmark-url', [] , payload.metadata.publisher);
+        let thumbnailDiv = createElement(dom, 'div', 'kg-bookmark-thumbnail');
+        let thumbnailImg = createElement(dom, 'img', 'kg-bookmark-logo', [{
+            key: 'src',
+            value: payload.metadata.logo
+        }]);
+        thumbnailDiv.appendChild(thumbnailImg);
+        metadataDiv.appendChild(imgLogo);
+        metadataDiv.appendChild(authorSpan);
+        metadataDiv.appendChild(publisherSpan);
+        contentDiv.appendChild(titleDiv);
+        contentDiv.appendChild(descriptionDiv);
+        contentDiv.appendChild(metadataDiv);
+        linkTag.appendChild(contentDiv);
+        linkTag.appendChild(thumbnailDiv);
+        containerDiv.appendChild(linkTag);
+        figure.appendChild(containerDiv);
 
         if (payload.caption) {
             let figcaption = dom.createElement('figcaption');
