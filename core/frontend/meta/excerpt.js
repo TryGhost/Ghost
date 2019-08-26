16c3785b52cf7fc50ead4ba01f63a4d40f28d80a
Nazar Gargol <nazargargol@gmail.com>
2019-08-26 17:01:20 +0200

    🐛 Fixed lack of space in excerpt generated from paragraphs
    closes #10531
    
    - Adds space when encountering closing </p> and <br> tags
    - The mobiledoc-to-html conversion produces these tags in this exact
    syntax, so there is no need to account for more cases like additional spaces or alternative syntax like  <br />
    - Added test cases that cover spacing use-casei
    - Changed some existing tests to contain more close-to-real-world markup
    - The downside of this approach is generating multiple spaces in case there are empty paragraphs in the markup. The same concern is true for current "new line" substitution:
    > excerpt.replace(/(\r\n|\n|\r)+/gm, ' ')
    but it never has been a concern as in real world when multiple spaces are used inside of the `<p>` tag they are rendered as single space.

--- core/frontend/meta/excerpt.js
+++ core/frontend/meta/excerpt.js
@@ -5,6 +5,10 @@ function getExcerpt(html, truncateOptions) {
     // Strip inline and bottom footnotes
     var excerpt = html.replace(/<a href="#fn.*?rel="footnote">.*?<\/a>/gi, '');
     excerpt = excerpt.replace(/<div class="footnotes"><ol>.*?<\/ol><\/div>/, '');
+
+    // Make sure to have space between paragraphs and new lines
+    excerpt = excerpt.replace(/(<\/p>|<br>)/gi, ' ');
+
     // Strip other html
     excerpt = excerpt.replace(/<\/?[^>]+>/gi, '');
     excerpt = excerpt.replace(/(\r\n|\n|\r)+/gm, ' ');
