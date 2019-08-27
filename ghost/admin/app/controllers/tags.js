99a4c1c4c202eceef1cd4be1ba047e51efae739c
Rishabh Garg <rish@ghost.org>
2019-08-27 19:21:31 +0530

    🎨 Updated tags screen design and usability (#1283)
    no issue
    
    Updates design and usability for tags list and details screen

--- ghost/admin/app/controllers/tags.js
+++ ghost/admin/app/controllers/tags.js
@@ -1,20 +1,20 @@
 import Controller, {inject as controller} from '@ember/controller';
-import {alias, equal, sort} from '@ember/object/computed';
+import {alias, sort} from '@ember/object/computed';
 import {computed} from '@ember/object';
-import {run} from '@ember/runloop';
 
 export default Controller.extend({
 
     tagController: controller('tags.tag'),
 
+    queryParams: ['type'],
+    type: 'public',
     tags: alias('model'),
     selectedTag: alias('tagController.tag'),
 
-    tagListFocused: equal('keyboardFocus', 'tagList'),
-    tagContentFocused: equal('keyboardFocus', 'tagContent'),
-
-    filteredTags: computed('tags.@each.isNew', function () {
-        return this.tags.filterBy('isNew', false);
+    filteredTags: computed('tags.@each.isNew', 'type', function () {
+        return this.tags.filter((tag) => {
+            return (!tag.isNew && (!this.type || tag.visibility === this.type));
+        });
     }),
 
     // tags are sorted by name
@@ -24,37 +24,8 @@ export default Controller.extend({
     }),
 
     actions: {
-        leftMobile() {
-            let firstTag = this.get('tags.firstObject');
-            // redirect to first tag if possible so that you're not left with
-            // tag settings blank slate when switching from portrait to landscape
-            if (firstTag && !this.get('tagController.tag')) {
-                this.transitionToRoute('tags.tag', firstTag);
-            }
+        changeType(type) {
+            this.set('type', type);
         }
-    },
-
-    scrollTagIntoView(tag) {
-        run.scheduleOnce('afterRender', this, function () {
-            let id = `#gh-tag-${tag.get('id')}`;
-            let element = document.querySelector(id);
-
-            if (element) {
-                let scroll = document.querySelector('.tag-list');
-                let {scrollTop} = scroll;
-                let scrollHeight = scroll.offsetHeight;
-                let element = document.querySelector(id);
-                let elementTop = element.offsetTop;
-                let elementHeight = element.offsetHeight;
-
-                if (elementTop < scrollTop) {
-                    element.scrollIntoView(true);
-                }
-
-                if (elementTop + elementHeight > scrollTop + scrollHeight) {
-                    element.scrollIntoView(false);
-                }
-            }
-        });
     }
 });
