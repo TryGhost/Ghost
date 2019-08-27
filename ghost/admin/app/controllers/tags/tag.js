50637143823fe2c88b8ff1c8cdf8b314a729ff0d
Rishabh Garg <rish@ghost.org>
2019-08-27 19:21:31 +0530

    🎨 Updated tags screen design and usability (#1283)
    no issue
    
    Updates design and usability for tags list and details screen

--- ghost/admin/app/controllers/tags/tag.js
+++ ghost/admin/app/controllers/tags/tag.js
@@ -2,6 +2,8 @@ import Controller, {inject as controller} from '@ember/controller';
 import windowProxy from 'ghost-admin/utils/window-proxy';
 import {alias} from '@ember/object/computed';
 import {inject as service} from '@ember/service';
+import {slugify} from '@tryghost/string';
+import {task} from 'ember-concurrency';
 
 export default Controller.extend({
     tagsController: controller('tags'),
@@ -24,6 +26,47 @@ export default Controller.extend({
 
         deleteTag() {
             return this._deleteTag();
+        },
+        save() {
+            return this.save.perform();
+        },
+
+        toggleUnsavedChangesModal(transition) {
+            let leaveTransition = this.leaveScreenTransition;
+
+            if (!transition && this.showUnsavedChangesModal) {
+                this.set('leaveScreenTransition', null);
+                this.set('showUnsavedChangesModal', false);
+                return;
+            }
+
+            if (!leaveTransition || transition.targetName === leaveTransition.targetName) {
+                this.set('leaveScreenTransition', transition);
+
+                // if a save is running, wait for it to finish then transition
+                if (this.save.isRunning) {
+                    return this.save.last.then(() => {
+                        transition.retry();
+                    });
+                }
+
+                // we genuinely have unsaved data, show the modal
+                this.set('showUnsavedChangesModal', true);
+            }
+        },
+
+        leaveScreen() {
+            let transition = this.leaveScreenTransition;
+
+            if (!transition) {
+                this.notifications.showAlert('Sorry, there was an error in the application. Please let the Ghost team know what happened.', {type: 'error'});
+                return;
+            }
+
+            // roll back changes on model props
+            this.tag.rollbackAttributes();
+
+            return transition.retry();
         }
     },
 
@@ -42,29 +85,43 @@ export default Controller.extend({
         }
 
         tag.set(propKey, newValue);
+
+        // Generate slug based on name for new tag when empty
+        if (propKey === 'name' && !tag.get('slug') && isNewTag) {
+            let slugValue = slugify(newValue);
+            tag.set('slug', slugValue);
+        }
         // TODO: This is required until .validate/.save mark fields as validated
         tag.get('hasValidated').addObject(propKey);
+    },
 
-        tag.save().then((savedTag) => {
+    save: task(function* () {
+        let tag = this.tag;
+        let isNewTag = tag.get('isNew');
+        try {
+            let savedTag = yield tag.save();
             // replace 'new' route with 'tag' route
             this.replaceRoute('tags.tag', savedTag);
 
             // update the URL if the slug changed
-            if (propKey === 'slug' && !isNewTag) {
+            if (!isNewTag) {
                 let currentPath = window.location.hash;
 
                 let newPath = currentPath.split('/');
-                newPath[newPath.length - 1] = savedTag.get('slug');
-                newPath = newPath.join('/');
+                if (newPath[newPath.length - 1] !== savedTag.get('slug')) {
+                    newPath[newPath.length - 1] = savedTag.get('slug');
+                    newPath = newPath.join('/');
 
-                windowProxy.replaceState({path: newPath}, '', newPath);
+                    windowProxy.replaceState({path: newPath}, '', newPath);
+                }
             }
-        }).catch((error) => {
+            return savedTag;
+        } catch (error) {
             if (error) {
                 this.notifications.showAPIError(error, {key: 'tag.save'});
             }
-        });
-    },
+        }
+    }),
 
     _deleteTag() {
         let tag = this.tag;
--- ghost/admin/app/routes/tags/tag.js
+++ ghost/admin/app/routes/tags/tag.js
@@ -1,7 +1,23 @@
 /* eslint-disable camelcase */
 import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
+import CurrentUserSettings from 'ghost-admin/mixins/current-user-settings';
+import {inject as service} from '@ember/service';
 
-export default AuthenticatedRoute.extend({
+export default AuthenticatedRoute.extend(CurrentUserSettings, {
+    router: service(),
+
+    init() {
+        this._super(...arguments);
+        this.router.on('routeWillChange', (transition) => {
+            this.showUnsavedChangesModal(transition);
+        });
+    },
+
+    beforeModel() {
+        this._super(...arguments);
+        return this.get('session.user')
+            .then(this.transitionAuthor());
+    },
 
     model(params) {
         return this.store.queryRecord('tag', {slug: params.tag_slug});
@@ -11,14 +27,34 @@ export default AuthenticatedRoute.extend({
         return {tag_slug: model.get('slug')};
     },
 
-    setupController(controller, model) {
+    setupController() {
         this._super(...arguments);
-        this.controllerFor('tags').scrollTagIntoView(model);
     },
 
     // reset the model so that mobile screens react to an empty selectedTag
     deactivate() {
         this._super(...arguments);
+        let {controller} = this;
+        controller.model.rollbackAttributes();
         this.set('controller.model', null);
+    },
+
+    actions: {
+        save() {
+            this.controller.send('save');
+        }
+    },
+
+    showUnsavedChangesModal(transition) {
+        if (transition.from && transition.from.name.match(/^tags\.tag/) && transition.targetName) {
+            let {controller} = this;
+
+            if (!controller.tag.isDeleted && controller.tag.hasDirtyAttributes) {
+                transition.abort();
+                controller.send('toggleUnsavedChangesModal', transition);
+                return;
+            }
+        }
     }
+
 });
