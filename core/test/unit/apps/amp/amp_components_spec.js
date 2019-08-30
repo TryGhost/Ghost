bf4e7f7321a3094831fe1d36ceac9f417872d307
Edward Kerstein <ekerstein@gmail.com>
2019-08-30 17:06:07 -0400

    amp-youtube script unit test

--- core/test/unit/apps/amp/amp_components_spec.js
+++ core/test/unit/apps/amp/amp_components_spec.js
@@ -31,7 +31,21 @@ describe('{{amp_components}} helper', function () {
         should.exist(rendered);
         rendered.should.match(/<script async custom-element="amp-iframe" src="https:\/\/cdn.ampproject.org\/v0\/amp-iframe-0.1.js"><\/script>/);
     });
+    
+    it('adds script tag for a youtube embed', function () {
+        var post = {
+                html: '<iframe src="https://www.youtube.com/embed/zqNTltOGh5c" frameborder="0"></iframe>'
+            },
+            rendered;
 
+        rendered = ampComponentsHelper.call(
+            {relativeUrl: '/post/amp/', safeVersion: '0.3', context: ['amp', 'post'], post: post},
+            {data: {root: {context: ['amp', 'post']}}});
+
+        should.exist(rendered);
+        rendered.should.match(/<script async custom-element="amp-youtube" src="https:\/\/cdn.ampproject.org\/v0\/amp-youtube-0.1.js"><\/script>/);
+    });
+    
     it('adds script tag for an audio tag', function () {
         var post = {
                 html: '<audio src="myaudiofile.mp3"/>'
