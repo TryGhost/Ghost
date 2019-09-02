ffe6dd5b83901dfca4a98496541d9dc3ce970a79
Kevin Ansfield <kevin@ghost.org>
2019-09-02 11:34:13 +0100

    🔥 Removed RSS pagination (#11069)
    no issue
    
    - removed support for `/rss/:page/` URLs
    - removed `/rss/1/` -> `/rss/` redirect

--- core/frontend/services/routing/middlewares/page-param.js
+++ core/frontend/services/routing/middlewares/page-param.js
@@ -11,19 +11,13 @@ const common = require('../../../../server/lib/common'),
  */
 module.exports = function handlePageParam(req, res, next, page) {
     // routeKeywords.page: 'page'
-    const pageRegex = new RegExp('/page/(.*)?/'),
-        rssRegex = new RegExp('/rss/(.*)?/');
+    const pageRegex = new RegExp('/page/(.*)?/');
 
     page = parseInt(page, 10);
 
     if (page === 1) {
         // CASE: page 1 is an alias for the collection index, do a permanent 301 redirect
-        // @TODO: this belongs into the rss router!
-        if (rssRegex.test(req.url)) {
-            return urlUtils.redirect301(res, req.originalUrl.replace(rssRegex, '/rss/'));
-        } else {
-            return urlUtils.redirect301(res, req.originalUrl.replace(pageRegex, '/'));
-        }
+        return urlUtils.redirect301(res, req.originalUrl.replace(pageRegex, '/'));
     } else if (page < 1 || isNaN(page)) {
         return next(new common.errors.NotFoundError({
             message: common.i18n.t('errors.errors.pageNotFound')
