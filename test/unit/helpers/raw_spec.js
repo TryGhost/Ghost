67b8fbf6cfbe1cc1e2fe097ca9ee34fca166f6bf
Joseph Coffland <joseph@cauldrondevelopment.com>
2019-09-02 04:18:27 -0700

    ✨ Added raw handlebars helper
    - Allows using the 4-bracket raw block syntax e.g: {{{{raw}}}}{{{{/raw}}}}
    - This allows you to include handlebars inside a template that is not compiled and executed
    - The common usecase is if you want to include client-side handlebars templates inside server-side ones

---
+++ test/unit/helpers/raw_spec.js
@@ -0,0 +1,40 @@
+const should = require('should');
+const helpers = require('../../../core/frontend/helpers');
+const handlebars = require('../../../core/frontend/services/themes/engine').handlebars;
+
+let defaultGlobals;
+
+function compile(templateString) {
+    const template = handlebars.compile(templateString);
+    template.with = (locals = {}, globals) => {
+        globals = globals || defaultGlobals;
+
+        return template(locals, globals);
+    };
+
+    return template;
+}
+
+describe('{{raw}} helper', function () {
+    before(function () {
+        handlebars.registerHelper('raw', helpers.raw);
+    });
+
+    it('can correctly compile space', function () {
+        compile('{{{{raw}}}} {{{{/raw}}}}')
+            .with({})
+            .should.eql(' ');
+    });
+
+    it('can correctly ignore handlebars', function () {
+        compile('{{{{raw}}}}{{test}}{{{{/raw}}}}')
+            .with({tag: {}})
+            .should.eql('{{test}}');
+    });
+
+    it('can correctly compile recursive', function () {
+        compile('{{{{raw}}}}{{{{raw}}}}{{{{/raw}}}}{{{{/raw}}}}')
+            .with({tag: {}})
+            .should.eql('{{{{raw}}}}{{{{/raw}}}}');
+    });
+});
