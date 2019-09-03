494513cc0ea90ce14031857eb01414365a906c89
Rish <zrishabhgarg@gmail.com>
2019-09-03 14:09:26 +0530

    🔥 Dropped unused `ghost_auth_*` user fields
    no issue
    
    - Drops `ghost_auth_access_token` and `ghost_auth_id` fields since not used anymore
    - Adds migration for dropping these columns from users table
    - Drops Auth strategy - `ghostStrategy` - since its not used anymore

--- core/test/regression/api/v3/content/utils.js
+++ core/test/regression/api/v3/content/utils.js
@@ -33,8 +33,6 @@ const expectedProperties = {
         .without(
             'password',
             'email',
-            'ghost_auth_access_token',
-            'ghost_auth_id',
             'created_at',
             'created_by',
             'updated_at',
