af6c897a1410689d7cd9e803c335e25bd60256d4
Fabien O'Carroll <fabien@allou.is>
2019-09-03 12:32:00 +0800

    Updated members-api to use magic-link
    no-issue
    
    This removes a *lot* of funtionality, stripping the members-api module
    to *only* handle the magic link signin flow.

--- ghost/members-api/index.js
+++ ghost/members-api/index.js
@@ -1,256 +1,101 @@
 const {Router} = require('express');
-const {EventEmitter} = require('events');
 const body = require('body-parser');
+const MagicLink = require('@tryghost/magic-link');
 
-const {getData, handleError} = require('./lib/util');
-
-const Cookies = require('./lib/cookies');
 const Tokens = require('./lib/tokens');
 const Users = require('./lib/users');
-const Subscriptions = require('./lib/subscriptions');
 const common = require('./lib/common');
 
 module.exports = function MembersApi({
-    authConfig: {
+    tokenConfig: {
         issuer,
         privateKey,
-        publicKey,
-        sessionSecret,
-        ssoOrigin,
-        accessControl
+        publicKey
     },
-    paymentConfig,
+    auth: {
+        getSigninURL
+    },
+    mail: {
+        transporter
+    },
     createMember,
-    validateMember,
-    updateMember,
     getMember,
     deleteMember,
-    listMembers,
-    sendEmail,
-    siteConfig
+    listMembers
 }) {
-    const {encodeToken, decodeToken, getPublicKeys} = Tokens({privateKey, publicKey, issuer});
-
-    let subscriptions = new Subscriptions(paymentConfig);
+    const {encodeIdentityToken} = Tokens({privateKey, publicKey, issuer});
 
     let users = Users({
-        subscriptions,
         createMember,
-        updateMember,
         getMember,
         deleteMember,
-        validateMember,
-        sendEmail,
-        encodeToken,
-        listMembers,
-        decodeToken
+        listMembers
     });
 
-    const apiRouter = Router();
+    const magicLinkService = new MagicLink({
+        transporter,
+        publicKey,
+        privateKey,
+        getSigninURL
+    });
 
-    apiRouter.use(body.json());
-
-    /* session */
-    const {getCookie, setCookie, removeCookie} = Cookies(sessionSecret);
-
-    function validateAccess({audience, origin}) {
-        const audienceLookup = accessControl[origin] || {
-            [origin]: accessControl['*']
-        };
-
-        const tokenSettings = audienceLookup[audience];
-
-        if (tokenSettings) {
-            return Promise.resolve(tokenSettings);
-        }
-
-        return Promise.reject();
+    async function sendEmailWithMagicLink(email){
+        return magicLinkService.sendMagicLink({email, user: {email}});
     }
-
-    /* token */
-    apiRouter.post('/token', getData('audience'), (req, res) => {
-        const {signedin} = getCookie(req);
-        if (!signedin) {
-            res.writeHead(401, {
-                'Set-Cookie': removeCookie()
-            });
-            return res.end();
+    async function getMemberDataFromMagicLinkToken(token){
+        const user = await magicLinkService.getUserFromToken(token);
+        const email = user && user.email;
+        if (!email) {
+            return null;
         }
-
-        const {audience, origin} = req.data;
-
-        validateAccess({audience, origin})
-            .then(({tokenLength}) => {
-                return users.get({id: signedin})
-                    .then(member => encodeToken({
-                        sub: member.id,
-                        plans: member.plans,
-                        exp: tokenLength,
-                        aud: audience
-                    }));
-            })
-            .then(token => res.end(token))
-            .catch(handleError(403, res));
-    });
-
-    apiRouter.get('/config', (req, res) => {
-        subscriptions.getAdapters()
-            .then((adapters) => {
-                return Promise.all(adapters.map((adapter) => {
-                    return subscriptions.getPublicConfig(adapter);
-                }));
-            })
-            .then(paymentConfig => res.json({
-                paymentConfig,
-                issuer,
-                siteConfig
-            }))
-            .catch(handleError(500, res));
-    });
-
-    /* security */
-    function ssoOriginCheck(req, res, next) {
-        if (!req.data.origin || req.data.origin !== ssoOrigin) {
-            res.writeHead(403);
-            return res.end();
+        const member = await getMemberIdentityData(email);
+        if (member) {
+            return member;
         }
-        next();
+        return users.create({email});
     }
-
-    /* subscriptions */
-    apiRouter.post('/subscription', getData('adapter', 'plan', 'stripeToken', {name: 'coupon', required: false}), ssoOriginCheck, (req, res) => {
-        const {signedin} = getCookie(req);
-        if (!signedin) {
-            res.writeHead(401, {
-                'Set-Cookie': removeCookie()
-            });
-            return res.end();
-        }
-
-        const {plan, adapter, stripeToken, coupon} = req.data;
-
-        subscriptions.getAdapters()
-            .then((adapters) => {
-                if (!adapters.includes(adapter)) {
-                    throw new Error('Invalid adapter');
-                }
-            })
-            .then(() => users.get({id: signedin}))
-            .then((member) => {
-                return subscriptions.createSubscription(member, {
-                    adapter,
-                    plan,
-                    stripeToken,
-                    coupon
-                });
-            })
-            .then(() => {
-                res.end();
-            })
-            .catch(handleError(500, res));
-    });
-
-    /* users, token, emails */
-    apiRouter.post('/request-password-reset', getData('email'), ssoOriginCheck, (req, res) => {
-        const {email} = req.data;
-
-        users.requestPasswordReset({email}).then(() => {
-            res.writeHead(200);
-            res.end();
-        }).catch(handleError(500, res));
-    });
-
-    /* users, token */
-    apiRouter.post('/reset-password', getData('token', 'password'), ssoOriginCheck, (req, res) => {
-        const {token, password} = req.data;
-
-        users.resetPassword({token, password}).then((member) => {
-            res.writeHead(200, {
-                'Set-Cookie': setCookie(member)
-            });
-            res.end();
-        }).catch(handleError(401, res));
-    });
-
-    /* users, email */
-    apiRouter.post('/signup', getData('name', 'email', 'password'), ssoOriginCheck, (req, res) => {
-        const {name, email, password} = req.data;
-
-        // @TODO this should attempt to reset password before creating member
-        users.create({name, email, password}).then((member) => {
-            res.writeHead(200, {
-                'Set-Cookie': setCookie(member)
-            });
-            res.end();
-        }).catch(handleError(400, res));
-    });
-
-    /* users, session */
-    apiRouter.post('/signin', getData('email', 'password'), ssoOriginCheck, (req, res) => {
-        const {email, password} = req.data;
-
-        users.validate({email, password}).then((member) => {
-            res.writeHead(200, {
-                'Set-Cookie': setCookie(member)
-            });
-            res.end();
-        }).catch(handleError(401, res));
-    });
-
-    /* session */
-    apiRouter.post('/signout', getData(), (req, res) => {
-        res.writeHead(200, {
-            'Set-Cookie': removeCookie()
-        });
-        res.end();
-    });
-
-    /* http */
-    const staticRouter = Router();
-    staticRouter.get('/gateway', (req, res) => {
-        res.status(200).send(`
-            <script>
-                window.membersApiUrl = "${issuer}";
-            </script>
-            <script src="bundle.js"></script>
-        `);
-    });
-    staticRouter.get('/bundle.js', (req, res) => {
-        res.status(200).sendFile(require('path').join(__dirname, './gateway/bundle.js'));
-    });
+    async function getMemberIdentityData(email){
+        return users.get(email);
+    }
+    async function getMemberIdentityToken(email){
+        const member = await getMemberIdentityData(email);
+        return encodeIdentityToken({sub: member.email});
+    }
 
     const apiInstance = new Router();
-    const eventBus = new EventEmitter();
+    apiInstance.use(body.json());
 
-    subscriptions._ready.then(() => {
-        eventBus.emit('ready');
-    }, (err) => {
-        eventBus.emit('error', err);
+    apiInstance.post('/send-magic-link', async function (req, res) {
+        const email = req.body.email;
+        if (!email) {
+            res.writeHead(400);
+            return res.end('Bad Request.');
+        }
+        try {
+            await sendEmailWithMagicLink(email);
+            res.writeHead(201);
+            return res.end('Created.');
+        } catch (err) {
+            res.writeHead(500);
+            return res.end('Internal Server Error.');
+        }
     });
 
-    apiInstance.bus = eventBus;
+    apiInstance.getMemberDataFromMagicLinkToken = getMemberDataFromMagicLinkToken;
+    apiInstance.getMemberIdentityData = getMemberIdentityData;
+    apiInstance.getMemberIdentityToken = getMemberIdentityToken;
 
-    apiInstance.use(apiRouter);
-    apiInstance.use('/static', staticRouter);
+    apiInstance.setLogger = common.logging.setLogger;
 
-    apiInstance.apiRouter = apiRouter;
-    apiInstance.staticRouter = staticRouter;
-
-    apiInstance.members = users;
-    apiInstance.getPublicKeys = getPublicKeys;
     apiInstance.getPublicConfig = function () {
         return Promise.resolve({
             publicKey,
             issuer
         });
     };
-    apiInstance.getMember = function (id, token) {
-        return decodeToken(token).then(() => {
-            return users.get({id});
-        });
-    };
 
-    apiInstance.setLogger = common.logging.setLogger;
+    apiInstance.bus = new (require('events').EventEmitter)();
+    process.nextTick(() => apiInstance.bus.emit('ready'));
 
     return apiInstance;
 };
