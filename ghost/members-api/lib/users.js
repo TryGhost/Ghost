d1bddc64e2d5f78f653bd537245ff80a73831286
Fabien O'Carroll <fabien@allou.is>
2019-09-06 13:16:35 +0800

    Added subscription data when fetching member

--- ghost/members-api/lib/users.js
+++ ghost/members-api/lib/users.js
@@ -1,13 +1,40 @@
 module.exports = function ({
+    stripe,
     createMember,
     getMember,
     listMembers,
     deleteMember
 }) {
+    async function get(data, options) {
+        const member = await getMember(data, options);
+        if (!member) {
+            return member;
+        }
+        if (!stripe) {
+            return Object.assign(member, {
+                plans: []
+            });
+        }
+        try {
+            const subscription = await stripe.getSubscription(member);
+            if (subscription.status !== 'active') {
+                return Object.assign(member, {
+                    plans: []
+                });
+            }
+
+            return Object.assign(member, {
+                plans: [subscription.plan]
+            });
+        } catch (err) {
+            return null;
+        }
+    }
+
     return {
         create: createMember,
         list: listMembers,
-        get: getMember,
+        get,
         destroy: deleteMember
     };
 };
