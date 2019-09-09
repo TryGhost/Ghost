1404745b50617372b28bae2e8411d7ab53988ff2
Hannah Wolfe <erisds@gmail.com>
2019-09-09 12:52:55 +0100

    Added guard to asset helper for missing paths
    refs #10496
    
    - currently {{asset this/is/not/a.string}} would throw a 500 error
    - this commit changes that to make it throw a sensible 400 + incorrect usage error

--- core/server/helpers/asset.js
+++ core/server/helpers/asset.js
@@ -4,11 +4,19 @@
 // Returns the path to the specified asset. The ghost flag outputs the asset path for the Ghost admin
 const proxy = require('./proxy'),
     get = require('lodash/get'),
+    i18n = proxy.i18n,
+    errors = proxy.errors,
     getAssetUrl = proxy.metaData.getAssetUrl,
     SafeString = proxy.SafeString;
 
 module.exports = function asset(path, options) {
     const hasMinFile = get(options, 'hash.hasMinFile');
+
+    if (!path) {
+        throw new errors.IncorrectUsageError({
+            message: i18n.t('warnings.helpers.asset.pathIsRequired')
+        });
+    }
 
     return new SafeString(
         getAssetUrl(path, hasMinFile)
