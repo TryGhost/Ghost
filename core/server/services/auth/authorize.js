d4f4f821cfc300cb3da495dd373d6888f9e4c613
Naz Gargol <nazargargol@gmail.com>
2019-09-11 19:40:48 +0200

    🔥 Removed v0.1 auth services (#11104)

--- core/server/services/auth/authorize.js
+++ core/server/services/auth/authorize.js
@@ -2,55 +2,6 @@ const labs = require('../labs');
 const common = require('../../lib/common');
 
 const authorize = {
-    // Workaround for missing permissions
-    // TODO: rework when https://github.com/TryGhost/Ghost/issues/3911 is  done
-    requiresAuthorizedUser: function requiresAuthorizedUser(req, res, next) {
-        if (req.user && req.user.id) {
-            return next();
-        } else {
-            return next(new common.errors.NoPermissionError({
-                message: common.i18n.t('errors.middleware.auth.pleaseSignIn')
-            }));
-        }
-    },
-
-    // ### Require user depending on public API being activated.
-    requiresAuthorizedUserPublicAPI: function requiresAuthorizedUserPublicAPI(req, res, next) {
-        if (labs.isSet('publicAPI') === true) {
-            return next();
-        } else {
-            if (req.user && req.user.id) {
-                return next();
-            } else {
-                // CASE: has no user access and public api is disabled
-                if (labs.isSet('publicAPI') !== true) {
-                    return next(new common.errors.NoPermissionError({
-                        message: common.i18n.t('errors.middleware.auth.publicAPIDisabled.error'),
-                        context: common.i18n.t('errors.middleware.auth.publicAPIDisabled.context'),
-                        help: common.i18n.t('errors.middleware.auth.forInformationRead', {url: 'https://ghost.org/docs/api/content/'})
-                    }));
-                }
-
-                return next(new common.errors.NoPermissionError({
-                    message: common.i18n.t('errors.middleware.auth.pleaseSignIn')
-                }));
-            }
-        }
-    },
-
-    // Requires the authenticated client to match specific client
-    requiresAuthorizedClient: function requiresAuthorizedClient(client) {
-        return function doAuthorizedClient(req, res, next) {
-            if (client && (!req.client || !req.client.name || req.client.name !== client)) {
-                return next(new common.errors.NoPermissionError({
-                    message: common.i18n.t('errors.permissions.noPermissionToAction')
-                }));
-            }
-
-            return next();
-        };
-    },
-
     authorizeContentApi(req, res, next) {
         const hasApiKey = req.api_key && req.api_key.id;
         const hasMember = req.member;
