858ff1e83a650b528a9147adcb2967a0755864e5
Rish <zrishabhgarg@gmail.com>
2019-09-14 19:58:47 +0530

    Updated lodash wrapper for mapper

--- core/server/api/canary/utils/serializers/output/utils/mapper.js
+++ core/server/api/canary/utils/serializers/output/utils/mapper.js
@@ -58,8 +58,10 @@ const mapPost = (model, frame) => {
         });
     }
 
+
     // Transforms post/page metadata to flat structure
-    _.keys(_.omit(postsMetaSchema, ['id', 'post_id'])).filter((k) => {
+    let metaAttrs = _.keys(_.omit(postsMetaSchema, ['id', 'post_id']));
+    _(metaAttrs).filter((k) => {
         return (!frame.options.columns || (frame.options.columns && frame.options.columns.includes(k)));
     }).each((attr) => {
         jsonModel[attr] = _.get(jsonModel.posts_meta, attr) || null;
