3b65081b5e5d90595b20e5e2520d35e21905b40d
Nazar Gargol <nazargargol@gmail.com>
2019-09-18 11:53:41 +0200

    🔥 Removed deprecated markdown wrapper

--- core/server/lib/mobiledoc/cards/markdown.js
+++ core/server/lib/mobiledoc/cards/markdown.js
@@ -6,19 +6,11 @@ module.exports = createCard({
     render: function (opts) {
         let converters = require('../converters');
         let payload = opts.payload;
-        let version = opts.options && opts.options.version || 2;
         // convert markdown to HTML ready for insertion into dom
         let html = converters.markdownConverter.render(payload.markdown || '');
 
         if (!html) {
             return '';
-        }
-
-        /**
-         * @deprecated Ghost 1.0's markdown-only renderer wrapped cards. Remove in Ghost 3.0
-         */
-        if (version === 1) {
-            html = `<div class="kg-card-markdown">${html}</div>`;
         }
 
         // use the SimpleDOM document to create a raw HTML section.
