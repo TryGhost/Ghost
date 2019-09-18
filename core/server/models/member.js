ff2054913c979f2a0ed70657577458ae258f347c
Fabien O'Carroll <fabien@allou.is>
2019-09-18 16:08:58 +0800

    Removed unused password logic from Member model
    no-issue

--- core/server/models/member.js
+++ core/server/models/member.js
@@ -1,33 +1,7 @@
 const ghostBookshelf = require('./base');
-const security = require('../lib/security');
 
 const Member = ghostBookshelf.Model.extend({
-    tableName: 'members',
-
-    onSaving() {
-        ghostBookshelf.Model.prototype.onSaving.apply(this, arguments);
-
-        if (this.hasChanged('password')) {
-            return security.password.hash(String(this.get('password')))
-                .then((hash) => {
-                    this.set('password', hash);
-                });
-        }
-    },
-
-    comparePassword(rawPassword) {
-        return security.password.compare(rawPassword, this.get('password'));
-    },
-
-    toJSON(unfilteredOptions) {
-        var options = Member.filterOptions(unfilteredOptions, 'toJSON'),
-            attrs = ghostBookshelf.Model.prototype.toJSON.call(this, options);
-
-        // remove password hash and tokens for security reasons
-        delete attrs.password;
-
-        return attrs;
-    }
+    tableName: 'members'
 });
 
 const Members = ghostBookshelf.Collection.extend({
