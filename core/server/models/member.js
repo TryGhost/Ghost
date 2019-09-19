c5c7bf55573df1080f8ff0966ea4197db04d2eff
Fabien O'Carroll <fabien@allou.is>
2019-09-19 11:50:55 +0800

    Updated Member model to handle stripe_info property
    no-issue
    
    This maps the stripe_info property to the MemberStripeInfo model, so
    that we can update the member model, and correctly add/edit rows in the
    members-stripe-info table.

--- core/server/models/member.js
+++ core/server/models/member.js
@@ -1,7 +1,24 @@
 const ghostBookshelf = require('./base');
 
 const Member = ghostBookshelf.Model.extend({
-    tableName: 'members'
+    tableName: 'members',
+
+    relationships: ['stripe_info'],
+    relationshipBelongsTo: {
+        stripe_info: 'members_stripe_info'
+    },
+
+    permittedAttributes(...args) {
+        return ghostBookshelf.Model.prototype.permittedAttributes.apply(this, args).concat(this.relationships);
+    },
+
+    stripe_info() {
+        return this.hasMany('MemberStripeInfo', 'member_id');
+    }
+}, {
+    permittedOptions(...args) {
+        return ghostBookshelf.Model.permittedOptions.apply(this, args).concat(['withRelated']);
+    }
 });
 
 const Members = ghostBookshelf.Collection.extend({
