066cfb3135351ba828fa47078a1172d7107cc548
Nazar Gargol <nazargargol@gmail.com>
2019-09-24 16:01:59 +0200

    Removed subscriber related permissions

--- core/test/unit/data/schema/fixtures/utils_spec.js
+++ core/test/unit/data/schema/fixtures/utils_spec.js
@@ -150,18 +150,18 @@ describe('Migration Fixture Utils', function () {
             fixtureUtils.addFixturesForRelation(fixtures.relations[0]).then(function (result) {
                 should.exist(result);
                 result.should.be.an.Object();
-                result.should.have.property('expected', 61);
-                result.should.have.property('done', 61);
+                result.should.have.property('expected', 56);
+                result.should.have.property('done', 56);
 
                 // Permissions & Roles
                 permsAllStub.calledOnce.should.be.true();
                 rolesAllStub.calledOnce.should.be.true();
-                dataMethodStub.filter.callCount.should.eql(61);
+                dataMethodStub.filter.callCount.should.eql(56);
                 dataMethodStub.find.callCount.should.eql(7);
-                baseUtilAttachStub.callCount.should.eql(61);
+                baseUtilAttachStub.callCount.should.eql(56);
 
-                fromItem.related.callCount.should.eql(61);
-                fromItem.findWhere.callCount.should.eql(61);
+                fromItem.related.callCount.should.eql(56);
+                fromItem.findWhere.callCount.should.eql(56);
                 toItem[0].get.callCount.should.eql(122);
 
                 done();
