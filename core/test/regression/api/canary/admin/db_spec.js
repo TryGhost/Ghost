bb8bc8830deb9805ac31cd1ce1628e2b0af355c5
Nazar Gargol <nazargargol@gmail.com>
2019-09-30 22:32:11 +0200

    Fixed db regression tests
    refs https://github.com/TryGhost/Ghost/commit/978201f653035bfa5402db3956e6d84b3aa78d3f
    
    - The change in the ref didn't take into account increasing counts in regression tests

--- core/test/regression/api/canary/admin/db_spec.js
+++ core/test/regression/api/canary/admin/db_spec.js
@@ -59,7 +59,7 @@ describe('DB API', function () {
                 const jsonResponse = res.body;
                 should.exist(jsonResponse.db);
                 jsonResponse.db.should.have.length(1);
-                Object.keys(jsonResponse.db[0].data).length.should.eql(27);
+                Object.keys(jsonResponse.db[0].data).length.should.eql(28);
             });
     });
 
