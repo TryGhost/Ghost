a33d5feb431500a5455dfcb302715270be87775c
Nazar Gargol <nazargargol@gmail.com>
2019-09-30 22:32:11 +0200

    Fixed db regression tests
    refs https://github.com/TryGhost/Ghost/commit/6859e9a9a1907a5bc4ca43d133c98ece834a9c0c
    
    - The change in the ref didn't take into account increasing counts in regression tests

--- core/test/regression/api/v0.1/db_spec.js
+++ core/test/regression/api/v0.1/db_spec.js
@@ -88,7 +88,7 @@ describe('DB API', function () {
                 var jsonResponse = res.body;
                 should.exist(jsonResponse.db);
                 jsonResponse.db.should.have.length(1);
-                Object.keys(jsonResponse.db[0].data).length.should.eql(25);
+                Object.keys(jsonResponse.db[0].data).length.should.eql(26);
                 done();
             });
     });
@@ -106,7 +106,7 @@ describe('DB API', function () {
                 const jsonResponse = res.body;
                 should.exist(jsonResponse.db);
                 jsonResponse.db.should.have.length(1);
-                Object.keys(jsonResponse.db[0].data).length.should.eql(27);
+                Object.keys(jsonResponse.db[0].data).length.should.eql(28);
                 done();
             });
     });
