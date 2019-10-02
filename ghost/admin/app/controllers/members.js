61c91ac77ec18b05f5918de90995f210ca9fbfdc
Rish <zrishabhgarg@gmail.com>
2019-10-02 17:05:50 +0530

    Updated members list/detail screens base structure
    no issue

--- ghost/admin/app/controllers/members.js
+++ ghost/admin/app/controllers/members.js
@@ -32,63 +32,7 @@ export default Controller.extend({
 
         return filtered;
     }),
-
-    chartData: computed('members.@each', function () {
-        let {members} = this;
-        let dateFormat = 'DD-MM-YYYY';
-        let monthData = [];
-        let dateLabel = [];
-        let startDate = moment().subtract(29, 'days');
-        for (let i = 0; i < 30; i++) {
-            let m = moment(startDate).add(i, 'days');
-            dateLabel.push(m.format(dateFormat));
-            let membersTillDate = members.filter((member) => {
-                let isValid = moment(member.createdAt).isSameOrBefore(m, 'day');
-                return isValid;
-            }).length;
-            monthData.push(membersTillDate);
-        }
-        return {
-            data: {
-                labels: dateLabel,
-                datasets: [
-                    {
-                        label: 'Total Members',
-                        data: monthData,
-                        fill: false,
-                        strokeColor: 'rgba(151,187,205,1)',
-                        pointColor: 'rgba(151,187,205,1)',
-                        pointStrokeColor: '#fff',
-                        pointHighlightFill: '#fff',
-                        pointHighlightStroke: 'rgba(151,187,205,1)'
-                    }
-                ]
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                title: {
-                    display: true,
-                    text: 'Total members in last 30 days'
-                },
-                scales: {
-                    xAxes: [{
-                        type: 'time',
-                        time: {
-                            format: dateFormat,
-                            tooltipFormat: 'll'
-                        },
-                        scaleLabel: {
-                            display: true,
-                            labelString: 'Date'
-                        },
-                        display: true
-                    }]
-                }
-            }
-        };
-    }),
-
+    
     fetchMembers: task(function* () {
         let newFetchDate = new Date();
         let results;
