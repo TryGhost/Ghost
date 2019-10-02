522679aa112d40b4d8a40352964d33db1207d1b1
Fabien O'Carroll <fabien@allou.is>
2019-10-02 10:34:27 +0700

    Added list and update stripe requests
    no-issue
    
    These will be used for listing and updating webhooks on configuration

--- ghost/members-api/lib/stripe/api/stripeRequests.js
+++ ghost/members-api/lib/stripe/api/stripeRequests.js
@@ -4,8 +4,16 @@ const retrieve = createStripeRequest(function (stripe, resource, id) {
     return stripe[resource].retrieve(id);
 });
 
+const list = createStripeRequest(function (stripe, resource, options) {
+    return stripe[resource].list(options);
+});
+
 const create = createStripeRequest(function (stripe, resource, object) {
     return stripe[resource].create(object);
+});
+
+const update = createStripeRequest(function (stripe, resource, id, object) {
+    return stripe[resource].update(id, object);
 });
 
 const del = createStripeRequest(function (stripe, resource, id) {
@@ -14,6 +22,8 @@ const del = createStripeRequest(function (stripe, resource, id) {
 
 module.exports = {
     retrieve,
+    list,
     create,
+    update,
     del
 };
