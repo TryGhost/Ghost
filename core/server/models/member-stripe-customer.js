026cbb23c5c34678068f2bb2f9268f9c4aad5f02
Fabien O'Carroll <fabien@allou.is>
2019-10-08 13:10:20 +0700

    Added upsert method to stripe models
    no-issue
    
    This is kind of copied from the session model, but simplified
    This will allow much easier integration with members-api

--- core/server/models/member-stripe-customer.js
+++ core/server/models/member-stripe-customer.js
@@ -2,6 +2,17 @@ const ghostBookshelf = require('./base');
 
 const MemberStripeCustomer = ghostBookshelf.Model.extend({
     tableName: 'members_stripe_customers'
+}, {
+    async upsert(data, unfilteredOptions) {
+        const customerId = data.customer_id;
+        const model = await this.findOne({customer_id: customerId}, unfilteredOptions);
+        if (model) {
+            return this.edit(data, Object.assign({}, unfilteredOptions, {
+                id: model.id
+            }));
+        }
+        return this.add(data, unfilteredOptions);
+    }
 });
 
 module.exports = {
--- core/server/models/stripe-customer-subscription.js
+++ core/server/models/stripe-customer-subscription.js
@@ -2,6 +2,17 @@ const ghostBookshelf = require('./base');
 
 const StripeCustomerSubscription = ghostBookshelf.Model.extend({
     tableName: 'stripe_customers_subscriptions'
+}, {
+    async upsert(data, unfilteredOptions) {
+        const subscriptionId = unfilteredOptions.subscription_id;
+        const model = await this.findOne({subscription_id: subscriptionId}, unfilteredOptions);
+        if (model) {
+            return this.edit(data, Object.assign({}, unfilteredOptions, {
+                id: model.id
+            }));
+        }
+        return this.add(data, unfilteredOptions);
+    }
 });
 
 module.exports = {
