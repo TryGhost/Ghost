a8d6db8cc80fae792f06af22645d1bb02d63cf20
Fabien O'Carroll <fabien@allou.is>
2019-10-08 17:07:29 +0700

    🐛 Fixed error when during migration to 2.32.0 on mysql (#11208)
    closes #11207
    
    MySQL doesn't allow unqiue keys with a length of more than 191 when using InnoDB with utfmb4. These changes will ensure any incorrect tables created are fixed and have the
    correct length for customer_id
    
    * Changed `customer_id` to non-unique column
    * Nooped the 2.32 `members_stripe_customers` migration
    * Added migration to recreate `members_stripe_customers` table
      * sqlite doesn't allow `ALTER TABLE` queries so this is the cleanest solution considering the table is not yet in use

--- core/server/data/migrations/versions/2.32/01-add-members-stripe-customers-table.js
+++ core/server/data/migrations/versions/2.32/01-add-members-stripe-customers-table.js
@@ -1,34 +1,4 @@
-const common = require('../../../../lib/common');
-const commands = require('../../../schema/commands');
-
 module.exports = {
-    config: {
-        transaction: true
-    },
-
-    async up(options){
-        const conn = options.transacting || options.connection;
-        const hasTable = await conn.schema.hasTable('members_stripe_customers');
-
-        if (hasTable) {
-            common.logging.warn('Adding table: members_stripe_customers');
-            return;
-        }
-
-        common.logging.info('Adding table: members_stripe_customers');
-        return commands.createTable('members_stripe_customers', conn);
-    },
-
-    async down(options){
-        const conn = options.transacting || options.connection;
-        const hasTable = await conn.schema.hasTable('members_stripe_customers');
-
-        if (!hasTable) {
-            common.logging.warn('Dropping table: members_stripe_customers');
-            return;
-        }
-
-        common.logging.info('Dropping table: members_stripe_customers');
-        return commands.deleteTable('members_stripe_customers', conn);
-    }
+    async up(){},
+    async down(){}
 };
