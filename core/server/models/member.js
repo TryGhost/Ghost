f05ba9f7845feb4519a9ccfe3ee8b2d669d8e25a
Fabien O'Carroll <fabien@allou.is>
2019-10-10 13:48:20 +0700

    Removed unused stripe_customers relationship
    no-issue

--- core/server/models/member.js
+++ core/server/models/member.js
@@ -1,24 +1,7 @@
 const ghostBookshelf = require('./base');
 
 const Member = ghostBookshelf.Model.extend({
-    tableName: 'members',
-
-    relationships: ['stripe_customers'],
-    relationshipBelongsTo: {
-        stripe_customers: 'members_stripe_customers'
-    },
-
-    permittedAttributes(...args) {
-        return ghostBookshelf.Model.prototype.permittedAttributes.apply(this, args).concat(this.relationships);
-    },
-
-    stripe_customers() {
-        return this.hasMany('MemberStripeCustomer', 'member_id');
-    }
-}, {
-    permittedOptions(...args) {
-        return ghostBookshelf.Model.permittedOptions.apply(this, args).concat(['withRelated']);
-    }
+    tableName: 'members'
 });
 
 const Members = ghostBookshelf.Collection.extend({
