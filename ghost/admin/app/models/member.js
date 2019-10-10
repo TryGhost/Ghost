62fcd9a8bbbc0392dac20418b14593c80e4c3efe
Rish <zrishabhgarg@gmail.com>
2019-10-10 17:29:35 +0530

    Enabled write operation for member name and note
    no issue
    
    - Allows editing member's name and note in admin

--- ghost/admin/app/models/member.js
+++ ghost/admin/app/models/member.js
@@ -4,6 +4,7 @@ import attr from 'ember-data/attr';
 export default DS.Model.extend({
     name: attr('string'),
     email: attr('string'),
+    note: attr('string'),
     createdAt: attr('moment-utc'),
     stripe: attr('member-subscription')
 });
