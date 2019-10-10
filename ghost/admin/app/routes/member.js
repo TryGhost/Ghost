1d0da1a485f48c4717be2755379e1db731073257
Rish <zrishabhgarg@gmail.com>
2019-10-10 18:06:33 +0530

    Added unsaved changes check to member details page
    no issue
    
    - Adds unsaved changes check to member detail page for write operations

--- ghost/admin/app/routes/member.js
+++ ghost/admin/app/routes/member.js
@@ -1,6 +1,24 @@
 import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
+import CurrentUserSettings from 'ghost-admin/mixins/current-user-settings';
+import {inject as service} from '@ember/service';
 
-export default AuthenticatedRoute.extend({
+export default AuthenticatedRoute.extend(CurrentUserSettings, {
+
+    router: service(),
+
+    init() {
+        this._super(...arguments);
+        this.router.on('routeWillChange', (transition) => {
+            this.showUnsavedChangesModal(transition);
+        });
+    },
+
+    beforeModel() {
+        this._super(...arguments);
+        return this.get('session.user')
+            .then(this.transitionAuthor());
+    },
+
     model(params) {
         this._isMemberUpdated = true;
         return this.store.findRecord('member', params.member_id, {
@@ -19,10 +37,31 @@ export default AuthenticatedRoute.extend({
         this._super(...arguments);
 
         // clear the properties
+        let {controller} = this;
+        controller.model.rollbackAttributes();
+        this.set('controller.model', null);
         this._isMemberUpdated = false;
+    },
+
+    actions: {
+        save() {
+            this.controller.send('save');
+        }
     },
 
     titleToken() {
         return this.controller.get('member.name');
+    },
+
+    showUnsavedChangesModal(transition) {
+        if (transition.from && transition.from.name.match(/^member$/) && transition.targetName) {
+            let {controller} = this;
+
+            if (!controller.member.isDeleted && controller.member.hasDirtyAttributes) {
+                transition.abort();
+                controller.send('toggleUnsavedChangesModal', transition);
+                return;
+            }
+        }
     }
 });
