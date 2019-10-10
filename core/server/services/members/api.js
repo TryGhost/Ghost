99681e692a7a48a094b591e1b0b9fbd06d672a99
Fabien O'Carroll <fabien@allou.is>
2019-10-10 13:45:43 +0700

    Updated the create,get&update member functions
    no-issue
    
    This updates them to async functions, and defaults falsy name and note to null

--- core/server/services/members/api.js
+++ core/server/services/members/api.js
@@ -7,26 +7,26 @@ const ghostVersion = require('../../lib/ghost-version');
 const mail = require('../mail');
 const models = require('../../models');
 
-function createMember({email, name, note}) {
-    return models.Member.add({
+async function createMember({email, name, note}, options = {}) {
+    const model = await models.Member.add({
         email,
-        name,
-        note
-    }).then((member) => {
-        return member.toJSON();
+        name: name || null,
+        note: note || null
     });
+    const member = model.toJSON(options);
+    return member;
 }
 
-function getMember(data, options = {}) {
+async function getMember(data, options = {}) {
     if (!data.email && !data.id) {
         return Promise.resolve(null);
     }
-    return models.Member.findOne(data, options).then((model) => {
-        if (!model) {
-            return null;
-        }
-        return model.toJSON(options);
-    });
+    const model = await models.Member.findOne(data, options);
+    if (!model) {
+        return null;
+    }
+    const member = model.toJSON(options);
+    return member;
 }
 
 async function setMetadata(module, metadata) {
@@ -71,8 +71,14 @@ async function getMetadata(module, member) {
     };
 }
 
-function updateMember({name, note}, options) {
-    return models.Member.edit({name, note}, options);
+async function updateMember({name, note}, options = {}) {
+    const model = await models.Member.edit({
+        name: name || null,
+        note: note || null
+    }, options);
+
+    const member = model.toJSON(options);
+    return member;
 }
 
 function deleteMember(options) {
