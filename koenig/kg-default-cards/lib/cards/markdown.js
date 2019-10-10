f73e53dce2df87a0b67907d7e5e1df530f231443
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-10-10 16:35:29 +0100

    🐛 Fixed rendering and url transformation of v1 "card-markdown" aliased cards
    no issue
    
    - Ghost 1.x stored markdown cards with the name `card-markdown`, this was changed in Ghost 2.x to be `markdown`. To keep compatibility with the older mobiledoc content the `markdown` card was aliased using a straightforward `Object.assign()`. Unfortunately this failed to work adequately when the url transformation functions were added to cards and resulted in corrupted data being returned in API responses
    - moved the markdown card definition into a factory function so that a clean card definition object can be used for both the `markdown` and `card-markdown` cards

--- koenig/kg-default-cards/lib/cards/markdown.js
+++ koenig/kg-default-cards/lib/cards/markdown.js
@@ -1,38 +1,6 @@
+const markdownCard = require('./_markdown');
 const createCard = require('../create-card');
 
-module.exports = createCard({
-    name: 'markdown',
-    type: 'dom',
-    render: function (opts) {
-        let converters = require('../converters');
-        let payload = opts.payload;
-        let version = opts.options && opts.options.version || 2;
-        // convert markdown to HTML ready for insertion into dom
-        let html = converters.markdownConverter.render(payload.markdown || '');
-
-        if (!html) {
-            return '';
-        }
-
-        /**
-         * @deprecated Ghost 1.0's markdown-only renderer wrapped cards. Remove in Ghost 3.0
-         */
-        if (version === 1) {
-            html = `<div class="kg-card-markdown">${html}</div>`;
-        }
-
-        // use the SimpleDOM document to create a raw HTML section.
-        // avoids parsing/rendering of potentially broken or unsupported HTML
-        return opts.env.dom.createRawHTMLSection(html);
-    },
-
-    absoluteToRelative(urlUtils, payload, options) {
-        payload.markdown = payload.markdown && urlUtils.markdownAbsoluteToRelative(payload.markdown, options);
-        return payload;
-    },
-
-    relativeToAbsolute(urlUtils, payload, options) {
-        payload.markdown = payload.markdown && urlUtils.markdownRelativeToAbsolute(payload.markdown, options);
-        return payload;
-    }
-});
+// uses the _markdown card definition function so that the card definition
+// can be re-used in aliased cards
+module.exports = createCard(markdownCard());
