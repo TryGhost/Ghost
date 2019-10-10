cb1c851f217994c5a29d64cc5ea2a918f6afb824
Rish <zrishabhgarg@gmail.com>
2019-10-10 18:06:33 +0530

    Added unsaved changes check to member details page
    no issue
    
    - Adds unsaved changes check to member detail page for write operations

--- ghost/admin/app/controllers/member.js
+++ ghost/admin/app/controllers/member.js
@@ -25,7 +25,7 @@ export default Controller.extend({
         setProperty(propKey, value) {
             this._saveMemberProperty(propKey, value);
         },
-        toggleDeleteTagModal() {
+        toggleDeleteMemberModal() {
             this.toggleProperty('showDeleteMemberModal');
         },
         finaliseDeletion() {
@@ -35,6 +35,44 @@ export default Controller.extend({
                 this.members.decrementProperty('meta.pagination.total');
             }
             this.router.transitionTo('members');
+        },
+
+        toggleUnsavedChangesModal(transition) {
+            let leaveTransition = this.leaveScreenTransition;
+    
+            if (!transition && this.showUnsavedChangesModal) {
+                this.set('leaveScreenTransition', null);
+                this.set('showUnsavedChangesModal', false);
+                return;
+            }
+    
+            if (!leaveTransition || transition.targetName === leaveTransition.targetName) {
+                this.set('leaveScreenTransition', transition);
+    
+                // if a save is running, wait for it to finish then transition
+                if (this.save.isRunning) {
+                    return this.save.last.then(() => {
+                        transition.retry();
+                    });
+                }
+    
+                // we genuinely have unsaved data, show the modal
+                this.set('showUnsavedChangesModal', true);
+            }
+        },
+
+        leaveScreen() {
+            let transition = this.leaveScreenTransition;
+
+            if (!transition) {
+                this.notifications.showAlert('Sorry, there was an error in the application. Please let the Ghost team know what happened.', {type: 'error'});
+                return;
+            }
+
+            // roll back changes on model props
+            this.member.rollbackAttributes();
+
+            return transition.retry();
         }
     },
 
