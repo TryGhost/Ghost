06adc2774c9ff776f0c9a79a8a3ed1101cb42b0a
Peter Zimon <peter.zimon@gmail.com>
2019-10-11 10:20:30 +0200

    Updated members Labs settings UI
    no issue
    - new settings have been introduced for members, switched to expand/collapse UI pattern for members settings

--- ghost/admin/app/components/gh-members-lab-setting.js
+++ ghost/admin/app/components/gh-members-lab-setting.js
@@ -23,6 +23,7 @@ export default Component.extend({
         subscriptionSettings.stripeConfig = stripeProcessor.config;
         subscriptionSettings.requirePaymentForSetup = !!subscriptionSettings.requirePaymentForSetup;
         subscriptionSettings.fromAddress = subscriptionSettings.fromAddress || 'noreply';
+
         return subscriptionSettings;
     }),
 
