4a0909a46408ce94e7c3a0644d9dd204eac9f86f
Naz Gargol <nazargargol@gmail.com>
2019-10-17 10:49:30 +0200

    💄Updated fixtures for Ghost v3 content + assets (#11247)
    no issue 
    
    - Updated post images and content
    - Updated default blog cover asset
    - Updated default Ghost user avatar

--- core/test/unit/data/schema/integrity_spec.js
+++ core/test/unit/data/schema/integrity_spec.js
@@ -20,7 +20,7 @@ var should = require('should'),
 describe('DB version integrity', function () {
     // Only these variables should need updating
     const currentSchemaHash = '09ab28b8a18dc7f7f4fb5dbd5ccc10ac';
-    const currentFixturesHash = 'a15f00c333f207418952a9559fc3239f';
+    const currentFixturesHash = '59714063fa30b2f3fbfc929551b3c1e5';
 
     // If this test is failing, then it is likely a change has been made that requires a DB version bump,
     // and the values above will need updating as confirmation
