7866546fab4eb946677764fa4530168556991251
Rishabh Garg <rish@ghost.org>
2019-11-04 13:15:13 +0700

    Added email settings to post settings menu (#1398)
    no issue

--- ghost/admin/app/validators/post.js
+++ ghost/admin/app/validators/post.js
@@ -18,7 +18,8 @@ export default BaseValidator.create({
         'twitterTitle',
         'twitterDescription',
         'publishedAtBlogTime',
-        'publishedAtBlogDate'
+        'publishedAtBlogDate',
+        'emailSubject'
     ],
 
     title(model) {
@@ -120,6 +121,14 @@ export default BaseValidator.create({
             this.invalidate();
         }
     },
+
+    emailSubject(model) {
+        if (!validator.isLength(model.emailSubject || '', 0, 300)) {
+            model.errors.add('emailSubject', 'Email Subject cannot be longer than 300 characters.');
+            this.invalidate();
+        }
+    },
+
     // for posts which haven't been published before and where the blog date/time
     // is blank we should ignore the validation
     _shouldValidatePublishedAtBlog(model) {
