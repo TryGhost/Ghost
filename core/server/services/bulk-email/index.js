6f45990fe30b31ce25ca60e06b5fac5f7e0b0ae0
Fabien O'Carroll <fabien@allou.is>
2019-11-04 12:36:10 +0700

    Added initial bulk-email service
    no-issue
    
    This is a simple wrapper around the current ghost mailer service for now

---
+++ core/server/services/bulk-email/index.js
@@ -0,0 +1,38 @@
+// @ts-check
+const mailService = require('../mail');
+const ghostMailer = new mailService.GhostMailer();
+const common = require('../../lib/common');
+
+/*
+ * An email address
+ * @typedef { string } EmailAddress
+ */
+
+/*
+ * An object representing an email to send
+ * @typedef { Object } Email
+ * @property { string } html - The html content of the email
+ * @property { string } subject - The subject of the email
+ */
+
+module.exports = {
+    /**
+     * @param {Email} message - The message to send
+     * @param {[EmailAddress]} recipients - the recipients to send the email to
+     * @returns {Promise<boolean>} A promise representing the success of the email sending
+     */
+    async send(message, recipients) {
+        for (const recipient in recipients) {
+            const messageToSend = Object.assign({}, message, {
+                to: recipient
+            });
+            try {
+                await ghostMailer.send(messageToSend);
+            } catch (err) {
+                // @TODO log this somewhere with state?
+                common.logging.warn(`Oh no! an email failed to send :( ${recipient}`);
+            }
+        }
+        return true;
+    }
+};
