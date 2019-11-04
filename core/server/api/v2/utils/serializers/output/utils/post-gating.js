30ee55ee85c5226c274bf6d489576a0ba7b7ec1c
Fabien O'Carroll <fabien@allou.is>
2019-11-04 16:58:01 +0700

    Refactored members content gating check
    no-issue

--- core/server/api/v2/utils/serializers/output/utils/post-gating.js
+++ core/server/api/v2/utils/serializers/output/utils/post-gating.js
@@ -1,37 +1,11 @@
-const _ = require('lodash');
+const membersService = require('../../../../../../services/members');
 const labs = require('../../../../../../services/labs');
-
-// Checks if request should hide members only content
-function hideMembersOnlyContent({visibility}, frame) {
-    const PERMIT_CONTENT = false;
-    const BLOCK_CONTENT = true;
-
-    if (visibility === 'public') {
-        return PERMIT_CONTENT;
-    }
-
-    const requestFromMember = frame.original.context.member;
-
-    if (!requestFromMember) {
-        return BLOCK_CONTENT;
-    } else if (visibility === 'members') {
-        return PERMIT_CONTENT;
-    }
-
-    const memberHasPlan = !!(_.get(frame, 'original.context.member.stripe.subscriptions', [])).length;
-
-    if (visibility === 'paid' && memberHasPlan) {
-        return PERMIT_CONTENT;
-    }
-
-    return BLOCK_CONTENT;
-}
 
 const forPost = (attrs, frame) => {
     if (labs.isSet('members')) {
-        const hideFormatsData = hideMembersOnlyContent(attrs, frame);
+        const memberHasAccess = membersService.contentGating.checkPostAccess(attrs, frame.original.context.member);
 
-        if (hideFormatsData) {
+        if (!memberHasAccess) {
             ['plaintext', 'html'].forEach((field) => {
                 attrs[field] = '';
             });
