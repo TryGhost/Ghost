12382f23b44d6e0df4439c80bacc8eb68a62f357
Fabien O'Carroll <fabien@allou.is>
2019-11-05 17:01:02 +0700

    Added uuid column to members table
    no-issue
    
    This will be used for unsubscribe links

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -323,6 +323,7 @@ module.exports = {
     },
     members: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
+        uuid: {type: 'string', maxlength: 36, nullable: true, unique: true, validations: {isUUID: true}},
         email: {type: 'string', maxlength: 191, nullable: false, unique: true, validations: {isEmail: true}},
         name: {type: 'string', maxlength: 191, nullable: true},
         note: {type: 'string', maxlength: 2000, nullable: true},
