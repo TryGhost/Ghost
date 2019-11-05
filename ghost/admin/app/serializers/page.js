3e26fb397b6dff2795071f636db3fdb2dcd12448
Nazar Gargol <nazargargol@gmail.com>
2019-11-05 18:43:26 +0700

    Removed post specific fields from page model serializer

--- ghost/admin/app/serializers/page.js
+++ ghost/admin/app/serializers/page.js
@@ -1,3 +1,13 @@
 import PostSerializer from './post';
 
-export default PostSerializer.extend({});
+export default PostSerializer.extend({
+    serialize(/*snapshot, options*/) {
+        let json = this._super(...arguments);
+
+        // Properties that exist on the model but we don't want sent in the payload
+        delete json.email_subject;
+        delete json.send_email_when_published;
+
+        return json;
+    }
+});
