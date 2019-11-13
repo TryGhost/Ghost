8f417085b80176f3f21e20f58c503261ed11788b
Peter Zimon <peter.zimon@gmail.com>
2019-11-13 15:31:42 +0100

    Updated post status in editor

--- ghost/admin/app/components/gh-editor-post-status.js
+++ ghost/admin/app/components/gh-editor-post-status.js
@@ -1,14 +1,35 @@
 import Component from '@ember/component';
 import config from 'ghost-admin/config/environment';
+import moment from 'moment';
+import {computed} from '@ember/object';
+import {inject as service} from '@ember/service';
 import {task, timeout} from 'ember-concurrency';
 
 export default Component.extend({
+    clock: service(),
     post: null,
     isSaving: false,
 
     'data-test-editor-post-status': true,
 
     _isSaving: false,
+
+    countdown: computed('post.{publishedAtUTC,isScheduled}', 'clock.second', function () {
+        let isScheduled = this.get('post.isScheduled');
+        let publishTime = this.get('post.publishedAtUTC') || moment.utc();
+
+        let timeUntilPublished = publishTime.diff(moment.utc(), 'minutes', true);
+        let isPublishedSoon = timeUntilPublished > 0 && timeUntilPublished < 15;
+
+        // force a recompute
+        this.get('clock.second');
+
+        if (isScheduled && isPublishedSoon) {
+            return moment(publishTime).fromNow();
+        } else {
+            return false;
+        }
+    }),
 
     // isSaving will only be true briefly whilst the post is saving,
     // we want to ensure that the "Saving..." message is shown for at least
