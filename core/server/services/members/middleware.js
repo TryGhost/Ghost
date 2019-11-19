db10ceed4c00935c0f3db64174f787a9487e73bd
Nazar Gargol <nazargargol@gmail.com>
2019-11-19 17:22:20 +0700

    Moved obvious member specific code into members service
    - The middleware is initialized in similar fasion to apps and sitemapHandler.
    - It's a mixed bag of routes and request/response data modifications so can't be just one thing like `siteApp.use(membersMiddleware)`

---
+++ core/server/services/members/middleware.js
@@ -0,0 +1,71 @@
+const common = require('../../lib/common');
+const shared = require('../../web/shared');
+const labsService = require('../labs');
+const membersService = require('./index');
+
+module.exports.use = (siteApp) => {
+    // @TODO only loads this stuff if members is enabled
+    // Set req.member & res.locals.member if a cookie is set
+    siteApp.get('/members/ssr', shared.middlewares.labs.members, async function (req, res) {
+        try {
+            const token = await membersService.ssr.getIdentityTokenForMemberFromSession(req, res);
+            res.writeHead(200);
+            res.end(token);
+        } catch (err) {
+            common.logging.warn(err.message);
+            res.writeHead(err.statusCode);
+            res.end(err.message);
+        }
+    });
+
+    siteApp.delete('/members/ssr', shared.middlewares.labs.members, async function (req, res) {
+        try {
+            await membersService.ssr.deleteSession(req, res);
+            res.writeHead(204);
+            res.end();
+        } catch (err) {
+            common.logging.warn(err.message);
+            res.writeHead(err.statusCode);
+            res.end(err.message);
+        }
+    });
+
+    siteApp.post('/members/webhooks/stripe', (req, res, next) => membersService.api.middleware.handleStripeWebhook(req, res, next));
+    siteApp.use(async function (req, res, next) {
+        if (!labsService.isSet('members')) {
+            req.member = null;
+            return next();
+        }
+        try {
+            const member = await membersService.ssr.getMemberDataFromSession(req, res);
+            Object.assign(req, {member});
+            next();
+        } catch (err) {
+            common.logging.warn(err.message);
+            Object.assign(req, {member: null});
+            next();
+        }
+    });
+
+    siteApp.use(async function (req, res, next) {
+        if (!labsService.isSet('members')) {
+            return next();
+        }
+        if (!req.url.includes('token=')) {
+            return next();
+        }
+        try {
+            const member = await membersService.ssr.exchangeTokenForSession(req, res);
+            Object.assign(req, {member});
+            next();
+        } catch (err) {
+            common.logging.warn(err.message);
+            return next();
+        }
+    });
+
+    siteApp.use(function (req, res, next) {
+        res.locals.member = req.member;
+        next();
+    });
+};
