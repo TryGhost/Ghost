193c179110dfe208dd843123befbb3606b35a634
Naz Gargol <nazargargol@gmail.com>
2019-11-21 10:01:24 +0700

    Extracted members-specific middleware from site app module (#11405)
    no issue
    
    - In order to keep site/app.js module tidy and less coupled with members module we need to extract some of the functionality where it belongs conceptually
    - Added "members enabled check" middleware to stripe webhook endpoint
    - Reshuffled members middleware so that siteApp is in control of mounting points. This is meant to be a more explicit way to see which endpoints are being handled by members middleware
    - Extracted member-specific public file middleware
    - Unified use of `labs.member` alias method. Done for code style consistency
    - Added basic members' test suite. This is a base we could work from when more modifications are needed
    - Removed route handler for unexisting members file "members-theme-bindings.js". Calling this route otherwise causes a 500. Looks like a leftover from 49672a1e4d268683feb67a574429b910417100c1

---
+++ core/server/services/members/middleware.js
@@ -0,0 +1,97 @@
+const common = require('../../lib/common');
+const constants = require('../../lib/constants');
+const shared = require('../../web/shared');
+const labsService = require('../labs');
+const membersService = require('./index');
+
+const login = async function (req, res) {
+    try {
+        const token = await membersService.ssr.getIdentityTokenForMemberFromSession(req, res);
+        res.writeHead(200);
+        res.end(token);
+    } catch (err) {
+        common.logging.warn(err.message);
+        res.writeHead(err.statusCode);
+        res.end(err.message);
+    }
+};
+
+const logout = async function (req, res) {
+    try {
+        await membersService.ssr.deleteSession(req, res);
+        res.writeHead(204);
+        res.end();
+    } catch (err) {
+        common.logging.warn(err.message);
+        res.writeHead(err.statusCode);
+        res.end(err.message);
+    }
+};
+
+const getMemberDataFromSession = async function (req, res, next) {
+    if (!labsService.isSet('members')) {
+        req.member = null;
+        return next();
+    }
+    try {
+        const member = await membersService.ssr.getMemberDataFromSession(req, res);
+        Object.assign(req, {member});
+        next();
+    } catch (err) {
+        common.logging.warn(err.message);
+        Object.assign(req, {member: null});
+        next();
+    }
+};
+
+const exchangeTokenForSession = async function (req, res, next) {
+    if (!labsService.isSet('members')) {
+        return next();
+    }
+    if (!req.url.includes('token=')) {
+        return next();
+    }
+    try {
+        const member = await membersService.ssr.exchangeTokenForSession(req, res);
+        Object.assign(req, {member});
+        next();
+    } catch (err) {
+        common.logging.warn(err.message);
+        return next();
+    }
+};
+
+const decorateResponse = function (req, res, next) {
+    res.locals.member = req.member;
+    next();
+};
+
+// @TODO only loads this stuff if members is enabled
+// Set req.member & res.locals.member if a cookie is set
+module.exports = {
+    public: [
+        shared.middlewares.labs.members,
+        shared.middlewares.servePublicFile.createPublicFileMiddleware(
+            'public/members.js',
+            'application/javascript',
+            constants.ONE_HOUR_S
+        )
+    ],
+    authentication: [
+        getMemberDataFromSession,
+        exchangeTokenForSession,
+        decorateResponse
+    ],
+    login: [
+        shared.middlewares.labs.members,
+        login
+    ],
+    logout: [
+        shared.middlewares.labs.members,
+        logout
+    ],
+    stripeWebhooks: [
+        shared.middlewares.labs.members,
+        (req, res, next) => membersService.api.middleware.handleStripeWebhook(req, res, next)
+    ]
+};
