f78375ab7284376a02d85d6d86020dd3d187a03a
Rish <zrishabhgarg@gmail.com>
2019-11-26 08:55:56 +0530

    Allowed brand color in public settings

--- core/server/services/settings/public.js
+++ core/server/services/settings/public.js
@@ -11,6 +11,7 @@ module.exports = {
     description: 'description',
     logo: 'logo',
     icon: 'icon',
+    brand_color: 'brand_color',
     cover_image: 'cover_image',
     facebook: 'facebook',
     twitter: 'twitter',
