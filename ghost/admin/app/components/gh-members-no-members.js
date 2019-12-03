4bd482590207e4c47d00bad97e2bd59103ba69fe
Naz Gargol <nazargargol@gmail.com>
2019-12-03 12:04:04 +0700

    ✨ Improved adding first members on no-members screen (#1416)
    no issue
    
    - There was a need to make the experience of adding a first member(s) as frictionless as possible, which lead to adding some actionable controls to the "no-members" screen.
    - Adds the ability to make yourself a member
    - Adds the ability to import CSV member
    - Link to add a member manually

---
+++ ghost/admin/app/components/gh-members-no-members.js
@@ -0,0 +1,34 @@
+import Component from '@ember/component';
+import {inject as service} from '@ember/service';
+import {task} from 'ember-concurrency';
+
+export default Component.extend({
+    session: service(),
+    store: service(),
+    notifications: service(),
+
+    actions: {
+        addYourself() {
+            return this.add.perform();
+        }
+    },
+
+    add: task(function* () {
+        const member = this.store.createRecord('member', {
+            email: this.get('session.user.email'),
+            name: this.get('session.user.name')
+        });
+
+        try {
+            // NOTE: has to be before member.save() is performed otherwise component is
+            //       destroyed before notification is shown
+            this.notifications.showNotification('You\'ve successfully added yourself as a member!'.htmlSafe());
+
+            return yield member.save();
+        } catch (error) {
+            if (error) {
+                this.notifications.showAPIError(error, {key: 'member.save'});
+            }
+        }
+    }).drop()
+});
