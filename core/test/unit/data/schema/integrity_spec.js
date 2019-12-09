dd1d478966f2add13fb38f9e0e48e29e99143a7e
Nazar Gargol <nazargargol@gmail.com>
2019-12-09 15:30:17 +0700

    Updated schema and added migration for `current_period_end` column
    - As discussed we only add single column to  subscriptions table to avoid preoptimizing for future cases

--- core/test/unit/data/schema/integrity_spec.js
+++ core/test/unit/data/schema/integrity_spec.js
@@ -19,7 +19,7 @@ var should = require('should'),
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = 'f4610067bb9175c49dd74ff206011b6e';
+    const currentSchemaHash = '3ec33e7039a21dba597ada2a03de0526';
     const currentFixturesHash = '1a0f96fa1d8b976d663eb06719be031c';
 
     // If this test is failing, then it is likely a change has been made that requires a DB version bump,
