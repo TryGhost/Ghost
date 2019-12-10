4e1fec07bae1e7d9a9244d79097657cb9da2b8a4
Nazar Gargol <nazargargol@gmail.com>
2019-12-10 14:02:34 +0700

    Added test coverage for {{cancel_link}} helper

---
+++ core/test/unit/helpers/cancel_link_spec.js
@@ -0,0 +1,96 @@
+const should = require('should');
+const hbs = require('../../../frontend/services/themes/engine');
+const helpers = require('../../../frontend/helpers');
+const configUtils = require('../../utils/configUtils');
+
+describe('{{cancel_link}} helper', function () {
+    before(function (done) {
+        hbs.express4({partialsDir: [configUtils.config.get('paths').helperTemplates]});
+
+        hbs.cachePartials(function () {
+            done();
+        });
+    });
+
+    const defaultButtonClass = /class="cancel-subscription-button"/;
+    const defaultCancelButtonText = /Cancel subscription/;
+    const defaultContinueButtonText = /Continue subscription/;
+
+    it('should throw if subscription data is incorrect', function () {
+        var runHelper = function (data) {
+                return function () {
+                    helpers.cancel_link.call(data);
+                };
+            }, expectedMessage = 'The {{cancel_link}} helper was used outside of a subscription context. See https://ghost.org/docs/api/handlebars-themes/helpers/cancel_link/.';
+
+        runHelper('not an object').should.throwError(expectedMessage);
+        runHelper(function () {
+        }).should.throwError(expectedMessage);
+    });
+
+    it('can render cancel subscription button', function () {
+        const rendered = helpers.cancel_link.call({
+            id: 'sub_cancel',
+            cancel_at_period_end: false
+        });
+        should.exist(rendered);
+
+        rendered.string.should.match(defaultButtonClass);
+        rendered.string.should.match(/data-members-cancel-subscription="sub_cancel"/);
+        rendered.string.should.match(defaultCancelButtonText);
+    });
+
+    it('can render continue subscription button', function () {
+        const rendered = helpers.cancel_link.call({
+            id: 'sub_continue',
+            cancel_at_period_end: true
+        });
+        should.exist(rendered);
+
+        rendered.string.should.match(defaultButtonClass);
+        rendered.string.should.match(/data-members-continue-subscription="sub_continue"/);
+        rendered.string.should.match(defaultContinueButtonText);
+    });
+
+    it('can render custom button class', function () {
+        const rendered = helpers.cancel_link.call({
+            id: 'sub_cancel',
+            cancel_at_period_end: false
+        }, {
+            hash: {
+                className: 'custom-button-class'
+            }
+        });
+        should.exist(rendered);
+
+        rendered.string.should.match(/custom-button-class/);
+    });
+
+    it('can render custom cancel subscription button attributes', function () {
+        const rendered = helpers.cancel_link.call({
+            id: 'sub_cancel',
+            cancel_at_period_end: false
+        }, {
+            hash: {
+                cancelLabel: 'custom cancel button text'
+            }
+        });
+        should.exist(rendered);
+
+        rendered.string.should.match(/custom cancel button text/);
+    });
+
+    it('can render custom continue subscription button attributes', function () {
+        const rendered = helpers.cancel_link.call({
+            id: 'sub_cancel',
+            cancel_at_period_end: true
+        }, {
+            hash: {
+                continueLabel: 'custom continue button text'
+            }
+        });
+        should.exist(rendered);
+
+        rendered.string.should.match(/custom continue button text/);
+    });
+});
