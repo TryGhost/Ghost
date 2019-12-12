bc073b5822c97132e9018320da4bba5341e39d6a
Nazar Gargol <nazargargol@gmail.com>
2019-12-12 15:48:19 +0700

    Cleaned up helper description

--- core/frontend/helpers/cancel_link.js
+++ core/frontend/helpers/cancel_link.js
@@ -1,9 +1,9 @@
 // # {{cancel_link}} Helper
-// Usage: `{{cancel_link}}`, `{{cancel_link className="custom-cancel-class"}}`, `{{cancel_link cancelLabel="Cancel please!"}}`
+// Usage: `{{cancel_link}}`, `{{cancel_link class="custom-cancel-class"}}`, `{{cancel_link cancelLabel="Cancel please!"}}`
 //
 // Outputs cancel/renew links to manage subscription renewal after the subscription period ends.
 //
-// Defaults to class="cancel-subscription-link" errorClass="cancel-subscription-error" cancelLabel="Cancel subscription"  continueLabel="Continue subscription"
+// Defaults to class="cancel-subscription-link" errorClass="cancel-subscription-error" cancelLabel="Cancel subscription" continueLabel="Continue subscription"
 
 const proxy = require('./proxy');
 
