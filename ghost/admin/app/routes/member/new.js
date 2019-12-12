5585a781b9e1e090b8e01301150d19f339ae4708
Kevin Ansfield <kevin@lookingsideways.co.uk>
2019-12-12 13:35:52 +0000

    Unified tag and member screen code
    no issue
    
    The tag and member screens share the same underlying UI/UX patterns but were using different code patterns. This brings both in line so that we have consistent code patterns that can be re-used for other screens.
    
    - fixed cleanup of new tags by adding the `deactivate` hook to the `tag` route
    - updated `member` and `member.new` route/controller setup to match tag route/controller setup
      - added `save` action to member controller so that Ctrl/Cmd+S works on member screen
      - updated tag route/controller to utilise the same instant display w/background refresh when accessing the tag details screen
    - completed transition of non-component tag/members templates over to angle bracket component syntax

--- ghost/admin/app/routes/member/new.js
+++ ghost/admin/app/routes/member/new.js
@@ -1,45 +1,6 @@
-import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
-import CurrentUserSettings from 'ghost-admin/mixins/current-user-settings';
-import {isEmpty} from '@ember/utils';
-import {inject as service} from '@ember/service';
+import MemberRoute from '../member';
 
-export default AuthenticatedRoute.extend(CurrentUserSettings, {
-
-    router: service(),
-
-    controllerName: 'member.new',
-    templateName: 'member/new',
-
-    init() {
-        this._super(...arguments);
-        this.router.on('routeWillChange', (transition) => {
-            this.showUnsavedChangesModal(transition);
-        });
-    },
-
-    model() {
-        return this.store.createRecord('member');
-    },
-
-    // reset the model so that mobile screens react to an empty selectedMember
-    deactivate() {
-        this._super(...arguments);
-
-        let {controller} = this;
-        controller.model.rollbackAttributes();
-        controller.set('model', null);
-    },
-
-    showUnsavedChangesModal(transition) {
-        if (transition.from && transition.from.name.match(/^members\.new/) && transition.targetName) {
-            let {controller} = this;
-            let isUnchanged = isEmpty(Object.keys(controller.member.changedAttributes()));
-            if (!controller.member.isDeleted && !isUnchanged) {
-                transition.abort();
-                controller.send('toggleUnsavedChangesModal', transition);
-                return;
-            }
-        }
-    }
-
+export default MemberRoute.extend({
+    controllerName: 'member',
+    templateName: 'member'
 });
