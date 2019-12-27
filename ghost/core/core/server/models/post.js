af8fe56a36c616984ec986498cb10e33ecc62145
sapphi-red <green@sapphi.red>
2019-12-27 23:18:41 +0900

    エラー出力を追加

--- ghost/core/core/server/models/post.js
+++ ghost/core/core/server/models/post.js
@@ -670,6 +670,7 @@ Post = ghostBookshelf.Model.extend({
             try {
                 this.set('html', mobiledocLib.mobiledocHtmlRenderer.render(JSON.parse(this.get('mobiledoc'))));
             } catch (err) {
+                console.error('Mobiledoc Convertion Error:', err);
                 throw new errors.ValidationError({
                     message: tpl(messages.invalidMobiledocStructure),
                     help: 'https://ghost.org/docs/publishing/'
