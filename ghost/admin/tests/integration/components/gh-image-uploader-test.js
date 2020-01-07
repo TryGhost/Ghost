68c7147887d4e0ae2c66776600e4c56f45f7fb52
Kevin Ansfield <kevin@lookingsideways.co.uk>
2020-01-07 09:11:42 +0000

    Fixed linting

--- ghost/admin/tests/integration/components/gh-image-uploader-test.js
+++ ghost/admin/tests/integration/components/gh-image-uploader-test.js
@@ -4,7 +4,7 @@ import Service from '@ember/service';
 import hbs from 'htmlbars-inline-precompile';
 import sinon from 'sinon';
 import {UnsupportedMediaTypeError} from 'ghost-admin/services/ajax';
-import {click, find, findAll, render, settled, triggerEvent, waitFor, waitUntil} from '@ember/test-helpers';
+import {click, find, findAll, render, settled, triggerEvent} from '@ember/test-helpers';
 import {createFile, fileUpload} from '../../helpers/file-upload';
 import {describe, it} from 'mocha';
 import {expect} from 'chai';
