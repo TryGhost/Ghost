2b48394e266bd7487e7c61e934f87bb55014d9ae
Kevin Ansfield <kevin@lookingsideways.co.uk>
2020-01-09 17:20:13 +0000

    Unsilenced deprecations that are no longer triggered
    no issue
    
    - cleaned up the `deprecation-workflow.js` file by removing deprecations that are no longer being triggered by our test suite or general app usage (if they are still triggered in untested areas it will be useful to see the logs)
    - updated usages of `{{gh-text-field}}` to use it's `shouldFocus` attribute rather than directly overriding it's `autofocus` computed property

--- ghost/admin/config/deprecation-workflow.js
+++ ghost/admin/config/deprecation-workflow.js
@@ -2,12 +2,6 @@ self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
     workflow: [
         {handler: 'silence', matchId: 'ember-views.curly-components.jquery-element'},
-        {handler: 'silence', matchId: 'computed-property.override'},
-        {handler: 'silence', matchId: 'application-controller.router-properties'},
-        {handler: 'silence', matchId: 'events.remove-all-listeners'},
-        {handler: 'silence', matchId: 'ember-polyfills.deprecate-merge'},
-        {handler: 'silence', matchId: 'events.inherited-function-listeners'},
-        {handler: 'silence', matchId: 'ember-component.send-action'},
         // revert once ember-infinity removes usage of `isVisible`
         // https://github.com/ember-infinity/ember-infinity/pull/399
         {handler: 'silence', matchId: 'ember-component.is-visible'}
