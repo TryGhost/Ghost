4bdfd648dbcd77ae6faa5ff1632426001271040e
Kevin Ansfield <kevin@lookingsideways.co.uk>
2020-01-10 15:12:39 +0000

    Removed deprecated usage of `this.$()` in components
    no issue
    
    - converted remaining uses of `this.$()` that I could find over to native DOM
    - deprecation is still silenced for now because both `liquid-fire` and `liquid-wormhole` trigger it

--- ghost/admin/config/deprecation-workflow.js
+++ ghost/admin/config/deprecation-workflow.js
@@ -1,6 +1,7 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
     workflow: [
+        // revert one liquid-fire and liquid-wormhole remove uses of `this.$()`
         {handler: 'silence', matchId: 'ember-views.curly-components.jquery-element'},
         // revert once ember-infinity removes usage of `isVisible`
         // https://github.com/ember-infinity/ember-infinity/pull/399
