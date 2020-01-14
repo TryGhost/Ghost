681e2c5699d197bd0f10c4f4c3fa36d06a5e43bd
Kevin Ansfield <kevin@lookingsideways.co.uk>
2020-01-14 15:39:31 +0000

    Silenced Component#sendAction deprecation
    no issue
    
    - `ember-drag-drop` still triggers this and spams the console when re-ordering tags in the tags input

--- ghost/admin/config/deprecation-workflow.js
+++ ghost/admin/config/deprecation-workflow.js
@@ -1,9 +1,14 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
     workflow: [
-        // revert one liquid-fire and liquid-wormhole remove uses of `this.$()`
+        // remove once ember-drag-drop removes usage of Component#sendAction
+        // https://github.com/mharris717/ember-drag-drop/issues/155
+        {handler: 'silence', matchId: 'ember-component.send-action'},
+
+        // remove once liquid-fire and liquid-wormhole remove uses of `this.$()`
         {handler: 'silence', matchId: 'ember-views.curly-components.jquery-element'},
-        // revert once ember-infinity removes usage of `isVisible`
+
+        // remove once ember-infinity removes usage of `isVisible`
         // https://github.com/ember-infinity/ember-infinity/pull/399
         {handler: 'silence', matchId: 'ember-component.is-visible'}
     ]
