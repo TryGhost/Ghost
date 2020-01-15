6cd37dbf514b8c1137f548bce340d00d3e5c7ae7
Kevin Ansfield <kevin@ghost.org>
2020-01-15 13:53:51 +0000

    Upgraded `ember-power-*` addons (#1459)
    no issue
    
    - bump deps
    - adjust usage for breaking changes... 
      - https://github.com/cibernox/ember-power-select/blob/master/CHANGELOG.md#300-beta1
      - https://github.com/cibernox/ember-basic-dropdown/blob/master/CHANGELOG.md#200-beta3
      - https://github.com/cibernox/ember-power-datepicker/blob/master/CHANGELOG.md#070
    - update overridden component and template files to match latest addon code
      - switch to class syntax w/decorators
      - adjust for angle bracket syntax

--- ghost/admin/app/components/gh-basic-dropdown.js
+++ ghost/admin/app/components/gh-basic-dropdown.js
@@ -1,19 +1,20 @@
 import BasicDropdown from 'ember-basic-dropdown/components/basic-dropdown';
-import layout from 'ember-basic-dropdown/templates/components/basic-dropdown';
+import templateLayout from 'ember-basic-dropdown/templates/components/basic-dropdown';
+import {layout} from '@ember-decorators/component';
 import {inject as service} from '@ember/service';
 
-export default BasicDropdown.extend({
-    dropdown: service(),
+@layout(templateLayout)
+class GhBasicDropdown extends BasicDropdown {
+    @service dropdown
 
-    layout,
-
-    didInsertElement() {
-        this._super(...arguments);
+    onInit() {
         this.dropdown.on('close', this, this.close);
-    },
-
-    willDestroyElement() {
-        this._super(...arguments);
-        this.dropdown.off('close', this, this.close);
     }
-});
+
+    willDestroy() {
+        this.dropdown.off('close', this, this.close);
+        super.willDestroyElement(...arguments);
+    }
+}
+
+export default GhBasicDropdown;
