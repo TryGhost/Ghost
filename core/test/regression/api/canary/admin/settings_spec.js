02d96139a6d2cf1640f33b19547e3525515bbcc9
Daniel Lockyer <hi@daniellockyer.com>
2020-01-30 10:23:33 +0000

    Fixed select failures in regressions tests
    no issue
    
    - a couple of regressions tests would repeatedly fail on CI. They would
      always follow tests that closed the Ghost server beforehand.
    - this commit doesn't close the server after those tests have completed,
      which is similar functionality to all other tests
    - the actual cause is unknown at this point, but I suspect it's some
      sort of race condition

--- core/test/regression/api/canary/admin/settings_spec.js
+++ core/test/regression/api/canary/admin/settings_spec.js
@@ -21,10 +21,6 @@ describe('Settings API (canary)', function () {
                 });
         });
 
-        after(function () {
-            return ghostServer.stop();
-        });
-
         it('Can\'t read core setting', function () {
             return request
                 .get(localUtils.API.getApiQuery('settings/db_hash/'))
