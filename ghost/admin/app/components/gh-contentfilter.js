2a77c0fe51c0c4facc641a4569c8a884fa042ada
Kevin Ansfield <kevin@lookingsideways.co.uk>
2020-01-30 15:35:36 +0000

    ✨ Added default and custom post views (filters) to the admin sidebar (#1474)
    no issue
    
    - list custom post views in collapsable sidebar navigation
      - default views: Draft, Scheduled, Published (except for contributors)
      - record expanded/collapsed state of the navigation menus in user settings via new `navigation` service
    - adds `customViews` service that manages custom views
      - provides list of default views
      - gives access to "active" custom view based on current route and query params
      - manages loading/saving of custom views to user settings
    - show "Add view" button in the content filter when the posts list has been filtered
    - show "Edit view" button in the content filter when the posts list filter matches a saved view
    
    Co-authored-by: Peter Zimon <peter.zimon@gmail.com>

--- ghost/admin/app/components/gh-contentfilter.js
+++ ghost/admin/app/components/gh-contentfilter.js
@@ -2,5 +2,20 @@ import Component from '@glimmer/component';
 import {inject as service} from '@ember/service';
 
 export default class GhContentfilterComponent extends Component {
-    @service session
+    @service customViews;
+    @service router;
+    @service session;
+
+    calculateActionsDropdownPosition(trigger, content) {
+        let {top, left, width, height} = trigger.getBoundingClientRect();
+        // content.firstElementChild is required because we use .dropdown-menu which is absolute positioned
+        let {width: contentWidth} = content.firstElementChild.getBoundingClientRect();
+
+        let style = {
+            left: left + width - contentWidth,
+            top: top + height + 5
+        };
+
+        return {style};
+    }
 }
