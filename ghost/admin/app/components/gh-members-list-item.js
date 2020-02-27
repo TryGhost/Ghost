82778691e6af0052c87314e2dbbf692d47c48037
Kevin Ansfield <kevin@lookingsideways.co.uk>
2020-02-27 12:17:48 +0000

    Refactored <GhMembersListItem>
    no issue
    
    - switched to using a glimmer component
      - cleaned up property access in the template
      - used helpers for basic display logic in the template
    - removed unused computed properties, aliases, and injected services

--- ghost/admin/app/components/gh-members-list-item.js
+++ ghost/admin/app/components/gh-members-list-item.js
@@ -1,29 +1,8 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import moment from 'moment';
-import {alias, or} from '@ember/object/computed';
-import {computed} from '@ember/object';
-import {inject as service} from '@ember/service';
 
-export default Component.extend({
-    ghostPaths: service(),
-    notifications: service(),
-    router: service(),
-
-    tagName: 'li',
-    classNames: ['gh-list-row', 'gh-members-list-item'],
-
-    active: false,
-
-    id: alias('member.id'),
-    email: alias('member.email'),
-    name: alias('member.name'),
-
-    displayName: or('member.name', 'member.email'),
-
-    memberSince: computed('member.createdAtUTC', function () {
-        return moment(this.member.createdAtUTC).from(moment());
-    }),
-    createdDate: computed('member.createdAtUTC', function () {
-        return moment(this.member.createdAtUTC).format('MMM DD, YYYY');
-    })
-});
+export default class GhMembersListItemComponent extends Component {
+    get memberSince() {
+        return moment(this.args.member.createdAtUTC).from(moment());
+    }
+}
