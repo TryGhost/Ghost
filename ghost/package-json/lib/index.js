b4afc727abaa73e9c58d3df38af24896cd9a07b0
Hannah Wolfe <erisds@gmail.com>
2020-03-19 14:58:59 +0000

    Remove Apps
    - Apps are marked as removed in 3.0, never officially launched and have been deprecated for at least 2 years.
    - We've slowly removed bits that got in our way or were insecure over time meaning they mostly didn't work
    - This cleans up the remainder of the logic
    - The tables should be cleaned up in a future major

--- ghost/package-json/lib/index.js
+++ ghost/package-json/lib/index.js
@@ -3,9 +3,7 @@
  *
  * Ghost has / is in the process of gaining support for several different types of sub-packages:
  * - Themes: have always been packages, but we're going to lean more heavily on npm & package.json in future
- * - Adapters: an early version of apps, replace fundamental pieces like storage, will become npm modules
- * - Apps: plugins that can be installed whilst Ghost is running & modify behaviour
- * - More?
+ * - Adapters: replace fundamental pieces like storage, will become npm modules
  *
  * These utils facilitate loading, reading, managing etc, packages from the file system.
  */
