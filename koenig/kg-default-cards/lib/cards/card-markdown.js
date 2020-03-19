de18264afbd73cf724b0b31510100d823b9af065
Kevin Ansfield <kevin@lookingsideways.co.uk>
2020-03-19 13:54:55 +0000

    Moved `create-card.js`
    no issue
    
    - prep for easier move to external repo

--- koenig/kg-default-cards/lib/cards/card-markdown.js
+++ koenig/kg-default-cards/lib/cards/card-markdown.js
@@ -1,7 +1,7 @@
 // this card is just an alias of the `markdown` card which is necessary because
 // our markdown-only editor was using the `card-markdown` card name
 const markdownCard = require('./_markdown');
-const createCard = require('../create-card');
+const createCard = require('./lib/create-card');
 
 const v1CompatMarkdownCard = markdownCard();
 v1CompatMarkdownCard.name = 'card-markdown';
