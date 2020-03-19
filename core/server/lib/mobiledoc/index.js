533502daa7649f3cc409c27e1fa4f8244e068bcb
Kevin Ansfield <kevin@lookingsideways.co.uk>
2020-03-19 12:18:54 +0000

    Moved `htmlToMobiledocConverter()` out of renderers
    no issue
    
    - prep for extraction of various Koenig repos
    - html->mobiledoc doesn't really fit into the "renderer" naming as it's more of a converter than a renderer and doesn't follow the same pattern

--- core/server/lib/mobiledoc/index.js
+++ core/server/lib/mobiledoc/index.js
@@ -1,8 +1,6 @@
-module.exports = {
-    activate() {
-        // needed by ghost
-    },
+const common = require('../common');
 
+module.exports = {
     get cards() {
         return require('./cards');
     },
@@ -13,5 +11,21 @@ module.exports = {
 
     get renderers() {
         return require('./renderers');
+    },
+
+    get htmlToMobiledocConverter() {
+        try {
+            return require('@tryghost/html-to-mobiledoc').toMobiledoc;
+        } catch (err) {
+            return () => {
+                throw new common.errors.InternalServerError({
+                    message: 'Unable to convert from source HTML to Mobiledoc',
+                    context: 'The html-to-mobiledoc package was not installed',
+                    help: 'Please review any errors from the install process by checking the Ghost logs',
+                    code: 'HTML_TO_MOBILEDOC_INSTALLATION',
+                    err: err
+                });
+            };
+        }
     }
 };
--- core/server/lib/mobiledoc/renderers/index.js
+++ core/server/lib/mobiledoc/renderers/index.js
@@ -1,5 +1,3 @@
-const common = require('../../common');
-
 module.exports = {
     get mobiledocHtmlRenderer() {
         return require('./mobiledoc-html-renderer');
@@ -7,21 +5,5 @@ module.exports = {
 
     get markdownHtmlRenderer() {
         return require('./markdown-html-renderer');
-    },
-
-    get htmlToMobiledocConverter() {
-        try {
-            return require('@tryghost/html-to-mobiledoc').toMobiledoc;
-        } catch (err) {
-            return () => {
-                throw new common.errors.InternalServerError({
-                    message: 'Unable to convert from source HTML to Mobiledoc',
-                    context: 'The html-to-mobiledoc package was not installed',
-                    help: 'Please review any errors from the install process by checking the Ghost logs',
-                    code: 'HTML_TO_MOBILEDOC_INSTALLATION',
-                    err: err
-                });
-            };
-        }
     }
 };
