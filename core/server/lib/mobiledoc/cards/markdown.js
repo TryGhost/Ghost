a120d02231d5b011255d63567dea0dc7d0698127
Kevin Ansfield <kevin@lookingsideways.co.uk>
2020-03-19 13:54:55 +0000

    Moved `create-card.js`
    no issue
    
    - prep for easier move to external repo

--- core/server/lib/mobiledoc/cards/markdown.js
+++ core/server/lib/mobiledoc/cards/markdown.js
@@ -1,5 +1,5 @@
 const markdownCard = require('./_markdown');
-const createCard = require('../create-card');
+const createCard = require('./lib/create-card');
 
 // uses the _markdown card definition function so that the card definition
 // can be re-used in aliased cards
