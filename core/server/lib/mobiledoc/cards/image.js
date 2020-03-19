TryGhost/Ghost

commit 9ddadc929373d5283690c3ccea9c78e59b0ff712

Moved `create-card.js`
no issue

- prep for easier move to external repo

core/server/lib/mobiledoc/cards/image.js
@@ -1,4 +1,4 @@
-const createCard = require('../create-card');
+const createCard = require('./lib/create-cardd');
 
 module.exports = createCard({
     name: 'image',

core/server/lib/mobiledoc/cards/bookmark.js
@@ -17,7 +17,7 @@
 </figure>
  */
 
-const createCard = require('../create-card');
+const createCard = require('./lib/create-card');
 
 function createElement(dom, elem, classNames = '', attributes = [], text) {
     let element = dom.createElement(elem);

core/server/lib/mobiledoc/cards/card-markdown.js
@@ -1,7 +1,7 @@
 // this card is just an alias of the `markdown` card which is necessary because
 // our markdown-only editor was using the `card-markdown` card name
 const markdownCard = require('./_markdown');
-const createCard = require('../create-card');
+const createCard = require('./lib/create-card');
 
 const v1CompatMarkdownCard = markdownCard();
 v1CompatMarkdownCard.name = 'card-markdown';

core/server/lib/mobiledoc/cards/code.js
@@ -1,4 +1,4 @@
-const createCard = require('../create-card');
+const createCard = require('./lib/create-card');
 
 module.exports = createCard({
     name: 'code',

core/server/lib/mobiledoc/cards/embed.js
@@ -1,4 +1,4 @@
-const createCard = require('../create-card');
+const createCard = require('./lib/create-card');
 
 module.exports = createCard({
     name: 'embed',

core/server/lib/mobiledoc/cards/gallery.js
@@ -15,7 +15,7 @@
  * </figure>
  */
 
-const createCard = require('../create-card');
+const createCard = require('./lib/create-card');
 
 const MAX_IMG_PER_ROW = 3;
 

core/server/lib/mobiledoc/cards/hr.js
@@ -1,4 +1,4 @@
-const createCard = require('../create-card');
+const createCard = require('./lib/create-card');
 
 module.exports = createCard({
     name: 'hr',

core/server/lib/mobiledoc/cards/html.js
@@ -1,4 +1,4 @@
-const createCard = require('../create-card');
+const createCard = require('./lib/create-card');
 
 module.exports = createCard({
     name: 'html',

commit 54f71b1a5579d78f81d36d98093699d7a7aea03c

Fixed typo
no issue

- typo in require introduced in https://github.com/TryGhost/Ghost/commit/9ddadc929373d5283690c3ccea9c78e59b0ff712

core/server/lib/mobiledoc/cards/image.js
@@ -1,4 +1,4 @@
-const createCard = require('./lib/create-cardd');
+const createCard = require('./lib/create-card');
 
 module.exports = createCard({
     name: 'image',