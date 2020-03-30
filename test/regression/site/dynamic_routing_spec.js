99acfaada11b57d8b6659193bbce9fde6a0c773d
Hannah Wolfe <erisds@gmail.com>
2020-03-30 16:01:27 +0100

    re-skip tests

--- test/regression/site/dynamic_routing_spec.js
+++ test/regression/site/dynamic_routing_spec.js
@@ -322,7 +322,7 @@ describe('Dynamic Routing', function () {
             });
         });
 
-        describe('Paged', function () {
+        describe.skip('Paged', function () {
             // Inserting more posts takes a bit longer
             this.timeout(20000);
 
