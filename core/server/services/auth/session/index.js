30efb6437b8110842952dc5ce577f880e5e7c1b8
Fabien O'Carroll <fabien@allou.is>
2020-03-31 17:59:26 +0300

    Instantiated and exported the new SessionService
    no-issue

--- core/server/services/auth/session/index.js
+++ core/server/services/auth/session/index.js
@@ -1,3 +1,86 @@
+const session = require('express-session');
+const constants = require('../../../lib/constants');
+const config = require('../../../config');
+const settingsCache = require('../../settings/cache');
+const models = require('../../../models');
+const urlUtils = require('../../../lib/url-utils');
+const url = require('url');
+
+const SessionService = require('./service');
+const SessionStore = require('./store');
+
+function getOriginOfRequest(req) {
+    const origin = req.get('origin');
+    const referrer = req.get('referrer');
+
+    if (!origin && !referrer) {
+        return null;
+    }
+
+    if (origin) {
+        return origin;
+    }
+
+    const {protocol, host} = url.parse(referrer);
+    if (protocol && host) {
+        return `${protocol}//${host}`;
+    }
+    return null;
+}
+
+async function getSession(req, res) {
+    if (req.session) {
+        return req.session;
+    }
+    return new Promise((resolve, reject) => {
+        expressSessionMiddleware(req, res, function (err) {
+            if (err) {
+                return reject(err);
+            }
+            resolve(req.session);
+        });
+    });
+}
+
+function findUserById({id}) {
+    return models.User.findOne({id});
+}
+
+let expressSessionMiddleware;
+function initExpressSessionMiddleware() {
+    if (!expressSessionMiddleware) {
+        expressSessionMiddleware = session({
+            store: new SessionStore(models.Session),
+            secret: settingsCache.get('session_secret'),
+            resave: false,
+            saveUninitialized: false,
+            name: 'ghost-admin-api-session',
+            cookie: {
+                maxAge: constants.SIX_MONTH_MS,
+                httpOnly: true,
+                path: urlUtils.getSubdir() + '/ghost',
+                sameSite: 'lax',
+                secure: urlUtils.isSSL(config.get('url'))
+            }
+        });
+    }
+}
+
+let sessionService;
+function initSessionService() {
+    if (!sessionService) {
+        if (!expressSessionMiddleware) {
+            initExpressSessionMiddleware();
+        }
+
+        sessionService = SessionService({
+            getOriginOfRequest,
+            getSession,
+            findUserById
+        });
+    }
+}
+
 module.exports = {
     // @TODO: expose files/units and not functions of units
     get createSession() {
@@ -10,5 +93,12 @@ module.exports = {
 
     get authenticate() {
         return require('./middleware').authenticate;
+    },
+
+    get service() {
+        if (!sessionService) {
+            initSessionService();
+        }
+        return sessionService;
     }
 };
