ee3fd5a18e3ff4f54a9545e4bba492065e9b6278
Rish <zrishabhgarg@gmail.com>
2020-04-02 13:25:10 +0530

    Updated Frame component to a simpler version
    no issue
    
    Makes frame component more efficient and easier to understand

--- ghost/portal/src/components/FrameComponent.js
+++ ghost/portal/src/components/FrameComponent.js
@@ -2,42 +2,20 @@ import React, { Component } from 'react'
 import { createPortal } from 'react-dom'
 
 export default class Frame extends Component {
-  constructor(props) {
-    super(props)
-
-    this.setContentRef = (node) => {
-        this.contentRef = ((!node || !node.contentWindow) && null) || (node && node.contentWindow.document.body);
-        if (this.contentRef) {
-            this.setState({
-                refUpdated: this.state.refUpdated + 1
-            })
-        }
+    componentDidMount() {
+      this.iframeHead = this.node.contentDocument.head;
+      this.iframeRoot = this.node.contentDocument.body;
+      this.iframeRoot.style.margin = '0px';
+      this.forceUpdate()
     }
 
-    this.state = {
-        refUpdated: 0
+    render() {
+      const { children, head, title="", style={}, ...rest } = this.props
+      return (
+        <iframe {...rest} ref={node => (this.node = node)} title={title} style={style} frameBorder="0">
+          {this.iframeHead && createPortal(head, this.iframeHead)}
+          {this.iframeRoot && createPortal(children, this.iframeRoot)}
+        </iframe>
+      )
     }
   }
-
-
-  renderFrameChildren() {
-    const { children } = this.props // eslint-disable-line
-      if (this.contentRef) {
-          return createPortal(
-            React.Children.only(children),
-            this.contentRef
-          );
-      }
-      return null;
-  }
-
-  render() {
-    const { children, title = "membersjs-frame", ...props } = this.props // eslint-disable-line
-    const style = this.props.style || {};
-    return (
-      <iframe title= {title} ref={this.setContentRef} style={style} frameBorder="0">
-          {this.renderFrameChildren()}
-      </iframe>
-    )
-  }
-}
