2ef931d5d224fb1754fdbd2e9ea3bb2691f10c81
Fabien O'Carroll <fabien@allou.is>
2020-04-05 18:52:48 +0200

    Refactored existing adapters to use adapter-manager
    no-issue
    
    This removes duplications and reduces maintentence going forward.

--- core/server/adapters/scheduling/index.js
+++ core/server/adapters/scheduling/index.js
@@ -6,18 +6,8 @@ const postScheduling = require('./post-scheduling');
  * We currently only support post-scheduling: publish posts/pages when scheduled.
  *
  * @param {Object} options
- *                 {
- *                      schedulerUrl: [String] Remote scheduler domain.
- *                      active:       [String] Name of the custom scheduler.
- *                      apiUrl:       [String] Target Ghost API url.
- *                      internalPath: [String] Folder path where to find the default scheduler.
- *                      contentPath:  [String] Folder path where to find custom schedulers.
- *                  }
- *
- * @TODO: Simplify the passed in options.
+ * @param {string} options.apiUrl - Target Ghost API url
  */
 exports.init = function init(options) {
-    options = options || {};
-
     return postScheduling.init(options);
 };
