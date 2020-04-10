4f1c95edb823116b9236550b725124d77181e1f4
Hannah Wolfe <erisds@gmail.com>
2020-04-10 16:30:21 +0100

    Removed service worker code
    - I don't think it will ever be appropriate to use a service worker in our widget context

--- ghost/portal/src/index.js
+++ ghost/portal/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 function initMembersJS(data) {
   ReactDOM.render(
@@ -19,9 +18,3 @@ function initMembersJS(data) {
 window.GhostMembers = {
   initMembersJS: initMembersJS
 }
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
