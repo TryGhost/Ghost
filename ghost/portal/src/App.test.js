909eb16cedb091b93c7c983a9b15e4d095aba443
Rish <zrishabhgarg@gmail.com>
2020-04-15 21:15:56 +0530

    Fixed basic top-level App test

--- ghost/portal/src/App.test.js
+++ ghost/portal/src/App.test.js
@@ -2,8 +2,18 @@ import React from 'react';
 import {render} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-    const {getByText} = render(<App />);
-    const linkElement = getByText(/learn react/i);
-    expect(linkElement).toBeInTheDocument();
+test('renders App', () => {
+    const data = {
+        site: {
+            siteUrl: '',
+            adminUrl: ''
+        }
+    };
+    const {container} = render(
+        <App data={data} />
+    );
+
+    // dashboard component should be rendered on root route
+    const element = container.querySelector('.App');
+    expect(element).toBeInTheDocument();
 });
