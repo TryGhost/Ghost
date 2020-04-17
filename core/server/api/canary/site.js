6a9186258e9e1037c42bee7cdd7620dec3640126
Rish <zrishabhgarg@gmail.com>
2020-04-17 15:00:31 +0530

    Updated public site endpoint with more data
    no issue
    
    This adds new public settings - description, logo, brand - and some public member settings - plans, allowSelfSignup to the open site endpoint which will be used by members.js for data initialization

--- core/server/api/canary/site.js
+++ core/server/api/canary/site.js
@@ -1,6 +1,7 @@
 const ghostVersion = require('../../lib/ghost-version');
 const settingsCache = require('../../services/settings/cache');
 const urlUtils = require('../../lib/url-utils');
+const membersService = require('../../services/members');
 
 const site = {
     docName: 'site',
@@ -10,7 +11,12 @@ const site = {
         query() {
             return {
                 title: settingsCache.get('title'),
+                description: settingsCache.get('description'),
+                logo: settingsCache.get('logo'),
+                brand: settingsCache.get('brand'),
                 url: urlUtils.urlFor('home', true),
+                plans: membersService.config.getPublicPlans(),
+                allowSelfSignup: membersService.config.getPublicPlans(),
                 version: ghostVersion.safe
             };
         }
