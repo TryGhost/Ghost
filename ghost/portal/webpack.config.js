89a1cab4c1141e300bfb22e22b6b50d769b0660a
Rish <zrishabhgarg@gmail.com>
2020-04-20 11:57:08 +0530

    Fixed lint for webpack config
    no issue

--- ghost/portal/webpack.config.js
+++ ghost/portal/webpack.config.js
@@ -1,20 +1,20 @@
-const path = require("path")
-const glob = require("glob")
+const path = require('path');
+const glob = require('glob');
 
 module.exports = {
-  entry: {
-    "bundle.js": glob.sync("build/static/?(js|css)/main.*.?(js|css)").map(f => path.resolve(__dirname, f)),
-  },
-  output: {
-    filename: "build/static/js/bundle.min.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  plugins: [],
-}
+    entry: {
+        'bundle.js': glob.sync('build/static/?(js|css)/main.*.?(js|css)').map(f => path.resolve(__dirname, f))
+    },
+    output: {
+        filename: 'build/static/js/bundle.min.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: []
+};
