e59e8186bdef308d9b7b69ae1c34667ce7400ac6
Rish <zrishabhgarg@gmail.com>
2020-04-20 22:12:08 +0530

    Cleaned up script intiailization data
    refs https://github.com/TryGhost/members.js/issues/6
    
    Removes all data initialization setup except `adminUrl` as we now fetch everything directly from API

--- ghost/portal/src/index.js
+++ ghost/portal/src/index.js
@@ -16,17 +16,8 @@ window.GhostMembers = {
     initMembersJS: initMembersJS
 };
 
-// This will automatically load for local if an .env.local file is present
+// This will automatically load for local if an .env.development.local file is present
 if (process.env.REACT_APP_ADMIN_URL) {
-    let site = {
-        adminUrl: process.env.REACT_APP_ADMIN_URL,
-        siteUrl: process.env.REACT_APP_SITE_URL || process.env.REACT_APP_ADMIN_URL,
-        plans: {
-            monthly: process.env.REACT_APP_SITE_MONTHLY_PLAN || '10',
-            yearly: process.env.REACT_APP_SITE_YEARLY_PLAN || '99',
-            currency: process.env.REACT_APP_SITE_PLAN_CURRENCY || 'USD',
-            currencySymbol: process.env.REACT_APP_SITE_PLAN_CURRENCY_SYMBOL || '$'
-        }
-    };
-    initMembersJS({site});
+    const adminUrl = process.env.REACT_APP_ADMIN_URL;
+    initMembersJS({adminUrl});
 }
