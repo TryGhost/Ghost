ae46fb7d29e49a8b36fd0f359a5947e25b6af4f3
Hannah Wolfe <erisds@gmail.com>
2020-04-21 19:01:49 +0100

    Removed unused middleware code
    - removed empty theme-handler_spec.js file
    - removed unused export of uncapitalise
    - removed unused export of utils
    - remove unused blog-icon and profile-image validation code (now lives in core/server/api/[version]/utils/validators/input)

--- core/server/web/shared/middlewares/validation/index.js
+++ core/server/web/shared/middlewares/validation/index.js
@@ -1,13 +1,5 @@
 module.exports = {
     get upload() {
         return require('./upload');
-    },
-
-    get blogIcon() {
-        return require('./blog-icon');
-    },
-
-    get profileImage() {
-        return require('./profile-image');
     }
 };
