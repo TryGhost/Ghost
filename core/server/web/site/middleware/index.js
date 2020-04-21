37c3ffd83d060093ce30678a709776e0b57ecffd
Hannah Wolfe <erisds@gmail.com>
2020-04-21 15:42:22 +0100

    Moved handle-image-sizes mw into site app
    - Moved handle-image-sizes from shared to site as it is not shared
    - This file is only used in one place, this updates the code structure to reflect this
    - This is one of many similar changes needed to make it easier to refactor to the existing setup

--- core/server/web/site/middleware/index.js
+++ core/server/web/site/middleware/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     adminRedirects: require('./admin-redirects'),
+    handleImageSizes: require('./handle-image-sizes'),
     serveFavicon: require('./serve-favicon'),
     staticTheme: require('./static-theme')
 };
