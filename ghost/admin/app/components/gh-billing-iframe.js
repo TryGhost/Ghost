9a822c499293c951f273fae3be746268b352a501
Nazar Gargol <nazargargol@gmail.com>
2020-04-21 18:54:29 +1200

    Added billing "update" CTA button
    no issue
    
    - Added billing update button to navigation menu. Ghost-Admin communicates with billing iframe and displays this button based on the plan data that iframe returns
    - Ghost-Admin communicates with an iframe using same mechanism as with token exchange - throu `window.postMessage` API

--- ghost/admin/app/components/gh-billing-iframe.js
+++ ghost/admin/app/components/gh-billing-iframe.js
@@ -1,13 +1,11 @@
 import Component from '@ember/component';
-import {computed} from '@ember/object';
 import {inject as service} from '@ember/service';
 
 export default Component.extend({
+    billing: service(),
     config: service(),
     ghostPaths: service(),
     ajax: service(),
-
-    billingEndpoint: computed.reads('config.billingUrl'),
 
     didRender() {
         let iframe = this.element.querySelector('#billing-frame');
