de9864db300825f37ecca1de7bfa3a9170e34fa8
Hannah Wolfe <erisds@gmail.com>
2020-04-22 07:37:02 +0100

    Moved upload validation mw into api app
    - Moved upload validation mw from shared to api as it is not shared (except within the API)
    - Co-located the code with the upload middleware, as it's small and gives us a nice API of .upload.single and .upload.validation
    - This file is only used in one part of the app, this updates the code structure to reflect this
    - This is one of many similar changes needed to make it easier to refactor to the existing setup

--- test/unit/web/api/middleware/upload_spec.js
+++ test/unit/web/api/middleware/upload_spec.js
@@ -1,30 +1,30 @@
 const should = require('should');
-const uploadValidation = require('../../../../../core/server/web/shared/middlewares/validation/upload')._test;
+const validation = require('../../../../../core/server/web/api/middleware/upload')._test;
 
 describe('web utils', function () {
     describe('checkFileExists', function () {
         it('should return true if file exists in input', function () {
-            uploadValidation.checkFileExists({mimetype: 'file', path: 'path'}).should.be.true();
+            validation.checkFileExists({mimetype: 'file', path: 'path'}).should.be.true();
         });
 
         it('should return false if file does not exist in input', function () {
-            uploadValidation.checkFileExists({}).should.be.false();
+            validation.checkFileExists({}).should.be.false();
         });
 
         it('should return false if file is incorrectly structured', function () {
-            uploadValidation.checkFileExists({type: 'file'}).should.be.false();
+            validation.checkFileExists({type: 'file'}).should.be.false();
         });
     });
 
     describe('checkFileIsValid', function () {
         it('returns true if file has valid extension and type', function () {
-            uploadValidation.checkFileIsValid({
+            validation.checkFileIsValid({
                 name: 'test.txt',
                 mimetype: 'text',
                 ext: '.txt'
             }, ['text'], ['.txt']).should.be.true();
 
-            uploadValidation.checkFileIsValid({
+            validation.checkFileIsValid({
                 name: 'test.jpg',
                 mimetype: 'jpeg',
                 ext: '.jpg'
@@ -32,12 +32,12 @@ describe('web utils', function () {
         });
 
         it('returns false if file has invalid extension', function () {
-            uploadValidation.checkFileIsValid({name: 'test.txt', mimetype: 'text'}, ['text'], ['.tar']).should.be.false();
-            uploadValidation.checkFileIsValid({name: 'test', mimetype: 'text'}, ['text'], ['.txt']).should.be.false();
+            validation.checkFileIsValid({name: 'test.txt', mimetype: 'text'}, ['text'], ['.tar']).should.be.false();
+            validation.checkFileIsValid({name: 'test', mimetype: 'text'}, ['text'], ['.txt']).should.be.false();
         });
 
         it('returns false if file has invalid type', function () {
-            uploadValidation.checkFileIsValid({name: 'test.txt', mimetype: 'text'}, ['archive'], ['.txt']).should.be.false();
+            validation.checkFileIsValid({name: 'test.txt', mimetype: 'text'}, ['archive'], ['.txt']).should.be.false();
         });
     });
 });
