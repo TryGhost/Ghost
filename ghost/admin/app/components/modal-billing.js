d691b15f820de7ed5e5d8b788827b3dab12621b9
Naz <nazargargol@gmail.com>
2020-04-22 15:35:56 +1200

    Added handling for /billing route with query param passing to BMA (#1551)
    no issue
    
    - This change allows to open BMA popup using external link and pass in information using query parameters. Main use case being redirects from external sites

--- ghost/admin/app/components/modal-billing.js
+++ ghost/admin/app/components/modal-billing.js
@@ -1,4 +1,12 @@
 import ModalComponent from 'ghost-admin/components/modal-base';
+import {inject as service} from '@ember/service';
 
 export default ModalComponent.extend({
+    billing: service(),
+
+    actions: {
+        closeModal() {
+            this.billing.closeBillingWindow();
+        }
+    }
 });
