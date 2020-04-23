0a5e0648c88dba42b1805b80f4f45bf7f16e3360
Rish <zrishabhgarg@gmail.com>
2020-04-23 12:43:28 +0530

    Refactored fixutres data for site and member
    refs https://github.com/TryGhost/members.js/issues/10
    
    Refactored to include free member data and added dummy data for testing

--- ghost/portal/src/test/fixtures/data.js
+++ ghost/portal/src/test/fixtures/data.js
@@ -1,20 +1,29 @@
 export const site = {
     title: 'Ghost Site',
-    description: 'Thoughts, stories and ideas',
-    logo: '',
-    siteUrl: window.location.origin,
-    adminUrl: window.location.origin + '/ghost',
+    description: 'Thoughts, stories and ideas.',
+    logo: 'https://pbs.twimg.com/profile_images/1111773508231667713/mf2N0uqc_400x400.png',
+    brand: {
+        primaryColor: '#abcdef'
+    },
+    url: 'http://localhost:2368/',
     plans: {
-        monthly: '10',
-        yearly: '99',
+        monthly: 12,
+        yearly: 110,
         currency: 'USD',
-        currencySymbol: '$'
-    }
+        currency_symbol: '$'
+    },
+    allowSelfSignup: true,
+    version: '3.13'
 };
 
 export const member = {
-    email: 'member@example.com',
-    isPaid: false,
-    avatarImage: '',
-    subscriptions: []
+    free: {
+        uuid: 'd7d3b1a0-90f4-4b93-a51f-76b56213b535',
+        email: 'member@example.com',
+        name: 'Example Member',
+        firstname: null,
+        avatar_image: '',
+        subscriptions: [],
+        paid: false
+    }
 };
