be10039f76679daf323f2ec801394c86857cd23f
Hannah Wolfe <erisds@gmail.com>
2020-04-27 16:54:31 +0100

    Renamed app related files+variables for clarity
    - renamed the parentApp in index.js to ghostApp, to reduce confusion with the layer that is named parentApp
    - renamed the adminApp inside of parentApp to backendApp to reflect the fact it's both admin+api
    - renamed a bunch more variables there to be backend, rather than admin
    - renamed the api index.js file to app.js and created a new index which is an actual index

---
+++ core/server/web/api/app.js
@@ -0,0 +1,31 @@
+const debug = require('ghost-ignition').debug('web:api:default:app');
+const express = require('express');
+const urlUtils = require('../../lib/url-utils');
+const errorHandler = require('../shared/middlewares/error-handler');
+const sentry = require('../../sentry');
+
+module.exports = function setupApiApp() {
+    debug('Parent API setup start');
+    const apiApp = express();
+    apiApp.use(sentry.requestHandler);
+
+    // Mount different API versions
+    apiApp.use(urlUtils.getVersionPath({version: 'v2', type: 'content'}), require('./v2/content/app')());
+    apiApp.use(urlUtils.getVersionPath({version: 'v2', type: 'admin'}), require('./v2/admin/app')());
+
+    apiApp.use(urlUtils.getVersionPath({version: 'v3', type: 'content'}), require('./canary/content/app')());
+    apiApp.use(urlUtils.getVersionPath({version: 'v3', type: 'admin'}), require('./canary/admin/app')());
+    apiApp.use(urlUtils.getVersionPath({version: 'v3', type: 'members'}), require('./canary/members/app')());
+
+    apiApp.use(urlUtils.getVersionPath({version: 'canary', type: 'content'}), require('./canary/content/app')());
+    apiApp.use(urlUtils.getVersionPath({version: 'canary', type: 'admin'}), require('./canary/admin/app')());
+    apiApp.use(urlUtils.getVersionPath({version: 'canary', type: 'members'}), require('./canary/members/app')());
+
+    // Error handling for requests to non-existent API versions
+    apiApp.use(sentry.errorHandler);
+    apiApp.use(errorHandler.resourceNotFound);
+    apiApp.use(errorHandler.handleJSONResponse);
+
+    debug('Parent API setup end');
+    return apiApp;
+};
