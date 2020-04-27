d3a9832374d532631bc5cd3ab05e52dd100373ca
Hannah Wolfe <erisds@gmail.com>
2020-04-27 16:54:31 +0100

    Renamed app related files+variables for clarity
    - renamed the parentApp in index.js to ghostApp, to reduce confusion with the layer that is named parentApp
    - renamed the adminApp inside of parentApp to backendApp to reflect the fact it's both admin+api
    - renamed a bunch more variables there to be backend, rather than admin
    - renamed the api index.js file to app.js and created a new index which is an actual index

--- index.js
+++ index.js
@@ -14,19 +14,19 @@ debug('Required ghost');
 const express = require('express');
 const common = require('./core/server/lib/common');
 const urlService = require('./core/frontend/services/url');
-const parentApp = express();
+const ghostApp = express();
 
-parentApp.use(sentry.requestHandler);
+ghostApp.use(sentry.requestHandler);
 
 debug('Initialising Ghost');
 
 ghost().then(function (ghostServer) {
     // Mount our Ghost instance on our desired subdirectory path if it exists.
-    parentApp.use(urlService.utils.getSubdir(), ghostServer.rootApp);
+    ghostApp.use(urlService.utils.getSubdir(), ghostServer.rootApp);
 
     debug('Starting Ghost');
     // Let Ghost handle starting our server instance.
-    return ghostServer.start(parentApp)
+    return ghostServer.start(ghostApp)
         .then(function afterStart() {
             common.logging.info('Ghost boot', (Date.now() - startTime) / 1000 + 's');
         });
