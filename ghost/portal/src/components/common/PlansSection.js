9aefe26ba7d6cb2ab07fea0e7dd66248f816df3b
Rish <zrishabhgarg@gmail.com>
2020-04-30 00:24:31 +0530

    Fixed lint

--- ghost/portal/src/components/common/PlansSection.js
+++ ghost/portal/src/components/common/PlansSection.js
@@ -1,32 +1,5 @@
 import React from 'react';
 
-const Styles = ({style = {}}) => {
-    return {
-        input: {
-            display: 'block',
-            padding: '0 .6em',
-            width: '100%',
-            height: '44px',
-            outline: '0',
-            border: '1px solid #c5d2d9',
-            color: 'inherit',
-            textDecoration: 'none',
-            background: '#fff',
-            borderRadius: '9px',
-            fontSize: '14px',
-            marginBottom: '12px',
-            boxSizing: 'border-box',
-            ...(style.input || {}) // Override any custom style
-        },
-        label: {
-            marginBottom: '3px',
-            fontSize: '12px',
-            fontWeight: '700',
-            ...(style.label || {}) // Override any custom style
-        }
-    };
-};
-
 function Checkbox({name, onPlanSelect, isChecked}) {
     const style = {
         width: '20px',
--- ghost/portal/src/components/common/PlansSection.test.js
+++ ghost/portal/src/components/common/PlansSection.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, fireEvent} from '@testing-library/react';
+import {render} from '@testing-library/react';
 import PlansSection from './PlansSection';
 
 const setup = (overrides = {}) => {
