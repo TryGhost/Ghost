712ac5cd74179200913afe9bf11df8f02e485a43
Rish <zrishabhgarg@gmail.com>
2020-04-30 11:49:18 +0530

    Updated tests for magiclink page
    no issue
    
    - Add test for close button firing correct action(closePopup) on magic link page
    - Use common test utils setup

--- ghost/portal/src/components/pages/MagicLinkPage.test.js
+++ ghost/portal/src/components/pages/MagicLinkPage.test.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, fireEvent} from 'test-utils';
 import MagicLinkPage from './MagicLinkPage';
+
+const setup = (overrides) => {
+    const {mockOnActionFn, ...utils} = render(
+        <MagicLinkPage />
+    );
+    const inboxText = utils.getByText(/check your inbox!/i);
+    const closeBtn = utils.queryByRole('button', {name: 'Close'});
+    return {
+        inboxText,
+        closeBtn,
+        mockOnActionFn,
+        ...utils
+    };
+};
 
 describe('MagicLinkPage', () => {
     test('renders', () => {
-        const {getByText} = render(
-            <MagicLinkPage />
-        );
-
-        const inboxText = getByText(/check your inbox!/i);
+        const {inboxText, closeBtn} = setup();
 
         expect(inboxText).toBeInTheDocument();
+        expect(closeBtn).toBeInTheDocument();
+    });
+
+    test('calls on action with close popup', () => {
+        const {closeBtn, mockOnActionFn} = setup();
+
+        fireEvent.click(closeBtn);
+        expect(mockOnActionFn).toHaveBeenCalledWith('closePopup');
     });
 });
