bc6e0ace33c19e9eed96a29a8f5a3b0d293753ba
Hannah Wolfe <erisds@gmail.com>
2020-04-30 17:29:51 +0100

    Moved members API out of backend

--- core/server/web/api/app.js
+++ core/server/web/api/app.js
@@ -13,11 +13,9 @@ module.exports = function setupApiApp() {
 
     apiApp.use(urlUtils.getVersionPath({version: 'v3', type: 'content'}), require('./canary/content/app')());
     apiApp.use(urlUtils.getVersionPath({version: 'v3', type: 'admin'}), require('./canary/admin/app')());
-    apiApp.use(urlUtils.getVersionPath({version: 'v3', type: 'members'}), require('./canary/members/app')());
 
     apiApp.use(urlUtils.getVersionPath({version: 'canary', type: 'content'}), require('./canary/content/app')());
     apiApp.use(urlUtils.getVersionPath({version: 'canary', type: 'admin'}), require('./canary/admin/app')());
-    apiApp.use(urlUtils.getVersionPath({version: 'canary', type: 'members'}), require('./canary/members/app')());
 
     // Error handling for requests to non-existent API versions
     apiApp.use(errorHandler.resourceNotFound);
