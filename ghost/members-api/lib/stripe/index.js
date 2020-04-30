d6d187776d0631d2c4589286a9e78e59fb150861
Rish <zrishabhgarg@gmail.com>
2020-04-30 15:56:04 +0530

    Added ability to prefill customer email for anonymous checkouts
    refs https://github.com/TryGhost/members.js/issues/10
    
    - Allows passing an additional `customerEmail` value to our checkout creation API
    - This value is used to pass `customer_email` option to stripe's checkout session - https://stripe.com/docs/api/checkout/sessions/create#create_checkout_session-customer_email.
    
    The `customer_email` allows pre-filling the customer's email field in case of an anonymous checkout as customer doesn't exist already, and also ensures the stripe subscription is created with same email address as given by user during signup flow.

--- ghost/members-api/lib/stripe/index.js
+++ ghost/members-api/lib/stripe/index.js
@@ -113,11 +113,13 @@ module.exports = class StripePaymentProcessor {
             customer = null;
         }
         const plan = this._plans.find(plan => plan.nickname === planName);
+        const customerEmail = (!customer && options.customerEmail) ? options.customerEmail : undefined;
         const session = await this._stripe.checkout.sessions.create({
             payment_method_types: ['card'],
             success_url: options.successUrl || this._checkoutSuccessUrl,
             cancel_url: options.cancelUrl || this._checkoutCancelUrl,
             customer: customer ? customer.id : undefined,
+            customer_email: customerEmail,
             subscription_data: {
                 items: [{
                     plan: plan.id
