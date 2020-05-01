45c685872a485a0dd8948d9ef1de626485779b1f
Rish <zrishabhgarg@gmail.com>
2020-05-01 22:23:37 +0530

    Added fixture for paid member
    no issue
    
    - Adds fixture for paid member for local development

--- ghost/portal/src/utils/fixtures.js
+++ ghost/portal/src/utils/fixtures.js
@@ -25,5 +25,34 @@ export const member = {
         avatar_image: '',
         subscriptions: [],
         paid: false
+    },
+    paid: {
+        uuid: '7dcc8939-3be0-4ac8-a363-96d19f909de6',
+        email: 'paidmember+@example.com',
+        name: 'Paid Member',
+        firstname: 'Member',
+        avatar_image: 'https://gravatar.com/avatar/eb0ef27b5faa9528c900170cba4c11dc?s=250&d=blank',
+        subscriptions: [{
+            id: 'sub_HCLyRzHcGciDWJ',
+            customer: {
+                id: 'cus_HCLy4Y3eLt50YJ',
+                name: null,
+                email: 'paidmember+@example.com'
+            },
+            plan: {
+                id: 'fd43b943666b97640188afb382cca39479de30f799985679dd7a71ad2925ac6c',
+                nickname: 'Monthly',
+                interval: 'month',
+                amount: 1200,
+                currency: 'USD',
+                currency_symbol: '$'
+            },
+            status: 'active',
+            start_date: '2019-05-01T11:42:40.000Z',
+            default_payment_card_last4: '4242',
+            cancel_at_period_end: false,
+            current_period_end: '2019-06-01T11:42:40.000Z'
+        }],
+        paid: true
     }
 };
