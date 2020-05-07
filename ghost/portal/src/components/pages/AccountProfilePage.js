32559b3ed0b9afe705ba71af5fde0740f6d79ab0
Rish <zrishabhgarg@gmail.com>
2020-05-07 21:04:31 +0530

    Added free account home to use account profile
    refs https://github.com/TryGhost/members.js/issues/20
    
    - Clicking settings on free member's account home now takes them to profile page

--- ghost/portal/src/components/pages/AccountProfilePage.js
+++ ghost/portal/src/components/pages/AccountProfilePage.js
@@ -33,7 +33,7 @@ export default class AccountProfilePage extends React.Component {
 
     renderAccountFooter() {
         return (
-            <div style={{display: 'flex', padding: '0 24px', marginTop: '42px', color: this.context.brandColor, fontWeight: 'bold', fontSize: '13px'}}>
+            <div style={{display: 'flex', padding: '0 24px', marginTop: '42px', color: this.context.brandColor, fontWeight: 'bold', fontSize: '13px', alignItems: 'center'}}>
                 <div style={{cursor: 'pointer', color: 'red'}} role='button'> Delete Account </div>
                 <div style={{display: 'flex', flexGrow: 1, justifyContent: 'flex-end'}}>
                     <ActionButton
