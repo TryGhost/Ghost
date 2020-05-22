5d59670ac35851a89ed8081cbcaaf08422ad55e0
Nazar Gargol <nazargargol@gmail.com>
2020-05-22 14:44:37 +1200

    Fixed browser URL syncronization with embeded iframe state
    no issue
    
    - Opted in to use explicit `hisotry.replaceState` and setting iframe's `src` using assignment instead of tracking it through computed property. This allows for tighter control over when iframe's history is updated which was causing problems when `src` was bound to computed property
    - Added billing page metadata. This way browser history records appear with nicer signature
    - Removed "update button" iframe and rewrote "global iframe" to not use modals. This allows to have single iframe on a page, which simplifies `postMessage` communication and preserve history inside iframe to be able to navigate it after closure
    - Added route change handler responding to BMA app route changes. Allows to sync browser URL visible to the user with active route in BMA iframe. The sync is based on `hisory.replaceState` method that makes sure singular history records are kept in the browser history
    - Added nested wildcard billing route. This is meant to catch all the nested routes inside of BMA iframe

--- ghost/admin/app/routes/billing.js
+++ ghost/admin/app/routes/billing.js
@@ -8,15 +8,41 @@ export default Route.extend({
         action: {refreshModel: true}
     },
 
+    beforeModel(transition) {
+        this.billing.set('previousTransition', transition);
+    },
+
     model(params) {
         if (params.action) {
             this.billing.set('action', params.action);
         }
 
-        this.billing.set('billingWindowOpen', true);
+        this.billing.setBillingWindowOpen(true);
+    },
 
-        // NOTE: if this route is ever triggered it was opened through external link because
-        //       the route has no underlying templates to render we redirect to root route
-        this.transitionTo('/');
+    actions: {
+        willTransition(transition) {
+            let isBillingTransition = false;
+
+            if (transition) {
+                let destinationUrl = (typeof transition.to === 'string')
+                    ? transition.to
+                    : (transition.intent
+                        ? transition.intent.url
+                        : '');
+
+                if (destinationUrl.includes('/billing')) {
+                    isBillingTransition = true;
+                }
+            }
+
+            this.billing.setBillingWindowOpen(isBillingTransition);
+        }
+    },
+
+    buildRouteInfoMetadata() {
+        return {
+            titleToken: 'Billing'
+        };
     }
 });
--- ghost/admin/app/services/billing.js
+++ ghost/admin/app/services/billing.js
@@ -1,31 +1,91 @@
 import Service from '@ember/service';
-import {computed} from '@ember/object';
 import {inject as service} from '@ember/service';
 
 export default Service.extend({
+    router: service(),
     config: service(),
     ghostPaths: service(),
 
+    billingRouteRoot: '#/billing',
     billingWindowOpen: false,
-    upgrade: false,
-    action: null,
+    subscription: null,
+    previousRoute: null,
 
-    closeBillingWindow() {
-        this.set('billingWindowOpen', false);
-        this.set('action', null);
+    init() {
+        this._super(...arguments);
+
+        if (this.config.get('billingUrl')) {
+            window.addEventListener('message', (event) => {
+                if (event && event.data && event.data.route) {
+                    this.handleRouteChangeInIframe(event.data.route);
+                }
+            });
+        }
     },
 
-    endpoint: computed('config.billingUrl', 'billingWindowOpen', 'action', function () {
+    handleRouteChangeInIframe(destinationRoute) {
+        if (this.get('billingWindowOpen')) {
+            let billingRoute = this.get('billingRouteRoot');
+
+            if (destinationRoute !== '/') {
+                billingRoute += destinationRoute;
+            }
+
+            if (window.location.hash !== billingRoute) {
+                window.history.replaceState(window.history.state, '', billingRoute);
+            }
+        }
+    },
+
+    getIframeURL() {
         let url = this.config.get('billingUrl');
 
-        if (this.get('upgrade')) {
-            url = this.ghostPaths.url.join(url, 'plans');
-        }
+        if (window.location.hash && window.location.hash.includes(this.get('billingRouteRoot'))) {
+            let destinationRoute = window.location.hash.replace(this.get('billingRouteRoot'), '');
 
-        if (this.get('action')) {
-            url += `?action=${this.get('action')}`;
+            if (destinationRoute) {
+                url += destinationRoute;
+            }
         }
 
         return url;
-    })
+    },
+
+    // Controls billing window modal visibility and sync of the URL visible in browser
+    // and the URL opened on the iframe. It is responsible to non user triggered iframe opening,
+    // for example: by entering "/billing" route in the URL or using history navigation (back and forward)
+    setBillingWindowOpen(value) {
+        let billingIframe = this.getBillingIframe();
+
+        if (billingIframe && value) {
+            billingIframe.contentWindow.location.replace(this.getIframeURL());
+        }
+
+        this.set('billingWindowOpen', value);
+    },
+
+    // Controls navigation to billing window modal which is triggered from the application UI.
+    // For example: pressing "View Billing" link in navigation menu. It's main side effect is
+    // remembering the route from which the action has been triggered - "previousRoute" so it
+    // could be reused when closing billing window
+    openBillingWindow(currentRoute, childRoute) {
+        this.set('previousRoute', currentRoute);
+
+        // Ensures correct "getIframeURL" calculation when syncing iframe location
+        // in setBillingWindowOpen
+        window.location.hash = childRoute || '/billing';
+
+        this.router.transitionTo(childRoute || '/billing');
+    },
+
+    closeBillingWindow() {
+        this.set('billingWindowOpen', false);
+
+        let transitionRoute = this.get('previousRoute') || '/';
+        this.router.transitionTo(transitionRoute);
+    },
+
+    getBillingIframe() {
+        return document.getElementById('billing-frame');
+    }
 });
