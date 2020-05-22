ae8fd4d5dcd6bfe6f5c50cbc0caf9de081b8acfc
Vikas Potluri <vikaspotluri123.github@gmail.com>
2020-05-21 22:39:41 -0500

    refactored remaining `core/server/api/v2` to destructure common imports

--- core/server/api/v2/pages-public.js
+++ core/server/api/v2/pages-public.js
@@ -1,4 +1,5 @@
-const common = require('../../lib/common');
+const {i18n} = require('../../lib/common');
+const errors = require('@tryghost/errors');
 const models = require('../../models');
 const ALLOWED_INCLUDES = ['tags', 'authors'];
 
@@ -61,8 +62,8 @@ module.exports = {
             return models.Post.findOne(frame.data, frame.options)
                 .then((model) => {
                     if (!model) {
-                        throw new common.errors.NotFoundError({
-                            message: common.i18n.t('errors.api.pages.pageNotFound')
+                        throw new errors.NotFoundError({
+                            message: i18n.t('errors.api.pages.pageNotFound')
                         });
                     }
 
