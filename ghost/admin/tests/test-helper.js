2c0b1ca809162af57b754b40788a44908f19e94d
Kevin Ansfield <kevin@lookingsideways.co.uk>
2020-06-08 08:37:14 +0100

    Removed ember-cli-chai in favour of direct ember-auto-import imports
    no issue
    
    - removes some deprecated babel plugins from our dependencies

--- ghost/admin/tests/test-helper.js
+++ ghost/admin/tests/test-helper.js
@@ -4,6 +4,10 @@ import registerWaiter from 'ember-raf-scheduler/test-support/register-waiter';
 import start from 'ember-exam/test-support/start';
 import {setApplication} from '@ember/test-helpers';
 
+import chai from 'chai';
+import chaiDom from 'chai-dom';
+chai.use(chaiDom);
+
 setApplication(Application.create(config.APP));
 
 registerWaiter();
