300f478598da1ca005f132d8b0c75614c568ba50
Nazar Gargol <nazargargol@gmail.com>
2020-06-12 00:46:04 +1200

    Fixed linting error for using this.get() in octane syntax
    no issue
    
    - Switched to getter as this.get() is not available in native classes

--- ghost/admin/app/services/members-utils.js
+++ ghost/admin/app/services/members-utils.js
@@ -4,8 +4,8 @@ import {inject as service} from '@ember/service';
 export default class MembersUtilsService extends Service {
     @service settings;
 
-    isStripeEnabled() {
-        let membersSubscriptionSettings = this.settings.parseSubscriptionSettings(this.get('settings.membersSubscriptionSettings'));
+    get isStripeEnabled() {
+        let membersSubscriptionSettings = this.settings.parseSubscriptionSettings(this.settings.get('membersSubscriptionSettings'));
         let stripeEnabled = membersSubscriptionSettings && !!(membersSubscriptionSettings.paymentProcessors[0].config.secret_token) && !!(membersSubscriptionSettings.paymentProcessors[0].config.public_token);
         return stripeEnabled;
     }
