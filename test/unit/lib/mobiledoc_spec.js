5a39ef709188abe9b05071f02b713d00ad66920b
Kevin Ansfield <kevin@lookingsideways.co.uk>
2020-06-11 13:27:56 +0100

    ✨ Added responsive-image `srcset` attribute to image and gallery card output
    no issue
    
    - adds a set of hardcoded "content image sizes" to the base config
    - adjusts `handle-image-sizes` middleware to always allow the hardcoded content image sizes to be genreated
    - updates `@tryghost/kg-card-factory` to allow passthrough of options to card renderers
    - updates `@tryghost/kg-default-cards` to add `srcset` output for image and gallery cards

--- test/unit/lib/mobiledoc_spec.js
+++ test/unit/lib/mobiledoc_spec.js
@@ -36,8 +36,9 @@ describe('lib/mobiledoc', function () {
                     }],
                     ['gallery', {
                         images: [{
+                            row: 0,
                             fileName: 'test.png',
-                            src: '/test.png',
+                            src: '/content/images/test.png',
                             width: 1000,
                             height: 500
                         }]
@@ -67,7 +68,7 @@ describe('lib/mobiledoc', function () {
             };
 
             mobiledocLib.mobiledocHtmlRenderer.render(mobiledoc)
-                .should.eql('<p>One<br>Two</p><!--kg-card-begin: markdown--><h1 id="markdowncard">Markdown card</h1>\n<p>Some markdown</p>\n<!--kg-card-end: markdown--><p>Three</p><hr><figure class="kg-card kg-image-card kg-width-wide kg-card-hascaption"><img src="/content/images/2018/04/NatGeo06.jpg" class="kg-image"><figcaption>Birdies</figcaption></figure><p>Four</p><!--kg-card-begin: html--><h2>HTML card</h2>\n<div><p>Some HTML</p></div><!--kg-card-end: html--><figure class="kg-card kg-embed-card"><h2>Embed card</h2></figure><figure class="kg-card kg-gallery-card kg-width-wide"><div class="kg-gallery-container"></div></figure>');
+                .should.eql('<p>One<br>Two</p><!--kg-card-begin: markdown--><h1 id="markdowncard">Markdown card</h1>\n<p>Some markdown</p>\n<!--kg-card-end: markdown--><p>Three</p><hr><figure class="kg-card kg-image-card kg-width-wide kg-card-hascaption"><img src="/content/images/2018/04/NatGeo06.jpg" class="kg-image" srcset="/content/images/size/w600/2018/04/NatGeo06.jpg 600w, /content/images/size/w1000/2018/04/NatGeo06.jpg 1000w, /content/images/size/w1600/2018/04/NatGeo06.jpg 1600w, /content/images/size/w2400/2018/04/NatGeo06.jpg 2400w"><figcaption>Birdies</figcaption></figure><p>Four</p><!--kg-card-begin: html--><h2>HTML card</h2>\n<div><p>Some HTML</p></div><!--kg-card-end: html--><figure class="kg-card kg-embed-card"><h2>Embed card</h2></figure><figure class="kg-card kg-gallery-card kg-width-wide"><div class="kg-gallery-container"><div class="kg-gallery-row"><div class="kg-gallery-image"><img src="/content/images/test.png" width="1000" height="500" srcset="/content/images/size/w600/test.png 600w, /content/images/size/w1000/test.png 1000w, /content/images/size/w1600/test.png 1600w, /content/images/size/w2400/test.png 2400w"></div></div></div></figure>');
         });
     });
 });
