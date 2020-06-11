f2bc57bbf5d47d58c02ac3e89670d3b74b11c545
Peter Zimon <zimo@ghost.org>
2020-06-11 15:15:44 +0200

    Stripe connect design (#1603)
    no issues
    
    Updated and refined Stripe Connect design in Labs/Members settings:
    - refined visuals and UI logic
    - updated copy
    - added switch for test mode
    - added error handling

--- ghost/admin/app/models/setting.js
+++ ghost/admin/app/models/setting.js
@@ -33,6 +33,7 @@ export default Model.extend(ValidationEngine, {
     defaultContentVisibility: attr('string'),
     membersSubscriptionSettings: attr('string'),
     stripeConnectIntegrationToken: attr('string'),
+    stripeConnectIntegration: attr('string'),
     metaTitle: attr('string'),
     metaDescription: attr('string'),
     twitterTitle: attr('string'),
