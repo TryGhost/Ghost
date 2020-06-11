f8a6e082d8e99b652bae5925a5aa26583479cb8b
Fabien O'Carroll <fabien@allou.is>
2020-06-11 16:45:45 +0200

    Updated isStripeEnabled to use stripeConnectIntegration
    no-issue
    
    This is now the central place for checking if stripe is configured for
    members and we want to make sure that the Stripe Connect correctly
    affects this value

--- ghost/admin/app/services/members-utils.js
+++ ghost/admin/app/services/members-utils.js
@@ -5,8 +5,19 @@ export default class MembersUtilsService extends Service {
     @service settings;
 
     get isStripeEnabled() {
+        let stripeConnectIntegration;
+
+        try {
+            stripeConnectIntegration = JSON.parse(this.settings.get('stripeConnectIntegration'));
+        } catch (err) {
+            stripeConnectIntegration = null;
+        }
+
+        let stripeConnectEnabled = stripeConnectIntegration && stripeConnectIntegration.account_id;
+
         let membersSubscriptionSettings = this.settings.parseSubscriptionSettings(this.settings.get('membersSubscriptionSettings'));
         let stripeEnabled = membersSubscriptionSettings && !!(membersSubscriptionSettings.paymentProcessors[0].config.secret_token) && !!(membersSubscriptionSettings.paymentProcessors[0].config.public_token);
-        return stripeEnabled;
+
+        return stripeEnabled || stripeConnectEnabled;
     }
 }
