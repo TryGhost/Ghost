80f268a7fed5f3241863bc2f0181befa179dba1f
Nazar Gargol <nazargargol@gmail.com>
2020-06-12 18:28:15 +1200

    Fixed isStripeEnabled usage in import validator tests
    refs https://github.com/TryGhost/Ghost-Admin/commit/5aff06907d5ee15ddf2b0d13700a2cfb5cbf7062
    
    - Reason for the fix same as in the referenced commit

--- ghost/admin/tests/integration/services/member-import-validator-test.js
+++ ghost/admin/tests/integration/services/member-import-validator-test.js
@@ -5,7 +5,7 @@ import {expect} from 'chai';
 import {setupTest} from 'ember-mocha';
 
 let MembersUtilsStub = Service.extend({
-    isStripeEnabled: () => (true)
+    isStripeEnabled: true
 });
 
 describe('Integration: Service: member-import-validator', function () {
@@ -39,7 +39,7 @@ describe('Integration: Service: member-import-validator', function () {
         const result = await service.check([]);
 
         expect(result.length).to.equal(1);
-        expect(result[0].message).to.equal('No data present in selected file.');
+        expect(result[0].message).to.equal('File is empty, nothing to import. Please select a different file.');
     });
 
     it('returns validation error for data with invalid email', async function () {
@@ -56,7 +56,7 @@ describe('Integration: Service: member-import-validator', function () {
 
     it('returns validation error for data with stripe_customer_id but no connected Stripe', async function () {
         this.owner.register('service:membersUtils', Service.extend({
-            isStripeEnabled: () => (false)
+            isStripeEnabled: false
         }));
 
         let service = this.owner.lookup('service:member-import-validator');
