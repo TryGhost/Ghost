6aefc005f88da7ea0c45d21e19263c5d638054c8
Nazar Gargol <nazargargol@gmail.com>
2020-06-19 14:20:56 +1200

    Removed email validation test from members import validator
    refs https://github.com/TryGhost/Ghost-Admin/commit/2dfff367b92d184ee575e47dc2b2bde8e7176f82
    
    - The test was removed because feature is no longer needed.
    - Qoute from Zimo: " reason: it’s based on a sample so we don’t know for sure how much of the data is actually affected. we might bring it back in the future though"

--- ghost/admin/tests/integration/services/member-import-validator-test.js
+++ ghost/admin/tests/integration/services/member-import-validator-test.js
@@ -42,19 +42,6 @@ describe('Integration: Service: member-import-validator', function () {
         expect(result[0].message).to.equal('File is empty, nothing to import. Please select a different file.');
     });
 
-    // NOTE: need to check if email validation has been remove intentionally
-    xit('returns validation error for data with invalid email', async function () {
-        let service = this.owner.lookup('service:member-import-validator');
-
-        const result = await service.check([{
-            name: 'Egg',
-            email: 'notAnEmail'
-        }]);
-
-        expect(result.length).to.equal(1);
-        expect(result[0].message).to.equal('Emails in provided data don\'t appear to be valid email addresses.');
-    });
-
     it('returns validation error for data with stripe_customer_id but no connected Stripe', async function () {
         this.owner.register('service:membersUtils', Service.extend({
             isStripeEnabled: false
