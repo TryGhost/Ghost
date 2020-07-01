8eb6f6f9138e11024eed41bbc4d211382f2e1648
Kevin Ansfield <kevin@lookingsideways.co.uk>
2020-07-01 18:39:03 +0100

    Converted <GhPostsListItem> to glimmer component
    no issue
    
    - moved tag name and classes into template
    - updated backing class to native class syntax

--- ghost/admin/app/components/gh-posts-list-item.js
+++ ghost/admin/app/components/gh-posts-list-item.js
@@ -1,16 +1,10 @@
-import Component from '@ember/component';
-import {computed} from '@ember/object';
+import Component from '@glimmer/component';
 import {inject as service} from '@ember/service';
 
-export default Component.extend({
-    session: service(),
+export default class GhPostsListItemComponent extends Component {
+    @service session;
 
-    tagName: 'li',
-    classNames: ['gh-list-row', 'gh-posts-list-item'],
-
-    authorNames: computed('post.authors.[]', function () {
-        let authors = this.get('post.authors');
-
-        return authors.map(author => author.get('name') || author.get('email')).join(', ');
-    })
-});
+    get authorNames() {
+        return this.args.post.authors.map(author => author.name || author.email).join(', ');
+    }
+}
