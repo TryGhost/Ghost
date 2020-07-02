e9ae0d9baa104e205cb58c4012ba7176fe70ab69
Kevin Ansfield <kevin@lookingsideways.co.uk>
2020-07-02 18:03:22 +0100

    🐛 Fixed `srcset` and `sizes` attributes being rendered when image resizing unavailable
    closes https://github.com/TryGhost/Ghost/issues/11944
    
    - updates `@tryghost/image-transform` to version that exposes `canTransformFiles()` which checks for `sharp` availibility
    - updates `@tryghost/kg-default-cards` to version that accepts a `canTransformImage()` method as an option
    - updates our `mobiledoc` lib to pass a `canTransformImage()` function that returns false if sharp is unavailable, the image extension is not supported, or the storage engine in use does not support image transforms
    - updates `populateImageSizes` to fetch image sizes when transforms are unavailable as the render/not-render is now handled in the renderer and we don't need to worry about adding size information to the mobiledoc source

--- core/server/web/site/middleware/handle-image-sizes.js
+++ core/server/web/site/middleware/handle-image-sizes.js
@@ -73,6 +73,11 @@ module.exports = function (req, res, next) {
             return;
         }
 
+        // exit early if sharp isn't installed to avoid extra file reads
+        if (!imageTransform.canTransformFiles()) {
+            return redirectToOriginal();
+        }
+
         const imagePath = path.relative(sizeImageDir, req.url);
         const {dir, name, ext} = path.parse(imagePath);
         const [imageNameMatched, imageName, imageNumber] = name.match(/^(.+?)(-\d+)?$/) || [null];
