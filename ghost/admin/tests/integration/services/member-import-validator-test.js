0f3cbe29109b2a839f40906d6640779af2d55ca1
Nazar Gargol <nazargargol@gmail.com>
2020-07-07 00:28:30 +1200

    Improved validation process for members CSV import
    no issue
    
    - A new validation stage has been introduced as a second step after CSV file has been dropped. It is meant to catch any obvious validation errors and output detailed statistics about
    - These improvements also improve sampling logic which increases sample size to 30 non-empty cells which are used to validate stripe_customer_ids when there's such need.
    - New sampling logic also introduces improved automatic field type detection which allows to better map email and stripe_customer_id fields to CSV columns

--- ghost/admin/tests/integration/services/member-import-validator-test.js
+++ ghost/admin/tests/integration/services/member-import-validator-test.js
@@ -25,21 +25,22 @@ describe('Integration: Service: member-import-validator', function () {
     it('checks correct data without Stripe customer', async function () {
         let service = this.owner.lookup('service:member-import-validator');
 
-        const result = await service.check([{
+        const {validationErrors, mapping} = await service.check([{
             name: 'Rish',
             email: 'validemail@example.com'
         }]);
 
-        expect(result).to.equal(true);
+        expect(validationErrors.length).to.equal(0);
+        expect(mapping.email).to.equal('email');
     });
 
     it('returns validation error when no data is provided', async function () {
         let service = this.owner.lookup('service:member-import-validator');
 
-        const result = await service.check([]);
+        const {validationErrors} = await service.check([]);
 
-        expect(result.length).to.equal(1);
-        expect(result[0].message).to.equal('File is empty, nothing to import. Please select a different file.');
+        expect(validationErrors.length).to.equal(1);
+        expect(validationErrors[0].message).to.equal('File is empty, nothing to import. Please select a different file.');
     });
 
     it('returns validation error for data with stripe_customer_id but no connected Stripe', async function () {
@@ -49,13 +50,162 @@ describe('Integration: Service: member-import-validator', function () {
 
         let service = this.owner.lookup('service:member-import-validator');
 
-        const result = await service.check([{
+        const {validationErrors, mapping} = await service.check([{
             name: 'Kevin',
             email: 'goodeamil@example.com',
             stripe_customer_id: 'cus_XXXX'
         }]);
 
-        expect(result.length).to.equal(1);
-        expect(result[0].message).to.equal('<strong>Missing Stripe connection</strong><br>You need to <a href="#/settings/labs">connect to Stripe</a> to import Stripe customers.');
+        expect(validationErrors.length).to.equal(1);
+        expect(validationErrors[0].message).to.equal('Missing stripe connection');
+        expect(mapping.email).to.equal('email');
+        expect(mapping.stripe_customer_id).to.equal('stripe_customer_id');
+    });
+
+    it('returns validation error for no valid emails', async function () {
+        this.owner.register('service:membersUtils', Service.extend({
+            isStripeEnabled: false
+        }));
+
+        let service = this.owner.lookup('service:member-import-validator');
+
+        const {validationErrors} = await service.check([{
+            email: 'invalid_email'
+        }]);
+
+        expect(validationErrors.length).to.equal(1);
+        expect(validationErrors[0].message).to.equal('No email addresses found in provided data.');
+    });
+
+    it('returns validation error for invalid email', async function () {
+        this.owner.register('service:membersUtils', Service.extend({
+            isStripeEnabled: false
+        }));
+
+        let service = this.owner.lookup('service:member-import-validator');
+
+        const {validationErrors} = await service.check([{
+            email: 'invalid_email'
+        }, {
+            email: 'email@example.com'
+        }]);
+
+        expect(validationErrors.length).to.equal(1);
+        expect(validationErrors[0].message).to.equal('Invalid email address (1)');
+    });
+
+    describe('data sampling method', function () {
+        it('returns whole data set when sampled size is less then default 30', async function () {
+            this.owner.register('service:membersUtils', Service.extend({
+                isStripeEnabled: false
+            }));
+
+            let service = this.owner.lookup('service:member-import-validator');
+
+            const result = await service._sampleData([{
+                email: 'email@example.com'
+            }, {
+                email: 'email2@example.com'
+            }]);
+
+            expect(result.length).to.equal(2);
+        });
+
+        it('returns dataset with sample size for non empty values only', async function () {
+            this.owner.register('service:membersUtils', Service.extend({
+                isStripeEnabled: false
+            }));
+
+            let service = this.owner.lookup('service:member-import-validator');
+            let data = [{
+                email: null
+            }, {
+                email: 'email2@example.com'
+            }, {
+                email: 'email3@example.com'
+            }, {
+                email: 'email4@example.com'
+            }, {
+                email: ''
+            }];
+
+            const result = await service._sampleData(data, 3);
+
+            expect(result.length).to.equal(3);
+            expect(result[0].email).to.equal('email2@example.com');
+            expect(result[1].email).to.equal('email3@example.com');
+            expect(result[2].email).to.equal('email4@example.com');
+        });
+
+        it('returns dataset with sample size for non empty values for objects with multiple properties', async function () {
+            this.owner.register('service:membersUtils', Service.extend({
+                isStripeEnabled: false
+            }));
+
+            let service = this.owner.lookup('service:member-import-validator');
+            let data = [{
+                email: null,
+                other_prop: 'non empty 1'
+            }, {
+                email: 'email2@example.com',
+                other_prop: 'non empty 2'
+            }, {
+                email: 'email3@example.com',
+                other_prop: ''
+            }, {
+                email: 'email4@example.com'
+            }, {
+                email: '',
+                other_prop: 'non empty 5'
+            }];
+
+            const result = await service._sampleData(data, 3);
+
+            expect(result.length).to.equal(3);
+            expect(result[0].email).to.equal('email2@example.com');
+            expect(result[0].other_prop).to.equal('non empty 1');
+            expect(result[1].email).to.equal('email3@example.com');
+            expect(result[1].other_prop).to.equal('non empty 2');
+            expect(result[2].email).to.equal('email4@example.com');
+            expect(result[2].other_prop).to.equal('non empty 5');
+        });
+    });
+
+    describe('data detection method', function () {
+        it('correctly detects only email mapping', async function () {
+            this.owner.register('service:membersUtils', Service.extend({
+                isStripeEnabled: false
+            }));
+
+            let service = this.owner.lookup('service:member-import-validator');
+
+            const result = service._detectDataTypes([{
+                correo_electronico: 'email@example.com'
+            }, {
+                correo_electronico: 'email2@example.com'
+            }]);
+
+            expect(result.email).to.equal('correo_electronico');
+            expect(result.stripe_customer_id).to.equal(undefined);
+        });
+
+        it('correctly detects only email mapping', async function () {
+            this.owner.register('service:membersUtils', Service.extend({
+                isStripeEnabled: false
+            }));
+
+            let service = this.owner.lookup('service:member-import-validator');
+
+            const result = service._detectDataTypes([{
+                correo_electronico: 'email@example.com',
+                stripe_id: ''
+            }, {
+                correo_electronico: '',
+                stripe_id: 'cus_'
+            }]);
+
+            expect(result.email).to.equal('correo_electronico');
+            expect(result.stripe_customer_id).to.equal('stripe_id');
+        });
     });
 });
