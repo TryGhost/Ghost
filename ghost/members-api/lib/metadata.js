0a98b5dacbf78099b2f3fed7a7a91f1932a9d59d
Fabien 'egg' O'Carroll <fabien@allou.is>
2020-07-09 16:40:48 +0200

    Refactored webhook creation (#175)
    no-issue
    
    * Refactored model dependencies
      This groups all of the model depenencies into a single models object,
      and renames the models with more concise identifiers
    
    * Fixed spacing
    * Added webhook support to metadata
    * Refactored stripe configure to have better logging
    * Refactored webhook creation to reuse existing webhook
    * Installed @types/stripe

--- ghost/members-api/lib/metadata.js
+++ ghost/members-api/lib/metadata.js
@@ -1,54 +1,55 @@
-let MemberStripeCustomer;
-let StripeCustomerSubscription;
-
-async function setMetadata(module, metadata) {
-    if (module !== 'stripe') {
-        return;
-    }
-
-    if (metadata.customer) {
-        await MemberStripeCustomer.upsert(metadata.customer, {
-            customer_id: metadata.customer.customer_id
-        });
-    }
-
-    if (metadata.subscription) {
-        await StripeCustomerSubscription.upsert(metadata.subscription, {
-            subscription_id: metadata.subscription.subscription_id
-        });
-    }
-
-    return;
-}
-
-async function getMetadata(module, member) {
-    if (module !== 'stripe') {
-        return;
-    }
-
-    const customers = (await MemberStripeCustomer.findAll({
-        filter: `member_id:${member.id}`
-    })).toJSON();
-
-    const subscriptions = await customers.reduce(async (subscriptionsPromise, customer) => {
-        const customerSubscriptions = await StripeCustomerSubscription.findAll({
-            filter: `customer_id:${customer.customer_id}`
-        });
-        return (await subscriptionsPromise).concat(customerSubscriptions.toJSON());
-    }, []);
-
-    return {
-        customers: customers,
-        subscriptions: subscriptions
-    };
-}
-
 module.exports = function ({
-    memberStripeCustomerModel,
-    stripeCustomerSubscriptionModel
+    StripeWebhook,
+    StripeCustomer,
+    StripeCustomerSubscription
 }) {
-    MemberStripeCustomer = memberStripeCustomerModel;
-    StripeCustomerSubscription = stripeCustomerSubscriptionModel;
+    async function setMetadata(module, metadata) {
+        if (module !== 'stripe') {
+            return;
+        }
+
+        if (metadata.customer) {
+            await StripeCustomer.upsert(metadata.customer, {
+                customer_id: metadata.customer.customer_id
+            });
+        }
+
+        if (metadata.subscription) {
+            await StripeCustomerSubscription.upsert(metadata.subscription, {
+                subscription_id: metadata.subscription.subscription_id
+            });
+        }
+
+        if (metadata.webhook) {
+            await StripeWebhook.upsert(metadata.webhook, {
+                webhook_id: metadata.webhook.webhook_id
+            });
+        }
+
+        return;
+    }
+
+    async function getMetadata(module, member) {
+        if (module !== 'stripe') {
+            return;
+        }
+
+        const customers = (await StripeCustomer.findAll({
+            filter: `member_id:${member.id}`
+        })).toJSON();
+
+        const subscriptions = await customers.reduce(async (subscriptionsPromise, customer) => {
+            const customerSubscriptions = await StripeCustomerSubscription.findAll({
+                filter: `customer_id:${customer.customer_id}`
+            });
+            return (await subscriptionsPromise).concat(customerSubscriptions.toJSON());
+        }, []);
+
+        return {
+            customers: customers,
+            subscriptions: subscriptions
+        };
+    }
 
     return {
         setMetadata,
