9a4f02a8f391d2deb69720da81c15f46cbe13f95
Fabien 'egg' O'Carroll <fabien@allou.is>
2020-07-15 13:45:31 +0200

    ✨ Added UI for AMP GA Tracking ID (#1638)
    refs https://github.com/TryGhost/Ghost/pull/12039
    
    Adds the initial UI for setting the `amp_gtag_id` setting

--- ghost/admin/app/models/setting.js
+++ ghost/admin/app/models/setting.js
@@ -25,6 +25,7 @@ export default Model.extend(ValidationEngine, {
     password: attr('string'),
     slack: attr('slack-settings'),
     amp: attr('boolean'),
+    ampGtagId: attr('string'),
     unsplash: attr('unsplash-settings', {
         defaultValue() {
             return {isActive: true};
