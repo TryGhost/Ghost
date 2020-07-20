57a4ead4d2eec2566b88af39a664317f9a11b554
Peter Zimon <zimo@ghost.org>
2020-07-20 14:52:47 +0200

    Distributed CSS in JS styles to components
    no refs.
    - moved switch and action button styles to their respective components to tidy up the style structure

--- ghost/portal/src/components/common/ActionButton.js
+++ ghost/portal/src/components/common/ActionButton.js
@@ -1,6 +1,40 @@
 import React from 'react';
 import getContrastColor from '../../utils/contrast-color';
 
+export const ActionButtonStyles = `
+.gh-portal-btn {
+    position: relative;
+    display: inline-block;
+    padding: 0 1.8rem;
+    height: 44px;
+    border: 0;
+    font-size: 1.5rem;
+    line-height: 42px;
+    font-weight: 500;
+    letter-spacing: 0.2px;
+    text-align: center;
+    text-decoration: none;
+    white-space: nowrap;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: .4s ease;
+    box-shadow: none;
+    user-select: none;
+    width: 100%;
+}
+
+.gh-portal-btn:hover::before {
+    position: absolute;
+    content: "";
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: rgba(255, 255, 255, 0.08);
+    border-radius: 5px;
+}
+`;
+
 const Styles = ({brandColor, retry, disabled, style = {}}) => {
     let backgroundColor = (brandColor || '#3eb0ef');
     if (retry) {
