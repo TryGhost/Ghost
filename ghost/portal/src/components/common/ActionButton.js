8b4fe472d6942f0db21838c78e4994a11b5764d7
Peter Zimon <zimo@ghost.org>
2020-07-20 17:33:10 +0200

    Updated paid account styles
    no refs.
    - added basic styling to paid account settings styles

--- ghost/portal/src/components/common/ActionButton.js
+++ ghost/portal/src/components/common/ActionButton.js
@@ -2,37 +2,20 @@ import React from 'react';
 import getContrastColor from '../../utils/contrast-color';
 
 export const ActionButtonStyles = `
-.gh-portal-btn {
-    position: relative;
-    display: inline-block;
-    padding: 0 1.8rem;
-    height: 44px;
-    border: 0;
-    font-size: 1.5rem;
-    line-height: 42px;
-    font-weight: 500;
-    letter-spacing: 0.2px;
-    text-align: center;
-    text-decoration: none;
-    white-space: nowrap;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: .4s ease;
-    box-shadow: none;
-    user-select: none;
-    width: 100%;
-}
-
-.gh-portal-btn:hover::before {
-    position: absolute;
-    content: "";
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: rgba(255, 255, 255, 0.08);
-    border-radius: 5px;
-}
+    .gh-portal-btn-main {
+        width: 100%;
+        box-shadow: none;
+    }
+    .gh-portal-btn-primary:hover::before {
+        position: absolute;
+        content: "";
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: rgba(255, 255, 255, 0.08);
+        border-radius: 5px;
+    }
 `;
 
 const Styles = ({brandColor, retry, disabled, style = {}}) => {
@@ -57,7 +40,7 @@ const Styles = ({brandColor, retry, disabled, style = {}}) => {
 function ActionButton({label, onClick, disabled, retry, brandColor, style}) {
     let Style = Styles({disabled, retry, brandColor, style});
     return (
-        <button onClick={e => onClick(e)} style={Style.button} className="gh-portal-btn gh-portal-btn-primary" disabled={disabled}>
+        <button onClick={e => onClick(e)} style={Style.button} className="gh-portal-btn gh-portal-btn-main gh-portal-btn-primary" disabled={disabled}>
             {label}
         </button>
     );
