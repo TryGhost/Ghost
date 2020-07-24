c9a7691917709125ee9c8e81413f0ccdaed9cdb4
Fabien O'Carroll <fabien@allou.is>
2020-07-24 15:29:21 +0200

    Added default options to transactional model methods
    no-issue
    
    This protects against calling model methods without passing an options
    object

--- core/server/models/member-stripe-customer.js
+++ core/server/models/member-stripe-customer.js
@@ -28,7 +28,7 @@ const MemberStripeCustomer = ghostBookshelf.Model.extend({
         return this.add(data, unfilteredOptions);
     },
 
-    add(data, unfilteredOptions) {
+    add(data, unfilteredOptions = {}) {
         if (!unfilteredOptions.transacting) {
             return ghostBookshelf.transaction((transacting) => {
                 return this.add(data, Object.assign({transacting}, unfilteredOptions));
@@ -37,7 +37,7 @@ const MemberStripeCustomer = ghostBookshelf.Model.extend({
         return ghostBookshelf.Model.add.call(this, data, unfilteredOptions);
     },
 
-    edit(data, unfilteredOptions) {
+    edit(data, unfilteredOptions = {}) {
         if (!unfilteredOptions.transacting) {
             return ghostBookshelf.transaction((transacting) => {
                 return this.edit(data, Object.assign({transacting}, unfilteredOptions));
@@ -46,7 +46,7 @@ const MemberStripeCustomer = ghostBookshelf.Model.extend({
         return ghostBookshelf.Model.edit.call(this, data, unfilteredOptions);
     },
 
-    destroy(unfilteredOptions) {
+    destroy(unfilteredOptions = {}) {
         if (!unfilteredOptions.transacting) {
             return ghostBookshelf.transaction((transacting) => {
                 return this.destroy(Object.assign({transacting}, unfilteredOptions));
--- core/server/models/member.js
+++ core/server/models/member.js
@@ -230,7 +230,7 @@ const Member = ghostBookshelf.Model.extend({
         return options;
     },
 
-    add(data, unfilteredOptions) {
+    add(data, unfilteredOptions = {}) {
         if (!unfilteredOptions.transacting) {
             return ghostBookshelf.transaction((transacting) => {
                 return this.add(data, Object.assign({transacting}, unfilteredOptions));
@@ -239,7 +239,7 @@ const Member = ghostBookshelf.Model.extend({
         return ghostBookshelf.Model.add.call(this, data, unfilteredOptions);
     },
 
-    edit(data, unfilteredOptions) {
+    edit(data, unfilteredOptions = {}) {
         if (!unfilteredOptions.transacting) {
             return ghostBookshelf.transaction((transacting) => {
                 return this.edit(data, Object.assign({transacting}, unfilteredOptions));
@@ -248,7 +248,7 @@ const Member = ghostBookshelf.Model.extend({
         return ghostBookshelf.Model.edit.call(this, data, unfilteredOptions);
     },
 
-    destroy(unfilteredOptions) {
+    destroy(unfilteredOptions = {}) {
         if (!unfilteredOptions.transacting) {
             return ghostBookshelf.transaction((transacting) => {
                 return this.destroy(Object.assign({transacting}, unfilteredOptions));
