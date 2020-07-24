5484d5e2d887fa0bfc6226049445edf6805f6608
Peter Zimon <zimo@ghost.org>
2020-07-24 10:46:13 +0200

    Cleaned up style attributes

--- ghost/portal/src/components/common/MemberGravatar.js
+++ ghost/portal/src/components/common/MemberGravatar.js
@@ -11,29 +11,29 @@ export const AvatarStyles = `
         margin-bottom: 8px;
         border-radius: 999px;
     }
+
+    .gh-portal-avatar img {
+        display: block;
+        position: absolute;
+        top: -1px;
+        right: -1px;
+        bottom: -1px;
+        left: -1px;
+        width: calc(100% + 2px);
+        height: calc(100% + 2px);
+        opacity: 1;
+        maxWidth: unset;
+    }
 `;
 
 const Styles = ({style = {}}) => {
     return {
         userIcon: {
-            width: '20px',
-            height: '20px',
+            width: '56px',
+            height: '56px',
             color: '#fff',
             ...(style.userIcon || {}) // Override any custom style
         },
-        gravatar: {
-            display: 'block',
-            position: 'absolute',
-            top: '-1px',
-            right: '-1px',
-            bottom: '-1px',
-            left: '-1px',
-            width: 'calc(100% + 2px)',
-            height: 'calc(100% + 2px)',
-            opacity: '1',
-            maxWidth: 'unset',
-            ...(style.gravatar || {}) // Override any custom style
-        }
     };
 };
 
@@ -42,7 +42,7 @@ function MemberGravatar({gravatar, style}) {
     return (
         <figure className='gh-portal-avatar'>
             <UserIcon style={Style.userIcon} />
-            {gravatar ? <img style={Style.gravatar} src={gravatar} alt="Gravatar" /> : null}
+            {gravatar ? <img src={gravatar} alt="Gravatar" /> : null}
         </figure>
     );
 }
