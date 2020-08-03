8f5a70a79eb9f205063df154843044e8af5dddc0
Daniel Lockyer <hi@daniellockyer.com>
2020-08-03 20:06:37 +0100

    Removed explicit loading of Bookshelf registry plugin
    - as per https://github.com/bookshelf/bookshelf/commit/5a5a5d162e74022e38ffff8825aaac7466957ac6, the Bookshelf registry plugin is now in core
    - we no longer need to explicitly load the plugin, and it displays a
      warning if you do
    - this change also turns `._models` into `.registry.models`, so our code has
      been updated to reflect that

--- core/server/models/base/index.js
+++ core/server/models/base/index.js
@@ -31,9 +31,6 @@ let proto;
 // Initializes a new Bookshelf instance called ghostBookshelf, for reference elsewhere in Ghost.
 ghostBookshelf = bookshelf(db.knex);
 
-// Load the Bookshelf registry plugin, which helps us avoid circular dependencies
-ghostBookshelf.plugin('registry');
-
 ghostBookshelf.plugin(plugins.eagerLoad);
 
 // Add committed/rollback events.
@@ -1290,17 +1287,17 @@ ghostBookshelf.Model = ghostBookshelf.Model.extend({
 
                 if (!withRelated) {
                     return _.map(objects, (object) => {
-                        object = ghostBookshelf._models[modelName].prototype.toJSON.bind({
+                        object = ghostBookshelf.registry.models[modelName].prototype.toJSON.bind({
                             attributes: object,
                             related: function (key) {
                                 return object[key];
                             },
-                            serialize: ghostBookshelf._models[modelName].prototype.serialize,
-                            formatsToJSON: ghostBookshelf._models[modelName].prototype.formatsToJSON
+                            serialize: ghostBookshelf.registry.models[modelName].prototype.serialize,
+                            formatsToJSON: ghostBookshelf.registry.models[modelName].prototype.formatsToJSON
                         })();
 
-                        object = ghostBookshelf._models[modelName].prototype.fixBools(object);
-                        object = ghostBookshelf._models[modelName].prototype.fixDatesWhenFetch(object);
+                        object = ghostBookshelf.registry.models[modelName].prototype.fixBools(object);
+                        object = ghostBookshelf.registry.models[modelName].prototype.fixDatesWhenFetch(object);
                         return object;
                     });
                 }
@@ -1364,7 +1361,7 @@ ghostBookshelf.Model = ghostBookshelf.Model.extend({
                                 object[relation] = relationsToAttach[relation][object.id];
                             });
 
-                            object = ghostBookshelf._models[modelName].prototype.toJSON.bind({
+                            object = ghostBookshelf.registry.models[modelName].prototype.toJSON.bind({
                                 attributes: object,
                                 _originalOptions: {
                                     withRelated: Object.keys(relationsToAttach)
@@ -1372,12 +1369,12 @@ ghostBookshelf.Model = ghostBookshelf.Model.extend({
                                 related: function (key) {
                                     return object[key];
                                 },
-                                serialize: ghostBookshelf._models[modelName].prototype.serialize,
-                                formatsToJSON: ghostBookshelf._models[modelName].prototype.formatsToJSON
+                                serialize: ghostBookshelf.registry.models[modelName].prototype.serialize,
+                                formatsToJSON: ghostBookshelf.registry.models[modelName].prototype.formatsToJSON
                             })();
 
-                            object = ghostBookshelf._models[modelName].prototype.fixBools(object);
-                            object = ghostBookshelf._models[modelName].prototype.fixDatesWhenFetch(object);
+                            object = ghostBookshelf.registry.models[modelName].prototype.fixBools(object);
+                            object = ghostBookshelf.registry.models[modelName].prototype.fixDatesWhenFetch(object);
                             return object;
                         });
 
