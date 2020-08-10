0a44c967db601f1bf1943df5217da2a466186481
Hannah Wolfe <erisds@gmail.com>
2020-08-10 16:19:09 +0100

    Added a server testmode to help test behaviour
    - A simple way to test behaviours without having to do complex interactions to e.g. generate errors or slow requests
    - Makes it easier to test the new shutdown behaviour, among other things

--- core/server/web/api/app.js
+++ core/server/web/api/app.js
@@ -1,4 +1,5 @@
 const debug = require('ghost-ignition').debug('web:api:default:app');
+const config = require('../../../shared/config');
 const express = require('../../../shared/express');
 const urlUtils = require('../../../shared/url-utils');
 const errorHandler = require('../shared/middlewares/error-handler');
@@ -6,6 +7,10 @@ const errorHandler = require('../shared/middlewares/error-handler');
 module.exports = function setupApiApp() {
     debug('Parent API setup start');
     const apiApp = express('api');
+
+    if (config.get('server:testmode')) {
+        apiApp.use(require('./testmode')());
+    }
 
     // Mount different API versions
     apiApp.use(urlUtils.getVersionPath({version: 'v2', type: 'content'}), require('./v2/content/app')());
