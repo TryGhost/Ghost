2c1417da882d7e860124d5ba38aa1d6616c083d2
Hannah Wolfe <erisds@gmail.com>
2020-08-11 21:13:31 +0100

    ✨ Added new job manager package
    - Minimal working version of a job manager
    - Uses fastq for handling the queue
    - Exposes 2 methods: addJob and shutdown

---
+++ ghost/job-manager/test/hello.test.js
@@ -0,0 +1,10 @@
+// Switch these lines once there are useful utils
+// const testUtils = require('./utils');
+require('./utils');
+
+describe('Hello world', function () {
+    it('Runs a test', function () {
+        // TODO: Write me!
+        'hello'.should.eql('hello');
+    });
+});
