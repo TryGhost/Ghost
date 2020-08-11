14a53f696edc08dcab52d0cdf3f925bdddaedc6f
Daniel Lockyer <hi@daniellockyer.com>
2020-08-11 13:37:26 +0100

    Populated index.js with exports to package components
    - pulled lib/index.js up to root and fixed paths

---
+++ ghost/security/index.js
@@ -0,0 +1,21 @@
+module.exports = {
+    get url() {
+        return require('./lib/url');
+    },
+
+    get tokens() {
+        return require('./lib/tokens');
+    },
+
+    get string() {
+        return require('./lib/string');
+    },
+
+    get identifier() {
+        return require('./lib/identifier');
+    },
+
+    get password() {
+        return require('./lib/password');
+    }
+};
