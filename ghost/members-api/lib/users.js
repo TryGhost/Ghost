3fdd3b97dc0f8d8ae0cc972a2f3eabc8a03042eb
Fabien 'egg' O'Carroll <fabien@allou.is>
2020-08-12 12:57:28 +0100

    Used models internally and for exported API (#195)
    no-issue
    
    Using models internally and in the exported API means that we avoid expensive
    `toJSON` calls, which affects performance when looping through large lists of
    members. It also allows us to take advantage of the new relations used in the
    models.
    
    The addition of "ByID" methods for linking stripe customers and setting
    complimentary subscriptions allows bulk imports to avoid the overhead of creating
    a model for each members, instead passing an id string. n.b. currently the impl
    _does_ still create models, but it makes it easier to optimise and refactor in the 
    future.

--- ghost/members-api/lib/users.js
+++ ghost/members-api/lib/users.js
@@ -1,184 +1,89 @@
 const _ = require('lodash');
 const debug = require('ghost-ignition').debug('users');
-const common = require('./common');
 
 module.exports = function ({
     stripe,
     Member
 }) {
-    async function createMember({email, name, note, labels, geolocation}) {
-        const model = await Member.add({
+    async function get(data, options) {
+        debug(`get id:${data.id} email:${data.email}`);
+        return Member.findOne(data, options);
+    }
+
+    async function destroy(data, options) {
+        debug(`destroy id:${data.id} email:${data.email}`);
+        const member = await Member.findOne(data, options);
+        if (!member) {
+            return;
+        }
+
+        if (stripe && options.cancelStripeSubscriptions) {
+            await stripe.cancelStripeSubscriptions(member);
+        }
+
+        return Member.destroy({
+            id: data.id
+        }, options);
+    }
+
+    async function update(data, options) {
+        debug(`update id:${options.id}`);
+        return Member.edit(_.pick(data, [
+            'email',
+            'name',
+            'note',
+            'labels',
+            'geolocation'
+        ]), options);
+    }
+
+    async function list(options = {}) {
+        return Member.findPage(options);
+    }
+
+    async function create({email, name, note, labels, geolocation}, options) {
+        debug(`create email:${email}`);
+
+        /** Member.add model method expects label object array*/
+        if (labels) {
+            labels.forEach((label, index) => {
+                if (_.isString(label)) {
+                    labels[index] = {name: label};
+                }
+            });
+        }
+
+        return Member.add({
             email,
             name,
             note,
             labels,
             geolocation
-        });
-        const member = model.toJSON();
-        return member;
+        }, options);
     }
 
-    async function getMember(data, options = {}) {
-        if (!data.email && !data.id && !data.uuid) {
-            return null;
-        }
-        const model = await Member.findOne(data, options);
-        if (!model) {
-            return null;
-        }
-        const member = model.toJSON(options);
-        return member;
-    }
-
-    async function updateMember(data, options = {}) {
-        const attrs = _.pick(data, ['email', 'name', 'note', 'subscribed', 'geolocation']);
-
-        const model = await Member.edit(attrs, options);
-
-        const member = model.toJSON(options);
-        return member;
-    }
-
-    function deleteMember(options) {
-        options = options || {};
-        return Member.destroy(options);
-    }
-
-    function listMembers(options) {
-        return Member.findPage(options).then((models) => {
-            return {
-                members: models.data.map(model => model.toJSON(options)),
-                meta: models.meta
-            };
-        });
-    }
-
-    async function getStripeSubscriptions(member) {
-        if (!stripe) {
-            return [];
-        }
-
-        return await stripe.getActiveSubscriptions(member);
-    }
-
-    async function cancelStripeSubscriptions(member) {
-        if (stripe) {
-            await stripe.cancelAllSubscriptions(member);
-        }
-    }
-
-    async function setComplimentarySubscription(member) {
-        if (stripe) {
-            await stripe.setComplimentarySubscription(member);
-        }
-    }
-
-    async function cancelComplimentarySubscription(member) {
-        if (stripe) {
-            await stripe.cancelComplimentarySubscription(member);
-        }
-    }
-
-    async function linkStripeCustomer(id, member) {
-        if (stripe) {
-            await stripe.linkStripeCustomer(id, member);
-        }
-    }
-
-    async function getStripeCustomer(id) {
-        if (stripe) {
-            return await stripe.getStripeCustomer(id);
-        }
-    }
-
-    async function get(data, options) {
-        debug(`get id:${data.id} email:${data.email}`);
-        const member = await getMember(data, options);
-        if (!member) {
-            return member;
-        }
-
-        try {
-            const subscriptions = await getStripeSubscriptions(member);
-
-            return Object.assign(member, {
-                stripe: {
-                    subscriptions
-                }
-            });
-        } catch (err) {
-            common.logging.error(err);
-            return null;
-        }
-    }
-
-    async function destroy(data, options) {
-        debug(`destroy id:${data.id} email:${data.email}`);
-        const member = await getMember(data, options);
-        if (!member) {
-            return;
-        }
-
-        await cancelStripeSubscriptions(member);
-
-        return deleteMember(data);
-    }
-
-    async function update(data, options) {
-        debug(`update id:${options.id}`);
-
-        const member = await updateMember(data, options);
-        if (!member) {
-            return member;
-        }
-
-        try {
-            const subscriptions = await getStripeSubscriptions(member);
-
-            return Object.assign(member, {
-                stripe: {
-                    subscriptions
-                }
-            });
-        } catch (err) {
-            common.logging.error(err);
-            return null;
-        }
-    }
-
-    async function list(options) {
-        const {meta, members} = await listMembers(options);
-
-        const membersWithSubscriptions = await Promise.all(members.map(async function (member) {
-            const subscriptions = await getStripeSubscriptions(member);
-
-            return Object.assign(member, {
-                stripe: {
-                    subscriptions
-                }
-            });
-        }));
-
-        return {
-            meta,
-            members: membersWithSubscriptions
+    function safeStripe(methodName) {
+        return async function (...args) {
+            if (stripe) {
+                return await stripe[methodName](...args);
+            }
         };
     }
 
-    async function create(data) {
-        debug(`create email:${data.email}`);
-
-        /** Member.add model method expects label object array*/
-        if (data.labels) {
-            data.labels.forEach((label, index) => {
-                if (_.isString(label)) {
-                    data.labels[index] = {name: label};
-                }
-            });
+    async function linkStripeCustomerById(customerId, memberId) {
+        if (!stripe) {
+            return;
         }
+        const member = await get({id: memberId});
+        return stripe.linkStripeCustomer(customerId, member);
+    }
 
-        const member = await createMember(data);
-        return member;
+    async function setComplimentarySubscriptionById(memberId) {
+        if (!stripe) {
+            return;
+        }
+        const member = await get({id: memberId});
+        return stripe.setComplimentarySubscription(member);
     }
 
     return {
@@ -187,11 +92,12 @@ module.exports = function ({
         list,
         get,
         destroy,
-        getStripeSubscriptions,
-        setComplimentarySubscription,
-        cancelComplimentarySubscription,
-        cancelStripeSubscriptions,
-        getStripeCustomer,
-        linkStripeCustomer
+        setComplimentarySubscription: safeStripe('setComplimentarySubscription'),
+        setComplimentarySubscriptionById,
+        cancelComplimentarySubscription: safeStripe('cancelComplimentarySubscription'),
+        cancelStripeSubscriptions: safeStripe('cancelComplimentarySubscription'),
+        getStripeCustomer: safeStripe('getStripeCustomer'),
+        linkStripeCustomer: safeStripe('linkStripeCustomer'),
+        linkStripeCustomerById
     };
 };
