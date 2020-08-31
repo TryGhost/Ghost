TryGhost/Ghost

commit 38e5708c896c82290eb297c9d21a7ed01e6d0695

Updated tags output serialiasation for v2 API
no-issue

This ensures that the v2 API only outputs the tag properties we specify,
and doesn't include any new fields, like the new metadata columns.

core/server/api/v2/utils/serializers/output/utils/clean.js
@@ -3,25 +3,43 @@ const localUtils = require('../../../index');
 
 const tag = (attrs, frame) => {
     if (localUtils.isContentAPI(frame)) {
-        delete attrs.created_at;
-        delete attrs.updated_at;
+        const contentAttrs = _.pick(attrs, [
+            'description',
+            'feature_image',
+            'id',
+            'meta_description',
+            'meta_title',
+            'name',
+            'slug',
+            'url',
+            'visibility'
+        ]);
 
         // We are standardising on returning null from the Content API for any empty values
-        if (attrs.meta_title === '') {
-            attrs.meta_title = null;
+        if (contentAttrs.meta_title === '') {
+            contentAttrs.meta_title = null;
         }
-        if (attrs.meta_description === '') {
-            attrs.meta_description = null;
+        if (contentAttrs.meta_description === '') {
+            contentAttrs.meta_description = null;
         }
-        if (attrs.description === '') {
-            attrs.description = null;
+        if (contentAttrs.description === '') {
+            contentAttrs.description = null;
         }
     }
 
-    delete attrs.parent_id;
-    delete attrs.parent;
-
-    return attrs;
+    return _.pick(attrs, [
+        'created_at',
+        'description',
+        'feature_image',
+        'id',
+        'meta_description',
+        'meta_title',
+        'name',
+        'slug',
+        'updated_at',
+        'url',
+        'visibility'
+    ]);
 };
 
 const author = (attrs, frame) => {

core/server/api/v2/utils/serializers/output/utils/mapper.js
@@ -21,9 +21,9 @@ const mapTag = (model, frame) => {
     const jsonModel = model.toJSON ? model.toJSON(frame.options) : model;
 
     url.forTag(model.id, jsonModel, frame.options);
-    clean.tag(jsonModel, frame);
+    const cleanedAttrs = clean.tag(jsonModel, frame);
 
-    return jsonModel;
+    return cleanedAttrs;
 };
 
 const mapPost = (model, frame) => {

commit 63ba6a1bd6bb05f8a3418990775e3f704dba9393

🐛 Fixed Tags API v2 to return posts count
closes #12167

- Tags API v2 was ignoring `count.posts` include parameter.
- Regression was introduced with 38e5708c896c82290eb297c9d21a7ed01e6d0695
- Introduced regression tests across all Content API versions to avoid similar bug in the future

core/server/api/v2/utils/serializers/output/utils/clean.js
@@ -12,7 +12,8 @@ const tag = (attrs, frame) => {
             'name',
             'slug',
             'url',
-            'visibility'
+            'visibility',
+            'count'
         ]);
 
         // We are standardising on returning null from the Content API for any empty values
@@ -25,6 +26,8 @@ const tag = (attrs, frame) => {
         if (contentAttrs.description === '') {
             contentAttrs.description = null;
         }
+
+        return contentAttrs;
     }
 
     return _.pick(attrs, [
@@ -38,7 +41,8 @@ const tag = (attrs, frame) => {
         'slug',
         'updated_at',
         'url',
-        'visibility'
+        'visibility',
+        'count'
     ]);
 };
 

test/regression/api/canary/content/tags_spec.js
@@ -25,7 +25,7 @@ describe('api/canary/content/tags', function () {
         configUtils.restore();
     });
 
-    it('can read tags with fields', function () {
+    it('Can read tags with fields', function () {
         return request
             .get(localUtils.API.getApiQuery(`tags/${testUtils.DataGenerator.Content.tags[0].id}/?key=${validKey}&fields=name,slug`))
             .set('Origin', testUtils.API.getURL())
@@ -37,7 +37,38 @@ describe('api/canary/content/tags', function () {
             });
     });
 
-    it('browse tags with slug filter, should order in slug order', function () {
+    it('Can request all tags with count.posts field and custom order', function () {
+        return request
+            .get(localUtils.API.getApiQuery(`tags/?key=${validKey}&include=count.posts&order=name%20DESC`))
+            .set('Origin', testUtils.API.getURL())
+            .set('Origin', config.get('url'))
+            .expect('Content-Type', /json/)
+            .expect('Cache-Control', testUtils.cacheRules.private)
+            .expect(200)
+            .then((res) => {
+                should.not.exist(res.headers['x-cache-invalidate']);
+                const jsonResponse = res.body;
+                should.exist(jsonResponse);
+                should.exist(jsonResponse.tags);
+                jsonResponse.tags.should.have.length(4);
+                localUtils.API.checkResponse(jsonResponse.tags[0], 'tag', ['count', 'url']);
+
+                jsonResponse.meta.pagination.should.have.property('page', 1);
+                jsonResponse.meta.pagination.should.have.property('limit', 15);
+                jsonResponse.meta.pagination.should.have.property('pages', 4);
+                jsonResponse.meta.pagination.should.have.property('total', 56);
+                jsonResponse.meta.pagination.should.have.property('next', 2);
+                jsonResponse.meta.pagination.should.have.property('prev', null);
+
+                jsonResponse.tags[0].url.should.eql(`${config.get('url')}/tag/kitchen-sink/`);
+                jsonResponse.tags[1].url.should.eql(`${config.get('url')}/tag/getting-started/`);
+
+                should.exist(jsonResponse.tags[0].count.posts);
+                jsonResponse.tags[0].count.posts.should.equal(2);
+            });
+    });
+
+    it('Browse tags with slug filter, should order in slug order', function () {
         return request.get(localUtils.API.getApiQuery(`tags/?key=${validKey}&filter=slug:[kitchen-sink,bacon,chorizo]`))
             .expect('Content-Type', /json/)
             .expect('Cache-Control', testUtils.cacheRules.private)

test/regression/api/v2/content/tags_spec.js
@@ -25,7 +25,7 @@ describe('api/v2/content/tags', function () {
 
     const validKey = localUtils.getValidKey();
 
-    it('can read tags with fields', function () {
+    it('Can read tags with fields', function () {
         return request
             .get(localUtils.API.getApiQuery(`tags/${testUtils.DataGenerator.Content.tags[0].id}/?key=${validKey}&fields=name,slug`))
             .set('Origin', testUtils.API.getURL())
@@ -34,6 +34,37 @@ describe('api/v2/content/tags', function () {
             .expect(200)
             .then((res) => {
                 localUtils.API.checkResponse(res.body.tags[0], 'tag', null, null, ['id', 'name', 'slug']);
+            });
+    });
+
+    it('Can request all tags with count.posts field and custom order', function () {
+        return request
+            .get(localUtils.API.getApiQuery(`tags/?key=${validKey}&include=count.posts&order=name%20DESC`))
+            .set('Origin', testUtils.API.getURL())
+            .set('Origin', config.get('url'))
+            .expect('Content-Type', /json/)
+            .expect('Cache-Control', testUtils.cacheRules.private)
+            .expect(200)
+            .then((res) => {
+                should.not.exist(res.headers['x-cache-invalidate']);
+                const jsonResponse = res.body;
+                should.exist(jsonResponse);
+                should.exist(jsonResponse.tags);
+                jsonResponse.tags.should.have.length(4);
+                localUtils.API.checkResponse(jsonResponse.tags[0], 'tag', ['count', 'url']);
+
+                jsonResponse.meta.pagination.should.have.property('page', 1);
+                jsonResponse.meta.pagination.should.have.property('limit', 15);
+                jsonResponse.meta.pagination.should.have.property('pages', 4);
+                jsonResponse.meta.pagination.should.have.property('total', 56);
+                jsonResponse.meta.pagination.should.have.property('next', 2);
+                jsonResponse.meta.pagination.should.have.property('prev', null);
+
+                jsonResponse.tags[0].url.should.eql(`${config.get('url')}/tag/kitchen-sink/`);
+                jsonResponse.tags[1].url.should.eql(`${config.get('url')}/tag/getting-started/`);
+
+                should.exist(jsonResponse.tags[0].count.posts);
+                jsonResponse.tags[0].count.posts.should.equal(2);
             });
     });
 });

test/regression/api/v2/content/utils.js
@@ -59,6 +59,19 @@ const expectedProperties = {
         .without('parent_id', 'parent')
         // v2 Tag API doesn't return date fields
         .without('created_at', 'updated_at')
+        // v2 Tag API doesn't return extended meta fields
+        .without(
+            'og_image',
+            'og_title',
+            'og_description',
+            'twitter_image',
+            'twitter_title',
+            'twitter_description',
+            'codeinjection_head',
+            'codeinjection_foot',
+            'canonical_url',
+            'accent_color'
+        )
 };
 
 _.each(expectedProperties, (value, key) => {

test/regression/api/v3/content/tags_spec.js
@@ -25,7 +25,7 @@ describe('api/v3/content/tags', function () {
 
     const validKey = localUtils.getValidKey();
 
-    it('can read tags with fields', function () {
+    it('Can read tags with fields', function () {
         return request
             .get(localUtils.API.getApiQuery(`tags/${testUtils.DataGenerator.Content.tags[0].id}/?key=${validKey}&fields=name,slug`))
             .set('Origin', testUtils.API.getURL())
@@ -34,6 +34,52 @@ describe('api/v3/content/tags', function () {
             .expect(200)
             .then((res) => {
                 localUtils.API.checkResponse(res.body.tags[0], 'tag', null, null, ['id', 'name', 'slug']);
+            });
+    });
+
+    it('Can request all tags with count.posts field and custom order', function () {
+        return request
+            .get(localUtils.API.getApiQuery(`tags/?key=${validKey}&include=count.posts&order=name%20DESC`))
+            .set('Origin', testUtils.API.getURL())
+            .set('Origin', config.get('url'))
+            .expect('Content-Type', /json/)
+            .expect('Cache-Control', testUtils.cacheRules.private)
+            .expect(200)
+            .then((res) => {
+                should.not.exist(res.headers['x-cache-invalidate']);
+                const jsonResponse = res.body;
+                should.exist(jsonResponse);
+                should.exist(jsonResponse.tags);
+                jsonResponse.tags.should.have.length(4);
+                localUtils.API.checkResponse(jsonResponse.tags[0], 'tag', ['count', 'url']);
+
+                jsonResponse.meta.pagination.should.have.property('page', 1);
+                jsonResponse.meta.pagination.should.have.property('limit', 15);
+                jsonResponse.meta.pagination.should.have.property('pages', 4);
+                jsonResponse.meta.pagination.should.have.property('total', 56);
+                jsonResponse.meta.pagination.should.have.property('next', 2);
+                jsonResponse.meta.pagination.should.have.property('prev', null);
+
+                jsonResponse.tags[0].url.should.eql(`${config.get('url')}/tag/kitchen-sink/`);
+                jsonResponse.tags[1].url.should.eql(`${config.get('url')}/tag/getting-started/`);
+
+                should.exist(jsonResponse.tags[0].count.posts);
+                jsonResponse.tags[0].count.posts.should.equal(2);
+            });
+    });
+
+    it('Browse tags with slug filter, should order in slug order', function () {
+        return request.get(localUtils.API.getApiQuery(`tags/?key=${validKey}&filter=slug:[kitchen-sink,bacon,chorizo]`))
+            .expect('Content-Type', /json/)
+            .expect('Cache-Control', testUtils.cacheRules.private)
+            .expect(200)
+            .then((res) => {
+                const jsonResponse = res.body;
+
+                jsonResponse.tags.should.be.an.Array().with.lengthOf(3);
+                jsonResponse.tags[0].slug.should.equal('kitchen-sink');
+                jsonResponse.tags[1].slug.should.equal('bacon');
+                jsonResponse.tags[2].slug.should.equal('chorizo');
             });
     });
 });