57425656721cf23f01dbea8cfb7cd01017855cb7
Rish <zrishabhgarg@gmail.com>
2020-09-01 17:44:33 +0530

    Added subscription cancel/continue button
    no issue
    
    - Allows member to cancel or continue their subscription at period end
    - Updates subscription cancellation setting in stripe

---
+++ ghost/portal/src/utils/date-time.js
@@ -0,0 +1,7 @@
+export const getDateString = (isoDate) => {
+    const date = new Date(isoDate);
+    const month = date.toDateString().substring(4, 7);
+    const day = date.toDateString().substring(8, 10);
+    const year = date.toDateString().substring(11, 15);
+    return `${day} ${month} ${year}`;
+};
