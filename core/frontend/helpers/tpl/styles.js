48b13504f54e77403256141fcfa8ebc3fe3acc42
Peter Zimon <zimo@ghost.org>
2020-09-07 18:24:11 +0200

    Refined CSS class names and copy
    no refs.
    - added 'gh' namespace to CSS classes and refined naming
    - refined copy in CTA

--- core/frontend/helpers/tpl/styles.js
+++ core/frontend/helpers/tpl/styles.js
@@ -1,6 +1,6 @@
 /* Default CSS Styles for helpers which are appended in theme via ghost_head */
-const contentHelper = `.post-upgrade-cta-content,
-.post-upgrade-cta {
+const contentHelper = `.gh-post-upgrade-cta-content,
+.gh-post-upgrade-cta {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -11,12 +11,12 @@ const contentHelper = `.post-upgrade-cta-content,
     font-size: 16px;
 }
 
-.post-upgrade-cta-content {
+.gh-post-upgrade-cta-content {
     border-radius: 8px;
     padding: 40px 4vw;
 }
 
-.post-upgrade-cta h2 {
+.gh-post-upgrade-cta h2 {
     color: #ffffff;
     font-size: 28px;
     letter-spacing: -0.2px;
@@ -24,17 +24,17 @@ const contentHelper = `.post-upgrade-cta-content,
     padding: 0;
 }
 
-.post-upgrade-cta p {
+.gh-post-upgrade-cta p {
     margin: 20px 0 0;
     padding: 0;
 }
 
-.post-upgrade-cta small {
+.gh-post-upgrade-cta small {
     font-size: 16px;
     letter-spacing: -0.2px;
 }
 
-.post-upgrade-cta a {
+.gh-post-upgrade-cta a {
     color: #ffffff;
     cursor: pointer;
     font-weight: 500;
@@ -42,14 +42,14 @@ const contentHelper = `.post-upgrade-cta-content,
     text-decoration: underline;
 }
 
-.post-upgrade-cta a:hover {
+.gh-post-upgrade-cta a:hover {
     color: #ffffff;
     opacity: 0.8;
     box-shadow: none;
     text-decoration: underline;
 }
 
-.post-upgrade-cta a.button {
+.gh-post-upgrade-cta a.gh-btn {
     display: block;
     background: #ffffff;
     text-decoration: none;
@@ -60,7 +60,7 @@ const contentHelper = `.post-upgrade-cta-content,
     font-weight: 600;
 }
 
-.post-upgrade-cta a.button:hover {
+.gh-post-upgrade-cta a.gh-btn:hover {
     opacity: 0.92;
 }`;
 
