a8202eb80d6f24afc996288f14873f58963d58bd
Nazar Gargol <nazargargol@gmail.com>
2020-09-10 11:20:38 +1200

    Fixed regression test for integrity check
    no issue
    
    - The file that is better suited for integrity check is the `*-default.yaml` config because it's the one that gets copied through when there is none or configuration is broken

--- test/unit/data/schema/integrity_spec.js
+++ test/unit/data/schema/integrity_spec.js
@@ -40,7 +40,7 @@ describe('DB version integrity', function () {
     // If this test is failing, then it is likely a change has been made that requires a DB version bump,
     // and the values above will need updating as confirmation
     it('should not change without fixing this test', function () {
-        const routesPath = path.join(config.getContentPath('settings'), 'routes.yaml');
+        const routesPath = path.join(config.getContentPath('settings'), 'routes-default.yaml');
         const defaultRoutes = validateFrontendSettings(yaml.safeLoad(fs.readFileSync(routesPath, 'utf-8')));
 
         const tablesNoValidation = _.cloneDeep(schema.tables);
