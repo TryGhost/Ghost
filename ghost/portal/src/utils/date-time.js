8947a2ddc1335b9111c79af75ba3ba6466051685
Rish <zrishabhgarg@gmail.com>
2020-09-16 23:35:13 +0530

    Removed preceding 0 in date string
    no issue
    
    - Removes preceding 0 in `date` section of a date string
    For ex. - 05 June -> 5 June

--- ghost/portal/src/utils/date-time.js
+++ ghost/portal/src/utils/date-time.js
@@ -1,7 +1,7 @@
 export const getDateString = (isoDate) => {
     const date = new Date(isoDate);
     const month = date.toDateString().substring(4, 7);
-    const day = date.toDateString().substring(8, 10);
+    const day = date.toDateString().substring(8, 10).replace(/^0+/g, '');
     const year = date.toDateString().substring(11, 15);
     return `${day} ${month} ${year}`;
 };
