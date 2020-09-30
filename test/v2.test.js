d0cd2bb97b122b46f2ac7ba12743c97b8cd2a355
Naz <nazargargol@gmail.com>
2020-09-30 14:58:35 +1300

    Fixed failing admin-api-schema test after merge

--- test/v2.test.js
+++ test/v2.test.js
@@ -6,6 +6,6 @@ const postSchema = require('@tryghost/admin-api-schema/v2/posts.json');
 describe('Exposes v2 schemas', function () {
     it('Can access posts resource schema', function () {
         postSchema.title.should.eql('posts');
-        Object.keys(postSchema.definitions.post.properties).length.should.equal(39);
+        Object.keys(postSchema.definitions.post.properties).length.should.equal(41);
     });
 });
