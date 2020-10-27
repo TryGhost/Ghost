94b69db65b463896898721362333468ef1c2d11f
Fabien O'Carroll <fabien@allou.is>
2020-10-27 10:44:06 +0000

    Refactored members-csv to remove unused columnsToExtract
    no-issue
    
     - Was not used by the importer and removed for simplicity.
     - Updates the header mapping to happen in place, rather than in a loop
     - Updates the parsing of values to give correct types

--- ghost/members-csv/lib/parse.js
+++ ghost/members-csv/lib/parse.js
@@ -1,134 +1,76 @@
 const Promise = require('bluebird');
+const pump = require('pump');
 const papaparse = require('papaparse');
-const _ = require('lodash');
 const fs = require('fs-extra');
 
-const mapRowsWithRegexes = (rows, columnsToExtract) => {
-    let results = [];
-    const columnMap = {};
-    // If CSV is single column - return all values including header
-    const headers = _.keys(rows[0]);
-
-    if (columnsToExtract.length === 1 && headers.length === 1) {
-        results = _.map(rows, function (value) {
-            let result = {};
-            result[columnsToExtract[0].name] = value[headers[0]];
-            return result;
-        });
-    } else {
-        // If there are multiple columns in csv file
-        // try to match headers using lookup value
-        _.map(columnsToExtract, function findMatches(column) {
-            _.each(headers, function checkheader(header) {
-                if (column.lookup.test(header)) {
-                    columnMap[column.name] = header;
-                }
-            });
-        });
-
-        results = _.map(rows, function evaluateRow(row) {
-            const result = {};
-            _.each(columnMap, function returnMatches(value, key) {
-                result[key] = row[value];
-            });
-            return result;
-        });
-    }
-
-    return results;
-};
-
-const mapRowsWithMappings = (rows, mapping) => {
-    const results = rows.map((row) => {
-        for (const key in mapping) {
-            row[key] = row[mapping[key]];
-
-            if (key !== mapping[key]) {
-                delete row[mapping[key]];
-            }
-        }
-
-        return row;
-    });
-
-    return results;
-};
-
-const readCSV = ({path, columnsToExtract, mapping}) => {
-    const rows = [];
-
+module.exports = (path, mapping, defaultLabels = []) => {
     return new Promise(function (resolve, reject) {
-        const readFile = fs.createReadStream(path);
-
-        readFile.on('err', function (err) {
-            reject(err);
-        })
-            .pipe(papaparse.parse(papaparse.NODE_STREAM_INPUT, {
-                header: true,
-                transform: function (value) {
-                    if (value === '') {
-                        return null;
+        const csvFileStream = fs.createReadStream(path);
+        const csvParserStream = papaparse.parse(papaparse.NODE_STREAM_INPUT, {
+            header: true,
+            transformHeader(_header) {
+                let header = _header;
+                if (mapping && Reflect.has(mapping, _header)) {
+                    header = mapping[_header];
+                }
+                if (header === 'subscribed_to_emails') {
+                    return 'subscribed';
+                }
+                return header;
+            },
+            transform(value, header) {
+                if (header === 'labels') {
+                    if (value && typeof value === 'string') {
+                        return value.split(',').map(name => ({name}));
                     }
-
-                    return value;
-                }
-            }))
-            .on('data', function (row) {
-                rows.push(row);
-            })
-            .on('end', function () {
-                let results = [];
-
-                if (columnsToExtract) {
-                    results = mapRowsWithRegexes(rows, columnsToExtract);
-                } else {
-                    results = mapRowsWithMappings(rows, mapping);
                 }
 
-                resolve(results);
-            });
+                if (header === 'subscribed_to_emails') {
+                    return value.toLowerCase() !== 'false';
+                }
+
+                if (header === 'complimentary_plan') {
+                    return value.toLowerCase() === 'true';
+                }
+
+                if (value === '') {
+                    return null;
+                }
+
+                if (value === 'undefined') {
+                    return null;
+                }
+
+                if (value.toLowerCase() === 'false') {
+                    return false;
+                }
+
+                if (value.toLowerCase() === 'true') {
+                    return true;
+                }
+
+                return value;
+            }
+        });
+
+        const rows = [];
+        const parsedCSVStream = pump(csvFileStream, csvParserStream, (err) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(rows);
+        });
+
+        parsedCSVStream.on('data', (row) => {
+            if (row.labels) {
+                row.labels = row.labels.concat(defaultLabels);
+            } else {
+                row.labels = defaultLabels;
+            }
+            rows.push(row);
+        });
     });
 };
 
-const parse = async (filePath, mapping) => {
-    const columnsToExtract = [{
-        name: 'email',
-        lookup: /^email/i
-    }, {
-        name: 'name',
-        lookup: /name/i
-    }, {
-        name: 'note',
-        lookup: /note/i
-    }, {
-        name: 'subscribed_to_emails',
-        lookup: /subscribed_to_emails/i
-    }, {
-        name: 'stripe_customer_id',
-        lookup: /stripe_customer_id/i
-    }, {
-        name: 'complimentary_plan',
-        lookup: /complimentary_plan/i
-    }, {
-        name: 'labels',
-        lookup: /labels/i
-    }, {
-        name: 'created_at',
-        lookup: /created_at/i
-    }];
-
-    const options = {
-        path: filePath
-    };
-
-    if (mapping) {
-        options.mapping = mapping;
-    } else {
-        options.columnsToExtract = columnsToExtract;
-    }
-
-    return await readCSV(options);
-};
-
-module.exports = parse;
-module.exports.readCSV = readCSV;
+// @TODO do we need this???
+module.exports.readCSV = ({path, mapping, defaultLabels}) => module.exports(path, mapping, defaultLabels);
