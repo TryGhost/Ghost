79bbd61de2d8c446b52b12a8aac9992b00f9c45e
Fabien O'Carroll <fabien@allou.is>
2020-11-03 14:46:26 +0000

    Added migration to populate email_recipient_filter
    no-issue
    
    This populates the email_recipient_filter column for existing posts
    based on 1. if they were sent as an email and 2. their visibility.

---
+++ core/server/data/migrations/versions/3.38/02-populate-email-recipient-filter-column.js
@@ -0,0 +1,34 @@
+const {createTransactionalMigration} = require('../../utils');
+const logging = require('../../../../../shared/logging');
+
+module.exports = createTransactionalMigration(
+    async function up(connection) {
+        logging.info('Updating email_recipient_filter values based on visibility and send_email_when_published');
+        await connection('posts')
+            .update('email_recipient_filter', 'paid')
+            .where({
+                send_email_when_published: true,
+                visibility: 'paid'
+            });
+
+        await connection('posts')
+            .update('email_recipient_filter', 'all')
+            .where({
+                send_email_when_published: true,
+                visibility: 'members'
+            });
+
+        await connection('posts')
+            .update('email_recipient_filter', 'all')
+            .where({
+                send_email_when_published: true,
+                visibility: 'public'
+            });
+    },
+
+    async function down(connection) {
+        logging.info('Updating email_recipient_filter values to none');
+        await connection('posts')
+            .update('email_recipient_filter', 'none');
+    }
+);
