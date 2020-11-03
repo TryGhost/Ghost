5715aa21553f7c15f44cabf287a1b5ed8d178179
Kukhyeon Heo <sainthkh@naver.com>
2020-11-04 08:08:32 +0900

    ✨ Added .yaml format support  in redirects configuration (#12187)
    closes #11085
    
    - Ghost has been using YAML format for other configurations (e.g. routes). The plan is to move to this format for all user-edited settings files. By default JSON format is still used in Ghost Admin API v2/v3, but will be changed to YAML in API v4. Check referenced issue for more context.
    - New format supports all the features available before. The main noticeable change is the structure of config file. It is now grouped by redirect HTTP code instead of specifying `"permanent": true | false` attribute for each config property. Example format for YAML config:
    ```
    302:
      /from-url/: /to-url/
    
    301:
      /category/([a-z0-9\-]+)/i: /tag/$1/
      /v([0-9\.]+)/docs/([a-z0-9\-]+)/i: /docs/$2/
    ```
    - Added 2 new endpoints: `POST redirects/upload` and `GET redirects/download`. These serve as an alias to current GET/POST `/redirects/json. "upload/download" naming pattern is introduced to match the convention with other resources that can be uploaded and downloaded (images, themes etc.). `/redirects/json`  endpoints will be removed in Admin API v4
    - The parsing code from `custom-redirects.js` has been moved to `frontend/services/redirects/settings.js`. This location is more appropriate for this logic and eventually `custom-redirects.js` middlewear might be moved into "frontend" as this middlewear plays a role mostly effecting that area.

--- core/frontend/services/redirects/settings.js
+++ core/frontend/services/redirects/settings.js
@@ -1,8 +1,8 @@
 const fs = require('fs-extra');
 const path = require('path');
-const Promise = require('bluebird');
 const moment = require('moment-timezone');
-
+const yaml = require('js-yaml');
+const Promise = require('bluebird');
 const validation = require('./validation');
 
 const config = require('../../../shared/config');
@@ -11,17 +11,6 @@ const errors = require('@tryghost/errors');
 
 const readRedirectsFile = (redirectsPath) => {
     return fs.readFile(redirectsPath, 'utf-8')
-        .then((content) => {
-            try {
-                content = JSON.parse(content);
-            } catch (err) {
-                throw new errors.BadRequestError({
-                    message: i18n.t('errors.general.jsonParse', {context: err.message})
-                });
-            }
-
-            return content;
-        })
         .catch((err) => {
             if (err.code === 'ENOENT') {
                 return Promise.resolve([]);
@@ -37,15 +26,118 @@ const readRedirectsFile = (redirectsPath) => {
         });
 };
 
-const setFromFilePath = (filePath) => {
-    const redirectsPath = path.join(config.getContentPath('data'), 'redirects.json');
-    const backupRedirectsPath = path.join(config.getContentPath('data'), `redirects-${moment().format('YYYY-MM-DD-HH-mm-ss')}.json`);
+const parseRedirectsFile = (content, ext) => {
+    if (ext === '.json') {
+        let redirects;
 
-    return fs.pathExists(redirectsPath)
-        .then((redirectExists) => {
-            if (!redirectExists) {
+        try {
+            redirects = JSON.parse(content);
+        } catch (err) {
+            throw new errors.BadRequestError({
+                message: i18n.t('errors.general.jsonParse', {context: err.message})
+            });
+        }
+
+        return redirects;
+    }
+
+    if (ext === '.yaml') {
+        let redirects = [];
+        let configYaml = yaml.safeLoad(content);
+
+        // yaml.safeLoad passes almost every yaml code.
+        // Because of that, it's hard to detect if there's an error in the file.
+        // But one of the obvious errors is the plain string output.
+        // Here we check if the user made this mistake.
+        if (typeof configYaml === 'string') {
+            throw new errors.BadRequestError({
+                message: i18n.t('errors.api.redirects.yamlParse'),
+                help: 'https://ghost.org/docs/api/handlebars-themes/routing/redirects/'
+            });
+        }
+
+        /**
+         * 302: Temporary redirects
+         */
+        for (const redirect in configYaml['302']) {
+            redirects.push({
+                from: redirect,
+                to: configYaml['302'][redirect],
+                permanent: false
+            });
+        }
+
+        /**
+         * 301: Permanent redirects
+         */
+        for (const redirect in configYaml['301']) {
+            redirects.push({
+                from: redirect,
+                to: configYaml['301'][redirect],
+                permanent: true
+            });
+        }
+
+        return redirects;
+    }
+
+    throw new errors.IncorrectUsageError();
+};
+
+const createRedirectsFilePath = (ext) => {
+    return path.join(config.getContentPath('data'), `redirects${ext}`);
+};
+
+const getRedirectsFilePath = async () => {
+    const yamlPath = createRedirectsFilePath('.yaml');
+    const jsonPath = createRedirectsFilePath('.json');
+
+    const yamlExists = await fs.pathExists(yamlPath);
+
+    if (yamlExists) {
+        return yamlPath;
+    }
+
+    const jsonExist = await fs.pathExists(jsonPath);
+
+    if (jsonExist) {
+        return jsonPath;
+    }
+
+    return null;
+};
+
+const getCurrentRedirectsFilePathSync = () => {
+    const yamlPath = createRedirectsFilePath('.yaml');
+    const jsonPath = createRedirectsFilePath('.json');
+
+    if (fs.existsSync(yamlPath)) {
+        return yamlPath;
+    }
+
+    if (fs.existsSync(jsonPath)) {
+        return jsonPath;
+    }
+
+    return null;
+};
+
+const getBackupRedirectsFilePath = (filePath) => {
+    const {dir, name, ext} = path.parse(filePath);
+
+    return path.join(dir, `${name}-${moment().format('YYYY-MM-DD-HH-mm-ss')}${ext}`);
+};
+
+// '.json' is the default here because 'yaml' redirects file format is added later in v3.
+// @TODO: change the default to '.yaml' in v4. It may be even removed if '.json' format is deprecated.
+const setFromFilePath = (filePath, ext = '.json') => {
+    return getRedirectsFilePath()
+        .then((redirectsFilePath) => {
+            if (!redirectsFilePath) {
                 return null;
             }
+
+            const backupRedirectsPath = getBackupRedirectsFilePath(redirectsFilePath);
 
             return fs.pathExists(backupRedirectsPath)
                 .then((backupExists) => {
@@ -56,21 +148,55 @@ const setFromFilePath = (filePath) => {
                     return fs.unlink(backupRedirectsPath);
                 })
                 .then(() => {
-                    return fs.move(redirectsPath, backupRedirectsPath);
+                    return fs.move(redirectsFilePath, backupRedirectsPath);
                 });
         })
         .then(() => {
             return readRedirectsFile(filePath)
                 .then((content) => {
+                    return parseRedirectsFile(content, ext);
+                })
+                .then((content) => {
                     validation.validate(content);
-                    return fs.writeFile(redirectsPath, JSON.stringify(content), 'utf-8');
+
+                    if (ext === '.json') {
+                        return fs.writeFile(createRedirectsFilePath('.json'), JSON.stringify(content), 'utf-8');
+                    }
+
+                    if (ext === '.yaml') {
+                        return fs.copy(filePath, createRedirectsFilePath('.yaml'));
+                    }
                 });
         });
 };
 
+// @TODO: When yaml has been changed as the default redirects file format,
+// change this like `const defaultRedirectsContent = ''`.
+// Default json content is []. But the default YAML content is an empty string.
+const defaultJsonFileContent = [];
+
 const get = () => {
-    return readRedirectsFile(path.join(config.getContentPath('data'), 'redirects.json'));
+    return getRedirectsFilePath().then((filePath) => {
+        if (filePath === null) {
+            return defaultJsonFileContent;
+        }
+
+        return readRedirectsFile(filePath).then((content) => {
+            return path.extname(filePath) === '.json'
+                ? parseRedirectsFile(content, '.json')
+                : content;
+        });
+    });
+};
+
+const loadRedirectsFile = () => {
+    const filePath = getCurrentRedirectsFilePathSync();
+    const content = fs.readFileSync(filePath);
+
+    return parseRedirectsFile(content, path.extname(filePath));
 };
 
 module.exports.get = get;
 module.exports.setFromFilePath = setFromFilePath;
+module.exports.getRedirectsFilePath = getRedirectsFilePath;
+module.exports.loadRedirectsFile = loadRedirectsFile;
