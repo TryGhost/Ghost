9feda2942289735a3510a13a203165e5eeeaa821
Peter Zimon <zimo@ghost.org>
2020-11-10 16:00:50 +0100

    Notification refinements
    no refs.
    - refined notification styles for single and double lines
    - refined copy

--- ghost/portal/src/components/Notification.styles.js
+++ ghost/portal/src/components/Notification.styles.js
@@ -15,7 +15,7 @@ const NotificationStyles = `
         top: 12px;
         right: 12px;
         width: 100%;
-        padding: 14px 44px 16px 20px;
+        padding: 14px 44px 18px 20px;
         max-width: 380px;
         min-height: 66px;
         font-size: 1.3rem;
@@ -39,7 +39,7 @@ const NotificationStyles = `
     .gh-portal-notification p {
         flex-grow: 1;
         font-size: 1.4rem;
-        line-height: 1.45em;
+        line-height: 1.5em;
         text-align: left;
         margin: 0;
         padding: 0 0 0 40px;
@@ -63,7 +63,7 @@ const NotificationStyles = `
 
     .gh-portal-notification-icon {
         position: absolute;
-        top: 21px;
+        top: calc(50% - 14px);
         left: 17px;
         width: 28px;
         height: 28px;
