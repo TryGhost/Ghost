57fceffd74165117029fc5944876e83496fe7299
Peter Zimon <zimo@ghost.org>
2020-11-13 12:18:19 +0100

    Refined newsletter settings modal
    no refs.
    - set fake sender, site icon and title dynamic
    - removed scrollbar for preview window

--- ghost/admin/app/components/modal-email-design-settings.js
+++ ghost/admin/app/components/modal-email-design-settings.js
@@ -3,6 +3,7 @@ import {inject as service} from '@ember/service';
 import {task} from 'ember-concurrency';
 
 export default ModalComponent.extend({
+    config: service(),
     settings: service(),
 
     showHeader: true,
