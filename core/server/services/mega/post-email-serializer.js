838f4e763a3b9998d17a0104fe514c2cb5a2e903
Fabien 'egg' O'Carroll <fabien@allou.is>
2020-11-13 13:18:47 +0000

    Added setting for newsletter footer content (#12359)
    no-issue
    
    We want to give users to ability to customise the content of their newsletter, and the first step
    toward that is a setting in which we can store text or html to embed in the template

--- core/server/services/mega/post-email-serializer.js
+++ core/server/services/mega/post-email-serializer.js
@@ -155,7 +155,8 @@ const serialize = async (postModel, options = {isBrowserPreview: false}) => {
     const templateSettings = {
         showSiteHeader: settingsCache.get('newsletter_show_header'),
         bodyFontCategory: settingsCache.get('newsletter_body_font_category'),
-        showBadge: settingsCache.get('newsletter_show_badge')
+        showBadge: settingsCache.get('newsletter_show_badge'),
+        footerContent: settingsCache.get('newsletter_footer_content')
     };
     let htmlTemplate = template({post, site: getSite(), templateSettings});
     if (options.isBrowserPreview) {
