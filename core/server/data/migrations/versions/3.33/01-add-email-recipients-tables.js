ee9988e2132939e22bf20f76254ea1c3d0b912e2
Daniel Lockyer <hi@daniellockyer.com>
2020-11-19 09:53:58 +0000

    Refactored 'add table' migrations to use helper
    - this removes a lot of code duplication and streamlines how we do
      things across the codebase

--- core/server/data/migrations/versions/3.33/01-add-email-recipients-tables.js
+++ core/server/data/migrations/versions/3.33/01-add-email-recipients-tables.js
@@ -1,35 +1,8 @@
-const Promise = require('bluebird');
-const logging = require('../../../../../shared/logging');
-const commands = require('../../../schema').commands;
+const {addTable, combineNonTransactionalMigrations} = require('../../utils');
 
-module.exports = {
-    async up({connection}) {
-        // table creation order is important because of foreign key constraints,
-        // email_recipients references email_batches so email_batches has to exist when creating
-        return Promise.each(['email_batches', 'email_recipients'], async (table) => {
-            const tableExists = await connection.schema.hasTable(table);
-
-            if (tableExists) {
-                return logging.warn(`Skipping add table "${table}" - already exists`);
-            }
-
-            logging.info(`Adding table: ${table}`);
-            return commands.createTable(table, connection);
-        });
-    },
-
-    async down({connection}) {
-        // table deletion order is important because of foreign key constraints,
-        // email_recipients references email_batches so it has to be deleted first to not break constraints
-        return Promise.each(['email_recipients', 'email_batches'], async (table) => {
-            const tableExists = await connection.schema.hasTable(table);
-
-            if (!tableExists) {
-                return logging.warn(`Skipping drop table "${table}" - does not exist`);
-            }
-
-            logging.info(`Dropping table: ${table}`);
-            return commands.deleteTable(table, connection);
-        });
-    }
-};
+// table order is important because of foreign key constraints,
+// email_recipients references email_batches so email_batches has to exist when creating
+module.exports = combineNonTransactionalMigrations(
+    addTable('email_batches'),
+    addTable('email_recipients')
+);
