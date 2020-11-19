2c3a3570b95f60c2f33c87a51087e992b39a2359
Naz <nazargargol@gmail.com>
2020-11-19 17:58:41 +1300

    ✨ Added support for worker threads in scheduled jobs
    no issue
    
    - When jobs are performing CPU intensive tasks they block main process'
    event loop. They also can cause memory leaks or unexpected crashes
    effectively crashing the parent proccess. To address these issues jobs need to be performed off of main
    process. Worker Threads (https://nodejs.org/dist/latest-v12.x/docs/api/worker_threads.html)
    are the best candidate for such work.
    - These changes introduce an integration on top of bree
    (https://github.com/breejs/bree/) which allows to run recurring
    jobs in worker thereads. It falls back to child process execution for
    Node v10 running without `--experimental-worker` flag.
    - bree was chosen not only because it gives a polyfill for older Node
    versions. It has support for some of the future use-cases Ghost is looking to
    implement, like scheduled jobs.
    - This changeset also includes a complete example of job running on an
    interval with a possibility for graceful shutdown

--- ghost/job-manager/test/job-manager.test.js
+++ ghost/job-manager/test/job-manager.test.js
@@ -1,6 +1,7 @@
 // Switch these lines once there are useful utils
 // const testUtils = require('./utils');
 require('./utils');
+const path = require('path');
 const sinon = require('sinon');
 const delay = require('delay');
 
@@ -58,19 +59,29 @@ describe('Job Manager', function () {
     });
 
     describe('Schedule Job', function () {
-        it('fails to run for invalid scheduling expression', function () {
+        it('fails to schedule for invalid scheduling expression', function () {
+            const jobManager = new JobManager(logging);
+
+            try {
+                jobManager.scheduleJob('invalid expression', 'jobName', {});
+            } catch (err) {
+                err.message.should.equal('Invalid schedule format');
+            }
+        });
+
+        it('fails to schedule for no job name', function () {
             const jobManager = new JobManager(logging);
 
             try {
                 jobManager.scheduleJob('invalid expression', () => {}, {});
             } catch (err) {
-                err.message.should.equal('Invalid schedule format');
+                err.message.should.equal('Name parameter should be present if job is a function');
             }
         });
     });
 
     describe('Shutdown', function () {
-        it('gracefully shuts down synchronous jobs', async function () {
+        it('gracefully shuts down a synchronous jobs', async function () {
             const jobManager = new JobManager(logging);
 
             jobManager.addJob(require('./jobs/timed-job'), 200);
@@ -80,6 +91,20 @@ describe('Job Manager', function () {
             await jobManager.shutdown();
 
             should(jobManager.queue.idle()).be.true();
+        });
+
+        it('gracefully shuts down an interval job', async function () {
+            const jobManager = new JobManager(logging);
+
+            jobManager.scheduleJob('every 5 seconds', path.resolve(__dirname, './jobs/graceful.js'));
+
+            await delay(1); // let the job execution kick in
+
+            should(Object.keys(jobManager.bree.workers).length).equal(1);
+
+            await jobManager.shutdown();
+
+            should(Object.keys(jobManager.bree.workers).length).equal(0);
         });
     });
 });
