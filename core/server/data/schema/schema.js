1ee22522172aebf517ddf5fba42f83e2d3a00c81
Fabien 'egg' O'Carroll <fabien@allou.is>
2020-11-23 15:36:14 +0000

    Added cancellation_reason column to stripe subscriptions (#12405)
    refs #12403 
    
    This will allow us to store the reason for each individual subscription
    cancellation.

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -416,6 +416,7 @@ module.exports = {
         plan_id: {type: 'string', maxlength: 255, nullable: false, unique: false},
         status: {type: 'string', maxlength: 50, nullable: false},
         cancel_at_period_end: {type: 'bool', nullable: false, defaultTo: false},
+        cancellation_reason: {type: 'string', maxlength: 500, nullable: true},
         current_period_end: {type: 'dateTime', nullable: false},
         start_date: {type: 'dateTime', nullable: false},
         default_payment_card_last4: {type: 'string', maxlength: 4, nullable: true},
