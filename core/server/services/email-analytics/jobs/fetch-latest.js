c001600bf490d6bff75c310d7ede9339c8b5b406
Kevin Ansfield <kevin@lookingsideways.co.uk>
2020-11-24 11:05:49 +0000

    let job manager handle errors

--- core/server/services/email-analytics/jobs/fetch-latest.js
+++ core/server/services/email-analytics/jobs/fetch-latest.js
@@ -8,41 +8,32 @@ const {parentPort} = require('worker_threads');
 const MAX_EVENTS = 3000;
 
 (async () => {
-    try {
-        const models = require('../../../models');
-        const settingsService = require('../../settings');
+    const models = require('../../../models');
+    const settingsService = require('../../settings');
 
-        // must be initialized before emailAnalyticsService is required otherwise
-        // requires are in the wrong order and settingsCache will always be empty
-        await models.init();
-        await settingsService.init();
+    // must be initialized before emailAnalyticsService is required otherwise
+    // requires are in the wrong order and settingsCache will always be empty
+    await models.init();
+    await settingsService.init();
 
-        const emailAnalyticsService = require('../');
+    const emailAnalyticsService = require('../');
 
-        const fetchStartDate = new Date();
-        logging.info('Starting email analytics fetch of latest events');
-        const eventStats = await emailAnalyticsService.fetchLatest({maxEvents: MAX_EVENTS});
-        logging.info(`Finished fetching ${eventStats.totalEvents} analytics events in ${Date.now() - fetchStartDate}ms`);
+    const fetchStartDate = new Date();
+    logging.info('Starting email analytics fetch of latest events');
+    const eventStats = await emailAnalyticsService.fetchLatest({maxEvents: MAX_EVENTS});
+    logging.info(`Finished fetching ${eventStats.totalEvents} analytics events in ${Date.now() - fetchStartDate}ms`);
 
-        const aggregateStartDate = new Date();
-        logging.info(`Starting email analytics aggregation for ${eventStats.emailIds.length} emails`);
-        await emailAnalyticsService.aggregateStats(eventStats);
-        logging.info(`Finished aggregating email analytics in ${Date.now() - aggregateStartDate}ms`);
+    const aggregateStartDate = new Date();
+    logging.info(`Starting email analytics aggregation for ${eventStats.emailIds.length} emails`);
+    await emailAnalyticsService.aggregateStats(eventStats);
+    logging.info(`Finished aggregating email analytics in ${Date.now() - aggregateStartDate}ms`);
 
-        if (parentPort) {
-            parentPort.postMessage('done');
-        } else {
-            // give the logging pipes time finish writing before exit
-            setTimeout(() => {
-                process.exit(0);
-            }, 1000);
-        }
-    } catch (error) {
-        logging.error(error);
-
+    if (parentPort) {
+        parentPort.postMessage('done');
+    } else {
         // give the logging pipes time finish writing before exit
         setTimeout(() => {
-            process.exit(1);
+            process.exit(0);
         }, 1000);
     }
 })();
