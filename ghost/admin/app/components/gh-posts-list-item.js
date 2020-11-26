f7d0f525aaad60db5feadb16cc7f22a3014c8b1d
Sanne de Vries <65487235+sanne-san@users.noreply.github.com>
2020-11-26 18:19:05 +0100

    ✨ Added email open rate to posts list in admin (#1772)
    no issue
    
    - added "Sends" and "Opens" columns to the posts list to see newsletter performance at a glance
    - "Sends" will show the type of members sent to (free, paid, all) as a tooltip
    - "Opens" shows open rate by default and total opens on hover

--- ghost/admin/app/components/gh-posts-list-item.js
+++ ghost/admin/app/components/gh-posts-list-item.js
@@ -1,10 +1,14 @@
 import Component from '@glimmer/component';
+import {action} from '@ember/object';
 import {formatPostTime} from 'ghost-admin/helpers/gh-format-post-time';
 import {inject as service} from '@ember/service';
+import {tracked} from '@glimmer/tracking';
 
 export default class GhPostsListItemComponent extends Component {
     @service session;
     @service settings;
+
+    @tracked isHovered = false;
 
     get authorNames() {
         return this.args.post.authors.map(author => author.name || author.email).join(', ');
@@ -19,10 +23,6 @@ export default class GhPostsListItemComponent extends Component {
         let {post} = this.args;
         let text = [];
 
-        if (post.emailRecipientFilter && post.emailRecipientFilter !== 'none') {
-            text.push(`and sent to ${post.emailRecipientFilter} members`);
-        }
-
         let formattedTime = formatPostTime(
             post.publishedAtUTC,
             {timezone: this.settings.get('timezone'), scheduled: true}
@@ -30,5 +30,15 @@ export default class GhPostsListItemComponent extends Component {
         text.push(formattedTime);
 
         return text.join(' ');
+    }
+
+    @action
+    mouseOver() {
+        this.isHovered = true;
+    }
+
+    @action
+    mouseLeave() {
+        this.isHovered = false;
     }
 }
