e2bdab50984a5ac91d7c27ad255ec20c631dae61
Naz <nazargargol@gmail.com>
2020-12-03 15:53:10 +1300

    Updated example of scheduled on off job using date

--- ghost/job-manager/test/examples/scheduled-one-off.js
+++ ghost/job-manager/test/examples/scheduled-one-off.js
@@ -1,36 +1,24 @@
-/* eslint-disable no-console */
-
+const path = require('path');
 const pWaitFor = require('p-wait-for');
-// const path = require('path');
-const setTimeoutPromise = require('util').promisify(setTimeout);
+const addSeconds = require('date-fns/addSeconds');
 const JobManager = require('../../lib/job-manager');
 
-const jobManager = new JobManager({
-    info: console.log,
-    warn: console.log,
-    error: console.log
-});
+const jobManager = new JobManager(console);
 
-process.on('SIGINT', () => {
-    shutdown('SIGINT');
-});
-
-async function shutdown(signal) {
-    console.log(`shutting down via: ${signal}`);
-
-    await jobManager.shutdown();
-}
+const isJobQueueEmpty = (bree) => {
+    return (Object.keys(bree.workers).length === 0)
+        && (Object.keys(bree.intervals).length === 0)
+        && (Object.keys(bree.timeouts).length === 0);
+};
 
 (async () => {
-    jobManager.scheduleJob('in 10 seconds', () => {
-        return require('../jobs/timed-job')();
-    }, {
+    const dateInTenSeconds = addSeconds(new Date(), 10);
+
+    jobManager.scheduleJob(dateInTenSeconds, path.resolve(__dirname, '../jobs/timed-job.js'), {
         ms: 2000
     }, 'one-off-scheduled-job');
 
-    await setTimeoutPromise(100); // allow job to get scheduled
-
-    await pWaitFor(() => (Object.keys(jobManager.bree.workers).length === 0) && (Object.keys(jobManager.bree.intervals).length === 0));
+    await pWaitFor(() => (isJobQueueEmpty(jobManager.bree)));
 
     process.exit(0);
 })();
