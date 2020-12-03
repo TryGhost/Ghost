d19ea808603136efa5ec5cdf254e8dba6b99c6b8
Daniel Lockyer <hi@daniellockyer.com>
2020-12-03 17:50:53 +0000

    Updated logging message for webhooks
    - removes double space and tidies up the output

--- core/server/services/webhooks/trigger.js
+++ core/server/services/webhooks/trigger.js
@@ -82,7 +82,7 @@ module.exports = (event, model) => {
                             retry: 5
                         };
 
-                        logging.info(`Trigger Webhook for  "${webhook.get('event')}" with url "${url}".`);
+                        logging.info(`Triggering webhook for "${webhook.get('event')}" with url "${url}"`);
 
                         request(url, opts)
                             .then(response.onSuccess(webhook))
