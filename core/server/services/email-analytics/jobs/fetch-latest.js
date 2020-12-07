c1e3788570a729c86c62fb46bcad9bed15b6319d
Naz <nazargargol@gmail.com>
2020-12-07 16:43:57 +1300

    Added central error handler to job manager
    refs https://github.com/TryGhost/Ghost-Utils/issues/118
    
    - Duplicating error handling across jobs is not best developer experience. Also, having custom error handling logic did not allow for recommended worker script behavior: allowing for unhandled exceptions to bubble up and be managed by parent process

--- core/server/services/email-analytics/jobs/fetch-latest.js
+++ core/server/services/email-analytics/jobs/fetch-latest.js
@@ -1,6 +1,5 @@
 const logging = require('../../../../shared/logging');
 const {parentPort} = require('bthreads');
-const sentry = require('../../../../shared/sentry');
 const debug = require('ghost-ignition').debug('jobs:email-analytics:fetch-latest');
 
 // recurring job to fetch analytics since the most recently seen event timestamp
@@ -28,46 +27,36 @@ if (parentPort) {
 }
 
 (async () => {
-    try {
-        const models = require('../../../models');
-        const settingsService = require('../../settings');
+    const models = require('../../../models');
+    const settingsService = require('../../settings');
 
-        // must be initialized before emailAnalyticsService is required otherwise
-        // requires are in the wrong order and settingsCache will always be empty
-        await models.init();
-        await settingsService.init();
+    // must be initialized before emailAnalyticsService is required otherwise
+    // requires are in the wrong order and settingsCache will always be empty
+    await models.init();
+    await settingsService.init();
 
-        const emailAnalyticsService = require('../');
+    const emailAnalyticsService = require('../');
 
-        const fetchStartDate = new Date();
-        debug('Starting email analytics fetch of latest events');
-        const eventStats = await emailAnalyticsService.fetchLatest();
-        const fetchEndDate = new Date();
-        debug(`Finished fetching ${eventStats.totalEvents} analytics events in ${fetchEndDate - fetchStartDate}ms`);
+    const fetchStartDate = new Date();
+    debug('Starting email analytics fetch of latest events');
+    const eventStats = await emailAnalyticsService.fetchLatest();
+    const fetchEndDate = new Date();
+    debug(`Finished fetching ${eventStats.totalEvents} analytics events in ${fetchEndDate - fetchStartDate}ms`);
 
-        const aggregateStartDate = new Date();
-        debug(`Starting email analytics aggregation for ${eventStats.emailIds.length} emails`);
-        await emailAnalyticsService.aggregateStats(eventStats);
-        const aggregateEndDate = new Date();
-        debug(`Finished aggregating email analytics in ${aggregateEndDate - aggregateStartDate}ms`);
+    const aggregateStartDate = new Date();
+    debug(`Starting email analytics aggregation for ${eventStats.emailIds.length} emails`);
+    await emailAnalyticsService.aggregateStats(eventStats);
+    const aggregateEndDate = new Date();
+    debug(`Finished aggregating email analytics in ${aggregateEndDate - aggregateStartDate}ms`);
 
-        logging.info(`Fetched ${eventStats.totalEvents} events and aggregated stats for ${eventStats.emailIds.length} emails and ${eventStats.memberIds.length} members in ${aggregateEndDate - fetchStartDate}ms`);
+    logging.info(`Fetched ${eventStats.totalEvents} events and aggregated stats for ${eventStats.emailIds.length} emails in ${aggregateEndDate - fetchStartDate}ms`);
 
-        if (parentPort) {
-            parentPort.postMessage('done');
-        } else {
-            // give the logging pipes time finish writing before exit
-            setTimeout(() => {
-                process.exit(0);
-            }, 1000);
-        }
-    } catch (error) {
-        logging.error(error);
-        sentry.captureException(error);
-
+    if (parentPort) {
+        parentPort.postMessage('done');
+    } else {
         // give the logging pipes time finish writing before exit
         setTimeout(() => {
-            process.exit(1);
+            process.exit(0);
         }, 1000);
     }
 })();
--- core/server/services/jobs/job-service.js
+++ core/server/services/jobs/job-service.js
@@ -5,7 +5,13 @@
 
 const JobManager = require('@tryghost/job-manager');
 const logging = require('../../../shared/logging');
+const sentry = require('../../../shared/sentry');
 
-const jobManager = new JobManager(logging);
+const errorHandler = (error, workerMeta) => {
+    logging.info(`Capturing error for worker during execution of job: ${workerMeta.name}`);
+    logging.error(error);
+    sentry.captureException(error);
+};
+const jobManager = new JobManager({logging, errorHandler});
 
 module.exports = jobManager;
