e59ee1a17e648618057e2f1eee88cb5735e2558f
Kevin Ansfield <kevin@ghost.org>
2020-12-08 12:20:17 +0000

    Added migration for members.email_open_rate column (#12457)
    refs https://github.com/TryGhost/Ghost/issues/12421
    
    - nullable so we can distinguish between members that have and haven't received any trackable emails
    - indexed because we'll be using this column for sorting

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -378,6 +378,7 @@ module.exports = {
         note: {type: 'string', maxlength: 2000, nullable: true},
         geolocation: {type: 'string', maxlength: 2000, nullable: true},
         subscribed: {type: 'bool', nullable: true, defaultTo: true},
+        email_open_rate: {type: 'integer', unsigned: true, nullable: true, index: true},
         created_at: {type: 'dateTime', nullable: false},
         created_by: {type: 'string', maxlength: 24, nullable: false},
         updated_at: {type: 'dateTime', nullable: true},
