a1dfda33968241d52ceccaa9bd94da9cb3e3bcb8
Kukhyeon Heo <sainthkh@naver.com>
2021-01-05 10:11:06 +0900

    🐛 Fixed redirects "to" query params forwarding  (#12333)
    ref #10898
    
    - The redirects configuration's `to` & `from` URL parameters used to ignore it's query string parameters, which resulted in unexpected behavior
    - Current changeset only partially fixes the issue. Now `to` URL's query parameters always take precedence over incoming query parameters and the rest of query parameters are passed through.

--- core/server/web/shared/middlewares/custom-redirects.js
+++ core/server/web/shared/middlewares/custom-redirects.js
@@ -1,5 +1,6 @@
 const express = require('../../../../shared/express');
 const url = require('url');
+const querystring = require('querystring');
 const debug = require('ghost-ignition').debug('web:shared:mw:custom-redirects');
 const config = require('../../../../shared/config');
 const urlUtils = require('../../../../shared/url-utils');
@@ -52,10 +53,14 @@ _private.registerRoutes = () => {
             customRedirectsRouter.get(new RegExp(redirect.from, options), function (req, res) {
                 const maxAge = redirect.permanent ? config.get('caching:customRedirects:maxAge') : 0;
                 const toURL = url.parse(redirect.to);
+                const toURLParams = querystring.parse(toURL.query);
                 const currentURL = url.parse(req.url);
+                const currentURLParams = querystring.parse(currentURL.query);
+                const params = Object.assign({}, currentURLParams, toURLParams);
+                const search = querystring.stringify(params);
 
                 toURL.pathname = currentURL.pathname.replace(new RegExp(redirect.from, options), toURL.pathname);
-                toURL.search = currentURL.search;
+                toURL.search = search !== '' ? `?${search}` : null;
 
                 /**
                  * Only if the URL is internal should we prepend the Ghost subdirectory
