a3d115ec0b59fb97b032354a59246cea098b0930
Rishabh Garg <zrishabhgarg@gmail.com>
2021-01-15 20:01:40 +0530

    ✨ Added new FirstPromoter integration (#1825)
    no issue
    
    Adds new FirstPromoter integration on the integrations page. FirstPromoter enables sites to launch their own members referral program, and integration allows Site admins to directly add their FirstPromoter tracking ID in the settings to enable FirstPromoter script on their site.

---
+++ ghost/admin/app/routes/settings/integrations/firstpromoter.js
@@ -0,0 +1,39 @@
+import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
+import CurrentUserSettings from 'ghost-admin/mixins/current-user-settings';
+import {inject as service} from '@ember/service';
+
+export default AuthenticatedRoute.extend(CurrentUserSettings, {
+    settings: service(),
+
+    beforeModel() {
+        this._super(...arguments);
+        return this.get('session.user')
+            .then(this.transitionAuthor())
+            .then(this.transitionEditor())
+            .then(this.settings.reload());
+    },
+
+    actions: {
+        save() {
+            this.controller.send('save');
+        },
+
+        willTransition(transition) {
+            let controller = this.controller;
+            let modelIsDirty = this.settings.get('hasDirtyAttributes');
+
+            if (modelIsDirty) {
+                transition.abort();
+                controller.send('toggleLeaveSettingsModal', transition);
+                return;
+            }
+        }
+    },
+
+    buildRouteInfoMetadata() {
+        return {
+            titleToken: 'FirstPromoter'
+        };
+    }
+
+});
