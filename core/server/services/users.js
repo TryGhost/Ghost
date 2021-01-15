567b055debf6d35a8fc778d13e2d04c1a043c920
Thibaut Patel <thibaut.patel@gmail.com>
2021-01-15 16:34:06 +0000

    Moved the users service to use our DI pattern
    refs #12537

--- core/server/services/users.js
+++ core/server/services/users.js
@@ -1,37 +1,64 @@
+// @ts-check
 const path = require('path');
-const dbBackup = require('../data/db/backup');
-const models = require('../models');
 
-async function destroyUser(frameOptions) {
-    const backupPath = await dbBackup.backup();
-    const parsedFileName = path.parse(backupPath);
-    const filename = `${parsedFileName.name}${parsedFileName.ext}`;
+/**
+ * @typedef {Object} IdbBackup
+ * @prop {() => Promise<string>} backup
+ */
 
-    return models.Base.transaction((t) => {
-        frameOptions.transacting = t;
+/**
+ * @typedef {Object} IModels
+ * @prop {Object} Base
+ * @prop {(callback: function) => Promise} Base.transaction
+ * @prop {Object} Post
+ * @prop {(frameOptions: Object) => Promise} Post.destroyByAuthor
+ * @prop {Object} ApiKey
+ * @prop {(Object) => Promise} ApiKey.destroy
+ * @prop {Object} ApiKey.NotFoundError
+ * @prop {Object} User
+ * @prop {(Object) => Promise} User.destroy
+ */
 
-        return models.Post.destroyByAuthor(frameOptions)
-            .then(() => {
-                return models.ApiKey.destroy({
-                    ...frameOptions,
-                    require: true,
-                    destroyBy: {
-                        user_id: frameOptions.id
-                    }
-                }).catch((err) => {
-                    if (err instanceof models.ApiKey.NotFoundError) {
-                        return; //Do nothing here as it's ok
-                    }
-                    throw err;
-                });
-            })
-            .then(() => {
-                return models.User.destroy(Object.assign({status: 'all'}, frameOptions));
-            })
-            .then(() => filename);
-    });
+class Users {
+    /**
+     * @param {Object} dependencies
+     * @param {IdbBackup} dependencies.dbBackup
+     * @param {IModels} dependencies.models
+     */
+    constructor({dbBackup, models}) {
+        this.dbBackup = dbBackup;
+        this.models = models;
+    }
+
+    async destroyUser(frameOptions) {
+        const backupPath = await this.dbBackup.backup();
+        const parsedFileName = path.parse(backupPath);
+        const filename = `${parsedFileName.name}${parsedFileName.ext}`;
+
+        return this.models.Base.transaction((t) => {
+            frameOptions.transacting = t;
+
+            return this.models.Post.destroyByAuthor(frameOptions)
+                .then(() => {
+                    return this.models.ApiKey.destroy({
+                        ...frameOptions,
+                        require: true,
+                        destroyBy: {
+                            user_id: frameOptions.id
+                        }
+                    }).catch((err) => {
+                        if (err instanceof this.models.ApiKey.NotFoundError) {
+                            return; //Do nothing here as it's ok
+                        }
+                        throw err;
+                    });
+                })
+                .then(() => {
+                    return this.models.User.destroy(Object.assign({status: 'all'}, frameOptions));
+                })
+                .then(() => filename);
+        });
+    }
 }
 
-module.exports = {
-    destroyUser
-};
+module.exports = Users;
