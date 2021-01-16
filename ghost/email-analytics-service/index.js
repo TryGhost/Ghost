88c648636c3b9fcd0e118e4da6d558f296e1dece
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-01-16 16:22:52 +0000

    Initial update of email analytics packages to work as external modules
    refs https://github.com/TryGhost/Ghost/pull/12541
    
    - make `EventProcessor` a super-class designed to be inherited from in consumer applications for application-level implementation
      - helps to keep application-level concerns for event handling (eg, what to do with spam complaints) and things like application database knowledge in the consumer
    - removed all database knowledge from `EmailAnalyticsService`
      - requires a `queries` option to be passed in that lets the consuming application provide knowledge and define how fetched stats should be aggregated

---
+++ ghost/email-analytics-service/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    EmailAnalyticsService: require('./lib/email-analytics-service'),
+    EventProcessingResult: require('./lib/event-processing-result'),
+    EventProcessor: require('./lib/event-processor')
+};
---
+++ ghost/email-analytics-service/test/event-processing-service.test.js
@@ -0,0 +1,7 @@
+// Switch these lines once there are useful utils
+// const testUtils = require('./utils');
+require('./utils');
+
+describe('EventProcessingService', function () {
+
+});
