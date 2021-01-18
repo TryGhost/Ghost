ff3d8aea370e23517584bb2b259b35de5feb30a6
Fabien O'Carroll <fabien@allou.is>
2021-01-18 12:36:02 +0000

    fixup! Added multiple use grace period to tokens

--- test/regression/models/model_single_use_token_spec.js
+++ test/regression/models/model_single_use_token_spec.js
@@ -1,13 +1,20 @@
 const models = require('../../../core/server/models');
 const should = require('should');
+const sinon = require('sinon');
 
+let clock;
 describe('Regression: models/single-use-token', function () {
     before(function () {
         models.init();
+        clock = sinon.useFakeTimers();
+    });
+
+    after(function () {
+        clock.restore();
     });
 
     describe('findOne', function () {
-        it('Does not allow the same token to be read twice', async function () {
+        it('Does not allow the same token to be read twice after the grace period', async function () {
             const insertedToken = await models.SingleUseToken.add({
                 data: 'some_data'
             }, {});
@@ -18,6 +25,8 @@ describe('Regression: models/single-use-token', function () {
 
             should.exist(tokenFirstRead);
             should.equal(tokenFirstRead.id, insertedToken.id);
+
+            clock.tick(10000);
 
             const tokenSecondRead = await models.SingleUseToken.findOne({
                 token: insertedToken.get('token')
