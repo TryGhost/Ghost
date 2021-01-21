23e50c6cccc4f7876d6a0fe88ef6615e4dc07b14
Thibaut Patel <thibaut@ghost.org>
2021-01-21 10:00:25 +0100

    Pointed the v3 unit tests to the actual v3 code
    refs https://github.com/TryGhost/Team/issues/221

--- test/unit/api/v3/utils/serializers/output/utils/date_spec.js
+++ test/unit/api/v3/utils/serializers/output/utils/date_spec.js
@@ -1,7 +1,7 @@
 const should = require('should');
 const sinon = require('sinon');
 const settingsCache = require('../../../../../../../../core/server/services/settings/cache');
-const dateUtil = require('../../../../../../../../core/server/api/canary/utils/serializers/output/utils/date');
+const dateUtil = require('../../../../../../../../core/server/api/v3/utils/serializers/output/utils/date');
 
 describe('Unit: v3/utils/serializers/output/utils/date', function () {
     afterEach(function () {
