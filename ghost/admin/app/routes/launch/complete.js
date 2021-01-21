a4db6b2b6b65e5d773cc31fc245e680b89a71d4b
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-01-21 09:02:19 +0000

    Reverted launch wizard prototype
    no issue
    
    - removing launch wizard from master ready to re-apply in 4.0 branch

--- ghost/admin/app/routes/launch/complete.js
+++
@@ -1,4 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class LaunchCompleteRoute extends Route {
-}
--- ghost/admin/app/routes/launch/index.js
+++
@@ -1,7 +0,0 @@
-import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
-
-export default class LaunchIndexRoute extends AuthenticatedRoute {
-    beforeModel() {
-        this.transitionTo('launch.customise-design');
-    }
-}
