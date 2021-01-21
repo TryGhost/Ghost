a6b11b8c0c1fdb61339593227e5685eb40e4a452
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-01-21 09:11:51 +0000

    Added blank dashboard screen
    no issue
    
    - adds blank `/dashboard` route ready for future 4.0 related changes
    - `/` now redirects to `/dashboard` instead of `/site`
    - adds "Dashboard" link to main nav menu

---
+++ ghost/admin/tests/acceptance/dashboard-test.js
@@ -0,0 +1,35 @@
+import {authenticateSession} from 'ember-simple-auth/test-support';
+import {currentURL, visit} from '@ember/test-helpers';
+import {describe, it} from 'mocha';
+import {expect} from 'chai';
+import {setupApplicationTest} from 'ember-mocha';
+import {setupMirage} from 'ember-cli-mirage/test-support';
+
+describe('Acceptance: Dashboard', function () {
+    const hooks = setupApplicationTest();
+    setupMirage(hooks);
+
+    it('is not accessible when logged out', async function () {
+        await visit('/dashboard');
+        expect(currentURL()).to.equal('/signin');
+    });
+
+    describe('when logged in', function () {
+        beforeEach(async function () {
+            let role = this.server.create('role', {name: 'Administrator'});
+            this.server.create('user', {roles: [role]});
+
+            return await authenticateSession();
+        });
+
+        it('can visit /dashboard', async function () {
+            await visit('/dashboard');
+            expect(currentURL()).to.equal('/dashboard');
+        });
+
+        it('/ redirects to /dashboard', async function () {
+            await visit('/');
+            expect(currentURL()).to.equal('/dashboard');
+        });
+    });
+});
