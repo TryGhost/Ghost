d6333336c0b09d575f2ca669703c7c27a7aee9bf
Peter Zimon <zimo@ghost.org>
2021-01-22 12:24:45 +0100

    Separate theme design (#1831)
    * Separated theme and design settings

---
+++ ghost/admin/app/routes/settings/theme/uploadtheme.js
@@ -0,0 +1,18 @@
+import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
+
+export default AuthenticatedRoute.extend({
+
+    model() {
+        return this.store.findAll('theme');
+    },
+
+    setupController(controller, model) {
+        controller.set('themes', model);
+    },
+
+    actions: {
+        cancel() {
+            this.transitionTo('settings.theme');
+        }
+    }
+});
