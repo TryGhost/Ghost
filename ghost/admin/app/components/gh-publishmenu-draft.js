d94494b37e8d90df9eb3bea36345f5e71758d2ba
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-01-25 09:23:03 +0000

    Added post.{isPost,isPage} computed properties
    no issue
    
    - removes the need to check `post.displayName === 'post/page'` any time we need a conditional on post type

--- ghost/admin/app/components/gh-publishmenu-draft.js
+++ ghost/admin/app/components/gh-publishmenu-draft.js
@@ -47,10 +47,10 @@ export default Component.extend({
         return '';
     }),
 
-    canSendEmail: computed('feature.labs.members', 'post.{displayName,email}', 'settings.{mailgunApiKey,mailgunDomain,mailgunBaseUrl}', 'config.mailgunIsConfigured', function () {
+    canSendEmail: computed('feature.labs.members', 'post.{isPost,email}', 'settings.{mailgunApiKey,mailgunDomain,mailgunBaseUrl}', 'config.mailgunIsConfigured', function () {
         let membersEnabled = this.feature.get('labs.members');
         let mailgunIsConfigured = this.get('settings.mailgunApiKey') && this.get('settings.mailgunDomain') && this.get('settings.mailgunBaseUrl') || this.get('config.mailgunIsConfigured');
-        let isPost = this.post.displayName === 'post';
+        let isPost = this.post.isPost;
         let hasSentEmail = !!this.post.email;
 
         return membersEnabled && mailgunIsConfigured && isPost && !hasSentEmail;
