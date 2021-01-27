50a0c50c85dc490e74fb9760cd327b8ae1c8ec3b
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-01-27 23:16:54 +0000

    Added accent colour to customise design step of launch wizard
    refs https://github.com/TryGhost/Team/issues/451
    
    - adds accent colour picker to "customise design" step
    - updates colour picker to save & refresh 500ms after a value change
    - saves and refreshes immediately on blur of input field unless value hasn't changed since last save and refresh

--- ghost/admin/app/components/gh-launch-wizard/customise-design.js
+++ ghost/admin/app/components/gh-launch-wizard/customise-design.js
@@ -6,7 +6,10 @@ import {
     IMAGE_MIME_TYPES
 } from 'ghost-admin/components/gh-image-uploader';
 import {action} from '@ember/object';
+import {htmlSafe} from '@ember/string';
 import {inject as service} from '@ember/service';
+import {task} from 'ember-concurrency-decorators';
+import {timeout} from 'ember-concurrency';
 import {tracked} from '@glimmer/tracking';
 
 export default class GhLaunchWizardCustomiseDesignComponent extends Component {
@@ -18,6 +21,22 @@ export default class GhLaunchWizardCustomiseDesignComponent extends Component {
     iconMimeTypes = ICON_MIME_TYPES;
     imageExtensions = IMAGE_EXTENSIONS;
     imageMimeTypes = IMAGE_MIME_TYPES;
+
+    get accentColor() {
+        const color = this.settings.get('accentColor');
+        if (color && color[0] === '#') {
+            return color.slice(1);
+        }
+        return color;
+    }
+
+    get accentColorPickerValue() {
+        return this.settings.get('accentColor') || '#ffffff';
+    }
+
+    get accentColorBgStyle() {
+        return htmlSafe(`background-color: ${this.accentColorPickerValue}`);
+    }
 
     constructor() {
         super(...arguments);
@@ -43,6 +62,56 @@ export default class GhLaunchWizardCustomiseDesignComponent extends Component {
         this.settings.set(imageName, '');
         await this.settings.save();
         this.refreshPreview();
+    }
+
+    @action
+    async updateAccentColor(event) {
+        let newColor = event.target.value;
+        const oldColor = this.settings.get('accentColor');
+
+        // reset errors and validation
+        this.settings.errors.remove('accentColor');
+        this.settings.hasValidated.removeObject('accentColor');
+
+        if (newColor === '') {
+            if (newColor === oldColor) {
+                return;
+            }
+
+            // clear out the accent color
+            this.settings.set('accentColor', '');
+            await this.settings.save();
+            this.refreshPreview();
+            return;
+        }
+
+        // accentColor will be null unless the user has input something
+        if (!newColor) {
+            newColor = oldColor;
+        }
+
+        if (newColor[0] !== '#') {
+            newColor = `#${newColor}`;
+        }
+
+        if (newColor.match(/#[0-9A-Fa-f]{6}$/)) {
+            if (newColor === oldColor) {
+                return;
+            }
+
+            this.settings.set('accentColor', newColor);
+            await this.settings.save();
+            this.refreshPreview();
+        } else {
+            this.settings.errors.add('accentColor', 'The colour should be in valid hex format');
+            this.settings.hasValidated.pushObject('accentColor');
+        }
+    }
+
+    @task({restartable: true})
+    *debounceUpdateAccentColor(event) {
+        yield timeout(500);
+        this.updateAccentColor(event);
     }
 
     refreshPreview() {
