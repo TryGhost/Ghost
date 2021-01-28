256b77b0e4b3a83cb742940f6717843e34611751
Hannah Wolfe <erisds@gmail.com>
2021-01-28 15:10:34 +0000

    Moved sqlite3 override rules to db connection lib
    - We have an already established location for special rules based on db client
    - Removes the need to require config inside of overrides

--- core/server/data/db/connection.js
+++ core/server/data/db/connection.js
@@ -12,7 +12,12 @@ function configure(dbConfig) {
     const client = dbConfig.client;
 
     if (client === 'sqlite3') {
+        // Backwards compatibility with old knex behaviour
         dbConfig.useNullAsDefault = Object.prototype.hasOwnProperty.call(dbConfig, 'useNullAsDefault') ? dbConfig.useNullAsDefault : true;
+
+        // Force bthreads to use child_process backend until a worker_thread-compatible version of sqlite3 is published
+        // https://github.com/mapbox/node-sqlite3/issues/1386
+        process.env.BTHREADS_BACKEND = 'child_process';
     }
 
     if (client === 'mysql') {
