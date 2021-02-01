2c1b535ad9dd3c9600d5f4d822b54d72f3c10281
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-02-01 09:23:10 +0000

    Fixed missing seo description in post preview modal
    refs https://github.com/TryGhost/Team/issues/459
    
    - the SERP description was not falling back to the server-side generated `excerpt` field on the post
    - removed outdated dynamic description generation from markdown card, should be replaced with dynamic description generation from mobiledoc if needed

--- ghost/admin/app/components/modal-post-preview/social.js
+++ ghost/admin/app/components/modal-post-preview/social.js
@@ -1,5 +1,4 @@
 import Component from '@glimmer/component';
-import formatMarkdown from 'ghost-admin/utils/format-markdown';
 import {inject as service} from '@ember/service';
 
 export default class ModalPostPreviewSocialComponent extends Component {
@@ -9,7 +8,6 @@ export default class ModalPostPreviewSocialComponent extends Component {
     get _fallbackDescription() {
         return this.args.post.customExcerpt ||
             this.serpDescription ||
-            this.args.post.excerpt ||
             this.settings.get('description');
     }
 
@@ -43,27 +41,7 @@ export default class ModalPostPreviewSocialComponent extends Component {
     }
 
     get serpDescription() {
-        const metaDescription = this.args.post.metaDescription;
-
-        if (metaDescription) {
-            return metaDescription;
-        }
-
-        const mobiledoc = this.args.post.scratch;
-        const [markdownCard] = (mobiledoc && mobiledoc.cards) || [];
-        const markdown = markdownCard && markdownCard[1] && markdownCard[1].markdown;
-
-        let serpDescription;
-
-        const div = document.createElement('div');
-        div.innerHTML = formatMarkdown(markdown, false);
-
-        // Strip HTML
-        serpDescription = div.textContent;
-        // Replace new lines and trim
-        serpDescription = serpDescription.replace(/\n+/g, ' ').trim();
-
-        return serpDescription;
+        return this.args.post.metaDescription || this.args.post.excerpt;
     }
 
     // Facebook
