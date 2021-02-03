c262dc2a6eff3438f02995a8e6ee4e1602fc5d3c
Fabien O'Carroll <fabien@allou.is>
2021-02-03 16:15:04 +0000

    Added members_subscribe_events table
    refs https://github.com/TryGhost/Ghost/issues/12602

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -397,6 +397,13 @@ module.exports = {
         plan_amount: {type: 'integer', nullable: false},
         plan_currency: {type: 'string', maxLength: 3, nullable: false}
     },
+    members_subscribe_events: {
+        id: {type: 'string', maxlength: 24, nullable: false, primary: true},
+        member_id: {type: 'string', maxlength: 24, nullable: false, unique: false, references: 'members.id', cascadeDelete: true},
+        subscribed: {type: 'bool', nullable: false, defaultTo: true},
+        created_at: {type: 'dateTime', nullable: false},
+        source: {type: 'string', maxlength: 50, nullable: true}
+    },
     actions: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
         resource_id: {type: 'string', maxlength: 24, nullable: true},
