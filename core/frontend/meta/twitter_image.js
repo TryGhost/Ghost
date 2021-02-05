ec22b9c699ea817718073a59ad3901e00294ef6d
Naz <nazargargol@gmail.com>
2021-02-05 18:50:11 +1300

    Updated twitter/og structured data rules
    refs https://github.com/TryGhost/Ghost/issues/10921
    refs https://github.com/TryGhost/Ghost/pull/11068
    
    - When there is no twitter or og image specified for a post or a page the new default falls back to appropriate site-wide twitter/og image or site's cover image.
    - New rules of follback follow this logic:
    
    post/page/amp = post.twitter_image || post.feature_image || settings.twitter_image || settings.cover_image;
    
    post/page/amp = post.og_image || post.feature_image  || settings.og_image || settings.cover_image;

--- core/frontend/meta/twitter_image.js
+++ core/frontend/meta/twitter_image.js
@@ -17,6 +17,10 @@ function getTwitterImage(data) {
             return urlUtils.relativeToAbsolute(contextObject.twitter_image);
         } else if (contextObject.feature_image) {
             return urlUtils.relativeToAbsolute(contextObject.feature_image);
+        } else if (settingsCache.get('twitter_image')) {
+            return urlUtils.relativeToAbsolute(settingsCache.get('twitter_image'));
+        } else if (settingsCache.get('cover_image')) {
+            return urlUtils.relativeToAbsolute(settingsCache.get('cover_image'));
         }
     }
 
