0d250d0bffa47d2ccb9a6462defcbd96f36281ed
Fabien O'Carroll <fabien@allou.is>
2021-02-09 09:26:14 +0000

    Added members_email_change_events table
    refs https://github.com/TryGhost/Ghost/issues/12602

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -368,6 +368,12 @@ module.exports = {
         member_id: {type: 'string', maxlength: 24, nullable: false, references: 'members.id', cascadeDelete: true},
         created_at: {type: 'dateTime', nullable: false}
     },
+    members_email_change_events: {
+        id: {type: 'string', maxlength: 24, nullable: false, primary: true},
+        member_id: {type: 'string', maxlength: 24, nullable: false, references: 'members.id', cascadeDelete: true},
+        email: {type: 'string', maxlength: 191, nullable: false, unique: false, validations: {isEmail: true}},
+        created_at: {type: 'dateTime', nullable: false}
+    },
     labels: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
         name: {type: 'string', maxlength: 191, nullable: false, unique: true},
