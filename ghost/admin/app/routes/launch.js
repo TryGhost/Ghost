272d97eec50256ae37632412e56571c58ed76240
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-02-09 12:07:44 +0000

    Fixed liquid-wormhole error when directly accessing /launch
    refs https://github.com/TryGhost/Admin/commit/27962488f061d76d1f80e26f1fa33f4fb706b90c
    
    - liquid-wormhole was throwing an error with the previous approach because it appeared to be trying to run it's destroy routines after the element had already been removed or hadn't been rendered yet

--- ghost/admin/app/routes/launch.js
+++ ghost/admin/app/routes/launch.js
@@ -3,6 +3,17 @@ import {inject as service} from '@ember/service';
 
 export default class LaunchRoute extends AuthenticatedRoute {
     @service session;
+    @service ui;
+
+    activate() {
+        // disable before rendering template to avoid issues with liquid-wormhole
+        // attempting to destroy elements mid-render if disabled via component hooks
+        this.ui.set('showTour', false);
+    }
+
+    deactivate() {
+        this.ui.set('showTour', true);
+    }
 
     beforeModel() {
         super.beforeModel(...arguments);
