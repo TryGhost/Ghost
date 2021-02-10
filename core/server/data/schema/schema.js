7f374f19df85d4d9322f1418df42c909800e23ff
Fabien O'Carroll <fabien@allou.is>
2021-02-10 09:26:24 +0000

    Added members_paid_subscription_events
    refs https://github.com/TryGhost/Ghost/issues/12602

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -384,6 +384,20 @@ module.exports = {
         },
         created_at: {type: 'dateTime', nullable: false}
     },
+    members_paid_subscription_events: {
+        id: {type: 'string', maxlength: 24, nullable: false, primary: true},
+        member_id: {type: 'string', maxlength: 24, nullable: false, references: 'members.id', cascadeDelete: true},
+        from_plan: {type: 'string', maxlength: 50, nullable: true},
+        to_plan: {type: 'string', maxlength: 50, nullable: true},
+        currency: {type: 'string', maxLength: 3, nullable: false},
+        source: {
+            type: 'string', maxlength: 50, nullable: false, validations: {
+                isIn: [['stripe']]
+            }
+        },
+        mrr_delta: {type: 'integer', nullable: false},
+        created_at: {type: 'dateTime', nullable: false}
+    },
     labels: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
         name: {type: 'string', maxlength: 191, nullable: false, unique: true},
