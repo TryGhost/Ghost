db0e8fe0c552804e9b1f1c0ce20dbdbacf28a8b0
Fabien 'egg' O'Carroll <fabien@allou.is>
2021-02-11 12:45:18 +0000

    Updated members_paid_subscription_events table (#12636)
    refs https://github.com/TryGhost/Ghost/issues/12602
    
    This allows us to store the stripe_id of the subscription events

--- test/unit/data/schema/integrity_spec.js
+++ test/unit/data/schema/integrity_spec.js
@@ -32,7 +32,7 @@ const defaultSettings = require('../../../../core/server/data/schema/default-set
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '091a6cf44f3eb51115f0e1e422db4db6';
+    const currentSchemaHash = '801faa4e969102a3840191b19e339e55';
     const currentFixturesHash = '370d0da0ab7c45050b2ff30bce8896ba';
     const currentSettingsHash = '162f9294cc427eb32bc0577006c385ce';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
