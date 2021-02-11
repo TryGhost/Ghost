0bd52fc87402d1dd54167a25a07a8b253c2d5588
Thibaut Patel <thibaut@ghost.org>
2021-02-11 19:13:46 +0100

    Added the frontmatter column to meta
    issue https://github.com/TryGhost/Team/issues/452

--- test/unit/data/schema/integrity_spec.js
+++ test/unit/data/schema/integrity_spec.js
@@ -32,7 +32,7 @@ const defaultSettings = require('../../../../core/server/data/schema/default-set
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '801faa4e969102a3840191b19e339e55';
+    const currentSchemaHash = '860fafa2fc5f78df158ddd8254d4993b';
     const currentFixturesHash = '370d0da0ab7c45050b2ff30bce8896ba';
     const currentSettingsHash = '162f9294cc427eb32bc0577006c385ce';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
