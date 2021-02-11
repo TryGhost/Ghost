b3c007c22f4fa34e016ecc73eb66c6e0a0f66281
Fabien O'Carroll <fabien@allou.is>
2021-02-11 10:35:48 +0000

    Fixed db tests

--- test/unit/data/schema/integrity_spec.js
+++ test/unit/data/schema/integrity_spec.js
@@ -32,7 +32,7 @@ const defaultSettings = require('../../../../core/server/data/schema/default-set
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '9dc50d6350aa7eb4030a0e423b99e4b0';
+    const currentSchemaHash = '091a6cf44f3eb51115f0e1e422db4db6';
     const currentFixturesHash = '370d0da0ab7c45050b2ff30bce8896ba';
     const currentSettingsHash = '162f9294cc427eb32bc0577006c385ce';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
