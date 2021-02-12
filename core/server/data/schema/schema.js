b682312e052ea865d402917ed254e6a6c88e2740
Rish <zrishabhgarg@gmail.com>
2021-02-12 20:51:37 +0530

    Updated naming

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -372,8 +372,8 @@ module.exports = {
     members_email_change_events: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
         member_id: {type: 'string', maxlength: 24, nullable: false, references: 'members.id', cascadeDelete: true},
-        email: {type: 'string', maxlength: 191, nullable: false, unique: false, validations: {isEmail: true}},
-        previous_email: {type: 'string', maxlength: 191, nullable: false, unique: false, validations: {isEmail: true}},
+        to_email: {type: 'string', maxlength: 191, nullable: false, unique: false, validations: {isEmail: true}},
+        from_email: {type: 'string', maxlength: 191, nullable: false, unique: false, validations: {isEmail: true}},
         created_at: {type: 'dateTime', nullable: false}
     },
     members_status_events: {
