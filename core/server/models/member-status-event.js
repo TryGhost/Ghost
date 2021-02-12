d6e447d1e1370d1e9a2cdebd983d78dace63d1cc
Fabien O'Carroll <fabien@allou.is>
2021-02-12 14:55:27 +0000

    Added customQuery for MemberStatusEvent
    refs https://github.com/TryGhost/Ghost/issues/12602
    
    This allows us to build up a count of member statuses over time

--- core/server/models/member-status-event.js
+++ core/server/models/member-status-event.js
@@ -2,7 +2,22 @@ const errors = require('@tryghost/errors');
 const ghostBookshelf = require('./base');
 
 const MemberStatusEvent = ghostBookshelf.Model.extend({
-    tableName: 'members_status_events'
+    tableName: 'members_status_events',
+    customQuery(qb, options) {
+        if (options.aggregateStatusCounts) {
+            if (options.limit || options.filter) {
+                throw new errors.IncorrectUsageError('aggregateStatusCounts does not work when passed a filter or limit');
+            }
+            const knex = ghostBookshelf.knex;
+            return qb.clear('select')
+                .select(knex.raw('DATE(created_at) as date'))
+                .select(knex.raw(`SUM(CASE WHEN status='paid' THEN 1 ELSE 0 END) as paid_delta`))
+                .select(knex.raw(`SUM(CASE WHEN status='comped' THEN 1 ELSE 0 END) as comped_delta`))
+                .select(knex.raw(`SUM(CASE WHEN status='free' THEN 1 ELSE 0 END) as free_delta`))
+                .groupByRaw('DATE(created_at)')
+                .orderByRaw('DATE(created_at)');
+        }
+    }
 }, {
     async edit() {
         throw new errors.IncorrectUsageError('Cannot edit MemberStatusEvent');
