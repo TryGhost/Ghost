bf58c77568ba9a851c1e9e3eb45a8a4d7f1f84b9
Rish <zrishabhgarg@gmail.com>
2021-02-12 20:37:12 +0530

    Updated `email_change_event` db to capture previous email

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -373,6 +373,7 @@ module.exports = {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
         member_id: {type: 'string', maxlength: 24, nullable: false, references: 'members.id', cascadeDelete: true},
         email: {type: 'string', maxlength: 191, nullable: false, unique: false, validations: {isEmail: true}},
+        previous_email: {type: 'string', maxlength: 191, nullable: false, unique: false, validations: {isEmail: true}},
         created_at: {type: 'dateTime', nullable: false}
     },
     members_status_events: {
