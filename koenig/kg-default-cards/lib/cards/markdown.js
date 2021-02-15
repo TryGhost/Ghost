030d7e1a4c4dd5ab96ad01e5e6397e8d1e0cc21d
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-02-15 19:36:11 +0000

    🔥 Dropped deprecated `card-markdown` support
    refs https://github.com/TryGhost/Ghost/issues/12646
    
    - the version param used to add the markdown wrapper element hasn't been used since Ghost 3.0
    - Ghost 4.0 will migrate old `card-markdown` cards to `markdown` meaning there's really no need for the older card anymore

--- koenig/kg-default-cards/lib/cards/markdown.js
+++ koenig/kg-default-cards/lib/cards/markdown.js
@@ -1,5 +1,45 @@
-const markdownCard = require('./_markdown');
+const markdownHtmlRenderer = require('@tryghost/kg-markdown-html-renderer');
+const {
+    markdownAbsoluteToRelative,
+    markdownRelativeToAbsolute
+} = require('@tryghost/url-utils/lib/utils');
 
-// uses the _markdown card definition function so that the card definition
-// can be re-used in aliased cards
-module.exports = markdownCard();
+module.exports = {
+    name: 'markdown',
+    type: 'dom',
+    config: {
+        commentWrapper: true
+    },
+
+    render: function ({payload, env: {dom}/*, options*/}) {
+        // convert markdown to HTML ready for insertion into dom
+        let html = markdownHtmlRenderer.render(payload.markdown || '');
+
+        if (!html) {
+            return dom.createTextNode('');
+        }
+
+        // use the SimpleDOM document to create a raw HTML section.
+        // avoids parsing/rendering of potentially broken or unsupported HTML
+        return dom.createRawHTMLSection(html);
+    },
+
+    absoluteToRelative(payload, options) {
+        payload.markdown = payload.markdown && markdownAbsoluteToRelative(
+            payload.markdown,
+            options.siteUrl,
+            options
+        );
+        return payload;
+    },
+
+    relativeToAbsolute(payload, options) {
+        payload.markdown = payload.markdown && markdownRelativeToAbsolute(
+            payload.markdown,
+            options.siteUrl,
+            options.itemUrl,
+            options
+        );
+        return payload;
+    }
+};
