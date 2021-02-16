99d84aed0af8638e8d0bad64e8745f04120a48c0
Fabien 'egg' O'Carroll <fabien@allou.is>
2021-02-16 16:14:20 +0000

    Added member relation to events (#12656)
    refs https://github.com/TryGhost/Ghost/issues/12602
    
    When listing site-wide event, we want to include member information so
    that we can contextualise the event when displaying it.

--- core/server/models/member-status-event.js
+++ core/server/models/member-status-event.js
@@ -3,6 +3,11 @@ const ghostBookshelf = require('./base');
 
 const MemberStatusEvent = ghostBookshelf.Model.extend({
     tableName: 'members_status_events',
+
+    member() {
+        return this.belongsTo('Member', 'member_id', 'id');
+    },
+
     customQuery(qb, options) {
         if (options.aggregateStatusCounts) {
             if (options.limit || options.filter) {
