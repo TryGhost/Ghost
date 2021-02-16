24a204e1f68d90e319da1d1cf046e9384743f4fc
Peter Zimon <zimo@ghost.org>
2021-02-16 18:12:24 +0100

    Added dashboard UI skeleton

--- ghost/admin/app/routes/dashboard.js
+++ ghost/admin/app/routes/dashboard.js
@@ -1,4 +1,9 @@
 import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
 
 export default class DashboardRoute extends AuthenticatedRoute {
+    buildRouteInfoMetadata() {
+        return {
+            mainClasses: ['gh-main-wide']
+        };
+    }
 }
