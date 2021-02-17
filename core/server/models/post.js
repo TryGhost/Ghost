08a107e8e5a360504137004ff4c0b0f618d79b1c
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-02-17 19:38:55 +0000

    extracted `htmlToPlaintext` from post model to shared util for re-use

--- core/server/models/post.js
+++ core/server/models/post.js
@@ -6,7 +6,7 @@ const Promise = require('bluebird');
 const {sequence} = require('@tryghost/promise');
 const {i18n} = require('../lib/common');
 const errors = require('@tryghost/errors');
-const htmlToText = require('html-to-text');
+const htmlToPlaintext = require('../../shared/html-to-plaintext');
 const ghostBookshelf = require('./base');
 const config = require('../../shared/config');
 const settingsCache = require('../services/settings/cache');
@@ -481,14 +481,7 @@ Post = ghostBookshelf.Model.extend({
             if (this.get('html') === null) {
                 plaintext = null;
             } else {
-                plaintext = htmlToText.fromString(this.get('html'), {
-                    wordwrap: 80,
-                    ignoreImage: true,
-                    hideLinkHrefIfSameAsText: true,
-                    preserveNewlines: true,
-                    returnDomByDefault: true,
-                    uppercaseHeadings: false
-                });
+                plaintext = htmlToPlaintext(this.get('html'));
             }
 
             // CASE: html is e.g. <p></p>
