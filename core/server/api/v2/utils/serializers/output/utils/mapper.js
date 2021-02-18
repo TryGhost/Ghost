c7a29b4e7cdea32ecc99cc48740d80f39e4a1554
Kevin Ansfield <kevin@ghost.org>
2021-02-18 14:12:06 +0000

    🐛 Fixed unexpected members-only content appearing in excerpt field (#12670)
    closes https://github.com/TryGhost/Team/issues/468
    
    - updated post-gating
      - clears excerpt if there's no access
      - rebuilds excerpt from free preview if paywall card is used and there's no custom excerpt

--- core/server/api/v2/utils/serializers/output/utils/mapper.js
+++ core/server/api/v2/utils/serializers/output/utils/mapper.js
@@ -35,6 +35,8 @@ const mapPost = (model, frame) => {
 
     url.forPost(model.id, jsonModel, frame);
 
+    extraAttrs.forPost(frame, model, jsonModel);
+
     if (utils.isContentAPI(frame)) {
         // Content api v2 still expects page prop
         if (!frame.options.columns || frame.options.columns.includes('page')) {
@@ -48,7 +50,6 @@ const mapPost = (model, frame) => {
         gating.forPost(jsonModel, frame);
     }
 
-    extraAttrs.forPost(frame, model, jsonModel);
     clean.post(jsonModel, frame);
 
     if (frame.options && frame.options.withRelated) {
