c56ba0c71e4f436430ce127e827cc40d912479a3
Fabien 'egg' O'Carroll <fabien@allou.is>
2021-02-18 14:17:10 +0000

    Added initial event timeline to dashboard (#1840)
    refs https://github.com/TryGhost/Team/issues/469
    
    * Updated stats service to fetch event timeline
    * Updated dashboard controller to load events
    * Added currency utils
    * Added GhEventTimeline component
    * Updated dashboard to render GhEventTimeline

--- ghost/admin/app/controllers/dashboard.js
+++ ghost/admin/app/controllers/dashboard.js
@@ -1,7 +1,32 @@
 import Controller from '@ember/controller';
 import {inject as service} from '@ember/service';
+import {tracked} from '@glimmer/tracking';
 
 export default class DashboardController extends Controller {
     @service feature;
     @service session;
+    @service membersStats;
+
+    @tracked
+    events = {
+        data: null,
+        error: null,
+        loading: false
+    };
+
+    constructor(...args) {
+        super(...args);
+        this.loadEvents();
+    }
+
+    loadEvents() {
+        this.events.loading = true;
+        this.membersStats.fetchTimeline().then(({events}) => {
+            this.events.data = events;
+            this.events.loading = false;
+        }, (error) => {
+            this.events.error = error;
+            this.events.loading = false;
+        });
+    }
 }
