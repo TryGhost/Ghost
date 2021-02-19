50c8ee0b98e944d72749e2299f40a0ea9749966b
Hannah Wolfe <erisds@gmail.com>
2021-02-19 11:51:01 +0000

    Cleaned up, commented + co-located new boot
    - moved the DB init back into the boot file so everything is colocated - want it to be EASY to reason about whats happening
    - added some more debug, and made the method signatures all the same
    - expanded on comments to make it clear what the intentions are here, as there's a lot more work to do

--- core/db.js
+++
@@ -1,8 +0,0 @@
-const config = require('./shared/config');
-const logging = require('./shared/logging');
-
-module.exports.ready = async () => {
-    const DatabaseStateManager = require('./server/data/db/state-manager');
-    const dbStateManager = new DatabaseStateManager({knexMigratorFilePath: config.get('paths:appRoot')});
-    await dbStateManager.makeReady({logging});
-};
