8a718ca99ae127e1d4b1d4ba31ac2d20e2a92fd8
Naz <nazargargol@gmail.com>
2021-02-22 19:35:04 +1300

    Migrated jobs to use parentPort.postMessage
    refs https://github.com/TryGhost/Ghost/issues/12496
    
    - Using ghost-ignition logging caused file handle leaks. As there is no straight-forward way to handle write streams with bunyan (ghost-ignition's underlying logging library) this method of logging was chosen as an alternative to keep the amount of open file handles to minimum
    - The follow up changes will include custom formatter for jobs service which should make logging match the same format  as has been used inside the jobs

--- core/server/web/api/testmode/jobs/graceful-job.js
+++ core/server/web/api/testmode/jobs/graceful-job.js
@@ -1,12 +1,10 @@
 const {parentPort} = require('bthreads');
 const util = require('util');
-const logging = require('../../../../../shared/logging');
-const models = require('../../../../models');
 
 let shutdown = false;
 
 parentPort.on('message', (message) => {
-    logging.info(`paret message received: ${message}`);
+    parentPort.postMessage(`parent message received: ${message}`);
 
     if (message === 'cancel') {
         shutdown = true;
@@ -17,32 +15,29 @@ const setTimeoutPromise = util.promisify(setTimeout);
 const internalContext = {context: {internal: true}};
 
 (async () => {
-    try {
-        await models.init();
+    const models = require('../../../../models');
 
-        logging.info(`Fetching tags`);
-        const tags = await models.Tag.findPage(internalContext);
+    await models.init();
 
-        logging.info(`Found ${tags.data.length} tags. First one: ${tags.data[0].toJSON().slug}`);
+    parentPort.postMessage(`Fetching tags`);
+    const tags = await models.Tag.findPage(internalContext);
 
-        logging.info(`Waiting 5 seconds to perform second part of the job`);
-        await setTimeoutPromise(5 * 1000);
+    parentPort.postMessage(`Found ${tags.data.length} tags. First one: ${tags.data[0].toJSON().slug}`);
 
-        if (shutdown) {
-            logging.info(`Job shutting down gracefully`);
-            process.exit(0);
-        }
+    parentPort.postMessage(`Waiting 5 seconds to perform second part of the job`);
+    await setTimeoutPromise(5 * 1000);
 
-        logging.info(`Fetching posts`);
-        const posts = await models.Post.findPage(internalContext);
-
-        logging.info(`Found ${posts.data.length} posts. First one: ${posts.data[0].toJSON().slug}`);
-
-        logging.info('Graceful job has completed!');
-
+    if (shutdown) {
+        parentPort.postMessage(`Job shutting down gracefully`);
         process.exit(0);
-    } catch (err) {
-        logging.error(err);
-        process.exit(1);
     }
+
+    parentPort.postMessage(`Fetching posts`);
+    const posts = await models.Post.findPage(internalContext);
+
+    parentPort.postMessage(`Found ${posts.data.length} posts. First one: ${posts.data[0].toJSON().slug}`);
+
+    parentPort.postMessage('Graceful job has completed!');
+
+    process.exit(0);
 })();
