2a6e8d3ff278838c4ae6e846866777b647b12f0b
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-02-23 10:18:37 +0000

    Added success notification on launch wizard completion
    no issue
    
    - provide an indication of success and a "next step" option

--- ghost/admin/app/components/gh-launch-wizard/finalise.js
+++ ghost/admin/app/components/gh-launch-wizard/finalise.js
@@ -1,14 +1,20 @@
 import Component from '@glimmer/component';
+import {htmlSafe} from '@ember/string';
 import {inject as service} from '@ember/service';
 import {task} from 'ember-concurrency-decorators';
 
 export default class GhLaunchWizardFinaliseComponent extends Component {
     @service feature;
+    @service notifications;
     @service router;
 
     @task
     *finaliseTask() {
         yield this.feature.set('launchComplete', true);
         this.router.transitionTo('dashboard');
+        this.notifications.showNotification(
+            'Launch complete!',
+            {type: 'success', actions: htmlSafe('<a href="#/posts">Start creating content</a>')}
+        );
     }
 }
