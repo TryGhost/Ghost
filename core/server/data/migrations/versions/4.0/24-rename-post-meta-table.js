1d8c3386ace04a63d976a434aacde344085b877e
Naz <nazargargol@gmail.com>
2021-02-23 13:57:32 +1300

    🏗  Renamed "posts_meta" table to "extras"
    refs https://github.com/TryGhost/Team/issues/452
    
    - "metadata" table is meant to be used by other then `posts` table in the future so it needs to become a more generic replacement

---
+++ core/server/data/migrations/versions/4.0/24-rename-post-meta-table.js
@@ -0,0 +1,21 @@
+const {renameTable} = require('../../utils');
+
+const fromTable = 'posts_meta';
+const toTable = 'metadata';
+
+const postsMetaTableSpec = {
+    id: {type: 'string', maxlength: 24, nullable: false, primary: true},
+    post_id: {type: 'string', maxlength: 24, nullable: false, references: 'posts.id', unique: true},
+    og_image: {type: 'string', maxlength: 2000, nullable: true},
+    og_title: {type: 'string', maxlength: 300, nullable: true},
+    og_description: {type: 'string', maxlength: 500, nullable: true},
+    twitter_image: {type: 'string', maxlength: 2000, nullable: true},
+    twitter_title: {type: 'string', maxlength: 300, nullable: true},
+    twitter_description: {type: 'string', maxlength: 500, nullable: true},
+    meta_title: {type: 'string', maxlength: 2000, nullable: true, validations: {isLength: {max: 300}}},
+    meta_description: {type: 'string', maxlength: 2000, nullable: true, validations: {isLength: {max: 500}}},
+    email_subject: {type: 'string', maxlength: 300, nullable: true},
+    frontmatter: {type: 'text', maxlength: 65535, nullable: true}
+};
+
+module.exports = renameTable(fromTable, toTable, postsMetaTableSpec);
