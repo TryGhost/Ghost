4ac59a736ae60e6b04d46159f329a897345b63ab
Peter Zimon <zimo@ghost.org>
2021-02-24 15:33:00 +0100

    Updated Dashboard visibility
    - only owner/admins can view Dashboard, everyone else is redirected to "View site"

--- ghost/admin/app/routes/dashboard.js
+++ ghost/admin/app/routes/dashboard.js
@@ -1,6 +1,15 @@
 import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
 
 export default class DashboardRoute extends AuthenticatedRoute {
+    beforeModel() {
+        super.beforeModel(...arguments);
+        return this.session.user.then((user) => {
+            if (!user.isOwnerOrAdmin) {
+                return this.transitionTo('site');
+            }
+        });
+    }
+
     buildRouteInfoMetadata() {
         return {
             mainClasses: ['gh-main-wide']
