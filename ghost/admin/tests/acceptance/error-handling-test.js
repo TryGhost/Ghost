acc4254f3cf65c5f1fd7c51d31971e95f71abd18
Peter Zimon <zimo@ghost.org>
2021-03-02 17:39:52 +0100

    Basic sign in UI update
    - updated sign in UI to match better with the rest of Ghost.org

--- ghost/admin/tests/acceptance/error-handling-test.js
+++ ghost/admin/tests/acceptance/error-handling-test.js
@@ -73,7 +73,7 @@ describe('Acceptance: Error Handling', function () {
                 await visit('/signin');
                 await fillIn('[name="identification"]', 'test@example.com');
                 await fillIn('[name="password"]', 'password');
-                await click('.gh-btn-blue');
+                await click('.gh-btn-primary');
 
                 // has the refresh to update alert
                 expect(findAll('.gh-alert').length).to.equal(1);
