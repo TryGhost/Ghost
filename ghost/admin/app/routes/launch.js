7f2144e4faaa5f98b4e41ea8690a65e51e0b1f51
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-03-02 14:19:52 +0000

    Removed remaining uses of tour item components
    no issue
    
    - tour items in general are being removed
    - preparation for full removal of all tour-related code (to be done in following commit to allow future revert without merge errors through template divergence)

--- ghost/admin/app/routes/launch.js
+++ ghost/admin/app/routes/launch.js
@@ -3,17 +3,6 @@ import {inject as service} from '@ember/service';
 
 export default class LaunchRoute extends AuthenticatedRoute {
     @service session;
-    @service ui;
-
-    activate() {
-        // disable before rendering template to avoid issues with liquid-wormhole
-        // attempting to destroy elements mid-render if disabled via component hooks
-        this.ui.set('showTour', false);
-    }
-
-    deactivate() {
-        this.ui.set('showTour', true);
-    }
 
     beforeModel() {
         super.beforeModel(...arguments);
