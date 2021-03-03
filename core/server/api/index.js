aff2665d5110aae374151b2e8811db6beab3c001
Naz <nazargargol@gmail.com>
2021-03-04 11:27:09 +1300

    Reordered API exports to improve readability
    refs https://github.com/TryGhost/Team/issues/513
    
    - Having exports in no particular order was making it hard to spot if there's anything missing
    - Having a DESC order on the exported API versions makes it very easy to modify and maintain in the future

--- core/server/api/index.js
+++ core/server/api/index.js
@@ -1,6 +1,6 @@
 module.exports = require('./v3');
-module.exports.v2 = require('./v2');
 module.exports.canary = require('./canary');
-module.exports.v3 = require('./v3');
-module.exports.shared = require('./shared');
 module.exports.v4 = require('./canary');
+module.exports.v3 = require('./v3');
+module.exports.v2 = require('./v2');
+module.exports.shared = require('./shared');
