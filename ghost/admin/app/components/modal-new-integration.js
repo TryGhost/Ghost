f8ccd360ce4259a3cb1baf9fcf43d9a68f1b71c1
Hannah Wolfe <erisds@gmail.com>
2021-03-04 20:49:15 +0000

    Updated new integration modal to handle host error
    refs: https://github.com/TryGhost/Team/issues/510
    
    - We need to display host limit errors if we get them
    - Host limit errors have a custom message added to them, so we show the original context message

--- ghost/admin/app/components/modal-new-integration.js
+++ ghost/admin/app/components/modal-new-integration.js
@@ -1,6 +1,7 @@
 import ModalComponent from 'ghost-admin/components/modal-base';
 import {alias} from '@ember/object/computed';
 import {A as emberA} from '@ember/array';
+import {isHostLimitError} from 'ghost-admin/services/ajax';
 import {isInvalidError} from 'ember-ajax/errors';
 import {inject as service} from '@ember/service';
 import {task} from 'ember-concurrency';
@@ -8,10 +9,11 @@ import {task} from 'ember-concurrency';
 export default ModalComponent.extend({
     router: service(),
 
+    errorMessage: null,
+
     confirm() {},
 
     integration: alias('model'),
-
     actions: {
         updateName(name) {
             this.integration.set('name', name);
@@ -40,6 +42,11 @@ export default ModalComponent.extend({
                     this.get('integration.hasValidated').pushObject('name');
                     return;
                 }
+            }
+
+            if (isHostLimitError(error)) {
+                this.set('errorMessage', error.payload.errors[0].context);
+                return;
             }
 
             // bubble up to the global error handler
