288f8db93ce47ce2a712a9000aa9829d9d6c9e31
Fabien O'Carroll <fabien@allou.is>
2021-03-05 11:38:45 +0000

    Excluded member events tables from site exports
    refs https://github.com/TryGhost/Team/issues/525
    
    We expect the member event tables to be large, and they contain what is
    considered metadata. For this reason we do not want to include them in
    the export of sites.

--- test/api-acceptance/admin/db_spec.js
+++ test/api-acceptance/admin/db_spec.js
@@ -47,7 +47,7 @@ describe('DB API', function () {
         const jsonResponse = res.body;
         should.exist(jsonResponse.db);
         jsonResponse.db.should.have.length(1);
-        Object.keys(jsonResponse.db[0].data).length.should.eql(34);
+        Object.keys(jsonResponse.db[0].data).length.should.eql(28);
     });
 
     it('Can import a JSON database exported from Ghost v2', async function () {
