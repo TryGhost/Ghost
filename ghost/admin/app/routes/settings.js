a3092558f4e9c8c9eba65c8163412bf22d7927de
Peter Zimon <zimo@ghost.org>
2021-03-05 07:51:49 +0100

    Settings main page permission fix
    - fixes a bug that settings main page was accessible for non-owners/admins

--- ghost/admin/app/routes/settings.js
+++ ghost/admin/app/routes/settings.js
@@ -1,4 +1,11 @@
 import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
+import CurrentUserSettings from 'ghost-admin/mixins/current-user-settings';
 
-export default class SettingsRoute extends AuthenticatedRoute {
-}
+export default AuthenticatedRoute.extend(CurrentUserSettings, {
+    beforeModel() {
+        this._super(...arguments);
+        return this.get('session.user')
+            .then(this.transitionAuthor())
+            .then(this.transitionEditor());
+    },
+});
