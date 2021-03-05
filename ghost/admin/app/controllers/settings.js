427851fbaa58d71e4058e3dd3398590e19f6ff3e
Peter Zimon <zimo@ghost.org>
2021-03-05 15:23:23 +0100

    Fix: hiding owner-only setting links from admins
    - hides "Email newsletter" and "Payments" setting links from administrators on settings main page, as they are owner-only

--- ghost/admin/app/controllers/settings.js
+++ ghost/admin/app/controllers/settings.js
@@ -5,6 +5,7 @@ import {inject as service} from '@ember/service';
 export default Controller.extend({
 
     settings: service(),
+    session: service(),
 
     queryParams: ['showPortalSettings', 'showBrandingModal'],
 
