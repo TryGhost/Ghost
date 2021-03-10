b18b76d5ee2abcf1fda6e8b5c2a8c34ca216ffcd
Rish <zrishabhgarg@gmail.com>
2021-03-10 15:09:13 +0530

    Fixed lint
    no refs

--- ghost/portal/src/components/common/InputField.js
+++ ghost/portal/src/components/common/InputField.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, {useEffect, useRef} from 'react';
 import {isCookiesDisabled} from '../../utils/helpers';
 
 export const InputFieldStyles = `
