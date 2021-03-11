2de33b1ba6741d3f3ac37b37df1a0eed53eceb2f
Fabien O'Carroll <fabien@allou.is>
2021-03-11 12:02:28 +0000

    Refactored handlebares template tests to use proxy
    no-issue
    
    Since this module is intended to be used via the proxy, we should test
    it in the same way. We have uncovered a circular dependency issue, which
    would not be possible to catch in tests unless the tests were to go via
    the proxy.

--- test/unit/services/themes/handlebars/template_spec.js
+++ test/unit/services/themes/handlebars/template_spec.js
@@ -1,12 +1,11 @@
 const should = require('should');
-const hbs = require('../../../../../core/frontend/services/themes/engine');
-const template = require('../../../../../core/frontend/services/themes/handlebars/template');
+const proxy = require('../../../../../core/frontend/services/proxy');
 
 describe('Helpers Template', function () {
     it('can execute a template', function () {
-        hbs.registerPartial('test', '<h1>Hello {{name}}</h1>');
+        proxy.hbs.registerPartial('test', '<h1>Hello {{name}}</h1>');
 
-        const safeString = template.execute('test', {name: 'world'});
+        const safeString = proxy.templates.execute('test', {name: 'world'});
 
         should.exist(safeString);
         safeString.should.have.property('string').and.equal('<h1>Hello world</h1>');
