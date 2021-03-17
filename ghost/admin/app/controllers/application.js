9b6d4822e72425ceec192723f4d469060afe1ea5
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-03-17 11:48:36 +0000

    Refactored ApplicationController to use native class
    no issue
    
    - updated to use Ember Octane idioms

--- ghost/admin/app/controllers/application.js
+++ ghost/admin/app/controllers/application.js
@@ -1,34 +1,34 @@
 /* eslint-disable ghost/ember/alias-model-in-controller */
 import Controller from '@ember/controller';
-import {computed} from '@ember/object';
 import {inject as service} from '@ember/service';
 
-export default Controller.extend({
-    billing: service(),
-    customViews: service(),
-    config: service(),
-    dropdown: service(),
-    router: service(),
-    session: service(),
-    settings: service(),
-    ui: service(),
+export default class ApplicationController extends Controller {
+    @service billing;
+    @service customViews;
+    @service config;
+    @service dropdown;
+    @service router;
+    @service session;
+    @service settings;
+    @service ui;
 
-    showBilling: computed.reads('config.hostSettings.billing.enabled'),
-    showNavMenu: computed('router.currentRouteName', 'session.{isAuthenticated,user.isFulfilled}', 'ui.isFullScreen', function () {
-        let {router, session, ui} = this;
+    get showBilling() {
+        return this.config.get('hostSettings.billing.enabled');
+    }
 
+    get showNavMenu() {
         // if we're in fullscreen mode don't show the nav menu
-        if (ui.isFullScreen) {
+        if (this.ui.isFullScreen) {
             return false;
         }
 
         // we need to defer showing the navigation menu until the session.user
         // promise has fulfilled so that gh-user-can-admin has the correct data
-        if (!session.isAuthenticated || !session.user.isFulfilled) {
+        if (!this.session.isAuthenticated || !this.session.user.isFulfilled) {
             return false;
         }
 
-        return (router.currentRouteName !== 'error404' || session.isAuthenticated)
-                && !router.currentRouteName.match(/(signin|signup|setup|reset)/);
-    })
-});
+        return (this.router.currentRouteName !== 'error404' || this.session.isAuthenticated)
+                && !this.router.currentRouteName.match(/(signin|signup|setup|reset)/);
+    }
+}
