78ad67a8e123b4632b835cad19b6a2c1e75266ed
Todd <toddbirchard@gmail.com>
2021-03-16 20:31:53 -0400

    Disable `eslint` & `jshint` enforcement of single quotes during migration.

--- core/server/data/migrations/versions/4.0/21-sanitize-email-batches-provider-id.js
+++ core/server/data/migrations/versions/4.0/21-sanitize-email-batches-provider-id.js
@@ -2,7 +2,10 @@ const logging = require('../../../../../shared/logging');
 const {createIrreversibleMigration} = require('../../utils');
 
 module.exports = createIrreversibleMigration(async (knex) => {
+    /* eslint-disable */
+    /* jshint ignore:start */
     logging.info('Sanitizing provider_id values in email_batches');
 
     await knex.raw("UPDATE email_batches SET provider_id = REPLACE(REPLACE(provider_id, '<', ''), '>', '');");
+    /* jshint ignore:end */
 });
