b5de4f4cf5e13dc990b22fa3642c27369cd59f69
Naz <nazargargol@gmail.com>
2021-03-17 20:01:12 +1300

    Reverted dropped model usage in post serializer
    refs https://github.com/TryGhost/Ghost/commit/6b07d4b2a0a4af871c10756468802b97e1b02151
    
    - The model is needed here, because it contains full set of fields. In some cases, like email-preview, the "plaintext" field is not present in "attrs" which causes the logic to fail.
    - This should be sorted along with https://github.com/TryGhost/Ghost/issues/10396

--- core/server/api/v2/utils/serializers/output/utils/extra-attrs.js
+++ core/server/api/v2/utils/serializers/output/utils/extra-attrs.js
@@ -1,12 +1,15 @@
+const urlUtils = require('../../../../../../../shared/url-utils');
+
 module.exports.forPost = (frame, model, attrs) => {
     const _ = require('lodash');
 
     if (!Object.prototype.hasOwnProperty.call(frame.options, 'columns') ||
         (frame.options.columns.includes('excerpt') && frame.options.formats && frame.options.formats.includes('plaintext'))) {
         if (_.isEmpty(attrs.custom_excerpt)) {
-            const plaintext = attrs.plaintext;
+            let plaintext = model.get('plaintext');
 
             if (plaintext) {
+                plaintext = urlUtils.transformReadyToAbsolute(plaintext);
                 attrs.excerpt = plaintext.substring(0, 500);
             } else {
                 attrs.excerpt = null;
