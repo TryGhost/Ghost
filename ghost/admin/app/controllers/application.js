7b1b840124b4616bf3fbcbb9e8972604549e037c
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-03-17 12:00:14 +0000

    Revert "Refactored ApplicationController to use native class"
    This reverts commit 64a0f621c5867a5d35c8a9fa56c8b21696b18eea.
    
    - there is an issue with properties not being tracked correctly and the menu not being shown when returning from the editor
    - reverting to working version with computed properties for now

--- ghost/admin/app/controllers/application.js
+++ ghost/admin/app/controllers/application.js
@@ -1,34 +1,34 @@
 /* eslint-disable ghost/ember/alias-model-in-controller */
 import Controller from '@ember/controller';
+import {computed} from '@ember/object';
 import {inject as service} from '@ember/service';
 
-export default class ApplicationController extends Controller {
-    @service billing;
-    @service customViews;
-    @service config;
-    @service dropdown;
-    @service router;
-    @service session;
-    @service settings;
-    @service ui;
+export default Controller.extend({
+    billing: service(),
+    customViews: service(),
+    config: service(),
+    dropdown: service(),
+    router: service(),
+    session: service(),
+    settings: service(),
+    ui: service(),
 
-    get showBilling() {
-        return this.config.get('hostSettings.billing.enabled');
-    }
+    showBilling: computed.reads('config.hostSettings.billing.enabled'),
+    showNavMenu: computed('router.currentRouteName', 'session.{isAuthenticated,user.isFulfilled}', 'ui.isFullScreen', function () {
+        let {router, session, ui} = this;
 
-    get showNavMenu() {
         // if we're in fullscreen mode don't show the nav menu
-        if (this.ui.isFullScreen) {
+        if (ui.isFullScreen) {
             return false;
         }
 
         // we need to defer showing the navigation menu until the session.user
         // promise has fulfilled so that gh-user-can-admin has the correct data
-        if (!this.session.isAuthenticated || !this.session.user.isFulfilled) {
+        if (!session.isAuthenticated || !session.user.isFulfilled) {
             return false;
         }
 
-        return (this.router.currentRouteName !== 'error404' || this.session.isAuthenticated)
-                && !this.router.currentRouteName.match(/(signin|signup|setup|reset)/);
-    }
-}
+        return (router.currentRouteName !== 'error404' || session.isAuthenticated)
+                && !router.currentRouteName.match(/(signin|signup|setup|reset)/);
+    })
+});
