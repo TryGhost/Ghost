cd1772835acfc14027716ee333c789d6d2916439
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-03-18 17:11:08 +0000

    fixed tests

--- test/unit/web/site/middleware/serve-favicon_spec.js
+++ test/unit/web/site/middleware/serve-favicon_spec.js
@@ -83,7 +83,7 @@ describe('Serve Favicon', function () {
                         statusCode.should.eql(200);
                     },
                     end: function (body) {
-                        body.length.should.eql(15086);
+                        body.length.should.eql(15406);
                         done();
                     }
                 };
