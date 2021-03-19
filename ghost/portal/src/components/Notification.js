6127066c3830d04efe08d7698db301bc90c6eb3c
Rish <zrishabhgarg@gmail.com>
2021-03-19 12:54:19 +0530

    Added class selector to notification iframe
    no refs

--- ghost/portal/src/components/Notification.js
+++ ghost/portal/src/components/Notification.js
@@ -221,7 +221,7 @@ export default class Notification extends React.Component {
         const {type, status, autoHide, duration} = this.state;
         if (type && status) {
             return (
-                <Frame style={frameStyle} title="portal-notification" head={this.renderFrameStyles()}>
+                <Frame style={frameStyle} title="portal-notification" head={this.renderFrameStyles()} className='gh-portal-notification-iframe' >
                     <NotificationContent {...{type, status, autoHide, duration}} onHideNotification={e => this.onHideNotification(e)} />
                 </Frame>
             );
