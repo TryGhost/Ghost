7240485bd73a5dd7e58709f54bb6d98af2ee5ebe
Christopher Thornton <rmdirbin@gmail.com>
2021-03-19 16:23:16 -0700

    Update migration to use standard SQL strings instead of double quoted column names
    Double quoted strings are used to indicate column names, and may
    cause problems for some databases, and MySQL using strict settings

--- core/server/data/migrations/versions/4.0/21-sanitize-email-batches-provider-id.js
+++ core/server/data/migrations/versions/4.0/21-sanitize-email-batches-provider-id.js
@@ -4,5 +4,5 @@ const {createIrreversibleMigration} = require('../../utils');
 module.exports = createIrreversibleMigration(async (knex) => {
     logging.info('Sanitizing provider_id values in email_batches');
 
-    await knex.raw('UPDATE email_batches SET provider_id = REPLACE(REPLACE(provider_id, "<", ""), ">", "")');
+    await knex.raw('UPDATE email_batches SET provider_id = REPLACE(REPLACE(provider_id, ?, ?), ?, ?)', '<', '', '<', '');
 });
