73fc153e172d1bdde55cac5c7fa6548bad3d3e20
Robert <github@doogie.de>
2021-03-20 10:47:25 +0100

    Feature: Allow themes to have any configuration properties in package.json
    Provide a way so that theme authors can make their themes configurable.
    For example: A theme might offer the possibility to optionally show a tag cloud. 
    There should be a way so that the theme user can easily configure this.
    
    And actually there already would be such a way. It's just simple forbidden in the current code. There already is a `config` section in the themes package.json  But the code filters and only allows the attributes `posts_per_page` [as documented](https://ghost.org/docs/themes/helpers/config/) and `image_sizes`. Themes can directly access these variables as `{{@config.posts_per_page}}
    
    Why restrict this to just these two variables. Allow any variables for theme authors.

--- core/frontend/services/themes/config/index.js
+++ core/frontend/services/themes/config/index.js
@@ -1,12 +1,12 @@
 const _ = require('lodash');
 const defaultConfig = require('./defaults');
-const allowedKeys = ['posts_per_page', 'image_sizes'];
+//const allowedKeys = ['posts_per_page', 'image_sizes'];
 
 module.exports.create = function configLoader(packageJson) {
     let config = _.cloneDeep(defaultConfig);
 
     if (packageJson && Object.prototype.hasOwnProperty.call(packageJson, 'config')) {
-        config = _.assign(config, _.pick(packageJson.config, allowedKeys));
+        //config = _.assign(config, _.pick(packageJson.config, allowedKeys));
     }
 
     return config;
