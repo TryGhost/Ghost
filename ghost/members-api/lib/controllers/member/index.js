438fff5ee40bd2d75a9b59b84715addb71cb9c84
Fabien 'egg' O'Carroll <fabien@allou.is>
2021-03-25 12:19:01 +0000

    Added tests for updateSubscription
    refs https://github.com/TryGhost/Team/issues/530
    
    The RouterController was a grab bag of all controller methods, making it
    difficult to mock & test. This adds a MemberController with a smaller
    API - making it easier to test.

---
+++ ghost/members-api/lib/controllers/member/index.js
@@ -0,0 +1,104 @@
+const errors = require('ghost-ignition').errors;
+
+/**
+ * MemberController
+ *
+ * @param {object} deps
+ * @param {any} deps.memberRepository
+ * @param {any} deps.stripePlansService
+ * @param {any} deps.tokenService
+ */
+module.exports = class MemberController {
+    constructor({
+        memberRepository,
+        stripePlansService,
+        tokenService
+    }) {
+        this._memberRepository = memberRepository;
+        this._stripePlansService = stripePlansService;
+        this._tokenService = tokenService;
+    }
+
+    async updateSubscription(req, res) {
+        try {
+            const identity = req.body.identity;
+            const subscriptionId = req.params.id;
+            const cancelAtPeriodEnd = req.body.cancel_at_period_end;
+            const cancellationReason = req.body.cancellation_reason;
+            const planName = req.body.planName;
+
+            if (cancelAtPeriodEnd === undefined && planName === undefined) {
+                throw new errors.BadRequestError({
+                    message: 'Updating subscription failed!',
+                    help: 'Request should contain "cancel_at_period_end" or "planName" field.'
+                });
+            }
+
+            if ((cancelAtPeriodEnd === undefined || cancelAtPeriodEnd === false) && cancellationReason !== undefined) {
+                throw new errors.BadRequestError({
+                    message: 'Updating subscription failed!',
+                    help: '"cancellation_reason" field requires the "cancel_at_period_end" field to be true.'
+                });
+            }
+
+            if (cancellationReason && cancellationReason.length > 500) {
+                throw new errors.BadRequestError({
+                    message: 'Updating subscription failed!',
+                    help: '"cancellation_reason" field can be a maximum of 500 characters.'
+                });
+            }
+
+            let email;
+            try {
+                if (!identity) {
+                    throw new errors.BadRequestError({
+                        message: 'Updating subscription failed! Could not find member'
+                    });
+                }
+
+                const claims = await this._tokenService.decodeToken(identity);
+                email = claims && claims.sub;
+            } catch (err) {
+                res.writeHead(401);
+                return res.end('Unauthorized');
+            }
+
+            if (!email) {
+                throw new errors.BadRequestError({
+                    message: 'Invalid token'
+                });
+            }
+
+            if (planName !== undefined) {
+                const plan = this._stripePlansService.getPlan(planName);
+                if (!plan) {
+                    throw new errors.BadRequestError({
+                        message: 'Updating subscription failed! Could not find plan'
+                    });
+                }
+                await this._memberRepository.updateSubscription({
+                    email,
+                    subscription: {
+                        subscription_id: subscriptionId,
+                        plan: plan.id
+                    }
+                });
+            } else if (cancelAtPeriodEnd !== undefined) {
+                await this._memberRepository.updateSubscription({
+                    email,
+                    subscription: {
+                        subscription_id: subscriptionId,
+                        cancel_at_period_end: cancelAtPeriodEnd,
+                        cancellationReason
+                    }
+                });
+            }
+
+            res.writeHead(204);
+            res.end();
+        } catch (err) {
+            res.writeHead(err.statusCode || 500);
+            res.end(err.message);
+        }
+    }
+};
---
+++ ghost/members-api/test/unit/lib/controllers/member/index.test.js
@@ -0,0 +1,94 @@
+const sinon = require('sinon');
+const MemberController = require('../../../../../lib/controllers/member');
+
+describe('MemberController', function () {
+    describe('updateSubscription', function () {
+        it('Updates a subscriptions plan via the member repository', async function () {
+            const tokenService = {
+                decodeToken: sinon.fake.resolves({sub: 'fake@email.com'})
+            };
+            const stripePlansService = {
+                getPlan: sinon.fake.returns({id: 'plan_id'})
+            };
+
+            const memberRepository = {
+                updateSubscription: sinon.mock('updateSubscription').once().withArgs({
+                    email: 'fake@email.com',
+                    subscription: {
+                        subscription_id: 'subscription_id',
+                        plan: 'plan_id'
+                    }
+                })
+            };
+
+            const controller = new MemberController({
+                memberRepository,
+                stripePlansService,
+                tokenService
+            });
+
+            const req = {
+                body: {
+                    identity: 'token',
+                    planName: 'plan_name'
+                },
+                params: {
+                    id: 'subscription_id'
+                }
+            };
+            const res = {
+                writeHead() {},
+                end() {}
+            };
+
+            await controller.updateSubscription(req, res);
+
+            memberRepository.updateSubscription.verify();
+        });
+
+        it('Updates a subscriptions cancel_at_period_end via the member repository', async function () {
+            const tokenService = {
+                decodeToken: sinon.fake.resolves({sub: 'fake@email.com'})
+            };
+            const stripePlansService = {
+                getPlan: sinon.fake.returns({id: 'plan_id'})
+            };
+
+            const memberRepository = {
+                updateSubscription: sinon.mock('updateSubscription').once().withArgs({
+                    email: 'fake@email.com',
+                    subscription: {
+                        subscription_id: 'subscription_id',
+                        cancellationReason: 'For reasonable reasons',
+                        cancel_at_period_end: true
+                    }
+                })
+            };
+
+            const controller = new MemberController({
+                memberRepository,
+                stripePlansService,
+                tokenService
+            });
+
+            const req = {
+                body: {
+                    identity: 'token',
+                    cancel_at_period_end: true,
+                    cancellation_reason: 'For reasonable reasons'
+                },
+                params: {
+                    id: 'subscription_id'
+                }
+            };
+            const res = {
+                writeHead() {},
+                end() {}
+            };
+
+            await controller.updateSubscription(req, res);
+
+            memberRepository.updateSubscription.verify();
+        });
+    });
+});
