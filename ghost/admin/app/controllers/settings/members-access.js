c1904d7d846b668e4f084759b4e3b14685827657
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-03-29 12:49:09 +0100

    Added blank members access settings screen behind dev flag
    refs https://github.com/TryGhost/Team/issues/579
    
    - empty screen ready for signup access and default post access level settings to be moved to
    - has unsaved settings changes modal ready

---
+++ ghost/admin/app/controllers/settings/members-access.js
@@ -0,0 +1,37 @@
+import Controller from '@ember/controller';
+import {action} from '@ember/object';
+import {inject as service} from '@ember/service';
+import {task} from 'ember-concurrency-decorators';
+import {tracked} from '@glimmer/tracking';
+
+export default class MembersAccessController extends Controller {
+    @service settings;
+
+    @tracked showLeaveSettingsModal = false;
+
+    leaveRoute(transition) {
+        if (this.settings.get('hasDirtyAttributes')) {
+            transition.abort();
+            this.leaveSettingsTransition = transition;
+            this.showLeaveSettingsModal = true;
+        }
+    }
+
+    @action
+    async confirmLeave() {
+        this.settings.rollbackAttributes();
+        this.showLeaveSettingsModal = false;
+        this.leaveSettingsTransition.retry();
+    }
+
+    @action
+    cancelLeave() {
+        this.showLeaveSettingsModal = false;
+        this.leaveSettingsTransition = null;
+    }
+
+    @task({drop: true})
+    *saveSettingsTask() {
+        return yield this.settings.save();
+    }
+}
