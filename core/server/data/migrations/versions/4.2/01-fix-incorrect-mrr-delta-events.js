c911eae7c78a28094cdfc2c8a7e05d81310b6b9b
Rish <zrishabhgarg@gmail.com>
2021-04-06 19:35:40 +0530

    Rounded MRR value on set

--- core/server/data/migrations/versions/4.2/01-fix-incorrect-mrr-delta-events.js
+++ core/server/data/migrations/versions/4.2/01-fix-incorrect-mrr-delta-events.js
@@ -4,7 +4,7 @@ const logging = require('../../../../../shared/logging');
 module.exports = createTransactionalMigration(
     async function up(knex) {
         logging.info('Fixing incorrect mrr_delta in members_paid_subscription_events table');
-        await knex.raw('UPDATE members_paid_subscription_events SET mrr_delta = mrr_delta / 2 WHERE from_plan = to_plan');
+        await knex.raw('UPDATE members_paid_subscription_events SET mrr_delta = ROUND(mrr_delta / 2) WHERE from_plan = to_plan');
     },
     async function down() {
         // noop
