1407361fd529ea1656213ae1ffb13ec0ee334f77
Rish <zrishabhgarg@gmail.com>
2021-04-06 20:47:21 +0530

    Added down migration to reverse the delta change

--- core/server/data/migrations/versions/4.2/01-fix-incorrect-mrr-delta-events.js
+++ core/server/data/migrations/versions/4.2/01-fix-incorrect-mrr-delta-events.js
@@ -6,7 +6,8 @@ module.exports = createTransactionalMigration(
         logging.info('Fixing incorrect mrr_delta in members_paid_subscription_events table');
         await knex.raw('UPDATE members_paid_subscription_events SET mrr_delta = ROUND(mrr_delta / 2) WHERE from_plan = to_plan');
     },
-    async function down() {
-        // noop
+    async function down(knex) {
+        logging.info('Reverting mrr_delta to old value in members_paid_subscription_events table');
+        await knex.raw('UPDATE members_paid_subscription_events SET mrr_delta = ROUND(mrr_delta * 2) WHERE from_plan = to_plan');
     }
 );
