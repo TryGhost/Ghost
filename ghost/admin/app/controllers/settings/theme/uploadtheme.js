95f1cdb255219b9e4e150c8a77d874c0b4d45e66
Thibaut Patel <thibaut@ghost.org>
2021-04-07 15:51:22 +0200

    Added configurable limit on custom themes
    issue https://github.com/TryGhost/Team/issues/590

---
+++ ghost/admin/app/controllers/settings/theme/uploadtheme.js
@@ -0,0 +1,10 @@
+import Controller from '@ember/controller';
+import {inject as service} from '@ember/service';
+
+export default class UploadThemeController extends Controller {
+    @service config;
+
+    get isAllowed() {
+        return !this.config.get('hostSettings')?.limits?.customThemes;
+    }
+}
