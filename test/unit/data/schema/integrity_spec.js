25182b7b822a31fae49adcf1a8ca1fa28b1c5af5
Fabien 'egg' O'Carroll <fabien@allou.is>
2021-04-08 14:15:30 +0100

    Added `products` and `members_products` tables (#12844)
    refs https://github.com/TryGhost/Team/issues/586
    
    - Add the products table, so that we can store Products in Ghost 
    - Add the members_products table, so that we can associate Members w/ Products
    - Use sort_order on the members_products table to follow the same convention in members_labels
    - Populate the products table with a single product, using the name from the stripe_product_name setting
    - Populate the members_products table with relations based on the status column of the members table
    
    Populating the tables allows us to transition from the current system, which does not care about products, into the
    new system, where Products are used to group members. The intention is that all existing paid members have the
    same product

--- test/unit/data/schema/integrity_spec.js
+++ test/unit/data/schema/integrity_spec.js
@@ -32,7 +32,7 @@ const defaultSettings = require('../../../../core/server/data/schema/default-set
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '559cdbb49a7eeb5758caf0c6e3bf790d';
+    const currentSchemaHash = '9d62f0a673a4f02af8a980495793ac4f';
     const currentFixturesHash = '779f29a247161414025637e10e99a278';
     const currentSettingsHash = '7ac732b994a5bb1565f88c8a84872964';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
