94766c05bf69eb0bb1d02e1060a5357d0b92c8aa
Rishabh Garg <zrishabhgarg@gmail.com>
2021-04-08 20:41:00 +0530

    Added `stripe_products` and `stripe_prices` tables (#12858)
    refs https://github.com/TryGhost/Team/issues/586
    
    - Add the `stripe_products` table, so that we can map Stripe Products to Products in Ghost
    - Add the `stripe_prices` table, so that we can associate Stripe Prices to Products table

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -475,6 +475,27 @@ module.exports = {
         created_at: {type: 'dateTime', nullable: false},
         source: {type: 'string', maxlength: 50, nullable: true}
     },
+    stripe_products: {
+        id: {type: 'string', maxlength: 24, nullable: false, primary: true},
+        product_id: {type: 'string', maxlength: 24, nullable: false, unique: false, references: 'products.id'},
+        stripe_product_id: {type: 'string', maxlength: 255, nullable: false, unique: true},
+        created_at: {type: 'dateTime', nullable: false},
+        updated_at: {type: 'dateTime', nullable: true}
+    },
+    stripe_prices: {
+        id: {type: 'string', maxlength: 24, nullable: false, primary: true},
+        stripe_price_id: {type: 'string', maxlength: 255, nullable: false, unique: true},
+        stripe_product_id: {type: 'string', maxlength: 255, nullable: false, unique: false, references: 'stripe_products.stripe_product_id'},
+        active: {type: 'boolean', nullable: false},
+        livemode: {type: 'boolean', nullable: false},
+        nickname: {type: 'string', maxlength: 50, nullable: true},
+        currency: {type: 'string', maxLength: 3, nullable: false},
+        amount: {type: 'integer', nullable: false},
+        type: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'recurring', validations: {isIn: [['recurring', 'one_time']]}},
+        interval: {type: 'string', maxlength: 50, nullable: false},
+        created_at: {type: 'dateTime', nullable: false},
+        updated_at: {type: 'dateTime', nullable: true}
+    },
     actions: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
         resource_id: {type: 'string', maxlength: 24, nullable: true},
