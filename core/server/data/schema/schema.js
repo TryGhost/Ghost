c19a056a2b360ec436602f2a0fbf3c6afec50e73
Rish <zrishabhgarg@gmail.com>
2021-04-09 20:59:35 +0530

    Updated stripe prices table schema
    refs https://github.com/TryGhost/Team/issues/586
    
    - Change the interval column to be `nullable` as one time payments won't have any interval
    - Remove the `livemode` column as we store the connected account's livemode status at top level

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -490,12 +490,11 @@ module.exports = {
         stripe_price_id: {type: 'string', maxlength: 255, nullable: false, unique: true},
         stripe_product_id: {type: 'string', maxlength: 255, nullable: false, unique: false, references: 'stripe_products.stripe_product_id'},
         active: {type: 'boolean', nullable: false},
-        livemode: {type: 'boolean', nullable: false},
         nickname: {type: 'string', maxlength: 50, nullable: true},
         currency: {type: 'string', maxLength: 3, nullable: false},
         amount: {type: 'integer', nullable: false},
         type: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'recurring', validations: {isIn: [['recurring', 'one_time']]}},
-        interval: {type: 'string', maxlength: 50, nullable: false},
+        interval: {type: 'string', maxlength: 50, nullable: true},
         created_at: {type: 'dateTime', nullable: false},
         updated_at: {type: 'dateTime', nullable: true}
     },
