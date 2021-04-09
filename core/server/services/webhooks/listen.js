b10cc5f62dcee0b829e5bb3e5f6a96d7eaca30cc
Naz <nazargargol@gmail.com>
2021-04-10 00:48:46 +1200

    Blocked webhooks when integration limit is in place
    https://github.com/TryGhost/Team/issues/599
    
    - When custom integration limit is enabled all webhooks belonging to integrations have to be disabled as well. The result is the webhook would stop working and to discover that a user would need to navigate to Admin UI (this changes is yet to come, see refed issue)

--- core/server/services/webhooks/listen.js
+++ core/server/services/webhooks/listen.js
@@ -1,5 +1,7 @@
 const _ = require('lodash');
+const limitService = require('../../services/limits');
 const {events} = require('../../lib/common');
+const logging = require('../../../shared/logging');
 const trigger = require('./trigger');
 
 const WEBHOOKS = [
@@ -39,7 +41,18 @@ const WEBHOOKS = [
     'page.tag.detached'
 ];
 
-const listen = () => {
+const listen = async () => {
+    if (limitService.isLimited('customIntegrations')) {
+        // NOTE: using "checkWouldGoOverLimit" instead of "checkIsOverLimit" here because flag limits don't have
+        //       a concept of measuring if the limit has been surpassed
+        const overLimit = await limitService.checkWouldGoOverLimit('customIntegrations');
+
+        if (overLimit) {
+            logging.info(`Skipped subscribing webhooks to events. The "customIntegrations" plan limit is enabled."`);
+            return;
+        }
+    }
+
     _.each(WEBHOOKS, (event) => {
         events.on(event, (model, options) => {
             // CASE: avoid triggering webhooks when importing
