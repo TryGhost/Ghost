a93c03062d7e4878ac7d8be57ebe8930144d29a8
Rishabh Garg <zrishabhgarg@gmail.com>
2021-04-12 19:18:19 +0530

    Updated stripe prices table schema (#12863)
    refs https://github.com/TryGhost/Team/issues/586
    
    - Change the interval column to be `nullable` as one time payments won't have any interval
    - Remove the `livemode` column as we store the connected account's livemode status at top level

--- test/unit/data/schema/integrity_spec.js
+++ test/unit/data/schema/integrity_spec.js
@@ -32,7 +32,7 @@ const defaultSettings = require('../../../../core/server/data/schema/default-set
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = 'a507f692edce7d13d1b75c124b3a30b7';
+    const currentSchemaHash = '19f3f2750320798dac398be2eb51d3e5';
     const currentFixturesHash = '779f29a247161414025637e10e99a278';
     const currentSettingsHash = '7ac732b994a5bb1565f88c8a84872964';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
