274e3785e4bcc27d4af17b3d4cfcb60ac429a887
Fabien O'Carroll <fabien@allou.is>
2021-04-14 16:49:23 +0100

    fixup! Added permissions for the Products resource

--- test/regression/migrations/migration_spec.js
+++ test/regression/migrations/migration_spec.js
@@ -282,7 +282,7 @@ describe('Database Migration (special functions)', function () {
                     result.roles.at(7).get('name').should.eql('Scheduler Integration');
 
                     // Permissions
-                    result.permissions.length.should.eql(80);
+                    result.permissions.length.should.eql(85);
                     result.permissions.toJSON().should.be.CompletePermissions();
                 });
             });
