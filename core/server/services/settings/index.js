9a5c22ee234d03ecfe4c95e0a4cc8d0996b6f39a
Thibaut Patel <thibaut@ghost.org>
2021-04-16 18:05:13 +0200

    Fixed the previous commit
    commit https://github.com/TryGhost/Ghost/commit/4d93ba77f645088a7795311f82747e45b4ee7829

--- core/server/services/settings/index.js
+++ core/server/services/settings/index.js
@@ -5,6 +5,22 @@
 const models = require('../../models');
 const SettingsCache = require('./cache');
 
+// The string returned when a setting is set as write-only
+const obfuscatedSetting = '••••••••';
+
+// The function used to decide whether a setting is write-only
+function isSecretSetting(setting) {
+    return /secret/.test(setting.key);
+}
+
+// The function that obfuscates a write-only setting
+function hideValueIfSecret(setting) {
+    if (setting.value && isSecretSetting(setting)) {
+        return {...setting, value: obfuscatedSetting};
+    }
+    return setting;
+}
+
 module.exports = {
     async init() {
         const settingsCollection = await models.Settings.populateDefaults();
@@ -46,19 +62,7 @@ module.exports = {
         }
     },
 
-    // The string returned when a setting is set as write-only
-    obfuscatedSetting: '••••••••',
-
-    // The function used to decide whether a setting is write-only
-    isSecretSetting(setting) {
-        return /secret/.test(setting.key);
-    },
-
-    // The function that obfuscates a write-only setting
-    hideValueIfSecret(setting) {
-        if (setting.value && this.isSecretSetting(setting)) {
-            return {...setting, value: this.obfuscatedSetting};
-        }
-        return setting;
-    }
+    obfuscatedSetting,
+    isSecretSetting,
+    hideValueIfSecret
 };
