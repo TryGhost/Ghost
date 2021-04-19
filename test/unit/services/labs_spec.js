b665ea84835b8934881437508aaa5cd06a7e8aee
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-04-19 18:28:51 +0100

    Set `@labs.members` to false when `members_signup_acess == 'none'`
    refs https://github.com/TryGhost/Team/issues/579
    
    - setting `members_signup_access` to `'none'` effectively disables all built-in members functions on the front-end so setting `@labs.members` to `false` allows themes to react accordingly
    - `@labs.members` keeps backwards compatibility with pre-4.0 versions where themes were using it to toggle member-related functionality

--- test/unit/services/labs_spec.js
+++ test/unit/services/labs_spec.js
@@ -2,18 +2,33 @@ const should = require('should');
 const sinon = require('sinon');
 
 const labs = require('../../../core/server/services/labs');
+const settingsCache = require('../../../core/server/services/settings/cache');
 
 describe('Labs Service', function () {
     afterEach(function () {
         sinon.restore();
     });
 
-    it('always returns members true flag', function () {
+    it('members flag is true when members_signup_access setting is "all"', function () {
+        sinon.stub(settingsCache, 'get');
+        settingsCache.get.withArgs('members_signup_access').returns('all');
+
         labs.getAll().should.eql({
             members: true
         });
 
         labs.isSet('members').should.be.true;
+    });
+
+    it('members flag is false when members_signup_access setting is "none"', function () {
+        sinon.stub(settingsCache, 'get');
+        settingsCache.get.withArgs('members_signup_access').returns('none');
+
+        labs.getAll().should.eql({
+            members: false
+        });
+
+        labs.isSet('members').should.be.false;
     });
 
     it('isSet returns false for undefined', function () {
