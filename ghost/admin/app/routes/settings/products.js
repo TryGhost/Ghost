a544005c1f343a81a32863b422f0b460d8339dda
Rishabh Garg <zrishabhgarg@gmail.com>
2021-04-22 22:17:19 +0530

    Wired products data to Product settings screen (#1927)
    refs https://github.com/TryGhost/Team/issues/627
    
    Wires the real Products data from API to the mock Product settings screen including Product Prices list as well as opening of edit/new price modals. The new Product setting is still behind the developer experiment flag as is under active development, the changes in this commit only allows readonly data but not save/edit upstream.
    
    Co-authored-by: Fabien O'Carroll <fabien@allou.is>

--- ghost/admin/app/routes/settings/products.js
+++ ghost/admin/app/routes/settings/products.js
@@ -1,9 +1,16 @@
 import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
+import {inject as service} from '@ember/service';
 
 export default class ProductsRoute extends AuthenticatedRoute {
+    @service store
+
     buildRouteInfoMetadata() {
         return {
             titleToken: 'Settings - Products'
         };
+    }
+
+    model() {
+        return this.store.findAll('product');
     }
 }
