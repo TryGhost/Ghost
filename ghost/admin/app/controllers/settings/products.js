6bde09bd11e9c38b62398d849a0081b26988416e
Rishabh <rish@ghost.org>
2021-04-27 13:55:35 +0530

    Added default sorting to product list
    refs https://github.com/TryGhost/Team/issues/633
    
    Adds default alphabetical sorted order for Product list

--- ghost/admin/app/controllers/settings/products.js
+++ ghost/admin/app/controllers/settings/products.js
@@ -2,6 +2,6 @@ import Controller from '@ember/controller';
 
 export default class ProductsController extends Controller {
     get products() {
-        return this.model;
+        return this.model.sortBy('name');
     }
 }
