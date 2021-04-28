cc14dbc7382f46361b6fcf76d08e2162a524e0f0
Peter Zimon <zimo@ghost.org>
2021-04-28 12:40:52 +0200

    Updated invite-only styles

--- ghost/portal/src/App.js
+++ ghost/portal/src/App.js
@@ -16,7 +16,7 @@ const DEV_MODE_DATA = {
     showPopup: true,
     site: Fixtures.site,
     member: Fixtures.member.paid,
-    page: 'accountHome'
+    page: 'signup'
 };
 export default class App extends React.Component {
     constructor(props) {
