11b6cab003008eec94464beff7bba81956a122f7
Naz <nazargargol@gmail.com>
2021-04-28 12:13:24 +0400

    Fixed upgrade modal cta when publishing a post
    refs https://github.com/TryGhost/Team/issues/589
    
    - Previous upgrade button behavior was opening up a new browser window leading to now retired billing site. Tha behavior change looks like an oversight we missed to cleanup when introducing an in-admin billing pabe "/pro"
    - The change follows similar pattern used in other limit check mesages where tha "upgrade" button takes the user to the billing page

--- ghost/admin/app/components/modal-upgrade-host-limit.js
+++ ghost/admin/app/components/modal-upgrade-host-limit.js
@@ -1,17 +1,17 @@
 import ModalComponent from 'ghost-admin/components/modal-base';
 import {computed} from '@ember/object';
+import {inject as service} from '@ember/service';
 
 export default ModalComponent.extend({
+    router: service(),
+
     upgradeMessage: computed('details', function () {
         const {limit, total} = this.model.details;
         return {limit, total};
     }),
     actions: {
         upgrade: function () {
-            const upgradeLink = this.model.upgradeLink;
-            window.open(upgradeLink);
-            this.closeModal();
-            return true;
+            this.router.transitionTo('pro');
         }
     }
 });
