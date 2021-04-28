fffd99474f1b85d7434100372da86ff440e3401b
Naz <nazargargol@gmail.com>
2021-04-28 14:15:48 +0400

    Fixed upgrade modal cta when activating a theme
    refs https://github.com/TryGhost/Team/issues/590
    
    - Previous upgrade button behavior was opening up a new browser window leading to now retired billing site. Tha behavior change looks like an oversight we missed to cleanup when introducing an in-admin billing pabe "/pro"
    - The change follows similar pattern used in other limit check mesages where tha "upgrade" button takes the user to the billing page

--- ghost/admin/app/components/modal-upgrade-host-limit-custom-theme.js
+++ ghost/admin/app/components/modal-upgrade-host-limit-custom-theme.js
@@ -1,12 +1,12 @@
 import ModalComponent from 'ghost-admin/components/modal-base';
+import {inject as service} from '@ember/service';
 
 export default ModalComponent.extend({
+    router: service(),
+
     actions: {
         upgrade: function () {
-            const upgradeLink = this.model.upgradeLink;
-            window.open(upgradeLink);
-            this.closeModal();
-            return true;
+            this.router.transitionTo('pro');
         }
     }
 });
