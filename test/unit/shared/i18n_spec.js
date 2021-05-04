631be305c1ad276b19c2dd28827d68fcce38f8e7
Hannah Wolfe <erisds@gmail.com>
2021-05-04 14:28:15 +0100

    Improved i18n with unified getCandidateString fn
    - the core i18n library and theme i18n library have slightly different methods of getting a candidate string
    - both of them use forms of jsonpath, meaning they both require jsonpath as a dependency
    - to try to get to a point of being able to rip more things out of ghost, we want to have less dependencies
    - so instead of overloading the method, we pass in a stringMode as an argument
    - eventually we might not need an overloaded class for themeI18n at all, which would simplify the codebase

--- test/unit/shared/i18n_spec.js
+++ test/unit/shared/i18n_spec.js
@@ -1,4 +1,5 @@
 const should = require('should');
+const sinon = require('sinon');
 
 const I18n = require('../../../core/shared/i18n').I18n;
 
@@ -6,5 +7,59 @@ describe('I18n Class Behaviour', function () {
     it('defaults to en', function () {
         const i18n = new I18n();
         i18n.locale().should.eql('en');
+    });
+
+    describe('dot notation (default behaviour)', function () {
+        const fakeStrings = {
+            test: {string: {path: 'I am correct'}}
+        };
+        let i18n;
+
+        beforeEach(function initBasicI18n() {
+            i18n = new I18n();
+            sinon.stub(i18n, '_loadStrings').returns(fakeStrings);
+            i18n.init();
+        });
+
+        it('correctly loads strings', function () {
+            i18n._strings.should.eql(fakeStrings);
+        });
+
+        it('correctly uses dot notation', function () {
+            i18n.t('test.string.path').should.eql('I am correct');
+        });
+
+        it('uses fallback correctly', function () {
+            i18n.t('unknown.string').should.eql('An error occurred');
+        });
+
+        it('errors for invalid strings', function () {
+            should(function () {
+                i18n.t('unknown string');
+            }).throw('i18n.t() called with an invalid path: unknown string');
+        });
+    });
+
+    describe('fulltext notation (theme behaviour)', function () {
+        const fakeStrings = {'Full text': 'I am correct'};
+        let i18n;
+
+        beforeEach(function initFulltextI18n() {
+            i18n = new I18n({stringMode: 'fulltext'});
+            sinon.stub(i18n, '_loadStrings').returns(fakeStrings);
+            i18n.init();
+        });
+
+        it('correctly loads strings', function () {
+            i18n._strings.should.eql(fakeStrings);
+        });
+
+        it('correctly uses fulltext with bracket notation', function () {
+            i18n.t('Full text').should.eql('I am correct');
+        });
+
+        it('uses fallback correctly', function () {
+            i18n.t('unknown string').should.eql('unknown string');
+        });
     });
 });
