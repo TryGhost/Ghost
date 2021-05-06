61be89c739979a1ca14a0c74c30f313ccdf767b7
Naz <nazargargol@gmail.com>
2021-05-06 15:19:36 +0400

    Added utility calculating date of last period start
    refs https://github.com/TryGhost/Team/issues/588
    
    - There's a need to calculate when the last period has started to be able to generate correct counting queries for the "maxPeriodical" limit
    - It operatest on ISO strings as an input and output in UTC timezone to take timezone calculations out of the equation
    - Refer to inclucded unit tests for example calculations

--- ghost/limit-service/lib/date-utils.js
+++ ghost/limit-service/lib/date-utils.js
@@ -1,5 +1,30 @@
+const differenceInMonths = require('date-fns/differenceInMonths');
+const parseISO = require('date-fns/parseISO');
+const addMonths = require('date-fns/addMonths');
+
 const SUPPORTED_INTERVALS = ['month'];
+/**
+ * Calculates the start of the last period (billing, cycle, etc.) based on the start date
+ * and the interval at which the cycle renews.
+ *
+ * @param {String} startDate - date in ISO 8601 format (https://en.wikipedia.org/wiki/ISO_8601)
+ * @param {('month')} interval - currently only supports 'month' value, in the future might support 'year', etc.
+ *
+ * @returns {String} - date in ISO 8601 format (https://en.wikipedia.org/wiki/ISO_8601) of the last period start
+ */
+const lastPeriodStart = (startDate, interval) => {
+    if (interval === 'month') {
+        const startDateISO = parseISO(startDate);
+        const fullPeriodsPast = differenceInMonths(new Date(), startDateISO);
+        const lastPeriodStartDate = addMonths(startDateISO, fullPeriodsPast);
+
+        return lastPeriodStartDate.toISOString();
+    }
+
+    throw new Error('Invalid interval specified. Only "month" value is accepted.');
+};
 
 module.exports = {
+    lastPeriodStart,
     SUPPORTED_INTERVALS
 };
