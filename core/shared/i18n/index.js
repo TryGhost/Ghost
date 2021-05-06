723370e13017d88afd2daea1ab21f4dfa253e9a8
Hannah Wolfe <github.erisds@gmail.com>
2021-05-06 19:51:38 +0100

    Moved i18n basePath concept from themes to core
    - we need the basePath concept for the main i18n class so we can pull it out into a module
    - we already had this in the themeI18n class, so I just had to move it up
    - also I added a default of __dirname, so we don't have to declare this constantly in the tests

--- core/shared/i18n/index.js
+++ core/shared/i18n/index.js
@@ -1,6 +1,6 @@
+const path = require('path');
 const logging = require('../logging');
-
 const I18n = require('./i18n');
 
-module.exports = new I18n({logging});
+module.exports = new I18n({logging, basePath: path.join(__dirname, 'translations')});
 module.exports.I18n = I18n;
