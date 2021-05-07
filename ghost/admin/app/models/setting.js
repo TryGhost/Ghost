1f3383c0804da7b1c6cf7b763f890a791bf51f5b
Kevin Ansfield <kevin@ghost.org>
2021-05-07 10:02:19 +0100

    ✨ Added default newsletter recipients setting (#1946)
    refs https://github.com/TryGhost/Team/issues/496
    reqs https://github.com/TryGhost/Ghost/pull/12925
    
    The publish menu was meant to default to matching post visibility but that wasn't working consistently and didn't make sense for sites which don't email every post to their members.
    
    A "Default newsletter recipients" option has been added to the "Email newsletter" settings screen and the publish menu updated to reflect the option. The free/paid toggles in the publish menu have also been swapped out for a multi-select style component that will cater to more complex member segmentation.

--- ghost/admin/app/models/setting.js
+++ ghost/admin/app/models/setting.js
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
 import Model, {attr} from '@ember-data/model';
 import ValidationEngine from 'ghost-admin/mixins/validation-engine';
+import {and} from '@ember/object/computed';
 
 export default Model.extend(ValidationEngine, {
     validationType: 'setting',
@@ -51,8 +52,8 @@ export default Model.extend(ValidationEngine, {
     /**
      * Members settings
      */
-    defaultContentVisibility: attr('string'),
     membersSignupAccess: attr('string'),
+    defaultContentVisibility: attr('string'),
     membersFromAddress: attr('string'),
     membersSupportAddress: attr('string'),
     membersReplyAddress: attr('string'),
@@ -79,5 +80,12 @@ export default Model.extend(ValidationEngine, {
      * OAuth settings
      */
     oauthClientId: attr('string'),
-    oauthClientSecret: attr('string')
+    oauthClientSecret: attr('string'),
+    /**
+     * Editor settings
+     */
+    editorDefaultEmailRecipients: attr('string'),
+    editorDefaultEmailRecipientsFilter: attr('members-segment-string'),
+
+    mailgunIsConfigured: and('mailgunApiKey', 'mailgunDomain', 'mailgunBaseUrl')
 });
