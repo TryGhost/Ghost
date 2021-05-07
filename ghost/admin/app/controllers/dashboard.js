14e427364835b472851838ac6278c42a908b6c03
Rishabh <rish@ghost.org>
2021-05-07 16:14:07 +0530

    Reverted "Added home redirect for pricing wizard"
    This reverts commit 181cdcdaf0c0da08f06bb4872f83cb29d5955cf4.
    
    Hiding the launch wizard when there are existing prices causes an annoying flicker on the dashboard everytime we reload the page or navigate away. Instead of handling this client side, we'll push up the logic to hide the wizard on server.

--- ghost/admin/app/controllers/dashboard.js
+++ ghost/admin/app/controllers/dashboard.js
@@ -2,7 +2,6 @@ import Controller from '@ember/controller';
 import {action} from '@ember/object';
 import {getSymbol} from 'ghost-admin/utils/currency';
 import {inject as service} from '@ember/service';
-import {task} from 'ember-concurrency-decorators';
 import {tracked} from '@glimmer/tracking';
 
 export default class DashboardController extends Controller {
@@ -36,7 +35,6 @@ export default class DashboardController extends Controller {
     @tracked whatsNewEntries = null;
     @tracked whatsNewEntriesLoading = null;
     @tracked whatsNewEntriesError = null;
-    @tracked product = null;
 
     get topMembersDataHasOpenRates() {
         return this.topMembersData && this.topMembersData.find((member) => {
@@ -49,26 +47,10 @@ export default class DashboardController extends Controller {
     }
 
     initialise() {
-        this.loadProducts.perform();
         this.loadEvents();
         this.loadTopMembers();
         this.loadCharts();
         this.loadWhatsNew();
-    }
-
-    get showLaunchWizard() {
-        const hasPrices = this.product && this.product.get('stripePrices').length > 0;
-        return !this.feature.launchComplete && !hasPrices;
-    }
-
-    @task({drop: true})
-    *loadProducts() {
-        try {
-            const products = yield this.store.query('product', {include: 'stripe_prices'});
-            this.product = products.firstObject;
-        } catch (e) {
-            this.product = null;
-        }
     }
 
     loadMRRStats() {
--- ghost/admin/app/routes/launch.js
+++ ghost/admin/app/routes/launch.js
@@ -6,17 +6,10 @@ export default class LaunchRoute extends AuthenticatedRoute {
 
     beforeModel() {
         super.beforeModel(...arguments);
-
         return this.session.user.then((user) => {
             if (!user.isOwner) {
                 return this.transitionTo('home');
             }
-            this.store.query('product', {include: 'stripe_prices'}).then((products) => {
-                const defaultProduct = products.firstObject;
-                if (defaultProduct.get('stripePrices').length > 0) {
-                    return this.transitionTo('home');
-                }
-            });
         });
     }
 }
