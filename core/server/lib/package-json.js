23ef33da6f6afb7fc163e59133e320ce3dda3e88
Daniel Lockyer <hi@daniellockyer.com>
2021-05-07 12:11:53 +0100

    Extracted `package-json` lib to Utils
    refs https://github.com/TryGhost/Utils/commit/37ebe723c6867a3f85d5570c0c2abb2ac2b365be
    
    - `package-json` was a standalone library using dependency injection so
      we could pull it out into its own package in Utils
    - this was done in the commit referenced above
    - this commit removes the implementation and tests in Ghost and replaces
      the require in the initialization wrapper with the new package

--- core/server/lib/package-json.js
+++ core/server/lib/package-json.js
@@ -1,4 +1,4 @@
-const PackageJson = require('./fs/package-json/package-json');
+const PackageJson = require('@tryghost/package-json');
 const i18n = require('../../shared/i18n');
 
 module.exports = new PackageJson({i18n});
