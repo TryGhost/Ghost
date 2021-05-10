ef93e7165fa8e1b0d58e3cd5fcdac795fc5d535c
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-05-10 18:10:51 +0100

    testing CI error

--- test/frontend-acceptance/members_spec.js
+++ test/frontend-acceptance/members_spec.js
@@ -114,9 +114,10 @@ describe('Front-end members behaviour', function () {
         });
     });
 
-    describe('Price data', function () {
+    describe.only('Price data', function () {
         it('Can be used as a number, and with the price helper', async function () {
             const res = await request.get('/');
+            console.log(res.text);
 
             // Check out test/utils/fixtures/themes/price-data-test-theme/index.hbs
             // To see where this is coming from.
