42463e4fdd8b787d5aa00c9dbfdfc717579f7ec3
Rishabh <zrishabhgarg@gmail.com>
2021-05-10 19:18:30 +0530

    Added error handling for product details page
    refs https://github.com/TryGhost/Team/issues/678
    
    Product name is a mandatory field for a custom product, this change adds error handling on save and custom error message if product is attempted to save without name.

--- ghost/admin/app/validators/product.js
+++ ghost/admin/app/validators/product.js
@@ -5,6 +5,10 @@ export default BaseValidator.create({
     properties: ['name'],
 
     name(model) {
+        if (!model.name) {
+            model.errors.add('name', 'Please enter Name.');
+            this.invalidate();
+        }
         if (!validator.isLength(model.name || '', 0, 191)) {
             model.errors.add('name', 'Name cannot be longer than 191 characters.');
             this.invalidate();
