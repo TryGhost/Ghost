42ef8df7ca46748e77c0fdbb159c4b07f190b8bf
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-05-14 15:44:10 +0100

    Switched signup and default post access settings to dropdowns
    no issue
    
    - provides clearer indication of current setting without having to expand anything
    - has more minimal UI which helps when settings screen has multiple areas

--- ghost/admin/app/components/settings/members-subscription-access.js
+++ ghost/admin/app/components/settings/members-subscription-access.js
@@ -1,21 +1,36 @@
 import Component from '@glimmer/component';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
-import {tracked} from '@glimmer/tracking';
 
 export default class SettingsMembersSubscriptionAccess extends Component {
     @service settings;
 
-    @tracked signupAccessOpen = false;
+    get options() {
+        return [{
+            name: 'Anyone can sign up',
+            description: 'All visitors will be able to subscribe and sign in',
+            value: 'all'
+        }, {
+            name: 'Only people I invite',
+            description: 'People can sign in from your site but won\'t be able to sign up',
+            value: 'invite'
+        }, {
+            name: 'Nobody',
+            description: 'No one will be able to subscribe or sign in',
+            value: 'none'
+        }];
+    }
 
-    @action
-    toggleSignupAccess() {
-        this.signupAccessOpen = !this.signupAccessOpen;
+    get selectedOption() {
+        return this.options.find(o => o.value === this.settings.get('membersSignupAccess'));
     }
 
     @action
-    setSignupAccess(value) {
-        this.settings.set('membersSignupAccess', value);
-        this.settings.set('defaultContentVisibility', 'public');
+    setSignupAccess(option) {
+        this.settings.set('membersSignupAccess', option.value);
+
+        if (option.value === 'none') {
+            this.settings.set('defaultContentVisibility', 'public');
+        }
     }
 }
