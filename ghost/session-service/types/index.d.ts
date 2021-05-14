1be260f13537d62cb1c3a1195a6cddc721facb26
Thibaut Patel <thibaut@ghost.org>
2021-05-14 17:38:28 +0200

    Added a CSRF bypass to enable OAuth
    issue https://github.com/TryGhost/Team/issues/614

--- ghost/session-service/types/index.d.ts
+++ ghost/session-service/types/index.d.ts
@@ -1,8 +1,8 @@
 declare function _exports({ getSession, findUserById, getOriginOfRequest }: {
-    getSession: (req: import("express").Request<import("express-serve-static-core").ParamsDictionary>, res: import("express").Response<any>) => Promise<import("./lib/SessionService").Session>;
+    getSession: (req: import("express").Request<import("express-serve-static-core").ParamsDictionary, any, any, qs.ParsedQs, Record<string, any>>, res: import("express").Response<any, Record<string, any>>) => Promise<import("./lib/SessionService").Session>;
     findUserById: (data: {
         id: string;
     }) => Promise<import("./lib/SessionService").User>;
-    getOriginOfRequest: (req: import("express").Request<import("express-serve-static-core").ParamsDictionary>) => string;
+    getOriginOfRequest: (req: import("express").Request<import("express-serve-static-core").ParamsDictionary, any, any, qs.ParsedQs, Record<string, any>>) => string;
 }): import("./lib/SessionService").SessionService;
 export = _exports;
