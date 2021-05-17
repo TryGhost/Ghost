fa69744575e2c69504562e1401314e2b1d3efd61
Rishabh <zrishabhgarg@gmail.com>
2021-05-17 13:28:15 +0530

    Added basic portal settings in new membership area
    no refs
    
    Members related settings are being consolidated into a single screen.
    
    - Adds basic portal settings UI

--- ghost/admin/app/controllers/settings/membership.js
+++ ghost/admin/app/controllers/settings/membership.js
@@ -8,12 +8,37 @@ export default class MembersAccessController extends Controller {
     @service settings;
 
     @tracked showLeaveSettingsModal = false;
+    @tracked showPortalSettings = false;
+
+    queryParams = ['showPortalSettings'];
 
     leaveRoute(transition) {
         if (this.settings.get('hasDirtyAttributes')) {
             transition.abort();
             this.leaveSettingsTransition = transition;
             this.showLeaveSettingsModal = true;
+        }
+    }
+
+    @action
+    async leavePortalSettings() {
+        this.settings.rollbackAttributes();
+        this.showPortalSettings = false;
+        this.showLeaveSettingsModal = false;
+    }
+
+    @action
+    openStripeSettings() {
+        // Open stripe settings here
+    }
+
+    @action
+    closePortalSettings() {
+        const changedAttributes = this.settings.changedAttributes();
+        if (changedAttributes && Object.keys(changedAttributes).length > 0) {
+            this.showLeaveSettingsModal = true;
+        } else {
+            this.showPortalSettings = false;
         }
     }
 
