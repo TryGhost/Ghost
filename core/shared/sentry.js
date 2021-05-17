6590ebb543eeb7973853ef006ffb59c6e72126e0
Thibaut Patel <thibaut@ghost.org>
2021-05-17 13:32:46 +0200

    Fixed the version sent to Sentry
    commit https://github.com/TryGhost/Ghost/commit/cd1f739910478e737179b7db2c04b68265335c37
    issue https://github.com/TryGhost/Team/issues/694
    
    `process.env.npm_package_version` is only filled when Ghost is started via npm scripts, so it would have been empty when starting Ghost using `node index.js`

--- core/shared/sentry.js
+++ core/shared/sentry.js
@@ -4,7 +4,7 @@ const errors = require('@tryghost/errors');
 
 if (sentryConfig && !sentryConfig.disabled) {
     const Sentry = require('@sentry/node');
-    const version = process.env.npm_package_version;
+    const version = require('../../package.json').version;
     const environment = config.get('env');
     Sentry.init({
         dsn: sentryConfig.dsn,
