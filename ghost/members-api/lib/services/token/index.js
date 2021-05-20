8bf8bd24a79dda297314c1449c9b44d3f1e6dce3
Fabien O'Carroll <fabien@allou.is>
2021-05-20 15:17:12 +0100

    Refactored Token Service to use async/await
    refs https://github.com/TryGhost/Team/issues/657
    
    - Removes the use of "plans" from token service - method wasn't used
    - Updates to use async/await so the code is clearer
    - Install types for the node-jose module
    - Install types for the jsonwebtoken module

--- ghost/members-api/lib/services/token/index.js
+++ ghost/members-api/lib/services/token/index.js
@@ -14,21 +14,9 @@ module.exports = class TokenService {
         this._issuer = issuer;
     }
 
-    encodeAPIToken({sub, aud = this._issuer, plans, exp}) {
-        return this._keyStoreReady.then(jwk => jwt.sign({
-            sub,
-            plans,
-            kid: jwk.kid
-        }, this._privateKey, {
-            algorithm: 'RS512',
-            audience: aud,
-            expiresIn: exp,
-            issuer: this._issuer
-        }));
-    }
-
-    encodeIdentityToken({sub}) {
-        return this._keyStoreReady.then(jwk => jwt.sign({
+    async encodeIdentityToken({sub}) {
+        const jwk = await this._keyStoreReady;
+        return jwt.sign({
             sub,
             kid: jwk.kid
         }, this._privateKey, {
@@ -36,20 +24,23 @@ module.exports = class TokenService {
             audience: this._issuer,
             expiresIn: '10m',
             issuer: this._issuer
-        }));
-    }
-
-    decodeToken(token) {
-        return this._keyStoreReady.then(jwk => jwt.verify(token, this._publicKey, {
-            algorithm: 'RS512',
-            kid: jwk.kid,
-            issuer: this._issuer
-        })).then(() => jwt.decode(token));
-    }
-
-    getPublicKeys() {
-        return this._keyStoreReady.then(() => {
-            this._keyStore.toJSON();
         });
+    }
+
+    /**
+     * @param {string} token
+     */
+    async decodeToken(token) {
+        await this._keyStoreReady;
+
+        return jwt.verify(token, this._publicKey, {
+            algorithms: ['RS512'],
+            issuer: this._issuer
+        });
+    }
+
+    async getPublicKeys() {
+        await this._keyStoreReady;
+        return this._keyStore.toJSON();
     }
 };
