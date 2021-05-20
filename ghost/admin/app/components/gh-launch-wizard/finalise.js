4dafcc8fc69cf6ee5dbdcb10d16dff190603828a
Rishabh <zrishabhgarg@gmail.com>
2021-05-20 17:00:52 +0530

    Updated pricing setup on launch wizard
    no refs
    
    Reworks the set pricing page on the launch wizard to work similar to membership settings, where instead of being disabled when prices exist, it shows existing monthly/yearly prices as well as handles creation of new prices and updating portal settings.

--- ghost/admin/app/components/gh-launch-wizard/finalise.js
+++ ghost/admin/app/components/gh-launch-wizard/finalise.js
@@ -14,23 +14,97 @@ export default class GhLaunchWizardFinaliseComponent extends Component {
         this.settings.rollbackAttributes();
     }
 
+    updatePortalPlans(monthlyPriceId, yearlyPriceId, data) {
+        let portalPlans = this.settings.get('portalPlans') || [];
+        const currentMontlyPriceId = this.settings.get('membersMonthlyPriceId');
+        const currentYearlyPriceId = this.settings.get('membersYearlyPriceId');
+        if (portalPlans.includes(currentMontlyPriceId)) {
+            portalPlans = portalPlans.filter(priceId => priceId !== currentMontlyPriceId);
+        }
+        if (data.isMonthlyChecked) {
+            portalPlans.pushObject(monthlyPriceId);
+        }
+
+        if (portalPlans.includes(currentYearlyPriceId)) {
+            portalPlans = portalPlans.filter(priceId => priceId !== currentYearlyPriceId);
+        }
+        if (data.isYearlyChecked) {
+            portalPlans.pushObject(yearlyPriceId);
+        }
+        portalPlans = portalPlans.filter(priceId => priceId !== 'free');
+        if (data.isFreeChecked) {
+            portalPlans.pushObject('free');
+        }
+        this.settings.set('portalPlans', portalPlans);
+    }
+
+    async saveProduct() {
+        const data = this.args.getData();
+        this.product = data?.product;
+        if (this.product) {
+            const stripePrices = this.product.stripePrices || [];
+            const monthlyAmount = data.monthlyAmount * 100;
+            const yearlyAmount = data.yearlyAmount * 100;
+            const currency = data.currency;
+            const getActivePrice = (prices, type, amount) => {
+                return prices.find((price) => {
+                    return (
+                        price.active && price.amount === amount && price.type === 'recurring' &&
+                        price.interval === type && price.currency.toLowerCase() === currency.toLowerCase()
+                    );
+                });
+            };
+            const monthlyPrice = getActivePrice(stripePrices, 'month', monthlyAmount);
+            const yearlyPrice = getActivePrice(stripePrices, 'year', yearlyAmount);
+
+            if (!monthlyPrice) {
+                stripePrices.push(
+                    {
+                        nickname: 'Monthly',
+                        amount: monthlyAmount,
+                        active: 1,
+                        currency: currency,
+                        interval: 'month',
+                        type: 'recurring'
+                    }
+                );
+            }
+            if (!yearlyPrice) {
+                stripePrices.push(
+                    {
+                        nickname: 'Yearly',
+                        amount: yearlyAmount,
+                        active: 1,
+                        currency: currency,
+                        interval: 'year',
+                        type: 'recurring'
+                    }
+                );
+            }
+            if (monthlyPrice && yearlyPrice) {
+                this.updatePortalPlans(monthlyPrice.id, yearlyPrice.id, data);
+                this.settings.set('membersMonthlyPriceId', monthlyPrice.id);
+                this.settings.set('membersYearlyPriceId', yearlyPrice.id);
+                return this.product;
+            } else {
+                this.product.set('stripePrices', stripePrices);
+                const savedProduct = await this.product.save();
+                const updatedStripePrices = savedProduct.stripePrices || [];
+                const updatedMonthlyPrice = getActivePrice(updatedStripePrices, 'month', monthlyAmount);
+                const updatedYearlyPrice = getActivePrice(updatedStripePrices, 'year', yearlyAmount);
+                this.updatePortalPlans(updatedMonthlyPrice.id, updatedYearlyPrice.id, data);
+                this.settings.set('membersMonthlyPriceId', updatedMonthlyPrice.id);
+                this.settings.set('membersYearlyPriceId', updatedYearlyPrice.id);
+                return savedProduct;
+            }
+        }
+    }
+
     @task
     *finaliseTask() {
         const data = this.args.getData();
-        if (data && data.product) {
-            const updatedProduct = yield data.product.save();
-            const monthlyPrice = updatedProduct.get('stripePrices').find(d => d.nickname === 'Monthly');
-            const yearlyPrice = updatedProduct.get('stripePrices').find(d => d.nickname === 'Yearly');
-            const portalPlans = this.settings.get('portalPlans') || [];
-            let allowedPlans = [...portalPlans];
-            if (data.isMonthlyChecked && monthlyPrice && !allowedPlans.includes(monthlyPrice.id)) {
-                allowedPlans.push(monthlyPrice.id);
-            }
-
-            if (data.isYearlyChecked && yearlyPrice && !allowedPlans.includes(yearlyPrice.id)) {
-                allowedPlans.push(yearlyPrice.id);
-            }
-            this.settings.set('portalPlans', allowedPlans);
+        if (data?.product) {
+            yield this.saveProduct();
             yield this.settings.save();
         }
         yield this.feature.set('launchComplete', true);
--- ghost/admin/app/controllers/launch.js
+++ ghost/admin/app/controllers/launch.js
@@ -156,6 +156,7 @@ export default class LaunchController extends Controller {
 
     @action
     reset() {
+        this.data = null;
         this.step = 'customise-design';
         this.skippedSteps = [];
     }
