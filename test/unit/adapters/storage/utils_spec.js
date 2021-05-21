ce4db8d62d4e7ac5ec168a4fa95615b023534ce6
Hannah Wolfe <github.erisds@gmail.com>
2021-05-21 19:53:38 +0100

    Removed other ancient skipped storge test
    - My IDE "fixed" this skipped test by unskipping it without me realising when I removed the other one 🙈

--- test/unit/adapters/storage/utils_spec.js
+++ test/unit/adapters/storage/utils_spec.js
@@ -104,23 +104,6 @@ describe('storage utils', function () {
             result.should.be.equal(true);
         });
 
-        // Very unlikely that this is necessary, because Ghost will redirect the request beforehand.
-        // See https://github.com/TryGhost/Ghost/blob/master/core/server/web/shared/middlewares/url-redirects.js#L76
-        // TODO: Change the code to make this test work
-        it('should return local file storage path for https request, when blog setup as http', function () {
-            const url = 'https://myblog.com/content/images/2017/07/ghost-logo.png';
-            let result;
-
-            urlForStub = sinon.stub(urlUtils, 'urlFor');
-            urlForStub.withArgs('home').returns('http://myblog.com/');
-            urlGetSubdirStub = sinon.stub(urlUtils, 'getSubdir');
-            urlGetSubdirStub.returns('');
-
-            result = storageUtils.isLocalImage(url);
-            should.exist(result);
-            result.should.be.equal(true);
-        });
-
         it('should return true when absolute URL with subdirectory and local file', function () {
             const url = 'http://myblog.com/blog/content/images/2017/07/ghost-logo.png';
             let result;
