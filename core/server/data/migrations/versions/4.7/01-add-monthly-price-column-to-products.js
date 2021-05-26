3b864f73ef10d792f144e488c6290b1e03fe792b
Fabien O'Carroll <fabien@allou.is>
2021-05-26 10:52:31 +0100

    fixup! Added {monthly,yearly}_price columns to products

--- core/server/data/migrations/versions/4.7/01-add-monthly-price-column-to-products.js
+++ core/server/data/migrations/versions/4.7/01-add-monthly-price-column-to-products.js
@@ -1,8 +1,8 @@
-const {createAddColumnMigration} = require('../../utils')
+const {createAddColumnMigration} = require('../../utils');
 
 module.exports = createAddColumnMigration('products', 'monthly_price', {
     type: 'string',
     maxlength: 24,
     references: 'stripe_prices.id',
     nullable: true
-})
+});
