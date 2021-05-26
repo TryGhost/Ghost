3b366dc55d3a6355cdf24f279b1f48cd64520690
Kevin Ansfield <kevin@ghost.org>
2021-05-26 12:10:19 +0100

    Fixed max-api-complexity linting warnings for site and config APIs (#12982)
    no issue
    
    - moved `config` and `site` API output generation to a `public-config` service allowing all API versions to use `publicConfig.config` or `publicConfig.site` in their query methods
    - updated `config` and `site` API output serializers to use an allow-list that limits the data returned for each API version

--- core/server/api/v3/utils/serializers/output/config.js
+++ core/server/api/v3/utils/serializers/output/config.js
@@ -1,10 +1,25 @@
+const _ = require('lodash');
 const debug = require('ghost-ignition').debug('api:v3:utils:serializers:output:config');
 
 module.exports = {
     all(data, apiConfig, frame) {
         debug('all');
+
         frame.response = {
-            config: data
+            config: _.pick(data, [
+                'version',
+                'environment',
+                'database',
+                'mail',
+                'useGravatar',
+                'labs',
+                'clientExtensions',
+                'enableDeveloperExperiments',
+                'stripeDirect',
+                'mailgunIsConfigured',
+                'emailAnalytics',
+                'billingUrl'
+            ])
         };
     }
 };
