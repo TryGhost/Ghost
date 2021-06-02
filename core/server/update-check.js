bd51dd09db98c8ae90cf0944ce18c7615641b472
Naz <nazargargol@gmail.com>
2021-06-02 14:31:07 +0400

    Limited the API surface of the UpdateCheckService
    refs https://github.com/TryGhost/Team/issues/728
    
    - This is continuation of the previous commit. TLDR: Passing only the necessary API endpoint function makes it easier to reason about what dependencies the UpdateCheckService has to deal with
    - Limited urlUtils to only one function as that's all the UpdateCheck uses. Next step will be removing the function completely as and passing a 'blogURL' as a config value (way better readability this way)

--- core/server/update-check.js
+++ core/server/update-check.js
@@ -48,7 +48,7 @@ module.exports = () => {
             },
             i18n,
             logging,
-            urlUtils,
+            urlFor: urlUtils.urlFor,
             request,
             ghostVersion,
             ghostMailer
