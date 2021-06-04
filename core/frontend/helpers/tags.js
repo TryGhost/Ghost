724db487a01a4408283b7a9de1a2983ad4d57881
Hannah Wolfe <github.erisds@gmail.com>
2021-06-04 19:58:07 +0100

    Swapped _.escape for proxy.escapeExpression
    - we don't need to use _.escape from lodash as we already have escapeExpression from handlebars
    - it's more correct to use the escape utility from our theme engine when escaping strings _for_ our theme engine!
    - Note there is a minor difference between the two:
      - Lodash: &, <, >, " and '
        - refs: https://lodash.com/docs/4.17.15#escape
      - Handlebars: &, <, >, ", ', ` and =
        - refs: https://handlebarsjs.com/api-reference/utilities.html#helper-utilities
    
    - This could cause slightly weird behaviour in themes around ` and = characters, but as it's just convering to html entities it should be fine

--- core/frontend/helpers/tags.js
+++ core/frontend/helpers/tags.js
@@ -5,18 +5,18 @@
 // By default, tags are separated by commas.
 //
 // Note that the standard {{#each tags}} implementation is unaffected by this helper
-const {urlService, SafeString, templates} = require('../services/proxy');
-const _ = require('lodash');
+const {urlService, SafeString, escapeExpression, templates} = require('../services/proxy');
+const isString = require('lodash/isString');
 const ghostHelperUtils = require('@tryghost/helpers').utils;
 
 module.exports = function tags(options) {
     options = options || {};
     options.hash = options.hash || {};
 
-    const autolink = !(_.isString(options.hash.autolink) && options.hash.autolink === 'false');
-    const separator = _.isString(options.hash.separator) ? options.hash.separator : ', ';
-    const prefix = _.isString(options.hash.prefix) ? options.hash.prefix : '';
-    const suffix = _.isString(options.hash.suffix) ? options.hash.suffix : '';
+    const autolink = !(isString(options.hash.autolink) && options.hash.autolink === 'false');
+    const separator = isString(options.hash.separator) ? options.hash.separator : ', ';
+    const prefix = isString(options.hash.prefix) ? options.hash.prefix : '';
+    const suffix = isString(options.hash.suffix) ? options.hash.suffix : '';
     const limit = options.hash.limit ? parseInt(options.hash.limit, 10) : undefined;
     let output = '';
     let from = options.hash.from ? parseInt(options.hash.from, 10) : 1;
@@ -26,8 +26,8 @@ module.exports = function tags(options) {
         function processTag(tag) {
             return autolink ? templates.link({
                 url: urlService.getUrlByResourceId(tag.id, {withSubdirectory: true}),
-                text: _.escape(tag.name)
-            }) : _.escape(tag.name);
+                text: escapeExpression(tag.name)
+            }) : escapeExpression(tag.name);
         }
 
         return ghostHelperUtils.visibility.filter(tagsList, options.hash.visibility, processTag);
