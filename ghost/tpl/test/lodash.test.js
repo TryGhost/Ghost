331352560293ea17aa1102d488206c684d64a23e
Hannah Wolfe <github.erisds@gmail.com>
2021-06-08 16:46:20 +0100

    Added minimal tpl() template helper
    - This mini helper wraps lodash template and supports `{...}` as the delimiters
    - It's designed to use and support the exact same patterns we already have in our en.json strings
    E.g. The {flagName} flag must be enabled in labs if you wish to use the \\{\\{{helperName}\\}\\} helper.
    - This allows us to get rid of our old, broken i18n helper and still keep some of the smart messaging we have setup
    - It also keeps the refactor surface area minimal

---
+++ ghost/tpl/test/lodash.test.js
@@ -0,0 +1,12 @@
+// Switch these lines once there are useful utils
+// const testUtils = require('./utils');
+require('./utils');
+
+describe('Lodash Template', function () {
+    it('Does not get clobbered by this lib', function () {
+        require('../lib/tpl');
+        let _ = require('lodash');
+
+        _.templateSettings.interpolate.should.eql(/<%=([\s\S]+?)%>/g);
+    });
+});
