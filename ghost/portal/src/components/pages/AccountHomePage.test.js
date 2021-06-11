986542dffebea180ff1d0ef231bb457e8a7c3ade
Rishabh <zrishabhgarg@gmail.com>
2021-06-11 23:23:44 +0530

    Removed deprecated NODE_PATH usage in tests
    no refs
    
    Using NODE_PATH is deprecated in future versions of Create React App in favor of using jsconfig.json , this change removes files relying on NODE_PATH and uses relative path instead till we update usage to jsconfig in future as needed.

--- ghost/portal/src/components/pages/AccountHomePage.test.js
+++ ghost/portal/src/components/pages/AccountHomePage.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render, fireEvent} from 'test-utils';
+import {render, fireEvent} from '../../utils/test-utils';
 import AccountHomePage from './AccountHomePage';
 
 const setup = (overrides) => {
