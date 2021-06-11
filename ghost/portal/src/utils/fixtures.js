602a2e778ee0e473675e5ad450a3c336ec548aec
Rishabh <zrishabhgarg@gmail.com>
2021-06-11 12:55:24 +0530

    Updated default product fixtures
    refs https://github.com/TryGhost/Team/issues/767
    
    - cleans up default set of products available in fixtures for easy testing

--- ghost/portal/src/utils/fixtures.js
+++ ghost/portal/src/utils/fixtures.js
@@ -26,27 +26,27 @@ const prices = [
 const products = [
     {
         id: 'product_1',
-        name: 'Main Product',
-        description: 'The default product',
+        name: 'Gold',
+        description: 'Access to all members articles, weekly podcast and exclusive interviews',
         monthlyPrice: {
-            id: '6086d2c776909b1a2382369a',
+            id: '6085adc776909b1a2382369a',
             stripe_price_id: '7d6c89c0289ca1731226e86b95b5a162085b8561ca0d10d3a4f03afd3e3e6ba6',
             stripe_product_id: '109c85c734fb9992e7bc30a26af66c22f5c94d8dc62e0a33cb797be902c06b2d',
             active: 1,
             nickname: 'Monthly',
             currency: 'usd',
-            amount: 500,
+            amount: 2000,
             type: 'recurring',
             interval: 'month'
         },
         yearlyPrice: {
-            id: '6086eff0823dd7240afc8083',
+            id: '6086eff0823dd7345afc8083',
             stripe_price_id: 'price_1IkXgCFToJelIqAsTP3V1paQ',
             stripe_product_id: 'prod_JNGGBrrogUXcoM',
             active: 1,
             nickname: 'Yearly',
             currency: 'usd',
-            amount: 5000,
+            amount: 100000,
             type: 'recurring',
             interval: 'year'
         },
@@ -77,10 +77,10 @@ const products = [
     },
     {
         id: 'product_2',
-        name: 'Advanced Product',
-        description: 'For extra content',
+        name: 'Silver',
+        description: 'Access to all members articles and weekly podcast',
         monthlyPrice: {
-            id: '6086d2c776909b1a2382369a',
+            id: '6086d2c776909b111182369a',
             stripe_price_id: '7d6c89c0289ca1731226e86b95b5a162085b8561ca0d10d3a4f03afd3e3e6ba6',
             stripe_product_id: '109c85c734fb9992e7bc30a26af66c22f5c94d8dc62e0a33cb797be902c06b2d',
             active: 1,
@@ -91,13 +91,64 @@ const products = [
             interval: 'month'
         },
         yearlyPrice: {
-            id: '6086eff0823dd7240afc8083',
+            id: '6086eff0823dd72402fc8012',
             stripe_price_id: 'price_1IkXgCFToJelIqAsTP3V1paQ',
             stripe_product_id: 'prod_JNGGBrrogUXcoM',
             active: 1,
             nickname: 'Yearly',
             currency: 'usd',
             amount: 12000,
+            type: 'recurring',
+            interval: 'year'
+        },
+        prices: [
+            {
+                id: '6086d2c776909b1a2382369a',
+                stripe_price_id: '7d6c89c0289ca1731226e86b95b5a162085b8561ca0d10d3a4f03afd3e3e6ba6',
+                stripe_product_id: '109c85c734fb9992e7bc30a26af66c22f5c94d8dc62e0a33cb797be902c06b2d',
+                active: 1,
+                nickname: 'Monthly',
+                currency: 'usd',
+                amount: 1200,
+                type: 'recurring',
+                interval: 'month'
+            },
+            {
+                id: '6086eff0823dd7240afc8083',
+                stripe_price_id: 'price_1IkXgCFToJelIqAsTP3V1paQ',
+                stripe_product_id: 'prod_JNGGBrrogUXcoM',
+                active: 1,
+                nickname: 'Yearly',
+                currency: 'usd',
+                amount: 12000,
+                type: 'recurring',
+                interval: 'year'
+            }
+        ]
+    },
+    {
+        id: 'product_3',
+        name: 'Bronze',
+        description: 'Access to all members articles',
+        monthlyPrice: {
+            id: '6086d2c776909b1a23823622',
+            stripe_price_id: '7d6c89c0289ca1731226e86b95b5a162085b8561ca0d10d3a4f03afd3e3e6ba6',
+            stripe_product_id: '109c85c734fb9992e7bc30a26af66c22f5c94d8dc62e0a33cb797be902c06b2d',
+            active: 1,
+            nickname: 'Monthly',
+            currency: 'usd',
+            amount: 700,
+            type: 'recurring',
+            interval: 'month'
+        },
+        yearlyPrice: {
+            id: '6086eff0823dd7240afc8012',
+            stripe_price_id: 'price_1IkXgCFToJelIqAsTP3V1paQ',
+            stripe_product_id: 'prod_JNGGBrrogUXcoM',
+            active: 1,
+            nickname: 'Yearly',
+            currency: 'usd',
+            amount: 7000,
             type: 'recurring',
             interval: 'year'
         },
