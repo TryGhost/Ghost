b284e2e4da1339c8134fb395d76f8b2261712f93
Daniel Lockyer <hi@daniellockyer.com>
2021-06-11 09:13:26 +0100

    🐛 Fixed sticky "unsaved settings" modal
    fixes https://github.com/TryGhost/Team/issues/777
    
    - the unsaved settings modal would come back if you left a settings
      sub-page with unsaved changes and then came back to the page
    - if you then clicked on "Leave", Admin would crash with a Sentry error
      of `Cannot read property 'finally' of undefined`
    - we had a similar bug in
      https://github.com/TryGhost/Admin/commit/39c850f4fe2018061215263b31219646aa0bdc59
      and the fix was to remove the `.finally` because it assumed the
      confirm action returned a promise
    - this commit performs a similar fix and seems to resolve the problem

--- ghost/admin/app/components/modal-leave-settings.js
+++ ghost/admin/app/components/modal-leave-settings.js
@@ -4,11 +4,8 @@ import RSVP from 'rsvp';
 export default ModalComponent.extend({
     actions: {
         confirm() {
-            this.confirm().finally(() => {
-                if (!this.isDestroyed && !this.isDestroying) {
-                    this.send('closeModal');
-                }
-            });
+            this.confirm();
+            this.send('closeModal');
         }
     },
 
