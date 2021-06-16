db4dabd6636f73e4e0991c589db0a5a50640a605
Rishabh <zrishabhgarg@gmail.com>
2021-06-16 20:45:02 +0530

    Updated Portal build script to use rewired webpack config
    no issues
    
    - updates Portal build script to use rewired react-scripts config
    - updated config handles css embed as well as output location/name for portal bundle as part of cra build
    - makes extra webpack bundling redundant for now
    - updates dev mode to map the portal source map useful for testing build version locally
    - updates custom webpack config with copy plugin for future use
    
    refs -
    https://github.com/facebook/create-react-app/issues/5306#issuecomment-603772477
    https://gist.github.com/phdesign/3fd306db2bc53f6368e6f0f73bbeff19

--- ghost/portal/scripts/build-combined.js
+++ ghost/portal/scripts/build-combined.js
@@ -1,4 +1,5 @@
 const rewire = require('rewire');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const defaults = rewire('react-scripts/scripts/build.js');
 let config = defaults.__get__('config');
 
@@ -9,3 +10,20 @@ config.optimization.splitChunks = {
 };
 
 config.optimization.runtimeChunk = false;
+
+// JS: Save built file in `/umd`
+config.output.filename = '../umd/portal.min.js';
+
+// CSS: Remove MiniCssPlugin from list of plugins
+config.plugins = config.plugins.filter(plugin => !(plugin instanceof MiniCssExtractPlugin));
+// CSS: replaces all MiniCssExtractPlugin.loader with style-loader to embed CSS in JS
+config.module.rules[1].oneOf = config.module.rules[1].oneOf.map((rule) => {
+    if (!Object.prototype.hasOwnProperty.call(rule, 'use')) {
+        return rule;
+    }
+    return Object.assign({}, rule, {
+        use: rule.use.map(options => (/mini-css-extract-plugin/.test(options.loader)
+            ? {loader: require.resolve('style-loader'), options: {}}
+            : options))
+    });
+});
