bdfac2524ea0dc24ebf7f8fcbf1da0a2b227e813
Sam Lord <hello@samlord.me>
2021-06-17 09:36:47 +0100

    Revert "Re-export logging in ./core/shared/logging"
    This reverts commit 588bbcab8483f45364ff091350610d288b727dc3.

--- core/shared/logging.js
+++
@@ -1,7 +0,0 @@
-const logging = require('@tryghost/logging');
-
-/**
- * TODO: Remove this file once Ghost (Pro) no longer relies on ./core/shared/logging.js
- * This re-exports the same interface that the original logging module provided
- */
-module.exports = logging;
