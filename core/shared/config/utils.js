a0908bce458670ed638d59f1cad623b903915eb4
Hannah Wolfe <github.erisds@gmail.com>
2021-06-18 21:19:16 +0100

    Removed bind from internal-only config helpers
    - We were using the same bind pattern for both internal-only and public helpers
    - Binding helpers to config makes them available throughout the codebase
    - Removing the binding doesn't make the code much more complicated, but it does make the Public API of the config module a lot clearer

--- core/shared/config/utils.js
+++ core/shared/config/utils.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs-extra');
+const os = require('os');
 const _ = require('lodash');
 
 /**
@@ -10,24 +11,22 @@ const _ = require('lodash');
  * Path must match minimum one / or \
  * Path can be a "." to re-present current folder
  */
-const makePathsAbsolute = function makePathsAbsolute(obj, parent) {
-    const self = this;
-
+const makePathsAbsolute = function makePathsAbsolute(nconf, obj, parent) {
     _.each(obj, function (configValue, pathsKey) {
         if (_.isObject(configValue)) {
-            makePathsAbsolute.bind(self)(configValue, parent + ':' + pathsKey);
+            makePathsAbsolute(nconf, configValue, parent + ':' + pathsKey);
         } else if (
             _.isString(configValue) &&
             (configValue.match(/\/+|\\+/) || configValue === '.') &&
             !path.isAbsolute(configValue)
         ) {
-            self.set(parent + ':' + pathsKey, path.normalize(path.join(__dirname, '../../..', configValue)));
+            nconf.set(parent + ':' + pathsKey, path.normalize(path.join(__dirname, '../../..', configValue)));
         }
     });
 };
 
-const doesContentPathExist = function doesContentPathExist() {
-    if (!fs.pathExistsSync(this.get('paths:contentPath'))) {
+const doesContentPathExist = function doesContentPathExist(contentPath) {
+    if (!fs.pathExistsSync(contentPath)) {
         // new Error is allowed here, as we do not want config to depend on @tryghost/error
         // @TODO: revisit this decision when @tryghost/error is no longer dependent on all of ghost-ignition
         // eslint-disable-next-line no-restricted-syntax
@@ -38,9 +37,7 @@ const doesContentPathExist = function doesContentPathExist() {
 /**
 * Check if the URL in config has a protocol and sanitise it if not including a warning that it should be changed
 */
-const checkUrlProtocol = function checkUrlProtocol() {
-    const url = this.get('url');
-
+const checkUrlProtocol = function checkUrlProtocol(url) {
     if (!url.match(/^https?:\/\//i)) {
         // new Error is allowed here, as we do not want config to depend on @tryghost/error
         // @TODO: revisit this decision when @tryghost/error is no longer dependent on all of ghost-ignition
@@ -56,10 +53,10 @@ const checkUrlProtocol = function checkUrlProtocol() {
  * this.clear('key') does not work
  * https://github.com/indexzero/nconf/issues/235#issuecomment-257606507
  */
-const sanitizeDatabaseProperties = function sanitizeDatabaseProperties() {
-    const database = this.get('database');
+const sanitizeDatabaseProperties = function sanitizeDatabaseProperties(nconf) {
+    const database = nconf.get('database');
 
-    if (this.get('database:client') === 'mysql') {
+    if (nconf.get('database:client') === 'mysql') {
         delete database.connection.filename;
     } else {
         delete database.connection.host;
@@ -68,7 +65,19 @@ const sanitizeDatabaseProperties = function sanitizeDatabaseProperties() {
         delete database.connection.database;
     }
 
-    this.set('database', database);
+    nconf.set('database', database);
+
+    if (nconf.get('database:client') === 'sqlite3') {
+        makePathsAbsolute(nconf, nconf.get('database:connection'), 'database:connection');
+
+        // In the default SQLite test config we set the path to /tmp/ghost-test.db,
+        // but this won't work on Windows, so we need to replace the /tmp bit with
+        // the Windows temp folder
+        const filename = nconf.get('database:connection:filename');
+        if (_.isString(filename) && filename.match(/^\/tmp/)) {
+            nconf.set('database:connection:filename', filename.replace(/^\/tmp/, os.tmpdir()));
+        }
+    }
 };
 
 module.exports = {
