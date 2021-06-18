c1841fd58b74dbd8db065d2f7853afbc1e873b46
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-06-18 09:46:38 +0100

    Removed unused from address confirmation modal component
    no issue
    
    - email send confirmation and instructions are shown inline rather than a modal meaning this component wasn't being used

--- ghost/admin/app/components/modal-from-address-confirmation.js
+++
@@ -1,8 +0,0 @@
-import ModalComponent from 'ghost-admin/components/modal-base';
-import {alias} from '@ember/object/computed';
-
-export default ModalComponent.extend({
-
-    confirm() {},
-    fromAddress: alias('model.fromAddress')
-});
