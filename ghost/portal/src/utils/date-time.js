37d8483345a86f5eaa6c6fb4f1bc5135f89e5ea0
Rishabh <zrishabhgarg@gmail.com>
2021-06-20 13:32:46 +0530

    Fixed lint
    no refs

--- ghost/portal/src/utils/date-time.js
+++ ghost/portal/src/utils/date-time.js
@@ -1,5 +1,5 @@
 export const getDateString = (isoDate) => {
     const event = new Date(isoDate);
-    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    const options = {year: 'numeric', month: 'short', day: 'numeric'};
     return event.toLocaleDateString('en-GB', options);
 };
