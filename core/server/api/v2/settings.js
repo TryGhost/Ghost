dbb36a7da20907ea39a404c8e0e15ed707766ee4
Hannah Wolfe <github.erisds@gmail.com>
2021-06-28 12:09:02 +0100

    Moved route settings to new server service
    - The main goal here is getting this settings related code out of the routing service as it really doesn't belong there
    - This settings file is used purely by the API to get and set files - its not really anything to do with actual routing
    - This file calls out to the bridge to do a reload, which helps decouple slightly
    - More refactoring is needed to get rid of the urlService dependency
    - Note this file is really similar to the redirects one, it would be good to merge them

--- core/server/api/v2/settings.js
+++ core/server/api/v2/settings.js
@@ -1,7 +1,7 @@
 const Promise = require('bluebird');
 const _ = require('lodash');
 const models = require('../../models');
-const frontendRouting = require('../../../frontend/services/routing');
+const routeSettings = require('../../services/route-settings');
 const frontendSettings = require('../../../frontend/services/settings');
 const i18n = require('../../../shared/i18n');
 const {NoPermissionError, NotFoundError} = require('@tryghost/errors');
@@ -168,7 +168,7 @@ module.exports = {
             method: 'edit'
         },
         async query(frame) {
-            await frontendRouting.settings.setFromFilePath(frame.file.path);
+            await routeSettings.setFromFilePath(frame.file.path);
             const getRoutesHash = () => frontendSettings.getCurrentHash('routes');
             await settingsService.syncRoutesHash(getRoutesHash);
         }
@@ -188,7 +188,7 @@ module.exports = {
             method: 'browse'
         },
         query() {
-            return frontendRouting.settings.get();
+            return routeSettings.get();
         }
     }
 };
