1ff4f6ce7d152fcabd96d66f553a6df5e75d8c9b
Daniel Lockyer <hi@daniellockyer.com>
2021-06-29 12:14:48 +0100

    Added guards against `parentPort` being `null`
    fixes https://github.com/TryGhost/Team/issues/834
    
    - see referenced issue for context
    - there are times when `parentPort` can be null and the job crashes
      because `parentPort.postMessage` won't work
    - this commit adds guards around `parentPort`, or moves code inside
      existing guards, to protect against this

--- core/server/web/api/testmode/jobs/graceful-job.js
+++ core/server/web/api/testmode/jobs/graceful-job.js
@@ -3,13 +3,21 @@ const util = require('util');
 
 let shutdown = false;
 
-parentPort.on('message', (message) => {
-    parentPort.postMessage(`parent message received: ${message}`);
-
-    if (message === 'cancel') {
-        shutdown = true;
+const postParentPortMessage = (message) => {
+    if (parentPort) {
+        parentPort.postMessage(message);
     }
-});
+};
+
+if (parentPort) {
+    parentPort.on('message', (message) => {
+        parentPort.postMessage(`parent message received: ${message}`);
+
+        if (message === 'cancel') {
+            shutdown = true;
+        }
+    });
+}
 
 const setTimeoutPromise = util.promisify(setTimeout);
 const internalContext = {context: {internal: true}};
@@ -19,25 +27,24 @@ const internalContext = {context: {internal: true}};
 
     await models.init();
 
-    parentPort.postMessage(`Fetching tags`);
+    postParentPortMessage(`Fetching tags`);
     const tags = await models.Tag.findPage(internalContext);
 
-    parentPort.postMessage(`Found ${tags.data.length} tags. First one: ${tags.data[0].toJSON().slug}`);
+    postParentPortMessage(`Found ${tags.data.length} tags. First one: ${tags.data[0].toJSON().slug}`);
 
-    parentPort.postMessage(`Waiting 5 seconds to perform second part of the job`);
+    postParentPortMessage(`Waiting 5 seconds to perform second part of the job`);
     await setTimeoutPromise(5 * 1000);
 
     if (shutdown) {
-        parentPort.postMessage(`Job shutting down gracefully`);
+        postParentPortMessage(`Job shutting down gracefully`);
         process.exit(0);
     }
 
-    parentPort.postMessage(`Fetching posts`);
+    postParentPortMessage(`Fetching posts`);
     const posts = await models.Post.findPage(internalContext);
 
-    parentPort.postMessage(`Found ${posts.data.length} posts. First one: ${posts.data[0].toJSON().slug}`);
-
-    parentPort.postMessage('Graceful job has completed!');
+    postParentPortMessage(`Found ${posts.data.length} posts. First one: ${posts.data[0].toJSON().slug}`);
+    postParentPortMessage('Graceful job has completed!');
 
     process.exit(0);
 })();
