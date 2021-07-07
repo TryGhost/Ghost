1a6759748bd6ff55dd1eda01259f2aa5bc101cc7
Naz <nazargargol@gmail.com>
2021-07-07 18:32:41 +0400

    Refactor imports in API's routes files
    refs https://github.com/TryGhost/Team/issues/856
    
    - There were two problems with routes.js files defining API routes:
    - First, the module requires wen too deep into the "api" module and used specific api modules directly. We have an "index.js" file which defines an API for whole API, it should be used as an entry point to anything to do with the API.
    - Second, The naming was inconsistent between the routes.js files for "api", "apiV2", "apiCanary" - it is an extra maintenance burden to go on and change each "api" name when the new version is introduced. The only thing that should be changed within these files is a single line on very top that "requires" a specific API version like so: "const api = require('../../../../api').canary;" - way less maintenance to change that canary to v5 instead of doing an extra rename for all "apiCanary" to "apiV5"

--- core/server/web/api/v2/admin/routes.js
+++ core/server/web/api/v2/admin/routes.js
@@ -1,5 +1,5 @@
 const express = require('../../../../../shared/express');
-const apiv2 = require('../../../../api/v2');
+const api = require('../../../../api').v2;
 const mw = require('./middleware');
 const apiMw = require('../../middleware');
 
@@ -13,153 +13,153 @@ module.exports = function apiRoutes() {
 
     router.use(apiMw.cors);
 
-    const http = apiv2.http;
+    const http = api.http;
 
     // ## Public
-    router.get('/site', mw.publicAdminApi, http(apiv2.site.read));
+    router.get('/site', mw.publicAdminApi, http(api.site.read));
 
     // ## Configuration
-    router.get('/config', mw.authAdminApi, http(apiv2.config.read));
+    router.get('/config', mw.authAdminApi, http(api.config.read));
 
     // ## Posts
-    router.get('/posts', mw.authAdminApi, http(apiv2.posts.browse));
-    router.post('/posts', mw.authAdminApi, http(apiv2.posts.add));
-    router.get('/posts/:id', mw.authAdminApi, http(apiv2.posts.read));
-    router.get('/posts/slug/:slug', mw.authAdminApi, http(apiv2.posts.read));
-    router.put('/posts/:id', mw.authAdminApi, http(apiv2.posts.edit));
-    router.del('/posts/:id', mw.authAdminApi, http(apiv2.posts.destroy));
+    router.get('/posts', mw.authAdminApi, http(api.posts.browse));
+    router.post('/posts', mw.authAdminApi, http(api.posts.add));
+    router.get('/posts/:id', mw.authAdminApi, http(api.posts.read));
+    router.get('/posts/slug/:slug', mw.authAdminApi, http(api.posts.read));
+    router.put('/posts/:id', mw.authAdminApi, http(api.posts.edit));
+    router.del('/posts/:id', mw.authAdminApi, http(api.posts.destroy));
 
     // ## Pages
-    router.get('/pages', mw.authAdminApi, http(apiv2.pages.browse));
-    router.post('/pages', mw.authAdminApi, http(apiv2.pages.add));
-    router.get('/pages/:id', mw.authAdminApi, http(apiv2.pages.read));
-    router.get('/pages/slug/:slug', mw.authAdminApi, http(apiv2.pages.read));
-    router.put('/pages/:id', mw.authAdminApi, http(apiv2.pages.edit));
-    router.del('/pages/:id', mw.authAdminApi, http(apiv2.pages.destroy));
+    router.get('/pages', mw.authAdminApi, http(api.pages.browse));
+    router.post('/pages', mw.authAdminApi, http(api.pages.add));
+    router.get('/pages/:id', mw.authAdminApi, http(api.pages.read));
+    router.get('/pages/slug/:slug', mw.authAdminApi, http(api.pages.read));
+    router.put('/pages/:id', mw.authAdminApi, http(api.pages.edit));
+    router.del('/pages/:id', mw.authAdminApi, http(api.pages.destroy));
 
     // # Integrations
 
-    router.get('/integrations', mw.authAdminApi, http(apiv2.integrations.browse));
-    router.get('/integrations/:id', mw.authAdminApi, http(apiv2.integrations.read));
-    router.post('/integrations', mw.authAdminApi, http(apiv2.integrations.add));
-    router.put('/integrations/:id', mw.authAdminApi, http(apiv2.integrations.edit));
-    router.del('/integrations/:id', mw.authAdminApi, http(apiv2.integrations.destroy));
+    router.get('/integrations', mw.authAdminApi, http(api.integrations.browse));
+    router.get('/integrations/:id', mw.authAdminApi, http(api.integrations.read));
+    router.post('/integrations', mw.authAdminApi, http(api.integrations.add));
+    router.put('/integrations/:id', mw.authAdminApi, http(api.integrations.edit));
+    router.del('/integrations/:id', mw.authAdminApi, http(api.integrations.destroy));
 
     // ## Schedules
-    router.put('/schedules/:resource/:id', mw.authAdminApiWithUrl, http(apiv2.schedules.publish));
+    router.put('/schedules/:resource/:id', mw.authAdminApiWithUrl, http(api.schedules.publish));
 
     // ## Settings
-    router.get('/settings/routes/yaml', mw.authAdminApi, http(apiv2.settings.download));
+    router.get('/settings/routes/yaml', mw.authAdminApi, http(api.settings.download));
     router.post('/settings/routes/yaml',
         mw.authAdminApi,
         apiMw.upload.single('routes'),
         apiMw.upload.validation({type: 'routes'}),
-        http(apiv2.settings.upload)
+        http(api.settings.upload)
     );
 
-    router.get('/settings', mw.authAdminApi, http(apiv2.settings.browse));
-    router.get('/settings/:key', mw.authAdminApi, http(apiv2.settings.read));
-    router.put('/settings', mw.authAdminApi, http(apiv2.settings.edit));
+    router.get('/settings', mw.authAdminApi, http(api.settings.browse));
+    router.get('/settings/:key', mw.authAdminApi, http(api.settings.read));
+    router.put('/settings', mw.authAdminApi, http(api.settings.edit));
 
     // ## Users
-    router.get('/users', mw.authAdminApi, http(apiv2.users.browse));
-    router.get('/users/:id', mw.authAdminApi, http(apiv2.users.read));
-    router.get('/users/slug/:slug', mw.authAdminApi, http(apiv2.users.read));
+    router.get('/users', mw.authAdminApi, http(api.users.browse));
+    router.get('/users/:id', mw.authAdminApi, http(api.users.read));
+    router.get('/users/slug/:slug', mw.authAdminApi, http(api.users.read));
     // NOTE: We don't expose any email addresses via the public api.
-    router.get('/users/email/:email', mw.authAdminApi, http(apiv2.users.read));
+    router.get('/users/email/:email', mw.authAdminApi, http(api.users.read));
 
-    router.put('/users/password', mw.authAdminApi, http(apiv2.users.changePassword));
-    router.put('/users/owner', mw.authAdminApi, http(apiv2.users.transferOwnership));
-    router.put('/users/:id', mw.authAdminApi, http(apiv2.users.edit));
-    router.del('/users/:id', mw.authAdminApi, http(apiv2.users.destroy));
+    router.put('/users/password', mw.authAdminApi, http(api.users.changePassword));
+    router.put('/users/owner', mw.authAdminApi, http(api.users.transferOwnership));
+    router.put('/users/:id', mw.authAdminApi, http(api.users.edit));
+    router.del('/users/:id', mw.authAdminApi, http(api.users.destroy));
 
     // ## Tags
-    router.get('/tags', mw.authAdminApi, http(apiv2.tags.browse));
-    router.get('/tags/:id', mw.authAdminApi, http(apiv2.tags.read));
-    router.get('/tags/slug/:slug', mw.authAdminApi, http(apiv2.tags.read));
-    router.post('/tags', mw.authAdminApi, http(apiv2.tags.add));
-    router.put('/tags/:id', mw.authAdminApi, http(apiv2.tags.edit));
-    router.del('/tags/:id', mw.authAdminApi, http(apiv2.tags.destroy));
+    router.get('/tags', mw.authAdminApi, http(api.tags.browse));
+    router.get('/tags/:id', mw.authAdminApi, http(api.tags.read));
+    router.get('/tags/slug/:slug', mw.authAdminApi, http(api.tags.read));
+    router.post('/tags', mw.authAdminApi, http(api.tags.add));
+    router.put('/tags/:id', mw.authAdminApi, http(api.tags.edit));
+    router.del('/tags/:id', mw.authAdminApi, http(api.tags.destroy));
 
     // ## Roles
-    router.get('/roles/', mw.authAdminApi, http(apiv2.roles.browse));
+    router.get('/roles/', mw.authAdminApi, http(api.roles.browse));
 
     // ## Slugs
-    router.get('/slugs/:type/:name', mw.authAdminApi, http(apiv2.slugs.generate));
+    router.get('/slugs/:type/:name', mw.authAdminApi, http(api.slugs.generate));
 
     // ## Themes
-    router.get('/themes/', mw.authAdminApi, http(apiv2.themes.browse));
+    router.get('/themes/', mw.authAdminApi, http(api.themes.browse));
 
     router.get('/themes/:name/download',
         mw.authAdminApi,
-        http(apiv2.themes.download)
+        http(api.themes.download)
     );
 
     router.post('/themes/upload',
         mw.authAdminApi,
         apiMw.upload.single('file'),
         apiMw.upload.validation({type: 'themes'}),
-        http(apiv2.themes.upload)
+        http(api.themes.upload)
     );
 
     router.put('/themes/:name/activate',
         mw.authAdminApi,
-        http(apiv2.themes.activate)
+        http(api.themes.activate)
     );
 
     router.del('/themes/:name',
         mw.authAdminApi,
-        http(apiv2.themes.destroy)
+        http(api.themes.destroy)
     );
 
     // ## Notifications
-    router.get('/notifications', mw.authAdminApi, http(apiv2.notifications.browse));
-    router.post('/notifications', mw.authAdminApi, http(apiv2.notifications.add));
-    router.del('/notifications/:notification_id', mw.authAdminApi, http(apiv2.notifications.destroy));
+    router.get('/notifications', mw.authAdminApi, http(api.notifications.browse));
+    router.post('/notifications', mw.authAdminApi, http(api.notifications.add));
+    router.del('/notifications/:notification_id', mw.authAdminApi, http(api.notifications.destroy));
 
     // ## DB
-    router.get('/db', mw.authAdminApi, http(apiv2.db.exportContent));
+    router.get('/db', mw.authAdminApi, http(api.db.exportContent));
     router.post('/db',
         mw.authAdminApi,
         apiMw.upload.single('importfile'),
         apiMw.upload.validation({type: 'db'}),
-        http(apiv2.db.importContent)
+        http(api.db.importContent)
     );
-    router.del('/db', mw.authAdminApi, http(apiv2.db.deleteAllContent));
+    router.del('/db', mw.authAdminApi, http(api.db.deleteAllContent));
     router.post('/db/backup',
         mw.authAdminApi,
-        http(apiv2.db.backupContent)
+        http(api.db.backupContent)
     );
 
     // ## Mail
-    router.post('/mail', mw.authAdminApi, http(apiv2.mail.send));
-    router.post('/mail/test', mw.authAdminApi, http(apiv2.mail.sendTest));
+    router.post('/mail', mw.authAdminApi, http(api.mail.send));
+    router.post('/mail/test', mw.authAdminApi, http(api.mail.sendTest));
 
     // ## Slack
-    router.post('/slack/test', mw.authAdminApi, http(apiv2.slack.sendTest));
+    router.post('/slack/test', mw.authAdminApi, http(api.slack.sendTest));
 
     // ## Sessions
-    router.get('/session', mw.authAdminApi, http(apiv2.session.read));
+    router.get('/session', mw.authAdminApi, http(api.session.read));
     // We don't need auth when creating a new session (logging in)
     router.post('/session',
         shared.middlewares.brute.globalBlock,
         shared.middlewares.brute.userLogin,
-        http(apiv2.session.add)
+        http(api.session.add)
     );
-    router.del('/session', mw.authAdminApi, http(apiv2.session.delete));
+    router.del('/session', mw.authAdminApi, http(api.session.delete));
 
     // ## Authentication
     router.post('/authentication/passwordreset',
         shared.middlewares.brute.globalReset,
         shared.middlewares.brute.userReset,
-        http(apiv2.authentication.generateResetToken)
+        http(api.authentication.generateResetToken)
     );
-    router.put('/authentication/passwordreset', shared.middlewares.brute.globalBlock, http(apiv2.authentication.resetPassword));
-    router.post('/authentication/invitation', http(apiv2.authentication.acceptInvitation));
-    router.get('/authentication/invitation', http(apiv2.authentication.isInvitation));
-    router.post('/authentication/setup', http(apiv2.authentication.setup));
-    router.put('/authentication/setup', mw.authAdminApi, http(apiv2.authentication.updateSetup));
-    router.get('/authentication/setup', http(apiv2.authentication.isSetup));
+    router.put('/authentication/passwordreset', shared.middlewares.brute.globalBlock, http(api.authentication.resetPassword));
+    router.post('/authentication/invitation', http(api.authentication.acceptInvitation));
+    router.get('/authentication/invitation', http(api.authentication.isInvitation));
+    router.post('/authentication/setup', http(api.authentication.setup));
+    router.put('/authentication/setup', mw.authAdminApi, http(api.authentication.updateSetup));
+    router.get('/authentication/setup', http(api.authentication.isSetup));
 
     // ## Images
     router.post('/images/upload',
@@ -167,34 +167,34 @@ module.exports = function apiRoutes() {
         apiMw.upload.single('file'),
         apiMw.upload.validation({type: 'images'}),
         apiMw.normalizeImage,
-        http(apiv2.images.upload)
+        http(api.images.upload)
     );
 
     // ## Invites
-    router.get('/invites', mw.authAdminApi, http(apiv2.invites.browse));
-    router.get('/invites/:id', mw.authAdminApi, http(apiv2.invites.read));
-    router.post('/invites', mw.authAdminApi, http(apiv2.invites.add));
-    router.del('/invites/:id', mw.authAdminApi, http(apiv2.invites.destroy));
+    router.get('/invites', mw.authAdminApi, http(api.invites.browse));
+    router.get('/invites/:id', mw.authAdminApi, http(api.invites.read));
+    router.post('/invites', mw.authAdminApi, http(api.invites.add));
+    router.del('/invites/:id', mw.authAdminApi, http(api.invites.destroy));
 
     // ## Redirects (JSON based)
-    router.get('/redirects/json', mw.authAdminApi, http(apiv2.redirects.download));
+    router.get('/redirects/json', mw.authAdminApi, http(api.redirects.download));
     router.post('/redirects/json',
         mw.authAdminApi,
         apiMw.upload.single('redirects'),
         apiMw.upload.validation({type: 'redirects'}),
-        http(apiv2.redirects.upload)
+        http(api.redirects.upload)
     );
 
     // ## Webhooks (RESTHooks)
-    router.post('/webhooks', mw.authAdminApi, http(apiv2.webhooks.add));
-    router.put('/webhooks/:id', mw.authAdminApi, http(apiv2.webhooks.edit));
-    router.del('/webhooks/:id', mw.authAdminApi, http(apiv2.webhooks.destroy));
+    router.post('/webhooks', mw.authAdminApi, http(api.webhooks.add));
+    router.put('/webhooks/:id', mw.authAdminApi, http(api.webhooks.edit));
+    router.del('/webhooks/:id', mw.authAdminApi, http(api.webhooks.destroy));
 
     // ## Oembed (fetch response from oembed provider)
-    router.get('/oembed', mw.authAdminApi, http(apiv2.oembed.read));
+    router.get('/oembed', mw.authAdminApi, http(api.oembed.read));
 
     // ## Actions
-    router.get('/actions/:type/:id', mw.authAdminApi, http(apiv2.actions.browse));
+    router.get('/actions/:type/:id', mw.authAdminApi, http(api.actions.browse));
 
     return router;
 };
