89c7618efda833bed74982cbfac2fbbc5070881e
Hannah Wolfe <github.erisds@gmail.com>
2021-07-07 11:51:04 +0100

    Refactored theme service to use async/await
    refs: https://github.com/TryGhost/Team/issues/831
    
    - We prefer async/await over promise chains because it makes the code much more readable
    - the Theme Service needs further work and this should make that work much easier
       - e.g. https://github.com/TryGhost/Team/issues/831
       - e.g. fixing up the index.js file

--- core/server/services/themes/validate.js
+++ core/server/services/themes/validate.js
@@ -16,63 +16,59 @@ const canActivate = function canActivate(checkedTheme) {
     return !checkedTheme.results.error.length || (config.get('env') === 'development') && !checkedTheme.results.hasFatalErrors;
 };
 
-const check = function check(theme, isZip) {
+const check = async function check(theme, isZip) {
     debug('Begin: Check');
     // gscan can slow down boot time if we require on boot, for now nest the require.
     const gscan = require('gscan');
-    let checkPromise;
+    let checkedTheme;
 
     if (isZip) {
         debug('zip mode');
-        checkPromise = gscan.checkZip(theme, {
+        checkedTheme = await gscan.checkZip(theme, {
             keepExtractedDir: true,
             checkVersion: 'canary'
         });
     } else {
         debug('non-zip mode');
-        checkPromise = gscan.check(theme.path, {
+        checkedTheme = await gscan.check(theme.path, {
             checkVersion: 'canary'
         });
     }
 
-    return checkPromise
-        .then(function resultHandler(checkedTheme) {
-            checkedTheme = gscan.format(checkedTheme, {
-                onlyFatalErrors: config.get('env') === 'production',
-                checkVersion: 'canary'
-            });
+    checkedTheme = gscan.format(checkedTheme, {
+        onlyFatalErrors: config.get('env') === 'production',
+        checkVersion: 'canary'
+    });
 
-            debug('End: Check');
-            return checkedTheme;
-        });
+    debug('End: Check');
+    return checkedTheme;
 };
 
-const checkSafe = function checkSafe(theme, isZip) {
-    return check(theme, isZip)
-        .then((checkedTheme) => {
-            if (canActivate(checkedTheme)) {
-                return checkedTheme;
-            }
+const checkSafe = async function checkSafe(theme, isZip) {
+    const checkedTheme = await check(theme, isZip);
 
-            // NOTE: When theme cannot be activated and gscan explicitly keeps extracted files (after
-            //       being called with `keepExtractedDir: true`), this is the closes place for a cleanup.
-            // TODO: The `keepExtractedDir` flag is the cause of confusion for when and where the cleanup
-            //       should be done. It's probably best if gscan is called directly with path to the extracted
-            //       directory, this would allow keeping gscan to do just one thing - validate the theme, and
-            //       file manipulations could be left to another module/library
-            if (isZip) {
-                fs.remove(checkedTheme.path);
-            }
+    if (canActivate(checkedTheme)) {
+        return checkedTheme;
+    }
 
-            return Promise.reject(new errors.ThemeValidationError({
-                message: tpl(messages.invalidTheme),
-                errorDetails: Object.assign(
-                    _.pick(checkedTheme, ['checkedVersion', 'name', 'path', 'version']), {
-                        errors: checkedTheme.results.error
-                    }
-                )
-            }));
-        });
+    // NOTE: When theme cannot be activated and gscan explicitly keeps extracted files (after
+    //       being called with `keepExtractedDir: true`), this is the best place for a cleanup.
+    // TODO: The `keepExtractedDir` flag is the cause of confusion for when and where the cleanup
+    //       should be done. It's probably best if gscan is called directly with path to the extracted
+    //       directory, this would allow keeping gscan to do just one thing - validate the theme, and
+    //       file manipulations could be left to another module/library
+    if (isZip) {
+        fs.remove(checkedTheme.path);
+    }
+
+    return Promise.reject(new errors.ThemeValidationError({
+        message: tpl(messages.invalidTheme),
+        errorDetails: Object.assign(
+            _.pick(checkedTheme, ['checkedVersion', 'name', 'path', 'version']), {
+                errors: checkedTheme.results.error
+            }
+        )
+    }));
 };
 
 module.exports.check = check;
