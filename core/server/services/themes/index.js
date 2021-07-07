4da7e7f0cbd3b082567ddf840bdcc7861cfcfcdf
Hannah Wolfe <github.erisds@gmail.com>
2021-07-07 14:14:20 +0100

    Rework the themeService public API
    refs: https://github.com/TryGhost/Team/issues/831
    
    - This ultimately fixes the index.js file
    - It also makes it super clear what methods in the themeService are used by the API, and which are part of the service loading logic
    - It also moves the activate and init function into a single file in a way that highlights they are very similar
    - They are also very similar to what happens in storage.setFromZip but that code is mixed up with storage code at the moment

--- core/server/services/themes/index.js
+++ core/server/services/themes/index.js
@@ -1,73 +1,31 @@
-const debug = require('@tryghost/debug')('themes');
-const logging = require('@tryghost/logging');
-const errors = require('@tryghost/errors');
-const tpl = require('@tryghost/tpl');
+const activate = require('./activate');
 const themeLoader = require('./loader');
-const validate = require('./validate');
-const list = require('./list');
-const activator = require('./activation-bridge');
+const storage = require('./storage');
+const getJSON = require('./to-json');
+
 const settingsCache = require('../../../shared/settings-cache');
 
-const messages = {
-    activeThemeIsMissing: 'The currently active theme "{theme}" is missing.',
-    themeCannotBeActivated: '{themeName} cannot be activated because it was not found in the theme directory.'
-};
-
 module.exports = {
-    // Init themes module
-    // TODO: move this once we're clear what needs to happen here
+    /*
+     * Load the currently active theme
+     */
     init: async () => {
-        const activeThemeName = settingsCache.get('active_theme');
+        const themeName = settingsCache.get('active_theme');
 
-        debug('init themes', activeThemeName);
-        try {
-            // Just read the active theme for now
-            const theme = await themeLoader.loadOneTheme(activeThemeName);
-            // Validate
-            // @NOTE: this is now the only usage of check, rather than checkSafe...
-            const checkedTheme = await validate.check(theme);
-
-            if (!validate.canActivate(checkedTheme)) {
-                logging.error(validate.getThemeValidationError('activeThemeHasFatalErrors', activeThemeName, checkedTheme));
-            } else if (checkedTheme.results.error.length) {
-                // CASE: inform that the theme has errors, but not fatal (theme still works)
-                logging.warn(validate.getThemeValidationError('activeThemeHasErrors', activeThemeName, checkedTheme));
-            }
-
-            activator.activateFromBoot(activeThemeName, theme, checkedTheme);
-        } catch (err) {
-            if (err instanceof errors.NotFoundError) {
-                // CASE: active theme is missing, we don't want to exit because the admin panel will still work
-                err.message = tpl(messages.activeThemeIsMissing, {theme: activeThemeName});
-            }
-
-            // CASE: theme threw an odd error, we don't want to exit because the admin panel will still work
-            // This is the absolute catch-all, at this point, we do not know what went wrong!
-            logging.error(err);
-        }
+        return activate.loadAndActivate(themeName);
     },
-    getJSON: require('./to-json'),
-    activate: async (themeName) => {
-        const loadedTheme = list.get(themeName);
-
-        if (!loadedTheme) {
-            throw new errors.ValidationError({
-                message: tpl(messages.themeCannotBeActivated, {themeName: themeName}),
-                errorDetails: themeName
-            });
-        }
-
-        const checkedTheme = await validate.checkSafe(themeName, loadedTheme);
-
-        activator.activateFromAPI(themeName, loadedTheme, checkedTheme);
-
-        return checkedTheme;
-    },
-    storage: require('./storage'),
     /**
      * Load all inactive themes
      */
-    loadInactiveThemes: async () => {
-        return await themeLoader.loadAllThemes();
+    loadInactiveThemes: themeLoader.loadAllThemes,
+    /**
+     * Methods used in the API
+     */
+    api: {
+        getJSON,
+        activate: activate.activate,
+        getZip: storage.getZip,
+        setFromZip: storage.setFromZip,
+        destroy: storage.destroy
     }
 };
