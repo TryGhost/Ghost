461c303881e1271ceafed2607d9d8cc8fd031768
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-07-07 19:11:19 +0100

    Fixed error when requesting resize of a blank image
    closes https://github.com/TryGhost/Team/issues/819
    
    - adds guard for an empty buffer when reading file from storage for resizing, if a blank image is loaded then redirect to the original file

--- test/unit/web/site/middleware/handle-image-sizes.test.js
+++ test/unit/web/site/middleware/handle-image-sizes.test.js
@@ -1,4 +1,7 @@
 const should = require('should');
+const sinon = require('sinon');
+const storage = require('../../../../../core/server/adapters/storage');
+const activeTheme = require('../../../../../core/frontend/services/theme-engine/active');
 const handleImageSizes = require('../../../../../core/server/web/site/middleware/handle-image-sizes.js');
 
 // @TODO make these tests lovely and non specific to implementation
@@ -39,6 +42,75 @@ describe('handleImageSizes middleware', function () {
         };
         handleImageSizes(fakeReq, {}, function next() {
             done();
+        });
+    });
+
+    describe('file handling', function () {
+        let dummyStorage;
+        let dummyTheme;
+
+        this.beforeEach(function () {
+            dummyStorage = {
+                async exists() {
+                    return true;
+                },
+
+                read() {
+                    return Buffer.from([]);
+                },
+
+                async saveRaw(buf, url) {
+                    return url;
+                }
+            };
+
+            dummyTheme = {
+                config(key) {
+                    if (key === 'image_sizes') {
+                        return {
+                            l: {
+                                width: 1000
+                            }
+                        };
+                    }
+                }
+            };
+
+            sinon.stub(storage, 'getStorage').returns(dummyStorage);
+            sinon.stub(activeTheme, 'get').returns(dummyTheme);
+        });
+
+        this.afterEach(function () {
+            sinon.restore();
+        });
+
+        it('returns original URL if file is empty', function (done) {
+            dummyStorage.exists = async function (path) {
+                if (path === '/blank_o.png') {
+                    return true;
+                }
+                if (path === '/size/w1000/blank.png') {
+                    return false;
+                }
+            };
+
+            const fakeReq = {
+                url: '/size/w1000/blank.png',
+                originalUrl: '/size/w1000/blank.png'
+            };
+            const fakeRes = {
+                redirect(url) {
+                    url.should.equal('/blank.png');
+                    done();
+                }
+            };
+
+            handleImageSizes(fakeReq, fakeRes, function next(err) {
+                if (err) {
+                    return done(err);
+                }
+                done(new Error('Should not have called next'));
+            });
         });
     });
 });
