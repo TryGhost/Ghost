dfe1089ee2ce846fb74b6651582e5a3a76dbdec5
Naz <nazargargol@gmail.com>
2021-07-07 18:44:21 +0400

    Bumped default API version export to be canary/v4
    refs https://github.com/TryGhost/Team/issues/856
    
    - The default internal version of the API is expected to be the latest one available which is v4/canary at the moment.
    - There will be more information posted in the referenced issue later around how to approach the "default version", for now it's just a change to make a small step into a right direction.

--- core/server/api/index.js
+++ core/server/api/index.js
@@ -1,6 +1,10 @@
-module.exports = require('./v3');
-module.exports.canary = require('./canary');
-module.exports.v4 = require('./canary');
+module.exports.shared = require('./shared');
+
+const defaultAPI = require('./canary');
+
+module.exports = defaultAPI;
+module.exports.canary = defaultAPI;
+module.exports.v4 = defaultAPI;
 module.exports.v3 = require('./v3');
 module.exports.v2 = require('./v2');
-module.exports.shared = require('./shared');
+
