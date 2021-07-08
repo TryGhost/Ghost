7ab0db9be73ed2ef0ac6d200a71da93e841095ef
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-07-08 22:44:52 +0100

    Fixed ember-test-selectors bound attributes deprecations
    no issue
    
    - converted publish menu and `gh-tags-list-item` components to glimmer syntax so data attributes can be passed in via `...attributes`
    - added explicit `data-test-button` bound attribute to `gh-task-button` component
    - moved `modal-theme-warnings` auto-bound data attribute from JS file to explicit html attribute in template

--- ghost/admin/app/components/gh-publishmenu-scheduled.js
+++ ghost/admin/app/components/gh-publishmenu-scheduled.js
@@ -1,74 +1,70 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import moment from 'moment';
-import {computed} from '@ember/object';
+import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
+import {tracked} from '@glimmer/tracking';
 
-export default Component.extend({
-    clock: service(),
-    session: service(),
-    feature: service(),
-    settings: service(),
-    config: service(),
-
-    post: null,
-    saveType: null,
-    isClosing: null,
+export default class GhPublishmenuScheduledComponent extends Component {
+    @service clock;
+    @service session;
+    @service feature;
+    @service settings;
+    @service config;
 
     // used to set minDate in datepicker
-    _minDate: null,
+    @tracked minDate = null;
 
-    'data-test-publishmenu-scheduled': true,
-
-    timeToPublished: computed('post.publishedAtUTC', 'clock.second', function () {
-        let publishedAtUTC = this.get('post.publishedAtUTC');
+    get timeToPublished() {
+        let publishedAtUTC = this.args.post.publishedAtUTC;
 
         if (!publishedAtUTC) {
             return null;
         }
 
-        this.get('clock.second');
+        this.clock.get('second');
 
         return publishedAtUTC.toNow(true);
-    }),
+    }
 
-    didInsertElement() {
-        this.set('_minDate', new Date());
-        this.setSaveType('schedule');
-    },
+    constructor() {
+        super(...arguments);
+        this.minDate = new Date();
+    }
 
-    actions: {
-        setSaveType(type) {
-            if (this.saveType !== type) {
-                this.set('_minDate', new Date());
-                this.setSaveType(type);
+    @action
+    setSaveType(type) {
+        if (this.saveType !== type) {
+            this.minDate = new Date();
+            this.args.setSaveType(type);
 
-                // when draft switch to now to avoid validation errors
-                // when schedule switch back to saved date to avoid unnecessary re-scheduling
-                if (type === 'draft') {
-                    this.post.set('publishedAtBlogTZ', new Date());
-                } else {
-                    this.post.set('publishedAtBlogTZ', this.get('post.publishedAtUTC'));
-                }
-
-                this.post.validate();
+            // when draft switch to now to avoid validation errors
+            // when schedule switch back to saved date to avoid unnecessary re-scheduling
+            if (type === 'draft') {
+                this.args.post.set('publishedAtBlogTZ', new Date());
+            } else {
+                this.args.post.set('publishedAtBlogTZ', this.args.post.publishedAtUTC);
             }
-        },
 
-        setDate(date) {
-            let post = this.post;
-            let dateString = moment(date).format('YYYY-MM-DD');
-
-            post.set('publishedAtBlogDate', dateString);
-            return post.validate();
-        },
-
-        setTime(time) {
-            let post = this.post;
-
-            if (!this.isClosing) {
-                post.set('publishedAtBlogTime', time);
-                return post.validate();
-            }
+            this.args.post.validate();
         }
     }
-});
+
+    @action
+    setDate(date) {
+        let post = this.args.post;
+        let dateString = moment(date).format('YYYY-MM-DD');
+
+        post.set('publishedAtBlogDate', dateString);
+        return post.validate();
+    }
+
+    @action
+    setTime(time) {
+        let post = this.args.post;
+
+        if (!this.args.isClosing) {
+            post.set('publishedAtBlogTime', time);
+            return post.validate();
+        }
+    }
+}
