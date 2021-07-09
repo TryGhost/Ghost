c3d56e03016f783f15b45965b2efe7e1356ee2b9
Rishabh <zrishabhgarg@gmail.com>
2021-07-09 19:51:17 +0530

    🐛 Fixed API errors when including member counts for labels
    closes https://github.com/TryGhost/Team/issues/607
    
    - patch was added in `bookshelf-include-count@0.1.3` to fix member counts for labels
    - bumps `bookshelf-plugins` to include the patch

--- test/api-acceptance/admin/labels.test.js
+++ test/api-acceptance/admin/labels.test.js
@@ -44,4 +44,22 @@ describe('Labels API', function () {
         should.exist(res.headers.location);
         res.headers.location.should.equal(`http://127.0.0.1:2369${localUtils.API.getApiQuery('labels/')}${res.body.labels[0].id}/`);
     });
+
+    it('Can browse with member count', async function () {
+        const res = await request
+            .get(localUtils.API.getApiQuery('labels/?include=count.members'))
+            .set('Origin', config.get('url'))
+            .expect('Content-Type', /json/)
+            .expect('Cache-Control', testUtils.cacheRules.private)
+            .expect(200);
+
+        should.not.exist(res.headers['x-cache-invalidate']);
+        const jsonResponse = res.body;
+        should.exist(jsonResponse);
+        should.exist(jsonResponse.labels);
+
+        jsonResponse.labels.should.have.length(1);
+        should.exist(jsonResponse.labels[0].count);
+        jsonResponse.labels[0].count.members.should.equal(0);
+    });
 });
