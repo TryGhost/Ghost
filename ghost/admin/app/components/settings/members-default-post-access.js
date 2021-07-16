0f9177919c5fccf6a11b81fae1163311716b20e6
Rishabh <zrishabhgarg@gmail.com>
2021-07-16 18:31:23 +0530

    Refined segment selection in default post access setting
    closes https://github.com/TryGhost/Team/issues/862
    
    - allows deleting a single product in the segment dropdown
    - shows error message in segment when no product is selected

--- ghost/admin/app/components/settings/members-default-post-access.js
+++ ghost/admin/app/components/settings/members-default-post-access.js
@@ -1,10 +1,12 @@
 import Component from '@glimmer/component';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
+import {tracked} from '@glimmer/tracking';
 
 export default class SettingsMembersDefaultPostAccess extends Component {
     @service settings;
     @service feature;
+    @tracked showSegmentError;
 
     get options() {
         const defaultOptions = [{
@@ -56,6 +58,10 @@ export default class SettingsMembersDefaultPostAccess extends Component {
     setVisibility(segment) {
         if (segment) {
             this.settings.set('defaultContentVisibility', segment);
+            this.showSegmentError = false;
+        } else {
+            this.settings.set('defaultContentVisibility', '');
+            this.showSegmentError = true;
         }
     }
 
