2fccc9933735acb6786758f6fe81ff2aeb38f039
Naz <nazargargol@gmail.com>
2021-07-28 14:47:16 +0400

    Displed special modal header for disabled email limit
    closes https://github.com/TryGhost/Team/issues/915
    refs https://github.com/TryGhost/Admin/commit/6f50ec5f0
    
    - When the disabled email limit is returned we want to have a special header along with the modal copy

--- ghost/admin/app/components/modal-upgrade-host-limit.js
+++ ghost/admin/app/components/modal-upgrade-host-limit.js
@@ -5,6 +5,16 @@ import {inject as service} from '@ember/service';
 export default ModalComponent.extend({
     router: service(),
 
+    headerMessage: computed('details', function () {
+        let header = 'Upgrade to enable publishing';
+
+        if (this.model.message && this.model.message.match(/account is currently in review/gi)) {
+            header = `Hold up, we're migssing some details`;
+        }
+
+        return header;
+    }),
+
     upgradeMessage: computed('details', function () {
         const {limit, total} = this.model.details || {};
         const message = this.model.message;
