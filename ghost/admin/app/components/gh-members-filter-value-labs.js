281681719ebbddf461a42702ad0d6b1975482f0f
Rishabh <zrishabhgarg@gmail.com>
2021-08-12 13:33:32 +0530

    Added dynamic value selection UI for filter dropdown
    refs https://github.com/TryGhost/Team/issues/943
    
    The filter UI behind labs in Admin allows filtering members list across several filters. Since each filter type can have its own specific set of values to choose from, this change adds custom UI based on filter type to select filter value.

---
+++ ghost/admin/app/components/gh-members-filter-value-labs.js
@@ -0,0 +1,49 @@
+import Component from '@glimmer/component';
+import {action} from '@ember/object';
+
+const FILTER_OPTIONS = {
+    subscriptionPriceInterval: [
+        {label: 'Monthly', name: 'month'},
+        {label: 'Yearly', name: 'year'}
+    ],
+    status: [
+        {label: 'Paid', name: 'paid'},
+        {label: 'Free', name: 'free'},
+        {label: 'Complimentary', name: 'comped'}
+    ],
+    subscribed: [
+        {label: 'Subscribed', name: 'true'},
+        {label: 'Unsubscribed', name: 'false'}
+    ],
+    subscriptionStripeStatus: [
+        {label: 'Active', name: 'active'},
+        {label: 'Trialing', name: 'trialing'},
+        {label: 'Canceled', name: 'canceled'},
+        {label: 'Unpaid', name: 'unpaid'},
+        {label: 'Past Due', name: 'past_due'},
+        {label: 'Incomplete', name: 'incomplete'},
+        {label: 'Incomplete - Expired', name: 'incomplete_expired'}
+    ]
+};
+
+export default class GhMembersFilterValueLabs extends Component {
+    constructor(...args) {
+        super(...args);
+        this.availableFilterOptions = FILTER_OPTIONS;
+    }
+
+    @action
+    setInputFilterValue(filterType, filterId, event) {
+        this.args.setFilterValue(filterType, filterId, event.target.value);
+    }
+
+    @action
+    setLabelsFilterValue(filterType, filterId, labels) {
+        this.args.setFilterValue(filterType, filterId, labels.map(label => label.slug));
+    }
+
+    @action
+    setFilterValue(filterType, filterId, value) {
+        this.args.setFilterValue(filterType, filterId, value);
+    }
+}
