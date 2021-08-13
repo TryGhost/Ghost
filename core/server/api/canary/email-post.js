f300446c207b697d82dca504122f1fd4e8de8070
Naz <nazargargol@gmail.com>
2021-08-13 10:12:54 +0400

    Added an internal API for email-only posts
    refs https://github.com/TryGhost/Team/issues/899
    
    - The internal API is needed to be able to fetch email-only posts through email router. The concept is similar to Preview API with a difference that only posts with `sent` status are accessible and there is content-gating present.

---
+++ core/server/api/canary/email-post.js
@@ -0,0 +1,41 @@
+const i18n = require('../../../shared/i18n');
+const errors = require('@tryghost/errors');
+const models = require('../../models');
+const ALLOWED_INCLUDES = ['authors', 'tags'];
+
+module.exports = {
+    docName: 'email_post',
+
+    read: {
+        permissions: true,
+        options: [
+            'include'
+        ],
+        data: [
+            'slug'
+        ],
+        validation: {
+            options: {
+                include: {
+                    values: ALLOWED_INCLUDES
+                }
+            },
+            data: {
+                slug: {
+                    required: true
+                }
+            }
+        },
+        async query(frame) {
+            const model = await models.Post.findOne(Object.assign(frame.data, {status: 'sent'}), frame.options);
+
+            if (!model) {
+                throw new errors.NotFoundError({
+                    message: i18n.t('errors.api.posts.postNotFound')
+                });
+            }
+
+            return model;
+        }
+    }
+};
