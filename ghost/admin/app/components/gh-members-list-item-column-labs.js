13106d7c806375c233bd514e171b0525448b97e4
Rishabh <zrishabhgarg@gmail.com>
2021-08-16 14:43:23 +0530

    Updated filtered column labels in table
    refs https://github.com/TryGhost/Team/issues/943
    
    - fixes label shown for billing period and subscription status on the table for filtered data

--- ghost/admin/app/components/gh-members-list-item-column-labs.js
+++ ghost/admin/app/components/gh-members-list-item-column-labs.js
@@ -9,4 +9,15 @@ export default class GhMembersListItemColumnLabs extends Component {
         const labelData = this.args.member.get('labels') || [];
         return labelData.map(label => label.name).join(', ');
     }
+
+    get subscriptionStatus() {
+        const subscriptions = this.args.member.get('subscriptions') || [];
+        return subscriptions[0]?.status;
+    }
+
+    get billingPeriod() {
+        const subscriptions = this.args.member.get('subscriptions') || [];
+        const billingPeriod = subscriptions[0]?.price?.interval;
+        return billingPeriod;
+    }
 }
