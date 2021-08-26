cefdbf24b7e82746379c31f870c0e8b83b9ca321
Fabien 'egg' O'Carroll <fabien@allou.is>
2021-08-26 12:00:40 +0200

    Gave Administrators permission to connect to Stripe (#13228)
    refs https://github.com/TryGhost/Team/issues/994
    
    This adds the permission required to connect to Stripe to the
    Administrator role, as required by the linked issue.

--- test/unit/data/schema/fixtures/utils.test.js
+++ test/unit/data/schema/fixtures/utils.test.js
@@ -160,19 +160,19 @@ describe('Migration Fixture Utils', function () {
             fixtureUtils.addFixturesForRelation(fixtures.relations[0]).then(function (result) {
                 should.exist(result);
                 result.should.be.an.Object();
-                result.should.have.property('expected', 79);
-                result.should.have.property('done', 79);
+                result.should.have.property('expected', 80);
+                result.should.have.property('done', 80);
 
                 // Permissions & Roles
                 permsAllStub.calledOnce.should.be.true();
                 rolesAllStub.calledOnce.should.be.true();
-                dataMethodStub.filter.callCount.should.eql(79);
+                dataMethodStub.filter.callCount.should.eql(80);
                 dataMethodStub.find.callCount.should.eql(7);
-                baseUtilAttachStub.callCount.should.eql(79);
+                baseUtilAttachStub.callCount.should.eql(80);
 
-                fromItem.related.callCount.should.eql(79);
-                fromItem.findWhere.callCount.should.eql(79);
-                toItem[0].get.callCount.should.eql(158);
+                fromItem.related.callCount.should.eql(80);
+                fromItem.findWhere.callCount.should.eql(80);
+                toItem[0].get.callCount.should.eql(160);
 
                 done();
             }).catch(done);
