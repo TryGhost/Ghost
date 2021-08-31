a2bbdd7b87a291ef63da882e013b826ab09cbab2
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-08-31 09:59:04 +0100

    Removed `savedIndicator` labs flag
    refs https://github.com/TryGhost/Team/issues/779
    
    - Admin change is no longer behind the flag so it's now unused

--- core/shared/labs.js
+++ core/shared/labs.js
@@ -24,7 +24,6 @@ const BETA_FEATURES = [
 const ALPHA_FEATURES = [
     'emailCardSegments',
     'multipleProducts',
-    'savedIndicator',
     'featureImgDragDrop',
     'oauthLogin',
     'membersFiltering',
