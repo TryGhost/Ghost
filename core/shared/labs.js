dcaab2e9565b10acf2ed8b383019a9da2ac91132
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-08-31 14:22:14 +0100

    Removed `featureImgDragDrop` labs flag
    closes https://github.com/TryGhost/Team/issues/884
    
    - Admin change is no longer behind the flag so it's now unused

--- core/shared/labs.js
+++ core/shared/labs.js
@@ -24,7 +24,6 @@ const BETA_FEATURES = [
 const ALPHA_FEATURES = [
     'emailCardSegments',
     'multipleProducts',
-    'featureImgDragDrop',
     'oauthLogin',
     'membersFiltering',
     'emailOnlyPosts',
