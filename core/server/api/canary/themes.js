84c88683ba8c12b13564605d69d7221fdffa2d9c
Naz <nazargargol@gmail.com>
2021-09-03 20:29:37 +0400

    Removed method complexity in themes API controller
    refs https://github.com/TryGhost/Team/issues/694
    
    - The controller code is not meant to contain complex business logic.
    - Kept the pattern used in all modules under services/themes. The install module shold be refactored into a class with DI pattern when touched next.

--- core/server/api/canary/themes.js
+++ core/server/api/canary/themes.js
@@ -1,13 +1,6 @@
-const fs = require('fs-extra');
-const os = require('os');
-const path = require('path');
-const security = require('@tryghost/security');
 const themeService = require('../../services/themes');
 const limitService = require('../../services/limits');
 const models = require('../../models');
-const request = require('@tryghost/request');
-const errors = require('@tryghost/errors/lib/errors');
-const i18n = require('../../../shared/i18n');
 
 // Used to emit theme.uploaded which is used in core/server/analytics-events
 const events = require('../../lib/common/events');
@@ -83,62 +76,15 @@ module.exports = {
         },
         async query(frame) {
             if (frame.options.source === 'github') {
-                const [org, repo] = frame.options.ref.toLowerCase().split('/');
+                const {theme, themeOverridden} = await themeService.api.installFromGithub(frame.options.ref);
 
-                //TODO: move the organization check to config
-                if (limitService.isLimited('customThemes') && org.toLowerCase() !== 'tryghost') {
-                    await limitService.errorIfWouldGoOverLimit('customThemes', {value: repo.toLowerCase()});
+                if (themeOverridden) {
+                    this.headers.cacheInvalidate = true;
                 }
 
-                // omit /:ref so we fetch the default branch
-                const zipUrl = `https://api.github.com/repos/${org}/${repo}/zipball`;
-                const zipName = `${repo}.zip`;
+                events.emit('theme.uploaded', {name: theme.name});
 
-                // store zip in a unique temporary folder to avoid conflicts
-                const downloadBase = path.join(os.tmpdir(), security.identifier.uid(10));
-                const downloadPath = path.join(downloadBase, zipName);
-
-                await fs.ensureDir(downloadBase);
-
-                try {
-                    // download zip file
-                    const response = await request(zipUrl, {
-                        followRedirect: true,
-                        headers: {
-                            accept: 'application/vnd.github.v3+json'
-                        },
-                        encoding: null
-                    });
-
-                    await fs.writeFile(downloadPath, response.body);
-
-                    // install theme from zip
-                    const zip = {
-                        path: downloadPath,
-                        name: zipName
-                    };
-                    const {theme, themeOverridden} = await themeService.api.setFromZip(zip);
-
-                    if (themeOverridden) {
-                        this.headers.cacheInvalidate = true;
-                    }
-
-                    events.emit('theme.uploaded', {name: theme.name});
-
-                    return theme;
-                } catch (e) {
-                    if (e.statusCode && e.statusCode === 404) {
-                        return Promise.reject(new errors.BadRequestError({
-                            message: i18n.t('errors.api.themes.repoDoesNotExist'),
-                            context: zipUrl
-                        }));
-                    }
-
-                    throw e;
-                } finally {
-                    // clean up tmp dir with downloaded file
-                    fs.remove(downloadBase);
-                }
+                return theme;
             }
         }
     },
