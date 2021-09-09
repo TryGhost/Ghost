6e3d2bef3a8c5b2e091162fa052e1c7a0bb33195
Rishabh <zrishabhgarg@gmail.com>
2021-09-09 15:13:21 +0530

    Updated filter UI to show existing filters on page refresh
    refs https://github.com/TryGhost/Team/issues/972
    
    If a filter is applied in the new members list then the URL gets updated and the filter dropdown contains the filters that have been created. When refreshing the page while a filter is applied, the URL remains the same, the filter is still applied but the UI in the filter dropdown resets and doesn't show the appropriate filter fields and values.
    
    This change parses the nql filter from URL using `nql-lang` library and rebuilds the filter UI blocks based on current filter in URL. It also ignores any invalid or unexpected filters in the UI.

--- ghost/admin/app/components/gh-member-label-input-labs.js
+++ ghost/admin/app/components/gh-member-label-input-labs.js
@@ -1,12 +1,9 @@
 import Component from '@glimmer/component';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
-import {tracked} from '@glimmer/tracking';
 
 export default class GhMemberLabelInputLabs extends Component {
     @service store;
-
-    @tracked selectedLabels = [];
 
     get availableLabels() {
         return this._availableLabels.toArray().sort((labelA, labelB) => {
@@ -25,7 +22,18 @@ export default class GhMemberLabelInputLabs extends Component {
         // store and be updated when the above query returns
         this.store.query('label', {limit: 'all'});
         this._availableLabels = this.store.peekAll('label');
-        this.selectedLabels = this.args.labels || [];
+    }
+
+    get selectedLabels() {
+        if (typeof this.args.labels === 'object') {
+            if (this.args.labels?.length && typeof this.args.labels[0] === 'string') {
+                return this.args.labels.map((d) => {
+                    return this.availableLabels.find(label => label.slug === d);
+                }) || [];
+            }
+            return this.args.labels || [];
+        }
+        return [];
     }
 
     willDestroy() {
@@ -54,7 +62,6 @@ export default class GhMemberLabelInputLabs extends Component {
         });
 
         // update labels
-        this.selectedLabels = newLabels;
         this.args.onChange(newLabels);
     }
 
