f1a04ddb5979632198ddf0ce163a13fc745bac33
Rishabh <zrishabhgarg@gmail.com>
2021-09-09 17:08:05 +0530

    ✨ Added new Tiers feature to Beta features section
    refs https://github.com/TryGhost/Team/issues/1026
    
    Tiers is being released as a beta feature with an early-access opt-in flow. Site owners can now opt-in for early access to Tiers feature in Ghost by enabling it from the Beta features section, but note it's a one way door and its not possible to switch it off once enabled. This is to ensure that sites don't break in any unexpected ways once the tiers feature is enabled by switching it off.

--- ghost/admin/app/components/modal-enable-tiers.js
+++ ghost/admin/app/components/modal-enable-tiers.js
@@ -1,3 +1,23 @@
 import ModalComponent from 'ghost-admin/components/modal-base';
+import {inject as service} from '@ember/service';
+import {task} from 'ember-concurrency';
 
-export default ModalComponent.extend({});
+export default ModalComponent.extend({
+    feature: service(),
+    // Allowed actions
+    confirm: () => {},
+
+    actions: {
+        confirm() {
+            this.enableTiers.perform();
+        }
+    },
+
+    enableTiers: task(function* () {
+        try {
+            yield this.feature.set('multipleProducts', true);
+        } finally {
+            this.send('closeModal');
+        }
+    }).drop()
+});
