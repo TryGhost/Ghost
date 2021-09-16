2b0d1ee357f09b9d7976495f47f2502dc9da1a6d
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-09-16 20:26:49 +0100

    Fixed customize design route-based modal handling + added unsaved change confirmation
    refs https://github.com/TryGhost/Team/issues/559
    refs https://github.com/TryGhost/Team/issues/1045
    
    - removed need for `{{will-destroy}}` in the modal template by using `willTransition()` in the route that opened the modal instead
    - used the property of new modals acting as promises to implement an unsaved changes confirmation modal
      - added `confirmUnsavedChanges()` method that opens a "are you sure you want to leave?" modal that is treated as a promise. If the modal is closed by the "Leave" button the promise returns true in which case we rollback the unsaved changes. The modal is returned so that other behaviour can use this method and wait for the confirmation result
      - added `willTransition()` route hook that calls `confirmUnsavedChanges()` when there are unsaved changes and will only continue to transition if the "Leave" button is pressed
      - added `beforeClose()` hook to the customize modal when opening so that we can prevent the customize modal from closing when "Stay" is clicked in the confirmation modal
    - updated `activate()` hook to store the modal instance so it can be closed later
    - updated `deactivate()` hook to call `.close()` on the modal instance rather than using private data/methods on the modals service

--- ghost/admin/app/components/modals/design/customize.js
+++ ghost/admin/app/components/modals/design/customize.js
@@ -5,17 +5,11 @@ import {inject as service} from '@ember/service';
 export default class ModalsDesignAdvancedComponent extends Component {
     @service config;
     @service settings;
-    @service router;
 
     previewIframe = null;
 
     @action
     registerPreviewIframe(iframe) {
         this.previewIframe = iframe;
-    }
-
-    @action
-    willClose() {
-        this.router.transitionTo('settings.design');
     }
 }
