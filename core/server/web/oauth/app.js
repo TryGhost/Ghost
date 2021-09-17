02cb74ef6cceccbca8d1b476ca001f5401b0f88e
Matt Hanley <git@matthanley.co.uk>
2021-09-17 16:33:22 +0100

    Updated OAuth middleware to use the correct URL helpers
    no-ref
    Hardcoded redirect URLs were breaking when admin and site URLs were
    different, or when Ghost is configured with a subdirectory.

--- core/server/web/oauth/app.js
+++ core/server/web/oauth/app.js
@@ -36,11 +36,8 @@ module.exports = function setupOAuthApp() {
      */
     function googleOAuthMiddleware(clientId, secret) {
         return (req, res, next) => {
-            // TODO: use url config instead of the string /ghost
-
             //Create the callback url to be sent to Google
-            const callbackUrl = new URL(urlUtils.getSiteUrl());
-            callbackUrl.pathname = '/ghost/oauth/google/callback';
+            const callbackUrl = new URL(`${urlUtils.urlFor('admin', true)}oauth/google/callback`);
 
             passport.authenticate(new GoogleStrategy({
                 clientID: clientId,
@@ -55,7 +52,7 @@ module.exports = function setupOAuthApp() {
                     const emails = profile.emails.filter(email => email.verified === true).map(email => email.value);
 
                     if (!emails.includes(req.user.get('email'))) {
-                        return res.redirect('/ghost/#/staff/?message=oauth-linking-failed');
+                        return res.redirect(`${urlUtils.urlFor('admin')}#/staff/?message=oauth-linking-failed`);
                     }
 
                     // TODO: configure the oauth data for this user (row in the oauth table)
@@ -70,7 +67,7 @@ module.exports = function setupOAuthApp() {
                     //TODO: instead find the oauth row with the email use the provider id
                     const emails = profile.emails.filter(email => email.verified === true);
                     if (emails.length < 1) {
-                        return res.redirect('/ghost/#/signin?message=login-failed');
+                        return res.redirect(`${urlUtils.urlFor('admin')}#/signin?message=login-failed`);
                     }
                     const email = emails[0].value;
 
@@ -85,7 +82,7 @@ module.exports = function setupOAuthApp() {
                         let invite = await models.Invite.findOne({email, status: 'sent'}, options);
 
                         if (!invite || invite.get('expires') < Date.now()) {
-                            return res.redirect('/ghost/#/signin?message=login-failed');
+                            return res.redirect(`${urlUtils.urlFor('admin')}#/signin?message=login-failed`);
                         }
 
                         //Accept invite
@@ -106,12 +103,11 @@ module.exports = function setupOAuthApp() {
 
                 await auth.session.sessionService.createSessionForUser(req, res, req.user);
 
-                return res.redirect('/ghost/');
+                return res.redirect(urlUtils.urlFor('admin'));
             }), {
                 scope: ['profile', 'email'],
                 session: false,
-                prompt: 'consent',
-                accessType: 'offline'
+                prompt: 'consent'
             })(req, res, next);
         };
     }
@@ -133,7 +129,7 @@ module.exports = function setupOAuthApp() {
 
     oauthApp.get('/:provider/callback', (req, res, next) => {
         // Set the referrer as the ghost instance domain so that the session is linked to the ghost instance domain
-        req.headers.referrer = urlUtils.getSiteUrl();
+        req.headers.referrer = urlUtils.getAdminUrl();
         next();
     }, auth.authenticate.authenticateAdminApi, (req, res, next) => {
         if (req.params.provider !== 'google') {
