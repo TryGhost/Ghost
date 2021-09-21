9fca7ce8f38b52af449bb9c42b08591e54c247a0
Fabien O'Carroll <fabien@allou.is>
2021-09-21 18:27:56 +0200

    Added missing events for analytics
    refs https://github.com/TryGhost/Team/issues/1054
    
    In order to listen to events we must define them! This adds the missing
    events that we need to listen to for member analytics.

--- ghost/member-events/index.js
+++ ghost/member-events/index.js
@@ -1,3 +1,7 @@
 module.exports = {
-    MemberEntryViewEvent: require('./lib/MemberEntryViewEvent')
+    MemberEntryViewEvent: require('./lib/MemberEntryViewEvent'),
+    MemberUnsubscribeEvent: require('./lib/MemberUnsubscribeEvent'),
+    MemberSignupEvent: require('./lib/MemberSignupEvent'),
+    MemberPaidConverstionEvent: require('./lib/MemberPaidConversionEvent'),
+    MemberPaidCancellationEvent: require('./lib/MemberPaidCancellationEvent')
 };
