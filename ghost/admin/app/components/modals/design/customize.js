a1280be244c985268b203f0ed6dae157ca085acc
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-09-21 16:15:23 +0100

    Fixed preview flicker when moving between general and theme settings
    refs https://github.com/TryGhost/Team/issues/1045
    
    - update of the preview was being triggered by the general settings tab rendering (copy/paste from branding setup) meaning the iframe reloaded when switching between general/theme tabs
    - moved the preview update task into the `customize` modal so that it can be called by the general/theme settings components as needed
    - moved initial preview rendering to the iframe registration method

--- ghost/admin/app/components/modals/design/customize.js
+++ ghost/admin/app/components/modals/design/customize.js
@@ -1,9 +1,12 @@
 import Component from '@glimmer/component';
+import config from 'ghost-admin/config/environment';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
+import {task} from 'ember-concurrency-decorators';
 import {tracked} from '@glimmer/tracking';
 
 export default class ModalsDesignCustomizeComponent extends Component {
+    @service ajax;
     @service config;
     @service settings;
 
@@ -19,6 +22,7 @@ export default class ModalsDesignCustomizeComponent extends Component {
     @action
     registerPreviewIframe(iframe) {
         this.previewIframe = iframe;
+        this.updatePreviewTask.perform();
     }
 
     @action
@@ -28,5 +32,39 @@ export default class ModalsDesignCustomizeComponent extends Component {
             this.previewIframe.contentWindow.document.write(html);
             this.previewIframe.contentWindow.document.close();
         }
+    }
+
+    @task
+    *updatePreviewTask() {
+        // skip during testing because we don't have mocks for the front-end
+        if (config.environment === 'test' || !this.previewIframe) {
+            return;
+        }
+
+        // grab the preview html
+        const ajaxOptions = {
+            contentType: 'text/html;charset=utf-8',
+            dataType: 'text',
+            headers: {
+                'x-ghost-preview': this.previewData
+            }
+        };
+
+        // TODO: config.blogUrl always removes trailing slash - switch to always have trailing slash
+        const frontendUrl = `${this.config.get('blogUrl')}/`;
+        const previewContents = yield this.ajax.post(frontendUrl, ajaxOptions);
+
+        // inject extra CSS to disable navigation and prevent clicks
+        const injectedCss = `html { pointer-events: none; }`;
+
+        const domParser = new DOMParser();
+        const htmlDoc = domParser.parseFromString(previewContents, 'text/html');
+
+        const stylesheet = htmlDoc.querySelector('style');
+        const originalCSS = stylesheet.innerHTML;
+        stylesheet.innerHTML = `${originalCSS}\n\n${injectedCss}`;
+
+        // replace the iframe contents with the doctored preview html
+        this.replacePreviewContents(htmlDoc.documentElement.innerHTML);
     }
 }
