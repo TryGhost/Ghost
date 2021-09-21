d51f7aafbad5a3cc69978ba94eed39bb4a51d67f
Fabien O'Carroll <fabien@allou.is>
2021-09-21 18:35:24 +0200

    Added listeners for remaining events
    refs https://github.com/TryGhost/Team/issues/1054
    
    This adds the missing listeners for the rest of the analytics events
    we'd like to capture.

--- ghost/member-analytics-service/lib/EventHandler.js
+++ ghost/member-analytics-service/lib/EventHandler.js
@@ -1,5 +1,11 @@
 const DomainEvents = require('@tryghost/domain-events');
-const {MemberEntryViewEvent} = require('@tryghost/member-events');
+const {
+    MemberEntryViewEvent,
+    MemberUnsubscribeEvent,
+    MemberSignupEvent,
+    MemberPaidConverstionEvent,
+    MemberPaidCancellationEvent
+} = require('@tryghost/member-events');
 
 const AnalyticEvent = require('./AnalyticEvent');
 
@@ -23,6 +29,60 @@ class EventHandler {
                 memberStatus: ev.data.memberStatus,
                 entryId: ev.data.entryId,
                 sourceUrl: ev.data.entryUrl,
+                timestamp: ev.timestamp
+            });
+
+            await this.repository.save(event);
+        });
+
+        DomainEvents.subscribe(MemberUnsubscribeEvent, async (ev) => {
+            const event = AnalyticEvent.create({
+                name: 'unsubscribe',
+                memberId: ev.data.memberId,
+                memberStatus: ev.data.memberStatus,
+                entryId: ev.data.entryId,
+                sourceUrl: ev.data.sourceUrl,
+                timestamp: ev.timestamp
+            });
+
+            await this.repository.save(event);
+        });
+
+        DomainEvents.subscribe(MemberSignupEvent, async (ev) => {
+            const event = AnalyticEvent.create({
+                name: 'signup',
+                memberId: ev.data.memberId,
+                memberStatus: 'free',
+                entryId: ev.data.entryId,
+                sourceUrl: ev.data.sourceUrl,
+                timestamp: ev.timestamp
+            });
+
+            await this.repository.save(event);
+        });
+
+        DomainEvents.subscribe(MemberPaidCancellationEvent, async (ev) => {
+            const event = AnalyticEvent.create({
+                name: 'paid_cancellation',
+                memberId: ev.data.memberId,
+                memberStatus: ev.data.memberStatus,
+                entryId: ev.data.entryId,
+                sourceUrl: ev.data.sourceUrl,
+                metadata: ev.data.subscriptionId,
+                timestamp: ev.timestamp
+            });
+
+            await this.repository.save(event);
+        });
+
+        DomainEvents.subscribe(MemberPaidConverstionEvent, async (ev) => {
+            const event = AnalyticEvent.create({
+                name: 'paid_conversion',
+                memberId: ev.data.memberId,
+                memberStatus: ev.data.memberStatus,
+                entryId: ev.data.entryId,
+                sourceUrl: ev.data.sourceUrl,
+                metadata: ev.data.subscriptionId,
                 timestamp: ev.timestamp
             });
 
