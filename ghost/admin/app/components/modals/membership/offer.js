ce29f7d1f278ebbd9539bc3db5a2e2546d5f1b82
Peter Zimon <zimo@ghost.org>
2021-09-22 17:23:26 +0200

    Added static list of offers

--- ghost/admin/app/components/modals/membership/offer.js
+++ ghost/admin/app/components/modals/membership/offer.js
@@ -2,5 +2,6 @@ import Component from '@glimmer/component';
 import {inject as service} from '@ember/service';
 
 export default class ModalsMembershipOfferComponent extends Component {
+    @service config;
     @service settings;
 }
