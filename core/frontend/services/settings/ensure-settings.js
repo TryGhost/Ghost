411663f10c612eb2be50a22a996bae5c01252978
Naz <nazargargol@gmail.com>
2021-09-23 13:21:44 +0200

    Reworked ensure-settings module to take in singular file path
    refs https://linear.app/tryghost/issue/CORE-35/refactor-route-and-redirect-settings
    
    - This is a micro-step towards getting rid of multiple "knownSettings" concept. Since the introduction of an array of knowSettings there was never-ever a need to handle anything but a single `routes.yaml` file. Getting rid of this concept first to have a simpler module. Next step would be getting rid of filesystem reads/writes in the "frontend"

--- core/frontend/services/settings/ensure-settings.js
+++ core/frontend/services/settings/ensure-settings.js
@@ -7,41 +7,33 @@ const errors = require('@tryghost/errors');
 const config = require('../../../shared/config');
 
 /**
- * Makes sure that all supported settings files are in the
- * `/content/settings` directory. If not, copy the default files
- * over.
- * @param {Array} knownSettings
- * @returns {Promise}
- * @description Reads the `/settings` folder of the content path and makes
- * sure that the associated yaml file for each setting exists. If it doesn't
- * copy the default yaml file over.
+* Makes sure file is in the `/content/settings` directory. If not, copy the default over.
+ * @param {String} fileName - name of the setting file
+ * @returns {Promise<any>}
  */
-module.exports = function ensureSettingsFiles(knownSettings) {
+module.exports = function ensureSettingsFile(fileName) {
     const contentPath = config.getContentPath('settings');
     const defaultSettingsPath = config.get('paths').defaultSettings;
 
-    return Promise.each(knownSettings, function (setting) {
-        const fileName = `${setting}.yaml`;
-        const defaultFileName = `default-${fileName}`;
-        const filePath = path.join(contentPath, fileName);
+    const defaultFileName = `default-${fileName}`;
+    const filePath = path.join(contentPath, fileName);
 
-        return Promise.resolve(fs.readFile(filePath, 'utf8'))
-            .catch({code: 'ENOENT'}, () => {
-                const defaultFilePath = path.join(defaultSettingsPath, defaultFileName);
-                // CASE: file doesn't exist, copy it from our defaults
-                return fs.copy(
-                    defaultFilePath,
-                    filePath
-                ).then(() => {
-                    debug(`'${defaultFileName}' copied to ${contentPath}.`);
-                });
-            }).catch((error) => {
-                // CASE: we might have a permission error, as we can't access the directory
-                throw new errors.GhostError({
-                    message: i18n.t('errors.services.settings.ensureSettings', {path: contentPath}),
-                    err: error,
-                    context: error.path
-                });
+    return Promise.resolve(fs.readFile(filePath, 'utf8'))
+        .catch({code: 'ENOENT'}, () => {
+            const defaultFilePath = path.join(defaultSettingsPath, defaultFileName);
+            // CASE: file doesn't exist, copy it from our defaults
+            return fs.copy(
+                defaultFilePath,
+                filePath
+            ).then(() => {
+                debug(`'${defaultFileName}' copied to ${contentPath}.`);
             });
-    });
+        }).catch((error) => {
+            // CASE: we might have a permission error, as we can't access the directory
+            throw new errors.GhostError({
+                message: i18n.t('errors.services.settings.ensureSettings', {path: contentPath}),
+                err: error,
+                context: error.path
+            });
+        });
 };
