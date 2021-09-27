b6d9bad6dc1ebbc90c49f29489b56ff74a2a5a00
Kevin Ansfield <kevin@ghost.org>
2021-09-27 09:59:09 +0100

    Added custom theme settings browse/edit permissions for Administrators (#13361)
    refs https://github.com/TryGhost/Team/issues/1070TryGhost/Team#1070
    
    - initial implementation will only allow browse+edit via the API

--- test/regression/migrations/migration.test.js
+++ test/regression/migrations/migration.test.js
@@ -45,7 +45,7 @@ describe('Database Migration (special functions)', function () {
             const permissions = this.obj;
 
             // If you have to change this number, please add the relevant `havePermission` checks below
-            permissions.length.should.eql(86);
+            permissions.length.should.eql(88);
 
             permissions.should.havePermission('Export database', ['Administrator', 'DB Backup Integration']);
             permissions.should.havePermission('Import database', ['Administrator', 'DB Backup Integration']);
@@ -154,6 +154,9 @@ describe('Database Migration (special functions)', function () {
             permissions.should.havePermission('Delete Products', ['Administrator']);
 
             permissions.should.havePermission('Reset all passwords', ['Administrator']);
+
+            permissions.should.havePermission('Browse custom theme settings', ['Administrator']);
+            permissions.should.havePermission('Edit custom theme settings', ['Administrator']);
         });
 
         describe('Populate', function () {
@@ -211,7 +214,7 @@ describe('Database Migration (special functions)', function () {
                     result.roles.at(7).get('name').should.eql('Scheduler Integration');
 
                     // Permissions
-                    result.permissions.length.should.eql(86);
+                    result.permissions.length.should.eql(88);
                     result.permissions.toJSON().should.be.CompletePermissions();
                 });
             });
