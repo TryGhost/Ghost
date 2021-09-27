aca4b9bd92c0af8561d44f495eb85a88b674df5f
Kevin Ansfield <kevin@ghost.org>
2021-09-27 11:32:48 +0100

    Wired up custom theme settings display (#2096)
    refs https://github.com/TryGhost/Team/issues/1070
    reqs https://github.com/TryGhost/Ghost/pull/13362
    
    - added custom theme setting model
    - added fetch of custom theme settings when customize modal is opened
    - used loaded theme settings to populate settings list in theme settings tab
    - hid the general/theme toggle when no theme settings are present

---
+++ ghost/admin/app/models/custom-theme-setting.js
@@ -0,0 +1,9 @@
+import Model, {attr} from '@ember-data/model';
+
+export default Model.extend({
+    key: attr('string'),
+    type: attr('string'),
+    options: attr(),
+    default: attr('string'),
+    value: attr('string')
+});
