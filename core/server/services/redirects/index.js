8b6a2bb87f419ffe922100cc612256d9e13df395
Naz <nazargargol@gmail.com>
2021-09-27 19:39:47 +0200

    Simplified redirects module api
    refs https://linear.app/tryghost/issue/CORE-35/refactor-route-and-redirect-settings
    
    - Made clear distinction around the methods that are exposed for the API use and for the internal use. Same pattern can be found in themese's module API

--- core/server/services/redirects/index.js
+++ core/server/services/redirects/index.js
@@ -1,9 +1,15 @@
-module.exports = {
-    get settings() {
-        return require('./settings');
-    },
+const settings = require('./settings');
+const validation = require('./validation');
 
-    get validation() {
-        return require('./validation');
+module.exports = {
+    loadRedirectsFile: settings.loadRedirectsFile,
+    validate: validation.validate,
+    /**
+     * Methods used in the API
+     */
+    api: {
+        getRedirectsFilePath: settings.getRedirectsFilePath,
+        get: settings.get,
+        setFromFilePath: settings.setFromFilePath
     }
 };
