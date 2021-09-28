80a28c1c7904886d56f6fbf92069edf59b962332
Naz <nazargargol@gmail.com>
2021-09-28 09:47:42 +0200

    Simplified route settings module api
    refs https://linear.app/tryghost/issue/CORE-35/refactor-route-and-redirect-settings
    
    - Made clear distinction around the methods that are exposed for the API use and for the internal use. Same pattern can be found in themese's and redirects module APIs

--- core/server/services/route-settings/index.js
+++ core/server/services/route-settings/index.js
@@ -1,1 +1,23 @@
-module.exports = require('./route-settings');
+const routeSettings = require('./route-settings');
+const SettingsLoader = require('./loader');
+const ensureSettingsFile = require('./ensure-settings');
+
+module.exports = {
+    init: async () => {
+        // Make sure that supported settings files are available
+        // inside of the `content/setting` directory
+        return ensureSettingsFile('routes.yaml');
+    },
+
+    loadRouteSettingsSync: SettingsLoader.loadSettingsSync,
+    loadRouteSettings: SettingsLoader.loadSettings,
+    getDefaultHash: routeSettings.getDefaultHash,
+    /**
+     * Methods used in the API
+     */
+    api: {
+        setFromFilePath: routeSettings.setFromFilePath,
+        get: routeSettings.get,
+        getCurrentHash: routeSettings.getCurrentHash
+    }
+};
