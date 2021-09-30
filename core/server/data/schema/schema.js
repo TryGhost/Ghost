2b0237b16954b9cefba58f29539f89ce3698f185
Rishabh <zrishabhgarg@gmail.com>
2021-09-30 22:39:41 +0530

    Added `offers` table
    refs https://github.com/TryGhost/Team/issues/1088
    
    - adds schema for new offers table
    - adds permission fixtures for new offers table
    - adds migrations for new table and permissions

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -384,6 +384,23 @@ module.exports = {
         created_at: {type: 'dateTime', nullable: false},
         updated_at: {type: 'dateTime', nullable: true}
     },
+    offers: {
+        id: {type: 'string', maxlength: 24, nullable: false, primary: true},
+        name: {type: 'string', maxlength: 191, nullable: false, unique: true},
+        code: {type: 'string', maxlength: 191, nullable: false, unique: true},
+        product_id: {type: 'string', maxlength: 24, nullable: false},
+        stripe_coupon_id: {type: 'string', maxlength: 255, nullable: false, unique: true},
+        interval: {type: 'string', maxlength: 50, nullable: false, validations: {isIn: [['month', 'year']]}},
+        currency: {type: 'string', maxlength: 50, nullable: true},
+        discount_type: {type: 'string', maxlength: 50, nullable: false, validations: {isIn: [['percent', 'amount']]}},
+        discount_amount: {type: 'integer', nullable: false},
+        duration: {type: 'string', maxlength: 50, nullable: false},
+        duration_in_months: {type: 'integer', nullable: true},
+        portal_title: {type: 'string', maxlength: 191, nullable: false},
+        portal_description: {type: 'string', maxlength: 2000, nullable: true},
+        created_at: {type: 'dateTime', nullable: false},
+        updated_at: {type: 'dateTime', nullable: true}
+    },
     benefits: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
         name: {type: 'string', maxlength: 191, nullable: false},
