397e587e114a095bfab8e53b0b00326500481a59
Peter Zimon <zimo@ghost.org>
2021-10-04 15:00:41 +0200

    Update offer details structure
    - Added static UI to offer details page
    - Deleted offer modal

--- ghost/admin/app/controllers/offer.js
+++ ghost/admin/app/controllers/offer.js
@@ -1,7 +1,51 @@
 import Controller from '@ember/controller';
 import {action} from '@ember/object';
+import {inject as service} from '@ember/service';
+import {task} from 'ember-concurrency-decorators';
+import {tracked} from '@glimmer/tracking';
 
 export default class MembersController extends Controller {
+    @service config;
+    @service settings;
+    @service store;
+    @tracked cadences = [];
+    @tracked products = [];
+    @tracked selectedDiscountType = 'percentage';
+
+    constructor() {
+        super(...arguments);
+        this.setup();
+    }
+
+    @task({drop: true})
+    *fetchProducts() {
+        this.products = yield this.store.query('product', {include: 'monthly_price,yearly_price,benefits'});
+        const cadences = [];
+        this.products.forEach((product) => {
+            cadences.push({
+                label: `${product.name} - Monthly`,
+                name: product.monthlyPrice.id
+            });
+            cadences.push({
+                label: `${product.name} - Yearly`,
+                name: product.yearlyPrice.id
+            });
+        });
+        this.cadences = cadences;
+    }
+
     @action
-    save() {}
+    setup() {
+        this.fetchProducts.perform();
+    }
+
+    @action
+    setDiscountType(discountType) {
+        this.selectedDiscountType = discountType;
+    }
+
+    @action
+    updateVisibility(tab) {
+        this.tab = tab;
+    }
 }
--- ghost/admin/app/router.js
+++ ghost/admin/app/router.js
@@ -44,9 +44,7 @@ Router.map(function () {
 
     this.route('settings');
     this.route('settings.general', {path: '/settings/general'});
-    this.route('settings.membership', {path: '/settings/members'}, function () {
-        this.route('offer');
-    });
+    this.route('settings.membership', {path: '/settings/members'});
     this.route('settings.members-email', {path: '/settings/members-email'});
     this.route('settings.code-injection', {path: '/settings/code-injection'});
 
