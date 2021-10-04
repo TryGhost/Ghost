394ca79a9f9d12b8c0f07fa7533b64ed5001851b
Megharth Lakhataria <m.c.lakhataria@gmail.com>
2021-10-04 04:57:08 -0400

    Replaced i18n.t w/ tpl helper in email-post.js and email-preview.js (#13418)
    refs: #13380
    
    - this is to replace i18n.t with tpl because i18n.t is deprecated
    - Replaced i18n.t with tpl helper in email-post.js
    - Replaced i18n.t with tpl helper in email-preview.js

--- core/server/api/canary/email-post.js
+++ core/server/api/canary/email-post.js
@@ -1,7 +1,11 @@
-const i18n = require('../../../shared/i18n');
+const tpl = require('@tryghost/tpl');
 const errors = require('@tryghost/errors');
 const models = require('../../models');
 const ALLOWED_INCLUDES = ['authors', 'tags'];
+
+const messages = {
+    postNotFound: 'Post not found.'
+};
 
 module.exports = {
     docName: 'email_post',
@@ -31,7 +35,7 @@ module.exports = {
 
             if (!model) {
                 throw new errors.NotFoundError({
-                    message: i18n.t('errors.api.posts.postNotFound')
+                    message: tpl(messages.postNotFound)
                 });
             }
 
