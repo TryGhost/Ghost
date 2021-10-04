cda041d424779b1376dc5d9e5fc248a0037207ab
Fabien O'Carroll <fabien@allou.is>
2021-10-04 13:18:22 +0200

    Moved StripeAPIService to its own service
    refs https://github.com/TryGhost/Team/issues/1083
    
    The Offers service is going to need access to the StripeAPIService too,
    so we need to move it out of the @tryghost/members-api module and make
    it accessible to both.

--- core/server/services/auth/members/index.js
+++ core/server/services/auth/members/index.js
@@ -5,42 +5,43 @@ const config = require('../../../../shared/config');
 
 let UNO_MEMBERINO;
 
+async function createMiddleware() {
+    const url = require('url');
+    const {protocol, host} = url.parse(config.get('url'));
+    const siteOrigin = `${protocol}//${host}`;
+
+    const membersConfig = await membersService.api.getPublicConfig();
+    return jwt({
+        credentialsRequired: false,
+        requestProperty: 'member',
+        audience: siteOrigin,
+        issuer: membersConfig.issuer,
+        algorithms: ['RS512'],
+        secret: membersConfig.publicKey,
+        getToken(req) {
+            if (!req.get('authorization')) {
+                return null;
+            }
+
+            const [scheme, credentials] = req.get('authorization').split(/\s+/);
+
+            if (scheme !== 'GhostMembers') {
+                return null;
+            }
+
+            return credentials;
+        }
+    });
+}
+
 module.exports = {
     get authenticateMembersToken() {
-        if (!UNO_MEMBERINO) {
-            const url = require('url');
-            const {protocol, host} = url.parse(config.get('url'));
-            const siteOrigin = `${protocol}//${host}`;
-
-            UNO_MEMBERINO = membersService.api.getPublicConfig().then(({issuer}) => jwt({
-                credentialsRequired: false,
-                requestProperty: 'member',
-                audience: siteOrigin,
-                issuer,
-                algorithms: ['RS512'],
-                secret(req, payload, done) {
-                    membersService.api.getPublicConfig().then(({publicKey}) => {
-                        done(null, publicKey);
-                    }).catch(done);
-                },
-                getToken(req) {
-                    if (!req.get('authorization')) {
-                        return null;
-                    }
-
-                    const [scheme, credentials] = req.get('authorization').split(/\s+/);
-
-                    if (scheme !== 'GhostMembers') {
-                        return null;
-                    }
-
-                    return credentials;
-                }
-            }));
-        }
         return async function (req, res, next) {
+            if (!UNO_MEMBERINO) {
+                UNO_MEMBERINO = await createMiddleware();
+            }
             try {
-                const middleware = await UNO_MEMBERINO;
+                const middleware = UNO_MEMBERINO;
 
                 middleware(req, res, function (err, ...rest) {
                     if (err && err.name === 'UnauthorizedError') {
