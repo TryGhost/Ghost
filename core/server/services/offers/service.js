54141ff1fa3bea0123170cb97b40bbc8e41ada03
Fabien O'Carroll <fabien@allou.is>
2021-10-05 13:17:18 +0200

    Added Offers service
    refs https://github.com/TryGhost/Team/issues/1083
    
    This adds the Offers service which will allow us to build out an Admin
    API to communicate with it. As well as wiring up the redirects to the
    site app.

---
+++ core/server/services/offers/service.js
@@ -0,0 +1,53 @@
+const labs = require('../../../shared/labs');
+const events = require('../../lib/common/events');
+
+const DynamicRedirectManager = require('@tryghost/express-dynamic-redirects');
+const OffersModule = require('@tryghost/members-offers');
+
+const stripeService = require('../stripe');
+
+const config = require('../../../shared/config');
+const urlUtils = require('../../../shared/url-utils');
+const models = require('../../models');
+
+const redirectManager = new DynamicRedirectManager({
+    permanentMaxAge: config.get('caching:customRedirects:maxAge')
+}, urlUtils);
+
+module.exports = {
+    async init() {
+        const offersModule = OffersModule.create({
+            OfferModel: models.Offer,
+            redirectManager: redirectManager,
+            stripeAPIService: stripeService.api
+        });
+
+        this.api = offersModule.api;
+
+        if (labs.isSet('offers')) {
+            // handles setting up redirects
+            await offersModule.init();
+        }
+
+        // TODO: Delete after GA
+        let offersEnabled = labs.isSet('offers');
+        events.on('settings.labs.edited', async () => {
+            if (labs.isSet('offers') !== offersEnabled) {
+                offersEnabled = labs.isSet('offers');
+
+                if (offersEnabled) {
+                    const offers = await this.api.listOffers();
+                    for (const offer of offers) {
+                        redirectManager.addRedirect(`/${offer.code}`, `/#/portal/offers/${offer.id}`, {permanent: false});
+                    }
+                } else {
+                    redirectManager.removeAllRedirects();
+                }
+            }
+        });
+    },
+
+    api: null,
+
+    middleware: redirectManager.handleRequest
+};
