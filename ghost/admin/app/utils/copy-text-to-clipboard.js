6155645a4549c1e85680dcf77eea549dcd9412d8
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-10-05 15:32:23 +0100

    🐛 Fixed copy-to-clipboard buttons in Chrome + Firefox
    no issue
    
    - the method we used for copying text to the clipboard for older browsers has broken on some newer browsers
    - the more modern `navigator.clipboard.writeText` API is now universally supported by our target browsers so we're able to switch to that instead which is working across the board

--- ghost/admin/app/utils/copy-text-to-clipboard.js
+++ ghost/admin/app/utils/copy-text-to-clipboard.js
@@ -1,11 +1,3 @@
 export default function copyTextToClipboard(text) {
-    let textarea = document.createElement('textarea');
-    textarea.value = text;
-    textarea.setAttribute('readonly', '');
-    textarea.style.position = 'absolute';
-    textarea.style.left = '-9999px';
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textarea);
+    navigator.clipboard.writeText(text);
 }
