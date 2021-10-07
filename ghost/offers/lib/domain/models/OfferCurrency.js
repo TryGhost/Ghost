f879cae09ae97d5bb73907170ef8ddfa921ecc8e
Fabien O'Carroll <fabien@allou.is>
2021-10-07 17:33:26 +0200

    Added support for creating fixed offers with currency
    refs https://github.com/TryGhost/Team/issues/1083
    
    We now allow creating offers for a fixed amount, rather than a
    percentage. These require a currency to be passed as a fixed amount is
    meaningless without one.

---
+++ ghost/offers/lib/domain/models/OfferCurrency.js
@@ -0,0 +1,18 @@
+const ValueObject = require('../../shared/ValueObject');
+const InvalidOfferCurrency = require('../../errors').InvalidOfferCurrency;
+
+/** @extends ValueObject<string> */
+class OfferCurrency extends ValueObject {
+    /** @param {unknown} currency */
+    static create(currency) {
+        if (typeof currency !== 'string') {
+            throw new InvalidOfferCurrency({
+                message: 'Offer `currency` must be a string.'
+            });
+        }
+        // TODO: Validate it is a country code we support?
+        return new OfferCurrency(currency);
+    }
+}
+
+module.exports = OfferCurrency;
