59a710bbf991318f5f757496fcdf3ee5758471f5
Indrakant <ik.dana24@gmail.com>
2021-10-08 14:57:56 +0530

    Replace i18n.t w/ tpl in exported.js

--- core/server/data/exporter/exporter.js
+++ core/server/data/exporter/exporter.js
@@ -1,19 +1,24 @@
-const _ = require('lodash');
-const Promise = require('bluebird');
-const db = require('../../data/db');
-const commands = require('../schema').commands;
-const ghostVersion = require('@tryghost/version');
-const i18n = require('../../../shared/i18n');
-const errors = require('@tryghost/errors');
+const _ = require("lodash");
+const Promise = require("bluebird");
+const db = require("../../data/db");
+const commands = require("../schema").commands;
+const ghostVersion = require("@tryghost/version");
+const tpl = require("@tryghost/tpl");
+const errors = require("@tryghost/errors");
 
-const {
-    TABLES_ALLOWLIST,
-    SETTING_KEYS_BLOCKLIST
-} = require('./table-lists');
+const messages = {
+    errorExportingData: "Error exporting data",
+};
+
+const { TABLES_ALLOWLIST, SETTING_KEYS_BLOCKLIST } = require("./table-lists");
 
 const exportTable = function exportTable(tableName, options) {
-    if (TABLES_ALLOWLIST.includes(tableName) ||
-        (options.include && _.isArray(options.include) && options.include.indexOf(tableName) !== -1)) {
+    if (
+        TABLES_ALLOWLIST.includes(tableName) ||
+        (options.include &&
+            _.isArray(options.include) &&
+            options.include.indexOf(tableName) !== -1)
+    ) {
         const query = (options.transacting || db.knex)(tableName);
 
         return query.select();
@@ -21,13 +26,16 @@ const exportTable = function exportTable(tableName, options) {
 };
 
 const getSettingsTableData = function getSettingsTableData(settingsData) {
-    return settingsData && settingsData.filter((setting) => {
-        return !SETTING_KEYS_BLOCKLIST.includes(setting.key);
-    });
+    return (
+        settingsData &&
+        settingsData.filter((setting) => {
+            return !SETTING_KEYS_BLOCKLIST.includes(setting.key);
+        })
+    );
 };
 
 const doExport = async function doExport(options) {
-    options = options || {include: []};
+    options = options || { include: [] };
 
     try {
         const tables = await commands.getTables(options.transacting);
@@ -39,15 +47,15 @@ const doExport = async function doExport(options) {
         const exportData = {
             meta: {
                 exported_on: new Date().getTime(),
-                version: ghostVersion.full
+                version: ghostVersion.full,
             },
             data: {
                 // Filled below
-            }
+            },
         };
 
         tables.forEach((name, i) => {
-            if (name === 'settings') {
+            if (name === "settings") {
                 exportData.data[name] = getSettingsTableData(tableData[i]);
             } else {
                 exportData.data[name] = tableData[i];
@@ -58,7 +66,7 @@ const doExport = async function doExport(options) {
     } catch (err) {
         throw new errors.DataExportError({
             err: err,
-            context: i18n.t('errors.data.export.errorExportingData')
+            context: tpl(messages.errorExportingData),
         });
     }
 };
