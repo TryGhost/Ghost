dd0ecf75b900dd0375664a42bcd5bf520b1e04da
Rishabh <zrishabhgarg@gmail.com>
2021-10-08 19:45:48 +0530

    Added basic acceptance tests for offers
    refs https://github.com/TryGhost/Team/issues/1084
    refs https://github.com/TryGhost/Team/issues/1085

---
+++ ghost/admin/mirage/factories/offer.js
@@ -0,0 +1,12 @@
+import faker from 'faker';
+import moment from 'moment';
+import {Factory} from 'ember-cli-mirage';
+
+let randomDate = function randomDate(start = moment().subtract(30, 'days').toDate(), end = new Date()) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+};
+
+export default Factory.extend({
+    name() { return `${faker.name.firstName()} ${faker.name.lastName()}`; },
+    createdAt() { return randomDate(); }
+});
