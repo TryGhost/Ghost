1d0cd801589e771af340565059a48e023b39eebd
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-10-11 21:09:42 +0100

    Added basic reset to design preview on nav bar interaction
    refs https://github.com/TryGhost/Team/issues/1130
    
    - when opening/closing sections or interacting with the general design or custom theme settings we want to switch back to showing the preview so the sidebar acts more like typical navigation and so form changes show their effect

--- ghost/admin/app/components/gh-html-iframe.js
+++ ghost/admin/app/components/gh-html-iframe.js
@@ -2,19 +2,12 @@ import Component from '@glimmer/component';
 import {action} from '@ember/object';
 
 export default class GhHtmlIframeComponent extends Component {
-    iframe = null;
-
     @action
-    registerIframe(iframe) {
-        this.iframe = iframe;
-    }
-
-    @action
-    replaceIframeContents(iframe, html) {
-        if (this.iframe) {
-            this.iframe.contentWindow.document.open();
-            this.iframe.contentWindow.document.write(html);
-            this.iframe.contentWindow.document.close();
+    replaceIframeContents(iframe) {
+        if (iframe) {
+            iframe.contentWindow.document.open();
+            iframe.contentWindow.document.write(this.args.html);
+            iframe.contentWindow.document.close();
         }
     }
 }
