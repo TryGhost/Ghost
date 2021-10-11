9aaaeba1004100300d4d3a17b09bc8bd6b0408b0
william chandra <williamchans57@gmail.com>
2021-10-11 20:30:05 +0700

    Replaced i18n.t w/ tpl in core/server/api/v3/utils/validators/input/users.js (#13517)
    refs: #13380
    
    - The i18n package is deprecated. It is being replaced with the tpl package.

--- core/server/api/v3/utils/validators/input/users.js
+++ core/server/api/v3/utils/validators/input/users.js
@@ -1,7 +1,11 @@
 const Promise = require('bluebird');
 const debug = require('@tryghost/debug')('api:v3:utils:validators:input:users');
-const i18n = require('../../../../../../shared/i18n');
+const tpl = require('@tryghost/tpl');
 const errors = require('@tryghost/errors');
+
+const messages = {
+    newPasswordsDoNotMatch: 'Your new passwords do not match'
+};
 
 module.exports = {
     changePassword(apiConfig, frame) {
@@ -11,7 +15,7 @@ module.exports = {
 
         if (data.newPassword !== data.ne2Password) {
             return Promise.reject(new errors.ValidationError({
-                message: i18n.t('errors.models.user.newPasswordsDoNotMatch')
+                message: tpl(messages.newPasswordsDoNotMatch)
             }));
         }
     }
