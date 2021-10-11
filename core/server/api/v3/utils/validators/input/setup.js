a95c42e30eeb06e70078c6d6ea355a80c46baa25
Indrakant Dana <60315832+Indrakant-Dana@users.noreply.github.com>
2021-10-12 01:58:01 +0530

    Replaced i18n.t w/ tpl in core/server/api/v3/utils/validators/input/setup.js (#13534)
    refs: TryGhost#13380
    
    - The i18n package is deprecated. It is being replaced with the tpl package.

--- core/server/api/v3/utils/validators/input/setup.js
+++ core/server/api/v3/utils/validators/input/setup.js
@@ -1,13 +1,17 @@
 const debug = require('@tryghost/debug')('api:v3:utils:validators:input:updateSetup');
-const i18n = require('../../../../../../shared/i18n');
+const tpl = require('@tryghost/tpl');
 const errors = require('@tryghost/errors');
+
+const messages = {
+    notTheBlogOwner: 'You are not the site owner.'
+};
 
 module.exports = {
     updateSetup(apiConfig, frame) {
         debug('resetPassword');
 
         if (!frame.options.context || !frame.options.context.user) {
-            throw new errors.NoPermissionError({message: i18n.t('errors.api.authentication.notTheBlogOwner')});
+            throw new errors.NoPermissionError({message: tpl(messages.notTheBlogOwner)});
         }
     }
 };
