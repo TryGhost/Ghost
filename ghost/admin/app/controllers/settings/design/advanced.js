0a3feca9b3ea39edff6c13b85cebea4ee53ccd17
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-10-11 19:30:58 +0100

    Refactored theme upload process and linked from design screens
    refs https://github.com/TryGhost/Team/issues/1130
    
    - copied theme upload modal to new modals system and refactored for Octane syntax
      -  updated to use `themeManagement` service rather than passed in actions so the modal-based process can be opened from any screen
      - added default `beforeClose()` for the modal to the modals service so it won't close if an upload is in-progress (defaults were moved directly into the class so it had access to services)
    - added `themeManagement.upload` action for triggering the upload modal and providing a central place for limit checks
    - added upload-triggering buttons to change-theme and advanced design screens

--- ghost/admin/app/controllers/settings/design/advanced.js
+++ ghost/admin/app/controllers/settings/design/advanced.js
@@ -3,6 +3,7 @@ import {inject as service} from '@ember/service';
 
 export default class AdvancedThemeSettingsController extends Controller {
     @service store;
+    @service themeManagement;
 
     get themes() {
         return this.store.peekAll('theme');
