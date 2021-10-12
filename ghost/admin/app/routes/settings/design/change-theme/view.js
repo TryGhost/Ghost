e12de2a6684a71ce9489b5e948db7bb972384fa3
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-10-12 14:27:51 +0100

    Added initial view-theme modal for installing from themes list
    refs https://github.com/TryGhost/Team/issues/1130
    
    - added new route for viewing themes so back/forward buttons can be used
      - takes theme name as a parameter
      - opens a fullscreen modal with an iframe containing the theme demo
    - changed installable themes list to link to new route
    - swapped `previewUrl` in theme data to point at the real demo rather than the ghost.org demo page

---
+++ ghost/admin/app/routes/settings/design/change-theme/view.js
@@ -0,0 +1,34 @@
+import Route from '@ember/routing/route';
+import {inject as service} from '@ember/service';
+
+export default class ViewThemeRoute extends Route {
+    @service modals;
+
+    themeModal = null;
+
+    model(params, transition) {
+        const changeThemeController = this.controllerFor('settings.design.change-theme');
+        const knownThemes = changeThemeController.marketplaceThemes;
+
+        const foundTheme = knownThemes.find(theme => theme.name === params.theme_name);
+
+        if (foundTheme) {
+            return foundTheme;
+        }
+
+        const path = transition.intent.url.replace(/^\//, '');
+        return this.replaceWith('error404', {path, status: 404});
+    }
+
+    setupController(controller, model) {
+        this.themeModal?.close();
+
+        this.themeModal = this.modals.open('modals/design/view-theme', {
+            theme: model
+        });
+    }
+
+    deactivate() {
+        this.themeModal?.close();
+    }
+}
