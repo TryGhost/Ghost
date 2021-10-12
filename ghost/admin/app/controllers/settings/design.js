6c3c051dff4f54ebd95131fa750d962886eaeb6a
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-10-12 15:19:27 +0100

    Fixed save button on design settings screen
    refs https://github.com/TryGhost/Team/issues/1130
    
    - when moving the design settings screen from a modal to normal routes the save process was misplaced and the template not updated
    - moved all the design settings behaviour over to the index controller where it belongs and updated the template to use `this.saveTask` rather than `@data.saveTask` as was used in the modal

--- ghost/admin/app/controllers/settings/design.js
+++ ghost/admin/app/controllers/settings/design.js
@@ -1,32 +1,4 @@
 import Controller from '@ember/controller';
-import {inject as service} from '@ember/service';
-import {task} from 'ember-concurrency-decorators';
 
 export default class SettingsDesignController extends Controller {
-    @service customThemeSettings;
-    @service notifications;
-    @service router;
-    @service settings;
-
-    @task
-    *saveTask() {
-        try {
-            if (this.settings.get('errors').length !== 0) {
-                return;
-            }
-
-            yield Promise.all([
-                this.settings.save(),
-                this.customThemeSettings.save()
-            ]);
-
-            // ensure task button switches to success state
-            return true;
-        } catch (error) {
-            if (error) {
-                this.notifications.showAPIError(error);
-                throw error;
-            }
-        }
-    }
 }
--- ghost/admin/app/controllers/settings/design/index.js
+++ ghost/admin/app/controllers/settings/design/index.js
@@ -1,8 +1,33 @@
 import Controller from '@ember/controller';
 import {inject as service} from '@ember/service';
+import {task} from 'ember-concurrency-decorators';
 
-export default class DesignIndexController extends Controller {
+export default class SettingsDesignIndexController extends Controller {
     @service config;
+    @service customThemeSettings;
+    @service notifications;
     @service settings;
-    @service themeManagement
+    @service themeManagement;
+
+    @task
+    *saveTask() {
+        try {
+            if (this.settings.get('errors').length !== 0) {
+                return;
+            }
+
+            yield Promise.all([
+                this.settings.save(),
+                this.customThemeSettings.save()
+            ]);
+
+            // ensure task button switches to success state
+            return true;
+        } catch (error) {
+            if (error) {
+                this.notifications.showAPIError(error);
+                throw error;
+            }
+        }
+    }
 }
