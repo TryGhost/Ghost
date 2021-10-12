b48c16c11871cf30cdf5eb0efdc58dcdb05133e3
Fabien 'egg' O'Carroll <fabien@allou.is>
2021-10-12 15:20:45 +0200

    Added active column to offers table (#13592)
    refs https://github.com/TryGhost/Team/issues/1131
    
    This will be used as a flag to determine whether or not an Offer is
    archived or active.

--- test/unit/server/data/schema/integrity.test.js
+++ test/unit/server/data/schema/integrity.test.js
@@ -32,7 +32,7 @@ const defaultSettings = require('../../../../../core/server/data/schema/default-
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = 'c1c45b460f39504a01b26fcd7a62f395';
+    const currentSchemaHash = '2b5213b4ead41042194d4a9051bf0e8a';
     const currentFixturesHash = 'c064a1b57c594e6a8d36f9e884df0a2a';
     const currentSettingsHash = 'aa3fcbc8ab119b630aeda7366ead5493';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
