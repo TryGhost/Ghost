aad15f292ec7f429923f3d32c1c7792f2e66aebe
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-10-13 13:22:49 +0100

    Wired up desktop/mobile toggle on design settings screen
    no issue
    
    - ensured that we're only swapping classes on existing elements or inserting/removing elements around the iframe to avoid re-rendering and the associated loading+layout flash

--- ghost/admin/app/controllers/settings/design/index.js
+++ ghost/admin/app/controllers/settings/design/index.js
@@ -1,6 +1,8 @@
 import Controller from '@ember/controller';
+import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
 import {task} from 'ember-concurrency-decorators';
+import {tracked} from '@glimmer/tracking';
 
 export default class SettingsDesignIndexController extends Controller {
     @service config;
@@ -8,6 +10,21 @@ export default class SettingsDesignIndexController extends Controller {
     @service notifications;
     @service settings;
     @service themeManagement;
+
+    @tracked previewSize = 'desktop';
+
+    get isDesktopPreview() {
+        return this.previewSize === 'desktop';
+    }
+
+    get isMobilePreview() {
+        return this.previewSize === 'mobile';
+    }
+
+    @action
+    setPreviewSize(size) {
+        this.previewSize = size;
+    }
 
     @task
     *saveTask() {
