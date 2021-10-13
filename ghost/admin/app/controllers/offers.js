8c0642ea6f28d62c8160c483c0d81b30cc991862
Rishabh <zrishabhgarg@gmail.com>
2021-10-13 21:23:21 +0530

    Updated offers list to filter on archived offers
    refs https://github.com/TryGhost/Team/issues/1136
    
    - adds dropdown for active/archived offers selection
    - wires active/archived offers on list screen to API
    - filters offers list on archived/active based on selection

--- ghost/admin/app/controllers/offers.js
+++ ghost/admin/app/controllers/offers.js
@@ -4,16 +4,49 @@ import {inject as service} from '@ember/service';
 import {task} from 'ember-concurrency-decorators';
 import {tracked} from '@glimmer/tracking';
 
+const TYPES = [{
+    name: 'Active',
+    value: 'active'
+},{
+    name: 'Archived',
+    value: 'archived'
+}];
+
 export default class MembersController extends Controller {
-    @tracked offers = [];
     @service modals;
+    @service router;
+
+    @tracked offers = [];
+    @tracked type = 'active';
+
+    queryParams = [
+        'type'
+    ];
 
     constructor() {
         super(...arguments);
+        this.availableTypes = TYPES;
+    }
+
+    get filteredOffers() {
+        return this.offers.filter((offer) => {
+            return offer.status === this.type;
+        });
     }
 
     get offersExist() {
         return this.offers.length > 0;
+    }
+
+    get selectedType() {
+        return this.type ? TYPES.find((d) => {
+            return this.type === d.value;
+        }) : TYPES[0];
+    }
+
+    @action
+    onTypeChange(type) {
+        this.type = type.value;
     }
 
     @action
