61fda55d033287587e5910d5f54f34f91177bdab
Fabien O'Carroll <fabien@allou.is>
2021-10-13 14:34:01 +0200

    Added offer_redemptions table to database
    refs https://github.com/TryGhost/Team/issues/1132
    
    This will be used to store Offer Redemptions, which will be used to list
    the Offers which a Member has redeemed, as well as the number of times
    an Offer has been redeemed.

--- test/unit/server/data/schema/integrity.test.js
+++ test/unit/server/data/schema/integrity.test.js
@@ -32,7 +32,7 @@ const defaultSettings = require('../../../../../core/server/data/schema/default-
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '2b5213b4ead41042194d4a9051bf0e8a';
+    const currentSchemaHash = '33ec13330fc7384b849ae4b0e506017d';
     const currentFixturesHash = 'c064a1b57c594e6a8d36f9e884df0a2a';
     const currentSettingsHash = 'aa3fcbc8ab119b630aeda7366ead5493';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
