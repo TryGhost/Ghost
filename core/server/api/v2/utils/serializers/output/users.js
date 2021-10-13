5acf70df384d13031b7b142f6c995e4f77a43bc1
Levi Richardson <litbelb1@outlook.com>
2021-10-13 00:44:04 -0700

    Switch i18n.t to tpl in users.js (#13574)
    refs: #13380
    
    - i18n is deprecated and is being replaced with tpl

--- core/server/api/v2/utils/serializers/output/users.js
+++ core/server/api/v2/utils/serializers/output/users.js
@@ -1,7 +1,9 @@
 const debug = require('@tryghost/debug')('api:v2:utils:serializers:output:users');
-const i18n = require('../../../../../../shared/i18n');
+const tpl = require('@tryghost/tpl');
 const mapper = require('./utils/mapper');
-
+const messages = {
+    pwdChangedSuccessfully: 'Password changed successfully.'
+};
 module.exports = {
     browse(models, apiConfig, frame) {
         debug('browse');
@@ -29,7 +31,7 @@ module.exports = {
         debug('changePassword');
 
         frame.response = {
-            password: [{message: i18n.t('notices.api.users.pwdChangedSuccessfully')}]
+            password: [{message: tpl(messages.pwdChangedSuccessfully)}]
         };
     },
 
