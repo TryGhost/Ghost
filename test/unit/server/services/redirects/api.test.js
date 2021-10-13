23cc2c66aec06d661cd55516730e935b1974484e
Naz <nazargargol@gmail.com>
2021-10-13 11:07:51 +0200

    Removed redirects regression tests in favor of unit test
    refs https://linear.app/tryghost/issue/CORE-84/have-a-look-at-the-eggs-redirects-refactor-branch
    
    - Removed "download" regression tests as those cases were ported over to much faster unit tests

--- test/unit/server/services/redirects/api.test.js
+++ test/unit/server/services/redirects/api.test.js
@@ -1,4 +1,5 @@
 const should = require('should');
+const sinon = require('sinon');
 const path = require('path');
 const fs = require('fs-extra');
 
@@ -6,10 +7,21 @@ const DynamicRedirectManager = require('@tryghost/express-dynamic-redirects');
 const CustomRedirectsAPI = require('../../../../../core/server/services/redirects/api');
 
 describe('UNIT: redirects CustomRedirectsAPI class', function () {
+    beforeEach(function () {
+        sinon.stub(fs, 'pathExists');
+        sinon.stub(fs, 'readFile');
+    });
+
+    afterEach(function () {
+        sinon.restore();
+    });
+
     describe('get', function () {
-        it('returns nothing if file does not exist', async function () {
-            // Just set any content folder, which does not contain a redirects file.
-            const basePath = path.join(__dirname, '../../../utils/fixtures/does-not-exist/');
+        it('returns empty array if file does not exist', async function () {
+            const basePath = path.join(__dirname, '../../../../utils/fixtures/data/');
+            fs.pathExists.withArgs(`${basePath}redirects.yaml`).resolves(false);
+            fs.pathExists.withArgs(`${basePath}redirects.json`).resolves(false);
+
             const redirectManager = new DynamicRedirectManager({
                 permanentMaxAge: 100,
                 getSubdirectoryURL: (pathname) => {
@@ -24,6 +36,59 @@ describe('UNIT: redirects CustomRedirectsAPI class', function () {
             const file = await customRedirectsAPI.get();
 
             should.deepEqual(file, []);
+        });
+
+        it('returns a redirects YAML file if it exists', async function () {
+            const basePath = path.join(__dirname, '../../../../utils/fixtures/data/');
+            fs.pathExists.withArgs(`${basePath}redirects.yaml`).resolves(true);
+            fs.pathExists.withArgs(`${basePath}redirects.json`).resolves(false);
+
+            fs.readFile.withArgs(`${basePath}redirects.yaml`, 'utf-8').resolves('yaml content');
+            fs.readFile.withArgs(`${basePath}redirects.json`, 'utf-8').resolves(null);
+
+            const redirectManager = new DynamicRedirectManager({
+                permanentMaxAge: 100,
+                getSubdirectoryURL: (pathname) => {
+                    return `/ghost/${pathname}`;
+                }
+            });
+
+            const customRedirectsAPI = new CustomRedirectsAPI({
+                basePath
+            }, redirectManager);
+
+            const file = await customRedirectsAPI.get();
+
+            should.deepEqual(file, 'yaml content');
+        });
+
+        it('returns a redirects JSON file if YAML does not exists', async function () {
+            const basePath = path.join(__dirname, '../../../../utils/fixtures/data/');
+            const redirectJSONFixture = [{
+                from: '^/post/[0-9]+/([a-z0-9\\-]+)',
+                to: '/$1'
+            }];
+
+            fs.pathExists.withArgs(`${basePath}redirects.yaml`).resolves(false);
+            fs.pathExists.withArgs(`${basePath}redirects.json`).resolves(true);
+
+            fs.readFile.withArgs(`${basePath}redirects.yaml`, 'utf-8').resolves(null);
+            fs.readFile.withArgs(`${basePath}redirects.json`, 'utf-8').resolves(JSON.stringify(redirectJSONFixture));
+
+            const redirectManager = new DynamicRedirectManager({
+                permanentMaxAge: 100,
+                getSubdirectoryURL: (pathname) => {
+                    return `/ghost/${pathname}`;
+                }
+            });
+
+            const customRedirectsAPI = new CustomRedirectsAPI({
+                basePath
+            }, redirectManager);
+
+            const file = await customRedirectsAPI.get();
+
+            should.deepEqual(file, redirectJSONFixture);
         });
     });
 });
