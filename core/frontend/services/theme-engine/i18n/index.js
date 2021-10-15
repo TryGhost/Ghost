b0a7abd0181c863ca7fce8297d6d0149a0ff1284
Hannah Wolfe <github.erisds@gmail.com>
2021-10-15 20:25:56 +0100

    Moved core/shared/i18n to theme-engine/i18n
    - core/shared/i18n is no longer used. Remove it to prevent temptation!
    - this class needs merging with the one in themes, but for now just co-locate them as that's quicker and easier

--- core/frontend/services/theme-engine/i18n/index.js
+++ core/frontend/services/theme-engine/i18n/index.js
@@ -1,7 +1,7 @@
 const config = require('../../../../shared/config');
 const logging = require('@tryghost/logging');
 
-const ThemeI18n = require('./i18n');
+const ThemeI18n = require('./theme-i18n');
 
 module.exports = new ThemeI18n({logging, basePath: config.getContentPath('themes')});
 module.exports.ThemeI18n = ThemeI18n;
--- core/shared/i18n/index.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const logging = require('@tryghost/logging');
-const I18n = require('./i18n');
-
-module.exports = new I18n({logging, basePath: path.join(__dirname, 'translations')});
-module.exports.I18n = I18n;
