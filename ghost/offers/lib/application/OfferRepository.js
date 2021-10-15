3cd26bc11b2428422a00a12c6a7872db926237a1
Fabien O'Carroll <fabien@allou.is>
2021-10-15 11:16:55 +0200

    Updated OfferRepository to handle redemptions
    refs https://github.com/TryGhost/Team/issues/1132
    
    The OfferRepository needs to read from the OfferRedemptionModel to get
    this data.

--- ghost/offers/lib/application/OfferRepository.js
+++ ghost/offers/lib/application/OfferRepository.js
@@ -37,45 +37,22 @@ const mongoTransformer = flowRight(statusTransformer, rejectNonStatusTransformer
  */
 
 /**
- * @extends BaseOptions
  * @typedef {object} ListOptions
+ * @prop {import('knex').Transaction} transacting
  * @prop {string} filter
  */
-
-/**
- * @param {any} json
- * @returns {Offer.OfferCreateProps}
- */
-function toDomain(json) {
-    return {
-        id: json.id,
-        name: json.name,
-        code: json.code,
-        display_title: json.portal_title,
-        display_description: json.portal_description,
-        type: json.discount_type === 'amount' ? 'fixed' : 'percent',
-        amount: json.discount_amount,
-        cadence: json.interval,
-        currency: json.currency,
-        duration: json.duration,
-        duration_in_months: json.duration_in_months,
-        stripe_coupon_id: json.stripe_coupon_id,
-        status: json.active ? 'active' : 'archived',
-        tier: {
-            id: json.product.id,
-            name: json.product.name
-        }
-    };
-}
 
 class OfferRepository {
     /**
      * @param {{forge: (data: object) => import('bookshelf').Model<Offer.OfferProps>}} OfferModel
+     * @param {{forge: (data: object) => import('bookshelf').Model<any>}} OfferRedemptionModel
      * @param {import('@tryghost/members-stripe-service')} stripeAPIService
      */
-    constructor(OfferModel, stripeAPIService) {
+    constructor(OfferModel, OfferRedemptionModel, stripeAPIService) {
         /** @private */
         this.OfferModel = OfferModel;
+        /** @private */
+        this.OfferRedemptionModel = OfferRedemptionModel;
         /** @private */
         this.stripeAPIService = stripeAPIService;
     }
@@ -116,6 +93,40 @@ class OfferRepository {
     }
 
     /**
+     * @private
+     * @param {import('bookshelf').Model<any>} model
+     * @param {BaseOptions} options
+     * @returns {Promise<Offer>}
+     */
+    async mapToOffer(model, options) {
+        const json = model.toJSON();
+
+        const count = await this.OfferRedemptionModel.forge({offer_id: json.id}).count('id', {
+            transacting: options.transacting
+        });
+        return Offer.create({
+            id: json.id,
+            name: json.name,
+            code: json.code,
+            display_title: json.portal_title,
+            display_description: json.portal_description,
+            type: json.discount_type === 'amount' ? 'fixed' : 'percent',
+            amount: json.discount_amount,
+            cadence: json.interval,
+            currency: json.currency,
+            duration: json.duration,
+            duration_in_months: json.duration_in_months,
+            redemptionCount: count,
+            stripe_coupon_id: json.stripe_coupon_id,
+            status: json.active ? 'active' : 'archived',
+            tier: {
+                id: json.product.id,
+                name: json.product.name
+            }
+        }, null);
+    }
+
+    /**
      * @param {string} id
      * @param {BaseOptions} [options]
      * @returns {Promise<Offer>}
@@ -126,9 +137,7 @@ class OfferRepository {
             withRelated: ['product']
         });
 
-        const json = model.toJSON();
-
-        return Offer.create(toDomain(json));
+        return this.mapToOffer(model, options);
     }
 
     /**
@@ -141,7 +150,14 @@ class OfferRepository {
             mongoTransformer,
             withRelated: ['product']
         });
-        return Promise.all(models.toJSON().map(toDomain).map(Offer.create));
+
+        const mapOptions = {
+            transacting: options && options.transacting
+        };
+
+        const offers = models.map(model => this.mapToOffer(model, mapOptions));
+
+        return Promise.all(offers);
     }
 
     /**
