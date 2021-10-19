7572c2db1e40e9760269eac800b815e0d179dc03
Rishabh <zrishabhgarg@gmail.com>
2021-10-19 13:05:18 +0530

    Added error handling and validation for offer
    closes https://github.com/TryGhost/Team/issues/1159
    
    - validates offer fields before save client-side

--- ghost/admin/app/validators/offer.js
+++ ghost/admin/app/validators/offer.js
@@ -2,7 +2,7 @@ import BaseValidator from './base';
 import validator from 'validator';
 
 export default BaseValidator.create({
-    properties: ['name'],
+    properties: ['name', 'amount', 'displayTitle', 'displayDescription', 'code', 'durationInMonths'],
 
     name(model) {
         if (!model.name) {
@@ -11,6 +11,41 @@ export default BaseValidator.create({
         }
         if (!validator.isLength(model.name || '', 0, 191)) {
             model.errors.add('name', 'Name cannot be longer than 191 characters.');
+            this.invalidate();
+        }
+    },
+
+    amount(model) {
+        if (!model.amount) {
+            model.errors.add('amount', 'Please enter Amount.');
+            this.invalidate();
+        }
+    },
+
+    displayTitle(model) {
+        if (!model.displayTitle) {
+            model.errors.add('displayTitle', 'Please enter Display title.');
+            this.invalidate();
+        }
+    },
+
+    displayDescription(model) {
+        if (!validator.isLength(model.displayDescription || '', 0, 191)) {
+            model.errors.add('displayDescription', 'Display description cannot be longer than 191 characters.');
+            this.invalidate();
+        }
+    },
+
+    durationInMonths(model) {
+        if (model.duration === 'repeating' && !model.durationInMonths) {
+            model.errors.add('durationInMonths', 'Please enter duration in months.');
+            this.invalidate();
+        }
+    },
+
+    code(model) {
+        if (!model.code) {
+            model.errors.add('code', 'Please enter code.');
             this.invalidate();
         }
     }
