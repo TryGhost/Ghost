2872c7253f24e1950dfbe0bb413e39e50952cdae
Fabien O'Carroll <fabien@allou.is>
2021-10-19 18:17:42 +0200

    Removed NOT_NULL constraint from stripe_coupon_id
    no-issue
    
    When handling disconnecting from Stripe - we remove all Stripe data from
    our database to ensure we do not have bad/invalid data stored. Removing
    this constraint will allow us to set the value to NULL.
    
    Because this is in alpha it is simpler to just drop the tables and
    re-add them, due to offer_redemptions depending on offers, we also drop
    this table and re-add it.

--- test/unit/server/data/schema/integrity.test.js
+++ test/unit/server/data/schema/integrity.test.js
@@ -32,7 +32,7 @@ const defaultSettings = require('../../../../../core/server/data/schema/default-
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '33ec13330fc7384b849ae4b0e506017d';
+    const currentSchemaHash = '3ab8e82bd81ec7ec8f69a65e34d6f9de';
     const currentFixturesHash = 'c064a1b57c594e6a8d36f9e884df0a2a';
     const currentSettingsHash = 'aa3fcbc8ab119b630aeda7366ead5493';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
