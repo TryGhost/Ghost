08fdb463756d99219c931a90efa485e1b1597ebd
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-10-20 13:04:05 +0100

    Added animation of sidebar between main and contextual menus
    refs https://github.com/TryGhost/Team/issues/1149
    
    - added `ember-css-transitions` library that provides a modifier that adds/removes classes used for animating elements in and out, automatically creating a clone for destroyed elements
    - added a wrapper class around the `main` and `design` nav menu contents so they could be individually animated
    - used the new `{{css-transition}}` modifier to animate the sidebar menus in/out
    - ensured main menu doesn't animate on first render of the parent `<GhNavMenu>` component so there's no animation when Admin is loaded

--- ghost/admin/app/components/gh-nav-menu.js
+++ ghost/admin/app/components/gh-nav-menu.js
@@ -1,7 +1,10 @@
 import Component from '@glimmer/component';
 import {inject as service} from '@ember/service';
+import {tracked} from '@glimmer/tracking';
 
 export default class GhNavMenuComponent extends Component {
     @service settings;
     @service ui;
+
+    @tracked firstRender = true;
 }
