ac238c8554f600f105b7b24f5c3a426eb07ea5ff
Fabien 'egg' O'Carroll <fabien@allou.is>
2021-10-20 14:22:37 +0200

    Updated Offers related tables (#13609)
    no-issue
    
    * Removed NOT_NULL constraint from stripe_coupon_id
    
    When handling disconnecting from Stripe - we remove all Stripe data from
    our database to ensure we do not have bad/invalid data stored. Removing
    this constraint will allow us to set the value to NULL.
    
    * Added created_at column to offer_redemptions
    
    Offer Redemptions are not just a joining table, but an event. A created_at
    date allows them to be ordered
    
    Because this is in alpha it is simpler to just drop the tables and
    re-add them, due to offer_redemptions depending on offers, we also drop
    this table and re-add it.

--- test/unit/server/data/schema/integrity.test.js
+++ test/unit/server/data/schema/integrity.test.js
@@ -32,7 +32,7 @@ const defaultSettings = require('../../../../../core/server/data/schema/default-
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '33ec13330fc7384b849ae4b0e506017d';
+    const currentSchemaHash = '06c1007b471faba9bb82d053f6ba6cc1';
     const currentFixturesHash = 'c064a1b57c594e6a8d36f9e884df0a2a';
     const currentSettingsHash = 'aa3fcbc8ab119b630aeda7366ead5493';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
