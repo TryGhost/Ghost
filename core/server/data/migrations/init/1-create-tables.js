9c7b987f49a34587abba76faf8091f4ef84c6017
Hannah Wolfe <github.erisds@gmail.com>
2021-10-21 10:03:22 +0100

    Reworked init migrations to use async/await
    - use async/await and fat arrow syntax
    - makes it easier to refactor later as "this" context is then reliable

--- core/server/data/migrations/init/1-create-tables.js
+++ core/server/data/migrations/init/1-create-tables.js
@@ -4,27 +4,26 @@ const schema = require('../../schema').tables;
 const logging = require('@tryghost/logging');
 const schemaTables = Object.keys(schema);
 
-module.exports.up = function createTables(options) {
+module.exports.up = async (options) => {
     const connection = options.connection;
 
-    return Promise.mapSeries(schemaTables, function createTable(table) {
+    await Promise.mapSeries(schemaTables, async (table) => {
         logging.info('Creating table: ' + table);
-        return commands.createTable(table, connection);
+        await commands.createTable(table, connection);
     });
 };
 
 /**
- *
  @TODO: This works, but is very dangerous in the current state of the knex-migrator v3.
- @TODO: Enable if knex-migrator v3 is stable.
- module.exports.down = function dropTables(options) {
+ @TODO: Decide if we should enable or delete this
+ module.exports.down = async (options) => {
         var connection = options.connection;
 
         // Reference between tables!
         schemaTables.reverse();
-        return Promise.mapSeries(schemaTables, function dropTable(table) {
+        await Promise.mapSeries(schemaTables, async (table) => {
             logging.info('Drop table: ' + table);
-            return commands.deleteTable(table, connection);
+            await commands.deleteTable(table, connection);
         });
     };
  */
