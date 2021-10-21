88899ec4627893ea9f8c436d76c1a68634d4ec0d
Hannah Wolfe <github.erisds@gmail.com>
2021-10-21 16:48:01 +0100

    Fixed dependency on the order of models
    - this has been a niggle for ages, we shouldn't need to care what order our models are loaded in
    - this is stopping us from having a built-in model loader and "frameworkizing" models

--- core/server/models/index.js
+++ core/server/models/index.js
@@ -13,6 +13,7 @@ require('./base/listeners');
 exports = module.exports;
 
 const models = [
+    'action',
     'permission',
     'post',
     'role',
@@ -51,10 +52,7 @@ const models = [
     'email-recipient',
     'label',
     'single-use-token',
-    'snippet',
-    // Action model MUST be loaded last as it loops through all of the registered models
-    // Please do not append items to this array.
-    'action'
+    'snippet'
 ];
 
 function init() {
