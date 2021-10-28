709eeea2785ad9f8fe42b87fc39fdb7391a4fbd2
Rishabh <zrishabhgarg@gmail.com>
2021-10-28 18:59:21 +0530

    Moved `launchComplete` user setting as global editor setting
    refs https://github.com/TryGhost/Team/issues/807
    
    We stored the launch wizard completed flag in `feature.launchComplete` which is per user, ie. an administrator would have access to the launch wizard even if the owner had completed it in their own session. This was the wrong pattern as we only want to show launch wizard once per site till completed, and both owner and admin don't need to go through it individually

--- core/server/data/migrations/utils.js
+++ core/server/data/migrations/utils.js
@@ -409,12 +409,63 @@ function createDropColumnMigration(table, column, columnDefinition) {
     );
 }
 
+/**
+ * Creates a migration which will insert a new setting in settings table
+ * @param {object} settingSpec - setting key, value, group and type
+ *
+ * @returns {Object} migration object returning config/up/down properties
+ */
+function addSetting({key, value, type, group}) {
+    return createNonTransactionalMigration(
+        async function up(connection) {
+            const settingExists = await connection('settings')
+                .where('key', '=', key)
+                .first();
+            if (settingExists) {
+                logging.warn(`Skipping adding setting: ${key} - setting already exists`);
+                return;
+            }
+
+            logging.info(`Adding setting: ${key}`);
+            const now = connection.raw('CURRENT_TIMESTAMP');
+
+            return connection('settings')
+                .insert({
+                    id: ObjectId().toHexString(),
+                    key,
+                    value,
+                    group,
+                    type,
+                    created_at: now,
+                    created_by: MIGRATION_USER,
+                    updated_at: now,
+                    updated_by: MIGRATION_USER
+                });
+        },
+        async function down(connection) {
+            const settingExists = await connection('settings')
+                .where('key', '=', key)
+                .first();
+            if (!settingExists) {
+                logging.warn(`Skipping dropping setting: ${key} - setting does not exist`);
+                return;
+            }
+
+            logging.info(`Dropping setting: ${key}`);
+            return connection('settings')
+                .where('key', '=', key)
+                .del();
+        }
+    );
+}
+
 module.exports = {
     addTable,
     dropTables,
     addPermission,
     addPermissionToRole,
     addPermissionWithRoles,
+    addSetting,
     createTransactionalMigration,
     createNonTransactionalMigration,
     createIrreversibleMigration,
