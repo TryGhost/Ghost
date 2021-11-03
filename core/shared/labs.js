cecf8d64c50339a28ce46baf012044d8b7688d9d
Fabien 'egg' O'Carroll <fabien@allou.is>
2021-11-03 17:11:48 +0200

    Removed references to Offers labs flag (#13709)
    refs https://github.com/TryGhost/Team/issues/1115
    
    This feature is now GA, and the flag has been hardcoded to `true`, here we clean up the
    remaining references as they're no longer needed.

--- core/shared/labs.js
+++ core/shared/labs.js
@@ -15,8 +15,7 @@ const messages = {
 
 // flags in this list always return `true`, allows quick global enable prior to full flag removal
 const GA_FEATURES = [
-    'customThemeSettings',
-    'offers'
+    'customThemeSettings'
 ];
 
 // NOTE: this allowlist is meant to be used to filter out any unexpected
