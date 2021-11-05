f079e548cf2a28766b1c9ae6f19d14b98acba8a3
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-11-05 09:12:14 +0000

    Added RSS capitalisation in custom theme setting labels
    no issue
    
    - ensures capitalisation of custom theme setting labels matches expectations

--- ghost/admin/app/helpers/humanize-setting-key.js
+++ ghost/admin/app/helpers/humanize-setting-key.js
@@ -4,7 +4,8 @@ import {humanize} from 'ember-cli-string-helpers/helpers/humanize';
 export function humanizeSettingKey([key]) {
     let humanized = humanize([key]);
 
-    const allCaps = ['API', 'CTA'];
+    const allCaps = ['API', 'CTA', 'RSS'];
+
     allCaps.forEach((str) => {
         const regex = new RegExp(`(^| )(${str})( |$)`, 'gi');
         humanized = humanized.replace(regex, `$1${str}$3`);
