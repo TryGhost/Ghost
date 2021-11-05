95d4111d58db1e2c5e6f3f989cd2c6e9d7a742af
Hannah Wolfe <github.erisds@gmail.com>
2021-11-05 13:57:20 +0000

    Removed completed TODO for helper init
    refs: https://github.com/TryGhost/Ghost/commit/9d7049cd3f07d98b727134d36e3302a7edc9a376
    
    - The helper service refactor I did  a little while back moved this to a better location:
    https://github.com/TryGhost/Ghost/commit/9d7049cd3f07d98b727134d36e3302a7edc9a376#diff-81327b09890d6df7b87046cfdfa924f5f03c51221bf29980a015a30bee6d1884R134
    - But I forgot to clean up. Removing the commented block and now completed TODO

--- core/frontend/web/site.js
+++ core/frontend/web/site.js
@@ -118,14 +118,6 @@ module.exports = function setupSiteApp(options = {}) {
     // Serve blog media using the storage adapter
     siteApp.use(STATIC_MEDIA_URL_PREFIX, labs.enabledMiddleware('mediaAPI'), storage.getStorage('media').serve());
 
-    // @TODO find this a better home
-    // We do this here, at the top level, because helpers require so much stuff.
-    // Moving this to being inside themes, where it probably should be requires the proxy to be refactored
-    // Else we end up with circular dependencies
-    // themeEngine.loadCoreHelpers();
-    // themeEngine.registerHandlebarsHelpers();
-    // debug('Helpers done');
-
     // Global handling for member session, ensures a member is logged in to the frontend
     siteApp.use(membersService.middleware.loadMemberSession);
 
