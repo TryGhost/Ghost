a789528a561a9004fe3e8f0edf84036f9e5bae8b
Peter Zimon <zimo@ghost.org>
2021-11-09 13:27:07 +0100

    Added NFT card alpha flag

--- core/shared/labs.js
+++ core/shared/labs.js
@@ -33,7 +33,8 @@ const ALPHA_FEATURES = [
     'filesAPI',
     'membersAutoLogin',
     'buttonCard',
-    'calloutCard'
+    'calloutCard',
+    'nftCard'
 ];
 
 module.exports.GA_KEYS = [...GA_FEATURES];
