e29e48cb38acdd789f200b7a2e244831393b0b33
Rishabh <zrishabhgarg@gmail.com>
2021-11-09 20:36:01 +0530

    Added first version for accordion card
    refs https://github.com/TryGhost/Team/issues/1209
    
    Added first version for toggle cards with heading and content input and basic rendering in Editor

---
+++ ghost/admin/lib/koenig-editor/addon/components/koenig-card-accordion.js
@@ -0,0 +1,87 @@
+import Component from '@glimmer/component';
+import {action} from '@ember/object';
+import {isBlank} from '@ember/utils';
+import {run} from '@ember/runloop';
+import {inject as service} from '@ember/service';
+import {set} from '@ember/object';
+
+export default class KoenigCardAccordionComponent extends Component {
+    @service config;
+    @service feature;
+    @service store;
+    @service membersUtils;
+    @service ui;
+
+    get toolbar() {
+        if (this.args.isEditing) {
+            return false;
+        }
+
+        return {
+            items: [{
+                buttonClass: 'fw4 flex items-center white',
+                icon: 'koenig/kg-edit',
+                iconClass: 'fill-white',
+                title: 'Edit',
+                text: '',
+                action: run.bind(this, this.args.editCard)
+            }]
+        };
+    }
+
+    constructor() {
+        super(...arguments);
+        this.args.registerComponent(this);
+
+        const payloadDefaults = {};
+
+        Object.entries(payloadDefaults).forEach(([key, value]) => {
+            if (this.args.payload[key] === undefined) {
+                this._updatePayloadAttr(key, value);
+            }
+        });
+    }
+
+    // required for snippet rects to be calculated - editor reaches in to component,
+    // expecting a non-Glimmer component with a .element property
+    @action
+    registerElement(element) {
+        this.element = element;
+    }
+
+    @action
+    setContent(event) {
+        this._updatePayloadAttr('content', event.target.value);
+    }
+
+    @action
+    setHeading(event) {
+        this._updatePayloadAttr('heading', event.target.value);
+    }
+
+    @action
+    leaveEditMode() {
+        const {html, content, heading} = this.args.payload;
+
+        if (isBlank(html) && isBlank(heading) && isBlank(content)) {
+            // afterRender is required to avoid double modification of `isSelected`
+            // TODO: see if there's a way to avoid afterRender
+            run.scheduleOnce('afterRender', this, this.args.deleteCard);
+        }
+    }
+
+    @action
+    focusElement(selector, event) {
+        event.preventDefault();
+        document.querySelector(selector)?.focus();
+    }
+
+    _updatePayloadAttr(attr, value) {
+        let payload = this.args.payload;
+
+        set(payload, attr, value);
+
+        // update the mobiledoc and stay in edit mode
+        this.args.saveCard?.(payload, false);
+    }
+}
