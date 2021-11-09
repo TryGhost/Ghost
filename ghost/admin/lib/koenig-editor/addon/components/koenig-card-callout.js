71bfbcdb406c2dff93ef539c4d3e9eddb01e0cd0
Thibaut Patel <thibaut@ghost.org>
2021-11-09 10:39:47 +0100

    Added first pass of callout card
    refs https://github.com/TryGhost/Team/issues/1205
    
    - adds card that contains a static emoji and configurable text

---
+++ ghost/admin/lib/koenig-editor/addon/components/koenig-card-callout.js
@@ -0,0 +1,132 @@
+import Browser from 'mobiledoc-kit/utils/browser';
+import Component from '@glimmer/component';
+import {action} from '@ember/object';
+import {formatTextReplacementHtml} from './koenig-text-replacement-html-input';
+import {isBlank} from '@ember/utils';
+import {run} from '@ember/runloop';
+import {inject as service} from '@ember/service';
+import {set} from '@ember/object';
+
+export default class KoenigCardCalloutComponent extends Component {
+    @service config;
+    @service feature;
+    @service store;
+    @service membersUtils;
+    @service ui;
+
+    get formattedHtml() {
+        return formatTextReplacementHtml(this.payload.calloutText);
+    }
+
+    get toolbar() {
+        if (this.args.isEditing) {
+            return false;
+        }
+
+        return {
+            items: [{
+                buttonClass: 'fw4 flex items-center white',
+                icon: 'koenig/kg-edit',
+                iconClass: 'fill-white',
+                title: 'Edit',
+                text: '',
+                action: run.bind(this, this.args.editCard)
+            }]
+        };
+    }
+
+    constructor() {
+        super(...arguments);
+        this.args.registerComponent(this);
+
+        const payloadDefaults = {
+            calloutEmoji: '⚠️'
+        };
+
+        Object.entries(payloadDefaults).forEach(([key, value]) => {
+            if (this.args.payload[key] === undefined) {
+                this._updatePayloadAttr(key, value);
+            }
+        });
+    }
+
+    // required for snippet rects to be calculated - editor reaches in to component,
+    // expecting a non-Glimmer component with a .element property
+    @action
+    registerElement(element) {
+        this.element = element;
+    }
+
+    @action
+    setCalloutText(text) {
+        this._updatePayloadAttr('calloutText', text);
+    }
+
+    @action
+    setCalloutEmoji(emoji) {
+        this._updatePayloadAttr('calloutEmoji', emoji);
+    }
+
+    @action
+    leaveEditMode() {
+        const {calloutText} = this.args.payload;
+
+        if (isBlank(calloutText)) {
+            // afterRender is required to avoid double modification of `isSelected`
+            // TODO: see if there's a way to avoid afterRender
+            run.scheduleOnce('afterRender', this, this.args.deleteCard);
+        }
+    }
+
+    @action
+    focusElement(selector, event) {
+        event.preventDefault();
+        document.querySelector(selector)?.focus();
+    }
+
+    @action
+    registerEditor(textReplacementEditor) {
+        let commands = {
+            'META+ENTER': run.bind(this, this._enter, 'meta'),
+            'CTRL+ENTER': run.bind(this, this._enter, 'ctrl')
+        };
+
+        Object.keys(commands).forEach((str) => {
+            textReplacementEditor.registerKeyCommand({
+                str,
+                run() {
+                    return commands[str](textReplacementEditor, str);
+                }
+            });
+        });
+
+        this._textReplacementEditor = textReplacementEditor;
+
+        run.scheduleOnce('afterRender', this, this._placeCursorAtEnd);
+    }
+
+    _enter(modifier) {
+        if (this.args.isEditing && (modifier === 'meta' || (modifier === 'crtl' && Browser.isWin()))) {
+            this.args.editCard();
+        }
+    }
+
+    _placeCursorAtEnd() {
+        if (!this._textReplacementEditor) {
+            return;
+        }
+
+        let tailPosition = this._textReplacementEditor.post.tailPosition();
+        let rangeToSelect = tailPosition.toRange();
+        this._textReplacementEditor.selectRange(rangeToSelect);
+    }
+
+    _updatePayloadAttr(attr, value) {
+        let payload = this.args.payload;
+
+        set(payload, attr, value);
+
+        // update the mobiledoc and stay in edit mode
+        this.args.saveCard?.(payload, false);
+    }
+}
