f253e2523d515d67213f12f0bccea875a061d996
Kevin Ansfield <kevin@lookingsideways.co.uk>
2021-11-10 12:45:26 +0000

    Fixed tests expecting old theme settings screen
    refs https://github.com/TryGhost/Team/issues/1164
    
    - removed conditional logic for links on settings index now that the old theme settings routes do not exist
    - updated error handling test to use new design settings screens
      - modified templates' test attributes to work with new design
    - fixed `enableLabsFlag()` test helper error when fixtures have already been loaded

--- ghost/admin/tests/helpers/enable-labs-flag.js
+++ ghost/admin/tests/helpers/enable-labs-flag.js
@@ -1,11 +1,17 @@
 export default function (server, flag) {
-    server.loadFixtures('configs');
+    if (!server.schema.configs.all().length) {
+        server.loadFixtures('configs');
+    }
+
+    if (!server.schema.settings.all().length) {
+        server.loadFixtures('settings');
+    }
+
     const config = server.schema.configs.first();
     config.update({enableDeveloperExperiments: true});
 
     const labsSetting = {};
     labsSetting[flag] = true;
 
-    server.loadFixtures('settings');
     server.db.settings.update({key: 'labs'}, {value: JSON.stringify(labsSetting)});
 }
