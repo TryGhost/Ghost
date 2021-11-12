5b2009a268eb9dfc42b935fcb5ed3323f8f1c619
Naz <nazargargol@gmail.com>
2021-11-12 13:24:51 +0400

    Removed unuseful comment
    no issue
    
    - The file has been moved to the frontend and we have linting rules to remind moving "server" requires

--- core/frontend/web/routes.js
+++ core/frontend/web/routes.js
@@ -1,7 +1,6 @@
 const debug = require('@tryghost/debug')('routing');
 
 const routing = require('../services/routing');
-// NOTE: temporary import from the frontend, will become a backend service soon
 const urlService = require('../../server/services/url');
 const routeSettings = require('../../server/services/route-settings');
 
