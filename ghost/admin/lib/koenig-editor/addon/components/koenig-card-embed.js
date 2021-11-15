e3550974ce8246589f63ce979ca83c59eba9dacf
Fabien O'Carroll <fabien@allou.is>
2021-11-15 10:48:55 +0200

    Fixed embeds without iframe resizing iframe
    refs https://github.com/TryGhost/Admin/commit/c3a7d9c11
    
    The listener for resize is added on the first render of the component,
    so we cannot check for the existence of the iframe then.

--- ghost/admin/lib/koenig-editor/addon/components/koenig-card-embed.js
+++ ghost/admin/lib/koenig-editor/addon/components/koenig-card-embed.js
@@ -315,6 +315,9 @@ export default Component.extend({
     },
 
     __debouncedResizeIframe(iframe) {
+        if (!iframe) {
+            return;
+        }
         iframe.style.height = null;
 
         // get ratio from nested iframe if present (eg, Vimeo)
