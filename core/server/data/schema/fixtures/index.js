869cbe0da29ab38d01afb87bbbfb165a79169be0
Hannah Wolfe <github.erisds@gmail.com>
2021-11-16 13:03:20 +0000

    Changed to use different fixtures during testing
    - this keeps production and test fixtures separate, so that changing the prod fixtures doesn't change the shape of our tests.
    - we may still want to test that the production fixtures do what we expect, but that can be handled in a separate integration test, by specifically setting the fixture path

--- core/server/data/schema/fixtures/index.js
+++ core/server/data/schema/fixtures/index.js
@@ -1,5 +1,8 @@
 const FixtureManager = require('./fixture-manager');
-const fixtures = require('./fixtures');
+const config = require('../../../../shared/config');
+
+const fixturePath = config.get('paths').fixtures;
+const fixtures = require(fixturePath);
 
 module.exports.FixtureManager = FixtureManager;
 module.exports.fixtureManager = new FixtureManager(fixtures);
