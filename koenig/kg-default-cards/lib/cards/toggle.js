d7c31de330c608b1ec9b4730815739f511ecf24a
Rishabh <zrishabhgarg@gmail.com>
2021-11-17 15:17:52 +0530

    Added generic class for card to toggle card
    refs https://github.com/TryGhost/Team/issues/1209

--- koenig/kg-default-cards/lib/cards/toggle.js
+++ koenig/kg-default-cards/lib/cards/toggle.js
@@ -16,7 +16,7 @@ module.exports = {
         }
 
         const template = hbs`
-            <div data-kg-card="toggle" class="kg-toggle-card">
+            <div class="kg-card kg-toggle-card" data-kg-card="toggle" >
                 <div class="kg-toggle-heading">
                     <svg id="Regular" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.cls-1{fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.5px;fill-rule:evenodd;}</style></defs><title>arrow-down-1</title><path class="cls-1" d="M23.25,7.311,12.53,18.03a.749.749,0,0,1-1.06,0L.75,7.311"/></svg>
                     <div class="kg-toggle-heading-text">{{{heading}}}</div>
