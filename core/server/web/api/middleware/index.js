76d04ee994ae41955e0d98283d1aaa3e6fb725d4
Sam Lord <hello@samlord.me>
2021-11-17 10:57:57 +0000

    Revert "Replace update user last seen middleware with package version"
    This reverts commit 006cd6f2bf321e80eacf38fc167abffb5186368c.
    
    Revert "Remove unused unit test file"
    
    This reverts commit c9744a788003c987c8b03f8d5655202e2e6dab01.

--- core/server/web/api/middleware/index.js
+++ core/server/web/api/middleware/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     cors: require('./cors'),
     normalizeImage: require('./normalize-image'),
-    updateUserLastSeen: require('@tryghost/mw-update-user-last-seen'),
+    updateUserLastSeen: require('./update-user-last-seen'),
     upload: require('./upload'),
     versionMatch: require('./version-match')
 };
