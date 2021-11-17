d21419ef17872803af5eaad34311d6e43cdef183
Naz <nazargargol@gmail.com>
2021-11-17 20:05:17 +0400

    Introduced booting from fixture url cache in test environment
    refs https://github.com/TryGhost/Toolbox/issues/135
    
    - To be able to reliably start ghost instance without a frontend the process needs access to urls/resources caches
    - Storing the configuration in "paths" for now as there's no better place for it untill we are able to mock the content folder in pre-boot

--- core/server/services/url/index.js
+++ core/server/services/url/index.js
@@ -5,8 +5,17 @@ const UrlService = require('./UrlService');
 // NOTE: instead of a path we could give UrlService a "data-resolver" of some sort
 //       so it doesn't have to contain the logic to read data at all. This would be
 //       a possible improvement in the future
-const urlsCachePath = path.join(config.getContentPath('data'), 'urls.json');
-const resourcesCachePath = path.join(config.getContentPath('data'), 'resources.json');
+
+let urlsCachePath = path.join(config.getContentPath('data'), 'urls.json');
+let resourcesCachePath = path.join(config.getContentPath('data'), 'resources.json');
+
+// TODO: remove this hack in favor of loading from the content path when it's possible to do so
+//       by mocking content folders in pre-boot phase
+if (process.env.NODE_ENV.match(/^testing/)){
+    urlsCachePath = path.join(config.get('paths').urlCache, 'urls.json');
+    resourcesCachePath = path.join(config.get('paths').urlCache, 'resources.json');
+}
+
 const urlService = new UrlService({urlsCachePath, resourcesCachePath});
 
 // Singleton
