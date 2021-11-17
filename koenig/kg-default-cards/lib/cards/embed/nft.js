114abe0c2c0235dc4d42d8589e514aed0ae25b58
Peter Zimon <zimo@ghost.org>
2021-11-17 14:06:18 +0100

    NFT card refinements

--- koenig/kg-default-cards/lib/cards/embed/nft.js
+++ koenig/kg-default-cards/lib/cards/embed/nft.js
@@ -21,7 +21,37 @@ module.exports = {
 
         if (options.target === 'email') {
             html = `
-                ${html}
+            <table cellspacing="0" cellpadding="0" border="0" style="border: 1px solid #DDE1E5; border-radius: 5px; width: auto; margin: 0 auto;">
+                <tr>
+                    <td>
+                        <a href="${payload.url}"><img src="${payload.metadata.image_url}" style="max-width: 512px; border: none; width: 100%;" border="0"></a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <table cellspacing="0" cellpadding="0" border="0" width="100%">
+                            <tr>
+                                <td><a href="${payload.url}" class="kg-nft-link" style="font-size: 17px !important; font-weight: 600; padding-top: 8px; padding-bottom: 4px;">${payload.metadata.title}</a></td>
+                                <td align="right">
+                                <a href="${payload.url}" class="kg-nft-link"></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                <a href="${payload.url}" class="kg-nft-link" style="padding-top: 0px; ${(!payload.metadata.description ? ` padding-bottom: 20px;` : ``)}"><span style="color: #ABB4BE;">Created by</span> <span style="color: #15171A; font-weight: 500;">${payload.metadata.author_name}</span> ${(payload.metadata.collection_name ? `<span style="color: #ABB4BE;">&bull; ${payload.metadata.collection_name}</span>` : ``)}</a>
+                                </td>
+                            </tr>
+                            ${(payload.metadata.description ? `
+                            <tr>
+                                <td colspan="2">
+                                <a href="${payload.url}" class="kg-nft-link" style="padding-bottom: 20px; max-width: 440px;">${payload.metadata.description}</a>
+                                </td>
+                            </tr>
+                            ` : ``)}
+                        </table>
+                    </td>
+                </tr>
+            </table>
             `;
         }
 
