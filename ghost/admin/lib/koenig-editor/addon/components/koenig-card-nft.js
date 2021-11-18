4ca109aff768f046966f62d01acc42807777cc00
Fabien O'Carroll <fabien@allou.is>
2021-11-18 14:29:23 +0200

    Removed unused NFT Card files
    https://github.com/TryGhost/Team/issues/1211
    
    These were added before we made the NFT card a subset of the Embed card.

--- ghost/admin/lib/koenig-editor/addon/components/koenig-card-nft.js
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class KoenigCardNftComponent extends Component {
-    constructor() {
-        super(...arguments);
-        this.args.registerComponent(this);
-    }
-}
