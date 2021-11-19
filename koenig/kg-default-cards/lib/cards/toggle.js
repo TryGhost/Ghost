4f8c8a883c8060f360ca3888c729efec4646e637
djordje vlaisavljevic <dzvlais@gmail.com>
2021-11-19 18:14:04 +0100

    Updated accordion frontend and newsletter template

--- koenig/kg-default-cards/lib/cards/toggle.js
+++ koenig/kg-default-cards/lib/cards/toggle.js
@@ -16,9 +16,9 @@ module.exports = {
         }
 
         const frontendTemplate = hbs`
-                <div class="kg-card kg-toggle-card" data-kg-card="toggle" >
+                <div class="kg-card kg-toggle-card" data-kg-card="toggle" data-kg-toggle-state="close">
                 <div class="kg-toggle-heading">
-                    <div class="kg-toggle-heading-text">{{{heading}}}</div>
+                    <div class="kg-toggle-heading-text"><h4>{{{heading}}}</h4></div>
                     <div class="kg-toggle-card-icon">
                         <svg id="Regular" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.cls-1{fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.5px;fill-rule:evenodd;}</style></defs><title>arrow</title><path class="cls-1" d="M23.25,7.311,12.53,18.03a.749.749,0,0,1-1.06,0L.75,7.311"/></svg>
                     </div>
@@ -29,8 +29,8 @@ module.exports = {
 
         const emailTemplate = hbs`
             <div style="border: 1px solid rgba(127, 127, 127, 0.15); border-radius: 4px; padding: 20px;">
-                <div style="font-size: 2rem; font-weight: 600; margin-bottom: 8px">{{{heading}}}</div>
-                <div style="font-size: 1.6rem; line-height: 1.5">{{{content}}}</div>
+                <h4 style="font-size: 1.375rem; font-weight: 600; margin-bottom: 8px; margin-top:0px">{{{heading}}}</h4>
+                <div style="font-size: 1rem; line-height: 1.5; margin-bottom: -1.5em;">{{{content}}}</div>
             </div>
         `;
 
