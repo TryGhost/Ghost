c8fe32d111108e00c0d464a5e211daaa3f48fe1b
Naz <nazargargol@gmail.com>
2021-11-22 10:51:54 +0400

    Renamed withBackend/withFrotned to backend/frontend
    refs https://github.com/TryGhost/Toolbox/issues/135
    
    - Shorter name still makes sense and the "with" might be causing confusion understanding what's gonig on during the boot time

--- test/e2e-api/admin/themes.test.js
+++ test/e2e-api/admin/themes.test.js
@@ -26,9 +26,12 @@ describe('Themes API', function () {
     };
 
     before(async function () {
+        // NOTE: this flag should not be here! the URL service re-initialization should be fixed instead
+        //       The reason why this init doesn't work without "forceStart" is because during the "restartModeGhostStart"
+        //       the routing.routerManager is never called with "start". That's why a full boot is needed
         await localUtils.startGhost({
-            forceStart: true, // NOTE: this flag should not be here! the URL service re-initialization should be fixe instead
-            withFrontend: true
+            forceStart: true,
+            frontend: true
         });
         ownerRequest = supertest.agent(config.get('url'));
         await localUtils.doAuth(ownerRequest);
