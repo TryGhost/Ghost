29cde494742d5408c024f0d746800c5bd2d8aec4
Naz <nazargargol@gmail.com>
2021-11-23 12:38:49 +0400

    Moved all settings module bootstrap logic into init method
    refs refs https://linear.app/tryghost/issue/CORE-35/refactor-route-and-redirect-settings
    
    - The initialization outside the init method is causing problems when the configs are mocked during the test setup

--- core/server/services/route-settings/index.js
+++ core/server/services/route-settings/index.js
@@ -1,37 +1,54 @@
-const RouteSettings = require('./route-settings');
-const SettingsLoader = require('./settings-loader');
 const config = require('../../../shared/config');
 const parseYaml = require('./yaml-parser');
-const DefaultSettingsManager = require('./default-settings-manager');
 
-const defaultSettingsManager = new DefaultSettingsManager({
-    type: 'routes',
-    extension: '.yaml',
-    destinationFolderPath: config.getContentPath('settings'),
-    sourceFolderPath: config.get('paths').defaultSettings
-});
-
-const settingsLoader = new SettingsLoader({
-    parseYaml,
-    storageFolderPath: config.getContentPath('settings')
-});
-
-const routeSettings = new RouteSettings();
+let settingsLoader;
+let routeSettings;
 
 module.exports = {
     init: async () => {
+        const RouteSettings = require('./route-settings');
+        const SettingsLoader = require('./settings-loader');
+        const DefaultSettingsManager = require('./default-settings-manager');
+
+        routeSettings = new RouteSettings();
+
+        const defaultSettingsManager = new DefaultSettingsManager({
+            type: 'routes',
+            extension: '.yaml',
+            destinationFolderPath: config.getContentPath('settings'),
+            sourceFolderPath: config.get('paths').defaultSettings
+        });
+
+        settingsLoader = new SettingsLoader({
+            parseYaml,
+            storageFolderPath: config.getContentPath('settings')
+        });
+
         return await defaultSettingsManager.ensureSettingsFileExists();
     },
 
-    loadRouteSettingsSync: settingsLoader.loadSettingsSync.bind(settingsLoader),
-    loadRouteSettings: settingsLoader.loadSettings.bind(settingsLoader),
-    getDefaultHash: routeSettings.getDefaultHash.bind(routeSettings),
+    get loadRouteSettingsSync() {
+        return settingsLoader.loadSettingsSync.bind(settingsLoader);
+    },
+    get loadRouteSettings() {
+        return settingsLoader.loadSettings.bind(settingsLoader);
+    },
+    get getDefaultHash() {
+        return routeSettings.getDefaultHash.bind(routeSettings);
+    },
+
     /**
      * Methods used in the API
      */
     api: {
-        setFromFilePath: routeSettings.setFromFilePath.bind(routeSettings),
-        get: routeSettings.get.bind(routeSettings),
-        getCurrentHash: routeSettings.getCurrentHash.bind(routeSettings)
+        get setFromFilePath() {
+            return routeSettings.setFromFilePath.bind(routeSettings);
+        },
+        get get() {
+            return routeSettings.get.bind(routeSettings);
+        },
+        get getCurrentHash() {
+            return routeSettings.getCurrentHash.bind(routeSettings);
+        }
     }
 };
