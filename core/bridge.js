0ede559d5b9ac62f69d15eddcceac883cdabc3df
Hannah Wolfe <github.erisds@gmail.com>
2021-11-23 12:47:46 +0000

    🐛 Fixed card asset init/reload behaviour
    - Card asset reloading was incorrectly only happening if the API version changed 🙈
    - In addition, having an init function was redundant, as theme activation happens on boot
    - This meant that the card assets were being generated twice on boot
    - Instead, we now only generate them on theme activation, which covers the boot case and simplifies all the logic

--- core/bridge.js
+++ core/bridge.js
@@ -16,6 +16,7 @@ const config = require('./shared/config');
 const logging = require('@tryghost/logging');
 const tpl = require('@tryghost/tpl');
 const themeEngine = require('./frontend/services/theme-engine');
+const cardAssetService = require('./frontend/services/card-assets');
 const routerManager = require('./frontend/services/routing').routerManager;
 const settingsCache = require('./shared/settings-cache');
 
@@ -65,6 +66,11 @@ class Bridge {
             themeEngine.setActive(settings, loadedTheme, checkedTheme);
             const currentGhostAPI = this.getActiveTheme().engine('ghost-api');
 
+            const cardAssetConfig = this.getCardAssetConfig();
+
+            debug('reload card assets config', cardAssetConfig);
+            cardAssetService.load(cardAssetConfig);
+
             if (previousGhostAPI !== undefined && (previousGhostAPI !== currentGhostAPI)) {
                 events.emit('services.themes.api.changed');
                 this.reloadFrontend();
@@ -95,11 +101,6 @@ class Bridge {
 
     reloadFrontend() {
         const apiVersion = this.getFrontendApiVersion();
-        const cardAssetConfig = this.getCardAssetConfig();
-
-        debug('reload card assets config', cardAssetConfig);
-        const cardAssetService = require('./frontend/services/card-assets');
-        cardAssetService.load(cardAssetConfig);
 
         debug('reload frontend', apiVersion);
         const siteApp = require('./frontend/web/site');
