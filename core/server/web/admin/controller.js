2af9e2e12550fc7f921261229efd1741b31fea0c
Hannah Wolfe <github.erisds@gmail.com>
2021-11-24 12:30:41 +0000

    Fixed error handling for missing admin templates
    - If the admin templates default.html or default-prod.html are missing, don't throw a 500
    - Instead throw a well considered 400 error with extra help for what to do to fix it

--- core/server/web/admin/controller.js
+++ core/server/web/admin/controller.js
@@ -1,9 +1,19 @@
 const debug = require('@tryghost/debug')('web:admin:controller');
+const errors = require('@tryghost/errors');
+const tpl = require('@tryghost/tpl');
 const path = require('path');
 const fs = require('fs');
 const crypto = require('crypto');
 const config = require('../../../shared/config');
 const updateCheck = require('../../update-check');
+
+const messages = {
+    templateError: {
+        message: 'Unable to find admin template file {templatePath}',
+        context: 'These template files are generated as part of the build process',
+        help: 'Please see {link}'
+    }
+};
 
 /**
  * @description Admin controller to handle /ghost/ requests.
@@ -23,18 +33,31 @@ module.exports = function adminController(req, res) {
     const templatePath = path.resolve(config.get('paths').adminViews, defaultTemplate);
     const headers = {};
 
-    // Generate our own ETag header
-    //   `sendFile` by default uses filesize+lastmod date to generate an etag.
-    //   That doesn't work for admin templates because the filesize doesn't change between versions
-    //   and `npm pack` sets a fixed lastmod date for every file meaning the default etag never changes
-    const fileBuffer = fs.readFileSync(templatePath);
-    const hashSum = crypto.createHash('md5');
-    hashSum.update(fileBuffer);
-    headers.ETag = hashSum.digest('hex');
+    try {
+        // Generate our own ETag header
+        //   `sendFile` by default uses filesize+lastmod date to generate an etag.
+        //   That doesn't work for admin templates because the filesize doesn't change between versions
+        //   and `npm pack` sets a fixed lastmod date for every file meaning the default etag never changes
+        const fileBuffer = fs.readFileSync(templatePath);
+        const hashSum = crypto.createHash('md5');
+        hashSum.update(fileBuffer);
+        headers.ETag = hashSum.digest('hex');
 
-    if (config.get('adminFrameProtection')) {
-        headers['X-Frame-Options'] = 'sameorigin';
+        if (config.get('adminFrameProtection')) {
+            headers['X-Frame-Options'] = 'sameorigin';
+        }
+
+        res.sendFile(templatePath, {headers});
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new errors.IncorrectUsageError({
+                message: tpl(messages.templateError.message, {templatePath}),
+                context: tpl(messages.templateError.context),
+                help: tpl(messages.templateError.help, {link: 'https://ghost.org/docs/install/source/'}),
+                error: error
+            });
+        } else {
+            throw error;
+        }
     }
-
-    res.sendFile(templatePath, {headers});
 };
