c58876f6db77e7363792d2acadec258ec3af274a
Hannah Wolfe <github.erisds@gmail.com>
2021-11-30 15:25:13 +0000

    Split theme engine middleware into separate files
    - one big file full of stuff is never good for clarity
    - separating it out helps us see what requires what
    - it also highlights the awful naming and opaque behaviour we have in themes - much to do, but this helps us start

---
+++ core/frontend/services/theme-engine/middleware/index.js
@@ -0,0 +1,6 @@
+module.exports = [
+    require('./ensure-active-theme'),
+    require('./update-global-template-options'),
+    require('./update-local-template-data'),
+    require('./update-local-template-options')
+];
