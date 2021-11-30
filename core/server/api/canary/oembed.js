c4021b6eb431b9d74a8c5515016414e1632f2fe9
Fabien egg O'Carroll <fabien@allou.is>
2021-11-30 10:32:38 +0200

    Added OpenSea API key to config
    refs https://github.com/TryGhost/Team/issues/1237
    
    We want to use an API key for OpenSea where possible so that we do not
    run into rate limit issues.

--- core/server/api/canary/oembed.js
+++ core/server/api/canary/oembed.js
@@ -4,7 +4,11 @@ const externalRequest = require('../../lib/request-external');
 const OEmbed = require('../../services/oembed');
 const oembed = new OEmbed({config, externalRequest});
 const NFT = require('../../services/nft-oembed');
-const nft = new NFT();
+const nft = new NFT({
+    config: {
+        apiKey: config.get('opensea').privateReadOnlyApiKey
+    }
+});
 oembed.registerProvider(nft);
 
 module.exports = {
