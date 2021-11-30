d88eae4e4c0b34f06809adbdfa2ee8540f0b71c5
Sam Lord <hello@samlord.me>
2021-11-30 13:58:27 +0000

    Fix @tryghost/error utils
    refs: https://github.com/TryGhost/Toolbox/issues/147

--- ghost/errors/test/utils/assertions.js
+++ ghost/errors/test/utils/assertions.js
@@ -1,11 +1,29 @@
 /**
- * Custom Should Assertions
- *
- * Add any custom assertions to this file.
- */
+* Custom Should Assertions
+*
+* Add any custom assertions to this file.
+*/
 
-// Example Assertion
-// should.Assertion.add('ExampleAssertion', function () {
-//     this.params = {operator: 'to be a valid Example Assertion'};
-//     this.obj.should.be.an.Object;
-// });
+var _ = require('lodash');
+var errorProps = ['id', 'title', 'detail', 'status', 'code', 'meta'];
+
+should.Assertion.add('JSONErrorObject', function () {
+    this.params = {operator: 'to be a valid JSON Error Object'};
+    this.obj.should.be.an.Object;
+    this.obj.should.have.properties(errorProps);
+});
+
+should.Assertion.add('JSONErrorResponse', function (match) {
+    this.params = {operator: 'to be a valid JSON Error Response'};
+    
+    this.obj.should.have.property('errors').which.is.an.Array;
+    this.obj.errors.length.should.be.above(0);
+    
+    this.obj.errors.forEach(function (err) {
+        err.should.be.a.JSONErrorObject();
+    });
+    
+    if (match) {
+        _.some(this.obj.errors, match).should.be.true();
+    }
+});
