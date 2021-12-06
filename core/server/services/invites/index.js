97451a93cb53c58a7a54fb8b685ebca7abb494c3
Sam Lord <sam@ghost.org>
2021-12-06 18:00:55 +0000

    Extract logging from DI patterns, only use @tryghost/logging package
    refs: https://github.com/TryGhost/Toolbox/issues/146
    
    Switched to @tryghost/logging instead of passing around the library. The main sticking points of this change are jobs. When jobs are launched we don't want them to use a separate @tryghost/logging instance because they would start parallel rotation jobs. @tryghost/logging v2.x passes all logs to the parent process if run in a child process, so that we can use the same patterns in jobs and the rest of the codebase.

--- core/server/services/invites/index.js
+++ core/server/services/invites/index.js
@@ -1,12 +1,10 @@
 const settingsCache = require('../../../shared/settings-cache');
 const mailService = require('../../services/mail');
-const logging = require('@tryghost/logging');
 const urlUtils = require('../../../shared/url-utils');
 const Invites = require('./invites');
 
 module.exports = new Invites({
     settingsCache,
-    logging,
     mailService,
     urlUtils
 });
