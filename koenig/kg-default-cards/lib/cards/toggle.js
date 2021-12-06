bdc228649a03cc7e6717bfa9855b6f99c2c1e5bb
djordje vlaisavljevic <dzvlais@gmail.com>
2021-12-06 12:58:16 +0100

    Moved svg arrow styles from inline to CSS file
    refs https://github.com/TryGhost/Team/issues/1209

--- koenig/kg-default-cards/lib/cards/toggle.js
+++ koenig/kg-default-cards/lib/cards/toggle.js
@@ -22,7 +22,7 @@ module.exports = {
                 <div class="kg-toggle-heading">
                     <h4 class="kg-toggle-heading-text">{{{heading}}}</h4>
                     <button class="kg-toggle-card-icon">
-                        <svg id="Regular" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.cls-1{fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.5px;fill-rule:evenodd;}</style></defs><title>arrow</title><path class="cls-1" d="M23.25,7.311,12.53,18.03a.749.749,0,0,1-1.06,0L.75,7.311"/></svg>
+                        <svg id="Regular" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path class="cls-1" d="M23.25,7.311,12.53,18.03a.749.749,0,0,1-1.06,0L.75,7.311"/></svg>
                     </button>
                 </div>
                 <div class="kg-toggle-content">{{{content}}}</div>
