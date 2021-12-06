f598f14e5251e5faffe8c85bdbf92ff4f297d3c4
Sam Lord <hello@samlord.me>
2021-12-06 17:29:01 +0000

    Fix lint error

--- core/server/services/invites/index.js
+++ core/server/services/invites/index.js
@@ -1,6 +1,5 @@
 const settingsCache = require('../../../shared/settings-cache');
 const mailService = require('../../services/mail');
-const logging = require('@tryghost/logging');
 const urlUtils = require('../../../shared/url-utils');
 const Invites = require('./invites');
 
