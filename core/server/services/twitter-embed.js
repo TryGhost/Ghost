dd9a470882c8a671bfd87e354601ba5ea9d17320
Fabien egg O'Carroll <fabien@allou.is>
2021-12-07 11:16:11 +0200

    Improved rendering for twitter newsletter embeds
    refs https://github.com/TryGhost/Team/issues/1001
    
    This also adds the missing alpha flag so that we can release this when
    it's ready.

--- core/server/services/twitter-embed.js
+++ core/server/services/twitter-embed.js
@@ -1,5 +1,6 @@
 const {extract} = require('oembed-parser');
 const logging = require('@tryghost/logging');
+const labs = require('../../shared/labs');
 
 /**
  * @typedef {import('./oembed').ICustomProvider} ICustomProvider
@@ -42,7 +43,7 @@ class TwitterOEmbedProvider {
         /** @type {object} */
         const oembedData = await extract(url.href);
 
-        if (this.dependencies.config.bearerToken) {
+        if (this.dependencies.config.bearerToken && labs.isSet('richTwitterNewsletters')) {
             const query = {
                 expansions: ['attachments.poll_ids', 'attachments.media_keys', 'author_id', 'entities.mentions.username', 'geo.place_id', 'in_reply_to_user_id', 'referenced_tweets.id', 'referenced_tweets.id.author_id'],
                 'media.fields': ['duration_ms', 'height', 'media_key', 'preview_image_url', 'type', 'url', 'width', 'public_metrics', 'alt_text'],
