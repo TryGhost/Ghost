90b8f0b8af9ec061f0a060995bb7de41b38fd63b
Peter Zimon <zimo@ghost.org>
2021-12-07 17:15:42 +0100

    Twitter embed refinements

--- koenig/kg-default-cards/lib/cards/embed/twitter.js
+++ koenig/kg-default-cards/lib/cards/embed/twitter.js
@@ -21,7 +21,7 @@ module.exports = {
             const retweetCount = numberFormatter.format(tweetData.public_metrics.retweet_count);
             const likeCount = numberFormatter.format(tweetData.public_metrics.like_count);
             const authorUser = tweetData.includes.users.find(user => user.id === tweetData.author_id);
-            const tweetDate = DateTime.fromISO(tweetData.created_at).toLocaleString(DateTime.DATE_MED);
+            const tweetDate = DateTime.fromISO(tweetData.created_at).toLocaleString(DateTime.DATETIME_MED);
 
             const mentions = tweetData.entities && tweetData.entities.mentions || [];
             const urls = tweetData.entities && tweetData.entities.urls || [];
@@ -89,31 +89,42 @@ module.exports = {
 
             if (options.target === 'email') {
                 html = `
-                <table cellspacing="0" cellpadding="0" border="0" style="border: 1px solid #E9E9E9; border-radius: 5px; width: auto; margin: 0 auto; width: 100%">
-                    ${hasImageOrVideo ? `<tr>
-                        <td align="center" style="width: 100%;">
-                            <a href="https://twitter.com/twitter/status/${tweetId}"><img src="${tweetImageUrl}" style="width: 100%; border: none; max-width: 560px; border-radius: 4px 4px 0 0;" border="0"></a>
-                        </td>
-                    </tr>` : ''}
+                <table cellspacing="0" cellpadding="0" border="0" style="border: 1px solid #E9E9E9; border-radius: 12px; width: auto; margin: 0 auto; width: 100%">
                     <tr>
                         <td>
                             <table cellspacing="0" cellpadding="0" border="0" width="100%">
                                 <tr>
                                     <td width="48" style="width: 48px;">
-                                        <a href="https://twitter.com/twitter/status/${tweetId}" class="kg-twitter-link" style="padding-left: 20px; padding-top: 20px;"><img src="${authorUser.profile_image_url}" style="max-width: 512px; border: none; width: 48px; height: 48px; border-radius: 999px;" border="0"></a>
+                                        <a href="https://twitter.com/twitter/status/${tweetId}" class="kg-twitter-link" style="padding-left: 16px; padding-top: 16px;"><img src="${authorUser.profile_image_url}" style="max-width: 512px; border: none; width: 48px; height: 48px; border-radius: 999px;" border="0"></a>
                                     </td>
                                     <td style="line-height: 1.3em; width: 100%;">
-                                        <a href="https://twitter.com/twitter/status/${tweetId}" class="kg-twitter-link" style="font-size: 15px !important; font-weight: 600; width: 100%; padding-top: 24px; padding-bottom: 18px;">${authorUser.name} <br> <span style="color: #ABB4BE; font-size: 14px; font-weight: 500;">@${authorUser.username}</span></a>
+                                        <a href="https://twitter.com/twitter/status/${tweetId}" class="kg-twitter-link" style="font-size: 15px !important; font-weight: 600; width: 100%; padding-top: 20px; padding-bottom: 18px;">${authorUser.name} <br> <span style="color: #ABB4BE; font-size: 14px; font-weight: 500;">@${authorUser.username}</span></a>
                                     </td>
                                     <td align="right" width="24" style="width: 24px;">
-                                        <a href="https://twitter.com/twitter/status/${tweetId}" class="kg-twitter-link" style="padding-right: 20px; padding-top: 24px; width: 24px; height: 38px;"><img src="https://static.ghost.org/v4.0.0/images/twitter-logo-small.png" width="24" border="0"></a>
+                                        <a href="https://twitter.com/twitter/status/${tweetId}" class="kg-twitter-link" style="padding-right: 16px; padding-top: 20px; width: 24px; height: 38px;"><img src="https://static.ghost.org/v4.0.0/images/twitter-logo-small.png" width="24" border="0"></a>
                                     </td>
                                 </tr>
                                 <tr>
                                     <td colspan="3">
-                                        <a href="https://twitter.com/twitter/status/${tweetId}" class="kg-twitter-link" style="font-size: 15px; line-height: 1.4em; padding-top: 8px; padding-left: 20px; padding-right: 20px; padding-bottom: 20px;">${tweetContent}
+                                        <a href="https://twitter.com/twitter/status/${tweetId}" class="kg-twitter-link" style="font-size: 15px; line-height: 1.4em; padding-top: 8px; padding-left: 16px; padding-right: 16px; padding-bottom: 16px;">${tweetContent}
                                         ${hasPoll ? `<br><span style="color: #1DA1F2;">View poll &rarr;</span>` : ''}
                                         </a>
+                                    </td>
+                                </tr>
+                                ${hasImageOrVideo ? `<tr>
+                                    <td colspan="3" align="center" style="width: 100%;">
+                                        <a href="https://twitter.com/twitter/status/${tweetId}"><img src="${tweetImageUrl}" style="width: 100%; border: 1px solid #E9E9E9; max-width: 520px; border-radius: 10px;" border="0"></a>
+                                    </td>
+                                </tr>` : ''}
+                                <tr>
+                                    <td colspan="3" style="width: 100%;">
+                                        <table cellspacing="0" cellpadding="0" border="0" width="100%">
+                                            <tr>
+                                                <td>
+                                                <a href="https://twitter.com/twitter/status/${tweetId}" class="kg-twitter-link" style="padding-top: 4px; padding-right: 16px; padding-bottom: 12px; padding-left: 16px;"><span style="color: #838383;">${tweetDate}</span></a>
+                                                </td>
+                                            </tr>
+                                        </table>
                                     </td>
                                 </tr>
                                 <tr>
@@ -121,13 +132,10 @@ module.exports = {
                                         <table cellspacing="0" cellpadding="0" border="0" width="100%" style="border-top: 1px solid #E9E9E9;">
                                             <tr>
                                                 <td>
-                                                    <a href="https://twitter.com/twitter/status/${tweetId}" class="kg-twitter-link" style="padding-top: 20px; padding-bottom: 20px; padding-left: 20px;">
-                                                        <span style="font-weight: 600;">${retweetCount}</span> <span style="color: #838383;">retweets &bull;</span>
-                                                        <span style="font-weight: 600;">${likeCount}</span> <span style="color: #838383;">likes</span>
+                                                    <a href="https://twitter.com/twitter/status/${tweetId}" class="kg-twitter-link" style="padding-top: 12px; padding-right: 16px; padding-bottom: 12px; padding-left: 16px;">
+                                                        <span style="font-weight: 600;">${likeCount}</span> <span style="color: #838383;">likes &bull;</span>
+                                                        <span style="font-weight: 600;">${retweetCount}</span> <span style="color: #838383;">retweets</span>
                                                     </a>
-                                                </td>
-                                                <td align="right">
-                                                <a href="https://twitter.com/twitter/status/${tweetId}" class="kg-twitter-link" style="padding-top: 20px; padding-bottom: 20px; padding-right: 20px;"><span style="color: #838383;">${tweetDate}</span></a>
                                                 </td>
                                             </tr>
                                         </table>
