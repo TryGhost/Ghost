38ba1d16c442aaac4e252b9204b0a875b0898dcb
Naz <nazargargol@gmail.com>
2021-12-08 13:00:45 +0400

    Integrated chai-jest-snapshot
    refs https://github.com/TryGhost/Toolbox/issues/129
    
    - Minimal version of chai-jest-snapshot with property matcher support. It runs off the forked version of https://github.com/suchipi/chai-jest-snapshot

--- test/regression/api/canary/admin/site.test.js
+++ test/regression/api/canary/admin/site.test.js
@@ -1,27 +1,35 @@
-const should = require('should');
+const chai = require('chai');
+const {expect} = require('chai');
+const {any, stringMatching} = require('expect');
+const chaiJestSnapshot = require('@ethanresnick/chai-jest-snapshot');
 const framework = require('../../../../utils/e2e-framework');
-const testUtils = require('../../../../utils');
-const localUtils = require('./utils');
 const config = require('../../../../../core/shared/config');
+
+chai.use(chaiJestSnapshot);
 
 describe('Config API', function () {
     let request;
 
     before(async function () {
+        chaiJestSnapshot.resetSnapshotRegistry();
         request = await framework.getAgent('/ghost/api/canary/admin/');
+    });
+
+    beforeEach(function () {
+        chaiJestSnapshot.configureUsingMochaContext(this);
     });
 
     it('can retrieve config and all expected properties', async function () {
         const res = await request
             .get('site/')
-            .set('Origin', config.get('url'))
-            .expect('Content-Type', /json/)
-            .expect('Cache-Control', testUtils.cacheRules.private)
-            .expect(200);
+            .set('Origin', config.get('url'));
 
-        localUtils.API.checkResponse(res.body.site, 'site');
-
-        // minor (safe) version
-        res.body.site.version.should.match(/\d+\.\d+/);
+        expect(res.body.site).to.matchSnapshot({
+            version: stringMatching(/\d+\.\d+/)
+        });
+        expect(res.headers).to.matchSnapshot({
+            date: any(String),
+            etag: any(String)
+        });
     });
 });
