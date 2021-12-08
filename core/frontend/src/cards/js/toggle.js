9e20627e5973a573a4f8171c8be71630de23ab81
Rishabh <zrishabhgarg@gmail.com>
2021-12-08 22:58:22 +0530

    Fixed global variable leak for new card scripts
    no issue
    
    - wraps the card js scripts into an IIFE to avoid polluting global variables on the site
    - also any fixes errors caused by `swup` and its scripts-plugin that trips over already created global variables

--- core/frontend/src/cards/js/toggle.js
+++ core/frontend/src/cards/js/toggle.js
@@ -1,16 +1,18 @@
-const toggleHeadingElements = document.getElementsByClassName("kg-toggle-heading");
+(function() {
+    const toggleHeadingElements = document.getElementsByClassName("kg-toggle-heading");
 
-const toggleFn = function(event) {
-    const targetElement = event.target;
-    const parentElement = targetElement.closest('.kg-toggle-card');
-    var toggleState = parentElement.getAttribute("data-kg-toggle-state");
-    if (toggleState === 'close') {
-        parentElement.setAttribute('data-kg-toggle-state', 'open');
-    } else {
-        parentElement.setAttribute('data-kg-toggle-state', 'close');
+    const toggleFn = function(event) {
+        const targetElement = event.target;
+        const parentElement = targetElement.closest('.kg-toggle-card');
+        var toggleState = parentElement.getAttribute("data-kg-toggle-state");
+        if (toggleState === 'close') {
+            parentElement.setAttribute('data-kg-toggle-state', 'open');
+        } else {
+            parentElement.setAttribute('data-kg-toggle-state', 'close');
+        }
+    };
+
+    for (let i = 0; i < toggleHeadingElements.length; i++) {
+        toggleHeadingElements[i].addEventListener('click', toggleFn, false);
     }
-};
-
-for (let i = 0; i < toggleHeadingElements.length; i++) {
-    toggleHeadingElements[i].addEventListener('click', toggleFn, false);
-}
+})();
