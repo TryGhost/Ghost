d3f4a717ece41e4d5a48be97e0fa462169469918
Rishabh <zrishabhgarg@gmail.com>
2021-12-13 17:28:59 +0530

    Fixed loop behavior for video card on themes
    refs https://github.com/TryGhost/Team/issues/1229
    
    - if loop is set to true for a video, it should also autoplay while muted

--- koenig/kg-default-cards/lib/cards/video.js
+++ koenig/kg-default-cards/lib/cards/video.js
@@ -22,7 +22,7 @@ module.exports = {
 
         const frontendTemplate = hbs`
             <figure class="{{cardClasses}}">
-                <video src="{{payload.src}}" poster="{{posterSpacerSrc}}" width="{{payload.width}}" height="{{payload.height}}"{{#if payload.loop}} loop{{/if}} preload="metadata" style="background: transparent url('{{thumbnailSrc}}') 50% 50% / cover no-repeat;" /></video>
+                <video src="{{payload.src}}" poster="{{posterSpacerSrc}}" width="{{payload.width}}" height="{{payload.height}}"{{#if payload.loop}} loop autoplay muted{{/if}} preload="metadata" style="background: transparent url('{{thumbnailSrc}}') 50% 50% / cover no-repeat;" /></video>
                 <div class="kg-player">
                     <button class="kg-video-play-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
