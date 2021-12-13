be02e3b0871951acc68aabc831b672ed702d3efe
Rishabh <zrishabhgarg@gmail.com>
2021-12-13 18:59:06 +0530

    Renamed video card generic class names
    refs https://github.com/TryGhost/Team/issues/1229

--- koenig/kg-default-cards/lib/cards/video.js
+++ koenig/kg-default-cards/lib/cards/video.js
@@ -23,7 +23,7 @@ module.exports = {
         const frontendTemplate = hbs`
             <figure class="{{cardClasses}}">
                 <video src="{{payload.src}}" poster="{{posterSpacerSrc}}" width="{{payload.width}}" height="{{payload.height}}"{{#if payload.loop}} loop autoplay muted{{/if}} preload="metadata" style="background: transparent url('{{thumbnailSrc}}') 50% 50% / cover no-repeat;" /></video>
-                <div class="kg-player">
+                <div class="kg-video-player">
                     <button class="kg-video-play-icon">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                             <path d="M23.14 10.608 2.253.164A1.559 1.559 0 0 0 0 1.557v20.887a1.558 1.558 0 0 0 2.253 1.392L23.14 13.393a1.557 1.557 0 0 0 0-2.785Z"/>
