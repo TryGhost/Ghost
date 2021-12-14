f59a5a24690484675a441569a278b34f9ced9fa1
Peter Zimon <zimo@ghost.org>
2021-12-14 14:37:54 +0100

    Updated file card frontend markup

--- koenig/kg-default-cards/lib/cards/file.js
+++ koenig/kg-default-cards/lib/cards/file.js
@@ -4,11 +4,6 @@ const {
     toTransformReady
 } = require('@tryghost/url-utils/lib/utils');
 
-const {
-    hbs,
-    dedent
-} = require('../utils');
-
 function bytesToSize(bytes) {
     if (!bytes) {
         return '0 Byte';
@@ -29,48 +24,33 @@ module.exports = {
         if (!payload.src) {
             return dom.createTextNode('');
         }
-        let thumbnailCls = 'kg-file-thumbnail';
-        let emptyThumbnailCls = 'kg-file-thumbnail';
-        if (!payload.thumbnailSrc) {
-            thumbnailCls += ' kg-file-hide';
-        } else {
-            emptyThumbnailCls += ' kg-file-hide';
+
+        let classNames;
+        if (!payload.fileTitle && !payload.fileCaption) {
+            classNames = 'kg-file-card-small';
+        } else if (!payload.fileTitle || !payload.fileCaption) {
+            classNames = 'kg-file-card-medium';
         }
-        const frontendTemplate = hbs`
-            <div class="kg-card kg-file-card">
-                <img src="{{thumbnailSrc}}" alt="file-thumbnail" class="${thumbnailCls}">
-                <div class="${emptyThumbnailCls}">
-                    <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M7.5 15.33a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm-2.25.75a2.25 2.25 0 1 1 4.5 0 2.25 2.25 0 0 1-4.5 0ZM15 13.83a.75.75 0 1 0 0 1.5.75.75 0 0 0 0-1.5Zm-2.25.75a2.25 2.25 0 1 1 4.5 0 2.25 2.25 0 0 1-4.5 0Z"/>
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M14.486 6.81A2.25 2.25 0 0 1 17.25 9v5.579a.75.75 0 0 1-1.5 0v-5.58a.75.75 0 0 0-.932-.727.755.755 0 0 1-.059.013l-4.465.744a.75.75 0 0 0-.544.72v6.33a.75.75 0 0 1-1.5 0v-6.33a2.25 2.25 0 0 1 1.763-2.194l4.473-.746Z"/>
-                        <path fill-rule="evenodd" clip-rule="evenodd" d="M3 1.5a.75.75 0 0 0-.75.75v19.5a.75.75 0 0 0 .75.75h18a.75.75 0 0 0 .75-.75V5.133a.75.75 0 0 0-.225-.535l-.002-.002-3-2.883A.75.75 0 0 0 18 1.5H3ZM1.409.659A2.25 2.25 0 0 1 3 0h15a2.25 2.25 0 0 1 1.568.637l.003.002 3 2.883a2.25 2.25 0 0 1 .679 1.61V21.75A2.25 2.25 0 0 1 21 24H3a2.25 2.25 0 0 1-2.25-2.25V2.25c0-.597.237-1.169.659-1.591Z"/>
-                    </svg>
-                </div>
-                <div class="kg-file-card-container">
-                    <div class="kg-file-title">{{fileTitle}}</div>
-                    <div class="kg-file-caption">{{fileCaption}}</div>
-                    <div class="kg-file-details">
-                        <div> {{fileName}} </div>
-                        <div style="margin-left: 12px"> {{fileSize}} </div>
-                        <a href="{{src}}" download="{{fileName}}" style="margin-left: 12px">Download</a>
-                    </div>
+        
+        let html = `
+        <a class="kg-card kg-file-card ${classNames}" href="${payload.src}">
+            <div class="kg-file-card-container">
+                ${payload.fileTitle ? `<div class="kg-file-card-title">${payload.fileTitle}</div>` : ``}
+                ${payload.fileCaption ? `<div class="kg-file-card-caption">${payload.fileCaption}</div>` : ``}
+                <div class="kg-file-card-metadata">
+                    <div class="kg-file-card-filename">${payload.fileName}</div>
+                    <div class="kg-file-card-filesize">${bytesToSize(payload.fileSize)}</div>
                 </div>
             </div>
+            <div class="kg-file-card-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.a{fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.5px;}</style></defs><title>download-circle</title><polyline class="a" points="8.25 14.25 12 18 15.75 14.25"/><line class="a" x1="12" y1="6.75" x2="12" y2="18"/><circle class="a" cx="12" cy="12" r="11.25"/></svg>
+            </div>
+        </a>
         `;
 
-        const emailTemplate = hbs`
-            File card placeholder
-        `;
-
-        const renderTemplate = options.target === 'email' ? emailTemplate : frontendTemplate;
-
-        const html = dedent(renderTemplate({
-            src: payload.src,
-            fileName: payload.fileName,
-            fileTitle: payload.fileTitle,
-            fileSize: bytesToSize(payload.fileSize),
-            fileCaption: payload.fileCaption
-        }));
+        if (options.target === 'email') {
+            html = `Email file`;
+        }
 
         return dom.createRawHTMLSection(html);
     },
