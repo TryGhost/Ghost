ce68e14b7a8d011217bb7cb662613e06950f1ee4
Hannah Wolfe <github.erisds@gmail.com>
2021-12-14 13:01:39 +0000

    Fixed overly specific default routes test
    - This test failed after updating Casper, because Casper has changed how it outputs next/previous type posts at the bottom
    - This test was overly specific. If we want to test if the next and previous posts are output, we should have a particular assertion/test that clearly does that
    - I've changed the test to not use cheerio and just do a couple of checks to make sure that head and body contain something sensible
    - This will still be specific to the fixtures though

--- test/e2e-frontend/default_routes.test.js
+++ test/e2e-frontend/default_routes.test.js
@@ -120,18 +120,14 @@ describe('Default Frontend routing', function () {
             await request.get('/welcome/')
                 .expect('Content-Type', /html/)
                 .expect('Cache-Control', testUtils.cacheRules.public)
+                .expect(200)
                 .expect(assertCorrectFrontendHeaders)
                 .expect((res) => {
-                    const $ = cheerio.load(res.text);
-
-                    // NOTE: This is the title from the settings.
-                    $('title').text().should.equal('Start here for a quick overview of everything you need to know');
-
-                    $('body.post-template').length.should.equal(1);
-                    $('body.tag-getting-started').length.should.equal(1);
-                    $('article.post').length.should.equal(2);
-                    $('article.tag-getting-started').length.should.equal(2);
-
+                    // Test that head and body have rendered something...
+                    res.text.should.containEql('<title>Start here for a quick overview of everything you need to know</title>');
+                    res.text.should.match(/<h1[^>]*?>Start here for a quick overview of everything you need to know<\/h1>/);
+                    // We should write a single test for this, or encapsulate it as an assertion
+                    // E.g. res.text.should.not.containInvalidUrls()
                     res.text.should.not.containEql('__GHOST_URL__');
                 });
         });
