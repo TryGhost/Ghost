16c6e535be40aa90744bf1fbd2aa60bca020744a
Fabien egg O'Carroll <fabien@allou.is>
2021-12-15 16:25:54 +0200

    Fixed image widths for before/after slider
    refs https://github.com/TryGhost/Team/issues/1249
    
    Instead of attempting to calculate teh width of the container, we leave
    one image to fill up space naturally as the browser allows it, and then
    use that to set the width of the secondary image.

--- core/frontend/src/cards/js/before-after.js
+++ core/frontend/src/cards/js/before-after.js
@@ -2,14 +2,10 @@
     const beforeAfterCards = [...document.querySelectorAll('.kg-before-after-card')];
 
     for (let card of beforeAfterCards) {
-        const isFullWidth = card.classList.contains('kg-width-full');
         const input = card.querySelector('input');
         const overlay = card.querySelector('.kg-before-after-card-image-before');
         const button = card.querySelector('.kg-before-after-card-slider-button');
         const images = [...card.querySelectorAll('img')];
-        const smallestImageWidth = Math.min(
-            ...images.map(img => parseInt(img.getAttribute('width')))
-        );
 
         function updateSlider() {
             overlay.setAttribute('style', `width: ${input.value}%`);
@@ -17,11 +13,9 @@
         }
 
         function updateDimensions() {
-            const containerWidth = parseInt(getComputedStyle(card).getPropertyValue('width'));
-            const width = isFullWidth ? containerWidth : Math.min(smallestImageWidth, containerWidth);
-            for (let image of images) {
-                image.setAttribute('style', `width: ${width.toString()}px;`);
-            }
+            const imageWidth = getComputedStyle(images[0]).getPropertyValue('width');
+
+            images[1].setAttribute('style', `width: ${imageWidth}`);
         }
 
         input.addEventListener('input', function () {
