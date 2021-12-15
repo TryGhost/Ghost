f805d66c5656f1dc8518a5eaa490da6c6e190f08
Peter Zimon <zimo@ghost.org>
2021-12-15 10:08:57 +0100

    Updated file card makrup

--- koenig/kg-default-cards/lib/cards/file.js
+++ koenig/kg-default-cards/lib/cards/file.js
@@ -25,7 +25,7 @@ module.exports = {
             return dom.createTextNode('');
         }
 
-        let classNames;
+        let classNames = '';
         let emailStyles = {
             icon: 'height: 24px; width: 24px; max-width: 24px; margin-top: 8px;'
         };
@@ -39,19 +39,21 @@ module.exports = {
         }
         
         let html = `
-        <a class="kg-card kg-file-card ${classNames}" href="${payload.src}">
-            <div class="kg-file-card-container">
-                ${payload.fileTitle ? `<div class="kg-file-card-title">${payload.fileTitle}</div>` : ``}
-                ${payload.fileCaption ? `<div class="kg-file-card-caption">${payload.fileCaption}</div>` : ``}
-                <div class="kg-file-card-metadata">
-                    <div class="kg-file-card-filename">${payload.fileName}</div>
-                    <div class="kg-file-card-filesize">${bytesToSize(payload.fileSize)}</div>
+        <div class="kg-card kg-file-card ${classNames}">
+            <a class="kg-file-card-container" href="${payload.src}">
+                <div class="kg-file-card-contents">
+                    ${payload.fileTitle ? `<div class="kg-file-card-title">${payload.fileTitle}</div>` : ``}
+                    ${payload.fileCaption ? `<div class="kg-file-card-caption">${payload.fileCaption}</div>` : ``}
+                    <div class="kg-file-card-metadata">
+                        <div class="kg-file-card-filename">${payload.fileName}</div>
+                        <div class="kg-file-card-filesize">${bytesToSize(payload.fileSize)}</div>
+                    </div>
                 </div>
-            </div>
-            <div class="kg-file-card-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.a{fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.5px;}</style></defs><title>download-circle</title><polyline class="a" points="8.25 14.25 12 18 15.75 14.25"/><line class="a" x1="12" y1="6.75" x2="12" y2="18"/><circle class="a" cx="12" cy="12" r="11.25"/></svg>
-            </div>
-        </a>
+                <div class="kg-file-card-icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><defs><style>.a{fill:none;stroke:currentColor;stroke-linecap:round;stroke-linejoin:round;stroke-width:1.5px;}</style></defs><title>download-circle</title><polyline class="a" points="8.25 14.25 12 18 15.75 14.25"/><line class="a" x1="12" y1="6.75" x2="12" y2="18"/><circle class="a" cx="12" cy="12" r="11.25"/></svg>
+                </div>
+            </a>
+        </div>
         `;
 
         const postUrl = options.postUrl || 'https://ghost.org';
