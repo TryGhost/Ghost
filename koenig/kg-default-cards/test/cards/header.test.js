e8925563247a3967aacf06519469115d339ecf11
Peter Zimon <peter.zimon@gmail.com>
2021-12-17 07:41:18 +0100

    Fixed tests

--- koenig/kg-default-cards/test/cards/header.test.js
+++ koenig/kg-default-cards/test/cards/header.test.js
@@ -24,7 +24,7 @@ describe('Header card', function () {
             }
         };
 
-        serializer.serialize(card.render(opts)).should.equal(`<div class="kg-header-card kg-width-full kg-size-small kg-style-dark" style="" data-kg-background-image="https://example.com/image.jpg"><h2 class="kg-header-card-header">This is the header card</h2><h3 class="kg-header-card-subheader">hi</h3><a href="https://example.com/" class="kg-header-card-button">The button</a></div>`);
+        serializer.serialize(card.render(opts)).should.equal(`<div class="kg-card kg-header-card kg-width-full kg-size-small kg-style-dark" style="" data-kg-background-image="https://example.com/image.jpg"><h2 class="kg-header-card-header">This is the header card</h2><h3 class="kg-header-card-subheader">hi</h3><a href="https://example.com/" class="kg-header-card-button">The button</a></div>`);
     });
 
     it('renders nothing when header and subheader is undefined and the button is disabled', function () {
@@ -64,7 +64,7 @@ describe('Header card', function () {
             }
         };
 
-        serializer.serialize(card.render(opts)).should.equal(`<div class="kg-header-card kg-width-full kg-size-small kg-style-dark" style="" data-kg-background-image=""><h2 class="kg-header-card-header">hi</h2></div>`);
+        serializer.serialize(card.render(opts)).should.equal(`<div class="kg-card kg-header-card kg-width-full kg-size-small kg-style-dark" style="" data-kg-background-image=""><h2 class="kg-header-card-header">hi</h2></div>`);
     });
 
     it('transforms urls absolute to relative', function () {
