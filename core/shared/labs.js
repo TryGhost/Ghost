1e15cae79dfcdb24b90a589bc1bc49e23c3658b3
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-01-04 14:57:46 +0000

    Removed `headerCard` GA labs flag
    refs https://github.com/TryGhost/Team/issues/1258
    
    - labs flag conditionals have been removed from Admin so the GA flag is no longer necessary

--- core/shared/labs.js
+++ core/shared/labs.js
@@ -15,7 +15,6 @@ const messages = {
 
 // flags in this list always return `true`, allows quick global enable prior to full flag removal
 const GA_FEATURES = [
-    'headerCard'
 ];
 
 // NOTE: this allowlist is meant to be used to filter out any unexpected
