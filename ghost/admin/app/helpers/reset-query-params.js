cdbba8530f379dc9826baa49ab8486adf99395f8
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-01-05 12:52:00 +0000

    Fixed `?search=` empty param after members bulk delete
    no issue
    
    - the members query reset was using `""` rather than `null` due to a bug in an old version of search/filtering but that meant we could end up with an empty param
    - switched to `null` as it no longer appears necessary to use a blank string, this matches behaviour of other params

--- ghost/admin/app/helpers/reset-query-params.js
+++ ghost/admin/app/helpers/reset-query-params.js
@@ -18,7 +18,7 @@ export const DEFAULT_QUERY_PARAMS = {
     'members.index': {
         label: null,
         paid: null,
-        search: '',
+        search: null,
         filter: null,
         order: null
     }
