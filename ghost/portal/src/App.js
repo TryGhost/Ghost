64fbf193e54655a51ddeb277e6deb270ef51dc7a
Peter Zimon <peter.zimon@gmail.com>
2022-01-05 16:38:52 +0100

    Removed separator line hack from signup footer

--- ghost/portal/src/App.js
+++ ghost/portal/src/App.js
@@ -19,7 +19,7 @@ const DEV_MODE_DATA = {
     showPopup: true,
     site: Fixtures.site,
     member: Fixtures.member.paid,
-    page: 'offer',
+    page: 'signup',
     pageData: Fixtures.offer
 };
 
