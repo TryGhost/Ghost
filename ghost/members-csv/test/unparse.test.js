f5cb7837256beab788d351d2c5f81f0c997fea13
Fabien "egg" O'Carroll <fabien@allou.is>
2022-01-11 16:26:11 +0200

    Fixed CSV unparsing for subscribed flag
    no-issue
    
    We have a special mapping for subscribed_to_emails -> subscribed in the
    parse method, but were not mapping it in the unparse method, which meant
    we were losing information during CSV imports.

--- ghost/members-csv/test/unparse.test.js
+++ ghost/members-csv/test/unparse.test.js
@@ -16,4 +16,19 @@ describe('unparse', function () {
         const expected = `id,email,name,note,subscribed_to_emails,complimentary_plan,stripe_customer_id,created_at,deleted_at,labels,products\r\n,email@example.com,Sam Memberino,Early supporter,,,,,,,`;
         should.equal(result, expected);
     });
+
+    it('maps the subscribed property to subscribed_to_emails', function () {
+        const json = [{
+            email: 'do-not-email-me@email.com',
+            subscribed: false
+        }];
+
+        const columns = Object.keys(json[0]);
+
+        const result = unparse(json, columns);
+
+        const expected = `email,subscribed_to_emails\r\ndo-not-email-me@email.com,false`;
+
+        should.equal(result, expected);
+    });
 });
