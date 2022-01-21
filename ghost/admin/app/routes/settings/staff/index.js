046892848bce204837af267c7227dd668949f401
Sanne de Vries <sannedv@protonmail.com>
2022-01-21 12:46:27 +0000

    Revert "Updated profile page for Authors & Contributors"
    This reverts commit a464f81a32ccb2d257e0d7e3a6b14e16441a15ef.

--- ghost/admin/app/routes/settings/staff/index.js
+++ ghost/admin/app/routes/settings/staff/index.js
@@ -6,16 +6,6 @@ export default class IndexRoute extends AuthenticatedRoute {
     @service infinity;
     @service session;
 
-    beforeModel() {
-        super.beforeModel(...arguments);
-
-        const user = this.session.user;
-
-        if (!user.isAdmin) {
-            return this.transitionTo('settings.staff.user', user);
-        }
-    }
-
     model() {
         return this.session.user;
     }
