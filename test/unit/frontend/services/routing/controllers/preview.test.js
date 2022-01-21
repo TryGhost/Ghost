d8b3fe74429b472f8343cf6e6b61b49ee0a4f121
Hannah Wolfe <github.erisds@gmail.com>
2022-01-21 14:20:52 +0000

    Removed some hidden v2 and v3 tests
    refs: https://github.com/TryGhost/Toolbox/issues/168
    
    - These are all test files I missed in ffcd3fbe313b4a413833da9a7473376cb21246fd
    
    We're going to be dropping the idea of having multiple versions of the API in each Ghost version.
    Because this has not achieved the goal of making it easier to make breaking changes, but it has
    created an ordinate amount of technical debt and maintenance overhead.
    
    As we know this is going away in the next major, there is no benefit to us constantly running tests
    that check if those versions still work, especially given how long they take.
    
    Instead we're starting work to ensure that all of our test work on canary, and that canary has
    excellent test coverage so that we can be sure that our one API version works really well and that
    any changes, no matter how subtle are deliberate, tracked and understood.

--- test/unit/frontend/services/routing/controllers/preview.test.js
+++ test/unit/frontend/services/routing/controllers/preview.test.js
@@ -3,12 +3,11 @@ const sinon = require('sinon');
 const Promise = require('bluebird');
 const testUtils = require('../../../../../utils');
 const configUtils = require('../../../../../utils/configUtils');
-const api = require('../../../../../../core/server/api');
+const api = require('../../../../../../core/server/api').canary;
 const controllers = require('../../../../../../core/frontend/services/routing/controllers');
 const helpers = require('../../../../../../core/frontend/services/routing/helpers');
 const urlService = require('../../../../../../core/server/services/url');
 const urlUtils = require('../../../../../../core/shared/url-utils');
-const EDITOR_URL = '/#/editor/post/';
 
 describe('Unit - services/routing/controllers/preview', function () {
     let secureStub;
@@ -30,213 +29,71 @@ describe('Unit - services/routing/controllers/preview', function () {
         configUtils.restore();
     });
 
-    describe('v2', function () {
-        let previewStub;
+    let previewStub;
 
-        beforeEach(function () {
-            post = testUtils.DataGenerator.forKnex.createPost({status: 'draft'});
+    beforeEach(function () {
+        post = testUtils.DataGenerator.forKnex.createPost({status: 'draft'});
 
-            apiResponse = {
-                preview: [post]
-            };
+        apiResponse = {
+            preview: [post]
+        };
 
-            req = {
-                path: '/',
-                params: {
-                    uuid: 'something'
-                },
-                route: {}
-            };
+        req = {
+            path: '/',
+            params: {
+                uuid: 'something'
+            },
+            route: {}
+        };
 
-            res = {
-                routerOptions: {
-                    query: {controller: 'preview', resource: 'preview'}
-                },
-                locals: {
-                    apiVersion: 'v2'
-                },
-                render: sinon.spy(),
-                redirect: sinon.spy(),
-                set: sinon.spy()
-            };
+        res = {
+            routerOptions: {
+                query: {controller: 'preview', resource: 'preview'}
+            },
+            locals: {
+                apiVersion: 'canary'
+            },
+            render: sinon.spy(),
+            redirect: sinon.spy(),
+            set: sinon.spy()
+        };
 
-            secureStub = sinon.stub();
+        secureStub = sinon.stub();
 
-            sinon.stub(urlUtils, 'redirectToAdmin');
-            sinon.stub(urlUtils, 'redirect301');
-            sinon.stub(urlService, 'getUrlByResourceId');
+        sinon.stub(urlUtils, 'redirectToAdmin');
+        sinon.stub(urlUtils, 'redirect301');
+        sinon.stub(urlService, 'getUrlByResourceId');
 
-            sinon.stub(helpers, 'secure').get(function () {
-                return secureStub;
-            });
-
-            renderStub = sinon.stub();
-            sinon.stub(helpers, 'renderEntry').get(function () {
-                return function () {
-                    return renderStub;
-                };
-            });
-
-            previewStub = sinon.stub();
-            previewStub.withArgs({
-                uuid: req.params.uuid,
-                status: 'all',
-                include: 'authors,tags'
-            }).resolves(apiResponse);
-
-            sinon.stub(api.v2, 'preview').get(() => {
-                return {
-                    read: previewStub
-                };
-            });
+        sinon.stub(helpers, 'secure').get(function () {
+            return secureStub;
         });
 
-        it('should render post', function (done) {
-            controllers.preview(req, res, failTest(done)).then(function () {
-                renderStub.called.should.be.true();
-                secureStub.called.should.be.true();
-                done();
-            }).catch(done);
+        renderStub = sinon.stub();
+        sinon.stub(helpers, 'renderEntry').get(function () {
+            return function () {
+                return renderStub;
+            };
+        });
+
+        previewStub = sinon.stub();
+        previewStub.withArgs({
+            uuid: req.params.uuid,
+            status: 'all',
+            include: 'authors,tags'
+        }).resolves(apiResponse);
+
+        sinon.stub(api, 'preview').get(() => {
+            return {
+                read: previewStub
+            };
         });
     });
 
-    describe('canary', function () {
-        let previewStub;
-
-        beforeEach(function () {
-            post = testUtils.DataGenerator.forKnex.createPost({status: 'draft'});
-
-            apiResponse = {
-                preview: [post]
-            };
-
-            req = {
-                path: '/',
-                params: {
-                    uuid: 'something'
-                },
-                route: {}
-            };
-
-            res = {
-                routerOptions: {
-                    query: {controller: 'preview', resource: 'preview'}
-                },
-                locals: {
-                    apiVersion: 'canary'
-                },
-                render: sinon.spy(),
-                redirect: sinon.spy(),
-                set: sinon.spy()
-            };
-
-            secureStub = sinon.stub();
-
-            sinon.stub(urlUtils, 'redirectToAdmin');
-            sinon.stub(urlUtils, 'redirect301');
-            sinon.stub(urlService, 'getUrlByResourceId');
-
-            sinon.stub(helpers, 'secure').get(function () {
-                return secureStub;
-            });
-
-            renderStub = sinon.stub();
-            sinon.stub(helpers, 'renderEntry').get(function () {
-                return function () {
-                    return renderStub;
-                };
-            });
-
-            previewStub = sinon.stub();
-            previewStub.withArgs({
-                uuid: req.params.uuid,
-                status: 'all',
-                include: 'authors,tags'
-            }).resolves(apiResponse);
-
-            sinon.stub(api.canary, 'preview').get(() => {
-                return {
-                    read: previewStub
-                };
-            });
-        });
-
-        it('should render post', function (done) {
-            controllers.preview(req, res, failTest(done)).then(function () {
-                renderStub.called.should.be.true();
-                secureStub.called.should.be.true();
-                done();
-            }).catch(done);
-        });
-    });
-
-    describe('v3', function () {
-        let previewStub;
-
-        beforeEach(function () {
-            post = testUtils.DataGenerator.forKnex.createPost({status: 'draft'});
-
-            apiResponse = {
-                preview: [post]
-            };
-
-            req = {
-                path: '/',
-                params: {
-                    uuid: 'something'
-                },
-                route: {}
-            };
-
-            res = {
-                routerOptions: {
-                    query: {controller: 'preview', resource: 'preview'}
-                },
-                locals: {
-                    apiVersion: 'v3'
-                },
-                render: sinon.spy(),
-                redirect: sinon.spy(),
-                set: sinon.spy()
-            };
-
-            secureStub = sinon.stub();
-
-            sinon.stub(urlUtils, 'redirectToAdmin');
-            sinon.stub(urlUtils, 'redirect301');
-            sinon.stub(urlService, 'getUrlByResourceId');
-
-            sinon.stub(helpers, 'secure').get(function () {
-                return secureStub;
-            });
-
-            renderStub = sinon.stub();
-            sinon.stub(helpers, 'renderEntry').get(function () {
-                return function () {
-                    return renderStub;
-                };
-            });
-
-            previewStub = sinon.stub();
-            previewStub.withArgs({
-                uuid: req.params.uuid,
-                status: 'all',
-                include: 'authors,tags'
-            }).resolves(apiResponse);
-
-            sinon.stub(api.v3, 'preview').get(() => {
-                return {
-                    read: previewStub
-                };
-            });
-        });
-
-        it('should render post', function (done) {
-            controllers.preview(req, res, failTest(done)).then(function () {
-                renderStub.called.should.be.true();
-                secureStub.called.should.be.true();
-                done();
-            }).catch(done);
-        });
+    it('should render post', function (done) {
+        controllers.preview(req, res, failTest(done)).then(function () {
+            renderStub.called.should.be.true();
+            secureStub.called.should.be.true();
+            done();
+        }).catch(done);
     });
 });
