0999e7ce86360a49cd935805727fba8b513395d3
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-01-22 15:35:08 +0000

    Added fetching of member events to members-activity screen
    refs https://github.com/TryGhost/Team/issues/1277
    
    - added `ember-could-get-used-to-this` dependency to get access to Resources
      - context: https://www.pzuraq.com/introducing-use/
    - added MembersEventFetcher resource for loading members events
      - using a Resource allows for a better data loading experience using only components
      - uses raw data from the API rather than going through Ember Data because we don't need full models or session-length caching that gives us
      - data is kept around in memory for the lifecycle of the resource, if the `filter` param changes the resource is torn down and recreated so it starts from page 1 again, once it's no longer used by a component it's torn down so data isn't kept around
      - paginates by using a `created_at:<={lastSeenTimestamp}` filter - this assumes the API can handle that effectively as a cursor with no duplicate or skipped records
    - updated `members-activity` template to use the new resource and control data loading using infinite scroll
    - moved overall screen structure from the `<MembersActivity::Table>` component into the `members-activity` template so that the table component can stay focused on just the table display

---
+++ ghost/admin/app/helpers/members-event-fetcher.js
@@ -0,0 +1,92 @@
+import moment from 'moment';
+import {Resource} from 'ember-could-get-used-to-this';
+import {TrackedArray} from 'tracked-built-ins';
+import {action} from '@ember/object';
+import {inject as service} from '@ember/service';
+import {task} from 'ember-concurrency-decorators';
+import {timeout} from 'ember-concurrency';
+import {tracked} from '@glimmer/tracking';
+
+export default class MembersEventsFetcher extends Resource {
+    @service ajax;
+    @service ghostPaths;
+
+    @tracked data = new TrackedArray([]);
+    @tracked isLoading = false;
+    @tracked isError = false;
+    @tracked hasReachedEnd = false;
+
+    cursor = null;
+
+    get value() {
+        return {
+            isLoading: this.isLoading,
+            isError: this.isError,
+            data: this.data,
+            loadNextPage: this.loadNextPage,
+            hasReachedEnd: this.hasReachedEnd
+        };
+    }
+
+    async setup() {
+        this.cursor = moment.utc().format('YYYY-MM-DD HH:mm:ss');
+        let filter = `created_at:<='${this.cursor}'`;
+
+        if (this.args.named.filter) {
+            filter += `+${this.args.named.filter}`;
+        }
+
+        return this.loadEventsTask.perform({filter});
+    }
+
+    @action
+    loadNextPage() {
+        // NOTE: assumes data is always ordered by created_at desc
+        const lastEvent = this.data[this.data.length - 1];
+
+        if (!lastEvent?.data?.created_at) {
+            this.hasReachedEnd = true;
+            return;
+        }
+
+        const cursor = moment.utc(lastEvent.data.created_at).format('YYYY-MM-DD HH:mm:ss');
+
+        if (cursor === this.cursor) {
+            this.hasReachedEnd = true;
+            return;
+        }
+
+        this.cursor = cursor;
+        let filter = `created_at:<='${this.cursor}'`;
+
+        if (this.args.named.filter) {
+            filter += `+${this.args.named.filter}`;
+        }
+
+        this.loadEventsTask.perform({filter});
+    }
+
+    @task
+    *loadEventsTask(queryParams) {
+        try {
+            this.isLoading = true;
+
+            const url = this.ghostPaths.url.api('members/events');
+            const data = Object.assign({}, queryParams, {limit: this.args.named.pageSize});
+            const {events} = yield this.ajax.request(url, {data});
+
+            if (events.length < data.limit) {
+                this.hasReachedEnd = true;
+            }
+
+            this.data.push(...events);
+        } catch (e) {
+            this.isError = true;
+            // TODO: expose error message
+            // TODO: log to Sentry
+            console.error(e); // eslint-disable-line
+        } finally {
+            this.isLoading = false;
+        }
+    }
+}
