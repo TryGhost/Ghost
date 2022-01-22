eedfeeb2493f357cb26a4f4d44d996b9f6d87ebc
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-01-22 16:04:54 +0000

    Updated dashboard member activity widget to use fetcher resource
    refs https://github.com/TryGhost/Team/issues/1277
    
    - re-uses same fetcher resource used on member activities screen
      - allows for component to be simplified as it no longer needs to care about handling data loading itself
      - drops use of 1-minute data caching as there is no real need for it in typical usage and can be confusing when the dashboard didn't update as expected
    - exposed error message on `members-event-fetcher` if one is encountered

--- ghost/admin/app/components/dashboard/latest-member-activity.js
+++ ghost/admin/app/components/dashboard/latest-member-activity.js
@@ -1,17 +1,9 @@
 import Component from '@glimmer/component';
 import {inject as service} from '@ember/service';
-import {tracked} from '@glimmer/tracking';
 
 export default class DashboardLatestMemberActivityComponent extends Component {
-    @service dataCache;
-    @service feature;
-    @service membersActivity;
     @service session;
     @service settings;
-
-    @tracked eventsData = null;
-    @tracked eventsError = null;
-    @tracked eventsLoading = false;
 
     get shouldDisplay() {
         const isOwner = this.session.user?.isOwnerOnly;
@@ -22,40 +14,5 @@ export default class DashboardLatestMemberActivityComponent extends Component {
         }
 
         return true;
-    }
-
-    constructor() {
-        super(...arguments);
-
-        if (this.shouldDisplay) {
-            this.loadEvents();
-        }
-    }
-
-    async loadEvents() {
-        const limit = 5;
-        const filter = this.feature.membersActivity ?
-            'type:-[email_delivered_event,email_opened_event,email_failed_event]' :
-            '';
-
-        const dataKey = `dashboard-member-activity::${JSON.stringify({limit, filter})}`;
-
-        if (this.dataCache.get(dataKey)) {
-            this.eventsData = this.dataCache.get(dataKey);
-            return;
-        }
-
-        try {
-            this.eventsLoading = true;
-            const {events} = await this.membersActivity.fetchTask.perform({limit, filter});
-            this.eventsData = events;
-
-            const ONE_MINUTE = 1 * 60 * 1000;
-            this.dataCache.set(dataKey, events, ONE_MINUTE);
-        } catch (error) {
-            this.eventsError = error;
-        } finally {
-            this.eventsLoading = false;
-        }
     }
 }
