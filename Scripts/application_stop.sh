2e62811d7219ff3ffa74d7114fa8db0cb84e1240
OlufM <97524960+OlufM@users.noreply.github.com>
2022-01-25 17:20:29 +0000

    Update application_stop.sh

--- Scripts/application_stop.sh
+++ Scripts/application_stop.sh
@@ -13,7 +13,7 @@ curl -0- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm install node
 
 #create our working directory if it doesn't exist
-DIR="home/ec2-user/Ghost-1
+DIR="home/ec2-user/Ghost
 if [ -d "$DIR" ]; then
  echo "${DIR} exists"
 else
