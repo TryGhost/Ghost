bff126b9fe2d3356fa7c53f5d71d8f4c88a771db
Sanne de Vries <sannedv@protonmail.com>
2022-01-26 14:49:50 +0000

    Updated Author user profile page and sidebar
    No issue
    
    - Authors don't need access to staff page and don't need to see breadcrumbs on their profile page
    - There's enough space in the side navigation for the post views to be visible by default

--- ghost/admin/app/routes/settings/staff/index.js
+++ ghost/admin/app/routes/settings/staff/index.js
@@ -6,6 +6,16 @@ export default class IndexRoute extends AuthenticatedRoute {
     @service infinity;
     @service session;
 
+    beforeModel() {
+        super.beforeModel(...arguments);
+
+        const user = this.session.user;
+
+        if (user.isAuthorOrContributor) {
+            return this.transitionTo('settings.staff.user', user);
+        }
+    }
+
     model() {
         return this.session.user;
     }
