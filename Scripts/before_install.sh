65bb1428c0217325e171be468529b62c632e48b6
OlufM <97524960+OlufM@users.noreply.github.com>
2022-01-30 22:57:45 +0000

    Update before_install.sh

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -4,7 +4,9 @@
 sudo yum install python 3
 sudo yum-config-manager --disable github.com_OlufM_Ghost.git
 sudo yum update
-sudo yum install nodejs -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
 
 # Install nodemon
 # sudo npm install nodemon -g
