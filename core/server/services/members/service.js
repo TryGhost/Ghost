53caac121147db2e10c77a70bd79214d25e548a8
Sam Lord <hello@samlord.me>
2022-02-01 12:00:01 +0000

    Fix usage of "ghostMailer" in email verification trigger
    no issue
    
    Introduced bug when refactoring the verification trigger by referencing an undefined member.

--- core/server/services/members/service.js
+++ core/server/services/members/service.js
@@ -108,7 +108,7 @@ module.exports = {
                 const fromAddress = config.get('user_email');
     
                 if (escalationAddress) {
-                    this._ghostMailer.send({
+                    ghostMailer.send({
                         subject,
                         html: tpl(message, {
                             amountImported,
