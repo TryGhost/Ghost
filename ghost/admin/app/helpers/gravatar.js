c849bea5495a1881a31fc53672bcd478f4ce5167
Gabriel Csapo <gabecsapo@gmail.com>
2022-02-02 02:38:52 -0800

    [chore] runs native classes codemod for apps/helpers (#2241)
    refs https://github.com/TryGhost/Admin/pull/2227
    
    - a continuation of #2227 that runs the native classes codemod against app/helpers

--- ghost/admin/app/helpers/gravatar.js
+++ ghost/admin/app/helpers/gravatar.js
@@ -1,10 +1,13 @@
 import Helper from '@ember/component/helper';
+import classic from 'ember-classic-decorator';
 import md5 from 'blueimp-md5';
 import {isEmpty} from '@ember/utils';
 import {inject as service} from '@ember/service';
 
-export default Helper.extend({
-    config: service(),
+@classic
+export default class Gravatar extends Helper {
+    @service
+    config;
 
     compute([email], {size = 180, d = 'blank'}/*, hash*/) {
         if (!this.get('config.useGravatar')) {
@@ -17,4 +20,4 @@ export default Helper.extend({
 
         return `https://www.gravatar.com/avatar/${md5(email)}?s=${size}&d=${d}`;
     }
-});
+}
