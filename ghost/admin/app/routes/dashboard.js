b5995419dc510e49759b389423eed0cf7e71c558
Peter Zimon <peter.zimon@gmail.com>
2022-02-02 10:12:16 +0100

    Updated default redirect for contributors

--- ghost/admin/app/routes/dashboard.js
+++ ghost/admin/app/routes/dashboard.js
@@ -4,7 +4,9 @@ export default class DashboardRoute extends AuthenticatedRoute {
     beforeModel() {
         super.beforeModel(...arguments);
 
-        if (!this.session.user.isAdmin) {
+        if (this.session.user.isContributor) {
+            return this.transitionTo('posts');
+        } else if (!this.session.user.isAdmin) {
             return this.transitionTo('site');
         }
     }
