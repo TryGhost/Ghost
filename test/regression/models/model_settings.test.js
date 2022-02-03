be9589657badc287db1b523ce1a262c275369ec2
Rishabh <zrishabhgarg@gmail.com>
2022-02-03 15:13:50 +0530

    Updated tests for default content visibility
    refs https://github.com/TryGhost/Team/issues/1071

--- test/regression/models/model_settings.test.js
+++ test/regression/models/model_settings.test.js
@@ -17,7 +17,7 @@ describe('Settings Model', function () {
             await models.Settings.populateDefaults();
 
             const settingsPopulated = await models.Settings.findAll();
-            settingsPopulated.length.should.equal(94);
+            settingsPopulated.length.should.equal(95);
         });
 
         it('doesn\'t overwrite any existing settings', async function () {
