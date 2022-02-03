40e9789913210d51b74d3f12fa516103d1b02eb1
Thibaut Patel <thibaut@ghost.org>
2022-02-03 09:03:59 +0100

    Added a `theme` parameter to the `/authentication/setup` route
    refs https://github.com/TryGhost/Team/issues/1296
    
    - The `theme` must be a github `org/repo` string
    - This uses the internal API instead of the services because the API has extra implementation details not present in the services.

--- core/server/services/auth/setup.js
+++ core/server/services/auth/setup.js
@@ -149,11 +149,47 @@ function sendWelcomeEmail(email, mailAPI) {
     return Promise.resolve();
 }
 
+async function installTheme(data, api) {
+    const {theme: themeName} = data.userData;
+
+    if (!themeName) {
+        return data;
+    }
+
+    // Use the api instead of the services as the api performs extra logic
+    try {
+        const installResults = await api.themes.install({
+            source: 'github',
+            ref: themeName,
+            context: {internal: true}
+        });
+        const theme = installResults.themes[0];
+
+        await api.themes.activate({
+            name: theme.name,
+            context: {internal: true}
+        });
+    } catch (e) {
+        //Fallback to Casper by doing nothing as the theme setting update is the last step
+
+        await api.notifications.add({
+            notifications: [{
+                custom: true, //avoids update-check from deleting the notification
+                type: 'warn',
+                message: 'The installation of the theme you have selected wasn\'t successful.'
+            }]
+        }, {context: {internal: true}});
+    }
+
+    return data;
+}
+
 module.exports = {
     checkIsSetup: checkIsSetup,
     assertSetupCompleted: assertSetupCompleted,
     setupUser: setupUser,
     doSettings: doSettings,
     doProduct: doProduct,
-    sendWelcomeEmail: sendWelcomeEmail
+    sendWelcomeEmail: sendWelcomeEmail,
+    installTheme: installTheme
 };
