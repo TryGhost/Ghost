2525a43156c9b83e2fb527339028ad85aea5d0ed
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-02-04 17:06:40 +0000

    Changed firstStart flow to show a streamlined settings screen
    closes https://github.com/TryGhost/Team/issues/1310
    
    - added `/setup/finishing-touches` route
      - refreshes theme preview on access
      - uses existing setting form components to build a reduced settings menu
    - updated `/?firstStart=true` handling to transition to `/setup/finishing-touches` instead of showing the get-started modal
    - updated standard setup flow to show the get-started modal upon completion
      - `/?firstStart=true` flow for now will only be used when a theme has been chosen before site creation

--- ghost/admin/app/router.js
+++ ghost/admin/app/router.js
@@ -15,6 +15,7 @@ Router.map(function () {
         this.route('two');
         this.route('three');
     });
+    this.route('setup.finishing-touches', {path: '/setup/finishing-touches'});
 
     this.route('signin');
     this.route('signout');
