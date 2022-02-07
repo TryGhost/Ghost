97a8b0f337103b6212f8035f76d8ba00b6a27640
Daniel Lockyer <hi@daniellockyer.com>
2022-02-07 16:46:35 +0100

    Restored quick database reset method via table truncation
    refs https://github.com/TryGhost/Ghost/commit/2a7ee4cd81249151cc52192e9176fb8905c5e2d6
    
    - in the referenced commit, I made a util to speed up resetting the DB
      for SQLite by copying the database file
    - I inadvertently removed an optimization we had before - where we
      truncate the tables and insert the fixtures instead of dropping the
      entire database
    - this would be missing on MySQL tests
    - this seems to have a big difference so this commit re-adds the
      optimization in

--- test/utils/e2e-utils.js
+++ test/utils/e2e-utils.js
@@ -105,7 +105,7 @@ const restartModeGhostStart = async ({frontend}) => {
     // TODO: figure out why we need this if we reset again later?
     urlServiceUtils.reset();
 
-    await dbUtils.reset();
+    await dbUtils.reset({truncate: true});
 
     debug('init done');
 
