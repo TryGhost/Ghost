0458e0b48334180614ee207549dbf968d588fc7a
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-02-07 11:11:19 +0000

    Updated conditionals for editorIsLaunchComplete with improvedOnboarding feature
    closes https://github.com/TryGhost/Team/issues/1294
    
    - ensures latest member activity is shown when improvedOnboarding feature is enabled no matter what the `editorIsLaunchComplete` value is to better show what the dashboard will look like when the launch wizard is removed

--- ghost/admin/app/components/dashboard/latest-member-activity.js
+++ ghost/admin/app/components/dashboard/latest-member-activity.js
@@ -2,10 +2,15 @@ import Component from '@glimmer/component';
 import {inject as service} from '@ember/service';
 
 export default class DashboardLatestMemberActivityComponent extends Component {
+    @service feature;
     @service session;
     @service settings;
 
     get shouldDisplay() {
+        if (this.feature.improvedOnboarding) {
+            return true;
+        }
+
         const isOwner = this.session.user?.isOwnerOnly;
         const hasCompletedLaunchWizard = this.settings.get('editorIsLaunchComplete');
 
