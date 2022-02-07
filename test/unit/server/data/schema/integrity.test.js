479df36a48183e6d23df125ca7e49c828cf5eccf
Thibaut Patel <thibaut@ghost.org>
2022-02-07 16:04:38 +0100

    Added the `last_seen_at` column to members
    refs https://github.com/TryGhost/Team/issues/1304
    
    - This migration adds a column to store when a members was last seen
    - The utils.js e2e test file was refactored according to the commit https://github.com/TryGhost/Ghost/commit/06dd9bac5957d4cd3415a3086c275b2a7501fe3e

--- test/unit/server/data/schema/integrity.test.js
+++ test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '9e4eeb5c260047fe21d1f93c523bd771';
+    const currentSchemaHash = 'c8442427463a0bf25ecca871b01016dc';
     const currentFixturesHash = 'beb040c0376a492c2a44767fdd825a3e';
     const currentSettingsHash = '437d4c6da8759f5c35f11f811b86e5bc';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
