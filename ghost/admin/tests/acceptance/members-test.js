8d0b3b895815a8f4244e9dd445bf8186b2bd965d
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-02-08 11:48:55 +0000

    Cleaned up members filter labs components
    no issue
    
    - the members filtering is out of labs, we don't need to keep the labs component naming around
    - renamed `-labs` components/classes to non-labs naming, replacing old and unused non-labs components where necessary

--- ghost/admin/tests/acceptance/members-test.js
+++ ghost/admin/tests/acceptance/members-test.js
@@ -157,7 +157,7 @@ describe('Acceptance: Members', function () {
 
             // a filter is needed for the delete-selected button to show
             await click('[data-test-button="members-filter-actions"]');
-            await click('.gh-member-label-input-labs input');
+            await click('.gh-member-label-input input');
             await click(`[data-test-label-filter="${label.name}"]`);
             await click(`[data-test-button="members-apply-filter"]`);
 
