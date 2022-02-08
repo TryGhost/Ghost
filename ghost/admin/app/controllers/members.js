386c72d4660f2ed5eb1046e59abc6615481fb3b8
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-02-08 11:36:49 +0000

    🐛 Fixed "Show all members" button not clearing search field when no results match
    no issue
    
    - the reset button was only clearing the `filter` param rather than resetting all query params
    - removed unnecessary `searchText` indirection, the search input can work directly with the query param

--- ghost/admin/app/controllers/members.js
+++ ghost/admin/app/controllers/members.js
@@ -45,7 +45,6 @@ export default class MembersController extends Controller {
     ];
 
     @tracked members = A([]);
-    @tracked searchText = '';
     @tracked searchParam = '';
     @tracked filterParam = null;
     @tracked softFilterParam = null;
@@ -77,13 +76,13 @@ export default class MembersController extends Controller {
     // Computed properties -----------------------------------------------------
 
     get listHeader() {
-        let {searchText, selectedLabel, members} = this;
+        let {searchParam, selectedLabel, members} = this;
 
         if (members.loading) {
             return 'Loading...';
         }
 
-        if (searchText) {
+        if (searchParam) {
             return 'Search result';
         }
 
@@ -369,10 +368,6 @@ export default class MembersController extends Controller {
         // params is undefined when called as a "refresh" of the model
         let {label, paidParam, searchParam, orderParam, filterParam} = typeof params === 'undefined' ? this : params;
 
-        if (!searchParam) {
-            this.resetSearch();
-        }
-
         // use a fixed created_at date so that subsequent pages have a consistent index
         let startDate = new Date();
 
@@ -536,10 +531,6 @@ export default class MembersController extends Controller {
         return response?.bulk?.meta;
     }
     // Internal ----------------------------------------------------------------
-
-    resetSearch() {
-        this.searchText = '';
-    }
 
     resetFilters(params) {
         if (!params?.filterParam) {
