678254c2bf402dffe79153508f940aad0c1291f1
Fabien 'egg' O'Carroll <fabien@allou.is>
2022-02-09 11:04:58 +0200

    Added validation for Tier prices
    refs https://github.com/TryGhost/Team/issues/1319
    refs https://github.com/TryGhost/Team/issues/521
    
    This updates the members-api to explicitly validate Tier prices rather
    than deferring to Stripe and using whichever errors they throw.

---
+++ test/e2e-api/admin/products.test.js
@@ -0,0 +1,69 @@
+const should = require('should');
+const sinon = require('sinon');
+const labs = require('../../../core/shared/labs');
+
+const {agentProvider, fixtureManager} = require('../../utils/e2e-framework');
+
+describe('Tiers API', function () {
+    let agent;
+
+    before(async function () {
+        agent = await agentProvider.getAdminAPIAgent();
+        await fixtureManager.init('members');
+        await agent.loginAsOwner();
+    });
+
+    beforeEach(function () {
+        sinon.stub(labs, 'isSet').withArgs('multipleProducts').returns(false);
+        const stripeService = require('../../../core/server/services/stripe');
+        stripeService.api._configured = true;
+    });
+
+    afterEach(function () {
+        sinon.restore();
+        const stripeService = require('../../../core/server/services/stripe');
+        stripeService.api._configured = false;
+    });
+
+    it('Errors when price is non-integer', async function () {
+        const tier = {
+            name: 'Blah',
+            monthly_price: {
+                amount: 99.99
+            }
+        };
+
+        await agent
+            .post('/products/')
+            .body({products: [tier]})
+            .expectStatus(422);
+    });
+
+    it('Errors when price is negative', async function () {
+        const tier = {
+            name: 'Blah',
+            monthly_price: {
+                amount: -100
+            }
+        };
+
+        await agent
+            .post('/products/')
+            .body({products: [tier]})
+            .expectStatus(422);
+    });
+
+    it('Errors when price is too large', async function () {
+        const tier = {
+            name: 'Blah',
+            monthly_price: {
+                amount: Number.MAX_SAFE_INTEGER
+            }
+        };
+
+        await agent
+            .post('/products/')
+            .body({products: [tier]})
+            .expectStatus(422);
+    });
+});
