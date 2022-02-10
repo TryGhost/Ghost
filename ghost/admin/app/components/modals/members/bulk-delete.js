f4c6555ccf7bc080e1480fc6c2bdf00cda7c7627
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-02-10 18:37:31 +0000

    Fixed linter error

--- ghost/admin/app/components/modals/members/bulk-delete.js
+++ ghost/admin/app/components/modals/members/bulk-delete.js
@@ -1,11 +1,11 @@
 import Component from '@glimmer/component';
+import config from 'ghost-admin/config/environment';
 import fetch from 'fetch';
 import moment from 'moment';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
 import {task} from 'ember-concurrency';
 import {tracked} from '@glimmer/tracking';
-import config from 'ghost-admin/config/environment';
 
 export default class MembersBulkDeleteModal extends Component {
     @service ajax;
