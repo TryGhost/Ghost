e9bc18417fdb36101e9ad0c554dc9a40066a0b6e
Rishabh <zrishabhgarg@gmail.com>
2022-02-15 13:13:07 +0530

    Added free member signup flow tests for multiple tiers
    closes https://github.com/TryGhost/Team/issues/1345
    
    - adds free member signup tests for sites with multiple tiers
    - updates test fixtures

--- ghost/portal/src/tests/SignupFlow.test.js
+++ ghost/portal/src/tests/SignupFlow.test.js
@@ -51,14 +51,63 @@ const setup = async ({site, member = null}) => {
     };
 };
 
-describe('App with tiers disabled', () => {
+const multiTierSetup = async ({site, member = null}) => {
+    const ghostApi = setupGhostApi({siteUrl: 'https://example.com'});
+    ghostApi.init = jest.fn(() => {
+        return Promise.resolve({
+            site,
+            member
+        });
+    });
+
+    ghostApi.member.sendMagicLink = jest.fn(() => {
+        return Promise.resolve('success');
+    });
+
+    const utils = appRender(
+        <App api={ghostApi} />
+    );
+    const freeTierDescription = site.products?.find(p => p.type === 'free')?.description;
+    const triggerButtonFrame = await utils.findByTitle(/portal-trigger/i);
+    const popupFrame = utils.queryByTitle(/portal-popup/i);
+    const popupIframeDocument = popupFrame.contentDocument;
+    const emailInput = within(popupIframeDocument).queryByLabelText(/email/i);
+    const nameInput = within(popupIframeDocument).queryByLabelText(/name/i);
+    const submitButton = within(popupIframeDocument).queryByRole('button', {name: 'Continue'});
+    const signinButton = within(popupIframeDocument).queryByRole('button', {name: 'Sign in'});
+    const siteTitle = within(popupIframeDocument).queryByText(site.title);
+    const freePlanTitle = within(popupIframeDocument).queryAllByText(/free$/i);
+    const freePlanDescription = within(popupIframeDocument).queryAllByText(freeTierDescription);
+    const monthlyPlanTitle = within(popupIframeDocument).queryByText('Monthly');
+    const yearlyPlanTitle = within(popupIframeDocument).queryByText('Yearly');
+    const fullAccessTitle = within(popupIframeDocument).queryByText('Full access');
+    return {
+        ghostApi,
+        popupIframeDocument,
+        popupFrame,
+        triggerButtonFrame,
+        siteTitle,
+        emailInput,
+        nameInput,
+        signinButton,
+        submitButton,
+        freePlanTitle,
+        monthlyPlanTitle,
+        yearlyPlanTitle,
+        fullAccessTitle,
+        freePlanDescription,
+        ...utils
+    };
+};
+
+describe('Single tier site', () => {
     describe('Signup page', () => {
-        test('renders basic signup page', async () => {
+        test('renders with default settings', async () => {
             const {
                 ghostApi, popupFrame, triggerButtonFrame, emailInput, nameInput, signinButton, submitButton,
                 siteTitle, popupIframeDocument, freePlanTitle, monthlyPlanTitle, yearlyPlanTitle, fullAccessTitle
             } = await setup({
-                site: FixtureSite.tiersDisabled.basic
+                site: FixtureSite.singleTier.basic
             });
 
             expect(popupFrame).toBeInTheDocument();
@@ -88,12 +137,12 @@ describe('App with tiers disabled', () => {
             expect(magicLink).toBeInTheDocument();
         });
 
-        test('renders signup page without name', async () => {
+        test('renders without portal name', async () => {
             const {
                 ghostApi, popupFrame, triggerButtonFrame, emailInput, nameInput, signinButton, submitButton,
                 siteTitle, popupIframeDocument, freePlanTitle, monthlyPlanTitle, yearlyPlanTitle, fullAccessTitle
             } = await setup({
-                site: FixtureSite.tiersDisabled.withoutName
+                site: FixtureSite.singleTier.withoutName
             });
 
             expect(popupFrame).toBeInTheDocument();
@@ -124,12 +173,12 @@ describe('App with tiers disabled', () => {
             expect(magicLink).toBeInTheDocument();
         });
 
-        test('renders signup page with only free plan', async () => {
+        test('renders with only free plan', async () => {
             let {
                 ghostApi, popupFrame, triggerButtonFrame, emailInput, nameInput, signinButton, submitButton,
                 siteTitle, popupIframeDocument, freePlanTitle, monthlyPlanTitle, yearlyPlanTitle, fullAccessTitle
             } = await setup({
-                site: FixtureSite.tiersDisabled.onlyFreePlan
+                site: FixtureSite.singleTier.onlyFreePlan
             });
 
             expect(popupFrame).toBeInTheDocument();
@@ -141,6 +190,111 @@ describe('App with tiers disabled', () => {
             expect(monthlyPlanTitle).not.toBeInTheDocument();
             expect(yearlyPlanTitle).not.toBeInTheDocument();
             expect(fullAccessTitle).not.toBeInTheDocument();
+            expect(signinButton).toBeInTheDocument();
+            expect(submitButton).not.toBeInTheDocument();
+            submitButton = within(popupIframeDocument).queryByRole('button', {name: 'Sign up'});
+
+            fireEvent.change(emailInput, {target: {value: 'jamie@example.com'}});
+            fireEvent.change(nameInput, {target: {value: 'Jamie Larsen'}});
+
+            expect(emailInput).toHaveValue('jamie@example.com');
+            expect(nameInput).toHaveValue('Jamie Larsen');
+            fireEvent.click(submitButton);
+
+            expect(ghostApi.member.sendMagicLink).toHaveBeenLastCalledWith({
+                email: 'jamie@example.com',
+                name: 'Jamie Larsen',
+                plan: 'free'
+            });
+
+            // Check if magic link page is shown
+            const magicLink = await within(popupIframeDocument).findByText(/now check your email/i);
+            expect(magicLink).toBeInTheDocument();
+        });
+    });
+});
+
+describe('Multiple tiers site', () => {
+    describe('Signup page', () => {
+        test('renders with default settings', async () => {
+            const {
+                ghostApi, popupFrame, triggerButtonFrame, emailInput, nameInput, signinButton, submitButton,
+                siteTitle, popupIframeDocument, freePlanTitle
+            } = await multiTierSetup({
+                site: FixtureSite.multipleTiers.basic
+            });
+
+            expect(popupFrame).toBeInTheDocument();
+            expect(triggerButtonFrame).toBeInTheDocument();
+            expect(siteTitle).toBeInTheDocument();
+            expect(emailInput).toBeInTheDocument();
+            expect(nameInput).toBeInTheDocument();
+            expect(freePlanTitle[0]).toBeInTheDocument();
+            expect(signinButton).toBeInTheDocument();
+            expect(submitButton).toBeInTheDocument();
+
+            fireEvent.change(nameInput, {target: {value: 'Jamie Larsen'}});
+            fireEvent.change(emailInput, {target: {value: 'jamie@example.com'}});
+
+            expect(emailInput).toHaveValue('jamie@example.com');
+            expect(nameInput).toHaveValue('Jamie Larsen');
+            fireEvent.click(submitButton);
+            expect(ghostApi.member.sendMagicLink).toHaveBeenLastCalledWith({
+                email: 'jamie@example.com',
+                name: 'Jamie Larsen',
+                plan: 'free'
+            });
+            const magicLink = await within(popupIframeDocument).findByText(/now check your email/i);
+            expect(magicLink).toBeInTheDocument();
+        });
+
+        test('renders without portal name', async () => {
+            const {
+                ghostApi, popupFrame, triggerButtonFrame, emailInput, nameInput, signinButton, submitButton,
+                siteTitle, popupIframeDocument, freePlanTitle
+            } = await multiTierSetup({
+                site: FixtureSite.multipleTiers.withoutName
+            });
+
+            expect(popupFrame).toBeInTheDocument();
+            expect(triggerButtonFrame).toBeInTheDocument();
+            expect(siteTitle).toBeInTheDocument();
+            expect(emailInput).toBeInTheDocument();
+            expect(nameInput).not.toBeInTheDocument();
+            expect(freePlanTitle[0]).toBeInTheDocument();
+            expect(signinButton).toBeInTheDocument();
+            expect(submitButton).toBeInTheDocument();
+
+            fireEvent.change(emailInput, {target: {value: 'jamie@example.com'}});
+
+            expect(emailInput).toHaveValue('jamie@example.com');
+            fireEvent.click(submitButton);
+
+            expect(ghostApi.member.sendMagicLink).toHaveBeenLastCalledWith({
+                email: 'jamie@example.com',
+                name: '',
+                plan: 'free'
+            });
+
+            // Check if magic link page is shown
+            const magicLink = await within(popupIframeDocument).findByText(/now check your email/i);
+            expect(magicLink).toBeInTheDocument();
+        });
+
+        test('renders with only free plan', async () => {
+            let {
+                ghostApi, popupFrame, triggerButtonFrame, emailInput, nameInput, signinButton, submitButton,
+                siteTitle, popupIframeDocument, freePlanTitle
+            } = await multiTierSetup({
+                site: FixtureSite.multipleTiers.onlyFreePlan
+            });
+
+            expect(popupFrame).toBeInTheDocument();
+            expect(triggerButtonFrame).toBeInTheDocument();
+            expect(siteTitle).toBeInTheDocument();
+            expect(emailInput).toBeInTheDocument();
+            expect(nameInput).toBeInTheDocument();
+            expect(freePlanTitle.length).toBe(0);
             expect(signinButton).toBeInTheDocument();
             expect(submitButton).not.toBeInTheDocument();
             submitButton = within(popupIframeDocument).queryByRole('button', {name: 'Sign up'});
