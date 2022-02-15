c77da00f02052f73019120b32f54300f63d24b45
Hannah Wolfe <github.erisds@gmail.com>
2022-02-15 12:03:44 +0000

    Removed @babel/eslint-parser, set ecmaVersion instead
    refs: https://github.com/TryGhost/Members/commit/e548d1789ca76a0a3a75824ed3150726dc51e016
    
    - @babel/eslint-parser was added to enable support for static members on classes but this causes other problems as it fails on eslint ugprades etc
    - a better solution is to set ecmaVersion to 2022, as our default is 2018 which does not support static, yet 2022 does

--- ghost/stripe/.eslintrc.js
+++ ghost/stripe/.eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
-    parser: '@babel/eslint-parser',
     parserOptions: {
-        requireConfigFile: false
+        ecmaVersion: 2022
     },
     plugins: ['ghost'],
     extends: [
