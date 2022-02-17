daa39bf4fe8f0af9f74ca11d4d1d9ac93fa82922
Fabien 'egg' O'Carroll <fabien@allou.is>
2022-02-17 14:51:40 +0200

    🐛 Fixed error handling webhooks for unknown member (#14155)
    https://github.com/TryGhost/Team/issues/1374
    
    When we receive a web hook to update payment details for a customer
    which is not associated with a Member, instead of proceeding and erroring
    we now return early and respond to Stripe with a 200. A test has been 
    added to ensure this

--- test/e2e-api/members/webhooks.test.js
+++ test/e2e-api/members/webhooks.test.js
@@ -162,6 +162,109 @@ describe('Members API', function () {
                     to: 'checkout-webhook-test@email.com'
                 });
             });
+
+            it('Does not 500 if the member is unknown', async function () {
+                const setupIntent = {
+                    id: 'setup_intent_456',
+                    payment_method: 'card_456',
+                    metadata: {
+                        customer_id: 'cus_456',
+                        subscription_id: 'sub_456'
+                    }
+                };
+
+                const paymentMethod = {
+                    id: 'card_456'
+                };
+
+                const webhookPayload = JSON.stringify({
+                    type: 'checkout.session.completed',
+                    data: {
+                        object: {
+                            mode: 'setup',
+                            customer: 'cus_456',
+                            setup_intent: 'setup_intent_456'
+                        }
+                    }
+                });
+
+                const webhookSignature = stripe.webhooks.generateTestHeaderString({
+                    payload: webhookPayload,
+                    secret: process.env.WEBHOOK_SECRET
+                });
+
+                const subscription = {
+                    id: 'sub_456',
+                    customer: 'cus_456',
+                    status: 'active',
+                    items: {
+                        type: 'list',
+                        data: [{
+                            id: 'item_456',
+                            price: {
+                                id: 'price_456',
+                                product: 'product_456',
+                                active: true,
+                                nickname: 'Monthly',
+                                currency: 'USD',
+                                recurring: {
+                                    interval: 'month'
+                                },
+                                unit_amount: 500,
+                                type: 'recurring'
+                            }
+                        }]
+                    },
+                    start_date: Date.now() / 1000,
+                    current_period_end: Date.now() / 1000 + (60 * 60 * 24 * 31),
+                    cancel_at_period_end: false
+                };
+
+                nock('https://api.stripe.com')
+                    .persist()
+                    .get(/v1\/.*/)
+                    .reply((uri, body) => {
+                        const [match, resource, id] = uri.match(/\/?v1\/(\w+)\/?(\w+)/) || [null];
+
+                        if (!match) {
+                            return [500];
+                        }
+
+                        if (resource === 'setup_intents') {
+                            return [200, setupIntent];
+                        }
+
+                        if (resource === 'subscriptions') {
+                            return [200, subscription];
+                        }
+                    });
+
+                nock('https://api.stripe.com')
+                    .persist()
+                    .post(/v1\/.*/)
+                    .reply((uri, body) => {
+                        const [match, resource, id, action] = uri.match(/\/?v1\/(\w+)(?:\/?(\w+)){0,2}/) || [null];
+
+                        if (!match) {
+                            return [500];
+                        }
+
+                        if (resource === 'payment_methods') {
+                            return [200, paymentMethod];
+                        }
+
+                        if (resource === 'subscriptions') {
+                            return [200, subscription];
+                        }
+
+                        return [500];
+                    });
+
+                await membersAgent.post('/webhooks/stripe/')
+                    .body(webhookPayload)
+                    .header('stripe-signature', webhookSignature)
+                    .expectStatus(200);
+            });
         });
     });
 });
