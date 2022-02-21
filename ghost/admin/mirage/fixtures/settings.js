52f230b11572054a2eb2f0146aecf80cef3f2a48
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-02-21 14:31:19 +0000

    🐛 Fixed "Enable email open-rate" toggle
    closes https://github.com/TryGhost/Team/issues/1381
    refs https://github.com/TryGhost/Admin/commit/d0b197eb848770579f2bcd17872fb90814123943
    
    - when the email newsletter settings was refactored, the `emailTrackOpens: reads('settings.emailTrackOpens')` was removed as it's a confusing redirection but it's usage in the toggle action was missed

--- ghost/admin/mirage/fixtures/settings.js
+++ ghost/admin/mirage/fixtures/settings.js
@@ -222,5 +222,45 @@ export default [
         updated_at: '2021-11-01T15:44:43.494Z',
         updated_by: 1,
         value: 'casper'
+    },
+    {
+        id: 28,
+        created_at: '2022-02-16T09:38:00.000Z',
+        created_by: 1,
+        key: 'members_signup_access',
+        group: 'members',
+        updated_at: '2022-02-16T09:38:00.000Z',
+        updated_by: 1,
+        value: 'all'
+    },
+    {
+        id: 29,
+        created_at: '2022-02-16T09:38:00.000Z',
+        created_by: 1,
+        key: 'default_content_visibility',
+        group: 'members',
+        updated_at: '2022-02-16T09:38:00.000Z',
+        updated_by: 1,
+        value: 'public'
+    },
+    {
+        id: 30,
+        created_at: '2022-02-16T09:38:00.000Z',
+        created_by: 1,
+        key: 'default_content_visibility_tiers',
+        group: 'members',
+        updated_at: '2022-02-16T09:38:00.000Z',
+        updated_by: 1,
+        value: '[]'
+    },
+    {
+        id: 31,
+        created_at: '2022-02-21T13:47:00.000Z',
+        created_by: 1,
+        key: 'email_track_opens',
+        group: 'email',
+        updated_at: '2022-02-21T13:47:00.000Z',
+        updated_by: 1,
+        value: 'true'
     }
 ];
