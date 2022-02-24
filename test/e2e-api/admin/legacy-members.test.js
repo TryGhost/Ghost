535e3ab1cf3760c8ba57da3c133b00aa50d44419
Thibaut Patel <thibaut@ghost.org>
2022-02-24 17:40:31 +0100

    Enabled the last_seen_at flag in the legacy member tests
    refs https://github.com/TryGhost/Team/issues/1393
    
    - Added the `last_seen_at` property back in the api answers. The property was removed in https://github.com/TryGhost/Ghost/commit/d3de7d090bab62c7747f3b35834b61c2d6f0a7b1 without updating the member legacy snapshots

--- test/e2e-api/admin/legacy-members.test.js
+++ test/e2e-api/admin/legacy-members.test.js
@@ -14,6 +14,7 @@ describe('Legacy Members API', function () {
 
     beforeEach(function () {
         mockManager.mockLabsDisabled('multipleProducts');
+        mockManager.mockLabsEnabled('membersLastSeenFilter');
     });
 
     afterEach(function () {
