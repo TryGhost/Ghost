e7b284727bec685a7cd1b9d2eb4c93ef18522969
Thibaut Patel <thibaut@ghost.org>
2022-02-28 14:36:58 +0100

    Moved from a floating 24h window to a UTC-aligned window
    refs https://github.com/TryGhost/Ghost/pull/14197
    
    - Moved from updating the last_seen_at value "at most every 24h" to "at most every UTC day".
    - It will simplify explaining the following behavior: a publication is set in UTC-10, a user visits at 2pm on Monday and at 1pm on Tuesday, the last_seen_at value is still Monday.
    - There is no way to go around the above issue due to the technical constraint of updating the `last_seen_at` value at most once a day.
    - This might create database write spikes at midnight UTC

--- ghost/members-events-service/test/last-seen-at-updater.test.js
+++ ghost/members-events-service/test/last-seen-at-updater.test.js
@@ -4,49 +4,51 @@ require('./utils');
 
 const assert = require('assert');
 const sinon = require('sinon');
-const LastSeenAtUpdater = require('../lib/last-seen-at-updater');
+const {LastSeenAtUpdater} = require('../');
 const DomainEvents = require('@tryghost/domain-events');
 const {MemberPageViewEvent, MemberSubscribeEvent} = require('@tryghost/member-events');
+const moment = require('moment');
 
 describe('LastSeenAtUpdater', function () {
     it('Fires on MemberPageViewEvent events', async function () {
-        const now = new Date();
-        const previousLastSeen = new Date(now.getTime() - 48 * 3600 * 1000).toISOString(); // 48 hours
+        const now = moment('2022-02-28T18:00:00Z').utc();
+        const previousLastSeen = moment('2022-02-27T23:00:00Z').toISOString();
         const spy = sinon.spy();
         new LastSeenAtUpdater({
             memberModel: {
                 update: spy
             }
         });
-        DomainEvents.dispatch(MemberPageViewEvent.create({memberId: '1', memberLastSeenAt: previousLastSeen, url: '/'}, now));
+        DomainEvents.dispatch(MemberPageViewEvent.create({memberId: '1', memberLastSeenAt: previousLastSeen, url: '/'}, now.toDate()));
         assert(spy.calledOnceWithExactly({
-            last_seen_at: now.toISOString().replace('T', ' ').replace(/\..+/, '')
+            last_seen_at: now.format('YYYY-MM-DD HH:mm:ss')
         }, {
             id: '1'
         }), 'The LastSeenAtUpdater should attempt a member update with the current date.');
     });
 
     it('Doesn\'t update when last_seen_at is too recent', async function () {
-        const now = new Date();
-        const previousLastSeen = new Date(now.getTime() - 1 * 3600 * 1000).toISOString(); // 1 hour
+        const now = moment('2022-02-28T18:00:00Z');
+        const previousLastSeen = moment('2022-02-28T00:00:00Z').toISOString();
         const spy = sinon.spy();
         new LastSeenAtUpdater({
             memberModel: {
                 update: spy
             }
         });
-        DomainEvents.dispatch(MemberPageViewEvent.create({memberId: '1', memberLastSeenAt: previousLastSeen, url: '/'}, now));
+        DomainEvents.dispatch(MemberPageViewEvent.create({memberId: '1', memberLastSeenAt: previousLastSeen, url: '/'}, now.toDate()));
         assert(spy.notCalled, 'The LastSeenAtUpdater should\'t update a member when the previous last_seen_at is close to the event timestamp.');
     });
 
     it('Doesn\'t fire on other events', async function () {
+        const now = moment('2022-02-28T18:00:00Z');
         const spy = sinon.spy();
         new LastSeenAtUpdater({
             memberModel: {
                 update: spy
             }
         });
-        DomainEvents.dispatch(MemberSubscribeEvent.create({memberId: '1', source: 'api'}, new Date()));
+        DomainEvents.dispatch(MemberSubscribeEvent.create({memberId: '1', source: 'api'}, now.toDate()));
         assert(spy.notCalled, 'The LastSeenAtUpdater should never fire on MemberPageViewEvent events.');
     });
 });
