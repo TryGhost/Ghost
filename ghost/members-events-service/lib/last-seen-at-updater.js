b7e503df62d3912869cabdf27d62075a2e115b50
Thibaut Patel <thibaut@ghost.org>
2022-02-28 14:36:58 +0100

    Moved from a floating 24h window to a UTC-aligned window
    refs https://github.com/TryGhost/Ghost/pull/14197
    
    - Moved from updating the last_seen_at value "at most every 24h" to "at most every UTC day".
    - It will simplify explaining the following behavior: a publication is set in UTC-10, a user visits at 2pm on Monday and at 1pm on Tuesday, the last_seen_at value is still Monday.
    - There is no way to go around the above issue due to the technical constraint of updating the `last_seen_at` value at most once a day.
    - This might create database write spikes at midnight UTC

--- ghost/members-events-service/lib/last-seen-at-updater.js
+++ ghost/members-events-service/lib/last-seen-at-updater.js
@@ -19,15 +19,18 @@ class LastSeenAtUpdater {
     }
 
     /**
-     * Updates the member.last_seen_at field if it was updated more than 24 hours ago
+     * Updates the member.last_seen_at field if it wasn't updated in the current UTC day yet
+     * Example: current time is 2022-02-28 18:00:00 UTC
+     * - memberLastSeenAt is 2022-02-27 23:00:00 UTC, timestamp is current time, then `last_seen_at` is set to the current time
+     * - memberLastSeenAt is 2022-02-28 01:00:00 UTC, timestamp is current time, then `last_seen_at` isn't changed
      * @param {string} memberId The id of the member to be udpated
      * @param {string} memberLastSeenAt The previous last_seen_at property value for the current member
      * @param {Date} timestamp The event timestamp
      */
     async updateLastSeenAt(memberId, memberLastSeenAt, timestamp) {
-        if (memberLastSeenAt === null || moment(timestamp).diff(memberLastSeenAt, 'hours') > 24) {
+        if (memberLastSeenAt === null || moment(moment.utc(timestamp).startOf('day')).isAfter(moment.utc(memberLastSeenAt).startOf('day'))) {
             await this._memberModel.update({
-                last_seen_at: moment(timestamp).utc().format('YYYY-MM-DD HH:mm:ss')
+                last_seen_at: moment.utc(timestamp).format('YYYY-MM-DD HH:mm:ss')
             }, {
                 id: memberId
             });
