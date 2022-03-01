e002096997d1e44f6c42e57455c012865322e42b
Thibaut Patel <thibaut@ghost.org>
2022-03-01 10:28:45 +0100

    Moved the last-seen-at-updater to use the publication timezone
    refs https://github.com/TryGhost/Team/issues/1306
    
    - This removes the limitation described in commit https://github.com/TryGhost/Members/pull/370/commits/ff46449ad6069b53cef85a8c2f78daed672668ed
    - The only edge case is that when a publication changes their timezone, it will have maximum 24 hours where the member last_seen_at could be incorrect

--- ghost/members-events-service/test/last-seen-at-updater.test.js
+++ ghost/members-events-service/test/last-seen-at-updater.test.js
@@ -14,9 +14,63 @@ describe('LastSeenAtUpdater', function () {
         const now = moment('2022-02-28T18:00:00Z').utc();
         const previousLastSeen = moment('2022-02-27T23:00:00Z').toISOString();
         const spy = sinon.spy();
+        const settingsCache = sinon.stub().returns('Etc/UTC');
         new LastSeenAtUpdater({
-            memberModel: {
-                update: spy
+            models: {
+                Member: {
+                    update: spy
+                }
+            },
+            services: {
+                settingsCache: {
+                    get: settingsCache
+                }
+            }
+        });
+        DomainEvents.dispatch(MemberPageViewEvent.create({memberId: '1', memberLastSeenAt: previousLastSeen, url: '/'}, now.toDate()));
+        assert(spy.calledOnceWithExactly({
+            last_seen_at: now.format('YYYY-MM-DD HH:mm:ss')
+        }, {
+            id: '1'
+        }), 'The LastSeenAtUpdater should attempt a member update with the current date.');
+    });
+
+    it('works correctly on another timezone (not updating last_seen_at)', async function () {
+        const now = moment('2022-02-28T04:00:00Z').utc();
+        const previousLastSeen = moment('2022-02-27T20:00:00Z').toISOString();
+        const spy = sinon.spy();
+        const settingsCache = sinon.stub().returns('Asia/Bangkok');
+        new LastSeenAtUpdater({
+            models: {
+                Member: {
+                    update: spy
+                }
+            },
+            services: {
+                settingsCache: {
+                    get: settingsCache
+                }
+            }
+        });
+        DomainEvents.dispatch(MemberPageViewEvent.create({memberId: '1', memberLastSeenAt: previousLastSeen, url: '/'}, now.toDate()));
+        assert(spy.notCalled, 'The LastSeenAtUpdater should attempt a member update when the new timestamp is within the same day in the publication timezone.');
+    });
+
+    it('works correctly on another timezone (updating last_seen_at)', async function () {
+        const now = moment('2022-02-28T04:00:00Z').utc();
+        const previousLastSeen = moment('2022-02-27T20:00:00Z').toISOString();
+        const spy = sinon.spy();
+        const settingsCache = sinon.stub().returns('Europe/Paris');
+        new LastSeenAtUpdater({
+            models: {
+                Member: {
+                    update: spy
+                }
+            },
+            services: {
+                settingsCache: {
+                    get: settingsCache
+                }
             }
         });
         DomainEvents.dispatch(MemberPageViewEvent.create({memberId: '1', memberLastSeenAt: previousLastSeen, url: '/'}, now.toDate()));
@@ -31,9 +85,17 @@ describe('LastSeenAtUpdater', function () {
         const now = moment('2022-02-28T18:00:00Z');
         const previousLastSeen = moment('2022-02-28T00:00:00Z').toISOString();
         const spy = sinon.spy();
+        const settingsCache = sinon.stub().returns('Etc/UTC');
         new LastSeenAtUpdater({
-            memberModel: {
-                update: spy
+            models: {
+                Member: {
+                    update: spy
+                }
+            },
+            services: {
+                settingsCache: {
+                    get: settingsCache
+                }
             }
         });
         DomainEvents.dispatch(MemberPageViewEvent.create({memberId: '1', memberLastSeenAt: previousLastSeen, url: '/'}, now.toDate()));
@@ -43,9 +105,17 @@ describe('LastSeenAtUpdater', function () {
     it('Doesn\'t fire on other events', async function () {
         const now = moment('2022-02-28T18:00:00Z');
         const spy = sinon.spy();
+        const settingsCache = sinon.stub().returns('Etc/UTC');
         new LastSeenAtUpdater({
-            memberModel: {
-                update: spy
+            models: {
+                Member: {
+                    update: spy
+                }
+            },
+            services: {
+                settingsCache: {
+                    get: settingsCache
+                }
             }
         });
         DomainEvents.dispatch(MemberSubscribeEvent.create({memberId: '1', source: 'api'}, now.toDate()));
