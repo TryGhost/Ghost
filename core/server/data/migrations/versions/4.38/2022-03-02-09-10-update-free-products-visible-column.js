4d7c52077120f3e8cc9ef15f15cbc323c2da3d7e
Fabien "egg" O'Carroll <fabien@allou.is>
2022-03-02 11:14:58 +0200

    Migrated visible column from portal settings
    https://github.com/TryGhost/Team/issues/1387
    
    This is split into two migrations, one for the portal_products setting
    and one for the portal_plans setting, as dealing with both of them in a
    single migration led to too many branches.

---
+++ core/server/data/migrations/versions/4.38/2022-03-02-09-10-update-free-products-visible-column.js
@@ -0,0 +1,64 @@
+const logging = require('@tryghost/logging');
+const {createTransactionalMigration} = require('../../utils');
+
+module.exports = createTransactionalMigration(
+    async function up(knex) {
+        const portalPlanSetting = await knex('settings').select('value').where('key', 'portal_plans').first();
+
+        if (!portalPlanSetting) {
+            logging.warn('Could not find portal_plans setting - skipping migration');
+            return;
+        }
+
+        try {
+            const settingData = JSON.parse(portalPlanSetting.value);
+
+            if (!settingData.includes('free')) {
+                logging.warn(`portal_plans does not include "free" - skipping migration`);
+                return;
+            }
+
+            logging.info(`Updating free products to visible`);
+            await knex('products').update('visible', true).where('type', 'free');
+        } catch (err) {
+            logging.warn('portal_products setting is invalid - skipping migration');
+            return;
+        }
+    },
+    async function down(knex) {
+        const freeTier = await knex('products').select('id').where('type', 'free').first();
+        const portalPlanSetting = await knex('settings').select('value').where('key', 'portal_plans').first();
+
+        if (!freeTier) {
+            logging.info('Free tier is not visible, not updating portal_plans');
+            return;
+        }
+
+        if (!portalPlanSetting) {
+            logging.warn('Could not find portal_plans setting - skipping migration');
+            return;
+        }
+
+        try {
+            const existingSettingData = JSON.parse(portalPlanSetting.value);
+            let settingData;
+
+            if (freeTier.visible) {
+                if (existingSettingData.includes('free')) {
+                    settingData = existingSettingData;
+                } else {
+                    settingData = existingSettingData.concat('free');
+                }
+            } else {
+                settingData = existingSettingData.filter(value => value !== 'free');
+            }
+
+            logging.info(`Updating portal_plans to ${settingData}`);
+            await knex('settings').update('value', JSON.stringify(settingData)).where('key', 'portal_plans');
+        } catch (err) {
+            logging.warn('portal_products setting is invalid - skipping migration');
+            return;
+        }
+
+    }
+);
