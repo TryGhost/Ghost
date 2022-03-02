2275530f5515a696627295d81c35c12090d0d251
Daniel Lockyer <hi@daniellockyer.com>
2022-03-02 15:37:26 +0100

    Aliased `mysql` to `mysql2` in schema client
    refs https://github.com/TryGhost/Toolbox/issues/174
    
    - this just uses the same SQL queries for certain queries when using the
      `mysql2` library as when using the `mysql` one
    - we can remove the `mysql` line when we fully switch to `mysql2`

--- core/server/data/schema/clients/index.js
+++ core/server/data/schema/clients/index.js
@@ -3,5 +3,6 @@ const mysql = require('./mysql');
 
 module.exports = {
     sqlite3: sqlite3,
-    mysql: mysql
+    mysql: mysql, //TODO: remove this once we switch to `mysql2`
+    mysql2: mysql
 };
