b46bec13541f292d657012a8e873434bb65e1a55
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-03-03 22:43:50 +0000

    Added "Next billing date" members filter
    no issue
    
    - added datepicker based filter for the next billing date of paid subscriptions
    - updated table to add "Next billing date" column when filtered, showing the paid subscription's current period end date

--- ghost/admin/app/components/members/filter.js
+++ ghost/admin/app/components/members/filter.js
@@ -24,6 +24,7 @@ const FILTER_PROPERTIES = [
     {label: 'Billing period', name: 'subscriptions.plan_interval', group: 'Subscription'},
     {label: 'Stripe subscription status', name: 'subscriptions.status', group: 'Subscription'},
     {label: 'Start date', name: 'subscriptions.start_date', valueType: 'date', group: 'Subscription', feature: 'membersTimeFilters'},
+    {label: 'Next billing date', name: 'subscriptions.current_period_end', valueType: 'date', group: 'Subscription', feature: 'membersTimeFilters'},
 
     // Emails
     {label: 'Emails sent (all time)', name: 'email_count', group: 'Email'},
@@ -85,6 +86,16 @@ const FILTER_RELATIONS_OPTIONS = {
         {label: 'is not', name: 'is-not'}
     ],
     'subscriptions.start_date': [
+        {label: 'before', name: 'is-less'},
+        {label: 'on or before', name: 'is-or-less'},
+        // TODO: these cause problems because they require multiple NQL statements, eg:
+        // created_at:>='2022-03-02 00:00'+created_at:<'2022-03-03 00:00'
+        // {label: 'on', name: 'is'},
+        // {label: 'not on', name: 'is-not'},
+        {label: 'after', name: 'is-greater'},
+        {label: 'on or after', name: 'is-or-greater'}
+    ],
+    'subscriptions.current_period_end': [
         {label: 'before', name: 'is-less'},
         {label: 'on or before', name: 'is-or-less'},
         // TODO: these cause problems because they require multiple NQL statements, eg:
