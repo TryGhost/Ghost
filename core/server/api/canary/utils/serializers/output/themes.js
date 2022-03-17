fe7b55d3e2032e923cdc3d738d1d29c35521c9da
Hannah Wolfe <github.erisds@gmail.com>
2022-03-17 15:42:58 +0000

    Added serializer for themes.destroy
     refs: https://github.com/TryGhost/Toolbox/issues/245
    
    - The destroy endpoint was missing a serializer
    - Instead of adding one, I've refactored to use an all method that's a passthrough
    - Updated the tests to use the same pattern as others to make it clearer this is tested

--- core/server/api/canary/utils/serializers/output/themes.js
+++ core/server/api/canary/utils/serializers/output/themes.js
@@ -1,30 +1,9 @@
 const debug = require('@tryghost/debug')('api:canary:utils:serializers:output:themes');
 
 module.exports = {
-    browse(themes, apiConfig, frame) {
-        debug('browse');
+    all(themes, apiConfig, frame) {
+        debug('all');
 
         frame.response = themes;
-    },
-
-    upload() {
-        debug('upload');
-        this.browse(...arguments);
-    },
-
-    install() {
-        debug('install');
-        this.browse(...arguments);
-    },
-
-    activate() {
-        debug('activate');
-        this.browse(...arguments);
-    },
-
-    download(fn, apiConfig, frame) {
-        debug('download');
-
-        frame.response = fn;
     }
 };
