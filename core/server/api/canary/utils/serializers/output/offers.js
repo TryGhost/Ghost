ac130954029207f057a25a10c07f178bf6d70af2
Hannah Wolfe <github.erisds@gmail.com>
2022-03-20 15:17:39 +0000

    Added serializer for offers
    refs: https://github.com/TryGhost/Toolbox/issues/245
    
    - Offers didn't have a serializer because it's deliberately set at the API level
    - With the upcoming refactor we want to have all the serializers defined explicitly
    - This will allow us to change the default behaviour
    - Tests were added to cover this endpoint in ...

---
+++ core/server/api/canary/utils/serializers/output/offers.js
@@ -0,0 +1,8 @@
+const debug = require('@tryghost/debug')('api:canary:utils:serializers:output:offers');
+
+module.exports = {
+    all() {
+        debug('all');
+        // Offers has frame.response already set
+    }
+};
