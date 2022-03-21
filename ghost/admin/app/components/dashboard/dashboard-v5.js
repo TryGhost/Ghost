0be71faf6e927328c971096de9447d0c16e3e8e2
Simon Backx <simon@ghost.org>
2022-03-21 19:02:57 +0100

    Added some basic prototype toggles
    refs https://github.com/TryGhost/Team/issues/1432

--- ghost/admin/app/components/dashboard/dashboard-v5.js
+++ ghost/admin/app/components/dashboard/dashboard-v5.js
@@ -1,4 +1,25 @@
 import Component from '@glimmer/component';
+import {tracked} from '@glimmer/tracking';
 
 export default class DashboardDashboardV5Component extends Component {
+    @tracked mockPaidTiers = true;
+    @tracked mockStripeEnabled = true;
+    @tracked mockNewslettersEnabled = true;
+    @tracked mockMembersEnabled = true;
+
+    get hasPaidTiers() {
+        return this.mockPaidTiers;
+    }
+
+    get isStripeEnabled() {
+        return this.mockStripeEnabled;
+    }    
+
+    get areNewslettersEnabled() {
+        return this.mockNewslettersEnabled;
+    }
+
+    get areMembersEnabled() {
+        return this.mockMembersEnabled;
+    }
 }
