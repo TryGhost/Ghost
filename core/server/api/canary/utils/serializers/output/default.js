3bd4d0989ae6bc65576684c71ab5a649b03377c7
Hannah Wolfe <github.erisds@gmail.com>
2022-03-21 11:46:41 +0000

    Added default serializer + handling
    refs: https://github.com/TryGhost/Toolbox/issues/245
    
    - Added a serializer called default to the canary API
      - Ideally, this would be part of the shared framework, but this would change v2/v3 and we're about to get rid of them
      - Therefore, we change just canary for now, and we can refactor again later.
    - Added wiring to handler that uses the default serializer, if there is a default, and isn't an explicit serializer for the endpoint
    - Removed the invites serializer, so that one endpoint now uses the default
    
    Note: previous commits have added explicit serializers to every endpoint, this is the first step towards paring
    that back so that we have less serializers overall, not more!

---
+++ core/server/api/canary/utils/serializers/output/default.js
@@ -0,0 +1,35 @@
+const debug = require('@tryghost/debug')('api:canary:utils:serializers:output:default');
+const mappers = require('./mappers');
+
+const mapResponse = (docName, mappable, frame) => {
+    if (mappers[docName]) {
+        return mappers[docName](mappable, frame);
+    } else if (mappable.toJSON) {
+        return mappable.toJSON(frame.options);
+    }
+
+    return mappable;
+};
+
+module.exports = {
+    all(response, apiConfig, frame) {
+        const {docName, method} = apiConfig;
+        debug('serializing', docName, method);
+
+        if (!response) {
+            return;
+        }
+
+        frame.response = {};
+
+        if (response.data) {
+            frame.response[docName] = response.data.map(model => mapResponse(docName, model, frame));
+        } else {
+            frame.response[docName] = [mapResponse(docName, response, frame)];
+        }
+
+        if (response.meta) {
+            frame.response.meta = response.meta;
+        }
+    }
+};
