e575b3084e4d7dabf59f27d3e6fe6f97b1d8c786
James Morris <moreofmorris@users.noreply.github.com>
2022-03-22 15:37:17 +0000

    Better faked graphs and various tweaks for Dashboard 5.0 prototype
    refs: https://github.com/TryGhost/Team/issues/1436
    
    - added in lots of demo graphs using EmberChart
    - renamed some files in v5 folder for better conventions
    - put each graph for new dashboard into separate components

---
+++ ghost/admin/app/components/dashboard/v5/chart-engagement.js
@@ -0,0 +1,11 @@
+import Component from '@glimmer/component';
+
+export default class ChartEngagement extends Component {
+    get data30Days() {
+        return '67%';
+    }
+
+    get data7Days() {
+        return '31%';
+    }
+}
