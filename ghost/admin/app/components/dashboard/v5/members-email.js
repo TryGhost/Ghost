b2ab32b228fd2577872fb4ba4b7c737b4097708a
Simon Backx <simon@ghost.org>
2022-03-22 12:11:00 +0100

    Added placeholder components and skeleton for dashboard 5 prototype
    refs https://github.com/TryGhost/Team/issues/1432

---
+++ ghost/admin/app/components/dashboard/v5/members-email.js
@@ -0,0 +1,7 @@
+import Component from '@glimmer/component';
+
+export default class MembersEmail extends Component {
+    get subscribersCount() {
+        return '9.150';
+    }
+}
---
+++ ghost/admin/app/components/dashboard/v5/members-engagement.js
@@ -0,0 +1,7 @@
+import Component from '@glimmer/component';
+
+export default class MembersEngagement extends Component {
+    get placeholderGetter() {
+        return '9.150';
+    }
+}
