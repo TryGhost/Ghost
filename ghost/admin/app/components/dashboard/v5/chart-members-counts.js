c98a4b16126e115a8ba10fc30deb1896ccc99556
Simon Backx <simon@ghost.org>
2022-03-23 09:51:53 +0100

    Added dashboard 5.0 stats service and days dropdown
    refs https://github.com/TryGhost/Team/issues/1432
    
    - Added very basic state selection at the bottom of dashboard 5.0
    - Added a dashboard stats service, who is responsible for fetching and returning stats data
    - Added POC for days dropdown with communication and reload between ember components
    - Added proper automatic number and plural formatting for member counts

--- ghost/admin/app/components/dashboard/v5/chart-members-counts.js
+++ ghost/admin/app/components/dashboard/v5/chart-members-counts.js
@@ -1,15 +1,27 @@
 import Component from '@glimmer/component';
+import {inject as service} from '@ember/service';
 
 export default class ChartMembersCounts extends Component {
-    get dataTotalMembers() {
-        return '10,000';
+    @service dashboardStats;
+
+    constructor() {
+        super(...arguments);
+        this.loadCharts();
     }
 
-    get dataPaidMembers() {
-        return '1,500';
+    loadCharts() {
+        this.dashboardStats.loadMembersCounts();
     }
 
-    get dataFreeMembers() {
-        return '8,500';
+    get totalMembers() {
+        return this.dashboardStats.memberCounts?.total ?? 0;
+    }
+
+    get paidMembers() {
+        return this.dashboardStats.memberCounts?.paid ?? 0;
+    }
+
+    get freeMembers() {
+        return this.dashboardStats.memberCounts?.free ?? 0;
     }
 }
--- ghost/admin/app/components/dashboard/v5/chart-total-members.js
+++ ghost/admin/app/components/dashboard/v5/chart-total-members.js
@@ -1,15 +1,39 @@
 import Component from '@glimmer/component';
+import moment from 'moment';
+import {action} from '@ember/object';
+import {inject as service} from '@ember/service';
 
 export default class ChartTotalMembers extends Component {
+    @service dashboardStats;
+
+    constructor() {
+        super(...arguments);
+        this.loadCharts();
+    }
+
+    /**
+     * Call this method when you need to fetch new data from the server. In this component, it will get called
+     * when the days parameter changes and on initialisation.
+     */
+    @action
+    loadCharts() {
+        // The dashboard stats service will take care or reusing and limiting API-requests between charts
+        this.dashboardStats.loadMemberCountStats(this.args.days);
+    }
+    
     get chartType() {
         return 'line';
     }
 
     get chartData() {
+        const stats = this.fillCountDates(this.dashboardStats.memberCountStats, this.args.days);
+        const labels = Object.keys(stats);
+        const data = Object.values(stats).map(stat => stat.total);
+
         return {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June'],
+            labels: labels,
             datasets: [{
-                data: [65, 59, 80, 81, 56, 55, 40],
+                data: data,
                 fill: false,
                 borderColor: '#14b8ff',
                 tension: 0.1
@@ -27,5 +51,44 @@ export default class ChartTotalMembers extends Component {
 
     get chartHeight() {
         return 300;
+    }
+
+    /**
+     * This method is borrowed from the members stats service and would need an update
+     */
+    fillCountDates(data = {}, days) {
+        let currentRangeDate = moment().subtract(days, 'days');
+
+        let endDate = moment().add(1, 'hour');
+        const output = {};
+        const firstDateInRangeIndex = data.findIndex((val) => {
+            return moment(val.date).isAfter(currentRangeDate);
+        });
+        let initialDateInRangeVal = firstDateInRangeIndex > 0 ? data[firstDateInRangeIndex - 1] : null;
+        if (firstDateInRangeIndex === 0 && !initialDateInRangeVal) {
+            initialDateInRangeVal = data[firstDateInRangeIndex];
+        }
+        if (data.length > 0 && !initialDateInRangeVal && firstDateInRangeIndex !== 0) {
+            initialDateInRangeVal = data[data.length - 1];
+        }
+        let lastVal = {
+            paid: initialDateInRangeVal ? initialDateInRangeVal.paid : 0,
+            free: initialDateInRangeVal ? initialDateInRangeVal.free : 0,
+            comped: initialDateInRangeVal ? initialDateInRangeVal.comped : 0,
+            total: initialDateInRangeVal ? (initialDateInRangeVal.paid + initialDateInRangeVal.free + initialDateInRangeVal.comped) : 0
+        };
+        while (currentRangeDate.isBefore(endDate)) {
+            let dateStr = currentRangeDate.format('YYYY-MM-DD');
+            const dataOnDate = data.find(d => d.date === dateStr);
+            output[dateStr] = dataOnDate ? {
+                paid: dataOnDate.paid,
+                free: dataOnDate.free,
+                comped: dataOnDate.comped,
+                total: dataOnDate.paid + dataOnDate.free + dataOnDate.comped
+            } : lastVal;
+            lastVal = output[dateStr];
+            currentRangeDate = currentRangeDate.add(1, 'day');
+        }
+        return output;
     }
 }
