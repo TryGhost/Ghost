c860bb28a91df574339ef37e3ac004105ba626cd
Rishabh <zrishabhgarg@gmail.com>
2022-03-29 13:49:25 +0530

    Added popup notification for newsletter preference toggle
    refs https://github.com/TryGhost/Team/issues/1440

--- ghost/portal/src/utils/helpers.js
+++ ghost/portal/src/utils/helpers.js
@@ -544,7 +544,7 @@ export const formatNumber = (amount) => {
     return amount.toLocaleString();
 };
 
-export const createPopupNotification = ({type, status, autoHide, duration, closeable, state, message, meta = {}}) => {
+export const createPopupNotification = ({type, status, autoHide, duration = 2600, closeable, state, message, meta = {}}) => {
     let count = 0;
     if (state && state.popupNotification) {
         count = (state.popupNotification.count || 0) + 1;
