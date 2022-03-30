0407e6ed7b522a3ed7aebf2bdb6860c14e944992
Fabien "egg" O'Carroll <fabien@allou.is>
2022-03-30 11:18:20 +0100

    WIP - recursivley generate mrr sequences

--- core/server/data/migrations/versions/4.42/2022-03-28-15-25-backfill-mrr-adjustments-for-offers.js
+++ core/server/data/migrations/versions/4.42/2022-03-28-15-25-backfill-mrr-adjustments-for-offers.js
@@ -2,29 +2,26 @@ const logging = require('@tryghost/logging');
 
 const {createTransactionalMigration} = require('../../utils');
 
-
 module.exports = createTransactionalMigration(
     async function up(knex) {
         const offerRedemptions = await knex('offer_redemptions').select('*');
-        const foreverOffers = await knex('offers').select('id').where({duration: 'forever'});
+        const foreverOffers = await knex('offers').select('*').where({duration: 'forever'});
         const foreverOfferIds = foreverOffers.map(x => x.id);
 
-        const redemptionsKeyedBySubscriptionID = offerRedemptions.reduce((memo, redemption) => {
-            if (!foreverOfferIds.includes(redemption.offer_id)) {
-                return memo;
-            }
+        const foreverOfferRedemptions = offerRedemptions.filter(redemption => foreverOfferIds.includes(redemption.offer_id));
 
+        const redemptionsKeyedByMemberID = foreverOfferRedemptions.reduce((memo, redemption) => {
             return {
-                [redemption.subscription_id]: redemption,
+                [redemption.member_id]: memo[redemption.member_id] ? memo[redemption.member_id].concat(redemption) : [redemption],
                 ...memo
             };
         }, {});
 
-        const mrrEvents = await knex('members_paid_subscription_events').select('*').whereIn('subscription_id', Object.keys(redemptionsKeyedBySubscriptionID));
+        const mrrEvents = await knex('members_paid_subscription_events').select('*').whereIn('member_id', Object.keys(redemptionsKeyedByMemberID));
 
-        const eventsKeyedByID = mrrEvents.reduce((memo, event) => {
+        const eventsKeyedByMemberID = mrrEvents.reduce((memo, event) => {
             return {
-                [event.subscription_id]: memo[event.subscription_id] ? memo[event.subscription_id].concat(event) : [event],
+                [event.member_id]: memo[event.member_id] ? memo[event.member_id].concat(event) : [event],
                 ...memo
             };
         }, {});
@@ -36,9 +33,41 @@ module.exports = createTransactionalMigration(
             };
         }, {});
 
-        for (const eventID in eventsKeyedByID) {
-            const events = eventsKeyedByID[eventID];
+        function findNextEvent(events, current) {
+            if (current.to_plan === null || current.to_plan === current.from_plan) {
+                return null;
+            }
+            const candidates = events.filter(event => event.from_plan === event.to_plan);
+            if (candidates.length === 0) {
+                return null;
+            }
+            return candidates[0];
+        }
+
+        function buildSequence(events, sequence) {
+            const next = findNextEvent(events, sequence[sequence.length - 1]);
+
+            if (!next) {
+                return sequence;
+            }
+
+            return buildSequence(events.filter(event => event.id !== next.id), sequence.concat(next));
+        }
+
+        const mrrEventSequences = Object.keys(eventsKeyedByMemberID).reduce((memo, memberID) => {
+            const initialEvents = eventsKeyedByMemberID[memberID].filter(event => event.from_plan === null);
+            const nonInitialEvents = eventsKeyedByMemberID[memberID].filter(event => event.from_plan !== null);
+
+            const sequences = initialEvents.map(event => {
+                return buildSequence(nonInitialEvents, [event]);
+            });
+
+            return memo.concat(sequences);
+        }, []);
+
+        for (const events of mrrEventSequences) {
             let diff;
+
             if (events[0]) {
                 const event = events[0];
 
@@ -52,20 +81,34 @@ module.exports = createTransactionalMigration(
                     continue;
                 }
 
-                const redemption = redemptionsKeyedBySubscriptionID[event.subscription_id];
+                const redemptions = redemptionsKeyedByMemberID[event.member_id];
+                let redemption;
+
+                if (redemptions.length === 1) {
+                    redemption = redemptions[0];
+                } else {
+                    redemption = redemptions.find(r => {
+                        // find closest one to the created_at date
+                    });
+                    redemption = redemptions[0];
+                }
+
                 const offer = foreverOffersKeyedByID[redemption.offer_id];
 
                 if (offer.discount_type === 'percent') {
-                    // diff = mrr * (100 - percent) / 100
+                    diff = event.mrr_delta * (100 - offer.discount_amount) / 100;
                 } else {
                     if (offer.interval === 'month') {
-                        // diff = amount
+                        diff = offer.discount_amount;
                     } else {
-                        // diff = amount / 12
+                        diff = offer.discount_amount / 12;
                     }
                 }
 
-                // update MRR to take off "diff"
+                console.log(`About to update mrr_delta from ${event.mrr_delta} to ${event.mrr_delta - diff}`);
+                // await knex('members_paid_subscription_events')
+                //     .update({mrr_delta: event.mrr_delta - diff})
+                //     .where({id: event.id});
             }
 
             if (events[1]) {
@@ -76,12 +119,10 @@ module.exports = createTransactionalMigration(
                     continue;
                 }
 
-                if (event.from_plan === event.to_plan) {
-                    logging.error('Invalid event found');
-                    continue;
-                }
-
-                // update MRR to add on "diff"
+                console.log(`About to update mrr_delta from ${event.mrr_delta} to ${event.mrr_delta + diff}`);
+                // await knex('members_paid_subscription_events')
+                //     .update({mrr_delta: event.mrr_delta + diff})
+                //     .where({id: event.id});
             }
         }
     },
