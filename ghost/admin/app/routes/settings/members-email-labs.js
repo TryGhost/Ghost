d6487d363031ccb40119aec9cd06c804cc62e88e
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-03-31 11:06:21 +0100

    Created labs versions of members-email settings code
    refs https://github.com/TryGhost/Team/issues/1441
    
    - duplicated route/controller/template and component to `-labs` versions so larger changes and refactors can be made without affecting the GA code
    - added redirect for `/settings/members-email -> /settings/members-email-labs` when `multipleNewsletters` flag is enabled
    - cleaned up multiple-newsletters related code from the non-labs component

--- ghost/admin/app/routes/settings/members-email-labs.js
+++ ghost/admin/app/routes/settings/members-email-labs.js
@@ -2,7 +2,7 @@ import AdminRoute from 'ghost-admin/routes/admin';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
 
-export default class MembersEmailRoute extends AdminRoute {
+export default class MembersEmailLabsRoute extends AdminRoute {
     @service feature;
     @service notifications;
     @service settings;
@@ -10,8 +10,8 @@ export default class MembersEmailRoute extends AdminRoute {
     beforeModel(transition) {
         super.beforeModel(...arguments);
 
-        if (this.feature.multipleNewsletters) {
-            return this.transitionTo('settings.members-email-labs');
+        if (!this.feature.multipleNewsletters) {
+            return this.transitionTo('settings.members-email');
         }
 
         if (transition.to.queryParams?.fromAddressUpdate === 'success') {
