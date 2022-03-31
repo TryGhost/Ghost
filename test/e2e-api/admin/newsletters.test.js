bb8ed1548854b91ac483121294be94d7540e0a4e
Thibaut Patel <thibaut@ghost.org>
2022-03-31 10:52:04 +0200

    Added the newsletter API permissions
    refs https://github.com/TryGhost/Team/issues/1463
    
    - Allow admins to perform all newsletter operations
    - We can adjust and be more permissive in the future if needed
    - Added the tests back as permissions are configured correctly now

--- test/e2e-api/admin/newsletters.test.js
+++ test/e2e-api/admin/newsletters.test.js
@@ -21,76 +21,76 @@ describe('Newsletters API', function () {
         mockManager.restore();
     });
 
-    // it('Can add a newsletter', async function () {
-    //     const newsletter = {
-    //         name: 'My test newsletter',
-    //         sender_name: 'Test',
-    //         sender_email: 'test@example.com',
-    //         sender_reply_to: 'test@example.com',
-    //         default: false,
-    //         status: 'active',
-    //         recipient_filter: '',
-    //         subscribe_on_signup: true,
-    //         sort_order: 0
-    //     };
+    it('Can add a newsletter', async function () {
+        const newsletter = {
+            name: 'My test newsletter',
+            sender_name: 'Test',
+            sender_email: 'test@example.com',
+            sender_reply_to: 'test@example.com',
+            default: false,
+            status: 'active',
+            recipient_filter: '',
+            subscribe_on_signup: true,
+            sort_order: 0
+        };
 
-    //     await agent
-    //         .post(`newsletters/`)
-    //         .body({newsletters: [newsletter]})
-    //         .expectStatus(201)
-    //         .matchBodySnapshot({
-    //             newsletters: [newsletterSnapshot]
-    //         })
-    //         .matchHeaderSnapshot({
-    //             etag: anyEtag,
-    //             location: anyString
-    //         });
+        await agent
+            .post(`newsletters/`)
+            .body({newsletters: [newsletter]})
+            .expectStatus(201)
+            .matchBodySnapshot({
+                newsletters: [newsletterSnapshot]
+            })
+            .matchHeaderSnapshot({
+                etag: anyEtag,
+                location: anyString
+            });
 
-    //     await agent.get('newsletters/')
-    //         .expectStatus(200)
-    //         .matchBodySnapshot({
-    //             newsletters: [newsletterSnapshot]
-    //         })
-    //         .matchHeaderSnapshot({
-    //             etag: anyEtag
-    //         });
-    // });
+        await agent.get('newsletters/')
+            .expectStatus(200)
+            .matchBodySnapshot({
+                newsletters: [newsletterSnapshot]
+            })
+            .matchHeaderSnapshot({
+                etag: anyEtag
+            });
+    });
 
-    // it('Can browse newsletters', async function () {
-    //     await agent.get('newsletters/')
-    //         .expectStatus(200)
-    //         .matchBodySnapshot({
-    //             newsletters: [newsletterSnapshot]
-    //         })
-    //         .matchHeaderSnapshot({
-    //             etag: anyEtag
-    //         });
-    // });
+    it('Can browse newsletters', async function () {
+        await agent.get('newsletters/')
+            .expectStatus(200)
+            .matchBodySnapshot({
+                newsletters: [newsletterSnapshot]
+            })
+            .matchHeaderSnapshot({
+                etag: anyEtag
+            });
+    });
 
-    // it('Can edit newsletters', async function () {
-    //     const res = await agent.get('newsletters?limit=1')
-    //         .expectStatus(200)
-    //         .matchBodySnapshot({
-    //             newsletters: [newsletterSnapshot]
-    //         })
-    //         .matchHeaderSnapshot({
-    //             etag: anyEtag
-    //         });
+    it('Can edit newsletters', async function () {
+        const res = await agent.get('newsletters?limit=1')
+            .expectStatus(200)
+            .matchBodySnapshot({
+                newsletters: [newsletterSnapshot]
+            })
+            .matchHeaderSnapshot({
+                etag: anyEtag
+            });
 
-    //     const id = res.body.newsletters[0].id;
+        const id = res.body.newsletters[0].id;
 
-    //     await agent.put(`newsletters/${id}`)
-    //         .body({
-    //             newsletters: [{
-    //                 name: 'Updated newsletter name'
-    //             }]
-    //         })
-    //         .expectStatus(200)
-    //         .matchBodySnapshot({
-    //             newsletters: [newsletterSnapshot]
-    //         })
-    //         .matchHeaderSnapshot({
-    //             etag: anyEtag
-    //         });
-    // });
+        await agent.put(`newsletters/${id}`)
+            .body({
+                newsletters: [{
+                    name: 'Updated newsletter name'
+                }]
+            })
+            .expectStatus(200)
+            .matchBodySnapshot({
+                newsletters: [newsletterSnapshot]
+            })
+            .matchHeaderSnapshot({
+                etag: anyEtag
+            });
+    });
 });
