da972bfe58f665c6db67e97da9718019203b8356
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-04-01 12:46:58 +0100

    🐛 Fixed non-functioning "Leave" button on unsaved-changes confirmation modals
    no issue
    
    - bumped version of our `ember-promise-modals` fork containing a workaround for modal promises missing their return values
      - upstream issue: https://github.com/simplabs/ember-promise-modals/issues/565
    - switched from a GitHub ref to a proper published package to avoid issues with yarn not bumping versions of the fork for developers who installed an earlier version

--- ghost/admin/app/components/epm-modal-container.js
+++ ghost/admin/app/components/epm-modal-container.js
@@ -1,3 +1,3 @@
-import EpmModalContainer from 'ember-promise-modals/components/modal-container';
+import EpmModalContainer from '@tryghost/ember-promise-modals/components/modal-container';
 
 export default class extends EpmModalContainer {}
