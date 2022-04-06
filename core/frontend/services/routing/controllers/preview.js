3bb16cdf0190efc11441870106e26aa4ecb474d2
Hannah Wolfe <github.erisds@gmail.com>
2022-04-06 11:53:04 +0100

    Removed res.locals.apiVersion
    - we are getting rid of the concept of having multiple api versions in a single ghost install
    - we no longer need to pass the apiVersion around using res.locals
    - To simplify code that uses our frontend proxy the proxy now _only_ exposes canary

--- core/frontend/services/routing/controllers/preview.js
+++ core/frontend/services/routing/controllers/preview.js
@@ -14,7 +14,7 @@ const renderer = require('../../rendering');
 module.exports = function previewController(req, res, next) {
     debug('previewController');
 
-    const api = require('../../proxy').api[res.locals.apiVersion];
+    const api = require('../../proxy').api;
 
     const params = {
         uuid: req.params.uuid,
@@ -53,9 +53,7 @@ module.exports = function previewController(req, res, next) {
                 return urlUtils.redirect301(res, routerManager.getUrlByResourceId(post.id, {withSubdirectory: true}));
             }
 
-            if (res.locals.apiVersion !== 'v0.1' && res.locals.apiVersion !== 'v2') {
-                post.access = !!post.html;
-            }
+            post.access = !!post.html;
 
             // @TODO: See renderer/secure
             renderer.secure(req, post);
