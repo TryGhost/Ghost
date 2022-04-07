d991c7cb16cea79a0feee30fe277d81923ede931
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-04-07 16:42:43 +0100

    Added drag-and-drop sorting of newsletters
    closes https://github.com/TryGhost/Team/issues/1483
    
    - updated newsletters list to use `<SortableObjects>` and `<DraggableObject>`
    - swapped the `filteredNewsletters` reactive getter for a tracked property because `<SortableObjects>` replaces the value with a new array when re-ordering which isn't compatible with a getter
      - required explicit update of the list any time the list should change through filter swap, archive, unarchive
      - required explicit update of the list via router event when leaving the "new newsletter" route so any newly added newsletter can be displayed
    - added action for handling update of `sort_order` properties when a re-order occurs
      - updates every newsletter setting `sort_order` of the active newsletters first based on the new order, followed by all archived newsletters following their existing order
    - disabled sorting of newsletters in the archived newsletters list
      - hides the grab handle and disables the resize on hover
      - allowing re-order of archived newsletters would complicate the re-order+save algorithm and re-ordering archived newsletters doesn't have a use-case

--- ghost/admin/app/components/settings/members-email-labs/newsletter-management.js
+++ ghost/admin/app/components/settings/members-email-labs/newsletter-management.js
@@ -6,8 +6,10 @@ import {tracked} from '@glimmer/tracking';
 
 export default class NewsletterManagementComponent extends Component {
     @service store;
+    @service router;
 
     @tracked statusFilter = 'active';
+    @tracked filteredNewsletters = [];
 
     statusFilters = ['active', 'archived'];
     newsletters = this.store.peekAll('newsletter');
@@ -15,6 +17,13 @@ export default class NewsletterManagementComponent extends Component {
     constructor() {
         super(...arguments);
         this.loadNewslettersTask.perform();
+
+        this.router.on('routeDidChange', this.handleNewRouteChange);
+    }
+
+    willDestroy() {
+        super.willDestroy(...arguments);
+        this.router.off('routeDidChange', this.handleNewRouteChange);
     }
 
     get activeNewsletters() {
@@ -25,23 +34,44 @@ export default class NewsletterManagementComponent extends Component {
         return this.newsletters.filter(n => n.status === 'archived');
     }
 
-    get filteredNewsletters() {
-        return this.newsletters.filter(n => n.status === this.statusFilter);
-    }
-
     get displayingDefault() {
         return this.statusFilter === 'active' && this.activeNewsletters.length === 1;
     }
 
     @action
-    changeStatusFilter(newFilter) {
-        this.statusFilter = newFilter;
+    changeStatusFilter(status) {
+        this.statusFilter = status;
+        this.updateFilteredNewsletters();
+    }
+
+    @action
+    updateFilteredNewsletters() {
+        this.filteredNewsletters = this.newsletters.filter((n) => {
+            return n.status === this.statusFilter
+                && !n.isNew
+                && !n.isDestroyed;
+        });
+    }
+
+    @action
+    handleNewRouteChange(transition) {
+        // NOTE: this is necessary because ember-drag-drop has forced us into using
+        // an explicit tracked filteredNewsletters property rather than using a reactive
+        // getter that automatically displays newly added newsletters
+
+        if (transition.from.name === 'settings.members-email-labs.new-newsletter') {
+            this.updateFilteredNewsletters();
+        }
     }
 
     @task
     *archiveNewsletterTask(newsletter) {
         newsletter.status = 'archived';
-        return yield newsletter.save();
+        const result = yield newsletter.save();
+
+        this.updateFilteredNewsletters();
+
+        return result;
     }
 
     @task
@@ -53,11 +83,44 @@ export default class NewsletterManagementComponent extends Component {
             this.statusFilter = 'active';
         }
 
+        this.updateFilteredNewsletters();
+
         return result;
     }
 
     @task
     *loadNewslettersTask() {
-        return yield this.store.findAll('newsletter');
+        const newsletters = yield this.store.findAll('newsletter');
+
+        this.updateFilteredNewsletters();
+
+        return newsletters;
+    }
+
+    @task
+    *reorderNewslettersTask() {
+        // filteredNewsletters is the array that gets updated by <SortableObjects> whilst dragging.
+        // we only want to update ordering when _active_ newsletters are re-ordered to make sure
+        // archived newsletters don't end up with a lower sort order than active ones
+
+        if (this.statusFilter !== 'active') {
+            return;
+        }
+
+        // use filteredNewsletters rather than activeNewsletters so we're using'the ordered array
+        const activeNewsletters = this.filteredNewsletters;
+
+        const otherNewsletters = this.newsletters
+            .filter(n => !activeNewsletters.includes(n))
+            .sort(({sortOrder: a}, {sortOrder: b}) => b - a);
+
+        let sortOrder = 0;
+
+        for (const n of [...activeNewsletters, ...otherNewsletters]) {
+            n.sortOrder = sortOrder;
+            yield n.save();
+
+            sortOrder += 1;
+        }
     }
 }
