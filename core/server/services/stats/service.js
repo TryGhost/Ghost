132726fe20dd14413b57d44cc17d1db65dd86284
Simon Backx <simon@ghost.org>
2022-04-08 09:18:04 +0200

    Added MRR stats service and endpoint (#14427)
    refs https://github.com/TryGhost/Team/issues/1470
    
    Instead of counting the MRR by resolving all the deltas from the past until now, we should start with the current calculated MRR and resolve it until the first event. That would give a more accurate recent MRR (in exchange for a less accurate MRR for older data) and allows us to limit the amount of returned days in the future.
    
    - Includes MRR stats service that can fetch the current MRR per currency
    - The service can return a history of the MRR for every day and currency
    - New admin API endpoint /stats/mrr that returns the MRR history
    - Includes tests for these new service and endpoint

--- core/server/services/stats/service.js
+++ core/server/services/stats/service.js
@@ -1,6 +1,8 @@
 const db = require('../../data/db');
 const MemberStatsService = require('./lib/members-stats-service');
+const MrrStatsService = require('./lib/mrr-stats-service');
 
 module.exports = {
-    members: new MemberStatsService({db})
+    members: new MemberStatsService({db}),
+    mrr: new MrrStatsService({db})
 };
