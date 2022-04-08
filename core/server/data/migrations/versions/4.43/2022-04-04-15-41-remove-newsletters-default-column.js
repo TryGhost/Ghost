aca51b62c28b13674e0f1d869ba0dfc848d3753f
Rishabh Garg <zrishabhgarg@gmail.com>
2022-04-08 19:06:30 +0530

    Cleaned up newsletter schema migrations (#14441)
    refs https://github.com/TryGhost/Team/issues/1500
    
    The newsletter table schema has bunch of changes to go through for new and existing columns, this consolidates the schema changes into a single re-create table migration that drops and adds the newsletter table with correct schema. The table re-create migration needs to run before any of the tables using newsletter as foreign key. The changes include -
    
    - new columns for design related fields
    - new slug column for filtering
    - unique constraint to `name` column
    - remove `default` column (noops the existing default column migration)
    - `sender_reply_to` has a default of newsletter and a validation of ['newsletter', 'support']
    - updated default values for `subscribe_on_signup` and  `recipient_filter`

--- core/server/data/migrations/versions/4.43/2022-04-04-15-41-remove-newsletters-default-column.js
+++
@@ -1,7 +0,0 @@
-const {createDropColumnMigration} = require('../../utils');
-
-module.exports = createDropColumnMigration('newsletters', 'default', {
-    type: 'bool',
-    nullable: false,
-    defaultTo: false
-});
--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -10,20 +10,30 @@
 module.exports = {
     newsletters: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
-        name: {type: 'string', maxlength: 191, nullable: false},
+        name: {type: 'string', maxlength: 191, nullable: false, unique: true},
         description: {type: 'string', maxlength: 2000, nullable: true},
+        slug: {type: 'string', maxlength: 191, nullable: false, unique: true},
         sender_name: {type: 'string', maxlength: 191, nullable: false},
-        sender_email: {type: 'string', maxlength: 191, nullable: false, validations: {isEmail: true}},
-        sender_reply_to: {type: 'string', maxlength: 191, nullable: false, validations: {isEmail: true}},
+        sender_email: {type: 'string', maxlength: 191, nullable: true},
+        sender_reply_to: {type: 'string', maxlength: 191, nullable: false, defaultTo: 'newsletter', validations: {isIn: ['newsletter', 'support']}},
         status: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'active'},
-        recipient_filter: {
-            type: 'text',
-            maxlength: 1000000000,
+        visibility: {
+            type: 'string',
+            maxlength: 50,
             nullable: false,
-            defaultTo: ''
+            defaultTo: 'members'
         },
-        subscribe_on_signup: {type: 'bool', nullable: false, defaultTo: false},
-        sort_order: {type: 'integer', nullable: false, unsigned: true, defaultTo: 0}
+        subscribe_on_signup: {type: 'bool', nullable: false, defaultTo: true},
+        sort_order: {type: 'integer', nullable: false, unsigned: true, defaultTo: 0},
+        header_image: {type: 'string', maxlength: 2000, nullable: true},
+        show_header_icon: {type: 'bool', nullable: false, defaultTo: true},
+        show_header_title: {type: 'bool', nullable: false, defaultTo: true},
+        title_font_category: {type: 'string', maxlength: 191, nullable: false, defaultTo: 'sans_serif', validations: {isIn: ['serif', 'sans_serif']}},
+        title_alignment: {type: 'string', maxlength: 191, nullable: false, defaultTo: 'center', validations: {isIn: ['center', 'left']}},
+        show_feature_image: {type: 'bool', nullable: false, defaultTo: true},
+        body_font_category: {type: 'string', maxlength: 191, nullable: false, defaultTo: 'sans_serif', validations: {isIn: ['serif', 'sans_serif']}},
+        footer_content: {type: 'text', maxlength: 1000000000, nullable: true},
+        show_badge: {type: 'bool', nullable: false, defaultTo: true}
     },
     posts: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
