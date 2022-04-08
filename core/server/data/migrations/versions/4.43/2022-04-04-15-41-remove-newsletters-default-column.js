e2ea9afa8b322ecf6895cff97eb1e26ed6239975
Rishabh <zrishabhgarg@gmail.com>
2022-04-08 15:06:50 +0530

    fixup! Cleaned up newsletter schema migrations

--- core/server/data/migrations/versions/4.43/2022-04-04-15-41-remove-newsletters-default-column.js
+++
@@ -1,9 +0,0 @@
-module.exports.up = () => {
-    // noop - superceded by previous table re-create migration
-    return Promise.resolve();
-};
-
-module.exports.down = () => {
-    // noop - superceded by previous table re-create migration
-    return Promise.resolve();
-};
