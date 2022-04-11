ce88be6d675566600219f85f90d61687b1d9e311
Thibaut Patel <thibaut@ghost.org>
2022-04-11 19:24:33 +0200

    Added the default newsletter
    refs https://github.com/TryGhost/Team/issues/1473
    
    - We use the title to populate the newsletter same, slug and sender name
    - We use the description to populate the newsletter description
    - We use the global design settings to populate the corresponding newsletter design settings

---
+++ core/server/data/migrations/versions/4.44/2022-04-11-12-13-add-default-newsletter.js
@@ -0,0 +1,63 @@
+const ObjectId = require('bson-objectid');
+const logging = require('@tryghost/logging');
+const {createTransactionalMigration} = require('../../utils');
+const {slugify} = require('@tryghost/string');
+
+module.exports = createTransactionalMigration(
+    async function up(knex) {
+        // Make sure the newsletter table is empty
+        const newsletters = await knex('newsletters').count('*', {as: 'total'});
+
+        if (newsletters[0].total !== 0) {
+            logging.info('Skipping adding the default newsletter - There is already at least one newsletter');
+            return;
+        }
+
+        /**
+         * Helper to return a setting or a default value
+         */
+        async function getSetting(name, defaultValue) {
+            const setting = await knex('settings')
+                .where('key', name)
+                .select(['value'])
+                .first();
+
+            if (setting) {
+                return setting.value;
+            }
+
+            return defaultValue;
+        }
+
+        const title = await getSetting('title', 'Ghost');
+        const newsletter = {
+            id: ObjectId().toHexString(),
+            name: title,
+            description: await getSetting('description', ''),
+            slug: slugify(title),
+            sender_name: title,
+            sender_email: null,
+            sender_reply_to: 'newsletter',
+            status: 'active',
+            visibility: 'members',
+            subscribe_on_signup: true,
+            sort_order: 0
+        };
+
+        const designSettings = ['header_image', 'show_header_icon', 'show_header_title', 'title_font_category', 'title_alignment', 'show_feature_image', 'body_font_category', 'footer_content', 'show_badge'];
+        // eslint-disable-next-line no-restricted-syntax
+        for (const setting of designSettings) {
+            let value = await getSetting('newsletter_' + setting);
+            if (['show_badge', 'show_feature_image', 'show_header_icon', 'show_header_title'].includes(setting)) {
+                value = value === 'true';
+            }
+            newsletter[setting] = value;
+        }
+
+        await knex('newsletters').insert(newsletter);
+    }, async function down() {
+        // Noop because we don't want to reset the default newsletter values
+        logging.info(`Skipping newsletter design settings backfill rollack - not needed`);
+        return Promise.resolve();
+    }
+);
