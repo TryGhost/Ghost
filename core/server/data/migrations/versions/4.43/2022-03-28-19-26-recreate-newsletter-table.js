814059d6e94db7a73fc384e6ce5d243069820ccd
Rishabh Garg <zrishabhgarg@gmail.com>
2022-04-11 15:05:02 +0530

    Fixed newsletter schema column validation (#14456)
    refs https://github.com/TryGhost/Team/issues/1500
    
    - newsletter schema columns were using incorrect syntax for `isIn` validations
    - adds schema test to make sure schema `isIn` validations are in correct format

--- core/server/data/migrations/versions/4.43/2022-03-28-19-26-recreate-newsletter-table.js
+++ core/server/data/migrations/versions/4.43/2022-03-28-19-26-recreate-newsletter-table.js
@@ -7,7 +7,7 @@ module.exports = recreateTable('newsletters', {
     slug: {type: 'string', maxlength: 191, nullable: false, unique: true},
     sender_name: {type: 'string', maxlength: 191, nullable: false},
     sender_email: {type: 'string', maxlength: 191, nullable: true},
-    sender_reply_to: {type: 'string', maxlength: 191, nullable: false, defaultTo: 'newsletter', validations: {isIn: ['newsletter', 'support']}},
+    sender_reply_to: {type: 'string', maxlength: 191, nullable: false, defaultTo: 'newsletter', validations: {isIn: [['newsletter', 'support']]}},
     status: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'active'},
     visibility: {
         type: 'string',
@@ -20,10 +20,10 @@ module.exports = recreateTable('newsletters', {
     header_image: {type: 'string', maxlength: 2000, nullable: true},
     show_header_icon: {type: 'bool', nullable: false, defaultTo: true},
     show_header_title: {type: 'bool', nullable: false, defaultTo: true},
-    title_font_category: {type: 'string', maxlength: 191, nullable: false, defaultTo: 'sans_serif', validations: {isIn: ['serif', 'sans_serif']}},
-    title_alignment: {type: 'string', maxlength: 191, nullable: false, defaultTo: 'center', validations: {isIn: ['center', 'left']}},
+    title_font_category: {type: 'string', maxlength: 191, nullable: false, defaultTo: 'sans_serif', validations: {isIn: [['serif', 'sans_serif']]}},
+    title_alignment: {type: 'string', maxlength: 191, nullable: false, defaultTo: 'center', validations: {isIn: [['center', 'left']]}},
     show_feature_image: {type: 'bool', nullable: false, defaultTo: true},
-    body_font_category: {type: 'string', maxlength: 191, nullable: false, defaultTo: 'sans_serif', validations: {isIn: ['serif', 'sans_serif']}},
+    body_font_category: {type: 'string', maxlength: 191, nullable: false, defaultTo: 'sans_serif', validations: {isIn: [['serif', 'sans_serif']]}},
     footer_content: {type: 'text', maxlength: 1000000000, nullable: true},
     show_badge: {type: 'bool', nullable: false, defaultTo: true}
 });
