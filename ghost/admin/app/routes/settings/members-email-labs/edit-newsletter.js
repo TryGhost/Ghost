e193ff15a5f36ee5d1f9da38e665b1a6a96fe688
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-04-12 21:32:11 +0100

    Added unsaved changes confirmation modals to new/edit newsletter routes
    closes https://github.com/TryGhost/Team/issues/1475
    
    - updated new+edit newsletter routes to open a confirmation modal if the form modal is closed or the route is exited
    - added z-index to modal background so the layering behaves as expected when modals are stacked

--- ghost/admin/app/routes/settings/members-email-labs/edit-newsletter.js
+++ ghost/admin/app/routes/settings/members-email-labs/edit-newsletter.js
@@ -1,5 +1,6 @@
 import AdminRoute from 'ghost-admin/routes/admin';
-import EditNewsletterModal from 'ghost-admin/components/modals/edit-newsletter';
+import ConfirmUnsavedChangesModal from '../../../components/modals/confirm-unsaved-changes';
+import EditNewsletterModal from '../../../components/modals/edit-newsletter';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
 
@@ -25,23 +26,72 @@ export default class EditNewsletterRoute extends AdminRoute {
         });
     }
 
+    @action
+    afterSave() {
+        this.router.transitionTo('settings.members-email-labs');
+    }
+
     deactivate() {
         this.isLeaving = true;
         this.newsletterModal?.close();
 
         this.isLeaving = false;
         this.newsletterModal = null;
+
+        this.confirmModal = null;
+        this.hasConfirmed = false;
     }
 
     @action
-    afterSave() {
-        this.router.transitionTo('settings.members-email-labs');
-    }
-
-    @action
-    beforeModalClose() {
-        if (this.newsletterModal && !this.isLeaving) {
-            this.router.transitionTo('settings.members-email-labs');
+    async willTransition(transition) {
+        if (this.hasConfirmed) {
+            return true;
         }
+
+        transition.abort();
+
+        // wait for any existing confirm modal to be closed before allowing transition
+        if (this.confirmModal) {
+            return;
+        }
+
+        const shouldLeave = await this.confirmUnsavedChanges();
+
+        if (shouldLeave) {
+            this.hasConfirmed = true;
+            return transition.retry();
+        }
+    }
+
+    async confirmUnsavedChanges() {
+        const newsletter = this.newsletterModal?._data.newsletter;
+
+        if (newsletter?.hasDirtyAttributes) {
+            this.confirmModal = this.modals.open(ConfirmUnsavedChangesModal)
+                .then((discardChanges) => {
+                    if (discardChanges === true) {
+                        newsletter.rollbackAttributes();
+                    }
+                    return discardChanges;
+                }).finally(() => {
+                    this.confirmModal = null;
+                });
+
+            return this.confirmModal;
+        }
+
+        return true;
+    }
+
+    @action
+    async beforeModalClose() {
+        const shouldLeave = await this.confirmUnsavedChanges();
+
+        if (shouldLeave && !this.isLeaving) {
+            this.router.transitionTo('settings.members-email-labs');
+            return true;
+        }
+
+        return false;
     }
 }
