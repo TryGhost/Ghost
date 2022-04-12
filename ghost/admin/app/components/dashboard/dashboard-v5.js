23fcb7a10c29c77738ccc1b31dd713b6ae7547e0
James Morris <moreofmorris@users.noreply.github.com>
2022-04-12 16:03:52 +0100

    Moved the new dashboard over to a CSS Grid layout and cleaned up a ton of the code
    refs: https://github.com/TryGhost/Team/issues/1507
    
    - changed the layout over to use css grids instead of the random flex and divs
    - cleaned up the based dashboard-v5 file so its rendering much cleaner components
    - refactored and took out a ton of old css that wasn't needed anymore
    - moved the dashboard-v5 css into it's own css file to keep things cleaner
    - includes a few stylistic changes along the way

--- ghost/admin/app/components/dashboard/dashboard-v5.js
+++ ghost/admin/app/components/dashboard/dashboard-v5.js
@@ -2,40 +2,12 @@ import Component from '@glimmer/component';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
 
-const DAYS_OPTIONS = [{
-    name: '7 Days',
-    value: 7
-}, {
-    name: '30 Days',
-    value: 30
-}, {
-    name: '90 Days',
-    value: 90
-}, {
-    name: 'All Time',
-    value: 'all'
-}];
-
 export default class DashboardDashboardV5Component extends Component {
     @service dashboardStats;
-
-    daysOptions = DAYS_OPTIONS;
-
-    get days() {
-        return this.dashboardStats.chartDays;
-    }
-
-    set days(days) {
-        this.dashboardStats.chartDays = days;
-    }
 
     @action
     onInsert() {
         this.dashboardStats.loadSiteStatus();
-    }
-
-    get selectedDaysOption() {
-        return this.daysOptions.find(d => d.value === this.days);
     }
 
     get isLoading() {
@@ -52,10 +24,5 @@ export default class DashboardDashboardV5Component extends Component {
 
     get areMembersEnabled() {
         return this.dashboardStats.siteStatus?.membersEnabled;
-    }
-
-    @action 
-    onDaysChange(selected) {
-        this.days = selected.value;
     }
 }
