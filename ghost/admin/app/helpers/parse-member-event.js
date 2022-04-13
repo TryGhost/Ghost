2072209d4df6f428529090125c4854e815eb6dcb
Fabien 'egg' O'Carroll <fabien@allou.is>
2022-04-13 10:28:13 +0100

    Updated subscription event handling (#2332)
    refs https://github.com/TryGhost/Team/issues/1302
    
    * Tweaked the getIcon() function to better support the new event data variable for cancelled
    * Added back in info for events across all activity lists
    
    Co-authored-by: James Morris <moreofmorris@users.noreply.github.com>

--- ghost/admin/app/helpers/parse-member-event.js
+++ ghost/admin/app/helpers/parse-member-event.js
@@ -51,11 +51,7 @@ function getIcon(event) {
     if (event.type === 'subscription_event') {
         icon = 'subscriptions';
 
-        if (event.data.from_plan === null) {
-            icon = 'subscriptions';
-        }
-
-        if (event.data.to_plan === null) {
+        if (event.data.type === 'canceled') {
             icon = 'canceled-subscription';
         }
     }
@@ -97,12 +93,20 @@ function getAction(event) {
     }
 
     if (event.type === 'subscription_event') {
-        if (event.data.from_plan === null) {
+        if (event.data.type === 'created') {
             return 'started';
         }
-
-        if (event.data.to_plan === null) {
-            return 'cancelled';
+        if (event.data.type === 'updated') {
+            return 'changed';
+        }
+        if (event.data.type === 'canceled') {
+            return 'canceled';
+        }
+        if (event.data.type === 'reactivated') {
+            return 'reactivated';
+        }
+        if (event.data.type === 'expired') {
+            return 'ended';
         }
 
         return 'changed';
