b86cf527bde3c0c05a5bc6fbf370890c7b532010
Simon Backx <simon@ghost.org>
2022-04-13 15:36:12 +0200

    Cleaned up email chart

--- ghost/admin/app/components/dashboard/v5/charts/email.js
+++ ghost/admin/app/components/dashboard/v5/charts/email.js
@@ -13,7 +13,6 @@ export default class Email extends Component {
         // The dashboard stats service will take care or reusing and limiting API-requests between charts
         this.dashboardStats.loadNewsletterSubscribers();
         this.dashboardStats.loadEmailsSent();
-        this.dashboardStats.loadEmailOpenRateStats();
     }
     
     get dataSubscribers() {
@@ -25,113 +24,7 @@ export default class Email extends Component {
         };
     }
 
-    get currentOpenRate() {
-        if (this.dashboardStats.emailOpenRateStats === null || this.dashboardStats.emailOpenRateStats.length === 0) {
-            return '-';
-        }
-
-        return this.dashboardStats.emailOpenRateStats[this.dashboardStats.emailOpenRateStats.length - 1].openRate;
-    }
-
     get dataEmailsSent() {
         return this.dashboardStats.emailsSent30d ?? 0;
-    }
-
-    get loading() {
-        return this.dashboardStats.emailOpenRateStats === null;
-    }
-
-    get chartType() {
-        return 'bar';
-    }
-
-    get chartData() {
-        const stats = this.dashboardStats.emailOpenRateStats;
-        const labels = stats.map(stat => stat.subject);
-        const data = stats.map(stat => stat.openRate);
-
-        return {
-            labels,
-            datasets: [{
-                data,
-                fill: false,
-                backgroundColor: '#14B8FF',
-                cubicInterpolationMode: 'monotone',
-                barThickness: 18
-            }]
-        };
-    }
-
-    get chartOptions() {
-        return {
-            responsive: true,
-            maintainAspectRatio: false,
-            title: {
-                display: false
-            },
-            legend: {
-                display: false
-            },
-            hover: {
-                onHover: function (e) {
-                    e.target.style.cursor = 'pointer';
-                }
-            },
-            tooltips: {
-                intersect: false,
-                mode: 'index',
-                displayColors: false,
-                backgroundColor: '#15171A',
-                xPadding: 7,
-                yPadding: 7,
-                cornerRadius: 5,
-                caretSize: 7,
-                caretPadding: 5,
-                bodyFontSize: 12.5,
-                titleFontSize: 12,
-                titleFontStyle: 'normal',
-                titleFontColor: 'rgba(255, 255, 255, 0.7)',
-                titleMarginBottom: 3
-            },
-            scales: {
-                yAxes: [{
-                    gridLines: {
-                        drawTicks: false,
-                        display: false,
-                        drawBorder: false
-                    },
-                    ticks: {
-                        display: false,
-                        maxTicksLimit: 5,
-                        fontColor: '#7C8B9A',
-                        padding: 8,
-                        precision: 0
-                    }
-                }],
-                xAxes: [{
-                    gridLines: {
-                        color: '#DDE1E5',
-                        borderDash: [4,4],
-                        display: true,
-                        drawBorder: true,
-                        drawTicks: false,
-                        zeroLineWidth: 1,
-                        zeroLineColor: '#DDE1E5',
-                        zeroLineBorderDash: [4,4]
-                    },
-                    ticks: {
-                        display: false,
-                        maxTicksLimit: 5,
-                        autoSkip: true,
-                        maxRotation: 0,
-                        minRotation: 0
-                    }
-                }]
-            }
-        };
-    }
-
-    get chartHeight() {
-        return 150;
     }
 }
