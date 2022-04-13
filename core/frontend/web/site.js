8094d272fb7ededbc9ee33586bf05e1a8280f5c9
Hannah Wolfe <github.erisds@gmail.com>
2022-04-13 11:32:11 +0100

    Moved cors middleware to own file
    - This cors middleware function has been stuck at the top of the site file for a while
    - Move it to it's own file in keeping with our usual patterns for middleware

--- core/frontend/web/site.js
+++ core/frontend/web/site.js
@@ -1,9 +1,6 @@
 const debug = require('@tryghost/debug')('frontend');
 const path = require('path');
 const express = require('../../shared/express');
-const cors = require('cors');
-const {URL} = require('url');
-const errors = require('@tryghost/errors');
 const DomainEvents = require('@tryghost/domain-events');
 const {MemberPageViewEvent} = require('@tryghost/member-events');
 
@@ -31,50 +28,6 @@ const STATIC_FILES_URL_PREFIX = `/${constants.STATIC_FILES_URL_PREFIX}`;
 
 let router;
 
-const corsOptionsDelegate = function corsOptionsDelegate(req, callback) {
-    const origin = req.header('Origin');
-    const corsOptions = {
-        origin: false, // disallow cross-origin requests by default
-        credentials: true // required to allow admin-client to login to private sites
-    };
-
-    if (!origin || origin === 'null') {
-        return callback(null, corsOptions);
-    }
-
-    let originUrl;
-    try {
-        originUrl = new URL(origin);
-    } catch (err) {
-        return callback(new errors.BadRequestError({err}));
-    }
-
-    // originUrl will definitely exist here because according to WHATWG URL spec
-    // The class constructor will either throw a TypeError or return a URL object
-    // https://url.spec.whatwg.org/#url-class
-
-    // allow all localhost and 127.0.0.1 requests no matter the port
-    if (originUrl.hostname === 'localhost' || originUrl.hostname === '127.0.0.1') {
-        corsOptions.origin = true;
-    }
-
-    // allow the configured host through on any protocol
-    const siteUrl = new URL(config.get('url'));
-    if (originUrl.host === siteUrl.host) {
-        corsOptions.origin = true;
-    }
-
-    // allow the configured admin:url host through on any protocol
-    if (config.get('admin:url')) {
-        const adminUrl = new URL(config.get('admin:url'));
-        if (originUrl.host === adminUrl.host) {
-            corsOptions.origin = true;
-        }
-    }
-
-    callback(null, corsOptions);
-};
-
 function SiteRouter(req, res, next) {
     router(req, res, next);
 }
@@ -89,7 +42,7 @@ module.exports = function setupSiteApp(options = {}) {
     siteApp.set('view engine', 'hbs');
 
     // enable CORS headers (allows admin client to hit front-end when configured on separate URLs)
-    siteApp.use(cors(corsOptionsDelegate));
+    siteApp.use(mw.cors);
 
     siteApp.use(offersService.middleware);
 
