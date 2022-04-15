4ebd4e56322732323957cd0960a5a7c9874b7955
Thibaut Patel <thibaut@ghost.org>
2022-04-15 17:04:42 +0200

    switched to Matt's implementation
    - removed loading the full lodash library in favor for only loading the `startsWith` library

--- core/server/data/migrations/versions/4.44/2022-04-13-13-00-add-default-newsletter.js
+++ core/server/data/migrations/versions/4.44/2022-04-13-13-00-add-default-newsletter.js
@@ -1,28 +1,31 @@
 const ObjectId = require('bson-objectid');
 const logging = require('@tryghost/logging');
+const startsWith = require('lodash/startsWith');
 const {createTransactionalMigration} = require('../../utils');
 
-const newsletterPrefixRegexp = /^newsletter_/;
-
-function forceBoolean(string) {
-    return string === 'true';
-}
-
-// This also contains the default settings from core/server/data/schema/default-settings/default-settings.json
-// The default settings are needed to avoid an edge case where this migration runs before the settings are populated
-const settingsConfig = [
-    {setting: 'title', default: 'Ghost'},
-    {setting: 'description', default: ''},
-    {setting: 'newsletter_body_font_category', default: 'sans_serif'},
-    {setting: 'newsletter_footer_content', default: ''},
-    {setting: 'newsletter_header_image', default: null},
-    {setting: 'newsletter_show_badge', default: 'true', modifier: forceBoolean},
-    {setting: 'newsletter_show_feature_image', default: 'true', modifier: forceBoolean},
-    {setting: 'newsletter_show_header_icon', default: 'true', modifier: forceBoolean},
-    {setting: 'newsletter_show_header_title', default: 'true', modifier: forceBoolean},
-    {setting: 'newsletter_title_alignment', default: 'center'},
-    {setting: 'newsletter_title_font_category', default: 'sans_serif'}
-];
+// This uses the default settings from core/server/data/schema/default-settings/default-settings.json
+const newsletter = {
+    id: (new ObjectId()).toHexString(),
+    name: 'Ghost',
+    description: '',
+    slug: 'default-newsletter',
+    sender_name: null,
+    sender_email: null,
+    sender_reply_to: 'newsletter',
+    status: 'active',
+    visibility: 'members',
+    subscribe_on_signup: true,
+    sort_order: 0,
+    body_font_category: 'sans_serif',
+    footer_content: '',
+    header_image: null,
+    show_badge: true,
+    show_feature_image: true,
+    show_header_icon: true,
+    show_header_title: true,
+    title_alignment: 'center',
+    title_font_category: 'sans_serif'
+};
 
 module.exports = createTransactionalMigration(
     async function up(knex) {
@@ -36,49 +39,42 @@ module.exports = createTransactionalMigration(
 
         // Get all settings in one query
         const settings = await knex('settings')
-            .whereIn('key', settingsConfig.map(config => config.setting))
+            .whereIn('key', [
+                'title',
+                'description',
+                'newsletter_body_font_category',
+                'newsletter_footer_content',
+                'newsletter_header_image',
+                'newsletter_show_badge',
+                'newsletter_show_feature_image',
+                'newsletter_show_header_icon',
+                'newsletter_show_header_title',
+                'newsletter_title_alignment',
+                'newsletter_title_font_category'
+            ])
             .select(['key', 'value']);
 
-        // Create a settings map taking into account the default value
-        const settingsMap = {};
         // eslint-disable-next-line no-restricted-syntax
-        for (const config of settingsConfig) {
-            const setting = settings.find(s => s.key === config.setting);
-            if (setting) {
-                settingsMap[config.setting] = setting.value;
+        for (let {key, value} of settings) {
+            // Use site title for the newsletter name
+            if (key === 'title') {
+                key = 'name';
+            }
+            // Settings have a `newsletter_` prefix which isn't present on the newsletters table
+            if (startsWith(key, 'newsletter_')) {
+                key = key.slice(11);
+            }
+            if (typeof newsletter[key] === 'boolean') {
+                newsletter[key] = value === 'true';
             } else {
-                settingsMap[config.setting] = config.default;
+                newsletter[key] = value;
             }
-            if (config.modifier) {
-                settingsMap[config.setting] = config.modifier(settingsMap[config.setting]);
-            }
-        }
-
-        const newsletter = {
-            id: ObjectId().toHexString(),
-            name: settingsMap.title,
-            description: settingsMap.description,
-            slug: 'default-newsletter',
-            sender_name: null,
-            sender_email: null,
-            sender_reply_to: 'newsletter',
-            status: 'active',
-            visibility: 'members',
-            subscribe_on_signup: true,
-            sort_order: 0
-        };
-
-        // Special case for the design settings because we are getting rid of the `newsletter_` prefix
-        const designSettings = settingsConfig.filter(config => newsletterPrefixRegexp.test(config.setting));
-        // eslint-disable-next-line no-restricted-syntax
-        for (const config of designSettings) {
-            newsletter[config.setting.replace(newsletterPrefixRegexp, '')] = settingsMap[config.setting];
         }
 
         logging.info('Adding the default newsletter');
-
         await knex('newsletters').insert(newsletter);
-    }, async function down() {
+    },
+    async function down() {
         // Noop because we don't want to reset the default newsletter values
         logging.info(`Skipping newsletter design settings backfill rollack - not needed`);
         return Promise.resolve();
