67dca08df846684352a9570e5b5e23946c9064fd
Naz <nazargargol@gmail.com>
2022-04-15 10:55:52 +0800

    Refactored trigger module to be testable
    refs https://github.com/TryGhost/Toolbox/issues/283
    
    - Current trigger module handling webhook paypload delivery isn't testable! It sucks to add features to it without assurance things still work
    - Apart from expanding the test suite this changeset also needs live testing - setting up webhooks etc.

--- core/server/services/webhooks/trigger.js
+++ core/server/services/webhooks/trigger.js
@@ -1,19 +1,22 @@
-const _ = require('lodash');
 const debug = require('@tryghost/debug')('services:webhooks:trigger');
 const logging = require('@tryghost/logging');
 const request = require('@tryghost/request');
-const models = require('../../models');
-const payload = require('./payload');
+const ghostVersion = require('@tryghost/version');
 
-const webhooks = {
+class WebhookTrigger {
+    constructor({models, payload}){
+        this.models = models;
+        this.payload = payload;
+    }
+
     getAll(event) {
-        return models
+        return this.models
             .Webhook
             .findAllByEvent(event, {context: {internal: true}});
-    },
+    }
 
     update(webhook, data) {
-        models
+        this.models
             .Webhook
             .edit({
                 last_triggered_at: Date.now(),
@@ -23,36 +26,34 @@ const webhooks = {
             .catch(() => {
                 logging.warn(`Unable to update "last_triggered" for webhook: ${webhook.id}`);
             });
-    },
+    }
 
     destroy(webhook) {
-        return models
+        return this.models
             .Webhook
             .destroy({id: webhook.id}, {context: {internal: true}})
             .catch(() => {
                 logging.warn(`Unable to destroy webhook ${webhook.id}.`);
             });
     }
-};
 
-const response = {
     onSuccess(webhook) {
         return (res) => {
-            webhooks.update(webhook, {
+            this.update(webhook, {
                 statusCode: res.statusCode
             });
         };
-    },
+    }
 
     onError(webhook) {
         return (err) => {
             if (err.statusCode === 410) {
                 logging.info(`Webhook destroyed (410 response) for "${webhook.get('event')}" with url "${webhook.get('target_url')}".`);
 
-                return webhooks.destroy(webhook);
+                return this.destroy(webhook);
             }
 
-            webhooks.update(webhook, {
+            this.update(webhook, {
                 statusCode: err.statusCode,
                 error: `Request failed: ${err.code || 'unknown'}`
             });
@@ -60,34 +61,39 @@ const response = {
             logging.warn(`Request to ${webhook.get('target_url') || null} failed because of: ${err.code || ''}.`);
         };
     }
-};
 
-module.exports = (event, model) => {
-    webhooks.getAll(event)
-        .then((hooks) => {
-            debug(`${hooks.models.length} webhooks found for ${event}.`);
+    async trigger(event, model) {
+        const response = {
+            onSuccess: this.onSuccess.bind(this),
+            onError: this.onError.bind(this)
+        };
 
-            _.each(hooks.models, (webhook) => {
-                payload(webhook.get('event'), model)
-                    .then((hookPayload) => {
-                        const reqPayload = JSON.stringify(hookPayload);
-                        const url = webhook.get('target_url');
-                        const opts = {
-                            body: reqPayload,
-                            headers: {
-                                'Content-Length': Buffer.byteLength(reqPayload),
-                                'Content-Type': 'application/json'
-                            },
-                            timeout: 2 * 1000,
-                            retry: 5
-                        };
+        const hooks = await this.getAll(event);
 
-                        logging.info(`Triggering webhook for "${webhook.get('event')}" with url "${url}"`);
+        debug(`${hooks.models.length} webhooks found for ${event}.`);
 
-                        request(url, opts)
-                            .then(response.onSuccess(webhook))
-                            .catch(response.onError(webhook));
-                    });
-            });
+        hooks.models.forEach(async (webhook) => {
+            const hookPayload = await this.payload(webhook.get('event'), model);
+
+            const reqPayload = JSON.stringify(hookPayload);
+            const url = webhook.get('target_url');
+            const opts = {
+                body: reqPayload,
+                headers: {
+                    'Content-Length': Buffer.byteLength(reqPayload),
+                    'Content-Type': 'application/json'
+                },
+                timeout: 2 * 1000,
+                retry: 5
+            };
+
+            logging.info(`Triggering webhook for "${webhook.get('event')}" with url "${url}"`);
+
+            request(url, opts)
+                .then(response.onSuccess(webhook))
+                .catch(response.onError(webhook));
         });
-};
+    }
+}
+
+module.exports = WebhookTrigger;
