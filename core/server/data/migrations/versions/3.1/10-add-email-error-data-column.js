ccc3fcc9f7bd077465915beed89ca3243ac1fb9a
Daniel Lockyer <hi@daniellockyer.com>
2022-04-19 21:43:04 +0100

    Deleted all v1/v2/v3 migrations
    refs https://github.com/TryGhost/Toolbox/issues/300
    
    - due to Node compatibility, it only makes sense that users on the latest
      v3 (and v4) can update to v5
    - therefore, we don't need v1/2/3 migrations as it's more maintenance in
      the long run
    - this deletes over 5000 lines of code (!!)

--- core/server/data/migrations/versions/3.1/10-add-email-error-data-column.js
+++
@@ -1,8 +0,0 @@
-const {createAddColumnMigration} = require('../../utils');
-
-module.exports = createAddColumnMigration('emails', 'error_data', {
-    type: 'text',
-    maxlength: 1000000000,
-    fieldtype: 'long',
-    nullable: true
-});
--- core/server/data/migrations/versions/3.12/01-add-identity-permission.js
+++
@@ -1,7 +0,0 @@
-const {addPermission} = require('../../utils');
-
-module.exports = addPermission({
-    name: 'Read identities',
-    action: 'read',
-    object: 'identity'
-});
