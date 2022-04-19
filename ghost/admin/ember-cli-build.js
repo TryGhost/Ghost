070db905b081ba60b1970ca26964a21a8bf5e9ea
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-04-19 21:56:13 +0100

    Added auto-subscribe newsletter count when confirming newsletter creation
    refs https://github.com/TryGhost/Team/issues/1528
    
    - used the already-loaded newsletters list to pass the slugs of active newsletters to the confirm modal, this allows use of the `{{members-count-fetcher}}` resource with the query `/members/?filter=newsletters:[...slugs]` to fetch a member and full count metadata
    - added inclusion of `{{join}}` helper from `ember-composable-helpers` addon

--- ghost/admin/ember-cli-build.js
+++ ghost/admin/ember-cli-build.js
@@ -117,7 +117,7 @@ module.exports = function (defaults) {
             includePolyfill: false
         },
         'ember-composable-helpers': {
-            only: ['optional', 'pick', 'toggle', 'toggle-action']
+            only: ['join', 'optional', 'pick', 'toggle', 'toggle-action']
         },
         'ember-promise-modals': {
             excludeCSS: true
