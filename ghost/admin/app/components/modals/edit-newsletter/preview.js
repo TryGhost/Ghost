6d5c31c16573886c502b1a0161388f534f6c5f9c
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-04-20 16:04:58 +0100

    Added `showHeaderName` newsletter attr for controlling newsletter title/subtitle
    refs https://github.com/TryGhost/Team/issues/1532
    
    - added `showHeaderName` attribute to newsletter model
    - added attribute toggle to the newsletter design settings form
    - updated newsletter preview behaviour
      - if both title and name are on, show name as a subtitle
      - if title is off but name is on, show name as main title
      - adjusted higher-level "show header" and "show header title" conditionals to account for the new setting

--- ghost/admin/app/components/modals/edit-newsletter/preview.js
+++ ghost/admin/app/components/modals/edit-newsletter/preview.js
@@ -3,14 +3,35 @@ import {htmlSafe} from '@ember/template';
 import {inject as service} from '@ember/service';
 
 export default class EditNewsletterPreview extends Component {
-    @service config;
     @service ghostPaths;
     @service session;
     @service settings;
 
     get showHeader() {
         return (this.args.newsletter.showHeaderIcon && this.settings.get('icon'))
-            || this.args.newsletter.showHeaderTitle;
+            || this.headerTitle;
+    }
+
+    get showHeaderTitle() {
+        return this.headerTitle || this.headerSubtitle;
+    }
+
+    get headerTitle() {
+        if (this.args.newsletter.showHeaderTitle) {
+            return this.settings.get('title');
+        } else if (this.args.newsletter.showHeaderName) {
+            return this.args.newsletter.name;
+        }
+
+        return null;
+    }
+
+    get headerSubtitle() {
+        if (this.args.newsletter.showHeaderTitle && this.args.newsletter.showHeaderName) {
+            return this.args.newsletter.name;
+        }
+
+        return null;
     }
 
     get featureImageUrl() {
