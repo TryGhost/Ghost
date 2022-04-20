4c4fca80055d1cb6dc9d67a5e2233b8ed7f784ce
Thibaut Patel <thibaut@ghost.org>
2022-04-20 15:25:41 +0200

    Added newsletter read permission (#14519)
    refs https://github.com/TryGhost/Team/issues/1533
    
    - Needed to create the read newsletter endpoint to make the newsletter resource more consistent with the other resources
    - Read is available to admins like other newsletter actions

--- test/integration/migrations/migration.test.js
+++ test/integration/migrations/migration.test.js
@@ -45,7 +45,7 @@ describe('Database Migration (special functions)', function () {
             const permissions = this.obj;
 
             // If you have to change this number, please add the relevant `havePermission` checks below
-            permissions.length.should.eql(95);
+            permissions.length.should.eql(96);
 
             permissions.should.havePermission('Export database', ['Administrator', 'DB Backup Integration']);
             permissions.should.havePermission('Import database', ['Administrator', 'DB Backup Integration']);
@@ -164,6 +164,7 @@ describe('Database Migration (special functions)', function () {
             permissions.should.havePermission('Edit custom theme settings', ['Administrator']);
 
             permissions.should.havePermission('Browse newsletters', ['Administrator']);
+            permissions.should.havePermission('Read newsletters', ['Administrator']);
             permissions.should.havePermission('Edit newsletters', ['Administrator']);
             permissions.should.havePermission('Add newsletters', ['Administrator']);
         });
@@ -223,7 +224,7 @@ describe('Database Migration (special functions)', function () {
                     result.roles.at(7).get('name').should.eql('Scheduler Integration');
 
                     // Permissions
-                    result.permissions.length.should.eql(95);
+                    result.permissions.length.should.eql(96);
                     result.permissions.toJSON().should.be.CompletePermissions();
                 });
             });
