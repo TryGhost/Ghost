188707c5bb909a28269a6eff43273e2bfb34a663
Naz <nazargargol@gmail.com>
2022-04-20 11:08:56 +0800

    Added previously handled checking logic
    refs https://github.com/TryGhost/Toolbox/issues/292
    
    - Added additional logic needed to check if the passed in `Accept-Version` header has been handled

--- ghost/api-version-compatibility-service/lib/api-version-compatibility-service.js
+++ ghost/api-version-compatibility-service/lib/api-version-compatibility-service.js
@@ -1,15 +1,27 @@
 class APIVersionCompatibilityService {
-    constructor({sendEmail}) {
+    /**
+     *
+     * @param {Object} options
+     * @param {Function} options.sendEmail - email sending function
+     * @param {(acceptVersion: String) => Promise<any>} options.fetchHandled - retrives already handled compatibility notifications
+     * @param {(acceptVersion: String) => Promise<any>} options.saveHandled - persists already handled compatibility notifications
+    */
+    constructor({sendEmail, fetchHandled, saveHandled}) {
         this.sendEmail = sendEmail;
+        this.fetchHandled = fetchHandled;
+        this.saveHandled = saveHandled;
     }
 
-    handleMismatch({acceptVersion, contentVersion, userAgent}) {
-        const emailTemplate = `
-            ${userAgent} integration expected Ghost version: ${acceptVersion}
-            Current Ghost version: ${contentVersion}
-        `;
+    async handleMismatch({acceptVersion, contentVersion, userAgent}) {
+        if (!await this.fetchHandled(acceptVersion)) {
+            const emailTemplate = `
+                ${userAgent} integration expected Ghost version: ${acceptVersion}
+                Current Ghost version: ${contentVersion}
+            `;
 
-        this.sendEmail(emailTemplate);
+            await this.sendEmail(emailTemplate);
+            await this.saveHandled(acceptVersion);
+        }
     }
 }
 
