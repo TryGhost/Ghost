5d839a472520ddfeae49696760d2f8921a4ad894
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-04-21 10:35:24 +0100

    Added Ctrl/Cmd+Shift+P shortcut for starting publish flow
    refs https://github.com/TryGhost/Team/issues/1542
    
    With the old publish menu flow the `Ctrl/Cmd+Shift+P` would immediately publish without showing any of the publish options which isn't ideal. For the redesign we'd like to try showing the publish flow modal instead.
    
    - added key handler for the shortcut to the publish-flow triggering button
    - added `ignoreBackdropClick` option to the publish flow modal and support for it in our modal backdrop click handler
      - the shortcut simulates a click on the button which when the modal is open equates to a backdrop click meaning repeated presses of the Cmd+Shift+P shortcut resulted in glitchy modal open/close behaviour

--- ghost/admin/app/components/modals/editor-labs/publish-flow.js
+++ ghost/admin/app/components/modals/editor-labs/publish-flow.js
@@ -3,6 +3,7 @@ import Component from '@glimmer/component';
 export default class PublishModalComponent extends Component {
     static modalOptions = {
         className: 'fullscreen-modal-total-overlay',
-        omitBackdrop: true
+        omitBackdrop: true,
+        ignoreBackdropClick: true
     };
 }
