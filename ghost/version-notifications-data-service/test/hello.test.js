04b557038f41d755744aaa16dd8c9c7668ea8b8f
Naz <nazargargol@gmail.com>
2022-04-22 13:45:27 +0800

    Added scaffolding for version notification data service
    refs https://github.com/TryGhost/Toolbox/issues/292
    
    - This will be filled up with version nofitication data access logic

---
+++ ghost/version-notifications-data-service/test/hello.test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+describe('Version Notification Data Service', function () {
+    it('Runs a test', function () {
+        // TODO: Write me!
+        assert.equal('hello', 'hello');
+    });
+});
