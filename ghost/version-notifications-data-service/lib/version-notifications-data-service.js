55309242750fd53dca51e9b6319937f1a70dc966
Naz <nazargargol@gmail.com>
2022-04-22 14:46:02 +0800

    Implemented a version notification data service
    refs https://github.com/TryGhost/Toolbox/issues/292
    
    - The version notification data service serves as data manger for version compatibility service
    - There's not much logic mostly fetching data from proficed services and filtering/serializing it into a needed format

---
+++ ghost/version-notifications-data-service/lib/version-notifications-data-service.js
@@ -0,0 +1,49 @@
+const internalContext = {
+    internal: true
+};
+
+class VersionNotificationsDataService {
+    constructor({UserModel, settingsService}) {
+        this.UserModel = UserModel;
+        this.settingsService = settingsService;
+    }
+
+    async fetchNotification(acceptVersion) {
+        const setting = await this.settingsService.read('version_notifications', internalContext);
+        const versionNotifications = JSON.parse(setting.version_notifications.value);
+
+        return versionNotifications.find(version => version === acceptVersion);
+    }
+
+    async saveNotification(acceptVersion) {
+        const setting = await this.settingsService.read('version_notifications', internalContext);
+        const versionNotifications = JSON.parse(setting.version_notifications.value);
+
+        if (!versionNotifications.find(version => version === acceptVersion)) {
+            versionNotifications.push(acceptVersion);
+
+            return this.settingsService.edit([{
+                key: 'version_notifications',
+                value: JSON.stringify(versionNotifications)
+            }], {
+                context: internalContext
+            });
+        }
+    }
+
+    async getNotificationEmails() {
+        const data = await this.UserModel.findAll(Object.assign({
+            withRelated: ['roles'],
+            filter: 'status:active'
+        }, internalContext));
+
+        const adminEmails = data
+            .toJSON()
+            .filter(user => ['Owner', 'Administrator'].includes(user.roles[0].name))
+            .map(user => user.email);
+
+        return adminEmails;
+    }
+}
+
+module.exports = VersionNotificationsDataService;
--- ghost/version-notifications-data-service/test/hello.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-
-describe('Version Notification Data Service', function () {
-    it('Runs a test', function () {
-        // TODO: Write me!
-        assert.equal('hello', 'hello');
-    });
-});
