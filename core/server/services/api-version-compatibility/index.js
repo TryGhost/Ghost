083203d1dd56f6b4d87c02cebe4993dfb9facc8b
Naz <nazargargol@gmail.com>
2022-04-22 14:52:00 +0800

    Extracted data access code to external module
    refs https://github.com/TryGhost/Toolbox/issues/280
    
    - Keeps the code in the core to the minimum. Ideally we should not be adding any logic apart form "hooking things together"

--- core/server/services/api-version-compatibility/index.js
+++ core/server/services/api-version-compatibility/index.js
@@ -1,22 +1,23 @@
 const APIVersionCompatibilityService = require('@tryghost/api-version-compatibility-service');
+const VersionNotificationsDataService = require('@tryghost/version-notifications-data-service');
 const {GhostMailer} = require('../mail');
-
-const {
-    getNotificationEmails,
-    fetchNotification,
-    saveNotification
-} = require('./version-notifications-data-service');
-
-const ghostMailer = new GhostMailer();
+const settingsService = require('../../services/settings');
+const models = require('../../models');
 
 const init = () => {
+    const ghostMailer = new GhostMailer();
+    const versionNotificationsDataService = new VersionNotificationsDataService({
+        UserModel: models.User,
+        settingsService: settingsService.getSettingsBREADServiceInstance()
+    });
+
     this.APIVersionCompatibilityServiceInstance = new APIVersionCompatibilityService({
         sendEmail: (options) => {
             return ghostMailer.send(options);
         },
-        fetchEmailsToNotify: getNotificationEmails,
-        fetchHandled: fetchNotification,
-        saveHandled: saveNotification
+        fetchEmailsToNotify: versionNotificationsDataService.getNotificationEmails.bind(versionNotificationsDataService),
+        fetchHandled: versionNotificationsDataService.fetchNotification.bind(versionNotificationsDataService),
+        saveHandled: versionNotificationsDataService.saveNotification.bind(versionNotificationsDataService)
     });
 };
 
