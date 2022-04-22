d4387acdd2ed0b3a0ef66d8899b69aa421ff057e
Fabien "egg" O'Carroll <fabien@allou.is>
2022-04-22 12:46:31 +0100

    fixup! Backfilled canceled events for subscriptions

--- core/server/data/migrations/versions/5.0/2022-04-21-15-44-backfill-mrr-events-for-canceled-subscriptions.js
+++ core/server/data/migrations/versions/5.0/2022-04-21-15-44-backfill-mrr-events-for-canceled-subscriptions.js
@@ -1,24 +1,26 @@
 const ObjectID = require('bson-objectid').default;
 const logging = require('@tryghost/logging');
 
-const {createIrreversibleMigration} = require('../../utils');
+const {createTransactionalMigration} = require('../../utils');
 
-module.exports = createIrreversibleMigration(
+module.exports = createTransactionalMigration(
     async function up(knex) {
         const subscriptions = await knex('members_stripe_customers_subscriptions')
+            .leftJoin('members_paid_subscription_events', 'members_stripe_customers_subscriptions.id', '=', 'members_paid_subscription_events.subscription_id')
             .join('members_stripe_customers', 'members_stripe_customers_subscriptions.customer_id', '=', 'members_stripe_customers.customer_id')
             .join('members', 'members_stripe_customers.member_id', '=', 'members.id')
-            .where('cancel_at_period_end', true)
-            .whereNotIn('status', ['trialing', 'canceled', 'incomplete', 'incomplete_expired'])
+            .where('members_stripe_customers_subscriptions.cancel_at_period_end', '=', true)
+            .where('members_stripe_customers_subscriptions.mrr', '!=', 0)
+            .where(knex.raw(`(members_paid_subscription_events.type = 'canceled' OR members_paid_subscription_events.type is null)`))
             .select(
+                'members_paid_subscription_events.id AS event_id',
                 'members_stripe_customers_subscriptions.id',
+                'members_stripe_customers_subscriptions.updated_at',
                 'members_stripe_customers_subscriptions.mrr',
                 'members_stripe_customers_subscriptions.stripe_price_id',
                 'members_stripe_customers_subscriptions.plan_currency',
                 'members_stripe_customers.member_id'
             );
-
-        const now = knex.raw('CURRENT_TIMESTAMP');
 
         if (subscriptions.length === 0) {
             logging.info('No canceled subscriptions found, skipping migration.');
@@ -27,21 +29,38 @@ module.exports = createIrreversibleMigration(
             logging.info(`Found ${subscriptions.length} canceled subscriptions, inserting MRR events.`);
         }
 
-        const events = subscriptions.map((subscription) => {
-            return {
-                id: ObjectID().toHexString(),
-                type: 'canceled',
-                source: 'migration',
-                created_at: now,
-                from_plan: subscription.stripe_price_id,
-                to_plan: subscription.stripe_price_id,
-                subscription_id: subscription.id,
-                member_id: subscription.member_id,
-                currency: subscription.plan_currency,
-                mrr_delta: -subscription.mrr
-            };
-        });
+        const {toUpdate, toInsert} = subscriptions.reduce((memo, subscription) => {
+            if (subscription.event_id) {
+                return {
+                    toInsert: memo.toInsert,
+                    toUpdate: memo.toUpdate.concat({
+                        id: subscription.event_id,
+                        mrr_delta: -subscription.mrr
+                    })
+                };
+            }
 
-        await knex.batchInsert('members_paid_subscription_events', events);
-    }
+            return {
+                toUpdate: memo.toUpdate,
+                toInsert: memo.toInsert.concat({
+                    id: ObjectID().toHexString(),
+                    type: 'canceled',
+                    source: 'migration',
+                    created_at: subscription.updated_at,
+                    from_plan: subscription.stripe_price_id,
+                    to_plan: subscription.stripe_price_id,
+                    subscription_id: subscription.id,
+                    member_id: subscription.member_id,
+                    currency: subscription.plan_currency,
+                    mrr_delta: -subscription.mrr
+                })
+            };
+        }, {toUpdate: [], toInsert: []});
+
+        await knex.batchInsert('members_paid_subscription_events', toInsert);
+        for (const event of toUpdate) {
+            await knex('members_paid_subscription_events').update('mrr_delta', event.mrr_delta).where('id', event.id);
+        }
+    },
+    async function down() {}
 );
