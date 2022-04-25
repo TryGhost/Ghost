f1d94cb2dc599aca3e06d95fbd04c8ac87418b49
Simon Backx <git@simonbackx.com>
2022-04-25 16:37:58 +0200

    Swapped Date object with knex.raw CURRENT_TIMESTAMP

--- core/server/data/migrations/versions/4.46/2022-04-13-12-02-fill-created-at-newsletters.js
+++ core/server/data/migrations/versions/4.46/2022-04-13-12-02-fill-created-at-newsletters.js
@@ -6,9 +6,10 @@ module.exports = createTransactionalMigration(
     async function up(knex) {
         logging.info('Setting missing created_at values for existing newsletters');
 
+        const now = knex.raw('CURRENT_TIMESTAMP');
         const updatedRows = await knex('newsletters')
             .where('created_at', null)
-            .update('created_at', new Date());
+            .update('created_at', now);
 
         logging.info(`Updated ${updatedRows} newsletters with created_at = now`);
     },
