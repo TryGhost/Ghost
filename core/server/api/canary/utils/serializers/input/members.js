67aa5d853eeba019c53a8d0c6f630de9456824b0
Matt Hanley <git@matthanley.co.uk>
2022-04-25 16:29:09 +0100

    Added input serialiser mapping

--- core/server/api/canary/utils/serializers/input/members.js
+++ core/server/api/canary/utils/serializers/input/members.js
@@ -2,6 +2,7 @@ const _ = require('lodash');
 const debug = require('@tryghost/debug')('api:canary:utils:serializers:input:members');
 const mapNQLKeyValues = require('@tryghost/nql').utils.mapKeyValues;
 const labsService = require('../../../../../../shared/labs');
+const models = require('../../../../../models');
 
 function defaultRelations(frame) {
     if (frame.options.withRelated) {
@@ -13,6 +14,39 @@ function defaultRelations(frame) {
     }
 
     frame.options.withRelated = ['labels'];
+}
+
+async function mapSubscribedFlagToNewsletters(frame) {
+    // CASE: ignore `subscribed` if `newsletters` is also set
+    if (Object.prototype.hasOwnProperty.call(frame.data.members[0], 'newsletters')) {
+        debug('both subscribed and newsletters present - ignoring subscribed');
+        return;
+    }
+
+    if (frame.data.members[0].subscribed) {
+        // CASE: ensure the member has at least one subscription
+        debug('subscribed=true');
+        if (frame.options.id) {
+            const existingMember = await models.Member.findOne(
+                {id: frame.options.id},
+                {withRelated: ['newsletters']}
+            );
+            if (existingMember && existingMember.related('newsletters').length > 0) {
+                debug('subscribed=true and we have newsletters already - nothing to do');
+                return;
+            }
+        }
+
+        // subscribed=true but we don't have any newsletters
+        debug('subscribing to the default newsletter');
+        const defaultNewsletter = await models.Newsletter.getDefaultNewsletter(frame.options);
+        frame.data.members[0].newsletters = [{id: defaultNewsletter.id}];
+    } else {
+        // CASE: unsubscribe from all emails
+        // subscribed=false -> newsletters=[]
+        debug('unsubscribing from newsletters');
+        frame.data.members[0].newsletters = [];
+    }
 }
 
 module.exports = {
@@ -54,6 +88,7 @@ module.exports = {
                 }
             });
         }
+        mapSubscribedFlagToNewsletters(frame);
         defaultRelations(frame);
     },
 
