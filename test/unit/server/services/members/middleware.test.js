1114cd461970b1e944f6e70b8dba87715ca47397
Rishabh Garg <zrishabhgarg@gmail.com>
2022-04-25 20:38:55 +0530

    Updated portal to manage single newsletter with existing UI (#14573)
    refs https://github.com/TryGhost/Team/issues/1554
    
    - bumps portal to manage single site newsletter with old subscribe UI
    - adds unit test for portal site endpoint

--- test/unit/server/services/members/middleware.test.js
+++ test/unit/server/services/members/middleware.test.js
@@ -1,11 +1,14 @@
 const should = require('should');
 const sinon = require('sinon');
+const ghostVersion = require('@tryghost/version');
 
 const urlUtils = require('../../../../../core/shared/url-utils');
 const membersService = require('../../../../../core/server/services/members');
+const newslettersService = require('../../../../../core/server/services/newsletters');
 const membersMiddleware = require('../../../../../core/server/services/members/middleware');
 const settingsCache = require('../../../../../core/shared/settings-cache');
 const models = require('../../../../../core/server/models');
+const config = require('../../../../../core/shared/config');
 
 describe('Members Service Middleware', function () {
     describe('createSessionFromMagicLink', function () {
@@ -143,6 +146,96 @@ describe('Members Service Middleware', function () {
             next.calledOnce.should.be.false();
             res.redirect.calledOnce.should.be.true();
             res.redirect.firstCall.args[0].should.eql('https://custom.com/paid/');
+        });
+    });
+
+    describe('getMemberSiteData', function () {
+        let req;
+        let res = {};
+        let next;
+
+        before(function () {
+            models.init();
+            sinon.stub(membersService, 'config').get(() => {
+                return {
+                    isStripeConnected: () => {
+                        return true;
+                    },
+                    getAllowSelfSignup: () => {
+                        return true;
+                    }
+                };
+            });
+            sinon.stub(membersService, 'api').get(() => {
+                return {
+                    productRepository: {
+                        list: () => {
+                            return {
+                                data: []
+                            };
+                        }
+                    },
+                    getAllowSelfSignup: () => {
+                        return true;
+                    }
+                };
+            });
+            sinon.stub(newslettersService, 'browse').returns(Promise.resolve([]));
+            sinon.stub(urlUtils, 'urlFor').returns('https://example.com');
+
+            sinon.stub(settingsCache, 'get');
+            sinon.stub(config, 'get');
+            config.get.withArgs('portal:version').returns('1.22');
+            settingsCache.get.withArgs('title').returns('Ghost');
+            settingsCache.get.withArgs('description').returns('site description');
+            settingsCache.get.withArgs('logo').returns('/content/images/site-logo.png');
+            settingsCache.get.withArgs('amp').returns(true);
+            settingsCache.get.returns('');
+        });
+
+        beforeEach(function () {
+            req = {};
+            res = {
+                json: sinon.spy()
+            };
+            next = sinon.stub();
+            sinon.stub(ghostVersion, 'safe').value('4.44');
+        });
+
+        afterEach(function () {
+            sinon.restore();
+        });
+
+        it('returns site data for portal', async function () {
+            await membersMiddleware.getMemberSiteData(req, res);
+            res.json.calledWith({
+                site: {
+                    title: 'Ghost',
+                    description: 'site description',
+                    logo: '/content/images/site-logo.png',
+                    icon: '',
+                    accent_color: '',
+                    url: 'https://example.com',
+                    version: '4.44',
+                    portal_version: '1.22',
+                    free_price_name: '',
+                    free_price_description: '',
+                    allow_self_signup: true,
+                    members_signup_access: '',
+                    is_stripe_configured: true,
+                    portal_button: '',
+                    portal_name: '',
+                    portal_plans: '',
+                    portal_button_icon: '',
+                    portal_button_signup_text: '',
+                    portal_button_style: '',
+                    firstpromoter_id: '',
+                    members_support_address: 'noreply@example.com',
+                    prices: [],
+                    products: [],
+                    portal_products: ''
+                }
+            }).should.be.true();
         });
     });
 });
