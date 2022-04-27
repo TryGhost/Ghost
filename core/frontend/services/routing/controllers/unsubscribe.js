175608af74a97a931b81b44fff9f021b60cd42ef
Rishabh <zrishabhgarg@gmail.com>
2022-04-27 12:11:41 +0530

    Added portal to unsubscribe page
    refs https://github.com/TryGhost/Team/issues/1495
    
    With multiple newsletters, the unsubscribe email link allows a member to manage all their newsletter preferences via UI. Since the current unsubscribe page is a simple handlebars template, this change allows loading Portal UI on the unsubscribe page for more consistent UI to manage newsletter prefs.

--- core/frontend/services/routing/controllers/unsubscribe.js
+++ core/frontend/services/routing/controllers/unsubscribe.js
@@ -2,14 +2,20 @@ const debug = require('@tryghost/debug')('services:routing:controllers:unsubscri
 const path = require('path');
 const megaService = require('../../../../server/services/mega');
 const renderer = require('../../rendering');
+const {config, labs} = require('../../proxy');
 
 module.exports = async function unsubscribeController(req, res) {
     debug('unsubscribeController');
 
     let data = {};
+    const portalUrl = config.get('portal:url');
 
     try {
-        data.member = await megaService.mega.handleUnsubscribeRequest(req);
+        if (labs.isSet('multipleNewslettersUI')) {
+            data.portalUrl = portalUrl;
+        } else {
+            data.member = await megaService.mega.handleUnsubscribeRequest(req);
+        }
     } catch (err) {
         data.error = err.message;
     }
