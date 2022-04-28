8c692b1d733ac73abe9f69cd6e04c824c5547885
James Morris <moreofmorris@users.noreply.github.com>
2022-04-28 17:55:36 +0100

    Fixed linting bugs and tweaked mini chart starting heights
    refs: https://github.com/TryGhost/Team/issues/1531

--- ghost/admin/app/components/dashboard/v5/resources/resources.js
+++ ghost/admin/app/components/dashboard/v5/resources/resources.js
@@ -1,9 +1,9 @@
 import Component from '@glimmer/component';
 import fetch from 'fetch';
 import {action} from '@ember/object';
+import {inject as service} from '@ember/service';
 import {task} from 'ember-concurrency';
 import {tracked} from '@glimmer/tracking';
-import {inject as service} from '@ember/service';
 
 const API_URL = 'https://resources.ghost.io/resources';
 const API_KEY = 'b30afc1721f5d8d021ec3450ef';
@@ -58,6 +58,6 @@ export default class Resources extends Component {
     }
 
     get hasNothingEnabled() {
-        return (!this.areMembersEnabled && !areNewslettersEnabled && !hasPaidTiers);
+        return (!this.areMembersEnabled && !this.areNewslettersEnabled && !this.hasPaidTiers);
     }
 }
