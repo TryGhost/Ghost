33690009cca63a34bd27e7a30239628b078811d7
Rishabh <zrishabhgarg@gmail.com>
2022-04-28 13:33:07 +0530

    Added homepage redirect for unsubscribe urls
    refs https://github.com/TryGhost/Team/issues/1495
    
    With multiple newsletters, members are allowed to manage their newsletter pref via email unsubscribe link with member uuid. Since Portal is now taking over handling unsubscribe for members, we don't need to keep the current `/unsubscribe` page as Portal can load the member's newsletter pref on site home page directly.
    The redirect change is only enabled behind the `multipleNewslettersUI` flag as its in beta.

--- core/frontend/services/routing/controllers/unsubscribe.js
+++ core/frontend/services/routing/controllers/unsubscribe.js
@@ -1,10 +1,32 @@
 const debug = require('@tryghost/debug')('services:routing:controllers:unsubscribe');
 const path = require('path');
+const url = require('url');
+
+const urlUtils = require('../../../../shared/url-utils');
 const megaService = require('../../../../server/services/mega');
 const renderer = require('../../rendering');
+const labs = require('../../../../shared/labs');
 
 module.exports = async function unsubscribeController(req, res) {
     debug('unsubscribeController');
+
+    if (labs.isSet('multipleNewslettersUI')) {
+        const {query} = url.parse(req.url, true);
+
+        if (!query || !query.uuid) {
+            res.writeHead(400);
+            return res.end('Email address not found.');
+        }
+
+        const redirectUrl = new URL(urlUtils.urlFor('home', true));
+        redirectUrl.searchParams.append('uuid', query.uuid);
+        if (query.newsletter) {
+            redirectUrl.searchParams.append('newsletter', query.newsletter);
+        }
+        redirectUrl.searchParams.append('action', 'unsubscribe');
+
+        return res.redirect(302, redirectUrl.href);
+    }
 
     let data = {};
 
