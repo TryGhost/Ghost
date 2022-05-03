f775b2e958cb66a9bd902e8406c5688028ed6dc1
Fabien 'egg' O'Carroll <fabien@allou.is>
2022-05-03 12:01:23 +0100

    Fixed mergeDates zero and one item in list (#2360)
    The exit clause of the mergeDates function should return an array rather
    than an object, to ensure that an array is always returned. Because we
    are using the concat method when recursing, this continues to work for
    longer lists.
    
    We want to return an empty array if list is empty, this protects against
    returning [undefined]

--- ghost/admin/app/services/dashboard-stats.js
+++ ghost/admin/app/services/dashboard-stats.js
@@ -376,7 +376,7 @@ export default class DashboardStatsService extends Service {
             const [current, ...rest] = list;
 
             if (!current) {
-                return entry;
+                return entry ? [entry] : [];
             }
 
             if (!entry) {
