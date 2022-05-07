1ef0d0c05558996b761d7e1cd65364c168ce5bd9
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-05-07 16:37:49 +0200

    fixed linting errors

--- core/server/data/migrations/versions/5.0/2022-05-07-16-22-add-image-backup-table.js
+++ core/server/data/migrations/versions/5.0/2022-05-07-16-22-add-image-backup-table.js
@@ -1,7 +1,7 @@
 const utils = require('../../utils');
 
-module.exports = utils.addTable('image_backups', {
+module.exports = utils.addTable('image_backups',{
     id: {type: 'string', maxlength: 24, nullable: false, primary: true},
     created_at: {type: 'dateTime', nullable: false},
-    backup_completed: { type: 'bool', nullable: false, defaultTo: false},
+    backup_completed: { type: 'bool', nullable: false, defaultTo: false}
 });
