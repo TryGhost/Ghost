5059ad00152f3471a2c47d01f34dbf1a8d280a9a
Fabien "egg" O'Carroll <fabien@allou.is>
2022-05-09 10:03:11 +0100

    Removed all offer duplicate redemptions for SQLite

--- core/server/data/migrations/versions/5.0/2022-03-14-12-33-delete-duplicate-offer-redemptions.js
+++ core/server/data/migrations/versions/5.0/2022-03-14-12-33-delete-duplicate-offer-redemptions.js
@@ -1,9 +1,23 @@
+const DatabaseInfo = require('@tryghost/database-info/lib/database-info');
 const logging = require('@tryghost/logging');
 
 const {createTransactionalMigration} = require('../../utils');
 
 module.exports = createTransactionalMigration(
     async function up(knex) {
+        if (DatabaseInfo.isSQLite(knex)) {
+            const duplicates = await knex('offer_redemptions')
+                .select('subscription_id')
+                .count('subscription_id as count')
+                .groupBy('subscription_id')
+                .having('count', '>', 1);
+
+            logging.info(`Deleting all offer redemptions which have duplicates`);
+            await knex('offer_redemptions')
+                .whereIn('subscription_id', duplicates.map(row => row.subscription_id))
+                .del();
+            return;
+        }
         const result = await knex.raw(`
             DELETE
                 duplicate_redemptions
