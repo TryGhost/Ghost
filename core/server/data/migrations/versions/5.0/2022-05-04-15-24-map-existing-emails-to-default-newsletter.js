76b48d476e98229719a8f2d7d6684ad55e3366cd
Matt Hanley <git@matthanley.co.uk>
2022-05-09 12:27:09 +0100

    Added logging for resolved newsletter

--- core/server/data/migrations/versions/5.0/2022-05-04-15-24-map-existing-emails-to-default-newsletter.js
+++ core/server/data/migrations/versions/5.0/2022-05-04-15-24-map-existing-emails-to-default-newsletter.js
@@ -9,7 +9,7 @@ module.exports = createTransactionalMigration(
         // Note we intentionally use the default newsletter slug instead of the usual orderBy logic
         let newsletter = await knex('newsletters')
             .where('slug', 'default-newsletter')
-            .first('id');
+            .first('id', 'slug');
 
         if (!newsletter) {
             // Fall back to orderBy - just in case
@@ -19,13 +19,15 @@ module.exports = createTransactionalMigration(
                 .orderBy('sort_order', 'asc')
                 .orderBy('created_at', 'asc')
                 .orderBy('id', 'asc')
-                .first('id');
+                .first('id', 'slug');
         }
 
         if (!newsletter) {
             logging.error(`Newsletter not found - skipping`);
             return;
         }
+
+        logging.info(`Assigning existing emails to newsletter ID ${newsletter.id} (${newsletter.slug})`);
 
         // Set newsletter ID only on posts with related email records without a newsletter assigned
         await knex('posts')
