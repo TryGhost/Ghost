a2271c99cc55c55b89b98496e33bb6bb876d10b1
Naz <nazargargol@gmail.com>
2022-05-10 14:44:55 +0800

    Simplified returned data from api key extraction
    refs https://github.com/TryGhost/Toolbox/issues/292
    
    - We can query the data base by the API key itself, there's no need for type of the API  at any point yet

--- ghost/extract-api-key/test/extract-api-key.test.js
+++ ghost/extract-api-key/test/extract-api-key.test.js
@@ -2,25 +2,23 @@ const assert = require('assert');
 const extractApiKey = require('../index');
 
 describe('Extract API Key', function () {
-    it('returns nulls for a request without any key', function () {
-        const {key, type} = extractApiKey({
+    it('Returns nulls for a request without any key', function () {
+        const key = extractApiKey({
             query: {
                 filter: 'status:active'
             }
         });
 
         assert.equal(key, null);
-        assert.equal(type, null);
     });
 
     it('Extracts Content API key from the request', function () {
-        const {key, type} = extractApiKey({
+        const key = extractApiKey({
             query: {
                 key: '123thekey'
             }
         });
 
         assert.equal(key, '123thekey');
-        assert.equal(type, 'content');
     });
 });
