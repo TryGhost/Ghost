db6b123330f9922e0862c922526ff8c6c51f8831
Naz <nazargargol@gmail.com>
2022-05-10 14:18:38 +0800

    Added extra test for output when request has no keys
    refs https://github.com/TryGhost/Toolbox/issues/292
    
    - Providing extra coverage so the outputs are clear

--- ghost/extract-api-key/test/extract-api-key.test.js
+++ ghost/extract-api-key/test/extract-api-key.test.js
@@ -2,6 +2,17 @@ const assert = require('assert');
 const extractApiKey = require('../index');
 
 describe('Extract API Key', function () {
+    it('returns nulls for a request without any key', function () {
+        const {key, type} = extractApiKey({
+            query: {
+                filter: 'status:active'
+            }
+        });
+
+        assert.equal(key, null);
+        assert.equal(type, null);
+    });
+
     it('Extracts Content API key from the request', function () {
         const {key, type} = extractApiKey({
             query: {
