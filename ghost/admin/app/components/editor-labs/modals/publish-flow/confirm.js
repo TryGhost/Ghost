1b167ad2c4906ab12ffd721f20726fdce56f06dc
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-05-11 12:48:31 +0100

    Added error handling to publish flow confirm step
    closes https://github.com/TryGhost/Team/issues/1555
    closes https://github.com/TryGhost/Team/issues/1588
    
    - added task to the confirm component's backing class so it can be wrapped in step-specific error handling
    - added display of general/email error message above the confirm button
    - fixed error state of the confirm button showing as green and having incorrect spacing due to the icon being hidden in CSS rather than not displayed

--- ghost/admin/app/components/editor-labs/modals/publish-flow/confirm.js
+++ ghost/admin/app/components/editor-labs/modals/publish-flow/confirm.js
@@ -1,9 +1,22 @@
 import Component from '@glimmer/component';
 import moment from 'moment';
+import {action} from '@ember/object';
+import {htmlSafe} from '@ember/template';
+import {isArray} from '@ember/array';
+import {isServerUnreachableError} from 'ghost-admin/services/ajax';
 import {inject as service} from '@ember/service';
+import {task} from 'ember-concurrency';
+import {tracked} from '@glimmer/tracking';
+
+function isString(str) {
+    return toString.call(str) === '[object String]';
+}
 
 export default class PublishFlowOptions extends Component {
     @service settings;
+
+    @tracked errorMessage;
+    @tracked emailErrorMessage;
 
     // store any derived state from PublishOptions on creation so the copy
     // doesn't change whilst the post is saving
@@ -31,5 +44,46 @@ export default class PublishFlowOptions extends Component {
         }
 
         return buttonText;
+    }
+
+    @task({drop: true})
+    *confirmTask() {
+        this.errorMessage = null;
+        this.emailErrorMessage = null;
+
+        try {
+            yield this.args.saveTask.perform();
+        } catch (e) {
+            if (e === undefined && this.publishOptions.post.errors.length !== 0) {
+                // validation error
+                const validationError = this.args.publishOptions.post.errors.messages[0];
+                this.errorMessage = `Validation failed: ${validationError}`;
+                return false;
+            }
+
+            if (e?.name === 'EmailFailedError') {
+                this.emailErrorMessage = e.message;
+                return false;
+            }
+
+            let errorMessage = '';
+
+            if (isServerUnreachableError(e)) {
+                errorMessage = 'Unable to connect, please check your connection and try again';
+            } else if (e && isString(e)) {
+                errorMessage = e;
+            } else if (e && isArray(e)) {
+                // This is here because validation errors are returned as an array
+                // TODO: remove this once validations are fixed
+                errorMessage = e[0];
+            } else if (e?.payload?.errors?.[0].message) {
+                errorMessage = e.payload.errors[0].message;
+            } else {
+                errorMessage = 'Unknown Error';
+            }
+
+            this.errorMessage = htmlSafe(errorMessage);
+            return false;
+        }
     }
 }
