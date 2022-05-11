311c905d41b9c991155c325f04d44f127f13d336
Rishabh <zrishabhgarg@gmail.com>
2022-05-11 15:22:19 +0530

    fixup! Added newsletters endpoint to Content API

--- core/server/api/canary/newsletters-public.js
+++ core/server/api/canary/newsletters-public.js
@@ -1,26 +1,16 @@
 const models = require('../../models');
 
-const allowedIncludes = ['count.posts', 'count.members'];
-
 module.exports = {
     docName: 'newsletters',
 
     browse: {
         options: [
-            'include',
             'filter',
             'fields',
             'limit',
             'order',
             'page'
         ],
-        validation: {
-            options: {
-                include: {
-                    values: allowedIncludes
-                }
-            }
-        },
         permissions: true,
         query(frame) {
             return models.Newsletter.findPage(frame.options);
