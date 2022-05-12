fa1535a002d40dc09b0d21bc007a3cfe9c583956
Rishabh <zrishabhgarg@gmail.com>
2022-05-12 15:48:55 +0530

    fixup! Added offer read endpoint to content API

--- core/server/api/canary/utils/serializers/output/offers.js
+++ core/server/api/canary/utils/serializers/output/offers.js
@@ -1,8 +1,16 @@
 const debug = require('@tryghost/debug')('api:canary:utils:serializers:output:offers');
+const utils = require('../../index');
 
 module.exports = {
-    all() {
+    all(_models, _apiConfig, frame) {
         debug('all');
         // Offers has frame.response already set
+
+        // Cleanup response for content API
+        // TODO: remove and set explicit allowlist when moved to mapper
+        if (utils.isContentAPI(frame) && frame.response?.offers?.[0]) {
+            delete frame.response.offers[0].redemption_count;
+            delete frame.response.offers[0].code;
+        }
     }
 };
