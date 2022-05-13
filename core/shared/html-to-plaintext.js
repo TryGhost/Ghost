f73a84abac3a326f2780c4797cf9418adf4b4539
Hannah Wolfe <github.erisds@gmail.com>
2022-05-13 11:16:01 +0100

    Update dependency html-to-text to v8
    refs: https://github.com/TryGhost/Ghost/pull/13035
    refs: https://github.com/TryGhost/Team/issues/1609
    
    - Upgraded to the latest html-to-text
    - Matched the options to what we had as close as we can
    - The output changes slightly, but in most cases those are bugfixes
    - We are going to tune this output more soon, so accept the changes for now

--- core/shared/html-to-plaintext.js
+++ core/shared/html-to-plaintext.js
@@ -1,12 +1,28 @@
 module.exports = function htmlToPlaintext(html) {
-    const htmlToText = require('html-to-text');
+    const {convert} = require('html-to-text');
 
-    return htmlToText.fromString(html, {
+    return convert(html, {
         wordwrap: false,
-        ignoreImage: true,
-        hideLinkHrefIfSameAsText: true,
         preserveNewlines: true,
-        returnDomByDefault: true,
-        uppercaseHeadings: false
+
+        // equiv returnDomByDefault: true,
+        baseElements: {returnDomByDefault: true},
+        selectors: [
+            // Ignore images, equiv ignoreImage: true
+            {selector: 'img', format: 'skip'} ,
+            // disable uppercase headings, equiv uppercaseHeadings: false
+            {selector: 'h1', options: {uppercase: false}},
+            {selector: 'h2', options: {uppercase: false}},
+            {selector: 'h3', options: {uppercase: false}},
+            {selector: 'h4', options: {uppercase: false}},
+            {selector: 'h5', options: {uppercase: false}},
+            {selector: 'h6', options: {uppercase: false}},
+            {selector: 'table', options: {uppercaseHeaderCells: false}},
+            // equiv hideLinkHrefIfSameAsText: true
+            {selector: 'a', options: {hideLinkHrefIfSameAsText: true}},
+
+            // Backwards compatibility with html-to-text 5.1.1
+            {selector: 'div', format: 'inline'}
+        ]
     });
 };
