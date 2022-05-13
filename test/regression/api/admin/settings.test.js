bc47bc6a2a2ed752abc392aa5b6197e7b3781fa8
Hannah Wolfe <github.erisds@gmail.com>
2022-05-13 05:05:25 +0100

    Removed editor_is_launch_complete

--- test/regression/api/admin/settings.test.js
+++ test/regression/api/admin/settings.test.js
@@ -335,11 +335,6 @@ const defaultSettingsKeyTypes = [
         group: 'editor'
     },
     {
-        key: 'editor_is_launch_complete',
-        type: 'boolean',
-        group: 'editor'
-    },
-    {
         key: 'labs',
         type: 'object',
         group: 'labs'
