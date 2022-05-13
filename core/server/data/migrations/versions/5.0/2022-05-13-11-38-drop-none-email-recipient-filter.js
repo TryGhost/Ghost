ecc0c143cf89a93f711664bd1e49212dde3903df
Simon Backx <simon@ghost.org>
2022-05-13 16:02:36 +0200

    Updated migration to also set newsletter_id to null

--- core/server/data/migrations/versions/5.0/2022-05-13-11-38-drop-none-email-recipient-filter.js
+++ core/server/data/migrations/versions/5.0/2022-05-13-11-38-drop-none-email-recipient-filter.js
@@ -4,13 +4,16 @@ const {createTransactionalMigration} = require('../../utils');
 module.exports = createTransactionalMigration(
     async function up(knex) {
         // The 'none' value is no longer supported/used
-        logging.info(`Updating posts with email_recipient_filter 'none' to 'all'`);
+        logging.info(`Updating posts with email_recipient_filter 'none' to 'all' and newsletter_id to null`);
 
         const affectedRows = await knex('posts')
-            .update('email_recipient_filter', 'all')
+            .update({
+                email_recipient_filter: 'all',
+                newsletter_id: null
+            })
             .where('email_recipient_filter', 'none');
 
-        logging.info(`Updated ${affectedRows} posts' email_recipient_filter to 'all'`);
+        logging.info(`Updated ${affectedRows} posts' email_recipient_filter to 'all' and newsletter_id to null`);
     },
     async function down(knex) {
         // In previous versions, none meant that no email was sent.
