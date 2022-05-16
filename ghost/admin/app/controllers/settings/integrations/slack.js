385240e03dc04126aa6dd3b0ccdfecad1f1b79eb
Hannah Wolfe <github.erisds@gmail.com>
2022-05-16 11:38:32 +0100

    Refactored to use individual slack settings (#2384)
    refs: TryGhost/Team#1625
    
    - we want to remove backwards compatibility code for slack being a single setting
    
    Co-authored-by: Rishabh <zrishabhgarg@gmail.com>

--- ghost/admin/app/controllers/settings/integrations/slack.js
+++ ghost/admin/app/controllers/settings/integrations/slack.js
@@ -1,10 +1,8 @@
 import classic from 'ember-classic-decorator';
 import {action} from '@ember/object';
-import {empty} from '@ember/object/computed';
 import {inject as service} from '@ember/service';
 /* eslint-disable ghost/ember/alias-model-in-controller */
 import Controller from '@ember/controller';
-import boundOneWay from 'ghost-admin/utils/bound-one-way';
 import {isInvalidError} from 'ember-ajax/errors';
 import {task} from 'ember-concurrency';
 
@@ -16,49 +14,19 @@ export default class SlackController extends Controller {
     @service settings;
 
     leaveSettingsTransition = null;
-    slackArray = null;
 
     init() {
         super.init(...arguments);
-        this.slackArray = [];
     }
 
-    @boundOneWay('settings.slack.firstObject')
-        slackSettings;
-
-    @empty('slackSettings.url')
-        testNotificationDisabled;
+    get testNotificationDisabled() {
+        const slackUrl = this.settings.get('slackUrl');
+        return !slackUrl;
+    }
 
     @action
     save() {
         this.saveTask.perform();
-    }
-
-    @action
-    updateURL(value) {
-        value = typeof value === 'string' ? value.trim() : value;
-        this.set('slackSettings.url', value);
-        this.get('slackSettings.errors').clear();
-    }
-
-    @action
-    updateUsername(value) {
-        value = typeof value === 'string' ? value.trimLeft() : value;
-        this.set('slackSettings.username', value);
-        this.get('slackSettings.errors').clear();
-    }
-
-    @action
-    triggerDirtyState() {
-        let slack = this.slackSettings;
-        let slackArray = this.slackArray;
-        let settings = this.settings;
-
-        // Hack to trigger the `isDirty` state on the settings model by setting a new Array
-        // for slack rather that replacing the existing one which would still point to the
-        // same reference and therfore not setting the model into a dirty state
-        slackArray.clear().pushObject(slack);
-        settings.set('slack', slackArray);
     }
 
     @action
@@ -90,7 +58,6 @@ export default class SlackController extends Controller {
     leaveSettings() {
         let transition = this.leaveSettingsTransition;
         let settings = this.settings;
-        let slackArray = this.slackArray;
 
         if (!transition) {
             this.notifications.showAlert('Sorry, there was an error in the application. Please let the Ghost team know what happened.', {type: 'error'});
@@ -99,22 +66,14 @@ export default class SlackController extends Controller {
 
         // roll back changes on model props
         settings.rollbackAttributes();
-        slackArray.clear();
 
         return transition.retry();
     }
 
     @(task(function* () {
-        let slack = this.slackSettings;
-        let settings = this.settings;
-        let slackArray = this.slackArray;
-
         try {
-            yield slack.validate();
-            // clear existing objects in slackArray to make sure we only push the validated one
-            slackArray.clear().pushObject(slack);
-            yield settings.set('slack', slackArray);
-            return yield settings.save();
+            yield this.settings.validate();
+            return yield this.settings.save();
         } catch (error) {
             if (error) {
                 this.notifications.showAPIError(error);
@@ -134,10 +93,12 @@ export default class SlackController extends Controller {
             notifications.showNotification('Test notification sent', {type: 'info', key: 'slack-test.send.success', description: 'Check your Slack channel for the test message'});
             return true;
         } catch (error) {
-            notifications.showAPIError(error, {key: 'slack-test:send'});
+            if (error) {
+                notifications.showAPIError(error, {key: 'slack-test:send'});
 
-            if (!isInvalidError(error)) {
-                throw error;
+                if (!isInvalidError(error)) {
+                    throw error;
+                }
             }
         }
     }).drop())
