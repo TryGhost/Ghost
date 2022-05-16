f60efc218a6eb4cf2791add06e5bb90d0b244335
Simon Backx <simon@ghost.org>
2022-05-16 14:11:51 +0200

    Removed non multipleNewslettersUI tests

--- ghost/admin/tests/acceptance/settings/newsletters-test.js
+++ ghost/admin/tests/acceptance/settings/newsletters-test.js
@@ -6,41 +6,6 @@ import {setupApplicationTest} from 'ember-mocha';
 import {setupMirage} from 'ember-cli-mirage/test-support';
 import {visit} from '../../helpers/visit';
 
-describe('Acceptance: Settings - Newsletters (multipleNewsletters)', function () {
-    const hooks = setupApplicationTest();
-    setupMirage(hooks);
-
-    beforeEach(async function () {
-        this.server.loadFixtures('configs');
-
-        const role = this.server.create('role', {name: 'Owner'});
-        this.server.create('user', {roles: [role]});
-
-        enableLabsFlag(this.server, 'multipleNewsletters');
-
-        return await authenticateSession();
-    });
-
-    it('with flag - redirects original to labs', async function () {
-        await visit('/settings/members-email');
-        expect(currentURL()).to.equal('/settings/newsletters');
-    });
-
-    it('can manage open rate tracking', async function () {
-        this.server.db.settings.update({key: 'email_track_opens'}, {value: 'true'});
-
-        await visit('/settings/newsletters');
-        expect(find('[data-test-checkbox="email-track-opens"]')).to.be.checked;
-
-        await click('[data-test-label="email-track-opens"]');
-        expect(find('[data-test-checkbox="email-track-opens"]')).to.not.be.checked;
-
-        await click('[data-test-button="save-members-settings"]');
-
-        expect(this.server.db.settings.findBy({key: 'email_track_opens'}).value).to.equal(false);
-    });
-});
-
 describe('Acceptance: Settings - Newsletters (multipleNewslettersUI)', function () {
     const hooks = setupApplicationTest();
     setupMirage(hooks);
@@ -55,6 +20,11 @@ describe('Acceptance: Settings - Newsletters (multipleNewslettersUI)', function 
         enableLabsFlag(this.server, 'multipleNewslettersUI');
 
         return await authenticateSession();
+    });
+
+    it('redirects old path', async function () {
+        await visit('/settings/members-email');
+        expect(currentURL()).to.equal('/settings/newsletters');
     });
 
     it('can manage open rate tracking', async function () {
