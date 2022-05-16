12aff14dff54bf54f08324f935596d38b73c52f9
Hannah Wolfe <github.erisds@gmail.com>
2022-05-16 17:03:02 +0100

    Removed oauth experiment
    refs: https://github.com/TryGhost/Team/issues/1625
    
    - this didn't work the way we wanted to
    - removing this will free up the namespaces to start over

--- test/unit/server/data/schema/integrity.test.js
+++ test/unit/server/data/schema/integrity.test.js
@@ -37,7 +37,7 @@ describe('DB version integrity', function () {
     // Only these variables should need updating
     const currentSchemaHash = '2f4266e6e5087ad92dd30f3e721d46e5';
     const currentFixturesHash = '2509ff2c1f6e0293a3c3d84f08593b2f';
-    const currentSettingsHash = '840ed1334fc8a1d5387bb4ceb134845f';
+    const currentSettingsHash = '0b138cdd40e48b5b7dc4ebac2a7819a7';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
 
     // If this test is failing, then it is likely a change has been made that requires a DB version bump,
