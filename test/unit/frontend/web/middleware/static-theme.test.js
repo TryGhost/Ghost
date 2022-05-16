5090d75d963d865f34284e9d238f4b3afa4bc41f
Hannah Wolfe <github.erisds@gmail.com>
2022-05-16 18:54:44 +0100

    Improved theme asset handling - permissible assets
    refs: https://github.com/TryGhost/Team/issues/1633
    
    - this makes /assets/ a more permissible folder - it can serve anything _except_ hbs files
    - meanwhile the root folder becomes less permissible, and won't serve theme dev files commonly found in the root

--- test/unit/frontend/web/middleware/static-theme.test.js
+++ test/unit/frontend/web/middleware/static-theme.test.js
@@ -59,6 +59,39 @@ describe('staticTheme', function () {
         });
     });
 
+    it('should skip for .lock file', function (done) {
+        req.path = 'yarn.lock';
+
+        staticTheme()(req, res, function next() {
+            activeThemeStub.called.should.be.false();
+            expressStaticStub.called.should.be.false();
+
+            done();
+        });
+    });
+
+    it('should skip for gulp file', function (done) {
+        req.path = 'gulpfile.js';
+
+        staticTheme()(req, res, function next() {
+            activeThemeStub.called.should.be.false();
+            expressStaticStub.called.should.be.false();
+
+            done();
+        });
+    });
+
+    it('should skip for Grunt file', function (done) {
+        req.path = 'Gulpfile.js';
+
+        staticTheme()(req, res, function next() {
+            activeThemeStub.called.should.be.false();
+            expressStaticStub.called.should.be.false();
+
+            done();
+        });
+    });
+
     it('should call express.static for .css file', function (done) {
         req.path = 'myvalidfile.css';
 
@@ -119,6 +152,34 @@ describe('staticTheme', function () {
             should.exist(expressStaticStub.firstCall.args);
             expressStaticStub.firstCall.args[0].should.eql('my/fake/path');
             expressStaticStub.firstCall.args[1].should.be.an.Object().with.property('maxAge');
+
+            done();
+        });
+    });
+
+    it('should NOT skip if file is in assets', function (done) {
+        req.path = '/assets/whatever.json';
+
+        staticTheme()(req, res, function next() {
+            // Specifically gets called twice
+            activeThemeStub.calledTwice.should.be.true();
+            expressStaticStub.called.should.be.true();
+
+            // Check that express static gets called with the theme path + maxAge
+            should.exist(expressStaticStub.firstCall.args);
+            expressStaticStub.firstCall.args[0].should.eql('my/fake/path');
+            expressStaticStub.firstCall.args[1].should.be.an.Object().with.property('maxAge');
+
+            done();
+        });
+    });
+
+    it('should skip for .hbs file EVEN in /assets', function (done) {
+        req.path = '/assets/mytemplate.hbs';
+
+        staticTheme()(req, res, function next() {
+            activeThemeStub.called.should.be.false();
+            expressStaticStub.called.should.be.false();
 
             done();
         });
