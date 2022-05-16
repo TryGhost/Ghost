b6bdd6431d1366ec6b1dea08be548477d34fe9ae
Hannah Wolfe <github.erisds@gmail.com>
2022-05-16 18:54:44 +0100

    Improved theme asset handling - permissible assets
    refs: https://github.com/TryGhost/Team/issues/1633
    
    - this makes /assets/ a more permissible folder - it can serve anything _except_ hbs files
    - meanwhile the root folder becomes less permissible, and won't serve theme dev files commonly found in the root

--- core/frontend/web/middleware/static-theme.js
+++ core/frontend/web/middleware/static-theme.js
@@ -5,17 +5,24 @@ const themeEngine = require('../../services/theme-engine');
 const express = require('../../../shared/express');
 
 function isDeniedFile(file) {
-    const deniedFileTypes = ['.hbs', '.md', '.json'];
-    const ext = path.extname(file);
+    const deniedFileTypes = ['.hbs', '.md', '.json', '.lock', '.log'];
+    const deniedFiles = ['gulpfile.js', 'gruntfile.js'];
 
-    return deniedFileTypes.includes(ext);
+    const ext = path.extname(file);
+    const base = path.basename(file);
+
+    return deniedFiles.includes(base) || deniedFileTypes.includes(ext);
 }
 
 function isAllowedFile(file) {
     const allowedFiles = ['manifest.json'];
+    const allowedPath = '/assets/';
+    const alwaysDeny = ['.hbs'];
+
+    const ext = path.extname(file);
     const base = path.basename(file);
 
-    return allowedFiles.includes(base);
+    return allowedFiles.includes(base) || (file.startsWith(allowedPath) && !alwaysDeny.includes(ext));
 }
 
 function forwardToExpressStatic(req, res, next) {
@@ -25,14 +32,15 @@ function forwardToExpressStatic(req, res, next) {
 
     const configMaxAge = config.get('caching:theme:maxAge');
 
-    express.static(themeEngine.getActive().path,
-        {maxAge: (configMaxAge || configMaxAge === 0) ? configMaxAge : constants.ONE_YEAR_MS}
+    express.static(themeEngine.getActive().path, {
+        maxAge: (configMaxAge || configMaxAge === 0) ? configMaxAge : constants.ONE_YEAR_MS
+    }
     )(req, res, next);
 }
 
 function staticTheme() {
     return function denyStatic(req, res, next) {
-        if (!isAllowedFile(req.path) && isDeniedFile(req.path)) {
+        if (!isAllowedFile(req.path.toLowerCase()) && isDeniedFile(req.path.toLowerCase())) {
             return next();
         }
 
