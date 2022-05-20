667980c453810795fe9619a6142cc49c5851e601
Matt Hanley <git@matthanley.co.uk>
2022-05-20 14:59:24 +0100

    Validate stripe products against imported and existing products

--- core/server/data/importer/importers/data/stripe-products.js
+++ core/server/data/importer/importers/data/stripe-products.js
@@ -7,7 +7,8 @@ class StripeProductsImporter extends BaseImporter {
         super(allDataFromFile, {
             modelName: 'StripeProduct',
             dataKeyToImport: 'stripe_products',
-            requiredImportedData: ['products']
+            requiredImportedData: ['products'],
+            requiredExistingData: ['products']
         });
     }
 
@@ -18,14 +19,22 @@ class StripeProductsImporter extends BaseImporter {
         let invalidProducts = [];
         _.each(this.dataToImport, (objectInFile) => {
             const importedObject = _.find(this.requiredImportedData.products, {originalId: objectInFile.product_id});
+            // CASE: we've imported the product and need to map the ID
             if (importedObject) {
                 debug(`replaced product_id ${objectInFile.product_id} with ${importedObject.id}`);
                 objectInFile.product_id = importedObject.id;
-            } else {
-                invalidProducts.push(objectInFile.id);
+                return;
             }
+            const existingObject = _.find(this.requiredExistingData.products, {id: objectInFile.product_id});
+            // CASE: the product exists in the db already
+            if (existingObject) {
+                return;
+            }
+            // CASE: we don't know what product this is for
+            debug(`ignoring stripe product ${objectInFile.stripe_product_id}`);
+            invalidProducts.push(objectInFile.id);
         });
-        // ignore prices without products
+        // ignore Stripe products without Ghost products
         debug(`ignoring ${invalidProducts.length} products`);
         this.dataToImport = this.dataToImport.filter(item => !invalidProducts.includes(item.id));
 
