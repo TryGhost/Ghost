61aa30bba88c8de68098a0be3785169680ea3707
Simon Backx <simon@ghost.org>
2022-05-20 18:46:02 +0200

    🐛 Fixed member exports timing out for large sites (#14876)
    refs https://github.com/TryGhost/Team/issues/1641
    
    This commit adds a custom query for the members export, to improve the performance and to prevent any timeouts from happening when exporting large amounts of members.

--- core/server/api/canary/members.js
+++ core/server/api/canary/members.js
@@ -5,7 +5,6 @@ const moment = require('moment-timezone');
 const errors = require('@tryghost/errors');
 const models = require('../../models');
 const membersService = require('../../services/members');
-const labsService = require('../../../shared/labs');
 
 const settingsCache = require('../../../shared/settings-cache');
 const tpl = require('@tryghost/tpl');
@@ -362,16 +361,9 @@ module.exports = {
         },
         validation: {},
         async query(frame) {
-            frame.options.withRelated = ['labels', 'stripeSubscriptions', 'stripeSubscriptions.customer'];
-            if (labsService.isSet('multipleProducts')) {
-                frame.options.withRelated.push('products');
-            }
-            if (labsService.isSet('multipleNewsletters')) {
-                frame.options.withRelated.push('newsletters');
-            }
-            const page = await membersService.api.members.list(frame.options);
-
-            return page;
+            return {
+                data: await membersService.export(frame.options)
+            };
         }
     },
 
