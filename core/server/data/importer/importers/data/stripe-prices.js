8ae0d1bc2d63cbf277214a7ce1f4a291c3e5f1d0
Matt Hanley <git@matthanley.co.uk>
2022-05-20 14:19:20 +0100

    Mapped stripe_products to imported products

--- core/server/data/importer/importers/data/stripe-prices.js
+++ core/server/data/importer/importers/data/stripe-prices.js
@@ -7,6 +7,9 @@ class StripePricesImporter extends BaseImporter {
             dataKeyToImport: 'stripe_prices'
         });
     }
+
+    //@TODO: ensure stripe_product_id exists in either the import or the db
+    // stripe_prices.stripe_product_id -> stripe_products.stripe_product_id
 }
 
 module.exports = StripePricesImporter;
--- core/server/data/importer/importers/data/stripe-products.js
+++ core/server/data/importer/importers/data/stripe-products.js
@@ -1,11 +1,35 @@
+const _ = require('lodash');
+const debug = require('@tryghost/debug')('importer:stripeproducts');
 const BaseImporter = require('./base');
 
 class StripeProductsImporter extends BaseImporter {
     constructor(allDataFromFile) {
         super(allDataFromFile, {
             modelName: 'StripeProduct',
-            dataKeyToImport: 'stripe_products'
+            dataKeyToImport: 'stripe_products',
+            requiredImportedData: ['products']
         });
+    }
+
+    replaceIdentifiers() {
+        debug('replaceIdentifiers');
+
+        // map product_id -> product.id
+        let invalidProducts = [];
+        _.each(this.dataToImport, (objectInFile) => {
+            const importedObject = _.find(this.requiredImportedData.products, {originalId: objectInFile.product_id});
+            if (importedObject) {
+                debug(`replaced product_id ${objectInFile.product_id} with ${importedObject.id}`);
+                objectInFile.product_id = importedObject.id;
+            } else {
+                invalidProducts.push(objectInFile.id);
+            }
+        });
+        // ignore prices without products
+        debug(`ignoring ${invalidProducts.length} products`);
+        this.dataToImport = this.dataToImport.filter(item => !invalidProducts.includes(item.id));
+
+        return super.replaceIdentifiers();
     }
 }
 
