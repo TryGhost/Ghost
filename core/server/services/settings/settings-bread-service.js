d53ec39d60401028030624896f65d68093ddc577
Simon Backx <git@simonbackx.com>
2022-05-24 16:19:56 +0200

    Removed duplicate edit line
    Co-authored-by: Matt Hanley <3798302+matthanley@users.noreply.github.com>

--- core/server/services/settings/settings-bread-service.js
+++ core/server/services/settings/settings-bread-service.js
@@ -152,7 +152,6 @@ class SettingsBREADService {
             });
         }
 
-        await this.SettingsModel.edit(filteredSettings, options);
 
         return this.SettingsModel.edit(filteredSettings, options).then((result) => {
             return this._formatBrowse(_.keyBy(_.invokeMap(result, 'toJSON'), 'key'), options.context);
