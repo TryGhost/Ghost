d677f5f1498844e6157c24749d4f3164f52c18e9
naz <nazargargol@gmail.com>
2022-05-24 19:20:30 +0800

    🐛 Fixed signing key identificaiton in JWKs
    refs https://github.com/TryGhost/Team/issues/1640
    
    - The signing key returned by `GET /ghost/.well-known/jwks.json` was missing an OPTIONAL `use: "sig"` attribute needed to identify it as a signing key in client libraries. E.g. pyton lib: "pyjwt" or node lib: "jwks-client"
    - More about the "use" attribute at RFC7515 - https://www.rfc-editor.org/rfc/rfc7515#section-4.1.4

--- core/server/web/well-known.js
+++ core/server/web/well-known.js
@@ -16,7 +16,20 @@ module.exports = function setupWellKnownApp() {
 
     wellKnownApp.get('/jwks.json', async (req, res) => {
         const jwks = await getSafePublicJWKS();
-        res.json(jwks);
+
+        // there's only one key in the store atm
+        // based on this setting all of the keys to have
+        // "use": "sig" property
+        const keys = jwks.keys
+            .map(key => ({
+                e: key.e,
+                kid: key.kid,
+                kty: key.kty,
+                n: key.n,
+                use: 'sig'
+            }));
+
+        res.json({keys});
     });
 
     return wellKnownApp;
