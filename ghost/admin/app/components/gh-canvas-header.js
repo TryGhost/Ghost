0b7bb4732a03d60a2db562fd38c15829b4d6dc7c
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-05-25 10:06:35 +0100

    Converted <GhCanvasHeader> to glimmer component
    no issue
    
    - added `{{on-scroll}}` modifier to replace custom setup and teardown of event handlers inside the component

--- ghost/admin/app/components/gh-canvas-header.js
+++ ghost/admin/app/components/gh-canvas-header.js
@@ -1,33 +1,13 @@
-import Component from '@ember/component';
-import classic from 'ember-classic-decorator';
+import Component from '@glimmer/component';
 import {action} from '@ember/object';
-import {run} from '@ember/runloop';
-import {tagName} from '@ember-decorators/component';
 
-@classic
-@tagName('')
 export default class GhCanvasHeader extends Component {
     @action
-    initScrollWatch(element) {
-        this._onScroll = run.bind(this, this.onScroll, element);
-        this._scrollContainer = element.closest('.gh-main');
-        if (this._scrollContainer) {
-            this._scrollContainer.addEventListener('scroll', this._onScroll, {passive: true});
-        }
-    }
-
-    @action
-    clearScrollWatch() {
-        if (this._scrollContainer) {
-            this._scrollContainer.removeEventListener('scroll', this._onScroll);
-        }
-    }
-
-    onScroll(element) {
-        if (this._isSticky && this._scrollContainer.scrollTop < 10) {
+    onScroll(element, scrollContainer) {
+        if (this._isSticky && scrollContainer.scrollTop < 10) {
             element.classList.remove('gh-canvas-header--sticky');
             this._isSticky = false;
-        } else if (!this._isSticky && this._scrollContainer.scrollTop > 10) {
+        } else if (!this._isSticky && scrollContainer.scrollTop > 10) {
             element.classList.add('gh-canvas-header--sticky');
             this._isSticky = true;
         }
