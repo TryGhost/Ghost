1c239cf632b529aa4bcf79042e240620f1cfdd15
Simon Backx <simon@ghost.org>
2022-05-27 16:20:40 +0200

    🎨 Reduced favicon requirements (#2408)
    refs TryGhost/Team#1652
    
    - Support picking new image types in the favicon image uploader.
    - Added support for non-square and not resizable files (e.g., svg files) as favicon (cover background image).

--- ghost/admin/app/components/gh-image-uploader.js
+++ ghost/admin/app/components/gh-image-uploader.js
@@ -14,11 +14,11 @@ import {run} from '@ember/runloop';
 import {inject as service} from '@ember/service';
 
 export const IMAGE_MIME_TYPES = 'image/gif,image/jpg,image/jpeg,image/png,image/svg+xml,image/webp';
-export const IMAGE_EXTENSIONS = ['gif', 'jpg', 'jpeg', 'png', 'svg', 'webp'];
+export const IMAGE_EXTENSIONS = ['gif', 'jpg', 'jpeg', 'png', 'svg', 'svgz', 'webp'];
 export const IMAGE_PARAMS = {purpose: 'image'};
 
-export const ICON_EXTENSIONS = ['ico', 'png'];
-export const ICON_MIME_TYPES = 'image/png,image/x-icon';
+export const ICON_EXTENSIONS = ['gif', 'ico', 'jpg', 'jpeg', 'png', 'svg', 'svgz', 'webp'];
+export const ICON_MIME_TYPES = 'image/x-icon,image/vnd.microsoft.icon,image/gif,image/jpg,image/jpeg,image/png,image/svg+xml,image/webp';
 export const ICON_PARAMS = {purpose: 'icon'};
 
 export default Component.extend({
