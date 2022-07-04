e2294ace228f31f6ac0d9a743e9a9402e17589fd
Rishabh <zrishabhgarg@gmail.com>
2022-07-04 15:41:14 +0200

    Added custom build for minified script
    refs TryGhost/Team#1665
    
    - overrides webpack config to generate single script for publishing
    - adds custom scripts for dev and production mode

--- ghost/sodo-search/src/App.js
+++ ghost/sodo-search/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        Sodo Search
     </div>
   );
 }
