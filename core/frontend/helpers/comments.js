21a43344634f59b8375ddaccaff6eafef2000be6
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-07-04 15:33:40 +0200

    Added `{{comments}}` helper
    refs https://github.com/TryGhost/Team/issues/1664
    
    - added `comments:url` config for a similar setup to Portal
    - added `{{comments}}` helper that's behind the `comments` labs flag
      - currently outputs a `<script>` tag that points to the comments script with API location+key data attributes

---
+++ core/frontend/helpers/comments.js
@@ -0,0 +1,44 @@
+const {SafeString} = require('../services/handlebars');
+const {config, urlUtils, getFrontendKey, labs} = require('../services/proxy');
+
+async function comments() {
+    const commentId = this.comment_id;
+
+    if (!commentId) {
+        return;
+    }
+
+    const frontendKey = await getFrontendKey();
+
+    const data = {
+        ghost: urlUtils.getSiteUrl(),
+        api: urlUtils.urlFor('api', {type: 'content'}, true),
+        key: frontendKey,
+        'comment-id': commentId
+    };
+
+    let dataAttributes = '';
+
+    Object.entries(data).forEach(([key, value]) => {
+        dataAttributes += `data-${key}="${value}" `;
+    });
+
+    return new SafeString(`
+        <script defer src="${config.get('comments:url')}" ${dataAttributes} crossorigin="anonymous"></script>
+    `);
+}
+
+module.exports = async function commentsLabsWrapper() {
+    const self = this;
+    const args = arguments;
+
+    return labs.enabledHelper({
+        flagKey: 'comments',
+        flagName: 'Comments',
+        helperName: 'comments'
+    }, () => {
+        return comments.apply(self, args);
+    });
+};
+
+module.exports.async = true;
