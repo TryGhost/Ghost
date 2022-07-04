aeb644cf84a0a53a207e216d24e04bebd9ef55f4
Rishabh <zrishabhgarg@gmail.com>
2022-07-04 16:00:02 +0200

    Fixed lint

--- apps/comments-ui/src/App.test.js
+++ apps/comments-ui/src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
