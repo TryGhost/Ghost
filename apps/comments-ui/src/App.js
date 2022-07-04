c71db17200b52e79d3be636421e9d78565d16529
Rishabh <zrishabhgarg@gmail.com>
2022-07-04 15:59:11 +0200

    Fixed tests

--- apps/comments-ui/src/App.js
+++ apps/comments-ui/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        Hello world
-    </div>
-  );
+    return (
+        <div className="App">
+            Hello world
+        </div>
+    );
 }
 
 export default App;
--- apps/comments-ui/src/App.test.js
+++ apps/comments-ui/src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<App />);
+    const linkElement = screen.getByText(/hello world/i);
+    expect(linkElement).toBeInTheDocument();
 });
