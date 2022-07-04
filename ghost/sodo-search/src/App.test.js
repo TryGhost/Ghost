ac43c86c2e52682c9a8c8fbb769ea1d375282ae9
Rishabh <zrishabhgarg@gmail.com>
2022-07-04 15:55:30 +0200

    Fixed tests

--- ghost/sodo-search/src/App.test.js
+++ ghost/sodo-search/src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import React from 'react';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Sodo Search/i);
   expect(linkElement).toBeInTheDocument();
 });
