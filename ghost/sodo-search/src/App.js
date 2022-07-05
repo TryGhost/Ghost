2359960db193d352cb403cd9f2a1034637eb5e9a
Naz <nazargargol@gmail.com>
2022-07-05 10:09:10 +0200

    Added search index functionality
    refs https://github.com/TryGhost/Team/issues/1665
    refs https://github.com/TryGhost/Themes/blob/402e54987e0b75695f24b2983a87fa790ea3a919/packages/ease/assets/js/main.js#L16-L127
    
    - This is a React adaptation of the referenced sodo-search functionality
    - The module exposes two methods: init and search. Init requires an API URL parameter and an API key; search requires a text value and returns matching search results

--- ghost/sodo-search/src/App.js
+++ ghost/sodo-search/src/App.js
@@ -2,8 +2,25 @@ import React from 'react';
 import './App.css';
 import AppContext from './AppContext';
 import PopupModal from './components/PopupModal';
+import {init as initSearchIndex} from './search-index.js';
 
 export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            apiUrl: props.apiUrl,
+            apiKey: props.apiKey
+        };
+    }
+
+    componentDidMount() {
+        initSearchIndex({
+            apiUrl: this.state.apiUrl,
+            apiKey: this.state.apiKey
+        });
+    }
+
     render() {
         return (
             <AppContext.Provider value={{
