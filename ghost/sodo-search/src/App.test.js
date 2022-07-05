66a263a031b0b9b3d61cf768b245e50ef7e4fee7
Naz <nazargargol@gmail.com>
2022-07-05 10:03:56 +0200

    Renamed App test
    no issue
    
    - Just a minor cleanup

--- ghost/sodo-search/src/App.test.js
+++ ghost/sodo-search/src/App.test.js
@@ -2,7 +2,7 @@ import {render, screen} from '@testing-library/react';
 import App from './App';
 import React from 'react';
 
-test('renders learn react link', () => {
+test('renders Sodo Search app component', () => {
     render(<App />);
     const linkElement = screen.getByText(/Sodo Search/i);
     expect(linkElement).toBeInTheDocument();
