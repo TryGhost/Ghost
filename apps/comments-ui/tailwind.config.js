c6fb93d280ea44328e23e91a24a803a7080617d9
Peter Zimon <peter.zimon@gmail.com>
2022-07-05 14:19:05 +0200

    Updated Tailwind config

--- apps/comments-ui/tailwind.config.js
+++ apps/comments-ui/tailwind.config.js
@@ -44,6 +44,38 @@ module.exports = {
             80: '32rem',
             96: '38.4rem'
         },
+        maxWidth: {
+            none: 'none',
+            0: '0rem',
+            xs: '32rem',
+            sm: '38.4rem',
+            md: '44.8rem',
+            lg: '51.2rem',
+            xl: '57.6rem',
+            '2xl': '67.2rem',
+            '3xl': '76.8rem',
+            '4xl': '89.6rem',
+            '5xl': '102.4rem',
+            '6xl': '115.2rem',
+            '7xl': '128rem',
+            '8xl': '140rem',
+            '9xl': '156rem',
+            full: '100%',
+            min: 'min-content',
+            max: 'max-content',
+            fit: 'fit-content',
+            prose: '65ch'
+        },
+        borderRadius: {
+            sm: '0.2rem',
+            DEFAULT: '0.4rem',
+            md: '0.6rem',
+            lg: '0.8rem',
+            xl: '1.2rem',
+            '2xl': '1.6rem',
+            '3xl': '2.4rem',
+            full: '9999px'
+        },
         fontSize: {
             xs: '1.2rem',
             sm: '1.4rem',
