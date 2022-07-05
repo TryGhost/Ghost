e8a3230a9a0c390d8eb1b4c4af83e8a6bcdf276b
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-07-05 11:17:17 +0200

    Added comments table creation migration (#14998)
    refs https://github.com/TryGhost/Team/issues/1664
    
    Field notes:
    
    - `parent_id` - used for nested comments but will be limited to 1 level in app-level validation
    - `member_id` - when a member is deleted for now the member id is kept but in the future may be removed, hence `nullable: true`
    - `status` - "hidden" status will be used when a staff user hides a comment, "deleted" is used when a comment author deletes
    - `html` - will store pre-sanitised html
    - `edited_at` - used to show an "X edited at Y" note when displaying comments, separate to `updated_at` because changing the status would also change `updated_at` but shouldn't show the "edited at" UI

--- core/server/data/schema/schema.js
+++ core/server/data/schema/schema.js
@@ -742,5 +742,16 @@ module.exports = {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
         member_id: {type: 'string', maxlength: 24, nullable: false, references: 'members.id', cascadeDelete: true},
         newsletter_id: {type: 'string', maxlength: 24, nullable: false, references: 'newsletters.id', cascadeDelete: true}
+    },
+    comments: {
+        id: {type: 'string', maxlength: 24, nullable: false, primary: true},
+        post_id: {type: 'string', maxlength: 24, nullable: false, unique: false, references: 'posts.id', cascadeDelete: true},
+        member_id: {type: 'string', maxlength: 24, nullable: true, unique: false, references: 'members.id'},
+        parent_id: {type: 'string', maxlength: 24, nullable: true, unique: false, references: 'comments.id'},
+        status: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'published', validations: {isIn: [['published', 'hidden', 'deleted']]}},
+        html: {type: 'text', maxlength: 1000000000, fieldtype: 'long', nullable: true},
+        edited_at: {type: 'dateTime', nullable: true},
+        created_at: {type: 'dateTime', nullable: false},
+        updated_at: {type: 'dateTime', nullable: false}
     }
 };
