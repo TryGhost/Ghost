bf57712e0d780121c32e9cf2d1588fb52fbe0353
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-07-05 17:50:01 +0200

    Added `users.comment_notifications` field
    refs: https://github.com/TryGhost/Team/issues/1668
    
    - temporary solution until `user_settings` table is in place

--- test/unit/server/data/schema/integrity.test.js
+++ test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '89e2e6356edb2a3cd1c7a9962ef1fc88';
+    const currentSchemaHash = 'c85378b0bbd7d5eeb1fff39796c30886';
     const currentFixturesHash = '1d6562a1963a9b9d10d06070d537f11f';
     const currentSettingsHash = '0b138cdd40e48b5b7dc4ebac2a7819a7';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
