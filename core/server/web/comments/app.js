00110e541e50e8fb0802e65348d7947547f9774a
Hannah Wolfe <github.erisds@gmail.com>
2022-07-05 12:07:20 +0200

    Added basic comment CRUD model + API
    refs https://github.com/TryGhost/Team/issues/1664

--- core/server/web/comments/app.js
+++ core/server/web/comments/app.js
@@ -1,9 +1,11 @@
 const debug = require('@tryghost/debug')('comments');
 const errorHandler = require('@tryghost/mw-error-handler');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const express = require('../../../shared/express');
 const urlUtils = require('../../../shared/url-utils');
 const sentry = require('../../../shared/sentry');
+const routes = require('./routes');
 
 module.exports = function setupCommentsApp() {
     debug('Comments App setup start');
@@ -13,8 +15,10 @@ module.exports = function setupCommentsApp() {
     const siteUrl = new URL(urlUtils.getSiteUrl());
     commentsApp.use(cors(siteUrl.origin));
 
+    commentsApp.use(bodyParser.json({limit: '50mb'}));
+
     // Routing
-    commentsApp.get('/api/comments');
+    commentsApp.use('/api/', routes());
 
     // API error handling
     commentsApp.use('/api', errorHandler.resourceNotFound);
