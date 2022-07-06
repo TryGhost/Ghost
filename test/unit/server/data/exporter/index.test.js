e53f7ee282c36e7c6c2710ab75931eb9afc24e26
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-07-06 09:51:19 +0200

    Added `comments_enabled` setting
    refs https://github.com/TryGhost/Team/issues/1664
    
    - comments are disabled by default, but can be enabled on any site

--- test/unit/server/data/exporter/index.test.js
+++ test/unit/server/data/exporter/index.test.js
@@ -234,7 +234,7 @@ describe('Exporter', function () {
 
             // NOTE: if default settings changed either modify the settings keys blocklist or increase allowedKeysLength
             //       This is a reminder to think about the importer/exporter scenarios ;)
-            const allowedKeysLength = 69;
+            const allowedKeysLength = 70;
             totalKeysLength.should.eql(SETTING_KEYS_BLOCKLIST.length + allowedKeysLength);
         });
     });
