50d5d74d91dcea329a1f492a0020783ce809b5ff
James Morris <moreofmorris@users.noreply.github.com>
2022-07-06 16:46:57 +0200

    Added autofocus to edit when it's on

--- apps/comments-ui/src/components/EditForm.js
+++ apps/comments-ui/src/components/EditForm.js
@@ -48,7 +48,7 @@ class EditForm extends React.Component {
                     </div>
                     <div className="ml-14 pr-3 font-sans leading-normal dark:text-neutral-300">
                         <div className="relative w-full">
-                            <textarea className="w-full resize-none rounded-md border h-32 p-3 font-sans mb-1 leading-normal focus:outline-0 dark:bg-[rgba(255,255,255,0.08)] dark:border-none dark:text-neutral-300" value={this.state.message} onChange={this.handleChange} />
+                            <textarea className="w-full resize-none rounded-md border h-32 p-3 font-sans mb-1 leading-normal focus:outline-0 dark:bg-[rgba(255,255,255,0.08)] dark:border-none dark:text-neutral-300" value={this.state.message} onChange={this.handleChange} autofocus="true" />
                             <div className="flex flex-start">
                                 <button type="submit" className="rounded-md border p-2 font-sans text-sm text-center bg-black font-semibold text-white dark:bg-[rgba(255,255,255,0.8)] dark:text-neutral-800">Edit comment</button>
                                 <button className="font-sans text-sm font-medium ml-2.5 text-neutral-500" onClick={this.props.toggle}>Cancel</button>
