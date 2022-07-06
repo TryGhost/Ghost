feae7836d72c3ccd2b55f628ce896cba74c6e07d
James Morris <moreofmorris@users.noreply.github.com>
2022-07-06 12:16:29 +0200

    Lots of little action tweaks

--- apps/comments-ui/src/components/Like.js
+++ apps/comments-ui/src/components/Like.js
@@ -2,7 +2,7 @@ import {ReactComponent as LikeIcon} from '../images/icons/like.svg';
 
 function Like() {
     return (
-        <button className="flex font-sans mr-5"><LikeIcon className='gh-comments-icon gh-comments-icon-like mr-1' />3</button>
+        <button className="flex font-sans text-[14px] items-center"><LikeIcon className='gh-comments-icon gh-comments-icon-like mr-1' />3</button>
     );
 }
   
--- apps/comments-ui/src/components/Reply.js
+++ apps/comments-ui/src/components/Reply.js
@@ -2,7 +2,7 @@ import {ReactComponent as ReplyIcon} from '../images/icons/reply.svg';
 
 function Reply() {
     return (
-        <button className="flex font-sans mr-5"><ReplyIcon className='gh-comments-icon gh-comments-icon-reply mr-1' />Reply</button>
+        <button className="flex font-sans text-[14px] items-center"><ReplyIcon className='gh-comments-icon gh-comments-icon-reply mr-1' />Reply</button>
     );
 }
   
