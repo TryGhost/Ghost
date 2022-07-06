8e0efd8bfa6de7875bcd7d05d73d5010625a4241
Simon Backx <simon@ghost.org>
2022-07-06 17:36:37 +0200

    Removed console log

--- apps/comments-ui/src/components/Replies.js
+++ apps/comments-ui/src/components/Replies.js
@@ -3,7 +3,6 @@ import Comment from './Comment';
 const Replies = (props) => {
     return (
         <div>
-            {console.log(props.replies)}
             {props.replies.map((reply => <Comment comment={reply} key={reply.id} />))}
         </div>
     );
