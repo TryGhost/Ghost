70f57210b10425e5a4dbd9431bcff93e15d65e03
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-07-06 16:27:06 +0200

    Wired up comment notification email data
    refs https://github.com/TryGhost/Team/issues/1664

--- core/server/services/comments/emails/new-comment.txt.js
+++ core/server/services/comments/emails/new-comment.txt.js
@@ -1,18 +1,14 @@
 module.exports = function (data) {
     return `
-        Hey there!
+        Hey there,
 
-        Tap the link below to complete the signup process for ${data.siteTitle}, and be automatically signed in:
+        Someone just posted a comment on your post "${data.postTitle}"
 
-        ${data.url}
-
-        For your security, the link will expire in 24 hours time.
-
-        See you soon!
+        ${data.postUrl}#comments-area
 
         ---
 
-        Sent to ${data.email}
-        If you did not make this request, you can simply delete this message. You will not be signed up, and no account will be created for you.
+        Sent to ${data.toEmail} from ${data.siteDomain}.
+        You can manage your notification preferences at ${data.staffUrl}.
     `;
 };
