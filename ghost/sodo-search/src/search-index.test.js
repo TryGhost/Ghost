2b71791d2f26a35e6b3fb5e0fc2a5590c70609ab
Naz <nazargargol@gmail.com>
2022-07-06 10:37:49 +0200

    Connected authors to the search index
    refs https://github.com/TryGhost/Team/issues/1665
    
    - Authors should be searchable. This change hooks up the Authors Content API with the search index

--- ghost/sodo-search/src/search-index.test.js
+++ ghost/sodo-search/src/search-index.test.js
@@ -12,9 +12,16 @@ describe('search index', function () {
         const searchIndex = new SearchIndex({apiUrl, apiKey, storage: localStorage});
 
         const scope = nock('http://localhost/ghost/api/content')
-            .get('/posts/?key=secret_key&limit=all&fields=id,title,excerpt,url,updated_at,visibility&order=updated_at%20desc&formats=plaintext')
+            .get('/posts/?key=secret_key&limit=all&fields=id,slug,title,excerpt,url,updated_at,visibility&order=updated_at%20desc&formats=plaintext')
             .reply(200, {
                 posts: [{}]
+            })
+            .get('/authors/?key=secret_key&limit=all&fields=id,slug,name,profile_image')
+            .reply(200, {
+                authors: [{
+                    id: 'different_uniq',
+                    name: 'Barcelona Author'
+                }]
             });
 
         await searchIndex.init({apiUrl, apiKey});
@@ -22,18 +29,27 @@ describe('search index', function () {
         expect(scope.isDone()).toBeTruthy();
     });
 
-    test('allows to search for indexed posts', async () => {
+    test('allows to search for indexed posts and authors', async () => {
         const apiUrl = 'http://localhost/ghost/api/content';
         const apiKey = 'secret_key';
         const searchIndex = new SearchIndex({apiUrl, apiKey, storage: localStorage});
 
         nock('http://localhost/ghost/api/content')
-            .get('/posts/?key=secret_key&limit=all&fields=id,title,excerpt,url,updated_at,visibility&order=updated_at%20desc&formats=plaintext')
+            .get('/posts/?key=secret_key&limit=all&fields=id,slug,title,excerpt,url,updated_at,visibility&order=updated_at%20desc&formats=plaintext')
             .reply(200, {
                 posts: [{
                     id: 'sounique',
                     title: 'Awesome Barcelona Life',
                     excerpt: 'We are sitting by the pool and smashing out search features'
+                }]
+            })
+            .get('/authors/?key=secret_key&limit=all&fields=id,slug,name,profile_image')
+            .reply(200, {
+                authors: [{
+                    id: 'different_uniq',
+                    slug: 'barcelona-author',
+                    name: 'Barcelona Author',
+                    profile_image: 'https://url_to_avatar/barcelona.png'
                 }]
             });
 
@@ -42,6 +58,7 @@ describe('search index', function () {
         let searchResults = searchIndex.search('Barcelona');
         expect(searchResults.posts.length).toEqual(1);
         expect(searchResults.posts[0].title).toEqual('Awesome Barcelona Life');
+        expect(searchResults.authors[0].name).toEqual('Barcelona Author');
 
         searchResults = searchIndex.search('Nothing like this');
         expect(searchResults.posts.length).toEqual(0);
