c394037cab1e2668cbe64dfc131bb8f4ac7d52d9
James Morris <moreofmorris@users.noreply.github.com>
2022-07-06 17:02:18 +0200

    Added basic toggle state to like component

--- apps/comments-ui/src/components/Like.js
+++ apps/comments-ui/src/components/Like.js
@@ -1,9 +1,30 @@
+import React from 'react';
+import AppContext from '../AppContext';
 import {ReactComponent as LikeIcon} from '../images/icons/like.svg';
 
-function Like() {
-    return (
-        <button className="flex font-sans text-[14px] items-center"><LikeIcon className='gh-comments-icon gh-comments-icon-like mr-1' />3</button>
-    );
+class Like extends React.Component {
+    static contextType = AppContext;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            liked: false
+        };
+
+        this.toggleLike = this.toggleLike.bind(this);
+    }
+
+    toggleLike() {
+        this.setState(state => ({
+            liked: !state.liked
+        }));
+    }
+
+    render() {
+        return (
+            <button className="flex font-sans text-[14px] items-center" onClick={this.toggleLike}><LikeIcon className={`gh-comments-icon gh-comments-icon-like mr-1 ${this.state.liked ? 'fill-black' : ''}`} />3</button>
+        );
+    }
 }
-  
+
 export default Like;
