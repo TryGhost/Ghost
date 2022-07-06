37837b63a58f35034ab2c3aa5a8dd19d2f88652b
Rishabh <zrishabhgarg@gmail.com>
2022-07-06 12:32:36 +0200

    Fixed App ui tests
    refs https://github.com/TryGhost/Team/issues/1665
    
    - search modal is not opened by default and only triggered on the url

--- ghost/sodo-search/src/App.test.js
+++ ghost/sodo-search/src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 import React from 'react';
 
 test('renders Sodo Search app component', () => {
+    window.location.hash = '#/sodo-search';
     render(<App />);
     // const containerElement = screen.getElementsByClassName('gh-portal-popup-container');
     const containerElement = document.querySelector('.gh-root-frame');
