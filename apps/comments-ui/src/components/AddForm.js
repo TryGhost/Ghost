6a11c5ed8d978c81218219ea3c1a6e05f6b7781a
Simon Backx <simon@ghost.org>
2022-07-07 17:18:40 +0200

    Revert "Added TipTap editor"
    This reverts commit f2c1b3397ccd77a29c7634159a9bb8df9f86bcfd.

--- apps/comments-ui/src/components/AddForm.js
+++ apps/comments-ui/src/components/AddForm.js
@@ -1,40 +1,22 @@
-import React, {useContext, useState, useEffect} from 'react';
+import React, {useContext, useState} from 'react';
 import {Transition} from '@headlessui/react';
 import AppContext from '../AppContext';
 import Avatar from './Avatar';
-import {useEditor, EditorContent} from '@tiptap/react';
-import StarterKit from '@tiptap/starter-kit';
-import Placeholder from '@tiptap/extension-placeholder';
 
 const AddForm = (props) => {
+    const [message, setMessage] = useState('');
+    const [focused, setFocused] = useState(false);
     const {member, postId, onAction} = useContext(AppContext);
-    const editor = useEditor({
-        extensions: [
-            StarterKit,
-            Placeholder.configure({
-                placeholder: 'Join the discussion'
-            })
-        ],
-        content: '',
-        autofocus: true,
-        editorProps: {
-            attributes: {
-                class: `min-h-40 w-full rounded-md border border-slate-50 px-0 py-3 font-sans mb-1 leading-normal focus:outline-0 dark:bg-[rgba(255,255,255,0.08)] dark:border-none dark:text-neutral-300 cursor-text`
-            }
-        }
-    });
-
-    const focused = editor?.isFocused || !editor?.isEmpty;
 
     const getHTML = () => {
         // Convert newlines to <br> for now (until we add a real editor)
-        return editor.getHTML();
+        return message.replace('\n', '<br>');
     };
 
     const submitForm = async (event) => {
         event.preventDefault();
 
-        if (editor.isEmpty) {
+        if (message.length === 0) {
             return;
         }
 
@@ -46,33 +28,46 @@ const AddForm = (props) => {
                 html: getHTML()
             });
 
-            // Clear message and blur on success
-            editor.chain().clearContent().blur().run();
+            // Clear message on success
+            setMessage('');
+            setFocused(false);
         } catch (e) {
             // eslint-disable-next-line no-console
             console.error(e);
         }
     };
 
-    const focusEditor = (event) => {
-        editor.commands.focus();
+    const handleChange = (event) => {
+        setMessage(event.target.value);
+    };
+
+    const handleBlur = (event) => {
+        if (message === '') {
+            setFocused(false);
+        }
+    };
+
+    const handleFocus = (event) => {
+        setFocused(true);
     };
 
     return (
-        <form onClick={focusEditor} className={`comment-form transition duration-200 rounded-md px-3 pt-3 pb-2 -ml-[13px] -mr-3 -mt-[15px] mb-10 shadow-lg hover:shadow-xl ${focused ? 'cursor-default' : 'cursor-pointer'}`}>
+        <form onSubmit={submitForm} onClick={handleFocus} className={`comment-form transition duration-200 rounded-md px-3 pt-3 pb-2 -ml-[13px] -mr-3 -mt-[15px] mb-10 shadow-lg hover:shadow-xl ${focused ? 'cursor-default' : 'cursor-pointer'}`}>
             <div className="w-full relative">
                 <div className="pr-3 font-sans leading-normal dark:text-neutral-300">
-                    <div className="relative w-full">
-                        {/* This animation doesn't work yet because of the editor needing two divs */}
-                        <EditorContent
-                            className={`transition-[height] duration-150 w-full pl-[56px] px-0 py-[14px] ${focused ? 'mt-8' : '-mt-[2px] mb-1'} ${focused ? 'cursor-text h-40' : 'cursor-pointer overflow-hidden h-11 hover:border-slate-300'}`}
-                            editor={editor} 
+                    <div className="relative w-full ">
+                        <textarea
+                            className={`transition-[height] pl-[56px] px-0 py-[14px] ${focused ? 'mt-8' : '-mt-[2px] mb-1'} duration-150 w-full placeholder:text-neutral-300 border-none resize-none rounded-md border border-slate-50 font-sans text-[16.5px] leading-normal focus:outline-0 dark:bg-[rgba(255,255,255,0.08)] dark:border-none dark:text-neutral-300 ${focused ? 'cursor-text h-40' : 'cursor-pointer overflow-hidden h-11 hover:border-slate-300'}`}
+                            value={message}
+                            onChange={handleChange}
+                            onFocus={handleFocus}
+                            onBlur={handleBlur}
+                            autofocus={true}
+                            placeholder='Join the discussion'
                         />
                         <button
                             className={`transition-[opacity] duration-150 absolute -right-[9px] bottom-[4px] rounded-md border py-3 px-4 font-sans text-sm text-center bg-black font-semibold text-white dark:bg-[rgba(255,255,255,0.8)] dark:text-neutral-800`}
-                            type="button"
-                            onClick={submitForm}
-                        >
+                            type="submit">
                             Add comment
                         </button>
                     </div>
