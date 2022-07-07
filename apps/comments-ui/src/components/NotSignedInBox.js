e582f8723ad7bcd6f9c5730392c8dea54b2f6d61
James Morris <moreofmorris@users.noreply.github.com>
2022-07-07 12:03:46 +0200

    Tweaked the link for sign in

--- apps/comments-ui/src/components/NotSignedInBox.js
+++ apps/comments-ui/src/components/NotSignedInBox.js
@@ -2,7 +2,7 @@ function NotSignedInBox() {
     return (
         <section className="text-center mb-1 bg-neutral-50 rounded-lg pt-12 pb-10 px-8">
             <h1 className="text-center text-3xl font-sans font-bold mb-6 tracking-tight">Want to join the discussion?</h1>
-            <a className="bg-black text-white font-sans py-3 px-4 mb-6 rounded inline-block" href="#/portal">Sign in to comment</a>
+            <a className="bg-black text-white font-sans py-3 px-4 mb-6 rounded inline-block" href="#/portal/signin">Sign in to comment</a>
         </section>
     );
 }
