6ef488494486bc33e81133dfc3fc81617d7a3b72
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-07-08 14:24:37 +0200

    Revert "Set members helper to return 0 if api cannot be reached"
    This reverts commit 06ed36e4e522f1c93eb0830d9d63edb0f219c835.

--- core/frontend/helpers/total_members.js
+++ core/frontend/helpers/total_members.js
@@ -5,9 +5,9 @@ const {SafeString} = require('../services/handlebars');
 const {memberCountRounding, getMemberStats} = require('../utils/member-count');
 
 module.exports = async function total_members () { //eslint-disable-line
-    let memberStats = await getMemberStats() || {total: 0};
+    let memberStats = await getMemberStats();
     const {total} = memberStats;
-    return new SafeString(total > 0 ? memberCountRounding(total) : total);
+    return new SafeString(total > 0 ? memberCountRounding(total) : 0);
 };
 
 module.exports.async = true;
--- core/frontend/helpers/total_paid_members.js
+++ core/frontend/helpers/total_paid_members.js
@@ -4,9 +4,9 @@ const {SafeString} = require('../services/handlebars');
 const {memberCountRounding, getMemberStats} = require('../utils/member-count');
 
 module.exports = async function total_paid_members () { //eslint-disable-line
-    let memberStats = await getMemberStats() || {paid: 0};
+    let memberStats = await getMemberStats();
     const {paid} = memberStats;
-    return new SafeString(paid > 0 ? memberCountRounding(paid) : paid);
+    return new SafeString(paid > 0 ? memberCountRounding(paid) : 0);
 };
 
 module.exports.async = true;
