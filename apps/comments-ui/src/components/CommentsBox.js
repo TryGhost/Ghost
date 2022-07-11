3a5813fbf5f2bc857d8c1e65b70103f34cde6e17
Peter Zimon <peter.zimon@gmail.com>
2022-07-11 15:23:20 +0200

    Fixed bug for darkmode
    - Fixed a bug for darkmode not being loaded properly

--- apps/comments-ui/src/components/CommentsBox.js
+++ apps/comments-ui/src/components/CommentsBox.js
@@ -50,7 +50,7 @@ const CommentsBox = (props) => {
     };
 
     return (
-        <section className={'ghost-display' + containerClass} style={style}>
+        <section className={'ghost-display ' + containerClass} style={style}>
             <Pagination />
             <div className={!pagination ? 'mt-4' : ''}>
                 {commentsElements}
