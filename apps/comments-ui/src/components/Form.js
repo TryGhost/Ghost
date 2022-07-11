e7c2a0bbf6306e6aa6d74a5211cc766994b842dd
James Morris <moreofmorris@users.noreply.github.com>
2022-07-11 16:23:10 +0100

    Little tweak to make the transition work again on the form opening

--- apps/comments-ui/src/components/Form.js
+++ apps/comments-ui/src/components/Form.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from 'react';
+import React, {useContext} from 'react';
 import {Transition} from '@headlessui/react';
 import AppContext from '../AppContext';
 import Avatar from './Avatar';
@@ -12,12 +12,12 @@ const Form = (props) => {
     if (props.isReply) {
         config = {
             placeholder: 'Reply to comment',
-            autofocus: true
+            autofocus: false
         };
     } else if (props.isEdit) {
         config = {
             placeholder: 'Edit this comment',
-            autofocus: false, // don't use autofocus, because else the useEffect thing won't work
+            autofocus: true,
             content: props.comment.html
         };
     } else {
@@ -31,29 +31,6 @@ const Form = (props) => {
         ...getEditorConfig(config)
     });
 
-    useEffect(() => {
-        if (!editor) {
-            return;
-        }
-
-        // Focus editor + jump to end
-        if (!props.isEdit) {
-            return;
-        }
-
-        // jump to end
-        editor
-            .chain()
-            .focus()
-            .command(({tr, commands}) => {
-                return commands.setTextSelection({
-                    from: tr.doc.content.size,
-                    to: tr.doc.content.size
-                });
-            })
-            .run();
-    }, [editor, props.isEdit]);
-   
     const focused = editor?.isFocused || !editor?.isEmpty;
 
     const submitForm = async (event) => {
@@ -131,17 +108,10 @@ const Form = (props) => {
 
     return (
         <form onClick={focusEditor} className={`
-            comment-form
-            bg-white
-            transition
-            duration-200
-            rounded-md
-            pt-3 pb-2 px-3
-            -mt-[12px] -mr-3 mb-10 -ml-[12px]
-            shadow-lg
-            dark:bg-[rgba(255,255,255,0.08)]
-            dark:shadow-transparent
-            hover:shadow-xl
+            transition duration-200
+            -mt-[12px] -mr-3 mb-10 -ml-[12px] pt-3 pb-2 px-3
+            bg-white dark:bg-[rgba(255,255,255,0.08)]
+            rounded-md shadow-lg dark:shadow-transparent hover:shadow-xl
             ${!commentsCount && !props.isEdit && !props.isReply && '-mt-0 -mr-0 -ml-0'}
             ${focused ? 'cursor-default' : 'cursor-pointer'}`
         }>
@@ -150,32 +120,33 @@ const Form = (props) => {
                     <div className="relative w-full">
                         <EditorContent
                             className={`
-                                transition-[min-height]
-                                pl-[56px] px-0 py-[10px] pr-4
-                                duration-150
-                                bg-transparent
-                                w-full
-                                border-none
-                                resize-none
-                                rounded-md
-                                border
-                                border-slate-50 
-                                font-sans
-                                text-[16.5px]
-                                leading-normal
+                                transition-all duration-150
+                                w-full pl-[56px] px-0 py-[10px] pr-4
+                                bg-transparent rounded-md border-none border border-slate-50 dark:border-none
+                                font-sans text-[16.5px] leading-normal dark:text-neutral-300 
                                 focus:outline-0
-                                placeholder:text-neutral-300
-                                dark:placeholder:text-[rgba(255,255,255,0.3)]
-                                dark:border-none
-                                dark:text-neutral-300
-                                ${focused ? 'cursor-text min-h-[144px] pt-[44px] pb-[68px]' : 'cursor-pointer overflow-hidden min-h-[48px] hover:border-slate-300'}
+                                placeholder:text-neutral-300 dark:placeholder:text-[rgba(255,255,255,0.3)]  
+                                resize-none
+                                ${focused ? `cursor-textmin-h-[144px] pt-[44px] pb-[68px]` : 'cursor-pointer overflow-hidden min-h-[48px] hover:border-slate-300'}
                             `}
                             editor={editor} 
                         />
-                        <div className="flex space-x-4 transition-[opacity] duration-150 absolute -right-3 bottom-[2px]">
-                            {props.isEdit && <button type="button" className="font-sans text-sm font-medium ml-2.5 text-neutral-500 dark:text-neutral-400" onClick={props.toggle}>Cancel</button>}
+                        <div className="
+                            absolute -right-3 bottom-[2px]
+                            flex space-x-4
+                            transition-[opacity] duration-150 
+                        ">
+                            {props.isEdit &&
+                                <button type="button" onClick={props.toggle} className="font-sans text-sm font-medium ml-2.5 text-neutral-500 dark:text-neutral-400">Cancel</button>}
                             <button
-                                className={`transition-[opacity] duration-150 rounded-[4px] border py-3 px-4 font-sans text-sm text-center bg-neutral-900 font-semibold text-white dark:bg-[rgba(255,255,255,0.9)] dark:text-neutral-800`}
+                                className={`
+                                    transition-[opacity] duration-150
+                                    bg-neutral-900 dark:bg-[rgba(255,255,255,0.9)]
+                                    rounded-[4px] border
+                                    py-3 px-4
+                                    text-sm text-center font-sans font-semibold
+                                    text-white dark:text-neutral-800
+                                `}
                                 type="button"
                                 onClick={submitForm}
                             >
