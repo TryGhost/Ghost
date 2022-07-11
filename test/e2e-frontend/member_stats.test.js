1df5ba6827fef143f79b6610ecb734baebf076a6
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-07-11 19:57:57 +0200

    Added test for to check if function returns Total members.

--- test/e2e-frontend/member_stats.test.js
+++ test/e2e-frontend/member_stats.test.js
@@ -7,14 +7,22 @@ describe('Front-end member stats ', function () {
         const {free} = members;
         should.exist(free);
     });
+
     it('should return paid', async function () {
         const members = await getMemberStats();
         const {paid} = members;
         should.exist(paid);
     });
+
     it('should return comped', async function () {
         const members = await getMemberStats();
         const {comped} = members;
         should.exist(comped);
+    });
+
+    it('should return total', async function () {
+        const members = await getMemberStats();
+        const {total} = members;
+        should.exist(total);
     });
 });
