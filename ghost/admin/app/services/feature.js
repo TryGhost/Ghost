cea3ed146170daf569cf4cef1a187ab985ba1681
Rishabh <zrishabhgarg@gmail.com>
2022-07-12 13:22:26 +0530

    Removed sodo search alpha feature flag
    refs https://github.com/TryGhost/Team/issues/1665
    
    - removes feature flag for sodo search as its now bumped to GA

--- ghost/admin/app/services/feature.js
+++ ghost/admin/app/services/feature.js
@@ -58,7 +58,6 @@ export default class FeatureService extends Service {
     // labs flags
     @feature('urlCache') urlCache;
     @feature('beforeAfterCard') beforeAfterCard;
-    @feature('sodoSearch') sodoSearch;
     @feature('comments') comments;
 
     _user = null;
