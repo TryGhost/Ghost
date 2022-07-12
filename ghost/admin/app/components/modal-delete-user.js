7999e05ea8cf072e23f91465930785da26fc8dd2
Naz <nazargargol@gmail.com>
2022-07-12 13:09:02 +0100

    Updated copy for user deletion modal
    refs https://github.com/TryGhost/Ghost/issues/15008
    
    - When the user is removed the new flow adds a special tag and reassignes all the user-owned posts to the owner user. Updated copy to inform about this behavior clearly

--- ghost/admin/app/components/modal-delete-user.js
+++ ghost/admin/app/components/modal-delete-user.js
@@ -1,8 +1,11 @@
 import ModalComponent from 'ghost-admin/components/modal-base';
 import {alias} from '@ember/object/computed';
+import {inject as service} from '@ember/service';
 import {task} from 'ember-concurrency';
 
 export default ModalComponent.extend({
+    store: service(),
+
     // Allowed actions
     confirm: () => {},
 
@@ -12,6 +15,10 @@ export default ModalComponent.extend({
         confirm() {
             this.deleteUser.perform();
         }
+    },
+
+    get ownerUser() {
+        return this.store.peekAll('user').findBy('isOwnerOnly', true);
     },
 
     deleteUser: task(function* () {
