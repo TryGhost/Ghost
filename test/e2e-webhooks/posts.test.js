f0da458dc834a934554ef1b5cb0f23530b9ccf72
Naz <nazargargol@gmail.com>
2022-07-13 11:58:23 +0100

    Updated webhook snapshots
    refs TryGhost/Toolbox#340
    refs TryGhost#13451
    
    - Fixed the snapshot files which had to include new fields like "comment_notifications" as well as other properties that came along with the hacky solution to the webhook's auhor inclusion bug
    - Also documented the "roles" being included inconsistently across different events - it's a BUG and should be looked into one day when we care enough.

--- test/e2e-webhooks/posts.test.js
+++ test/e2e-webhooks/posts.test.js
@@ -7,13 +7,27 @@ const tierSnapshot = {
     updated_at: anyISODateTime
 };
 
-const authorSnapshot = {
-    last_seen: anyISODateTime,
-    created_at: anyISODateTime,
-    updated_at: anyISODateTime
+const buildAuthorSnapshot = (roles = false) => {
+    const authorSnapshot = {
+        last_seen: anyISODateTime,
+        created_at: anyISODateTime,
+        updated_at: anyISODateTime
+    };
+
+    // NOTE: this is such a bad hack! for the reasons I did not investigate the "add" event does not include
+    //       the roles but the "published" does! massive inconsistency and needs to be fixed one day
+    if (roles) {
+        authorSnapshot.roles = new Array(1).fill({
+            id: anyObjectId,
+            created_at: anyISODateTime,
+            updated_at: anyISODateTime
+        });
+    }
+
+    return authorSnapshot;
 };
 
-const buildPostSnapshotWithTiers = ({published, tiersCount}) => {
+const buildPostSnapshotWithTiers = ({published, tiersCount, roles = false}) => {
     return {
         id: anyObjectId,
         uuid: anyUuid,
@@ -25,8 +39,8 @@ const buildPostSnapshotWithTiers = ({published, tiersCount}) => {
         //        this matcher should be removed once the issue is solved
         url: stringMatching(/http:\/\/127.0.0.1:2369\/\w+\//),
         tiers: new Array(tiersCount).fill(tierSnapshot),
-        primary_author: authorSnapshot,
-        authors: new Array(1).fill(authorSnapshot)
+        primary_author: buildAuthorSnapshot(roles),
+        authors: new Array(1).fill(buildAuthorSnapshot(roles))
     };
 };
 
@@ -55,7 +69,7 @@ describe('post.* events', function () {
         mockManager.restore();
     });
 
-    it('post.published even is triggered', async function () {
+    it('post.published event is triggered', async function () {
         const webhookURL = 'https://test-webhook-receiver.com/post-published/';
         await webhookMockReceiver.mock(webhookURL);
         await fixtureManager.insertWebhook({
@@ -91,14 +105,17 @@ describe('post.* events', function () {
                 post: {
                     current: buildPostSnapshotWithTiers({
                         published: true,
-                        tiersCount: 2
+                        tiersCount: 2,
+                        roles: true
                     }),
-                    previous: buildPreviousPostSnapshotWithTiers({tiersCount: 2})
+                    previous: buildPreviousPostSnapshotWithTiers({
+                        tiersCount: 2
+                    })
                 }
             });
     });
 
-    it('post.added even is triggered', async function () {
+    it('post.added event is triggered', async function () {
         const webhookURL = 'https://test-webhook-receiver.com/post-added/';
         await webhookMockReceiver.mock(webhookURL);
         await fixtureManager.insertWebhook({
