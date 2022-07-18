39b4767defd3bbfff5de83b35d1c66959a943db6
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-07-18 10:50:03 +0100

    Fixed linter error for use of `{{action}}` in editor-loading controller
    refs https://github.com/TryGhost/Ghost/issues/14101
    
    - `{{action}}` is deprecated and should be replaced with `{{on}}` and `{{fn}}` with direct use of methods
    - switched to calling `ui.setMainClass` action directly

--- ghost/admin/app/controllers/editor/edit-loading.js
+++ ghost/admin/app/controllers/editor/edit-loading.js
@@ -1,9 +1,5 @@
 import Controller from '@ember/controller';
-import classic from 'ember-classic-decorator';
 import {inject as service} from '@ember/service';
-
-/* eslint-disable ghost/ember/alias-model-in-controller */
-@classic
 export default class EditLoadingController extends Controller {
     @service ui;
 }
