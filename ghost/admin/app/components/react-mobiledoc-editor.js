38e7f0406489125b95489f6455451ca458770dd3
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-07-19 11:34:26 +0100

    Switched to dynamically loaded external react-mobiledoc-editor package
    no issue
    
    - dropped the bundled `react-mobiledoc-editor` package
    - updated `<ReactMobiledocEditor>` component to pull in editor components dynamically
      - added a resource function to dynamically import the external module
      - added `Container` and `Editor` components that read from the resource function and wrap components of the same name from the external module
      - added `<Suspense>` around the `Container` and `Editor` components so that React will show a loading state whilst the external components are still being fetched
      - added `<ErrorHandler>` so we can show an error state if loading fails

--- ghost/admin/app/components/react-mobiledoc-editor.js
+++ ghost/admin/app/components/react-mobiledoc-editor.js
@@ -1,12 +1,85 @@
-import React from 'react';
-import {Container, Editor} from 'react-mobiledoc-editor';
+import React, {Suspense} from 'react';
 
-export default class ReactMobiledocEditor extends React.Component {
-    render() {
-        return (<Container
-            mobiledoc={this.props.mobiledoc}
-        >
-            <Editor />
-        </Container>);
+class ErrorHandler extends React.Component {
+    state = {
+        hasError: false
+    };
+
+    static getDerivedStateFromError() {
+        return {hasError: true};
     }
+
+    render() {
+        if (this.state.hasError) {
+            return (
+                <p>Loading has failed. Try refreshing the browser!</p>
+            );
+        }
+
+        return this.props.children;
+    }
+}
+
+const fetchMobiledocEditor = function () {
+    let status = 'pending';
+    let response;
+
+    const fetchPackage = async () => {
+        if (window.ReactMobiledocEditor) {
+            return window.ReactMobiledocEditor;
+        }
+
+        await import('https://unpkg.com/kevinansfield-react-mobiledoc-editor@~0.13.2/dist/main.js');
+        return window.ReactMobiledocEditor;
+    };
+
+    const suspender = fetchPackage().then(
+        (res) => {
+            status = 'success';
+            response = res;
+        },
+        (err) => {
+            status = 'error';
+            response = err;
+        }
+    );
+
+    const read = () => {
+        switch (status) {
+        case 'pending':
+            throw suspender;
+        case 'error':
+            throw response;
+        default:
+            return response;
+        }
+    };
+
+    return {read};
+};
+
+const editorResource = fetchMobiledocEditor();
+
+const Container = (props) => {
+    const MobiledocEditor = editorResource.read();
+    return <MobiledocEditor.Container {...props} />;
+};
+
+const Editor = (props) => {
+    const MobiledocEditor = editorResource.read();
+    return <MobiledocEditor.Editor {...props} />;
+};
+
+export default function ReactMobiledocEditorComponent(props) {
+    return (
+        <ErrorHandler>
+            <Suspense fallback={<p>Loading editor...</p>}>
+                <Container
+                    mobiledoc={props.mobiledoc}
+                >
+                    <Editor />
+                </Container>
+            </Suspense>
+        </ErrorHandler>
+    );
 }
