4b504130d86346554323feb12ba93e91db05b6b1
James Morris <moreofmorris@users.noreply.github.com>
2022-07-19 15:24:55 +0100

    Working progress of the button when sending reporting
    refs https://github.com/TryGhost/Team/issues/1685

--- apps/comments-ui/src/components/modals/NotAuthorContextMenu.js
+++ apps/comments-ui/src/components/modals/NotAuthorContextMenu.js
@@ -5,23 +5,41 @@ import ReportDialog from './ReportDialog';
 
 const NotAuthorContextMenu = (props) => {
     // const {dispatchAction} = useContext(AppContext);
-    let [isOpen, setIsOpen] = useState(false);
+    let [isOpen, setOpen] = useState(false);
+    let [reportProgress, setProgress] = useState('default'); // states for button: default, sending, sent
 
-    const reportComment = (event) => {
-        // dispatchAction('reportComment', props.comment);
-        setIsOpen(true);
+    const openModal = () => {
+        setProgress('default');
+        setOpen(true);
     };
 
     const closeModal = () => {
-        setIsOpen(false);
+        setProgress('default');
+        setOpen(false);
+    };
+
+    const reportComment = (event) => {
+        event.stopPropagation();
+
+        setProgress('sending');
+
+        // faked timing of the report being sent for user feedback purposes
+        setTimeout(() => {
+            setProgress('sent');
+            // dispatchAction('reportComment', props.comment);
+
+            setTimeout(() => {
+                setOpen(false);
+            }, 750);
+        }, 1000);
     };
 
     return (
         <div className="flex flex-col">
-            <button className="text-[14px]" onClick={reportComment}>
+            <button className="text-[14px]" onClick={openModal}>
                 Report comment
             </button>
-            <ReportDialog show={isOpen} submit={reportComment} cancel={closeModal} />
+            <ReportDialog show={isOpen} progress={reportProgress} submit={reportComment} cancel={closeModal} />
         </div>
     );
 };
--- apps/comments-ui/src/components/modals/ReportDialog.js
+++ apps/comments-ui/src/components/modals/ReportDialog.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import GenericDialog from './GenericDialog';
+import {ReactComponent as SpinnerIcon} from '../../images/icons/spinner.svg';
+import {ReactComponent as SuccessIcon} from '../../images/icons/success.svg';
 
 const ReportDialog = (props) => {
+    let buttonColor = 'bg-red-600';
+    if (props.progress === 'sent') {
+        buttonColor = 'bg-green-600';
+    }
+
+    let buttonText = 'Yes';
+    if (props.progress === 'sending') {
+        buttonText = 'Sending';
+    } else if (props.progress === 'sent') {
+        buttonText = 'Sent';
+    }
+
+    let buttonIcon = null;
+    if (props.progress === 'sending') {
+        buttonIcon = <SpinnerIcon className="w-[16px] h-[16px] mr-2" />;
+    } else if (props.progress === 'sent') {
+        buttonIcon = <SuccessIcon className="w-[16px] h-[16px] mr-2" />;
+    }
+
     return (
-        <GenericDialog show={props.show} submit={props.submit} cancel={props.cancel}>
+        <GenericDialog show={props.show} cancel={props.cancel}>
             <h1 className="font-sans font-bold tracking-tight text-[24px] mb-3">You sure you want to report?</h1>
             <p className="font-sans text-[1.45rem] text-neutral-500">You request will be sent to the owner of this site.</p>
             <div className="mt-10">
-                <button className="w-full h-[44px] bg-red-600 px-8 flex items-center justify-center rounded-md text-white font-sans font-semibold text-lg" onClick={props.submit}>Yes</button>
+                <button
+                    className={`transition duration-200 ease-linear w-full h-[44px] px-8 flex items-center justify-center rounded-md text-white font-sans font-semibold text-[15px] ${buttonColor}`}
+                    onClick={props.submit}
+                >
+                    {buttonIcon}{buttonText}
+                </button>
                 <p className="font-sans font-medium text-[1.45rem] text-neutral-500 mt-4 -mb-1">No, <button className="font-sans underline" onClick={props.cancel}>I've changed my mind</button></p>
             </div>
         </GenericDialog>
