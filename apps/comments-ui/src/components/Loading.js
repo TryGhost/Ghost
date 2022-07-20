48b4aaf83808e312ac564d3cb9ee837726b0b776
James Morris <moreofmorris@users.noreply.github.com>
2022-07-20 16:36:15 +0100

    Fixed up the loading state with proper positioning and adjusted colours
    refs https://github.com/TryGhost/Team/issues/1692

--- apps/comments-ui/src/components/Loading.js
+++ apps/comments-ui/src/components/Loading.js
@@ -3,8 +3,8 @@ import {ReactComponent as SpinnerIcon} from '../images/icons/spinner.svg';
 
 function Loading() {
     return (
-        <div className="ghost-display flex items-center justify-center w-full h-36">
-            <SpinnerIcon className="w-12 h-12 mb-6 fill-[rgba(0,0,0,0.3)] dark:fill-[rgba(255,255,255,0.6)]" />
+        <div className="flex items-center justify-center w-full h-32">
+            <SpinnerIcon className="w-12 h-12 mb-6 fill-[rgb(225,225,225,0.9)] dark:fill-[rgba(255,255,255,0.6)]" />
         </div>
     );
 }
