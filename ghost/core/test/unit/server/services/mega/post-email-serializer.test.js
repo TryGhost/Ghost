1b3ba7e70a171221792dc6c732bac2b1d85fbf76
Rishabh <zrishabhgarg@gmail.com>
2022-07-21 20:52:10 +0530

    Added post email serializer tests

--- ghost/core/test/unit/server/services/mega/post-email-serializer.test.js
+++ ghost/core/test/unit/server/services/mega/post-email-serializer.test.js
@@ -3,10 +3,16 @@ const sinon = require('sinon');
 const settingsCache = require('../../../../../core/shared/settings-cache');
 const models = require('../../../../../core/server/models');
 const urlUtils = require('../../../../../core/shared/url-utils');
+const urlService = require('../../../../../core/server/services/url');
+const labs = require('../../../../../core/shared/labs');
 
-const {parseReplacements, renderEmailForSegment, _getTemplateSettings, createUnsubscribeUrl} = require('../../../../../core/server/services/mega/post-email-serializer');
+const {parseReplacements, renderEmailForSegment, _getTemplateSettings, createUnsubscribeUrl, createPostSignupUrl} = require('../../../../../core/server/services/mega/post-email-serializer');
 
 describe('Post Email Serializer', function () {
+    afterEach(function () {
+        sinon.restore();
+    });
+
     it('creates replacement pattern for valid format and value', function () {
         const html = '<html>Hey %%{first_name}%%, what is up?</html>';
         const plaintext = 'Hey %%{first_name}%%, what is up?';
@@ -84,6 +90,74 @@ describe('Post Email Serializer', function () {
             output.html.should.equal('hello');
             output.plaintext.should.equal('hello');
         });
+
+        it('should show paywall content for free members on paid posts', function () {
+            sinon.stub(urlService, 'getUrlByResourceId').returns('https://site.com/blah/');
+            sinon.stub(labs, 'isSet').returns(true);
+            const email = {
+                post: {
+                    status: 'published',
+                    visibility: 'paid'
+                },
+                html: '<p>Free content</p><!--members-only--><p>Members content</p>',
+                plaintext: 'Free content. Members content'
+            };
+
+            let output = renderEmailForSegment(email, 'status:free');
+            output.html.should.equal(`<p>Free content</p><div class="align-center" style="text-align: center;">\n    <hr style="position: relative; display: block; width: 100%; margin: 3em 0; padding: 0; height: 1px; border: 0; border-top: 1px solid #e5eff5;">\n    <h2 style="margin-top: 0; font-family: -apple-system, BlinkMacSystemFont, &#39;Segoe UI&#39;, Roboto, Helvetica, Arial, sans-serif, &#39;Apple Color Emoji&#39;, &#39;Segoe UI Emoji&#39;, &#39;Segoe UI Symbol&#39;; line-height: 1.11em; font-weight: 700; text-rendering: optimizeLegibility; margin: 1.5em 0 0.5em 0; font-size: 26px;">\n        Subscribe to continue reading.</h2>\n    <p style="margin: 0 0 1.5em 0; line-height: 1.6em;">Become a paid member of Ghost to get access to all\n        subscriber-only content.</p>\n    <p style="margin: 0 0 1.5em 0; line-height: 1.6em;"></p>\n    <div class="btn btn-accent" style="box-sizing: border-box; width: 100%; display: table;">\n        <table border="0" cellspacing="0" cellpadding="0" align="center" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;">\n            <tbody>\n                <tr>\n                    <td align="center" style="font-family: -apple-system, BlinkMacSystemFont, &#39;Segoe UI&#39;, Roboto, Helvetica, Arial, sans-serif, &#39;Apple Color Emoji&#39;, &#39;Segoe UI Emoji&#39;, &#39;Segoe UI Symbol&#39;; font-size: 18px; vertical-align: top; color: #15212A; border-radius: 5px; text-align: center; background-color: #6D2EFF;" valign="top" bgcolor="undefined"><a href="https://site.com/blah/#/portal/signup" style="overflow-wrap: anywhere; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; display: inline-block; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; text-decoration: none; background-color: undefined; border-color: undefined; color: #FFFFFF;" target="_blank">Subscribe</a></td>\n                </tr>\n            </tbody>\n        </table>\n    </div>\n    <p style="margin: 0 0 1.5em 0; line-height: 1.6em;"></p>\n</div>&gt;`);
+            output.plaintext.should.equal(`Free content\n\n\n----------------------------------------\n\n\n\n\nSubscribe to continue reading.\n\n\nBecome a paid member of Ghost to get access to all\nsubscriber-only content.\n\n\n\n\n\n\n\n\n\nSubscribe [https://site.com/blah/#/portal/signup]\n\n\n\n\n\n\n\n\n\n>`);
+        });
+
+        it('should show full cta for paid members on paid posts', function () {
+            sinon.stub(urlService, 'getUrlByResourceId').returns('https://site.com/blah/');
+            sinon.stub(labs, 'isSet').returns(true);
+            const email = {
+                post: {
+                    status: 'published',
+                    visibility: 'paid'
+                },
+                html: '<p>Free content</p><!--members-only--><p>Members content</p>',
+                plaintext: 'Free content. Members content'
+            };
+
+            let output = renderEmailForSegment(email, 'status:-free');
+            output.html.should.equal(`<p>Free content</p><!--members-only--><p>Members content</p>`);
+            output.plaintext.should.equal(`Free content\n\nMembers content`);
+        });
+
+        it('should show full content for free members on free posts', function () {
+            sinon.stub(urlService, 'getUrlByResourceId').returns('https://site.com/blah/');
+            sinon.stub(labs, 'isSet').returns(true);
+            const email = {
+                post: {
+                    status: 'published',
+                    visibility: 'public'
+                },
+                html: '<p>Free content</p><!--members-only--><p>Members content</p>',
+                plaintext: 'Free content. Members content'
+            };
+
+            let output = renderEmailForSegment(email, 'status:free');
+            output.html.should.equal(`<p>Free content</p><!--members-only--><p>Members content</p>`);
+            output.plaintext.should.equal(`Free content\n\nMembers content`);
+        });
+
+        it('should show full content for paid members on free posts', function () {
+            sinon.stub(urlService, 'getUrlByResourceId').returns('https://site.com/blah/');
+            sinon.stub(labs, 'isSet').returns(true);
+            const email = {
+                post: {
+                    status: 'published',
+                    visibility: 'public'
+                },
+                html: '<p>Free content</p><!--members-only--><p>Members content</p>',
+                plaintext: 'Free content. Members content'
+            };
+
+            let output = renderEmailForSegment(email, 'status:-free');
+            output.html.should.equal(`<p>Free content</p><!--members-only--><p>Members content</p>`);
+            output.plaintext.should.equal(`Free content\n\nMembers content`);
+        });
     });
 
     describe('createUnsubscribeUrl', function () {
@@ -111,6 +185,35 @@ describe('Post Email Serializer', function () {
             sinon.stub(urlUtils, 'getSiteUrl').returns('https://site.com/blah');
             const unsubscribeUrl = createUnsubscribeUrl('post-abcd', 'newsletter-abcd');
             unsubscribeUrl.should.eql('https://site.com/blah/unsubscribe/?uuid=post-abcd&newsletter=newsletter-abcd');
+        });
+    });
+
+    describe('createPostSignupUrl', function () {
+        before(function () {
+            models.init();
+        });
+
+        afterEach(function () {
+            sinon.restore();
+        });
+
+        it('generates signup url on post for published post', function () {
+            sinon.stub(urlService, 'getUrlByResourceId').returns('https://site.com/blah/');
+            const unsubscribeUrl = createPostSignupUrl({
+                status: 'published',
+                id: 'abc123'
+            });
+            unsubscribeUrl.should.eql('https://site.com/blah/#/portal/signup');
+        });
+
+        it('generates signup url on homepage for email only post', function () {
+            sinon.stub(urlService, 'getUrlByResourceId').returns('https://site.com/test/404/');
+            sinon.stub(urlUtils, 'getSiteUrl').returns('https://site.com/test/');
+            const unsubscribeUrl = createPostSignupUrl({
+                status: 'sent',
+                id: 'abc123'
+            });
+            unsubscribeUrl.should.eql('https://site.com/test/#/portal/signup');
         });
     });
 
