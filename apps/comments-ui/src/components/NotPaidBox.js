59e29f30b00ced03e52fbe7102535cd461e82678
James Morris <moreofmorris@users.noreply.github.com>
2022-07-21 18:43:32 +0100

    Fixed the bug where the portal windows open inside the iFrame
    refs https://github.com/TryGhost/Team/issues/1706

--- apps/comments-ui/src/components/NotPaidBox.js
+++ apps/comments-ui/src/components/NotPaidBox.js
@@ -14,12 +14,16 @@ const NotPaidBox = (props) => {
 
     const titleText = (props.isFirst ? 'Want to be the first to comment?' : 'Want to join the discussion?');
 
+    const handleSubscribeClick = (event) => {
+        window.location.href = '#/portal/signup';
+    };
+
     return (
         <section className="text-center mb-1 bg-neutral-900 rounded-lg pt-12 pb-10 px-8" style={boxStyle}>
             <h1 className="text-center text-white text-[28px] font-sans font-semibold mb-6 tracking-tight">{titleText}</h1>
-            <a className="bg-white font-sans py-3 px-4 mb-6 rounded inline-block font-medium" style={buttonStyle} href="#/portal/signup">
+            <button onClick={handleSubscribeClick} className="bg-white font-sans py-3 px-4 mb-6 rounded inline-block font-medium" style={buttonStyle}>
                 Subscribe now
-            </a>
+            </button>
             <p className="font-sans text-center text-white">
                 You need to be subscribed to a paid plan to be able to join the discussion.
             </p>
--- apps/comments-ui/src/components/NotSignedInBox.js
+++ apps/comments-ui/src/components/NotSignedInBox.js
@@ -14,15 +14,23 @@ const NotSignedInBox = (props) => {
 
     const titleText = (props.isFirst ? 'Want to be the first to comment?' : 'Want to join the discussion?');
 
+    const handleSubscribeClick = (event) => {
+        window.location.href = '#/portal/signup';
+    };
+
+    const handleSignInClick = (event) => {
+        window.location.href = '#/portal/signin';
+    };
+
     return (
         <section className="text-center mb-1 bg-neutral-900 rounded-lg pt-12 pb-10 px-8" style={boxStyle}>
             <h1 className="text-center text-white text-[28px] font-sans font-semibold mb-6 tracking-tight">{titleText}</h1>
-            <a className="bg-white font-sans py-3 px-4 mb-6 rounded inline-block font-medium" style={buttonStyle} href="#/portal/signup">
+            <button onClick={handleSubscribeClick} className="bg-white font-sans py-3 px-4 mb-6 rounded inline-block font-medium" style={buttonStyle}>
                 Subscribe now
-            </a>
+            </button>
             <p className="font-sans text-center text-white">
                 <span className='inline-block mr-1'>Already have an account?</span>
-                <a className="text-white underline" href="#/portal/signin">Sign in</a>
+                <button onClick={handleSignInClick} className="text-white underline">Sign in</button>
             </p>
         </section>
     );
