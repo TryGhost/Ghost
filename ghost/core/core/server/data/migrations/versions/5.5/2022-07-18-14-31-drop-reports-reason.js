97c349552065c111ca1567e6c9de6fd35c0985ac
Simon Backx <simon@ghost.org>
2022-07-22 10:55:10 +0200

    fixup! Added report API for comments

--- ghost/core/core/server/data/migrations/versions/5.5/2022-07-18-14-31-drop-reports-reason.js
+++ ghost/core/core/server/data/migrations/versions/5.5/2022-07-18-14-31-drop-reports-reason.js
@@ -1,3 +1,3 @@
 const {createDropColumnMigration} = require('../../utils');
 
-module.exports = createDropColumnMigration('comment_reports', 'reason', {});
+module.exports = createDropColumnMigration('comment_reports', 'reason', {type: 'text', maxlength: 65535, nullable: false});
