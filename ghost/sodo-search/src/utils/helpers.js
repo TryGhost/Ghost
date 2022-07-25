99b87d8d10c51cfa2076a871e1bce433e2e3fb19
Rishabh Garg <zrishabhgarg@gmail.com>
2022-07-25 22:23:09 +0530

    Updated hardcoded styles url to use config instead (#2)
    refs https://github.com/TryGhost/Team/issues/1719
    
    - the stylesheet link was hardcoded in the app, which made it impossible to configure if config changed in Ghost
    - fetches styles url from the data attribute on script instead, which allows app to load styles based on config in Ghost

--- ghost/sodo-search/src/utils/helpers.js
+++
@@ -1,7 +0,0 @@
-export function getBundledCssLink({appVersion}) {
-    if (process.env.NODE_ENV === 'production' && appVersion) {
-        return `https://unpkg.com/@tryghost/sodo-search@~${appVersion}/umd/main.css`;
-    } else {
-        return 'http://localhost:3000/main.css';
-    }
-}
