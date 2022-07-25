544a6811a78210da3c29ed2df039ae8169fcb03d
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-07-25 17:13:24 +0200

    Fixed H1 for test to pass.

--- koenig/koenig-react/src/App.js
+++ koenig/koenig-react/src/App.js
@@ -5,7 +5,7 @@ import './index.css';
 function koenigEditor() {
     return (
         <>
-            <h1 className='font-bold text-5xl'>The Editor</h1>
+            <h1 className='font-bold text-5xl'>The Editor!</h1>
             <Container className="my-2 px-2 md:mx-auto md:my-16 max-w-xl w-full">
                 <Toolbar className="flex" />
                 <Editor className="prose"/>
