fe653156dcab488e3b004986033367cf30cdf1f6
Daniel Lockyer <hi@daniellockyer.com>
2022-07-26 12:37:57 +0200

    Deleted `moleculer-service-from-class` package
    refs https://github.com/TryGhost/Toolbox/issues/354
    
    - this is no longer used as the original project it was developed for
      was done in a different way

--- ghost/moleculer-service-from-class/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./lib/moleculer-service-from-class.js');
--- ghost/moleculer-service-from-class/types/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _exports: typeof import("./lib/moleculer-service-from-class.js");
-export = _exports;
