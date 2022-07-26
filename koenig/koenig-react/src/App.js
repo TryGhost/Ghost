6b14c19fd849f49406f23d3e56351a3ac40e62df
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-07-26 14:40:42 +0100

    Added passthrough of mobiledoc and onChange props
    no issue
    
    - pass through `mobiledoc` and `onChange` props to the `react-mobiledoc-editor` components so that the consuming app can provide loaded content and react to changes

--- koenig/koenig-react/src/App.js
+++ koenig/koenig-react/src/App.js
@@ -2,11 +2,11 @@ import * as React from 'react';
 import Koenig from './components/Koenig';
 import './index.css';
 
-function koenigEditor() {
+function koenigEditor({mobiledoc, onChange}) {
     return (
         <>
             <h1 className='font-bold text-5xl'>The Editor!</h1>
-            <Koenig />
+            <Koenig mobiledoc={mobiledoc} onChange={onChange} />
         </>
     );
 }
--- koenig/koenig-react/src/components/Koenig.js
+++ koenig/koenig-react/src/components/Koenig.js
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import {Editor, Container, Toolbar} from 'react-mobiledoc-editor';
 
-function koenigEditor() {
+function koenigEditor({mobiledoc, onChange}) {
     return (
-        <Container className="my-2 px-2 md:mx-auto md:my-16 max-w-xl w-full">
+        <Container
+            className="my-2 px-2 md:mx-auto md:my-16 max-w-xl w-full"
+            mobiledoc={mobiledoc}
+            onChange={onChange}
+        >
             <Toolbar className="flex" />
             <Editor className="prose"/>
         </Container>
