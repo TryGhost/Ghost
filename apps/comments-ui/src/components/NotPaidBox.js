0f3646681d9f83f20d4840abe11aa9fb72d3b707
Djordje Vlaisavljevic <dzvlais@gmail.com>
2022-07-27 10:11:51 +0200

    Updated CTA design
    refs #1704

--- apps/comments-ui/src/components/NotPaidBox.js
+++ apps/comments-ui/src/components/NotPaidBox.js
@@ -4,12 +4,8 @@ import AppContext from '../AppContext';
 const NotPaidBox = (props) => {
     const {accentColor} = useContext(AppContext);
 
-    const boxStyle = {
-        background: accentColor
-    };
-
     const buttonStyle = {
-        color: accentColor
+        backgroundColor: accentColor
     };
 
     const titleText = (props.isFirst ? 'Want to be the first to comment?' : 'Want to join the discussion?');
@@ -19,12 +15,12 @@ const NotPaidBox = (props) => {
     };
 
     return (
-        <section className="text-center mb-1 bg-neutral-900 rounded-lg pt-12 pb-10 px-8" style={boxStyle}>
-            <h1 className="text-center text-white text-[28px] font-sans font-semibold mb-6 tracking-tight">{titleText}</h1>
-            <button onClick={handleSubscribeClick} className="bg-white font-sans py-3 px-4 mb-6 rounded inline-block font-medium" style={buttonStyle}>
+        <section className="text-center mb-1 pt-12 pb-10 px-8 border-t border-[#f1f1f1]">
+            <h1 className="text-center text-black text-[22px] font-sans font-semibold mb-4 tracking-tight dark:text-white">{titleText}</h1>
+            <button onClick={handleSubscribeClick} className="text-white font-sans py-3 px-4 mb-6 rounded inline-block font-medium" style={buttonStyle}>
                 Subscribe now
             </button>
-            <p className="font-sans text-center text-white">
+            <p className="font-sans text-center text-neutral-500 dark:text-[rgba(255,255,255,0.5)]">
                 You need to be subscribed to a paid plan to be able to join the discussion.
             </p>
         </section>
--- apps/comments-ui/src/components/NotSignedInBox.js
+++ apps/comments-ui/src/components/NotSignedInBox.js
@@ -4,11 +4,11 @@ import AppContext from '../AppContext';
 const NotSignedInBox = (props) => {
     const {accentColor} = useContext(AppContext);
 
-    const boxStyle = {
-        background: accentColor
+    const buttonStyle = {
+        backgroundColor: accentColor
     };
 
-    const buttonStyle = {
+    const linkStyle = {
         color: accentColor
     };
 
@@ -23,14 +23,14 @@ const NotSignedInBox = (props) => {
     };
 
     return (
-        <section className="text-center mb-1 bg-neutral-900 rounded-lg pt-12 pb-10 px-8" style={boxStyle}>
-            <h1 className="text-center text-white text-[28px] font-sans font-semibold mb-6 tracking-tight">{titleText}</h1>
-            <button onClick={handleSubscribeClick} className="bg-white font-sans py-3 px-4 mb-6 rounded inline-block font-medium" style={buttonStyle}>
+        <section className="text-center mb-1 pt-12 pb-10 px-8 border-t border-[#f1f1f1]">
+            <h1 className="text-center text-black text-[22px] font-sans font-semibold mb-4 tracking-tight dark:text-white">{titleText}</h1>
+            <button onClick={handleSubscribeClick} className="text-white font-sans py-3 px-4 mb-6 rounded inline-block font-medium" style={buttonStyle}>
                 Subscribe now
             </button>
-            <p className="font-sans text-center text-white">
+            <p className="font-sans text-center text-neutral-500 dark:text-[rgba(255,255,255,0.5)]">
                 <span className='inline-block mr-1'>Already have an account?</span>
-                <button onClick={handleSignInClick} className="text-white underline">Sign in</button>
+                <button onClick={handleSignInClick} clasName="rounded-md" style={linkStyle}>Sign in</button>
             </p>
         </section>
     );
