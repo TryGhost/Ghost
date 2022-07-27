c6af3f989dc25e44956591f1d5a3c5d26097327a
Aileen Nowak <AileenCGN@gmail.com>
2022-07-27 14:55:48 +0100

    Added migrations to change Ghost Explore integration type
    no issue

---
+++ ghost/core/core/server/data/migrations/versions/5.6/2022-07-27-13-40-change-explore-type.js
@@ -0,0 +1,19 @@
+const logging = require('@tryghost/logging');
+const {createTransactionalMigration} = require('../../utils');
+
+module.exports = createTransactionalMigration(
+    async function up(knex) {
+        logging.info('Changing Ghost Explore Integration to type "builtin"');
+        await knex('integrations')
+            .where('slug', 'ghost-explore')
+            .update('type', 'builtin');
+        return;
+    },
+    async function down(knex) {
+        logging.info('Deleting Ghost Explore Integration to type "internal"');
+
+        await knex('integrations')
+            .where('slug', 'ghost-explore')
+            .update('type', 'internal');
+    }
+);
