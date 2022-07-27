db40b54f5b4467867b6a0c761e40e39619762974
Djordje Vlaisavljevic <dzvlais@gmail.com>
2022-07-27 10:11:13 +0200

    Updated empty state design
    refs #1698

--- apps/comments-ui/src/components/Empty.js
+++ apps/comments-ui/src/components/Empty.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Empty = (props) => {
     return (
-        <div className="pr-8 pt-2 pb-10 italic text-center font-sans leading-normal text-neutral-400 dark:text-[rgba(255,255,255,0.85)]">
+        <div className="px-2 pt-0 pb-10 font-sans text-center leading-normal text-neutral-400 dark:text-[rgba(255,255,255,0.85)]">
             There are currently no comments.
         </div>
     );
