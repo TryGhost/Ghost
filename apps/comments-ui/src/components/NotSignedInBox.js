a4fe453bfad9d2c2a8ce07059ca3df233d95ff85
Djordje Vlaisavljevic <dzvlais@gmail.com>
2022-07-27 11:15:36 +0200

    Fixed CTA spacing in empty state
    refs #1704

--- apps/comments-ui/src/components/NotSignedInBox.js
+++ apps/comments-ui/src/components/NotSignedInBox.js
@@ -12,6 +12,8 @@ const NotSignedInBox = (props) => {
         color: accentColor
     };
 
+    const firstComment = (props.isFirst ? {borderTop: '0px', paddingTop: '0'} : {});
+
     const titleText = (props.isFirst ? 'Want to be the first to comment?' : 'Want to join the discussion?');
 
     const handleSubscribeClick = (event) => {
@@ -23,7 +25,7 @@ const NotSignedInBox = (props) => {
     };
 
     return (
-        <section className="text-center mb-1 pt-12 pb-10 px-8 border-t border-[#f1f1f1]">
+        <section className="text-center mb-1 pt-12 pb-10 px-8 border-t border-[#f1f1f1]" style={firstComment}>
             <h1 className="text-center text-black text-[22px] font-sans font-semibold mb-4 tracking-tight dark:text-white">{titleText}</h1>
             <button onClick={handleSubscribeClick} className="text-white font-sans py-3 px-4 mb-6 rounded inline-block font-medium" style={buttonStyle}>
                 Subscribe now
