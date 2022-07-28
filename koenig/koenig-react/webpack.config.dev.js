75efc183fef37e26873fa9a40214d7e9acc38fb5
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-07-28 12:40:34 +0100

    whitespace adjustments

--- koenig/koenig-react/webpack.config.dev.js
+++ koenig/koenig-react/webpack.config.dev.js
@@ -55,9 +55,9 @@ module.exports = {
         function ignoreSourcemapsloaderWarnings(warning) {
             return (
                 warning.module &&
-        warning.module.resource.includes('node_modules') &&
-        warning.details &&
-        warning.details.includes('source-map-loader')
+                warning.module.resource.includes('node_modules') &&
+                warning.details &&
+                warning.details.includes('source-map-loader')
             );
         }
     ]
--- koenig/koenig-react/webpack.config.js
+++ koenig/koenig-react/webpack.config.js
@@ -61,9 +61,9 @@ module.exports = {
         function ignoreSourcemapsloaderWarnings(warning) {
             return (
                 warning.module &&
-            warning.module.resource.includes('node_modules') &&
-            warning.details &&
-            warning.details.includes('source-map-loader')
+                warning.module.resource.includes('node_modules') &&
+                warning.details &&
+                warning.details.includes('source-map-loader')
             );
         }
     ]
