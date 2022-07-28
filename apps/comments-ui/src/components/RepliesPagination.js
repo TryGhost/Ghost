d28f00656c0711ed642ff5a45334376545e45aa2
Peter Zimon <peter.zimon@gmail.com>
2022-07-28 14:17:19 +0200

    Updated CTA styles
    no issue
    
    - typography and spacing was a bit off in the signup / upgrade CTA boxes
    - the line style was inconsistent with the rest of the comments design
    - button sizes and spacings needed also a bit of refinement

--- apps/comments-ui/src/components/RepliesPagination.js
+++ apps/comments-ui/src/components/RepliesPagination.js
@@ -7,7 +7,7 @@ const RepliesPagination = (props) => {
     return (
         <button className="group w-full text-neutral-700 font-semibold px-0 pt-0 pb-3 mt-10 sm:mt-0 mb-10 font-sans text-md text-left dark:text-white flex items-center " onClick={loadMore}>
             <span className="whitespace-nowrap mr-4">↓ Show {count} more replies</span>
-            <span className="transition-[background-color] duration-200 ease-out inline-block w-full bg-neutral-100 dark:bg-[rgba(255,255,255,0.08)] group-hover:bg-neutral-200 rounded h-[3px] mt-[3px]" />
+            <span className="transition-[background-color] duration-200 ease-out inline-block w-full bg-neutral-100 dark:bg-[rgba(255,255,255,0.08)] group-hover:bg-neutral-200 rounded h-[2px] mt-[3px]" />
         </button>
     );
 };
