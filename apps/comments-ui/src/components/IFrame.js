beb880796672b4517671a58fb04ab90a67452ab0
Rishabh <zrishabhgarg@gmail.com>
2022-07-28 19:01:24 +0530

    Fixed resize observer error in tests
    - resize observer used in auth frame is not available in Jest, so we mock it instead
    - ignores missing methods on resize observer for tests

--- apps/comments-ui/src/components/IFrame.js
+++ apps/comments-ui/src/components/IFrame.js
@@ -13,7 +13,7 @@ export default class IFrame extends Component {
     componentWillUnmout() {
         this.node.removeEventListener('load', this.handleLoad);
     }
-    
+
     setupFrameBaseStyle() {
         if (this.node.contentDocument) {
             this.iframeHtml = this.node.contentDocument.documentElement;
@@ -22,7 +22,7 @@ export default class IFrame extends Component {
             this.forceUpdate();
 
             if (this.props.onResize) {
-                (new ResizeObserver(_ => this.props.onResize(this.iframeRoot))).observe(this.iframeRoot);
+                (new ResizeObserver(_ => this.props.onResize(this.iframeRoot)))?.observe?.(this.iframeRoot);
             }
         }
     }
--- apps/comments-ui/src/setupTests.js
+++ apps/comments-ui/src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+global.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn()
+}));
