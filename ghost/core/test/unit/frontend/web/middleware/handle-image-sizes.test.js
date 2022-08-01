6015731c82b955df8bbc3a4947476ff8f0d2c561
Simon Backx <simon@ghost.org>
2022-08-01 14:45:54 +0200

    ✨ Added format option to `img-url` helper (#14962)
    fixes https://github.com/TryGhost/Ghost/issues/14323
    
    - Fixed support for resizing images from Unsplash using the `img-url` helper (previously the size property was ignored for images from Unsplash)
    - Added support for `avif` file formats (supported by sharp out of the box)
    - Added support for setting the format of images, with a new  `format` option:
    
    E.g. to convert an image to webp (only works in combination with size for now, except for Unsplash where you can use it without size):
    ```
    {{img_url @site.cover_image size="s" format="webp"}}
    ```
    
    This can help improve the performance of a theme, by serving assets in `<picture>` elements with webp and fallback image formats.
    
    Usage example:
    ```html
    <picture>
        <source 
            srcset="{{img_url feature_image size="s" format="avif"}} 300w,
                    {{img_url feature_image size="m" format="avif"}} 600w,
                    {{img_url feature_image size="l" format="avif"}} 1000w,
                    {{img_url feature_image size="xl" format="avif"}} 2000w"
            sizes="(min-width: 1400px) 1400px, 92vw" 
            type="image/avif"
        >
        <source 
            srcset="{{img_url feature_image size="s" format="webp"}} 300w,
                    {{img_url feature_image size="m" format="webp"}} 600w,
                    {{img_url feature_image size="l" format="webp"}} 1000w,
                    {{img_url feature_image size="xl" format="webp"}} 2000w"
            sizes="(min-width: 1400px) 1400px, 92vw" 
            type="image/webp"
        >
        <img
            srcset="{{img_url feature_image size="s"}} 300w,
                    {{img_url feature_image size="m"}} 600w,
                    {{img_url feature_image size="l"}} 1000w,
                    {{img_url feature_image size="xl"}} 2000w"
            sizes="(min-width: 1400px) 1400px, 92vw"
            src="{{img_url feature_image size="xl"}}"
            alt="{{#if feature_image_alt}}{{feature_image_alt}}{{else}}{{title}}{{/if}}"
        >
    </picture>
    ```

--- ghost/core/test/unit/frontend/web/middleware/handle-image-sizes.test.js
+++ ghost/core/test/unit/frontend/web/middleware/handle-image-sizes.test.js
@@ -4,9 +4,14 @@ const storage = require('../../../../../core/server/adapters/storage');
 const activeTheme = require('../../../../../core/frontend/services/theme-engine/active');
 const handleImageSizes = require('../../../../../core/frontend/web/middleware/handle-image-sizes.js');
 const imageTransform = require('@tryghost/image-transform');
+const config = require('../../../../../core/shared/config');
 
 // @TODO make these tests lovely and non specific to implementation
 describe('handleImageSizes middleware', function () {
+    this.afterEach(function () {
+        sinon.restore();
+    });
+
     it('calls next immediately if the url does not match /size/something/', function (done) {
         const fakeReq = {
             url: '/size/something'
@@ -23,6 +28,32 @@ describe('handleImageSizes middleware', function () {
     it('calls next immediately if the url does not match /size/something/', function (done) {
         const fakeReq = {
             url: '/url/whatever/'
+        };
+        // CASE: second thing middleware does is try to match to a regex
+        fakeReq.url.match = function () {
+            throw new Error('Should have exited immediately');
+        };
+        handleImageSizes(fakeReq, {}, function next() {
+            done();
+        });
+    });
+
+    it('calls next immediately if the file extension is missing', function (done) {
+        const fakeReq = {
+            url: '/size/something/file'
+        };
+        // CASE: second thing middleware does is try to match to a regex
+        fakeReq.url.match = function () {
+            throw new Error('Should have exited immediately');
+        };
+        handleImageSizes(fakeReq, {}, function next() {
+            done();
+        });
+    });
+
+    it('calls next immediately if the file has a trailing slash', function (done) {
+        const fakeReq = {
+            url: '/size/something/file.jpg/'
         };
         // CASE: second thing middleware does is try to match to a regex
         fakeReq.url.match = function () {
@@ -74,7 +105,18 @@ describe('handleImageSizes middleware', function () {
                         return {
                             l: {
                                 width: 1000
-                            }
+                            },
+                            m: {
+                                width: 1000,
+                                height: 200
+                            },
+                            n: {
+                                height: 1000
+                            },
+                            h100: {
+                                height: 100
+                            },
+                            missing: {}
                         };
                     }
                 }
@@ -85,14 +127,56 @@ describe('handleImageSizes middleware', function () {
             resizeFromBufferStub = sinon.stub(imageTransform, 'resizeFromBuffer').resolves(Buffer.from([]));
         });
 
-        this.afterEach(function () {
-            sinon.restore();
-        });
-
         it('redirects for invalid format extension', function (done) {
             const fakeReq = {
                 url: '/size/w1000/format/test/image.jpg',
                 originalUrl: '/blog/content/images/size/w1000/format/test/image.jpg'
+            };
+            const fakeRes = {
+                redirect(url) {
+                    try {
+                        url.should.equal('/blog/content/images/image.jpg');
+                    } catch (e) {
+                        return done(e);
+                    }                    
+                    done();
+                }
+            };
+            handleImageSizes(fakeReq, fakeRes, function next(err) {
+                if (err) {
+                    return done(err);
+                }
+                done(new Error('Should not have called next'));
+            });
+        });
+
+        it('redirects for invalid sizes', function (done) {
+            const fakeReq = {
+                url: '/size/w123/image.jpg',
+                originalUrl: '/blog/content/images/size/w123/image.jpg'
+            };
+            const fakeRes = {
+                redirect(url) {
+                    try {
+                        url.should.equal('/blog/content/images/image.jpg');
+                    } catch (e) {
+                        return done(e);
+                    }                    
+                    done();
+                }
+            };
+            handleImageSizes(fakeReq, fakeRes, function next(err) {
+                if (err) {
+                    return done(err);
+                }
+                done(new Error('Should not have called next'));
+            });
+        });
+
+        it('redirects for invalid configured size', function (done) {
+            const fakeReq = {
+                url: '/size/missing/image.jpg',
+                originalUrl: '/blog/content/images/size/missing/image.jpg'
             };
             const fakeRes = {
                 redirect(url) {
@@ -148,6 +232,58 @@ describe('handleImageSizes middleware', function () {
             });
         });
 
+        it('returns original URL if unsupported storage adapter', function (done) {
+            dummyStorage.saveRaw = undefined;
+
+            const fakeReq = {
+                url: '/size/w1000/blank.png',
+                originalUrl: '/blog/content/images/size/w1000/blank.png'
+            };
+            const fakeRes = {
+                redirect(url) {
+                    try {
+                        url.should.equal('/blog/content/images/blank.png');
+                    } catch (e) {
+                        return done(e);
+                    }                    
+                    done();
+                }
+            };
+
+            handleImageSizes(fakeReq, fakeRes, function next(err) {
+                if (err) {
+                    return done(err);
+                }
+                done(new Error('Should not have called next'));
+            });
+        });
+
+        it('redirects if sharp is not installed', function (done) {
+            sinon.stub(imageTransform, 'canTransformFiles').returns(false);
+
+            const fakeReq = {
+                url: '/size/w1000/blank.png',
+                originalUrl: '/blog/content/images/size/w1000/blank.png'
+            };
+            const fakeRes = {
+                redirect(url) {
+                    try {
+                        url.should.equal('/blog/content/images/blank.png');
+                    } catch (e) {
+                        return done(e);
+                    }                    
+                    done();
+                }
+            };
+
+            handleImageSizes(fakeReq, fakeRes, function next(err) {
+                if (err) {
+                    return done(err);
+                }
+                done(new Error('Should not have called next'));
+            });
+        });
+        
         it('continues if file exists', function (done) {
             dummyStorage.exists = async function (path) {
                 if (path === '/size/w1000/blank.png') {
@@ -182,8 +318,8 @@ describe('handleImageSizes middleware', function () {
             const spy = sinon.spy(dummyStorage, 'read');
 
             const fakeReq = {
-                url: '/size/w1000/blank.png',
-                originalUrl: '/size/w1000/blank.png'
+                url: '/size/h100/blank.png',
+                originalUrl: '/size/h100/blank.png'
             };
             const fakeRes = {
                 redirect(url) {
@@ -408,6 +544,40 @@ describe('handleImageSizes middleware', function () {
                 try {
                     resizeFromBufferStub.calledOnceWithExactly(buffer, {withoutEnlargement: true, width: 1000, format: 'webp'}).should.be.true();
                     typeStub.calledOnceWithExactly('webp').should.be.true();
+                } catch (e) {
+                    return done(e);
+                }
+                done();
+            });
+        });
+
+        it('can format PNG to AVIF', function (done) {
+            dummyStorage.exists = async function (path) {
+                return false;
+            };
+            dummyStorage.read = async function (path) {
+                return buffer;
+            };
+
+            const fakeReq = {
+                url: '/size/w1000/format/avif/blank.png',
+                originalUrl: '/size/w1000/format/avif/blank.png'
+            };
+            const fakeRes = {
+                redirect(url) {
+                    done(new Error('Should not have called redirect'));
+                },
+                type: function () {}
+            };
+            const typeStub = sinon.spy(fakeRes, 'type');
+
+            handleImageSizes(fakeReq, fakeRes, function next(err) {
+                if (err) {
+                    return done(err);
+                }
+                try {
+                    resizeFromBufferStub.calledOnceWithExactly(buffer, {withoutEnlargement: true, width: 1000, format: 'avif'}).should.be.true();
+                    typeStub.calledOnceWithExactly('image/avif').should.be.true();
                 } catch (e) {
                     return done(e);
                 }
