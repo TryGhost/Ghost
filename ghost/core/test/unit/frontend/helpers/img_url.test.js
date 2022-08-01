b7f3892be07b1885da59a236257d102c8b5c916d
Simon Backx <simon@ghost.org>
2022-08-01 14:45:54 +0200

    ✨ Added format option to `img-url` helper (#14962)
    fixes https://github.com/TryGhost/Ghost/issues/14323
    
    - Fixed support for resizing images from Unsplash using the `img-url` helper (previously the size property was ignored for images from Unsplash)
    - Added support for `avif` file formats (supported by sharp out of the box)
    - Added support for setting the format of images, with a new  `format` option:
    
    E.g. to convert an image to webp (only works in combination with size for now, except for Unsplash where you can use it without size):
    ```
    {{img_url @site.cover_image size="s" format="webp"}}
    ```
    
    This can help improve the performance of a theme, by serving assets in `<picture>` elements with webp and fallback image formats.
    
    Usage example:
    ```html
    <picture>
        <source 
            srcset="{{img_url feature_image size="s" format="avif"}} 300w,
                    {{img_url feature_image size="m" format="avif"}} 600w,
                    {{img_url feature_image size="l" format="avif"}} 1000w,
                    {{img_url feature_image size="xl" format="avif"}} 2000w"
            sizes="(min-width: 1400px) 1400px, 92vw" 
            type="image/avif"
        >
        <source 
            srcset="{{img_url feature_image size="s" format="webp"}} 300w,
                    {{img_url feature_image size="m" format="webp"}} 600w,
                    {{img_url feature_image size="l" format="webp"}} 1000w,
                    {{img_url feature_image size="xl" format="webp"}} 2000w"
            sizes="(min-width: 1400px) 1400px, 92vw" 
            type="image/webp"
        >
        <img
            srcset="{{img_url feature_image size="s"}} 300w,
                    {{img_url feature_image size="m"}} 600w,
                    {{img_url feature_image size="l"}} 1000w,
                    {{img_url feature_image size="xl"}} 2000w"
            sizes="(min-width: 1400px) 1400px, 92vw"
            src="{{img_url feature_image size="xl"}}"
            alt="{{#if feature_image_alt}}{{feature_image_alt}}{{else}}{{title}}{{/if}}"
        >
    </picture>
    ```

--- ghost/core/test/unit/frontend/helpers/img_url.test.js
+++ ghost/core/test/unit/frontend/helpers/img_url.test.js
@@ -135,6 +135,7 @@ describe('{{img_url}} helper', function () {
             should.exist(rendered);
             rendered.should.equal('/content/images/size/w400/my-coole-img.jpg');
         });
+
         it('should output the correct url for protocol relative urls', function () {
             const rendered = img_url('//website.com/whatever/my-coole-img.jpg', {
                 hash: {
@@ -153,6 +154,7 @@ describe('{{img_url}} helper', function () {
             should.exist(rendered);
             rendered.should.equal('//website.com/whatever/my-coole-img.jpg');
         });
+
         it('should output the correct url for relative paths', function () {
             const rendered = img_url('/content/images/my-coole-img.jpg', {
                 hash: {
@@ -189,6 +191,287 @@ describe('{{img_url}} helper', function () {
             });
             should.exist(rendered);
             rendered.should.equal('content/images/size/w400/my-coole-img.jpg');
+        });
+
+        it('ignores invalid size options', function () {
+            const rendered = img_url('/content/images/author-image-relative-url.png', {hash: {size: 'invalid-size'}});
+            should.exist(rendered);
+            rendered.should.equal('/content/images/author-image-relative-url.png');
+            logWarnStub.called.should.be.false();
+        });
+
+        it('ignores misconfigured sizes', function () {
+            const rendered = img_url('/content/images/author-image-relative-url.png', {
+                hash: {
+                    size: 'medium'
+                }, 
+                data: {
+                    config: {
+                        image_sizes: {
+                            medium: {typo: 600}
+                        }
+                    }
+                }
+            });
+            should.exist(rendered);
+            rendered.should.equal('/content/images/author-image-relative-url.png');
+            logWarnStub.called.should.be.false();
+        });
+
+        it('ignores format if size is missing', function () {
+            const rendered = img_url('/content/images/author-image-relative-url.png', {
+                hash: {
+                    format: 'webp'
+                }, 
+                data: {
+                    config: {
+                        image_sizes: {
+                            w600: {width: 600}
+                        }
+                    }
+                }
+            });
+            should.exist(rendered);
+            rendered.should.equal('/content/images/author-image-relative-url.png');
+            logWarnStub.called.should.be.false();
+        });
+
+        it('adds format and size options', function () {
+            const rendered = img_url('/content/images/author-image-relative-url.png', {
+                hash: {
+                    size: 'w600',
+                    format: 'webp'
+                }, 
+                data: {
+                    config: {
+                        image_sizes: {
+                            w600: {width: 600}
+                        }
+                    }
+                }
+            });
+            should.exist(rendered);
+            rendered.should.equal('/content/images/size/w600/format/webp/author-image-relative-url.png');
+            logWarnStub.called.should.be.false();
+        });
+
+        it('ignores invalid formats', function () {
+            const rendered = img_url('/content/images/author-image-relative-url.png', {
+                hash: {
+                    size: 'w600',
+                    format: 'invalid'
+                }, 
+                data: {
+                    config: {
+                        image_sizes: {
+                            w600: {width: 600}
+                        }
+                    }
+                }
+            });
+            should.exist(rendered);
+            rendered.should.equal('/content/images/size/w600/author-image-relative-url.png');
+            logWarnStub.called.should.be.false();
+        });
+    });
+
+    describe('Unsplash', function () {
+        before(function () {
+            configUtils.set({url: 'http://localhost:65535/'});
+        });
+
+        after(function () {
+            configUtils.restore();
+        });
+
+        it('works without size option', function () {
+            const rendered = img_url('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80&w=2000', {
+                hash: {},
+                data: {
+                    config: {
+                        image_sizes: {
+                            medium: {
+                                width: 400
+                            }
+                        }
+                    }
+                }
+            });
+            should.exist(rendered);
+            rendered.should.equal('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80&w=2000');
+        });
+
+        it('can change the output width', function () {
+            const rendered = img_url('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80&w=2000', {
+                hash: {
+                    size: 'medium'
+                },
+                data: {
+                    config: {
+                        image_sizes: {
+                            medium: {
+                                width: 400
+                            }
+                        }
+                    }
+                }
+            });
+            should.exist(rendered);
+            rendered.should.equal('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80&w=400');
+        });
+
+        it('can change the output height', function () {
+            const rendered = img_url('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80', {
+                hash: {
+                    size: 'medium'
+                },
+                data: {
+                    config: {
+                        image_sizes: {
+                            medium: {
+                                height: 400
+                            }
+                        }
+                    }
+                }
+            });
+            should.exist(rendered);
+            rendered.should.equal('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80&h=400');
+        });
+
+        it('ignores invalid image size configurations', function () {
+            const rendered = img_url('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80', {
+                hash: {
+                    size: 'medium'
+                },
+                data: {
+                    config: {
+                        image_sizes: {
+                            medium: {
+                                typo: 400
+                            }
+                        }
+                    }
+                }
+            });
+            should.exist(rendered);
+            rendered.should.equal('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80');
+        });
+
+        it('ignores invalid urls', function () {
+            const invalid = ':https://images.unsplash.com/test';
+            const rendered = img_url(invalid, {
+                hash: {
+                    size: 'medium'
+                },
+                data: {
+                    config: {
+                        image_sizes: {
+                            medium: {
+                                width: 400
+                            }
+                        }
+                    }
+                }
+            });
+            should.exist(rendered);
+            rendered.should.equal(invalid);
+        });
+
+        it('ignores invalid sizes', function () {
+            const rendered = img_url('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80&w=2000', {
+                hash: {
+                    size: 'invalid'
+                },
+                data: {
+                    config: {
+                        image_sizes: {
+                            medium: {
+                                width: 400
+                            }
+                        }
+                    }
+                }
+            });
+            should.exist(rendered);
+            rendered.should.equal('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80&w=2000');
+        });
+
+        it('can change the output format', function () {
+            const rendered = img_url('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80&w=2000', {
+                hash: {
+                    format: 'webp'
+                },
+                data: {
+                    config: {
+                        image_sizes: {
+                            medium: {
+                                width: 400
+                            }
+                        }
+                    }
+                }
+            });
+            should.exist(rendered);
+            rendered.should.equal('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=webp&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80&w=2000');
+        });
+
+        it('ignores invalid formats', function () {
+            const rendered = img_url('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80&w=2000', {
+                hash: {
+                    format: 'invalid'
+                },
+                data: {
+                    config: {
+                        image_sizes: {
+                            medium: {
+                                width: 400
+                            }
+                        }
+                    }
+                }
+            });
+            should.exist(rendered);
+            rendered.should.equal('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80&w=2000');
+        });
+
+        it('transforms jpeg to jpg', function () {
+            const rendered = img_url('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=auto&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80&w=2000', {
+                hash: {
+                    format: 'jpeg'
+                },
+                data: {
+                    config: {
+                        image_sizes: {
+                            medium: {
+                                width: 400
+                            }
+                        }
+                    }
+                }
+            });
+            should.exist(rendered);
+            rendered.should.equal('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80&w=2000');
+        });
+
+        it('can change the output format and size', function () {
+            const rendered = img_url('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80&w=2000', {
+                hash: {
+                    format: 'webp',
+                    size: 'medium'
+                },
+                data: {
+                    config: {
+                        image_sizes: {
+                            medium: {
+                                width: 400
+                            }
+                        }
+                    }
+                }
+            });
+            should.exist(rendered);
+            rendered.should.equal('https://images.unsplash.com/photo-1657816793628-191deb91e20f?crop=entropy&cs=tinysrgb&fit=max&fm=webp&ixid=MnwxMTc3M3wwfDF8YWxsfDJ8fHx8fHwyfHwxNjU3ODkzNjU5&ixlib=rb-1.2.1&q=80&w=400');
         });
     });
 });
