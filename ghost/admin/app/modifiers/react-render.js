50aa6f4877e5513bf836c44c2656a1da5e444748
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-08-02 09:42:25 +0100

    Dropped app.import for react/react-dom in favor of ember-auto-import
    refs https://github.com/TryGhost/Admin/commit/a4a4136c2f7d9046f7336b2d5d01ae9191302b40#commitcomment-79676720
    
    - `ember-auto-import` does the right thing automatically so manual `app.import` is not necessary and avoids potential pitfalls with multiple copies of react being bundled
    - added the `window.*` global attachments after importing so that our async loaded UMD components have access to our bundled React and ReactDOM instances

--- ghost/admin/app/modifiers/react-render.js
+++ ghost/admin/app/modifiers/react-render.js
@@ -1,6 +1,12 @@
-/* global ReactDOM */
 import Modifier from 'ember-modifier';
-import {createElement} from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
+
+// make globals available for any pulled in UMD components
+// - avoids external components needing to bundle React and running into multiple version errors
+window.React = React;
+window.ReactDOM = ReactDOM;
 
 export default class ReactRenderModifier extends Modifier {
     didInstall() {
@@ -8,9 +14,9 @@ export default class ReactRenderModifier extends Modifier {
         const props = this.args.named;
 
         if (!this.root) {
-            this.root = ReactDOM.createRoot(this.element);
+            this.root = createRoot(this.element);
         }
-        this.root.render(createElement(reactComponent, {...props}));
+        this.root.render(React.createElement(reactComponent, {...props}));
     }
 
     willDestroy() {
