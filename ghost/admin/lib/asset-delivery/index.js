ddfeb90fc699e9bf3e1c0a229c99abf4665a5ec6
Gabriel Csapo <gabecsapo@gmail.com>
2022-08-02 05:43:28 -0700

    Updated ember-auto-import@1 -> ember-auto-import@2 (#2252)
    requires https://github.com/TryGhost/Ghost/pull/15128
    refs https://github.com/TryGhost/Admin/pull/2252
    
    - bumped `ember-auto-import` dependency
      - updated `autoImport` config to match new format
      - added dependencies for node packages that no longer have built-in polyfills in webpack@5
    - updated `asset-delivery` to work with `ember-auto-import@2`
      - reverted to standard ember `index.html` to let `ember-auto-import` handle it's insertion of multiple JS chunk files
      - updated the `asset-delivery` addon to copy the the `index.html` and `/assets` to `core/built/admin/{development|production}/` directory rather than splitting the `index.html` file apart from the assets inside Ghost's directory structure
    - switched to relative root URL in development/production
      - required because assets are served from the same directory the index file is served from in Ghost rather than from the root
      - Admin uses `/#/` URLs so it can be served from any subdirectory without requiring prior knowledge of that directory at build time
    
    Co-authored-by: Kevin Ansfield <kevin@lookingsideways.co.uk>

--- ghost/admin/lib/asset-delivery/index.js
+++ ghost/admin/lib/asset-delivery/index.js
@@ -4,48 +4,41 @@
 module.exports = {
     name: 'asset-delivery',
 
+    env: null,
+
     isDevelopingAddon() {
         return true;
     },
 
-    contentFor(type, config) {
-        let min = config.environment === 'production' ? '.min' : '';
-
-        if (type === 'minifiedInProductionCss') {
-            return `
-                <link rel="stylesheet" href="assets/vendor${min}.css">
-                <link rel="stylesheet" href="assets/ghost${min}.css" title="light">
-            `;
-        }
-
-        if (type === 'minifiedInProductionJs') {
-            return `
-                <script src="assets/vendor${min}.js"></script>
-                <script src="assets/ghost${min}.js"></script>
-            `;
+    config(env) {
+        // only set this.env on the first call otherwise when `postBuild()` is
+        // called this.env will always be 'test' due to multiple `config()` calls
+        if (!this.env) {
+            this.env = env;
         }
     },
 
     postBuild: function (results) {
-        var fs = this.project.require('fs-extra'),
-            walkSync = this.project.require('walk-sync'),
-            assetsIn = results.directory + '/assets',
-            templateOutDir = '../server/web/admin/views',
-            assetsOut = '../built/assets',
-            assets = walkSync(assetsIn);
+        const fs = this.project.require('fs-extra');
+        const walkSync = this.project.require('walk-sync');
 
+        const assetsOut = `../built/admin/${this.env}`;
         fs.ensureDirSync(assetsOut);
 
-        if (fs.existsSync(results.directory + '/index.min.html')) {
-            fs.copySync(results.directory + '/index.min.html', `${templateOutDir}/default-prod.html`, {overwrite: true, dereference: true});
-        } else {
-            fs.copySync(results.directory + '/index.html', `${templateOutDir}/default.html`, {overwrite: true, dereference: true});
-        }
+        // the dist folder contains more than just index.html and /assets, especially
+        // for development builds but for Ghost's purposes it only needs to serve
+        // index.html and /assets
+
+        // copy the index.html file
+        fs.copySync(`${results.directory}/index.html`, `${assetsOut}/index.html`, {overwrite: true, dereference: true});
+
+        // copy all the `/assets` files
+        const assets = walkSync(results.directory + '/assets');
 
         assets.forEach(function (relativePath) {
             if (relativePath.slice(-1) === '/') { return; }
 
-            fs.copySync(assetsIn + '/' + relativePath, assetsOut + '/' + relativePath, {overwrite: true, dereference: true});
+            fs.copySync(`${results.directory}/assets/${relativePath}`, `${assetsOut}/assets/${relativePath}`, {overwrite: true, dereference: true});
         });
     }
 };
