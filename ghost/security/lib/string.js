29d2a0b45a42c5ad1329cbb682f08ab65d6600a6
Vikas Potluri <vikaspotluri123.github@gmail.com>
2022-08-02 04:49:17 -0700

    refactored security.string to be more readable (#15127)
    refs #15126
    
    - Logic can be simplified
    - Add JSDoc types

--- ghost/security/lib/string.js
+++ ghost/security/lib/string.js
@@ -1,9 +1,9 @@
 const slugify = require('@tryghost/string').slugify;
 
+/**
+ * @param {string} string
+ * @param {{importing?: boolean}} [options]
+ */
 module.exports.safe = function safe(string, options = {}) {
-    let opts = {requiredChangesOnly: true};
-    if (!('importing' in options) || !options.importing) {
-        opts.requiredChangesOnly = false;
-    }
-    return slugify(string, opts);
+    return slugify(string, {requiredChangesOnly: options.importing === true});
 };
