6ef10a8d83878ec67c6941b1f56f520ca7fcf6e9
Fabien "egg" O'Carroll <fabien@allou.is>
2022-08-03 12:23:37 +0100

    Added default values for comment_count helper
    refs https://github.com/TryGhost/Team/issues/1695
    
    We add english defaults for the singular and plural params leaving the
    empty param blank so as to not draw attention to low engagement.

--- ghost/core/core/frontend/helpers/comment_count.js
+++ ghost/core/core/frontend/helpers/comment_count.js
@@ -3,12 +3,15 @@ const {labs} = require('../services/proxy');
 const {html} = require('common-tags');
 
 function commentCount(options) {
+    const empty = options.hash.empty === undefined ? '' : options.hash.empty;
+    const singular = options.hash.singular === undefined ? 'comment' : options.hash.singular;
+    const plural = options.hash.plural === undefined ? 'comments' : options.hash.plural;
     return new SafeString(html`
         <script
             data-ghost-comment-count="${this.id}"
-            data-ghost-comment-count-empty="${options.hash.empty}"
-            data-ghost-comment-count-singular="${options.hash.singular}"
-            data-ghost-comment-count-plural="${options.hash.plural}"
+            data-ghost-comment-count-empty="${empty}"
+            data-ghost-comment-count-singular="${singular}"
+            data-ghost-comment-count-plural="${plural}"
         >
         </script>
     `);
