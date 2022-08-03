2962b6797de90c888ae66d11fe1bedb90364cec6
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-08-03 14:24:09 +0100

    Fixed tailwind class name ordering lint rules
    no issue
    
    - turned class name ordering rule on
    - moved eslint config into separate file to match our other projects

---
+++ koenig/koenig-react/.eslintrc.js
@@ -0,0 +1,22 @@
+/* eslint-env node */
+module.exports = {
+    root: true,
+    extends: [
+        'react-app',
+        'react-app/jest',
+        'plugin:ghost/browser'
+    ],
+    plugins: [
+        'ghost',
+        'tailwindcss'
+    ],
+    rules: {
+        'tailwindcss/classnames-order': 'error',
+        'tailwindcss/enforces-negative-arbitrary-values': 'warn',
+        'tailwindcss/enforces-shorthand': 'warn',
+        'tailwindcss/migration-from-tailwind-2': 'warn',
+        'tailwindcss/no-arbitrary-value': 'off',
+        'tailwindcss/no-custom-classname': 'off',
+        'tailwindcss/no-contradicting-classname': 'error'
+    }
+};
