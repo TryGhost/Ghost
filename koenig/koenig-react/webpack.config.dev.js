a3dd3c749526cd94703c132ac62e88edba7ce73a
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-08-04 09:11:04 +0100

    Fixed "Invalid host header" when accessing through https proxy
    no issue
    
    - `webpack serve` has a host check that fails if you don't access via localhost:port
    - updated config to allow any host header

--- koenig/koenig-react/webpack.config.dev.js
+++ koenig/koenig-react/webpack.config.dev.js
@@ -45,7 +45,8 @@ module.exports = {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
             'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
-        }
+        },
+        allowedHosts: 'all'
     },
     ignoreWarnings: [
         {message: /source-map-loader/},
