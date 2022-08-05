2607bba77d3192035eab97667c07177c2fdab829
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-08-05 17:38:54 +0100

    Added hover+selected styling of card borders

--- koenig/koenig-react/src/components/Card.js
+++ koenig/koenig-react/src/components/Card.js
@@ -1,12 +1,17 @@
 const CardComponent = ({children, isSelected, ...props}) => {
     // TODO: add icon, action bar, classes etc
+    const classes = [
+        'hover:shadow-green-500',
+        'hover:shadow-[0_0_0_1px]'
+    ];
+
+    if (isSelected) {
+        classes.push('shadow-green-500 shadow-[0_0_0_2px]');
+    }
 
     return (
-        <div className="card-component">
+        <div className={classes.join(' ')}>
             {children}
-            <code>
-                isSelected: {JSON.stringify(isSelected)}
-            </code>
         </div>
     );
 };
