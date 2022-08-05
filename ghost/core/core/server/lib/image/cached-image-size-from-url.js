1c4f64f02ca58adc4a19cc0befc8187f6cde45a1
Naz <nazargargol@gmail.com>
2022-08-05 12:59:39 +0100

    Refactored cached image size to have cache as DI
    refs https://github.com/TryGhost/Toolbox/issues/364
    
    - Groundwork before swapping out existing cache for in-memory cache from the adapter

--- ghost/core/core/server/lib/image/cached-image-size-from-url.js
+++ ghost/core/core/server/lib/image/cached-image-size-from-url.js
@@ -1,10 +1,17 @@
 const debug = require('@tryghost/debug')('utils:image-size-cache');
 const errors = require('@tryghost/errors');
 const logging = require('@tryghost/logging');
+
 class CachedImageSizeFromUrl {
-    constructor({imageSize}) {
+    /**
+     *
+     * @param {Object} options
+     * @param {Object} options.imageSize - instance with "getImageSizeFromUrl" method
+     * @param {Object} options.cache - cache store instance
+     */
+    constructor({imageSize, cache}) {
         this.imageSize = imageSize;
-        this.cache = new Map();
+        this.cache = cache;
     }
 
     /**
@@ -19,31 +26,32 @@ class CachedImageSizeFromUrl {
         if (!url || url === undefined || url === null) {
             return;
         }
-    
+
         // image size is not in cache
         if (!this.cache.has(url)) {
             return this.imageSize.getImageSizeFromUrl(url).then((res) => {
                 this.cache.set(url, res);
-    
+
                 debug('Cached image:', url);
-    
+
                 return this.cache.get(url);
             }).catch(errors.NotFoundError, () => {
                 debug('Cached image (not found):', url);
                 // in case of error we just attach the url
                 this.cache.set(url, url);
-    
+
                 return this.cache.get(url);
             }).catch((err) => {
                 debug('Cached image (error):', url);
                 logging.error(err);
-    
+
                 // in case of error we just attach the url
                 this.cache.set(url, url);
-    
+
                 return this.cache.get(url);
             });
         }
+
         debug('Read image from cache:', url);
         // returns image size from cache
         return this.cache.get(url);
