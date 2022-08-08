36b7826c12589dab12ba090463af1ddfe744a15c
Daniel Lockyer <hi@daniellockyer.com>
2022-08-08 13:03:49 +0200

    Cleaned up core package Gruntfile
    - most of these tasks were duplicates or had been extracted out into
      npm/yarn scripts
    - in order to get closer to removing Grunt, I've cleaned up the majority
      of those script
    - this also removes an intermediate command to building Admin, so it starts
      ever so slightly quicker

--- ghost/core/Gruntfile.js
+++ ghost/core/Gruntfile.js
@@ -1,42 +1,14 @@
 const config = require('./core/shared/config');
 
 module.exports = function (grunt) {
-    // grunt dev - use yarn dev instead!
-    // - Start a server & build assets on the fly whilst developing
-    grunt.registerTask('dev:admin', 'Dev Mode; run Admin with livereload', 'shell:ember:watch');
-
-    // grunt build -- use yarn build instead!
-    // - Builds the admin without a watch task
-    grunt.registerTask('build', 'Build admin app in development mode', 'ember');
-
-    // Runs the asset generation tasks for production and duplicates default-prod.html to default.html
-    grunt.registerTask('release', 'Release task - creates a final built zip', 'prod');
-
-    // --- Sub Commands
-    // Used to make other commands work
-
-    // Updates submodules, then installs and builds the admin for you
-    grunt.registerTask('init', 'Prepare the project for development', 'build');
-
-    // Runs ember dev
-    grunt.registerTask('ember', 'Build JS & templates for development',
-        ['shell:ember:dev']);
-
-    // Production asset build
-    grunt.registerTask('prod', 'Build JS & templates for production', 'shell:ember:prod');
-
     // --- Configuration
-    const cfg = {
+    grunt.initConfig({
         shell: {
             ember: {
                 command: function (mode) {
                     const liveReloadBaseUrl = config.getSubdir() || '/ghost/';
 
                     switch (mode) {
-                    case 'dev':
-                        return 'yarn build';
-                    case 'prod':
-                        return 'yarn build:prod';
                     case 'watch':
                         return `yarn start --live-reload-base-url=${liveReloadBaseUrl} --live-reload-port=4201`;
                     }
@@ -68,14 +40,9 @@ module.exports = function (grunt) {
                 dest: '.git/hooks'
             }
         }
-    };
-
-    // --- Grunt Initialisation
+    });
 
     // Load all grunt tasks
     grunt.loadNpmTasks('grunt-contrib-symlink');
     grunt.loadNpmTasks('grunt-shell');
-
-    // Load the configuration
-    grunt.initConfig(cfg);
 };
