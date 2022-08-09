9b0d2cf4d8b21d66187472c9a678d7bdac15579c
Aileen Nowak <AileenCGN@gmail.com>
2022-08-09 11:09:13 +0100

    Allowed core integrations to work with Admin API

--- ghost/core/core/server/services/auth/api-key/content.js
+++ ghost/core/core/server/services/auth/api-key/content.js
@@ -43,11 +43,7 @@ const authenticateContentApiKey = async function authenticateContentApiKey(req, 
 
         // CASE: blocking all non-internal: "custom" and "builtin" integration requests when the limit is reached
         if (limitService.isLimited('customIntegrations')
-            && (apiKey.relations.integration
-                && !['internal'].includes(apiKey.relations.integration.get('type'))
-                && !['ghost-explore'].includes(apiKey.relations.integration.get('slug'))
-            )
-        ) {
+            && (apiKey.relations.integration && !['internal', 'core'].includes(apiKey.relations.integration.get('type')))) {
             // NOTE: using "checkWouldGoOverLimit" instead of "checkIsOverLimit" here because flag limits don't have
             //       a concept of measuring if the limit has been surpassed
             await limitService.errorIfWouldGoOverLimit('customIntegrations');
