710c5c264ea977f0dc4cde6b800b24046945503a
Aileen Nowak <AileenCGN@gmail.com>
2022-08-09 12:54:37 +0100

    Fixed limit service not allowing explore integration
    no issue
    
    - Don't run limit checks for the Ghost Explore integration

--- ghost/core/core/server/services/auth/api-key/admin.js
+++ ghost/core/core/server/services/auth/api-key/admin.js
@@ -127,7 +127,11 @@ const authenticateWithToken = async (req, res, next, {token, JWT_OPTIONS}) => {
 
         // CASE: blocking all non-internal: "custom" and "builtin" integration requests when the limit is reached
         if (limitService.isLimited('customIntegrations')
-            && (apiKey.relations.integration && !['internal'].includes(apiKey.relations.integration.get('type')))) {
+            && (apiKey.relations.integration
+                && !['internal'].includes(apiKey.relations.integration.get('type'))
+                && !['ghost-explore'].includes(apiKey.relations.integration.get('slug'))
+            )
+        ) {
             // NOTE: using "checkWouldGoOverLimit" instead of "checkIsOverLimit" here because flag limits don't have
             //       a concept of measuring if the limit has been surpassed
             await limitService.errorIfWouldGoOverLimit('customIntegrations');
