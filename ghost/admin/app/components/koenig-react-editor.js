cd9959b690957673261aeacf3118153790a77fe4
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-08-10 10:17:53 +0200

    Added `uploadUrl` props to React component no issue - passes `uploadUrl` to React component as a prop.

--- ghost/admin/app/components/koenig-react-editor.js
+++ ghost/admin/app/components/koenig-react-editor.js
@@ -87,6 +87,7 @@ export default class KoenigReactEditor extends Component {
                             mobiledoc={this.args.mobiledoc}
                             didCreateEditor={this.args.didCreateEditor}
                             onChange={this.args.onChange}
+                            uploadUrl={this.args.uploadUrl}
                         />
                     </Suspense>
                 </ErrorHandler>
