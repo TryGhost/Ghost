e000c6cca6f008be2f6620127fb37c0a294cd891
Daniel Lockyer <hi@daniellockyer.com>
2022-08-10 10:53:47 +0200

    Deleted unused schema util
    refs https://github.com/TryGhost/Ghost/commit/03cbc89a5326c6a9605d670484cc3e29854ba200
    
    - this util was added to fix an issue with the types of columns in
      MySQL, but the issue was back in 2014 (!!)
    - we're long past doing it in this way so we'll likely never use it
      again

--- ghost/core/core/server/data/schema/clients/mysql.js
+++ ghost/core/core/server/data/schema/clients/mysql.js
@@ -27,22 +27,7 @@ const getColumns = function getColumns(table, transaction) {
     });
 };
 
-// This function changes the type of posts.html and posts.markdown columns to mediumtext. Due to
-// a wrong datatype in schema.js some installations using mysql could have been created using the
-// data type text instead of mediumtext.
-// For details see: https://github.com/TryGhost/Ghost/issues/1947
-const checkPostTable = function checkPostTable(transaction = db.knex) {
-    return transaction.raw('SHOW FIELDS FROM posts where Field ="html" OR Field = "markdown"').then(function (response) {
-        return _.flatten(_.map(response[0], function (entry) {
-            if (entry.Type.toLowerCase() !== 'mediumtext') {
-                return (transaction || db.knex).raw('ALTER TABLE posts MODIFY ' + entry.Field + ' MEDIUMTEXT');
-            }
-        }));
-    });
-};
-
 module.exports = {
-    checkPostTable: checkPostTable,
     getTables: getTables,
     getIndexes: getIndexes,
     getColumns: getColumns
