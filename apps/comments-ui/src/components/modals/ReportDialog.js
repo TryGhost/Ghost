7b184465eb851c0d0f56e0ec7dc89b2db6e3639c
James Morris <moreofmorris@users.noreply.github.com>
2022-08-11 16:07:29 +0100

    Updated the report dialog to a nicer style
    refs https://github.com/TryGhost/Team/issues/1778

--- apps/comments-ui/src/components/modals/ReportDialog.js
+++ apps/comments-ui/src/components/modals/ReportDialog.js
@@ -1,9 +1,11 @@
 import React, {useContext, useState} from 'react';
 import {ReactComponent as SpinnerIcon} from '../../images/icons/spinner.svg';
 import {ReactComponent as SuccessIcon} from '../../images/icons/success.svg';
+import CloseButton from './CloseButton';
 import AppContext from '../../AppContext';
 
 const ReportDialog = (props) => {
+    const {dispatchAction} = useContext(AppContext);
     const [progress, setProgress] = useState('default');
 
     let buttonColor = 'bg-red-600';
@@ -11,7 +13,7 @@ const ReportDialog = (props) => {
         buttonColor = 'bg-green-600';
     }
 
-    let buttonText = 'Yes';
+    let buttonText = 'Report this comment';
     if (progress === 'sending') {
         buttonText = 'Sending';
     } else if (progress === 'sent') {
@@ -24,8 +26,6 @@ const ReportDialog = (props) => {
     } else if (progress === 'sent') {
         buttonIcon = <SuccessIcon className="w-[16px] h-[16px] mr-2" />;
     }
-
-    const {dispatchAction} = useContext(AppContext);
 
     const stopPropagation = (event) => {
         event.stopPropagation();
@@ -52,18 +52,20 @@ const ReportDialog = (props) => {
     };
 
     return (
-        <div className="relative bg-white w-screen sm:w-[500px] h-screen sm:h-auto p-[28px] sm:p-8 rounded-none sm:rounded-xl text-center shadow-modal" onMouseDown={stopPropagation}>
-            <h1 className="font-sans font-bold tracking-tight text-[24px] mb-1 text-black">You sure you want to report?</h1>
-            <p className="font-sans text-base text-neutral-500 px-4 leading-9">Your request will be sent to the owner of this site.</p>
-            <div className="mt-10">
+        <div className="relative bg-white w-screen sm:w-[500px] h-screen sm:h-auto p-[28px] sm:p-8 rounded-none sm:rounded-xl text-center sm:text-left shadow-modal" onMouseDown={stopPropagation}>
+            <h1 className="font-sans font-bold tracking-tight text-[24px] mb-1 text-black">You want to report<span className="hidden sm:inline"> this comment</span>?</h1>
+            <p className="font-sans text-base text-neutral-500 px-4 sm:pr-4 sm:pl-0 leading-9">Your request will be sent to the owner of this site.</p>
+            <div className="flex flex-col sm:flex-row justify-start items-center gap-4 mt-10">
                 <button
-                    className={`transition duration-200 ease-linear w-full h-[44px] px-8 flex items-center justify-center rounded-md text-white font-sans font-semibold text-[15px] ${buttonColor}`}
+                    className={`transition duration-200 ease-linear w-full sm:w-[200px] h-[44px] px-4 flex items-center justify-center rounded-md text-white font-sans font-semibold text-[15px] ${buttonColor} opacity-100 hover:opacity-90`}
                     onClick={submit}
+                    style={{backgroundColor: buttonColor ?? '#000000'}}
                 >
                     {buttonIcon}{buttonText}
                 </button>
-                <p className="font-sans font-medium text-[1.45rem] text-neutral-500 mt-4 -mb-1">No, <button className="font-sans underline" onClick={close}>I've changed my mind</button></p>
+                <button type="button" onClick={close} className="font-sans text-sm font-medium text-neutral-500 dark:text-neutral-400">Cancel</button>
             </div>
+            <CloseButton close={() => close(false)} />
         </div>
     );
 };
