39629145d0a03c6b0948097afa26625fa0a10901
James Morris <moreofmorris@users.noreply.github.com>
2022-08-12 13:08:32 +0100

    Added in more spacing for the CTAs to catch more ophans
    no issue

--- apps/comments-ui/src/components/CTABox.js
+++ apps/comments-ui/src/components/CTABox.js
@@ -23,11 +23,11 @@ const CTABox = (props) => {
     };
 
     return (
-        <section className={`flex flex-col items-center pt-[40px] ${member ? 'pb-[32px]' : 'pb-[48px]'} ${!props.isFirst && 'mt-4'} px-8 border-t-2 border-gray-100 dark:border-gray-100/10 border-b-2`}>
+        <section className={`flex flex-col items-center pt-[40px] ${member ? 'pb-[32px]' : 'pb-[48px]'} ${!props.isFirst && 'mt-4'} px-2 sm:px-8 border-t-2 border-gray-100 dark:border-gray-100/10 border-b-2`}>
             <h1 className={`mb-[8px] text-center text-black text-[24px] font-sans tracking-tight dark:text-[rgba(255,255,255,0.85)] ${props.isFirst ? 'font-semibold' : 'font-bold'}`}>
                 {titleText}
             </h1>
-            <p className="mb-[28px] px-0 sm:px-8 max-w-screen-sm font-sans text-[16px] text-center leading-normal text-neutral-600 dark:text-[rgba(255,255,255,0.85)]">
+            <p className="mb-[28px] px-0 sm:px-8 w-full sm:max-w-screen-sm font-sans text-[16px] text-center leading-normal text-neutral-600 dark:text-[rgba(255,255,255,0.85)]">
                 Become a {props.isPaid && 'paid'} member of <span className="font-semibold">{publication}</span> to start commenting.
             </p>
             <button onClick={handleSignUpClick} className="mb-[12px] text-white font-san py-[14px] px-5 rounded inline-block font-medium leading-none hover:opacity-90 transition-all" style={buttonStyle}>
