0caf488d28bb7db1065eb98480b37661d740431d
Daniel Lockyer <hi@daniellockyer.com>
2022-08-15 10:21:37 +0200

    WIP: test speed of bcrypt in CI

--- ghost/security/lib/password.js
+++ ghost/security/lib/password.js
@@ -1,9 +1,29 @@
 const bcrypt = require('bcryptjs');
+
+let hashTotal = 0;
+let hashCount = 0;
+
+let compareTotal = 0;
+let compareCount = 0;
+
 module.exports.hash = async function hash(plainPassword) {
-    const salt = await bcrypt.genSalt();
-    return bcrypt.hash(plainPassword, salt);
+    hashCount += 1;
+    const now = Date.now();
+
+    const salt = await bcrypt.genSalt(1);
+    const res = await bcrypt.hash(plainPassword, salt);
+    hashTotal += (Date.now() - now);
+    console.log(`hash: ${hashCount} executions = ${hashTotal}ms (average = ${Math.round(hashTotal / hashCount)}ms)`);
+    return res;
 };
 
-module.exports.compare = function compare(plainPassword, hashedPassword) {
-    return bcrypt.compare(plainPassword, hashedPassword);
+module.exports.compare = async function compare(plainPassword, hashedPassword) {
+    compareCount += 1;
+    const now = Date.now();
+
+    const res = await bcrypt.compare(plainPassword, hashedPassword);
+    compareTotal += (Date.now() - now);
+    console.log(`compare: ${compareCount} executions = ${compareTotal}ms (average = ${Math.round(compareTotal / compareCount)}ms)`);
+
+    return res;
 };
