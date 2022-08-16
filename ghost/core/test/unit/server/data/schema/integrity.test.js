f724a30b28f687f638d033f9ff2f25fe4a7f233b
Fabien "egg" O'Carroll <fabien@allou.is>
2022-08-16 18:34:23 -0400

    Added members_created_events table & model
    refs https://github.com/TryGhost/Team/issues/1802

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '45337ae85129a98e4c1b551cc91790da';
+    const currentSchemaHash = 'f5f7bc63bf9484bf50a0e554604802b3';
     const currentFixturesHash = '0ae1887dd0b42508be946bdbd20d41c8';
     const currentSettingsHash = 'd54210758b7054e2174fd34aa2320ad7';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
