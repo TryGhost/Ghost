010126b43004e75f6e262846367f1db486e60f89
Daniel Lockyer <hi@daniellockyer.com>
2022-08-16 12:40:33 +0200

    Added first version of Audit Log UI
    refs https://github.com/TryGhost/Toolbox/issues/356
    
    - skateboard version w/ loading data from API, settings button guarded
      by labs, and a basic UI populating a table with info

---
+++ ghost/admin/app/helpers/parse-audit-log-event.js
@@ -0,0 +1,31 @@
+export default function parseAuditLogEvent(ev) {
+    const actorName = getActorName(ev);
+    const action = getAction(ev);
+    const actionIcon = getActionIcon(ev);
+
+    return {
+        actorName,
+        actionIcon,
+        action,
+        timestamp: ev.created_at
+    };
+}
+
+function getActionIcon(ev) {
+    switch (ev.event) {
+    case 'added':
+        return 'add-stroke';
+    case 'edited':
+        return 'content';
+    case 'deleted':
+        return 'cross-circle';
+    }
+}
+
+function getActorName(ev) {
+    return ev.actor_id;
+}
+
+function getAction(ev) {
+    return `${ev.event} ${ev.resource_type}`;
+}
