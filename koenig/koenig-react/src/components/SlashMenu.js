688ab6085ff3ee3a46be25ed68d0961be0031f4b
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-08-17 17:39:27 +0100

    Replaced `useRef` usage in `<SlashMenu>` with `useReducer`
    no issue
    
    - `useReducer` allows us to manage state in a more React-like way without falling into the "pitfall" of writing/reading `useRef` values inside of render code

--- koenig/koenig-react/src/components/SlashMenu.js
+++ koenig/koenig-react/src/components/SlashMenu.js
@@ -6,6 +6,7 @@ const Y_OFFSET = 16;
 
 const SlashMenu = ({closeMenu, containerId, ...props}) => {
     const koenigEditor = React.useContext(koenigEditorContext);
+
     // watch the window for mousedown events so that we can close the
     // menu when we detect a click outside. This is preferable to
     // watching the range because the range will change and remove the
@@ -38,53 +39,89 @@ const SlashMenu = ({closeMenu, containerId, ...props}) => {
 
     return (
         <div className="absolute top-[-10px] left-[-16px] z-[9999999] m-0 mb-3 max-h-[376px] w-[312px] flex-col overflow-y-auto rounded-lg bg-white bg-clip-padding p-0 pt-0 text-sm shadow-xl" role="menu">
-            <CardMenuContent itemWasClicked={closeMenu} allowsKeyboardNav={true} koenigEditor={koenigEditor} {...props} />
+            <CardMenuContent itemWasClicked={closeMenu} allowsKeyboardNav={true} {...props} />
         </div>
     );
 };
 
+const menuStateReducer = (state, action) => {
+    switch (action.type) {
+    case 'update_selected_range': {
+        const {selectedRange} = action;
+        let {isShowingMenu, query, cachedRange} = state;
+
+        if (isShowingMenu && selectedRange) {
+            const {head: {section}} = selectedRange;
+
+            // close the menu if we're on a non-slash section (eg, when / is deleted)
+            if (section && (section.text || section.text === '') && section.text.indexOf('/') !== 0) {
+                isShowingMenu = false;
+                query = '';
+            }
+
+            // update the query when the menu is open and cursor is in our open range
+            if (section === state.cachedRange?.head.section) {
+                query = section.text.substring(
+                    cachedRange.head.offset,
+                    selectedRange.head.offset
+                );
+            }
+        }
+
+        return {...state, isShowingMenu, query, selectedRange};
+    }
+    case 'open_menu': {
+        if (state.isShowingMenu) {
+            return state;
+        }
+
+        const range = state.selectedRange;
+        const {head: {section}} = range;
+
+        // only open the menu if the slash is on an otherwise empty paragraph
+        if (range.isCollapsed && section && !section.isListItem && section.text === '/') {
+            const isShowingMenu = true;
+            const query = '';
+            const cachedRange = state.selectedRange;
+
+            return {...state, isShowingMenu, query, cachedRange};
+        }
+
+        return state;
+    }
+    case 'close_menu': {
+        const isShowingMenu = false;
+        const query = '';
+        return {...state, isShowingMenu, query};
+    }
+    default:
+        return state;
+    }
+};
+
 const SlashMenuContainer = ({selectedRange}) => {
+    const [state, dispatch] = React.useReducer(menuStateReducer, {
+        isShowingMenu: false,
+        query: '',
+        cachedRange: null
+    });
+
     const koenigEditor = React.useContext(koenigEditorContext);
     const containerEl = React.useRef(null);
-    const cachedRange = React.useRef(null);
-    const lastSelectedRange = React.useRef(null);
 
-    const [isShowingMenu, setIsShowingMenu] = React.useState(false);
-    const [query, setQuery] = React.useState('');
-
-    // hack to get around not being able to read props/state inside a useEffect callback
-    // without this we could end up registering/unregistering key commands on every keystroke
-    const selectedRangeRef = React.useRef(null);
-    selectedRangeRef.current = selectedRange;
-
-    const closeMenu = React.useCallback(() => {
-        setIsShowingMenu(false);
-        setQuery('');
-        cachedRange.current = null;
-    }, []);
+    // keep state updated with selected range, closing menu if needed
+    React.useEffect(() => {
+        dispatch({type: 'update_selected_range', selectedRange});
+    }, [selectedRange]);
 
     // on first render, register the / text command and trigger handler
     React.useEffect(() => {
-        const showMenu = () => {
-            const range = selectedRangeRef.current;
-            const {head: {section}} = range;
-
-            // only show the menu if the slash is on an otherwise empty paragraph
-            if (range.isCollapsed && section && !section.isListItem && section.text === '/') {
-                setIsShowingMenu((prevValue) => {
-                    if (prevValue === false) {
-                        setQuery('');
-                        cachedRange.current = selectedRangeRef.current;
-                        return true;
-                    }
-                });
-            }
-        };
-
         koenigEditor.mobiledocEditor.onTextInput({
             name: 'slash_menu',
             text: '/',
-            run: showMenu
+            run: () => {
+                dispatch({type: 'open_menu'});
+            }
         });
 
         return () => {
@@ -94,30 +131,9 @@ const SlashMenuContainer = ({selectedRange}) => {
 
     let containerStyle = {};
 
-    // handle selection changes
-    if (selectedRange !== lastSelectedRange.current) {
-        // close menu if open when selection changes
-        if (isShowingMenu && selectedRange) {
-            const {head: {section}} = selectedRange;
-
-            // close the menu if we're on a non-slash section (eg, when / is deleted)
-            if (section && (section.text || section.text === '') && section.text.indexOf('/') !== 0) {
-                closeMenu();
-            }
-
-            // update the query when the menu is open and cursor is in our open range
-            if (section === cachedRange.current?.head.section) {
-                setQuery(section.text.substring(
-                    cachedRange.current.head.offset,
-                    selectedRange.head.offset
-                ));
-            }
-        }
-    }
-
     // calculate position on every render
-    if (cachedRange.current || selectedRange) {
-        const {head: {section}} = cachedRange.current || selectedRange;
+    if (state.cachedRange || selectedRange) {
+        const {head: {section}} = state.cachedRange || selectedRange;
 
         if (section && section.renderNode.element) {
             const containerRect = containerEl.current.parentNode.getBoundingClientRect();
@@ -129,12 +145,14 @@ const SlashMenuContainer = ({selectedRange}) => {
         }
     }
 
-    // keep lastSelectedRange up to date so we can track changes
-    lastSelectedRange.current = selectedRange;
-
     return (
         <div id="koenig-slash-menu" className="absolute" style={containerStyle} ref={containerEl}>
-            {isShowingMenu && <SlashMenu closeMenu={closeMenu} containerId={containerEl.current.id} query={query} replacementRange={cachedRange.current} /> }
+            {state.isShowingMenu && <SlashMenu
+                closeMenu={() => dispatch({type: 'close_menu'})}
+                containerId={containerEl.current.id}
+                query={state.query}
+                replacementRange={state.cachedRange}
+            /> }
         </div>
     );
 };
