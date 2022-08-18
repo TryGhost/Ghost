722358d853da376886c90eee16edbcae1d6ac9f1
Daniel Lockyer <hi@daniellockyer.com>
2022-08-18 16:46:16 +0200

    Added actions events for integrations and webhooks
    refs https://github.com/TryGhost/Toolbox/issues/356
    
    - this allows Ghost to collect CRUD events for integrations and
      webhooks, to eventually be shown in the audit log

--- ghost/core/core/server/models/webhook.js
+++ ghost/core/core/server/models/webhook.js
@@ -7,6 +7,9 @@ let Webhooks;
 Webhook = ghostBookshelf.Model.extend({
     tableName: 'webhooks',
 
+    actionsCollectCRUD: true,
+    actionsResourceType: 'webhook',
+
     defaults() {
         return {
             api_version: `v${ghostVersion.safe}`,
