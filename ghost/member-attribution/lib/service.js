7c3d189222e19b0880bb902a55ccfc3d17e49c01
Simon Backx <simon@ghost.org>
2022-08-19 22:39:18 +0200

    Added member attribution to member details page (#15266)
    refs https://github.com/TryGhost/Team/issues/1817
    
    Co-authored-by: James Morris <moreofmorris@users.noreply.github.com>

--- ghost/member-attribution/lib/service.js
+++ ghost/member-attribution/lib/service.js
@@ -5,11 +5,19 @@ const AttributionBuilder = require('./attribution');
 const UrlHistory = require('./history');
 
 class MemberAttributionService {
-    constructor({MemberCreatedEvent, SubscriptionCreatedEvent, urlService, labsService}) {
+    constructor({Post, User, Tag, MemberCreatedEvent, SubscriptionCreatedEvent, urlService, labsService}) {
         const eventHandler = new MemberAttributionEventHandler({MemberCreatedEvent, SubscriptionCreatedEvent, DomainEvents, labsService});
         eventHandler.subscribe();
 
-        const urlTranslator = new UrlTranslator({urlService});
+        this.urlService = urlService;
+        this.models = {MemberCreatedEvent, SubscriptionCreatedEvent};
+
+        const urlTranslator = new UrlTranslator({
+            urlService, 
+            models: {
+                Post, User, Tag
+            }
+        });
         this.attributionBuilder = new AttributionBuilder({urlTranslator});
     }
 
@@ -21,6 +29,42 @@ class MemberAttributionService {
     getAttribution(historyArray) {
         const history = new UrlHistory(historyArray);
         return this.attributionBuilder.getAttribution(history);
+    }
+
+    /**
+     * Returns the parsed attribution for a member creation event
+     * @param {string} memberId 
+     * @returns {Promise<import('./attribution').AttributionResource|null>}
+     */
+    async getMemberCreatedAttribution(memberId) {
+        const memberCreatedEvent = await this.models.MemberCreatedEvent.findOne({member_id: memberId}, {require: false});
+        if (!memberCreatedEvent || !memberCreatedEvent.get('attribution_type')) {
+            return null;
+        }
+        const attribution = this.attributionBuilder.build({
+            id: memberCreatedEvent.get('attribution_id'),
+            url: memberCreatedEvent.get('attribution_url'),
+            type: memberCreatedEvent.get('attribution_type')
+        });
+        return await attribution.getResource();
+    }
+
+    /**
+     * Returns the last attribution for a given subscription ID
+     * @param {string} subscriptionId 
+     * @returns {Promise<import('./attribution').AttributionResource|null>}
+     */
+    async getSubscriptionCreatedAttribution(subscriptionId) {
+        const subscriptionCreatedEvent = await this.models.SubscriptionCreatedEvent.findOne({subscription_id: subscriptionId}, {require: false});
+        if (!subscriptionCreatedEvent || !subscriptionCreatedEvent.get('attribution_type')) {
+            return null;
+        }
+        const attribution = this.attributionBuilder.build({
+            id: subscriptionCreatedEvent.get('attribution_id'),
+            url: subscriptionCreatedEvent.get('attribution_url'),
+            type: subscriptionCreatedEvent.get('attribution_type')
+        });
+        return await attribution.getResource();
     }
 }
 
