02f5f7744aad9989bfd0af22bc042a231fa5cfbc
Hannah Wolfe <github.erisds@gmail.com>
2022-08-19 15:24:51 +0100

    🐛 Fixed error deleting post with comment replies
    closes: https://github.com/TryGhost/Ghost/issues/15252
    
    - comments are deleted when posts are deleted. Without cascade delete on parent_id, replies cannot be deleted
    - this change means that deleting a post will delete all comments and replies without error

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = 'ec9f46178cb70d3297d266203659ef4c';
+    const currentSchemaHash = 'ebc73ecd26ac795c34eaec6d8ec06304';
     const currentFixturesHash = '0ae1887dd0b42508be946bdbd20d41c8';
     const currentSettingsHash = 'd54210758b7054e2174fd34aa2320ad7';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
