331a508f46a5d4c3e5a216c24c31578c7b2395fa
Daniel Lockyer <hi@daniellockyer.com>
2022-08-22 15:33:18 +0200

    Added basic filtering and searching to audit log
    refs https://github.com/TryGhost/Toolbox/issues/356
    
    - this adds some basic filtering and search across the audit log events
    - not all of it works, but filtering by objects and searching for users
      should work

--- ghost/admin/app/controllers/settings/audit-log.js
+++ ghost/admin/app/controllers/settings/audit-log.js
@@ -1,4 +1,39 @@
 import Controller from '@ember/controller';
+import {action} from '@ember/object';
+import {inject as service} from '@ember/service';
+import {tracked} from '@glimmer/tracking';
 
 export default class AuditLogController extends Controller {
+    @service router;
+    @service settings;
+    @service store;
+
+    queryParams = ['excludedResources', 'user'];
+
+    @tracked excludedResources = null;
+    @tracked user = null;
+
+    get fullExcludedResources() {
+        return (this.excludedResources || '').split(',');
+    }
+
+    get userRecord() {
+        if (!this.user) {
+            return null;
+        }
+
+        // TODO: {reload: true} here shouldn't be needed but without it
+        // the template renders nothing if the record is already in the store
+        return this.store.findRecord('user', this.user, {reload: true});
+    }
+
+    @action
+    changeExcludedResources(newList) {
+        this.router.transitionTo({queryParams: {excludedResources: newList}});
+    }
+
+    @action
+    changeUser(user) {
+        this.router.transitionTo({queryParams: {user: user?.id}});
+    }
 }
