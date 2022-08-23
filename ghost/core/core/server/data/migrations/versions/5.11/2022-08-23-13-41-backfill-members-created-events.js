b149785941eb588691bf9f6431483b6e9642bbb0
Simon Backx <simon@ghost.org>
2022-08-23 18:21:10 +0200

    fixup! Added backfill migration for members created events

--- ghost/core/core/server/data/migrations/versions/5.11/2022-08-23-13-41-backfill-members-created-events.js
+++ ghost/core/core/server/data/migrations/versions/5.11/2022-08-23-13-41-backfill-members-created-events.js
@@ -8,23 +8,20 @@ module.exports = createTransactionalMigration(
         const members = await knex('members')
             .select('id', 'created_at');
 
-        const toInsert = [];
-
-        // eslint-disable-next-line no-restricted-syntax
-        for (const member of members) {
-            toInsert.push({
+        const toInsert = members.map((member) => {
+            return {
                 id: ObjectID().toHexString(),
                 member_id: member.id,
                 created_at: member.created_at,
                 source: 'member'
-            });
-        }
+            };
+        });
 
         logging.info(`Inserting ${toInsert.length} members created events`);
         await knex.batchInsert('members_created_events', toInsert);
     },
     async function down(knex) {
         logging.info(`Clearing all members created events`);
-        await knex('members_created_events').truncate();
+        await knex('members_created_events').del();
     }
 );
