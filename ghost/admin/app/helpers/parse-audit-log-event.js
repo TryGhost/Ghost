9effa119c630f431fbd9296e2a77e52577507cee
Daniel Lockyer <hi@daniellockyer.com>
2022-08-23 14:11:24 +0200

    Implemented `context` on Actions events
    refs https://github.com/TryGhost/Toolbox/issues/356
    
    - in order to show data that we might not necessarily still have around
      (ie. when you delete a post, you might want the title), we're going to
      start utilizing the `context` column
    - right now, we store the `primary_name` for deleted events, and we also
      store the `setting` `key` and `group` so we can reference it in the
      audit log

--- ghost/admin/app/helpers/parse-audit-log-event.js
+++ ghost/admin/app/helpers/parse-audit-log-event.js
@@ -9,7 +9,9 @@ export default class ParseAuditLogEvent extends Helper {
         const actionIcon = getActionIcon(ev);
         const getActor = () => this.store.findRecord(ev.actor_type, ev.actor_id, {reload: false});
         const getResource = () => this.store.findRecord(ev.resource_type, ev.resource_id, {reload: false});
-        const linkable = ['page', 'post'].includes(ev.resource_type);
+        const contextResource = getContextResource(ev);
+
+        const linkable = ['page', 'post'].includes(ev.resource_type) && ev.event !== 'deleted';
 
         return {
             get actor() {
@@ -18,6 +20,7 @@ export default class ParseAuditLogEvent extends Helper {
             get resource() {
                 return getResource();
             },
+            contextResource,
             linkable,
             actionIcon,
             action,
@@ -49,4 +52,17 @@ function getAction(ev) {
     }
 
     return `${ev.event} ${resourceType}`;
+}
+
+function getContextResource(ev) {
+    if (ev.resource_type === 'setting') {
+        if (ev.context?.group && ev.context?.key) {
+            return {
+                first: ev.context.group,
+                second: ev.context.key
+            };
+        }
+    }
+
+    return null;
 }
