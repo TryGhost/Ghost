2f84902bf18fdb165b9f946612f2da4a2826c0be
Rishabh <zrishabhgarg@gmail.com>
2022-08-24 18:24:55 +0530

    ✨ Enabled free trials via tiers and offers
    refs https://github.com/TryGhost/Team/issues/1724
    refs https://github.com/TryGhost/Team/issues/1726
    
    - allows site owners to add a default free trial period to their tiers
    - allows site owners to create custom offers that allow free trials on tiers

--- ghost/core/test/unit/server/models/stripe-customer-subscription.test.js
+++ ghost/core/test/unit/server/models/stripe-customer-subscription.test.js
@@ -25,35 +25,7 @@ describe('Unit: models/stripe-customer-subscription', function () {
             sinon.restore();
         });
 
-        it('ignores trial details when flag is off', function () {
-            const trialStartAt = new Date();
-            const trialEndAt = new Date(Date.now() + 7 * 86400 * 1000); // trial ending 7 days from now
-            const stripeSubscription = {
-                customer_id: 'fake_customer_id',
-                subscription_id: 'fake_subscription_id',
-                plan_id: 'fake_plan_id',
-                stripe_price_id: 'fake_plan_id',
-                plan_amount: 1337,
-                plan_nickname: 'e-LEET',
-                plan_interval: 'year',
-                plan_currency: 'btc',
-                status: 'active',
-                start_date: new Date(),
-                current_period_end: new Date(),
-                cancel_at_period_end: false,
-                trial_start_at: trialStartAt,
-                trial_end_at: trialEndAt
-            };
-
-            const json = serialize(stripeSubscription);
-            should.exist(json.plan);
-            should.exist(json.customer);
-            should.not.exist(json.trial_start_at);
-            should.not.exist(json.trial_end_at);
-        });
-
-        it('returns trial details with flag enabled', function () {
-            sinon.stub(labs, 'isSet').returns(true);
+        it('returns subscription trial start and end details', function () {
             const trialStartAt = new Date();
             const trialEndAt = new Date(Date.now() + 7 * 86400 * 1000); // trial ending 7 days from now
             const stripeSubscription = {
