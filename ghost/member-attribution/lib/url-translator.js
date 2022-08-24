284031d7c21793589bb8a2f218edac42936016ab
Simon Backx <simon@ghost.org>
2022-08-24 16:11:25 +0200

    Added member attributions to activity feed (#15283)
    refs https://github.com/TryGhost/Team/issues/1833
    refs https://github.com/TryGhost/Team/issues/1834
    
    We've added the attribution property to subscription and signup events when the
    flag is enabled. The attributions resource is fetched by creating multiple relations
    on the model, rather than polymorphic as we ran into issues with that as they can't
    be nullable/optional.
    
    The parse-member-event structure has been updated to make it easier to work with,
    specifically `getObject` is only used when the event is clickable, and there is now a 
    join property which makes it easier to join the action and the object.

--- ghost/member-attribution/lib/url-translator.js
+++ ghost/member-attribution/lib/url-translator.js
@@ -6,8 +6,7 @@
  */
 
 /**
- * Translate a url into a type and id
- * And also in reverse
+ * Translate a url into, (id+type), or a resource, and vice versa
  */
 class UrlTranslator {
     /**
@@ -81,9 +80,11 @@ class UrlTranslator {
         }
     }
 
-    async getResourceById(id, type, options = {absolute: true}) {
-        const url = this.urlService.getUrlByResourceId(id, options);
+    getUrlByResourceId(id, options = {absolute: true}) {
+        return this.urlService.getUrlByResourceId(id, options);
+    }
 
+    async getResourceById(id, type) {
         switch (type) {
         case 'post':
         case 'page': {
@@ -92,12 +93,7 @@ class UrlTranslator {
                 return null;
             }
     
-            return {
-                id: post.id,
-                type,
-                url,
-                title: post.get('title')
-            };
+            return post;
         }
         case 'author': {
             const user = await this.models.User.findOne({id}, {require: false});
@@ -105,12 +101,7 @@ class UrlTranslator {
                 return null;
             }
     
-            return {
-                id: user.id,
-                type,
-                url,
-                title: user.get('name')
-            };
+            return user;
         }
         case 'tag': {
             const tag = await this.models.Tag.findOne({id}, {require: false});
@@ -118,12 +109,7 @@ class UrlTranslator {
                 return null;
             }
     
-            return {
-                id: tag.id,
-                type,
-                url,
-                title: tag.get('name')
-            };
+            return tag;
         }
         }
         return null;
