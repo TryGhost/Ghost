de194419c0aec704208873d7debcf6819a73c5f1
Fabien 'egg' O'Carroll <fabien@allou.is>
2022-08-25 15:41:43 -0400

    Promoted Member Attribution to a private beta (#15316)
    We promote from alpha -> beta so that we don't require the
    enableDeveloperExperiments flag, the toggle in the UI is behind the
    flag still, so it will only be visible to developers or people using
    alpha features.

--- ghost/core/core/shared/labs.js
+++ ghost/core/core/shared/labs.js
@@ -24,14 +24,14 @@ const GA_FEATURES = [
 // NOTE: this allowlist is meant to be used to filter out any unexpected
 //       input for the "labs" setting value
 const BETA_FEATURES = [
-    'activitypub'
+    'activitypub',
+    'memberAttribution'
 ];
 
 const ALPHA_FEATURES = [
     'auditLog',
     'urlCache',
     'beforeAfterCard',
-    'memberAttribution',
     'emailAlerts'
 ];
 
