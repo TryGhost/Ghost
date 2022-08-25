f523e1eb6ba2c28c73c2de54731a1d5f9394e099
Fabien "egg" O'Carroll <fabien@allou.is>
2022-08-25 12:21:08 -0400

    Refactored UrlHistory to use static factory method
    This keeps the constructor clean, relying on types for validation,
    whilst preserving the validation when creating the instance. The
    constructor is now private so that the factory which handles
    validation is always used.
    
    The tests have also been updated to test the public factory interface
    rather than the internal validation methods. Validation has been
    rolled into a single method and slightly improved in the way of
    readability.

--- ghost/member-attribution/test/attribution.test.js
+++ ghost/member-attribution/test/attribution.test.js
@@ -56,18 +56,18 @@ describe('AttributionBuilder', function () {
     });
 
     it('Returns empty if empty history', function () {
-        const history = new UrlHistory([]);
+        const history = UrlHistory.create([]);
         should(attributionBuilder.getAttribution(history)).match({id: null, type: null, url: null});
     });
 
     it('Returns last url', function () {
-        const history = new UrlHistory([{path: '/dir/not-last', time: 123}, {path: '/dir/test/', time: 123}]);
+        const history = UrlHistory.create([{path: '/dir/not-last', time: 123}, {path: '/dir/test/', time: 123}]);
         should(attributionBuilder.getAttribution(history)).match({type: 'url', id: null, url: '/test/'});
     });
 
     it('Returns last post', function () {
-        const history = new UrlHistory([
-            {path: '/dir/my-post', time: 123}, 
+        const history = UrlHistory.create([
+            {path: '/dir/my-post', time: 123},
             {path: '/dir/test', time: 124},
             {path: '/dir/unknown-page', time: 125}
         ]);
@@ -75,25 +75,25 @@ describe('AttributionBuilder', function () {
     });
 
     it('Returns last post even when it found pages', function () {
-        const history = new UrlHistory([
-            {path: '/dir/my-post', time: 123}, 
-            {path: '/dir/my-page', time: 124}, 
+        const history = UrlHistory.create([
+            {path: '/dir/my-post', time: 123},
+            {path: '/dir/my-page', time: 124},
             {path: '/dir/unknown-page', time: 125}
         ]);
         should(attributionBuilder.getAttribution(history)).match({type: 'post', id: 123, url: '/my-post'});
     });
 
     it('Returns last page if no posts', function () {
-        const history = new UrlHistory([
-            {path: '/dir/other', time: 123}, 
-            {path: '/dir/my-page', time: 124}, 
+        const history = UrlHistory.create([
+            {path: '/dir/other', time: 123},
+            {path: '/dir/my-page', time: 124},
             {path: '/dir/unknown-page', time: 125}
         ]);
         should(attributionBuilder.getAttribution(history)).match({type: 'page', id: 845, url: '/my-page'});
     });
 
     it('Returns all null for invalid histories', function () {
-        const history = new UrlHistory('invalid');
+        const history = UrlHistory.create('invalid');
         should(attributionBuilder.getAttribution(history)).match({
             type: null,
             id: null,
@@ -102,7 +102,7 @@ describe('AttributionBuilder', function () {
     });
 
     it('Returns all null for empty histories', function () {
-        const history = new UrlHistory([]);
+        const history = UrlHistory.create([]);
         should(attributionBuilder.getAttribution(history)).match({
             type: null,
             id: null,
