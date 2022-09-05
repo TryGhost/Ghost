4625026fc224dc53f4cb4500019d16ab30b29ceb
James Morris <moreofmorris@users.noreply.github.com>
2022-09-05 16:53:05 +0100

    Made some tweaks to the pagination buttons to look less like dividers
    no issue

--- apps/comments-ui/src/components/content/Pagination.js
+++ apps/comments-ui/src/components/content/Pagination.js
@@ -20,8 +20,7 @@ const Pagination = (props) => {
 
     return (
         <button className="group mb-10 flex w-full items-center px-0 pt-0 pb-2 text-left font-sans text-md font-semibold text-neutral-700 dark:text-white " onClick={loadMore}>
-            <span className="mr-4 whitespace-nowrap">↑ Show {left} previous {left === 1 ? 'comment' : 'comments'}</span>
-            <span className="mt-[3px] inline-block h-[3px] w-full rounded bg-neutral-100 transition-[background-color] duration-200 ease-out group-hover:bg-neutral-200 dark:bg-[rgba(255,255,255,0.05)]" />
+            <span className="flex h-[39px] w-full items-center justify-center whitespace-nowrap rounded-[6px] bg-[rgb(229,229,229,0.4)] py-2 px-3 text-center font-sans text-sm font-semibold text-neutral-700 outline-0 transition-[opacity,background] duration-150 hover:bg-[rgb(229,229,229,0.7)] dark:bg-[rgba(255,255,255,0.08)] dark:text-neutral-100 dark:hover:bg-[rgba(255,255,255,0.1)]">↑ Show {left} previous {left === 1 ? 'comment' : 'comments'}</span>
         </button>
     );
 };
