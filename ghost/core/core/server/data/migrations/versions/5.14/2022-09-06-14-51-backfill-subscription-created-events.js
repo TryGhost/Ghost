9a1a95b035dafc85b705623cf66d247cca99376a
Simon Backx <simon@ghost.org>
2022-09-06 17:13:04 +0200

    Backfilled subscription created events
    fixes https://github.com/TryGhost/Team/issues/1857
    
    - Create new subscription created events for subscriptions that don't have one yet
    - Some subscriptions already have a subscription created event
    - Down operation is no-op because we don't want to loose data, and running the up operation multiple times will not add duplicate events

---
+++ ghost/core/core/server/data/migrations/versions/5.14/2022-09-06-14-51-backfill-subscription-created-events.js
@@ -0,0 +1,39 @@
+const ObjectID = require('bson-objectid').default;
+const logging = require('@tryghost/logging');
+
+const {createTransactionalMigration} = require('../../utils');
+
+module.exports = createTransactionalMigration(
+    async function up(knex) {
+        // Join all subscriptions without a subscription created event
+        // We need to filter this because new subscriptions will already have the events
+        const subscriptions = await knex('members_stripe_customers_subscriptions as s')
+            .select('s.id', 's.created_at', 'c.member_id')
+            .join('members_stripe_customers as c', 'c.customer_id', 's.customer_id')
+            .whereNotExists(function () {
+                this.select('*')
+                    .from('members_subscription_created_events as e')
+                    .whereRaw('e.subscription_id = s.id');
+            });
+
+        if (subscriptions.length === 0) {
+            logging.warn(`Skipping migration because no subscriptions without events found`);
+            return;
+        }
+
+        const toInsert = subscriptions.map((subscription) => {
+            return {
+                id: ObjectID().toHexString(),
+                subscription_id: subscription.id,
+                member_id: subscription.member_id,
+                created_at: subscription.created_at
+            };
+        });
+
+        logging.info(`Inserting ${toInsert.length} subscriptions created events`);
+        await knex.batchInsert('members_subscription_created_events', toInsert);
+    },
+    async function down() {
+        // no-op (attribution data will be lost otherwise)
+    }
+);
