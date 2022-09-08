1e4810d138334be69da8ee0af08e8af3414a88dd
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-09-08 08:20:54 +0200

    Added logging to new migration. ref https://github.com/TryGhost/Ghost/pull/15359#discussion_r965279084

--- ghost/core/core/server/data/migrations/versions/5.13/2022-09-02-12-55-rename-members-bio-to-expertise.js
+++ ghost/core/core/server/data/migrations/versions/5.13/2022-09-02-12-55-rename-members-bio-to-expertise.js
@@ -3,14 +3,15 @@ const {createNonTransactionalMigration} = require('../../utils');
 
 module.exports = createNonTransactionalMigration(
     async function up(knex) {
+        logging.info(`Changing column bio to expertise in members table`);
         await knex.schema.table('members', (table) => {
             table.renameColumn('bio', 'expertise');
         });
     },
     async function down(knex) {
-        // probably shouldn't be reversible though as its a breaking change?
         await knex.schema.table('members', (table) => {
+            logging.info(`Changing column expertise to bio in members table`);
             table.renameColumn('expertise', 'bio');
         });
     }
-)
+);
