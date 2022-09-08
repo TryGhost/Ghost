8ad1a9ea790f8676d812004ceabb75e2a7901c1d
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-09-08 13:08:49 +0200

    Revert "Added migration into conditional due to failing tests."
    This reverts commit dd53e45b48972eb74978301c972b1d8875be27cd.

--- ghost/core/core/server/data/migrations/versions/5.13/2022-09-02-12-55-rename-members-bio-to-expertise.js
+++ ghost/core/core/server/data/migrations/versions/5.13/2022-09-02-12-55-rename-members-bio-to-expertise.js
@@ -3,24 +3,15 @@ const {createNonTransactionalMigration} = require('../../utils');
 
 module.exports = createNonTransactionalMigration(
     async function up(knex) {
-        // check if the column exists before trying to rename it
-        const hasColumn = await knex.schema.hasColumn('members', 'bio');
-        if (hasColumn) {
-            logging.info('Renaming members.bio to members.expertise');
-            await knex.schema.table('members', (table) => {
-                table.renameColumn('bio', 'expertise');
-            }
-            );
-        }
+        logging.info(`Changing column bio to expertise in members table`);
+        await knex.schema.table('members', (table) => {
+            table.renameColumn('bio', 'expertise');
+        });
     },
     async function down(knex) {
-        const hasColumn = await knex.schema.hasColumn('members', 'expertise');
-        if (hasColumn) {
-            logging.info(`Renaming members.expertise to members.bio`);
-            await knex.schema.table('members', (table) => {
-                table.renameColumn('expertise', 'bio');
-            }
-            );
-        }
+        await knex.schema.table('members', (table) => {
+            logging.info(`Changing column expertise to bio in members table`);
+            table.renameColumn('expertise', 'bio');
+        });
     }
 );
