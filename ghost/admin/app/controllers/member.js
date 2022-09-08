187f065c99e1b86b5d7cab8c7ccafb8788e7a403
Hakim Razalan <hakim.razalan@gmail.com>
2022-09-08 23:40:10 +0800

    refactor - move updateMemberCount() invocation to refreshData on members controller

--- ghost/admin/app/controllers/member.js
+++ ghost/admin/app/controllers/member.js
@@ -129,7 +129,6 @@ export default class MemberController extends Controller {
         return this.member.destroyRecord(options).then(() => {
             this.members.refreshData();
             this.transitionToRoute('members');
-            this.membersStats.updateMemberCounts();
             return;
         }, (error) => {
             return this.notifications.showAPIError(error, {key: 'member.delete'});
@@ -182,7 +181,6 @@ export default class MemberController extends Controller {
             yield member.save();
             member.updateLabels();
             this.members.refreshData();
-            this.membersStats.updateMemberCounts();
 
             // replace 'member.new' route with 'member' route
             this.replaceRoute('member', member);
