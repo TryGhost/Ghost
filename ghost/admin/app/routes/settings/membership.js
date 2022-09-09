416932e118507a8918b3faa5e919a3dfce8d23df
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-09-09 16:21:32 +0100

    Switched membership unsaved changes modal to new modal pattern
    refs https://github.com/TryGhost/Team/issues/1734
    refs https://github.com/TryGhost/Team/issues/559
    refs https://github.com/TryGhost/Ghost/issues/14101
    
    - switches to newer modal patterns ready for later Ember upgrades

--- ghost/admin/app/routes/settings/membership.js
+++ ghost/admin/app/routes/settings/membership.js
@@ -1,5 +1,7 @@
 import AdminRoute from 'ghost-admin/routes/admin';
+import ConfirmUnsavedChangesModal from '../../components/modals/confirm-unsaved-changes';
 import VerifyEmail from '../../components/modals/settings/verify-email';
+import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
 
 export default class MembershipSettingsRoute extends AdminRoute {
@@ -41,11 +43,46 @@ export default class MembershipSettingsRoute extends AdminRoute {
         }
     }
 
-    actions = {
-        willTransition(transition) {
-            return this.controller.leaveRoute(transition);
+    @action
+    async willTransition(transition) {
+        if (this.hasConfirmed) {
+            return true;
         }
-    };
+
+        transition.abort();
+
+        // wait for any existing confirm modal to be closed before allowing transition
+        if (this.confirmModal) {
+            return;
+        }
+
+        if (this.controller.saveTask?.isRunning) {
+            await this.controller.saveTask.last;
+        }
+
+        const shouldLeave = await this.confirmUnsavedChanges();
+
+        if (shouldLeave) {
+            this.controller.reset();
+
+            this.hasConfirmed = true;
+            return transition.retry();
+        }
+    }
+
+    async confirmUnsavedChanges() {
+        if (this.controller.isDirty) {
+            this.confirmModal = this.modals
+                .open(ConfirmUnsavedChangesModal)
+                .finally(() => {
+                    this.confirmModal = null;
+                });
+
+            return this.confirmModal;
+        }
+
+        return true;
+    }
 
     buildRouteInfoMetadata() {
         return {
