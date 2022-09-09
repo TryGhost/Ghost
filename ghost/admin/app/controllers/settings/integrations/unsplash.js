6e11a24002a239aaf8aa6b9cd25708999804561c
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-09-09 17:13:07 +0100

    Switched Unsplash integration unsaved changes modal to new modal pattern
    refs https://github.com/TryGhost/Team/issues/1734
    refs https://github.com/TryGhost/Team/issues/559
    refs https://github.com/TryGhost/Ghost/issues/14101
    
    - switches to newer modal patterns ready for later Ember upgrades

--- ghost/admin/app/controllers/settings/integrations/unsplash.js
+++ ghost/admin/app/controllers/settings/integrations/unsplash.js
@@ -1,16 +1,11 @@
-import classic from 'ember-classic-decorator';
+import Controller from '@ember/controller';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
-/* eslint-disable ghost/ember/alias-model-in-controller */
-import Controller from '@ember/controller';
 import {task} from 'ember-concurrency';
 
-@classic
 export default class UnsplashController extends Controller {
     @service notifications;
     @service settings;
-
-    leaveSettingsTransition = null;
 
     @action
     update(value) {
@@ -22,48 +17,8 @@ export default class UnsplashController extends Controller {
         this.saveTask.perform();
     }
 
-    @action
-    toggleLeaveSettingsModal(transition) {
-        let leaveTransition = this.leaveSettingsTransition;
-
-        if (!transition && this.showLeaveSettingsModal) {
-            this.set('leaveSettingsTransition', null);
-            this.set('showLeaveSettingsModal', false);
-            return;
-        }
-
-        if (!leaveTransition || transition.targetName === leaveTransition.targetName) {
-            this.set('leaveSettingsTransition', transition);
-
-            // if a save is running, wait for it to finish then transition
-            if (this.saveTask.isRunning) {
-                return this.saveTask.last.then(() => {
-                    transition.retry();
-                });
-            }
-
-            // we genuinely have unsaved data, show the modal
-            this.set('showLeaveSettingsModal', true);
-        }
-    }
-
-    @action
-    leaveSettings() {
-        let transition = this.leaveSettingsTransition;
-        let settings = this.settings;
-
-        if (!transition) {
-            this.notifications.showAlert('Sorry, there was an error in the application. Please let the Ghost team know what happened.', {type: 'error'});
-            return;
-        }
-
-        // roll back changes on settings model
-        settings.rollbackAttributes();
-
-        return transition.retry();
-    }
-
-    @(task(function* () {
+    @task({drop: true})
+    *saveTask() {
         try {
             yield this.settings.validate();
             return yield this.settings.save();
@@ -71,6 +26,5 @@ export default class UnsplashController extends Controller {
             this.notifications.showAPIError(error);
             throw error;
         }
-    }).drop())
-        saveTask;
+    }
 }
