3e3fc71ed1e7ae9da34bb9695ed6e9a9c25d4e77
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-09-09 15:24:05 +0100

    Cleaned up unused code in settings controller
    no issue
    
    - removed old/unused unsaved-changes code
    - cleaned up unused service injections

--- ghost/admin/app/controllers/settings.js
+++ ghost/admin/app/controllers/settings.js
@@ -1,29 +1,12 @@
 import AboutModal from '../components/modals/settings/about';
-import classic from 'ember-classic-decorator';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
 /* eslint-disable ghost/ember/alias-model-in-controller */
 import Controller from '@ember/controller';
 
-@classic
 export default class SettingsController extends Controller {
-    @service settings;
-    @service session;
     @service modals;
     @service upgradeStatus;
-
-    showLeaveSettingsModal = false;
-
-    @action
-    closeLeaveSettingsModal() {
-        this.set('showLeaveSettingsModal', false);
-    }
-
-    @action
-    async leavePortalSettings() {
-        this.settings.rollbackAttributes();
-        this.set('showLeaveSettingsModal', false);
-    }
 
     @action
     openAbout() {
