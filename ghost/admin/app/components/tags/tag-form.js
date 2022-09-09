08b099e5274c3471724375924067381029b62220
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-09-09 13:27:00 +0100

    Refactored tags screens
    refs https://github.com/TryGhost/Ghost/issues/14101
    
    - migrated to full native class and octane syntax
    - cleaned up unnecessary use of "scratchTag"

--- ghost/admin/app/components/tags/tag-form.js
+++ ghost/admin/app/components/tags/tag-form.js
@@ -1,61 +1,29 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import Ember from 'ember';
-import {action, computed} from '@ember/object';
+import {action} from '@ember/object';
 import {htmlSafe} from '@ember/template';
-import {or} from '@ember/object/computed';
 import {run} from '@ember/runloop';
 import {inject as service} from '@ember/service';
+import {slugify} from '@tryghost/string';
 import {task, timeout} from 'ember-concurrency';
+import {tracked} from '@glimmer/tracking';
 
 const {Handlebars} = Ember;
 
-export default Component.extend({
-    feature: service(),
-    config: service(),
-    settings: service(),
+export default class TagForm extends Component {
+    @service feature;
+    @service config;
+    @service settings;
 
-    tag: null,
-    scratchTag: null,
+    @tracked metadataOpen = false;
+    @tracked twitterMetadataOpen = false;
+    @tracked facebookMetadataOpen = false;
+    @tracked codeInjectionOpen = false;
 
-    // Allowed actions
-    setProperty: () => {},
-
-    twitterTitle: or('scratchTag.twitterTitle', 'seoTitle'),
-    twitterDescription: or('scratchTag.twitterDescription', 'seoDescription', 'settings.metaDescription', ''),
-    twitterImage: or('tag.twitterImage', 'tag.featureImage'),
-
-    facebookTitle: or('scratchTag.ogTitle', 'seoTitle'),
-    facebookDescription: or('scratchTag.ogDescription', 'seoDescription', 'settings.metaDescription', ''),
-    facebookImage: or('tag.ogImage', 'tag.featureImage'),
-
-    accentColor: computed('tag.accentColor', function () {
-        let color = this.get('tag.accentColor');
-        if (color && color[0] === '#') {
-            return color.slice(1);
-        }
-        return color;
-    }),
-
-    accentColorPickerValue: computed('tag.accentColor', function () {
-        return this.tag.get('accentColor') || '#ffffff';
-    }),
-
-    accentColorBgStyle: computed('accentColorPickerValue', function () {
-        return htmlSafe(`background-color: ${this.accentColorPickerValue}`);
-    }),
-
-    title: computed('tag.isNew', function () {
-        if (this.get('tag.isNew')) {
-            return 'New tag';
-        } else {
-            return 'Tag settings';
-        }
-    }),
-
-    seoTitle: computed('scratchTag.{name,metaTitle}', function () {
-        const settingsTitle = this.get('settings.title') || '';
-        const tagName = settingsTitle ? `${this.scratchTag.name} - ${settingsTitle}` : this.scratchTag.name;
-        let metaTitle = this.scratchTag.metaTitle || tagName;
+    get seoTitle() {
+        const settingsTitle = this.settings.get('title') || '';
+        const tagName = settingsTitle ? `${this.args.tag.name} - ${settingsTitle}` : this.args.tag.name;
+        let metaTitle = this.args.tag.metaTitle || tagName;
 
         if (metaTitle && metaTitle.length > 70) {
             metaTitle = metaTitle.substring(0, 70).trim();
@@ -64,13 +32,27 @@ export default Component.extend({
         }
 
         return metaTitle;
-    }),
+    }
 
-    seoURL: computed('scratchTag.{canonicalUrl,slug}', function () {
-        let blogUrl = this.get('config.blogUrl');
-        let seoSlug = this.scratchTag.slug || '';
+    get seoDescription() {
+        let metaDescription = this.args.tag.metaDescription || '';
 
-        let seoURL = this.scratchTag.canonicalUrl || `${blogUrl}/tag/${seoSlug}`;
+        metaDescription = metaDescription.length > 0 ? metaDescription : this.args.tag.description;
+
+        if (metaDescription && metaDescription.length > 156) {
+            metaDescription = metaDescription.substring(0, 156).trim();
+            metaDescription = Handlebars.Utils.escapeExpression(metaDescription);
+            metaDescription = htmlSafe(`${metaDescription}&hellip;`);
+        }
+
+        return metaDescription;
+    }
+
+    get seoURL() {
+        const blogUrl = this.config.get('blogUrl');
+        const seoSlug = this.args.tag.slug || '';
+
+        let seoURL = this.args.tag.canonicalUrl || `${blogUrl}/tag/${seoSlug}`;
 
         // only append a slash to the URL if the slug exists
 
@@ -85,89 +67,119 @@ export default Component.extend({
         }
 
         return seoURL;
-    }),
+    }
 
-    seoDescription: computed('scratchTag.{description,metaDescription}', function () {
-        let metaDescription = this.scratchTag.metaDescription || '';
+    get twitterTitle() {
+        return this.args.tag.twitterTitle || this.seoTitle;
+    }
 
-        metaDescription = metaDescription.length > 0 ? metaDescription : this.scratchTag.description;
+    get twitterDescription() {
+        return this.args.tag.twitterDescription || this.seoDescription || this.settings.get('metaDescription') || '';
+    }
 
-        if (metaDescription && metaDescription.length > 156) {
-            metaDescription = metaDescription.substring(0, 156).trim();
-            metaDescription = Handlebars.Utils.escapeExpression(metaDescription);
-            metaDescription = htmlSafe(`${metaDescription}&hellip;`);
+    get twitterImage() {
+        return this.args.tag.twitterImage || this.args.tag.featureImage;
+    }
+
+    get facebookTitle() {
+        return this.args.tag.ogTitle || this.seoTitle;
+    }
+
+    get facebookDescription() {
+        return this.args.tag.facebookDescription || this.seoDescription || this.settings.get('metaDescription') || '';
+    }
+
+    get facebookImage() {
+        return this.args.tag.ogImage || this.args.tag.featureImage;
+    }
+
+    get accentColor() {
+        const color = this.args.tag.accentColor;
+        if (color && color[0] === '#') {
+            return color.slice(1);
+        }
+        return color;
+    }
+
+    get accentColorPickerValue() {
+        return this.args.tag.accentColor || '#ffffff';
+    }
+
+    get accentColorBgStyle() {
+        return htmlSafe(`background-color: ${this.accentColorPickerValue}`);
+    }
+
+    @action
+    setTagProperty(property, newValue) {
+        const {tag} = this.args;
+
+        if (newValue) {
+            newValue = newValue.trim();
         }
 
-        return metaDescription;
-    }),
+        tag[property] = newValue;
 
-    actions: {
-        setProperty(property, value) {
-            this.setProperty(property, value);
-        },
-
-        setTwitterImage(image) {
-            this.setProperty('twitterImage', image);
-        },
-
-        clearTwitterImage() {
-            this.setProperty('twitterImage', '');
-        },
-
-        setOgImage(image) {
-            this.setProperty('ogImage', image);
-        },
-
-        clearOgImage() {
-            this.setProperty('ogImage', '');
-        },
-
-        setCoverImage(image) {
-            this.setProperty('featureImage', image);
-        },
-
-        clearCoverImage() {
-            this.setProperty('featureImage', '');
-        },
-
-        validateCanonicalUrl() {
-            let newUrl = this.get('scratchTag.canonicalUrl');
-            let oldUrl = this.get('tag.canonicalUrl');
-            let errMessage = '';
-
-            this.get('tag.errors').remove('canonicalUrl');
-            this.get('tag.hasValidated').removeObject('canonicalUrl');
-
-            if (newUrl === '') {
-                this.setProperty('canonicalUrl', '');
-                return;
+        // Generate slug based on name for new tag when empty
+        if (property === 'name' && !tag.slug && tag.isNew) {
+            let slugValue = slugify(newValue);
+            if (/^#/.test(newValue)) {
+                slugValue = 'hash-' + slugValue;
             }
-
-            if (!newUrl) {
-                newUrl = oldUrl;
-            }
-
-            try {
-                new URL(newUrl);
-                this.setProperty('canonicalUrl', '');
-                run.schedule('afterRender', this, function () {
-                    this.setProperty('canonicalUrl', newUrl);
-                });
-            } catch (err) {
-                errMessage = 'The url should be a valid url';
-                this.get('tag.errors').add('canonicalUrl', errMessage);
-                this.get('tag.hasValidated').pushObject('canonicalUrl');
-            }
+            tag.slug = slugValue;
         }
-    },
 
-    updateAccentColor: action(async function (event) {
+        // clear validation message when typing
+        tag.hasValidated.addObject(property);
+    }
+
+    @action
+    validateTagProperty(property) {
+        return this.args.tag.validate({property});
+    }
+
+    @action
+    validateCanonicalUrl() {
+        const {tag} = this.args;
+
+        let newUrl = tag.canonicalUrl;
+        let [oldUrl] = tag.changedAttributes().canonicalUrl;
+        let errMessage = '';
+
+        tag.errors.remove('canonicalUrl');
+        tag.hasValidated.removeObject('canonicalUrl');
+
+        if (newUrl === '') {
+            tag.canonicalUrl = '';
+            return;
+        }
+
+        if (!newUrl) {
+            newUrl = oldUrl;
+        }
+
+        try {
+            new URL(newUrl);
+            tag.canonicalUrl = '';
+            run.schedule('afterRender', this, function () {
+                tag.canonicalUrl = newUrl;
+            });
+        } catch (err) {
+            errMessage = 'The url should be a valid url';
+            tag.errors.add('canonicalUrl', errMessage);
+            tag.hasValidated.pushObject('canonicalUrl');
+        }
+    }
+
+    @action
+    async updateAccentColor(event) {
+        const {tag} = this.args;
+
         let newColor = event.target.value;
-        const oldColor = this.tag.get('accentColor');
+        const oldColor = tag.accentColor;
 
         // reset errors and validation
-        this.tag.errors.remove('accentColor');
-        this.tag.hasValidated.removeObject('accentColor');
+        tag.errors.remove('accentColor');
+        tag.hasValidated.removeObject('accentColor');
 
         if (newColor === '') {
             if (newColor === oldColor) {
@@ -175,7 +187,7 @@ export default Component.extend({
             }
 
             // clear out the accent color
-            this.tag.set('accentColor', '');
+            tag.accentColor = '';
             return;
         }
 
@@ -193,16 +205,16 @@ export default Component.extend({
                 return;
             }
 
-            this.tag.set('accentColor', newColor);
-            this.scratchTag.set('accentColor', newColor);
+            tag.accentColor = newColor;
         } else {
-            this.tag.errors.add('accentColor', 'The colour should be in valid hex format');
-            this.tag.hasValidated.pushObject('accentColor');
+            tag.errors.add('accentColor', 'The colour should be in valid hex format');
+            tag.hasValidated.pushObject('accentColor');
         }
-    }),
+    }
 
-    debounceUpdateAccentColor: task(function*(event) {
+    @task
+    *debounceUpdateAccentColorTask(event) {
         yield timeout(10);
         this.updateAccentColor(event);
-    })
-});
+    }
+}
