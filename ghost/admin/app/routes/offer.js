7150a59abc101bd66670dfea664ecadc351da1b3
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-09-09 15:20:07 +0100

    Switched offer unsaved changes modal to new modal pattern
    refs https://github.com/TryGhost/Team/issues/1734
    refs https://github.com/TryGhost/Team/issues/559
    refs https://github.com/TryGhost/Ghost/issues/14101
    
    - switches to newer modal patterns ready for later Ember upgrades

--- ghost/admin/app/routes/offer.js
+++ ghost/admin/app/routes/offer.js
@@ -1,18 +1,13 @@
 import AdminRoute from 'ghost-admin/routes/admin';
+import ConfirmUnsavedChangesModal from '../components/modals/confirm-unsaved-changes';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
 
 export default class OffersRoute extends AdminRoute {
+    @service modals;
     @service router;
 
     _requiresBackgroundRefresh = true;
-
-    constructor() {
-        super(...arguments);
-        this.router.on('routeWillChange', (transition) => {
-            this.showUnsavedChangesModal(transition);
-        });
-    }
 
     model(params) {
         this._requiresBackgroundRefresh = false;
@@ -26,6 +21,7 @@ export default class OffersRoute extends AdminRoute {
 
     setupController(controller, offer) {
         super.setupController(...arguments);
+
         if (this._requiresBackgroundRefresh) {
             // `offer` is passed directly in `<LinkTo>` so it can be a proxy
             // object used by the sparse list requiring the use of .get()
@@ -34,10 +30,45 @@ export default class OffersRoute extends AdminRoute {
     }
 
     deactivate() {
-        super.deactivate(...arguments);
         // clean up newly created records and revert unsaved changes to existing
         this.controller.offer.rollbackAttributes();
         this._requiresBackgroundRefresh = true;
+    }
+
+    @action
+    async willTransition(transition) {
+        if (this.hasConfirmed) {
+            return true;
+        }
+
+        transition.abort();
+
+        // wait for any existing confirm modal to be closed before allowing transition
+        if (this.confirmModal) {
+            return;
+        }
+
+        const shouldLeave = await this.confirmUnsavedChanges();
+
+        if (shouldLeave) {
+            this.controller.model.rollbackAttributes();
+            this.hasConfirmed = true;
+            return transition.retry();
+        }
+    }
+
+    async confirmUnsavedChanges() {
+        if (this.controller.model?.hasDirtyAttributes) {
+            this.confirmModal = this.modals
+                .open(ConfirmUnsavedChangesModal)
+                .finally(() => {
+                    this.confirmModal = null;
+                });
+
+            return this.confirmModal;
+        }
+
+        return true;
     }
 
     @action
@@ -47,20 +78,5 @@ export default class OffersRoute extends AdminRoute {
 
     titleToken() {
         return this.controller.offer.name;
-    }
-
-    showUnsavedChangesModal(transition) {
-        if (transition.from && transition.from.name === this.routeName && transition.targetName) {
-            let {controller} = this;
-
-            // offer.changedAttributes is always true for new offers but number of changed attrs is reliable
-            let isChanged = Object.keys(controller.offer.changedAttributes()).length > 0;
-
-            if (!controller.offer.isDeleted && isChanged) {
-                transition.abort();
-                controller.toggleUnsavedChangesModal(transition);
-                return;
-            }
-        }
     }
 }
