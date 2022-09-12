81b00848a76265c63f1b21b6a946f4e681e820fa
Daniel Lockyer <hi@daniellockyer.com>
2022-09-12 14:37:07 +0100

    Cleaned up auditLog flag
    refs https://github.com/TryGhost/Toolbox/issues/356
    
    - this is now longer needed now Audit log/History is GA

--- ghost/admin/app/services/feature.js
+++ ghost/admin/app/services/feature.js
@@ -56,7 +56,6 @@ export default class FeatureService extends Service {
         nightShift;
 
     // labs flags
-    @feature('auditLog') auditLog;
     @feature('urlCache') urlCache;
     @feature('beforeAfterCard') beforeAfterCard;
     @feature('newsletterPaywall') newsletterPaywall;
