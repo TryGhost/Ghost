0eb9d15d4402f5463aed73e662cbf9753540f2fe
Daniel Lockyer <hi@daniellockyer.com>
2022-09-12 14:37:07 +0100

    Cleaned up auditLog flag
    refs https://github.com/TryGhost/Toolbox/issues/356
    
    - this is now longer needed now Audit log/History is GA

--- ghost/admin/app/routes/settings/history.js
+++ ghost/admin/app/routes/settings/history.js
@@ -1,16 +1,6 @@
 import AdminRoute from 'ghost-admin/routes/admin';
-import {inject as service} from '@ember/service';
 
 export default class HistoryRoute extends AdminRoute {
-    @service feature;
-
-    beforeModel() {
-        super.beforeModel(...arguments);
-        if (!this.feature.auditLog) {
-            return this.transitionTo('home');
-        }
-    }
-
     buildRouteInfoMetadata() {
         return {
             titleToken: 'History log'
