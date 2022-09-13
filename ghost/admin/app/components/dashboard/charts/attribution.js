fd7f53e8ff0584593e70e0bf3922c4184cc78ece
Rishabh <zrishabhgarg@gmail.com>
2022-09-13 14:08:14 +0530

    Added mock UI table for member source data
    refs https://github.com/TryGhost/Team/issues/1891
    
    - uses mock stats data to show UI for which sources are driving member signups/conversions

---
+++ ghost/admin/app/components/dashboard/charts/attribution.js
@@ -0,0 +1,25 @@
+import Component from '@glimmer/component';
+import {action} from '@ember/object';
+import {inject as service} from '@ember/service';
+// import {tracked} from '@glimmer/tracking';
+
+export default class Recents extends Component {
+    @service dashboardStats;
+
+    @action
+    loadData() {
+        this.dashboardStats.loadMemberAttributionStats();
+    }
+
+    get sources() {
+        return this.dashboardStats?.membersAttributionSources90d || [];
+    }
+
+    get areMembersEnabled() {
+        return this.dashboardStats.siteStatus?.membersEnabled;
+    }
+
+    get areNewslettersEnabled() {
+        return this.dashboardStats.siteStatus?.newslettersEnabled;
+    }
+}
