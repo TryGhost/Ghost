dae3a083fa497741846b6df8d03ba67c467668e5
Rishabh <zrishabhgarg@gmail.com>
2022-09-15 17:21:17 +0530

    Wired referral donut chart with mock data
    refs https://github.com/TryGhost/Team/issues/1891
    refs https://github.com/TryGhost/Team/issues/1905
    
    - wires referral sources chart with mock data

--- ghost/admin/app/components/dashboard/charts/attribution.js
+++ ghost/admin/app/components/dashboard/charts/attribution.js
@@ -1,18 +1,73 @@
 import Component from '@glimmer/component';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
-// import {tracked} from '@glimmer/tracking';
+import {tracked} from '@glimmer/tracking';
 
+const CHART_COLORS = [
+    '#853EED',
+    '#CA3FED',
+    '#E993CC',
+    '#EE9696',
+    '#FEC7C0'
+];
 export default class Recents extends Component {
     @service dashboardStats;
+    @tracked chartType = 'free';
 
     @action
     loadData() {
         this.dashboardStats.loadMemberAttributionStats();
     }
 
+    get chartOptions() {
+        return {
+            cutoutPercentage: 60,
+            borderColor: '#555',
+            legend: {
+                display: false,
+                labels: {
+                    color: 'rgb(255, 99, 132)'
+                }
+            }
+        };
+    }
+
+    get chartData() {
+        if (this.chartType === 'free') {
+            const sortedByFree = [...this.sources];
+            sortedByFree.sort((a, b) => {
+                return b.freeSignups - a.freeSignups;
+            });
+            return {
+                labels: sortedByFree.slice(0, 5).map(source => source.source),
+                datasets: [{
+                    label: 'Free Signups',
+                    data: sortedByFree.slice(0, 5).map(source => source.freeSignups),
+                    backgroundColor: CHART_COLORS.slice(0, 5),
+                    borderWidth: 2,
+                    borderColor: '#fff'
+                }]
+            };
+        } else {
+            const sortedByPaid = [...this.sources];
+            sortedByPaid.sort((a, b) => {
+                return b.paidPercentage - a.paidPercentage;
+            });
+            return {
+                labels: sortedByPaid.slice(0, 5).map(source => source.source),
+                datasets: [{
+                    label: 'Paid Conversions',
+                    data: sortedByPaid.slice(0, 5).map(source => source.paidConversions),
+                    backgroundColor: CHART_COLORS.slice(0, 5),
+                    borderWidth: 2,
+                    borderColor: '#fff'
+                }]
+            };
+        }
+    }
+
     get sources() {
-        return this.dashboardStats?.memberSourceAttributionCounts;
+        return this.dashboardStats?.memberSourceAttributionCounts || [];
     }
 
     get areMembersEnabled() {
