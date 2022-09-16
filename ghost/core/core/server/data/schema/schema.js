1581f439e9336b85ef66f03993e2d398cfa6b496
Kevin Ansfield <kevin@ghost.org>
2022-09-16 10:19:05 +0100

    Added `post_revisions` table (#15420)
    no issue
    
    - initially this will perform the same function as `mobiledoc_revisions` but storing `lexical` instead of `mobiledoc`
    - naming is intentionally generic ready for later expansions

--- ghost/core/core/server/data/schema/schema.js
+++ ghost/core/core/server/data/schema/schema.js
@@ -387,6 +387,13 @@ module.exports = {
         created_at_ts: {type: 'bigInteger', nullable: false},
         created_at: {type: 'dateTime', nullable: false}
     },
+    post_revisions: {
+        id: {type: 'string', maxlength: 24, nullable: false, primary: true},
+        post_id: {type: 'string', maxlength: 24, nullable: false, index: true},
+        lexical: {type: 'text', maxlength: 1000000000, fieldtype: 'long', nullable: true},
+        created_at_ts: {type: 'bigInteger', nullable: false},
+        created_at: {type: 'dateTime', nullable: false}
+    },
     members: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
         uuid: {type: 'string', maxlength: 36, nullable: true, unique: true, validations: {isUUID: true}},
