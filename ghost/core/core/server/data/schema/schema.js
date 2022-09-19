507d3495aa039213facaf7b70af8aaf8b7cf0820
Simon Backx <simon@ghost.org>
2022-09-19 11:20:36 +0200

    Added link_redirects and members_link_click_events tables (#15421)
    closes https://github.com/TryGhost/Team/issues/1912
    closes https://github.com/TryGhost/Team/issues/1913

--- ghost/core/core/server/data/schema/schema.js
+++ ghost/core/core/server/data/schema/schema.js
@@ -829,5 +829,19 @@ module.exports = {
         finished_at: {type: 'dateTime', nullable: true},
         created_at: {type: 'dateTime', nullable: false},
         updated_at: {type: 'dateTime', nullable: true}
+    },
+    link_redirects: {
+        id: {type: 'string', maxlength: 24, nullable: false, primary: true},
+        from: {type: 'string', maxlength: 2000, nullable: false},
+        to: {type: 'string', maxlength: 2000, nullable: false},
+        post_id: {type: 'string', maxlength: 24, nullable: true, unique: false, references: 'posts.id', setNullDelete: true},
+        created_at: {type: 'dateTime', nullable: false},
+        updated_at: {type: 'dateTime', nullable: true}
+    },
+    members_link_click_events: {
+        id: {type: 'string', maxlength: 24, nullable: false, primary: true},
+        member_id: {type: 'string', maxlength: 24, nullable: false, references: 'members.id', cascadeDelete: true},
+        link_id: {type: 'string', maxlength: 24, nullable: false, references: 'link_redirects.id', cascadeDelete: true},
+        created_at: {type: 'dateTime', nullable: false}
     }
 };
