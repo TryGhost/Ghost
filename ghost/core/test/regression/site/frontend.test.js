7b009bf1feb41a68b99ef8b55336508ea7fd52d9
Naz <hi@nazavo.com>
2022-09-20 17:47:59 +0800

    Enabled shared caching of 404 error responses
    refs https://github.com/TryGhost/Toolbox/issues/410
    
    - The 'private' value in 'Cache-Control' response header for all errors made it impossible for shared caches (e.g.: Fastly, Cloudflare) to cache 404 responses efficiently.
    - The change substitutes 'max-age=0' which should not effect the browser cache behavior but would allow shared caches to process such requests efficiently.
    - A more loose caching logic only applies to 404 responses from GET requests that are not user-specific (non-authenticated, non-cookie containing requests)

--- ghost/core/test/regression/site/frontend.test.js
+++ ghost/core/test/regression/site/frontend.test.js
@@ -65,7 +65,7 @@ describe('Frontend Routing', function () {
         describe('Error', function () {
             it('should 404 for unknown post with invalid characters', function (done) {
                 request.get('/$pec+acular~/')
-                    .expect('Cache-Control', testUtils.cacheRules.private)
+                    .expect('Cache-Control', testUtils.cacheRules.noCache)
                     .expect(404)
                     .expect(/Page not found/)
                     .end(doEnd(done));
@@ -74,7 +74,7 @@ describe('Frontend Routing', function () {
             it('should 404 for unknown frontend route', function (done) {
                 request.get('/spectacular/marvellous/')
                     .set('Accept', 'application/json')
-                    .expect('Cache-Control', testUtils.cacheRules.private)
+                    .expect('Cache-Control', testUtils.cacheRules.noCache)
                     .expect(404)
                     .expect(/Page not found/)
                     .end(doEnd(done));
@@ -82,7 +82,7 @@ describe('Frontend Routing', function () {
 
             it('should 404 for encoded char not 301 from uncapitalise', function (done) {
                 request.get('/|/')
-                    .expect('Cache-Control', testUtils.cacheRules.private)
+                    .expect('Cache-Control', testUtils.cacheRules.noCache)
                     .expect(404)
                     .expect(/Page not found/)
                     .end(doEnd(done));
@@ -192,7 +192,7 @@ describe('Frontend Routing', function () {
 
                 it('should 404 for non-edit parameter', async function () {
                     await request.get('/static-page-test/notedit/')
-                        .expect('Cache-Control', testUtils.cacheRules.private)
+                        .expect('Cache-Control', testUtils.cacheRules.noCache)
                         .expect(404)
                         .expect(/Page not found/)
                         .expect(assertCorrectFrontendHeaders);
@@ -231,7 +231,7 @@ describe('Frontend Routing', function () {
                 it('should not redirect to editor', function (done) {
                     request.get('/static-page-test/edit/')
                         .expect(404)
-                        .expect('Cache-Control', testUtils.cacheRules.private)
+                        .expect('Cache-Control', testUtils.cacheRules.noCache)
                         .end(doEnd(done));
                 });
             });
@@ -245,7 +245,7 @@ describe('Frontend Routing', function () {
                         // NOTE: only post pages are supported so the router doesn't have a way to distinguish if
                         //       the request was done after AMP 'Page' or 'Post'
                         request.get('/static-page-test/amp/')
-                            .expect('Cache-Control', testUtils.cacheRules.private)
+                            .expect('Cache-Control', testUtils.cacheRules.noCache)
                             .expect(404)
                             .expect(/Post not found/)
                             .end(doEnd(done));
