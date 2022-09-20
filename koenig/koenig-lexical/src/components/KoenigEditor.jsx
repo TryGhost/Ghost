bcf0bdb00892828e562f08feacf7db23f9f3a417
Sanne de Vries <sannedv@protonmail.com>
2022-09-20 14:42:06 +0100

    Fixed tailwind class ordering failing

--- koenig/koenig-lexical/src/components/KoenigEditor.jsx
+++ koenig/koenig-lexical/src/components/KoenigEditor.jsx
@@ -21,7 +21,7 @@ const KoenigEditor = ({
                 contentEditable={
                     <ContentEditable className="kg-prose" />
                 }
-                placeholder={<div className="font-serif text-grey-500 pointer-events-none text-xl absolute top-0 left-0 min-w-full cursor-text">Begin writing your post...</div>}
+                placeholder={<div className="text-grey-500 pointer-events-none absolute top-0 left-0 min-w-full cursor-text font-serif text-xl">Begin writing your post...</div>}
             />
             <OnChangePlugin onChange={_onChange} />
             <HistoryPlugin /> {/* adds undo/redo */}
