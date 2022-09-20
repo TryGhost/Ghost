dae9e0140209bce69bbf9b0d55de8555fd4adb01
Naz <hi@nazavo.com>
2022-09-20 17:47:59 +0800

    Enabled shared caching of 404 error responses
    refs https://github.com/TryGhost/Toolbox/issues/410
    refs https://github.com/TryGhost/Ghost/commit/7b009bf1feb41a68b99ef8b55336508ea7fd52d9
    
    - Back-ported from refed commit
    - The 'private' value in 'Cache-Control' response header for all errors made it impossible for shared caches (e.g.: Fastly, Cloudflare) to cache 404 responses efficiently.
    - The change substitutes 'max-age=0' which should not effect the browser cache behavior but would allow shared caches to process such requests efficiently.
    - A more loose caching logic only applies to 404 responses from GET requests that are not user-specific (non-authenticated, non-cookie containing requests)

--- test/utils/fixtures/cache-rules.js
+++ test/utils/fixtures/cache-rules.js
@@ -4,5 +4,6 @@ module.exports = {
     day: 'public, max-age=' + 86400,
     year: 'public, max-age=' + 31536000,
     yearImmutable: 'public, max-age=' + 31536000 + ', immutable',
-    private: 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'
+    private: 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0',
+    noCache: 'no-cache, max-age=0, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'
 };
