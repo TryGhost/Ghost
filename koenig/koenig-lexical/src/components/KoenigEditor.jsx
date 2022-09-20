328786f5f537de416ad8e0d5b5ec9e25e18fec5d
Sanne de Vries <sannedv@protonmail.com>
2022-09-20 14:01:17 +0100

    Updated Lexical editor placeholder
    Refs https://github.com/TryGhost/Team/issues/1935

--- koenig/koenig-lexical/src/components/KoenigEditor.jsx
+++ koenig/koenig-lexical/src/components/KoenigEditor.jsx
@@ -21,7 +21,7 @@ const KoenigEditor = ({
                 contentEditable={
                     <ContentEditable className="kg-prose" />
                 }
-                placeholder={<div className="pointer-events-none absolute top-0 left-0 min-w-full cursor-text">Enter some text...</div>}
+                placeholder={<div className="font-serif text-grey-500 pointer-events-none text-xl absolute top-0 left-0 min-w-full cursor-text">Begin writing your post...</div>}
             />
             <OnChangePlugin onChange={_onChange} />
             <HistoryPlugin /> {/* adds undo/redo */}
