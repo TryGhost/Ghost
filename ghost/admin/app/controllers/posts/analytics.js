64c2d28e43ecbae1de1f009acf7fcbfe23f3dbce
Simon Backx <simon@ghost.org>
2022-09-21 18:18:01 +0200

    Implemented referrers stats API on posts' analytics page
    refs https://github.com/TryGhost/Team/issues/1921

--- ghost/admin/app/controllers/posts/analytics.js
+++ ghost/admin/app/controllers/posts/analytics.js
@@ -1,34 +1,45 @@
 import Controller from '@ember/controller';
+import {action} from '@ember/object';
+import {inject as service} from '@ember/service';
+import {task} from 'ember-concurrency';
+import {tracked} from '@glimmer/tracking';
 
 /**
  * @typedef {import('../../services/dashboard-stats').SourceAttributionCount} SourceAttributionCount
 */
 
 export default class AnalyticsController extends Controller {
+    @service ajax;
+    @service ghostPaths;
+
+    @tracked sources = null;
+
     get post() {
         return this.model;
     }
 
-    /**
-     * @returns {SourceAttributionCount[]} - array of objects with source and count properties
-     */
-    get sources() {
-        return [
-            {
-                source: 'Twitter',
-                freeSignups: 12,
-                paidConversions: 50
-            },
-            {
-                source: 'Google',
-                freeSignups: 9,
-                paidConversions: 32
-            },
-            {
-                source: 'Direct',
-                freeSignups: 2,
-                paidConversions: 40
-            }
-        ];
+    @action
+    loadData() {
+        this.fetchReferrersStats();
+    }
+
+    async fetchReferrersStats() {
+        if (this._fetchReferrersStats.isRunning) {
+            return this._fetchReferrersStats.last;
+        }
+        return this._fetchReferrersStats.perform();
+    }
+
+    @task
+    *_fetchReferrersStats() {
+        let statsUrl = this.ghostPaths.url.api(`stats/referrers/posts/${this.post.id}`);
+        let result = yield this.ajax.request(statsUrl);
+        this.sources = result.stats.map((stat) => {
+            return {
+                source: stat.source ?? 'Direct',
+                signups: stat.signups,
+                paidConversions: stat.paid_conversions
+            };
+        });
     }
 }
