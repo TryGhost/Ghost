7cb3de604c20a304f66cfb4e41af1a919e58ddf8
Rishabh <zrishabhgarg@gmail.com>
2022-09-23 20:49:51 +0530

    Added referrer attribution data to member api
    refs https://github.com/TryGhost/Team/issues/1961
    
    - includes referrer source and medium information in member api

--- ghost/member-attribution/lib/service.js
+++ ghost/member-attribution/lib/service.js
@@ -69,7 +69,10 @@ class MemberAttributionService {
         const _attribution = this.attributionBuilder.build({
             id: eventModel.get('attribution_id'),
             url: eventModel.get('attribution_url'),
-            type: eventModel.get('attribution_type')
+            type: eventModel.get('attribution_type'),
+            refSource: eventModel.get('referrer_source'),
+            refMedium: eventModel.get('referrer_medium'),
+            refUrl: eventModel.get('referrer_url')
         });
 
         if (_attribution.type !== 'url') {
@@ -102,7 +105,10 @@ class MemberAttributionService {
         const attribution = this.attributionBuilder.build({
             id: memberCreatedEvent.get('attribution_id'),
             url: memberCreatedEvent.get('attribution_url'),
-            type: memberCreatedEvent.get('attribution_type')
+            type: memberCreatedEvent.get('attribution_type'),
+            refSource: memberCreatedEvent.get('referrer_source'),
+            refMedium: memberCreatedEvent.get('referrer_medium'),
+            refUrl: memberCreatedEvent.get('referrer_url')
         });
         return await attribution.fetchResource();
     }
@@ -120,7 +126,10 @@ class MemberAttributionService {
         const attribution = this.attributionBuilder.build({
             id: subscriptionCreatedEvent.get('attribution_id'),
             url: subscriptionCreatedEvent.get('attribution_url'),
-            type: subscriptionCreatedEvent.get('attribution_type')
+            type: subscriptionCreatedEvent.get('attribution_type'),
+            refSource: subscriptionCreatedEvent.get('referrer_source'),
+            refMedium: subscriptionCreatedEvent.get('referrer_medium'),
+            refUrl: subscriptionCreatedEvent.get('referrer_url')
         });
         return await attribution.fetchResource();
     }
