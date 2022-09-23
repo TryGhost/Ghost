2aef68bfcecdc88d13b7853278ac7bec5752ac89
Rishabh <zrishabhgarg@gmail.com>
2022-09-23 20:50:11 +0530

    Wired referrer information on member detail page
    refs https://github.com/TryGhost/Ghost/pull/15463

--- ghost/admin/app/components/gh-member-details.js
+++ ghost/admin/app/components/gh-member-details.js
@@ -3,4 +3,12 @@ import {inject as service} from '@ember/service';
 
 export default class extends Component {
     @service settings;
+
+    get referrerAttribution() {
+        const attribution = this.args.member?.attribution;
+        return {
+            source: attribution?.referrer_source || 'Unknown',
+            medium: attribution?.referrer_medium || '-'
+        };
+    }
 }
