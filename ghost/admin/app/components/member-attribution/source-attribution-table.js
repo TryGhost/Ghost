37801f0c09e399de297623da8720e911251fe50c
Rishabh <zrishabhgarg@gmail.com>
2022-09-25 00:15:12 +0530

    Added modal for showing all attribution sources
    refs https://github.com/TryGhost/Team/issues/1941
    
    - limits the source attribution list to top 5 on dashboard, and shows all sources on clicking on `Other` link
    
    Fixed lint

--- ghost/admin/app/components/member-attribution/source-attribution-table.js
+++ ghost/admin/app/components/member-attribution/source-attribution-table.js
@@ -1,20 +1,51 @@
+import AllSourcesModal from './modals/all-sources';
 import Component from '@glimmer/component';
+import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
 
 export default class SourceAttributionTable extends Component {
     @service membersUtils;
+    @service modals;
+
+    @action
+    openAllSources() {
+        this.modals.open(AllSourcesModal, {
+            sources: this.args.sources
+        });
+    }
+
+    get others() {
+        const availableSources = this.args.sources.filter(source => source.source);
+        const unavailableSource = this.args.sources.find(sourceData => !sourceData.source);
+        if (!availableSources.length && !unavailableSource) {
+            return null;
+        }
+
+        return availableSources.slice(5).reduce((acc, source) => {
+            return {
+                signups: acc.signups + source.signups,
+                paidConversions: acc.paidConversions + source.paidConversions
+            };
+        }, {
+            signups: unavailableSource?.signups ?? 0,
+            paidConversions: unavailableSource?.paidConversions ?? 0
+        });
+    }
 
     get sources() {
-        const availableSources = this.args.sources.filter(source => source.source);
-        const unavailableSources = this.args.sources.filter(sourceData => !sourceData.source).map((sourceData) => {
-            return {
-                ...sourceData,
-                source: 'Unavailable'
-            };
-        });
-        return [
-            ...availableSources,
-            ...unavailableSources
-        ];
+        return this.args.sources.filter(source => source.source).slice(0, 5);
+        // const availableSources = this.args.sources.filter(source => source.source);
+        // return availableSources.slice(0, 5);
+
+        // const unavailableSources = this.args.sources.filter(sourceData => !sourceData.source).map((sourceData) => {
+        //     return {
+        //         ...sourceData,
+        //         source: 'Unavailable'
+        //     };
+        // });
+        // return [
+        //     ...availableSources,
+        //     ...unavailableSources
+        // ];
     }
 }
--- ghost/admin/app/services/dashboard-stats.js
+++ ghost/admin/app/services/dashboard-stats.js
@@ -246,13 +246,14 @@ export default class DashboardStatsService extends Service {
             }
             return stat.date >= moment().add(-this.chartDays, 'days').format('YYYY-MM-DD');
         }).reduce((acc, stat) => {
-            const existingSource = acc.find(s => s.source === stat.source);
+            const statSource = stat.source ?? '';
+            const existingSource = acc.find(s => s.source === statSource);
             if (existingSource) {
                 existingSource.signups += stat.signups || 0;
                 existingSource.paidConversions += stat.paidConversions || 0;
             } else {
                 acc.push({
-                    source: stat.source,
+                    source: statSource,
                     signups: stat.signups || 0,
                     paidConversions: stat.paidConversions || 0
                 });
