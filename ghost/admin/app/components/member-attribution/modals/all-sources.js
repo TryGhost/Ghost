5f4b7de1ccf9b0711734b0ea1d9ae420fbe0c948
Rishabh Garg <zrishabhgarg@gmail.com>
2022-09-26 23:29:30 +0530

    Added sorting for attribution data on dashboard (#15474)
    refs https://github.com/TryGhost/Team/issues/1941
    
    - sorts attribution table and chart on signups or paid conversions

--- ghost/admin/app/components/member-attribution/modals/all-sources.js
+++ ghost/admin/app/components/member-attribution/modals/all-sources.js
@@ -3,18 +3,4 @@ import {inject as service} from '@ember/service';
 
 export default class FullAttributionTable extends Component {
     @service membersUtils;
-
-    get sources() {
-        const availableSources = this.args.data.sources.filter(source => source.source);
-        const unavailableSources = this.args.data.sources.filter(sourceData => !sourceData.source).map((sourceData) => {
-            return {
-                ...sourceData,
-                source: 'Unavailable'
-            };
-        });
-        return [
-            ...availableSources,
-            ...unavailableSources
-        ];
-    }
 }
