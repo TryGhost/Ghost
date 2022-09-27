8985fb8cf1764f33649b2fd3f2051f9219e3ebe0
Simon Backx <simon@ghost.org>
2022-09-27 19:03:57 +0200

    fixup! Added migration to set email_track_clicks based on current email_track_opens value

--- ghost/core/core/server/data/migrations/versions/5.17/2022-09-27-16-49-set-track-clicks-based-on-opens.js
+++ ghost/core/core/server/data/migrations/versions/5.17/2022-09-27-16-49-set-track-clicks-based-on-opens.js
@@ -8,18 +8,22 @@ module.exports = createTransactionalMigration(
             .where('key', '=', 'email_track_opens')
             .first();
 
-        if (!reuseValueOfSetting.value) {
+        if (!reuseValueOfSetting) {
             logging.warn(`Skipped setting email_track_clicks to current value of email_track_opens - email_track_opens not found`);
             return;
         }
 
-        logging.info(`Setting email_track_clicks to ${reuseValueOfSetting.value} (current email_track_opens value)`);
-
-        await connection('settings')
+        const affectedRows = await connection('settings')
             .update({
                 value: reuseValueOfSetting.value
             })
             .where('key', '=', 'email_track_clicks');
+
+        if (affectedRows === 1) {
+            logging.info(`Set email_track_clicks to ${reuseValueOfSetting.value} (current email_track_opens value)`);
+        } else {
+            logging.warn(`Tried setting email_track_clicks to ${reuseValueOfSetting.value} — ${affectedRows} changes`);
+        }
     },
     async function down() {
         // no-op: we don't need to change it back
