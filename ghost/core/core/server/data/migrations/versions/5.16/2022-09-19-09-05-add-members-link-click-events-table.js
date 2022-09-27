6385a0309708120c40dfbca439290bc01e8cef27
Fabien "egg" O'Carroll <fabien@allou.is>
2022-09-27 15:01:45 +0100

    Simplified link tracking related tables naming
    - Removes superfluous "link" from table names
    - Fixes type definititon of dropTables util
    - Updates & renames models
    - Noop existing migrations to avoid unnecessary work

--- ghost/core/core/server/data/migrations/versions/5.16/2022-09-19-09-05-add-members-link-click-events-table.js
+++ ghost/core/core/server/data/migrations/versions/5.16/2022-09-19-09-05-add-members-link-click-events-table.js
@@ -1,8 +1,4 @@
-const {addTable} = require('../../utils');
-
-module.exports = addTable('members_link_click_events', {
-    id: {type: 'string', maxlength: 24, nullable: false, primary: true},
-    member_id: {type: 'string', maxlength: 24, nullable: false, references: 'members.id', cascadeDelete: true},
-    link_id: {type: 'string', maxlength: 24, nullable: false, references: 'link_redirects.id', cascadeDelete: true},
-    created_at: {type: 'dateTime', nullable: false}
-});
+module.exports = {
+    async up() {},
+    async down() {}
+};
---
+++ ghost/core/core/server/data/migrations/versions/5.17/2022-09-27-13-53-remove-click-tracking-tables.js
@@ -0,0 +1,3 @@
+const {dropTables} = require('../../utils');
+
+module.exports = dropTables(['link_redirects', 'members_link_click_events']);
