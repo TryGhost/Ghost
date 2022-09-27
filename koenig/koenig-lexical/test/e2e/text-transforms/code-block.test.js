eedd772c81a3479fd0489f9443c09a50d734085d
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-09-27 13:11:13 +0200

    Fixed code-block test no issue
    - updated inline style height set by the `autoExpandTextArea` utility hook.

--- koenig/koenig-lexical/test/e2e/text-transforms/code-block.test.js
+++ koenig/koenig-lexical/test/e2e/text-transforms/code-block.test.js
@@ -21,6 +21,6 @@ describe('Renders code block node', async () => {
         await focusEditor(page);
         await page.keyboard.type('```javascript ');
         await assertHTML(page, html`
-        <div data-lexical-decorator="true" contenteditable="false"><div class="caret-grey-800 hover:shadow-green relative hover:shadow-[0_0_0_1px]" data-kg-card="true"><code><textarea autocorrect="off" autocapitalize="off" spellcheck="false" tabindex="0" class="bg-grey-50 min-h-170 w-full p-3" style="height: 52px;">javascript</textarea></code></div></div>`);
+        <div data-lexical-decorator="true" contenteditable="false"><div class="caret-grey-800 hover:shadow-green relative hover:shadow-[0_0_0_1px]" data-kg-card="true"><code><textarea autocorrect="off" autocapitalize="off" spellcheck="false" tabindex="0" class="bg-grey-50 min-h-170 w-full p-3" style="height: 28px;">javascript</textarea></code></div></div>`);
     });
 });
