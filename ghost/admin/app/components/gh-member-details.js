1ac68e7d0893a9dcefe9b4a47f4a95ae0f259783
Rishabh <zrishabhgarg@gmail.com>
2022-09-27 21:59:29 +0530

    Refined attribution data on member details screen
    refs https://github.com/TryGhost/Team/issues/1986
    
    - hides attribution section if attribution data is null
    - hides referrer section if referrer source is empty

--- ghost/admin/app/components/gh-member-details.js
+++ ghost/admin/app/components/gh-member-details.js
@@ -4,11 +4,7 @@ import {inject as service} from '@ember/service';
 export default class extends Component {
     @service settings;
 
-    get referrerAttribution() {
-        const attribution = this.args.member?.attribution;
-        return {
-            source: attribution?.referrer_source || 'Unknown',
-            medium: attribution?.referrer_medium || '-'
-        };
+    get referrerSource() {
+        return this.args.member.get('attribution')?.referrer_source;
     }
 }
