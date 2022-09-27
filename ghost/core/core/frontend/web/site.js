6041d0e23a9c794bb0334d54a241d2bbb15735f0
Naz <hi@nazavo.com>
2022-09-27 14:30:13 +0800

    Made members JWKS endpoint caching configurable
    refs https://github.com/TryGhost/Toolbox/issues/411
    refs https://github.com/TryGhost/Ghost/commit/d1104b23cef287cb19de47ebbb193c8c3167721e
    
    - Having hardcoded cache control values in the codebase makes it impossible to experiment with new values without a version release.
    - Having all values configurable by default will allow for easier caching experiments and customizations on self-hosting instances.
    - This change only changes the members endpoint caching configurability. The other JWKS endpoint will be modified separately (following commit), to keep changes concise

--- ghost/core/core/frontend/web/site.js
+++ ghost/core/core/frontend/web/site.js
@@ -94,7 +94,7 @@ module.exports = function setupSiteApp(routerConfig) {
     // /member/.well-known/* serves files (e.g. jwks.json) so it needs to be mounted before the prettyUrl mw to avoid trailing slashes
     siteApp.use(
         '/members/.well-known',
-        shared.middleware.cacheControl('public', {maxAge: constants.ONE_DAY_S}),
+        shared.middleware.cacheControl('public', {maxAge: config.get('caching:wellKnown:maxAge')}),
         (req, res, next) => membersService.api.middleware.wellKnown(req, res, next)
     );
 
