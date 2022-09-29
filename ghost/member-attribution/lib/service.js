fa081fe613a496869352b6e47158925c3e32d465
Rishabh Garg <zrishabhgarg@gmail.com>
2022-09-29 22:31:48 +0530

    Added referrer attribution from request context (#15499)
    closes TryGhost/Team#2007
    
    - uses request context to add referrer source and medium for a new member
    - uses integration name as referrer medium if exists

--- ghost/member-attribution/lib/service.js
+++ ghost/member-attribution/lib/service.js
@@ -16,6 +16,54 @@ class MemberAttributionService {
 
     /**
      *
+     * @param {Object} context instance of ghost framework context object
+     * @returns {Promise<import('./attribution').AttributionResource|null>}
+     */
+    async getAttributionFromContext(context) {
+        if (!context) {
+            return null;
+        }
+
+        const source = this._resolveContextSource(context);
+
+        // We consider only select internal context sources
+        if (['import', 'api', 'admin'].includes(source)) {
+            let attribution = {
+                id: null,
+                type: null,
+                url: null,
+                title: null,
+                referrerUrl: null,
+                referrerSource: null,
+                referrerMedium: null
+            };
+            if (source === 'import') {
+                attribution.referrerSource = 'Imported';
+                attribution.referrerMedium = 'Member Importer';
+            } else if (source === 'admin') {
+                attribution.referrerSource = 'Created manually';
+                attribution.referrerMedium = 'Ghost Admin';
+            } else if (source === 'api') {
+                attribution.referrerSource = 'Created via API';
+                attribution.referrerMedium = 'Admin API';
+            }
+
+            // If context has integration, set referrer medium as integration anme
+            if (context?.integration?.id) {
+                try {
+                    const integration = await this.models.Integration.findOne({id: context.integration.id});
+                    attribution.referrerSource = `Integration: ${integration?.get('name')}`;
+                } catch (error) {
+                    // ignore error for integration not found
+                }
+            }
+            return attribution;
+        }
+        return null;
+    }
+
+    /**
+     *
      * @param {import('./history').UrlHistoryArray} historyArray
      * @returns {Promise<import('./attribution').Attribution>}
      */
@@ -63,10 +111,6 @@ class MemberAttributionService {
      * @returns {import('./attribution').AttributionResource|null}
      */
     getEventAttribution(eventModel) {
-        if (eventModel.get('attribution_type') === null) {
-            return null;
-        }
-
         const _attribution = this.attributionBuilder.build({
             id: eventModel.get('attribution_id'),
             url: eventModel.get('attribution_url'),
@@ -76,7 +120,7 @@ class MemberAttributionService {
             referrerUrl: eventModel.get('referrer_url')
         });
 
-        if (_attribution.type !== 'url') {
+        if (_attribution.type && _attribution.type !== 'url') {
             // Find the right relation to use to fetch the resource
             const tryRelations = [
                 eventModel.related('postAttribution'),
@@ -100,7 +144,7 @@ class MemberAttributionService {
      */
     async getMemberCreatedAttribution(memberId) {
         const memberCreatedEvent = await this.models.MemberCreatedEvent.findOne({member_id: memberId}, {require: false, withRelated: []});
-        if (!memberCreatedEvent || !memberCreatedEvent.get('attribution_type')) {
+        if (!memberCreatedEvent) {
             return null;
         }
         const attribution = this.attributionBuilder.build({
@@ -121,7 +165,7 @@ class MemberAttributionService {
      */
     async getSubscriptionCreatedAttribution(subscriptionId) {
         const subscriptionCreatedEvent = await this.models.SubscriptionCreatedEvent.findOne({subscription_id: subscriptionId}, {require: false, withRelated: []});
-        if (!subscriptionCreatedEvent || !subscriptionCreatedEvent.get('attribution_type')) {
+        if (!subscriptionCreatedEvent) {
             return null;
         }
         const attribution = this.attributionBuilder.build({
@@ -133,6 +177,30 @@ class MemberAttributionService {
             referrerUrl: subscriptionCreatedEvent.get('referrer_url')
         });
         return await attribution.fetchResource();
+    }
+
+    /**
+     * Maps the framework context to source string
+     * @param {Object} context instance of ghost framework context object
+     * @returns {'import' | 'system' | 'api' | 'admin' | 'member'}
+     * @private
+     */
+    _resolveContextSource(context) {
+        let source;
+
+        if (context.import || context.importer) {
+            source = 'import';
+        } else if (context.internal) {
+            source = 'system';
+        } else if (context.api_key) {
+            source = 'api';
+        } else if (context.user) {
+            source = 'admin';
+        } else {
+            source = 'member';
+        }
+
+        return source;
     }
 }
 
