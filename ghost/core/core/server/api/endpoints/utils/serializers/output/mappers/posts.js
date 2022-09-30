fcab3c25c65b10009483025af80b5e7908fea91a
Fabien "egg" O'Carroll <fabien@allou.is>
2022-09-30 09:13:29 +0100

    fixup! Used the higher of click count and open count for email open count

--- ghost/core/core/server/api/endpoints/utils/serializers/output/mappers/posts.js
+++ ghost/core/core/server/api/endpoints/utils/serializers/output/mappers/posts.js
@@ -114,8 +114,8 @@ module.exports = async (model, frame, options = {}) => {
         if (jsonModel.email && jsonModel.count) {
             jsonModel.email.opened_count = Math.min(
                 Math.max(
-                    jsonModel.email.opened_count,
-                    jsonModel.count.clicks
+                    jsonModel.email.opened_count || 0,
+                    jsonModel.count.clicks || 0
                 ),
                 jsonModel.email.email_count
             );
