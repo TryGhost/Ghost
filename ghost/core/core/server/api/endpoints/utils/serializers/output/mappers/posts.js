906ee1a421e6820f3e079a3e2e541c6ad550f1b5
Fabien 'egg' O'Carroll <fabien@allou.is>
2022-10-03 09:29:58 +0100

    Used the higher of click count and open count for email open count (#15508)
    fixes https://github.com/TryGhost/Team/issues/2017
    
    We process clicks much faster than we process Mailgun events which can result in a higher click rater than open rate shown on the dashboard. This ensures that the open rate will never be lower than the click rate. This is a stopgap solution until we can get click events updating the opened_at time for email_recipients

--- ghost/core/core/server/api/endpoints/utils/serializers/output/mappers/posts.js
+++ ghost/core/core/server/api/endpoints/utils/serializers/output/mappers/posts.js
@@ -110,6 +110,18 @@ module.exports = async (model, frame, options = {}) => {
         });
     }
 
+    if (labs.isSet('emailClicks')) {
+        if (jsonModel.email && jsonModel.count) {
+            jsonModel.email.opened_count = Math.min(
+                Math.max(
+                    jsonModel.email.opened_count || 0,
+                    jsonModel.count.clicks || 0
+                ),
+                jsonModel.email.email_count
+            );
+        }
+    }
+
     if (!labs.isSet('memberAttribution') && !labs.isSet('emailClicks')) {
         delete jsonModel.count;
     }
