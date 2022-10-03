258f56ded9297f45a2299588223cab15f75516ac
Kevin Ansfield <kevin@ghost.org>
2022-10-03 12:05:58 +0100

    Added regression test for publishing as editor when over member limit (#15518)
    closes https://github.com/TryGhost/Team/issues/2010
    refs https://github.com/TryGhost/Ghost/commit/a67cb265fc3ab56e1b34b4a035981d3ba4d4e0b3
    
    - wrapped all members endpoints in a permission check that returns a 403 response if the logged in member is not an admin
    - added a publish-flow acceptance test that goes through the flow as an Editor user that fails if the stats endpoint is hit and throws a permissions error
    - removed some unnecessary waits in members acceptance test that were added for earlier versions of Ember

--- ghost/admin/tests/helpers/login-as-role.js
+++ ghost/admin/tests/helpers/login-as-role.js
@@ -1,6 +1,6 @@
 import {authenticateSession, invalidateSession} from 'ember-simple-auth/test-support';
 
-export default async function loginRole(roleName, server) {
+export default async function loginAsRole(roleName, server) {
     const role = server.create('role', {name: roleName});
     const user = server.create('user', {roles: [role], slug: 'test-user'});
     await invalidateSession();
