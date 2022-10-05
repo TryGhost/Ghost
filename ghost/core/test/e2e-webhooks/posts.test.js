a499f866f325163254056e2feffe361151ff87d8
Simon Backx <simon@ghost.org>
2022-10-05 14:25:00 +0200

    Prevented posts webhook tests from breaking on every release

--- ghost/core/test/e2e-webhooks/posts.test.js
+++ ghost/core/test/e2e-webhooks/posts.test.js
@@ -1,11 +1,13 @@
 const {agentProvider, mockManager, fixtureManager, matchers} = require('../utils/e2e-framework');
-const {anyObjectId, anyISODateTime, anyUuid, anyContentVersion, anyNumber, anyLocalURL} = matchers;
+const {stringMatching, anyObjectId, anyISODateTime, anyUuid, anyContentVersion, anyNumber, anyLocalURL} = matchers;
 
 const tierSnapshot = {
     id: anyObjectId,
     created_at: anyISODateTime,
     updated_at: anyISODateTime
 };
+
+const anyGhostAgent = stringMatching(/Ghost\/\d+\.\d+\.\d+\s\(https:\/\/github.com\/TryGhost\/Ghost\)/);
 
 const buildAuthorSnapshot = (roles = false) => {
     const authorSnapshot = {
@@ -101,7 +103,8 @@ describe('post.* events', function () {
         webhookMockReceiver
             .matchHeaderSnapshot({
                 'content-version': anyContentVersion,
-                'content-length': anyNumber
+                'content-length': anyNumber,
+                'user-agent': anyGhostAgent
             })
             .matchBodySnapshot({
                 post: {
@@ -140,7 +143,8 @@ describe('post.* events', function () {
         webhookMockReceiver
             .matchHeaderSnapshot({
                 'content-version': anyContentVersion,
-                'content-length': anyNumber
+                'content-length': anyNumber,
+                'user-agent': anyGhostAgent
             })
             .matchBodySnapshot({
                 post: {
