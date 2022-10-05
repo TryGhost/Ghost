1f463059644dcc66b59f3694a79876c2952d1720
Sanne de Vries <sannedv@protonmail.com>
2022-10-05 11:46:18 +0700

    Fixed linting errors

--- koenig/koenig-lexical/src/stories/Button.stories.jsx
+++ koenig/koenig-lexical/src/stories/Button.stories.jsx
@@ -1,40 +1,40 @@
 import React from 'react';
 
-import { Button } from './Button';
+import {Button} from './Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
-  component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+    title: 'Example/Button',
+    component: Button,
+    // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+    argTypes: {
+        backgroundColor: {control: 'color'}
+    }
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => <Button {...args} />;
+const Template = args => <Button {...args} />;
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Button',
+    primary: true,
+    label: 'Button'
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+    label: 'Button'
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
+    size: 'large',
+    label: 'Button'
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+    size: 'small',
+    label: 'Button'
 };
