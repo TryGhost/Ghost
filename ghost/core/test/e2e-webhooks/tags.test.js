eaa9727c364ddc9381fe7e5033613451c8bfeb62
Shubhadeep Das <dshubhadeep@gmail.com>
2022-10-07 14:20:16 +0530

    Added e2e test for `tag.deleted` webhook (#15553)
    refs https://github.com/TryGhost/Ghost/issues/15537
    
    - this adds an e2e test and test snapshot for the `tag.deleted` webhook so we can prevent regressions and bugs in the future

--- ghost/core/test/e2e-webhooks/tags.test.js
+++ ghost/core/test/e2e-webhooks/tags.test.js
@@ -5,8 +5,7 @@ const tagSnapshot = {
     created_at: anyISODateTime,
     description: anyString,
     id: anyObjectId,
-    updated_at: anyISODateTime,
-    url: anyLocalURL
+    updated_at: anyISODateTime
 };
 
 describe('tag.* events', function () {
@@ -56,7 +55,48 @@ describe('tag.* events', function () {
             })
             .matchBodySnapshot({
                 tag: {
-                    current: tagSnapshot
+                    current: {...tagSnapshot, url: anyLocalURL}
+                }
+            });
+    });
+
+    it('tag.deleted event is triggered', async function () {
+        const webhookURL = 'https://test-webhook-receiver.com/tag-deleted/';
+        await webhookMockReceiver.mock(webhookURL);
+        await fixtureManager.insertWebhook({
+            event: 'tag.deleted',
+            url: webhookURL
+        });
+
+        const res = await adminAPIAgent
+            .post('tags/')
+            .body({
+                tags: [{
+                    name: 'Test Tag 2',
+                    slug: 'test-tag-2',
+                    description: 'Test description'
+                }]
+            })
+            .expectStatus(201);
+
+        const id = res.body.tags[0].id;
+
+        await adminAPIAgent
+            .delete('tags/' + id)
+            .expectStatus(204);
+
+        await webhookMockReceiver.receivedRequest();
+
+        webhookMockReceiver
+            .matchHeaderSnapshot({
+                'content-version': anyContentVersion,
+                'content-length': anyNumber,
+                'user-agent': anyGhostAgent
+            })
+            .matchBodySnapshot({
+                tag: {
+                    current: {},
+                    previous: tagSnapshot
                 }
             });
     });
