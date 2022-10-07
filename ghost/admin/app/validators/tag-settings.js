3a0c31fa3f18a4eb4d00fd74f93333db20ed0cb4
Kevin Ansfield <kevin@ghost.org>
2022-10-07 20:13:42 +0100

    Updated ValidationEngine to support bare native class models (#15567)
    no issue
    
    - if a plain native class instance with tracked properties is validated against the `ValidationEngine` and it's associated validators would cause errors by assuming that the instance has a `.get()` method
    - updated all model access in `ValidationEngine` and the validators to use direct property access which works for both native class and `EmberObject` instances

--- ghost/admin/app/validators/tag-settings.js
+++ ghost/admin/app/validators/tag-settings.js
@@ -6,52 +6,52 @@ export default BaseValidator.create({
     properties: ['name', 'slug', 'description', 'metaTitle', 'metaDescription'],
 
     name(model) {
-        let name = model.get('name');
+        let name = model.name;
 
         if (isBlank(name)) {
-            model.get('errors').add('name', 'You must specify a name for the tag.');
+            model.errors.add('name', 'You must specify a name for the tag.');
             this.invalidate();
         } else if (name.match(/^,/)) {
-            model.get('errors').add('name', 'Tag names can\'t start with commas.');
+            model.errors.add('name', 'Tag names can\'t start with commas.');
             this.invalidate();
         } else if (!validator.isLength(name, 0, 191)) {
-            model.get('errors').add('name', 'Tag names cannot be longer than 191 characters.');
+            model.errors.add('name', 'Tag names cannot be longer than 191 characters.');
             this.invalidate();
         }
     },
 
     slug(model) {
-        let slug = model.get('slug');
+        let slug = model.slug;
 
         if (!validator.isLength(slug || '', 0, 191)) {
-            model.get('errors').add('slug', 'URL cannot be longer than 191 characters.');
+            model.errors.add('slug', 'URL cannot be longer than 191 characters.');
             this.invalidate();
         }
     },
 
     description(model) {
-        let description = model.get('description');
+        let description = model.description;
 
         if (!validator.isLength(description || '', 0, 500)) {
-            model.get('errors').add('description', 'Description cannot be longer than 500 characters.');
+            model.errors.add('description', 'Description cannot be longer than 500 characters.');
             this.invalidate();
         }
     },
 
     metaTitle(model) {
-        let metaTitle = model.get('metaTitle');
+        let metaTitle = model.metaTitle;
 
         if (!validator.isLength(metaTitle || '', 0, 300)) {
-            model.get('errors').add('metaTitle', 'Meta Title cannot be longer than 300 characters.');
+            model.errors.add('metaTitle', 'Meta Title cannot be longer than 300 characters.');
             this.invalidate();
         }
     },
 
     metaDescription(model) {
-        let metaDescription = model.get('metaDescription');
+        let metaDescription = model.metaDescription;
 
         if (!validator.isLength(metaDescription || '', 0, 500)) {
-            model.get('errors').add('metaDescription', 'Meta Description cannot be longer than 500 characters.');
+            model.errors.add('metaDescription', 'Meta Description cannot be longer than 500 characters.');
             this.invalidate();
         }
     }
