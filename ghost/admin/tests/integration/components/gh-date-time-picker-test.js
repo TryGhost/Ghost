1278dd56819794b4342cf3fb96a659980fc22826
Simon Backx <simon@ghost.org>
2022-10-07 11:56:14 +0200

    Added extra timezone tests

--- ghost/admin/tests/integration/components/gh-date-time-picker-test.js
+++ ghost/admin/tests/integration/components/gh-date-time-picker-test.js
@@ -8,12 +8,15 @@ import {describe, it} from 'mocha';
 import {expect} from 'chai';
 import {setupRenderingTest} from 'ember-mocha';
 
+let timezone = 'UTC';
 class SettingsStub extends Service {
-    timezone = 'UTC';
+    get timezone() {
+        return timezone;
+    }
 
     get(key) {
         if (key === 'timezone') {
-            return this.timezone;
+            return timezone;
         }
     }
 }
@@ -23,6 +26,7 @@ describe('Integration: Component: gh-date-time-picker', function () {
     let clock;
 
     beforeEach(async function () {
+        timezone = 'UTC';
         this.owner.register('service:settings', SettingsStub);
     });
 
@@ -86,6 +90,32 @@ describe('Integration: Component: gh-date-time-picker', function () {
         await blur('[data-test-date-time-picker-date-input]');
     });
 
+    it('can update date via date input with +12 timezone', async function () {
+        timezone = 'Pacific/Kwajalein'; // +12
+
+        // Current date/time doesn't really matter
+        this.set('date','2022-02-22');
+        this.set('time', '22:22');
+
+        this.set('updateDate', (newDate) => {
+            // Note: the newDate should be 2022-02-28 in the current blog timezone, this is not the same timezone as the user timezone
+            // Blog timezone is +12
+            expect(moment.utc(newDate).toISOString()).to.equal('2022-02-27T12:00:00.000Z');
+            this.set('date', newDate);
+        });
+
+        this.set('updateTime', (newTime) => {
+            expect(newTime).to.equal('22:22');
+            this.set('time', newTime);
+        });
+
+        await render(hbs`<GhDateTimePicker @date={{this.date}} @time={{this.time}} @setDate={{this.updateDate}} @setTime={{this.updateTime}} />`);
+        
+        // We enter 2022-02-28. In UTC this is 2022-02-28 00:00:00.000Z. But we should call updateDate to be 2022-02-28 in the blog timezone
+        await fillIn('[data-test-date-time-picker-date-input]', '2022-02-28');
+        await blur('[data-test-date-time-picker-date-input]');
+    });
+
     it('can update time via time input', async function () {
         this.set('date', '2022-02-22');
         this.set('time', '22:22');
@@ -109,7 +139,27 @@ describe('Integration: Component: gh-date-time-picker', function () {
         this.set('time', '12:00');
 
         this.set('updateDate', (newDate) => {
-            expect(moment.utc(newDate).toISOString()).to.equal('2022-02-27T00:00:00.000Z', newDate);
+            expect(moment.utc(newDate).toISOString()).to.equal('2022-02-27T00:00:00.000Z');
+            this.set('date', newDate);
+        });
+        this.set('updateTime', (newTime) => {
+            expect(newTime).to.equal('12:00');
+            this.set('time', newTime);
+        });
+
+        await render(hbs`<GhDateTimePicker @date={{this.date}} @time={{this.time}} @setDate={{this.updateDate}} @setTime={{this.updateTime}} />`);
+        await datepickerSelect('[data-test-date-time-picker-datepicker]', moment('2022-02-27T13:00:00.000Z').toDate());
+    });
+
+    it('can update date via datepicker with +12 timezone', async function () {
+        timezone = 'Pacific/Kwajalein'; // +12
+
+        this.set('date', '2022-02-22');
+        this.set('time', '12:00');
+
+        this.set('updateDate', (newDate) => {
+            // 12 hours earlier in UTC
+            expect(moment.utc(newDate).toISOString()).to.equal('2022-02-26T12:00:00.000Z');
             this.set('date', newDate);
         });
         this.set('updateTime', (newTime) => {
@@ -122,6 +172,38 @@ describe('Integration: Component: gh-date-time-picker', function () {
     });
 
     it('updates when @date is changed externally', async function () {
+        this.set('date','2022-02-22');
+        this.set('time', '12:00');
+
+        await render(hbs`<GhDateTimePicker @date={{this.date}} @time={{this.time}} />`);
+        expect(find('[data-test-date-time-picker-date-input]'), 'date input').to.have.value('2022-02-22');
+        expect(find('[data-test-date-time-picker-time-input]'), 'time input').to.have.value('12:00');
+
+        this.set('date', '2022-02-28');
+
+        expect(find('[data-test-date-time-picker-date-input]'), 'date input').to.have.value('2022-02-28');
+        expect(find('[data-test-date-time-picker-time-input]'), 'time input').to.have.value('12:00');
+    });
+
+    it('updates when @date is changed externally with +12 timezone', async function () {
+        timezone = 'Pacific/Kwajalein'; // +12
+
+        this.set('date','2022-02-22');
+        this.set('time', '12:00');
+
+        await render(hbs`<GhDateTimePicker @date={{this.date}} @time={{this.time}} />`);
+        expect(find('[data-test-date-time-picker-date-input]'), 'date input').to.have.value('2022-02-22');
+        expect(find('[data-test-date-time-picker-time-input]'), 'time input').to.have.value('12:00');
+
+        this.set('date', '2022-02-28');
+
+        expect(find('[data-test-date-time-picker-date-input]'), 'date input').to.have.value('2022-02-28');
+        expect(find('[data-test-date-time-picker-time-input]'), 'time input').to.have.value('12:00');
+    });
+
+    it('updates when @date is changed externally with -11 timezone', async function () {
+        timezone = 'Pacific/Pago_Pago'; // -11
+
         this.set('date','2022-02-22');
         this.set('time', '12:00');
 
@@ -212,6 +294,43 @@ describe('Integration: Component: gh-date-time-picker', function () {
 
             expect(find('[data-date="2022-02-10"]')).to.have.attribute('disabled');
             expect(find('[data-date="2022-02-25"]')).to.have.attribute('disabled');
+        });
+
+        it('Handles timezone of minimum date correctly', async function () {
+            clock = sinon.useFakeTimers({
+                now: moment('2022-01-01 10:00:00.000Z').toDate()
+            });
+
+            // Blog timezone is -11, so current date over there is 2021-12-31
+            timezone = 'Pacific/Pago_Pago'; // GMT-11
+
+            this.set('date', '2021-12-31');
+            this.set('time', '22:00');
+            this.set('minDate', 'now');
+
+            await render(hbs`<GhDateTimePicker @date={{this.date}} @time={{this.time}} @minDate={{this.minDate}} />`);
+            await click('[data-test-date-time-picker-datepicker]');
+
+            expect(find('[data-date="2021-12-30"]')).to.have.attribute('disabled');
+            expect(find('[data-date="2021-12-31"]')).not.to.have.attribute('disabled');
+        });
+
+        it('Handles timezone of maximum date correctly', async function () {
+            clock = sinon.useFakeTimers({
+                now: moment('2021-12-31 10:00:00.000Z').toDate()
+            });
+
+            // Blog timezone is -11, so current date over there is 2021-12-30
+            timezone = 'Pacific/Pago_Pago'; // GMT-11
+
+            this.set('date', '2021-12-30');
+            this.set('time', '22:00');
+            this.set('maxDate', 'now');
+
+            await render(hbs`<GhDateTimePicker @date={{this.date}} @time={{this.time}} @maxDate={{this.maxDate}} />`);
+            await click('[data-test-date-time-picker-datepicker]');
+            expect(find('[data-date="2021-12-30"]')).not.to.have.attribute('disabled');
+            expect(find('[data-date="2021-12-31"]')).to.have.attribute('disabled');
         });
 
         // TODO: move date validation into component?
