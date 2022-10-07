47b06beaa7ebedcdd0d75079086fc28ad1cb486c
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-10-07 19:07:51 +0100

    Refactored reset controller to Octane patterns
    refs https://github.com/TryGhost/Ghost/issues/14101
    
    - migrated to full native class syntax
    - swapped use of `<GhTextInput>` in favor of native `<input>` and removed use of `{{action}}` in associated template

--- ghost/admin/app/controllers/settings.js
+++ ghost/admin/app/controllers/settings.js
@@ -1,8 +1,7 @@
 import AboutModal from '../components/modals/settings/about';
+import Controller from '@ember/controller';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
-/* eslint-disable ghost/ember/alias-model-in-controller */
-import Controller from '@ember/controller';
 
 export default class SettingsController extends Controller {
     @service modals;
