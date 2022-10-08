af069737a66642f3d073d7c34df60bc59827bbb5
illiteratewriter <manukoshyabraham@gmail.com>
2022-10-08 09:59:19 +0530

    Added e2e test for `member.deleted` webhook
    #15537

--- ghost/core/test/e2e-webhooks/members.test.js
+++ ghost/core/test/e2e-webhooks/members.test.js
@@ -1,19 +1,32 @@
 const {agentProvider, mockManager, fixtureManager, matchers} = require('../utils/e2e-framework');
 const {anyGhostAgent, anyObjectId, anyISODateTime, anyUuid, anyContentVersion, anyNumber} = matchers;
 
-const newsLetterSnapshot = {
-    id: anyObjectId,
-    uuid: anyUuid,
-    created_at: anyISODateTime,
-    updated_at: anyISODateTime
+const buildNewsletterSnapshot = (deleteMember = false) => {
+    const newsLetterSnapshot = {
+        id: anyObjectId,
+        uuid: anyUuid,
+        created_at: anyISODateTime,
+        updated_at: anyISODateTime
+    };
+
+    if (deleteMember) {
+        newsLetterSnapshot._pivot_member_id = anyObjectId;
+        newsLetterSnapshot._pivot_newsletter_id = anyObjectId;
+    }
+
+    return newsLetterSnapshot;
 };
 
-const memberSnapshot = {
-    id: anyObjectId,
-    uuid: anyUuid,
-    created_at: anyISODateTime,
-    updated_at: anyISODateTime,
-    newsletters: new Array(1).fill(newsLetterSnapshot)
+const buildMemberSnapshot = (deleteMember = false) => {
+    const memberSnapshot = {
+        id: anyObjectId,
+        uuid: anyUuid,
+        created_at: anyISODateTime,
+        updated_at: anyISODateTime,
+        newsletters: new Array(1).fill(buildNewsletterSnapshot(deleteMember))
+    };
+
+    return memberSnapshot;
 };
 
 describe('member.* events', function () {
@@ -63,7 +76,48 @@ describe('member.* events', function () {
             })
             .matchBodySnapshot({
                 member: {
-                    current: memberSnapshot
+                    current: buildMemberSnapshot()
+                }
+            });
+    });
+
+    it('member.deleted event is triggered', async function () {
+        const webhookURL = 'https://test-webhook-receiver.com/member-deleted/';
+        await webhookMockReceiver.mock(webhookURL);
+        await fixtureManager.insertWebhook({
+            event: 'member.deleted',
+            url: webhookURL
+        });
+
+        const res = await adminAPIAgent
+            .post('members/')
+            .body({
+                members: [{
+                    name: 'Test Member2',
+                    email: 'testemail2@example.com',
+                    note: 'test note2'
+                }]
+            })
+            .expectStatus(201);
+        
+        const id = res.body.members[0].id;
+
+        await adminAPIAgent
+            .delete('members/' + id)
+            .expectStatus(204);
+
+        await webhookMockReceiver.receivedRequest();
+
+        webhookMockReceiver
+            .matchHeaderSnapshot({
+                'content-version': anyContentVersion,
+                'content-length': anyNumber,
+                'user-agent': anyGhostAgent
+            })
+            .matchBodySnapshot({
+                member: {
+                    current: {},
+                    previous: buildMemberSnapshot(true)
                 }
             });
     });
