d4db9d6f508a1b2bfe1d36cce61e1eb4a09dcc9e
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-10-13 13:47:07 +0100

    Extracted PlusMenu components from PlusCardMenuPlugin
    no issue
    
    - UI components should live in the `components` directory

---
+++ koenig/koenig-lexical/src/components/PlusMenu.jsx
@@ -0,0 +1,25 @@
+import CardMenu from './CardMenu';
+import {ReactComponent as PlusIcon} from '../assets/icons/plus.svg';
+
+export function PlusButton({onClick}) {
+    return (
+        <div className="absolute top-[-2px] left-[-66px]" data-kg-plus-button>
+            <button
+                type="button"
+                aria-label="Add a card"
+                className="group relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-full border border-grey bg-white transition-all ease-linear hover:border-grey-900 md:h-9 md:w-9"
+                onClick={onClick}
+            >
+                <PlusIcon className="h-4 w-4 stroke-grey-800 stroke-2 group-hover:stroke-grey-900" />
+            </button>
+        </div>
+    );
+}
+
+export function PlusMenu() {
+    return (
+        <div className="absolute left-[-16px]" data-kg-plus-menu>
+            <CardMenu />
+        </div>
+    );
+}
--- koenig/koenig-lexical/src/plugins/PlusCardMenuPlugin.jsx
+++ koenig/koenig-lexical/src/plugins/PlusCardMenuPlugin.jsx
@@ -1,32 +1,8 @@
 import React from 'react';
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
-import {ReactComponent as PlusIcon} from '../assets/icons/plus.svg';
 import {$getSelection, $isParagraphNode, $isRangeSelection} from 'lexical';
 import {getSelectedNode} from '../utils/getSelectedNode';
-import CardMenu from '../components/CardMenu';
-
-function PlusButton({onClick}) {
-    return (
-        <div className="absolute top-[-2px] left-[-66px]" data-kg-plus-button>
-            <button
-                type="button"
-                aria-label="Add a card"
-                className="group relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-full border border-grey bg-white transition-all ease-linear hover:border-grey-900 md:h-9 md:w-9"
-                onClick={onClick}
-            >
-                <PlusIcon className="h-4 w-4 stroke-grey-800 stroke-2 group-hover:stroke-grey-900" />
-            </button>
-        </div>
-    );
-}
-
-function PlusMenu() {
-    return (
-        <div className="absolute left-[-16px]" data-kg-plus-menu>
-            <CardMenu />
-        </div>
-    );
-}
+import {PlusButton, PlusMenu} from '../components/PlusMenu';
 
 function usePlusCardMenu(editor) {
     const [isShowingButton, setIsShowingButton] = React.useState(false);
