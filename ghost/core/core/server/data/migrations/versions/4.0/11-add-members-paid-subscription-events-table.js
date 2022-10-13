86994f3933896c4659bbe6fe63b3eeb95f7bd0fe
Daniel Lockyer <hi@daniellockyer.com>
2022-10-13 08:28:06 +0700

    Fixed incorrect maximum length definition in schema
    refs https://github.com/TryGhost/Toolbox/issues/441
    
    - whilst reviewing another PR, I noticed we were incorrectly using
      `maxLength` instead of `maxlength` in the schema column definition
    - it turns out we've already been doing this wrong for a while with
      other columns
    - this key is not acted upon, so the maximum column length was not applied
    - fixing up the DB to the correct maximum length is something to fix in the
      future but right now, the schema does not reflect the size of the
      column that actually got created
    - the fallback when `maxlength` is not provided is currently 191 [0], so
      this commit switches the schema and migrations to using the correct
      key name and column length that they are using when applied
    
    [0]: https://github.com/TryGhost/Ghost/blob/1ab695feef5b14d8d8b6289b26510d869faa02e7/ghost/core/core/server/data/schema/commands.js#L27

--- ghost/core/core/server/data/migrations/versions/4.0/11-add-members-paid-subscription-events-table.js
+++ ghost/core/core/server/data/migrations/versions/4.0/11-add-members-paid-subscription-events-table.js
@@ -5,7 +5,7 @@ module.exports = addTable('members_paid_subscription_events', {
     member_id: {type: 'string', maxlength: 24, nullable: false, references: 'members.id', cascadeDelete: true},
     from_plan: {type: 'string', maxlength: 255, nullable: true},
     to_plan: {type: 'string', maxlength: 255, nullable: true},
-    currency: {type: 'string', maxLength: 3, nullable: false},
+    currency: {type: 'string', maxlength: 191, nullable: false},
     source: {type: 'string', maxlength: 50, nullable: false},
     mrr_delta: {type: 'integer', nullable: false},
     created_at: {type: 'dateTime', nullable: false}
