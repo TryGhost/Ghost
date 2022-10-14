075e753af78efec452c5ce012c39b47f41ed21b7
e.baidakova <lena.baydakova@gmail.com>
2022-10-14 17:15:36 +0400

    fixup! Added the feedback buttons in the emails

--- ghost/core/core/server/services/mega/feedback-buttons.js
+++ ghost/core/core/server/services/mega/feedback-buttons.js
@@ -32,7 +32,7 @@ const getTemplate = (accentColor) => {
         <tr>
             <td dir="ltr" width="100%" style="background-color: #ffffff; text-align: center; padding: 40px 4px; border-bottom: 1px solid #e5eff5" align="center">
                 <h3 style="text-align: center; margin-bottom: 22px; font-size: 17px; letter-spacing: -0.2px; margin-top: 0 !important;">What did you think of this post?</h3>
-                <table class="inline" role="presentation" border="0" cellpadding="0" cellspacing="0" style="margin: auto;">
+                <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="margin: auto; width: auto !important;">
                     <tr>
                         ${likeButtonHtml}
                         ${dislikeButtonHtml}
@@ -50,7 +50,7 @@ function getButtonHtml(href, buttonText, accentColor) {
 
     return (`
         <td dir="ltr" valign="top" align="center" style="font-family: inherit; font-size: 14px; text-align: center;" nowrap>
-            <table class="inline" align="center" role="presentation" cellspacing="0" cellpadding="0" border="0">
+            <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" style="width: auto !important;">
                 <tr>
                     <td style="padding: 0 6px; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';">
                         <a href=${href} style="background-color: ${bgColor}; color: ${textColor}; border-radius: 22px; font-family: inherit; padding: 12px 20px; border: none; font-size: 14px; font-weight: bold; line-height: 100%; text-decoration: none; display: block;">
--- ghost/core/core/server/services/mega/template.js
+++ ghost/core/core/server/services/mega/template.js
@@ -71,10 +71,6 @@ table {
     width: 100%;
 }
 
-table.inline {
-  width: auto !important;
-}
-
 table td {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
     font-size: 18px;
