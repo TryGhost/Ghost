7e60a9709d9bd492846e90d18976ebea93664186
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-10-17 16:35:50 +0100

    Cleaned up formatting in image card tests
    no issue
    
    - added spacing to show separate steps in tests
    - added HTML indentation for better readability
    - removed unnecessary re-definition of `assertHTML` defaults in each call

--- koenig/koenig-lexical/test/e2e/cards/image-card.test.js
+++ koenig/koenig-lexical/test/e2e/cards/image-card.test.js
@@ -21,107 +21,118 @@ describe('Image card', async () => {
     test('renders image card node', async function () {
         await focusEditor(page);
         await page.keyboard.type('image! ');
+
         await assertHTML(page, html`
             <div data-lexical-decorator="true" contenteditable="false">
-            <div data-kg-card="true" data-kg-card-selected="false">
-            <div>
-            <div>
-            <figure>
-            <div>
-            <button name="placeholder-button">
-            <svg width="134" height="135" viewBox="0 0 134 135" xmlns="http://www.w3.org/2000/svg"></svg>
-            <p>Click to select an image</p>
-            </button>
+                <div data-kg-card="true" data-kg-card-selected="false">
+                    <div>
+                        <div>
+                            <figure>
+                                <div>
+                                    <button name="placeholder-button">
+                                        <svg width="134" height="135" viewBox="0 0 134 135" xmlns="http://www.w3.org/2000/svg"></svg>
+                                        <p>Click to select an image</p>
+                                    </button>
+                                </div>
+                            </figure>
+                        </div>
+                        <form><input name="image-input" type="file" accept="image/*" hidden="" /></form>
+                        <div></div>
+                    </div>
+                </div>
             </div>
-            </figure>
-            </div>
-            <form><input name="image-input" type="file" accept="image/*" hidden="" /></form>
-            <div>
-            </div>
-            </div>
-            </div>
-            </div>
-        `, {ignoreInlineStyles: true, ignoreClasses: true, ignoreInnerSVG: true});
+        `);
     });
 
     test('can upload an image', async function () {
         const filePath = path.relative(process.cwd(), __dirname + '/assets/large.png');
+
         await focusEditor(page);
         await page.keyboard.type('image! ');
+
         const [fileChooser] = await Promise.all([
             page.waitForFileChooser(),
             await page.click('button[name="placeholder-button"]')
         ]);
         await fileChooser.accept([filePath]);
+
         await assertHTML(page, html`
             <div data-lexical-decorator="true" contenteditable="false">
-            <div data-kg-card="true" data-kg-card-selected="true">
-            <div>
-            <figure>
-            <img src="data:image/png;" alt="" />
-            </figure>
-            <div>
-            <input placeholder="Type caption for image (optional)" value="" />
-            <button name="alt-toggle-button">Alt</button>
+                <div data-kg-card="true" data-kg-card-selected="true">
+                    <div>
+                        <figure>
+                            <img src="data:image/png;" alt="" />
+                        </figure>
+                        <div>
+                            <input placeholder="Type caption for image (optional)" value="" />
+                            <button name="alt-toggle-button">Alt</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            </div>
-            </div>
-            </div>
-        `, {ignoreInlineStyles: true, ignoreClasses: true, ignoreInnerSVG: true, ignoreBase64String: true});
+        `);
     });
 
     test('can toggle to alt text', async function () {
         const filePath = path.relative(process.cwd(), __dirname + '/assets/large.png');
+
         await focusEditor(page);
         await page.keyboard.type('image! ');
+
         const [fileChooser] = await Promise.all([
             page.waitForFileChooser(),
             await page.click('button[name="placeholder-button"]')
         ]);
         await fileChooser.accept([filePath]);
+
         await page.click('button[name="alt-toggle-button"]');
+
         await assertHTML(page, html`
             <div data-lexical-decorator="true" contenteditable="false">
-            <div data-kg-card="true" data-kg-card-selected="true">
-            <div>
-            <figure>
-            <img src="data:image/png;" alt="" />
-            </figure>
-            <div>
-            <input placeholder="Type alt text for image (optional)" value=""/>
-            <button name="alt-toggle-button">Alt</button>
+                <div data-kg-card="true" data-kg-card-selected="true">
+                    <div>
+                        <figure>
+                            <img src="data:image/png;" alt="" />
+                        </figure>
+                        <div>
+                            <input placeholder="Type alt text for image (optional)" value=""/>
+                            <button name="alt-toggle-button">Alt</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            </div>
-            </div>
-            </div>
-        `, {ignoreInlineStyles: true, ignoreClasses: true, ignoreInnerSVG: true, ignoreBase64String: true});
+        `);
     });
 
     test('renders caption if present', async function () {
         const filePath = path.relative(process.cwd(), __dirname + '/assets/large.png');
+
         await focusEditor(page);
         await page.keyboard.type('image! ');
+
         const [fileChooser] = await Promise.all([
             page.waitForFileChooser(),
             await page.click('button[name="placeholder-button"]')
         ]);
         await fileChooser.accept([filePath]);
+
         await page.click('input[placeholder="Type caption for image (optional)"]');
         await page.keyboard.type('This is a caption');
+
         await assertHTML(page, html`
             <div data-lexical-decorator="true" contenteditable="false">
-            <div data-kg-card="true" data-kg-card-selected="true">
-            <div>
-            <figure>
-            <img src="data:image/png;" alt="" />
-            </figure>
-            <div>
-            <input placeholder="Type caption for image (optional)" value="This is a caption" />
-            <button name="alt-toggle-button">Alt</button>
+                <div data-kg-card="true" data-kg-card-selected="true">
+                    <div>
+                        <figure>
+                            <img src="data:image/png;" alt="" />
+                        </figure>
+                        <div>
+                            <input placeholder="Type caption for image (optional)" value="This is a caption" />
+                            <button name="alt-toggle-button">Alt</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            </div>
-            </div>
-            </div>
-        `, {ignoreInlineStyles: true, ignoreClasses: true, ignoreInnerSVG: true, ignoreBase64String: true});
+        `);
     });
 });
