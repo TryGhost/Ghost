fd51809d2bd852f660b03b1b4c41c73f49b36edf
Sanne de Vries <sannedv@protonmail.com>
2022-10-19 10:56:44 +0700

    Added card wrapper to image card story
    Refs https://github.com/TryGhost/Team/issues/2108

--- koenig/koenig-lexical/src/components/ui/cards/ImageCard.stories.jsx
+++ koenig/koenig-lexical/src/components/ui/cards/ImageCard.stories.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import {ImageCard} from './ImageCard';
+import {CardWrapper} from './../CardWrapper';
 
 const story = {
     title: 'Cards/Image Card',
     component: ImageCard,
+    subcomponent: {CardWrapper},
     argTypes: {
         isSelected: {control: 'boolean'}
     }
 };
 export default story;
 
-const Template = args => <ImageCard {...args} />;
+const Template = args => (
+    <div className="w-[740px]">
+        <CardWrapper>
+            <ImageCard {...args} />
+        </CardWrapper>
+    </div>
+);
 
 export const Default = Template.bind({});
 
