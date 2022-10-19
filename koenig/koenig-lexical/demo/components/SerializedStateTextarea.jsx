1dc4d30fcdcba1cefb626db4782909fae11ce187
Sanne de Vries <sannedv@protonmail.com>
2022-10-19 10:29:53 +0700

    Updated Lexical state tree on demo page
    Refs https://github.com/TryGhost/Team/issues/2076

--- koenig/koenig-lexical/demo/components/SerializedStateTextarea.jsx
+++ koenig/koenig-lexical/demo/components/SerializedStateTextarea.jsx
@@ -19,7 +19,7 @@ const SerializedStateTextarea = () => {
             <CodeEditor
                 value={serializedJson}
                 language="JSON"
-                className="h-full w-full resize-none !overflow-auto bg-black !p-4 font-mono text-sm"
+                className="h-full w-full resize-none !overflow-auto bg-black !p-4 font-mono text-sm selection:bg-grey-800"
                 readOnly
             />
             <OnChangePlugin onChange={onChange} />
--- koenig/koenig-lexical/demo/components/TreeView.jsx
+++ koenig/koenig-lexical/demo/components/TreeView.jsx
@@ -6,11 +6,11 @@ const TreeViewPlugin = () => {
 
     return (
         <TreeView
-            viewClassName="tree-view-output"
-            timeTravelPanelClassName="debug-timetravel-panel"
-            timeTravelButtonClassName="debug-timetravel-button"
-            timeTravelPanelSliderClassName="debug-timetravel-panel-slider"
-            timeTravelPanelButtonClassName="debug-timetravel-panel-button"
+            viewClassName="m-[1rem] p-[1rem] pb-16 overflow-auto text-sm text-grey-300 font-mono relative selection:bg-grey-800"
+            timeTravelPanelClassName="absolute bottom-1 flex w-[400px]"
+            timeTravelButtonClassName="text-green pb-4 cursor-pointer font-sans text-md font-medium absolute bottom-0"
+            timeTravelPanelSliderClassName="m-3 bg-green flex-grow"
+            timeTravelPanelButtonClassName="text-green font-sans text-md font-medium"
             editor={editor}
         />
     );
