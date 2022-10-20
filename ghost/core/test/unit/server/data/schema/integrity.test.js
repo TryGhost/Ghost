773e5ab15fd294550362b3d1cfdc51cc713ec986
Rishabh Garg <rish@ghost.org>
2022-10-20 09:11:26 +0530

    Added permissions for link edit endpoints (#15664)
    refs https://github.com/TryGhost/Team/issues/2104
    
    - adds edit permissions for links endpoints to fixtures
    - new `bulkEdit` endpoint will use the permissions and allow fixing newsletter links via Admin

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -36,7 +36,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
 describe('DB version integrity', function () {
     // Only these variables should need updating
     const currentSchemaHash = '76cfd2230c02c428e9505f6b9a3936ec';
-    const currentFixturesHash = '6110a18edb75e3b7465d69385915984d';
+    const currentFixturesHash = 'dcb7ba7c66b4b98d6c26a722985e756a';
     const currentSettingsHash = '2978a5684a2d5fcf089f61f5d368a0c0';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
 
