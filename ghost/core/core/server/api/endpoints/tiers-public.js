9bba47a5ae7c59a2abfbffe4ca1a2af04d640cef
Fabien "egg" O'Carroll <fabien@allou.is>
2022-10-21 16:21:24 +0700

    Wired up Tiers service to Tiers Content & Admin API
    refs https://github.com/TryGhost/Team/issues/2078

--- ghost/core/core/server/api/endpoints/tiers-public.js
+++ ghost/core/core/server/api/endpoints/tiers-public.js
@@ -1,8 +1,4 @@
-// NOTE: We must not cache references to membersService.api
-// as it is a getter and may change during runtime.
-const membersService = require('../../services/members');
-
-const allowedIncludes = ['monthly_price', 'yearly_price', 'benefits'];
+const tiersService = require('../../services/tiers');
 
 module.exports = {
     docName: 'tiers',
@@ -11,22 +7,14 @@ module.exports = {
         options: [
             'limit',
             'fields',
-            'include',
             'filter',
             'order',
             'debug',
             'page'
         ],
         permissions: true,
-        validation: {
-            options: {
-                include: {
-                    values: allowedIncludes
-                }
-            }
-        },
         async query(frame) {
-            const page = await membersService.api.productRepository.list(frame.options);
+            const page = await tiersService.api.browse(frame.options);
 
             return page;
         }
