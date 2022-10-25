a10de295a3ea9e9b134e4cefed3787e11e537637
Simon Backx <simon@ghost.org>
2022-10-25 16:21:10 +0200

    Added breadcrumbs to members pages when coming from analytics
    fixes https://github.com/TryGhost/Team/issues/2140

--- ghost/admin/app/routes/members.js
+++ ghost/admin/app/routes/members.js
@@ -5,6 +5,8 @@ export default class MembersRoute extends AdminRoute {
     @service store;
     @service feature;
 
+    fromAnalytics = false;
+
     queryParams = {
         label: {refreshModel: true},
         searchParam: {refreshModel: true, replace: true},
@@ -24,15 +26,39 @@ export default class MembersRoute extends AdminRoute {
     }
 
     // trigger a background load of members plus labels for filter dropdown
-    setupController(controller) {
+    setupController(controller, model, transition) {
         super.setupController(...arguments);
         controller.fetchLabelsTask.perform();
+
+        if (transition.from?.name === 'posts.analytics') {
+            // Sadly transition.from.params is not reliable to use (not populated on transitions)
+            const oldParams = transition.router?.oldState?.params['posts.analytics'] ?? {};
+            
+            // We need to store analytics in 'this' to have it accessible for the member route
+            this.fromAnalytics = Object.values(oldParams);
+            controller.fromAnalytics = this.fromAnalytics;
+        } else if (transition.from?.metadata?.fromAnalytics) {
+            // Handle returning from member route
+            const fromAnalytics = transition.from?.metadata.fromAnalytics ?? null;
+            controller.fromAnalytics = fromAnalytics;
+            this.fromAnalytics = fromAnalytics;
+        } else {
+            controller.fromAnalytics = null;
+            this.fromAnalytics = null;
+        }
+    }
+
+    resetController() {
+        super.resetController(...arguments);
+        // don't reset fromAnalytics here, we need to reuse it. We reset it in setup
+        //controller.fromAnalytics = null;
     }
 
     buildRouteInfoMetadata() {
         return {
             titleToken: 'Members',
-            mainClasses: ['gh-main-fullwidth']
+            mainClasses: ['gh-main-fullwidth'],
+            fromAnalytics: this.fromAnalytics
         };
     }
 }
