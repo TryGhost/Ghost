b79006d7f3ae25be5a9bb42c58d2d45592be5309
Aileen Nowak <AileenCGN@gmail.com>
2022-10-25 16:02:35 +0100

    ✨ Added Ghost Explore app
    no issue
    
    - bumps Ghost Explore integrated app  to GA from alpha

--- ghost/admin/app/services/feature.js
+++ ghost/admin/app/services/feature.js
@@ -65,7 +65,6 @@ export default class FeatureService extends Service {
     @feature('emailAlerts') emailAlerts;
     @feature('sourceAttribution') sourceAttribution;
     @feature('lexicalEditor') lexicalEditor;
-    @feature('exploreApp') exploreApp;
     @feature('audienceFeedback') audienceFeedback;
     @feature('fixNewsletterLinks') fixNewsletterLinks;
 
