d29d185fe56162b26c30e2fe32bc6aa9eb59827a
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-10-26 11:19:40 +0100

    Removed commented BLUR_COMMAND handling in KoenigCardWrapper
    closes https://github.com/TryGhost/Team/issues/2131
    
    - the blur handling was overzealous which meant it was commented out so that toolbars could be selected
    - without the blur handling the main behaviour is the same as our mobiledoc editor so it wasn't needed, if there are specific circumstances that come up later on which need some form of deselect-card-on-blur we can handle those as needed

--- koenig/koenig-lexical/src/components/KoenigCardWrapper.jsx
+++ koenig/koenig-lexical/src/components/KoenigCardWrapper.jsx
@@ -7,7 +7,6 @@ import {
     $isDecoratorNode,
     $isNodeSelection,
     $setSelection,
-    // BLUR_COMMAND,
     CLICK_COMMAND,
     COMMAND_PRIORITY_EDITOR,
     COMMAND_PRIORITY_LOW,
@@ -56,16 +55,6 @@ const KoenigCardWrapperComponent = ({nodeKey, children}) => {
                 },
                 COMMAND_PRIORITY_LOW
             ),
-            // editor.registerCommand(
-            //     BLUR_COMMAND,
-            //     (event) => {
-            //         if (isSelected && !ref.current.contains(event.relatedTarget)) {
-            //             clearSelected();
-            //         }
-            //         return false;
-            //     },
-            //     COMMAND_PRIORITY_LOW
-            // ),
             editor.registerCommand(
                 KEY_ENTER_COMMAND,
                 (event) => {
