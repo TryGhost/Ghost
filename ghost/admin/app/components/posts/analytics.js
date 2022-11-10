a5c4091620c439c62e5cfbf10fe57c880e207d9a
Simon Backx <simon@ghost.org>
2022-11-10 10:38:09 +0100

    Fixed analytics page feedback chart filter button links
    no issue
    
    Clicking the 'see members' button in the feedback chart tooltip went to a page with a hardcoded post id instead of the actual post id.

--- ghost/admin/app/components/posts/analytics.js
+++ ghost/admin/app/components/posts/analytics.js
@@ -94,8 +94,8 @@ export default class Analytics extends Component {
         const values = [this.post.count.positive_feedback, this.post.count.negative_feedback];
         const labels = ['More like this', 'Less like this'];
         const links = [
-            {filterParam: '(feedback.post_id:6368b75db2faed03adaee6b7+feedback.score:1)'},
-            {filterParam: '(feedback.post_id:6368b75db2faed03adaee6b7+feedback.score:0)'}
+            {filterParam: '(feedback.post_id:' + this.post.id + '+feedback.score:1)'},
+            {filterParam: '(feedback.post_id:' + this.post.id + '+feedback.score:0)'}
         ];
         const colors = ['#F080B2', '#8452f633'];
         return {values, labels, links, colors};
