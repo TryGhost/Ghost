99047e8f5f57e0fa08f452a9bdc9805bf87971a4
Rishabh <zrishabhgarg@gmail.com>
2022-11-11 12:55:49 +0530

    Cleaned up old member analytics services
    refs https://github.com/TryGhost/Team/issues/2216
    
    This change removes old analytics code which was added under `membersActivity` flag as an experimental alpha feature to test the first versions of member analytics, and is no longer active or in use.
    
    This change removes the remaining services and its usage that were created to manage this version of analytics but is no longer active or maintained.
    
    - removes `members-analytics-ingress` service that was used to ingest events from Portal in this experimental feature
    - removes `member-analytics-service` service that managed the events from this experimental feature
    - removes usages of the 2 services and their dependency in `members-api`
    - removes `member-analytic-event` model as the corresponding table for it does not exist anymore and was dropped in 5.0

--- ghost/core/core/server/models/member-analytic-event.js
+++
@@ -1,9 +0,0 @@
-const ghostBookshelf = require('./base');
-
-const MemberAnalyticEvent = ghostBookshelf.Model.extend({
-    tableName: 'temp_member_analytic_events'
-});
-
-module.exports = {
-    MemberAnalyticEvent: ghostBookshelf.model('MemberAnalyticEvent', MemberAnalyticEvent)
-};
