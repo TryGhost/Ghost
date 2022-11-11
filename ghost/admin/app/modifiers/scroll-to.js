4b3a9ab96674a1a52f99468d55030f22bc35e9e5
Kevin Ansfield <kevin@lookingsideways.co.uk>
2022-11-11 11:53:53 +0000

    Renamed `{{scroll-to}}`'s `shouldScroll` argument to `when`
    no issue
    
    - `when` makes the modifier a little easier to read for it's typical use-case, eg:
      - `{{scroll-to when=(eq entry.slug this.entry)}}`
      - `{{scroll-to when=(eq this.focusArea "analytics")}}`

--- ghost/admin/app/modifiers/scroll-to.js
+++ ghost/admin/app/modifiers/scroll-to.js
@@ -1,9 +1,9 @@
 import getScrollParent from 'ghost-admin/utils/get-scroll-parent';
 import {modifier} from 'ember-modifier';
 
-export default modifier((element, positional, {shouldScroll = true}) => {
-    if (shouldScroll) {
-        // setTimeout needed to ensure layout has finished and we have accurate
+export default modifier((element, positional, {when = true}) => {
+    if (when) {
+        // setTimeout needed to ensure layout has finished and we have accurate positioning
         setTimeout(() => {
             const scrollParent = getScrollParent(element);
             const y = element.offsetTop;
