5cd4e6bd9a47796395b52f1b6562e2244a67036d
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-11-15 09:34:57 +0700

    Removed defaults method

--- ghost/core/core/server/models/member-newsletter.js
+++ ghost/core/core/server/models/member-newsletter.js
@@ -1,16 +1,7 @@
 const ghostBookshelf = require('./base');
-const uuid = require('uuid');
 
 const MemberNewsletter = ghostBookshelf.Model.extend({
-    tableName: 'members_newsletters',
-
-    defaults() {
-        return {
-            id: uuid.v4(),
-            member_id: null,
-            newsletter_id: null
-        };
-    }
+    tableName: 'members_newsletters'
 });
 
 module.exports = {
