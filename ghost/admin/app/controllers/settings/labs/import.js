20e6789c325e1c9de37f087f02eab6a98e18483f
Sam Lord <hello@samlord.me>
2022-11-16 10:22:49 +0000

    Replaced "Open Importer" button with one that works
    no issue

--- ghost/admin/app/controllers/settings/labs/import.js
+++ ghost/admin/app/controllers/settings/labs/import.js
@@ -1,13 +1,12 @@
-import Controller, {inject as controller} from '@ember/controller';
+import Controller from '@ember/controller';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
 
 export default class ImportController extends Controller {
     @service router;
-    @controller labs;
 
     @action
     close() {
-        this.router.transitionTo('labs');
+        this.router.transitionTo('settings.labs');
     }
 }
