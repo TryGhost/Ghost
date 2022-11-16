8c31715ce6ddb5cfe61a30a10ec66738cc0d69e8
Sam Lord <hello@samlord.me>
2022-11-16 10:22:49 +0000

    Replaced "Open Importer" button with one that works
    no issue

--- ghost/core/test/e2e-browser/frontend.spec.js
+++ ghost/core/test/e2e-browser/frontend.spec.js
@@ -1,8 +1,20 @@
 const {expect, test} = require('@playwright/test');
+const testUtils = require('../utils');
+const express = require('express');
 
 let siteUrl = process.env.TEST_URL || 'http://localhost:2368';
 
-test('Homepage is 200', async ({page}) => {
-    const response = await page.goto(siteUrl);
-    expect(response.status()).toEqual(200);
+test.describe('Ghost frontend', function () {
+    test.beforeAll(async function () {
+        const app = await testUtils.startGhost();
+    });
+
+    test.afterAll(async () => {
+        await testUtils.stopGhost();
+    });
+
+    test('Loads the homepage', async ({page}) => {
+        const response = await page.goto(siteUrl);
+        expect(response.status()).toEqual(200);
+    });
 });
