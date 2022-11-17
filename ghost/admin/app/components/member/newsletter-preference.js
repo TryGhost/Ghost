cee6feac5820a16a71a13a5e04296d95e8554421
e.baidakova <lena.baydakova@gmail.com>
2022-11-17 13:00:46 +0400

    Add member's email status on member page
    closes TryGhost/Team#2270
    - Show emails status depending on the reason email was blocked (spam/fail)

--- ghost/admin/app/components/member/newsletter-preference.js
+++ ghost/admin/app/components/member/newsletter-preference.js
@@ -1,6 +1,44 @@
 import Component from '@glimmer/component';
+import moment from 'moment-timezone';
 import {action} from '@ember/object';
 import {tracked} from '@glimmer/tracking';
+
+// todo: remove when backend will be ready
+const getMockedData = (email) => {
+    const timestamp = '2022-11-17T07:15:45.000Z';
+    const mockSuppressedInfoSpam = {
+        reason: 'spam',
+        timestamp
+    };
+
+    const mockSuppressedInfoFail = {
+        reason: 'fail',
+        timestamp
+    };
+
+    const data = {
+        emailSuppression: {
+            suppressed: false,
+            info: null
+        }
+    };
+
+    if (email === 'spam@ghost.org') {
+        data.emailSuppression = {
+            suppressed: true,
+            info: mockSuppressedInfoSpam
+        };
+    }
+
+    if (email === 'fail@ghost.org') {
+        data.emailSuppression = {
+            suppressed: true,
+            info: mockSuppressedInfoFail
+        };
+    }
+
+    return data;
+};
 
 export default class MembersNewsletterPreference extends Component {
     @tracked filterValue;
@@ -24,6 +62,19 @@ export default class MembersNewsletterPreference extends Component {
             });
         }
         return [];
+    }
+
+    get suppressionData() {
+        // todo: replace `getMockedData(this.args.member.email)` on `this.args.member` when backend will be ready
+        const {emailSuppression} = getMockedData(this.args.member.email);
+        const timestamp = emailSuppression.info?.timestamp;
+        const formattedDate = timestamp ? moment(new Date(timestamp)).format('D MMM YYYY') : null;
+
+        return {
+            suppressed: emailSuppression.suppressed,
+            reason: emailSuppression.info?.reason,
+            date: formattedDate
+        };
     }
 
     @action
