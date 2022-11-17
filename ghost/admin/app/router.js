ddfa65ee7283240749fd82aa2036556fab9cc404
Sam Lord <hello@samlord.me>
2022-11-17 11:00:13 +0000

    Fixed lint issues
    no issue

--- ghost/admin/app/router.js
+++ ghost/admin/app/router.js
@@ -102,7 +102,7 @@ Router.map(function () {
     this.route('settings.integrations.zapier', {path: '/settings/integrations/zapier'});
 
     this.route('settings.navigation', {path: '/settings/navigation'});
-    this.route('settings.labs', {path: '/settings/labs'}, function() {
+    this.route('settings.labs', {path: '/settings/labs'}, function () {
         this.route('import');
     });
     // this.route('settings.labs.import', {path: '/settings/labs/import'});
