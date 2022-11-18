47f1f7836c70db67ea0d451b61e6a1b6f282b4f9
e.baidakova <lena.baydakova@gmail.com>
2022-11-18 12:50:33 +0400

    Remove frontend mock data
    closes TryGhost/Team#2248

--- ghost/admin/app/components/member/newsletter-preference.js
+++ ghost/admin/app/components/member/newsletter-preference.js
@@ -3,43 +3,6 @@ import moment from 'moment-timezone';
 import {action} from '@ember/object';
 import {tracked} from '@glimmer/tracking';
 
-// todo: remove when backend will be ready
-const getMockedData = (email) => {
-    const timestamp = '2022-11-17T07:15:45.000Z';
-    const mockSuppressedInfoSpam = {
-        reason: 'spam',
-        timestamp
-    };
-
-    const mockSuppressedInfoFail = {
-        reason: 'fail',
-        timestamp
-    };
-
-    const data = {
-        emailSuppression: {
-            suppressed: false,
-            info: null
-        }
-    };
-
-    if (email === 'spam@ghost.org') {
-        data.emailSuppression = {
-            suppressed: true,
-            info: mockSuppressedInfoSpam
-        };
-    }
-
-    if (email === 'fail@ghost.org') {
-        data.emailSuppression = {
-            suppressed: true,
-            info: mockSuppressedInfoFail
-        };
-    }
-
-    return data;
-};
-
 export default class MembersNewsletterPreference extends Component {
     @tracked filterValue;
 
@@ -65,14 +28,13 @@ export default class MembersNewsletterPreference extends Component {
     }
 
     get suppressionData() {
-        // todo: replace `getMockedData(this.args.member.email)` on `this.args.member` when backend will be ready
-        const {emailSuppression} = getMockedData(this.args.member.email);
-        const timestamp = emailSuppression.info?.timestamp;
+        const {emailSuppression} = this.args.member;
+        const timestamp = emailSuppression?.info?.timestamp;
         const formattedDate = timestamp ? moment(new Date(timestamp)).format('D MMM YYYY') : null;
 
         return {
-            suppressed: emailSuppression.suppressed,
-            reason: emailSuppression.info?.reason,
+            suppressed: emailSuppression?.suppressed,
+            reason: emailSuppression?.info?.reason,
             date: formattedDate
         };
     }
