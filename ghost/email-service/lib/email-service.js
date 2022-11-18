bd379ef228425f6bb43eeb6b013aef92f920b7c8
Simon Backx <simon@ghost.org>
2022-11-18 16:37:27 +0100

    Added email service package
    fixes https://github.com/TryGhost/Team/issues/2282
    
    Added a new email service package that is used when the email stability flag is enabled. Currently not yet implemented so will throw an error for all entry points.

---
+++ ghost/email-service/lib/email-service.js
@@ -0,0 +1,26 @@
+class EmailService {
+    constructor(dependencies) {
+        // ...
+    }
+
+    async createEmail(post) {
+        // eslint-disable-next-line no-restricted-syntax
+        throw new Error('Not implemented');
+    }
+    async retryEmail(email) {
+        // eslint-disable-next-line no-restricted-syntax
+        throw new Error('Not implemented');
+    }
+
+    async previewEmail(post, newsletter, segment) {
+        // eslint-disable-next-line no-restricted-syntax
+        throw new Error('Previewing an email has not been implemented yet. Turn off the email stability flag is you need this functionality.');
+    }
+
+    async sendTestEmail(post, newsletter, segment, emails) {
+        // eslint-disable-next-line no-restricted-syntax
+        throw new Error('Sending a test email has not been implemented yet. Turn off the email stability flag is you need this functionality.');
+    }
+}
+
+module.exports = EmailService;
