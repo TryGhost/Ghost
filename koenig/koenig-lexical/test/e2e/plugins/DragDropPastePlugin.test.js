156ea1be598501574070752dbf181c442d1f34af
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-11-23 07:05:10 +0700

    Added `can drag and drop image on the editor test` ref https://github.com/TryGhost/Team/issues/2198
    - Checks whether an image card will be rendered when dropping an image onto the editor

--- koenig/koenig-lexical/test/e2e/plugins/DragDropPastePlugin.test.js
+++ koenig/koenig-lexical/test/e2e/plugins/DragDropPastePlugin.test.js
@@ -1,7 +1,7 @@
 import {afterAll, beforeAll, beforeEach, describe, test} from 'vitest';
-import {startApp, initialize} from '../../utils/e2e';
-// import path from 'path';
-// import fs from 'fs';
+import {startApp, initialize, focusEditor, assertHTML, html} from '../../utils/e2e';
+import path from 'path';
+import fs from 'fs';
 
 describe('Drag Drop Paste Plugin', async function () {
     let app;
@@ -19,39 +19,47 @@ describe('Drag Drop Paste Plugin', async function () {
         await initialize({page});
     });
 
-    test.todo('can drag and drop an image into the editor');
+    test('can drag and drop an image on the editor', async function () {
+        await focusEditor(page);
 
-    // test('can drop an image on the editor', async function () {
-    //     const fp = path.relative(process.cwd(), __dirname + '/../fixtures/large-image.png');
-    //     const buffer = fs.readFileSync(fp);
-    //     await focusEditor(page);
-    //     // eslint - disable - next - line
-    //     const dataset = await page.evaluate(async (fileBuffer) => {
-    //         const dt = new DataTransfer();
-    //         const file = new File([fileBuffer], 'large.png', {type: 'image/png'});
-    //         dt.items.add(file);
-    //         return dt;
-    //     }, buffer.toString('hex'));
-    //     // await page.dispatchEvent('.koenig-lexical', 'drop', {dataset});
-    //     await page.evaluateHandle(async (data) => {
-    //         const dropEvent = new DragEvent(data);
-    //         await document.dispatchEvent(dropEvent);
-    //     }, await dataset);
-    //     // set timeout
-    //     await page.waitForTimeout(3000);
-    //     await assertHTML(page, html`
-    //         <div data-lexical-decorator="true" contenteditable="false">
-    //             <div data-kg-card-selected="true" data-kg-card="image">
-    //                 <figure>
-    //                     <img src="data:image/png;base64,BASE64DATA" alt="" />
-    //                     <figcaption>
-    //                         <input placeholder="Type caption for image (optional)" value="" />
-    //                         <button name="alt-toggle-button">Alt</button>
-    //                     </figcaption>
-    //                 </figure>
-    //                 <div data-kg-card-toolbar="image"></div>
-    //             </div>
-    //         </div>
-    //     `);
-    // });
+        const filePath = path.relative(process.cwd(), __dirname + '/../fixtures/large-image.png');
+        const buffer = fs.readFileSync(filePath);
+
+        const dataTransfer = await page.evaluateHandle((data) => {
+            const dt = new DataTransfer();
+            const file = new File([data.toString('hex')], 'large-image.png', {type: 'image/png'});
+            dt.items.add(file);
+            return dt;
+        }, buffer);
+
+        await page.dispatchEvent(
+            '#root',
+            'dragenter',
+            {dataTransfer}
+        );
+        await page.dispatchEvent(
+            '#root',
+            'drop',
+            {dataTransfer}
+        );
+
+        await assertHTML(page, html`
+            <div data-lexical-decorator="true" contenteditable="false">
+                <div data-kg-card-selected="true" data-kg-card="image">
+                    <figure data-kg-card-width="regular">
+                        <div>
+                            <img
+                                src="blob:..."
+                                alt="upload in progress, 0 " />
+                        </div>
+                        <figcaption>
+                            <input placeholder="Type caption for image (optional)" value="" />
+                            <button name="alt-toggle-button">Alt</button>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+            <p><br /></p>
+        `);
+    });
 });
