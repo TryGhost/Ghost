0bdccb6497a7f23a2bd83c922fed57654536301c
Elena Baidakova <lena.baydakova@gmail.com>
2022-11-24 20:21:43 +0400

    Update member activity dropdown (#15875)
    closes TryGhost/Team#2250

--- ghost/admin/app/components/members-activity/event-type-filter.js
+++ ghost/admin/app/components/members-activity/event-type-filter.js
@@ -3,34 +3,45 @@ import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
 
 // todo: replace function with const after suppressionList feature flag will be removed
-const ALL_EVENT_TYPES = feature => ([
-    {event: 'signup_event', icon: 'event-filter-signup', name: 'Signups'},
-    {event: 'login_event', icon: 'event-filter-login', name: 'Logins'},
-    {event: 'subscription_event', icon: 'event-filter-subscription', name: 'Paid subscriptions'},
-    {event: 'payment_event', icon: 'event-filter-payment', name: 'Payments'},
-    {event: 'newsletter_event', icon: 'event-filter-newsletter', name: 'Email subscriptions'},
-    {event: 'email_opened_event', icon: 'event-filter-email-opened', name: 'Email opens'},
-    {event: 'email_delivered_event', icon: 'event-filter-email-delivered', name: 'Email deliveries'},
-    {event: 'email_failed_event', icon: 'event-filter-email-failed', name: feature.suppressionList ? 'Email bounces' : 'Email failures'}
-]);
+const ALL_EVENT_TYPES = [
+    {event: 'signup_event', icon: 'filter-dropdown-signups', name: 'Signups'},
+    {event: 'login_event', icon: 'filter-dropdown-logins', name: 'Logins'},
+    {event: 'subscription_event', icon: 'filter-dropdown-paid-subscriptions', name: 'Paid subscriptions'},
+    {event: 'payment_event', icon: 'filter-dropdown-payments', name: 'Payments'},
+    {event: 'newsletter_event', icon: 'filter-dropdown-email-subscriptions', name: 'Email subscriptions'},
+    {event: 'email_opened_event', icon: 'filter-dropdown-email-opened', name: 'Email opens'},
+    {event: 'email_delivered_event', icon: 'filter-dropdown-email-received', name: 'Email deliveries'},
+    {event: 'email_failed_event', icon: 'filter-dropdown-email-bounced', name: 'Email failures'}
+];
+
+const ALL_EVENT_TYPES_SUPPRESSION = [
+    {event: 'signup_event', icon: 'filter-dropdown-signups', name: 'Signups', group: 'auth'},
+    {event: 'login_event', icon: 'filter-dropdown-logins', name: 'Logins', group: 'auth'},
+    {event: 'subscription_event', icon: 'filter-dropdown-paid-subscriptions', name: 'Paid subscriptions', group: 'payments'},
+    {event: 'payment_event', icon: 'filter-dropdown-payments', name: 'Payments', group: 'payments'},
+    {event: 'newsletter_event', icon: 'filter-dropdown-email-subscriptions', name: 'Email subscriptions', group: 'emails'},
+    {event: 'email_opened_event', icon: 'filter-dropdown-email-opened', name: 'Email opened', group: 'emails'},
+    {event: 'email_delivered_event', icon: 'filter-dropdown-email-received', name: 'Email received', group: 'emails'},
+    {event: 'email_complaint_event', icon: 'filter-dropdown-email-flagged-as-spam', name: 'Email flagged as spam', group: 'emails'},
+    {event: 'email_failed_event', icon: 'filter-dropdown-email-bounced', name: 'Email bounced', group: 'emails'}
+];
 
 export default class MembersActivityEventTypeFilter extends Component {
     @service settings;
     @service feature;
 
     get availableEventTypes() {
-        const extended = [...ALL_EVENT_TYPES(this.feature)];
-        if (this.feature.suppressionList) {
-            extended.push({event: 'email_complaint_event', icon: 'event-filter-email-spam', name: 'Email spam'});
-        }
+        // todo: remove condition when feature will be enabled
+        const extended = this.feature.suppressionList ? [...ALL_EVENT_TYPES_SUPPRESSION] : [...ALL_EVENT_TYPES];
+
         if (this.settings.commentsEnabled !== 'off') {
-            extended.push({event: 'comment_event', icon: 'event-comment', name: 'Comments'});
-        }
-        if (this.settings.emailTrackClicks) {
-            extended.push({event: 'click_event', icon: 'event-click', name: 'Clicked link'});
+            extended.push({event: 'comment_event', icon: 'filter-dropdown-comments', name: 'Comments', group: 'others'});
         }
         if (this.feature.audienceFeedback) {
-            extended.push({event: 'feedback_event', icon: 'event-more-like-this', name: 'Feedback'});
+            extended.push({event: 'feedback_event', icon: 'filter-dropdown-feedback', name: 'Feedback', group: 'others'});
+        }
+        if (this.settings.emailTrackClicks) {
+            extended.push({event: 'click_event', icon: 'filter-dropdown-clicked-in-email', name: 'Clicked link in email', group: 'others'});
         }
 
         if (this.args.hiddenEvents?.length) {
@@ -43,12 +54,20 @@ export default class MembersActivityEventTypeFilter extends Component {
     get eventTypes() {
         const excludedEvents = (this.args.excludedEvents || '').split(',');
 
-        return this.availableEventTypes.map(type => ({
+        return this.availableEventTypes.map((type, i) => ({
             event: type.event,
             icon: type.icon,
             name: type.name,
+            divider: this.needDivider(type, this.availableEventTypes[i - 1]),
             isSelected: !excludedEvents.includes(type.event)
         }));
+    }
+
+    needDivider(event, prevEvent) {
+        if (!event?.group || !prevEvent?.group) {
+            return false;
+        }
+        return event.group !== prevEvent.group;
     }
 
     @action
