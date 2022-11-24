e47376c5736a348953a46cc6f577477ea7261137
Sam Lord <hello@samlord.me>
2022-11-24 15:11:40 +0000

    Added working Playwright tests
    refs: https://github.com/TryGhost/Toolbox/issues/479

--- ghost/core/test/e2e-browser/admin.spec.js
+++ ghost/core/test/e2e-browser/admin.spec.js
@@ -1,92 +1,82 @@
 const {expect, test} = require('@playwright/test');
-const testUtils = require('../utils');
-
-/** @type {import('@playwright/test').Page} */
-let page;
-let baseURL;
-
-const setupGhost = async ({browser}) => {
-    const app = await testUtils.startGhost();
-    baseURL = `${app.url}ghost/`;
-
-    page = await browser.newPage();
-    await page.goto(`${baseURL}`);
-    await page.getByPlaceholder('The Daily Awesome').click();
-    await page.getByPlaceholder('The Daily Awesome').fill('The Local Test');
-    await page.getByPlaceholder('Jamie Larson').fill('Testy McTesterson');
-    await page.getByPlaceholder('jamie@example.com').fill('testy@example.com');
-    await page.getByPlaceholder('At least 10 characters').fill('Mc.T3ster$0n');
-    await page.getByPlaceholder('At least 10 characters').press('Enter');
-    await page.locator('.gh-done-pink').click();
-};
-
-const teardownGhost = async () => {
-    await testUtils.stopGhost();
-    await page.close();
-};
+const {setupGhost, setupStripe, createTier} = require('./utils');
+const ObjectID = require('bson-objectid').default;
 
 test.describe('Ghost Admin', () => {
-    test.describe('Without fixtures', () => {
-        test.beforeAll(setupGhost);
-        test.afterAll(teardownGhost);
-
-        test('Loads admin', async () => {
-            const response = await page.goto(baseURL);
-            expect(response.status()).toEqual(200);
-        });
-
-        test('Is setup correctly', async () => {
-            await page.goto(baseURL);
-            await expect(page.locator('.gh-nav-menu-details-sitetitle')).toHaveText(/The Local Test/);
-        });
+    test.beforeEach(async ({page}) => {
+        // Will do initial setup or sign-in to Ghost
+        await setupGhost(page);
     });
 
-    test.describe('With default fixtures', () => {
-        test.beforeAll(async ({browser}) => {
-            await setupGhost({browser});
-            await testUtils.initFixtures('default', 'users', 'members');
-        });
-        test.afterAll(teardownGhost);
+    test('Loads admin', async ({page}) => {
+        const response = await page.goto('/ghost');
+        expect(response.status()).toEqual(200);
+    });
 
-        test('Has a set of posts', async () => {
-            await page.goto(baseURL);
-            await page.locator('[data-test-nav="posts"]').click();
-            await page.locator('.gh-post-list-title').first().click();
-            await page.getByRole('button', {name: 'sidemenu'}).click();
-            const now = new Date();
-            const currentDate = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
-            await expect(page.getByPlaceholder('YYYY-MM-DD')).toHaveValue(currentDate);
+    test('Is setup correctly', async ({page}) => {
+        await page.goto('/ghost');
+        await expect(page.locator('.gh-nav-menu-details-sitetitle')).toHaveText(/The Local Test/);
+    });
+
+    test('Has a set of posts', async ({page}) => {
+        await page.goto('/ghost');
+        await page.locator('[data-test-nav="posts"]').click();
+        await page.locator('.gh-post-list-title').first().click();
+        await page.getByRole('button', {name: 'sidemenu'}).click();
+        const now = new Date();
+        const currentDate = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
+        await expect(page.getByPlaceholder('YYYY-MM-DD')).toHaveValue(currentDate);
+    });
+
+    test('Can create a tier and offer', async ({page}) => {
+        await setupStripe(page);
+        const tierName = 'New Test Tier';
+        await createTier(page, {
+            name: tierName,
+            monthlyPrice: 5,
+            yearlyPrice: 50
         });
 
-        // TODO: Saving a tier in test mode causes a stripe error
-        test('Can create a tier and offer', async () => {
-            const tierName = 'New Test Tier';
+        await page.goto('/ghost');
+        await page.locator('[data-test-nav="offers"]').click();
 
-            await page.goto(baseURL);
-            await page.locator('[data-test-nav="settings"]').click();
-            await page.locator('[data-test-nav="members-membership"]').click();
-            // Expand the premium tier list
-            await page.getByRole('button', {name: 'Expand'}).nth(1).click({
-                delay: 10 // Wait 10 milliseconds to ensure tier information appears correctly
-            });
-            await page.locator('.gh-btn-add-tier').click();
-            await page.locator('input[data-test-input="tier-name"]').first().fill(tierName);
-            await page.locator('#monthlyPrice').fill('5');
-            await page.locator('#yearlyPrice').fill('50');
-            await page.locator('[data-test-button="save-tier"]').click();
-            await page.waitForSelector('input[data-test-input="tier-name"]', {state: 'detached'});
-            await page.locator('[data-test-nav="offers"]').click();
-            await page.getByRole('link', {name: 'New offer'}).click();
-            await page.locator('[data-test-input="offer-name"]').fill('Get 5% Off!');
-            await page.locator('input#amount').fill('5');
-            const priceId = await page.locator(`.gh-select-product-cadence>select>option[text()="${tierName} - Monthly"]`).getAttribute('value');
-            await page.locator('.gh-select-product-cadence>select').selectOption(priceId);
-            await page.getByRole('button', {name: 'Save'}).click();
-            // Click the "offers" link to go back
-            await page.locator('[data-test-link="offers-back"]').click();
-            await expect(page.locator('.gh-offers-list')).toContainText(/1 active offer/i);
-            await expect(page.locator('.gh-offers-list')).toContainText(/\$4.75/);
-            await expect(page.locator('.gh-offers-list')).toContainText(/New Test Tier/);
+        // Keep offer names unique
+        let offerName = `Get 5% Off! (${new ObjectID()})`;
+
+        if (await page.locator('.gh-offers-list').isVisible()) {
+            const listItem = page.locator('[data-test-list="offers-list-item"]').first();
+            while (await page.locator('[data-test-list="offers-list-item"]').first().isVisible().then(() => true).catch(() => false)) {
+                await listItem.getByRole('link', {name: 'arrow-right'}).click();
+                await page.getByRole('button', {name: 'Archive offer'}).click();
+                await page
+                    .locator('.modal-content')
+                    .filter({hasText: 'Archive offer'})
+                    .first()
+                    .getByRole('button', {name: 'Archive'})
+                    .click();
+
+                const statusDropdown = await page.getByRole('button', {name: 'Archived offers arrow-down-small'});
+                if (await statusDropdown.isVisible()) {
+                    await statusDropdown.click();
+                    await page.getByRole('option', {name: 'Active offers'}).click();
+                }
+            }
+        }
+
+        await page.getByRole('link', {name: 'New offer'}).click();
+        await page.locator('[data-test-input="offer-name"]').fill(offerName);
+        await page.locator('input#amount').fill('5');
+        const priceId = await page.locator(`.gh-select-product-cadence>select>option`).getByText(`${tierName} - Monthly`).getAttribute('value');
+        await page.locator('.gh-select-product-cadence>select').selectOption(priceId);
+        await page.getByRole('button', {name: 'Save'}).click();
+        await page.locator('[data-test-button="save"] [data-test-task-button-state="success"]').waitFor({
+            state: 'visible',
+            timeout: 1000
         });
+        // Click the "offers" link to go back
+        await page.locator('[data-test-nav="offers"]').click();
+        await page.locator('.gh-offers-list').waitFor({state: 'visible', timeout: 1000});
+        await expect(page.locator('.gh-offers-list')).toContainText(tierName);
+        await expect(page.locator('.gh-offers-list')).toContainText(offerName);
     });
 });
