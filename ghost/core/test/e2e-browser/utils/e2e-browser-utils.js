eb71e1a5c194131016cfed2920d4d93781d6dba0
Sam Lord <hello@samlord.me>
2022-11-24 17:46:58 +0000

    Added sample Portal test to PlayWright suite
    refs: https://github.com/TryGhost/Toolbox/issues/479

--- ghost/core/test/e2e-browser/utils/e2e-browser-utils.js
+++ ghost/core/test/e2e-browser/utils/e2e-browser-utils.js
@@ -1,4 +1,5 @@
 const DataGenerator = require('../../utils/fixtures/data-generator');
+const ObjectID = require('bson-objectid').default;
 
 /**
  * Setup Ghost Admin, or login if there's a login prompt
@@ -28,9 +29,13 @@ const setupGhost = async (page) => {
         page.locator('.gh-nav').waitFor(options).then(() => actions.noAction).catch(() => {})
     ]);
 
-    // TODO: Use env variables in CI
     // Add owner user data from usual fixture
     const ownerUser = DataGenerator.Content.users.find(user => user.id === '1');
+
+    if (process.env.CI && process.env.TEST_URL) {
+        ownerUser.email = process.env.TEST_OWNER_EMAIL;
+        ownerUser.password = process.env.TEST_OWNER_PASSWORD;
+    }
 
     if (action === actions.signin) {
         // Fill email + password
@@ -55,6 +60,7 @@ const setupGhost = async (page) => {
     }
 };
 
+// Only ever setup Stripe once, for performance reasons
 let isStripeSetup = false;
 
 /**
@@ -130,8 +136,79 @@ const createTier = async (page, {name, monthlyPrice, yearlyPrice}) => {
     await page.waitForSelector('input[data-test-input="tier-name"]', {state: 'detached'});
 };
 
+/**
+ * Create an offer on a tier
+ * @param {import('@playwright/test').Page} page
+ * @param {object} options
+ * @param {string} options.name
+ * @param {string} options.tierName
+ * @param {number} options.percentOff
+ * @returns {Promise<string>} Unique offer name
+ */
+const createOffer = async (page, {name, tierName, percentOff}) => {
+    await page.goto('/ghost');
+    await page.locator('[data-test-nav="offers"]').click();
+
+    // Keep offer names unique & <= 40 characters
+    let offerName = `${name} (${new ObjectID().toHexString().slice(0, 40 - name.length - 3)})`;
+
+    // Archive other offers to keep the list tidy
+    // We only need 1 offer to be active at a time
+    // Either the list of active offers loads, or the CTA when no offers exist
+    while (await Promise.race([
+        page.locator('.gh-offers-list .gh-list-header').filter({hasText: 'active'}).waitFor({state: 'visible', timeout: 1000}).then(() => true).catch(() => false),
+        page.locator('.gh-offers-list-cta').waitFor({state: 'visible', timeout: 1000}).then(() => false).catch(() => false)
+    ])) {
+        const listItem = page.locator('[data-test-list="offers-list-item"]').first();
+        await listItem.getByRole('link', {name: 'arrow-right'}).click();
+        await page.getByRole('button', {name: 'Archive offer'}).click();
+        await page
+            .locator('.modal-content')
+            .filter({hasText: 'Archive offer'})
+            .first()
+            .getByRole('button', {name: 'Archive'})
+            .click();
+
+        // TODO: Use a more resilient selector
+        const statusDropdown = await page.getByRole('button', {name: 'Archived offers arrow-down-small'});
+        await statusDropdown.waitFor({
+            state: 'visible',
+            timeout: 1000
+        });
+        await statusDropdown.click();
+        await page.getByRole('option', {name: 'Active offers'}).click();
+    }
+
+    await page.getByRole('link', {name: 'New offer'}).click();
+    await page.locator('[data-test-input="offer-name"]').fill(offerName);
+    await page.locator('input#amount').fill(`${percentOff}`);
+    const priceId = await page.locator(`.gh-select-product-cadence>select>option`).getByText(`${tierName} - Monthly`).getAttribute('value');
+    await page.locator('.gh-select-product-cadence>select').selectOption(priceId);
+    await page.getByRole('button', {name: 'Save'}).click();
+    // Wait for the "Saved" button, ensures that next clicks don't trigger the unsaved work modal
+    await page.locator('[data-test-button="save"] [data-test-task-button-state="success"]').waitFor({
+        state: 'visible',
+        timeout: 1000
+    });
+    await page.locator('[data-test-nav="offers"]').click();
+
+    return offerName;
+};
+
+const completeStripeSubscription = async (page) => {
+    await page.locator('#cardNumber').fill('4242 4242 4242 4242');
+    await page.locator('#cardExpiry').fill('04 / 24');
+    await page.locator('#cardCvc').fill('424');
+    await page.locator('#billingName').fill('Testy McTesterson');
+    await page.getByRole('combobox', {name: 'Country or region'}).selectOption('US');
+    await page.locator('#billingPostalCode').fill('42424');
+    await page.getByTestId('hosted-payment-submit-button').click();
+};
+
 module.exports = {
     setupGhost,
     setupStripe,
-    createTier
+    createTier,
+    createOffer,
+    completeStripeSubscription
 };
