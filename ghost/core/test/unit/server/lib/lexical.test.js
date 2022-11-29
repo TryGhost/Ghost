b118c8876851f6dfd099123e24f471d88415f0b6
Kevin Ansfield <kevin@ghost.org>
2022-11-29 16:57:01 +0000

    Added URL transform for image cards in Lexical documents (#15890)
    refs https://github.com/TryGhost/Team/issues/2225
    
    - updated the `formatOnWrite` transform map for posts to include the new `nodes` and `transformMap` options used by `urlUtils` for transforming node payload data
    - added `nodes` to the `lexicalLib` module that pulls in our default nodes to be passed in to the URL transform utilities
    - added `urlTransformMap` to the `lexicalLib` module that maps transform type and data type to URL transform utility functions that accept a single URL argument

--- ghost/core/test/unit/server/lib/lexical.test.js
+++ ghost/core/test/unit/server/lib/lexical.test.js
@@ -9,5 +9,37 @@ describe('lib/lexical', function () {
             lexicalLib.lexicalHtmlRenderer.render(lexical)
                 .should.eql('<p>Lexical is <strong><em>rendering.</em></strong></p>');
         });
+
+        it('renders all default cards', function () {
+            const lexicalState = JSON.stringify({
+                root: {
+                    children: [
+                        {
+                            type: 'image',
+                            cardWidth: 'wide',
+                            src: '/content/images/2018/04/NatGeo06.jpg',
+                            width: 4000,
+                            height: 2000,
+                            caption: 'Birdies'
+                        }
+                    ],
+                    direction: null,
+                    format: '',
+                    indent: 0,
+                    type: 'root',
+                    version: 1
+                }
+            });
+
+            lexicalLib.lexicalHtmlRenderer.render(lexicalState)
+                .should.eql(`
+        <figure class="kg-card kg-image-card kg-width-wide">
+            <img src="/content/images/2018/04/NatGeo06.jpg" alt="" />
+                <figcaption>
+                Birdies
+                </figcaption>
+        </figure>
+        `);
+        });
     });
 });
