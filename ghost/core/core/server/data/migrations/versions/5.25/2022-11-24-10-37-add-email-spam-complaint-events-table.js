ebcf0ace00365c7402afd25f2973b61b5572ecb8
Fabien "egg" O'Carroll <fabien@allou.is>
2022-11-30 22:48:58 +0700

    Added unique constraint to email_spam_complaint_events table
    We can fetch the same event multiple times from Mailgun so we need to
    be able to protect against inserting duplicate events in the
    database. This will allow us to catch duplicate errors on insert when
    handling complaint events.

--- ghost/core/core/server/data/migrations/versions/5.25/2022-11-24-10-37-add-email-spam-complaint-events-table.js
+++ ghost/core/core/server/data/migrations/versions/5.25/2022-11-24-10-37-add-email-spam-complaint-events-table.js
@@ -5,5 +5,8 @@ module.exports = addTable('email_spam_complaint_events', {
     member_id: {type: 'string', maxlength: 24, nullable: false, references: 'members.id', cascadeDelete: true},
     email_id: {type: 'string', maxlength: 24, nullable: false, references: 'emails.id'},
     email_address: {type: 'string', maxlength: 191, nullable: false, unique: false},
-    created_at: {type: 'dateTime', nullable: false}
+    created_at: {type: 'dateTime', nullable: false},
+    '@@UNIQUE_CONSTRAINTS@@': [
+        ['email_id', 'member_id']
+    ]
 });
--- ghost/core/core/server/data/schema/schema.js
+++ ghost/core/core/server/data/schema/schema.js
@@ -972,6 +972,9 @@ module.exports = {
         member_id: {type: 'string', maxlength: 24, nullable: false, references: 'members.id', cascadeDelete: true},
         email_id: {type: 'string', maxlength: 24, nullable: false, references: 'emails.id'},
         email_address: {type: 'string', maxlength: 191, nullable: false, unique: false, validations: {isEmail: true}},
-        created_at: {type: 'dateTime', nullable: false}
+        created_at: {type: 'dateTime', nullable: false},
+        '@@UNIQUE_CONSTRAINTS@@': [
+            ['email_id', 'member_id']
+        ]
     }
 };
