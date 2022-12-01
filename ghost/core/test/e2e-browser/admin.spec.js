d41b64f8bfb7a6be34c9207bf925d77b4fadf785
Sam Lord <sam@ghost.org>
2022-12-01 00:34:19 +0000

    Allow playwright tests to run locally and remotely
    no issue
    
    This commit allows tests to run remotely by replacing selectors with production-suitable ones (no [data-test...]).
    
    It also allows running locally with Stripe webhooks by adding a new global setup function.

--- ghost/core/test/e2e-browser/admin.spec.js
+++ ghost/core/test/e2e-browser/admin.spec.js
@@ -1,12 +1,7 @@
 const {expect, test} = require('@playwright/test');
-const {setupGhost, setupStripe, createTier, createOffer} = require('./utils');
+const {setupGhost, createTier, createOffer} = require('./utils');
 
 test.describe('Ghost Admin', () => {
-    test.beforeEach(async ({page}) => {
-        // Will do initial setup or sign-in to Ghost
-        await setupGhost(page);
-    });
-
     test('Loads admin', async ({page}) => {
         const response = await page.goto('/ghost');
         expect(response.status()).toEqual(200);
@@ -19,16 +14,16 @@ test.describe('Ghost Admin', () => {
 
     test('Has a set of posts', async ({page}) => {
         await page.goto('/ghost');
-        await page.locator('[data-test-nav="posts"]').click();
+        await page.locator('.gh-nav a[href="#/posts/"]').click();
         await page.locator('.gh-post-list-title').first().click();
-        await page.getByRole('button', {name: 'sidemenu'}).click();
+        await page.locator('.settings-menu-toggle').click();
         const now = new Date();
         const currentDate = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`;
         await expect(page.getByPlaceholder('YYYY-MM-DD')).toHaveValue(currentDate);
     });
 
     test('Can create a tier and offer', async ({page}) => {
-        await setupStripe(page);
+        await page.goto('/ghost');
         const tierName = 'New Test Tier';
         await createTier(page, {
             name: tierName,
@@ -41,7 +36,7 @@ test.describe('Ghost Admin', () => {
             percentOff: 5
         });
 
-        await page.locator('[data-test-nav="offers"]').click();
+        await page.locator('.gh-nav a[href="#/offers/"]').click();
         await page.locator('.gh-offers-list').waitFor({state: 'visible', timeout: 1000});
         await expect(page.locator('.gh-offers-list')).toContainText(tierName);
         await expect(page.locator('.gh-offers-list')).toContainText(offerName);
