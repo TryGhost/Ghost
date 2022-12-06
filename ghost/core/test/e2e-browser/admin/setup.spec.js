97e5ca1de6ad63483b2573be487e55237fd8c079
Daniel Lockyer <hi@daniellockyer.com>
2022-12-06 19:05:58 +0700

    Extracted Playwright tests into separate files
    - we should start to keep tests grouped by their area, so first we split
      by Admin tests and then Portal tests, and within that we split into
      setup/Tiers/Offers etc

---
+++ ghost/core/test/e2e-browser/admin/setup.spec.js
@@ -0,0 +1,13 @@
+const {expect, test} = require('@playwright/test');
+
+test.describe('Setup', () => {
+    test('Loads Admin', async ({page}) => {
+        const response = await page.goto('/ghost');
+        expect(response.status()).toEqual(200);
+    });
+
+    test('Is setup correctly', async ({page}) => {
+        await page.goto('/ghost');
+        await expect(page.locator('.gh-nav-menu-details-sitetitle')).toHaveText(/The Local Test/);
+    });
+});
