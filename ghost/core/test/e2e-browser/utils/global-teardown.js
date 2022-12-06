6f5ec7188bfa6eb4339315d436ba19c53f09ce65
Naz <hi@nazavo.com>
2022-12-06 17:30:07 +0700

    Fixed local environment for playwright tests
    refs https://github.com/TryGhost/Toolbox/issues/479
    
    - this includes a handful of improvements to get Playwright working on a
      local environment including:
      - adding `testing-browser` environment so we don't nuke `development`
        environments, and makes all the necessary changes to get Ghost to
        behave when this is running
      - stopped running one global instance of Ghost as this doesn't provide
        a clean environment
      - copies a few default fixtures that are needed for the new
        environment

---
+++ ghost/core/test/e2e-browser/utils/global-teardown.js
@@ -0,0 +1,8 @@
+/**
+ * Teardown the environment
+ */
+const teardown = async (playwrightConfig) => {
+    // @NOTE: local environment should probably drop the db state here
+};
+
+module.exports = teardown;
