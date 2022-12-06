101bfca41e9c19d59e9de84fe8f89e9b770734ef
Daniel Lockyer <hi@daniellockyer.com>
2022-12-06 19:05:58 +0700

    Extracted Playwright tests into separate files
    - we should start to keep tests grouped by their area, so first we split
      by Admin tests and then Portal tests, and within that we split into
      setup/Tiers/Offers etc

---
+++ ghost/core/test/e2e-browser/portal/offers.spec.js
@@ -0,0 +1,39 @@
+const {expect, test} = require('@playwright/test');
+const {deleteAllMembers, createTier, createOffer, completeStripeSubscription} = require('../utils');
+
+test.describe('Offers', () => {
+    test('Uses an offer successfully', async ({page}) => {
+        page.goto('/ghost');
+        await deleteAllMembers(page);
+        const tierName = 'Portal Tier';
+        await createTier(page, {
+            name: tierName,
+            monthlyPrice: 6,
+            yearlyPrice: 60
+        });
+        const offerName = await createOffer(page, {
+            name: 'Black Friday Special',
+            tierName: tierName,
+            percentOff: 10
+        });
+
+        await page.locator('.gh-offers-list .gh-list-row').filter({hasText: offerName}).click();
+        const portalUrl = await page.locator('input#url').inputValue();
+
+        await page.goto(portalUrl);
+        const portalFrame = page.frameLocator('#ghost-portal-root div iframe');
+        await portalFrame.locator('#input-name').fill('Testy McTesterson');
+        await portalFrame.locator('#input-email').fill('testy@example.com');
+        await portalFrame.getByRole('button', {name: 'Continue'}).click();
+
+        await completeStripeSubscription(page);
+
+        await page.waitForSelector('h1.site-title', {state: 'visible'});
+        await page.goto('/ghost');
+        await page.locator('.gh-nav a[href="#/members/"]').click();
+
+        // 1 member, should be Testy, on Portal Tier
+        await expect(page.getByRole('link', {name: 'Testy McTesterson testy@example.com'}), 'Should have 1 paid member').toBeVisible();
+        await expect(page.getByRole('link', {name: tierName}), `Paid member should be on ${tierName}`).toBeVisible();
+    });
+});
