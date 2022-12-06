2ad7d2607cf5475ff357eac0208ad207a644475e
Daniel Lockyer <hi@daniellockyer.com>
2022-12-06 19:18:16 +0700

    Added test group names
    - this helps differentiate between the different tests we have available

--- ghost/core/test/e2e-browser/admin/tiers.spec.js
+++ ghost/core/test/e2e-browser/admin/tiers.spec.js
@@ -1,24 +1,26 @@
 const {expect, test} = require('@playwright/test');
 const {createTier, createOffer} = require('../utils');
 
-test.describe('Tiers', () => {
-    test('Can create a Tier and Offer', async ({page}) => {
-        await page.goto('/ghost');
-        const tierName = 'New Test Tier';
-        await createTier(page, {
-            name: tierName,
-            monthlyPrice: 5,
-            yearlyPrice: 50
-        });
-        const offerName = await createOffer(page, {
-            name: 'Get 5% Off!',
-            tierName,
-            percentOff: 5
-        });
+test.describe('Admin', () => {
+    test.describe('Tiers', () => {
+        test('Can create a Tier and Offer', async ({page}) => {
+            await page.goto('/ghost');
+            const tierName = 'New Test Tier';
+            await createTier(page, {
+                name: tierName,
+                monthlyPrice: 5,
+                yearlyPrice: 50
+            });
+            const offerName = await createOffer(page, {
+                name: 'Get 5% Off!',
+                tierName,
+                percentOff: 5
+            });
 
-        await page.locator('.gh-nav a[href="#/offers/"]').click();
-        await page.locator('.gh-offers-list').waitFor({state: 'visible', timeout: 1000});
-        await expect(page.locator('.gh-offers-list')).toContainText(tierName);
-        await expect(page.locator('.gh-offers-list')).toContainText(offerName);
+            await page.locator('.gh-nav a[href="#/offers/"]').click();
+            await page.locator('.gh-offers-list').waitFor({state: 'visible', timeout: 1000});
+            await expect(page.locator('.gh-offers-list')).toContainText(tierName);
+            await expect(page.locator('.gh-offers-list')).toContainText(offerName);
+        });
     });
 });
--- ghost/core/test/e2e-browser/portal/frontend.spec.js
+++
@@ -1,8 +0,0 @@
-const {expect, test} = require('@playwright/test');
-
-test.describe('Frontend', () => {
-    test('Loads the homepage', async ({page}) => {
-        const response = await page.goto('/');
-        expect(response.status()).toEqual(200);
-    });
-});
