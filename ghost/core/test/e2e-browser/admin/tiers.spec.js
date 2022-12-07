98cc999c1122fdefa32bac422da4ecb9ce144d8d
Djordje Vlaisavljevic <dzvlais@gmail.com>
2022-12-07 17:26:02 +0100

    Added playwright test for creating additional tier
    refs TryGhost/Team#2371
    
    - Tests that an additional tier can be created and that it'll appear in portal settings unselected by default

--- ghost/core/test/e2e-browser/admin/tiers.spec.js
+++ ghost/core/test/e2e-browser/admin/tiers.spec.js
@@ -23,5 +23,29 @@ test.describe('Admin', () => {
             await expect(page.locator('.gh-offers-list')).toContainText(tierName);
             await expect(page.locator('.gh-offers-list')).toContainText(offerName);
         });
+        test('Can create additional Tier', async ({page}) => {
+            await page.goto('/ghost');
+            const tierName = 'New Test Tier';
+            const enableInPortal = false;
+            await createTier(page, {
+                name: tierName,
+                monthlyPrice: 5,
+                yearlyPrice: 50
+            }, enableInPortal);
+        
+            // Open Portal settings
+            await page.locator('.gh-nav a[href="#/settings/"]').click();
+            await page.locator('.gh-setting-group').filter({hasText: 'Membership'}).click();
+            await page.locator('[data-test-toggle="portal-settings"]').click();
+        
+            // Wait until the list of tiers available at signup is visible
+            await page.locator('[data-test-tiers-at-signup]').first().waitFor({state: 'visible', timeout: 1000});
+        
+            // Make sure newly created tier is in the list
+            await expect(page.locator('[data-test-tier-at-signup] > label > p').last()).toContainText(tierName);
+
+            // Make sure newly created tier is in not selected
+            expect(await page.locator('[data-test-tier-at-signup] > label > input').last().isChecked()).toBeFalsy();
+        });
     });
 });
