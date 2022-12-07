b53c2c565916afd2a071b4e92a99011dbd1b6c21
Ronald Langeveld <hi@ronaldlangeveld.com>
2022-12-07 13:02:48 +0700

    Added Playwright test for editing a member in admin ref https://github.com/TryGhost/Team/issues/2371
    - tests for editing a member inside admin

--- ghost/core/test/e2e-browser/admin/members.spec.js
+++ ghost/core/test/e2e-browser/admin/members.spec.js
@@ -25,5 +25,27 @@ test.describe('Admin', () => {
             const memberEmail = page.locator('tbody > tr > a > div > div > p').nth(0);
             await expect(memberEmail).toHaveText(email);
         });
+
+        test('A member can be edited', async ({page}) => {
+            await page.goto('/ghost');
+            await page.locator('.gh-nav a[href="#/members/"]').click();
+            await page.locator('tbody > tr > a').nth(0).click();
+            await page.waitForSelector('input[name="name"]');
+            let name = 'Test Member Edited';
+            let email = 'tester.edited@example.com';
+            let note = 'This is an edited test member';
+            await page.fill('input[name="name"]', name);
+            await page.fill('input[name="email"]', email);
+            await page.fill('textarea[name="note"]', note);
+            await page.locator('button span:has-text("Save")').click();
+            await page.waitForSelector('button span:has-text("Saved")');
+            await page.locator('.gh-nav a[href="#/members/"]').click();
+            const count = await page.locator('tbody > tr').count();
+            expect(count).toBe(1);
+            const member = page.locator('tbody > tr > a > div > div > h3').nth(0);
+            await expect(member).toHaveText(name);
+            const memberEmail = page.locator('tbody > tr > a > div > div > p').nth(0);
+            await expect(memberEmail).toHaveText(email);
+        });
     });
 });
