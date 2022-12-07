d64d8c0ddd8245f18e8b9b664c3d80614cc16f12
Daniel Lockyer <hi@daniellockyer.com>
2022-12-07 11:29:05 +0700

    Added Playwright test for creating and using a free-trial Offer
    refs https://www.notion.so/ghost/Critical-Paths-980dd089c3e74a6fbc619271f5a9ce42
    
    - this test will create a free-trial Offer and go through Stripe
      checkout, ensuring the member is created as paid
    - also changes some utils to support creating the free-trial offer vs
      just a discounted Offer

--- ghost/core/test/e2e-browser/portal/offers.spec.js
+++ ghost/core/test/e2e-browser/portal/offers.spec.js
@@ -3,7 +3,44 @@ const {deleteAllMembers, createTier, createOffer, completeStripeSubscription} = 
 
 test.describe('Portal', () => {
     test.describe('Offers', () => {
-        test('Uses an offer successfully', async ({page}) => {
+        test('Creates and uses a free-trial Offer', async ({page}) => {
+            page.goto('/ghost');
+            await deleteAllMembers(page);
+            const tierName = 'Portal Tier';
+            await createTier(page, {
+                name: tierName,
+                monthlyPrice: 6,
+                yearlyPrice: 60
+            });
+
+            const offerName = await createOffer(page, {
+                name: 'Black Friday Special',
+                tierName,
+                offerType: 'freeTrial',
+                amount: 14
+            });
+
+            await page.locator('.gh-offers-list .gh-list-row').filter({hasText: offerName}).click();
+            const portalUrl = await page.locator('input#url').inputValue();
+
+            await page.goto(portalUrl);
+            const portalFrame = page.frameLocator('#ghost-portal-root div iframe');
+            await portalFrame.locator('#input-name').fill('Testy McTesterson');
+            await portalFrame.locator('#input-email').fill('testy@example.com');
+            await portalFrame.getByRole('button', {name: 'Start 14-day free trial'}).click();
+
+            await completeStripeSubscription(page);
+
+            await page.waitForSelector('h1.site-title', {state: 'visible'});
+            await page.goto('/ghost');
+            await page.locator('.gh-nav a[href="#/members/"]').click();
+
+            // 1 member, should be Testy, on Portal Tier
+            await expect(page.getByRole('link', {name: 'Testy McTesterson testy@example.com'}), 'Should have 1 paid member').toBeVisible();
+            await expect(page.getByRole('link', {name: tierName}), `Paid member should be on ${tierName}`).toBeVisible();
+        });
+
+        test('Creates and uses a discount Offer', async ({page}) => {
             page.goto('/ghost');
             await deleteAllMembers(page);
             const tierName = 'Portal Tier';
@@ -15,7 +52,8 @@ test.describe('Portal', () => {
             const offerName = await createOffer(page, {
                 name: 'Black Friday Special',
                 tierName: tierName,
-                percentOff: 10
+                offerType: 'discount',
+                amount: 10
             });
 
             await page.locator('.gh-offers-list .gh-list-row').filter({hasText: offerName}).click();
