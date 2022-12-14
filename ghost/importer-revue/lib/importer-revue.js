939c7b2b860cf7efc4d17b6abddebfcae58e9fb2
Hannah Wolfe <github.erisds@gmail.com>
2022-12-14 23:06:48 +0000

    Wired up pauls html processing

--- ghost/importer-revue/lib/importer-revue.js
+++ ghost/importer-revue/lib/importer-revue.js
@@ -1,5 +1,85 @@
 const papaparse = require('papaparse');
 const _ = require('lodash');
+const SimpleDom = require('simple-dom');
+const serializer = new SimpleDom.HTMLSerializer(SimpleDom.voidMap);
+const imageCard = require('@tryghost/kg-default-cards/lib/cards/image.js');
+const bookmarkCard = require('@tryghost/kg-default-cards/lib/cards/bookmark.js');
+const embedCard = require('@tryghost/kg-default-cards/lib/cards/embed.js');
+
+// Take the array of items for a specific post and return the converted HTML
+const convertItemToHTML = (items) => {
+    let itemHTMLChunks = [];
+    items.forEach((item) => {
+        let type = item.item_type;
+        if (type === 'header') {
+            itemHTMLChunks.push(`<h3>${item.title}</h3>`);
+        } else if (type === 'text') {
+            itemHTMLChunks.push(item.description); // THis is basic text HTML with <p>, <b>, <a>, etc (no media)
+        } else if (type === 'image') {
+            // We have 2 values to work with here. `image` is smaller and most suitable, and `original_image_url` is the full-res that would need to be resized
+            // - item.image (https://s3.amazonaws.com/revue/items/images/019/005/542/web/anita-austvika-C-JUrfmYqcw-unsplash.jpg?1667924147)
+            // - item.original_image_url (https://s3.amazonaws.com/revue/items/images/019/005/542/original/anita-austvika-C-JUrfmYqcw-unsplash.jpg?1667924147)
+            let cardOpts = {
+                env: {dom: new SimpleDom.Document()},
+                payload: {
+                    src: item.image,
+                    caption: item.description
+                }
+            };
+
+            itemHTMLChunks.push(serializer.serialize(imageCard.render(cardOpts)));
+        } else if (type === 'link') {
+            // Embedded link card
+            let cardOpts = {
+                env: {dom: new SimpleDom.Document()},
+                payload: {
+                    url: item.url,
+                    metadata: {
+                        url: item.url,
+                        title: item.title,
+                        description: item.description,
+                        thumbnail: item.image
+                    }
+                }
+            };
+
+            itemHTMLChunks.push(serializer.serialize(bookmarkCard.render(cardOpts)));
+        } else if (type === 'tweet') {
+            // Should this be an oEmbed call? Probably.
+            itemHTMLChunks.push(`<figure class="kg-card kg-embed-card">
+                <blockquote class="twitter-tweet"><a href="${item.url}"></a></blockquote>
+                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                </figure>`);
+        } else if (type === 'video') {
+            const isLongYouTube = /youtube.com/.test(item.url);
+            const isShortYouTube = /youtu.be/.test(item.url);
+            const isVimeo = /vimeo.com/.test(item.url);
+            let videoHTML = '';
+            if (isLongYouTube) {
+                let videoID = item.url.replace(/https?:\/\/(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]*)/gi, '$1');
+                videoHTML = `<iframe width="200" height="113" src="https://www.youtube.com/embed/${videoID}?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+            } else if (isShortYouTube) {
+                let videoID = item.url.replace(/https?:\/\/(?:www\.)?youtu\.be\/([a-zA-Z0-9_-]*)/gi, '$1');
+                videoHTML = `<iframe width="200" height="113" src="https://www.youtube.com/embed/${videoID}?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+            } else if (isVimeo) {
+                let videoID = item.url.replace(/vimeo.com\/([0-9]+)/gm, '$1');
+                videoHTML = `<iframe src="https://player.vimeo.com/video/${videoID}" width="200" height="113" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>`;
+            }
+            let cardOpts = {
+                env: {dom: new SimpleDom.Document()},
+                payload: {
+                    html: videoHTML,
+                    caption: item.description
+                }
+            };
+
+            itemHTMLChunks.push(serializer.serialize(embedCard.render(cardOpts)));
+        } else {
+            // This is all the cards we know about right now
+        }
+    });
+    return itemHTMLChunks.join('\n');
+};
 
 const RevueImporter = {
     type: 'revue',
@@ -12,8 +92,17 @@ const RevueImporter = {
             return importData;
         }
 
-        const csvData = papaparse.parse(importData.revue.revue.issues, {header: true});
-        // const jsonData = importData.revue.revue.items;
+        const csvData = papaparse.parse(importData.revue.revue.issues, {
+            header: true,
+            skipEmptyLines: true,
+            transform(value, header) {
+                if (header === 'id') {
+                    return parseInt(value);
+                }
+                return value;
+            }
+        });
+        const jsonData = importData.revue.revue.items;
 
         csvData.data.forEach((postMeta) => {
             // Convert issues to posts
@@ -26,12 +115,12 @@ const RevueImporter = {
             const revuePostID = postMeta.id;
             let postHTML = postMeta.description;
 
-            // const postItems = _.filter(jsonData, {issue_id: revuePostID});
-            // const sortedPostItems = (postItems) ? _.sortBy(postItems, o => o.order) : [];
-            // if (postItems) {
-            //     const convertedItems = convertItemToHTML(sortedPostItems);
-            //     postHTML = `${postMeta.description}${convertedItems}`;
-            // }
+            const postItems = _.filter(jsonData, {issue_id: revuePostID});
+            const sortedPostItems = (postItems) ? _.sortBy(postItems, o => o.order) : [];
+            if (postItems) {
+                const convertedItems = convertItemToHTML(sortedPostItems);
+                postHTML = `${postMeta.description}${convertedItems}`;
+            }
 
             posts.push({
                 comment_id: revuePostID,
