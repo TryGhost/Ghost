0e906bd82173aa73b5ef098dec53b96355f45adc
Hannah Wolfe <github.erisds@gmail.com>
2022-12-15 15:24:34 +0000

    Refactor and add unit tests

--- ghost/importer-revue/lib/importer-revue.js
+++ ghost/importer-revue/lib/importer-revue.js
@@ -1,86 +1,8 @@
 const debug = require('@tryghost/debug')('importer:revue');
 const papaparse = require('papaparse');
 const _ = require('lodash');
-const SimpleDom = require('simple-dom');
-const serializer = new SimpleDom.HTMLSerializer(SimpleDom.voidMap);
-const imageCard = require('@tryghost/kg-default-cards/lib/cards/image.js');
-const bookmarkCard = require('@tryghost/kg-default-cards/lib/cards/bookmark.js');
-const embedCard = require('@tryghost/kg-default-cards/lib/cards/embed.js');
 
-// Take the array of items for a specific post and return the converted HTML
-const convertItemToHTML = (items) => {
-    let itemHTMLChunks = [];
-    items.forEach((item) => {
-        let type = item.item_type;
-        if (type === 'header') {
-            itemHTMLChunks.push(`<h3>${item.title}</h3>`);
-        } else if (type === 'text') {
-            itemHTMLChunks.push(item.description); // THis is basic text HTML with <p>, <b>, <a>, etc (no media)
-        } else if (type === 'image') {
-            // We have 2 values to work with here. `image` is smaller and most suitable, and `original_image_url` is the full-res that would need to be resized
-            // - item.image (https://s3.amazonaws.com/revue/items/images/019/005/542/web/anita-austvika-C-JUrfmYqcw-unsplash.jpg?1667924147)
-            // - item.original_image_url (https://s3.amazonaws.com/revue/items/images/019/005/542/original/anita-austvika-C-JUrfmYqcw-unsplash.jpg?1667924147)
-            let cardOpts = {
-                env: {dom: new SimpleDom.Document()},
-                payload: {
-                    src: item.image,
-                    caption: item.description
-                }
-            };
-
-            itemHTMLChunks.push(serializer.serialize(imageCard.render(cardOpts)));
-        } else if (type === 'link') {
-            // Embedded link card
-            let cardOpts = {
-                env: {dom: new SimpleDom.Document()},
-                payload: {
-                    url: item.url,
-                    metadata: {
-                        url: item.url,
-                        title: item.title,
-                        description: item.description,
-                        thumbnail: item.image
-                    }
-                }
-            };
-
-            itemHTMLChunks.push(serializer.serialize(bookmarkCard.render(cardOpts)));
-        } else if (type === 'tweet') {
-            // Should this be an oEmbed call? Probably.
-            itemHTMLChunks.push(`<figure class="kg-card kg-embed-card">
-                <blockquote class="twitter-tweet"><a href="${item.url}"></a></blockquote>
-                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                </figure>`);
-        } else if (type === 'video') {
-            const isLongYouTube = /youtube.com/.test(item.url);
-            const isShortYouTube = /youtu.be/.test(item.url);
-            const isVimeo = /vimeo.com/.test(item.url);
-            let videoHTML = '';
-            if (isLongYouTube) {
-                let videoID = item.url.replace(/https?:\/\/(?:www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]*)/gi, '$1');
-                videoHTML = `<iframe width="200" height="113" src="https://www.youtube.com/embed/${videoID}?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-            } else if (isShortYouTube) {
-                let videoID = item.url.replace(/https?:\/\/(?:www\.)?youtu\.be\/([a-zA-Z0-9_-]*)/gi, '$1');
-                videoHTML = `<iframe width="200" height="113" src="https://www.youtube.com/embed/${videoID}?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-            } else if (isVimeo) {
-                let videoID = item.url.replace(/vimeo.com\/([0-9]+)/gm, '$1');
-                videoHTML = `<iframe src="https://player.vimeo.com/video/${videoID}" width="200" height="113" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>`;
-            }
-            let cardOpts = {
-                env: {dom: new SimpleDom.Document()},
-                payload: {
-                    html: videoHTML,
-                    caption: item.description
-                }
-            };
-
-            itemHTMLChunks.push(serializer.serialize(embedCard.render(cardOpts)));
-        } else {
-            // This is all the cards we know about right now
-        }
-    });
-    return itemHTMLChunks.join('\n');
-};
+const JSONToHTML = require('../lib/json-to-html');
 
 /**
  * Build posts out of the issue and item data
@@ -109,26 +31,27 @@ const fetchPostsFromData = (revueData) => {
             return;
         }
 
-        const isPublished = (postMeta.sent_at) ? true : false; // This is how we determine is a post is published or not
-        const postDate = (isPublished) ? new Date(postMeta.sent_at) : new Date();
         const revuePostID = postMeta.id;
         let postHTML = postMeta.description;
 
         const postItems = _.filter(itemData, {issue_id: revuePostID});
-        const sortedPostItems = (postItems) ? _.sortBy(postItems, o => o.order) : [];
+        const sortedPostItems = _.sortBy(postItems, o => o.order);
+
         if (postItems) {
-            const convertedItems = convertItemToHTML(sortedPostItems);
+            const convertedItems = JSONToHTML.itemsToHtml(sortedPostItems);
             postHTML = `${postMeta.description}${convertedItems}`;
         }
+
+        const postDate = JSONToHTML.getPostDate(postMeta);
 
         posts.push({
             comment_id: revuePostID,
             title: postMeta.subject,
-            status: (isPublished) ? 'published' : 'draft',
+            status: JSONToHTML.getStatus(postMeta),
             visibility: 'public',
-            created_at: postDate.toISOString(),
-            published_at: postDate.toISOString(),
-            updated_at: postDate.toISOString(),
+            created_at: postDate,
+            published_at: postDate,
+            updated_at: postDate,
             html: postHTML,
             tags: ['#revue']
 
@@ -180,14 +103,14 @@ const RevueImporter = {
             data: {}
         };
 
-        importData.data.data.posts = fetchPostsFromData(importData.revue.revue);
+        importData.data.data.posts = this.importPosts(importData.revue.revue);
 
         // No subscribers to import, we're done
         if (!importData?.revue?.revue?.subscribers) {
             return importData;
         }
 
-        importData.data.data.revue_subscribers = buildSubscriberList(importData.revue.revue);
+        importData.data.data.revue_subscribers = this.importSubscribers(importData.revue.revue);
 
         return importData;
     },
@@ -195,7 +118,10 @@ const RevueImporter = {
         debug('doImport');
 
         return importData;
-    }
+    },
+
+    importPosts: fetchPostsFromData,
+    importSubscribers: buildSubscriberList
 };
 
 module.exports = RevueImporter;
---
+++ ghost/importer-revue/test/importer-revue.test.js
@@ -0,0 +1,184 @@
+const assert = require('assert');
+const sinon = require('sinon');
+const RevueImporter = require('../index');
+
+const JSONToHTML = require('../lib/json-to-html');
+
+describe('Revue Importer', function () {
+    describe('preProcess', function () {
+        it('marks any object as processed', function () {
+            let result;
+
+            result = RevueImporter.preProcess({});
+            assert.deepEqual(result.preProcessedByRevue, true, 'marks the object as processed');
+
+            result = RevueImporter.preProcess({revue: {}});
+            assert.deepEqual(result.preProcessedByRevue, true, 'marks the object as processed');
+
+            result = RevueImporter.preProcess({data: {}});
+            assert.deepEqual(result.preProcessedByRevue, true, 'marks the object as processed');
+        });
+
+        it('ignores empty revue object', function () {
+            const result = RevueImporter.preProcess({revue: {}});
+            assert.deepEqual(result.revue, {}, 'revue object left empty');
+            assert.deepEqual(result.data, undefined, 'no data object set');
+        });
+
+        it('ignores empty nested revue object', function () {
+            const result = RevueImporter.preProcess({revue: {revue: {}}});
+            assert.deepEqual(result.revue.revue, {}, 'revue object left empty');
+            assert.deepEqual(result.data, undefined, 'no data object set');
+        });
+
+        it('handles revue with issue and item data', function () {
+            const result = RevueImporter.preProcess({revue: {revue: {issues: 'id', items: '{}'}}});
+            assert.deepEqual(result.revue.revue, {issues: 'id', items: '{}'}, 'revue object left as-is');
+            assert.deepEqual(result.data, {meta: {version: '5.0.0'}, data: {posts: []}}, 'data object is set');
+        });
+
+        it('handles revue with issue, item and subscribers data', function () {
+            const result = RevueImporter.preProcess({revue: {revue: {issues: 'id', items: '{}', subscribers: 'email'}}});
+            assert.deepEqual(result.revue.revue, {issues: 'id', items: '{}', subscribers: 'email'}, 'revue object left as-is');
+            assert.deepEqual(result.data, {meta: {version: '5.0.0'}, data: {posts: [], revue_subscribers: []}}, 'data object is set');
+        });
+    });
+
+    describe('doImport', function () {
+        it('does nothing', function () {
+            const result = RevueImporter.doImport({x: {y: 'z'}});
+            assert.deepEqual(result, {x: {y: 'z'}}, 'is just a pass-through');
+        });
+    });
+
+    describe('importPosts', function () {
+        it('can process a published post without items', function () {
+            const result = RevueImporter.importPosts({items: '[]', issues: 'id,description,sent_at,subject,preheader\n123456,"<p>Hello World!</p>",2022-12-01 01:01:30 UTC,Hello World - Issue #8,'});
+            assert.deepEqual(result, [
+                {
+                    comment_id: 123456,
+                    title: 'Hello World - Issue #8',
+                    status: 'published',
+                    visibility: 'public',
+                    created_at: '2022-12-01T01:01:30.000Z',
+                    published_at: '2022-12-01T01:01:30.000Z',
+                    updated_at: '2022-12-01T01:01:30.000Z',
+                    html: '<p>Hello World!</p>',
+                    tags: ['#revue']
+                }
+            ]);
+        });
+
+        it('doesnt process a post with no subject', function () {
+            const result = RevueImporter.importPosts({items: '[{"title":"","issue_id":123456,"item_type":"text","url":"","description":"\u003cp\u003eGoodbye World!\u003c/p\u003e","order":0}]', issues: 'id,description,sent_at,subject,preheader\n123456,"<p>Hello World!</p>",2022-12-01 01:01:30 UTC,,'});
+            assert.deepEqual(result, []);
+        });
+
+        it('can process a published post with items', function () {
+            const result = RevueImporter.importPosts({items: '[{"title":"","issue_id":123456,"item_type":"text","url":"","description":"\u003cp\u003eGoodbye World!\u003c/p\u003e","order":0}]', issues: 'id,description,sent_at,subject,preheader\n123456,"<p>Hello World!</p>",2022-12-01 01:01:30 UTC,Hello World - Issue #8,'});
+            assert.deepEqual(result, [
+                {
+                    comment_id: 123456,
+                    created_at: '2022-12-01T01:01:30.000Z',
+                    html: '<p>Hello World!</p><p>Goodbye World!</p>',
+                    published_at: '2022-12-01T01:01:30.000Z',
+                    status: 'published',
+                    tags: [
+                        '#revue'
+                    ],
+                    title: 'Hello World - Issue #8',
+                    updated_at: '2022-12-01T01:01:30.000Z',
+                    visibility: 'public'
+                }
+            ]);
+        });
+
+        it('can process a draft post with items', function () {
+            sinon.stub(JSONToHTML, 'getPostDate').returns('2022-12-01T01:02:03.123Z');
+
+            const result = RevueImporter.importPosts({items: '[{"title":"","issue_id":123456,"item_type":"text","url":"","description":"\u003cp\u003eGoodbye World!\u003c/p\u003e","order":0}]', issues: 'id,description,sent_at,subject,preheader\n123456,"<p>Hello World!</p>",,Hello World - Issue #8,'});
+            assert.deepEqual(result, [
+                {
+                    comment_id: 123456,
+                    title: 'Hello World - Issue #8',
+                    status: 'draft',
+                    visibility: 'public',
+                    created_at: '2022-12-01T01:02:03.123Z',
+                    published_at: '2022-12-01T01:02:03.123Z',
+                    updated_at: '2022-12-01T01:02:03.123Z',
+                    html: '<p>Hello World!</p><p>Goodbye World!</p>',
+                    tags: ['#revue']
+                }
+            ]);
+        });
+    });
+
+    describe('importSubscribers', function () {
+        it('can process a subscriber with only first name', function () {
+            const result = RevueImporter.importSubscribers({subscribers: 'email,first_name,last_name,created_at\njoe@bloggs.me,Joe,"",2022-12-01 01:02:03.123457'});
+
+            assert.deepEqual(result, [{email: 'joe@bloggs.me', name: 'Joe', created_at: '2022-12-01 01:02:03.123457'}]);
+        });
+
+        it('can process a subscriber with first and last name', function () {
+            const result = RevueImporter.importSubscribers({subscribers: 'email,first_name,last_name,created_at\njoe@bloggs.me,Joe,Bloggs,2022-12-01 01:02:03.123457'});
+
+            assert.deepEqual(result, [{email: 'joe@bloggs.me', name: 'Joe Bloggs', created_at: '2022-12-01 01:02:03.123457'}]);
+        });
+
+        it('can process multiple subscribers', function () {
+            const result = RevueImporter.importSubscribers({subscribers: 'email,first_name,last_name,created_at\njoe@bloggs.me,Joe,Bloggs,2022-12-01 01:02:03.123457\njo@bloggs.me,Jo,Bloggs,2022-12-01 01:02:04.123457'});
+
+            assert.deepEqual(result, [{email: 'joe@bloggs.me', name: 'Joe Bloggs', created_at: '2022-12-01 01:02:03.123457'},{email: 'jo@bloggs.me', name: 'Jo Bloggs', created_at: '2022-12-01 01:02:04.123457'}]);
+        });
+    });
+
+    describe('JSONToHTML helpers', function () {
+        describe('itemsToHtml', function () {
+            it('can handle header item', function () {
+                const result = JSONToHTML.itemsToHtml([{title: 'Hello World',issue_id: 123456,item_type: 'header',url: '',description: '',order: 0}]);
+
+                assert.deepEqual(result, '<h3>Hello World</h3>');
+            });
+
+            it('can handle link item', function () {
+                const result = JSONToHTML.itemsToHtml([{title: 'Google',issue_id: 123456,item_type: 'link',url: 'https://google.com/',description: 'A search engine.',order: 0,image: 'https://s3.amazonaws.com/revue/items/images/012/345/678/web/google.png?1234556'}]);
+
+                assert.deepEqual(result, '<figure class="kg-card kg-bookmark-card"><a class="kg-bookmark-container" href="https://google.com/"><div class="kg-bookmark-content"><div class="kg-bookmark-title">Google</div><div class="kg-bookmark-description">A search engine.</div><div class="kg-bookmark-metadata"></div></div><div class="kg-bookmark-thumbnail"><img src="https://s3.amazonaws.com/revue/items/images/012/345/678/web/google.png?1234556" alt=""></div></a></figure>');
+            });
+
+            it('can handle image item', function () {
+                const result = JSONToHTML.itemsToHtml([{title: '', issue_id: 123456, item_type: 'image', url: '', description: 'Hello', order: 0, image: 'https://s3.amazonaws.com/revue/items/images/012/345/678/web/google.png?1234556', original_image_url: 'https://s3.amazonaws.com/revue/items/images/012/345/678/original/google.png?1234556'}]);
+
+                assert.deepEqual(result, '<figure class="kg-card kg-image-card kg-card-hascaption"><img src="https://s3.amazonaws.com/revue/items/images/012/345/678/web/google.png?1234556" class="kg-image" alt loading="lazy"><figcaption>Hello</figcaption></figure>');
+            });
+
+            it('can handle tweet item', function () {
+                const result = JSONToHTML.itemsToHtml([{title: 'Ghost',issue_id: 123456,item_type: 'tweet',url: 'https://twitter.com/Ghost/status/123456',description: 'Hello world',order: 0,tweet_profile_image: 'https://s3.amazonaws.com/revue/tweet_items/profile_images/000/123/456/thumb/ABCD_normal.png?12345',tweet_handle: 'Ghost',tweet_description: '\u003cspan\u003eHello world\u003c/span\u003e',tweet_lang: 'en'}]);
+
+                assert.deepEqual(result, '<figure class="kg-card kg-embed-card">\n' +
+                '                <blockquote class="twitter-tweet"><a href="https://twitter.com/Ghost/status/123456"></a></blockquote>\n' +
+                '                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>\n' +
+                '                </figure>');
+            });
+
+            it('can handle long youtube video item', function () {
+                const result = JSONToHTML.itemsToHtml([{title: '',issue_id: 123456,item_type: 'video',url: 'https://www.youtube.com/watch?v=ABCDEF',description: 'Hello World',order: 0,image: 'https://s3.amazonaws.com/revue/items/images/012/345/678/web/maxresdefault.jpg?1667924432'}]);
+
+                assert.deepEqual(result, '<figure class="kg-card kg-embed-card kg-card-hascaption"><iframe width="200" height="113" src="https://www.youtube.com/embed/ABCDEF?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><figcaption>Hello World</figcaption></figure>');
+            });
+
+            it('can handle short youtube video item', function () {
+                const result = JSONToHTML.itemsToHtml([]);
+
+                assert.deepEqual(result, '');
+            });
+
+            it('can handle vimeo video item', function () {
+                const result = JSONToHTML.itemsToHtml([]);
+
+                assert.deepEqual(result, '');
+            });
+        });
+    });
+});
