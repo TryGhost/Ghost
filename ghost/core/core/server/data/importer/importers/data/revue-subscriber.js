d8361ecb34bd75b8f4f3ec2a706cebfc2250e0e9
Hannah Wolfe <github.erisds@gmail.com>
2022-12-15 20:29:08 +0000

    Updated to use proper members importer for Revue (#16018)
    refs:
    https://github.com/TryGhost/Ghost/commit/99084f9d5ab8d1b145b5be7477abd60cb9b5a8e0
    
    - The OG implementation of importing revue subscribers was very naive
    - This sures it up to use our proper member importer, which makes sure
    everything works perfectly:
      - adds an import label
      - ensures members are subscribed to newsletters
    
    Co-authored-by: Kevin Ansfield <kevin@lookingsideways.co.uk>

--- ghost/core/core/server/data/importer/importers/data/revue-subscriber.js
+++ ghost/core/core/server/data/importer/importers/data/revue-subscriber.js
@@ -1,6 +1,13 @@
 const debug = require('@tryghost/debug')('importer:revue-subscriber');
 const BaseImporter = require('./base');
 
+const papaparse = require('papaparse');
+const path = require('path');
+const fs = require('fs-extra');
+
+const config = require('../../../../../shared/config');
+const models = require('../../../../models');
+
 class RevueSubscriberImporter extends BaseImporter {
     constructor(allDataFromFile) {
         super(allDataFromFile, {
@@ -17,7 +24,31 @@ class RevueSubscriberImporter extends BaseImporter {
     async doImport(options, importOptions) {
         debug('doImport', this.modelName, this.dataToImport.length);
 
-        return super.doImport(options, importOptions);
+        // Don't do anything if there is no data to import
+        if (this.dataToImport.length === 0) {
+            return Promise.resolve();
+        }
+
+        // required here rather than top-level to avoid pulling in before it's initialized during boot
+        const membersService = require('../../../../services/members');
+
+        const importLabel = importOptions.importTag ? importOptions.importTag.replace(/^#/, '') : null;
+
+        const outputFileName = `Converted ${importLabel}.csv`;
+        const outputFilePath = path.join(config.getContentPath('data'), '/', outputFileName);
+        const csvData = papaparse.unparse(this.dataToImport);
+
+        const memberImporterOptions = {
+            pathToCSV: outputFilePath,
+            globalLabels: [{name: importLabel}],
+            importLabel: {name: importLabel},
+            LabelModel: models.Label,
+            forceInline: true
+        };
+
+        await fs.writeFile(outputFilePath, csvData);
+
+        return membersService.processImport(memberImporterOptions);
     }
 }
 
