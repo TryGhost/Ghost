7e3d31ed5ddb01bd8a694d8126edfed771de6665
Fabien "egg" O'Carroll <fabien@allou.is>
2023-01-04 11:27:24 +0700

    Renamed email_address column to email

--- ghost/core/core/server/data/migrations/versions/5.27/2023-01-04-04-12-add-suppressions-table.js
+++ ghost/core/core/server/data/migrations/versions/5.27/2023-01-04-04-12-add-suppressions-table.js
@@ -2,7 +2,7 @@ const {addTable} = require('../../utils');
 
 module.exports = addTable('suppressions', {
     id: {type: 'string', maxlength: 24, nullable: false, primary: true},
-    email_address: {type: 'string', maxlength: 191, nullable: false, unique: true},
+    email: {type: 'string', maxlength: 191, nullable: false, unique: true},
     email_id: {type: 'string', maxlength: 24, nullable: true, references: 'emails.id'},
     reason: {type: 'string', maxlength: 50, nullable: false},
     created_at: {type: 'dateTime', nullable: false}
