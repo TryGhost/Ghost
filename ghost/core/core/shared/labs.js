84cdf0c46c99636412ea61fe2b2be584b5200c25
Peter Zimon <peter.zimon@gmail.com>
2023-01-05 11:34:58 +0100

    Added static version of theme errors notification
    refs. https://github.com/TryGhost/Team/issues/2393
    
    - a labs flag had to be created so we avoid working in branches
    - permanent notification toast was added to make theme errors more discoverable
    - static modal was needed to hold theme error details

--- ghost/core/core/shared/labs.js
+++ ghost/core/core/shared/labs.js
@@ -31,7 +31,8 @@ const ALPHA_FEATURES = [
     'beforeAfterCard',
     'lexicalEditor',
     'suppressionList',
-    'emailStability'
+    'emailStability',
+    'themeErrorsNotification'
 ];
 
 module.exports.GA_KEYS = [...GA_FEATURES];
