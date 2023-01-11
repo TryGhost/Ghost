98981cfda63f673671b823246a7a972b61e27e1d
Elena Baidakova <lena.baydakova@gmail.com>
2023-01-11 13:53:07 +0400

    Added e2e tests for archive/unarchive tiers (#16101)
    refs TryGhost/Team#2371

--- ghost/core/test/e2e-browser/admin/tiers.spec.js
+++ ghost/core/test/e2e-browser/admin/tiers.spec.js
@@ -58,12 +58,11 @@ test.describe('Admin', () => {
             const updatedMonthlyPrice = '66';
             const updatedYearlyPrice = '666';
             const updatedDescription = 'Updated description text';
-            const enableInPortal = true;
             await createTier(page, {
                 name: tierName,
                 monthlyPrice: 5,
                 yearlyPrice: 50
-            }, enableInPortal);
+            });
 
             await test.step('Open Membership settings', async () => {
                 await page.locator('[data-test-nav="settings"]').click();
@@ -117,6 +116,75 @@ test.describe('Admin', () => {
                 await portalFrame.locator('[data-test-button="switch-monthly"]').click();
                 await expect(await portalTierCard.getByText('/month')).toBeVisible();
                 await expect(portalTierCard.locator('.amount').first()).toHaveText(updatedMonthlyPrice);
+            });
+        });
+
+        test('Can archive and unarchive a Tier', async ({page}) => {
+            await page.goto('/ghost');
+            const tierName = getUniqueName('Archive Tier');
+            const tierId = getSlug(tierName);
+            await createTier(page, {
+                name: tierName,
+                monthlyPrice: 5,
+                yearlyPrice: 50
+            });
+
+            await test.step('Open Membership settings', async () => {
+                await page.locator('[data-test-nav="settings"]').click();
+                await page.locator('[data-test-nav="members-membership"]').click();
+                // Tiers request can take time, so waiting until there is no connections before interacting with them
+                await page.waitForLoadState('networkidle');
+            });
+
+            const tierCard = await test.step('Expand the premium tier list and find the new tier', async () => {
+                await page.locator('[data-test-toggle-pub-info]').click();
+                await page.waitForSelector(`[data-test-tier-card="${tierId}"]`);
+
+                return await page.locator(`[data-test-tier-card="${tierId}"]`);
+            });
+
+            await test.step('Archive tier', async () => {
+                await tierCard.locator('[data-test-button="tiers-actions"]').click();
+                await tierCard.locator('[data-test-button="archive-tier"]').click();
+                const modal = page.locator('[data-test-modal="archive-tier"]');
+                await modal.locator('[data-test-button="archive-tier"]').click();
+            });
+
+            await test.step('Archived tier should not be available in active tiers', async () => {
+                await expect(page.locator(`[data-test-tier-card="${tierId}"]`)).toBeHidden();
+            });
+
+            await test.step('Archived tier should be available in archived tiers', async () => {
+                const tiersSelect = await page.locator('[data-test-select-tiers-list]');
+                await tiersSelect.click();
+                await page.getByRole('option', {name: 'Archived'}).click();
+                await expect(page.locator(`[data-test-tier-card="${tierId}"]`)).toBeVisible();
+            });
+
+            await test.step('Archived tier should not be available in portal settings', async () => {
+                await expect(page.locator(`[data-test-settings-tier-label="${tierName}"]`)).toBeHidden();
+            });
+
+            await test.step('Unarchive tier', async () => {
+                await tierCard.locator('[data-test-button="tiers-actions"]').click();
+                await tierCard.locator('[data-test-button="unarchive-tier"]').click();
+                const modal = page.locator('[data-test-modal="unarchive-tier"]');
+                await modal.locator('[data-test-button="unarchive-tier"]').click();
+            });
+
+            await test.step('Unarchived tier should be available in active tiers', async () => {
+                await expect(page.locator(`[data-test-tier-card="${tierId}"]`)).toBeVisible();
+            });
+
+            await test.step('Open Portal settings', async () => {
+                await page.locator('[data-test-toggle="portal-settings"]').click();
+                // Wait until the list of tiers available at signup is visible
+                await page.waitForSelector('[data-test-tiers-at-signup]');
+            });
+
+            await test.step('Unarchived tier should be available in portal settings', async () => {
+                await page.locator(`[data-test-settings-tier-label="${tierName}"]`);
+                expect(await page.locator(`[data-test-settings-tier-input="${tierName}"]`).isChecked()).toBeFalsy();
             });
         });
     });
