a2e9edce52a634c472b1a5cf78bf86162a44aacf
Simon Backx <simon@ghost.org>
2023-01-17 14:26:43 +0100

    Added logging to webmention endpoint (#16137)
    closes https://github.com/TryGhost/Team/issues/2424
    
    Adds the new webmention endpoint, responds with the correct statuscode
    and empty body at `https://site.ghost/webmentions/receive/`.
    
    Also updates the head link tag to the new endpoint location. We don't
    use /webmention because that could conflict with post slugs.

--- ghost/core/core/server/api/endpoints/mentions.js
+++ ghost/core/core/server/api/endpoints/mentions.js
@@ -1,3 +1,4 @@
+const logging = require('@tryghost/logging');
 const mentions = require('../../services/mentions');
 
 module.exports = {
@@ -14,6 +15,20 @@ module.exports = {
         permissions: false,
         query(frame) {
             return mentions.controller.browse(frame);
+        }
+    },
+
+    receive: {
+        statusCode: 202, // Required for the spec because we don't have a status page
+        headers: {},
+        options: [],
+        permissions: false,
+        response: {
+            format: 'plain'
+        },
+        query(frame) {
+            logging.info('[Webmention] ' + JSON.stringify(frame.data));
+            return null;
         }
     }
 };
