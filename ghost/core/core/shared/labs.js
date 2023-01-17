d70ddc5d00519c888e93459adfc5ce4f23755583
Simon Backx <simon@ghost.org>
2023-01-17 15:15:32 +0100

    Added `externalAttribution` feature flag
    fixes https://github.com/TryGhost/Team/issues/2430

--- ghost/core/core/shared/labs.js
+++ ghost/core/core/shared/labs.js
@@ -33,7 +33,8 @@ const ALPHA_FEATURES = [
     'urlCache',
     'beforeAfterCard',
     'lexicalEditor',
-    'webmentions'
+    'webmentions',
+    'externalAttribution'
 ];
 
 module.exports.GA_KEYS = [...GA_FEATURES];
