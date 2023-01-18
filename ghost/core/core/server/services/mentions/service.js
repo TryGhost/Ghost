8257d42afa7b94698bedf8779301c9fd0aedf677
Fabien "egg" O'Carroll <fabien@allou.is>
2023-01-18 14:43:57 +0700

    Wired up the Webmention receive endpoint to the Mentions service
    refs https://github.com/TryGhost/Team/issues/2419
    
    This is the initial stab at having everything wired up, we're not
    using a queue but we are handling the processing of the Webmention
    asyncrounsly so that the HTTP response can be end immediately.
    
    We've also laid the groundwork for extending and implementing the
    correct processing of Webmentions, for example checking if the target
    URL exists in the system, pulling out the metadata from the Webmention
    source and fetching any internal resources.

--- ghost/core/core/server/services/mentions/service.js
+++ ghost/core/core/server/services/mentions/service.js
@@ -1,4 +1,5 @@
 const MentionController = require('./MentionController');
+const WebmentionMetadata = require('./WebmentionMetadata');
 const {
     InMemoryMentionRepository,
     MentionsAPI
@@ -8,8 +9,23 @@ module.exports = {
     controller: new MentionController(),
     async init() {
         const repository = new InMemoryMentionRepository();
+        const webmentionMetadata = new WebmentionMetadata();
         const api = new MentionsAPI({
-            repository
+            repository,
+            webmentionMetadata,
+            resourceService: {
+                async getByURL() {
+                    return {
+                        type: null,
+                        id: null
+                    };
+                }
+            },
+            routingService: {
+                async pageExists() {
+                    return true;
+                }
+            }
         });
 
         api.processWebmention({
