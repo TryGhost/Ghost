2aea46983128661b80ba14f0bafc05d85be9c50b
Daniel Lockyer <hi@daniellockyer.com>
2023-01-18 18:21:36 +0100

    Added `yarn translate:portal` command
    refs https://github.com/TryGhost/Ghost/issues/15502
    
    - the amazing `i18next-parser` dependency will extract our translated
      strings from Portal and dump them into locale files, so we never have
      to add them manually

--- ghost/i18n/lib/i18n.js
+++ ghost/i18n/lib/i18n.js
@@ -1,5 +1,16 @@
 const i18next = require('i18next');
 
+const RESOURCES = {
+    en: {
+        translation: require('../locales/en.json')
+    },
+    nl: {
+        translation: require('../locales/nl.json')
+    }
+};
+
+const SUPPORTED_LOCALES = Object.keys(RESOURCES);
+
 module.exports = (lng = 'en') => {
     const i18nextInstance = i18next.createInstance();
     i18nextInstance.init({
@@ -13,15 +24,10 @@ module.exports = (lng = 'en') => {
         // do not load a fallback
         fallbackLng: false,
 
-        resources: {
-            en: {
-                translation: require('../locales/en.json')
-            },
-            nl: {
-                translation: require('../locales/nl.json')
-            }
-        }
+        resources: RESOURCES
     });
 
     return i18nextInstance;
 };
+
+module.exports.SUPPORTED_LOCALES = SUPPORTED_LOCALES;
