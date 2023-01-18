cbe8c77287a7768452e3d8a00e1a930955f9d377
Daniel Lockyer <hi@daniellockyer.com>
2023-01-18 13:21:17 +0100

    Added i18n package
    refs https://github.com/TryGhost/Ghost/issues/15502
    
    - this is an early implementation of an i18n provider by
      exporting an instance of `i18next`
    - there's a lot more to be done here but baby steps :)

---
+++ ghost/i18n/locales/en.js
@@ -0,0 +1,6 @@
+// Note: we probably don't need anything in here because we don't expect
+// to translate English to English, but it's here for completeness
+module.exports = {
+    translation: {
+    }
+};
