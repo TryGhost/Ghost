8b0dfc744b3adce69249c72cd45ca4ad0ef48e4d
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-01-19 17:02:23 +0800

    Fixed tests

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = 'a3df9e11b3db1c8afdfb87c7a206b53b';
+    const currentSchemaHash = 'fb9bfaf8ab4bb4524d6b4ddde6e05d66';
     const currentFixturesHash = 'f0ccdb0c7eccbc3311e38b5d145ed1db';
     const currentSettingsHash = '9acce72858e75420b831297718595bbd';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
