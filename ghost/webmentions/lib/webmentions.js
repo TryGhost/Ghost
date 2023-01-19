59412f832ae8c8bfee83d44630e717942a7d25d7
Simon Backx <simon@ghost.org>
2023-01-19 17:35:10 +0100

    Added MentionSendingService (#16151)
    fixes https://github.com/TryGhost/Team/issues/2409
    
    The MentionSendingService listens for post changes and sends webmentions
    for outbound links in the post.

--- ghost/webmentions/lib/webmentions.js
+++ ghost/webmentions/lib/webmentions.js
@@ -1,3 +1,4 @@
 module.exports.InMemoryMentionRepository = require('./InMemoryMentionRepository');
 module.exports.MentionsAPI = require('./MentionsAPI');
 module.exports.Mention = require('./Mention');
+module.exports.MentionSendingService = require('./MentionSendingService');
