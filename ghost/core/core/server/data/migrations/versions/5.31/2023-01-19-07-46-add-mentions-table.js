4838b1a1b592ec5a9e11ad8a7898395d1bd5453d
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-01-20 16:46:09 +0800

    Removed published column

--- ghost/core/core/server/data/migrations/versions/5.31/2023-01-19-07-46-add-mentions-table.js
+++ ghost/core/core/server/data/migrations/versions/5.31/2023-01-19-07-46-add-mentions-table.js
@@ -1,5 +1,6 @@
 const {addTable} = require('../../utils');
 
+// stay below 
 const mentionsTable = addTable('mentions', {
     id: {type: 'string', maxlength: 24, nullable: false, primary: true},
     source: {type: 'string', maxlength: 2000, nullable: false},
@@ -7,13 +8,12 @@ const mentionsTable = addTable('mentions', {
     source_site_title: {type: 'string', maxlength: 2000, nullable: true},
     source_excerpt: {type: 'string', maxlength: 2000, nullable: true},
     source_author: {type: 'string', maxlength: 2000, nullable: true},
-    source_publisher: {type: 'string', maxlength: 2000, nullable: true},
     source_featured_image: {type: 'string', maxlength: 2000, nullable: true},
     source_favicon: {type: 'string', maxlength: 2000, nullable: true},
     target: {type: 'string', maxlength: 2000, nullable: false},
     resource_id: {type: 'string', maxlength: 24, nullable: true},
     created_at: {type: 'dateTime', nullable: false},
-    payload: {type: 'text', maxlength: 65535, nullable: true}
+    payload: {type: 'text', maxlength: 1000000000, nullable: true}
 });
 
 module.exports = {
--- ghost/core/core/server/data/schema/schema.js
+++ ghost/core/core/server/data/schema/schema.js
@@ -987,12 +987,11 @@ module.exports = {
         source_site_title: {type: 'string', maxlength: 2000, nullable: true},
         source_excerpt: {type: 'string', maxlength: 2000, nullable: true},
         source_author: {type: 'string', maxlength: 2000, nullable: true},
-        source_publisher: {type: 'string', maxlength: 2000, nullable: true},
         source_featured_image: {type: 'string', maxlength: 2000, nullable: true},
         source_favicon: {type: 'string', maxlength: 2000, nullable: true},
         target: {type: 'string', maxlength: 2000, nullable: false},
         resource_id: {type: 'string', maxlength: 24, nullable: true},
         created_at: {type: 'dateTime', nullable: false},
-        payload: {type: 'text', maxlength: 65535, nullable: true}
+        payload: {type: 'text', maxlength: 1000000000, nullable: true}
     }
 };
