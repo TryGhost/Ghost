856952796909fc814684e2f3ed2abc20448ad1e8
Naz <hi@nazavo.com>
2023-01-20 20:05:24 +0800

    Excluded 'tiers' from fields that affect url generation for posts
    refs https://github.com/TryGhost/Toolbox/issues/503
    
    - Tier's are sometimes dynamically generated and are present in the "_changed" properties, causing full URL regeneration. They have no effect on post's URL, so should not trigger URL regeneration.

--- ghost/core/core/server/services/url/config.js
+++ ghost/core/core/server/services/url/config.js
@@ -31,7 +31,8 @@ module.exports = [
                 'twitter_title',
                 'twitter_description',
                 'custom_template',
-                'locale'
+                'locale',
+                'tiers'
             ],
             withRelated: ['tags', 'authors'],
             withRelatedPrimary: {
@@ -79,7 +80,8 @@ module.exports = [
                 'tags',
                 'authors',
                 'primary_tag',
-                'primary_author'
+                'primary_author',
+                'tiers'
             ],
             filter: 'status:published+type:page'
         },
