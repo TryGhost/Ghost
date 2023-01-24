94a6621c19d7fd09b42d2d718d64937773fc2d29
Rishabh <zrishabhgarg@gmail.com>
2023-01-24 21:55:50 +0530

    fixup! Restored missing tier mapping for paid members

--- ghost/core/core/server/data/migrations/versions/5.32/2023-01-24-08-00-fix-invalid-tier-expiry-for-paid-members.js
+++ ghost/core/core/server/data/migrations/versions/5.32/2023-01-24-08-00-fix-invalid-tier-expiry-for-paid-members.js
@@ -1,39 +1,30 @@
 const logging = require('@tryghost/logging');
-const {chunk: chunkArray} = require('lodash');
 const {createTransactionalMigration} = require('../../utils');
 
 module.exports = createTransactionalMigration(
     async function up(knex) {
         logging.info('Removing expiry dates for paid members');
+        try {
+            const memberIdsWithInvalidExpiry = await knex('members_products')
+                .select('members_products.member_id as id')
+                .leftJoin('members', 'members_products.member_id', 'members.id')
+                .where('members.status', '=', 'paid')
+                .whereNotNull('members_products.expiry_at').pluck('id');
 
-        const paidMembersWithExpiry = await knex('members_products')
-            .select('members_products.id')
-            .leftJoin('members', 'members_products.member_id', 'members.id')
-            .where('members.status', '=', 'paid')
-            .whereNotNull('members_products.expiry_at');
+            logging.info(`Found ${memberIdsWithInvalidExpiry.length} paid members with expiry dates`);
 
-        logging.info(`Found ${paidMembersWithExpiry.length} paid members with expiry dates`);
+            if (memberIdsWithInvalidExpiry.length === 0) {
+                return;
+            }
 
-        if (paidMembersWithExpiry.length === 0) {
-            return;
-        }
+            logging.info(`Removing expiry dates for ${memberIdsWithInvalidExpiry.length} paid members`);
 
-        logging.info(`Removing expiry dates for ${paidMembersWithExpiry.length} paid members`);
-
-        // SQLite >= 3.32.0 can support 32766 host parameters
-        // We use one for the SET clause, and the rest can be
-        // used to populate the WHERE IN (?... clause.
-        const chunkSize = 32765;
-
-        const paidMembersWithExpiryIds = paidMembersWithExpiry.map(row => row.id);
-
-        const chunks = chunkArray(paidMembersWithExpiryIds, chunkSize);
-
-        // eslint-disable-next-line no-restricted-syntax
-        for (const chunk of chunks) {
             await knex('members_products')
                 .update('expiry_at', null)
-                .whereIn('id', chunk);
+                .whereIn('member_id', memberIdsWithInvalidExpiry);
+        } catch (err) {
+            logging.warn('Failed to remove expiry dates for paid members');
+            logging.warn(err);
         }
     },
     async function down() {
