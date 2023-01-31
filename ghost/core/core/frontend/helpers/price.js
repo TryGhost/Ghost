9fda7107dae35230fb540584764b3f1448afb9a7
Naz <hi@nazavo.com>
2023-01-31 21:01:02 +0800

    Fixed {{price}} helper to render empty instead of throwing
    refs https://github.com/TryGhost/Toolbox/issues/497
    refs https://github.com/TryGhost/gscan/commit/fb7532bf5d2f74afde6dfbf3de0af27a17ef41da
    
    - We downgraded the 'GS090-NO-PRICE-DATA-CURRENCY-CONTEXT' rule in gscan to non-fatal, meaning Ghost should not be throwing an error but instead render an empty value for {{price}} helper when price data is empty.
    - For example, a legacy syntax like this: '{{price currency=@price.currency}}' should not cause a page render error but return an empty price string.
    - The pattern of returning an empty string instead of crashing is used in other helpers like {{img_url}} and and {{url}}

--- ghost/core/core/frontend/helpers/price.js
+++ ghost/core/core/frontend/helpers/price.js
@@ -8,10 +8,10 @@
 // Usage: `{{price 500 currency="USD"}}`
 // Usage: `{{price currency="USD"}}`
 //
-// Returns amount equal to the dominant denomintation of the currency.
+// Returns amount equal to the dominant denomination of the currency.
 // For example, if 2100 is passed, it will return 21.
-const errors = require('@tryghost/errors');
 const tpl = require('@tryghost/tpl');
+const logging = require('@tryghost/logging');
 const _ = require('lodash');
 
 const messages = {
@@ -82,22 +82,19 @@ module.exports = function price(planOrAmount, options) {
 
     // CASE: if no amount is passed, e.g. `{{price}}` we throw an error
     if (arguments.length < 2) {
-        throw new errors.IncorrectUsageError({
-            message: tpl(messages.attrIsRequired)
-        });
+        logging.warn(tpl(messages.attrIsRequired));
+        return '';
     }
 
     // CASE: if amount is passed, but it is undefined we throw an error
     if (amount === undefined) {
-        throw new errors.IncorrectUsageError({
-            message: tpl(messages.attrIsRequired)
-        });
+        logging.warn(tpl(messages.attrIsRequired));
+        return '';
     }
 
     if (!_.isNumber(amount)) {
-        throw new errors.IncorrectUsageError({
-            message: tpl(messages.attrMustBeNumeric)
-        });
+        logging.warn(tpl(messages.attrMustBeNumeric));
+        return '';
     }
 
     return amount / 100;
