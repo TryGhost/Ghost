62e95f92b51b9fc953071a1a70819c92764a9b26
Steve Larson <9larsons@gmail.com>
2023-02-01 16:49:58 -0600

    updated events that send mentions (#16219)
    refs TryGhost/Team#2477
    -removed post.edited as it was too inclusive
    -changed to post.published, post.published.edited, post.unpublished
    -blocked import and internal data from triggering mentions

--- ghost/webmentions/lib/MentionSendingService.js
+++ ghost/webmentions/lib/MentionSendingService.js
@@ -25,15 +25,25 @@ module.exports = class MentionSendingService {
     }
 
     /**
-     * Listen for changes in posts and automatically send webmentions.
+     * Listen for new and edited published posts and automatically send webmentions. Unpublished posts should send mentions
+     *  so the receiver can discover a 404 response and remove the mentions.
      * @param {*} events
      */
     listen(events) {
-        // Note: we don't need to listen for post.published (post.edited is also called at that time)
-        events.on('post.edited', this.sendForEditedPost.bind(this));
+        events.on('post.published', this.sendForPost.bind(this));
+        events.on('post.published.edited', this.sendForPost.bind(this));
+        events.on('post.unpublished', this.sendForPost.bind(this));
     }
 
-    async sendForEditedPost(post) {
+    async sendForPost(post, options) {
+        // NOTE: this is not ideal and shouldn't really be handled within the package...
+        // for now we don't want to evaluate mentions when importing data (at least needs queueing set up)
+        // we do not want to evaluate mentions with fixture (internal) data, e.g. generating posts
+        // TODO: real solution is likely suppressing event emission when building fixture data
+        if (options && (options.importing || options.context.internal)) {
+            return;
+        }
+
         try {
             if (!this.#isEnabled()) {
                 return;
@@ -53,7 +63,7 @@ module.exports = class MentionSendingService {
                 previousHtml: post.previous('status') === 'published' ? post.previous('html') : null
             });
         } catch (e) {
-            logging.error('Error in webmention sending service post.added event handler:');
+            logging.error('Error in webmention sending service post update event handler:');
             logging.error(e);
         }
     }
