81b56cb6edabdad8c538dbca9c22e5287fc8be55
Aileen Nowak <AileenCGN@gmail.com>
2023-02-01 12:13:16 +0000

    Milestone entity

---
+++ ghost/milestone-emails/lib/Milestone.js
@@ -0,0 +1,133 @@
+const ObjectID = require('bson-objectid').default;
+const {ValidationError} = require('@tryghost/errors');
+
+module.exports = class Milestone {
+    /**
+     * @type {ObjectID}
+     */
+    #id;
+    get id() {
+        return this.#id;
+    }
+
+    /**
+     * @type {'arr' | 'members'}
+     */
+    #type;
+    get type() {
+        return this.#type;
+    }
+
+    /**
+     * @type {string}
+     */
+    #name;
+    get name() {
+        return this.#name;
+    }
+
+    /** @type {number} */
+    #value;
+    get value() {
+        return this.#value;
+    }
+
+    /** @type {Date} */
+    #createdAt;
+    get createdAt() {
+        return this.#createdAt;
+    }
+
+    toJSON() {
+        return {
+            id: this.id,
+            name: this.name,
+            type: this.type,
+            value: this.value,
+            createdAt: this.createdAt
+        };
+    }
+
+    /** @private */
+    constructor(data) {
+        this.#id = data.id;
+        this.#name = data.name;
+        this.#type = data.type;
+        this.#value = data.value;
+        this.#createdAt = data.createdAt;
+    }
+
+    /**
+     * @param {any} data
+     * @returns {Promise<Milestone>}
+     */
+    static async create(data) {
+        const id = validateId(data.id);
+        const type = validateType(data.type);
+        const value = validateValue(data.value);
+        const name = validateName(data.name, value, type);
+
+        /** @type Date */
+        let createdAt;
+        if (data.createdAt instanceof Date) {
+            createdAt = data.createdAt;
+        } else if (data.createdAt) {
+            createdAt = new Date(data.createdAt);
+            if (isNaN(createdAt.valueOf())) {
+                throw new ValidationError({
+                    message: 'Invalid Date'
+                });
+            }
+        } else {
+            createdAt = new Date();
+        }
+
+        return new Milestone({
+            id,
+            name,
+            type,
+            value,
+            createdAt
+        });
+    }
+};
+
+function validateId(id) {
+    if (!id) {
+        return new ObjectID();
+    }
+    if (typeof id === 'string') {
+        return ObjectID.createFromHexString(id);
+    }
+    if (id instanceof ObjectID) {
+        return id;
+    }
+    return new ObjectID;
+}
+
+function validateValue(value) {
+    if (!value || typeof value !== 'number' || value === 0) {
+        throw new ValidationError({
+            message: 'Invalid value'
+        });
+    }
+
+    return value;
+}
+
+function validateType(type) {
+    if (type === 'arr') {
+        return 'arr';
+    }
+
+    return 'members';
+}
+
+function validateName(name, value, type) {
+    if (!name || typeof name !== 'string' || !name.match(/(arr|members)-\d{10}/i)) {
+        return `${type}-${value}`;
+    }
+
+    return name;
+}
+
