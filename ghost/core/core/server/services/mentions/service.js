5ccb778ac84a058f5355ee62d0845b8bc41becbd
Steve Larson <9larsons@gmail.com>
2023-02-03 14:52:20 -0600

    enabled always sending mentions (#16227)
    refs TryGhost/Team#2507
    -removed labs check from sending service
    -labs flag controls seeing mentions in admin and receiving mentions

--- ghost/core/core/server/services/mentions/service.js
+++ ghost/core/core/server/services/mentions/service.js
@@ -13,7 +13,6 @@ const events = require('../../lib/common/events');
 const externalRequest = require('../../../server/lib/request-external.js');
 const urlUtils = require('../../../shared/url-utils');
 const outputSerializerUrlUtil = require('../../../server/api/endpoints/utils/serializers/output/utils/url');
-const labs = require('../../../shared/labs');
 const urlService = require('../url');
 const settingsCache = require('../../../shared/settings-cache');
 const DomainEvents = require('@tryghost/domain-events');
@@ -89,7 +88,7 @@ module.exports = {
             externalRequest,
             getSiteUrl: () => urlUtils.urlFor('home', true),
             getPostUrl: post => getPostUrl(post),
-            isEnabled: () => labs.isSet('webmentions') && !settingsCache.get('is_private')
+            isEnabled: () => !settingsCache.get('is_private')
         });
         sendingService.listen(events);
     }
