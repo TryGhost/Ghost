ba26deb2b8b4da9bf740252177110c0be5d3a2fc
Fabien "egg" O'Carroll <fabien@allou.is>
2023-02-03 16:17:47 +0700

    Used TierRepository in serializers
    We want to cache access to Tiers, and it's easier to do that in the
    TierRepository. So we update a heavy user of Tiers to use the Tier
    service so it can take adv of caching. The serializers are a big
    offender for making calls to fetch Tiers.

--- ghost/core/core/server/api/endpoints/utils/serializers/output/email-posts.js
+++ ghost/core/core/server/api/endpoints/utils/serializers/output/email-posts.js
@@ -1,14 +1,31 @@
 const mappers = require('./mappers');
 const gating = require('./utils/post-gating');
-const membersService = require('../../../../../services/members');
+const tiersService = require('../../../../../services/tiers');
 
 module.exports = {
     async read(model, apiConfig, frame) {
-        const tiersModels = await membersService.api.productRepository.list({
-            withRelated: ['monthlyPrice', 'yearlyPrice']
-        });
-        const tiers = tiersModels.data && tiersModels.data.map(tierModel => tierModel.toJSON());
-
+        const tiersPage = await tiersService.api.browse({});
+        const tiers = tiersPage.data?.map((tierModel) => {
+            const json = tierModel.toJSON();
+            return {
+                id: json.id,
+                name: json.name,
+                slug: json.slug,
+                active: json.status === 'active',
+                welcome_page_url: json.welcomePageURL,
+                visibility: json.visibility,
+                trial_days: json.trialDays,
+                description: json.description,
+                type: json.type,
+                currency: json.type === 'paid' ? json.currency?.toLowerCase() : null,
+                monthly_price: json.monthlyPrice,
+                yearly_price: json.yearlyPrice,
+                created_at: json.createdAt,
+                updated_at: json.updatedAt,
+                monthly_price_id: null,
+                yearly_price_id: null
+            };
+        }) || [];
         const emailPost = await mappers.posts(model, frame, {tiers});
         gating.forPost(emailPost, frame);
 
