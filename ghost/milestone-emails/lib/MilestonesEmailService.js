e7a8379532571350671ccc67d6c3c7ba62271d6d
Aileen Nowak <AileenCGN@gmail.com>
2023-02-06 11:28:16 +0200

    Added basic method for ARR and members fetching

--- ghost/milestone-emails/lib/MilestonesEmailService.js
+++ ghost/milestone-emails/lib/MilestonesEmailService.js
@@ -9,7 +9,7 @@
  */
 
 /**
- * @typedef {object} IQueries
+ * @typedef {import('./MilestoneQueries')} IQueries
  * @prop {() => Promise<number>} getMembersCount
  * @prop {() => Promise<Object>} getARR
  * @prop {() => Promise<boolean>} hasImportedMembersInPeriod
@@ -20,61 +20,71 @@ module.exports = class MilestonesEmailService {
     #api;
 
     /** @type {Function} */
-    #sendEmail;
+    #mailer;
 
     /** @type {Object} */
     #config;
 
-    /** @type {IQueries} */
+    /** @type {import('./MilestoneQueries')} */
     #queries;
-
-    /** @type {boolean} */
-    #isEnabled;
 
     /**
      * @param {object} deps
-     * @param {Function} deps.sendEmail
+     * @param {Function} deps.mailer
      * @param {MilestonesAPI} deps.api
      * @param {Object} deps.config
      * @param {IQueries} deps.queries
      */
     async init(deps) {
-        this.#sendEmail = deps.sendEmail;
+        this.#mailer = deps.mailer;
         this.#api = deps.api;
         this.#config = deps.config;
         this.#queries = deps.queries;
     }
 
-    async runQueries() {
-        // const matchingCurrencies = [];
+    async runARRQueries() {
+        const matchingCurrencies = [];
         // TODO: do we need to check if Stripe is live enabled?
         // Fetch the current data
+        const currentARR = await this.#queries.getARR();
+        // const hasMembersImported = await this.#queries.hasImportedMembersInPeriod();
+
+        // Check the definitions in the config
+        const definedMilestones = this.#config.milestones;
+        const arrMilestoneSettings = definedMilestones.arr;
+
+        // Fetch the latest achieved milestones
+        // const latestARRMilestone = await this.#api.getLatestArrMilestone();
+
+        // Now the logic
+        // First check the currency matches
+        if (currentARR.length) {
+            arrMilestoneSettings.forEach(({currency}) => {
+                // see if the requested currency is used and returned from current ARR result
+                const filteredResults = currentARR.filter(({currency: curr}) => currency === curr);
+
+                if (filteredResults.length) {
+                    matchingCurrencies.push(filteredResults);
+                }
+            });
+
+            // if (matchingCurrencies.length) {
+            //     // now if we hit
+            //     console.log(latestARRMilestone);
+            // }
+        }
+    }
+
+    async runMemberQueries() {
+        // // Fetch the current data
         // const membersCount = await this.#queries.getMembersCount();
-        // const currentARR = await this.#queries.getARR();
         // const hasMembersImported = await this.#queries.hasImportedMembersInPeriod();
 
         // // Check the definitions in the config
         // const definedMilestones = this.#config.milestones;
-        // const arrMilestoneSettings = definedMilestones.arr;
         // const membersMilestones = definedMilestones.members;
 
         // // Fetch the latest achieved milestones
         // const latestMembersMilestone = await this.#api.getLatestMembersCountMilestone();
-        // const latestARRMilestone = await this.#api.getLatestArrMilestone();
-
-        // // Now the logic
-        // // First check the currency matches
-        // arrMilestoneSettings.forEach(({currency}) => {
-        //     // see if the requested currency is used and returned from current ARR result
-        //     const filteredResults = currentARR.filter(({currency: curr}) => currency === curr);
-
-        //     if (filteredResults.length) {
-        //         matchingCurrencies.push(filteredResults);
-        //     }
-        // });
-
-        // if (matchingCurrencies.length) {
-        //     // now if we hit
-        // }
     }
 };
