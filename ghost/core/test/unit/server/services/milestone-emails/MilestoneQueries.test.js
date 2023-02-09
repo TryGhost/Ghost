b144d2aea7f5480b7ddf8d77361a07b0d4a9aaf2
Aileen Nowak <AileenCGN@gmail.com>
2023-02-09 17:39:26 +0200

    Mock knex for MilestoneQueries unit test

--- ghost/core/test/unit/server/services/milestone-emails/MilestoneQueries.test.js
+++ ghost/core/test/unit/server/services/milestone-emails/MilestoneQueries.test.js
@@ -1,18 +1,35 @@
 const db = require('../../../../../core/server/data/db');
 const models = require('../../../../../core/server/models');
 const assert = require('assert');
+const sinon = require('sinon');
 
 describe('MilestoneQueries', function () {
     let milestoneQueries;
+    let queryMock;
+    let knexMock;
 
     before(function () {
         models.init();
+
+        queryMock = {
+            groupBy: sinon.stub(),
+            select: sinon.stub(),
+            raw: sinon.stub(),
+            count: sinon.stub(),
+            where: sinon.stub()
+        };
+
+        knexMock = sinon.stub().returns(queryMock);
+
+        sinon.stub(db, 'knex').get(function () {
+            return knexMock;
+        });
     });
 
     describe('Milestone Emails Service', function () {
         it('Provides expected public API', async function () {
             const MilestoneQueries = require('../../../../../core/server/services/milestone-emails/MilestoneQueries');
-            milestoneQueries = new MilestoneQueries({db});
+            milestoneQueries = new MilestoneQueries({db: knexMock});
 
             assert.ok(milestoneQueries.getMembersCount);
             assert.ok(milestoneQueries.getARR);
