ca3caa3dbc8ed0710eed95871c97d48b85c5fd2c
Kevin Ansfield <kevin@lookingsideways.co.uk>
2023-02-09 11:45:33 +0000

    Skipped rendering JSON Output updates when sidebar isn't open
    closes https://github.com/TryGhost/Team/issues/2341
    
    - there's no need to render and syntax highlight the JSON output view when the sidebar isn't open, it can be an expensive operation as it occurs on every keystroke whilst typing in the editor

--- koenig/koenig-lexical/demo/components/SerializedStateTextarea.jsx
+++ koenig/koenig-lexical/demo/components/SerializedStateTextarea.jsx
@@ -4,7 +4,7 @@ import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
 import {OnChangePlugin} from '@lexical/react/LexicalOnChangePlugin';
 import 'highlight.js/styles/atom-one-dark.css';
 
-const SerializedStateTextarea = () => {
+const SerializedStateTextarea = ({isOpen}) => {
     const [editor] = useLexicalComposerContext();
 
     const renderEditorState = () => JSON.stringify(editor.getEditorState().toJSON(), null, 2);
@@ -18,9 +18,11 @@ const SerializedStateTextarea = () => {
     return (
         <>
             <div className="h-full w-full resize-none !overflow-auto bg-black !p-4 font-mono text-sm text-grey-300 selection:bg-grey-800">
-                <Highlight className="json">
-                    {serializedJson}
-                </Highlight>
+                {isOpen && (
+                    <Highlight className="json">
+                        {serializedJson}
+                    </Highlight>
+                )}
             </div>
             <OnChangePlugin onChange={onChange} />
         </>
--- koenig/koenig-lexical/demo/components/Sidebar.jsx
+++ koenig/koenig-lexical/demo/components/Sidebar.jsx
@@ -4,8 +4,8 @@ import TreeView from './TreeView';
 const Sidebar = ({isOpen, view}) => {
     return (
         <div className={`h-full grow overflow-hidden border-grey-100 bg-black pb-16 transition-all ease-in-out ${isOpen ? 'right-0 w-full opacity-100 sm:w-[440px]' : 'right-[-100%] w-0 opacity-0'}`}>
-            {view === 'json' && <SerializedStateTextarea />}
-            {view === 'tree' && <TreeView />}
+            {view === 'json' && <SerializedStateTextarea isOpen={isOpen} />}
+            {view === 'tree' && <TreeView isOpen={isOpen} />}
         </div>
     );
 };
