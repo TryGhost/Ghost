72d88c9dfc2f0c869bede6eeed0b811b6d7dfefc
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-02-13 09:42:57 +0800

    Revert "Removed logging"
    This reverts commit 0bd1211044da69fd58d92ebf37f8475bd11eb69d.

--- ghost/core/test/e2e-api/webmentions/webmentions.test.js
+++ ghost/core/test/e2e-api/webmentions/webmentions.test.js
@@ -195,6 +195,7 @@ describe('Webmentions (receiving)', function () {
 
         const requests = [];
         for (let i = 0; i < webmentionBlock.freeRetries + 1; i++) {
+            console.log(i); // eslint-disable-line no-console
             const req = await agent.post('/receive/')
                 .body({
                     source: sourceUrl.href,
