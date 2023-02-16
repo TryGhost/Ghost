08ee1fe14d7907ac56e1e779247e497fe657ba38
Aileen Nowak <AileenCGN@gmail.com>
2023-02-16 14:43:07 +0200

    Fixed failing tests

--- ghost/core/core/server/services/slack-notifications/service.js
+++ ghost/core/core/server/services/slack-notifications/service.js
@@ -25,7 +25,7 @@ class SlackNotificationsServiceWrapper {
             logging,
             config: {
                 isEnabled,
-                webhookUrl: hostSettings.milestones.url
+                webhookUrl: hostSettings?.milestones?.url
             },
             siteUrl
         });
