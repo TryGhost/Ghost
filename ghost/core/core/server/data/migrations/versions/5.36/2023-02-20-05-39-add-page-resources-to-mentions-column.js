bb3e4181c9fedc2c53b5b914f9203a11eb0101e9
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-02-20 16:14:14 +0800

    Changed nullable to true

--- ghost/core/core/server/data/migrations/versions/5.36/2023-02-20-05-39-add-page-resources-to-mentions-column.js
+++ ghost/core/core/server/data/migrations/versions/5.36/2023-02-20-05-39-add-page-resources-to-mentions-column.js
@@ -3,5 +3,5 @@ const {createAddColumnMigration} = require('../../utils');
 module.exports = createAddColumnMigration('mentions', 'page_resources', {
     type: 'string',
     maxlength: 2000,
-    nullable: false
+    nullable: true
 });
