9697502f3305b323f02b7079a232dca721afce1e
Sanne de Vries <sannedv@protonmail.com>
2023-02-22 10:24:47 +0100

    Added toggle to demo page to switch between editor modes
    Refs https://github.com/TryGhost/Team/issues/2589

--- koenig/koenig-lexical/demo/components/Watermark.jsx
+++ koenig/koenig-lexical/demo/components/Watermark.jsx
@@ -3,11 +3,16 @@ import {ReactComponent as GhostFavicon} from './icons/ghost-favicon.svg';
 
 const Watermark = () => {
     return (
-        <a href="https://github.com/TryGhost/Koenig/tree/main/packages/koenig-lexical" target="_blank" rel="nofollow ugc noopener noreferrer" className="absolute bottom-4 left-6 z-20 flex items-center rounded bg-white py-1 pr-2 pl-1 font-mono text-sm tracking-tight text-black">
-            <GhostFavicon className="mr-2 h-6 w-6" />
-            <span className="pr-1 font-bold tracking-wide">Koenig</span> 
-            editor
-        </a>
+        <>
+            <a href="https://github.com/TryGhost/Koenig/tree/main/packages/koenig-lexical" target="_blank" rel="nofollow ugc noopener noreferrer" className="group absolute bottom-4 left-6 z-20 flex items-center rounded bg-white py-1 pr-2 pl-1 font-mono text-sm tracking-tight text-black">
+                <GhostFavicon className="mr-2 h-6 w-6" />
+                <span className="pr-1 font-bold tracking-wide">Koenig</span> 
+                <span className="group-hover:font-bold">full
+                    <span className="invisible ml-[.7rem] font-normal hover:font-bold group-hover:visible">/ minimal</span>
+                    <span className="invisible ml-[.7rem] font-normal hover:font-bold group-hover:visible">/ basic</span>
+                </span>
+            </a>
+        </>
     );
 };
 
