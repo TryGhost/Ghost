ead413b5c6f1df50d13fc39bffb4f260c24d80a3
Rishabh <zrishabhgarg@gmail.com>
2023-02-22 22:17:01 +0530

    Cleaned up duplicate key warning

--- koenig/koenig-lexical/demo/components/Watermark.jsx
+++ koenig/koenig-lexical/demo/components/Watermark.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {ReactComponent as GhostFavicon} from './icons/ghost-favicon.svg';
 
-function EditorLink({editorType, children}) {
+function EditorLink({editorType}) {
     return (
-        <a href={editorType?.url} rel="nofollow ugc noopener noreferrer" key={editorType}>
+        <a href={editorType?.url} rel="nofollow ugc noopener noreferrer">
             <span className="invisible ml-[.7rem] font-normal hover:font-bold group-hover:visible">/ {editorType?.name}</span>
         </a>
     );
@@ -35,7 +35,7 @@ const Watermark = ({editorType}) => {
     ];
 
     const remainingEditorTypes = editorTypes.filter(type => type.name !== editorType);
-    const editorLinks = remainingEditorTypes.map(type => <EditorLink editorType={type} />);
+    const editorLinks = remainingEditorTypes.map(type => <EditorLink editorType={type} key={type?.name} />);
     return (
         <>
             <div className="group absolute bottom-4 left-6 z-20 flex items-center rounded bg-white py-1 pr-2 pl-1 font-mono text-sm tracking-tight text-black">
