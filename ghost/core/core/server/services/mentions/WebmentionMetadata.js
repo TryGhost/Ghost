5252a1edf21554ce0ef24f4cb2e15e523acea859
Steve Larson <9larsons@gmail.com>
2023-02-28 08:39:28 -0600

    combined fetch for metadata and verification (#16330)
    refs TryGhost/Team#2582
    -removed duplicative fetch request
    -added mention as type in oembed service

--- ghost/core/core/server/services/mentions/WebmentionMetadata.js
+++ ghost/core/core/server/services/mentions/WebmentionMetadata.js
@@ -6,14 +6,15 @@ module.exports = class WebmentionMetadata {
      * @returns {Promise<import('@tryghost/webmentions/lib/MentionsAPI').WebmentionMetadata>}
      */
     async fetch(url) {
-        const data = await oembedService.fetchOembedDataFromUrl(url.href, 'bookmark');
+        const data = await oembedService.fetchOembedDataFromUrl(url.href, 'mention');
         const result = {
             siteTitle: data.metadata.publisher,
             title: data.metadata.title,
             excerpt: data.metadata.description,
             author: data.metadata.author,
             image: data.metadata.thumbnail ? new URL(data.metadata.thumbnail) : null,
-            favicon: data.metadata.icon ? new URL(data.metadata.icon) : null
+            favicon: data.metadata.icon ? new URL(data.metadata.icon) : null,
+            body: data.body
         };
         return result;
     }
