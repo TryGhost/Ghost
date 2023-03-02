c006a4b032d4375b04c2dbd610e943c87a3de886
Daniel Lockyer <hi@daniellockyer.com>
2023-03-02 12:48:29 +0100

    Added debug logging to measure model require time
    - these models should be as lightweight as possible to require, so we
      need to measure how long they take
    - this has already found a couple of models which need optimizing

--- ghost/core/core/server/models/index.js
+++ ghost/core/core/server/models/index.js
@@ -3,6 +3,7 @@
  */
 
 const _ = require('lodash');
+const debug = require('@tryghost/debug')('models');
 const glob = require('glob');
 
 // enable event listeners
@@ -14,12 +15,16 @@ require('./base/listeners');
 exports = module.exports;
 
 function init() {
+    const baseNow = Date.now();
     exports.Base = require('./base');
+    debug(`${Date.now() - baseNow}ms - Base.js require`);
 
     let modelsFiles = glob.sync('!(index).js', {cwd: __dirname});
     modelsFiles.forEach((model) => {
         const name = model.replace(/.js$/, '');
+        const modelNow = Date.now();
         _.extend(exports, require('./' + name));
+        debug(`${Date.now() - modelNow}ms - ${model} require`);
     });
 }
 
