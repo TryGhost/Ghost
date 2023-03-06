b3d526ec78e05ce8117d73ad59a2b0749857a80e
Tim Gates <tim.gates@iress.com>
2023-03-06 23:11:45 +1100

    Fixed typos (#16359)

--- ghost/portal/src/components/Frame.styles.js
+++ ghost/portal/src/components/Frame.styles.js
@@ -1,6 +1,6 @@
 /** By default, CRAs webpack bundle combines and appends the main css at root level, so they are not applied inside iframe
  * This uses a hack where we append `<style> </style>` tag with all CSS inside the head of iframe dynamically, thus making it available easily
- * We can create separate variables to keep styles grouped logically, and export them as one appeneded string
+ * We can create separate variables to keep styles grouped logically, and export them as one appended string
 */
 
 import {GlobalStyles} from './Global.styles';
