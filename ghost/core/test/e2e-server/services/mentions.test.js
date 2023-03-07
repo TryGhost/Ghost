5edef56073dd32aedf6813c44f7227d111ed076b
Simon Backx <simon@ghost.org>
2023-03-07 13:20:28 +0100

    Improved network access and mocking in tests (#16371)
    refs https://github.com/TryGhost/Team/issues/2667
    
    Some tests still accessed the internet. Now network access is disabled
    by default. This change also introduces two helper methods related to
    networking (mocking Slack and Mailgun).
    
    This fixes two unreliable tests:
    - Staff service was accessing a Slack test API -> timeout possible
    - MentionSendingService was trying to send webmentions for every post
    publish/change -> possible timeouts and job issues

--- ghost/core/test/e2e-server/services/mentions.test.js
+++ ghost/core/test/e2e-server/services/mentions.test.js
@@ -1,9 +1,7 @@
 const {agentProvider, fixtureManager, mockManager} = require('../../utils/e2e-framework');
-const sinon = require('sinon');
 const nock = require('nock');
 const assert = require('assert');
 const markdownToMobiledoc = require('../../utils/fixtures/data-generator').markdownToMobiledoc;
-const dnsPromises = require('dns').promises;
 const jobsService = require('../../../core/server/services/mentions-jobs');
 
 let agent;
@@ -30,9 +28,7 @@ describe('Mentions Service', function () {
 
     beforeEach(async function () {
         // externalRequest does dns lookup; stub to make sure we don't fail with fake domain names
-        sinon.stub(dnsPromises, 'lookup').callsFake(function () {
-            return Promise.resolve({address: '123.123.123.123'});
-        });
+        mockManager.disableNetwork();
 
         // mock response from website mentioned by post to provide endpoint
         mentionMock = nock(mentionUrl.href)
