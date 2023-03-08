25405581042ca65d451eb386aafa0f5e71443634
Simon Backx <simon@ghost.org>
2023-03-08 16:30:54 +0100

    🐛 Fixed HTML escaping when using outbound link tagging (#16380)
    fixes https://github.com/TryGhost/Team/issues/2666
    
    - Somehow occurrences of `&map_` got replaced with `&#x21A6;`
    - Disables escaping &, ', " and other HTML characters when not needed
    (escaping is already handled by mobiledoc/lexical)
    - Bumps unit test coverage of link replacer to 100%

--- ghost/link-replacer/test/LinkReplacer.test.js
+++ ghost/link-replacer/test/LinkReplacer.test.js
@@ -1,7 +1,13 @@
 const assert = require('assert');
 const linkReplacer = require('../lib/LinkReplacer');
+const cheerio = require('cheerio');
+const sinon = require('sinon');
 
 describe('LinkReplacementService', function () {
+    afterEach(function () {
+        sinon.restore();
+    });
+
     it('exported', function () {
         assert.equal(require('../index'), linkReplacer);
     });
@@ -13,6 +19,21 @@ describe('LinkReplacementService', function () {
 
             const replaced = await linkReplacer.replace(html, () => new URL('https://google.com/test-dir?test-query'));
             assert.equal(replaced, expected);
+        });
+
+        it('Doesn\'t break weird &map', async function () {
+            // Refs https://github.com/TryGhost/Team/issues/2666: somehow this gets replaced with https://example.com/test.jpg?test=true↦id=de76 if decoding entities is enabled
+            const html = '<img src="https://example.com/test.jpg?test=true&map_id=test">';
+            const expected = '<img src="https://example.com/test.jpg?test=true&map_id=test">';
+
+            const replaced = await linkReplacer.replace(html, () => new URL('https://google.com/test-dir?test-query'));
+            assert.equal(replaced, expected);
+        });
+
+        it('Does not escape HTML characters', async function () {
+            const html = 'This is a test & this \'should\' not "be" escaped';
+            const replaced = await linkReplacer.replace(html, () => new URL('https://google.com/test-dir?test-query'));
+            assert.equal(replaced, html);
         });
 
         it('Can replace to string', async function () {
@@ -29,6 +50,14 @@ describe('LinkReplacementService', function () {
 
             const replaced = await linkReplacer.replace(html, () => 'valid');
             assert.equal(replaced, expected);
+        });
+
+        it('Ignores cheerio errors', async function () {
+            sinon.stub(cheerio, 'load').throws(new Error('test'));
+            const html = '<a href="http://localhost:2368/dir/path">link</a>';
+
+            const replaced = await linkReplacer.replace(html, () => 'valid');
+            assert.equal(replaced, html);
         });
     });
 });
