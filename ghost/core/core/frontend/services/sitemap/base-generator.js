6320bccbb409225cf9afcb52040d9614a74c3a74
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-03-09 09:15:12 +0800

    Simplified condition

--- ghost/core/core/frontend/services/sitemap/base-generator.js
+++ ghost/core/core/frontend/services/sitemap/base-generator.js
@@ -23,10 +23,7 @@ class BaseSiteMapGenerator {
     }
 
     hasCanonicalUrl(datum) {
-        if (datum.canonical_url) {
-            return true;
-        }
-        return false;
+        return Boolean(datum?.canonical_url);
     }
 
     generateXmlFromNodes(page) {
