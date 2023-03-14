67382926f8d498bf3a4eb507185388487bb053dc
Djordje Vlaisavljevic <dzvlais@gmail.com>
2023-03-14 15:02:07 +0000

    Made toggle font leading tighter
    refs https://github.com/TryGhost/Team/issues/2643

--- koenig/koenig-lexical/src/components/ui/cards/ToggleCard.jsx
+++ koenig/koenig-lexical/src/components/ui/cards/ToggleCard.jsx
@@ -7,7 +7,7 @@ export function ToggleCard({isEditing, header, headerPlaceholder, content, conte
         <div className="rounded border border-grey/40 py-4 px-6">
             <div className="flex items-start justify-between">
                 { (isEditing || header) &&
-                    <div className={`mr-2 font-sans text-xl font-bold leading-relaxed text-black ${header ? 'opacity-100' : 'opacity-40'}`}>
+                    <div className={`mr-2 font-sans text-xl font-bold leading-snug text-black ${header ? 'opacity-100' : 'opacity-40'}`}>
                         {header || headerPlaceholder}
                     </div>
                 }
@@ -16,7 +16,7 @@ export function ToggleCard({isEditing, header, headerPlaceholder, content, conte
                 </div>
             </div>
             { (isEditing || content) &&
-                <div className={`mt-2 w-full font-serif text-xl font-normal leading-relaxed text-grey-900 ${content ? 'opacity-100' : 'opacity-40'} `}>
+                <div className={`mt-2 w-full font-serif text-xl font-normal leading-normal text-grey-900 ${content ? 'opacity-100' : 'opacity-40'} `}>
                     {content || contentPlaceholder}
                 </div>
             }
