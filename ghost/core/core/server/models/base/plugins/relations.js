a076240fd6e35ca9ee586a7f4d2339f7c589717b
Simon Backx <simon@ghost.org>
2023-03-14 10:58:58 +0100

    Fixed getLazyRelation returning new model for optional relations (#16411)
    no issue
    
    When using `getLazyRelation` on an optional relation that is not set, it
    will return a newly created model instead of a model from the database.
    - Adds a new require option to `getLazyRelation`, that throws an error
    if the relation is not set (off by default to match existing use cases)
    - This caused a bug (not visible because we always pass a newsletter id)
    in email previews, where when the newsletter id was not explicitly set,
    it would use `newsletter = (await post.getLazyRelation('newsletter')) ??
    (await this.models.Newsletter.getDefaultNewsletter());`, which always
    returned the first one, and could return a newly initiated newsletter
    with all properties set to undefined.
    - Some page snapshots are altered by this, because the usage of
    `getLazyRelation` on a post no longer sets the email relation to some
    new model.

--- ghost/core/core/server/models/base/plugins/relations.js
+++ ghost/core/core/server/models/base/plugins/relations.js
@@ -1,3 +1,5 @@
+const errors = require('@tryghost/errors');
+
 /**
  * @param {import('bookshelf')} Bookshelf
  */
@@ -9,6 +11,7 @@ module.exports = function (Bookshelf) {
          * @param {string} name Name of the relation to load
          * @param {Object} [options] Options to pass to the fetch when not yet loaded (or when force refreshing)
          * @param {boolean} [options.forceRefresh] If true, the relation will be fetched again even if it has already been loaded.
+         * @param {boolean} [options.require] Off by default. Throws an error if relation is not found.
          * @returns {Promise<import('bookshelf').Model|import('bookshelf').Collection|null>}
          */
         getLazyRelation: async function (name, options = {}) {
@@ -18,13 +21,32 @@ module.exports = function (Bookshelf) {
             }
 
             if (!this[name]) {
+                if (options.require) {
+                    throw new errors.NotFoundError();
+                }
                 return undefined;
             }
+
+            // Explicitly set require to false if it's not set, because default for .fetch is true (not false)
+            if (options.require) {
+                options.require = true;
+            } else {
+                options.require = false;
+            }
+
             // Not yet loaded, or force refresh
             // Note that we don't use .refresh on the relation on options.forceRefresh
             // Because the relation can also be a collection, which doesn't have a refresh method
             const instance = this[name]();
             await instance.fetch(options);
+
+            if (!instance.id && !(instance instanceof Bookshelf.Collection)) {
+                // Some weird behaviour in Bookshelf allows to just return a newly created model instance instead of throwing an error
+                if (options.require) {
+                    throw new errors.NotFoundError();
+                }
+                return undefined;
+            }
             this.relations[name] = instance;
             return instance;
         }
