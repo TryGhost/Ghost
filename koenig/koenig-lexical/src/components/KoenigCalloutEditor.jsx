9e575a04599dcf409cbcb5db2045e63f5bd1b8e3
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-03-20 16:48:26 +0800

    Fixed callout editor selection (TryGhost/Koenig#532)
    no issue
    
    there was an issue (currently similar experience with the photo
      / video captions) where editor doesn't stay focussed and updates the
    wrong node when there's multiple cards loaded with inner
    ComposableEditors

--- koenig/koenig-lexical/src/components/KoenigCalloutEditor.jsx
+++ koenig/koenig-lexical/src/components/KoenigCalloutEditor.jsx
@@ -2,7 +2,7 @@
 
 import CardContext from '../context/CardContext.jsx';
 import React, {useCallback, useContext} from 'react';
-import {$createParagraphNode, $getNodeByKey, $setSelection, COMMAND_PRIORITY_LOW, FOCUS_COMMAND, KEY_ENTER_COMMAND} from 'lexical';
+import {$createParagraphNode, $getNodeByKey, COMMAND_PRIORITY_LOW, KEY_ENTER_COMMAND} from 'lexical';
 import {HtmlOutputPlugin, KoenigComposableEditor, KoenigComposer, MINIMAL_NODES, MINIMAL_TRANSFORMERS, RestrictContentPlugin} from '../index.js';
 import {mergeRegister} from '@lexical/utils';
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
@@ -21,13 +21,13 @@ function CalloutEditorPlugin({parentEditor}) {
 
     // focus on caption editor when something is typed while card is selected
     const handleKeyDown = useCallback((event) => {
-        // don't focus caption input if any other input or textarea is focused
+    // don't focus caption input if any other input or textarea is focused
         if (event.target.matches('input, textarea')) {
             return;
         }
-    
-        // only if key is printable key, focus on editor
-        if (event.key.length === 1 && !event.ctrlKey && !event.metaKey && !event.altKey) {
+
+        // only trigger on Enter key
+        if (event.key === 'Enter') {
             editor.focus();
         }
     }, [editor]);
@@ -37,24 +37,11 @@ function CalloutEditorPlugin({parentEditor}) {
         return () => {
             document.removeEventListener('keydown', handleKeyDown);
         };
-    }, [handleKeyDown, editor]);
+    }, [handleKeyDown, editor, nodeKey]);
 
     React.useEffect(
         () => {
             return mergeRegister(
-                editor.registerCommand(
-                    FOCUS_COMMAND,
-                    () => {
-                        // focus on the parent node key
-                        parentEditor.update(() => {
-                            const cardNode = $getNodeByKey(nodeKey);
-                            $setSelection(cardNode);
-                        });
-
-                        return false;
-                    },
-                    COMMAND_PRIORITY_LOW
-                ),
                 editor.registerCommand(
                     KEY_ENTER_COMMAND,
                     (event) => {
