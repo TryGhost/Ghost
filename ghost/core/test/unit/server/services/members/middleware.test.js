507f24d9e549c1487909ea8456260f25ff8ba17d
Simon Backx <simon@ghost.org>
2023-03-24 15:17:08 +0100

    Fixed redirecting back to account home after sign in in Portal
    refs https://github.com/TryGhost/Team/issues/2674
    
    When going to /#/portal/account when not signed in, you are redirected to the login page. But once signed in, you aren't redirected back to the account page. This fixes this issue by adding an extra and optional redirect parameter when requesting a magic token via email.
    
    This new parameter allows to override the default behaviour of using the Referer HTTP header, which doesn't include the hash/fragment part of the URL.
    
    The referrer is already restricted to only allow redirects to the site, not external URLs.

--- ghost/core/test/unit/server/services/members/middleware.test.js
+++ ghost/core/test/unit/server/services/members/middleware.test.js
@@ -146,7 +146,7 @@ describe('Members Service Middleware', function () {
 
         it('redirects member to referrer param path on signup if it is on the site', async function () {
             req.url = '/members?token=test&action=signin&r=https%3A%2F%2Fsite.com%2Fblah%2Fmy-post%2F';
-            req.query = {token: 'test', action: 'signin', r: 'https://site.com/blah/my-post/'};
+            req.query = {token: 'test', action: 'signin', r: 'https://site.com/blah/my-post/#comment-123'};
 
             // Fake token handling failure
             membersService.ssr.exchangeTokenForSession.resolves({});
@@ -157,7 +157,7 @@ describe('Members Service Middleware', function () {
             // Check behavior
             next.calledOnce.should.be.false();
             res.redirect.calledOnce.should.be.true();
-            res.redirect.firstCall.args[0].should.eql('/blah/my-post/?success=true&action=signin');
+            res.redirect.firstCall.args[0].should.eql('https://site.com/blah/my-post/?success=true&action=signin#comment-123');
         });
 
         it('does not redirect to referrer param if it is external', async function () {
