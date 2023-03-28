4b3f26a9970c50de81bebd8cc99d15c3d0b4905b
Simon Backx <simon@ghost.org>
2023-03-28 11:38:43 +0200

    Added newsletter color columns (#16506)
    fixes https://github.com/TryGhost/Team/issues/2825
    
    Adds 3 new columns to the newsletters table to allow more customisation:
    - background_color, default to 'light'
    - border_color, nullable, null = no border
    - title_color, nullable, null = auto

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '67a16bde6f7dd46c5e07d7fca1005a53';
+    const currentSchemaHash = '395330cb390d669843e4913b63a4cf25';
     const currentFixturesHash = '869ceb3302303494c645f4201540ead3';
     const currentSettingsHash = '7b567742b9667d38191d8455c422c5d5';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
