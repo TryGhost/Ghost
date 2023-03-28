945041c9727e4b0116efdd9209d352153eb6db32
Rishabh Garg <rish@ghost.org>
2023-03-29 01:12:56 +0530

    Merged mentions UI and email lab flags (#16518)
    closes https://github.com/TryGhost/Team/issues/2851
    
    We had two separate flags to manage the Mentions beta, one for showing
    UI and other for sending emails. This change combines them both under
    the single `webmentions` flag that was previously only used to show the
    UI.

--- ghost/admin/tests/acceptance/staff-test.js
+++ ghost/admin/tests/acceptance/staff-test.js
@@ -81,7 +81,6 @@ describe('Acceptance: Staff', function () {
             enableMembers(this.server);
             enableStripe(this.server);
             enableLabsFlag(this.server, 'webmentions');
-            enableLabsFlag(this.server, 'webmentionEmails');
 
             admin = this.server.create('user', {email: 'admin@example.com', roles: [adminRole]});
 
