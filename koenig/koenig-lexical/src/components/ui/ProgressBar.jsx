f80b702fa18d060f8bb65bcadaa2b9e1c199af40
Djordje Vlaisavljevic <dzvlais@gmail.com>
2023-03-29 17:36:25 +0100

    Added dark mode design for File card loading state
    refs https://github.com/TryGhost/Team/issues/2739

--- koenig/koenig-lexical/src/components/ui/ProgressBar.jsx
+++ koenig/koenig-lexical/src/components/ui/ProgressBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export function ProgressBar({style, fullWidth, bgStyle}) {
     return (
-        <div className={`rounded-full bg-grey-200 ${fullWidth ? 'w-full' : 'mx-auto w-3/5'} ${bgStyle === 'transparent' ? 'bg-white/30' : 'bg-grey-200'}`} data-testid="progress-bar">
+        <div className={`rounded-full bg-grey-200 dark:bg-black ${fullWidth ? 'w-full' : 'mx-auto w-3/5'} ${bgStyle === 'transparent' ? 'bg-white/30' : 'bg-grey-200'}`} data-testid="progress-bar">
             <div className="rounded-full bg-green py-1 text-center text-xs leading-none text-white" style={style}></div>
         </div>
     );
