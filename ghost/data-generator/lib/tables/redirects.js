1d7572fed5258f92c222c7a263fe9f27c257852c
Sam Lord <sam@ghost.org>
2023-03-30 16:49:13 +0100

    Data generator: Fixed the URL formatting for newsletter clicks
    no issue

--- ghost/data-generator/lib/tables/redirects.js
+++ ghost/data-generator/lib/tables/redirects.js
@@ -24,7 +24,7 @@ class RedirectsImporter extends TableImporter {
         return {
             id: faker.database.mongodbObjectId(),
             from: `/r/${faker.datatype.hexadecimal({length: 32, prefix: '', case: 'lower'})}`,
-            to: `https://${faker.internet.url()}/${faker.helpers.slugify(`${faker.word.adjective()} ${faker.word.noun()}`).toLowerCase()}`,
+            to: `${faker.internet.url()}/${faker.helpers.slugify(`${faker.word.adjective()} ${faker.word.noun()}`).toLowerCase()}`,
             post_id: this.model.id,
             created_at: this.model.published_at,
             updated_at: this.model.published_at
