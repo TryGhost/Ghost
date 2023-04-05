a956e1a5dbc86f89842b811cafdae04b3a1079b6
Djordje Vlaisavljevic <dzvlais@gmail.com>
2023-04-05 22:03:01 +0100

    Updated error state design for Bookmark card
    refs https://github.com/TryGhost/Team/issues/2890

--- koenig/koenig-lexical/src/components/ui/UrlInput.jsx
+++ koenig/koenig-lexical/src/components/ui/UrlInput.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {ReactComponent as CloseIcon} from '../../assets/icons/kg-close.svg';
 
 export function UrlInput({dataTestId, value, placeholder, handleUrlChange, handleUrlInput, hasError, handlePasteAsLink, handleRetry, handleClose, isLoading}) {
     if (isLoading) {
@@ -7,29 +8,15 @@ export function UrlInput({dataTestId, value, placeholder, handleUrlChange, handl
         );
     }
     if (hasError) {
-        const containerStyles = {
-            backgroundColor: 'rgba(240,82,48,.05)',
-            borderColor: '#f85465',
-            borderRadius: '0.3rem',
-            borderStyle: `solid`,
-            borderWidth: '1px',
-            fontSize: '1.4rem',
-            color: '#f50b23',
-            display: 'flex',
-            minWidth: '500px',
-            paddingTop: '8px',
-            paddingBottom: '8px'
-        };
-        const itemStyles = {
-            marginRight: '1.2rem'
-        };
         return (
-            <div className="flex-row items-center" style={containerStyles}>
-                <span style={{paddingLeft: '10px', ...itemStyles}}>There was an error when parsing the URL.</span>
-                <button style={{color: '#c5081b', ...itemStyles}} type="button"><span className="underline" onClick={handleRetry}><strong>Retry</strong></span></button>
-                <button style={{color: '#c5081b', ...itemStyles}} type="button"><span className="underline" onClick={handlePasteAsLink}><strong>Paste URL as link</strong></span></button>
-                <button style={{paddingRight: '5px'}} type="button" onClick={handleClose}>
-                    X
+            <div className="min-width-[500px] flex flex-row items-center justify-between rounded-sm border border-red bg-red/5 px-3 py-2 text-sm leading-snug text-red">
+                <div>
+                    <span className="mr-3">There was an error when parsing the URL.</span>
+                    <button className="mr-3" type="button"><span className="underline" onClick={handleRetry}><strong>Retry</strong></span></button>
+                    <button className="mr-3" type="button"><span className="underline" onClick={handlePasteAsLink}><strong>Paste URL as link</strong></span></button>
+                </div>
+                <button type="button" onClick={handleClose}>
+                    <CloseIcon className="red h-3 w-3"/>
                 </button>
             </div>
         );
