6f4e96067901d9f721d69b5f0d9220ef2b49806f
Steve Larson <9larsons@gmail.com>
2023-04-05 15:43:52 -0500

    removed portal to fix focus
    no refs

--- koenig/koenig-lexical/src/components/ui/SettingsPanel.jsx
+++ koenig/koenig-lexical/src/components/ui/SettingsPanel.jsx
@@ -1,5 +1,4 @@
 import ImageUploadForm from './ImageUploadForm';
-import Portal from './Portal';
 import React from 'react';
 import useSettingsPanelReposition from '../../hooks/useSettingsPanelReposition';
 import {ButtonGroup} from './ButtonGroup';
@@ -19,16 +18,14 @@ export function SettingsPanel({children, darkMode}) {
     return (
         // Block with fixed position and transformed ancestor can be incorrectly positioned https://bugs.chromium.org/p/chromium/issues/detail?id=20574
         // Using Portal to avoid such issue as some cards using transformation
-        <Portal>
-            <div className={`${darkMode ? 'dark' : ''}`}>
-                <div ref={ref}
-                    className="not-kg-prose z-[9999999] m-0 flex w-[320px] flex-col gap-2 rounded-lg bg-white bg-clip-padding p-6 font-sans shadow dark:bg-grey-900"
-                    data-testid="settings-panel"
-                >
-                    {children}
-                </div>
+        <div className={`${darkMode ? 'dark' : ''}`}>
+            <div ref={ref}
+                className="not-kg-prose z-[9999999] m-0 flex w-[320px] flex-col gap-2 rounded-lg bg-white bg-clip-padding p-6 font-sans shadow dark:bg-grey-900"
+                data-testid="settings-panel"
+            >
+                {children}
             </div>
-        </Portal>
+        </div>
     );
 }
 
