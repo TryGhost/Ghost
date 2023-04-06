7ac3a60f86a6310ee4e016f7a7959f2e256c9dbf
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-04-06 17:47:09 +0800

    Reposition when close to the bottom of the page
    ref https://github.com/TryGhost/Team/issues/2865
    
    - Update handleScroll function to calculate available space below the button
    - Shift the portal a couple of pixels above the buttonRef when too close to the bottom
    - Add shiftPixels and pickerHeight constants for easy finetuning.

--- koenig/koenig-lexical/src/components/ui/EmojiPickerPortal.jsx
+++ koenig/koenig-lexical/src/components/ui/EmojiPickerPortal.jsx
@@ -5,12 +5,22 @@ import data from '@emoji-mart/data';
 const EmojiPickerPortal = ({onEmojiClick, buttonRef}) => {
     const [position, setPosition] = React.useState(null);
 
+    const shiftPixels = 35; // how many pixels we want to move it up when it's at the bottom of the screen
     const handleScroll = React.useCallback(() => {
         if (buttonRef.current) {
             const rect = buttonRef.current.getBoundingClientRect();
             const scrollX = document.documentElement.scrollLeft;
             const scrollY = document.documentElement.scrollTop;
-            setPosition({x: (rect.left + scrollX) / 1.5, y: rect.top + scrollY});
+            const windowHeight = window.innerHeight;
+            const pickerHeight = 352; // Approximate height of the emoji picker, adjust if needed
+    
+            let adjustedTop = rect.top + scrollY;
+    
+            if (adjustedTop + pickerHeight > windowHeight) {
+                adjustedTop = rect.top - pickerHeight - shiftPixels + scrollY;
+            }
+    
+            setPosition({x: (rect.left + scrollX) / 1.5, y: adjustedTop});
         }
     }, [buttonRef]);
     
