49917a629a264fdba53d8086bc408e93d8418eae
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-04-06 16:36:51 +0800

    Update EmojiPicker on Scroll position
    refs https://github.com/TryGhost/Team/issues/2865
    
    - Modify EmojiPickerPortal component to listen to the scroll event on the document
    - Use useCallback to optimize handleScroll function
    - Update position calculation to consider scroll offsets from the entire document

--- koenig/koenig-lexical/src/components/ui/EmojiPickerPortal.jsx
+++ koenig/koenig-lexical/src/components/ui/EmojiPickerPortal.jsx
@@ -5,27 +5,40 @@ import data from '@emoji-mart/data';
 const EmojiPickerPortal = ({onEmojiClick, buttonRef}) => {
     const [position, setPosition] = React.useState(null);
 
-    React.useEffect(() => {
+    const handleScroll = React.useCallback(() => {
         if (buttonRef.current) {
             const rect = buttonRef.current.getBoundingClientRect();
-            setPosition({x: rect.left / 1.5, y: rect.bottom});
-        } else {
-            setPosition(null);
+            const scrollX = document.documentElement.scrollLeft;
+            const scrollY = document.documentElement.scrollTop;
+            setPosition({x: (rect.left + scrollX) / 1.5, y: rect.top + scrollY});
         }
     }, [buttonRef]);
-  
+    
+    React.useEffect(() => {
+        handleScroll();
+        document.addEventListener('scroll', handleScroll, true); // Use true for capture phase
+        return () => {
+            document.removeEventListener('scroll', handleScroll, true);
+        };
+    }, [handleScroll]);
+    
     if (!position) {
         return null;
     }
-  
     const {x, y} = position;
-
+  
     const handleClick = (e) => {
         e.stopPropagation();
     };
+  
+    const style = {
+        left: x,
+        top: y,
+        position: 'fixed'
+    };
     return (
         <Portal>
-            <div className='fixed z-10 mr-9 mt-3 rounded-md bg-white' data-testid="emoji-picker-container" style={{left: x, top: y}} onClick={handleClick}>
+            <div className='z-10 mr-9 mt-10 rounded-md bg-white' data-testid="emoji-picker-container" style={style} onClick={handleClick}>
                 <div className=''>
                     <Picker
                         autoFocus='true'
