a5884aafdcbecdf968878ae711f0691849623655
Simon Backx <simon@ghost.org>
2023-04-07 16:00:34 +0200

    Added bulk destory posts api
    fixes https://github.com/TryGhost/Team/issues/2921

--- ghost/admin/app/components/posts-list/context-menu.js
+++ ghost/admin/app/components/posts-list/context-menu.js
@@ -1,11 +1,15 @@
 import Component from '@glimmer/component';
+import DeletePostsModal from './modals/delete-posts';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
+import {task} from 'ember-concurrency';
 
 export default class PostsContextMenu extends Component {
     @service ajax;
     @service ghostPaths;
     @service session;
+    @service infinity;
+    @service modals;
 
     get menu() {
         return this.args.menu;
@@ -16,10 +20,36 @@ export default class PostsContextMenu extends Component {
     }
 
     @action
-    deletePosts() {
-        // Use filter in menu.selectionList.filter
-        alert('Deleting posts not yet supported.');
+    async deletePosts() {
         this.menu.close();
+        const confirmedDelete = await this.modals.open(DeletePostsModal, {
+            isSingle: this.selectionList.isSingle,
+            confirm: this.deletePostsTask
+        });
+        if (!confirmedDelete) {
+            return;
+        }
+    }
+
+    @task
+    *deletePostsTask(close) {
+        const deletedModels = this.selectionList.availableModels;
+        yield this.performBulkDestroy();
+        const remainingModels = this.selectionList.infinityModel.content.filter((model) => {
+            return !deletedModels.includes(model);
+        });
+        // Deleteobjects method from infintiymodel is broken for all models except the first page, so we cannot use this
+        this.infinity.replace(this.selectionList.infinityModel, remainingModels);
+        this.selectionList.clearSelection();
+        close();
+
+        return true;
+    }
+
+    async performBulkDestroy() {
+        const filter = this.selectionList.filter;
+        let bulkUpdateUrl = this.ghostPaths.url.api(`posts`) + `?filter=${encodeURIComponent(filter)}`;
+        return await this.ajax.delete(bulkUpdateUrl);
     }
 
     async performBulkEdit(_action, meta = {}) {
