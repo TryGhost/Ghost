b5d85100778e4721c8614906ba059d52e4738b44
Naz <hi@nazavo.com>
2023-04-07 11:23:42 +0200

    Removed unnecessary mockMail call
    no issue
    
    - This test has nothing to do with outgoing emails, so no need to mock mail

--- ghost/core/test/e2e-server/well-known.test.js
+++ ghost/core/test/e2e-server/well-known.test.js
@@ -5,7 +5,6 @@ describe('.well-known', function () {
     let agentGhostAPI;
 
     before(async function () {
-        mockManager.mockMail();
         agentGhostAPI = await agentProvider.getGhostAPIAgent();
     });
 
