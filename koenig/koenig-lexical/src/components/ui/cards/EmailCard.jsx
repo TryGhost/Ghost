bf8822a8c3eff0afd0455efb0d9c072b8b200a4a
Sag <guptazy@gmail.com>
2023-04-07 16:08:12 +0200

    Fixed Email Content editor layout

--- koenig/koenig-lexical/src/components/ui/cards/EmailCard.jsx
+++ koenig/koenig-lexical/src/components/ui/cards/EmailCard.jsx
@@ -10,11 +10,11 @@ export function EmailCard({htmlEditor, isEditing}) {
                 autoFocus={true}
                 initialEditor={htmlEditor}
                 nodes='basic'
-                textClassName='px-3 kg-email-html'
+                textClassName='kg-email-html'
             />
 
             {isEditing && 
-                <div className="flex items-center justify-center bg-grey-100 p-2 font-sans text-sm font-normal leading-8 text-grey-600">
+                <div className="mt-0 flex items-center justify-center bg-grey-100 p-2 font-sans text-sm font-normal leading-8 text-grey-600">
                     Only visible when delivered by email, this card will not be published on your site.
                     <a href="https://ghost.org/help/email-newsletters/#email-cards" rel="noopener noreferrer" target="_blank">
                         <HelpIcon className="ml-1 mt-[1px] stroke-[1.2px] text-grey-600" />
