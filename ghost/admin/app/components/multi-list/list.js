114a6eb9539f1791d754020c79ca135b4995406e
Simon Backx <simon@ghost.org>
2023-04-11 15:35:27 +0200

    Fixed CMD+F and multiple post selection
    fixes https://github.com/TryGhost/Team/issues/2939
    
    When using CMD+F, the multiple selection list still thinks CMD is pressed because the keyup event is not fired. This is fixed by clearing the pressed keys when the window is blurred.

--- ghost/admin/app/components/multi-list/list.js
+++ ghost/admin/app/components/multi-list/list.js
@@ -27,6 +27,7 @@ export default class ListComponent extends Component {
         window.removeEventListener('keydown', this.onKeyDow, {passive: true});
         window.removeEventListener('keyup', this.onKeyUp, {passive: true});
         window.removeEventListener('click', this.onWindowClicked, {passive: true});
+        window.removeEventListener('blur', this.onWindowBlur, {passive: true});
     }
 
     @action
@@ -34,6 +35,7 @@ export default class ListComponent extends Component {
         window.addEventListener('keydown', this.onKeyDown, {passive: false});
         window.addEventListener('keyup', this.onKeyUp, {passive: true});
         window.addEventListener('click', this.onWindowClicked, {passive: true});
+        window.addEventListener('blur', this.onWindowBlur, {passive: true});
     }
 
     @action
@@ -42,6 +44,14 @@ export default class ListComponent extends Component {
         if (!event.metaKey && !event.ctrlKey) {
             this.selectionList.clearSelection();
         }
+    }
+
+    @action
+    onWindowBlur() {
+        // This is required because the keyup event won't be fired again
+        this.ctrlPressed = false;
+        this.metaPressed = false;
+        this.shiftPressed = false;
     }
 
     @action
