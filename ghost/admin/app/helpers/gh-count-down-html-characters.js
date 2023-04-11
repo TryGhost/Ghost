fd6d6fb8412e2901c053dd3108938d04002e9025
Simon Backx <simon@ghost.org>
2023-04-11 17:49:00 +0200

    Fixed gh-count-down-html-characters helper to support null values
    no issue
    
    The default null value for the sigup terms caused an error. This fixes that issue.

--- ghost/admin/app/helpers/gh-count-down-html-characters.js
+++ ghost/admin/app/helpers/gh-count-down-html-characters.js
@@ -4,6 +4,11 @@ import {helper} from '@ember/component/helper';
 export default helper(function (params) {
     let [content, maxCharacters] = params;
 
+    if (!content) {
+        // Protect against NULL content
+        content = '';
+    }
+
     // Strip HTML-tags and characters from content so we have a reliable character count
     content = content.replace(/<[^>]*>?/gm, '');
     content = content.replace(/&nbsp;/g, ' ');
