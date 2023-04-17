cfef45061b717843fd7859a911d03349fcba3f00
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-04-17 17:29:16 +0100

    Added rendered html

--- ghost/admin/app/components/modal-post-history.js
+++ ghost/admin/app/components/modal-post-history.js
@@ -4,17 +4,15 @@ import diff from 'node-htmldiff';
 export default ModalComponent.extend({
 
     get previous() {
-        return `{root: {children: [{children: [{detail: 0,format: 0,mode: 'normal',style: '',text: 'This is a sentence - changed ',type: 'text',version: 1}],direction: 'ltr',format: '',indent: 0,type: 'paragraph',version: 1},{children: [{detail: 0,format: 0,mode: 'normal',style: '',text: 'Awesome!',type: 'text',version: 1}],direction: 'ltr',format: '',indent: 0,type: 'paragraph',version: 1},{children: [{detail: 0,format: 0,mode: 'normal',style: '',text: 'Yala',type: 'text',version: 1}],direction: 'ltr',format: '',indent: 0,type: 'paragraph',version: 1}],direction: 'ltr',format: '',indent: 0,type: 'root',version: 1}}`;
+        return `<h1> Hello this is the past </h1>`;
     },
 
     get current() {
-        return `{root: {children: [{children: [{detail: 0,format: 0,mode: 'normal',style: '',text: 'Hello, world',type: 'text',version: 1}],direction: 'ltr',format: '',indent: 0,type: 'paragraph',version: 1},{children: [{detail: 0,format: 0,mode: 'normal',style: '',text: 'Weird...',type: 'text',version: 1}],direction: 'ltr',format: '',indent: 0,type: 'paragraph',version: 1},{children: [{detail: 0,format: 0,mode: 'normal',style: '',text: 'Awesome!! That\'s sooooo cool !',type: 'text',version: 1}],direction: 'ltr',format: '',indent: 0,type: 'paragraph',version: 1},{children: [],direction: null,format: '',indent: 0,type: 'paragraph',version: 1},{children: [{detail: 0,format: 0,mode: 'normal',style: '',text: 'hello??',type: 'text',version: 1}],direction: 'ltr',format: '',indent: 0,type: 'paragraph',version: 1},{children: [{detail: 0,format: 0,mode: 'normal',style: '',text: 'hmmm',type: 'text',version: 1}],direction: 'ltr',format: '',indent: 0,type: 'paragraph',version: 1},{children: [],direction: null,format: '',indent: 0,type: 'paragraph',version: 1},{children: [{detail: 0,format: 0,mode: 'normal',style: '',text: 'huh!',type: 'text',version: 1}],direction: 'ltr',format: '',indent: 0,type: 'paragraph',version: 1},{children: [],direction: null,format: '',indent: 0,type: 'paragraph',version: 1}],direction: 'ltr',format: '',indent: 0,type: 'root',version: 1}}`;
+        return `<h1> Hello this is the future </h1>`;
     },
      
     get postDiff() {
-        let post = this.get('post');
-        let previousPost = this.get('previousPost');
-        let diffHtml = diff(previousPost.get('html'), post.get('html'));
+        let diffHtml = diff(this.previous, this.current);
         return diffHtml;
     }
 });
