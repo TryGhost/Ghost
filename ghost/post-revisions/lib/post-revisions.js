b8ccc1c14718334813ab060fc001473db86dc5ab
Fabien "egg" O'Carroll <fabien@allou.is>
2023-04-17 14:14:33 +0100

    Implemented initial PostRevisions class
    This aims to implement the current strategy of revision generation so that we
    can switch to it and start tweaking it whilst the flag is enabled

--- ghost/post-revisions/lib/post-revisions.js
+++ ghost/post-revisions/lib/post-revisions.js
@@ -1,1 +1,76 @@
-// post-revisions.js
+/**
+ * @typedef {object} PostLike
+ * @property {string} id
+ * @property {string} lexical
+ */
+
+/**
+ * @typedef {object} Revision
+ * @property {string} post_id
+ * @property {string} lexical
+ * @property {number} created_at_ts
+ */
+
+class PostRevisions {
+    /**
+     * @param {object} deps
+     * @param {object} deps.config
+     * @param {number} deps.config.max_revisions
+     */
+    constructor(deps) {
+        this.config = deps.config;
+    }
+
+    /**
+     * @param {PostLike} previous
+     * @param {PostLike} current
+     * @param {Revision[]} revisions
+     * @returns {boolean}
+     */
+    shouldGenerateRevision(previous, current, revisions) {
+        if (!previous) {
+            return false;
+        }
+        if (revisions.length === 0) {
+            return true;
+        }
+        return previous.lexical !== current.lexical;
+    }
+
+    /**
+     * @param {PostLike} previous
+     * @param {PostLike} current
+     * @param {Revision[]} revisions
+     * @returns {Promise<Revision[]>}
+     */
+    async getRevisions(previous, current, revisions) {
+        if (!this.shouldGenerateRevision(previous, current, revisions)) {
+            return revisions;
+        }
+
+        const currentRevision = this.convertPostLikeToRevision(current);
+
+        if (revisions.length === 0) {
+            return [
+                this.convertPostLikeToRevision(previous, 1),
+                currentRevision
+            ];
+        }
+
+        return [...revisions, currentRevision].slice(-this.config.max_revisions);
+    }
+
+    /**
+     * @param {PostLike} input
+     * @returns {Revision}
+     */
+    convertPostLikeToRevision(input, offset = 0) {
+        return {
+            post_id: input.id,
+            lexical: input.lexical,
+            created_at_ts: Date.now() - offset
+        };
+    }
+}
+
+module.exports = PostRevisions;
