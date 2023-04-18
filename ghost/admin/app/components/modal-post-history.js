6aba4bf7973f9c38cb9f4a833ed0ffbc2ee3b463
Fabien "egg" O'Carroll <fabien@allou.is>
2023-04-18 14:44:54 +0100

    fixup! Added initial version of post revision diffing

--- ghost/admin/app/components/modal-post-history.js
+++ ghost/admin/app/components/modal-post-history.js
@@ -56,15 +56,27 @@ export default ModalComponent.extend({
         });
     },
 
-    get previousLexical() {
+    get selectedRevision() {
         let revisions = this.post.get('postRevisions').toArray();
         // Revisions are in chronological order, and the last revision is the
         // the current post, so the second to last is the previous revision
-        return revisions[revisions.length - 2].get('lexical');
+        return revisions[revisions.length - 2];
+    },
+
+    get previousLexical() {
+        return this.selectedRevision.get('lexical');
     },
 
     get currentLexical() {
         return this.post.get('lexical');
+    },
+
+    get previousTitle() {
+        return this.selectedRevision.get('title') || this.post.get('title');
+    },
+
+    get currentTitle() {
+        return this.post.get('title');
     },
 
     get cardConfig() {
