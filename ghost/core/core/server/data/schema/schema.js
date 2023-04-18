2d51f80f276f6aa95af0cd7caad42f984dc160a5
Chris Raible <chris@ghost.org>
2023-04-18 15:02:13 +0100

    Renamed foreign key constraint in migration v5.45 (#16664)
    - the autogenerated constraint name was too long for MySQL, yielding the
    error below (although it seems to be fine for sqlite)
    - this change adds an explicit, shorthand name for the constraint to
    stay under the limit on MySQL8
    
    Error: alter table `post_revisions` add constraint
    `post_revisions_author_id_foreign` foreign key (`author_id`) references
    `users` (`id`), algorithm=copy - Specified key was too long; max key
    length is 3072 bytes

--- ghost/core/core/server/data/schema/schema.js
+++ ghost/core/core/server/data/schema/schema.js
@@ -403,7 +403,7 @@ module.exports = {
         lexical: {type: 'text', maxlength: 1000000000, fieldtype: 'long', nullable: true},
         created_at_ts: {type: 'bigInteger', nullable: false},
         created_at: {type: 'dateTime', nullable: false},
-        author_id: {type: 'string', maxlength: 24, nullable: true, references: 'users.id', cascadeDelete: false},
+        author_id: {type: 'string', maxlength: 24, nullable: true, references: 'users.id', cascadeDelete: false, constraintName: 'post_revs_author_id_foreign'},
         title: {type: 'string', maxlength: 2000, nullable: true, validations: {isLength: {max: 255}}}
     },
     members: {
