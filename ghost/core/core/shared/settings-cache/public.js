de41b07228ab74967abb1150c759c4c2e825d044
Naz <hi@nazavo.com>
2023-04-19 16:01:40 +0200

    Exposed announcement settings in Content API
    refs https://github.com/TryGhost/Team/issues/3011
    
    - Allows to read following settings fields:
      - announcement - alias to announcement_content
      - announcement_background

--- ghost/core/core/shared/settings-cache/public.js
+++ ghost/core/core/shared/settings-cache/public.js
@@ -39,5 +39,7 @@ module.exports = {
     portal_plans: 'portal_plans',
     portal_name: 'portal_name',
     portal_button: 'portal_button',
-    comments_enabled: 'comments_enabled'
+    comments_enabled: 'comments_enabled',
+    announcement: 'announcement_content',
+    announcement_background: 'announcement_background'
 };
