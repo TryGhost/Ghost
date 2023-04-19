5e23e5948d52b21c51c780e29dd837e5f43ded02
Chris Raible <chris@ghost.org>
2023-04-19 10:08:03 +0100

    Added author details to post_revisions in the posts endpoints
    no issue
    
    - adds author details to post_revisions in the posts endpoints
    - to be used in the post history modal in the admin client

--- ghost/core/core/server/models/post-revision.js
+++ ghost/core/core/server/models/post-revision.js
@@ -1,8 +1,15 @@
 const ghostBookshelf = require('./base');
+const _ = require('lodash');
 
 const PostRevision = ghostBookshelf.Model.extend({
-    tableName: 'post_revisions'
-}, {
+    tableName: 'post_revisions',
+
+    relationships: ['author'],
+
+    author() {
+        return this.belongsTo('User', 'author_id');
+    },
+
     permittedOptions(methodName) {
         let options = ghostBookshelf.Model.permittedOptions.call(this, methodName);
         const validOptions = {
@@ -16,6 +23,14 @@ const PostRevision = ghostBookshelf.Model.extend({
         return options;
     },
 
+    defaultRelations: function defaultRelations(methodName, options) {
+        if (['edit', 'add', 'destroy'].indexOf(methodName) !== -1) {
+            options.withRelated = _.union(['author'], options.withRelated || []);
+        }
+
+        return options;
+    },
+
     orderDefaultRaw() {
         return 'created_at_ts DESC';
     },
@@ -23,9 +38,9 @@ const PostRevision = ghostBookshelf.Model.extend({
     toJSON(unfilteredOptions) {
         const options = PostRevision.filterOptions(unfilteredOptions, 'toJSON');
         const attrs = ghostBookshelf.Model.prototype.toJSON.call(this, options);
-
         // CASE: only for internal accuracy
         delete attrs.created_at_ts;
+        delete attrs.author_id;
         return attrs;
     }
 });
