48a77a2b6b093b00c127e5494b8bebb45aaf3477
Steve Larson <9larsons@gmail.com>
2023-04-19 10:10:01 +0100

    added mixtape parsing to bookmark card
    refs TryGhost/Team#3039

--- koenig/kg-default-nodes/lib/nodes/bookmark/BookmarkParser.js
+++ koenig/kg-default-nodes/lib/nodes/bookmark/BookmarkParser.js
@@ -37,6 +37,55 @@ export class BookmarkParser {
                     };
                 }
                 return null;
+            },
+            div: (nodeElem) => {
+                if (nodeElem.nodeType === 1 && nodeElem.tagName === 'DIV' && nodeElem.className.match(/graf--mixtapeEmbed/)) {
+                    return {
+                        conversion(domNode) {
+                            // Grab the relevant elements - Anchor wraps most of the data
+                            const anchorElement = domNode.querySelector('.markup--mixtapeEmbed-anchor');
+                            const titleElement = anchorElement.querySelector('.markup--mixtapeEmbed-strong');
+                            const descElement = anchorElement.querySelector('.markup--mixtapeEmbed-em');
+                            // Image is a top level field inside it's own a tag
+                            const imgElement = domNode.querySelector('.mixtapeImage');
+
+                            domNode.querySelector('br').remove();
+                            
+                            // Grab individual values from the elements
+                            const url = anchorElement.href;
+                            let title = '';
+                            let description = '';
+                            let thumbnail = '';
+
+                            if (titleElement && titleElement.innerHTML) {
+                                title = titleElement.innerHTML.trim();
+                                // Cleanup anchor so we can see what's left now that we've processed title
+                                anchorElement.removeChild(titleElement);
+                            }
+                    
+                            if (descElement && descElement.innerHTML) {
+                                description = descElement.innerHTML.trim();
+                                // Cleanup anchor so we can see what's left now that we've processed description
+                                anchorElement.removeChild(descElement);
+                            }
+
+                            // Publisher is the remaining text in the anchor, once title & desc are removed
+                            let publisher = anchorElement.innerHTML.trim();
+
+                            // Image is optional,
+                            // The element usually still exists with an additional has.mixtapeImage--empty class and has no background image
+                            if (imgElement && imgElement.style['background-image']) {
+                                thumbnail = imgElement.style['background-image'].match(/url\(([^)]*?)\)/)[1];
+                            }
+
+                            let payload = {url, title, description, publisher, thumbnail};
+                            const node = new self.NodeClass(payload);
+                            return {node};
+                        },
+                        priority: 1
+                    };
+                }
+                return null;
             }
         };
     }
--- koenig/kg-default-nodes/test/nodes/bookmark.test.js
+++ koenig/kg-default-nodes/test/nodes/bookmark.test.js
@@ -297,5 +297,36 @@ describe('BookmarkNode', function () {
             nodes[0].getThumbnail().should.equal(dataset.thumbnail);
             nodes[0].getCaption().should.equal(dataset.caption);
         }));
+
+        // mixtape embeds parse into bookmark cards
+        describe('mixtapes', function () {
+        // Mobiledoc {\"version\":\"0.3.1\",\"atoms\":[],\"cards\":[[\"bookmark\",{\"url\":\"https://slack.engineering/typescript-at-slack-a81307fa288d\",\"metadata\":{\"url\":\"https://slack.engineering/typescript-at-slack-a81307fa288d\",\"title\":\"TypeScript at Slack\",\"description\":\"When Brendan Eich created the very first version of JavaScript for Netscape Navigator 2.0 in merely ten days, it’s likely that he did not expect how far the Slack Desktop App would take his…\",\"author\":\"Felix Rieseberg\",\"publisher\":\"Several People Are Coding\",\"thumbnail\":\"https://miro.medium.com/max/1200/1*-h1bH8gB3I7gPh5AG1HmsQ.png\",\"icon\":\"https://cdn-images-1.medium.com/fit/c/152/152/1*8I-HPL0bfoIzGied-dzOvA.png\"},\"type\":\"bookmark\"}]],\"markups\":[],\"sections\":[[10,0],[1,\"p\",[]]]}
+        // Ghost HTML <figure class="kg-card kg-bookmark-card"><a class="kg-bookmark-container" href="https://slack.engineering/typescript-at-slack-a81307fa288d"><div class="kg-bookmark-content"><div class="kg-bookmark-title">TypeScript at Slack</div><div class="kg-bookmark-description">When Brendan Eich created the very first version of JavaScript for Netscape Navigator 2.0 in merely ten days, it’s likely that he did not expect how far the Slack Desktop App would take his…</div><div class="kg-bookmark-metadata"><img class="kg-bookmark-icon" src="https://cdn-images-1.medium.com/fit/c/152/152/1*8I-HPL0bfoIzGied-dzOvA.png"><span class="kg-bookmark-author">Felix Rieseberg</span><span class="kg-bookmark-publisher">Several People Are Coding</span></div></div><div class="kg-bookmark-thumbnail"><img src="https://miro.medium.com/max/1200/1*-h1bH8gB3I7gPh5AG1HmsQ.png"></div></a></figure>
+        // Medium Export HTML <div class="graf graf--mixtapeEmbed graf-after--p"><a href="https://slack.engineering/typescript-at-slack-a81307fa288d" data-href="https://slack.engineering/typescript-at-slack-a81307fa288d" class="markup--anchor markup--mixtapeEmbed-anchor" title="https://slack.engineering/typescript-at-slack-a81307fa288d"><strong class="markup--strong markup--mixtapeEmbed-strong">TypeScript at Slack</strong><br><em class="markup--em markup--mixtapeEmbed-em">Or, How I Learned to Stop Worrying &amp; Trust the Compiler</em>slack.engineering</a><a href="https://slack.engineering/typescript-at-slack-a81307fa288d" class="js-mixtapeImage mixtapeImage u-ignoreBlock" data-media-id="abc123" data-thumbnail-img-id="1*-h1bH8gB3I7gPh5AG1HmsQ.png" style="background-image: url(https://cdn-images-1.medium.com/fit/c/160/160/1*-h1bH8gB3I7gPh5AG1HmsQ.png);"></a></div>
+            
+            it('parses mixtape block with all data', editorTest(function () {
+                const dom = (new JSDOM(html`<div class="graf graf--mixtapeEmbed graf-after--p"><a href="https://slack.engineering/typescript-at-slack-a81307fa288d" data-href="https://slack.engineering/typescript-at-slack-a81307fa288d" class="markup--anchor markup--mixtapeEmbed-anchor" title="https://slack.engineering/typescript-at-slack-a81307fa288d"><strong class="markup--strong markup--mixtapeEmbed-strong">TypeScript at Slack</strong><br><em class="markup--em markup--mixtapeEmbed-em">Or, How I Learned to Stop Worrying &amp; Trust the Compiler</em>slack.engineering</a><a href="https://slack.engineering/typescript-at-slack-a81307fa288d" class="js-mixtapeImage mixtapeImage u-ignoreBlock" data-media-id="abc123" data-thumbnail-img-id="1*-h1bH8gB3I7gPh5AG1HmsQ.png" style="background-image: url(https://cdn-images-1.medium.com/fit/c/160/160/1*-h1bH8gB3I7gPh5AG1HmsQ.png);"></a></div>`)).window.document;
+                const nodes = $generateNodesFromDOM(editor, dom);
+
+                nodes.length.should.equal(1);
+                nodes[0].getUrl().should.equal('https://slack.engineering/typescript-at-slack-a81307fa288d');
+                nodes[0].getTitle().should.equal('TypeScript at Slack');
+                nodes[0].getDescription().should.equal('Or, How I Learned to Stop Worrying &amp; Trust the Compiler');
+                nodes[0].getPublisher().should.equal('slack.engineering');
+                nodes[0].getThumbnail().should.equal('https://cdn-images-1.medium.com/fit/c/160/160/1*-h1bH8gB3I7gPh5AG1HmsQ.png');
+            }));
+
+            it('parses mixtape with missing title', editorTest(function () {
+                const dom = (new JSDOM(html`<div class="graf graf--mixtapeEmbed graf-after--mixtapeEmbed"><a href="https://slack.engineering/typescript-at-slack-a81307fa288d" data-href="https://slack.engineering/typescript-at-slack-a81307fa288d" class="markup--anchor markup--mixtapeEmbed-anchor" title="https://slack.engineering/typescript-at-slack-a81307fa288d"><br><em class="markup--em markup--mixtapeEmbed-em">Or, How I Learned to Stop Worrying &amp; Trust the Compiler</em>slack.engineering</a><a href="https://slack.engineering/typescript-at-slack-a81307fa288d" class="js-mixtapeImage mixtapeImage u-ignoreBlock" data-media-id="abc123" data-thumbnail-img-id="1*-h1bH8gB3I7gPh5AG1HmsQ.png" style="background-image: url(https://cdn-images-1.medium.com/fit/c/160/160/1*-h1bH8gB3I7gPh5AG1HmsQ.png);"></a></div>`)).window.document;
+                const nodes = $generateNodesFromDOM(editor, dom);
+
+                nodes.length.should.equal(1);
+                nodes[0].getUrl().should.equal('https://slack.engineering/typescript-at-slack-a81307fa288d');
+                nodes[0].getTitle().should.equal('');
+                nodes[0].getDescription().should.equal('Or, How I Learned to Stop Worrying &amp; Trust the Compiler');
+                nodes[0].getPublisher().should.equal('slack.engineering');
+                nodes[0].getThumbnail().should.equal('https://cdn-images-1.medium.com/fit/c/160/160/1*-h1bH8gB3I7gPh5AG1HmsQ.png');
+            }));
+        });
     });
 });
