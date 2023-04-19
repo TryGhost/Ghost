c559c70dd532f369a2e80104b6c2504eaa157618
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2023-04-19 14:56:40 +0100

    Add the default background color class

--- ghost/announcement-bar/src/components/AnnouncementBar.js
+++ ghost/announcement-bar/src/components/AnnouncementBar.js
@@ -7,7 +7,7 @@ export function AnnouncementBar({settings}) {
     const [data, setData] = React.useState({content: '<p>Content with <a href="https://ghost.org/">link</a></p>'});
 
     return (
-        <div className="gh-announcement-bar">
+        <div className="gh-announcement-bar dark">
             <div className="gh-announcement-bar-content" dangerouslySetInnerHTML={{__html: data.content}}></div>
         </div>
     );
