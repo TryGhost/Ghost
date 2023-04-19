3a87d17ae48b6901d0d20c2b0ec27d25af2bee65
Chris Raible <chris@ghost.org>
2023-04-19 14:20:52 +0100

    Wired up the author details to the post history modal
    no issue
    
    - added post-revision serializer to allow for embedded records in the post-revision ember model

---
+++ ghost/admin/app/serializers/post-revision.js
@@ -0,0 +1,14 @@
+/* eslint-disable camelcase */
+import ApplicationSerializer from 'ghost-admin/serializers/application';
+import {EmbeddedRecordsMixin} from '@ember-data/serializer/rest';
+
+export default class PostRevisionSerializer extends ApplicationSerializer.extend(EmbeddedRecordsMixin) {
+    // settings for the EmbeddedRecordsMixin.
+    attrs = {
+        author: {embedded: 'always'},
+        lexical: {key: 'lexical'},
+        title: {key: 'title'},
+        createdAt: {key: 'created_at'},
+        postIdLocal: {key: 'post_id'}
+    };
+}
