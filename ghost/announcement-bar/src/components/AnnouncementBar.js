248d5833e4403c9d140f4b454d2fb037da6814b9
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2023-04-19 14:44:19 +0100

    Added basic styles

--- ghost/announcement-bar/src/components/AnnouncementBar.js
+++ ghost/announcement-bar/src/components/AnnouncementBar.js
@@ -6,10 +6,9 @@ export function AnnouncementBar({settings}) {
     // eslint-disable-next-line no-unused-vars
     const [data, setData] = React.useState({content: '<p>Content with <a href="https://ghost.org/">link</a></p>'});
 
-    const accentColor = settings?.accent_color;
     return (
-        <div className="gh-announcement-bar" style={{backgroundColor: accentColor}}>
-            <div dangerouslySetInnerHTML={{__html: data.content}}></div>
+        <div className="gh-announcement-bar">
+            <div className="gh-announcement-bar-content" dangerouslySetInnerHTML={{__html: data.content}}></div>
         </div>
     );
 }
