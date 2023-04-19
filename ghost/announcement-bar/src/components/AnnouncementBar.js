74389ce82ca7ac887cbb2495d59ddd670c5ad514
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2023-04-19 15:29:59 +0100

    Add closing logic

--- ghost/announcement-bar/src/components/AnnouncementBar.js
+++ ghost/announcement-bar/src/components/AnnouncementBar.js
@@ -4,13 +4,18 @@ import './AnnouncementBar.css';
 import {ReactComponent as CloseIcon} from '../icons/clear.svg';
 
 export function AnnouncementBar({settings}) {
+    const [visible, setVisible] = React.useState(true);
     // eslint-disable-next-line no-unused-vars
     const [data, setData] = React.useState({content: '<p>Content with <a href="https://ghost.org/">link</a></p>'});
+
+    if (!visible) {
+        return null;
+    }
 
     return (
         <div className="gh-announcement-bar dark">
             <div className="gh-announcement-bar-content" dangerouslySetInnerHTML={{__html: data.content}}></div>
-            <button>
+            <button onClick={() => setVisible(false)}>
                 <CloseIcon />
             </button>
         </div>
