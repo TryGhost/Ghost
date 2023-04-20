066b9e557e0c2d3b3addf9837a657e42ef309ec8
Elena Baidakova <lena.baydakova@gmail.com>
2023-04-20 16:39:44 +0400

    Added announcement close button state storage to sessionStorage (#16683)
    refs TryGhost/Team#3071

--- ghost/announcement-bar/src/components/AnnouncementBar.js
+++ ghost/announcement-bar/src/components/AnnouncementBar.js
@@ -4,7 +4,12 @@ import './AnnouncementBar.css';
 import {ReactComponent as CloseIcon} from '../icons/clear.svg';
 
 export function AnnouncementBar({settings}) {
-    const [visible, setVisible] = React.useState(true);
+    const [visible, setVisible] = React.useState(isBarVisible());
+
+    const handleButtonClick = () => {
+        setBarVisibility();
+        setVisible(false);
+    };
 
     if (!visible) {
         return null;
@@ -18,9 +23,18 @@ export function AnnouncementBar({settings}) {
     return (
         <div className={className}>
             <div className="gh-announcement-bar-content" dangerouslySetInnerHTML={{__html: settings?.announcement}}></div>
-            <button onClick={() => setVisible(false)}>
+            <button onClick={handleButtonClick}>
                 <CloseIcon />
             </button>
         </div>
     );
+}
+
+const BAR_STORAGE_KEY = 'isAnnouncementBarVisible';
+function setBarVisibility(state = 'true') {
+    sessionStorage.setItem(BAR_STORAGE_KEY, state);
+}
+
+function isBarVisible() {
+    return !sessionStorage.getItem(BAR_STORAGE_KEY);
 }
