7124b9caa082a886807de1118b04cce87331ab75
Elena Baidakova <lena.baydakova@gmail.com>
2023-04-20 10:08:13 +0400

    Added paragraph after card inserted from snippet (TryGhost/Koenig#631)
    refs TryGhost/Team#3055

--- koenig/koenig-lexical/src/plugins/KoenigSnippetPlugin.jsx
+++ koenig/koenig-lexical/src/plugins/KoenigSnippetPlugin.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import {$createParagraphNode, $getSelection, COMMAND_PRIORITY_LOW, createCommand} from 'lexical';
 import {$generateNodesFromSerializedNodes, $insertGeneratedNodes} from '@lexical/clipboard';
-import {$getSelection, COMMAND_PRIORITY_LOW, createCommand} from 'lexical';
+import {$isKoenigCard} from '@tryghost/kg-default-nodes';
+import {INSERT_CARD_COMMAND} from './KoenigBehaviourPlugin';
 import {mergeRegister} from '@lexical/utils';
 import {useLexicalComposerContext} from '@lexical/react/LexicalComposerContext';
 
@@ -17,8 +19,22 @@ export const KoenigSnippetPlugin = () => {
                     editor.update(() => {
                         const snippetData = JSON.parse(dataset.value);
                         const nodes = $generateNodesFromSerializedNodes(snippetData.nodes);
+                        const firstNode = nodes.length === 1 && nodes[0];
+                        const lastNode = !!nodes.length && nodes[nodes.length - 1];
+
+                        if (firstNode && $isKoenigCard(firstNode)) {
+                            editor.dispatchCommand(INSERT_CARD_COMMAND, {cardNode: firstNode});
+
+                            return true;
+                        }
+
                         const selection = $getSelection();
                         $insertGeneratedNodes(editor, nodes, selection);
+
+                        if (lastNode && $isKoenigCard(lastNode) && !lastNode.getNextSibling()) {
+                            const paragraph = $createParagraphNode();
+                            lastNode.getTopLevelElementOrThrow().insertAfter(paragraph);
+                        }
                     });
                     return true;
                 },
