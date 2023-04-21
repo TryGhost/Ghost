0345b9de5d82250081dc25ab876681c9519a6645
Ronald <hi@ronaldlangeveld.com>
2023-04-21 11:42:04 +0100

    Fixed linting

--- ghost/core/core/server/data/migrations/versions/5.45/2023-04-21-08-54-add-post-revision-status.js
+++ ghost/core/core/server/data/migrations/versions/5.45/2023-04-21-08-54-add-post-revision-status.js
@@ -1,20 +1,6 @@
 // For information on writing migrations, see https://www.notion.so/ghost/Database-migrations-eb5b78c435d741d2b34a582d57c24253
 
-const {createAddColumnMigration} = require('../../utils');
 const {combineNonTransactionalMigrations,createAddColumnMigration} = require('../../utils');
-
-// status: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'draft', validations: {isIn: [['published', 'draft', 'scheduled', 'sent']]}},
-
-// module.exports = createAddColumnMigration('post_revisions', 'post_status', {
-//     type: 'string',
-//     maxlength: 50,
-//     nullable: false,
-//     defaultTo: 'draft',
-//     validations: {
-//         isIn: [['published', 'draft', 'scheduled', 'sent']]
-//     }
-// });
-
 
 module.exports = combineNonTransactionalMigrations(
     createAddColumnMigration('post_revisions', 'post_status', {
@@ -29,6 +15,6 @@ module.exports = combineNonTransactionalMigrations(
     createAddColumnMigration('post_revisions', 'reason', {
         type: 'string',
         maxlength: 50,
-        nullable: true,
+        nullable: true
     })
 );
