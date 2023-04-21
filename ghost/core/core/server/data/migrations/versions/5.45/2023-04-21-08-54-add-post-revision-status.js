7d2d744bc6d5a4dd57407eb6e522fb139d73fad2
Ronald <hi@ronaldlangeveld.com>
2023-04-21 11:38:29 +0100

    Updated migration

--- ghost/core/core/server/data/migrations/versions/5.45/2023-04-21-08-54-add-post-revision-status.js
+++ ghost/core/core/server/data/migrations/versions/5.45/2023-04-21-08-54-add-post-revision-status.js
@@ -1,15 +1,34 @@
 // For information on writing migrations, see https://www.notion.so/ghost/Database-migrations-eb5b78c435d741d2b34a582d57c24253
 
 const {createAddColumnMigration} = require('../../utils');
+const {combineNonTransactionalMigrations,createAddColumnMigration} = require('../../utils');
 
 // status: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'draft', validations: {isIn: [['published', 'draft', 'scheduled', 'sent']]}},
 
-module.exports = createAddColumnMigration('post_revisions', 'post_status', {
-    type: 'string',
-    maxlength: 50,
-    nullable: false,
-    defaultTo: 'draft',
-    validations: {
-        isIn: [['published', 'draft', 'scheduled', 'sent']]
-    }
-});
+// module.exports = createAddColumnMigration('post_revisions', 'post_status', {
+//     type: 'string',
+//     maxlength: 50,
+//     nullable: false,
+//     defaultTo: 'draft',
+//     validations: {
+//         isIn: [['published', 'draft', 'scheduled', 'sent']]
+//     }
+// });
+
+
+module.exports = combineNonTransactionalMigrations(
+    createAddColumnMigration('post_revisions', 'post_status', {
+        type: 'string',
+        maxlength: 50,
+        nullable: false,
+        defaultTo: 'draft',
+        validations: {
+            isIn: [['published', 'draft', 'scheduled', 'sent']]
+        }
+    }),
+    createAddColumnMigration('post_revisions', 'reason', {
+        type: 'string',
+        maxlength: 50,
+        nullable: true,
+    })
+);
--- ghost/core/core/server/data/schema/schema.js
+++ ghost/core/core/server/data/schema/schema.js
@@ -405,7 +405,8 @@ module.exports = {
         created_at: {type: 'dateTime', nullable: false},
         author_id: {type: 'string', maxlength: 24, nullable: true, references: 'users.id', cascadeDelete: false, constraintName: 'post_revs_author_id_foreign'},
         title: {type: 'string', maxlength: 2000, nullable: true, validations: {isLength: {max: 255}}},
-        post_status: {type: 'string', maxlength: 50, nullable: true, validations: {isIn: [['draft', 'published', 'scheduled']]}}
+        post_status: {type: 'string', maxlength: 50, nullable: true, validations: {isIn: [['draft', 'published', 'scheduled']]}},
+        reason: {type: 'string', maxlength: 50, nullable: true}
     },
     members: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
