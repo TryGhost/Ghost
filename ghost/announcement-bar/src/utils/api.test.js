b762d7f3e3cf20376d236eae7f68ee37ef608bfc
Naz <hi@nazavo.com>
2023-04-27 12:55:16 +0200

    Migrated announcement bar script to use Members API
    https://github.com/TryGhost/Team/issues/3121
    
    - The announcement bar data is now exposed through Members API `/members/api/announcement` instead of Content API.

--- ghost/announcement-bar/src/utils/api.test.js
+++ ghost/announcement-bar/src/utils/api.test.js
@@ -4,16 +4,21 @@ test('should call settings endpoint on init', () => {
     jest.spyOn(window, 'fetch');
     window.fetch.mockResolvedValueOnce({
         ok: true,
-        json: async () => ({success: true})
+        json: async () => ({
+            announcement: [{
+                announcement_content: '<p>Test announcement</p>',
+                announcement_background: 'dark'
+            }]
+        })
     });
 
-    const api = setupGhostApi({apiKey: 'key', apiUrl: 'http://localhost'});
+    const api = setupGhostApi({apiKey: 'key', apiUrl: 'http://localhost/members/api/announcement/'});
 
     api.init();
 
     expect(window.fetch).toHaveBeenCalledTimes(1);
     expect(window.fetch).toHaveBeenCalledWith(
-        'http://localhost/settings/?key=key&keys=announcement,announcement_background',
+        'http://localhost/members/api/announcement/',
         expect.objectContaining({
             method: 'GET',
             headers: {'Content-Type': 'application/json'},
