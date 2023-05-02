b3caf16005289cc9909488391b4a26f3f4a66a90
Fabien "egg" O'Carroll <fabien@allou.is>
2023-05-02 07:59:17 -0400

    🔒 Fixed filtering on private Author fields in Content API
    refs https://github.com/TryGhost/Ghost/security/advisories/GHSA-r97q-ghch-82j9
    
    Because our filtering layer is so coupled to the DB and we don't generally
    apply restrictions, it was possible to fetch authors and filter by their
    password or email field. Coupled with the "starts with" operator this can be
    used to brute force the first character of these fields by trying random
    combinations until an author is included in the filter. After which the next
    character can be brute forced, and so on until the data has been leaked
    completely.

--- ghost/core/core/server/api/endpoints/authors-public.js
+++ ghost/core/core/server/api/endpoints/authors-public.js
@@ -1,12 +1,24 @@
 const Promise = require('bluebird');
 const tpl = require('@tryghost/tpl');
 const errors = require('@tryghost/errors');
+const {mapQuery} = require('@tryghost/mongo-utils');
 const models = require('../../models');
 const ALLOWED_INCLUDES = ['count.posts'];
 
 const messages = {
     notFound: 'Author not found.'
 };
+
+const rejectPrivateFieldsTransformer = input => mapQuery(input, function (value, key) {
+    const lowerCaseKey = key.toLowerCase();
+    if (lowerCaseKey.startsWith('password') || lowerCaseKey.startsWith('email')) {
+        return;
+    }
+
+    return {
+        [key]: value
+    };
+});
 
 module.exports = {
     docName: 'authors',
@@ -29,7 +41,11 @@ module.exports = {
         },
         permissions: true,
         query(frame) {
-            return models.Author.findPage(frame.options);
+            const options = {
+                ...frame.options,
+                mongoTransformer: rejectPrivateFieldsTransformer
+            };
+            return models.Author.findPage(options);
         }
     },
 
@@ -54,7 +70,11 @@ module.exports = {
         },
         permissions: true,
         query(frame) {
-            return models.Author.findOne(frame.data, frame.options)
+            const options = {
+                ...frame.options,
+                mongoTransformer: rejectPrivateFieldsTransformer
+            };
+            return models.Author.findOne(frame.data, options)
                 .then((model) => {
                     if (!model) {
                         return Promise.reject(new errors.NotFoundError({
