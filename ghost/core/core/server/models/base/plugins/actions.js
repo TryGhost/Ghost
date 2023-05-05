fbed93b866c7710bbc25eca8bd999ce12dfed03f
Simon Backx <simon@ghost.org>
2023-05-05 09:45:36 +0200

    🐛 Added missing history logs for post/page bulk actions (#16734)
    no issue
    
    The post/page bulk actions weren't logged in the history log / actions
    table.
    
    This change adds support for logging bulk actions.
    - New `addActions` static method on models. It creates an action log in
    the database for multiple models at once. If only one model was edited,
    deleted or added, it will fallback to `addAction`
    - `addAction` can also be called statically now
    - `actionName` option is now supported when using `addActions`,
    `addAction`, and as a result also in all bulk manipulation methods, and
    CRUD methods. This allows you to replace the default '5 posts edited'
    into something more specific like '5 posts featured'
    - Fixed support for null resource_id in the parse-history-event helper
    - Removed the default 'published' status requirement when using
    Post.findOne for internal queries.

--- ghost/core/core/server/models/base/plugins/actions.js
+++ ghost/core/core/server/models/base/plugins/actions.js
@@ -6,6 +6,54 @@ const logging = require('@tryghost/logging');
  * @param {import('bookshelf')} Bookshelf
  */
 module.exports = function (Bookshelf) {
+    const insertAction = (data, options) => {
+        // CASE: model does not support action for target event
+        if (!data) {
+            return;
+        }
+
+        const insert = (action) => {
+            Bookshelf.model('Action')
+                .add(action, {autoRefresh: false})
+                .catch((err) => {
+                    if (_.isArray(err)) {
+                        err = err[0];
+                    }
+
+                    logging.error(new errors.InternalServerError({
+                        err
+                    }));
+                });
+        };
+
+        if (options.transacting) {
+            options.transacting.once('committed', (committed) => {
+                if (!committed) {
+                    return;
+                }
+
+                insert(data);
+            });
+        } else {
+            insert(data);
+        }
+    };
+
+    // We need this addAction accessible from the static model and instances
+    const addAction = (model, event, options) => {
+        if (!model.wasChanged()) {
+            return;
+        }
+
+        // CASE: model does not support actions at all
+        if (!model.getAction) {
+            return;
+        }
+
+        const data = model.getAction(event, options);
+        insertAction(data, options);
+    };
+
     Bookshelf.Model = Bookshelf.Model.extend({
         /**
          * Constructs data to be stored in the database with info
@@ -33,7 +81,9 @@ module.exports = function (Bookshelf) {
                 return;
             }
 
-            let context = {};
+            let context = {
+                action_name: options.actionName
+            };
 
             if (this.actionsExtraContext && Array.isArray(this.actionsExtraContext)) {
                 for (const c of this.actionsExtraContext) {
@@ -74,48 +124,73 @@ module.exports = function (Bookshelf) {
          *
          * We could embed adding actions more nicely in the future e.g. plugin.
          */
-        addAction: (model, event, options) => {
-            if (!model.wasChanged()) {
+        addAction
+    }, {
+        addAction,
+        async addActions(event, ids, options) {
+            if (ids.length === 1) {
+                // We want to store an event for a single model in the actions table
+                // This is so we can include the name
+                const model = await this.findOne({[options.column ?? 'id']: ids[0]}, {require: true, transacting: options.transacting, context: {internal: true}});
+                this.addAction(model, event, options);
                 return;
             }
 
-            // CASE: model does not support actions at all
-            if (!model.getAction) {
+            const existingAction = this.getBulkAction(event, ids.length, options);
+            insertAction(existingAction, options);
+        },
+
+        /**
+         * Constructs data to be stored in the database with info
+         * on particular actions
+         */
+        getBulkAction(event, count, options) {
+            const actor = this.prototype.getActor(options);
+
+            // @NOTE: we ignore internal updates (`options.context.internal`) for now
+            if (!actor) {
                 return;
             }
 
-            const existingAction = model.getAction(event, options);
-
-            // CASE: model does not support action for target event
-            if (!existingAction) {
+            if (!this.prototype.actionsCollectCRUD) {
                 return;
             }
 
-            const insert = (action) => {
-                Bookshelf.model('Action')
-                    .add(action, {autoRefresh: false})
-                    .catch((err) => {
-                        if (_.isArray(err)) {
-                            err = err[0];
-                        }
+            let resourceType = this.prototype.actionsResourceType;
 
-                        logging.error(new errors.InternalServerError({
-                            err
-                        }));
-                    });
+            if (typeof resourceType === 'function') {
+                resourceType = resourceType.bind(this)();
+            }
+
+            if (!resourceType) {
+                return;
+            }
+
+            let context = {
+                count,
+                action_name: options.actionName
             };
 
-            if (options.transacting) {
-                options.transacting.once('committed', (committed) => {
-                    if (!committed) {
-                        return;
-                    }
-
-                    insert(existingAction);
-                });
-            } else {
-                insert(existingAction);
+            if (this.getBulkActionExtraContext && typeof this.getBulkActionExtraContext === 'function') {
+                context = {
+                    ...context,
+                    ...this.getBulkActionExtraContext.bind(this)(options)
+                };
             }
+
+            const data = {
+                event,
+                resource_id: null,
+                resource_type: resourceType,
+                actor_id: actor.id,
+                actor_type: actor.type
+            };
+
+            if (context && Object.keys(context).length) {
+                data.context = context;
+            }
+
+            return data;
         }
     });
 };
