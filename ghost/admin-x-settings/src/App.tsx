d57b6b22b4623d00c4ae1434baa3a7b17d17cb7b
Rishabh <zrishabhgarg@gmail.com>
2023-05-09 17:04:53 +0530

    Added basic admin x setup

---
+++ ghost/admin-x-settings/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+    );
+}
+
+export default App;
---
+++ ghost/admin-x-settings/src/index.tsx
@@ -0,0 +1,6 @@
+import './index.css';
+import App from './App.tsx';
+
+export {
+    App as AdminXApp
+};
