6e55b2eb88bd6dc0f3514844d7c0ddf5c7857e8d
Ronald <hi@ronaldlangeveld.com>
2023-05-12 14:06:34 +0200

    Changed renderer to template method
    no issue
    
    - modified the renderer to use template style rendering as opposed to
      the javascript method building.
    - this should make it easier to add more complex logic to the html as we
      develop the frontend rendering and easier to test.

--- koenig/kg-default-nodes/lib/nodes/signup/SignupRenderer.js
+++ koenig/kg-default-nodes/lib/nodes/signup/SignupRenderer.js
@@ -1,57 +1,32 @@
-import {addCreateDocumentOption} from '../../utils/add-create-document-option';
-
 // TODO this is a placeholder, we need to figure out what the signup card should look like and
 // which elements should be customizable inside the editor
 // ref https://ghost.org/docs/themes/members#signup-forms
+
+function cardTemplate(nodeData) {
+    return `
+        <form data-members-form="" style="background-image: url(${nodeData.backgroundImageSrc});">
+            <h1>${nodeData.header}</h1>
+            <h2>${nodeData.subheader}</h2>
+            <p>${nodeData.disclaimer}</p>
+            <label for="email">Email</label>
+            <input id="email" data-members-email="" type="email" required="true">
+            <button type="submit">${nodeData.buttonText}</button>
+        </form>
+    `;
+}
+
 export function renderSignupCardToDOM(dataset, options = {}) {
-    const nodeData = {
-        style: dataset.__style,
+    const node = {
         buttonText: dataset.__buttonText,
         header: dataset.__header,
         subheader: dataset.__subheader,
         disclaimer: dataset.__disclaimer,
-        backgroundImageSrc: dataset.__backgroundImageSrc
+        backgroundImageSrc: dataset.__backgroundImageSrc,
+        backgroundColor: dataset.__backgroundColor,
+        buttonColor: dataset.__buttonColor,
+        labels: dataset.__labels
     };
 
-    addCreateDocumentOption(options);
-
-    const document = options.createDocument();
-    const form = document.createElement('form');
-
-    form.setAttribute('data-members-form', '');
-    if (nodeData.backgroundImageSrc) {
-        form.style.backgroundImage = `url('${nodeData.backgroundImageSrc}')`;
-    }
-  
-    const header = document.createElement('h1');
-    header.textContent = nodeData.header;
-  
-    const subheader = document.createElement('h2');
-    subheader.textContent = nodeData.subheader;
-  
-    const disclaimer = document.createElement('p');
-    disclaimer.textContent = nodeData.disclaimer;
-  
-    const emailLabel = document.createElement('label');
-    emailLabel.setAttribute('for', 'email');
-    emailLabel.textContent = 'Email';
-  
-    const emailInput = document.createElement('input');
-    emailInput.setAttribute('id', 'email');
-    emailInput.setAttribute('data-members-email', '');
-    emailInput.setAttribute('type', 'email');
-    emailInput.setAttribute('required', true);
-  
-    const submitButton = document.createElement('button');
-    submitButton.setAttribute('type', 'submit');
-    submitButton.textContent = nodeData.buttonText;
-  
-    form.appendChild(header);
-    form.appendChild(subheader);
-    form.appendChild(disclaimer);
-    form.appendChild(emailLabel);
-    form.appendChild(emailInput);
-    form.appendChild(submitButton);
-  
-    return form.outerHTML;
+    const htmlString = options.target === 'email' ? '' : cardTemplate(node);
+    return htmlString;
 }
