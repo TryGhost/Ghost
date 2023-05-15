8448dac06e5bf7f57e7cd578b734aad94f0590b6
Sag <guptazy@gmail.com>
2023-05-15 07:41:10 +0200

    Added not-allowed cursor for the subscribe input field

--- koenig/koenig-lexical/src/components/ui/SubscribeForm.jsx
+++ koenig/koenig-lexical/src/components/ui/SubscribeForm.jsx
@@ -18,7 +18,7 @@ export function SubscribeForm({dataTestId, placeholder, value, inputBorderStyle,
         <div className='relative flex'>
             <input
                 ref={setRef}
-                className={`relative w-full py-2 px-4 font-sans font-normal text-grey-900 focus-visible:outline-none dark:border-grey-900 dark:bg-grey-900 dark:text-white dark:placeholder:text-grey-800 ${(buttonSize === 'small' ? 'h-10 text-md leading-[4rem]' : (buttonSize === 'medium' ? 'h-11 text-[1.6rem] leading-[4.4rem]' : 'h-12 text-lg leading-[4.8rem]'))}`}
+                className={`relative w-full py-2 px-4 font-sans font-normal text-grey-900 hover:cursor-not-allowed focus-visible:outline-none dark:border-grey-900 dark:bg-grey-900 dark:text-white dark:placeholder:text-grey-800 ${(buttonSize === 'small' ? 'h-10 text-md leading-[4rem]' : (buttonSize === 'medium' ? 'h-11 text-[1.6rem] leading-[4.4rem]' : 'h-12 text-lg leading-[4.8rem]'))}`}
                 placeholder={placeholder}
                 style={inputBorderStyle}
                 value={value}
