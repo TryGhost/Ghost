dc3df28ff03472a6734c33f6b71f3338c920955d
Peter Zimon <peter.zimon@gmail.com>
2023-05-16 17:44:11 +0200

    Added blockheading component to Admin X Settings
    refs. https://github.com/TryGhost/Team/issues/3150

---
+++ ghost/admin-x-settings/src/components/design-system/globals/BlockHeading.stories.tsx
@@ -0,0 +1,32 @@
+import type {Meta, StoryObj} from '@storybook/react';
+
+import BlockHeading from './BlockHeading';
+
+const meta = {
+    title: 'Global / Block heading',
+    component: BlockHeading,
+    tags: ['autodocs']
+} satisfies Meta<typeof BlockHeading>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    args: {
+        title: 'Block'
+    }
+};
+
+export const Grey: Story = {
+    args: {
+        title: 'Block',
+        color: 'grey'
+    }
+};
+
+export const Separator: Story = {
+    args: {
+        title: 'Block',
+        separator: true
+    }
+};
