f3f3d58acf32c3d2733fb71112f8da20eee2ca08
Naz <hi@nazavo.com>
2023-05-17 14:42:37 +0700

    Added PUT /collections/id to Admin API
    refs https://github.com/TryGhost/Team/issues/3167
    
    - This is part of scaffolding for collections API. Allows to edit collection resource

--- ghost/core/test/e2e-api/admin/collections.test.js
+++ ghost/core/test/e2e-api/admin/collections.test.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 const {
     agentProvider,
     fixtureManager,
@@ -61,5 +62,46 @@ describe('Collections API', function () {
             .matchBodySnapshot({
                 collections: [matchCollection]
             });
+    });
+
+    it('Can edit a Collection', async function () {
+        const collection = {
+            title: 'Test Collection to Edit'
+        };
+
+        const addResponse = await agent
+            .post('/collections/')
+            .body({
+                collections: [collection]
+            })
+            .expectStatus(201)
+            .matchHeaderSnapshot({
+                'content-version': anyContentVersion,
+                etag: anyEtag,
+                location: anyLocationFor('collections')
+            })
+            .matchBodySnapshot({
+                collections: [matchCollection]
+            });
+
+        const collectionId = addResponse.body.collections[0].id;
+
+        const editResponse = await agent
+            .put(`/collections/${collectionId}/`)
+            .body({
+                collections: [{
+                    title: 'Test Collection Edited'
+                }]
+            })
+            .expectStatus(200)
+            .matchHeaderSnapshot({
+                'content-version': anyContentVersion,
+                etag: anyEtag
+            })
+            .matchBodySnapshot({
+                collections: [matchCollection]
+            });
+
+        assert.equal(editResponse.body.collections[0].title, 'Test Collection Edited');
     });
 });
