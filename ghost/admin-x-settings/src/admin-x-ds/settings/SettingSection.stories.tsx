14578707afae1416a9d16e047838fed2923823a3
Peter Zimon <peter.zimon@gmail.com>
2023-05-17 13:54:07 +0200

    Fixed Admin design system bug
    refs. https://github.com/TryGhost/Team/issues/3150

--- ghost/admin-x-settings/src/admin-x-ds/settings/SettingSection.stories.tsx
+++ ghost/admin-x-settings/src/admin-x-ds/settings/SettingSection.stories.tsx
@@ -19,7 +19,7 @@ export const Default: Story = {
         title: 'Section header',
         children: 
         <>
-            <SettingGroup {...SettingGroupStories.Default.args} />
+            <SettingGroup {...SettingGroupStories.SingleColumn.args} />
             <SettingGroup {...SettingGroupStories.Editing.args} />
             <SettingGroup {...SettingGroupStories.Unsaved.args} />
         </>
