ca6aeb8d23c11d2a4f0679d5754ff7eb2ac48b19
Peter Zimon <peter.zimon@gmail.com>
2023-05-17 12:02:47 +0200

    Added setting group value component to AdminX DS
    refs. https://github.com/TryGhost/Team/issues/3150

---
+++ ghost/admin-x-settings/src/admin-x-ds/settings/SettingGroupValues.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import SettingValue from './SettingValue';
+import {ISettingValue} from './SettingValue';
+
+interface ISettingGroupValues {
+    columns?: 1 | 2;
+    values: Array<ISettingValue>
+}
+
+const SettingGroupValues: React.FC<ISettingGroupValues> = ({columns, values}) => {
+    let styles = 'flex flex-col gap-6';
+    if (columns === 2) {
+        styles = 'grid grid-cols-2 gap-6';
+    }
+
+    return (
+        <div className={styles}>
+            {values && values.map(value => (
+                <SettingValue key={value.key} heading={value.heading} value={value.value} />
+            ))}
+        </div>
+    );
+};
+
+export default SettingGroupValues;
