554deb3315fe9698164415a2d7a69719fb6780f1
Naz <hi@nazavo.com>
2023-05-17 13:42:35 +0700

    Extended save method to return saved collection data
    refs https://github.com/TryGhost/Team/issues/3167
    
    - The core client (API) needs a way to pass in information without an ID when creating a new entity, handling it on service/repository layer makes the most sense.
    - Used "require" syntax to import tpl/errors modules, otherwise TS compiler was complaining about type compatibility issues, this works as a temporary workaround and is tracked and an issue to improve in the future.

--- ghost/collections/src/CollectionsRepositoryInMemory.ts
+++ ghost/collections/src/CollectionsRepositoryInMemory.ts
@@ -1,9 +1,44 @@
+// have to use requires until there are type definitions for these modules
+const {ValidationError} = require('@tryghost/errors');
+const tpl = require('@tryghost/tpl');
+
+import ObjectID from 'bson-objectid';
 import {InMemoryRepository} from '@tryghost/in-memory-repository';
 import {Collection} from './Collection';
+
+const messages = {
+    invalidIDProvided: 'Invalid ID provided for Collection'
+};
 
 export class CollectionsRepositoryInMemory extends InMemoryRepository<string, Collection> {
     constructor() {
         super();
+    }
+
+    async create(data: any): Promise<Collection> {
+        let id;
+
+        if (!data.id) {
+            id = new ObjectID();
+        } else if (typeof data.id === 'string') {
+            id = ObjectID.createFromHexString(data.id);
+        } else if (data.id instanceof ObjectID) {
+            id = data.id;
+        } else {
+            throw new ValidationError({
+                message: tpl(messages.invalidIDProvided)
+            });
+        }
+
+        return {
+            id: id.toHexString(),
+            title: data.title,
+            description: data.description,
+            type: data.type,
+            filter: data.filter,
+            feature_image: data.feature_image,
+            deleted: data.deleted || false
+        };
     }
 
     protected toPrimitive(entity: Collection): object {
