cb3decdb2a47f12090e3bcdcb02b8daa636ca39c
Naz <hi@nazavo.com>
2023-05-17 14:42:37 +0700

    Added PUT /collections/id to Admin API
    refs https://github.com/TryGhost/Team/issues/3167
    
    - This is part of scaffolding for collections API. Allows to edit collection resource

--- ghost/core/core/server/api/endpoints/collections.js
+++ ghost/core/core/server/api/endpoints/collections.js
@@ -1,4 +1,10 @@
+const errors = require('@tryghost/errors');
+const tpl = require('@tryghost/tpl');
 const collectionsService = require('../../services/collections');
+
+const messages = {
+    collectionNotFound: 'Collection not found.'
+};
 
 module.exports = {
     docName: 'collections',
@@ -25,6 +31,42 @@ module.exports = {
         permissions: false,
         async query(frame) {
             return await collectionsService.api.add(frame.data.collections[0], frame.options);
+        }
+    },
+
+    edit: {
+        headers: {},
+        options: [
+            'id'
+        ],
+        validation: {
+            options: {
+                id: {
+                    required: true
+                }
+            }
+        },
+        // @NOTE: should have permissions when moving out of Alpha
+        permissions: false,
+        async query(frame) {
+            const model = await collectionsService.api.edit(frame.data.collections[0], frame.options);
+
+            if (!model) {
+                return Promise.reject(new errors.NotFoundError({
+                    message: tpl(messages.tagNotFound)
+                }));
+            }
+
+            // @NOTE: cache invalidation has to be done manually for now
+            //        because the model's wasChanged is not returned from
+            //        the service using in-memory repository layer
+            // if (model.wasChanged()) {
+            this.headers.cacheInvalidate = true;
+            // } else {
+            //     this.headers.cacheInvalidate = false;
+            // }
+
+            return model;
         }
     }
 };
