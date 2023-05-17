764d8c376ee93eada408c436613c009d1bf98f9c
Peter Zimon <peter.zimon@gmail.com>
2023-05-17 11:34:56 +0200

    Added setting value to AdminX design system
    refs. https://github.com/TryGhost/Team/issues/3150

--- ghost/admin-x-settings/src/admin-x-ds/settings/SettingValue.stories.tsx
+++ ghost/admin-x-settings/src/admin-x-ds/settings/SettingValue.stories.tsx
@@ -13,6 +13,22 @@ type Story = StoryObj<typeof SettingValue>;
 
 export const Default: Story = {
     args: {
-        value: 'Hello'
+        heading: 'Setting',
+        value: 'Setting value'
+    }
+};
+
+export const WithHelp: Story = {
+    args: {
+        heading: 'Setting',
+        value: 'Setting value',
+        help: 'Setting help text'
+    }
+};
+
+export const NoHeading: Story = {
+    args: {
+        value: 'Setting value',
+        help: 'Help text'
     }
 };
