beab61e78215adf81a2ac3ccff76946b167b2e0c
Peter Zimon <peter.zimon@gmail.com>
2023-05-17 15:19:16 +0200

    Added static state handling to Admin Settings
    refs. https://github.com/TryGhost/Team/issues/3150

--- ghost/admin-x-settings/src/admin-x-ds/global/TextField.tsx
+++ ghost/admin-x-settings/src/admin-x-ds/global/TextField.tsx
@@ -3,18 +3,25 @@ import React from 'react';
 import Heading from './Heading';
 
 interface ITextField {
-    title?: string,
-    value?: string,
-    placeholder?: string,
-    help?: React.ReactNode
+    title?: string;
+    value?: string;
+    placeholder?: string;
+    help?: React.ReactNode;
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const TextField: React.FC<ITextField> = ({title, value, placeholder, help}) => {
+const TextField: React.FC<ITextField> = ({title, value, placeholder, help, onChange, ...props}) => {
     return (
-        <div className='flex flex-col gap-2'>
+        <div className='flex flex-col'>
             {title && <Heading formLabel={true} grey={true}>{title}</Heading>}
-            <input className='-m-1 border-b border-grey-300 px-1 py-2 focus:border-grey-900' placeholder={placeholder} type='text' value={value} />
-            <span className='text-xs text-grey-700'>{help}</span>
+            <input
+                className='-m-1 border-b border-grey-300 px-1 py-2 focus:border-grey-900' 
+                placeholder={placeholder} 
+                type='text' 
+                value={value} 
+                onChange={onChange}
+                {...props} />
+            <span className='mt-2 inline-block text-xs text-grey-700'>{help}</span>
         </div>
     );
 };
--- ghost/admin-x-settings/src/components/settings/general/TitleAndDescription.tsx
+++ ghost/admin-x-settings/src/components/settings/general/TitleAndDescription.tsx
@@ -1,25 +1,111 @@
 import ButtonGroup from '../../../admin-x-ds/global/ButtonGroup';
-import React from 'react';
+import React, {useState} from 'react';
 import SettingGroup from '../../../admin-x-ds/settings/SettingGroup';
 import SettingGroupHeader from '../../../admin-x-ds/settings/SettingGroupHeader';
+import SettingGroupInputs from '../../../admin-x-ds/settings/SettingGroupInputs';
+import SettingGroupValues from '../../../admin-x-ds/settings/SettingGroupValues';
+import TextField from '../../../admin-x-ds/global/TextField';
 import {ButtonColors} from '../../../admin-x-ds/global/Button';
+import {TSettingGroupStates} from '../../../admin-x-ds/settings/SettingGroup';
 
 const TitleAndDescription: React.FC = () => {
-    const buttons = [
+    const [isEditMode, setIsEditMode] = useState(false);
+    const [isEdited, setIsEdited] = useState(false);
+    const [siteTitle, setSiteTitleValue] = useState(`Dr. Evil's secret volcano lair`);
+    const [siteDescription, setSiteDescriptionValue] = useState(`I'm doing this for fun`);
+
+    const handleEditClick = () => {
+        setIsEditMode(true);
+    };
+
+    const handleCancelClick = () => {
+        setIsEditMode(false);
+    };
+
+    const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setIsEdited(true);
+        setSiteTitleValue(e.target.value);
+    };
+
+    const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setIsEdited(true);
+        setSiteDescriptionValue(e.target.value);
+    };
+
+    const viewButtons = [
         {
             label: 'Edit',
+            key: 'edit',
+            color: ButtonColors.Green,
+            onClick: handleEditClick
+        }
+    ];
+
+    const editButtons = [
+        {
+            label: 'Cancel',
+            key: 'cancel',
+            onClick: handleCancelClick
+        },
+        {
+            label: 'Save',
+            key: 'save',
             color: ButtonColors.Green
         }
     ];
 
+    const viewValues = [
+        {
+            heading: 'Site title',
+            key: 'site-title',
+            value: siteTitle
+        },
+        {
+            heading: 'Site description',
+            key: 'site-description',
+            value: siteDescription
+        }
+    ];
+
+    const inputFields = (
+        <SettingGroupInputs columns={2}>
+            <TextField
+                help="The name of your site"
+                placeholder="Site title"
+                title="Site title"
+                value={siteTitle}
+                onChange={handleTitleChange}
+            />
+            <TextField
+                help="Used in your theme, meta data and search results"
+                placeholder="Enter something"
+                title="Site description"
+                value={siteDescription}
+                onChange={handleDescriptionChange}
+            />
+        </SettingGroupInputs>
+    );
+
+    let settingGroupState: TSettingGroupStates = 'view';
+
+    if (isEditMode) {
+        settingGroupState = 'edit';
+    }
+
+    if (isEdited) {
+        settingGroupState = 'unsaved';
+    }
+
     return (
-        <SettingGroup>
-            <SettingGroupHeader 
-                description="The details used to identify your publication around the web" 
+        <SettingGroup state={settingGroupState}>
+            <SettingGroupHeader
+                description="The details used to identify your publication around the web"
                 title="Title & description"
             >
-                <ButtonGroup buttons={buttons} link={true} />
+                <ButtonGroup buttons={isEditMode ? editButtons : viewButtons} link={true} />
             </SettingGroupHeader>
+            {isEditMode ? inputFields : <SettingGroupValues columns={2} values={viewValues} />
+            }
         </SettingGroup>
     );
 };
