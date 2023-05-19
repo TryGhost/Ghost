0ba8422cb46a9d495084e3d3c7775c91961ca500
Rishabh <zrishabhgarg@gmail.com>
2023-05-19 18:13:47 +0530

    Wired Timezone settings to use real data
    refs https://github.com/TryGhost/Team/issues/3150
    
    - wires Timezone setting to read list of timezones and allow editing/saving new value
    - handles read/write of real timezone setting from settings context
    - uses `@tryghost/timezone-data` to fetch list of all timezones
    - adds typings.d.ts to handle missing types for @tryghost/timezone-data

--- ghost/admin-x-settings/src/components/settings/general/TimeZone.tsx
+++ ghost/admin-x-settings/src/components/settings/general/TimeZone.tsx
@@ -1,14 +1,21 @@
-import ButtonGroup from '../../../admin-x-ds/global/ButtonGroup';
+import Dropdown from '../../../admin-x-ds/global/Dropdown';
 import React, {useContext, useEffect, useState} from 'react';
-import SettingGroup from '../../../admin-x-ds/settings/SettingGroup';
+import SettingGroup, {TSettingGroupStates} from '../../../admin-x-ds/settings/SettingGroup';
 import SettingGroupContent from '../../../admin-x-ds/settings/SettingGroupContent';
-import SettingGroupHeader from '../../../admin-x-ds/settings/SettingGroupHeader';
+import timezoneData from '@tryghost/timezone-data';
 import {SettingsContext} from '../../SettingsProvider';
 import {getLocalTime, getSettingValue} from '../../../utils/helpers';
 
+interface TimezoneDataDropdownOption {
+    name: string;
+    label: string;
+}
+
 const TimeZone: React.FC = () => {
-    const {settings} = useContext(SettingsContext) || {};
-    const publicationTimezone = getSettingValue(settings, 'timezone');
+    const [currentState, setCurrentState] = useState<TSettingGroupStates>('view');
+    const {settings, saveSettings} = useContext(SettingsContext) || {};
+    const savedPublicationTimezone = getSettingValue(settings, 'timezone');
+    const [publicationTimezone, setPublicationTimezone] = useState(savedPublicationTimezone);
 
     const [currentTime, setCurrentTime] = useState(getLocalTime(publicationTimezone));
     useEffect(() => {
@@ -21,12 +28,19 @@ const TimeZone: React.FC = () => {
         };
     }, [publicationTimezone]);
 
-    const buttons = [
-        {
-            label: 'Edit',
-            color: 'green'
-        }
-    ];
+    const handleStateChange = (newState: TSettingGroupStates) => {
+        setCurrentState(newState);
+    };
+
+    const handleSave = () => {
+        saveSettings?.([
+            {
+                key: 'timezone',
+                value: publicationTimezone
+            }
+        ]);
+        setCurrentState('view');
+    };
 
     const viewValues = [
         {
@@ -36,18 +50,37 @@ const TimeZone: React.FC = () => {
         }
     ];
 
-    const customHeader = (
-        <SettingGroupHeader
-            description="Set the time and date of your publication, used for all published posts"
-            title="Site timezone"
-        >
-            <ButtonGroup buttons={buttons} link={true} />
-        </SettingGroupHeader>
+    const timezoneOptions = timezoneData.map((tzOption: TimezoneDataDropdownOption) => {
+        return {
+            value: tzOption.name,
+            label: tzOption.label
+        };
+    });
+
+    const inputFields = (
+        <SettingGroupContent columns={1}>
+            <Dropdown
+                defaultSelectedOption={publicationTimezone}
+                hint={`The local time here is currently ${currentTime}`}
+                options={timezoneOptions}
+                title="Site timezone"
+                onSelect={(value) => {
+                    setCurrentState('unsaved');
+                    setPublicationTimezone(value);
+                }}
+            />
+        </SettingGroupContent>
     );
 
     return (
-        <SettingGroup customHeader={customHeader}>
-            <SettingGroupContent values={viewValues} />
+        <SettingGroup
+            description='Set the time and date of your publication, used for all published posts'
+            state={currentState}
+            title='Site timezone'
+            onSave={handleSave}
+            onStateChange={handleStateChange}
+        >
+            {currentState === 'view' ? <SettingGroupContent values={viewValues} /> : inputFields}
         </SettingGroup>
     );
 };
