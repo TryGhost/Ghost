576d8bdfb923177981fc527ae5933235bfbf91ac
Rishabh <zrishabhgarg@gmail.com>
2023-05-19 19:06:17 +0530

    Updated hint for admin-x publication language UI
    refs https://github.com/TryGhost/Team/issues/3151
    
    - updates hint to take both string and custom react node
    - allows adding custom/more complex hints to text fields

--- ghost/admin-x-settings/src/admin-x-ds/global/Hint.tsx
+++ ghost/admin-x-settings/src/admin-x-ds/global/Hint.tsx
@@ -6,9 +6,23 @@ interface HintProps {
 }
 
 const Hint: React.FC<HintProps> = ({children, color, ...props}) => {
-    return (
-        <span className={`mt-2 inline-block text-xs ${color ? `text-${color}` : `text-grey-700`}`} {...props}>{children}</span>
-    );
+    if (!children) {
+        return null;
+    }
+
+    if (typeof children === 'string') {
+        return (
+            <span className={`mt-2 inline-block text-xs ${color ? `text-${color}` : `text-grey-700`}`} {...props}>{children}</span>
+        );
+    }
+
+    if (React.isValidElement(children)) {
+        return (
+            <>{children}</>
+        );
+    }
+
+    return null;
 };
 
 export default Hint;
