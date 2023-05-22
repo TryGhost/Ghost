ad9d8f1d3b9423c74564769c48ad08402faf9952
Peter Zimon <peter.zimon@gmail.com>
2023-05-22 17:30:26 +0200

    Fixed hover on list items in Admin Design System
    refs. https://github.com/TryGhost/Team/issues/3150

--- ghost/admin-x-settings/src/admin-x-ds/global/ListItem.tsx
+++ ghost/admin-x-settings/src/admin-x-ds/global/ListItem.tsx
@@ -22,7 +22,7 @@ const ListItem: React.FC<ListItemProps> = ({id, title, detail, action, hideActio
     separator = (separator === undefined) ? true : separator;
 
     return (
-        <div className={`group flex items-center justify-between ${onClick && 'hover:bg-gradient-to-r hover:from-white hover:to-grey-50'} ${separator ? 'border-b border-grey-100 last-of-type:border-none' : ''}`}>
+        <div className={`group flex items-center justify-between hover:bg-gradient-to-r hover:from-white hover:to-grey-50 ${separator ? 'border-b border-grey-100 last-of-type:border-none' : ''}`}>
             <div className={`flex grow flex-col pr-6 ${separator ? 'py-3' : 'py-2'} ${onClick && 'cursor-pointer'}`} id={id} onClick={handleClick}>
                 <span>{title}</span>
                 {detail && <span className='text-xs text-grey-700'>{detail}</span>}
