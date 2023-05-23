d291306d44dd0da44b10c3a23825a7b5cfe6e4d6
Rishabh <zrishabhgarg@gmail.com>
2023-05-23 11:43:48 +0530

    Wired mailgun and analytics settings in admin-x
    refs https://github.com/TryGhost/Team/issues/3151
    
    - updates textfield to have password type
    - updates wiring for mailgun settings
    - updates wiring for analytics settings
    - fixes local value read or useSettingGroup

--- ghost/admin-x-settings/src/components/settings/membership/Analytics.tsx
+++ ghost/admin-x-settings/src/components/settings/membership/Analytics.tsx
@@ -1,67 +1,81 @@
 import Button from '../../../admin-x-ds/global/Button';
-import ButtonGroup from '../../../admin-x-ds/global/ButtonGroup';
-import React, {useState} from 'react';
+import React from 'react';
 import SettingGroup from '../../../admin-x-ds/settings/SettingGroup';
 import SettingGroupContent from '../../../admin-x-ds/settings/SettingGroupContent';
 import Toggle from '../../../admin-x-ds/global/Toggle';
-import {TSettingGroupStates} from '../../../admin-x-ds/settings/SettingGroup';
+import useSettingGroup from '../../../hooks/useSettingGroup';
 
 const Analytics: React.FC = () => {
-    const [currentState, setCurrentState] = useState<TSettingGroupStates>('view');
+    const {
+        currentState,
+        handleSave,
+        handleCancel,
+        updateSetting,
+        getSettingValues,
+        handleStateChange
+    } = useSettingGroup();
 
-    const handleStateChange = () => {
-        setCurrentState('unsaved');
+    const [trackEmailOpens, trackEmailClicks, trackMemberSources, outboundLinkTagging] = getSettingValues([
+        'email_track_opens', 'email_track_clicks', 'members_track_sources', 'outbound_link_tagging'
+    ]) as boolean[];
+
+    const handleToggleChange = (key: string, e: React.ChangeEvent<HTMLInputElement>) => {
+        updateSetting(key, e.target.checked);
     };
 
     const inputs = (
         <SettingGroupContent columns={2}>
             <Toggle
                 // direction='rtl'
+                checked={trackEmailOpens}
                 hint='Record when a member opens an email'
                 id='newsletter-opens'
                 label='Newsletter opens'
-                onChange={handleStateChange}
+                onChange={(e) => {
+                    handleToggleChange('email_track_opens', e);
+                }}
             />
             <Toggle
                 // direction='rtl'
+                checked={trackEmailClicks}
                 hint='Record when a member clicks on any link in an email'
                 id='newsletter-clicks'
                 label='Newsletter clicks'
-                onChange={handleStateChange}
+                onChange={(e) => {
+                    handleToggleChange('email_track_clicks', e);
+                }}
             />
             <Toggle
                 // direction='rtl'
+                checked={trackMemberSources}
                 hint='Track the traffic sources and posts that drive the most member growth'
                 id='member-sources'
                 label='Member sources'
-                onChange={handleStateChange}
+                onChange={(e) => {
+                    handleToggleChange('members_track_sources', e);
+                }}
             />
             <Toggle
                 // direction='rtl'
+                checked={outboundLinkTagging}
                 hint='Make it easier for other sites to track the traffic you send them in their analytics'
                 id='outbound-links'
                 label='Outbound link tagging'
-                onChange={handleStateChange}
+                onChange={(e) => {
+                    handleToggleChange('outbound_link_tagging', e);
+                }}
             />
         </SettingGroupContent>
     );
 
-    const buttons = <ButtonGroup buttons={[
-        {
-            label: 'Cancel'
-        },
-        {
-            label: 'Save',
-            color: 'green'
-        }
-    ]} link={true} />;
-
     return (
         <SettingGroup
-            customButtons={currentState === 'unsaved' ? buttons : <></>}
             description='Decide what data you collect from your members'
             state={currentState}
             title='Analytics'
+            onCancel={handleCancel}
+            onSave={handleSave}
+            onStateChange={handleStateChange}
         >
             {inputs}
             <div className='mt-1'>
