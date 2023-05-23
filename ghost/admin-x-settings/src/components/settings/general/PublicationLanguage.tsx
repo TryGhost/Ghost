f5b9645b357d8bf09abf58e7693548c6b034355f
Rishabh <zrishabhgarg@gmail.com>
2023-05-23 08:42:05 +0530

    Updated wiring for admin-x settings
    refs https://github.com/TryGhost/Team/issues/3151
    
    - adds new hook for managing state/changes in a Settings Group
    - updates existing settings to work with the new hook to clean the code
    - wires membership access settings for read/write
    - lint cleanup

--- ghost/admin-x-settings/src/components/settings/general/PublicationLanguage.tsx
+++ ghost/admin-x-settings/src/components/settings/general/PublicationLanguage.tsx
@@ -1,49 +1,35 @@
-import React, {useContext, useEffect, useRef, useState} from 'react';
-import SettingGroup, {TSettingGroupStates} from '../../../admin-x-ds/settings/SettingGroup';
+import React from 'react';
+import SettingGroup from '../../../admin-x-ds/settings/SettingGroup';
 import SettingGroupContent from '../../../admin-x-ds/settings/SettingGroupContent';
 import TextField from '../../../admin-x-ds/global/TextField';
-import {SettingsContext} from '../../SettingsProvider';
-import {getSettingValue} from '../../../utils/helpers';
+import useSettingGroup from '../../../hooks/useSettingGroup';
 
 const PublicationLanguage: React.FC = () => {
-    const [currentState, setCurrentState] = useState<TSettingGroupStates>('view');
-    const {settings, saveSettings} = useContext(SettingsContext) || {};
-    const savedPublicationLanguage = getSettingValue(settings, 'locale');
-    const [publicationLanguage, setPublicationLanguage] = useState(savedPublicationLanguage);
-    const languageRef = useRef<HTMLInputElement>(null);
+    const {
+        currentState,
+        handleSave,
+        handleCancel,
+        updateSetting,
+        focusRef,
+        getSettingValues,
+        handleStateChange
+    } = useSettingGroup();
 
-    useEffect(() => {
-        if (currentState !== 'view' && languageRef.current) {
-            languageRef.current.focus();
-        }
-    }, [currentState]);
-
-    const handleStateChange = (newState: TSettingGroupStates) => {
-        setCurrentState(newState);
-    };
-
-    const handleSave = () => {
-        saveSettings?.([
-            {
-                key: 'locale',
-                value: publicationLanguage
-            }
-        ]);
-        setCurrentState('view');
-    };
+    const [publicationLanguage] = getSettingValues(['locale']) as string[];
 
     const handleLanguageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setCurrentState('unsaved');
-        setPublicationLanguage(e.target.value);
+        updateSetting('locale', e.target.value);
     };
 
-    const viewValues = [
-        {
-            heading: 'Site language',
-            key: 'site-language',
-            value: publicationLanguage
-        }
-    ];
+    const values = (
+        <SettingGroupContent values={[
+            {
+                heading: 'Site language',
+                key: 'site-language',
+                value: publicationLanguage
+            }
+        ]} />
+    );
 
     const hint = (
         <>
@@ -56,7 +42,7 @@ const PublicationLanguage: React.FC = () => {
         <SettingGroupContent columns={1}>
             <TextField
                 hint={hint}
-                inputRef={languageRef}
+                inputRef={focusRef}
                 placeholder="Site language"
                 title='Site language'
                 value={publicationLanguage}
@@ -70,10 +56,11 @@ const PublicationLanguage: React.FC = () => {
             description="Set the language/locale which is used on your site"
             state={currentState}
             title="Publication Language"
+            onCancel={handleCancel}
             onSave={handleSave}
             onStateChange={handleStateChange}
         >
-            {currentState === 'view' ? <SettingGroupContent values={viewValues} /> : inputFields }
+            {currentState === 'view' ? values : inputFields }
         </SettingGroup>
     );
 };
--- ghost/admin-x-settings/src/components/settings/general/TitleAndDescription.tsx
+++ ghost/admin-x-settings/src/components/settings/general/TitleAndDescription.tsx
@@ -1,96 +1,78 @@
-import React, {useContext, useEffect, useRef, useState} from 'react';
+import React from 'react';
 import SettingGroup from '../../../admin-x-ds/settings/SettingGroup';
 import SettingGroupContent from '../../../admin-x-ds/settings/SettingGroupContent';
 import TextField from '../../../admin-x-ds/global/TextField';
-import {SettingsContext} from '../../SettingsProvider';
-import {TSettingGroupStates} from '../../../admin-x-ds/settings/SettingGroup';
-import {getSettingValue} from '../../../utils/helpers';
+import useSettingGroup from '../../../hooks/useSettingGroup';
 
 const TitleAndDescription: React.FC = () => {
-    const [currentState, setCurrentState] = useState<TSettingGroupStates>('view');
-    const {settings, saveSettings} = useContext(SettingsContext) || {};
-    const savedSiteTitle = getSettingValue(settings, 'title');
-    const savedSiteDescription = getSettingValue(settings, 'description');
-    const [siteTitle, setSiteTitleValue] = useState(savedSiteTitle);
-    const [siteDescription, setSiteDescriptionValue] = useState(savedSiteDescription);
-    const siteTitleRef = useRef<HTMLInputElement>(null);
+    const {
+        currentState,
+        focusRef,
+        handleSave,
+        handleCancel,
+        updateSetting,
+        getSettingValues,
+        handleStateChange
+    } = useSettingGroup();
 
-    useEffect(() => {
-        if (currentState !== 'view' && siteTitleRef.current) {
-            siteTitleRef.current.focus();
-        }
-    }, [currentState]);
-
-    const handleStateChange = (newState: TSettingGroupStates) => {
-        setCurrentState(newState);
-    };
-
-    const handleSave = () => {
-        saveSettings?.([
-            {
-                key: 'title',
-                value: siteTitle
-            },
-            {
-                key: 'description',
-                value: siteDescription
-            }
-        ]);
-        setCurrentState('view');
-    };
+    const [title, description] = getSettingValues(['title', 'description']) as string[];
 
     const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setCurrentState('unsaved');
-        setSiteTitleValue(e.target.value);
+        updateSetting('title', e.target.value);
     };
 
     const handleDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setCurrentState('unsaved');
-        setSiteDescriptionValue(e.target.value);
+        updateSetting('description', e.target.value);
     };
 
-    const viewValues = [
-        {
-            heading: 'Site title',
-            key: 'site-title',
-            value: siteTitle
-        },
-        {
-            heading: 'Site description',
-            key: 'site-description',
-            value: siteDescription
-        }
-    ];
+    const values = (
+        <SettingGroupContent
+            columns={2}
+            values={[
+                {
+                    heading: 'Site title',
+                    key: 'site-title',
+                    value: title
+                },
+                {
+                    heading: 'Site description',
+                    key: 'site-description',
+                    value: description
+                }
+            ]}
+        />
+    );
 
     const inputFields = (
         <SettingGroupContent columns={2}>
             <TextField
                 hint="The name of your site"
-                inputRef={siteTitleRef}
+                inputRef={focusRef}
                 placeholder="Site title"
                 title="Site title"
-                value={siteTitle}
+                value={title}
                 onChange={handleTitleChange}
             />
             <TextField
                 hint="Used in your theme, meta data and search results"
                 placeholder="Enter something"
                 title="Site description"
-                value={siteDescription}
+                value={description}
                 onChange={handleDescriptionChange}
             />
         </SettingGroupContent>
     );
 
     return (
-        <SettingGroup 
-            description='The details used to identify your publication around the web' 
+        <SettingGroup
+            description='The details used to identify your publication around the web'
             state={currentState}
             title='Title & description'
+            onCancel={handleCancel}
             onSave={handleSave}
             onStateChange={handleStateChange}
         >
-            {currentState === 'view' ? <SettingGroupContent columns={2} values={viewValues} /> : inputFields }
+            {currentState === 'view' ? values : inputFields }
         </SettingGroup>
     );
 };
