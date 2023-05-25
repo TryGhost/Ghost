e8818e16dc17541d20c8515dbc4edbd25d13d77e
Naz <hi@nazavo.com>
2023-05-25 11:39:48 +0700

    Grouped edit collection test cases
    refs https://github.com/TryGhost/Team/issues/3260
    
    - Cleanup before adding more test cases to the suite

--- ghost/core/test/e2e-api/admin/collections.test.js
+++ ghost/core/test/e2e-api/admin/collections.test.js
@@ -104,67 +104,69 @@ describe('Collections API', function () {
         assert.equal(readResponse.body.collections[0].title, 'Test Collection to Read');
     });
 
-    it('Can edit a Collection', async function () {
-        const collection = {
-            title: 'Test Collection to Edit'
-        };
+    describe('edit', function () {
+        it('Can edit a Collection', async function () {
+            const collection = {
+                title: 'Test Collection to Edit'
+            };
 
-        const addResponse = await agent
-            .post('/collections/')
-            .body({
-                collections: [collection]
-            })
-            .expectStatus(201)
-            .matchHeaderSnapshot({
-                'content-version': anyContentVersion,
-                etag: anyEtag,
-                location: anyLocationFor('collections')
-            })
-            .matchBodySnapshot({
-                collections: [matchCollection]
-            });
+            const addResponse = await agent
+                .post('/collections/')
+                .body({
+                    collections: [collection]
+                })
+                .expectStatus(201)
+                .matchHeaderSnapshot({
+                    'content-version': anyContentVersion,
+                    etag: anyEtag,
+                    location: anyLocationFor('collections')
+                })
+                .matchBodySnapshot({
+                    collections: [matchCollection]
+                });
 
-        const collectionId = addResponse.body.collections[0].id;
+            const collectionId = addResponse.body.collections[0].id;
 
-        const editResponse = await agent
-            .put(`/collections/${collectionId}/`)
-            .body({
-                collections: [{
-                    title: 'Test Collection Edited'
-                }]
-            })
-            .expectStatus(200)
-            .matchHeaderSnapshot({
-                'content-version': anyContentVersion,
-                etag: anyEtag
-            })
-            .matchBodySnapshot({
-                collections: [matchCollection]
-            });
+            const editResponse = await agent
+                .put(`/collections/${collectionId}/`)
+                .body({
+                    collections: [{
+                        title: 'Test Collection Edited'
+                    }]
+                })
+                .expectStatus(200)
+                .matchHeaderSnapshot({
+                    'content-version': anyContentVersion,
+                    etag: anyEtag
+                })
+                .matchBodySnapshot({
+                    collections: [matchCollection]
+                });
 
-        assert.equal(editResponse.body.collections[0].title, 'Test Collection Edited');
-    });
+            assert.equal(editResponse.body.collections[0].title, 'Test Collection Edited');
+        });
 
-    it('Fails to edit unexistent Collection', async function () {
-        const unexistentID = '5951f5fca366002ebd5dbef7';
-        await agent
-            .put(`/collections/${unexistentID}/`)
-            .body({
-                collections: [{
-                    id: unexistentID,
-                    title: 'Editing unexistent Collection'
-                }]
-            })
-            .expectStatus(404)
-            .matchBodySnapshot({
-                errors: [{
-                    id: anyErrorId
-                }]
-            })
-            .matchHeaderSnapshot({
-                'content-version': anyContentVersion,
-                etag: anyEtag
-            });
+        it('Fails to edit unexistent Collection', async function () {
+            const unexistentID = '5951f5fca366002ebd5dbef7';
+            await agent
+                .put(`/collections/${unexistentID}/`)
+                .body({
+                    collections: [{
+                        id: unexistentID,
+                        title: 'Editing unexistent Collection'
+                    }]
+                })
+                .expectStatus(404)
+                .matchBodySnapshot({
+                    errors: [{
+                        id: anyErrorId
+                    }]
+                })
+                .matchHeaderSnapshot({
+                    'content-version': anyContentVersion,
+                    etag: anyEtag
+                });
+        });
     });
 
     it('Can delete a Collection', async function () {
