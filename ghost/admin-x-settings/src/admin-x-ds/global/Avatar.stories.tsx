c62d73578bbc4e700a99ebdbfbfd008949ff7bf9
Djordje Vlaisavljevic <dzvlais@gmail.com>
2023-05-30 12:51:27 +0100

    Added support for images and fallback icon to `Avatar`
    refs https://github.com/TryGhost/Team/issues/3318

--- ghost/admin-x-settings/src/admin-x-ds/global/Avatar.stories.tsx
+++ ghost/admin-x-settings/src/admin-x-ds/global/Avatar.stories.tsx
@@ -19,6 +19,19 @@ export const Default: Story = {
     }
 };
 
+export const WithImage: Story = {
+    args: {
+        image: 'https://www.looper.com/img/gallery/the-untold-truth-of-the-navi-from-avatar/l-intro-1664914107.jpg',
+        label: 'DV'
+    }
+};
+
+export const WithFallback: Story = {
+    args: {
+        size: 'lg'
+    }
+};
+
 export const Small: Story = {
     args: {
         label: 'DV',
--- ghost/admin-x-settings/src/admin-x-ds/global/Avatar.tsx
+++ ghost/admin-x-settings/src/admin-x-ds/global/Avatar.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import {ReactComponent as UserIcon} from '../../assets/icons/single-user-fill.svg';
 
 type AvatarSize = 'sm' | 'md' | 'lg' | 'xl';
 
 interface AvatarProps {
+    image?: string;
     label?: string;
     labelColor?: string;
     bgColor?: string;
@@ -10,28 +12,47 @@ interface AvatarProps {
     className?: string;
 }
 
-const Avatar: React.FC<AvatarProps> = ({label, labelColor, bgColor, size, className}) => {
+const Avatar: React.FC<AvatarProps> = ({image, label, labelColor, bgColor, size, className}) => {
     let avatarSize = '';
+    let fallbackPosition = '';
 
     switch (size) {
     case 'sm':
         avatarSize = ' w-7 h-7 text-sm ';
+        fallbackPosition = ' -mb-2 ';
+        break;
+    case 'md':
+        avatarSize = ' w-10 h-10 text-md ';
+        fallbackPosition = ' -mb-2 ';
         break;
     case 'lg':
-        avatarSize = ' w-12 h-12 text-md';
+        avatarSize = ' w-12 h-12 text-md ';
+        fallbackPosition = ' -mb-2 ';
         break;
     case 'xl':
-        avatarSize = ' w-16 h-16 text-2xl';
+        avatarSize = ' w-16 h-16 text-2xl ';
+        fallbackPosition = ' -mb-3 ';
         break;
-    
     default:
         avatarSize = ' w-10 h-10 text-md ';
         break;
     }
 
-    return (
-        <div className={`${bgColor && `bg-${bgColor}`} ${labelColor && `text-${labelColor}`} inline-flex items-center justify-center rounded-full p-2 font-semibold ${avatarSize} ${className && className}`}>{label}</div>
-    );
+    if (image) {
+        return (
+            <img alt="" className={`inline-flex items-center justify-center rounded-full object-cover font-semibold ${avatarSize} ${className && className}`} src={image}/>
+        );
+    } else if (label) {
+        return (
+            <div className={`${bgColor && `bg-${bgColor}`} ${labelColor && `text-${labelColor}`} inline-flex items-center justify-center rounded-full p-2 font-semibold ${avatarSize} ${className && className}`}>{label}</div>
+        );
+    } else {
+        return (
+            <div className={`inline-flex items-center justify-center overflow-hidden rounded-full bg-grey-100 p-1 font-semibold ${avatarSize} ${className && className}`}>
+                <UserIcon className={`${fallbackPosition} h-full w-full  text-grey-300`} />
+            </div>
+        );
+    }
 };
 
 export default Avatar;
