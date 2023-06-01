19b75bffd574f56385a8bb4f44eb172f60ab3b68
Fabien "egg" O'Carroll <fabien@allou.is>
2023-06-01 00:11:43 -0400

    Update eslint and c8 to support using TS interfaces
    This will allow us to define the repository interface

--- ghost/collections/.eslintrc.js
+++ ghost/collections/.eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
-    plugins: ['ghost'],
+    plugins: ['ghost', '@typescript-eslint'],
     extends: [
         'plugin:ghost/node'
-    ]
+    ],
+    rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': ['error']
+    }
 };
