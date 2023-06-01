339182d23b8dbe069e632a1acebabd2f0b356401
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-06-01 13:52:18 +0200

    Fixed using wrong variable

--- ghost/signup-form/src/components/pages/SuccessView.tsx
+++ ghost/signup-form/src/components/pages/SuccessView.tsx
@@ -7,12 +7,12 @@ export const SuccessView: React.FC<{
     isMinimal: boolean;
     backgroundColor?: string;
 }> = ({email,isMinimal,backgroundColor}) => {
-    const {i} = useAppContext();
+    const {t} = useAppContext();
 
     if (isMinimal) {
         return (
             <div>
-                <h1 className="text-xl font-bold">{i(`Now check your email!`)}</h1>
+                <h1 className="text-xl font-bold">{t(`Now check your email!`)}</h1>
             </div>
         );
     }
@@ -22,8 +22,8 @@ export const SuccessView: React.FC<{
             data-testid="success-page"
             style={{backgroundColor, color: backgroundColor && textColorForBackgroundColor(backgroundColor)}}
         >
-            <h1 className='text-center text-lg font-bold sm:text-xl md:text-2xl lg:text-3xl'>{i(`Now check your email!`)}</h1>
-            <p className='mb-5 text-center'>{i(`An email has been send to ${email}`)}</p>
+            <h1 className='text-center text-lg font-bold sm:text-xl md:text-2xl lg:text-3xl'>{t(`Now check your email!`)}</h1>
+            <p className='mb-5 text-center'>{t(`An email has been send to ${email}`)}</p>
         </div>
     );
 };
