05d2db42c61898aaaab95d283dc6346f521e9119
Fabien "egg" O'Carroll <fabien@allou.is>
2023-06-01 00:15:36 -0400

    Update CollectionsService to remove CollectionPost usage

--- ghost/collections/src/CollectionsRepositoryInMemory.ts
+++ ghost/collections/src/CollectionsRepositoryInMemory.ts
@@ -1,20 +1,7 @@
 import {InMemoryRepository} from '@tryghost/in-memory-repository';
 import {Collection} from './Collection';
-import {CollectionPost} from './CollectionPost';
 
 export class CollectionsRepositoryInMemory extends InMemoryRepository<string, Collection> {
-    collectionsPostsRepository: any;
-
-    constructor(deps: any) {
-        super();
-
-        this.collectionsPostsRepository = deps.collectionsPostsRepository;
-    }
-
-    async saveCollectionPost(collectionPost: CollectionPost) {
-        await this.collectionsPostsRepository.save(collectionPost);
-    }
-
     protected toPrimitive(entity: Collection): object {
         return {
             title: entity.title,
