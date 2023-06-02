2c7a7c46e4ee923c31a4f50e754eb7477a384b0a
Aileen Booker <AileenCGN@gmail.com>
2023-06-02 13:03:43 -0400

    Added JSDoc types and inject more deps for `DomainEventsAnalytics`
    no issue

--- ghost/core/core/server/services/segment/DomainEventsAnalytics.js
+++ ghost/core/core/server/services/segment/DomainEventsAnalytics.js
@@ -1,40 +1,82 @@
-const _ = require('lodash');
-const logging = require('@tryghost/logging');
-const DomainEvents = require('@tryghost/domain-events');
 const {MilestoneCreatedEvent} = require('@tryghost/milestones');
 
+/**
+ * @typedef {import('@tryghost/logging')} logging
+ */
+
+/**
+ * @typedef {import('analytics-node')} analytics
+ */
+
+/**
+ * @typedef {import('../../../shared/sentry')} sentry
+ */
+
+/**
+ * @typedef {import('@tryghost/domain-events')} DomainEvents
+ */
+
+/**
+ * @typedef {object} IDomainEventsAnalytics
+ * @param {analytics} analytics
+ * @param {logging} logging
+ * @param {object} trackDefaults
+ * @param {string} prefix
+ * @param {sentry} sentry
+ * @param {DomainEvents} DomainEvents
+ * @prop {} subscribeToEvents
+ */
+
 module.exports = class DomainEventsAnalytics {
+    /** @type {analytics} */
     #analytics;
+    /** @type {object} */
     #trackDefaults;
+    /** @type {string} */
     #prefix;
+    /** @type {sentry} */
     #sentry;
+    /** @type {logging} */
+    #logging;
+    /** @type {DomainEvents} */
+    #DomainEvents;
 
     constructor(deps) {
         this.#analytics = deps.analytics;
         this.#trackDefaults = deps.trackDefaults;
         this.#prefix = deps.prefix;
         this.#sentry = deps.sentry;
+        this.#logging = deps.logging;
+        this.#DomainEvents = deps.DomainEvents;
     }
 
-    async #handleMilestoneCreatedEvent(type, event) {
-        if (type === MilestoneCreatedEvent
-            && event.data.milestone
+    /**
+     *
+     * @param {object} event
+     * @param {object} event.data
+     * @param {object} event.data.milestone
+     * @param {number} event.data.milestone.value
+     * @param {string} event.data.milestone.type
+     * @returns {Promise<void>}
+     */
+    async #handleMilestoneCreatedEvent(event) {
+        if (event.data.milestone
             && event.data.milestone.value === 100
         ) {
             const eventName = event.data.milestone.type === 'arr' ? '$100 MRR reached' : '100 members reached';
 
             try {
-                this.#analytics.track(_.extend(this.#trackDefaults, {}, {event: this.#prefix + eventName}));
+                this.#analytics.track(Object.assign(this.#trackDefaults, {}, {event: this.#prefix + eventName}));
             } catch (err) {
-                logging.error(err);
+                this.#logging.error(err);
                 this.#sentry.captureException(err);
             }
         }
     }
 
-    subscribeToDomainEvents() {
-        DomainEvents.subscribe(MilestoneCreatedEvent, async (event) => {
-            await this.#handleMilestoneCreatedEvent(MilestoneCreatedEvent, event);
+    subscribeToEvents() {
+        this.#DomainEvents.subscribe(MilestoneCreatedEvent, async (event) => {
+            await this.#handleMilestoneCreatedEvent(event);
         });
     }
 };
