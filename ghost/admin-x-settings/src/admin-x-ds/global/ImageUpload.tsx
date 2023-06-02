20bee9c72d79ec27864daa5012a11db8493e5ea4
Djordje Vlaisavljevic <dzvlais@gmail.com>
2023-06-02 14:04:38 +0100

    Added className prop to `ImageUpload`
    refs https://github.com/TryGhost/Team/issues/3318

--- ghost/admin-x-settings/src/admin-x-ds/global/ImageUpload.tsx
+++ ghost/admin-x-settings/src/admin-x-ds/global/ImageUpload.tsx
@@ -14,6 +14,7 @@ interface ImageUploadProps {
     deleteButtonContent?: React.ReactNode;
     onUpload: (file: File) => void;
     onDelete: () => void;
+    className?: string;
 }
 
 const ImageUpload: React.FC<ImageUploadProps> = ({
@@ -27,7 +28,8 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
     deleteButtonClassName = 'invisible absolute right-4 top-4 flex h-8 w-8 cursor-pointer items-center justify-center rounded bg-[rgba(0,0,0,0.75)] text-white hover:bg-black group-hover:!visible',
     deleteButtonContent = <Icon color='white' name='trash' size='sm' />,
     onUpload,
-    onDelete
+    onDelete,
+    className
 }) => {
     if (imageURL) {
         return (
