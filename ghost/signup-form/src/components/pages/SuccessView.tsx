a28ad661eafe15901fad65386ae4d17ffce0e5fc
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-06-02 06:42:10 +0200

    fixed linting errors

--- ghost/signup-form/src/components/pages/SuccessView.tsx
+++ ghost/signup-form/src/components/pages/SuccessView.tsx
@@ -19,12 +19,12 @@ export const SuccessView: React.FC<{
     }
     return (
         <div
-            className='bg-grey-200 flex h-[100vh] flex-col items-center justify-center px-4 sm:px-6 md:px-10'
+            className='flex h-[100vh] flex-col items-center justify-center bg-grey-200 px-4 sm:px-6 md:px-10'
             data-testid="success-page"
             style={{backgroundColor, color: textColor}}
         >
             {logo && <img alt={title} className='mb-2 h-[64px] w-auto' src={logo}/>}
-            <h1 className='text-center text-lg font-bold sm:text-xl md:text-2xl lg:text-3xl'>Now check your email!</h1>
+            <h1 className='text-center text-lg font-bold sm:text-xl md:text-2xl lg:text-3xl'>{t(`Now check your email!`)}</h1>
             <p className='mb-4 text-center sm:mb-[4.1rem]'>{t(`To complete signup, click the confirmation link in your inbox. If it doesn&apos;t arrive within 3 minutes, check your spam folder!`)}</p>
         </div>
     );
