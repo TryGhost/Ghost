7535fc73fc7c1558b0e8605ed165ac42be802197
Peter Zimon <peter.zimon@gmail.com>
2023-06-02 12:21:30 +0200

    Added click to menu items in AdminX
    rers. https://github.com/TryGhost/Team/issues/3318

--- ghost/admin-x-settings/src/admin-x-ds/global/Menu.stories.tsx
+++ ghost/admin-x-settings/src/admin-x-ds/global/Menu.stories.tsx
@@ -16,7 +16,9 @@ type Story = StoryObj<typeof Menu>;
 const items = [
     {id: 'item-1', label: 'Item 1'},
     {id: 'item-2', label: 'Item 2'},
-    {id: 'item-3', label: 'Item 3'}
+    {id: 'item-3', label: 'Click me', onClick: () => {
+        alert('Clicked!');
+    }}
 ];
 
 const longItems = [
