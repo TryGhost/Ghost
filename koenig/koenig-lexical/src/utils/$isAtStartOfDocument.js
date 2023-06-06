297bb53af483bb1af936381db0722caf4a0c6564
Steve Larson <9larsons@gmail.com>
2023-06-06 18:02:14 -0500

    Fixed title selection when starting with nested list
    closes TryGhost/Team#3237

--- koenig/koenig-lexical/src/utils/$isAtStartOfDocument.js
+++ koenig/koenig-lexical/src/utils/$isAtStartOfDocument.js
@@ -1,12 +1,22 @@
+import {$isListItemNode} from '@lexical/list';
 import {$isTextNode} from 'lexical';
 
 export function $isAtStartOfDocument(selection) {
     let [selectedNode] = selection.getNodes();
+    
     if ($isTextNode(selectedNode)) {
         selectedNode = selectedNode.getParent();
     }
+
+    let selectedTopLevelElement = selectedNode.getTopLevelElement();
+
+    // handle nested lists, where parent for a text node is not enough
+    if ($isListItemNode(selectedNode) && selectedTopLevelElement !== selectedNode.getParent()) {
+        return false;
+    }
+
     const selectedIndex = selectedNode.getIndexWithinParent();
-    const selectedTopLevelIndex = selectedNode.getTopLevelElement()?.getIndexWithinParent();
+    const selectedTopLevelIndex = selectedTopLevelElement ? selectedTopLevelElement.getIndexWithinParent() : undefined;
 
     return selectedIndex === 0
         && selectedTopLevelIndex === 0
