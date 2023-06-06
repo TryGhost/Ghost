e69583c1c9585881acf4ed41da99b87ed33d9f2d
Fabien "egg" O'Carroll <fabien@allou.is>
2023-06-06 12:01:06 +0200

    Fixed CollectionsRepositoryInMemory
    The toPrimitive method was not exposing all attributes
    which is required for filtering to work correctly.

--- ghost/collections/src/CollectionsRepositoryInMemory.ts
+++ ghost/collections/src/CollectionsRepositoryInMemory.ts
@@ -3,10 +3,6 @@ import {Collection} from './Collection';
 
 export class CollectionsRepositoryInMemory extends InMemoryRepository<string, Collection> {
     protected toPrimitive(entity: Collection): object {
-        return {
-            title: entity.title,
-            description: entity.description,
-            feature_image: entity.featureImage
-        };
+        return entity.toJSON();
     }
 }
