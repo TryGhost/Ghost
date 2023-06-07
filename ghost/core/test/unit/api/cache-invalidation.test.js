4fcc001e51143e531076c1925564bf6f63c542b5
Michael Barrett <mike@ghost.org>
2023-06-07 10:08:48 +0100

    add assertion for root controller path

--- ghost/core/test/unit/api/cache-invalidation.test.js
+++ ghost/core/test/unit/api/cache-invalidation.test.js
@@ -4,38 +4,42 @@ const path = require('path');
 const glob = require('glob');
 
 const models = require('../../../core/server/models');
-const settings = require('../../../core/server/services/settings/settings-service');
 
-describe('Cache Invalidation', function () {
-    before(async function () {
-        // Initialise models - Utilised by various endpoints to reference static fields (i.e models.Post.allowedFormats) when required in
-        models.init();
-
-        // Initialise settings cache - Utilised by the indentites endpoint when required in
-        await settings.init();
-    });
-
-    it('Controller actions explicitly declare cacheInvalidate header', async function () {
-        const endpointPaths = glob.sync('*.js', {
-            cwd: path.join(__dirname, '../../../core/server/api/endpoints'),
-            ignore: ['index.js'],
-            realpath: true
+describe('API', function () {
+    describe('Cache Invalidation', function () {
+        before(async function () {
+            // Initialise models - Utilised by various endpoints to reference static fields (i.e models.Post.allowedFormats) when required in
+            models.init();
         });
 
-        endpointPaths.forEach((endpointPath) => {
-            const endpointConfig = require(endpointPath);
-            const ignoreKeys = ['docName'];
+        it('Controller actions explicitly declare cacheInvalidate header', async function () {
+            const controllersRootPath = path.join(__dirname, '../../../core/server/api/endpoints');
+            const controllerPaths = glob.sync('*.js', {
+                cwd: controllersRootPath,
+                ignore: [
+                    'index.js',
+                    'identities.js' // The identities controller can not be required directly due to requiring other parts of Ghost to have been initialised first
+                ],
+                realpath: true
+            });
 
-            Object.keys(endpointConfig).forEach((key) => {
-                if (ignoreKeys.includes(key) || typeof endpointConfig[key] === 'function') {
-                    return;
-                }
+            assert.ok(controllerPaths.length > 0, `No controllers found in ${controllersRootPath}`);
 
-                assert.notEqual(
-                    endpointConfig[key].headers?.cacheInvalidate,
-                    undefined,
-                    `"${key}" action in controller: ${endpointPath} is missing cacheInvalidate header - This needs to be explicitly defined`
-                );
+            controllerPaths.forEach((controllerPath) => {
+                const controllerConfig = require(controllerPath);
+                const ignoreKeys = ['docName'];
+
+                Object.keys(controllerConfig).forEach((key) => {
+                    if (ignoreKeys.includes(key) || typeof controllerConfig[key] === 'function') {
+                        return;
+                    }
+
+                    assert.notEqual(
+                        controllerConfig[key].headers?.cacheInvalidate,
+                        undefined,
+                        `"${key}" action in controller: ${controllerPath} is missing cacheInvalidate header - This needs to be explicitly defined`
+                    );
+                });
             });
         });
     });
