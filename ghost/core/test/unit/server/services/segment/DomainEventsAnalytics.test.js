f13029a274f92c8b8ff4ed3917f011d2a317f0e7
Aileen Booker <AileenCGN@gmail.com>
2023-06-08 12:11:26 -0400

    Added new Stripe events to `DomainEventsAnalytics`
    no issue
    
    - We need to send information about Stripe being enabled or disabled in live mode to analytics
    - This hooks up the Domain events listeners in the analytics service and processes this information accordingly

--- ghost/core/test/unit/server/services/segment/DomainEventsAnalytics.test.js
+++ ghost/core/test/unit/server/services/segment/DomainEventsAnalytics.test.js
@@ -8,6 +8,7 @@ const DomainEventsAnalytics = require('../../../../../core/server/services/segme
 // Stubbed dependencies
 const DomainEvents = require('@tryghost/domain-events');
 const {MilestoneCreatedEvent} = require('@tryghost/milestones');
+const {StripeLiveEnabledEvent, StripeLiveDisabledEvent} = require('@tryghost/members-stripe-service').events;
 
 describe('DomainEventsAnalytics', function () {
     describe('Constructor', function () {
@@ -54,7 +55,7 @@ describe('DomainEventsAnalytics', function () {
             });
 
             domainEventsAnalytics.subscribeToEvents();
-            assert(domainEventsStub.callCount === 1);
+            assert(domainEventsStub.callCount === 3);
             assert(loggingStub.callCount === 0);
         });
 
@@ -170,6 +171,59 @@ describe('DomainEventsAnalytics', function () {
             await DomainEvents.allSettled();
             // Analytics should not be called again
             assert(analyticsStub.callCount === 1);
+            assert(loggingStub.callCount === 0);
+        });
+
+        it('handles Stripe live enabled and disabled events', async function () {
+            domainEventsAnalytics = new DomainEventsAnalytics({
+                analytics: {
+                    track: analyticsStub
+                },
+                trackDefaults: {
+                    userId: '9876',
+                    properties: {email: 'john+stripe@test.com'}
+                },
+                prefix: 'Pro: ',
+                exceptionHandler: {
+                    captureException: exceptionHandlerStub
+                },
+                DomainEvents,
+                logging: {
+                    error: loggingStub
+                }
+            });
+
+            domainEventsAnalytics.subscribeToEvents();
+
+            DomainEvents.dispatch(StripeLiveEnabledEvent.create({
+                message: 'Stripe live mode enabled'
+            }));
+
+            await DomainEvents.allSettled();
+
+            assert(analyticsStub.callCount === 1);
+            assert(analyticsStub.getCall(0).calledWith({
+                userId: '9876',
+                properties: {email: 'john+stripe@test.com'},
+                event: 'Pro: Stripe Live Enabled'
+            }));
+            assert(loggingStub.callCount === 0);
+
+            DomainEvents.dispatch(StripeLiveDisabledEvent.create({
+                message: 'Stripe live mode disabled'
+            }));
+
+            await DomainEvents.allSettled();
+
+            assert(analyticsStub.callCount === 2);
+            assert(analyticsStub.getCall(1).calledWith({
+                userId: '9876',
+                properties: {email: 'john+stripe@test.com'},
+                event: 'Pro: Stripe Live Disabled'
+            }));
+
+            await DomainEvents.allSettled();
+
             assert(loggingStub.callCount === 0);
         });
 
