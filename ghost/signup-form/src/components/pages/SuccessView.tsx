afe18d54de39b775edbf355b369d3a0f00146d3a
Sanne de Vries <sannedv@protonmail.com>
2023-06-09 11:35:36 +0200

    Added email icon to signup form success page
    Refs https://www.notion.so/ghost/Embed-testing-0193905e46d449b5ad865f7364176814?pvs=4#de29f9cc1a364a12a4a0eeebef68f3a4

--- ghost/signup-form/src/components/pages/SuccessView.tsx
+++ ghost/signup-form/src/components/pages/SuccessView.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
+import {ReactComponent as EmailIcon} from '../../../assets/icons/email.svg';
 import {useAppContext} from '../../AppContext';
 
 export const SuccessView: React.FC<{
     email: string;
-    title?: string;
-    icon?: string;
     backgroundColor?: string;
     textColor?: string;
-}> = ({title, icon, backgroundColor, textColor}) => {
+}> = ({backgroundColor, textColor}) => {
     const {t} = useAppContext();
     return (
         <div
@@ -15,7 +14,7 @@ export const SuccessView: React.FC<{
             data-testid="success-page"
             style={{backgroundColor, color: textColor}}
         >
-            {icon && <img alt={title} className='mb-2 h-[64px] w-auto' src={icon}/>}
+            <EmailIcon className='my-3 w-8' />
             <h1 className='text-center text-lg font-bold sm:text-xl md:text-2xl lg:text-3xl'>{t(`Now check your email!`)}</h1>
             <p className='mb-4 max-w-[600px] text-center sm:mb-[4.1rem]'>{t(`To complete signup, click the confirmation link in your inbox. If it doesn't arrive within 3 minutes, check your spam folder!`)}</p>
         </div>
