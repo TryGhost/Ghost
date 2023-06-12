08bccf6412cb2d7a1d639f52cd8d10c863decf10
Hannah Wolfe <github.erisds@gmail.com>
2023-06-12 15:27:12 +0100

    Added consistent linting pattern to all packages
    refs: https://github.com/TryGhost/Toolbox/issues/188
    
    - some of our older packages used a pattern for linting which missed using test config for linting tests
    - we need this to be consistent so that we can add more eslint rules for testing
    - two packages also didn't use the lib pattern, which made the lint pattern error - so this was fixed as well

--- ghost/oembed-service/test/hello.test.js
+++ ghost/oembed-service/test/hello.test.js
@@ -2,6 +2,6 @@ const assert = require('assert');
 
 describe('Hello world', function () {
     it('Runs a test', function () {
-        assert.equal('hello', 'hello');
+        assert.ok(require('../index'));
     });
 });
