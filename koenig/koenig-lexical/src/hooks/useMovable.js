dda3606574e087f8b6817bf6a3f5c72c2b12a4c1
Simon Backx <simon@ghost.org>
2023-06-13 10:53:35 +0200

    Improved dragging, resizing and moving of settings panel
    fixes https://github.com/TryGhost/Team/issues/3442
    
    This change should improve the general UX when working with the settings panel, and prevent it from jumping of the screen.
    
    - We no longer change both the transform-translate and left/top CSS properties at the same time. We only alter the transform property. This allows to prevent moving the panel outside the viewport.
    - Reduced usage of expensive getBoundingClientRect method
    - The panel can be dragged outside the viewport, but will always stay at least 40px inside, to allow for the user to drag it back in.
    - When resizing the viewport, the panel will move towards the default position if possible. It will also try to stay within the viewport. If the user moved the panel outside the viewport earlier, it will keep that into account.
    - Fixed threshold that was not working due to syntax errors (missing .current)
    - Fixed calculation errors
    - Removed unneeded variables in useMovable and cleaned up + documented meaning of all variables

--- koenig/koenig-lexical/src/hooks/useMovable.js
+++ koenig/koenig-lexical/src/hooks/useMovable.js
@@ -22,21 +22,28 @@ function guidFor() {
  * @example
  * const {ref} = useMovable();
  */
-export default function useMovable({adjustOnResize} = {}) {
+export default function useMovable({adjustOnResize, adjustOnDrag} = {}) {
     const ref = useRef(null);
 
     const moveThreshold = 3;
 
     // Use refs to avoid re-renders, see https://reactjs.org/docs/hooks-faq.html#is-there-something-like-instance-variables
-    let active = useRef(false);
-    let currentX = useRef();
-    let currentY = useRef();
-    let initialX = useRef();
-    let initialY = useRef();
-    let xOffset = useRef();
-    let yOffset = useRef();
-    let originalOverflow = useRef();
-    let guid = guidFor();
+    const active = useRef(false);
+    const currentX = useRef();
+    const currentY = useRef();
+
+    /**
+     * Cursor offset from the left top side of the panel on touchstart/mousedown
+     */
+    const offsetX = useRef();
+    const offsetY = useRef();
+
+    // Keep track of spacing, so we can allow negative spacing when resizing if the user placed the window outside the canvas
+    // Contains an object with top, left, right and bottom spacing between the panel and the viewport
+    const lastSpacing = useRef();
+
+    const originalOverflow = useRef();
+    const guid = guidFor();
 
     // React event handlers get added to the root element, so if we add listeners to the ref directly
     // and call stopPropagation they stop any React events on child nodes from firing.
@@ -61,6 +68,33 @@ export default function useMovable({adjustOnResize} = {}) {
     const setTranslate = useCallback((xPos, yPos) => {
         ref.current.style.transform = `translate3d(${xPos}px, ${yPos}px, 0)`;
     }, [ref]);
+
+    const setPosition = useCallback(({x, y}) => {
+        currentX.current = x;
+        currentY.current = y;
+
+        const width = ref.current.offsetWidth;
+        const height = ref.current.offsetHeight;
+
+        // Update spacing
+        const spacing = {
+            top: y,
+            left: x,
+            right: window.innerWidth - x - width,
+            bottom: window.innerHeight - y - height
+        };
+        lastSpacing.current = spacing;
+
+        setTranslate(x, y);
+    }, [setTranslate]);
+
+    const getPosition = useCallback(() => {
+        return {
+            x: currentX.current,
+            y: currentY.current,
+            lastSpacing: lastSpacing.current
+        };
+    }, []);
 
     const disableScroll = useCallback(() => {
         originalOverflow.current = ref.current?.style.overflow;
@@ -114,12 +148,11 @@ export default function useMovable({adjustOnResize} = {}) {
             eventY = e.clientY;
         }
 
-        if (!active) {
+        if (!active.current) {
             if (
-                Math.abs(Math.abs(initialX.current - eventX) - Math.abs(xOffset.current)) > moveThreshold ||
-                Math.abs(Math.abs(initialY.current - eventY) - Math.abs(yOffset.current)) > moveThreshold
+                Math.abs(eventX - offsetX.current - currentX.current) > moveThreshold ||
+                Math.abs(eventY - offsetY.current - currentY.current) > moveThreshold
             ) {
-                // dropdown.closeDropdowns();
                 disableScroll();
                 disableSelection();
                 disablePointerEvents();
@@ -127,24 +160,25 @@ export default function useMovable({adjustOnResize} = {}) {
             }
         }
 
-        if (active) {
-            currentX.current = eventX - initialX.current;
-            currentY.current = eventY - initialY.current;
-            xOffset.current = currentX.current;
-            yOffset.current = currentY.current;
+        if (active.current) {
+            let position = {
+                x: eventX - offsetX.current,
+                y: eventY - offsetY.current
+            };
 
-            setTranslate(currentX.current, currentY.current);
+            if (adjustOnDrag) {
+                position = adjustOnDrag(ref.current, {...position, lastSpacing: lastSpacing.current});
+            }
+
+            setPosition(position);
         }
-    }, [moveThreshold, setTranslate, disableScroll, disableSelection, disablePointerEvents]);
+    }, [moveThreshold, setPosition, disableScroll, disableSelection, disablePointerEvents, adjustOnDrag]);
 
     const dragEnd = useCallback((e) => {
         e.preventDefault();
         e.stopPropagation();
 
         active.current = false;
-
-        initialX.current = currentX.current;
-        initialY.current = currentY.current;
 
         window.removeEventListener('touchend', dragEnd, {capture: true, passive: false});
         window.removeEventListener('touchmove', drag, {capture: true, passive: false});
@@ -176,14 +210,15 @@ export default function useMovable({adjustOnResize} = {}) {
 
     const dragStart = useCallback((e) => {
         e.stopPropagation();
+        active.current = false;
 
         if (e.type === 'touchstart' || e.button === 0) {
             if (e.type === 'touchstart') {
-                initialX.current = e.touches[0].clientX - (xOffset.current || 0);
-                initialY.current = e.touches[0].clientY - (yOffset.current || 0);
+                offsetX.current = e.touches[0].clientX - (currentX.current || 0);
+                offsetY.current = e.touches[0].clientY - (currentY.current || 0);
             } else {
-                initialX.current = e.clientX - (xOffset.current || 0);
-                initialY.current = e.clientY - (yOffset.current || 0);
+                offsetX.current = e.clientX - (currentX.current || 0);
+                offsetY.current = e.clientY - (currentY.current || 0);
             }
 
             for (const element of (e.path || e.composedPath())) {
@@ -236,21 +271,16 @@ export default function useMovable({adjustOnResize} = {}) {
                     return;
                 }
 
-                const {x, y} = adjustOnResize(elem, {x: currentX.current, y: currentY.current});
+                const position = adjustOnResize(elem, {x: currentX.current, y: currentY.current, lastSpacing: lastSpacing.current});
 
-                if (x === currentX.current && y === currentY.current) {
-                    return;
+                if (position.x !== currentX.current || position.y !== currentY.current) {
+                    // Adjust offsetX and offsetY to account for the difference in position moved
+                    // This is to make sure we don't jump drag position if the element is resized just after touch start
+                    // Say you start dragging on a button that opens a collapsible section, if the section is resized -> this fixes glitches
+                    offsetX.current = offsetX.current - (position.x - currentX.current);
+                    offsetY.current = offsetY.current - (position.y - currentY.current);
+                    setPosition(position);
                 }
-
-                currentX.current = x;
-                initialX.current = x;
-                xOffset.current = x;
-
-                currentY.current = y;
-                initialY.current = y;
-                yOffset.current = y;
-
-                setTranslate(x, y);
             });
             _resizeObserver.observe(elem);
         }
@@ -265,5 +295,5 @@ export default function useMovable({adjustOnResize} = {}) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    return {ref};
+    return {ref, setPosition, getPosition};
 }
--- koenig/koenig-lexical/src/hooks/useSettingsPanelReposition.js
+++ koenig/koenig-lexical/src/hooks/useSettingsPanelReposition.js
@@ -1,38 +1,90 @@
 import useMovable from './useMovable.js';
 import {debounce} from 'lodash';
-import {useCallback, useLayoutEffect} from 'react';
+import {useCallback, useLayoutEffect, useRef} from 'react';
 
-const CARD_SPACING = 20;
+const CARD_SPACING = 20; // default distance between card and settings panel
 const MIN_RIGHT_SPACING = 20;
-const MIN_TOP_SPACING = 66 + 20; // 66 is publish menu and word count size
+const MIN_TOP_SPACING = 66; // 66 is publish menu and word count size
+const MIN_BOTTOM_SPACING = 20;
+const MIN_LEFT_SPACING = 20;
 
-export default function useSettingsPanelPosition({positionToRef} = {}) {
-    const {ref} = useMovable({adjustOnResize: calculateResizeAdjustment});
+function keepWithinSpacing(panelElem, {x, y, topSpacing, bottomSpacing, rightSpacing, leftSpacing, lastSpacing}) {
+    if (!panelElem) {
+        return {x, y};
+    }
 
-    useLayoutEffect(() => {
-        if (!ref) {
-            return;
-        }
+    // Take previous position into account, and adjust the spacing to allow negative spacing if the previous position was offscreen
+    if (lastSpacing && lastSpacing.top < topSpacing) {
+        topSpacing = lastSpacing.top;
+    }
+    if (lastSpacing && lastSpacing.bottom < bottomSpacing) {
+        bottomSpacing = lastSpacing.bottom;
+    }
+    if (lastSpacing && lastSpacing.right < rightSpacing) {
+        rightSpacing = lastSpacing.right;
+    }
+    if (lastSpacing && lastSpacing.left < leftSpacing) {
+        leftSpacing = lastSpacing.left;
+    }
 
-        positionPanel(ref.current);
+    const width = panelElem.offsetWidth;
+    const height = panelElem.offsetHeight;
 
-        const panelRepositionDebounced = debounce(() => {
-            positionPanel(ref.current);
-        }, 250);
-        window.addEventListener('resize', panelRepositionDebounced);
+    const right = x + width;
+    const bottom = y + height;
 
-        return () => {
-            window.removeEventListener('resize', panelRepositionDebounced);
-        };
-    }, [positionPanel, ref]);
+    const topIsOffscreen = y < topSpacing;
+    const bottomIsOffscreen = window.innerHeight - bottom < bottomSpacing;
+    const rightIsOffscreen = window.innerWidth - right < rightSpacing;
+    const leftIsOffscreen = x < leftSpacing;
+    let yAdjustment = 0;
+    let xAdjustment = 0;
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    function positionPanel(panelElem) {
-        if (!panelElem) {
-            return;
-        }
+    if (topIsOffscreen && !bottomIsOffscreen) {
+        yAdjustment = topSpacing - y;
+    }
 
-        const panelRect = panelElem.getBoundingClientRect();
+    if (bottomIsOffscreen && !topIsOffscreen) {
+        yAdjustment = -(bottomSpacing - (window.innerHeight - bottom));
+    }
+
+    if (rightIsOffscreen) {
+        xAdjustment = -(rightSpacing - (window.innerWidth - right));
+    }
+
+    if (leftIsOffscreen) {
+        xAdjustment = leftSpacing - x;
+    }
+
+    // no adjustment needed
+    return {x: x + xAdjustment, y: y + yAdjustment};
+}
+
+function keepWithinSpacingOnDrag(panelElem, {x, y}) {
+    const width = panelElem.offsetWidth;
+    const height = panelElem.offsetHeight;
+
+    // Make sure at least 40px is still visible
+    const MINIMUM_VISIBLE = 40;
+    const topSpacing = MINIMUM_VISIBLE - height;
+    const bottomSpacing = MINIMUM_VISIBLE - height;
+    const rightSpacing = MINIMUM_VISIBLE - width;
+    const leftSpacing = MINIMUM_VISIBLE - width;
+
+    // Last spacing is ignored
+    return keepWithinSpacing(panelElem, {x, y, topSpacing, bottomSpacing, rightSpacing, leftSpacing, lastSpacing: undefined});
+}
+
+function keepWithinSpacingOnResize(panelElem, {x, y, lastSpacing}) {
+    return keepWithinSpacingOnDrag(panelElem, keepWithinSpacing(panelElem, {x, y, topSpacing: MIN_TOP_SPACING, bottomSpacing: MIN_BOTTOM_SPACING, rightSpacing: MIN_RIGHT_SPACING, leftSpacing: MIN_LEFT_SPACING, lastSpacing}));
+}
+
+export default function useSettingsPanelReposition({positionToRef} = {}) {
+    const {ref, getPosition, setPosition} = useMovable({adjustOnResize: keepWithinSpacingOnResize, adjustOnDrag: keepWithinSpacingOnDrag});
+    const previousViewport = useRef({width: window.innerWidth, height: window.innerHeight});
+
+    const getInitialPosition = useCallback((panelElem) => {
+        const panelHeight = panelElem.offsetHeight;
         const cardElement = positionToRef || document.querySelector('[data-kg-card-editing="true"]');
 
         if (!cardElement) {
@@ -41,59 +93,71 @@ export default function useSettingsPanelPosition({positionToRef} = {}) {
         const containerRect = cardElement.getBoundingClientRect();
 
         // position vertically centered
-        let top;
-        const currentTop = panelElem.style.top;
         // if we already have top set, leave it so that toggling additional settings doesn't cause the panel to jump (unless it would be offscreen)
         const containerMiddle = containerRect.top + (containerRect.height / 2);
-        top = currentTop ? currentTop : Math.max(containerMiddle - (panelRect.height), MIN_TOP_SPACING);
-        // if part of panel would be off screen adjust to keep minimum distance from window top/botom
-        if (top + panelRect.height > window.innerHeight - MIN_TOP_SPACING) {
-            top = window.innerHeight - MIN_TOP_SPACING - panelRect.height;
-        }
+
+        const y = Math.max(containerMiddle - (panelHeight) / 2, MIN_TOP_SPACING);
 
         // position to right of panel
-        let left = containerRect.right + CARD_SPACING;
-        // if part of panel would be off screen adjust to keep minimum distance from window edge
-        if (left + panelRect.width > window.innerWidth - MIN_RIGHT_SPACING) {
-            left = window.innerWidth - panelRect.width - MIN_RIGHT_SPACING;
+        const x = containerRect.right + CARD_SPACING;
+
+        return keepWithinSpacingOnResize(panelElem, {x, y});
+    }, [positionToRef]);
+
+    const onResize = useCallback((panelElem) => {
+        let {x, y, lastSpacing} = getPosition();
+
+        // If the viewport size has increased, move the panel towards the initial position instead of keeping it in the same place
+        // This increases the UX when the viewport is too small and the user resizes or rotates the screen -> it will move towards the preferred position so that it is fully visible
+        if (window.innerHeight > previousViewport.current.height) {
+            const heightIncrease = window.innerHeight - previousViewport.current.height;
+            const initialPosition = getInitialPosition(panelElem);
+            if (initialPosition) {
+                if (initialPosition.y > y) {
+                    y += Math.min(initialPosition.y - y, heightIncrease);
+                }
+            }
         }
 
-        panelElem.style.position = 'fixed';
-        panelElem.style.top = `${top}px`;
-        panelElem.style.left = `${left}px`;
-    }
-
-    function calculateResizeAdjustment(panelElem, {x, y}) {
-        const panelRect = panelElem.getBoundingClientRect();
-
-        const topIsOffscreen = panelRect.top < 0;
-        const bottomIsOffscreen = panelRect.bottom > window.innerHeight;
-
-        if (topIsOffscreen && bottomIsOffscreen) {
-            // there's not much we can do here, the screen is too small.
-            // leave as-is to avoid any weird jumping
-            return {x, y};
+        if (window.innerWidth > previousViewport.current.width) {
+            const widthIncrease = window.innerWidth - previousViewport.current.width;
+            const initialPosition = getInitialPosition(panelElem);
+            if (initialPosition) {
+                if (initialPosition.x > x) {
+                    x += Math.min(initialPosition.x - x, widthIncrease);
+                }
+            }
         }
 
-        if (topIsOffscreen) {
-            const yAdjustment = Math.abs(panelRect.top) + 10;
+        setPosition(keepWithinSpacingOnResize(panelElem, {x, y, lastSpacing}));
 
-            return {x, y: y + yAdjustment};
+        previousViewport.current = {width: window.innerWidth, height: window.innerHeight};
+    }, [getInitialPosition, setPosition, getPosition]);
+
+    useLayoutEffect(() => {
+        if (!ref) {
+            return;
         }
 
-        if (bottomIsOffscreen) {
-            const yAdjustment = -Math.abs(panelRect.bottom - window.innerHeight) - 10;
-            return {x, y: y + yAdjustment};
+        const panelRepositionDebounced = debounce(() => {
+            onResize(ref.current);
+        }, 100, {leading: true, trailing: true});
+        window.addEventListener('resize', panelRepositionDebounced);
+
+        return () => {
+            window.removeEventListener('resize', panelRepositionDebounced);
+        };
+    }, [onResize, ref]);
+
+    useLayoutEffect(() => {
+        if (!ref || !ref.current) {
+            return;
         }
 
-        // no adjustment needed
-        return {x, y};
-    }
-
-    const repositionPanel = useCallback(() => positionPanel(ref.current), []);
+        setPosition(getInitialPosition(ref.current));
+    }, [getInitialPosition, setPosition, ref]);
 
     return {
-        ref,
-        repositionPanel
+        ref
     };
 }
