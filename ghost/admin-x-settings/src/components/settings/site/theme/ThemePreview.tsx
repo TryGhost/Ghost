902fb999a29492c6c54c640c6a02fadf511f92c1
Rishabh <zrishabhgarg@gmail.com>
2023-06-13 22:36:54 +0530

    Updated official themes view in adminX
    refs https://github.com/TryGhost/Team/issues/3432
    
    - updated theme preview urls to use admin assets
    - updated theme selection preview to include full theme info

--- ghost/admin-x-settings/src/components/settings/site/theme/ThemePreview.tsx
+++ ghost/admin-x-settings/src/components/settings/site/theme/ThemePreview.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import {OfficialTheme} from '../../../../models/themes';
 
 const NewThemePreview: React.FC<{
-    selectedTheme?: string;
+    selectedTheme?: OfficialTheme;
 }> = ({
     selectedTheme
 }) => {
+    if (!selectedTheme) {
+        return null;
+    }
     return (
-        <div>
-            Preview {selectedTheme}
+        <div className='flex h-full grow flex-col'>
+            <iframe className='h-full w-full'
+                src={selectedTheme?.previewUrl} title='Theme preview' />
         </div>
     );
 };
--- ghost/admin-x-settings/src/models/themes.ts
+++ ghost/admin-x-settings/src/models/themes.ts
@@ -9,6 +9,15 @@ export type Theme = {
     templates?: string[];
 }
 
+export type OfficialTheme = {
+    name: string;
+    category: string;
+    previewUrl: string;
+    ref: string;
+    image: string;
+    url?: string;
+};
+
 export function isActiveTheme(theme: Theme): boolean {
     return theme.active;
 }
