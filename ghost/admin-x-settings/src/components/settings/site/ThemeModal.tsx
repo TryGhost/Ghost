6e17089fb22d2de30dced6d706901556f24078ca
Peter Zimon <peter.zimon@gmail.com>
2023-06-14 10:56:58 +0200

    Updated theme preview in AdminX
    refs. https://github.com/TryGhost/Team/issues/3432

--- ghost/admin-x-settings/src/components/settings/site/ThemeModal.tsx
+++ ghost/admin-x-settings/src/components/settings/site/ThemeModal.tsx
@@ -1,15 +1,13 @@
 import AdvancedThemeSettings from './theme/AdvancedThemeSettings';
-import Breadcrumbs from '../../../admin-x-ds/global/Breadcrumbs';
 import Button from '../../../admin-x-ds/global/Button';
-import ButtonGroup from '../../../admin-x-ds/global/ButtonGroup';
 import FileUpload from '../../../admin-x-ds/global/form/FileUpload';
 import Modal from '../../../admin-x-ds/global/modal/Modal';
-import NewThemePreview from './theme/ThemePreview';
 import NiceModal, {NiceModalHandler, useModal} from '@ebay/nice-modal-react';
 import OfficialThemes from './theme/OfficialThemes';
 import PageHeader from '../../../admin-x-ds/global/layout/PageHeader';
 import React, {useState} from 'react';
 import TabView from '../../../admin-x-ds/global/TabView';
+import ThemePreview from './theme/ThemePreview';
 import {OfficialTheme} from '../../../models/themes';
 import {Theme} from '../../../types/api';
 import {showToast} from '../../../admin-x-ds/global/Toast';
@@ -23,10 +21,11 @@ interface ThemeToolbarProps {
     modal: NiceModalHandler<Record<string, unknown>>;
     themes: Theme[];
     setThemes: (themes: Theme[]) => void;
+    setPreviewMode: (mode: string) => void;
+    previewMode: string;
 }
 
 interface ThemeModalContentProps {
-    selectedTheme: OfficialTheme|null;
     onSelectTheme: (theme: OfficialTheme|null) => void;
     currentTab: string;
     themes: Theme[];
@@ -34,112 +33,56 @@ interface ThemeModalContentProps {
 }
 
 const ThemeToolbar: React.FC<ThemeToolbarProps> = ({
-    selectedTheme,
     setCurrentTab,
-    setSelectedTheme,
     modal,
     themes,
     setThemes
 }) => {
     const api = useApi();
+    const left =
+        <TabView
+            border={false}
+            tabs={[
+                {id: 'official', title: 'Official themes'},
+                {id: 'installed', title: 'Installed'}
+            ]}
+            onTabChange={(id: string) => {
+                setCurrentTab(id);
+            }} />;
 
-    let left, right;
+    const right =
+        <div className='flex items-center gap-3'>
+            <FileUpload id='theme-uplaod' onUpload={async (file: File) => {
+                const data = await api.themes.upload({file});
+                const uploadedTheme = data.themes[0];
+                setThemes([...themes, uploadedTheme]);
+                showToast({
+                    message: `Theme uploaded - ${uploadedTheme.name}`
+                });
+            }}>Upload theme</FileUpload>
+            <Button
+                className='min-w-[75px]'
+                color='black'
+                label='OK'
+                onClick = {() => {
+                    modal.remove();
+                }} />
+        </div>;
 
-    if (selectedTheme) {
-        const installedTheme = themes.find(theme => theme.name.toLowerCase() === selectedTheme.name.toLowerCase());
-
-        left =
-            <div className='flex items-center gap-2'>
-                <Breadcrumbs
-                    items={[
-                        {label: 'Official themes', onClick: () => {
-                            setCurrentTab('official');
-                            setSelectedTheme(null);
-                        }},
-                        {label: selectedTheme.name}
-                    ]}
-                />
-            </div>;
-
-        right =
-            <div className='flex justify-end gap-8'>
-                <ButtonGroup
-                    buttons={[
-                        {icon: 'laptop', link: true, size: 'sm'},
-                        {icon: 'mobile', iconColorClass: 'text-grey-500', link: true, size: 'sm'}
-                    ]}
-                />
-                <Button
-                    color='green'
-                    disabled={Boolean(installedTheme)}
-                    label={installedTheme?.active ? 'Activated' : (installedTheme ? 'Installed' : `Install ${selectedTheme?.name}`)}
-                    onClick={async () => {
-                        const data = await api.themes.install(selectedTheme.ref);
-                        const newlyInstalledTheme = data.themes[0];
-                        setThemes([
-                            ...themes.map(theme => ({...theme, active: false})),
-                            newlyInstalledTheme
-                        ]);
-                        showToast({
-                            message: `Theme installed - ${newlyInstalledTheme.name}`
-                        });
-                        setCurrentTab('installed');
-                    }}
-                />
-            </div>;
-    } else {
-        left =
-            <TabView
-                border={false}
-                tabs={[
-                    {id: 'official', title: 'Official themes'},
-                    {id: 'installed', title: 'Installed'}
-                ]}
-                onTabChange={(id: string) => {
-                    setCurrentTab(id);
-                }} />;
-
-        right =
-            <div className='flex items-center gap-3'>
-                <FileUpload id='theme-uplaod' onUpload={async (file: File) => {
-                    const data = await api.themes.upload({file});
-                    const uploadedTheme = data.themes[0];
-                    setThemes([...themes, uploadedTheme]);
-                    showToast({
-                        message: `Theme uploaded - ${uploadedTheme.name}`
-                    });
-                }}>Upload theme</FileUpload>
-                <Button
-                    className='min-w-[75px]'
-                    color='black'
-                    label='OK'
-                    onClick = {() => {
-                        modal.remove();
-                    }} />
-            </div>;
-    }
-
-    return <PageHeader containerClassName={selectedTheme! && 'bg-grey-50'} left={left} right={right} />;
+    return <PageHeader containerClassName='bg-white' left={left} right={right} />;
 };
 
 const ThemeModalContent: React.FC<ThemeModalContentProps> = ({
     currentTab,
-    selectedTheme,
     onSelectTheme,
     themes,
     setThemes
 }) => {
     switch (currentTab) {
     case 'official':
-        if (selectedTheme) {
-            return (
-                <NewThemePreview selectedTheme={selectedTheme} />
-            );
-        } else {
-            return (
-                <OfficialThemes onSelectTheme={onSelectTheme} />
-            );
-        }
+        return (
+            <OfficialThemes onSelectTheme={onSelectTheme} />
+        );
     case 'installed':
         return (
             <AdvancedThemeSettings
@@ -154,35 +97,69 @@ const ThemeModalContent: React.FC<ThemeModalContentProps> = ({
 const ChangeThemeModal = NiceModal.create(() => {
     const [currentTab, setCurrentTab] = useState('official');
     const [selectedTheme, setSelectedTheme] = useState<OfficialTheme|null>(null);
+    const [previewMode, setPreviewMode] = useState('desktop');
 
     const modal = useModal();
     const {themes, setThemes} = useThemes();
+    const api = useApi();
 
     const onSelectTheme = (theme: OfficialTheme|null) => {
         setSelectedTheme(theme);
     };
+
+    let installedTheme;
+    let onInstall;
+    if (selectedTheme) {
+        installedTheme = themes.find(theme => theme.name.toLowerCase() === selectedTheme!.name.toLowerCase());
+        onInstall = async () => {
+            const data = await api.themes.install(selectedTheme.ref);
+            const newlyInstalledTheme = data.themes[0];
+            setThemes([
+                ...themes.map(theme => ({...theme, active: false})),
+                newlyInstalledTheme
+            ]);
+            showToast({
+                message: `Theme installed - ${newlyInstalledTheme.name}`
+            });
+            setCurrentTab('installed');
+        };
+    }
 
     return (
         <Modal
             cancelLabel=''
             footer={false}
             noPadding={true}
+            scrolling={currentTab === 'official' ? false : true}
             size='full'
             title=''
         >
             <div className='flex h-full justify-between'>
                 <div className='grow'>
+                    {selectedTheme &&
+                        <ThemePreview
+                            installButtonLabel={
+                                installedTheme?.active ? 'Activated' : (installedTheme ? 'Installed' : `Install ${selectedTheme?.name}`)
+                            }
+                            selectedTheme={selectedTheme}
+                            themeInstalled={Boolean(installedTheme)}
+                            onBack={() => {
+                                setSelectedTheme(null);
+                            }}
+                            onInstall={onInstall} />
+                    }
                     <ThemeToolbar
                         modal={modal}
+                        previewMode={previewMode}
                         selectedTheme={selectedTheme}
                         setCurrentTab={setCurrentTab}
+                        setPreviewMode={setPreviewMode}
                         setSelectedTheme={setSelectedTheme}
                         setThemes={setThemes}
                         themes={themes}
                     />
                     <ThemeModalContent
                         currentTab={currentTab}
-                        selectedTheme={selectedTheme}
                         setThemes={setThemes}
                         themes={themes}
                         onSelectTheme={onSelectTheme}
