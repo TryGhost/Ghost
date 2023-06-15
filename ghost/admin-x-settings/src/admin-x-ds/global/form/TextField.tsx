3ab1d1f223ab8c5aed964b6c9b6fe3b445c7af05
Peter Zimon <peter.zimon@gmail.com>
2023-06-15 20:42:03 +0200

    Refined navigation design in AdminX
    refs. https://github.com/TryGhost/Team/issues/3432

--- ghost/admin-x-settings/src/admin-x-ds/global/form/TextField.tsx
+++ ghost/admin-x-settings/src/admin-x-ds/global/form/TextField.tsx
@@ -17,6 +17,8 @@ interface TextFieldProps {
     onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
     className?: string;
     maxLength?: number;
+    containerClassName?: string;
+    unstyled?: boolean;
 }
 
 const TextField: React.FC<TextFieldProps> = ({
@@ -33,33 +35,43 @@ const TextField: React.FC<TextFieldProps> = ({
     onBlur,
     className = '',
     maxLength,
+    containerClassName = '',
+    unstyled = false,
     ...props
 }) => {
     const id = useId();
 
-    return (
-        <div className='flex flex-col'>
-            {title && <Heading className={hideTitle ? 'sr-only' : ''} grey={value ? true : false} htmlFor={id} useLabelTag={true}>{title}</Heading>}
-            <input
-                ref={inputRef}
-                className={clsx(
-                    'h-10 border-b py-2',
-                    clearBg ? 'bg-transparent' : 'bg-grey-75 px-[10px]',
-                    error ? `border-red` : `border-grey-500 hover:border-grey-700 focus:border-black`,
-                    (title && !hideTitle && !clearBg) && `mt-2`,
-                    className
-                )}
-                id={id}
-                maxLength={maxLength}
-                placeholder={placeholder}
-                type={type}
-                value={value}
-                onBlur={onBlur}
-                onChange={onChange}
-                {...props} />
-            {hint && <Hint color={error ? 'red' : ''}>{hint}</Hint>}
-        </div>
+    const textFieldClasses = !unstyled && clsx(
+        'h-10 border-b py-2',
+        clearBg ? 'bg-transparent' : 'bg-grey-75 px-[10px]',
+        error ? `border-red` : `border-grey-500 hover:border-grey-700 focus:border-black`,
+        (title && !hideTitle && !clearBg) && `mt-2`,
+        className
     );
+
+    const field = <input
+        ref={inputRef}
+        className={textFieldClasses || className}
+        id={id}
+        maxLength={maxLength}
+        placeholder={placeholder}
+        type={type}
+        value={value}
+        onBlur={onBlur}
+        onChange={onChange}
+        {...props} />;
+
+    if (title || hint) {
+        return (
+            <div className={`flex flex-col ${containerClassName}`}>
+                {title && <Heading className={hideTitle ? 'sr-only' : ''} grey={value ? true : false} htmlFor={id} useLabelTag={true}>{title}</Heading>}
+                {field}
+                {hint && <Hint color={error ? 'red' : ''}>{hint}</Hint>}
+            </div>
+        );
+    } else {
+        return field;
+    }
 };
 
 export default TextField;
