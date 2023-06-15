9ce4c8590538da81e2ba1c9ef452c1f48be603d2
Michael Barrett <991592+mike182uk@users.noreply.github.com>
2023-06-15 15:10:56 +0100

    ✨ Added custom theme setting descriptions (#17024)
    refs https://github.com/TryGhost/Team/issues/3448
    
    Added the ability to specify a description for a custom theme setting
    
    ---------
    
    Co-authored-by: Sodbileg Gansukh <sodbileg.gansukh@gmail.com>

--- ghost/admin/app/models/custom-theme-setting.js
+++ ghost/admin/app/models/custom-theme-setting.js
@@ -3,6 +3,7 @@ import Model, {attr} from '@ember-data/model';
 export default Model.extend({
     key: attr('string'),
     type: attr('string'),
+    description: attr('string'),
     options: attr(),
     default: attr('string'),
     value: attr(),
