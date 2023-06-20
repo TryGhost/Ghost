2e4e66a01634075997f1b1a98bcf4242b4027a70
Niklas Buchfink <59048346+NiklasBuchfink@users.noreply.github.com>
2023-06-20 17:14:15 +0200

    Update inlang.config.js

--- inlang.config.js
+++ inlang.config.js
@@ -2,8 +2,8 @@
  * @type { import("@inlang/core/config").DefineConfig }
  */
 export async function defineConfig(env) {
-    const { default: jsonPlugin } = await env.$import(
-        "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@latest/dist/index.js"
+    const { default: i18nextPlugin } = await env.$import(
+        "https://cdn.jsdelivr.net/npm/@inlang/plugin-i18next@2/dist/index.js"
     );
 
     const { default: standardLintRules } = await env.$import(
@@ -13,7 +13,7 @@ export async function defineConfig(env) {
     return {
         referenceLanguage: "en",
         plugins: [
-            jsonPlugin({
+            i18nextPlugin({
                 pathPattern: "ghost/i18n/locales/{language}/*.json",
                 variableReferencePattern: ["{{", "}}"],
                 ignore: ["context.json"]
