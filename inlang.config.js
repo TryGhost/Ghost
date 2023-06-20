15133f875323c89f25d4bd5af6d3dffe3ca8137c
Niklas Buchfink <59048346+NiklasBuchfink@users.noreply.github.com>
2023-06-20 16:15:12 +0200

    Update inlang.config.js

--- inlang.config.js
+++ inlang.config.js
@@ -11,7 +11,7 @@ export async function defineConfig(env) {
     );
 
     return {
-        referenceLanguage: "en",
+        referenceLanguage: "de",
         plugins: [
             jsonPlugin({
                 pathPattern: "ghost/i18n/locales/{language}/*.json",
