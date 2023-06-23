9825556632421b3c9eb14dfa601e15bd671866b1
Naz <hi@nazavo.com>
2023-06-23 17:32:09 +0700

    Reduced the amount of events Collections listens on
    refs https://github.com/TryGhost/Team/issues/3169
    
    - When a not yet mapped event is produced by Ghost model the posts belonging to a collection are recalculated in a brute-force way. This causes a lot of strain on the database and times out some of the tests. Right now we only care about a handful of events, so limited the ones we listen to and will be uncommenting the rest when we have an optimal mapping/handling for them.

--- ghost/core/core/server/services/collections/model-to-domain-events-bridge.js
+++ ghost/core/core/server/services/collections/model-to-domain-events-bridge.js
@@ -34,17 +34,17 @@ const translateModelEventsToDomainEvents = () => {
         'post.deleted',
         'post.edited',
 
-        'tag.added',
-        'tag.edited',
-        'tag.attached',
-        'tag.detached',
-        'tag.deleted',
+        // 'tag.added',
+        // 'tag.edited',
+        // 'tag.attached',
+        // 'tag.detached',
+        // 'tag.deleted',
 
-        'user.activated',
-        'user.activated.edited',
-        'user.attached',
-        'user.detached',
-        'user.deleted'
+        // 'user.activated',
+        'user.activated.edited'
+        // 'user.attached',
+        // 'user.detached',
+        // 'user.deleted'
     ];
 
     for (const modelEvent of ghostModelUpdateEvents) {
