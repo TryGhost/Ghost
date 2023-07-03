8f1ab33edd881b99027eb35bc056e3d757eb39cc
Peter Zimon <peter.zimon@gmail.com>
2023-07-03 16:03:25 +0200

    Added tabs to preview

--- apps/admin-x-settings/src/components/settings/membership/PortalModal.tsx
+++ apps/admin-x-settings/src/components/settings/membership/PortalModal.tsx
@@ -1,6 +1,7 @@
 import AccountPage from './portal/AccountPage';
 import LookAndFeel from './portal/LookAndFeel';
 import NiceModal from '@ebay/nice-modal-react';
+import PortalPreview from './portal/PortalPreview';
 import React, {useState} from 'react';
 import SignupOptions from './portal/SignupOptions';
 import TabView, {Tab} from '../../../admin-x-ds/global/TabView';
@@ -39,12 +40,29 @@ const Sidebar: React.FC = () => {
 };
 
 const PortalModal: React.FC = () => {
+    const [selectedPreviewTab, setSelectedPreviewTab] = useState('signup');
+
+    const onSelectURL = (id: string) => {
+        setSelectedPreviewTab(id);
+    };
+
     const sidebar = <Sidebar />;
+    const preview = <PortalPreview selectedTab={selectedPreviewTab} />;
+
+    let previewTabs: Tab[] = [
+        {id: 'signup', title: 'Signup'},
+        {id: 'account', title: 'Account page'},
+        {id: 'links', title: 'Links'}
+    ];
 
     return <PreviewModalContent
-        preview={<>Preview</>}
+        preview={preview}
+        previewToolbarTabs={previewTabs}
+        selectedURL={selectedPreviewTab}
         sidebar={sidebar}
         testId='portal-modal'
+        title='Portal'
+        onSelectURL={onSelectURL}
     />;
 };
 
