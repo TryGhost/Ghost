ace3e7f53811c8a08bfa8266296d8a7b17f4cc9f
Daniel Lockyer <hi@daniellockyer.com>
2023-07-04 15:14:00 +0200

    Enabled parallel testing for Playwright tests of apps
    - we should allow parallel test execution because it's faster than
      serial

--- apps/admin-x-settings/playwright.config.ts
+++ apps/admin-x-settings/playwright.config.ts
@@ -13,8 +13,8 @@ export default defineConfig({
     forbidOnly: !!process.env.CI,
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
-    /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    /* Hardcode to use all cores in CI */
+    workers: process.env.CI ? '100%' : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
