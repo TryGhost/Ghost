fd6fa56b8850772ea040ab7585f416224aacedf7
Djordje Vlaisavljevic <dzvlais@gmail.com>
2023-07-11 22:31:12 +0100

    Fixed DesktopChrome overflow issue
    refs https://github.com/TryGhost/Product/issues/3349

--- apps/admin-x-settings/src/admin-x-ds/global/chrome/DesktopChrome.tsx
+++ apps/admin-x-settings/src/admin-x-ds/global/chrome/DesktopChrome.tsx
@@ -6,7 +6,7 @@ interface DesktopChromeProps {
 
 const DesktopChrome: React.FC<DesktopChromeProps & React.HTMLAttributes<HTMLDivElement>> = ({children, ...props}) => {
     return (
-        <div className='flex h-full w-full flex-col overflow-hidden px-5 pb-5' {...props}>
+        <div className='flex h-full w-full flex-col px-5 pb-5' {...props}>
             <div className="h-full w-full overflow-hidden rounded-[4px] shadow-xl">
                 {children}
             </div>
