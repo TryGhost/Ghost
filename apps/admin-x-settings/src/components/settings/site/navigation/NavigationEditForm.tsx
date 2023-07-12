a725f9d6c2fda35d61a74dc8d7a2cd9268454703
Peter Zimon <zimo@ghost.org>
2023-07-12 16:51:14 +0200

    Admin X Tiers settings refinements (#17322)
    refs. https://github.com/TryGhost/Product/issues/3580
    
    Soratable list needed stylistic updates and simplifications both in navigation and benefits list.

--- apps/admin-x-settings/src/components/settings/site/navigation/NavigationEditForm.tsx
+++ apps/admin-x-settings/src/components/settings/site/navigation/NavigationEditForm.tsx
@@ -8,17 +8,17 @@ const NavigationEditForm: React.FC<{
     baseUrl: string;
     navigation: NavigationEditor;
 }> = ({baseUrl, navigation}) => {
-    return <div className="w-full">
+    return <div className="w-full pt-2">
         <SortableList
             items={navigation.items}
+            itemSeparator={false}
             renderItem={item => (
                 <NavigationItemEditor
-                    action={<Button className='mr-2' icon="trash" size='sm' onClick={() => navigation.removeItem(item.id)} />}
+                    action={<Button className='self-center' icon="trash" size='sm' onClick={() => navigation.removeItem(item.id)} />}
                     baseUrl={baseUrl}
                     clearError={key => navigation.clearError(item.id, key)}
                     item={item}
                     updateItem={updates => navigation.updateItem(item.id, updates)}
-                    unstyled
                 />
             )}
             onMove={navigation.moveItem}
@@ -26,12 +26,11 @@ const NavigationEditForm: React.FC<{
         <NavigationItemEditor
             action={<Button className='self-center' color='green' data-testid="add-button" icon="add" iconColorClass='text-white' size='sm' onClick={navigation.addItem} />}
             baseUrl={baseUrl}
-            className="p-2 pl-9"
+            className="mt-1 pl-7"
             clearError={key => navigation.clearError(navigation.newItem.id, key)}
             data-testid="new-navigation-item"
             item={navigation.newItem}
             labelPlaceholder="New item label"
-            textFieldClasses="w-full ml-2"
             updateItem={navigation.setNewItem}
         />
     </div>;
