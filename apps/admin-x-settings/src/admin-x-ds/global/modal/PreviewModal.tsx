624a01014b2a2f8e2ca994aaabdc6e85be07a22f
Djordje Vlaisavljevic <dzvlais@gmail.com>
2023-07-12 15:58:14 +0100

    Fixed value-never-read error
    refs https://github.com/TryGhost/Product/issues/3349

--- apps/admin-x-settings/src/admin-x-ds/global/modal/PreviewModal.tsx
+++ apps/admin-x-settings/src/admin-x-ds/global/modal/PreviewModal.tsx
@@ -147,14 +147,12 @@ export const PreviewModalContent: React.FC<PreviewModalProps> = ({
         preview = (
             <div className={`min-w-100 absolute inset-y-0 left-0 right-[400px] flex grow flex-col overflow-y-scroll ${previewBgColor === 'grey' ? 'bg-grey-50' : 'bg-white'}`}>
                 {previewToolbar && <header className="relative flex h-[74px] shrink-0 items-center justify-center px-3 py-5">
-                    <div className='absolute left-5 flex h-full items-center'>
-                        <section>
-                            {toolbarLeft}
-                        </section>
-                    </div>
-                    <div className='absolute right-5 flex h-full items-center'>
+                    {leftToolbar && <div className='absolute left-5 flex h-full items-center'>
+                        {toolbarLeft}
+                    </div>}
+                    {rightToolbar && <div className='absolute right-5 flex h-full items-center'>
                         {toolbarRight}
-                    </div>
+                    </div>}
                 </header>}
                 <div className='flex h-full grow items-center justify-center text-sm text-grey-400'>
                     {preview}
