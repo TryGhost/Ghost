57a851227cad6e30c400636daf3bed517078de75
Jono M <reason.koan@gmail.com>
2023-07-12 15:21:54 +1200

    Added AdminX navigation settings components to design system (#17312)
    refs https://github.com/TryGhost/Product/issues/3349
    
    - Moved URLTextField to AdminX design system
    - Factored out sortable list into a design system component

--- apps/admin-x-settings/src/components/settings/site/navigation/NavigationItemEditor.tsx
+++ apps/admin-x-settings/src/components/settings/site/navigation/NavigationItemEditor.tsx
@@ -1,7 +1,7 @@
-import Icon from '../../../../admin-x-ds/global/Icon';
-import React, {ReactNode, forwardRef} from 'react';
+import React, {ReactNode} from 'react';
 import TextField from '../../../../admin-x-ds/global/form/TextField';
-import UrlTextField from './UrlTextField';
+import URLTextField from '../../../../admin-x-ds/global/form/URLTextField';
+import clsx from 'clsx';
 import {EditableItem, NavigationItem, NavigationItemErrors} from '../../../../hooks/site/useNavigationEditor';
 
 export type NavigationItemEditorProps = React.HTMLAttributes<HTMLDivElement> & {
@@ -9,23 +9,16 @@ export type NavigationItemEditorProps = React.HTMLAttributes<HTMLDivElement> & {
     item: EditableItem;
     clearError?: (key: keyof NavigationItemErrors) => void;
     updateItem?: (item: Partial<NavigationItem>) => void;
-    onDelete?: () => void;
-    dragHandleProps?: React.ButtonHTMLAttributes<HTMLButtonElement>;
     labelPlaceholder?: string
     unstyled?: boolean
-    containerClasses?: string
-    dragHandleClasses?: string
     textFieldClasses?: string
     action?: ReactNode
 }
 
-const NavigationItemEditor = forwardRef<HTMLDivElement, NavigationItemEditorProps>(function NavigationItemEditor({baseUrl, item, updateItem, onDelete, clearError, dragHandleProps, labelPlaceholder, unstyled, containerClasses, dragHandleClasses, textFieldClasses, action, ...props}, ref) {
+const NavigationItemEditor: React.FC<NavigationItemEditorProps> = ({baseUrl, item, updateItem, clearError, labelPlaceholder, unstyled, textFieldClasses, action, className, ...props}) => {
     return (
-        <div ref={ref} className={containerClasses} data-testid='navigation-item-editor' {...props}>
-            <button className={dragHandleClasses} type='button' {...dragHandleProps}>
-                <Icon colorClass='text-grey-500' name='hamburger' size='sm' />
-            </button>
-            <div className="flex flex-1">
+        <div className={clsx('flex w-full items-start gap-3', className)} data-testid='navigation-item-editor' {...props}>
+            <div className="flex flex-1 pt-1">
                 <TextField
                     className={textFieldClasses}
                     containerClassName="w-full"
@@ -41,8 +34,8 @@ const NavigationItemEditor = forwardRef<HTMLDivElement, NavigationItemEditorProp
                     onKeyDown={() => clearError?.('label')}
                 />
             </div>
-            <div className="flex flex-1">
-                <UrlTextField
+            <div className="flex flex-1 pt-1">
+                <URLTextField
                     baseUrl={baseUrl}
                     className={textFieldClasses}
                     containerClassName="w-full"
@@ -60,6 +53,6 @@ const NavigationItemEditor = forwardRef<HTMLDivElement, NavigationItemEditorProp
             {action}
         </div>
     );
-});
+};
 
 export default NavigationItemEditor;
