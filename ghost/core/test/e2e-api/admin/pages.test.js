c3fb46cda7eba4f7a20f2168dc8f268f2386478d
Kevin Ansfield <kevin@ghost.org>
2023-07-17 15:21:00 +0200

    Fixed pages API test snapshot having a flaky cache string (#17391)
    no issue
    
    - `x-cache-invalidate` will vary across different runs as the post's preview uuid will be different each time the post is created

--- ghost/core/test/e2e-api/admin/pages.test.js
+++ ghost/core/test/e2e-api/admin/pages.test.js
@@ -73,7 +73,8 @@ describe('Pages API', function () {
                 })
                 .matchHeaderSnapshot({
                     'content-version': anyContentVersion,
-                    etag: anyEtag
+                    etag: anyEtag,
+                    'x-cache-invalidate': anyString
                 });
         });
     });
