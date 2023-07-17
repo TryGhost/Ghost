f661f6d0fc59ed11bdb127f793903db793ea3cae
Sanne de Vries <sannedv@protonmail.com>
2023-07-17 10:04:10 +0200

    Updated card menu icon for collection card
    Closes https://github.com/TryGhost/Product/issues/3576

--- koenig/koenig-lexical/src/nodes/CollectionNode.jsx
+++ koenig/koenig-lexical/src/nodes/CollectionNode.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import cleanBasicHtml from '@tryghost/kg-clean-basic-html';
 import {$generateHtmlFromNodes} from '@lexical/html';
 import {CollectionNode as BaseCollectionNode} from '@tryghost/kg-default-nodes';
-import {ReactComponent as CollectionCardIcon} from '../assets/icons/kg-card-type-gallery.svg';
+import {ReactComponent as CollectionCardIcon} from '../assets/icons/kg-card-type-collection.svg';
 import {CollectionNodeComponent} from './CollectionNodeComponent';
 import {KoenigCardWrapper, MINIMAL_NODES} from '../index.js';
 import {createCommand} from 'lexical';
