e1460141c6a1a1480625401e743d812f3874edeb
Peter Zimon <peter.zimon@gmail.com>
2023-07-26 17:08:15 +0200

    Fixed buttoncolor bugs

--- apps/admin-x-settings/src/admin-x-ds/global/modal/PreviewModal.tsx
+++ apps/admin-x-settings/src/admin-x-ds/global/modal/PreviewModal.tsx
@@ -9,7 +9,7 @@ import Select, {SelectOption} from '../form/Select';
 import TabView, {Tab} from '../TabView';
 import clsx from 'clsx';
 import useGlobalDirtyState from '../../../hooks/useGlobalDirtyState';
-import {ButtonProps} from '../Button';
+import {ButtonColor, ButtonProps} from '../Button';
 import {confirmIfDirty} from '../../../utils/modals';
 
 export interface PreviewModalProps {
@@ -21,7 +21,7 @@ export interface PreviewModalProps {
     dirty?: boolean
     cancelLabel?: string;
     okLabel?: string;
-    okColor?: string;
+    okColor?: ButtonColor;
     buttonsDisabled?: boolean
     previewToolbar?: boolean;
     leftToolbar?: boolean;
