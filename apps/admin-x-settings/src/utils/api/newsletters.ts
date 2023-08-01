a54ad1609254578b4e13503b174390149462d18e
Jono Mingard <reason.koan@gmail.com>
2023-08-01 11:38:50 +0100

    Added newsletter APIs

---
+++ apps/admin-x-settings/src/utils/api/newsletters.ts
@@ -0,0 +1,46 @@
+import { Meta, createMutation, createQuery } from '../apiRequests';
+import { Newsletter } from '../../types/api';
+
+export interface NewslettersResponseType {
+    meta?: Meta
+    newsletters: Newsletter[]
+}
+
+const dataType = 'NewslettersResponseType';
+
+export const useBrowseNewsletters = createQuery<NewslettersResponseType>({
+    dataType,
+    path: '/newsletters/',
+    defaultSearchParams: {
+        limit: 'all'
+    }
+});
+
+export const useAddNewsletter = createMutation<NewslettersResponseType, Partial<Newsletter>>({
+    method: 'POST',
+    path: () => '/newsletters/',
+    body: newsletter => ({newsletters: [newsletter]}),
+    updateQueries: {
+        dataType,
+        update: (newData, currentData) => ({
+            ...(currentData as NewslettersResponseType),
+            newsletters: (currentData as NewslettersResponseType).newsletters.concat(newData.newsletters)
+        })
+    }
+});
+
+export const useEditNewsletter = createMutation<NewslettersResponseType, Newsletter>({
+    method: 'PUT',
+    path: newletter => `/newletters/${newletter.id}/`,
+    body: newletter => ({newletters: [newletter]}),
+    updateQueries: {
+        dataType,
+        update: (newData, currentData) => ({
+            ...(currentData as NewslettersResponseType),
+            newletters: (currentData as NewslettersResponseType).newsletters.map((newsletter) => {
+                const newNewsletter = newData.newsletters.find(({id}) => id === newsletter.id);
+                return newNewsletter || newsletter;
+            })
+        })
+    }
+});
