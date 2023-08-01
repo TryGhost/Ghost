d42200d252093a1f649fe6a2dfede145d7f4d86c
Jono M <reason.koan@gmail.com>
2023-08-01 18:24:23 +0100

    Wired up AdminX newsletter list and editing (#17556)
    refs https://github.com/TryGhost/Product/issues/3601

---
+++ apps/admin-x-settings/src/utils/api/newsletters.ts
@@ -0,0 +1,45 @@
+import { Meta, createMutation, createQuery } from '../apiRequests';
+import { Newsletter } from '../../types/api';
+
+export interface NewslettersResponseType {
+    meta?: Meta
+    newsletters: Newsletter[]
+}
+
+const dataType = 'NewslettersResponseType';
+
+export const useBrowseNewsletters = createQuery<NewslettersResponseType>({
+    dataType,
+    path: '/newsletters/',
+    defaultSearchParams: {include: 'count.active_members,count.posts', limit: 'all'}
+});
+
+export const useAddNewsletter = createMutation<NewslettersResponseType, Partial<Newsletter>>({
+    method: 'POST',
+    path: () => '/newsletters/',
+    body: newsletter => ({newsletters: [newsletter]}),
+    updateQueries: {
+        dataType,
+        update: (newData, currentData) => ({
+            ...(currentData as NewslettersResponseType),
+            newsletters: (currentData as NewslettersResponseType).newsletters.concat(newData.newsletters)
+        })
+    }
+});
+
+export const useEditNewsletter = createMutation<NewslettersResponseType, Newsletter>({
+    method: 'PUT',
+    path: newsletter => `/newsletters/${newsletter.id}/`,
+    body: newsletter => ({newsletters: [newsletter]}),
+    defaultSearchParams: {include: 'count.active_members,count.posts', limit: 'all'},
+    updateQueries: {
+        dataType,
+        update: (newData, currentData) => ({
+            ...(currentData as NewslettersResponseType),
+            newsletters: (currentData as NewslettersResponseType).newsletters.map((newsletter) => {
+                const newNewsletter = newData.newsletters.find(({id}) => id === newsletter.id);
+                return newNewsletter || newsletter;
+            })
+        })
+    }
+});
