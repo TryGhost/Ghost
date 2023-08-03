e12530ac6af0458bcb0637d59e529c82e4c0c81b
Djordje Vlaisavljevic <dzvlais@gmail.com>
2023-08-03 21:27:17 +0100

    Moved Tips&Donations to its own component
    refs https://github.com/TryGhost/Product/issues/3667

--- ghost/admin/app/components/settings/signup-form-embed.js
+++ ghost/admin/app/components/settings/signup-form-embed.js
@@ -2,24 +2,13 @@ import Component from '@glimmer/component';
 import SignupFormEmbedModal from '../../components/modals/settings/signup-form-embed';
 import {action} from '@ember/object';
 import {inject as service} from '@ember/service';
-import {task, timeout} from 'ember-concurrency';
-import {tracked} from '@glimmer/tracking';
 
 export default class SignupFormEmbed extends Component {
     @service modals;
     @service settings;
 
-    @tracked currency = 'USD';
-    @tracked allCurrencies = ['USD', 'RSD'];
-
     @action
     open() {
         this.modals.open(SignupFormEmbedModal, {}, {});
-    }
-
-    @task
-    *copyTipsAndDonationsLink() {
-        yield timeout(10);
-        return true;
     }
 }
