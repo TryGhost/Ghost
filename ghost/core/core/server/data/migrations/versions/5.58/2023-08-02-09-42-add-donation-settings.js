257a25335f7d7758f5e764714df22aa3c33a67a8
Sag <guptazy@gmail.com>
2023-08-03 20:25:50 +0200

    Removed donations_enabled settings
    refs https://github.com/TryGhost/Product/issues/3668
    - following a discussion with peter, we decided to not introduced a toggle for donations on/off. Instead, donations will be automatically enabled if stripe is connected, disabled otherwise

--- ghost/core/core/server/data/migrations/versions/5.58/2023-08-02-09-42-add-donation-settings.js
+++ ghost/core/core/server/data/migrations/versions/5.58/2023-08-02-09-42-add-donation-settings.js
@@ -2,13 +2,6 @@ const {combineTransactionalMigrations, addSetting} = require('../../utils');
 
 module.exports = combineTransactionalMigrations(
     addSetting({
-        key: 'donations_enabled',
-        value: false,
-        type: 'boolean',
-        group: 'donations',
-        flags: 'PUBLIC'
-    }),
-    addSetting({
         key: 'donations_currency',
         value: 'USD',
         type: 'string',
