671240d53ddb427615417cfe6b3f5f0dcc8067d9
Peter Zimon <peter.zimon@gmail.com>
2023-08-03 15:00:32 +0200

    Hide newsletter settings if disabled

--- apps/admin-x-settings/src/components/settings/email/EmailSettings.tsx
+++ apps/admin-x-settings/src/components/settings/email/EmailSettings.tsx
@@ -4,6 +4,8 @@ import MailGun from './Mailgun';
 import Newsletters from './Newsletters';
 import React from 'react';
 import SettingSection from '../../../admin-x-ds/settings/SettingSection';
+import useSettings from '../../../hooks/useSettings';
+import {getSettingValues} from '../../../utils/helpers';
 
 const searchKeywords = {
     enableNewsletters: ['newsletter', 'enable', 'disable', 'turn on'],
@@ -13,12 +15,19 @@ const searchKeywords = {
 };
 
 const EmailSettings: React.FC = () => {
+    const {settings} = useSettings();
+    const [newslettersEnabled] = getSettingValues(settings, ['editor_default_email_recipients']) as [string];
+
     return (
         <SettingSection keywords={Object.values(searchKeywords).flat()} title='Email newsletters'>
             <EnableNewsletters keywords={searchKeywords.enableNewsletters} />
-            <Newsletters keywords={searchKeywords.newsletters} />
-            <DefaultRecipients keywords={searchKeywords.defaultRecipients} />
-            <MailGun keywords={searchKeywords.mailgun} />
+            {newslettersEnabled !== 'disabled' && (
+                <>
+                    <Newsletters keywords={searchKeywords.newsletters} />
+                    <DefaultRecipients keywords={searchKeywords.defaultRecipients} />
+                    <MailGun keywords={searchKeywords.mailgun} />
+                </>
+            )}
         </SettingSection>
     );
 };
