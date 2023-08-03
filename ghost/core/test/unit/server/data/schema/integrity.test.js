bb9d213d3ff2063a5fe7a1b5fa2ad445fd17233d
Sag <guptazy@gmail.com>
2023-08-03 22:13:47 +0200

    Added migrations for Tips & Donations' settings (#17576)
    closes https://github.com/TryGhost/Product/issues/3668
    
    - Tips and Donations feature offers two settings: "donations_currency", and "donations_suggested_amount"
        - "donation_currency": the currency to be used for the donation. Defaults to "USD", not nullable.
        - "donation_suggested_amount": an anchor price for the donation. Defaults to 0, not nullable.
    - Both settings belong to a new group "donations"
    
    Tech Spec: https://www.notion.so/ghost/Tech-Spec-5cd6929f7960462ebcbf198176e0d899?pvs=4#6e8b34c45f0c4c78b48c9e7725a307c8

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -37,7 +37,7 @@ describe('DB version integrity', function () {
     // Only these variables should need updating
     const currentSchemaHash = '99a8fe2394b685cc1ce4c44d8e87a1ad';
     const currentFixturesHash = 'af43eef1ac4f14fc1bc0ea351300420f';
-    const currentSettingsHash = '4f23a583335dcb4cb3fae553122ea200';
+    const currentSettingsHash = 'dd0e318627ded65e41f188fb5bdf5b74';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
 
     // If this test is failing, then it is likely a change has been made that requires a DB version bump,
