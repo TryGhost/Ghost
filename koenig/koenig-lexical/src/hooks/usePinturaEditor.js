9a3da64c303da3efbf01730ed0c95b9f0b732c2a
Nick Moreton <nick@ghost.org>
2023-08-03 18:17:42 +0100

    Removed double declaration of trackEvent

--- koenig/koenig-lexical/src/hooks/usePinturaEditor.js
+++ koenig/koenig-lexical/src/hooks/usePinturaEditor.js
@@ -1,6 +1,5 @@
 import trackEvent from '../utils/analytics';
 import {useCallback, useEffect, useState} from 'react';
-import trackEvent from '../utils/analytics';
 
 export default function usePinturaEditor({
     config, disabled = false
