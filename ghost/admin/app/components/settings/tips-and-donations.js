071a68d51cac3dfec2633c72e57f0b62ae338a1d
Sag <guptazy@gmail.com>
2023-08-04 14:52:45 +0200

    Added logic to manage Tips & Donations settings
    refs https://github.com/TryGhost/Product/issues/3666

--- ghost/admin/app/components/settings/tips-and-donations.js
+++ ghost/admin/app/components/settings/tips-and-donations.js
@@ -1,17 +1,59 @@
 import Component from '@glimmer/component';
+import copyTextToClipboard from 'ghost-admin/utils/copy-text-to-clipboard';
+import {action} from '@ember/object';
+import {currencies} from 'ghost-admin/utils/currency';
+import {inject} from 'ghost-admin/decorators/inject';
 import {inject as service} from '@ember/service';
 import {task, timeout} from 'ember-concurrency';
 import {tracked} from '@glimmer/tracking';
 
+const CURRENCIES = currencies.map((currency) => {
+    return {
+        value: currency.isoCode,
+        label: `${currency.isoCode}`,
+        isoCode: currency.isoCode
+    };
+});
+
 export default class TipsAndDonations extends Component {
     @service settings;
 
-    @tracked currency = 'USD';
-    @tracked allCurrencies = ['USD', 'RSD'];
+    @inject config;
+
+    @tracked donationsCurrency;
+    @tracked donationsSuggestedAmount;
+
+    get allCurrencies() {
+        return CURRENCIES;
+    }
+
+    get selectedCurrency() {
+        return CURRENCIES.findBy('value', this.settings.donationsCurrency);
+    }
+
+    get siteUrl() {
+        return this.config.blogUrl;
+    }
 
     @task
     *copyTipsAndDonationsLink() {
-        yield timeout(10);
+        const link = document.getElementById('gh-tips-and-donations-link')?.value;
+
+        if (link) {
+            copyTextToClipboard(link);
+            yield timeout(this.isTesting ? 50 : 500);
+        }
+
         return true;
+    }
+
+    @action
+    setDonationsCurrency(event) {
+        this.settings.donationsCurrency = event.value;
+    }
+
+    @action
+    setDonationsSuggestedAmount(event) {
+        this.settings.donationsSuggestedAmount = Number(event.target.value);
     }
 }
