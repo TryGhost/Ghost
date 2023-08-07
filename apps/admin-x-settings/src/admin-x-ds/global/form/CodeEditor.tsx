12581442f5cf8f98f236526aae52d94ded41aad0
Jono Mingard <reason.koan@gmail.com>
2023-08-07 19:00:15 +0100

    Wired up header and footer code editing

--- apps/admin-x-settings/src/admin-x-ds/global/form/CodeEditor.tsx
+++ apps/admin-x-settings/src/admin-x-ds/global/form/CodeEditor.tsx
@@ -1,11 +1,12 @@
-import CodeMirror from '@uiw/react-codemirror';
+import CodeMirror, {ReactCodeMirrorProps, ReactCodeMirrorRef} from '@uiw/react-codemirror';
 import Heading from '../Heading';
 import Hint from '../Hint';
-import React, {useId} from 'react';
+import React, {forwardRef, useId} from 'react';
 import clsx from 'clsx';
+import {EditorView} from '@codemirror/view';
 import {Extension} from '@codemirror/state';
 
-interface CodeEditorProps {
+export interface CodeEditorProps extends Omit<ReactCodeMirrorProps, 'value' | 'onChange'> {
     title?: string;
     value?: string;
     height?: string;
@@ -16,7 +17,17 @@ interface CodeEditorProps {
     onChange?: (value: string) => void;
 }
 
-const CodeEditor: React.FC<CodeEditorProps> = ({
+const theme = EditorView.theme({
+    '& .cm-scroller': {
+        fontFamily: 'Consolas, Liberation Mono, Menlo, Courier, monospace'
+    },
+
+    '& .cm-activeLine, & .cm-activeLineGutter': {
+        backgroundColor: 'transparent'
+    }
+});
+
+const CodeEditor = forwardRef<ReactCodeMirrorRef, CodeEditorProps>(function CodeEditor({
     title,
     value,
     height = '200px',
@@ -24,30 +35,35 @@ const CodeEditor: React.FC<CodeEditorProps> = ({
     hint,
     clearBg = true,
     extensions,
-    onChange
-}) => {
+    onChange,
+    ...props
+}, ref) {
     const id = useId();
 
     let styles = clsx(
-        'peer order-2 rounded-sm border',
+        'peer order-2 overflow-hidden rounded-sm border',
         clearBg ? 'bg-transparent' : 'bg-grey-75',
         error ? 'border-red' : 'border-grey-500 hover:border-grey-700 focus:border-grey-800',
-        title && 'mt-2'
+        title && 'mt-2',
+        height === 'full' && 'h-full'
     );
 
     return (
-        <div className='flex flex-col'>
+        <div className={height === 'full' ? 'h-full' : ''}>
             <CodeMirror
+                ref={ref}
                 className={styles}
                 extensions={extensions}
-                height={height}
+                height={height === 'full' ? '100%' : height}
+                theme={theme}
                 value={value}
                 onChange={onChange}
+                {...props}
             />
             {title && <Heading className={'order-1 !text-grey-700 peer-focus:!text-black'} htmlFor={id} useLabelTag={true}>{title}</Heading>}
             {hint && <Hint className='order-3' color={error ? 'red' : ''}>{hint}</Hint>}
         </div>
     );
-};
+});
 
 export default CodeEditor;
--- apps/admin-x-settings/src/components/settings/advanced/CodeInjection.tsx
+++ apps/admin-x-settings/src/components/settings/advanced/CodeInjection.tsx
@@ -1,36 +1,59 @@
 import Button from '../../../admin-x-ds/global/Button';
-import CodeBlock from './code/CodeBlock';
-import React, {useState} from 'react';
+import CodeEditor from '../../../admin-x-ds/global/form/CodeEditor';
+import CodeModal from './code/CodeModal';
+import NiceModal from '@ebay/nice-modal-react';
+import React, {useRef, useState} from 'react';
 import SettingGroup from '../../../admin-x-ds/settings/SettingGroup';
 import TabView from '../../../admin-x-ds/global/TabView';
 import useSettingGroup from '../../../hooks/useSettingGroup';
+import {ReactCodeMirrorRef} from '@uiw/react-codemirror';
+import {getSettingValues} from '../../../utils/helpers';
+import {html} from '@codemirror/lang-html';
 
 const CodeInjection: React.FC<{ keywords: string[] }> = ({keywords}) => {
     const {
-        // localSettings,
+        localSettings,
         isEditing,
         saveState,
-        // focusRef,
         handleSave,
         handleCancel,
-        // updateSetting,
+        updateSetting,
         handleEditingChange
     } = useSettingGroup();
 
-    const [selectedTab, setSelectedTab] = useState('header');
+    const [headerContent, footerContent] = getSettingValues<string>(localSettings, ['codeinjection_head', 'codeinjection_foot']);
+
+    const [selectedTab, setSelectedTab] = useState<'header' | 'footer'>('header');
+
+    const headerEditorRef = useRef<ReactCodeMirrorRef>(null);
+    const footerEditorRef = useRef<ReactCodeMirrorRef>(null);
+
+    const headerProps = {
+        extensions: [html()],
+        hint: 'Code here will be injected into the {{ghost_head}} tag on every page of the site',
+        value: headerContent,
+        onChange: (value: string) => updateSetting('codeinjection_head', value)
+    };
+
+    const footerProps = {
+        extensions: [html()],
+        hint: 'Code here will be injected into the {{ghost_foot}} tag on every page of the site',
+        value: footerContent,
+        onChange: (value: string) => updateSetting('codeinjection_foot', value)
+    };
 
     const tabs = [
         {
             id: 'header',
             title: 'Site header',
-            contents: (<CodeBlock hint='Code here will be injected into the {{ghost_head}} tag on every page of the site' />)
+            contents: (<CodeEditor {...headerProps} ref={headerEditorRef} autoFocus />)
         },
         {
             id: 'footer',
             title: 'Site footer',
-            contents: (<CodeBlock hint='Code here will be injected into the {{ghost_foot}} tag on every page of the site' />)
+            contents: (<CodeEditor {...footerProps} ref={footerEditorRef} />)
         }
-    ];
+    ] as const;
 
     return (
         <SettingGroup
@@ -45,13 +68,26 @@ const CodeInjection: React.FC<{ keywords: string[] }> = ({keywords}) => {
             onEditingChange={handleEditingChange}
             onSave={handleSave}
         >
-            {isEditing &&
-            <div className='relative'>
-                <TabView selectedTab={selectedTab} tabs={tabs} onTabChange={setSelectedTab} />
-                <Button className='absolute right-0 top-1 text-sm' label='Fullscreen' unstyled />
-            </div>
-
-            }
+            {isEditing && (
+                <div className='relative'>
+                    <TabView<'header' | 'footer'> selectedTab={selectedTab} tabs={tabs} onTabChange={setSelectedTab} />
+                    <Button
+                        className='absolute right-0 top-1 text-sm'
+                        label='Fullscreen'
+                        unstyled
+                        onClick={() => NiceModal.show(CodeModal, {
+                            ...(selectedTab === 'header' ? headerProps : footerProps),
+                            afterClose: () => {
+                                if (selectedTab === 'header') {
+                                    headerEditorRef.current?.view?.focus();
+                                } else {
+                                    footerEditorRef.current?.view?.focus();
+                                }
+                            }
+                        })}
+                    />
+                </div>
+            )}
         </SettingGroup>
     );
 };
