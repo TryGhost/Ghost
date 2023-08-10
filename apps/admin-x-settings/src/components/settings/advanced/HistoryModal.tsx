5c077409e53f4c953b96a839863d127e66653528
Peter Zimon <peter.zimon@gmail.com>
2023-08-10 09:01:28 +0200

    Added list item

--- apps/admin-x-settings/src/components/settings/advanced/HistoryModal.tsx
+++ apps/admin-x-settings/src/components/settings/advanced/HistoryModal.tsx
@@ -1,3 +1,6 @@
+import Avatar from '../../../admin-x-ds/global/Avatar';
+import List from '../../../admin-x-ds/global/List';
+import ListItem from '../../../admin-x-ds/global/ListItem';
 import Modal from '../../../admin-x-ds/global/modal/Modal';
 import NiceModal from '@ebay/nice-modal-react';
 import useRouting from '../../../hooks/useRouting';
@@ -11,6 +14,8 @@ const HistoryModal = NiceModal.create(() => {
             afterClose={() => {
                 updateRoute('history');
             }}
+            cancelLabel='Close'
+            okLabel=''
             scrolling={true}
             size='md'
             stickyFooter={true}
@@ -18,7 +23,18 @@ const HistoryModal = NiceModal.create(() => {
             title='History'
         >
             <div className='-mb-8 mt-6'>
-                Full history
+                <List
+                    hint='End of history log'
+                >
+                    <ListItem
+                        avatar={
+                            <Avatar bgColor="green" label="DV" labelColor="white"/>
+                        }
+                        detail='09 Aug 2023'
+                        title='Settings edited'
+                        separator
+                    />
+                </List>
             </div>
         </Modal>
     );
