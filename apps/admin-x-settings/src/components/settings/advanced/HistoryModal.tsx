525a24042c622594ce345ef237a731346c19c84d
Peter Zimon <peter.zimon@gmail.com>
2023-08-10 12:56:30 +0200

    Updated static list item

--- apps/admin-x-settings/src/components/settings/advanced/HistoryModal.tsx
+++ apps/admin-x-settings/src/components/settings/advanced/HistoryModal.tsx
@@ -1,12 +1,35 @@
 import Avatar from '../../../admin-x-ds/global/Avatar';
+import Icon from '../../../admin-x-ds/global/Icon';
 import List from '../../../admin-x-ds/global/List';
 import ListItem from '../../../admin-x-ds/global/ListItem';
 import Modal from '../../../admin-x-ds/global/modal/Modal';
-import NiceModal from '@ebay/nice-modal-react';
+import NiceModal, {useModal} from '@ebay/nice-modal-react';
 import useRouting from '../../../hooks/useRouting';
+import {generateAvatarColor, getInitials} from '../../../utils/helpers';
+
+interface HistoryAvatarProps {
+    name?: string;
+    email: string;
+    profileImage?: string;
+}
+
+const HistoryAvatar: React.FC<HistoryAvatarProps> = ({
+    name,
+    email,
+    profileImage
+}) => {
+    return (
+        <div className='relative'>
+            <Avatar bgColor={generateAvatarColor((name ? name : email))} image={profileImage} label={getInitials(name)} labelColor='white' size='md' />
+            <div className='absolute -bottom-1 -right-1 flex items-center justify-center rounded-full border border-grey-100 bg-white p-1 shadow-sm'>
+                <Icon name='pen' size='xs' />
+            </div>
+        </div>
+    );
+};
 
 const HistoryModal = NiceModal.create(() => {
-    // const modal = useModal();
+    const modal = useModal();
     const {updateRoute} = useRouting();
 
     return (
@@ -14,13 +37,17 @@ const HistoryModal = NiceModal.create(() => {
             afterClose={() => {
                 updateRoute('history');
             }}
-            cancelLabel='Close'
-            okLabel=''
+            cancelLabel=''
+            okLabel='Close'
             scrolling={true}
             size='md'
             stickyFooter={true}
             testId='history-modal'
             title='History'
+            onOk={() => {
+                modal.remove();
+                updateRoute('history');
+            }}
         >
             <div className='-mb-8 mt-6'>
                 <List
@@ -28,10 +55,20 @@ const HistoryModal = NiceModal.create(() => {
                 >
                     <ListItem
                         avatar={
-                            <Avatar bgColor="green" label="DV" labelColor="white"/>
+                            // <Avatar bgColor="green" label="DV" labelColor="white"/>
+                            <HistoryAvatar
+                                email='jono@ghost.org'
+                                name='Jono'
+                            />
                         }
-                        detail='09 Aug 2023'
-                        title='Settings edited'
+                        detail='09 Aug 2023 | 15:40:22'
+                        title={
+                            <div className='text-sm'>
+                                <span>Settings edited: Members</span>
+                                <span className='text-xs'><code className='mb-1 bg-white text-grey-800'>(stripe_connect_publishable_key)</code> </span>
+                                <span>&mdash; by Jono</span>
+                            </div>
+                        }
                         separator
                     />
                 </List>
