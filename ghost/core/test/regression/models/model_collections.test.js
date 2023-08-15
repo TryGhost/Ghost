dc89013d58f913742c9d689ad323c61a9a4cb101
Fabien "egg" O'Carroll <fabien@allou.is>
2023-08-15 16:31:46 +0100

    Handled sort_order updates for Collections
    refs https://github.com/TryGhost/Arch/issues/73
    
    With the latest version of bookshelf-relations we're able to define a model
    specific hook which allows us to ignore sort_order updates on automatic
    collections, which don't require their order to be persisted.

--- ghost/core/test/regression/models/model_collections.test.js
+++ ghost/core/test/regression/models/model_collections.test.js
@@ -13,7 +13,7 @@ describe('Collection Model', function () {
     before(testUtils.setup('users:roles', 'posts'));
 
     describe('add', function () {
-        it('does not update the sort_order of the collections_posts table', async function () {
+        it('does not update the sort_order of the collections_posts table if the type is "automatic"', async function () {
             if (db?.knex?.client?.config?.client !== 'sqlite3') {
                 return this.skip();
             }
@@ -34,7 +34,44 @@ describe('Collection Model', function () {
 
             await models.Collection.add({
                 title: 'Test Collection',
-                slug: 'test-collection',
+                slug: 'test-collection-automatic',
+                description: 'Test description',
+                type: 'automatic',
+                filter: 'featured:true',
+                posts: posts.toJSON().map(post => ({id: post.id})),
+                feature_image: null
+            });
+
+            database.off('query', handler);
+
+            const actual = didUpdateCollectionPosts;
+            const expected = false;
+
+            assert.equal(actual, expected, 'collections_posts should not have been updated');
+        });
+
+        it('does update the sort_order of the collections_posts table if the type is "manual"', async function () {
+            if (db?.knex?.client?.config?.client !== 'sqlite3') {
+                return this.skip();
+            }
+            /** @type {import('sqlite3').Database} */
+            const database = db.knex.client;
+
+            let didUpdateCollectionPosts = false;
+
+            function handler(/** @type {{sql: string}} */ query) {
+                if (query.sql.toLowerCase().includes('update `collections_posts` set `sort_order`')) {
+                    didUpdateCollectionPosts = true;
+                }
+            }
+
+            const posts = await models.Post.findAll();
+
+            database.on('query', handler);
+
+            await models.Collection.add({
+                title: 'Test Collection',
+                slug: 'test-collection-manual',
                 description: 'Test description',
                 type: 'manual',
                 filter: null,
@@ -45,7 +82,7 @@ describe('Collection Model', function () {
             database.off('query', handler);
 
             const actual = didUpdateCollectionPosts;
-            const expected = false;
+            const expected = true;
 
             assert.equal(actual, expected, 'collections_posts should not have been updated');
         });
