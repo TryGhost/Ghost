1b8f83e95d28c6a74461862acf418fc011124a06
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-08-16 13:00:35 +0200

    Moved unsplash test to another dir

--- apps/admin-x-settings/test/e2e/advanced/integrations/unsplash.test.ts
+++ apps/admin-x-settings/test/e2e/advanced/integrations/unsplash.test.ts
@@ -1,25 +1,7 @@
 import {expect, test} from '@playwright/test';
-import {globalDataRequests, mockApi, updatedSettingsResponse} from '../../utils/e2e';
+import {globalDataRequests, mockApi, updatedSettingsResponse} from '../../../utils/e2e';
 
 test.describe('Unsplash integration', async () => {
-    test('Supports toggling unsplash integration modal', async ({page}) => {
-        await mockApi({page, requests: {
-            ...globalDataRequests,
-            editSettings: {method: 'PUT', path: '/settings/', response: updatedSettingsResponse([
-                {key: 'unsplash', value: true}
-            ])}
-        }});
-
-        await page.goto('/');
-        const section = page.getByTestId('integrations');
-        const unsplashElement = section.getByText('Unsplash').last();
-        await unsplashElement.hover();
-        await page.getByRole('button', {name: 'Configure'}).click();
-        const unsplashModal = page.getByTestId('unsplash-modal');
-        await page.getByRole('button', {name: 'Close'}).click();
-        expect(unsplashModal).not.toBeVisible();
-    });
-
     test('Supports toggling unsplash integration', async ({page}) => {
         const {lastApiRequests} = await mockApi({page, requests: {
             ...globalDataRequests,
