bbf6f9d026b47e71650b4da51e78e8866c8dbb62
Peter Zimon <zimo@ghost.org>
2023-08-18 08:20:46 +0200

    Static Tips & Donations settings in AdminX (#17757)
    refs. https://github.com/TryGhost/Product/issues/3746
    
    - Added staitc UI for Tips & donations in AdminX settings

--- apps/admin-x-settings/src/components/settings/membership/MembershipSettings.tsx
+++ apps/admin-x-settings/src/components/settings/membership/MembershipSettings.tsx
@@ -4,11 +4,13 @@ import Portal from './Portal';
 import React from 'react';
 import SettingSection from '../../../admin-x-ds/settings/SettingSection';
 import Tiers from './Tiers';
+import TipsOrDonations from './TipsOrDonations';
 
 const searchKeywords = {
     portal: ['portal', 'signup', 'sign up', 'signin', 'sign in', 'login', 'account', 'membership'],
+    access: ['default', 'access', 'subscription', 'post', 'membership'],
     tiers: ['tiers', 'payment', 'paid'],
-    access: ['access', 'subscription', 'post', 'membership'],
+    tips: ['tip', 'donation', 'one time', 'payment'],
     analytics: ['analytics', 'tracking', 'privacy', 'membership']
 };
 
@@ -18,6 +20,7 @@ const MembershipSettings: React.FC = () => {
             <Portal keywords={searchKeywords.portal} />
             <Access keywords={searchKeywords.access} />
             <Tiers keywords={searchKeywords.tiers} />
+            <TipsOrDonations keywords={searchKeywords.tips} />
             <Analytics keywords={searchKeywords.analytics} />
         </SettingSection>
     );
---
+++ apps/admin-x-settings/src/components/settings/membership/TipsOrDonations.tsx
@@ -0,0 +1,85 @@
+import Button from '../../../admin-x-ds/global/Button';
+import React from 'react';
+import Select from '../../../admin-x-ds/global/form/Select';
+import SettingGroup from '../../../admin-x-ds/settings/SettingGroup';
+import SettingGroupContent from '../../../admin-x-ds/settings/SettingGroupContent';
+import TextField from '../../../admin-x-ds/global/form/TextField';
+import useSettingGroup from '../../../hooks/useSettingGroup';
+
+const TipsOrDonations: React.FC<{ keywords: string[] }> = ({keywords}) => {
+    const {
+        isEditing,
+        saveState,
+        handleSave,
+        handleCancel,
+        focusRef,
+        handleEditingChange
+    } = useSettingGroup();
+
+    const values = (
+        <SettingGroupContent
+            columns={2}
+            values={[
+                {
+                    heading: 'Suggested amount',
+                    key: 'suggested-amount',
+                    value: '$12'
+                },
+                {
+                    heading: 'Sharable link',
+                    key: 'sharable-link',
+                    value: (
+                        <div className='w-100 group relative -m-1 overflow-hidden rounded p-1 hover:bg-grey-50'>
+                            https://example.com/tip
+                            <div className='invisible absolute right-0 top-[50%] flex translate-y-[-50%] gap-1 bg-white pl-1 group-hover:visible'>
+                                <Button color='outline' label='Copy' size='sm' />
+                            </div>
+                        </div>
+                    )
+                }
+            ]}
+        />
+    );
+
+    const inputFields = (
+        <SettingGroupContent className='max-w-[180px]'>
+            <TextField
+                inputRef={focusRef}
+                placeholder="0"
+                rightPlaceholder={(
+                    <Select
+                        border={false}
+                        options={[
+                            {label: 'USD', value: 'usd'},
+                            {label: 'EUR', value: 'eur'}
+                        ]}
+                        selectClassName='w-auto'
+                        onSelect={() => {}}
+                    />
+                )}
+                title='Suggested amount'
+                value='12'
+                onChange={() => {}}
+            />
+        </SettingGroupContent>
+    );
+
+    return (
+        <SettingGroup
+            description="Give your audience a one-time way to support your work, no membership required."
+            isEditing={isEditing}
+            keywords={keywords}
+            navid='tips-or-donations'
+            saveState={saveState}
+            testId='tips-or-donations'
+            title="Tips or donations"
+            onCancel={handleCancel}
+            onEditingChange={handleEditingChange}
+            onSave={handleSave}
+        >
+            {isEditing ? inputFields : values}
+        </SettingGroup>
+    );
+};
+
+export default TipsOrDonations;
