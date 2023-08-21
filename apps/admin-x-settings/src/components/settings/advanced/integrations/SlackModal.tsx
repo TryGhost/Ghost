b27363571449dc67ac4f0aa3f2c3778ff6daf964
Jono Mingard <reason.koan@gmail.com>
2023-08-22 00:20:37 +0100

    Wired up Slack integration in AdminX

--- apps/admin-x-settings/src/components/settings/advanced/integrations/SlackModal.tsx
+++ apps/admin-x-settings/src/components/settings/advanced/integrations/SlackModal.tsx
@@ -5,22 +5,43 @@ import Modal from '../../../../admin-x-ds/global/modal/Modal';
 import NiceModal from '@ebay/nice-modal-react';
 import TextField from '../../../../admin-x-ds/global/form/TextField';
 import useRouting from '../../../../hooks/useRouting';
+import useSettingGroup from '../../../../hooks/useSettingGroup';
 import {ReactComponent as Icon} from '../../../../assets/icons/slack.svg';
+import {getSettingValues} from '../../../../api/settings';
+import {showToast} from '../../../../admin-x-ds/global/Toast';
+import {useTestSlack} from '../../../../api/slack';
 
 const SlackModal = NiceModal.create(() => {
     const {updateRoute} = useRouting();
     const modal = NiceModal.useModal();
+
+    const {localSettings, updateSetting, handleSave} = useSettingGroup();
+    const [slackUrl, slackUsername] = getSettingValues<string>(localSettings, ['slack_url', 'slack_username']);
+
+    const {mutateAsync: testSlack} = useTestSlack();
+
+    const handleTestClick = async () => {
+        await handleSave();
+        await testSlack(null);
+        showToast({
+            message: 'Check your Slack channel for the test message',
+            type: 'neutral'
+        });
+    };
 
     return (
         <Modal
             afterClose={() => {
                 updateRoute('integrations');
             }}
+            dirty={localSettings.some(setting => setting.dirty)}
             okColor='black'
             okLabel='Save & close'
             title=''
-            onOk={() => {
+            onOk={async () => {
+                await handleSave();
                 modal.remove();
+                updateRoute('integrations');
             }}
         >
             <IntegrationHeader
@@ -32,18 +53,22 @@ const SlackModal = NiceModal.create(() => {
                 <Form marginBottom={false} title='Slack configuration' grouped>
                     <TextField
                         hint={<>
-                            Automatically send newly published posts to a channel in Slack or any Slack-compatible service like Discord or Mattermost. Set up a new incoming webhook here <strong className='text-red'>[&larr; link to be set]</strong>, and grab the URL.
+                            Automatically send newly published posts to a channel in Slack or any Slack-compatible service like Discord or Mattermost. Set up a new incoming webhook <a href='https://my.slack.com/apps/new/A0F7XDUAZ-incoming-webhooks'>here</a>, and grab the URL.
                         </>}
                         placeholder='https://hooks.slack.com/services/...'
                         title='Webhook URL'
+                        value={slackUrl}
+                        onChange={e => updateSetting('slack_url', e.target.value)}
                     />
                     <div className='flex w-full items-center gap-2'>
                         <TextField
                             containerClassName='flex-grow'
                             hint='The username to display messages from'
                             title='Username'
+                            value={slackUsername}
+                            onChange={e => updateSetting('slack_username', e.target.value)}
                         />
-                        <Button color='outline' label='Send test notification' />
+                        <Button color='outline' label='Send test notification' onClick={handleTestClick} />
                     </div>
                 </Form>
             </div>
