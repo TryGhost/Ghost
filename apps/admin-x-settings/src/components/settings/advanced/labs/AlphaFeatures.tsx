e9e1e0762b4b54b22951626d52e0b0c3fd66fa46
Djordje Vlaisavljevic <dzvlais@gmail.com>
2023-08-29 12:13:42 +0100

    Added Recommendations feature flag
    refs https://github.com/TryGhost/Product/issues/3787

--- apps/admin-x-settings/src/components/settings/advanced/labs/AlphaFeatures.tsx
+++ apps/admin-x-settings/src/components/settings/advanced/labs/AlphaFeatures.tsx
@@ -55,6 +55,10 @@ const features = [{
     title: 'Tips & donations',
     description: 'Enables publishers to collect one-time payments',
     flag: 'tipsAndDonations'
+},{
+    title: 'Recommendations',
+    description: 'Enables publishers to recommend sites to their audience',
+    flag: 'recommendations'
 }];
 
 const AlphaFeatures: React.FC = () => {
