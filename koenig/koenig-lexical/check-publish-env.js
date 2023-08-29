5cb3f98bf96702a7f3dcc394ab10f81afe749c74
Kevin Ansfield <kevin@lookingsideways.co.uk>
2023-08-29 13:37:41 +0100

    Removed debugging console.log

--- koenig/koenig-lexical/check-publish-env.js
+++ koenig/koenig-lexical/check-publish-env.js
@@ -1,7 +1,5 @@
 import 'dotenv/config';
 
-console.log({is_shipping: process.env.IS_SHIPPING});
-
 if (!process.env.VITE_SENTRY_AUTH_TOKEN) {
     throw new Error('VITE_SENTRY_AUTH_TOKEN is not set in your .env file - this is required when publishing');
 }
