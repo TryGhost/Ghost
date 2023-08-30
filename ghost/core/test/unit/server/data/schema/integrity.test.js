4d2eded5d9057be65cb3e555f214f3ba5b94fc01
Sag <guptazy@gmail.com>
2023-08-30 08:22:57 +0200

    Added Recommendations table
    closes https://github.com/TryGhost/Product/issues/3775
    
    - Recommendations tech spec: https://www.notion.so/ghost/Recommentions-Tech-Spec-598f09e4109f4eef8d9e32db7245a90c?pvs=4#d59dd64cf40144bc944319b65436a730

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = 'ad44bf95fee71a878704bff2a313a583';
+    const currentSchemaHash = '5674764ca6518961a9b2917e7d8e2ea9';
     const currentFixturesHash = '1803057343a6afa7b50f1dabbc21424d';
     const currentSettingsHash = 'dd0e318627ded65e41f188fb5bdf5b74';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
