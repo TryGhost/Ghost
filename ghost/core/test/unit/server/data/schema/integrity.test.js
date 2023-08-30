f1cd6432a8768d8179e0eb3373c60a7c70cae76e
Sag <guptazy@gmail.com>
2023-08-30 10:56:21 +0200

    Added new setting to enable Recommendations (#17856)
    closes https://github.com/TryGhost/Product/issues/3772
    
    - Recommendations are disabled by default and will get enabled when a
    first recommendation is added. The "recommendations_enabled" setting
    will be not shown explicitly in Admin, it will rather be set implicitly
    based on whether there is at least one recommendation
    - The "recommendations_enabled" setting belongs to a new setting group
    called "recommendations"
    
    Tech Spec: https://www.notion.so/ghost/Recommentions-Tech-Spec-598f09e4109f4eef8d9e32db7245a90c?pvs=4

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -37,7 +37,7 @@ describe('DB version integrity', function () {
     // Only these variables should need updating
     const currentSchemaHash = '5674764ca6518961a9b2917e7d8e2ea9';
     const currentFixturesHash = '31865c37aacfec9b8f16c1354b36a7de';
-    const currentSettingsHash = 'dd0e318627ded65e41f188fb5bdf5b74';
+    const currentSettingsHash = '3a7ca0aa6a06cba47e3e898aef7029c2';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
 
     // If this test is failing, then it is likely a change has been made that requires a DB version bump,
