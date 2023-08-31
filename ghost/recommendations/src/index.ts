96fefaea69b393c48e62a8de35ee8eef1150ad9e
Simon Backx <simon@ghost.org>
2023-08-31 16:57:18 +0200

    Added well known recommendations service (#17895)
    fixes https://github.com/TryGhost/Product/issues/3797 
    fixes https://github.com/TryGhost/Product/issues/3776 
    fixes https://github.com/TryGhost/Product/issues/3798
    
    - Added support for storing json webmentions
    - Improved handling deleted webmentions (set deleted to true instead of verified to false)

--- ghost/recommendations/src/index.ts
+++ ghost/recommendations/src/index.ts
@@ -3,3 +3,4 @@ export * from './RecommendationService';
 export * from './RecommendationRepository';
 export * from './InMemoryRecommendationRepository';
 export * from './Recommendation';
+export * from './WellknownService';
