8df132e4f3b03a7da60df761931799d35f059399
Naz <hi@nazavo.com>
2023-08-31 18:58:38 +0800

    Optimized the amount of logs produced by collections module
    refs https://github.com/TryGhost/Arch/issues/82
    
    - Collections logs are too verbose causing noise.
    - Moved some of the logs to use "debug" for now and made summarized logs for the information that we still need while collections code is actively monitored. The event info logs can be removed once we are passed the active phase of rolling out the collections feature

--- ghost/collections/src/libraries.d.ts
+++ ghost/collections/src/libraries.d.ts
@@ -1,5 +1,6 @@
-declare module '@tryghost/errors';
+declare module '@tryghost/debug';
 declare module '@tryghost/domain-events'
+declare module '@tryghost/errors';
 declare module '@tryghost/logging'
 declare module '@tryghost/nql'
 declare module '@tryghost/tpl';
