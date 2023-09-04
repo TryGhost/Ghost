0cf76d4d94c138ab5db6aa7f20ce00ac88939929
Peter Zimon <peter.zimon@gmail.com>
2023-09-04 13:38:36 +0200

    Added form

--- apps/admin-x-settings/src/components/settings/site/AnnouncementBarModal.tsx
+++ apps/admin-x-settings/src/components/settings/site/AnnouncementBarModal.tsx
@@ -1,18 +1,68 @@
+import AnnouncementBarPreview from './announcementBar/AnnouncementBarPreview';
+import CheckboxGroup from '../../../admin-x-ds/global/form/CheckboxGroup';
+import ColorIndicator from '../../../admin-x-ds/global/form/ColorIndicator';
+import Form from '../../../admin-x-ds/global/form/Form';
+import HtmlField from '../../../admin-x-ds/global/form/HtmlField';
 import NiceModal from '@ebay/nice-modal-react';
 import React from 'react';
 import useRouting from '../../../hooks/useRouting';
 import {PreviewModalContent} from '../../../admin-x-ds/global/modal/PreviewModal';
+import {useGlobalData} from '../../providers/GlobalDataProvider';
 
 const Sidebar: React.FC = () => {
+    const {config} = useGlobalData();
     return (
-        <div className='pt-4'>
-            sidebar
-        </div>
+        <Form>
+            <HtmlField
+                config={config}
+                nodes='MINIMAL_NODES'
+                placeholder='Highlight breaking news, offers or updates'
+                title='Announcement'
+            />
+            <ColorIndicator
+                isExpanded={false}
+                picker={false}
+                swatches={[
+                    {
+                        hex: '#08090c',
+                        title: 'Dark'
+                    },
+                    {
+                        hex: '#ffffff',
+                        title: 'Light'
+                    },
+                    {
+                        hex: '#ffdd00',
+                        title: 'Accent'
+                    }
+                ]}
+                swatchSize='lg'
+                title='Background color'
+                onSwatchChange={() => {}}
+                onTogglePicker={() => {}}
+            />
+            <CheckboxGroup
+                checkboxes={[
+                    {
+                        label: 'Logged out visitors',
+                        onChange: () => {},
+                        value: ''
+                    },
+                    {
+                        label: 'Free members',
+                        onChange: () => {},
+                        value: ''
+                    },
+                    {
+                        label: 'Paid members',
+                        onChange: () => {},
+                        value: ''
+                    }
+                ]}
+                title='Visibility'
+            />
+        </Form>
     );
-};
-
-const Preview: React.FC = () => {
-    return (<>Preview</>);
 };
 
 const AnnouncementBarModal: React.FC = () => {
@@ -21,16 +71,15 @@ const AnnouncementBarModal: React.FC = () => {
 
     const sidebar = <Sidebar />;
 
-    const preview = <Preview />;
-
     return <PreviewModalContent
         afterClose={() => {
             updateRoute('announcement-bar');
         }}
+        cancelLabel='Close'
         deviceSelector={false}
         dirty={false}
         okLabel='Save'
-        preview={preview}
+        preview={<AnnouncementBarPreview />}
         previewBgColor='greygradient'
         sidebar={sidebar}
         testId='announcement-bar-modal'
