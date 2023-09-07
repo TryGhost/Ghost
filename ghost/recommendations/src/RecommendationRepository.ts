444e45a0fce0527f1cb0220d14661863f6739ac0
Simon Backx <simon@ghost.org>
2023-09-07 17:01:14 +0200

    fix

--- ghost/recommendations/src/RecommendationRepository.ts
+++ ghost/recommendations/src/RecommendationRepository.ts
@@ -1,7 +1,17 @@
+import {OrderOption} from "@tryghost/bookshelf-repository";
 import {Recommendation} from "./Recommendation";
 
 export interface RecommendationRepository {
     save(entity: Recommendation): Promise<void>;
     getById(id: string): Promise<Recommendation | null>;
     getAll(): Promise<Recommendation[]>;
+    getPage({ filter, order, page, limit }: {
+        filter?: string;
+        order?: OrderOption<Recommendation>;
+        page: number;
+        limit: number | 'all';
+    }): Promise<Recommendation[]>;
+    getCount({ filter }?: {
+        filter?: string;
+    }): Promise<number>;
 };
