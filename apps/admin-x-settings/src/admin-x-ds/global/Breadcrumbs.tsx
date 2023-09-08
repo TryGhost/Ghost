5336cab53a794d50b3121886567bc870955d4426
Peter Zimon <peter.zimon@gmail.com>
2023-09-08 12:01:06 +0200

    Fixed change theme responsive issues

--- apps/admin-x-settings/src/admin-x-ds/global/Breadcrumbs.tsx
+++ apps/admin-x-settings/src/admin-x-ds/global/Breadcrumbs.tsx
@@ -1,5 +1,6 @@
 import Button from './Button';
 import React from 'react';
+import clsx from 'clsx';
 
 export type BreadcrumbItem = {
     label: React.ReactNode;
@@ -10,35 +11,58 @@ interface BreadcrumbsProps {
     items: BreadcrumbItem[];
     backIcon?: boolean;
     onBack?: () => void;
+    containerClassName?: string;
+    itemClassName?: string;
+    activeItemClassName?: string;
+    separatorClassName?: string;
 }
 
 const Breadcrumbs: React.FC<BreadcrumbsProps> = ({
     items,
     backIcon = false,
-    onBack
+    onBack,
+    containerClassName,
+    itemClassName,
+    activeItemClassName,
+    separatorClassName
 }) => {
     const allItems = items.length;
     let i = 0;
 
+    containerClassName = clsx(
+        'flex items-center gap-2 text-sm',
+        containerClassName
+    );
+
+    activeItemClassName = clsx(
+        'font-bold',
+        activeItemClassName
+    );
+
+    itemClassName = clsx(
+        'text-sm',
+        itemClassName
+    );
+
     return (
-        <div className='flex items-center gap-2 text-sm'>
+        <div className={containerClassName}>
             {backIcon &&
             <Button className='mr-6' icon='arrow-left' size='sm' link onClick={onBack} />
             }
             {items.map((item) => {
                 const bcItem = (i === allItems - 1 ?
-                    <span className='font-bold'>{item.label}</span>
+                    <span className={activeItemClassName}>{item.label}</span>
                     :
                     <>
                         <button
                             key={`bc-${i}`}
-                            className={` text-sm ${item.onClick && '-mx-1 cursor-pointer rounded-sm px-1 py-px hover:bg-grey-100'}`}
+                            className={`${itemClassName} ${item.onClick && '-mx-1 cursor-pointer rounded-sm px-1 py-px hover:bg-grey-100'}`}
                             type="button"
                             onClick={item.onClick}
                         >
                             {item.label}
                         </button>
-                        <span>/</span>
+                        <span className={separatorClassName}>/</span>
                     </>);
                 i = i + 1;
                 return bcItem;
