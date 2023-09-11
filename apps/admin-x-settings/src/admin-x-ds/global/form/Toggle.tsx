2ee4fbd4318b2226dcb329e49f9bb9a629743c59
Peter Zimon <peter.zimon@gmail.com>
2023-09-11 10:32:53 +0200

    Stripe connect modal

--- apps/admin-x-settings/src/admin-x-ds/global/form/Toggle.tsx
+++ apps/admin-x-settings/src/admin-x-ds/global/form/Toggle.tsx
@@ -67,7 +67,7 @@ const Toggle: React.FC<ToggleProps> = ({
     let toggleBgClass;
     switch (toggleBg) {
     case 'stripetest':
-        toggleBgClass = 'checked:bg-[#EC6803]';
+        toggleBgClass = 'checked:bg-[#EC6803] dark:checked:bg-[#EC6803]';
         break;
 
     case 'green':
@@ -75,7 +75,7 @@ const Toggle: React.FC<ToggleProps> = ({
         break;
 
     default:
-        toggleBgClass = 'checked:bg-black';
+        toggleBgClass = 'checked:bg-black dark:checked:bg-green';
         break;
     }
 
@@ -83,7 +83,7 @@ const Toggle: React.FC<ToggleProps> = ({
         <div>
             <div className={`group flex items-start gap-2 dark:text-white ${direction === 'rtl' && 'justify-between'} ${separator && 'pb-2'}`}>
                 <input checked={checked}
-                    className={`${toggleBgClass} appearance-none rounded-full bg-grey-300 transition after:absolute after:ml-0.5 after:mt-0.5 after:rounded-full after:border-none after:bg-white after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:after:absolute checked:after:rounded-full checked:after:border-none checked:after:bg-white checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer group-hover:opacity-80 dark:bg-grey-800 dark:checked:bg-green ${sizeStyles} ${direction === 'rtl' && ' order-2'}`}
+                    className={`${toggleBgClass} appearance-none rounded-full bg-grey-300 transition after:absolute after:ml-0.5 after:mt-0.5 after:rounded-full after:border-none after:bg-white after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:after:absolute checked:after:rounded-full checked:after:border-none checked:after:bg-white checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer group-hover:opacity-80 dark:bg-grey-800 ${sizeStyles} ${direction === 'rtl' && ' order-2'}`}
                     id={id}
                     role="switch"
                     type="checkbox"
