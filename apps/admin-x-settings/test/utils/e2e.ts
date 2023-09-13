9b2387a364dfc2cfaf4df7fde7bc9990b9eb4bf8
Jono M <reason.koan@gmail.com>
2023-09-13 08:10:33 +0100

    Fixed bugs in AdminX portal and theme settings (#18099)
    refs https://github.com/TryGhost/Product/issues/3832
    
    - Fixed portal preview when no tiers are enabled
    - Fixed portal preview not respecting access setting
    - Disabled portal customisation when nobody can sign up
    - Fixed custom theme settings not updated when theme is changed
    - Added publication icon setting in newsletters
    - Added extremely rudimentary editor role display
    - Fixed drag overlay position in modals

--- apps/admin-x-settings/test/utils/e2e.ts
+++ apps/admin-x-settings/test/utils/e2e.ts
@@ -51,7 +51,7 @@ export const globalDataRequests = {
     browseSettings: {method: 'GET', path: /^\/settings\/\?group=/, response: responseFixtures.settings},
     browseConfig: {method: 'GET', path: '/config/', response: responseFixtures.config},
     browseSite: {method: 'GET', path: '/site/', response: responseFixtures.site},
-    browseMe: {method: 'GET', path: '/users/me/', response: responseFixtures.me}
+    browseMe: {method: 'GET', path: '/users/me/?include=roles', response: responseFixtures.me}
 };
 
 export const limitRequests = {
