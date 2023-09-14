28c6a543b88fbf03c724882420edf54608ecbcdd
Kevin Ansfield <kevin@lookingsideways.co.uk>
2023-09-14 16:15:08 +0100

    Added page html reset and re-render to handle dynamic collection card content
    no issue
    
    Collection cards contain dynamic data that can change when there's any change to a published post but in Ghost all post/page content is rendered once on save and stored as a static string meaning we need a new approach for triggering a re-render of pages that plays well with caching.
    
    - fixed typo in the relations/authors code that meant we weren't correctly calling the prototype method on the Post model inside the `onFetchedCollection` event handler
    - updated Post model to clear the `html` field of all pages when saving or deleting a published post
    - updated Post model to re-render `html` fields when fetching individual posts or a collection of posts
    - modified `insertExtraPostsTags` fixture util to wrap it's concurrent post edits in a transaction otherwise MySQL errors because it hits a deadlock

--- ghost/core/test/utils/fixture-utils.js
+++ ghost/core/test/utils/fixture-utils.js
@@ -188,13 +188,15 @@ const fixtures = {
                 throw new Error('Trying to add more posts_tags than the number of posts.');
             }
 
-            return Promise.all(posts.slice(0, max).map((post) => {
-                post.tags = post.tags ? post.tags : [];
+            return models.Base.transaction((transacting) => {
+                return Promise.all(posts.slice(0, max).map((post) => {
+                    post.tags = post.tags ? post.tags : [];
 
-                return models.Post.edit({
-                    tags: post.tags.concat([_.find(DataGenerator.Content.tags, {id: injectionTagId})])
-                }, _.merge({id: post.id}, context.internal));
-            }));
+                    return models.Post.edit({
+                        tags: post.tags.concat([_.find(DataGenerator.Content.tags, {id: injectionTagId})])
+                    }, _.merge({id: post.id, transacting}, context.internal));
+                }));
+            });
         });
     },
 
