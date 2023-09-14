9aab7c0541b54ad6c03255d3dd0f945b3083ac7a
Princi Vershwal <vershwal.princi@gmail.com>
2023-09-14 14:29:10 +0530

    Update ghost/core/core/frontend/helpers/foreach.js
    Co-authored-by: Michael Barrett <mike182uk@gmail.com>

--- ghost/core/core/frontend/helpers/foreach.js
+++ ghost/core/core/frontend/helpers/foreach.js
@@ -31,7 +31,9 @@ module.exports = function foreach(items, options) {
         if (Object.values(items).length > 0 && checks.isPost(Object.values(items)[0])) {
             visibility = visibility || 'all';
         }
-    } if (_.isArray(items) && items.length > 0 && checks.isNewsletter(items[0])) {
+    } 
+    
+    if (_.isArray(items) && items.length > 0 && checks.isNewsletter(items[0])) {
         visibility = visibility || 'all';
     } else if (_.isObject(items) && _.isArray(Object.values(items))) {
         if (Object.values(items).length > 0 && checks.isNewsletter(Object.values(items)[0])) {
