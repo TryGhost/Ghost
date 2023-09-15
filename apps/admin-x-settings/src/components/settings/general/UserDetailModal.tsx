b5d5b8f1eae2fd29d91e8803c4e100cd1bd3d5f8
Jono Mingard <reason.koan@gmail.com>
2023-09-15 08:14:34 +0100

    Removed actions button for editors

--- apps/admin-x-settings/src/components/settings/general/UserDetailModal.tsx
+++ apps/admin-x-settings/src/components/settings/general/UserDetailModal.tsx
@@ -528,7 +528,7 @@ const UserDetailModalContent: React.FC<{user: User}> = ({user}) => {
     }
 
     const coverButtonContainerClassName = clsx(
-        canAccessSettings(currentUser) ? (
+        hasAdminAccess(currentUser) ? (
             userData.cover_image ? 'relative ml-10 mr-[106px] flex translate-y-[-80px] gap-3 md:ml-0 md:justify-end' : 'relative -mb-8 ml-10 mr-[106px] flex translate-y-[358px] md:ml-0 md:translate-y-[268px] md:justify-end'
         ) : (
             userData.cover_image ? 'relative ml-10 flex max-w-4xl translate-y-[-80px] gap-3 md:mx-auto md:justify-end' : 'relative -mb-8 ml-10 flex max-w-4xl translate-y-[358px] md:mx-auto md:translate-y-[268px] md:justify-end'
@@ -638,7 +638,7 @@ const UserDetailModalContent: React.FC<{user: User}> = ({user}) => {
                             handleImageUpload('cover_image', file);
                         }}
                     >Upload cover image</ImageUpload>
-                    {canAccessSettings(currentUser) && <div className="absolute bottom-12 right-12 z-10">
+                    {hasAdminAccess(currentUser) && <div className="absolute bottom-12 right-12 z-10">
                         <Menu items={menuItems} position='right' trigger={<UserMenuTrigger />}></Menu>
                     </div>}
                     <div className={`${!canAccessSettings(currentUser) ? 'mx-10 pl-0 md:max-w-[50%] min-[920px]:ml-[calc((100vw-920px)/2)] min-[920px]:max-w-[460px]' : 'max-w-[50%] pl-12'} relative flex flex-col items-start gap-4 pb-60 pt-10 md:flex-row md:items-center md:pb-7 md:pt-60`}>
