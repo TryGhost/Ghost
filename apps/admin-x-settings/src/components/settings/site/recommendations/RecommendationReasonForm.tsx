cce7152d721c51c30d69f182a389ad4630be0b53
Djordje Vlaisavljevic <dzvlais@gmail.com>
2023-09-18 11:46:49 +0100

    Fixed spacing issue
    refs https://github.com/TryGhost/Product/issues/3857

--- apps/admin-x-settings/src/components/settings/site/recommendations/RecommendationReasonForm.tsx
+++ apps/admin-x-settings/src/components/settings/site/recommendations/RecommendationReasonForm.tsx
@@ -36,11 +36,11 @@ const RecommendationReasonForm: React.FC<Props<EditOrAddRecommendation | Recomme
         <div>
             <Heading className='mb-2 block text-2xs font-semibold uppercase tracking-wider' grey={true} level={6}>Preview</Heading>
             <a className='flex flex-col rounded-sm border border-grey-300 p-3' href={formState.url} rel="noopener noreferrer" target="_blank">
-                <div className="mb-1 flex items-center gap-2">
+                <div className="flex items-center gap-2">
                     <RecommendationIcon {...formState} />
                     <span className='text-[1.6rem] font-semibold text-grey-900'>{formState.title}</span>
                 </div>
-                <span className='text-md font-medium leading-snug text-grey-700'>{formState.reason}</span>
+                <span className='mb-1 text-md font-medium leading-snug text-grey-700'>{formState.reason}</span>
             </a>
             {formState.one_click_subscribe && <Hint>This is a Ghost site, so your readers can subscribe with just one click</Hint>}
         </div>
