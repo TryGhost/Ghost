c5990a5986820e2d26422569185220464815cf20
Fabien "egg" O'Carroll <fabien@allou.is>
2023-09-22 15:04:48 +0700

    Added CollectionPost events
    refs https://github.com/TryGhost/Arch/issues/95
    
    These events can be used to know when an automatic collections posts have been
    updated, as well as by the repository to optimise the storage of
    CollectionPosts

--- ghost/collections/src/index.ts
+++ ghost/collections/src/index.ts
@@ -4,3 +4,5 @@ export * from './Collection';
 export * from './events/PostAddedEvent';
 export * from './events/PostEditedEvent';
 export * from './events/TagDeletedEvent';
+export * from './events/CollectionPostAdded';
+export * from './events/CollectionPostRemoved';
