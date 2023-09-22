36719d46bae28beb0c20c2f0837f200f7cc32bfc
Naz <hi@nazavo.com>
2023-09-22 21:38:18 +0800

    Nooped v5.64 migrations
    refs https://github.com/TryGhost/Arch/issues/95
    
    - The migrations in v5.65 are clearing the collections data again and only populate the "featured" collection.

--- ghost/core/core/server/data/migrations/versions/5.64/2023-09-19-04-25-40-truncate-stale-built-in-collections-posts.js
+++ ghost/core/core/server/data/migrations/versions/5.64/2023-09-19-04-25-40-truncate-stale-built-in-collections-posts.js
@@ -1,12 +1,10 @@
 const logging = require('@tryghost/logging');
-const {createNonTransactionalMigration} = require('../../utils');
 
-module.exports = createNonTransactionalMigration(
-    async function up(knex) {
-        logging.info('Clearing collections_posts table');
-        await knex('collections_posts').truncate();
+module.exports = {
+    async up() {
+        logging.warn('Skipping migration - noop');
     },
-    async function down() {
-        logging.info('Not doing anything - collections_posts table has been truncated');
+    async down() {
+        logging.warn('Skipping migration - noop');
     }
-);
+};
