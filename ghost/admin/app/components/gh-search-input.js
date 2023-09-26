88da81eedb20581bd0806a5025adc9df96740166
Jono Mingard <reason.koan@gmail.com>
2023-09-26 14:01:42 +0100

    Updated remaining links to old settings

--- ghost/admin/app/components/gh-search-input.js
+++ ghost/admin/app/components/gh-search-input.js
@@ -64,7 +64,7 @@ export default class GhSearchInputComponent extends Component {
 
         if (selected.searchable === 'Users') {
             let id = selected.id.replace('user.', '');
-            this.router.transitionTo('settings.staff.user', id);
+            this.router.transitionTo('settings-x.settings-x', `users/show/${id}`);
         }
 
         if (selected.searchable === 'Tags') {
