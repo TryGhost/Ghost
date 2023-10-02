7dffbc8d8184d151c4a7f2aea2889b1d3f2e189a
Simon Backx <simon@ghost.org>
2023-10-02 16:39:44 +0200

    fix

--- ghost/recommendations/src/RecommendationRepository.ts
+++ ghost/recommendations/src/RecommendationRepository.ts
@@ -4,7 +4,7 @@ import {Recommendation} from './Recommendation';
 export interface RecommendationRepository {
     save(entity: Recommendation): Promise<void>;
     getById(id: string): Promise<Recommendation | null>;
-    getByUrl(url: URL): Promise<Recommendation[]>;
+    getByUrl(url: URL): Promise<Recommendation|null>;
     getAll(options: Omit<AllOptions<Recommendation>, 'page'|'limit'>): Promise<Recommendation[]>;
     getPage(options: AllOptions<Recommendation> & Required<Pick<AllOptions<Recommendation>, 'page'|'limit'>>): Promise<Recommendation[]>;
     getCount(options: {
