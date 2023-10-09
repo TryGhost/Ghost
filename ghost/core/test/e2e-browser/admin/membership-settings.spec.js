61a5136eb8e15a2577e5ddf266bd5e8708716dde
Sam Lord <sam@ghost.org>
2023-10-09 17:04:06 +0100

    Enable parallel running of browser tests
    refs: https://github.com/TryGhost/DevOps/issues/78
    
    Instead of running a single instance of Ghost, we now run an instance of Ghost for each test worker.
    
    This has the unfortunate effect that a test failing will close and restart a new instance of Ghost, but in general will be multiple times faster than sequential execution of tests.

--- ghost/core/test/e2e-browser/admin/membership-settings.spec.js
+++ ghost/core/test/e2e-browser/admin/membership-settings.spec.js
@@ -1,4 +1,5 @@
-const {expect, test} = require('@playwright/test');
+const {expect} = require('@playwright/test');
+const test = require('../fixtures/ghost-test');
 const {disconnectStripe, setupStripe, generateStripeIntegrationToken} = require('../utils');
 
 test.describe('Membership Settings', () => {
