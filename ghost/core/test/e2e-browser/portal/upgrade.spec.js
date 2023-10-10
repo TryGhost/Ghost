91eeb436c92d91f56d17ae487bee2b0a33278cdf
Sam Lord <sam@ghost.org>
2023-10-10 15:12:10 +0100

    More test improvements
    no issue

--- ghost/core/test/e2e-browser/portal/upgrade.spec.js
+++ ghost/core/test/e2e-browser/portal/upgrade.spec.js
@@ -54,6 +54,9 @@ test.describe('Portal', () => {
     });
 
     test.describe('Upgrade: Single Tier', () => {
+        // Because memberUrl is set during first test, we need to run these tests in series
+        test.describe.configure({mode: 'serial'});
+
         let memberUrl;
         const tierName = 'The Local Test';
         const member = {
