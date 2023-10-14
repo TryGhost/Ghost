5014dae5958fbf538989af72e1eee1506ea72393
Mark Stosberg <mark@stosberg.com>
2023-10-14 16:43:11 -0400

    🐛 Fixed searching for "Code Injection" in AdminX
    Before, there was a "copy-paste-o" from email newsletter section.
    
    Now the expected keywords can be used to search for code injection.

--- apps/admin-x-settings/src/components/settings/advanced/AdvancedSettings.tsx
+++ apps/admin-x-settings/src/components/settings/advanced/AdvancedSettings.tsx
@@ -7,7 +7,7 @@ import SettingSection from '../../../admin-x-ds/settings/SettingSection';
 
 export const searchKeywords = {
     integrations: ['integration', 'zapier', 'slack', 'amp', 'unsplash', 'first promoter', 'firstpromoter', 'pintura', 'disqus', 'analytics', 'ulysses', 'typeform', 'buffer', 'plausible', 'github'],
-    codeInjection: ['newsletter', 'enable', 'disable', 'turn on'],
+    codeInjection: ['code', 'injection'],
     labs: ['labs', 'alpha', 'beta', 'flag', 'import', 'export', 'migrate', 'routes', 'redirect', 'translation', 'delete', 'content', 'editor', 'substack', 'migration', 'portal'],
     history: ['history', 'log', 'events', 'user events', 'staff']
 };
