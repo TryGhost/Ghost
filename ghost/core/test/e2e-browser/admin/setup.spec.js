123678ebaf72ae9414b8ad5a906cd0cf217aac1d
Sam Lord <sam@ghost.org>
2023-10-16 15:57:39 +0100

    Use a shared page between tests instead of a single page per test
    refs: https://github.com/TryGhost/DevOps/issues/78
    
    This speeds up the tests by another 30 seconds on my local machine, and hopefully takes some time off in CI too

--- ghost/core/test/e2e-browser/admin/setup.spec.js
+++ ghost/core/test/e2e-browser/admin/setup.spec.js
@@ -3,14 +3,14 @@ const test = require('../fixtures/ghost-test');
 
 test.describe('Admin', () => {
     test.describe('Setup', () => {
-        test('Loads Admin', async ({page}) => {
-            const response = await page.goto('/ghost');
+        test('Loads Admin', async ({sharedPage}) => {
+            const response = await sharedPage.goto('/ghost');
             expect(response.status()).toEqual(200);
         });
 
-        test('Is setup correctly', async ({page}) => {
-            await page.goto('/ghost');
-            await expect(page.locator('.gh-nav-menu-details-sitetitle')).toHaveText(/The Local Test/);
+        test('Is setup correctly', async ({sharedPage}) => {
+            await sharedPage.goto('/ghost');
+            await expect(sharedPage.locator('.gh-nav-menu-details-sitetitle')).toHaveText(/The Local Test/);
         });
     });
 });
