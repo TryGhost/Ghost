af39c1c7fbc811719bf21a609a879d4a30c9e36d
Jannis Fedoruk-Betschki <github@betschki.com>
2023-10-17 13:57:43 +0200

    🎨 Improved search keywords in Admin-X Settings (#18608)
    fixes https://github.com/TryGhost/Ghost/issues/18661

--- apps/admin-x-settings/src/components/settings/advanced/AdvancedSettings.tsx
+++ apps/admin-x-settings/src/components/settings/advanced/AdvancedSettings.tsx
@@ -6,8 +6,8 @@ import React from 'react';
 import SettingSection from '../../../admin-x-ds/settings/SettingSection';
 
 export const searchKeywords = {
-    integrations: ['integration', 'zapier', 'slack', 'amp', 'unsplash', 'first promoter', 'firstpromoter', 'pintura', 'disqus', 'analytics', 'ulysses', 'typeform', 'buffer', 'plausible', 'github'],
-    codeInjection: ['newsletter', 'enable', 'disable', 'turn on'],
+    integrations: ['integrations', 'zapier', 'slack', 'amp', 'unsplash', 'first promoter', 'firstpromoter', 'pintura', 'disqus', 'analytics', 'ulysses', 'typeform', 'buffer', 'plausible', 'github'],
+    codeInjection: ['code injection', 'head', 'footer'],
     labs: ['labs', 'alpha', 'beta', 'flag', 'import', 'export', 'migrate', 'routes', 'redirect', 'translation', 'delete', 'content', 'editor', 'substack', 'migration', 'portal'],
     history: ['history', 'log', 'events', 'user events', 'staff']
 };
