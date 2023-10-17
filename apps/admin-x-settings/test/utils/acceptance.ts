4d0bf8d5d5894bc376086c62d6eee07ee00c6aac
Sag <guptazy@gmail.com>
2023-10-17 10:04:35 -0300

    ✨ Added Recommendations feature (#18664)
    refs https://github.com/TryGhost/Product/issues/3744
    
    Co-authored-by: Simon Backx <simon@ghost.org>
    Co-authored-by: Djordje Vlaisavljevic <dzvlais@gmail.com>

--- apps/admin-x-settings/test/utils/acceptance.ts
+++ apps/admin-x-settings/test/utils/acceptance.ts
@@ -50,7 +50,6 @@ export const responseFixtures = {
 };
 
 let defaultLabFlags = {
-    recommendations: false,
     audienceFeedback: false,
     collections: false,
     themeErrorsNotification: false,
