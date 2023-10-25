db003bfe8ebb4332ea993d2c8cc982359cecce89
Peter Zimon <peter.zimon@gmail.com>
2023-10-25 15:25:58 +0200

    Updated searchbar

--- apps/admin-x-settings/src/MainContent.tsx
+++ apps/admin-x-settings/src/MainContent.tsx
@@ -57,7 +57,7 @@ const MainContent: React.FC = () => {
             {loadingModal && <div className={`fixed inset-0 z-40 h-[calc(100vh-55px)] w-[100vw] tablet:h-[100vh] ${topLevelBackdropClasses}`} />}
 
             {/* Sidebar */}
-            <div className="sticky top-0 z-20 mt-[-55px] min-w-[260px] grow-0 bg-white pt-[52px] dark:bg-black tablet:fixed tablet:top-[8vmin] tablet:mt-0 tablet:basis-[260px] tablet:pt-0">
+            <div className="sticky -top-px z-20 mt-[-55px] min-w-[260px] grow-0 bg-white pt-[52px] dark:bg-black tablet:fixed tablet:top-[8vmin] tablet:mt-0 tablet:basis-[260px] tablet:pt-0">
                 <div className="relative w-full bg-white dark:bg-black">
                     <Sidebar />
                 </div>
