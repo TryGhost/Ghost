2df798897f39483e217b1fac93b950315284f893
Kevin Ansfield <kevin@lookingsideways.co.uk>
2023-10-30 11:13:05 +0000

    Fixed cards not rendering in kg-lexical-html-renderer `shouldRender` test util
    no issue
    
    - we weren't passing a `createDocument()` method through to the renderer so cards were always blank

--- koenig/kg-lexical-html-renderer/test/utils/should-render.js
+++ koenig/kg-lexical-html-renderer/test/utils/should-render.js
@@ -1,10 +1,16 @@
+const {JSDOM} = require('jsdom');
 const Renderer = require('../../');
 
 function shouldRender({input, output, options = {}}) {
     return async function () {
+        const defaultRenderOptions = {
+            createDocument() {
+                return (new JSDOM()).window.document;
+            }
+        };
         const {nodes, ...renderOptions} = options;
         const renderer = new Renderer({nodes});
-        const renderedInput = await renderer.render(input, renderOptions);
+        const renderedInput = await renderer.render(input, {...defaultRenderOptions, ...renderOptions});
         renderedInput.should.equal(output);
     };
 }
