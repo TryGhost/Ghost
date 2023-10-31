4f161f1d48af61c8ffa3631087d5abf8e15540c8
Daniel Lockyer <daniellockyer@fastmail.com>
2023-10-31 15:03:38 +0100

    Added Lithuanian as a supported language

--- ghost/i18n/lib/i18n.js
+++ ghost/i18n/lib/i18n.js
@@ -19,6 +19,7 @@ const SUPPORTED_LOCALES = [
     'it', // Italian
     'ja', // Japanese
     'ko', // Korean
+    'lt', // Lithuanian
     'mn', // Mongolian
     'ms', // Malay
     'nl', // Dutch
