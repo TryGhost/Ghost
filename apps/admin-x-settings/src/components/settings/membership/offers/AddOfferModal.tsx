35557649d78002a83c0649a0756f3e80815c4d45
Jono Mingard <reason.koan@gmail.com>
2023-10-31 08:29:54 +0000

    Updated modals to hide if the feature flag is not enabled

--- apps/admin-x-settings/src/components/settings/membership/offers/AddOfferModal.tsx
+++ apps/admin-x-settings/src/components/settings/membership/offers/AddOfferModal.tsx
@@ -1,7 +1,21 @@
-import NiceModal from '@ebay/nice-modal-react';
+import NiceModal, {useModal} from '@ebay/nice-modal-react';
+import useFeatureFlag from '../../../../hooks/useFeatureFlag';
+import useRouting from '../../../../hooks/useRouting';
 import {PreviewModalContent} from '../../../../admin-x-ds/global/modal/PreviewModal';
+import {useEffect} from 'react';
 
 const AddOfferModal = () => {
+    const modal = useModal();
+    const {updateRoute} = useRouting();
+    const hasOffers = useFeatureFlag('adminXOffers');
+
+    useEffect(() => {
+        if (!hasOffers) {
+            modal.remove();
+            updateRoute('');
+        }
+    }, [hasOffers, modal, updateRoute]);
+
     return <PreviewModalContent sidebar={<></>} title='Add Offer' />;
 };
 
