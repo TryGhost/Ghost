954e332049e4470523f87016fbb80f7566cabd1f
Chris Raible <chris@ghost.org>
2023-11-01 11:34:20 -0700

    Fixed linter errors

--- ghost/admin/lib/sourcemap-postprocess/index.js
+++ ghost/admin/lib/sourcemap-postprocess/index.js
@@ -1,27 +1,27 @@
+/* eslint-disable */
 'use strict';
+
 const fs = require('fs');
 const path = require('path');
 
 module.exports = {
-  name: require('./package').name,
+    name: 'sourcemap-postprocess',
 
-  isDevelopingAddon() {
-    return true;
-  },
+    isDevelopingAddon() {
+        return true;
+    },
 
-  postBuild: function (results) {
-    const fs = this.project.require('fs-extra');
+    postBuild: function (results) {
+        // read all .map files in the /assets directory
+        const assets = fs.readdirSync(path.join(results.directory, 'assets'));
+        const mapFiles = assets.filter((file) => file.endsWith('.map'));
 
-    // read all .map files in the /assets directory
-    const assets = fs.readdirSync(path.join(results.directory, 'assets'));
-    const mapFiles = assets.filter((file) => file.endsWith('.map'));
-
-    // loop over the mapfiles and add a "sourceRoot" key to each one
-    mapFiles.forEach((file) => {
-      const mapFilePath = path.join(results.directory, 'assets', file);
-      const mapFile = JSON.parse(fs.readFileSync(mapFilePath, 'utf8'));
-      mapFile.sourceRoot = '../';
-      fs.writeFileSync(mapFilePath, JSON.stringify(mapFile));
-    });
-  }
+        // loop over the mapfiles and add a "sourceRoot" key to each one
+        mapFiles.forEach((file) => {
+            const mapFilePath = path.join(results.directory, 'assets', file);
+            const mapFile = JSON.parse(fs.readFileSync(mapFilePath, 'utf8'));
+            mapFile.sourceRoot = '../';
+            fs.writeFileSync(mapFilePath, JSON.stringify(mapFile));
+        });
+    }
 };
