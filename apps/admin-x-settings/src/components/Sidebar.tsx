94a8b428b92c3128cfa3d65713b22701551ad661
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2023-11-01 11:09:26 +0800

    Updated sidebar icon

--- apps/admin-x-settings/src/components/Sidebar.tsx
+++ apps/admin-x-settings/src/components/Sidebar.tsx
@@ -109,7 +109,7 @@ const Sidebar: React.FC = () => {
                     <SettingNavItem icon='emailfield' keywords={membershipSearchKeywords.embedSignupForm} navid='embed-signup-form' title="Embeddable signup form" onClick={handleSectionClick} />
                     {hasRecommendations && <SettingNavItem icon='heart' keywords={membershipSearchKeywords.recommendations} navid='recommendations' title="Recommendations" onClick={handleSectionClick} />}
                     <SettingNavItem icon='baseline-chart' keywords={membershipSearchKeywords.analytics} navid='analytics' title="Analytics" onClick={handleSectionClick} />
-                    {hasOffers && <SettingNavItem icon='ellipsis' keywords={membershipSearchKeywords.offers} navid='offers' title="Offers" onClick={handleSectionClick} />}
+                    {hasOffers && <SettingNavItem icon='discount' keywords={membershipSearchKeywords.offers} navid='offers' title="Offers" onClick={handleSectionClick} />}
                 </SettingNavSection>
 
                 <SettingNavSection keywords={Object.values(emailSearchKeywords).flat()} title="Email newsletter">
