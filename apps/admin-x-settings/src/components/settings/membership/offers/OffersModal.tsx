8bb77c579307d2a71c252163e77d269444bf8340
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2023-11-01 13:43:48 +0800

    List screen

--- apps/admin-x-settings/src/components/settings/membership/offers/OffersModal.tsx
+++ apps/admin-x-settings/src/components/settings/membership/offers/OffersModal.tsx
@@ -1,8 +1,27 @@
+import Button from '../../../../admin-x-ds/global/Button';
 import Modal from '../../../../admin-x-ds/global/modal/Modal';
 import NiceModal, {useModal} from '@ebay/nice-modal-react';
+import TabView, {Tab} from '../../../../admin-x-ds/global/TabView';
 import useFeatureFlag from '../../../../hooks/useFeatureFlag';
 import useRouting from '../../../../hooks/useRouting';
 import {useEffect} from 'react';
+
+const Offer: React.FC<{name: string}> = ({name}) => {
+    return <div className='flex flex-col gap-6 items-center text-center border border-transparent bg-grey-100 p-5 transition-all hover:border-grey-100 hover:bg-grey-75 hover:shadow-sm dark:bg-grey-950 dark:hover:border-grey-800'>
+        <h2 className='text-[1.6rem]'>{name}</h2>
+        <div className=''>
+            <div className='flex gap-3 uppercase text-sm leading-none'>
+                <span className='text-pink font-semibold'>10% off</span>
+                <span className='text-grey-700 line-through'>$5</span>
+            </div>
+            <span className='font-bold text-3xl'>$4</span>
+        </div>
+        <div className='flex flex-col items-center text-xs'>
+            <span className='font-medium'>Bronze monthly — First payment</span>
+            <a className='text-grey-700 hover:underline' href="/ghost/#/members">4 redemptions</a>
+        </div>
+    </div>;
+};
 
 const OffersModal = () => {
     const modal = useModal();
@@ -16,7 +35,34 @@ const OffersModal = () => {
         }
     }, [hasOffers, modal, updateRoute]);
 
-    return <Modal title='Offers' />;
+    let offersTabs: Tab[] = [
+        {id: 'active', title: 'Active'},
+        {id: 'archived', title: 'Archived'}
+    ];
+
+    return <Modal cancelLabel='' header={false} size='lg'>
+        <div className='pt-6'>
+            <header>
+                <div className='flex items-center justify-between'>
+                    <TabView
+                        border={false}
+                        selectedTab='active'
+                        tabs={offersTabs}
+                        width='wide'
+                        onTabChange={() => {}}
+                    />
+                    <Button color='green' icon='add' iconColorClass='green' label='New offer' link={true} size='sm' />
+                </div>
+                <h1 className='mt-12 border-b border-b-grey-300 pb-2.5 text-3xl'>Active offers</h1>
+            </header>
+            <div className='mt-8 grid grid-cols-3 gap-6'>
+                <Offer name='Black friday' />
+                <Offer name='Buy this right now' />
+                <Offer name='Desperate Sale!' />
+            </div>
+            <a className='absolute bottom-10 text-sm' href="https://ghost.org/help/offers" rel="noopener noreferrer" target="_blank">→ Learn about offers in Ghost</a>
+        </div>
+    </Modal>;
 };
 
 export default NiceModal.create(OffersModal);
