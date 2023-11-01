aa8b34806c38441d632f785ec04b350a4b1f4fa7
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2023-11-01 15:23:20 +0800

    Added offer type

--- apps/admin-x-settings/src/components/settings/membership/offers/OffersModal.tsx
+++ apps/admin-x-settings/src/components/settings/membership/offers/OffersModal.tsx
@@ -6,12 +6,30 @@ import useFeatureFlag from '../../../../hooks/useFeatureFlag';
 import useRouting from '../../../../hooks/useRouting';
 import {useEffect} from 'react';
 
-const Offer: React.FC<{name: string}> = ({name}) => {
+export type OfferType = 'percent' | 'fixed' | 'trial';
+
+const Offer: React.FC<{name: string, type: OfferType}> = ({name, type}) => {
+    let discountColor = '';
+
+    switch (type) {
+    case 'percent':
+        discountColor = 'text-green';
+        break;
+    case 'fixed':
+        discountColor = 'text-blue';
+        break;
+    case 'trial':
+        discountColor = 'text-pink';
+        break;
+    default:
+        break;
+    }
+
     return <div className='flex flex-col items-center gap-6 border border-transparent bg-grey-100 p-5 text-center transition-all hover:border-grey-100 hover:bg-grey-75 hover:shadow-sm dark:bg-grey-950 dark:hover:border-grey-800'>
         <h2 className='text-[1.6rem]'>{name}</h2>
         <div className=''>
             <div className='flex gap-3 text-sm uppercase leading-none'>
-                <span className='font-semibold text-pink'>10% off</span>
+                <span className={`font-semibold ${discountColor}`}>10% off</span>
                 <span className='text-grey-700 line-through'>$5</span>
             </div>
             <span className='text-3xl font-bold'>$4</span>
@@ -56,9 +74,9 @@ const OffersModal = () => {
                 <h1 className='mt-12 border-b border-b-grey-300 pb-2.5 text-3xl'>Active offers</h1>
             </header>
             <div className='mt-8 grid grid-cols-3 gap-6'>
-                <Offer name='Black friday' />
-                <Offer name='Buy this right now' />
-                <Offer name='Desperate Sale!' />
+                <Offer name='Black friday' type='percent' />
+                <Offer name='Buy this right now' type='fixed' />
+                <Offer name='Desperate Sale!' type='trial' />
             </div>
             <a className='absolute bottom-10 text-sm' href="https://ghost.org/help/offers" rel="noopener noreferrer" target="_blank">→ Learn about offers in Ghost</a>
         </div>
