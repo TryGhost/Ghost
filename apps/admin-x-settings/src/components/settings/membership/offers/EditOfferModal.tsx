f83e78586872a935a7658d7eac1a63e6e878caf7
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2023-11-02 13:21:07 +0800

    Added static design of the Offers edit screen in AdminX

--- apps/admin-x-settings/src/components/settings/membership/offers/EditOfferModal.tsx
+++ apps/admin-x-settings/src/components/settings/membership/offers/EditOfferModal.tsx
@@ -1,8 +1,56 @@
+import Button from '../../../../admin-x-ds/global/Button';
+import Form from '../../../../admin-x-ds/global/form/Form';
 import NiceModal, {useModal} from '@ebay/nice-modal-react';
+import TextArea from '../../../../admin-x-ds/global/form/TextArea';
+import TextField from '../../../../admin-x-ds/global/form/TextField';
 import useFeatureFlag from '../../../../hooks/useFeatureFlag';
 import useRouting from '../../../../hooks/useRouting';
 import {PreviewModalContent} from '../../../../admin-x-ds/global/modal/PreviewModal';
 import {useEffect} from 'react';
+
+const Sidebar: React.FC = () => {
+    return (
+        <div className='pt-7'>
+            <Form>
+                <TextField
+                    hint='Visible to members on Stripe Checkout page.'
+                    placeholder='Black Friday'
+                    title='Name'
+                    value='Black friday'
+                />
+                <section className='mt-4'>
+                    <h2 className='mb-4 text-lg'>Portal Settings</h2>
+                    <div className='flex flex-col gap-6'>
+                        <TextField
+                            placeholder='Black Friday Special'
+                            title='Display title'
+                            value='Black friday'
+                        />
+                        <TextField
+                            placeholder='black-friday'
+                            title='Offer code'
+                            value='black-friday'
+                        />
+                        <TextArea
+                            placeholder='Take advantage of this limited-time offer.'
+                            title='Display description'
+                            value='Get 20% off for Black Friday'
+                        />
+                        <div className='flex flex-col gap-1.5'>
+                            <TextField
+                                placeholder='https://www.example.com'
+                                title='URL'
+                                type='url'
+                                value='https://example.com/#/portal/offers/black-friday'
+                            />
+                            <Button color='green' fullWidth={true} label='Copy link' />
+                        </div>
+                    </div>
+                </section>
+            </Form>
+        </div>
+    );
+};
 
 const EditOfferModal = () => {
     const modal = useModal();
@@ -16,7 +64,9 @@ const EditOfferModal = () => {
         }
     }, [hasOffers, modal, updateRoute]);
 
-    return <PreviewModalContent sidebar={<></>} title='Edit Offer' />;
+    const sidebar = <Sidebar />;
+
+    return <PreviewModalContent deviceSelector={false} okLabel='Update' sidebar={sidebar} size='full' title='Offer' />;
 };
 
 export default NiceModal.create(EditOfferModal);
