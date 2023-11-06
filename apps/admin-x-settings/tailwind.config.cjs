2fe26eecc6218f26252ab3e27aadee2f52a9274a
Jono Mingard <reason.koan@gmail.com>
2023-11-06 08:52:32 +0000

    Updated Tailwind config to work with design system package

---
+++ apps/admin-x-settings/tailwind.config.cjs
@@ -0,0 +1,6 @@
+import adminXPreset from '@tryghost/admin-x-design/tailwind.cjs';
+
+export default {
+    presets: [adminXPreset('.admin-x-settings')],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', '../../node_modules/@tryghost/admin-x-design/**/*.{js,ts,jsx,tsx}']
+};
--- apps/admin-x-settings/tailwind.config.ts
+++
@@ -1,7 +0,0 @@
-import adminXPreset from './src/admin-x-ds/tailwind';
-import {Config} from 'tailwindcss';
-
-export default {
-    presets: [adminXPreset('.admin-x-settings')],
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}']
-} satisfies Config;
