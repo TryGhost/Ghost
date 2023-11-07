1524a8a3e87d81d26caf706bc48d8560f6c20150
Princi Vershwal <vershwal.princi@gmail.com>
2023-11-07 17:54:33 +0530

    Fixed lint error

--- apps/admin-x-settings/src/components/settings/membership/offers/OffersModal.tsx
+++ apps/admin-x-settings/src/components/settings/membership/offers/OffersModal.tsx
@@ -127,7 +127,9 @@ const OffersModal = () => {
                 {offers.filter(offer => offer.status === selectedTab).map((offer) => {
                     const offerTier = paidActiveTiers.find(tier => tier.id === offer?.tier.id);
 
-                    if(!offerTier) return null;
+                    if (!offerTier) {
+                        return null;
+                    }
 
                     return (
                         <OfferCard
