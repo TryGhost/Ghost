7aaa3cdcfb561fc72080215f92980558bdea14cc
Jono Mingard <reason.koan@gmail.com>
2023-11-08 10:18:43 +0000

    Fixed Tailwind config to work in storybook

--- apps/admin-x-design/tailwind.config.cjs
+++ apps/admin-x-design/tailwind.config.cjs
@@ -1,5 +1,8 @@
-// Used for the eslint plugin. Styles should not be compiled directly with this, they should be compiled by calling the function in tailwind.cjs
+// Used for eslint and storybook. Styles should not be compiled directly with this, they should be compiled by calling the function in tailwind.cjs
 module.exports = {
+    content: ['./src/**/*.{js,ts,jsx,tsx}'],
+    important: '.admin-x-design',
+
     corePlugins: {
         preflight: false // we're providing our own scoped CSS reset
     },
