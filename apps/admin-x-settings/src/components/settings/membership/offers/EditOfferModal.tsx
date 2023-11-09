ef6962bb89e0309133e1627191eb995d1d81e66b
Princi Vershwal <vershwal.princi@gmail.com>
2023-11-09 06:22:03 +0530

    Added updated offer functionality

--- apps/admin-x-settings/src/components/settings/membership/offers/EditOfferModal.tsx
+++ apps/admin-x-settings/src/components/settings/membership/offers/EditOfferModal.tsx
@@ -1,20 +1,45 @@
 import NiceModal, {useModal} from '@ebay/nice-modal-react';
 import useFeatureFlag from '../../../../hooks/useFeatureFlag';
 import useRouting from '../../../../hooks/useRouting';
-import {Button, Form, PreviewModalContent, TextArea, TextField} from '@tryghost/admin-x-design-system';
-import {Offer, useBrowseOffersById} from '../../../../api/offers';
+import {Button, Form, PreviewModalContent, TextArea, TextField, showToast} from '@tryghost/admin-x-design-system';
+import {Offer, useBrowseOffersById, useEditOffer} from '../../../../api/offers';
+import {getHomepageUrl} from '../../../../api/site';
 import {RoutingModalProps} from '../../../providers/RoutingProvider';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
+import {useGlobalData} from '../../../providers/GlobalDataProvider';
+import useForm, {ErrorMessages} from '../../../../hooks/useForm';
+import useHandleError from '../../../../utils/api/handleError';
 
-const Sidebar: React.FC<{offer: Offer}> = ({offer}) => {
+const Sidebar: React.FC<{offer: Offer, updateOffer: (fields: Partial<Offer>) => void, validate: () => void,
+    errors: ErrorMessages, clearError: (field: string) => void}> = ({offer, updateOffer, errors, validate, clearError}) => {
+    const {siteData} = useGlobalData();      
+    const [isCopied, setIsCopied] = useState(false);
+    
+    const offerUrl = `${getHomepageUrl(siteData!)}/#/portal/offers/${offer?.code}`;
+    const handleCopyClick = async () => {
+        try {
+            await navigator.clipboard.writeText(offerUrl);
+            setIsCopied(true);
+            setTimeout(() => setIsCopied(false), 1000); // reset after 1 seconds
+        } catch (err) {
+            // eslint-disable-next-line no-console
+            console.error('Failed to copy text: ', err);
+        }
+    };
+
     return (
         <div className='pt-7'>
             <Form>
                 <TextField
-                    hint='Visible to members on Stripe Checkout page.'
+                    error={Boolean(errors.name)}
                     placeholder='Black Friday'
                     title='Name'
                     value={offer?.name}
+                    onChange={e => updateOffer({name: e.target.value})}
+                    onBlur={validate}
+                    hint={errors.name || 'Visible to members on Stripe Checkout page'}
+                    onKeyDown={() => clearError('name')}
+
                 />
                 <section className='mt-4'>
                     <h2 className='mb-4 text-lg'>Portal Settings</h2>
@@ -23,25 +48,35 @@ const Sidebar: React.FC<{offer: Offer}> = ({offer}) => {
                             placeholder='Black Friday Special'
                             title='Display title'
                             value={offer?.display_title}
+                            onChange={e => updateOffer({display_title: e.target.value})}
                         />
                         <TextField
                             placeholder='black-friday'
+                            error={Boolean(errors.code)}
                             title='Offer code'
+                            onBlur={validate}
                             value={offer?.code}
+                            onKeyDown={() => clearError('name')}
+                            hint={errors.code}
+                            onChange={e => {
+                                updateOffer({code: e.target.value})
+                                }}
                         />
                         <TextArea
                             placeholder='Take advantage of this limited-time offer.'
                             title='Display description'
-                            value={offer.display_description}
+                            value={offer?.display_description}
+                            onChange={e => updateOffer({display_description: e.target.value})}
                         />
                         <div className='flex flex-col gap-1.5'>
                             <TextField
                                 placeholder='https://www.example.com'
                                 title='URL'
                                 type='url'
-                                value={`http://localhost:2368//#/portal/offers/${offer?.code}`}
+                                value={offerUrl}
+                                disabled={Boolean(true)}
                             />
-                            <Button color='green' fullWidth={true} label='Copy link' />
+                            <Button color={isCopied ? 'green' : 'black'} label={isCopied ? 'Copied!' : 'Copy code'} onClick={handleCopyClick} />
                         </div>
                     </div>
                 </section>
@@ -53,8 +88,9 @@ const Sidebar: React.FC<{offer: Offer}> = ({offer}) => {
 const EditOfferModal: React.FC<RoutingModalProps> = ({params}) => {
     const modal = useModal();
     const {updateRoute} = useRouting();
+    const handleError = useHandleError();
     const hasOffers = useFeatureFlag('adminXOffers');
-    let offer : Offer;
+    const {mutateAsync: editOffer} = useEditOffer();
 
     useEffect(() => {
         if (!hasOffers) {
@@ -64,19 +100,63 @@ const EditOfferModal: React.FC<RoutingModalProps> = ({params}) => {
     }, [hasOffers, modal, updateRoute]);
 
     const {data: {offers: offerById = []} = {}} = useBrowseOffersById(params?.id ? params?.id : '');
-    if (offerById.length === 0) {
-        return null;
-    }
-    offer = offerById[0];
 
-    const sidebar = <Sidebar
-        offer={offer}
+    const {formState, saveState, updateForm, setFormState, handleSave, validate, errors, clearError, okProps} = useForm({
+        initialState: offerById[0],
+        savingDelay: 500,
+        onSave: async () => {
+            await editOffer(formState);
+        },
+        onSaveError: handleError,
+        onValidate: () => {
+            const newErrors: Record<string, string> = {};
+
+            if (!formState?.name) {
+                newErrors.name = 'Please enter a name';
+            }
+
+            if (!formState?.code) {
+                newErrors.code = 'Please enter a code';
+            }
+
+            return newErrors;
+        }
+    });
+
+    useEffect(() => {
+        setFormState(() => offerById[0]);
+    }, [setFormState, offerById[0]]);
+
+    const updateOffer = (fields: Partial<Offer>) => {
+        updateForm(state => ({...state, ...fields}));
+    };
+
+    const sidebar =  <Sidebar
+        offer={formState}
+        updateOffer={updateOffer}
+        errors={errors}
+        validate={validate}
+        clearError={clearError}
+
     />;
 
-    return <PreviewModalContent deviceSelector={false} okLabel='Update' sidebar={sidebar} size='full' title={offer?.name} onCancel={() => {
-        modal.remove();
-        updateRoute('offers/edit');
-    }} />;
+    return offerById ? <PreviewModalContent deviceSelector={false}
+        onOk={async () => {
+            if (!(await handleSave({fakeWhenUnchanged: true}))) {
+                showToast({
+                    type: 'pageError',
+                    message: 'Can\'t save offer, please double check that you\'ve filled all mandatory fields.'
+                });
+            }
+        }}
+        dirty={saveState === 'unsaved'}
+        okColor={okProps.color}
+        okLabel={okProps.label || 'Save'}
+        sidebar={sidebar} size='full' title='Offer' 
+        onCancel={() => {
+            modal.remove();
+            updateRoute('offers/edit');
+        }} />: null;
 };
 
 export default NiceModal.create(EditOfferModal);
