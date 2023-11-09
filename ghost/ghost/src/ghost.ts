119224e3a59e4acbc485104163a9a6d2a165042a
Fabien "egg" O'Carroll <fabien@allou.is>
2023-11-09 11:22:58 +0700

    slimer new ghost

---
+++ ghost/ghost/src/ghost.ts
@@ -0,0 +1,2 @@
+// Main module file
+export {};
---
+++ ghost/ghost/test/hello.test.ts
@@ -0,0 +1,8 @@
+import assert from 'assert';
+
+describe('Hello world', function () {
+    it('Runs a test', function () {
+        // TODO: Write me!
+        assert.ok(require('../index'));
+    });
+});
