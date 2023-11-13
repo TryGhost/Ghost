2ff2d61f94039b076f620ccbb460cfc66a81c4b6
Sanne de Vries <65487235+sanne-san@users.noreply.github.com>
2023-11-13 16:03:54 +0100

    🎨 Improved accessibility of frontend buttons by adding aria-labels (#1070)
    Refs https://github.com/TryGhost/Ghost/issues/18931

--- ghost/core/core/server/services/koenig/node-renderers/video-renderer.js
+++ ghost/core/core/server/services/koenig/node-renderers/video-renderer.js
@@ -45,7 +45,7 @@ export function cardTemplate({node, cardClasses}) {
                     style="background: transparent url('${thumbnailSrc}') 50% 50% / cover no-repeat;"
                 ></video>
                 <div class="kg-video-overlay">
-                    <button class="kg-video-large-play-icon">
+                    <button class="kg-video-large-play-icon" aria-label="Play video">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                             <path d="M23.14 10.608 2.253.164A1.559 1.559 0 0 0 0 1.557v20.887a1.558 1.558 0 0 0 2.253 1.392L23.14 13.393a1.557 1.557 0 0 0 0-2.785Z"/>
                         </svg>
@@ -53,12 +53,12 @@ export function cardTemplate({node, cardClasses}) {
                 </div>
                 <div class="kg-video-player-container${hideControlsClass}">
                     <div class="kg-video-player">
-                        <button class="kg-video-play-icon">
+                        <button class="kg-video-play-icon" aria-label="Play video">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                 <path d="M23.14 10.608 2.253.164A1.559 1.559 0 0 0 0 1.557v20.887a1.558 1.558 0 0 0 2.253 1.392L23.14 13.393a1.557 1.557 0 0 0 0-2.785Z"></path>
                             </svg>
                         </button>
-                        <button class="kg-video-pause-icon kg-video-hide">
+                        <button class="kg-video-pause-icon kg-video-hide" aria-label="Pause video">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                 <rect x="3" y="1" width="7" height="22" rx="1.5" ry="1.5"></rect>
                                 <rect x="14" y="1" width="7" height="22" rx="1.5" ry="1.5"></rect>
@@ -69,13 +69,13 @@ export function cardTemplate({node, cardClasses}) {
                             /<span class="kg-video-duration">${node.formattedDuration}</span>
                         </div>
                         <input type="range" class="kg-video-seek-slider" max="100" value="0">
-                        <button class="kg-video-playback-rate">1&#215;</button>
-                        <button class="kg-video-unmute-icon">
+                        <button class="kg-video-playback-rate" aria-label="Adjust playback speed">1&#215;</button>
+                        <button class="kg-video-unmute-icon" aria-label="Unmute">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                 <path d="M15.189 2.021a9.728 9.728 0 0 0-7.924 4.85.249.249 0 0 1-.221.133H5.25a3 3 0 0 0-3 3v2a3 3 0 0 0 3 3h1.794a.249.249 0 0 1 .221.133 9.73 9.73 0 0 0 7.924 4.85h.06a1 1 0 0 0 1-1V3.02a1 1 0 0 0-1.06-.998Z"></path>
                             </svg>
                         </button>
-                        <button class="kg-video-mute-icon kg-video-hide">
+                        <button class="kg-video-mute-icon kg-video-hide" aria-label="Mute">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                 <path d="M16.177 4.3a.248.248 0 0 0 .073-.176v-1.1a1 1 0 0 0-1.061-1 9.728 9.728 0 0 0-7.924 4.85.249.249 0 0 1-.221.133H5.25a3 3 0 0 0-3 3v2a3 3 0 0 0 3 3h.114a.251.251 0 0 0 .177-.073ZM23.707 1.706A1 1 0 0 0 22.293.292l-22 22a1 1 0 0 0 0 1.414l.009.009a1 1 0 0 0 1.405-.009l6.63-6.631A.251.251 0 0 1 8.515 17a.245.245 0 0 1 .177.075 10.081 10.081 0 0 0 6.5 2.92 1 1 0 0 0 1.061-1V9.266a.247.247 0 0 1 .073-.176Z"></path>
                             </svg>
