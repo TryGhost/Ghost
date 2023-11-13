227eba1c089160c025fd1460adf741ca84b5bd54
Simon Backx <simon@ghost.org>
2023-11-13 16:04:36 +0100

    Fixed migration

--- ghost/core/core/server/data/migrations/versions/5.73/2023-11-09-14-17-drop-nullable-transient-id-column.js
+++ ghost/core/core/server/data/migrations/versions/5.73/2023-11-09-14-17-drop-nullable-transient-id-column.js
@@ -1,4 +1,4 @@
-const {createSetNullableMigration} = require('../../utils');
+const {createDropNullableMigration} = require('../../utils');
 
 // We need to disable foreign key checks because if MySQL is missing the STRICT_TRANS_TABLES mode, we cannot revert the migration
-module.exports = createSetNullableMigration('members', 'transient_id', {disableForeignKeyChecks: true});
+module.exports = createDropNullableMigration('members', 'transient_id', {disableForeignKeyChecks: true});
