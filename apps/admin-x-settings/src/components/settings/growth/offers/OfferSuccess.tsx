e982c03f9c3e9accd53f92637b3b45c74e7416c6
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-11-15 15:20:01 +0700

    Added copy link label toggle

--- apps/admin-x-settings/src/components/settings/growth/offers/OfferSuccess.tsx
+++ apps/admin-x-settings/src/components/settings/growth/offers/OfferSuccess.tsx
@@ -6,8 +6,8 @@ import {getHomepageUrl} from '@tryghost/admin-x-framework/api/site';
 import {useBrowseOffersById} from '@tryghost/admin-x-framework/api/offers';
 import {useEffect, useState} from 'react';
 import {useGlobalData} from '../../../providers/GlobalDataProvider';
-
 const OfferSuccess: React.FC<RoutingModalProps> = ({params}) => {
+    // const {updateRoute} = useRouting();
     const {data: {offers: offerById = []} = {}} = useBrowseOffersById(params?.id ? params?.id : '');
 
     const [offerLink, setOfferLink] = useState<string>('');
@@ -22,11 +22,13 @@ const OfferSuccess: React.FC<RoutingModalProps> = ({params}) => {
         }
     }, [offerById, siteData]);
 
+    const [isCopied, setIsCopied] = useState(false);
+
     const handleCopyClick = async () => {
         try {
             await navigator.clipboard.writeText(offerLink);
-            // setIsCopied(true);
-            // setTimeout(() => setIsCopied(false), 1000); // reset after 1 seconds
+            setIsCopied(true);
+            setTimeout(() => setIsCopied(false), 1000); // reset after 1 seconds
         } catch (err) {
             // eslint-disable-next-line no-console
             console.error('Failed to copy text: ', err);
@@ -43,7 +45,7 @@ const OfferSuccess: React.FC<RoutingModalProps> = ({params}) => {
             <h1 className='text-4xl'>Your new offer is live!</h1>
             <p className='mt-4 max-w-[510px] text-[1.6rem]'>You can share the link anywhere. In your newsletter, social media, a podcast, or in-person. It all just works.</p>
             <div className='mt-8 flex w-full max-w-md flex-col gap-8'>
-                <Button color='green' label='Copy link' fullWidth onClick={handleCopyClick} />
+                <Button color='green' label={isCopied ? 'Copied!' : 'Copy link'} fullWidth onClick={handleCopyClick} />
                 <div className='flex items-center gap-4 text-xs font-medium before:h-px before:grow before:bg-grey-300 before:content-[""] after:h-px after:grow after:bg-grey-300 after:content-[""]'>OR</div>
                 <div className='flex gap-2'>
                     <Button className='h-8 border border-grey-300' disabled={true} icon='twitter-x' iconColorClass='w-[14px] h-[14px]' size='sm' fullWidth />
