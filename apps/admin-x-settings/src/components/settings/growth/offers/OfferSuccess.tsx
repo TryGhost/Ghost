82e5776d3aedf6c7d17e58a161b954e38b668b86
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-11-15 15:14:24 +0700

    Wired up success modal to Offers x
    refs https://github.com/TryGhost/Product/issues/4148

--- apps/admin-x-settings/src/components/settings/growth/offers/OfferSuccess.tsx
+++ apps/admin-x-settings/src/components/settings/growth/offers/OfferSuccess.tsx
@@ -1,8 +1,38 @@
 import NiceModal from '@ebay/nice-modal-react';
 import {Button} from '@tryghost/admin-x-design-system';
 import {Modal} from '@tryghost/admin-x-design-system';
+import {RoutingModalProps} from '@tryghost/admin-x-framework/routing';
+import {getHomepageUrl} from '@tryghost/admin-x-framework/api/site';
+import {useBrowseOffersById} from '@tryghost/admin-x-framework/api/offers';
+import {useEffect, useState} from 'react';
+import {useGlobalData} from '../../../providers/GlobalDataProvider';
 
-const OfferSuccess = () => {
+const OfferSuccess: React.FC<RoutingModalProps> = ({params}) => {
+    const {data: {offers: offerById = []} = {}} = useBrowseOffersById(params?.id ? params?.id : '');
+
+    const [offerLink, setOfferLink] = useState<string>('');
+
+    const {siteData} = useGlobalData();
+
+    useEffect(() => {
+        if (offerById.length > 0) {
+            const offer = offerById[0];
+            const offerUrl = `${getHomepageUrl(siteData!)}${offer?.code}`;
+            setOfferLink(offerUrl);
+        }
+    }, [offerById, siteData]);
+
+    const handleCopyClick = async () => {
+        try {
+            await navigator.clipboard.writeText(offerLink);
+            // setIsCopied(true);
+            // setTimeout(() => setIsCopied(false), 1000); // reset after 1 seconds
+        } catch (err) {
+            // eslint-disable-next-line no-console
+            console.error('Failed to copy text: ', err);
+        }
+    };
+    
     return <Modal
         footer={false}
         height='full'
@@ -13,12 +43,12 @@ const OfferSuccess = () => {
             <h1 className='text-4xl'>Your new offer is live!</h1>
             <p className='mt-4 max-w-[510px] text-[1.6rem]'>You can share the link anywhere. In your newsletter, social media, a podcast, or in-person. It all just works.</p>
             <div className='mt-8 flex w-full max-w-md flex-col gap-8'>
-                <Button color='green' label='Copy link' fullWidth />
+                <Button color='green' label='Copy link' fullWidth onClick={handleCopyClick} />
                 <div className='flex items-center gap-4 text-xs font-medium before:h-px before:grow before:bg-grey-300 before:content-[""] after:h-px after:grow after:bg-grey-300 after:content-[""]'>OR</div>
                 <div className='flex gap-2'>
-                    <Button className='h-8 border border-grey-300' icon='twitter-x' iconColorClass='w-[14px] h-[14px]' size='sm' fullWidth />
-                    <Button className='h-8 border border-grey-300' icon='facebook' size='sm' fullWidth />
-                    <Button className='h-8 border border-grey-300' icon='linkedin' size='sm' fullWidth />
+                    <Button className='h-8 border border-grey-300' disabled={true} icon='twitter-x' iconColorClass='w-[14px] h-[14px]' size='sm' fullWidth />
+                    <Button className='h-8 border border-grey-300' disabled={true} icon='facebook' size='sm' fullWidth />
+                    <Button className='h-8 border border-grey-300' disabled={true} icon='linkedin' size='sm' fullWidth />
                 </div>
             </div>
         </div>
