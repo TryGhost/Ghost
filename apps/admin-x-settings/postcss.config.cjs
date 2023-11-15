de5a78547b568127d86249f9fa64b316d9d40044
Jono Mingard <reason.koan@gmail.com>
2023-11-15 21:01:00 +0000

    Updated AdminX config boilerplate to be more reusable

--- apps/admin-x-settings/postcss.config.cjs
+++ apps/admin-x-settings/postcss.config.cjs
@@ -1,8 +1,1 @@
-module.exports = {
-    plugins: {
-        'postcss-import': {},
-        'tailwindcss/nesting': {},
-        tailwindcss: {},
-        autoprefixer: {}
-    }
-};
+module.exports = require('@tryghost/admin-x-design-system/postcss.config.cjs');
