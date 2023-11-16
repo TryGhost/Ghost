3d945e539b68ee5a7d69878aefbb6d3bd6f8c148
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2023-11-16 10:09:09 +0800

    UI improvements for Offers in AdminX (#19008)

--- apps/admin-x-settings/src/components/settings/growth/offers/OfferSuccess.tsx
+++ apps/admin-x-settings/src/components/settings/growth/offers/OfferSuccess.tsx
@@ -1,11 +1,13 @@
 import NiceModal from '@ebay/nice-modal-react';
 import {Button} from '@tryghost/admin-x-design-system';
+import {Icon} from '@tryghost/admin-x-design-system';
 import {Modal} from '@tryghost/admin-x-design-system';
 import {RoutingModalProps} from '@tryghost/admin-x-framework/routing';
 import {getHomepageUrl} from '@tryghost/admin-x-framework/api/site';
 import {useBrowseOffersById} from '@tryghost/admin-x-framework/api/offers';
 import {useEffect, useState} from 'react';
 import {useGlobalData} from '../../../providers/GlobalDataProvider';
+
 const OfferSuccess: React.FC<RoutingModalProps> = ({params}) => {
     // const {updateRoute} = useRouting();
     const {data: {offers: offerById = []} = {}} = useBrowseOffersById(params?.id ? params?.id : '');
@@ -34,16 +36,17 @@ const OfferSuccess: React.FC<RoutingModalProps> = ({params}) => {
             console.error('Failed to copy text: ', err);
         }
     };
-    
+
     return <Modal
         footer={false}
         height='full'
         size='lg'
         topRightContent='close'
     >
-        <div className='flex h-full flex-col items-center justify-center text-center'>
-            <h1 className='text-4xl'>Your new offer is live!</h1>
-            <p className='mt-4 max-w-[510px] text-[1.6rem]'>You can share the link anywhere. In your newsletter, social media, a podcast, or in-person. It all just works.</p>
+        <div className='-mt-6 flex h-full flex-col items-center justify-center text-center'>
+            <Icon name='tags-check' size='xl' />
+            <h1 className='mt-6 text-4xl'>Your new offer is live!</h1>
+            <p className='mt-3 max-w-[510px] text-[1.6rem]'>You can share the link anywhere. In your newsletter, social media, a podcast, or in-person. It all just works.</p>
             <div className='mt-8 flex w-full max-w-md flex-col gap-8'>
                 <Button color='green' label={isCopied ? 'Copied!' : 'Copy link'} fullWidth onClick={handleCopyClick} />
                 <div className='flex items-center gap-4 text-xs font-medium before:h-px before:grow before:bg-grey-300 before:content-[""] after:h-px after:grow after:bg-grey-300 after:content-[""]'>OR</div>
