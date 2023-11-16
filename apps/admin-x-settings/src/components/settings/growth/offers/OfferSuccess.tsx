46ca3ab4e50305e83d1f9af46f1b91355fe7a708
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-11-16 13:49:50 +0700

    Fixed linting

--- apps/admin-x-settings/src/components/settings/growth/offers/OfferSuccess.tsx
+++ apps/admin-x-settings/src/components/settings/growth/offers/OfferSuccess.tsx
@@ -1,6 +1,5 @@
 import {Button} from '@tryghost/admin-x-design-system';
 import {Icon} from '@tryghost/admin-x-design-system';
-import {Modal} from '@tryghost/admin-x-design-system';
 import {getHomepageUrl} from '@tryghost/admin-x-framework/api/site';
 import {useBrowseOffersById} from '@tryghost/admin-x-framework/api/offers';
 import {useEffect, useState} from 'react';
@@ -34,24 +33,18 @@ const OfferSuccess: React.FC<{id: string}> = ({id}) => {
         }
     };
 
-    return <Modal
-        footer={false}
-        height='full'
-        size='lg'
-        topRightContent='close'
-    >
-        <div className='-mt-6 flex h-full flex-col items-center justify-center text-center'>
-            <Icon name='tags-check' size='xl' />
-            <h1 className='mt-6 text-4xl'>Your new offer is live!</h1>
-            <p className='mt-3 max-w-[510px] text-[1.6rem]'>You can share the link anywhere. In your newsletter, social media, a podcast, or in-person. It all just works.</p>
-            <div className='mt-8 flex w-full max-w-md flex-col gap-8'>
-                <Button color='green' label={isCopied ? 'Copied!' : 'Copy link'} fullWidth onClick={handleCopyClick} />
-                <div className='flex items-center gap-4 text-xs font-medium before:h-px before:grow before:bg-grey-300 before:content-[""] after:h-px after:grow after:bg-grey-300 after:content-[""]'>OR</div>
-                <div className='flex gap-2'>
-                    <Button className='h-8 border border-grey-300' disabled={true} icon='twitter-x' iconColorClass='w-[14px] h-[14px]' size='sm' fullWidth />
-                    <Button className='h-8 border border-grey-300' disabled={true} icon='facebook' size='sm' fullWidth />
-                    <Button className='h-8 border border-grey-300' disabled={true} icon='linkedin' size='sm' fullWidth />
-                </div>
+    return <div className='-mt-6 flex h-full flex-col items-center justify-center text-center'>
+        <Icon name='tags-check' size='xl' />
+        <h1 className='mt-6 text-4xl'>Your new offer is live!</h1>
+        <p className='mt-3 max-w-[510px] text-[1.6rem]'>You can share the link anywhere. In your newsletter, social media, a podcast, or in-person. It all just works.</p>
+        <div className='mt-8 flex w-full max-w-md flex-col gap-8'>
+            <Button color='green' label={isCopied ? 'Copied!' : 'Copy link'} fullWidth onClick={handleCopyClick} />
+            <div className='flex items-center gap-4 text-xs font-medium before:h-px before:grow before:bg-grey-300 before:content-[""] after:h-px after:grow after:bg-grey-300 after:content-[""]'>OR</div>
+            <div className='flex gap-2'>
+                <Button className='h-8 border border-grey-300' disabled={true} icon='twitter-x' iconColorClass='w-[14px] h-[14px]' size='sm' fullWidth />
+                <Button className='h-8 border border-grey-300' disabled={true} icon='facebook' size='sm' fullWidth />
+                <Button className='h-8 border border-grey-300' disabled={true} icon='linkedin' size='sm' fullWidth />
+            </div>
         </div>
     </div>;
 };
