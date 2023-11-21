378157c3c2f6e9ec33fa1355536172d70420d9d6
Peter Zimon <peter.zimon@gmail.com>
2023-11-21 14:55:01 +0100

    Updateed demo content

--- apps/admin-x-demo/src/MainContent.tsx
+++ apps/admin-x-demo/src/MainContent.tsx
@@ -1,12 +1,62 @@
-import {Button} from '@tryghost/admin-x-design-system';
+import {Button, ButtonGroup, Page, ViewContainer} from '@tryghost/admin-x-design-system';
 import {useRouting} from '@tryghost/admin-x-framework/routing';
 
 const MainContent = () => {
     const {updateRoute} = useRouting();
 
-    return <div>
-        <Button label='Open modal' onClick={() => updateRoute('demo-modal')} />
-    </div>;
+    const dummyActions = [
+        <Button label='Filter' link onClick={() => {
+            alert('Clicked filter');
+        }} />,
+        <Button label='Sort' link onClick={() => {
+            alert('Clicked sort');
+        }} />,
+        <Button icon='magnifying-glass' size='sm' link onClick={() => {
+            alert('Clicked search');
+        }} />,
+        <ButtonGroup buttons={[
+            {
+                icon: 'listview',
+                size: 'sm',
+                link: true,
+                iconColorClass: 'text-black',
+                onClick: () => {
+                    alert('Clicked list view');
+                }
+            },
+            {
+                icon: 'cardview',
+                size: 'sm',
+                link: true,
+                iconColorClass: 'text-grey-500',
+                onClick: () => {
+                    alert('Clicked card view');
+                }
+            }
+        ]} />
+    ];
+
+    const demoPage = (
+        <Page>
+            <ViewContainer
+                // stickyHeader={false}
+                actions={dummyActions}
+                firstOnPage={true}
+                primaryAction={{
+                    title: 'Add item',
+                    onClick: () => {
+                        updateRoute('demo-modal');
+                    }
+                }}
+                title='Demo page'
+                type='page'
+            >
+                <div>hello</div>
+            </ViewContainer>
+        </Page>
+    );
+
+    return demoPage;
 };
 
 export default MainContent;
