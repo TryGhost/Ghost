a5d4a39b616970485b16aa7745b28c2eaa03387d
Peter Zimon <peter.zimon@gmail.com>
2023-11-22 16:31:52 +0100

    Updated detail example mobile view

--- apps/admin-x-design-system/src/global/layout/Page.stories.tsx
+++ apps/admin-x-design-system/src/global/layout/Page.stories.tsx
@@ -357,7 +357,10 @@ export const ExampleDetailScreen: Story = {
         breadCrumbs: <Breadcrumbs
             items={[
                 {
-                    label: 'Members'
+                    label: 'Members',
+                    onClick: () => {
+                        alert('Clicked back');
+                    }
                 },
                 {
                     label: 'Emerson Vaccaro'
@@ -384,25 +387,25 @@ export const ExampleDetailScreen: Story = {
                 }
                 type='page'
             >
-                <div className='grid grid-cols-4 border-b border-grey-200 pb-5'>
-                    <div className='-ml-5 flex h-full flex-col px-5'>
+                <div className='grid grid-cols-3 border-b border-grey-200 pb-5 tablet:grid-cols-4'>
+                    <div className='col-span-3 -ml-5 mb-5 hidden h-full gap-4 px-5 tablet:col-span-1 tablet:mb-0 tablet:!flex tablet:flex-col tablet:gap-0'>
                         <span>Last seen on <strong>22 June 2023</strong></span>
-                        <span className='mt-2'>Created on <strong>27 Jan 2021</strong></span>
+                        <span className='tablet:mt-2'>Created on <strong>27 Jan 2021</strong></span>
                     </div>
-                    <div className='flex h-full flex-col px-5'>
+                    <div className='flex h-full flex-col tablet:px-5'>
                         <Heading level={6}>Emails received</Heading>
                         <span className='mt-1 text-4xl font-bold leading-none'>181</span>
                     </div>
-                    <div className='flex h-full flex-col px-5'>
+                    <div className='flex h-full flex-col tablet:px-5'>
                         <Heading level={6}>Emails opened</Heading>
                         <span className='mt-1 text-4xl font-bold leading-none'>104</span>
                     </div>
-                    <div className='-mr-5 flex h-full flex-col px-5'>
+                    <div className='-mr-5 flex h-full flex-col tablet:px-5'>
                         <Heading level={6}>Average open rate</Heading>
                         <span className='mt-1 text-4xl font-bold leading-none'>57%</span>
                     </div>
                 </div>
-                <div className='grid grid-cols-4 items-baseline py-5'>
+                <div className='grid grid-cols-2 items-baseline border-b border-grey-200 py-5 tablet:grid-cols-4'>
                     <div className='-ml-5 flex h-full flex-col gap-6 border-r border-grey-200 px-5'>
                         <div className='flex justify-between'>
                             <Heading level={5}>Member data</Heading>
@@ -418,10 +421,17 @@ export const ExampleDetailScreen: Story = {
                         </div>
                         <div>
                             <Heading level={6}>Labels</Heading>
-                            <div className='inline-block rounded-sm bg-grey-300 px-1 text-xs font-medium'>VIP</div>
+                            <div className='mt-2 flex gap-1'>
+                                <div className='inline-block rounded-sm bg-grey-200 px-1.5 text-xs font-medium'>VIP</div>
+                                <div className='inline-block rounded-sm bg-grey-200 px-1.5 text-xs font-medium'>Inner Circle</div>
+                            </div>
+                        </div>
+                        <div>
+                            <Heading level={6}>Notes</Heading>
+                            <div className='text-grey-500'>No notes.</div>
                         </div>
                     </div>
-                    <div className='flex h-full flex-col gap-6 border-r border-grey-200 px-5'>
+                    <div className='flex h-full flex-col gap-6 border-grey-200 px-5 tablet:border-r'>
                         <Heading level={5}>Newsletters</Heading>
                         <div className='flex flex-col gap-3'>
                             <div className='flex items-center gap-2'>
@@ -432,24 +442,44 @@ export const ExampleDetailScreen: Story = {
                                 <Toggle />
                                 <span>Weekly roundup</span>
                             </div>
+                            <div className='flex items-center gap-2'>
+                                <Toggle checked />
+                                <span>The Inner Circle</span>
+                            </div>
+                            <div className='mt-5 rounded border border-red p-4 text-sm text-red'>
+                                This member cannot receive emails due to permanent failure (bounce).
+                            </div>
                         </div>
                     </div>
-                    <div className='flex h-full flex-col gap-6 border-r border-grey-200 px-5'>
+                    <div className='-ml-5 flex h-full flex-col gap-6 border-r border-grey-200 px-5 pt-10 tablet:ml-0 tablet:pt-0'>
                         <Heading level={5}>Subscriptions</Heading>
-                        <div className='flex flex-col'>
-                            <span className='font-semibold'>Gold &mdash; $12/month</span>
-                            <span className='text-sm text-grey-500'>Renews 21 Jan 2024</span>
+                        <div className='flex items-center gap-3'>
+                            <div className='flex h-16 w-16 flex-col items-center justify-center rounded-md bg-grey-200'>
+                                <Heading level={5}>$5</Heading>
+                                <span className='text-xs text-grey-700'>Yearly</span>
+                            </div>
+                            <div className='flex flex-col'>
+                                <span className='font-semibold'>Gold</span>
+                                <span className='text-sm text-grey-500'>Renews 21 Jan 2024</span>
+                            </div>
                         </div>
                     </div>
-                    <div className='-mr-5 flex h-full flex-col gap-6 px-5'>
-                        <Heading level={5}>Activity</Heading>
-                        <div className='flex flex-col'>
-                            <span className='font-semibold'>Logged in</span>
-                            <span className='text-sm text-grey-500'>Renews 21 Jan 2024</span>
+                    <div className='-mr-5 flex h-full flex-col gap-6 px-5 pt-10 tablet:pt-0'>
+                        <div className='flex justify-between'>
+                            <Heading level={5}>Activity</Heading>
+                            <Button color='green' label='View all' link />
                         </div>
-                        <div className='flex flex-col'>
+                        <div className='flex flex-col text-sm'>
+                            <span className='font-semibold'>Logged in</span>
+                            <span className='text-sm text-grey-500'>13 days ago</span>
+                        </div>
+                        <div className='flex flex-col text-sm'>
                             <span className='font-semibold'>Subscribed to Daily News</span>
-                            <span className='text-sm text-grey-500'>Renews 21 Jan 2024</span>
+                            <span className='text-sm text-grey-500'>17 days ago</span>
+                        </div>
+                        <div className='flex flex-col text-sm'>
+                            <span className='font-semibold'>Logged in</span>
+                            <span className='text-sm text-grey-500'>21 days ago</span>
                         </div>
                     </div>
                 </div>
