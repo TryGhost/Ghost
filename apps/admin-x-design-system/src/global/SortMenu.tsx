9ae45adda18055a622500fff65b54f1c65018cd3
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2023-11-28 17:32:13 +0800

    Added sorting direction option to individual sort option (#19154)
    no issues
    
    - SortMenu component used to have only one global default direction
    option, however, the sorting options needed individual default sorting
    direction. e.g. desc for created data, or asc for name etc
    - this adds an optional sorting direction option to sorting options, so
    when they're defined, they'd override the global default sorting
    direction

--- apps/admin-x-design-system/src/global/SortMenu.tsx
+++ apps/admin-x-design-system/src/global/SortMenu.tsx
@@ -9,6 +9,7 @@ export type SortItem = {
     id: string,
     label: string;
     selected?: boolean;
+    direction?: SortDirection;
 }
 
 export interface SortMenuProps {
@@ -31,7 +32,7 @@ const SortMenu: React.FC<SortMenuProps> = ({
     position = 'left'
 }) => {
     const [localItems, setLocalItems] = useState<SortItem[]>(items);
-    const [localDirection, setLocalDirection] = useState<string>(direction || 'desc');
+    const [localDirection, setLocalDirection] = useState<SortDirection>(direction || 'desc');
 
     useEffect(() => {
         setLocalItems(items);
@@ -44,10 +45,16 @@ const SortMenu: React.FC<SortMenuProps> = ({
         }));
         setLocalItems(updatedItems);
 
+        if (localItems.find(item => item.id === selectedValue)?.direction) {
+            setLocalDirection(localItems.find(item => item.id === selectedValue)?.direction || 'desc');
+            onDirectionChange(localDirection);
+        }
+
         onSortChange(selectedValue);
     };
 
-    const handleSortDirection = () => {
+    const handleSortDirection = (e?: React.MouseEvent<HTMLElement, MouseEvent>) => {
+        e?.stopPropagation();
         setLocalDirection(currentDirection => (currentDirection === 'desc' ? 'asc' : 'desc'));
         onDirectionChange(localDirection);
     };
