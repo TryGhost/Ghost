a31660a6cb955abb118599d379b475711556b82b
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-11-29 12:26:08 +0200

    Uncomment initial tests
    no issue

--- apps/admin-x-settings/test/acceptance/membership/offers.test.ts
+++ apps/admin-x-settings/test/acceptance/membership/offers.test.ts
@@ -7,41 +7,41 @@ test.describe('Offers Modal', () => {
         toggleLabsFlag('adminXOffers', true);
     });
 
-    // test('Offers Modal is available', async ({page}) => {
-    //     await mockApi({page, requests: {
-    //         ...globalDataRequests,
-    //         browseSettings: {...globalDataRequests.browseSettings, response: settingsWithStripe}
-    //     }});
-    //     await page.goto('/');
-    //     const section = page.getByTestId('offers');
-    //     await section.getByRole('button', {name: 'Manage offers'}).click();
-    //     const modal = page.getByTestId('offers-modal');
-    //     await expect(modal).toBeVisible();
-    // });
+    test('Offers Modal is available', async ({page}) => {
+        await mockApi({page, requests: {
+            ...globalDataRequests,
+            browseSettings: {...globalDataRequests.browseSettings, response: settingsWithStripe}
+        }});
+        await page.goto('/');
+        const section = page.getByTestId('offers');
+        await section.getByRole('button', {name: 'Manage offers'}).click();
+        const modal = page.getByTestId('offers-modal');
+        await expect(modal).toBeVisible();
+    });
 
-    // test('Offers Add Modal is available', async ({page}) => {
-    //     await mockApi({page, requests: {
-    //         ...globalDataRequests,
-    //         browseSettings: {...globalDataRequests.browseSettings, response: settingsWithStripe},
-    //         browseOffers: {method: 'GET', path: '/offers/?limit=all', response: responseFixtures.offers},
-    //         browseTiers: {method: 'GET', path: '/tiers/', response: responseFixtures.tiers},
-    //         addOffer: {method: 'POST', path: '/offers/', response: {
-    //             offers: [{
-    //                 id: 'new-offer',
-    //                 name: 'New offer',
-    //                 code: 'new-offer'
-    //             }]
-    //         }}
-    //     }});
+    test('Offers Add Modal is available', async ({page}) => {
+        await mockApi({page, requests: {
+            ...globalDataRequests,
+            browseSettings: {...globalDataRequests.browseSettings, response: settingsWithStripe},
+            browseOffers: {method: 'GET', path: '/offers/?limit=all', response: responseFixtures.offers},
+            browseTiers: {method: 'GET', path: '/tiers/', response: responseFixtures.tiers},
+            addOffer: {method: 'POST', path: '/offers/', response: {
+                offers: [{
+                    id: 'new-offer',
+                    name: 'New offer',
+                    code: 'new-offer'
+                }]
+            }}
+        }});
 
-    //     await page.goto('/');
-    //     const section = page.getByTestId('offers');
-    //     await section.getByRole('button', {name: 'Manage offers'}).click();
-    //     const modal = page.getByTestId('offers-modal');
-    //     await modal.getByRole('button', {name: 'New offer'}).click();
-    //     const addModal = page.getByTestId('add-offer-modal');
-    //     await expect(addModal).toBeVisible();
-    // });
+        await page.goto('/');
+        const section = page.getByTestId('offers');
+        await section.getByRole('button', {name: 'Manage offers'}).click();
+        const modal = page.getByTestId('offers-modal');
+        await modal.getByRole('button', {name: 'New offer'}).click();
+        const addModal = page.getByTestId('add-offer-modal');
+        await expect(addModal).toBeVisible();
+    });
 
     test('Can add a new offer', async ({page}) => {
         const {lastApiRequests} = await mockApi({page, requests: {
