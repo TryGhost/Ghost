c736da63736ecc3263ec2b5f6db91c65f890c175
Fabien "egg" O'Carroll <fabien@allou.is>
2023-12-05 15:54:16 +0700

    Removed bookshelf repository

--- ghost/ghost/src/nestjs/AppModule.ts
+++ ghost/ghost/src/nestjs/AppModule.ts
@@ -1,5 +1,4 @@
 import {SnippetsController} from '../http/controllers/snippets.controller';
-import {SnippetRepositoryBookshelf} from '../core/snippets/snippets.repository.bookshelf';
 import {SnippetsService} from '../core/snippets/snippets.service';
 import {KnexSnippetsRepository} from '../db/knex/snippets.repository';
 
