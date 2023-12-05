bf8121277460fb9e35bb202bc9e9c7d7779ad944
Chris Raible <chris@ghost.org>
2023-12-04 16:39:03 -0800

    Added performance reporter

--- ghost/core/test/perf-api/admin/newsletters.perf.test.js
+++ ghost/core/test/perf-api/admin/newsletters.perf.test.js
@@ -1,37 +1,30 @@
-const should = require('should');
-const { Benchmark } = require('kelonio');
-const { agentProvider, fixtureManager } = require('../../utils/e2e-framework');
-
-const supertest = require('supertest');
-const testUtils = require('../../utils');
-
+const {benchmark} = require('kelonio');
+const {agentProvider, fixtureManager} = require('../../utils/e2e-framework');
 
 describe('Newsletters Performance', function () {
     let agent;
-    let benchmark;
 
     before(async function () {
         agent = await agentProvider.getAdminAPIAgent();
         await fixtureManager.init('newsletters', 'members:newsletters');
         await agent.loginAsOwner();
-        benchmark = new Benchmark();
     });
 
     it('browse newsletters', async function () {
         await benchmark.record(async () => {
-            await agent.get(`newsletters`)
-        }, { iterations: 10, meanUnder: 10});
+            await agent.get(`newsletters`).expectStatus(200);
+        }, {iterations: 10, meanUnder: 3});
     });
 
     it('browse newsletters with active member counts', async function () {
         await benchmark.record(async () => {
-            await agent.get(`newsletters/include=count.active_members`);
-        }, { iterations: 10, meanUnder: 1000});
+            await agent.get(`newsletters/?include=count.active_members`).expectStatus(200);
+        }, {iterations: 10, meanUnder: 10});
     });
 
     it('browse newsletters with total post counts', async function () {
         await benchmark.record(async () => {
-            await agent.get(`newsletters/include=count.posts`);
-        }, { iterations: 10, meanUnder: 1000});
+            await agent.get(`newsletters/?include=count.posts`).expectStatus(200);
+        }, {iterations: 10, meanUnder: 10});
     });
 });
