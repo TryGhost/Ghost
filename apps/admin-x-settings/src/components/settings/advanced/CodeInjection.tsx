14959b6ea0da6c5a7c67f8d301a56082f73cac66
Ronald Langeveld <hi@ronaldlangeveld.com>
2023-12-06 14:29:54 +0200

    Fixed code injection
    refs PROD-7 https://ghost.slack.com/archives/C0568LN2CGJ/p1701690959138659
    
    - we decided to replace the mini editor with a full screen editor to add
      code to inject to your webpages.

--- apps/admin-x-settings/src/components/settings/advanced/CodeInjection.tsx
+++ apps/admin-x-settings/src/components/settings/advanced/CodeInjection.tsx
@@ -1,93 +1,26 @@
 import CodeModal from './code/CodeModal';
 import NiceModal from '@ebay/nice-modal-react';
-import React, {useMemo, useRef, useState} from 'react';
+import React from 'react';
 import TopLevelGroup from '../../TopLevelGroup';
-import useSettingGroup from '../../../hooks/useSettingGroup';
-import {Button, CodeEditor, TabView, withErrorBoundary} from '@tryghost/admin-x-design-system';
-import {ReactCodeMirrorRef} from '@uiw/react-codemirror';
-import {getSettingValues} from '@tryghost/admin-x-framework/api/settings';
+import {Button, SettingGroupHeader, withErrorBoundary} from '@tryghost/admin-x-design-system';
 
 const CodeInjection: React.FC<{ keywords: string[] }> = ({keywords}) => {
-    const {
-        localSettings,
-        isEditing,
-        saveState,
-        handleSave,
-        handleCancel,
-        updateSetting,
-        handleEditingChange
-    } = useSettingGroup();
-
-    const [headerContent, footerContent] = getSettingValues<string>(localSettings, ['codeinjection_head', 'codeinjection_foot']);
-
-    const [selectedTab, setSelectedTab] = useState<'header' | 'footer'>('header');
-
-    const headerEditorRef = useRef<ReactCodeMirrorRef>(null);
-    const footerEditorRef = useRef<ReactCodeMirrorRef>(null);
-
-    const html = useMemo(() => import('@codemirror/lang-html').then(module => module.html()), []);
-
-    const headerProps = {
-        extensions: [html],
-        hint: 'Code here will be injected into the {{ghost_head}} tag on every page of the site',
-        value: headerContent || '',
-        onChange: (value: string) => updateSetting('codeinjection_head', value)
-    };
-
-    const footerProps = {
-        extensions: [html],
-        hint: 'Code here will be injected into the {{ghost_foot}} tag on every page of the site',
-        value: footerContent || '',
-        onChange: (value: string) => updateSetting('codeinjection_foot', value)
-    };
-
-    const tabs = [
-        {
-            id: 'header',
-            title: 'Site header',
-            contents: (<CodeEditor {...headerProps} ref={headerEditorRef} className='mt-2' data-testid='header-code' autoFocus />)
-        },
-        {
-            id: 'footer',
-            title: 'Site footer',
-            contents: (<CodeEditor {...footerProps} ref={footerEditorRef} className='mt-2' data-testid='footer-code' />)
-        }
-    ] as const;
-
     return (
         <TopLevelGroup
+            customHeader={
+                <div className='z-10 flex items-start justify-between'>
+                    <SettingGroupHeader description='Add custom code to your publication.' title='Code injection' />
+                    <Button color='green' label='Open' link linkWithPadding onClick={() => {
+                        NiceModal.show(CodeModal);
+                    }} />
+                </div>
+            }
             description="Add custom code to your publication"
-            isEditing={isEditing}
             keywords={keywords}
             navid='code-injection'
-            saveState={saveState}
-            testId='code-injection'
+            testId='codeinjection'
             title="Code injection"
-            onCancel={handleCancel}
-            onEditingChange={handleEditingChange}
-            onSave={handleSave}
-        >
-            {isEditing && (
-                <div className='relative'>
-                    <TabView<'header' | 'footer'> selectedTab={selectedTab} tabs={tabs} onTabChange={setSelectedTab} />
-                    <Button
-                        className='absolute right-0 top-1 text-sm'
-                        label='Fullscreen'
-                        unstyled
-                        onClick={() => NiceModal.show(CodeModal, {
-                            ...(selectedTab === 'header' ? headerProps : footerProps),
-                            afterClose: () => {
-                                if (selectedTab === 'header') {
-                                    headerEditorRef.current?.view?.focus();
-                                } else {
-                                    footerEditorRef.current?.view?.focus();
-                                }
-                            }
-                        })}
-                    />
-                </div>
-            )}
-        </TopLevelGroup>
+        />
     );
 };
 
--- apps/admin-x-settings/src/components/settings/advanced/code/CodeModal.tsx
+++ apps/admin-x-settings/src/components/settings/advanced/code/CodeModal.tsx
@@ -1,6 +1,9 @@
 import NiceModal, {useModal} from '@ebay/nice-modal-react';
-import React, {useMemo} from 'react';
-import {CodeEditor, Modal} from '@tryghost/admin-x-design-system';
+import React, {useMemo, useRef, useState} from 'react';
+import useSettingGroup from '../../../../hooks/useSettingGroup';
+import {CodeEditor, Modal, TabView} from '@tryghost/admin-x-design-system';
+import {ReactCodeMirrorRef} from '@uiw/react-codemirror';
+import {getSettingValues} from '@tryghost/admin-x-framework/api/settings';
 
 interface CodeModalProps {
     hint?: React.ReactNode;
@@ -9,18 +12,72 @@ interface CodeModalProps {
     afterClose?: () => void
 }
 
-const CodeModal: React.FC<CodeModalProps> = ({hint, value, onChange, afterClose}) => {
+const CodeModal: React.FC<CodeModalProps> = ({afterClose}) => {
+    const {
+        localSettings,
+        handleSave,
+        updateSetting
+    } = useSettingGroup();
     const modal = useModal();
+
+    const [headerContent, footerContent] = getSettingValues<string>(localSettings, ['codeinjection_head', 'codeinjection_foot']);
+
+    const [selectedTab, setSelectedTab] = useState<'header' | 'footer'>('header');
+
+    const headerEditorRef = useRef<ReactCodeMirrorRef>(null);
+    const footerEditorRef = useRef<ReactCodeMirrorRef>(null);
 
     const html = useMemo(() => import('@codemirror/lang-html').then(module => module.html()), []);
 
+    const headerProps = {
+        extensions: [html],
+        hint: 'Code here will be injected into the {{ghost_head}} tag on every page of the site',
+        value: headerContent || '',
+        onChange: (value: string) => updateSetting('codeinjection_head', value)
+    };
+
+    const footerProps = {
+        extensions: [html],
+        hint: 'Code here will be injected into the {{ghost_foot}} tag on every page of the site',
+        value: footerContent || '',
+        onChange: (value: string) => updateSetting('codeinjection_foot', value)
+    };
+
+    const tabs = [
+        {
+            id: 'header',
+            title: 'Site header',
+            contents: (<CodeEditor height='full' {...headerProps} ref={headerEditorRef} className='mt-2' data-testid='header-code' autoFocus />)
+        },
+        {
+            id: 'footer',
+            title: 'Site footer',
+            contents: (<CodeEditor height='full' {...footerProps} ref={footerEditorRef} className='mt-2' data-testid='footer-code' />)
+        }
+    ] as const;
+
     const onOk = () => {
         modal.remove();
+        handleSave();
         afterClose?.();
     };
 
-    return <Modal afterClose={afterClose} cancelLabel='' okColor='grey' okLabel='Done' size='full' testId='modal-code' onOk={onOk}>
-        <CodeEditor extensions={[html]} height='full' hint={hint} value={value} autoFocus onChange={onChange} />
+    return <Modal
+        afterClose={afterClose}
+        cancelLabel='Close'
+        height='full'
+        okColor='grey'
+        okLabel='Save'
+        size='full'
+        testId='modal-code'
+        title='Code injection'
+        onOk={onOk}
+    >
+        <TabView<'header' | 'footer'>
+            selectedTab={selectedTab}
+            tabs={tabs}
+            onTabChange={setSelectedTab}
+        />
     </Modal>;
 };
 
