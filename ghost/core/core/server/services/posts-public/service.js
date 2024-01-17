9d04a3f2e9e4784daee0d2648e06c9707dcf0ae9
Simon Backx <git@simonbackx.com>
2024-01-17 14:02:32 +0100

    Revert "Added support for "Refresh Ahead" caching strategy"

--- ghost/core/core/server/services/posts-public/service.js
+++ ghost/core/core/server/services/posts-public/service.js
@@ -8,18 +8,34 @@ class PostsPublicServiceWrapper {
         // Wire up all the dependencies
         const adapterManager = require('../adapter-manager');
         const config = require('../../../shared/config');
+        const EventAwareCacheWrapper = require('@tryghost/event-aware-cache-wrapper');
         const EventRegistry = require('../../lib/common/events');
 
         let postsCache;
         if (config.get('hostSettings:postsPublicCache:enabled')) {
-            postsCache = adapterManager.getAdapter('cache:postsPublic');
-            EventRegistry.on('site.changed', () => {
-                postsCache.reset();
+            const cache = adapterManager.getAdapter('cache:postsPublic');
+            postsCache = new EventAwareCacheWrapper({
+                cache: cache,
+                resetEvents: ['site.changed'],
+                eventRegistry: EventRegistry
             });
         }
 
+        let cache;
+        if (postsCache) {
+            // @NOTE: exposing cache through getter and setter to not loose the context of "this"
+            cache = {
+                get() {
+                    return postsCache.get(...arguments);
+                },
+                set() {
+                    return postsCache.set(...arguments);
+                }
+            };
+        }
+
         this.api = {
-            cache: postsCache
+            cache: cache
         };
     }
 }
--- ghost/core/core/server/services/tags-public/service.js
+++ ghost/core/core/server/services/tags-public/service.js
@@ -8,18 +8,33 @@ class TagsPublicServiceWrapper {
         // Wire up all the dependencies
         const adapterManager = require('../adapter-manager');
         const config = require('../../../shared/config');
+        const EventAwareCacheWrapper = require('@tryghost/event-aware-cache-wrapper');
         const EventRegistry = require('../../lib/common/events');
 
         let tagsCache;
         if (config.get('hostSettings:tagsPublicCache:enabled')) {
-            tagsCache = adapterManager.getAdapter('cache:tagsPublic');
-            EventRegistry.on('site.changed', () => {
-                tagsCache.reset();
+            let tagsPublicCache = adapterManager.getAdapter('cache:tagsPublic');
+            tagsCache = new EventAwareCacheWrapper({
+                cache: tagsPublicCache,
+                resetEvents: ['site.changed'],
+                eventRegistry: EventRegistry
             });
         }
 
+        let cache;
+        if (tagsCache) {
+            // @NOTE: exposing cache through getter and setter to not loose the context of "this"
+            cache = {
+                get() {
+                    return tagsCache.get(...arguments);
+                },
+                set() {
+                    return tagsCache.set(...arguments);
+                }
+            };
+        }
         this.api = {
-            cache: tagsCache
+            cache: cache
         };
     }
 }
