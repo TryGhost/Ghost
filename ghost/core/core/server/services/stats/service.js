c9ec37d960f501351f8685d4c63ea6e4d46b1d67
Simon Backx <simon@ghost.org>
2024-01-18 15:26:49 +0100

    Added cache config to stats endpoints (#19481)
    no issue
    
    Allows to enable cache via hostSettings.statsCache.enabled. This will
    need proper cache timeouts in order to function correctly.
    
    Usage in config:
    ```
    "hostSettings": {
            "statsCache": {
                "enabled": true
            }
        },
        "adapters": {
            "cache": {
                "Redis": {
                    "host": "127.0.0.1",
                    "port": 6379,
                    "username": "",
                    "password": "",
                    "ttl": 60,
                    "storeConfig": {
                        "maxRetriesPerRequest": 1,
                        "enableOfflineQueue": false,
                        "retryConnectSeconds": 60
                    }
                },
                "stats": {
                    "adapter": "Redis",
                    "ttl": 3600,
                    "refreshAheadFactor": 1,
                    "keyPrefix": "site:123456:stats"
                }
            }
        },
        ```

--- ghost/core/core/server/services/stats/service.js
+++ ghost/core/core/server/services/stats/service.js
@@ -1,4 +1,27 @@
-const StatsService = require('@tryghost/stats-service');
-const db = require('../../data/db');
+class StatsServiceWrapper {
+    constructor() {
+        this.api = null;
+        this.cache = null;
+    }
 
-module.exports = StatsService.create({knex: db.knex});
+    async init() {
+        if (this.api) {
+            // Already done
+            return;
+        }
+
+        const StatsService = require('@tryghost/stats-service');
+        const db = require('../../data/db');
+
+        this.api = StatsService.create({knex: db.knex});
+
+        const adapterManager = require('../adapter-manager');
+        const config = require('../../../shared/config');
+
+        if (config.get('hostSettings:statsCache:enabled')) {
+            this.cache = adapterManager.getAdapter('cache:stats');
+        }
+    }
+}
+
+module.exports = new StatsServiceWrapper();
