e4f60ee028996804e0efb78be5299eb7a5858c5e
Sag <guptazy@gmail.com>
2024-01-24 12:16:26 +0100

    Fixed member subscription status when multiple subs (#19530)
    fixes PROD-325
    - if a member has multiple subscriptions, show the status of the active
    subscription
    - if a member has multiple active subscriptins, show the status of the
    subscription with the latest current_period_end date

--- ghost/admin/app/components/members/filters/plan-interval.js
+++ ghost/admin/app/components/members/filters/plan-interval.js
@@ -1,11 +1,11 @@
 import {MATCH_RELATION_OPTIONS} from './relation-options';
 import {capitalizeFirstLetter} from 'ghost-admin/helpers/capitalize-first-letter';
-import {mostRecentlyUpdated} from 'ghost-admin/helpers/most-recently-updated';
+import {mostRelevantSubscription} from 'ghost-admin/helpers/most-relevant-subscription';
 
 export const PLAN_INTERVAL_FILTER = {
-    label: 'Billing period', 
-    name: 'subscriptions.plan_interval', 
-    columnLabel: 'Billing period', 
+    label: 'Billing period',
+    name: 'subscriptions.plan_interval',
+    columnLabel: 'Billing period',
     relationOptions: MATCH_RELATION_OPTIONS,
     valueType: 'options',
     options: [
@@ -13,7 +13,7 @@ export const PLAN_INTERVAL_FILTER = {
         {label: 'Yearly', name: 'year'}
     ],
     getColumnValue: (member) => {
-        const subscription = mostRecentlyUpdated(member.subscriptions);
+        const subscription = mostRelevantSubscription(member.subscriptions);
         if (!subscription) {
             return null;
         }
