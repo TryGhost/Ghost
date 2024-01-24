4c38f53283e397c61818bab1412e0cd0e694d557
Princi Vershwal <vershwal.princi@gmail.com>
2024-01-24 19:23:37 +0530

    Optimised iframe for offer

--- apps/admin-x-settings/src/components/settings/membership/portal/PortalFrame.tsx
+++ apps/admin-x-settings/src/components/settings/membership/portal/PortalFrame.tsx
@@ -20,7 +20,7 @@ const PortalFrame: React.FC<PortalFrameProps> = ({href, onDestroyed, selectedTab
             if (iframeRef.current) {
                 setIsInvisible(false);
             }
-        }, 300);
+        }, 500);
     }, [iframeRef]);
 
     useEffect(() => {
