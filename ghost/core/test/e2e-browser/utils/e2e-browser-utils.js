b49053498340228f60925564147abfa73af05270
Ronald Langeveld <hi@ronaldlangeveld.com>
2024-01-28 19:04:39 +0000

    Fixed flaky portal tests (#19596)
    no issue

--- ghost/core/test/e2e-browser/utils/e2e-browser-utils.js
+++ ghost/core/test/e2e-browser/utils/e2e-browser-utils.js
@@ -271,7 +271,7 @@ const createOffer = async (page, {name, tierName, offerType, amount, discountTyp
         await page.getByTestId('offer-item').nth(0).click();
         await page.getByRole('button', {name: 'Archive offer'}).click();
 
-        const confirmModal = page.getByTestId('confirmation-modal');
+        const confirmModal = await page.getByTestId('confirmation-modal');
         await confirmModal.getByRole('button', {name: 'Archive'}).click();
     }
 
@@ -320,7 +320,7 @@ const fillInputIfExists = async (page, selector, value) => {
 
 const completeStripeSubscription = async (page) => {
     await page.locator('#cardNumber').fill('4242 4242 4242 4242');
-    await page.locator('#cardExpiry').fill('04 / 24');
+    await page.locator('#cardExpiry').fill('04 / 26');
     await page.locator('#cardCvc').fill('424');
     await page.locator('#billingName').fill('Testy McTesterson');
     await page.getByRole('combobox', {name: 'Country or region'}).selectOption('US');
