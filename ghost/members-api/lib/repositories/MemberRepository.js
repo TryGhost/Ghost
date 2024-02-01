88594cdef7c450a64f79a35369644b23d7d148d5
Ronald Langeveld <hi@ronaldlangeveld.com>
2024-02-01 14:47:05 +0200

    Simplified code

--- ghost/members-api/lib/repositories/MemberRepository.js
+++ ghost/members-api/lib/repositories/MemberRepository.js
@@ -556,7 +556,7 @@ module.exports = class MemberRepository {
         // Keep track of the newsletters that were added and removed of a member so we can generate the corresponding events
         let newslettersToAdd = [];
         let newslettersToRemove = [];
-        let newslettersToIgnore = []; // This is used to keep track of archived newsletters where members are still subscribed to
+        // let newslettersToIgnore = []; // This is used to keep track of archived newsletters where members are still subscribed to
 
         if (needsNewsletters) {
             const existingNewsletters = initialMember.related('newsletters').models;
@@ -572,15 +572,13 @@ module.exports = class MemberRepository {
 
             // only ever populated with active newsletters - never archived ones
             if (memberData.newsletters) {
-                const archivedNewsletters = existingNewsletters.filter(n => n.get('status') === 'archived');
+                const archivedNewsletters = existingNewsletters.filter(n => n.get('status') === 'archived').map(n => n.id);
                 const existingNewsletterIds = existingNewsletters
                     .filter(newsletter => newsletter.attributes.status !== 'archived')
                     .map(newsletter => newsletter.id);
                 const incomingNewsletterIds = memberData.newsletters.map(newsletter => newsletter.id);
-                newslettersToIgnore = archivedNewsletters.map(n => n.id);
-
                 // make sure newslettersToAdd does not contain newslettersToIgnore (archived newsletters since that creates false events)
-                newslettersToAdd = _.differenceWith(_.differenceWith(incomingNewsletterIds, existingNewsletterIds), newslettersToIgnore);
+                newslettersToAdd = _.differenceWith(_.differenceWith(incomingNewsletterIds, existingNewsletterIds), archivedNewsletters);
                 newslettersToRemove = _.differenceWith(existingNewsletterIds, incomingNewsletterIds);
             }
 
