73cc25459fd53de964c5e94bdc0f58fa6b6bb175
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2024-02-05 19:56:43 +0700

    Added default background color to preview iframes (#19651)
    fixes DES-88
    
    - preview iframes should have default background as they simulate
    browser windows
    - transparent background causes unexpected inconsistency when themes
    don't have default background color set

--- apps/admin-x-settings/src/components/settings/site/designAndBranding/ThemePreview.tsx
+++ apps/admin-x-settings/src/components/settings/site/designAndBranding/ThemePreview.tsx
@@ -105,7 +105,7 @@ const ThemePreview: React.FC<ThemePreviewProps> = ({settings,url}) => {
     return (
         <IframeBuffering
             addDelay={false}
-            className="absolute h-[110%] w-[110%] origin-top-left scale-[.90909] max-[1600px]:h-[130%] max-[1600px]:w-[130%] max-[1600px]:scale-[.76923]"
+            className="absolute h-[110%] w-[110%] origin-top-left scale-[.90909] bg-white max-[1600px]:h-[130%] max-[1600px]:w-[130%] max-[1600px]:scale-[.76923]"
             generateContent={injectContentIntoIframe}
             height='100%'
             parentClassName="relative h-full w-full"
