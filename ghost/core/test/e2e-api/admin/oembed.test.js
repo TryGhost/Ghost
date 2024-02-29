898a3a0e9faee6b23376db12da06dc8db96756c1
Peter Zimon <peter.zimon@gmail.com>
2024-02-29 15:53:05 +0100

    Added test for broken favicon

--- ghost/core/test/e2e-api/admin/oembed.test.js
+++ ghost/core/test/e2e-api/admin/oembed.test.js
@@ -221,6 +221,33 @@ describe('Oembed API', function () {
 
             should.exist(res.body.errors);
         });
+
+        it('should replace icon URL when it returns 404', async function () {
+            // Mock the page so it contains a readable icon URL
+            nock('http://example.com')
+                .get('/page-with-icon')
+                .reply(200, {
+                    icon: 'http://example.com/icon.svg'
+                });
+
+            // Mock the icon URL to return 404
+            nock('http://example.com')
+                .head('/icon.svg')
+                .reply(404);
+
+            const url = encodeURIComponent('http://example.com/page-with-icon');
+            const res = await request.get(localUtils.API.getApiQuery(`oembed/?type=bookmark&url=${url}`))
+                .set('Origin', config.get('url'))
+                .expect('Content-Type', /json/)
+                .expect('Cache-Control', testUtils.cacheRules.private)
+                .expect(200);
+
+            // Check that the icon URL mock was loaded
+            nock.isDone().should.be.true();
+
+            // Check that the substitute icon URL is returned in place of the original
+            res.body.icon.should.equal('https://static.ghost.org/v5.0.0/images/link-icon.svg');
+        });
     });
 
     describe('with unknown provider', function () {
