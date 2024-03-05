c1b287a0fcc14e934f9e7b353654467bf795e595
Fabien O'Carroll <fabien@allou.is>
2024-03-04 20:13:32 -0500

    Passed up used @data to render call

--- ghost/core/core/frontend/services/theme-engine/engine.js
+++ ghost/core/core/frontend/services/theme-engine/engine.js
@@ -1,3 +1,6 @@
+const Handlebars = require('handlebars');
+const fs = require('fs');
+const path = require('path');
 const hbs = require('express-hbs');
 const config = require('../../../shared/config');
 const instance = hbs.create();
@@ -6,13 +9,83 @@ const instance = hbs.create();
 if (config.get('env') !== 'production') {
     instance.handlebars.logger.level = 0;
 }
+class DataReader extends Handlebars.Visitor {
+    constructor(data, filepath, exhbs, partialsPath) {
+        super();
+        this.data = data || new Set();
+        this.filename = filepath;
+        this.exhbs = exhbs;
+        this.partialsPath = partialsPath;
+    }
+
+    CommentStatement(comment) {
+        const layoutRegex = /<\s+([A-Za-z0-9\._\-\/]+)\s*/;
+        const [isLayout, layoutName] = comment.value.match(layoutRegex) || [false, null];
+
+        if (isLayout) {
+            try {
+                const layoutFilePath = this.exhbs.layoutPath(this.filename, layoutName);
+                const layoutContent = fs.readFileSync(layoutFilePath + '.hbs').toString();
+                const ast = Handlebars.parseWithoutProcessing(layoutContent);
+                const reader = new DataReader(this.data, layoutFilePath, this.exhbs, this.partialsPath);
+                reader.accept(ast);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+    }
+
+    PathExpression(path) {
+        if (path.data) {
+            this.data.add(path.original);
+        }
+    }
+
+    PartialStatement(partial) {
+        if (!this.partialsPath) {
+            throw new Error('No partials path, but found a partial');
+        }
+        const partialFilePath = path.join(this.partialsPath, partial.name.original);
+        try {
+            const partialContent = fs.readFileSync(partialFilePath + '.hbs').toString();
+            const ast = Handlebars.parseWithoutProcessing(partialContent);
+            const reader = new DataReader(this.data, partialFilePath, this.exhbs, this.partialsPath);
+            reader.accept(ast);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    static getData(exhbs, source, filename, partialsPath) {
+        const ast = Handlebars.parseWithoutProcessing(source);
+        const reader = new DataReader(new Set(), filename, exhbs, partialsPath);
+        reader.accept(ast);
+        return reader.data;
+    }
+}
 
 instance.escapeExpression = instance.handlebars.Utils.escapeExpression;
 
 instance.configure = function configure(partialsPath, themePath) {
+    const helperTemplatesPath = config.get('paths').helperTemplates;
     const hbsOptions = {
         partialsDir: [config.get('paths').helperTemplates],
-        onCompile: function onCompile(exhbs, source) {
+        onCompile: function onCompile(exhbs, source, filename) {
+            if (filename.startsWith(helperTemplatesPath)) {
+                return exhbs.handlebars.compile(source, {preventIndent: true});
+            }
+            if (partialsPath && filename.startsWith(partialsPath)) {
+                return exhbs.handlebars.compile(source, {preventIndent: true});
+            }
+
+            const data = DataReader.getData(exhbs, source, filename, partialsPath);
+
+            if (!exhbs.datamap) {
+                exhbs.datamap = {};
+            }
+
+            exhbs.datamap[filename] = data;
+
             return exhbs.handlebars.compile(source, {preventIndent: true});
         },
         restrictLayoutsTo: themePath
