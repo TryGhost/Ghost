e7f00205a497bd7d9041b585f86772c5e6eca764
Fabien 'egg' O'Carroll <fabien@allou.is>
2024-03-19 00:29:41 +0700

    Cached api controller pipelines (#19880)
    ref ENG-761
    ref https://linear.app/tryghost/issue/ENG-761
    
    Creating these pipelines is expensive, and we don't want to do it
    repeatedly for the same controller. Adding caching should reduce the
    amount of time spent setting up pipelines for each usage of the `get`
    helper.

--- ghost/core/test/regression/mock-express-style/api-vs-frontend.test.js
+++ ghost/core/test/regression/mock-express-style/api-vs-frontend.test.js
@@ -33,8 +33,8 @@ describe('Frontend behavior tests', function () {
 
         beforeEach(function () {
             sinon.stub(themeEngine.getActive(), 'config').withArgs('posts_per_page').returns(2);
-            const postsAPI = require('../../../core/server/api/endpoints/posts-public');
-            postSpy = sinon.spy(postsAPI.browse, 'query');
+            const postsAPI = require('../../../core/server/api/endpoints').postsPublic;
+            postSpy = sinon.spy(postsAPI, 'browse');
         });
 
         afterEach(function () {
@@ -143,22 +143,20 @@ describe('Frontend behavior tests', function () {
                     });
             });
 
-            it('serve tag', function () {
+            it('serve tag', async function () {
                 const req = {
                     method: 'GET',
                     url: '/tag/bacon/',
                     host: 'example.com'
                 };
 
-                return localUtils.mockExpress.invoke(app, req)
-                    .then(function (response) {
-                        response.statusCode.should.eql(200);
-                        response.template.should.eql('tag');
+                const response = await localUtils.mockExpress.invoke(app, req);
+                response.statusCode.should.eql(200);
+                response.template.should.eql('tag');
 
-                        postSpy.args[0][0].options.filter.should.eql('(tags:\'bacon\'+tags.visibility:public)+type:post');
-                        postSpy.args[0][0].options.page.should.eql(1);
-                        postSpy.args[0][0].options.limit.should.eql(2);
-                    });
+                postSpy.args[0][0].filter.should.eql('tags:\'bacon\'+tags.visibility:public');
+                postSpy.args[0][0].page.should.eql(1);
+                postSpy.args[0][0].limit.should.eql(2);
             });
 
             it('serve tag rss', function () {
