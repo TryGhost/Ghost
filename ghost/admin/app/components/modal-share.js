3fa657a319dac922c828c8ebba1f7d82d10a81e1
Djordje Vlaisavljevic <dzvlais@gmail.com>
2024-03-20 20:31:05 +0000

    Added function for copying publication link
    ref https://linear.app/tryghost/issue/IPC-90/add-share-modal

--- ghost/admin/app/components/modal-share.js
+++ ghost/admin/app/components/modal-share.js
@@ -1,5 +1,15 @@
 import ModalComponent from 'ghost-admin/components/modal-base';
+import copyTextToClipboard from 'ghost-admin/utils/copy-text-to-clipboard';
+import {inject} from 'ghost-admin/decorators/inject';
+import {task, timeout} from 'ember-concurrency';
 
 export default ModalComponent.extend({
+    config: inject(),
+
+    copySiteUrl: task(function* () {
+        copyTextToClipboard(this.config.blogUrl);
+        yield timeout(1000);
+        return true;
+    })
 
 });
