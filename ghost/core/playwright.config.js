e8426ea77e13f00f80da74b601fd86c770013ce0
Steve Larson <9larsons@gmail.com>
2024-03-26 16:04:17 -0500

    dsf

--- ghost/core/playwright.config.js
+++ ghost/core/playwright.config.js
@@ -10,8 +10,7 @@ const config = {
     workers: process.env.CI ? '100%' : (process.env.PLAYWRIGHT_SLOWMO ? 1 : undefined),
     reporter: process.env.CI ? [['list', {printSteps: true}], ['html']] : [['list', {printSteps: true}]],
     use: {
-        // trace: 'retain-on-failure',
-        trace: 'on-first-retry',
+        trace: 'retain-on-failure',
         // Use a single browser since we can't simultaneously test multiple browsers
         browserName: 'chromium',
         headless: !process.env.PLAYWRIGHT_DEBUG,
