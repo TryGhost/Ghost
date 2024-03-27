d223acfe48efa723b95e486ec6d3f9334b822079
Kevin Ansfield <kevin@lookingsideways.co.uk>
2024-03-27 18:22:19 +0000

    Wired up in-memory onboarding step completion
    refs https://linear.app/tryghost/issue/IPC-92/add-logic-for-completing-steps
    
    - added in-memory step completion to `onboarding` service
    - wired up the onboarding checklist to mark steps as completed when clicked
    - extracted re-used step template and logic into components/helpers

--- ghost/admin/app/services/onboarding.js
+++ ghost/admin/app/services/onboarding.js
@@ -1,11 +1,37 @@
 import Service, {inject as service} from '@ember/service';
+import {TrackedSet} from 'tracked-built-ins';
+import {action} from '@ember/object';
+import {tracked} from '@glimmer/tracking';
 
 export default class OnboardingService extends Service {
     @service feature;
     @service session;
 
+    @tracked _completedSteps = new TrackedSet();
+
+    ONBOARDING_STEPS = [
+        'customize-design',
+        'first-post',
+        'build-audience',
+        'share-publication'
+    ];
+
     get isChecklistShown() {
         return this.feature.onboardingChecklist
             && this.session.user.isOwnerOnly;
+    }
+
+    get nextStep() {
+        return this.ONBOARDING_STEPS.find(step => !this.isStepCompleted(step));
+    }
+
+    @action
+    isStepCompleted(step) {
+        return this._completedSteps.has(step);
+    }
+
+    @action
+    markStepCompleted(step) {
+        this._completedSteps.add(step);
     }
 }
