0a51b1930fe0856f471c3a33d6ba2007a7e6bb2f
Kevin Ansfield <kevin@ghost.org>
2024-03-28 15:35:35 +0000

    Migrated dashboard share modal to modern pattern (#19950)
    closes https://linear.app/tryghost/issue/IPC-92/add-logic-for-completing-steps
    closes https://linear.app/tryghost/issue/IPC-111/re-work-share-modal-to-use-the-right-pattern
    
    - migrated code over to the modern ember-promise-modals pattern
    - added share step completion when opening modal
    - removed unnecessary 1sec timeout when clicking "Copy" button
    - moved various share URLs into the template so there's less need to look back-and-forth between template and backing class

--- ghost/admin/app/components/dashboard/onboarding-checklist.js
+++ ghost/admin/app/components/dashboard/onboarding-checklist.js
@@ -1,16 +1,31 @@
 import Component from '@glimmer/component';
+import ShareModal from './onboarding/share-modal';
+import {action} from '@ember/object';
 import {inject} from 'ghost-admin/decorators/inject';
 import {inject as service} from '@ember/service';
-import {tracked} from '@glimmer/tracking';
 
 export default class OnboardingChecklist extends Component {
+    @service modals;
     @service onboarding;
 
     @inject config;
 
-    @tracked showMemberTierModal = false;
+    shareModal = null;
+
+    willDestroy() {
+        super.willDestroy(...arguments);
+        if (this.shareModal) {
+            this.shareModal.close();
+        }
+    }
 
     get siteUrl() {
         return this.config.blogTitle;
+    }
+
+    @action
+    openShareModal() {
+        this.onboarding.markStepCompleted('share-publication');
+        this.shareModal = this.modals.open(ShareModal);
     }
 }
