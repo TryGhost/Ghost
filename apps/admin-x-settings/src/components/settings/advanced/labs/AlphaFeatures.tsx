74bea206c98ce49b0e47a69a9a79cb6a1d324215
Sag <guptazy@gmail.com>
2024-04-03 18:10:58 +0200

    Removed leftover alpha lab (#19978)
    refs https://ghost.slack.com/archives/C02G9E68C/p1712150935653429
    
    - the 'newEmailAddresses' feature has been released to GA, end of
    January
    (https://github.com/TryGhost/Ghost/commit/85296ab701525ae7026d6a202870984526b0ddc9)
    - but, the flag was still shown as alpha in Settings

--- apps/admin-x-settings/src/components/settings/advanced/labs/AlphaFeatures.tsx
+++ apps/admin-x-settings/src/components/settings/advanced/labs/AlphaFeatures.tsx
@@ -52,10 +52,6 @@ const features = [{
     description: 'Adds a navigation link to the AdminX demo app',
     flag: 'adminXDemo'
 },{
-    title: 'New email addresses',
-    description: 'For self hosters, forces the usage of the mail.from config as from address for all outgoing emails',
-    flag: 'newEmailAddresses'
-},{
     title: 'Onboarding checklist',
     description: 'Onboarding checklist that helps new customers get started',
     flag: 'onboardingChecklist'
