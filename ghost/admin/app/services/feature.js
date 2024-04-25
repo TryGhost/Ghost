4b86b906f3d9232020a4096be80c52c8a1355636
Ronald Langeveld <hi@ronaldlangeveld.com>
2024-04-25 12:31:51 +0800

    Fixed flag naming in ember service

--- ghost/admin/app/services/feature.js
+++ ghost/admin/app/services/feature.js
@@ -82,7 +82,7 @@ export default class FeatureService extends Service {
     @feature('portalImprovements') portalImprovements;
     @feature('onboardingChecklist') onboardingChecklist;
     @feature('membersSpamPrevention') membersSpamPrevention;
-    @feature('activityPub') activityPub;
+    @feature('ActivityPub') ActivityPub;
 
     _user = null;
 
