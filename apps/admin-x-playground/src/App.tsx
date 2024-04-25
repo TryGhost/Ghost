0aa7ea901c0d34a68eb9d5369aa4bef484f41a21
Ronald Langeveld <hi@ronaldlangeveld.com>
2024-04-25 13:36:55 +0800

    Setup test files

--- apps/admin-x-playground/src/App.tsx
+++ apps/admin-x-playground/src/App.tsx
@@ -1,3 +1,4 @@
+import ListIndex from './components/ListIndex';
 import {DesignSystemApp, DesignSystemAppProps} from '@tryghost/admin-x-design-system';
 import {FrameworkProvider, TopLevelFrameworkProps} from '@tryghost/admin-x-framework';
 import {RoutingProvider} from '@tryghost/admin-x-framework/routing';
@@ -12,26 +13,7 @@ const App: React.FC<AppProps> = ({framework, designSystem}) => {
         <FrameworkProvider {...framework}>
             <RoutingProvider basePath='playground-x'>
                 <DesignSystemApp className='admin-x-playground' {...designSystem}>
-                    <div className='mx-auto my-0 w-full max-w-3xl p-12'>
-                        <h1 className='mb-6 text-black'>ActivityPub Demo</h1>
-                        <div className='flex flex-col'>
-                            <div className='mb-4 flex flex-col'>
-                                <h2 className='mb-2 text-2xl text-black'>This is a post title</h2>
-                                <p className='mb-2 text-lg text-grey-950'>This is some very short post content</p>
-                                <p className='text-md text-grey-700'>Publish McPublisher</p>
-                            </div>
-                            <div className='mb-4 flex flex-col'>
-                                <h2 className='mb-2 text-2xl text-black'>This is a post title</h2>
-                                <p className='mb-2 text-lg text-grey-950'>This is some very short post content</p>
-                                <p className='text-md text-grey-700'>Publish McPublisher</p>
-                            </div>
-                            <div className='mb-4 flex flex-col'>
-                                <h2 className='mb-2 text-2xl text-black'>This is a post title</h2>
-                                <p className='mb-2 text-lg text-grey-950'>This is some very short post content</p>
-                                <p className='text-md text-grey-700'>Publish McPublisher</p>
-                            </div>
-                        </div>
-                    </div>
+                    <ListIndex />
                 </DesignSystemApp>
             </RoutingProvider>
         </FrameworkProvider>
