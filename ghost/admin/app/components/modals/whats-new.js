22a2ac94f8980b07c3044524ea3ce9fd7e1db600
Peter Zimon <peter.zimon@gmail.com>
2024-05-02 18:30:57 +0200

    Added whats new list modal

--- ghost/admin/app/components/modals/whats-new.js
+++ ghost/admin/app/components/modals/whats-new.js
@@ -4,6 +4,10 @@ import {inject as service} from '@ember/service';
 export default class WhatsNewFeatured extends Component {
     @service whatsNew;
 
+    static modalOptions = {
+        className: 'fullscreen-modal-whatsnew'
+    };
+
     willDestroy() {
         super.willDestroy(...arguments);
         this.whatsNew.seen();
