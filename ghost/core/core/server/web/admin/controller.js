8149a853aa5127367062a5a07b03cf0e6f1d3bea
Daniel Lockyer <hi@daniellockyer.com>
2024-05-06 21:19:10 +0200

    Fixed passing error to IncorrectUsageError
    - we should pass it as `err` and not `error`
    - this probably slipped in because the catch parameter is called
      `error`, so I've updated that and fixed the references

--- ghost/core/core/server/web/admin/controller.js
+++ ghost/core/core/server/web/admin/controller.js
@@ -47,16 +47,15 @@ module.exports = function adminController(req, res) {
         }
 
         res.sendFile(templatePath, {headers});
-    } catch (error) {
-        if (error.code === 'ENOENT') {
+    } catch (err) {
+        if (err.code === 'ENOENT') {
             throw new errors.IncorrectUsageError({
                 message: tpl(messages.templateError.message, {templatePath}),
                 context: tpl(messages.templateError.context),
                 help: tpl(messages.templateError.help, {link: 'https://ghost.org/docs/install/source/'}),
-                error: error
+                err
             });
-        } else {
-            throw error;
         }
+        throw err;
     }
 };
