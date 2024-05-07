4f3bfebfea6b8757a6b1f7f73b62672bd772f90a
Ronald Langeveld <hi@ronaldlangeveld.com>
2024-05-07 16:58:42 +0800

    Revert "Added `latest_event_timestamp` to `email` table (#20118)" (#20149)
    This reverts commit 3246a8d2c97e9ddb8791661e86ab4510ddbd91c8.

--- ghost/core/core/server/data/migrations/versions/5.83/2024-05-02-06-01-13-add-latest-event-timestamp-to-email.js
+++
@@ -1,7 +0,0 @@
-// For information on writing migrations, see https://www.notion.so/ghost/Database-migrations-eb5b78c435d741d2b34a582d57c24253
-
-const {createAddColumnMigration} = require('../../utils');
-
-module.exports = createAddColumnMigration('emails', 'latest_event_timestamp', {
-    type: 'dateTime', nullable: true
-});
