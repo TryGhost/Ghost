e39eb7685ff10154810a96bed95478e593f157fb
Djordje Vlaisavljevic <dzvlais@gmail.com>
2024-05-08 16:20:43 +0100

    Updated test
    close https://linear.app/tryghost/issue/MOM-109/add-static-%F0%9F%9B%B9-design

--- apps/admin-x-activitypub/test/unit/ListIndex.test.tsx
+++ apps/admin-x-activitypub/test/unit/ListIndex.test.tsx
@@ -5,6 +5,6 @@ describe('Demo', function () {
     it('renders a component', async function () {
         render(<ListIndex/>);
 
-        expect(screen.getAllByRole('heading')[0].textContent).toEqual('ActivityPub Demo');
+        expect(screen.getAllByRole('heading')[0].textContent).toEqual('Outbox');
     });
 });
