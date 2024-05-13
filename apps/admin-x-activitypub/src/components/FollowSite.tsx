82efc18daa49dea281b0ba3b0742de218c1278a0
Fabien O'Carroll <fabien@allou.is>
2024-05-13 17:26:03 +0700

    Added Follow mutation

--- apps/admin-x-activitypub/src/components/FollowSite.tsx
+++ apps/admin-x-activitypub/src/components/FollowSite.tsx
@@ -1,14 +1,23 @@
 import NiceModal from '@ebay/nice-modal-react';
 import {Modal, TextField} from '@tryghost/admin-x-design-system';
+import {useFollow} from '@tryghost/admin-x-framework/api/activitypub';
 import {useRouting} from '@tryghost/admin-x-framework/routing';
 
 const FollowSite = NiceModal.create(() => {
     const {updateRoute} = useRouting();
     const modal = NiceModal.useModal();
+    const mutation = useFollow();
+
+    // mutation.isPending
+    // mutation.isError
+    // mutation.isSuccess
+    // mutation.mutate({username: '@index@site.com'})
+    // mutation.reset();
 
     return (
         <Modal
             afterClose={() => {
+                mutation.reset();
                 updateRoute('');
             }}
             cancelLabel='Cancel'
@@ -16,6 +25,7 @@ const FollowSite = NiceModal.create(() => {
             size='sm'
             title='Follow a Ghost site'
             onOk={() => {
+                // mutation.mutate({username: value});
                 updateRoute('');
                 modal.remove();
             }}
