0c1f8fa3f597e2f9d2a6992b6547d1aafd6dfa04
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2024-05-29 15:56:24 +0800

    Fixed label name not showing as title (#20275)
    ref DES-205

--- ghost/admin/app/components/gh-token-input/label-token.js
+++ ghost/admin/app/components/gh-token-input/label-token.js
@@ -1,10 +1,17 @@
 import DraggableObject from 'ember-drag-drop/components/draggable-object';
 import classic from 'ember-classic-decorator';
+import {alias} from '@ember/object/computed';
 import {attributeBindings, classNames} from '@ember-decorators/component';
+import {computed} from '@ember/object';
 
 @classic
 @attributeBindings('title')
 @classNames('label-token')
 export default class LabelToken extends DraggableObject {
-    title = this.name ?? 'Label';
+    @alias('content.name') name;
+
+    @computed('name')
+    get title() {
+        return this.name ?? 'Label';
+    }
 }
