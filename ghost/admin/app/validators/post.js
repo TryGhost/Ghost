833ac83921474da70694cf142b7aad3bfc27b1b4
Ronald Langeveld <hi@ronaldlangeveld.com>
2024-06-03 18:02:18 +0700

    Added validation logic to in-editor subtitle (#20284)
    closes https://linear.app/tryghost/issue/MOM-150
    
    - use our validation engine to display an error state when >300 characters have been typed in the subtitle input field
    
    ---------
    
    Co-authored-by: Sanne de Vries <sannedv@protonmail.com>
    Co-authored-by: Kevin Ansfield <kevin@lookingsideways.co.uk>

--- ghost/admin/app/validators/post.js
+++ ghost/admin/app/validators/post.js
@@ -62,7 +62,11 @@ export default BaseValidator.create({
 
     customExcerpt(model) {
         if (!validator.isLength(model.customExcerpt || '', 0, 300)) {
-            model.errors.add('customExcerpt', 'Excerpt cannot be longer than 300 characters.');
+            if (model.feature.editorSubtitle) {
+                model.errors.add('customExcerpt', 'Please keep the subtitle under 300 characters.');
+            } else {
+                model.errors.add('customExcerpt', 'Excerpt cannot be longer than 300 characters.');
+            }
             this.invalidate();
         }
     },
