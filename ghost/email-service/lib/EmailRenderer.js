72128185e7626f498a4c954abf30c908cb6b6be2
Kevin Ansfield <kevin@lookingsideways.co.uk>
2024-06-03 17:50:46 +0100

    Wired up subtitle in newsletter template
    closes https://linear.app/tryghost/issue/MOM-173
    
    - updated email renderer to add `post.customExcerpt` data
    - updated template to skip rendering subtitle when no custom excerpt is present
    - updated template to use actual custom excerpt

--- ghost/email-service/lib/EmailRenderer.js
+++ ghost/email-service/lib/EmailRenderer.js
@@ -1035,6 +1035,7 @@ class EmailRenderer {
                 commentUrl: commentUrl.href,
                 authors,
                 publishedAt,
+                customExcerpt: post.get('custom_excerpt'),
                 feature_image: postFeatureImage,
                 feature_image_width: postFeatureImageWidth,
                 feature_image_height: postFeatureImageHeight,
