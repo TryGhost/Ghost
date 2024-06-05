a9e402c457754b80f0d5f1a3e4125b394e330e47
Kevin Ansfield <kevin@lookingsideways.co.uk>
2024-06-05 14:00:23 +0100

    updated migration to support SQLite

--- ghost/core/core/server/data/migrations/versions/5.84/2024-06-05-08-42-34-populate-post-revisions-custom-excerpt.js
+++ ghost/core/core/server/data/migrations/versions/5.84/2024-06-05-08-42-34-populate-post-revisions-custom-excerpt.js
@@ -1,15 +1,28 @@
 const logging = require('@tryghost/logging');
 const {createTransactionalMigration} = require('../../utils');
+const DatabaseInfo = require('@tryghost/database-info');
 
 module.exports = createTransactionalMigration(
     async function up(knex) {
         logging.info('Populating post_revisions.custom_excerpt with post.excerpt');
 
-        await knex.raw(`
-            UPDATE post_revisions
-            JOIN posts ON post_revisions.post_id = posts.id
-            SET post_revisions.custom_excerpt = posts.custom_excerpt
-        `);
+        if (DatabaseInfo.isSQLite(knex)) {
+            // SQLite doesn't support JOINs in UPDATE queries
+            await knex.raw(`
+                UPDATE post_revisions
+                SET custom_excerpt = (
+                    SELECT posts.custom_excerpt
+                    FROM posts
+                    WHERE post_revisions.post_id = posts.id
+                )
+            `);
+        } else {
+            await knex.raw(`
+                UPDATE post_revisions
+                JOIN posts ON post_revisions.post_id = posts.id
+                SET post_revisions.custom_excerpt = posts.custom_excerpt
+            `);
+        }
 
         logging.info('Finished populating post_revisions.custom_excerpt');
     },
