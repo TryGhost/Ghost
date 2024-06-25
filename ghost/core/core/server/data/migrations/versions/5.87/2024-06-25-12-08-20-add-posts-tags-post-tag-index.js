c4ed887ff87006cd4d2472a53b0e4b8282727b84
Steve Larson <9larsons@gmail.com>
2024-06-25 12:33:31 -0500

    update migration with error handling

--- ghost/core/core/server/data/migrations/versions/5.87/2024-06-25-12-08-20-add-posts-tags-post-tag-index.js
+++ ghost/core/core/server/data/migrations/versions/5.87/2024-06-25-12-08-20-add-posts-tags-post-tag-index.js
@@ -1,4 +1,26 @@
-// For information on writing migrations, see https://www.notion.so/ghost/Database-migrations-eb5b78c435d741d2b34a582d57c24253
-const {createAddIndexMigration} = require('../../utils');
+const logging = require('@tryghost/logging');
+const {createNonTransactionalMigration} = require('../../utils');
+const {addIndex, dropIndex} = require('../../../schema/commands');
 
-module.exports = createAddIndexMigration('posts_tags',['post_id','tag_id'],'post_id');
+module.exports = createNonTransactionalMigration(
+    async function up(knex) {
+        await addIndex('posts_tags', ['post_id', 'tag_id'], knex);
+    },
+    async function down(knex) {
+        try {
+            await dropIndex('posts_tags', ['post_id', 'tag_id'], knex);
+        } catch (err) {
+            if (err.code === 'ER_DROP_INDEX_FK') {
+                logging.error({
+                    message: 'Error dropping index over posts_tags(post_id, tag_id), re-adding index for post_id'
+                });
+
+                await addIndex('posts_tags', ['post_id'], knex);
+                await dropIndex('posts_tags', ['post_id', 'tag_id'], knex);
+                return;
+            }
+
+            throw err;
+        }
+    }
+);
