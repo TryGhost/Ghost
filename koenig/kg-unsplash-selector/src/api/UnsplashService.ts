89d3dee0ce6bb3cff23afd773eda5605a609e3d1
Sag <guptazy@gmail.com>
2024-06-26 16:24:38 +0200

    Fixed TypeError when Unsplash search is exited quickly
    fixes https://linear.app/tryghost/issue/SLO-170

--- koenig/kg-unsplash-selector/src/api/UnsplashService.ts
+++ koenig/kg-unsplash-selector/src/api/UnsplashService.ts
@@ -18,42 +18,45 @@ export class UnsplashService implements IUnsplashService {
     private photoUseCases: PhotoUseCases;
     private masonryService: MasonryService;
     public photos: Photo[] = [];
-  
+
     constructor(photoUseCases: PhotoUseCases, masonryService: MasonryService) {
         this.photoUseCases = photoUseCases;
         this.masonryService = masonryService;
     }
-  
+
     async loadNew() {
         let images = await this.photoUseCases.fetchPhotos();
         this.photos = images;
         await this.layoutPhotos();
     }
-  
+
     async layoutPhotos() {
         this.masonryService.reset();
-        this.photos.forEach((photo) => {
-            photo.ratio = photo.height / photo.width;
-            this.masonryService.addPhotoToColumns(photo);
-        });
+
+        if (this.photos) {
+            this.photos.forEach((photo) => {
+                photo.ratio = photo.height / photo.width;
+                this.masonryService.addPhotoToColumns(photo);
+            });
+        }
     }
-  
+
     getColumns() {
         return this.masonryService.getColumns();
     }
-  
+
     async updateSearch(term: string) {
         let results = await this.photoUseCases.searchPhotos(term);
         this.photos = results;
         this.layoutPhotos();
     }
-  
+
     async loadNextPage() {
         const newPhotos = await this.photoUseCases.fetchNextPage() || [];
         this.photos = [...this.photos, ...newPhotos];
         this.layoutPhotos();
     }
-  
+
     clearPhotos() {
         this.photos = [];
     }
