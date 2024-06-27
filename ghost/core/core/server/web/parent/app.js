ccf05e759fe61ecce6784161b07856ab74ca16c9
Michael Barrett <mike@ghost.org>
2024-06-27 09:30:07 +0100

    Removed request queue enablement flag (#20466)
    refs
    [CFR-26](https://linear.app/tryghost/issue/CFR-26/remove-request-queue-config-flag)
    
    Removed request queue enablement flag and updated the logic so that the
    request queue is enabled when there is explicit configuration for it.

--- ghost/core/core/server/web/parent/app.js
+++ ghost/core/core/server/web/parent/app.js
@@ -29,7 +29,7 @@ module.exports = function setupParentApp() {
     // Enable request queuing if configured
     const queueConfig = config.get('optimization:requestQueue');
 
-    if (queueConfig?.enabled === true) {
+    if (queueConfig) {
         parentApp.use(mw.queueRequest(queueConfig));
     }
 
