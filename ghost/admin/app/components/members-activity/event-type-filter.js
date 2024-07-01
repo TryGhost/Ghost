7930ca47ba5b52eff5feb79a4d197934985f6fce
Sag <guptazy@gmail.com>
2024-07-01 14:36:00 +0200

    Added filter icon

--- ghost/admin/app/components/members-activity/event-type-filter.js
+++ ghost/admin/app/components/members-activity/event-type-filter.js
@@ -5,15 +5,14 @@ import {inject as service} from '@ember/service';
 const ALL_EVENT_TYPES = [
     {event: 'signup_event', icon: 'filter-dropdown-signups', name: 'Signups', group: 'auth'},
     {event: 'login_event', icon: 'filter-dropdown-logins', name: 'Logins', group: 'auth'},
-    // TODO: update icon when we have one:
-    {event: 'email_change_event', icon: 'filter-dropdown-logins', name: 'Email address changed', group: 'auth'},
     {event: 'subscription_event', icon: 'filter-dropdown-paid-subscriptions', name: 'Paid subscriptions', group: 'payments'},
     {event: 'payment_event', icon: 'filter-dropdown-payments', name: 'Payments', group: 'payments'},
     {event: 'newsletter_event', icon: 'filter-dropdown-email-subscriptions', name: 'Email subscriptions', group: 'emails'},
     {event: 'email_opened_event', icon: 'filter-dropdown-email-opened', name: 'Email opened', group: 'emails'},
     {event: 'email_delivered_event', icon: 'filter-dropdown-email-received', name: 'Email received', group: 'emails'},
     {event: 'email_complaint_event', icon: 'filter-dropdown-email-flagged-as-spam', name: 'Email flagged as spam', group: 'emails'},
-    {event: 'email_failed_event', icon: 'filter-dropdown-email-bounced', name: 'Email bounced', group: 'emails'}
+    {event: 'email_failed_event', icon: 'filter-dropdown-email-bounced', name: 'Email bounced', group: 'emails'},
+    {event: 'email_change_event', icon: 'filter-dropdown-email-address-changed', name: 'Email address changed', group: 'emails'}
 ];
 
 export default class MembersActivityEventTypeFilter extends Component {
