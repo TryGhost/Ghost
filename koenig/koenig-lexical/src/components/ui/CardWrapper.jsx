b6bf5a9dd76306c78c74cd935a989c10203025eb
Sanne de Vries <65487235+sanne-san@users.noreply.github.com>
2024-07-01 11:31:07 +0200

    Fixed the wide image card width on medium screens (TryGhost/Koenig#1293)
    REF DES-132

--- koenig/koenig-lexical/src/components/ui/CardWrapper.jsx
+++ koenig/koenig-lexical/src/components/ui/CardWrapper.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const CARD_WIDTH_CLASSES = {
     wide: [
         'w-[calc(75vw-var(--kg-breakout-adjustment-with-fallback)+2px)] mx-[calc(50%-(50vw-var(--kg-breakout-adjustment-with-fallback))-.8rem)] min-w-[calc(100%+3.6rem)] translate-x-[calc(50vw-50%+.8rem-var(--kg-breakout-adjustment-with-fallback))]',
-        'sm:min-w-[calc(100%+10rem)]',
+        'md:min-w-[calc(100%+10rem)]',
         'lg:min-w-[calc(100%+18rem)]'
     ].join(' '),
     full: 'inset-x-[-1px] mx-[calc(50%-50vw)] w-[calc(100vw+2px)] lg:mx-[calc(50%-50vw+(var(--kg-breakout-adjustment-with-fallback)/2))] lg:w-[calc(100vw-var(--kg-breakout-adjustment-with-fallback)+2px)]'
