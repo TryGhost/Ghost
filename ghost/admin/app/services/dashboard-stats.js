bbf24ca2bd7a5303bf6e902b9f38f70941a2aa30
Princi Vershwal <vershwal.princi@gmail.com>
2024-07-02 12:17:13 +0530

    Fixed analytics sources to not be case sensitive (#20506)
    fixes
    https://linear.app/tryghost/issue/ENG-925/analytics-sources-shouldnt-be-case-sensitive

--- ghost/admin/app/services/dashboard-stats.js
+++ ghost/admin/app/services/dashboard-stats.js
@@ -250,7 +250,7 @@ export default class DashboardStatsService extends Service {
             return stat.date >= moment().add(-this.chartDays, 'days').format('YYYY-MM-DD');
         }).reduce((acc, stat) => {
             const statSource = stat.source ?? '';
-            const existingSource = acc.find(s => s.source === statSource);
+            const existingSource = acc.find(s => s.source.toLowerCase() === statSource.toLowerCase());
             if (existingSource) {
                 existingSource.signups += stat.signups || 0;
                 existingSource.paidConversions += stat.paidConversions || 0;
