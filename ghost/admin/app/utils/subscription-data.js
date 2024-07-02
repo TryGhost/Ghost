f9581ba8b2eee60144e4b0f1d5014bb7bd7c1e4f
Fabien 'egg' O'Carroll <fabien@allou.is>
2024-07-02 20:58:20 +0700

    🐛 Fixed incorrect member subscription details in Admin (#20476)
    fixes https://linear.app/tryghost/issue/ENG-642
    
    - When a subscription is in the `canceled` state the corresponding
    Member has no access to the Ghost site. The only time a Member will
    continue to have access if their subscription is due to cancel at the
    period end is if it is still in an active state, which is one of
    `active` `trialing` `unpaid` or `past_due`
    - When a subscription is canceled immediately (i.e. before the end of
    the current billing period), we now render "Ended" without a date,
    because we don't store the cancellation date in the subscription object.
    We previously used "Ended {current_period_end}" which would sometimes
    lead to dates in the future
    - Bonus: refactored code and added unit tests
    
    ---------
    
    Co-authored-by: Sag <guptazy@gmail.com>

---
+++ ghost/admin/app/utils/subscription-data.js
@@ -0,0 +1,98 @@
+import moment from 'moment-timezone';
+import {getNonDecimal, getSymbol} from 'ghost-admin/utils/currency';
+
+export function getSubscriptionData(sub) {
+    const data = {
+        ...sub,
+        attribution: {
+            ...sub.attribution,
+            referrerSource: sub.attribution?.referrer_source || 'Unknown',
+            referrerMedium: sub.attribution?.referrer_medium || '-'
+        },
+        startDate: sub.start_date ? moment(sub.start_date).format('D MMM YYYY') : '-',
+        validUntil: validUntil(sub),
+        hasEnded: isCanceled(sub),
+        willEndSoon: isSetToCancel(sub),
+        cancellationReason: sub.cancellation_reason,
+        price: {
+            ...sub.price,
+            currencySymbol: getSymbol(sub.price.currency),
+            nonDecimalAmount: getNonDecimal(sub.price.amount)
+        },
+        isComplimentary: isComplimentary(sub),
+        compExpiry: compExpiry(sub),
+        trialUntil: trialUntil(sub)
+    };
+
+    data.validityDetails = validityDetails(data);
+
+    return data;
+}
+
+export function validUntil(sub) {
+    // If a subscription has been canceled immediately, don't render the end of validity date
+    // Reason: we don't store the exact cancelation date in the subscription object
+    if (sub.status === 'canceled' && !sub.cancel_at_period_end) {
+        return '';
+    }
+
+    // Otherwise, show the current period end date
+    if (sub.current_period_end) {
+        return moment(sub.current_period_end).format('D MMM YYYY');
+    }
+
+    return '';
+}
+
+export function isActive(sub) {
+    return ['active', 'trialing', 'past_due', 'unpaid'].includes(sub.status);
+}
+
+export function isComplimentary(sub) {
+    return !sub.id;
+}
+
+export function isCanceled(sub) {
+    return sub.status === 'canceled';
+}
+
+export function isSetToCancel(sub) {
+    return sub.cancel_at_period_end && isActive(sub);
+}
+
+export function compExpiry(sub) {
+    if (!sub.id && sub.tier && sub.tier.expiry_at) {
+        return moment(sub.tier.expiry_at).utc().format('D MMM YYYY');
+    }
+
+    return undefined;
+}
+
+export function trialUntil(sub) {
+    const inTrialMode = sub.trial_end_at && moment(sub.trial_end_at).isAfter(new Date(), 'day');
+    if (inTrialMode) {
+        return moment(sub.trial_end_at).format('D MMM YYYY');
+    }
+
+    return undefined;
+}
+
+export function validityDetails(data) {
+    if (data.isComplimentary && data.compExpiry) {
+        return `Expires ${data.compExpiry}`;
+    }
+
+    if (data.hasEnded) {
+        return `Ended ${data.validUntil}`;
+    }
+
+    if (data.willEndSoon) {
+        return `Has access until ${data.validUntil}`;
+    }
+
+    if (data.trialUntil) {
+        return `Ends ${data.trialUntil}`;
+    }
+
+    return `Renews ${data.validUntil}`;
+}
