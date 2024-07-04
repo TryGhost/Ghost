594e7ee03a55ffdd4942d2c5e78fa762a742aab0
Peter Zimon <peter.zimon@gmail.com>
2024-07-04 09:50:03 +0200

    Updated Portal notification styles
    DES-548
    
    Portal popup notification styles look outdated and harsh. Also in-popup notifications have several issues with positioning, alignment etc.

--- apps/portal/src/components/Notification.styles.js
+++ apps/portal/src/components/Notification.styles.js
@@ -15,16 +15,16 @@ const NotificationStyles = `
         top: 12px;
         right: 12px;
         width: 100%;
-        padding: 14px 44px 18px 20px;
+        padding: 16px 44px 16px 20px;
         max-width: 380px;
         min-height: 66px;
         font-size: 1.3rem;
         letter-spacing: 0.2px;
-        background: rgba(var(--grey1rgb),0.95);
+        background: var(--white);
         backdrop-filter: blur(8px);
-        color: var(--white);
+        color: var(--grey0);
         border-radius: 7px;
-        box-shadow: 0 3.2px 3.6px rgba(var(--black), 0.024), 0 8.8px 10px -5px rgba(var(--black), 0.08);
+        box-shadow: 0px 0px 1px 0px rgba(0, 0, 0, 0.30), 0px 51px 40px 0px rgba(0, 0, 0, 0.05), 0px 15.375px 12.059px 0px rgba(0, 0, 0, 0.03), 0px 6.386px 5.009px 0px rgba(0, 0, 0, 0.03), 0px 2.31px 1.812px 0px rgba(0, 0, 0, 0.02);
         animation: notification-slidein 0.55s cubic-bezier(0.215, 0.610, 0.355, 1.000);
     }
 
@@ -35,7 +35,7 @@ const NotificationStyles = `
     }
 
     .gh-portal-notification.slideout {
-        animation: notification-slideout 0.4s cubic-bezier(0.550, 0.055, 0.675, 0.190);
+        /*animation: notification-slideout 0.4s cubic-bezier(0.550, 0.055, 0.675, 0.190);*/
     }
 
     .gh-portal-notification.hide {
@@ -49,16 +49,16 @@ const NotificationStyles = `
         text-align: start;
         margin: 0;
         padding: 0;
-        color: var(--grey13);
+        color: var(--grey0);
         padding-inline-start: 40px;
     }
 
     .gh-portal-notification p strong {
-        color: var(--white);
+        color: var(--grey0);
     }
 
     .gh-portal-notification a {
-        color: var(--white);
+        color: var(--grey0);
         text-decoration: underline;
         transition: all 0.2s ease-in-out;
         outline: none;
@@ -70,10 +70,10 @@ const NotificationStyles = `
 
     .gh-portal-notification-icon {
         position: absolute;
-        top: calc(50% - 14px);
-        left: 17px;
-        width: 28px;
-        height: 28px;
+        top: calc(50% - 20px);
+        left: 16px;
+        width: 18px;
+        height: 18px;
     }
     html[dir="rtl"] .gh-portal-notification-icon {
         right: 17px;
@@ -93,7 +93,7 @@ const NotificationStyles = `
         top: 5px;
         bottom: 0;
         right: 5px;
-        color: var(--white);
+        color: var(--grey8);
         cursor: pointer;
         width: 12px;
         height: 12px;
@@ -104,7 +104,7 @@ const NotificationStyles = `
 
     .gh-portal-notification-closeicon:hover {
         opacity: 1.0;
-    }    
+    }
 
     @keyframes notification-slidein {
         0% { transform: translateX(380px); }
