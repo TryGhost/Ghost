a00110446cbd6f57ff34e3aa2d3a4622d93e25c3
Princi Vershwal <vershwal.princi@gmail.com>
2024-07-11 02:02:19 +0530

    Testing test run

--- apps/admin-x-design-system/test/unit/hello.test.js
+++ apps/admin-x-design-system/test/unit/hello.test.js
@@ -3,7 +3,6 @@ import assert from 'assert/strict';
 describe('Hello world', function () {
     it('Runs a test', function () {
         // TODO: Write me!
-        //assert.ok(require('../'));
-        assert.equal(1, 0);
+        assert.equal(1, 1);
     });
 });
