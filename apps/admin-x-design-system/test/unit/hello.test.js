f76a4e3f95057ddd9731926b8e98e7a972165bdf
Princi Vershwal <vershwal.princi@gmail.com>
2024-07-11 01:50:33 +0530

    Added initial test to admin-x-design-system : Testing tests

--- apps/admin-x-design-system/test/unit/hello.test.js
+++ apps/admin-x-design-system/test/unit/hello.test.js
@@ -3,6 +3,7 @@ import assert from 'assert/strict';
 describe('Hello world', function () {
     it('Runs a test', function () {
         // TODO: Write me!
-        assert.ok(require('../'));
+        //assert.ok(require('../'));
+        assert.equal(1, 0);
     });
 });
