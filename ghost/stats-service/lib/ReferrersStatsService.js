d18e680ee3c6e85439c1f0adb0a08c3afc1abb5f
Princi Vershwal <vershwal.princi@gmail.com>
2024-07-16 23:40:12 +0530

    Added 90 days filter to Top Sources Admin Dashboard query

--- ghost/stats-service/lib/ReferrersStatsService.js
+++ ghost/stats-service/lib/ReferrersStatsService.js
@@ -101,13 +101,15 @@ class ReferrersStatsService {
      **/
     async fetchAllPaidConversionSources() {
         const knex = this.knex;
+        const ninetyDaysAgo = moment.utc().subtract(90, 'days').startOf('day').utc().format('YYYY-MM-DD HH:mm:ss');
         const rows = await knex('members_subscription_created_events')
             .select(knex.raw(`DATE(created_at) as date`))
             .select(knex.raw(`COUNT(*) as paid_conversions`))
             .select(knex.raw(`referrer_source as source`))
-            .where('created_at', '>=', knex.raw(`DATE_SUB(CURRENT_DATE, INTERVAL 90 DAY)`))
+            .where('created_at', '>=', ninetyDaysAgo)
             .groupBy('date', 'referrer_source')
             .orderBy('date');
+        
         return rows;
     }
 
@@ -116,13 +118,15 @@ class ReferrersStatsService {
      **/
     async fetchAllSignupSources() {
         const knex = this.knex;
+        const ninetyDaysAgo = moment.utc().subtract(90, 'days').startOf('day').utc().format('YYYY-MM-DD HH:mm:ss');
         const rows = await knex('members_created_events')
             .select(knex.raw(`DATE(created_at) as date`))
             .select(knex.raw(`COUNT(*) as signups`))
             .select(knex.raw(`referrer_source as source`))
-            .where('created_at', '>=', knex.raw(`DATE_SUB(CURRENT_DATE, INTERVAL 90 DAY)`))
+            .where('created_at', '>=', ninetyDaysAgo)
             .groupBy('date', 'referrer_source')
             .orderBy('date');
+
         return rows;
     }
 }
