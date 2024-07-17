5ce7a65e914ca5fe01eace5a287f1470d4fc72f6
Steve Larson <9larsons@gmail.com>
2024-07-17 16:55:47 -0500

    ✨ Improved performance loading the posts list in admin (#20618)
    ref https://github.com/TryGhost/Ghost/pull/20503
    - undid the reversion for the performance improvements
    - built upon new tests for the posts list functionality in admin,
    including right click actions
    
    This was originally reverted because the changes to improve loading
    response times broke right click (bulk) actions in the posts list. This
    was not caught because it turned out we had near-zero test coverage of
    that part of the codebase. Test coverage has been expanded for the posts
    list, and while not comprehensive, is a much better place for us to be
    in.

--- ghost/admin/app/controllers/posts.js
+++ ghost/admin/app/controllers/posts.js
@@ -1,5 +1,5 @@
 import Controller from '@ember/controller';
-import SelectionList from 'ghost-admin/utils/selection-list';
+import SelectionList from 'ghost-admin/components/posts-list/selection-list';
 import {DEFAULT_QUERY_PARAMS} from 'ghost-admin/helpers/reset-query-params';
 import {action} from '@ember/object';
 import {inject} from 'ghost-admin/decorators/inject';
