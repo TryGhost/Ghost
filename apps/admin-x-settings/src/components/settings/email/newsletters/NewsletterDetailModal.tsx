90adb367b869cebd186a4933901b89a8f3b9ff41
Princi Vershwal <vershwal.princi@gmail.com>
2024-07-18 14:54:47 +0530

    fix

--- apps/admin-x-settings/src/components/settings/email/newsletters/NewsletterDetailModal.tsx
+++ apps/admin-x-settings/src/components/settings/email/newsletters/NewsletterDetailModal.tsx
@@ -170,13 +170,8 @@ const Sidebar: React.FC<{
             NiceModal.show(ConfirmationModal, {
                 title: 'Archive newsletter',
                 prompt: <>
-<<<<<<< HEAD
                     <div className="mb-6">Your newsletter <strong>{newsletter.name}</strong> will no longer be visible to members or available as an option when publishing new posts.</div>
                     <div>Existing posts previously sent as this newsletter will remain unchanged.</div>
-=======
-                    <p>Your newsletter <strong>{newsletter.name}</strong> will no longer be visible to members or available as an option when publishing new posts.</p>
-                    <p><br></br>Existing posts previously sent as this newsletter will remain unchanged.</p>
->>>>>>> f0fe8dbb14 (Changes)
                 </>,
                 okLabel: 'Archive',
                 okColor: 'red',
