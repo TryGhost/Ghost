a0589b1163fc0af3166f7996f6eccbd96c9a37f1
Ronald Langeveld <hi@ronaldlangeveld.com>
2024-07-24 10:34:48 +0800

    Added contentVisibility toggle to HTML Card (#1307)
    ref https://linear.app/tryghost/project/content-visibility-sponsorships-0d053a03ee20
    
    - Added contentVisibility toggle for HTML card under a feature flag

--- ghost/core/core/server/services/koenig/node-renderers/html-renderer.js
+++ ghost/core/core/server/services/koenig/node-renderers/html-renderer.js
@@ -7,12 +7,24 @@ export function renderHtmlNode(node, options = {}) {
 
     const html = node.html;
 
+    const segment = node.visibility.segment || '';
+
+    const isEmailOnly = node.visibility.emailOnly;
+
     if (!html) {
         return renderEmptyContainer(document);
     }
 
     const textarea = document.createElement('textarea');
     textarea.value = `\n<!--kg-card-begin: html-->\n${html}\n<!--kg-card-end: html-->\n`;
+
+    if (segment && isEmailOnly) {
+        textarea.setAttribute('data-gh-segment', segment);
+    }
+
+    if (isEmailOnly && options.target !== 'email') {
+        return renderEmptyContainer(document);
+    }
 
     // `type: 'value'` will render the value of the textarea element
     // @see @tryghost/kg-lexical-html-renderer package
