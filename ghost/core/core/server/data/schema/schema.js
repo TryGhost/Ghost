cb85d03d20f214b09dd05b99bf8e56ead3d13d4e
Princi Vershwal <vershwal.princi@gmail.com>
2024-08-01 15:32:23 +0530

    Added created_at index to the members_click_events table

--- ghost/core/core/server/data/schema/schema.js
+++ ghost/core/core/server/data/schema/schema.js
@@ -998,7 +998,7 @@ module.exports = {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
         member_id: {type: 'string', maxlength: 24, nullable: false, references: 'members.id', cascadeDelete: true},
         redirect_id: {type: 'string', maxlength: 24, nullable: false, references: 'redirects.id', cascadeDelete: true},
-        created_at: {type: 'dateTime', nullable: false}
+        created_at: {type: 'dateTime', nullable: false, index: true}
     },
     members_feedback: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
