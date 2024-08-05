853def491110e52a35d1178a91a516ae299453ec
Ronald Langeveld <hi@ronaldlangeveld.com>
2024-08-05 21:17:57 +0700

    Fixed browser tests for unsaved changes
    no issue
    
    - takes into account that there could be an additional editor in the DOM

--- ghost/core/test/e2e-browser/admin/publishing.spec.js
+++ ghost/core/test/e2e-browser/admin/publishing.spec.js
@@ -78,7 +78,7 @@ const createPage = async (page, {title = 'Hello world', body = 'This is my post 
     await page.locator('[data-test-editor-title-input]').fill(title);
 
     // wait for editor to be ready
-    await expect(page.locator('[data-lexical-editor="true"]')).toBeVisible();
+    await expect(page.locator('[data-lexical-editor="true"]').first()).toBeVisible();
 
     // Continue to the body by pressing enter
     await page.keyboard.press('Enter');
