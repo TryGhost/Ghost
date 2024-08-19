12630771ac671cb1a4ca7244a8c0c684ccdc6785
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2024-08-19 20:37:33 +0800

    Migrated the design system to Radix (#20244)
    ref DES-331

--- apps/admin-x-design-system/src/global/Tooltip.tsx
+++ apps/admin-x-design-system/src/global/Tooltip.tsx
@@ -1,3 +1,4 @@
+import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 import clsx from 'clsx';
 import React from 'react';
 
@@ -7,29 +8,32 @@ export interface TooltipProps {
     children?: React.ReactNode;
     containerClassName?: string;
     tooltipClassName?: string;
-    origin?: 'right' | 'center' | 'left'
+    origin?: 'start' | 'center' | 'end'
 }
 
 const Tooltip: React.FC<TooltipProps> = ({content, size = 'sm', children, containerClassName, tooltipClassName, origin = 'center'}) => {
     containerClassName = clsx(
-        'group/tooltip relative',
+        'will-change-[opacity]',
         containerClassName
     );
 
     tooltipClassName = clsx(
-        'absolute -mt-1 -translate-y-full whitespace-nowrap rounded-sm bg-black px-2 py-0.5 leading-normal text-white opacity-0 transition-all group-hover/tooltip:opacity-100 dark:bg-grey-950',
+        'select-none rounded-sm bg-black px-2 py-0.5 leading-normal text-white will-change-[transform,opacity]',
         size === 'sm' && 'text-xs',
-        size === 'md' && 'text-sm',
-        origin === 'center' && 'left-1/2 -translate-x-1/2',
-        origin === 'left' && 'left-0',
-        origin === 'right' && 'right-0'
+        size === 'md' && 'text-sm'
     );
 
     return (
-        <span className={containerClassName}>
-            {children}
-            <span className={tooltipClassName}>{content}</span>
-        </span>
+        <TooltipPrimitive.Provider delayDuration={0}>
+            <TooltipPrimitive.Root>
+                <TooltipPrimitive.Trigger className={containerClassName} onClick={event => event.preventDefault()}>
+                    {children}
+                </TooltipPrimitive.Trigger>
+                <TooltipPrimitive.Content align={origin} className={tooltipClassName} sideOffset={4} onPointerDownOutside={event => event.preventDefault()}>
+                    {content}
+                </TooltipPrimitive.Content>
+            </TooltipPrimitive.Root>
+        </TooltipPrimitive.Provider>
     );
 };
 
