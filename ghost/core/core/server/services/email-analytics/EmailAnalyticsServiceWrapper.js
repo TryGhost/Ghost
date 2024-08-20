7cb1538a27aa69de904cb31a4f4ee58e7932f549
Steve Larson <9larsons@gmail.com>
2024-08-20 12:25:01 -0500

    ✨ Updated email analytics job to prioritize open events (#20800)
    ref https://linear.app/tryghost/issue/ENG-1477
    - updated email analytics job to prioritize open events
    - put limits on non-open event fetching
    - updated job to now restart itself until processing is at a
    sufficiently low volume
    
    Previously the EmailAnalytics job would process all event data equally.
    When there's sufficient recipients (>20k), we could see delays in the
    open rate data in Admin because of all the delivered events being
    processed. Open events are far more important to users, so we've now
    prioritized processing those events before any others.
    
    Processing of events shouldn't be any faster or slower with this as this
    doesn't change throughput, just order.
    
    NOTE: Use the mailgun-mock-server in TryGhost/Toolbox for testing.

--- ghost/core/core/server/services/email-analytics/EmailAnalyticsServiceWrapper.js
+++ ghost/core/core/server/services/email-analytics/EmailAnalyticsServiceWrapper.js
@@ -57,11 +57,22 @@ class EmailAnalyticsServiceWrapper {
         });
     }
 
-    async fetchLatest({maxEvents} = {maxEvents: Infinity}) {
-        logging.info('[EmailAnalytics] Fetch latest started');
+    async fetchLatestOpenedEvents({maxEvents} = {maxEvents: Infinity}) {
+        logging.info('[EmailAnalytics] Fetch latest opened events started');
 
         const fetchStartDate = new Date();
-        const totalEvents = await this.service.fetchLatest({maxEvents});
+        const totalEvents = await this.service.fetchLatestOpenedEvents({maxEvents});
+        const fetchEndDate = new Date();
+
+        logging.info(`[EmailAnalytics] Fetched ${totalEvents} events and aggregated stats in ${fetchEndDate.getTime() - fetchStartDate.getTime()}ms (latest opens)`);
+        return totalEvents;
+    }
+
+    async fetchLatestNonOpenedEvents({maxEvents} = {maxEvents: Infinity}) {
+        logging.info('[EmailAnalytics] Fetch latest non-opened events started');
+
+        const fetchStartDate = new Date();
+        const totalEvents = await this.service.fetchLatestNonOpenedEvents({maxEvents});
         const fetchEndDate = new Date();
 
         logging.info(`[EmailAnalytics] Fetched ${totalEvents} events and aggregated stats in ${fetchEndDate.getTime() - fetchStartDate.getTime()}ms (latest)`);
@@ -69,7 +80,7 @@ class EmailAnalyticsServiceWrapper {
     }
 
     async fetchMissing({maxEvents} = {maxEvents: Infinity}) {
-        logging.info('[EmailAnalytics] Fetch missing started');
+        logging.info('[EmailAnalytics] Fetch missing events started');
 
         const fetchStartDate = new Date();
         const totalEvents = await this.service.fetchMissing({maxEvents});
@@ -83,7 +94,7 @@ class EmailAnalyticsServiceWrapper {
         if (maxEvents < 300) {
             return 0;
         }
-        logging.info('[EmailAnalytics] Fetch scheduled started');
+        logging.info('[EmailAnalytics] Fetch scheduled events started');
 
         const fetchStartDate = new Date();
         const totalEvents = await this.service.fetchScheduled({maxEvents});
@@ -100,13 +111,31 @@ class EmailAnalyticsServiceWrapper {
         }
         this.fetching = true;
 
+        // NOTE: Data shows we can process ~7500 events per minute on Pro; this can vary locally
         try {
-            const c1 = await this.fetchLatest({maxEvents: Infinity});
-            const c2 = await this.fetchMissing({maxEvents: Infinity});
+            // Prioritize opens since they are the most important (only data directly displayed to users)
+            await this.fetchLatestOpenedEvents({maxEvents: Infinity});
+
+            // Set limits on how much we fetch without checkings for opened events. During surge events (following newsletter send)
+            //  we want to make sure we don't spend too much time collecting delivery data.
+            const c1 = await this.fetchLatestNonOpenedEvents({maxEvents: 20000});
+            if (c1 > 15000) { 
+                this.fetching = false;
+                logging.info('[EmailAnalytics] Restarting fetch due to high event count');
+                this.startFetch();
+                return;
+            }
+            const c2 = await this.fetchMissing({maxEvents: 20000});
+            if ((c1 + c2) > 15000) {
+                this.fetching = false;
+                logging.info('[EmailAnalytics] Restarting fetch due to high event count');
+                this.startFetch();
+                return;
+            }
 
             // Only fetch scheduled if we didn't fetch a lot of normal events
             await this.fetchScheduled({maxEvents: 20000 - c1 - c2});
-
+            
             this.fetching = false;
         } catch (e) {
             logging.error(e, 'Error while fetching email analytics');
