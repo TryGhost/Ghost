548b6fbac7cc27b893b1e97042a239bf0ed654dd
Peter Zimon <peter.zimon@gmail.com>
2024-08-22 14:12:24 +0200

    Added basic inbox view

--- apps/admin-x-activitypub/src/components/Inbox.tsx
+++ apps/admin-x-activitypub/src/components/Inbox.tsx
@@ -6,7 +6,7 @@ import NiceModal from '@ebay/nice-modal-react';
 import React, {useState} from 'react';
 import {Activity} from './activities/ActivityItem';
 import {ActorProperties, ObjectProperties} from '@tryghost/admin-x-framework/api/activitypub';
-import {Button, Heading} from '@tryghost/admin-x-design-system';
+import {Button, Heading, Select, SelectOption, Tooltip} from '@tryghost/admin-x-design-system';
 import {useBrowseInboxForUser} from '../MainContent';
 
 interface InboxProps {}
@@ -15,6 +15,7 @@ const Inbox: React.FC<InboxProps> = ({}) => {
     const {data: activities = []} = useBrowseInboxForUser('index');
     const [, setArticleContent] = useState<ObjectProperties | null>(null);
     const [, setArticleActor] = useState<ActorProperties | null>(null);
+    const [layout, setLayout] = useState('inbox');
 
     const inboxTabActivities = activities.filter((activity: Activity) => {
         const isCreate = activity.type === 'Create' && ['Article', 'Note'].includes(activity.object.type);
@@ -32,13 +33,47 @@ const Inbox: React.FC<InboxProps> = ({}) => {
         });
     };
 
+    const selectOptions: SelectOption[] = [
+        {value: 'option-1', label: 'Home'},
+        {value: 'option-2', label: 'Posts'},
+        {value: 'option-3', label: 'Notes'},
+        {value: 'option-4', label: 'Media'}
+    ];
+
+    const defaultValue: SelectOption = {
+        value: 'option-1', label: 'Home'
+    };
+
+    const controlClasses = {
+        control: '!bg-transparent !hidden mr-2 !pl-0 text-xl font-bold',
+        menu: '!min-w-[120px]'
+    };
+
     return (
         <>
             <MainNavigation />
+            <div className='mx-auto mt-4 flex w-full max-w-[640px] items-center justify-between border-b border-grey-200 pb-4'>
+                <div>
+                    <Select controlClasses={controlClasses} defaultValue={defaultValue} options={selectOptions} unstyled onSelect={() => {}} />
+                    <h2 className='text-xl font-bold'>Home</h2>
+                </div>
+                <div className=''>
+                    <Tooltip content="Inbox">
+                        <Button className='!px-2' icon='listview' iconColorClass={layout === 'inbox' ? 'text-black' : 'text-grey-400'} size='sm' onClick={() => {
+                            setLayout('inbox');
+                        }} />
+                    </Tooltip>
+                    <Tooltip content="Feed">
+                        <Button className='!px-2' icon='cardview' iconColorClass={layout === 'feed' ? 'text-black' : 'text-grey-400'} size='sm' onClick={() => {
+                            setLayout('feed');
+                        }} />
+                    </Tooltip>
+                </div>
+            </div>
             <div className='z-0 flex w-full flex-col'>
                 <div className='w-full'>
                     {inboxTabActivities.length > 0 ? (
-                        <ul className='mx-auto flex max-w-[560px] flex-col py-8'>
+                        <ul className='mx-auto flex max-w-[640px] flex-col'>
                             {inboxTabActivities.reverse().map(activity => (
                                 <li
                                     key={activity.id}
@@ -47,7 +82,7 @@ const Inbox: React.FC<InboxProps> = ({}) => {
                                 >
                                     <FeedItem
                                         actor={activity.actor}
-                                        layout='feed'
+                                        layout={layout}
                                         object={activity.object}
                                         type={activity.type}
                                     />
--- apps/admin-x-activitypub/src/components/global/APAvatar.tsx
+++ apps/admin-x-activitypub/src/components/global/APAvatar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {ActorProperties} from '@tryghost/admin-x-framework/api/activitypub';
 import {Icon} from '@tryghost/admin-x-design-system';
 
-type AvatarSize = 'sm' | 'lg';
+type AvatarSize = 'xs' | 'sm' | 'lg';
 
 interface APAvatarProps {
     author?: ActorProperties;
@@ -10,23 +10,30 @@ interface APAvatarProps {
 }
 
 const APAvatar: React.FC<APAvatarProps> = ({author, size}) => {
-    let avatarSize = '';
+    let iconSize = 18;
+    let containerClass = '';
+    let imageClass = 'z-10 rounded w-10 h-10';
 
     switch (size) {
+    case 'xs':
+        iconSize = 12;
+        containerClass = 'z-10 rounded bg-grey-100 flex items-center justify-center p-[3px] w-6 h-6';
+        imageClass = 'z-10 rounded w-6 h-6';
+        break;
     case 'sm':
-        avatarSize = ' w-10 h-10 ';
+        containerClass = 'z-10 rounded bg-grey-100 flex items-center justify-center p-[10px] w-10 h-10';
         break;
     case 'lg':
-        avatarSize = ' w-22 h-22 ';
+        containerClass = 'z-10 rounded bg-grey-100 flex items-center justify-center p-[10px] w-22 h-22';
         break;
     default:
-        avatarSize = ' w-10 h-10 ';
+        containerClass = 'z-10 rounded bg-grey-100 flex items-center justify-center p-[10px] w-10 h-10';
         break;
     }
 
     return (
         <>
-            {author && author!.icon?.url ? <img className={`z-10 ${avatarSize} rounded`} src={author!.icon?.url}/> : <div className={`z-10 rounded bg-grey-100 ${avatarSize} flex items-center justify-center p-[10px]`}><Icon colorClass='text-grey-600' name='user' size={18} /></div>}
+            {author && author!.icon?.url ? <img className={imageClass} src={author!.icon?.url}/> : <div className={containerClass}><Icon colorClass='text-grey-600' name='user' size={iconSize} /></div>}
         </>
     );
 };
