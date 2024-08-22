99de5c95a30f9361ca81d0bc2dda9b03c0533e7e
Chris Raible <chris@ghost.org>
2024-08-22 16:21:00 -0700

    Added Prometheus metrics for OpenTelemetry
    ref https://linear.app/tryghost/issue/ENG-1505/start-monitoring-event-loop-utilization-in-production-with
    
    - The two main constraints we've observed in Ghost are the database connection pool and the CPU usage. However, there is a third constraint that we may be hitting, but can't currently observe: the event loop.
    - This commit re-enabled OpenTelemetry (behind a config flag), removes the problematic tracing instrumentation which was breaking the frontend, and adds a Prometheus endpoint to export the eventLoopUtilization metric.
    - This should give us visibility into whether we are hitting constraints in the event loop and address the root cause if we are.

--- ghost/core/core/shared/instrumentation.js
+++ ghost/core/core/shared/instrumentation.js
@@ -1,32 +1,38 @@
+const perf = require('perf_hooks');
+const logging = require('@tryghost/logging');
+
 async function initOpenTelemetry({config}) {
-    // Always enable in development environment
-    // In production, only enable if explicitly enabled via config `opentelemetry:enabled`
-    // TODO: Instrumentation currently breaks viewing posts - disabled until we can fix
-    // const isDevelopment = process.env.NODE_ENV === 'development';
-    const isConfigured = config.get('opentelemetry:enabled');
-    const enabled = isConfigured; // || isDevelopment;
+    // Only enable if explicitly enabled via config `opentelemetry:enabled`
+    const enabled = config.get('opentelemetry:enabled');
     if (!enabled) {
         return;
     }
-    const collectorOptions = {
-        url: config.get('opentelemetry:exporter:endpoint') || 'http://localhost:4318/v1/traces',
-        headers: {},
-        concurrencyLimit: 10
-    };
+    perf.performance.mark('opentelemetry:init:start');
+    logging.info('Initializing OpenTelemetry');
 
     // Lazyloaded to avoid boot time overhead when not enabled
     const {NodeSDK} = require('@opentelemetry/sdk-node');
-    const {OTLPTraceExporter} = require('@opentelemetry/exporter-trace-otlp-http');
-    const {getNodeAutoInstrumentations} = require('@opentelemetry/auto-instrumentations-node');
+    const {PrometheusExporter} = require('@opentelemetry/exporter-prometheus');
+    const {RuntimeNodeInstrumentation} = require('@opentelemetry/instrumentation-runtime-node');
+    perf.performance.mark('opentelemetry:init:packagesLoaded');
+
+    const prometheusExporter = new PrometheusExporter({
+        port: config.get('opentelemetry:prometheus:port'),
+        startServer: true
+    });
 
     const sdk = new NodeSDK({
         serviceName: 'ghost',
-        traceExporter: new OTLPTraceExporter(collectorOptions),
+        metricReader: prometheusExporter,
         instrumentations: [
-            getNodeAutoInstrumentations()
+            new RuntimeNodeInstrumentation({
+                eventLoopUtilizationMeasurementInterval: 5000
+            })
         ]
     });
     sdk.start();
+    perf.performance.mark('opentelemetry:init:finished');
+    logging.debug('OpenTelemetry initialized in', perf.performance.measure('opentelemetry:init:duration', 'opentelemetry:init:start', 'opentelemetry:init:finished').duration, 'ms');
 }
 
 module.exports = {
