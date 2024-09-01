c1798620bbe7bc2f7ee4ccced5180790973ddcce
Hannah Wolfe <github.erisds@gmail.com>
2024-09-01 16:33:31 +0100

    Added basic date-handling to stats page (#20887)
    ref https://linear.app/tryghost/issue/ANAL-21/filtering-the-dashboard
    
    - Added the very basic date filter back from the dashboard
    - Without this, it's very hard to see that the basic concept is working
    - This method can be reused for creating any filter

--- ghost/admin/app/controllers/stats.js
+++ ghost/admin/app/controllers/stats.js
@@ -1,4 +1,34 @@
 import Controller from '@ember/controller';
+import {action} from '@ember/object';
+import {tracked} from '@glimmer/tracking';
+
+// Options 30 and 90 need an extra day to be able to distribute ticks/gridlines evenly
+const DAYS_OPTIONS = [{
+    name: '7 Days',
+    value: 7
+}, {
+    name: '30 Days',
+    value: 30 + 1
+}, {
+    name: '90 Days',
+    value: 90 + 1
+}];
 
 export default class StatsController extends Controller {
+    daysOptions = DAYS_OPTIONS;
+
+    /**
+     * @type {number|'all'}
+     * Amount of days to load for member count and MRR related charts
+     */
+    @tracked chartDays = 30 + 1;
+
+    @action
+    onDaysChange(selected) {
+        this.chartDays = selected.value;
+    }
+
+    get selectedDaysOption() {
+        return this.daysOptions.find(d => d.value === this.chartDays);
+    }
 }
