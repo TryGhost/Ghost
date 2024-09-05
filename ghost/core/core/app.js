03ef4bd7ef3645f34db5298a25277f44a6c727d3
Sam Lord <hello@samlord.me>
2024-09-05 17:15:09 +0100

    Added middleware to prevent other sites' content from being served (#20922)
    ref ONC-294
    
    ---------
    
    Co-authored-by: Daniel Lockyer <hi@daniellockyer.com>

--- ghost/core/core/app.js
+++ ghost/core/core/app.js
@@ -1,6 +1,7 @@
 const sentry = require('./shared/sentry');
 const express = require('./shared/express');
 const config = require('./shared/config');
+const logging = require('@tryghost/logging');
 const urlService = require('./server/services/url');
 
 const fs = require('fs');
@@ -27,11 +28,32 @@ const maintenanceMiddleware = function maintenanceMiddleware(req, res, next) {
     fs.createReadStream(path.resolve(__dirname, './server/views/maintenance.html')).pipe(res);
 };
 
+// Used by Ghost (Pro) to ensure that requests cannot be served by the wrong site
+const siteIdMiddleware = function siteIdMiddleware(req, res, next) {
+    const configSiteId = config.get('hostSettings:siteId');
+    const headerSiteId = req.headers['x-site-id'];
+
+    if (`${configSiteId}` === `${headerSiteId}`) {
+        return next();
+    }
+
+    logging.warn(`Mismatched site id (expected ${configSiteId}, got ${headerSiteId})`);
+
+    res.set({
+        'Cache-Control': 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0'
+    });
+    res.writeHead(500);
+    res.end();
+};
+
 const rootApp = () => {
     const app = express('root');
     app.use(sentry.requestHandler);
     if (config.get('sentry')?.tracing?.enabled === true) {
         app.use(sentry.tracingHandler);
+    }
+    if (config.get('hostSettings:siteId')) {
+        app.use(siteIdMiddleware);
     }
     app.enable('maintenance');
     app.use(maintenanceMiddleware);
