dd183cf25e13119b6f0592fea1aa89e47f97a877
Peter Zimon <zimo@ghost.org>
2024-09-05 17:49:20 +0200

    Stats page refinements (#20924)
    [ANAL-43](https://linear.app/tryghost/issue/ANAL-50/update-colors-of-barlist)
    
    - Copy is too technical, doesn't follow conventions on Stats page
    - Range filter dropdown has to be updated with more meaningful values
    - KPI charts need a granularity dropdown to display meaninful charts
    depending on the context
    - Typography details should be updated
    - "Posts/pages" dropdown needs to be added to Content section. This is a
    Ghost specific filter that brings high value to customers
    - "Campaigns" dropdown needs to be added to Sources section to support
    ad tracking and filtering in the future
    - BarList colors should be updated to be less purple all over the place

--- ghost/admin/app/components/stats/charts/kpis.js
+++ ghost/admin/app/components/stats/charts/kpis.js
@@ -4,19 +4,19 @@ import Component from '@glimmer/component';
 import React from 'react';
 import moment from 'moment-timezone';
 import {AreaChart, useQuery} from '@tinybirdco/charts';
+import {hexToRgba} from 'ghost-admin/utils/stats';
 import {inject} from 'ghost-admin/decorators/inject';
+import {statsStaticColors} from '../../../utils/stats';
 
 export default class KpisComponent extends Component {
     @inject config;
 
     ReactComponent = (props) => {
-        let chartDays = props.chartDays;
+        let chartRange = props.chartRange;
         let audience = props.audience;
 
-        // @TODO: ATM there's a two day worth gap (padding) on the right side
-        // of the chart. endDate needs to be adjusted to get rid of it
         const endDate = moment().endOf('day');
-        const startDate = moment().subtract(chartDays - 1, 'days').startOf('day');
+        const startDate = moment().subtract(chartRange - 1, 'days').startOf('day');
 
         /**
          * @typedef {Object} Params
@@ -35,15 +35,52 @@ export default class KpisComponent extends Component {
             member_status: audience.length === 0 ? null : audience.join(',')
         };
 
-        const LINE_COLOR = '#8E42FF';
+        const LINE_COLOR = statsStaticColors[0];
         const INDEX = 'date';
-        const CATEGORY = props.selected;
+        const CATEGORY = props.selected === 'unique_visitors' ? 'visits' : props.selected;
 
         const {data, meta, error, loading} = useQuery({
             endpoint: `${this.config.stats.endpoint}/v0/pipes/kpis.json`,
             token: this.config.stats.token,
             params
         });
+
+        // Create an array with every second date value
+        const dateLabels = [];
+        let currentDate = startDate.clone();
+        let skipDays;
+        switch (chartRange) {
+        case 1:
+            skipDays = 0; // Show all hours for 1 day
+            break;
+        case 7:
+            skipDays = 0; // Skip every other day for 7 days
+            break;
+        case (30 + 1):
+            skipDays = 2; // Skip every 3rd day for 30 and 90 days
+            break;
+        case (90 + 1):
+            skipDays = 5; // Skip every 3rd day for 30 and 90 days
+            break;
+        case (365 + 1):
+        case (12 * (30 + 1)):
+            skipDays = 30; // Skip every 7th day for 1 year
+            break;
+        case 1000:
+            skipDays = 29; // Skip every 30th day for all time
+            break;
+        default:
+            skipDays = 1; // Default to skipping every other day
+        }
+
+        let dayCounter = 0;
+        while (currentDate.isSameOrBefore(endDate)) {
+            if (dayCounter % (skipDays + 1) === 0) {
+                dateLabels.push(currentDate.format('YYYY-MM-DD'));
+            }
+            currentDate.add(1, 'days');
+            dayCounter = dayCounter + 1;
+        }
 
         return (
             <AreaChart
@@ -53,7 +90,7 @@ export default class KpisComponent extends Component {
                 error={error}
                 index={INDEX}
                 categories={[CATEGORY]}
-                colorPalette={[LINE_COLOR, '#008060', '#0EB1B9', '#9263AF', '#5A6FC0']}
+                colorPalette={[LINE_COLOR]}
                 backgroundColor="transparent"
                 fontSize="13px"
                 textColor="#AEB7C1"
@@ -61,27 +98,30 @@ export default class KpisComponent extends Component {
                 params={params}
                 options={{
                     grid: {
-                        left: '0%',
-                        right: '0%',
+                        left: '10px',
+                        right: '10px',
                         top: '10%',
                         bottom: 0,
                         containLabel: true
                     },
                     xAxis: {
                         type: 'time',
-                        // min: startDate.toISOString(),
-                        // max: endDate.toISOString(),
-                        boundaryGap: ['0%', '0.5%'],
+                        min: startDate.toISOString(),
+                        max: endDate.subtract(1, 'day').toISOString(),
+                        boundaryGap: ['0%', '0%'],
                         axisLabel: {
-                            formatter: chartDays <= 7 ? '{ee}' : '{dd} {MMM}'
+                            formatter: chartRange <= 7 ? '{ee}' : '{d} {MMM}',
+                            customValues: dateLabels
                         },
                         axisTick: {
-                            alignWithLabel: true
+                            show: false,
+                            alignWithLabel: true,
+                            interval: 0
                         },
                         axisPointer: {
                             snap: true
                         },
-                        splitNumber: chartDays <= 7 ? 7 : 5,
+                        splitNumber: dateLabels.length,
                         splitLine: {
                             show: false
                         },
@@ -112,7 +152,26 @@ export default class KpisComponent extends Component {
                         },
                         extraCssText: 'box-shadow: 0px 100px 80px 0px rgba(0, 0, 0, 0.07), 0px 41.778px 33.422px 0px rgba(0, 0, 0, 0.05), 0px 22.336px 17.869px 0px rgba(0, 0, 0, 0.04), 0px 12.522px 10.017px 0px rgba(0, 0, 0, 0.04), 0px 6.65px 5.32px 0px rgba(0, 0, 0, 0.03), 0px 2.767px 2.214px 0px rgba(0, 0, 0, 0.02);',
                         formatter: function (fparams) {
-                            return `<div><div>${moment(fparams[0].value[0]).format('DD MMM, YYYY')}</div><div><span style="display: inline-block; margin-right: 16px; font-weight: 600;">Pageviews</span> ${fparams[0].value[1]}</div></div>`;
+                            let displayValue;
+                            let tooltipTitle;
+                            switch (CATEGORY) {
+                            case 'avg_session_sec':
+                                tooltipTitle = 'Visit duration';
+                                displayValue = fparams[0].value[1] !== null && (fparams[0].value[1] / 60).toFixed(0) + ' min';
+                                break;
+                            case 'bounce_rate':
+                                tooltipTitle = 'Bounce rate';
+                                displayValue = fparams[0].value[1] !== null && fparams[0].value[1].toFixed(2) + '%';
+                                break;
+                            default:
+                                tooltipTitle = 'Unique visitors';
+                                displayValue = fparams[0].value[1] !== null && fparams[0].value[1];
+                                break;
+                            }
+                            if (!displayValue) {
+                                displayValue = 'N/A';
+                            }
+                            return `<div><div>${moment(fparams[0].value[0]).format('D MMM, YYYY')}</div><div><span style="display: inline-block; margin-right: 16px; font-weight: 600;">${tooltipTitle}</span> ${displayValue}</div></div>`;
                         }
                     },
                     series: [
@@ -123,7 +182,6 @@ export default class KpisComponent extends Component {
                             type: 'line',
                             areaStyle: {
                                 opacity: 0.6,
-                                // color: 'rgba(198, 220, 255, 1)'
                                 color: {
                                     type: 'linear',
                                     x: 0,
@@ -131,11 +189,11 @@ export default class KpisComponent extends Component {
                                     x2: 0,
                                     y2: 1,
                                     colorStops: [{
-                                        offset: 0, color: 'rgba(142, 66, 255, 0.3)' // color at 0%
+                                        offset: 0, color: hexToRgba(LINE_COLOR, 0.3)
                                     }, {
-                                        offset: 1, color: 'rgba(142, 66, 255, 0.0)' // color at 100%
+                                        offset: 1, color: hexToRgba(LINE_COLOR, 0.0)
                                     }],
-                                    global: false // default is false
+                                    global: false
                                 }
                             },
                             lineStyle: {
@@ -152,11 +210,12 @@ export default class KpisComponent extends Component {
                             symbol: 'circle',
                             symbolSize: 10,
                             z: 8,
-                            smooth: false,
-                            name: props.selected,
+                            smooth: true,
+                            smoothMonotone: 'x',
+                            name: CATEGORY,
                             data: (data ?? []).map(row => [
                                 String(row[INDEX]),
-                                row[props.selected]
+                                row[CATEGORY]
                             ])
                         }
                     ]
--- ghost/admin/app/controllers/stats.js
+++ ghost/admin/app/controllers/stats.js
@@ -1,29 +1,16 @@
 import Controller from '@ember/controller';
-import {AUDIENCE_TYPES} from 'ghost-admin/components/stats/parts/audience-filter';
+import {AUDIENCE_TYPES, RANGE_OPTIONS} from 'ghost-admin/utils/stats';
 import {action} from '@ember/object';
 import {tracked} from '@glimmer/tracking';
 
-// Options 30 and 90 need an extra day to be able to distribute ticks/gridlines evenly
-const DAYS_OPTIONS = [{
-    name: '7 Days',
-    value: 7
-}, {
-    name: '30 Days',
-    value: 30 + 1
-}, {
-    name: '90 Days',
-    value: 90 + 1
-}];
-
 export default class StatsController extends Controller {
-    daysOptions = DAYS_OPTIONS;
+    rangeOptions = RANGE_OPTIONS;
     audienceOptions = AUDIENCE_TYPES;
-
     /**
      * @type {number|'all'}
-     * Amount of days to load for member count and MRR related charts
+     * Date range to load for member count and MRR related charts
      */
-    @tracked chartDays = 30 + 1;
+    @tracked chartRange = 30 + 1;
     /**
      * @type {array}
      * Filter by audience
@@ -32,8 +19,8 @@ export default class StatsController extends Controller {
     @tracked excludedAudiences = '';
 
     @action
-    onDaysChange(selected) {
-        this.chartDays = selected.value;
+    onRangeChange(selected) {
+        this.chartRange = selected.value;
     }
 
     @action
@@ -50,7 +37,7 @@ export default class StatsController extends Controller {
         }
     }
 
-    get selectedDaysOption() {
-        return this.daysOptions.find(d => d.value === this.chartDays);
+    get selectedRangeOption() {
+        return this.rangeOptions.find(d => d.value === this.chartRange);
     }
 }
