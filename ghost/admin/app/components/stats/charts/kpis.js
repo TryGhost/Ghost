d9c85449c9663c6237fa24f69ba28f04215c41eb
Peter Zimon <peter.zimon@gmail.com>
2024-09-05 14:05:00 +0200

    KPI chart refinements

--- ghost/admin/app/components/stats/charts/kpis.js
+++ ghost/admin/app/components/stats/charts/kpis.js
@@ -151,7 +151,26 @@ export default class KpisComponent extends Component {
                         },
                         extraCssText: 'box-shadow: 0px 100px 80px 0px rgba(0, 0, 0, 0.07), 0px 41.778px 33.422px 0px rgba(0, 0, 0, 0.05), 0px 22.336px 17.869px 0px rgba(0, 0, 0, 0.04), 0px 12.522px 10.017px 0px rgba(0, 0, 0, 0.04), 0px 6.65px 5.32px 0px rgba(0, 0, 0, 0.03), 0px 2.767px 2.214px 0px rgba(0, 0, 0, 0.02);',
                         formatter: function (fparams) {
-                            return `<div><div>${moment(fparams[0].value[0]).format('DD MMM, YYYY')}</div><div><span style="display: inline-block; margin-right: 16px; font-weight: 600;">Pageviews</span> ${fparams[0].value[1]}</div></div>`;
+                            let displayValue;
+                            let tooltipTitle;
+                            switch (CATEGORY) {
+                            case 'avg_session_sec':
+                                tooltipTitle = 'Visit duration';
+                                displayValue = fparams[0].value[1] !== null && (fparams[0].value[1] / 60).toFixed(0) + ' min';
+                                break;
+                            case 'bounce_rate':
+                                tooltipTitle = 'Bounce rate';
+                                displayValue = fparams[0].value[1] !== null && fparams[0].value[1].toFixed(2) + '%';
+                                break;
+                            default:
+                                tooltipTitle = 'Unique visitors';
+                                displayValue = fparams[0].value[1] !== null && fparams[0].value[1];
+                                break;
+                            }
+                            if (!displayValue) {
+                                displayValue = 'N/A';
+                            }
+                            return `<div><div>${moment(fparams[0].value[0]).format('D MMM, YYYY')}</div><div><span style="display: inline-block; margin-right: 16px; font-weight: 600;">${tooltipTitle}</span> ${displayValue}</div></div>`;
                         }
                     },
                     series: [
