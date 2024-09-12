3b2448768df01fa262e0d947979749542a2cb8bc
Djordje Vlaisavljevic <dzvlais@gmail.com>
2024-09-12 15:49:55 +0100

    Updated Feed and Replies design
    ref https://linear.app/tryghost/issue/AP-374/add-design-for-replies

--- apps/admin-x-activitypub/src/components/activities/ActivityItem.tsx
+++ apps/admin-x-activitypub/src/components/activities/ActivityItem.tsx
@@ -20,7 +20,7 @@ const ActivityItem: React.FC<ActivityItemProps> = ({children, url = null}) => {
 
     const Item = (
         <div className='flex w-full max-w-[560px] flex-col hover:bg-grey-75'>
-            <div className='flex w-full items-center gap-4 border-b border-grey-100 px-2 py-4'>
+            <div className='flex w-full gap-4 border-b border-grey-100 px-2 py-4'>
                 {childrenArray[0]}
                 {childrenArray[1]}
                 {childrenArray[2]}
