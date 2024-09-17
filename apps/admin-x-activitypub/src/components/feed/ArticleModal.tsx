441fb60d96f2260e9398b7ea5856790ff84f3254
Djordje Vlaisavljevic <dzvlais@gmail.com>
2024-09-17 23:43:35 +0100

    Updated AP reply box design
    ref https://linear.app/tryghost/issue/AP-307/component-for-replying-to-objects-in-the-inboxfeed

--- apps/admin-x-activitypub/src/components/feed/ArticleModal.tsx
+++ apps/admin-x-activitypub/src/components/feed/ArticleModal.tsx
@@ -75,6 +75,10 @@ const FeedItemDivider: React.FC = () => (
 );
 
 const ArticleModal: React.FC<ArticleModalProps> = ({object, actor, comments, allComments}) => {
+    const MODAL_SIZE_SM = 640;
+    const MODAL_SIZE_LG = 2800;
+
+    const [modalSize, setModalSize] = useState<number>(MODAL_SIZE_SM);
     const modal = useModal();
 
     // Navigation stack to navigate between comments - This could probably use a
@@ -136,7 +140,7 @@ const ArticleModal: React.FC<ArticleModalProps> = ({object, actor, comments, all
             </MainHeader>
             <div className='mt-10 w-auto'>
                 {object.type === 'Note' && (
-                    <div className='mx-auto max-w-[580px]'>
+                    <div className='mx-auto max-w-[580px] pb-16'>
                         <FeedItem
                             actor={actor}
                             comments={comments}
@@ -145,6 +149,7 @@ const ArticleModal: React.FC<ArticleModalProps> = ({object, actor, comments, all
                             type='Note'
                         />
                         <APReplyBox placeholder='Reply to @fake@user...'/>
+                        <FeedItemDivider />
 
                         {/* {object.content && <div dangerouslySetInnerHTML={({__html: object.content})} className='ap-note-content text-pretty text-[1.5rem] text-grey-900'></div>} */}
                         {/* {renderAttachment(object)} */}
