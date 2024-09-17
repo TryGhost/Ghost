99d8a5777fdd9326edada4f4b9be152bce772dd5
Sag <guptazy@gmail.com>
2024-09-17 20:55:03 +0200

    Fixed "Unsaved post" modal shown after publishing (#21028)
    ref https://linear.app/tryghost/issue/ENG-661
    ref INC-109
    
    - this PR reverts two commits: 5903dd7 and 426b1d4

--- ghost/oembed-service/lib/OEmbedService.js
+++ ghost/oembed-service/lib/OEmbedService.js
@@ -4,7 +4,6 @@ const logging = require('@tryghost/logging');
 const _ = require('lodash');
 const charset = require('charset');
 const iconv = require('iconv-lite');
-const path = require('path');
 
 // Some sites block non-standard user agents so we need to mimic a typical browser
 const USER_AGENT = 'Mozilla/5.0 (compatible; Ghost/5.0; +https://ghost.org/)';
@@ -53,11 +52,6 @@ const findUrlWithProvider = (url) => {
  */
 
 /**
- * @typedef {Object} IStorage
- * @prop {(feature: string) => Object} getStorage
- */
-
-/**
  * @typedef {(url: string, config: Object) => Promise} IExternalRequest
  */
 
@@ -72,12 +66,10 @@ class OEmbedService {
      *
      * @param {Object} dependencies
      * @param {IConfig} dependencies.config
-     * @param {IStorage} dependencies.storage
      * @param {IExternalRequest} dependencies.externalRequest
      */
-    constructor({config, externalRequest, storage}) {
+    constructor({config, externalRequest}) {
         this.config = config;
-        this.storage = storage;
 
         /** @type {IExternalRequest} */
         this.externalRequest = externalRequest;
@@ -124,55 +116,6 @@ class OEmbedService {
                 context: err.message
             });
         }
-    }
-
-    /**
-     * Fetches the image buffer from a URL using fetch
-     * @param {String} imageUrl - URL of the image to fetch
-     * @returns {Promise<Buffer>} - Promise resolving to the image buffer
-     */
-    async fetchImageBuffer(imageUrl) {
-        const response = await fetch(imageUrl);
-        
-        if (!response.ok) {
-            throw Error(`Failed to fetch image: ${response.statusText}`);
-        }
-
-        const arrayBuffer = await response.arrayBuffer();
-        
-        const buffer = Buffer.from(arrayBuffer);
-        return buffer;
-    }
-
-    /**
-     * Process and store image from a URL
-     * @param {String} imageUrl - URL of the image to process
-     * @param {String} imageType - What is the image used for. Example - icon, thumbnail
-     * @returns {Promise<String>} - URL where the image is stored
-     */
-    async processImageFromUrl(imageUrl, imageType) {
-        // Fetch image buffer from the URL
-        const imageBuffer = await this.fetchImageBuffer(imageUrl);
-        const store = this.storage.getStorage('images');
-
-        // Extract file name from URL
-        const fileName = path.basename(new URL(imageUrl).pathname);
-        let ext = path.extname(fileName);
-        let name;
-
-        if (ext) {
-            name = store.getSanitizedFileName(path.basename(fileName, ext));
-        } else {
-            name = store.getSanitizedFileName(path.basename(fileName));
-        }
-
-        let targetDir = path.join(this.config.getContentPath('images'), imageType);
-        const uniqueFilePath = await store.generateUnique(targetDir, name, ext, 0);
-        const targetPath = path.join(imageType, path.basename(uniqueFilePath));
-
-        const imageStoredUrl = await store.saveRaw(imageBuffer, targetPath);
-
-        return imageStoredUrl;
     }
 
     /**
@@ -266,7 +209,7 @@ class OEmbedService {
      *
      * @returns {Promise<Object>}
      */
-    async fetchBookmarkData(url, html, type) {
+    async fetchBookmarkData(url, html) {
         const gotOpts = {
             headers: {
                 'User-Agent': USER_AGENT
@@ -328,29 +271,12 @@ class OEmbedService {
             });
         }
 
-        if (type === 'bookmark') {
-            await this.processImageFromUrl(metadata.icon, 'icon')
-                .then((processedImageUrl) => {
-                    metadata.icon = processedImageUrl;
-                }).catch((err) => {
-                    metadata.icon = 'https://static.ghost.org/v5.0.0/images/link-icon.svg';
-                    logging.error(err);
-                });
-
-            await this.processImageFromUrl(metadata.thumbnail, 'thumbnail')
-                .then((processedImageUrl) => {
-                    metadata.thumbnail = processedImageUrl;
-                }).catch((err) => {
-                    logging.error(err);
-                });
-        } else {
-            if (metadata.icon) {
-                try {
-                    await this.externalRequest.head(metadata.icon);
-                } catch (err) {
-                    metadata.icon = 'https://static.ghost.org/v5.0.0/images/link-icon.svg';
-                    logging.error(err);
-                }
+        if (metadata.icon) {
+            try {
+                await this.externalRequest.head(metadata.icon);
+            } catch (err) {
+                metadata.icon = 'https://static.ghost.org/v5.0.0/images/link-icon.svg';
+                logging.error(err);
             }
         }
 
@@ -486,7 +412,7 @@ class OEmbedService {
 
             // fetch only bookmark when explicitly requested
             if (type === 'bookmark') {
-                return this.fetchBookmarkData(url, body, type);
+                return this.fetchBookmarkData(url, body);
             }
 
             // mentions need to return bookmark data (metadata) and body (html) for link verification
@@ -509,7 +435,7 @@ class OEmbedService {
                     };
                     return {...bookmark, body};
                 }
-                const bookmark = await this.fetchBookmarkData(url, body, type);
+                const bookmark = await this.fetchBookmarkData(url, body);
                 return {...bookmark, body, contentType};
             }
 
@@ -528,7 +454,7 @@ class OEmbedService {
 
             // fallback to bookmark when we can't get oembed
             if (!data && !type) {
-                data = await this.fetchBookmarkData(url, body, type);
+                data = await this.fetchBookmarkData(url, body);
             }
 
             // couldn't get anything, throw a validation error
