041cdb366cbb591599bbbed0e920dfdc2b185599
Hannah Wolfe <github.erisds@gmail.com>
2024-09-19 14:56:46 +0100

    Added helper scripts for recreating pipes/mvs
    - This is not the way to do this, but it works whilst we don't mind things being broken temporarily

---
+++ ghost/tinybird/scripts/remove_endpoints.sh
@@ -0,0 +1,7 @@
+tb pipe rm pipes/kpis.pipe  --yes
+tb pipe rm pipes/top_browsers.pipe  --yes
+tb pipe rm pipes/top_devices.pipe  --yes
+tb pipe rm pipes/top_locations.pipe  --yes
+tb pipe rm pipes/top_pages.pipe  --yes
+tb pipe rm pipes/top_sources.pipe  --yes
+tb pipe rm pipes/trend.pipe  --yes
---
+++ ghost/tinybird/scripts/remove_mvs_pipes.sh
@@ -0,0 +1,7 @@
+tb datasource rm analytics_pages_mv  --yes
+tb datasource rm analytics_sessions_mv  --yes
+tb datasource rm analytics_sources_mv  --yes
+tb pipe rm analytics_pages  --yes
+tb pipe rm analytics_sessions  --yes
+tb pipe rm analytics_sources  --yes
+tb pipe rm analytics_hits  --yes
