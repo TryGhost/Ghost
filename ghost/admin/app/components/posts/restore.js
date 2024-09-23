d99a50ca9d2e703e6f48896ee1dba9afd32441a5
Steve Larson <9larsons@gmail.com>
2024-09-23 14:29:24 -0500

    added some basic styles and restore button

--- ghost/admin/app/components/posts/restore.js
+++ ghost/admin/app/components/posts/restore.js
@@ -1,12 +1,21 @@
 import Component from '@glimmer/component';
-import {tracked} from '@glimmer/tracking';
+import {inject as service} from '@ember/service';
+import {task} from 'ember-concurrency';
 
 export default class PostsRestoreComponent extends Component {
-    @tracked localRevisions;
+    @service localRevisions;
+    @service notifications;
 
-    constructor() {
-        super(...arguments);
-        this.localRevisions = this.args.model || [];
-        console.log('Revisions in component:', this.localRevisions);
+    @task
+    *restorePostTask(revision) {
+        try {
+            yield this.localRevisions.restore(revision.key);
+            this.notifications.showNotification('Post restored successfully', {type: 'success'});
+            return true;
+        } catch (error) {
+            this.notifications.showNotification('Failed to restore post', {type: 'error'});
+            console.error('Failed to restore post:', error);
+            return false;
+        }
     }
 }
