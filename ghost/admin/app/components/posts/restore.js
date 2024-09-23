abea7d9139ddd6b99c2a067d7c19313fa2c08faf
Steve Larson <9larsons@gmail.com>
2024-09-23 18:54:14 -0500

    Updated styles

--- ghost/admin/app/components/posts/restore.js
+++ ghost/admin/app/components/posts/restore.js
@@ -1,10 +1,19 @@
 import Component from '@glimmer/component';
 import {inject as service} from '@ember/service';
 import {task} from 'ember-concurrency';
+import {tracked} from '@glimmer/tracking';
+import {action} from '@ember/object';
 
 export default class PostsRestoreComponent extends Component {
     @service localRevisions;
     @service notifications;
+
+    @tracked openRevision = null;
+
+    @action
+    toggleRevision(key) {
+        this.openRevision = this.openRevision === key ? null : key;
+    }
 
     @task
     *restorePostTask(revision) {
