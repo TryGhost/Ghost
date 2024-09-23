b12fc0a928a000d93ac9bc37dc9d28c892dc28b4
Steve Larson <9larsons@gmail.com>
2024-09-23 13:39:43 -0500

    Updated model for display

--- ghost/admin/app/components/posts/restore.js
+++ ghost/admin/app/components/posts/restore.js
@@ -1,23 +1,12 @@
 import Component from '@glimmer/component';
-// import moment from 'moment-timezone';
-// import {action} from '@ember/object';
-// import {didCancel, task, timeout} from 'ember-concurrency';
-// import {formatNumber} from 'ghost-admin/helpers/format-number';
-// import {ghPluralize} from 'ghost-admin/helpers/gh-pluralize';
-// import {inject as service} from '@ember/service';
-// import {tracked} from '@glimmer/tracking';
-export default class Restore extends Component {
-    // @service ajax;
-    // @service ghostPaths;
-    // @service settings;
-    // @service membersUtils;
-    // @service utils;
-    // @service feature;
-    // @service store;
+import {tracked} from '@glimmer/tracking';
 
-    // @tracked emailBatches = null;
-    // @tracked recipientFailures = null;
-    // @tracked loading = true;
-    // @tracked analyticsStatus = null;
-    // @tracked latestEmail = null;
+export default class PostsRestoreComponent extends Component {
+    @tracked localRevisions;
+
+    constructor() {
+        super(...arguments);
+        this.localRevisions = this.args.model || [];
+        console.log('Revisions in component:', this.localRevisions);
+    }
 }
