fcbe707cbefc94a05845b230302e4e36f6452e64
Sanne de Vries <sannedv@protonmail.com>
2024-09-24 11:01:50 +0200

    Updated comment report modal
    REF PLG-226
    - Changed title copy from "You want to report this comment?" to "Report this comment?"
    - Changed button copy from "Report this comment" to "Report"
    - Updated styles to be more responsive

--- apps/comments-ui/src/components/popups/ReportPopup.tsx
+++ apps/comments-ui/src/components/popups/ReportPopup.tsx
@@ -14,7 +14,7 @@ const ReportPopup = ({comment}: {comment: Comment}) => {
         buttonColor = 'bg-green-600';
     }
 
-    let buttonText = t('Report this comment');
+    let buttonText = t('Report');
     const defaultButtonText = buttonText;
 
     if (progress === 'sending') {
@@ -59,29 +59,30 @@ const ReportPopup = ({comment}: {comment: Comment}) => {
 
     return (
         <div className="shadow-modal relative h-screen w-screen rounded-none bg-white p-[28px] text-center sm:h-auto sm:w-[500px] sm:rounded-xl sm:p-8 sm:text-left" onMouseDown={stopPropagation}>
-            <h1 className="mb-1 font-sans text-[24px] font-bold tracking-tight text-black">
-                <span className="sm:hidden">{t('Report this comment?')}</span>
-                <span className="hidden sm:inline">{t('You want to report this comment?')}</span>
-            </h1>
-            <p className="px-4 font-sans text-base leading-9 text-neutral-500 sm:pl-0 sm:pr-4">{t('Your request will be sent to the owner of this site.')}</p>
-            <div className="mt-10 flex flex-col items-center justify-start gap-4 sm:flex-row">
-                <button
-                    className={`flex h-[44px] items-center justify-center rounded-md px-4 font-sans text-[15px] font-semibold text-white transition duration-200 ease-linear ${buttonColor} opacity-100 hover:opacity-90`}
-                    style={{backgroundColor: buttonColor ?? '#000000'}}
-                    type="button"
-                    onClick={submit}
-                >
-                    <span className="invisible whitespace-nowrap">
-                        {/* Take the largest width of all possibilities as the width for the button */}
-                        {defaultButtonText}<br />
-                        <span className='flex h-[44px] items-center justify-center whitespace-nowrap'>{buttonIcon1}{t('Sending')}</span><br />
-                        <span className='flex h-[44px] items-center justify-center whitespace-nowrap'>{buttonIcon2}{t('Sent')}</span>
-                    </span>
-                    <span className='absolute flex h-[44px] items-center justify-center whitespace-nowrap'>{buttonIcon}{buttonText}</span>
-                </button>
-                <button className="font-sans text-sm font-medium text-neutral-500 dark:text-neutral-400" type="button" onClick={close}>{t('Cancel')}</button>
+            <div className="flex h-full flex-col justify-center pt-10 sm:justify-normal sm:pt-0">
+                <h1 className="mb-1.5 font-sans text-[2.2rem] font-bold tracking-tight text-black">
+                    <span>{t('Report this comment?')}</span>
+                </h1>
+                <p className="text-md px-4 font-sans leading-9 text-black sm:pl-0 sm:pr-4">{t('Your request will be sent to the owner of this site.')}</p>
+                <div className="mt-auto flex flex-col items-center justify-start gap-4 sm:mt-8 sm:flex-row">
+                    <button
+                        className={`text-md flex h-[44px] w-full items-center justify-center rounded-md px-4 font-sans font-medium text-white transition duration-200 ease-linear sm:w-fit ${buttonColor} opacity-100 hover:opacity-90`}
+                        style={{backgroundColor: buttonColor ?? '#000000'}}
+                        type="button"
+                        onClick={submit}
+                    >
+                        <span className="invisible whitespace-nowrap">
+                            {/* Take the largest width of all possibilities as the width for the button */}
+                            {defaultButtonText}<br />
+                            <span className='flex h-[44px] items-center justify-center whitespace-nowrap'>{buttonIcon1}{t('Sending')}</span><br />
+                            <span className='flex h-[44px] items-center justify-center whitespace-nowrap'>{buttonIcon2}{t('Sent')}</span>
+                        </span>
+                        <span className='absolute flex h-[44px] items-center justify-center whitespace-nowrap'>{buttonIcon}{buttonText}</span>
+                    </button>
+                    <button className="text-md h-[44px] w-full px-2 font-sans font-medium text-neutral-500 sm:w-fit dark:text-neutral-400" type="button" onClick={close}>{t('Cancel')}</button>
+                </div>
+                <CloseButton close={close} />
             </div>
-            <CloseButton close={close} />
         </div>
     );
 };
