86082d84a03c1759485c4a104b893cde2517282a
Hannah Wolfe <github.erisds@gmail.com>
2024-09-24 12:23:27 +0100

    Improved the implementation of the stats page (#21088)
    ref
    https://linear.app/tryghost/issue/ANAL-53/10-stats-page-engineering-stuff
    
    - All of these changes are intended to improve developer experience
    going forward, to make it easier to implement further changes to the
    stats page.
    - Moved the modal into the stats components, as it has a lot of shared
    code, and it makes it easier to update them all
    - Removed various comment blocks that were outdated or didn't really add
    value
      - Fixed all imports to use the same pattern starting `ghost-admin/`
      - Ensured all the components had the correct name
    - Dried up the generation of params for Tinybird charts into a utility
    function as we'll need to add several more in the near future
    - Tried to use a consistent pattern everywhere for the order of
    operations
    - Dried up the implementation of technical.js which handles the
    device/browser charts

--- ghost/admin/app/components/stats/charts/kpis.js
+++ ghost/admin/app/components/stats/charts/kpis.js
@@ -5,40 +5,21 @@ import React from 'react';
 import moment from 'moment-timezone';
 import {AreaChart, useQuery} from '@tinybirdco/charts';
 import {formatNumber} from '../../../helpers/format-number';
+import {getDateRange, getStatsParams, statsStaticColors} from '../../../utils/stats';
 import {hexToRgba} from 'ghost-admin/utils/stats';
 import {inject} from 'ghost-admin/decorators/inject';
-import {statsStaticColors} from '../../../utils/stats';
 
 export default class KpisComponent extends Component {
     @inject config;
 
     ReactComponent = (props) => {
-        let chartRange = props.chartRange;
-        let audience = props.audience;
+        const {chartRange, audience, selected} = props;
 
-        const endDate = moment().endOf('day');
-        const startDate = moment().subtract(chartRange - 1, 'days').startOf('day');
-
-        /**
-         * @typedef {Object} Params
-         * @property {string} cid
-         * @property {string} [date_from]
-         * @property {string} [date_to]
-         * @property {string} [member_status]
-         * @property {number} [limit]
-         * @property {number} [skip]
-         */
-
-        const params = {
-            site_uuid: this.config.stats.id,
-            date_from: startDate.format('YYYY-MM-DD'),
-            date_to: endDate.format('YYYY-MM-DD'),
-            member_status: audience.length === 0 ? null : audience.join(',')
-        };
-
-        const LINE_COLOR = statsStaticColors[0];
-        const INDEX = 'date';
-        const CATEGORY = props.selected === 'unique_visits' ? 'visits' : props.selected;
+        const params = getStatsParams(
+            this.config,
+            chartRange,
+            audience
+        );
 
         const {data, meta, error, loading} = useQuery({
             endpoint: `${this.config.stats.endpoint}/v0/pipes/kpis.json`,
@@ -46,45 +27,16 @@ export default class KpisComponent extends Component {
             params
         });
 
-        // Create an array with every second date value
-        const dateLabels = [
-            startDate.format('YYYY-MM-DD'),
-            endDate.format('YYYY-MM-DD')
-        ];
-        // let currentDate = startDate.clone();
-        // let skipDays;
-        // switch (chartRange) {
-        // case 1:
-        //     skipDays = 0; // Show all hours for 1 day
-        //     break;
-        // case 7:
-        //     skipDays = 0; // Skip every other day for 7 days
-        //     break;
-        // case (30 + 1):
-        //     skipDays = 2; // Skip every 3rd day for 30 and 90 days
-        //     break;
-        // case (90 + 1):
-        //     skipDays = 5; // Skip every 3rd day for 30 and 90 days
-        //     break;
-        // case (365 + 1):
-        // case (12 * (30 + 1)):
-        //     skipDays = 30; // Skip every 7th day for 1 year
-        //     break;
-        // case 1000:
-        //     skipDays = 29; // Skip every 30th day for all time
-        //     break;
-        // default:
-        //     skipDays = 1; // Default to skipping every other day
-        // }
+        const LINE_COLOR = statsStaticColors[0];
+        const INDEX = 'date';
+        const CATEGORY = selected === 'unique_visits' ? 'visits' : selected;
 
-        // let dayCounter = 0;
-        // while (currentDate.isSameOrBefore(endDate)) {
-        //     if (dayCounter % (skipDays + 1) === 0) {
-        //         dateLabels.push(currentDate.format('YYYY-MM-DD'));
-        //     }
-        //     currentDate.add(1, 'days');
-        //     dayCounter = dayCounter + 1;
-        // }
+        const dateLabels = [
+            params.date_from,
+            params.date_to
+        ];
+
+        const {endDate, startDate} = getDateRange(chartRange);
 
         return (
             <AreaChart
