209155579d384a3cb1eda0f43887a26e78ee288d
Steve Larson <9larsons@gmail.com>
2024-09-23 19:13:28 -0500

    Refactored to be outside of posts route
    We could tap this into the posts model and access this, but it felt like we were screwing a bit with ember models. As this isn't tied directly into the posts model and only creates a post indirectly, it feels better to put this outside.

--- ghost/admin/app/controllers/restore-posts.js
+++ ghost/admin/app/controllers/restore-posts.js
@@ -1,28 +1,21 @@
-import Component from '@glimmer/component';
+import Controller from '@ember/controller';
 import {inject as service} from '@ember/service';
 import {task} from 'ember-concurrency';
-import {tracked} from '@glimmer/tracking';
-import {action} from '@ember/object';
 
-export default class PostsRestoreComponent extends Component {
+export default class RestorePostsController extends Controller {
     @service localRevisions;
     @service notifications;
 
-    @tracked openRevision = null;
-
-    @action
-    toggleRevision(key) {
-        this.openRevision = this.openRevision === key ? null : key;
-    }
-
     @task
     *restorePostTask(revision) {
+        console.log('restorePostTask', revision);
         try {
             yield this.localRevisions.restore(revision.key);
             this.notifications.showNotification('Post restored successfully', {type: 'success'});
             return true;
         } catch (error) {
             this.notifications.showNotification('Failed to restore post', {type: 'error'});
+            // eslint-disable-next-line no-console
             console.error('Failed to restore post:', error);
             return false;
         }
--- ghost/admin/app/router.js
+++ ghost/admin/app/router.js
@@ -33,7 +33,8 @@ Router.map(function () {
     this.route('posts.analytics', {path: '/posts/analytics/:post_id'});
     this.route('posts.mentions', {path: '/posts/analytics/:post_id/mentions'});
     this.route('posts.debug', {path: '/posts/analytics/:post_id/debug'});
-    this.route('posts.restore', {path: '/posts/restore'});
+    
+    this.route('restore-posts', {path: '/restore'});
 
     this.route('pages');
 
