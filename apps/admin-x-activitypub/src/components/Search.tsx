fed2b19ff16439ce2041bc976adb6fb96c7c39d5
Djordje Vlaisavljevic <dzvlais@gmail.com>
2024-09-25 16:44:18 +0100

    Updated design of Search page
    ref https://linear.app/tryghost/issue/AP-429/add-ui-for-searching-and-viewing-mastodon-profiles

--- apps/admin-x-activitypub/src/components/Search.tsx
+++ apps/admin-x-activitypub/src/components/Search.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useRef, useState} from 'react';
 
 import {ActorProperties} from '@tryghost/admin-x-framework/api/activitypub';
-import {Icon} from '@tryghost/admin-x-design-system';
+import {Button, Icon, LoadingIndicator, NoValueLabel, TextField} from '@tryghost/admin-x-design-system';
 import {useDebounce} from 'use-debounce';
 
 import APAvatar from './global/APAvatar';
@@ -61,15 +61,14 @@ const SearchResult: React.FC<SearchResultProps> = ({result}) => {
                 <div className='text-grey-600'>
                     <span className='font-bold text-black'>{result.actor.name} </span>{result.handle}
                 </div>
-                <div className='text-sm'>{result.followerCount} followers</div>
+                <div className='text-sm'>{new Intl.NumberFormat().format(result.followerCount)} followers</div>
             </div>
-            {result.isFollowing === false && (
-                <FollowButton
-                    className='ml-auto'
-                    toFollow={result.handle}
-                    type='link'
-                />
-            )}
+            <FollowButton
+                className='ml-auto'
+                isFollowing={result.isFollowing}
+                toFollow={result.handle}
+                type='link'
+            />
         </ActivityItem>
     );
 };
@@ -83,7 +82,7 @@ const Search: React.FC<SearchProps> = ({}) => {
     const results = data?.profiles || [];
 
     const showNoResults = results.length === 0 && query !== '' && !isFetching;
-    const showSuggestedResults = query === '' || (results.length === 0 && query !== '');
+    const showSuggestedResults = (query === '') || (results.length === 0 && query !== '' && !isFetching);
 
     // Focus the query input on first render
     useEffect(() => {
@@ -96,29 +95,40 @@ const Search: React.FC<SearchProps> = ({}) => {
         <>
             <MainNavigation title='Search' />
             <div className='z-0 flex w-full flex-col items-center pt-8'>
-                <div className='mb-6 flex w-full max-w-[560px] items-center gap-2 rounded-full bg-grey-100 px-3 py-2 text-grey-500'>
-                    <Icon name='magnifying-glass' size={18} />
-                    <input
-                        ref={queryInputRef}
-                        className='w-full bg-transparent'
-                        placeholder='Search the Fediverse'
+                <div className='relative flex w-full max-w-[560px] items-center '>
+                    <Icon className='absolute left-3 top-3 z-10' colorClass='text-grey-500' name='magnifying-glass' size='sm' />
+                    <TextField
+                        className='mb-6 mr-12 flex h-10 w-full items-center rounded-lg border border-transparent bg-grey-100 px-[33px] py-1.5 transition-colors focus:border-green focus:bg-white focus:outline-2 dark:border-transparent dark:bg-grey-925 dark:text-white dark:placeholder:text-grey-800 dark:focus:border-green dark:focus:bg-grey-950 tablet:mr-0'
+                        containerClassName='w-100'
+                        inputRef={queryInputRef}
+                        placeholder='Search accounts'
+                        title="Search"
                         type='text'
                         value={query}
+                        clearBg
+                        hideTitle
+                        unstyled
                         onChange={e => setQuery(e.target.value)}
                     />
+                    {query && <Button className='absolute top-3 p-1 sm:right-14 tablet:right-3' icon='close' iconColorClass='text-grey-700 !w-[10px] !h-[10px]' size='sm' unstyled onClick={() => {
+                        setQuery('');
+                        queryInputRef.current?.focus();
+                    }} />}
                 </div>
                 {isFetching && (
-                    <div className='mb-3 text-grey-500'>Loading...</div>
+                    <LoadingIndicator size='lg'/>
                 )}
                 {showNoResults && (
-                    <div className='mb-3 text-grey-500'>No results</div>
+                    <NoValueLabel icon='user'>
+                        No users matching this username
+                    </NoValueLabel>
                 )}
                 {results.map(result => (
                     <SearchResult key={(result as SearchResultItem).actor.id} result={result as SearchResultItem} />
                 ))}
                 {showSuggestedResults && (
                     <>
-                        <div className='mb-3 flex w-full max-w-[560px] font-bold'>Suggested accounts</div>
+                        <span className='mb-1 flex w-full max-w-[560px] font-semibold'>Suggested accounts</span>
                         {SUGGESTED_RESULTS.map(profile => (
                             <SearchResult key={profile.actor.id} result={profile} />
                         ))}
