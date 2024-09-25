2d50ba3800bb284eaf89c2391c0a53108c470ccd
Djordje Vlaisavljevic <dzvlais@gmail.com>
2024-09-25 18:59:51 +0100

    Improved profile design when there's no cover image
    ref https://linear.app/tryghost/issue/AP-429/add-ui-for-searching-and-viewing-mastodon-profiles

--- apps/admin-x-activitypub/src/components/search/ProfileSearchResultModal.tsx
+++ apps/admin-x-activitypub/src/components/search/ProfileSearchResultModal.tsx
@@ -37,45 +37,39 @@ const ProfileSearchResultModal: React.FC<ProfileSearchResultModalProps> = ({prof
                     </div>
                 </div>
             </div>
-            <div className='mt-4 w-auto'>
-                <div className='mx-auto max-w-[580px] pb-16'>
-                    <div className='z-0 flex w-full flex-col items-center'>
-                        <div className='mx-auto w-full'>
-                            <div className='h-[200px] w-full overflow-hidden rounded-lg bg-gradient-to-tr from-grey-200 to-grey-100'>
-                                {profile.actor.image && (
-                                    <img
-                                        alt={profile.actor.name}
-                                        className='h-full w-full object-cover'
-                                        src={profile.actor.image.url}
-                                    />
-                                )}
-                            </div>
-                            <div className='-mt-12 px-4'>
-                                <div className='flex items-end justify-between'>
-                                    <div className='rounded-xl outline outline-4 outline-white'>
-                                        <APAvatar
-                                            author={profile.actor}
-                                            size='lg'
-                                        />
-                                    </div>
-                                    {/* {profile.isFollowing === false && ( */}
-                                    <FollowButton isFollowing={profile.isFollowing} toFollow={profile.handle} />
-                                    {/* )} */}
-                                </div>
-                                <Heading className='mt-4' level={3}>{profile.actor.name}</Heading>
-                                <span className='mt-2 inline-block text-[1.5rem] text-grey-800'>{profile.handle}</span>
-                                <div
-                                    dangerouslySetInnerHTML={{__html: profile.actor.summary}}
-                                    className='ap-profile-content mt-3 text-[1.5rem] [&>p]:mb-3'
+            <div className='z-0 mx-auto mt-4 flex w-full max-w-[580px] flex-col items-center pb-16'>
+                <div className='mx-auto w-full'>
+                    {profile.actor.image && (<div className='h-[200px] w-full overflow-hidden rounded-lg bg-gradient-to-tr from-grey-200 to-grey-100'>
+                        <img
+                            alt={profile.actor.name}
+                            className='h-full w-full object-cover'
+                            src={profile.actor.image.url}
+                        />
+                    </div>)}
+                    <div className={`${profile.actor.image && '-mt-12'} px-4`}>
+                        <div className='flex items-end justify-between'>
+                            <div className='rounded-xl outline outline-4 outline-white'>
+                                <APAvatar
+                                    author={profile.actor}
+                                    size='lg'
                                 />
-                                {attachments.map(attachment => (
-                                    <span className='mt-3 line-clamp-1 flex flex-col text-[1.5rem]'>
-                                        <span className={`mr-1 text-xs font-semibold after:content-[":"]`}>{attachment.name}</span>
-                                        <span dangerouslySetInnerHTML={{__html: attachment.value}} className='ap-profile-content truncate'/>
-                                    </span>
-                                ))}
                             </div>
+                            {/* {profile.isFollowing === false && ( */}
+                            <FollowButton isFollowing={profile.isFollowing} toFollow={profile.handle} />
+                            {/* )} */}
                         </div>
+                        <Heading className='mt-4' level={3}>{profile.actor.name}</Heading>
+                        <span className='mt-2 inline-block text-[1.5rem] text-grey-800'>{profile.handle}</span>
+                        <div
+                            dangerouslySetInnerHTML={{__html: profile.actor.summary}}
+                            className='ap-profile-content mt-3 text-[1.5rem] [&>p]:mb-3'
+                        />
+                        {attachments.map(attachment => (
+                            <span className='mt-3 line-clamp-1 flex flex-col text-[1.5rem]'>
+                                <span className={`mr-1 text-xs font-semibold after:content-[":"]`}>{attachment.name}</span>
+                                <span dangerouslySetInnerHTML={{__html: attachment.value}} className='ap-profile-content truncate'/>
+                            </span>
+                        ))}
                     </div>
                 </div>
             </div>
