6b752cb6d04815abd35a9f5eaadbca29c3c7ed25
Cathy Sarisky <catsarisky@gmail.com>
2024-09-27 20:20:16 -0400

    very small contentkey helper

---
+++ ghost/core/core/frontend/helpers/contentkey.js
@@ -0,0 +1,23 @@
+// contentkey provides a new helper that returns the content API key.  
+
+const {SafeString} = require('../services/handlebars');
+const logging = require('@tryghost/logging');
+const {getFrontendKey} = require('../services/proxy');
+
+module.exports = async function contentkey() { // eslint-disable-line camelcase
+
+try {
+    const frontendKey = await getFrontendKey();
+
+    if (!frontendKey) {
+        logging.warn('contentkey: No content key found');
+        return '';
+    }
+    return new SafeString(frontendKey);
+
+} catch (error) {
+    logging.error(error);
+    return '';
+}
+}
+module.exports.async = true;
