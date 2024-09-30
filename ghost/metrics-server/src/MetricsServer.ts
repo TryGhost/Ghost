7d5e8e29d2568d48df6d776d8ba6a922ff3aa572
Chris Raible <chris@ghost.org>
2024-09-30 15:33:02 -0700

    Added graceful shutdown to MetricsServer

--- ghost/metrics-server/src/MetricsServer.ts
+++ ghost/metrics-server/src/MetricsServer.ts
@@ -1,6 +1,7 @@
 import debugModule from '@tryghost/debug';
 import express from 'express';
 import http from 'http';
+import stoppable from 'stoppable';
 
 const debug = debugModule('metrics-server');
 
@@ -21,7 +22,7 @@ const defaultHandler: express.Handler = (req, res) => {
 export class MetricsServer {
     private serverConfig: ServerConfig;
     private app: express.Application | null;
-    private httpServer: http.Server | null;
+    private httpServer: stoppable.StoppableServer | null;
     private handler: express.Handler;
 
     constructor({serverConfig, handler}: {serverConfig: ServerConfig, handler: express.Handler} = {serverConfig: defaultServerConfig, handler: defaultHandler}) {
@@ -37,9 +38,13 @@ export class MetricsServer {
         debug('Starting metrics server');
         this.app = express();
         this.app.get('/metrics', this.handler);
-        this.httpServer = this.app.listen(this.serverConfig.port, this.serverConfig.host, () => {
+        const httpServer = this.app.listen(this.serverConfig.port, this.serverConfig.host, () => {
             debug(`Metrics server listening at ${this.serverConfig.host}:${this.serverConfig.port}`);
         });
+        this.httpServer = stoppable(httpServer, 0);
+
+        process.on('SIGINT', () => this.shutdown());
+        process.on('SIGTERM', () => this.shutdown());
         return {app: this.app, httpServer: this.httpServer};
     }
 
@@ -47,7 +52,7 @@ export class MetricsServer {
         // stop the server
         debug('Stopping metrics server');
         if (this.httpServer) {
-            this.httpServer.close();
+            this.httpServer.stop();
         }
     }
 
