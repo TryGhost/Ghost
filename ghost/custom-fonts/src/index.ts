5e36dfe4ab38301bc65a62056d071ebbdc7c274d
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2024-10-08 15:20:39 +0100

    Updated the list of fonts in the custom fonts module

--- ghost/custom-fonts/src/index.ts
+++ ghost/custom-fonts/src/index.ts
@@ -1,17 +1,22 @@
-export type Font = 'EB Garamond' | 'Inter' | 'JetBrains Mono' | 'Libre Baskerville' | 'Lora' | 'Mulish' | 'Open Sans' | 'Poppins' | 'PT Serif' | 'Raleway';
+export type Font = 'Space Grotesk' | 'Bebas Neue' | 'Playfair Display' | 'Chakra Petch' | 'Noto Sans' | 'Poppins' | 'Fira Sans' | 'Inter' | 'Noto Serif' | 'Lora' | 'IBM Plex Serif' | 'EB Garamond' | 'Space Mono' | 'Fira Mono' | 'JetBrains Mono';
 
 export function generateCustomFontCss(font: Font) {
     const importStrings = {
-        'EB Garamond': '@import url(https://fonts.bunny.net/css?family=eb-garamond:400);',
-        Inter: '@import url(https://fonts.bunny.net/css?family=inter:400);',
-        'JetBrains Mono': '@import url(https://fonts.bunny.net/css?family=jetbrains-mono:400);',
-        'Libre Baskerville': '@import url(https://fonts.bunny.net/css?family=libre-baskerville:400);',
-        Lora: '@import url(https://fonts.bunny.net/css?family=lora:400);',
-        Mulish: '@import url(https://fonts.bunny.net/css?family=mulish:400);',
-        'Open Sans': '@import url(https://fonts.bunny.net/css?family=open-sans:400);',
-        Poppins: '@import url(https://fonts.bunny.net/css?family=poppins:400);',
-        'PT Serif': '@import url(https://fonts.bunny.net/css?family=pt-serif:400);',
-        Raleway: '@import url(https://fonts.bunny.net/css?family=raleway:400);'
+        'Space Grotesk': '@import url(https://fonts.bunny.net/css?family=space-grotesk:700)',
+        'Bebas Neue': '@import url(https://fonts.bunny.net/css?family=bebas-neue:400)',
+        'Playfair Display': '@import url(https://fonts.bunny.net/css?family=playfair-display:400)',
+        'Chakra Petch': '@import url(https://fonts.bunny.net/css?family=chakra-petch:400)',
+        'Noto Sans': '@import url(https://fonts.bunny.net/css?family=noto-sans:400,700)',
+        Poppins: '@import url(https://fonts.bunny.net/css?family=poppins:400,700)',
+        'Fira Sans': '@import url(https://fonts.bunny.net/css?family=fira-sans:400,700)',
+        Inter: '@import url(https://fonts.bunny.net/css?family=inter:400,700)',
+        'Noto Serif': '@import url(https://fonts.bunny.net/css?family=noto-serif:400,700)',
+        Lora: '@import url(https://fonts.bunny.net/css?family=lora:400,700)',
+        'IBM Plex Serif': '@import url(https://fonts.bunny.net/css?family=ibm-plex-serif:400,700)',
+        'EB Garamond': '@import url(https://fonts.bunny.net/css?family=eb-garamond:400,700)',
+        'Space Mono': '@import url(https://fonts.bunny.net/css?family=space-mono:400,700)',
+        'Fira Mono': '@import url(https://fonts.bunny.net/css?family=fira-mono:400,700)',
+        'JetBrains Mono': '@import url(https://fonts.bunny.net/css?family=jetbrains-mono:400,700)'
     };
 
     return `
