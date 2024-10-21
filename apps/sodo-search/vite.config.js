9f180476193c29e90067c2ea76b0f97bddf8bd4a
Chris Raible <chris@ghost.org>
2024-10-20 19:19:35 -0700

    Fixed app flags and added stripe cli

--- apps/sodo-search/vite.config.js
+++ apps/sodo-search/vite.config.js
@@ -18,6 +18,7 @@ export default defineConfig((config) => {
             'process.env.NODE_ENV': JSON.stringify(config.mode)
         },
         preview: {
+            host: '0.0.0.0',
             port: 4178
         },
         plugins: [
