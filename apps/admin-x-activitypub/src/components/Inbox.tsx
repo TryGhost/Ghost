57e09d60c8f49ffa0176e1c41d2e1ea2d4be5a1e
Djordje Vlaisavljevic <dzvlais@gmail.com>
2024-10-21 12:27:54 +0100

    Added suggestions sidebar
    ref https://linear.app/tryghost/issue/AP-464/feed-view-refinements

--- apps/admin-x-activitypub/src/components/Inbox.tsx
+++ apps/admin-x-activitypub/src/components/Inbox.tsx
@@ -1,13 +1,16 @@
+import APAvatar from './global/APAvatar';
+import ActivityItem, {type Activity} from './activities/ActivityItem';
 import ActivityPubWelcomeImage from '../assets/images/ap-welcome.png';
 import ArticleModal from './feed/ArticleModal';
 import FeedItem from './feed/FeedItem';
 import MainNavigation from './navigation/MainNavigation';
 import NiceModal from '@ebay/nice-modal-react';
 import React, {useEffect, useRef, useState} from 'react';
-import {type Activity} from './activities/ActivityItem';
+import getUsername from '../utils/get-username';
 import {ActorProperties, ObjectProperties} from '@tryghost/admin-x-framework/api/activitypub';
 import {Button, Heading, LoadingIndicator} from '@tryghost/admin-x-design-system';
-import {useActivitiesForUser} from '../hooks/useActivityPubQueries';
+import {useActivitiesForUser, useSuggestedProfiles} from '../hooks/useActivityPubQueries';
+import {useRouting} from '@tryghost/admin-x-framework/routing';
 
 interface InboxProps {}
 
@@ -29,6 +32,11 @@ const Inbox: React.FC<InboxProps> = ({}) => {
             type: ['Create:Article:notReply', 'Create:Note:notReply', 'Announce:Note']
         }
     });
+
+    const {updateRoute} = useRouting();
+
+    const {suggestedProfilesQuery} = useSuggestedProfiles('index', ['@quillmatiq@mastodon.social', '@miaq@flipboard.social', '@mallory@techpolicy.social']);
+    const {data: suggested = [], isLoading: isLoadingSuggested} = suggestedProfilesQuery;
 
     const activities = (data?.pages.flatMap(page => page.data) ?? []).filter((activity) => {
         return !activity.object.inReplyTo;
@@ -99,49 +107,88 @@ const Inbox: React.FC<InboxProps> = ({}) => {
         <>
             <MainNavigation page='home' title="Home" onLayoutChange={handleLayoutChange} />
             <div className='z-0 my-5 flex w-full flex-col'>
-                <div className='w-full'>
+                <div className='w-full px-8'>
                     {isLoading ? (
                         <div className='flex flex-col items-center justify-center space-y-4 text-center'>
                             <LoadingIndicator size='lg' />
                         </div>
                     ) : activities.length > 0 ? (
                         <>
-                            <ul className={`mx-auto flex ${layout === 'inbox' ? 'max-w-[740px]' : 'max-w-[540px]'} flex-col`}>
-                                {activities.map((activity, index) => (
-                                    <li
-                                        key={activity.id}
-                                        data-test-view-article
-                                        onClick={() => handleViewContent(
-                                            activity.object,
-                                            getContentAuthor(activity),
-                                            activity.object.replies
+                            <div className={`mx-auto flex items-start ${layout === 'inbox' ? 'max-w-6xl gap-14' : 'gap-8'}`}>
+                                <div className='flex w-full items-start'>
+                                    <ul className={`mx-auto flex ${layout === 'inbox' ? 'max-w-full' : 'max-w-[500px]'} flex-col`}>
+                                        {activities.map((activity, index) => (
+                                            <li
+                                                key={activity.id}
+                                                data-test-view-article
+                                                onClick={() => handleViewContent(
+                                                    activity.object,
+                                                    getContentAuthor(activity),
+                                                    activity.object.replies
+                                                )}
+                                            >
+                                                <FeedItem
+                                                    actor={activity.actor}
+                                                    comments={activity.object.replies}
+                                                    layout={layout}
+                                                    object={activity.object}
+                                                    type={activity.type}
+                                                    onCommentClick={() => handleViewContent(
+                                                        activity.object,
+                                                        getContentAuthor(activity),
+                                                        activity.object.replies,
+                                                        true
+                                                    )}
+                                                />
+                                                {index < activities.length - 1 && (
+                                                    <div className="h-px w-full bg-grey-200"></div>
+                                                )}
+                                            </li>
+                                        ))}
+                                        <div ref={loadMoreRef} className='h-1'></div>
+                                        {isFetchingNextPage && (
+                                            <div className='flex flex-col items-center justify-center space-y-4 text-center'>
+                                                <LoadingIndicator size='md' />
+                                            </div>
                                         )}
-                                    >
-                                        <FeedItem
-                                            actor={activity.actor}
-                                            comments={activity.object.replies}
-                                            layout={layout}
-                                            object={activity.object}
-                                            type={activity.type}
-                                            onCommentClick={() => handleViewContent(
-                                                activity.object,
-                                                getContentAuthor(activity),
-                                                activity.object.replies,
-                                                true
-                                            )}
-                                        />
-                                        {index < activities.length - 1 && (
-                                            <div className="h-px w-full bg-grey-200"></div>
-                                        )}
-                                    </li>
-                                ))}
-                            </ul>
-                            <div ref={loadMoreRef} className='h-1'></div>
-                            {isFetchingNextPage && (
-                                <div className='flex flex-col items-center justify-center space-y-4 text-center'>
-                                    <LoadingIndicator size='md' />
+                                    </ul>
                                 </div>
-                            )}
+                                <div className={`sticky top-[135px] ml-auto w-full max-w-[300px] max-lg:hidden ${layout === 'inbox' ? '' : ' xxl:fixed xxl:right-[40px]'}`}>
+                                    <h2 className='mb-2 text-lg font-semibold'>You might also like...</h2>
+                                    {isLoadingSuggested ? (
+                                        <LoadingIndicator size="sm" />
+                                    ) : (
+                                        <ul className='grow'>
+                                            {suggested.map((profile) => {
+                                                const actor = profile.actor;
+                                                // const isFollowing = profile.isFollowing;
+                                                return (
+                                                    <li key={actor.id}>
+                                                        <ActivityItem url={actor.url}>
+                                                            <APAvatar author={actor} />
+                                                            <div>
+                                                                <div className='text-grey-600'>
+                                                                    <span className='mr-1 truncate font-bold text-black'>{actor.name || actor.preferredUsername || 'Unknown'}</span>
+                                                                    <div className='truncate text-sm'>{getUsername(actor)}</div>
+                                                                </div>
+                                                            </div>
+                                                            {/* <FollowButton
+                                                                className='ml-auto'
+                                                                following={isFollowing}
+                                                                handle={getUsername(actor)}
+                                                                type='link'
+                                                                onFollow={() => updateSuggestedProfile(actor.id!, {isFollowing: true})}
+                                                                onUnfollow={() => updateSuggestedProfile(actor.id!, {isFollowing: false})}
+                                                            /> */}
+                                                        </ActivityItem>
+                                                    </li>
+                                                );
+                                            })}
+                                        </ul>
+                                    )}
+                                    <Button className='mt-4' color='grey' fullWidth={true} label='Explore' onClick={() => updateRoute('search')} />
+                                </div>
+                            </div>
                         </>
                     ) : (
                         <div className='flex items-center justify-center text-center'>
@@ -155,7 +202,7 @@ const Inbox: React.FC<InboxProps> = ({}) => {
                         Welcome to ActivityPub
                                 </Heading>
                                 <p className='text-pretty text-grey-800'>
-                        We’re so glad to have you on board! At the moment, you can follow other Ghost sites and enjoy their content right here inside Ghost.
+                        We&apos;re so glad to have you on board! At the moment, you can follow other Ghost sites and enjoy their content right here inside Ghost.
                                 </p>
                                 <p className='text-pretty text-grey-800'>
                         You can see all of the users on the right—find your favorite ones and give them a follow.
