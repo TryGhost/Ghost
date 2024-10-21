7c32527159ce9ea2028b8a07b6aea7d95dc7a831
Djordje Vlaisavljevic <dzvlais@gmail.com>
2024-10-21 20:24:36 +0100

    Updated ActivityPub design (#21327)
    ref https://linear.app/ghost/issue/AP-476/remove-static-buttons-from-notifications-and-resolve-css-issues, https://linear.app/ghost/issue/AP-449/remove-follow-button-and-component, https://linear.app/ghost/issue/AP-448/add-loading-state-for-home-tab, https://linear.app/ghost/issue/AP-446/update-top-nav-bar-design
    
    - Added the Suggestions sidebar
    - Added real data to `Your profile` tab
    - Switched navigation in top-bar to text-based
    - Added loading states to Home and Activity tabs
    - Fixed overflow and z-index CSS issues
    - Removed `Unfollow` modal since now have a more user-friendly way to follow users
    - Changed link color to blue

--- apps/admin-x-design-system/tailwind.config.cjs
+++ apps/admin-x-design-system/tailwind.config.cjs
@@ -13,6 +13,7 @@ module.exports = {
             md: '640px',
             lg: '1024px',
             xl: '1320px',
+            xxl: '1440px',
             tablet: '860px'
         },
         colors: {
