5c83414b8e5fd8cd75fa51ad3782c7a06b334088
Chris Raible <chris@ghost.org>
2024-10-22 22:27:56 +0000

    Reorganized commands and added mail config

--- .devcontainer/onCreateCommand.sh
+++ .devcontainer/onCreateCommand.sh
@@ -3,18 +3,7 @@
 set -e
 
 echo "Setting up local config file..."
-if [ "$CODESPACES" = "true" ]; then
-    CONFIG_FILE="ghost/core/config.local.json"
-    if [ ! -f "$CONFIG_FILE" ]; then
-        echo "Creating $CONFIG_FILE..."
-        URL="https://$CODESPACE_NAME-2368.$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN"
-        echo "{
-    \"url\": \"$URL\"
-}" > "$CONFIG_FILE"
-    else
-        echo "$CONFIG_FILE already exists. Skipping creation."
-    fi
-fi
+node .devcontainer/createLocalConfig.js
 
 echo "Updating git submodules..."
 git submodule update --init --recursive
