cd8eb1d1bf41f85ef2946eca9f70757a5380a927
Chris Raible <chris@ghost.org>
2024-10-22 06:50:44 +0000

    Improved post creation flow for devcontainers

--- .devcontainer/hooks/postCreateCommand.sh
+++ .devcontainer/hooks/postCreateCommand.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 # Runs in the devcontainer after it's created
 set -e
+
+echo "Updating git submodules..."
+git submodule update --init --recursive
+
 echo "Installing dependencies..."
 yarn install --check-files
+
+echo "Initializing the database..."
+yarn knex-migrator init
 
 echo "Clearing the build cache..."
 yarn nx reset --only-cache
@@ -10,6 +17,3 @@ yarn nx reset --only-workspace-data
 
 echo "Building typescript packages..."
 yarn nx run-many -t build:ts
-
-echo "Initializing the database..."
-yarn knex-migrator init
--- .devcontainer/hooks/updateContentCommand.sh
+++
@@ -1,3 +0,0 @@
-!#/bin/bash
-
-git submodule update --init --recursive
