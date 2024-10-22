1f3a334b1d69c6a117893d34e647a70ca08338d7
Chris Raible <chris@ghost.org>
2024-10-22 18:41:41 +0000

    Set config.local.json in onCreateCommand and added back unit tests

--- .devcontainer/hooks/onCreateCommand.sh
+++ .devcontainer/hooks/onCreateCommand.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+echo "Setting up local config file..."
+if [ "$CODESPACES" = "true" ]; then
+    CONFIG_FILE="ghost/core/config.local.json"
+    if [ ! -f "$CONFIG_FILE" ]; then
+        echo "Creating $CONFIG_FILE..."
+        URL="https://$CODESPACE_NAME-2368.$GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN"
+        echo "{
+    \"url\": \"$URL\"
+}" > "$CONFIG_FILE"
+    else
+        echo "$CONFIG_FILE already exists. Skipping creation."
+    fi
+fi
+
 echo "Updating git submodules..."
 git submodule update --init --recursive
 
@@ -11,6 +25,9 @@ yarn nx reset --only-workspace-data
 
 echo "Building typescript packages..."
 yarn nx run-many -t build:ts
+
+echo "Running unit tests..."
+yarn test:unit
 
 echo "Building Ghost admin..."
 yarn nx run ghost-admin:build
