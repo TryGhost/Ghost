c824430ba5de12d66d6eec54d7c733262ad71b4e
Chris Raible <chris@ghost.org>
2024-10-22 07:14:50 +0000

    Fixed failing postCreateCommand

--- .devcontainer/hooks/postCreateCommand.sh
+++ .devcontainer/hooks/postCreateCommand.sh
@@ -8,12 +8,12 @@ git submodule update --init --recursive
 echo "Installing dependencies..."
 yarn install --check-files
 
-echo "Initializing the database..."
-yarn knex-migrator init
-
 echo "Clearing the build cache..."
 yarn nx reset --only-cache
 yarn nx reset --only-workspace-data
 
 echo "Building typescript packages..."
 yarn nx run-many -t build:ts
+
+echo "Initializing the database..."
+yarn knex-migrator init
