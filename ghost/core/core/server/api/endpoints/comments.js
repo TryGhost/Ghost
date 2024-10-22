ef0eb607cd589f1878dc065f33a62481cd41c79d
Fabien O'Carroll <fabien@allou.is>
2024-10-22 18:58:18 +0700

    Fixed X-Invalidate-Cache headers when editing comments
    refs https://linear.app/ghost/issue/ENG-1673
    
    We had completely missed this endpoint when adding handling for clearing the
    comments cache

--- ghost/core/core/server/api/endpoints/comments.js
+++ ghost/core/core/server/api/endpoints/comments.js
@@ -1,5 +1,17 @@
 const models = require('../../models');
 
+function handleCacheHeaders(model, frame) {
+    if (model) {
+        const postId = model.get('post_id');
+        const parentId = model.get('parent_id');
+        const pathsToInvalidate = [
+            postId ? `/api/members/comments/post/${postId}/` : null,
+            parentId ? `/api/members/comments/${parentId}/replies/` : null
+        ].filter(path => path !== null);
+        frame.setHeader('X-Cache-Invalidate', pathsToInvalidate.join(', '));
+    }
+}
+
 /** @type {import('@tryghost/api-framework').Controller} */
 const controller = {
     docName: 'comments',
@@ -19,11 +31,15 @@ const controller = {
             }
         },
         permissions: true,
-        query(frame) {
-            return models.Comment.edit({
+        async query(frame) {
+            const result = await models.Comment.edit({
                 id: frame.data.comments[0].id,
                 status: frame.data.comments[0].status
             }, frame.options);
+
+            handleCacheHeaders(result, frame);
+
+            return result;
         }
     }
 };
