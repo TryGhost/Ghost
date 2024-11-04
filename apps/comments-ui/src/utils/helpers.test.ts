d3316784982270d7bef404f0b3eee4376efc0560
Sanne de Vries <sannedv@protonmail.com>
2024-11-04 14:29:23 +0100

    Added unit test for formatRelativeTime helper
    REF PLG-235

--- apps/comments-ui/src/utils/helpers.test.ts
+++ apps/comments-ui/src/utils/helpers.test.ts
@@ -1,4 +1,5 @@
 import * as helpers from './helpers';
+import sinon from 'sinon';
 import {buildAnonymousMember, buildComment, buildDeletedMember} from '../../test/utils/fixtures';
 
 describe('formatNumber', function () {
@@ -16,6 +17,63 @@ describe('formatNumber', function () {
 
     it('handles null', function () {
         expect((helpers.formatNumber as any)(null)).toEqual('');
+    });
+});
+
+describe('Date/Time Formatting', () => {
+    let clock: sinon.SinonFakeTimers;
+    const t = (key: string, vars?: Record<string, string | number>) => {
+        if (vars) {
+            return key.replace('{{amount}}', vars.amount.toString());
+        }
+        return key;
+    };
+
+    beforeEach(() => {
+        // Set a fixed time: 2024-02-15 15:00:00
+        clock = sinon.useFakeTimers(new Date('2024-02-15T15:00:00.000Z'));
+    });
+
+    afterEach(() => {
+        clock.restore();
+    });
+
+    describe('formatRelativeTime', () => {
+        it('handles just now', () => {
+            expect(helpers.formatRelativeTime('2024-02-15T14:59:57.000Z', t)).toBe('Just now');
+        });
+
+        it('handles seconds ago', () => {
+            expect(helpers.formatRelativeTime('2024-02-15T14:59:30.000Z', t)).toBe('30 seconds ago');
+        });
+
+        it('handles one minute ago', () => {
+            expect(helpers.formatRelativeTime('2024-02-15T14:59:00.000Z', t)).toBe('One min ago');
+        });
+
+        it('handles multiple minutes ago', () => {
+            expect(helpers.formatRelativeTime('2024-02-15T14:30:00.000Z', t)).toBe('30 mins ago');
+        });
+
+        it('handles one hour ago', () => {
+            expect(helpers.formatRelativeTime('2024-02-15T14:00:00.000Z', t)).toBe('One hour ago');
+        });
+
+        it('handles multiple hours ago', () => {
+            expect(helpers.formatRelativeTime('2024-02-15T12:00:00.000Z', t)).toBe('3 hrs ago');
+        });
+
+        it('handles yesterday', () => {
+            expect(helpers.formatRelativeTime('2024-02-14T15:00:00.000Z', t)).toBe('Yesterday');
+        });
+
+        it('handles date in current year', () => {
+            expect(helpers.formatRelativeTime('2024-01-15T15:00:00.000Z', t)).toBe('15 Jan');
+        });
+
+        it('handles date in different year', () => {
+            expect(helpers.formatRelativeTime('2023-02-15T15:00:00.000Z', t)).toBe('15 Feb 2023');
+        });
     });
 });
 
