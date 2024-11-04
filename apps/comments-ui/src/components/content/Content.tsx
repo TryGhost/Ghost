7216c79dcb14a463a6bc0135666c443e5c01eae2
Sanne de Vries <65487235+sanne-san@users.noreply.github.com>
2024-11-04 11:20:21 +0100

    Update apps/comments-ui/src/components/content/Content.tsx
    Co-authored-by: Kevin Ansfield <kevin@ghost.org>

--- apps/comments-ui/src/components/content/Content.tsx
+++ apps/comments-ui/src/components/content/Content.tsx
@@ -83,7 +83,7 @@ const Content = () => {
                                 <CTABox isFirst={isFirst} isPaid={isPaidOnly} />
                             </section>) : 
                             <section className={`flex flex-col items-center pt-[40px] ${member ? 'pb-[32px]' : 'pb-[48px]'} ${!isFirst && 'mt-4'} ${(!member || (member && isPaidOnly)) && commentCount ? 'border-t' : 'border-none'} border-[rgba(0,0,0,0.075)] sm:px-8 dark:border-[rgba(255,255,255,0.1)]`} data-testid="cta-box">
-                                <CTABox isFirst={pagination?.total === 0} isPaid={isPaidOnly} />
+                                <CTABox isFirst={isFirst} isPaid={isPaidOnly} />
                             </section>)
                         : null
                     }
