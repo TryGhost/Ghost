8a2925d11845838dcfe0342d46e129f0be335fad
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2024-11-05 19:28:45 +0800

    Hide typography related theme settings from official themes

--- apps/admin-x-settings/src/components/settings/site/designAndBranding/ThemeSettings.tsx
+++ apps/admin-x-settings/src/components/settings/site/designAndBranding/ThemeSettings.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ThemeSetting from './ThemeSetting';
+import useFeatureFlag from '../../../../hooks/useFeatureFlag';
 import {CustomThemeSetting} from '@tryghost/admin-x-framework/api/customThemeSettings';
 import {Form} from '@tryghost/admin-x-design-system';
+import {Theme, useBrowseThemes} from '@tryghost/admin-x-framework/api/themes';
 import {isCustomThemeSettingVisible} from '../../../../utils/isCustomThemeSettingsVisible';
+import {useOfficialThemes} from '../../../providers/SettingsAppProvider';
 
 interface ThemeSettingsProps {
     sections: Array<{
@@ -14,6 +17,13 @@ interface ThemeSettingsProps {
 }
 
 const ThemeSettings: React.FC<ThemeSettingsProps> = ({sections, updateSetting}) => {
+    const officialThemes = useOfficialThemes();
+    const officialThemeNames = new Set(officialThemes.map(theme => theme.name.toLowerCase()));
+    const {data: themesData} = useBrowseThemes();
+    const activeTheme = themesData?.themes.find((theme: Theme) => theme.active);
+    const activeThemeName = activeTheme?.name.toLowerCase() || '';
+    const hasCustomFonts = useFeatureFlag('customFonts');
+
     return (
         <>
             {sections.map((section) => {
@@ -29,9 +39,20 @@ const ThemeSettings: React.FC<ThemeSettingsProps> = ({sections, updateSetting}) 
                             if (setting.type === 'boolean' && previousType !== 'boolean' && previousType !== undefined) {
                                 spaceClass = 'mt-3';
                             }
-                            if ((setting.type === 'text' || setting.type === 'select') && (previousType === 'text' || previousType === 'select')) { 
+                            if ((setting.type === 'text' || setting.type === 'select') && (previousType === 'text' || previousType === 'select')) {
                                 spaceClass = 'mt-2';
                             }
+
+                            // hides typography related theme settings from official themes
+                            // should be removed once we remove the settings from the themes in 6.0
+                            if (hasCustomFonts) {
+                                if (['title_font', 'body_font', 'typography'].includes(setting.key)) {
+                                    if (officialThemeNames.has(activeThemeName)) {
+                                        spaceClass += ' hidden';
+                                    }
+                                }
+                            }
+
                             previousType = setting.type;
                             return <div key={setting.key} className={spaceClass}>
                                 <ThemeSetting
