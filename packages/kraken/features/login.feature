5df2f125003f1ff9ef6cef4dbd0d2c72465e9836
5x5x5-cube <ae.pena@uniandes.edu.co>
2024-11-06 21:04:11 -0500

    test: updated login tests

--- packages/kraken/features/login.feature
+++ packages/kraken/features/login.feature
@@ -1,7 +1,9 @@
-Feature: Login feature
+Feature: F012 - Login feature
+Esta funcionalidad permite ingresar las credenciales de 
+administrador para acceder a las funcionalidades del CSM
 
 @user1 @web
-Scenario: Successful login as administrator
+Scenario: E01201 - Inicio de sesión exitoso
   Given I navigate to page "<GHOST_URL>"
   When I wait for 2 seconds
   When I enter email "<ADMIN_USERNAME>"
--- packages/kraken/features/web/step_definitions/login.steps.js
+++ packages/kraken/features/web/step_definitions/login.steps.js
@@ -1,12 +1,23 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
 
+Given(
+    "I am an admin logged in with email {kraken-string} and password {kraken-string}",
+    async function (email, password) {
+        const element = await this.driver.$("#identification");
+        await element.setValue(email);
+        const element2 = await this.driver.$("#password");
+        await element2.setValue(password);
+        const element3 = await this.driver.$("#ember5");
+        await element3.click();
+    }
+);
+
 When("I enter email {kraken-string}", async function (email) {
     const element = await this.driver.$("#identification");
     return await element.setValue(email);
 });
 
 When("I enter password {kraken-string}", async function (password) {
-    console.log("received password: ", password);
     const element = await this.driver.$("#password");
     return await element.setValue(password);
 });
