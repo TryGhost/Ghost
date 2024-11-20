3265e257b7575595df50835045108ca3c6f9554d
Fabien O'Carroll <fabien@allou.is>
2024-11-20 17:54:30 +0700

    Fixed webhooks not firing for internal integrations
    ref https://linear.app/ghost/issue/AP-598
    
    We want to make sure that webhooks for internal integrations are fired even
    when the custom integrations limit has been hit. In order to test this we've
    had to update the fixtureManager to allow passing the integration type when
    inserting fixture webhooks into the db.

--- ghost/core/test/e2e-webhooks/site.test.js
+++ ghost/core/test/e2e-webhooks/site.test.js
@@ -87,4 +87,40 @@ describe('site.* events', function () {
             throw new Error('The webhook should not have been sent.');
         }
     });
+
+    it('site.changed event is triggered, custom integrations are limited but we have an internal webhook', async function () {
+        const webhookURL = 'https://test-webhook-receiver.com/site-changed';
+        await webhookMockReceiver.mock(webhookURL);
+        await fixtureManager.insertWebhook({
+            event: 'site.changed',
+            url: webhookURL,
+            integrationType: 'internal'
+        });
+
+        mockManager.mockLimitService('customIntegrations', {
+            isLimited: true,
+            wouldGoOverLimit: true
+        });
+
+        await adminAPIAgent
+            .post('posts/')
+            .body({
+                posts: [{
+                    title: 'webhookz',
+                    status: 'published',
+                    mobiledoc: fixtureManager.get('posts', 1).mobiledoc
+                }]
+            })
+            .expectStatus(201);
+
+        await webhookMockReceiver.receivedRequest();
+
+        webhookMockReceiver
+            .matchHeaderSnapshot({
+                'content-version': anyContentVersion,
+                'content-length': anyNumber,
+                'user-agent': anyGhostAgent
+            })
+            .matchBodySnapshot();
+    });
 });
