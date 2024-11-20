8f664827beb07495675d706df4de9df14a684b6b
Ronald Langeveld <hi@ronaldlangeveld.com>
2024-11-20 15:19:30 +0800

    Added deleted

--- apps/comments-ui/src/components/content/Comment.tsx
+++ apps/comments-ui/src/components/content/Comment.tsx
@@ -155,7 +155,7 @@ const UnpublishedComment: React.FC<UnpublishedCommentProps> = ({comment, openEdi
             <div className="mt-[-3px] flex items-start">
                 <div className="flex h-10 flex-row items-center gap-4 pb-[8px] pr-4">
                     <p className="text-md mt-[4px] font-sans italic leading-normal text-black/20 sm:text-lg dark:text-white/35">
-                        {!labs.commentImprovements ? notPublishedMessage : <span dangerouslySetInnerHTML={{__html: comment.html}} data-testId="hidden-comment-html"></span>}
+                        {labs.commentImprovements && comment.status !== 'deleted' ? <span dangerouslySetInnerHTML={{__html: comment.html}} data-testId="hidden-comment-html"></span> : notPublishedMessage}
                     </p>
                     <div className="mt-[4px]">
                         <MoreButton comment={comment} toggleEdit={openEditMode} />
