6ec729dc56534d1d38fefb082cf2a19a4786ea37
Djordje Vlaisavljevic <dzvlais@gmail.com>
2024-11-26 17:35:25 +0000

    Made inbox view easier to scan through (#21729)
    ref https://linear.app/ghost/issue/AP-609/give-inbox-ui-more-room-to-breathe
    
    - Added more white space
    - Made more lines of text for titles and excerpts visible
    - Added estimated reading time
    - Updated feature image size and position

---
+++ apps/admin-x-activitypub/src/utils/get-reading-time.ts
@@ -0,0 +1,12 @@
+export default function getReadingTime(content: string): string {
+    // Average reading speed (words per minute)
+    const wordsPerMinute = 238;
+
+    const wordCount = content.replace(/<[^>]*>/g, '')
+        .split(/\s+/)
+        .filter(word => word.length > 0)
+        .length;
+
+    const minutes = Math.ceil(wordCount / wordsPerMinute);
+    return `${minutes} min read`;
+}
