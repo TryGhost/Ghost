8db1569385a87a4c0eac07c08267ce1cf849f902
Sanne de Vries <sannedv@protonmail.com>
2024-11-26 13:45:35 +0100

    Fixed spinner being invisible in light mode
    REF https://linear.app/ghost/issue/PLG-272/look-into-potential-spinner-regression

--- apps/comments-ui/src/components/content/Loading.tsx
+++ apps/comments-ui/src/components/content/Loading.tsx
@@ -3,7 +3,7 @@ import {ReactComponent as SpinnerIcon} from '../../images/icons/spinner.svg';
 function Loading() {
     return (
         <div className="flex h-32 w-full items-center justify-center" data-testid="loading">
-            <SpinnerIcon className="mb-6 h-12 w-12 fill-white/90 dark:fill-white/60" />
+            <SpinnerIcon className="mb-6 h-12 w-12 fill-neutral-900/90 dark:fill-white/60" />
         </div>
     );
 }
