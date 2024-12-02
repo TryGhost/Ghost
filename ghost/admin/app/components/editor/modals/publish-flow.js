e4f89861579823f3af40570c954637c71c0bd93c
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2024-12-02 11:07:14 +0800

    Fixed email error flash on the publish flow (#21706)
    ref DES-990
    
    - previously, the publish flow was continued regardless of if there was an error while sending email
    - now it correctly stays on the error state, and goes to the normal flow once the error is fixed and retried

--- ghost/admin/app/components/editor/modals/publish-flow.js
+++ ghost/admin/app/components/editor/modals/publish-flow.js
@@ -12,6 +12,7 @@ export default class PublishModalComponent extends Component {
     };
 
     @service store;
+    @service router;
 
     @tracked emailErrorMessage = this.args.data.publishOptions.post.didEmailFail ? (this.args.data.publishOptions.post.email.error ?? 'Unknown error') : undefined;
     @tracked isConfirming = false;
@@ -52,20 +53,51 @@ export default class PublishModalComponent extends Component {
     @action
     setCompleted() {
         this.emailErrorMessage = undefined;
-        this.isConfirming = false;
         this.isComplete = true;
+
+        if (this.args.data.publishOptions.isScheduled) {
+            try {
+                localStorage.setItem('ghost-last-scheduled-post', JSON.stringify({
+                    id: this.args.data.publishOptions.post.id,
+                    type: this.args.data.publishOptions.post.displayName
+                }));
+            } catch (e) {
+                // ignore localStorage errors
+            }
+            if (this.args.data.publishOptions.post.displayName !== 'page') {
+                this.router.transitionTo('posts');
+            } else {
+                this.router.transitionTo('pages');
+            }
+        } else {
+            try {
+                localStorage.setItem('ghost-last-published-post', JSON.stringify({
+                    id: this.args.data.publishOptions.post.id,
+                    type: this.args.data.publishOptions.post.displayName
+                }));
+            } catch (e) {
+                // ignore localStorage errors
+            }
+            if (this.args.data.publishOptions.post.displayName !== 'page') {
+                if (this.args.data.publishOptions.post.hasEmail) {
+                    this.router.transitionTo('posts.analytics', this.args.data.publishOptions.post.id);
+                } else {
+                    this.router.transitionTo('posts');
+                }
+            } else {
+                this.router.transitionTo('pages');
+            }
+        }
     }
 
     @task
     *saveTask() {
         try {
             yield this.args.data.saveTask.perform();
-
-            this.isConfirming = false;
             this.isComplete = true;
         } catch (e) {
             if (e?.name === 'EmailFailedError') {
-                return this.emailErrorMessage = e.message;
+                this.emailErrorMessage = e.message;
             }
 
             throw e;
