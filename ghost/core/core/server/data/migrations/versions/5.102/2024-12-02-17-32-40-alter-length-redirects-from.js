fa05176d7ad062d51254d6760996fb2ee7263b7a
Steve Larson <9larsons@gmail.com>
2024-12-03 07:53:00 -0600

    Revert SQLite handling, knex does this for us

--- ghost/core/core/server/data/migrations/versions/5.102/2024-12-02-17-32-40-alter-length-redirects-from.js
+++ ghost/core/core/server/data/migrations/versions/5.102/2024-12-02-17-32-40-alter-length-redirects-from.js
@@ -2,78 +2,18 @@
 
 const logging = require('@tryghost/logging');
 const {createNonTransactionalMigration} = require('../../utils');
-const DatabaseInfo = require('@tryghost/database-info');
 
 module.exports = createNonTransactionalMigration(
     async function up(knex) {
         logging.info('Altering length of redirects.from');
-        // SQLite doesn't support altering column length
-        if (DatabaseInfo.isSQLite(knex)) {
-            logging.info('Creating temporary redirects table for SQLite');
-            try {
-                await knex.schema.dropTableIfExists('redirects_temp');
-                await knex.schema.createTable('redirects_temp', function (table) {
-                    table.string('id').primary();
-                    table.string('from', 191).notNullable();
-                    table.string('to', 2000).notNullable();
-                    table.string('post_id', 24).nullable();
-                    table.dateTime('created_at').notNullable();
-                    table.dateTime('updated_at').nullable();
-                });
-
-                logging.info('Inserting data into temporary redirects table');
-                await knex.raw(`
-                    INSERT INTO redirects_temp ("id", "from", "to", "post_id", "created_at", "updated_at")
-                    SELECT "id", substr("from", 1, 191), "to", "post_id", "created_at", "updated_at" FROM redirects
-                `);
-            } catch (error) {
-                logging.error('Error inserting data into temporary redirects table:', error);
-                logging.info('Dropping temporary redirects table due to error');
-                await knex.schema.dropTableIfExists('redirects_temp');
-                throw error; // Re-throw the error to ensure the migration fails
-            }
-
-            logging.info('Dropping original redirects table');
-            await knex.schema.dropTable('redirects');
-
-            logging.info('Renaming temporary redirects table to redirects');
-            await knex.schema.renameTable('redirects_temp', 'redirects');
-        } else {
-            await knex.schema.alterTable('redirects', function (table) {
-                table.string('from', 191).notNullable().alter();
-            });
-        }
+        await knex.schema.alterTable('redirects', function (table) {
+            table.string('from', 191).notNullable().alter();
+        });
     },
     async function down(knex) {
         logging.info('Reverting length of redirects.from');
-        // SQLite doesn't support altering column length
-        if (DatabaseInfo.isSQLite(knex)) {
-            logging.info('Creating temporary redirects table for SQLite');
-            await knex.schema.dropTableIfExists('redirects_temp');
-            await knex.schema.createTable('redirects_temp', function (table) {
-                table.string('id').primary();
-                table.string('from', 2000).notNullable();
-                table.string('to', 2000).notNullable();
-                table.string('post_id', 24).nullable();
-                table.dateTime('created_at').notNullable();
-                table.dateTime('updated_at').nullable();
-            });
-
-            logging.info('Inserting data into temporary redirects table');
-            await knex.raw(`
-                INSERT INTO redirects_temp ("id", "from", "to", "post_id", "created_at", "updated_at")
-                SELECT "id", "from", "to", "post_id", "created_at", "updated_at" FROM redirects
-            `);
-
-            logging.info('Dropping original redirects table');
-            await knex.schema.dropTable('redirects');
-
-            logging.info('Renaming temporary redirects table to redirects');
-            await knex.schema.renameTable('redirects_temp', 'redirects');
-        } else {
-            await knex.schema.alterTable('redirects', function (table) {
-                table.string('from', 191).notNullable().alter();
-            });
-        }
+        await knex.schema.alterTable('redirects', function (table) {
+            table.string('from', 191).notNullable().alter();
+        });
     }
 );
