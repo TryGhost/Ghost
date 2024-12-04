ec5b87df1044831286be9b2cc2914e1593cb9aa4
Kevin Ansfield <kevin@lookingsideways.co.uk>
2024-12-04 14:38:47 +0000

    Removed link from replied-to ref when deleted/hidden
    closes https://linear.app/ghost/issue/PLG-294
    
    - making the `replied to: [removed]` text a link was a bit confusing because clicking it does nothing
    - if the replied-to comment doesn't exist (e.g. hidden/deleted and not returned in API response) or has been unpublished we replace the `<a>` with a `<span>` to remove the link behaviour

--- apps/comments-ui/src/components/content/Comment.tsx
+++ apps/comments-ui/src/components/content/Comment.tsx
@@ -279,7 +279,7 @@ const AuthorName: React.FC<{comment: Comment}> = ({comment}) => {
     );
 };
 
-const RepliedToSnippet: React.FC<{comment: Comment}> = ({comment}) => {
+export const RepliedToSnippet: React.FC<{comment: Comment}> = ({comment}) => {
     const {comments, dispatchAction, t} = useAppContext();
     const inReplyToComment = findCommentById(comments, comment.in_reply_to_id);
 
@@ -298,19 +298,20 @@ const RepliedToSnippet: React.FC<{comment: Comment}> = ({comment}) => {
     };
 
     let inReplyToSnippet = comment.in_reply_to_snippet;
-    if (inReplyToComment && inReplyToComment.status !== 'published') {
+    // For public API requests hidden/deleted comments won't exist in the comments array
+    // unless it was only just deleted in which case it will exist but have a 'deleted' status
+    if (!inReplyToComment || inReplyToComment.status !== 'published') {
         inReplyToSnippet = `[${t('removed')}]`;
     }
 
+    const linkToReply = inReplyToComment && inReplyToComment.status === 'published';
+
+    const className = 'font-semibold text-neutral-900/60 transition-colors hover:text-neutral-900/70 dark:text-white/70 dark:hover:text-white/80';
+
     return (
-        <a
-            className="font-semibold text-neutral-900/60 transition-colors hover:text-neutral-900/70 dark:text-white/70 dark:hover:text-white/80"
-            data-testid="comment-in-reply-to"
-            href={`#${comment.in_reply_to_id}`}
-            onClick={scrollRepliedToCommentIntoView}
-        >
-            {inReplyToSnippet}
-        </a>
+        linkToReply
+            ? <a className={className} data-testid="comment-in-reply-to" href={`#${comment.in_reply_to_id}`} onClick={scrollRepliedToCommentIntoView}>{inReplyToSnippet}</a>
+            : <span className={className} data-testid="comment-in-reply-to">{inReplyToSnippet}</span>
     );
 };
 
