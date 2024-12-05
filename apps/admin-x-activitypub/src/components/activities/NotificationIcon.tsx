0c4a4c517721e1553335d1906058a49e68b7b674
Djordje Vlaisavljevic <dzvlais@gmail.com>
2024-12-05 14:40:55 +0000

    Made icons and avatars larger
    ref https://linear.app/ghost/issue/AP-625/implement-notification-grouping-for-follows-and-likes

--- apps/admin-x-activitypub/src/components/activities/NotificationIcon.tsx
+++ apps/admin-x-activitypub/src/components/activities/NotificationIcon.tsx
@@ -15,25 +15,25 @@ const NotificationIcon: React.FC<NotificationIconProps> = ({notificationType, cl
 
     switch (notificationType) {
     case 'follow':
-        icon = 'user-fill';
+        icon = 'user';
         iconColor = 'text-blue-500';
-        badgeColor = 'bg-blue-100';
+        badgeColor = 'bg-blue-100/50';
         break;
     case 'like':
-        icon = 'heart-fill';
+        icon = 'heart';
         iconColor = 'text-red-500';
-        badgeColor = 'bg-red-100';
+        badgeColor = 'bg-red-100/50';
         break;
     case 'reply':
-        icon = 'comment-fill';
+        icon = 'comment';
         iconColor = 'text-purple-500';
-        badgeColor = 'bg-purple-100';
+        badgeColor = 'bg-purple-100/50';
         break;
     }
 
     return (
-        <div className={`flex h-6 w-6 items-center justify-center rounded-md ${badgeColor} ${className}`}>
-            <Icon colorClass={iconColor} name={icon} size='xs' />
+        <div className={`flex h-10 w-10 items-center justify-center rounded-md ${badgeColor} ${className}`}>
+            <Icon colorClass={iconColor} name={icon} size='sm' />
         </div>
     );
 };
