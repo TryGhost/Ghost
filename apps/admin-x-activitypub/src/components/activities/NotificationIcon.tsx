e7a9b2a72040def67ade1c04ea2a95e57fa53157
Djordje Vlaisavljevic <dzvlais@gmail.com>
2024-12-05 21:52:03 +0000

    Made similar notifications appear grouped together (#21805)
    ref https://linear.app/ghost/issue/AP-625/implement-notification-grouping-for-follows-and-likes
    
    - Added basic frontend grouping for Follow and Like notifications, so the page is easier to scan through. For each loaded batch of notifications we check if they can be grouped (all follows get grouped, all likes for a certain post get grouped) and then merge those grouped pages of notifications.
    - Improved UI for notifications, with notification type icons for each group.
    - Added a utility for truncating text.

---
+++ apps/admin-x-activitypub/src/components/activities/NotificationIcon.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Icon} from '@tryghost/admin-x-design-system';
+
+export type NotificationType = 'like' | 'follow' | 'reply';
+
+interface NotificationIconProps {
+    notificationType: 'like' | 'follow' | 'reply';
+    className?: string;
+}
+
+const NotificationIcon: React.FC<NotificationIconProps> = ({notificationType, className}) => {
+    let icon = '';
+    let iconColor = '';
+    let badgeColor = '';
+
+    switch (notificationType) {
+    case 'follow':
+        icon = 'user';
+        iconColor = 'text-blue-500';
+        badgeColor = 'bg-blue-100/50';
+        break;
+    case 'like':
+        icon = 'heart';
+        iconColor = 'text-red-500';
+        badgeColor = 'bg-red-100/50';
+        break;
+    case 'reply':
+        icon = 'comment';
+        iconColor = 'text-purple-500';
+        badgeColor = 'bg-purple-100/50';
+        break;
+    }
+
+    return (
+        <div className={`flex h-10 w-10 items-center justify-center rounded-full ${badgeColor} ${className}`}>
+            <Icon colorClass={iconColor} name={icon} size='sm' />
+        </div>
+    );
+};
+
+export default NotificationIcon;
---
+++ apps/admin-x-activitypub/src/components/activities/NotificationItem.tsx
@@ -0,0 +1,60 @@
+import NotificationIcon, {NotificationType} from './NotificationIcon';
+import React from 'react';
+
+// Context to share common props between compound components
+interface NotificationContextType {
+    onClick?: () => void;
+    url?: string;
+}
+
+const NotificationContext = React.createContext<NotificationContextType | undefined>(undefined);
+
+// Root component
+interface NotificationItemProps {
+    children: React.ReactNode;
+    onClick?: () => void;
+    url?: string;
+    className?: string;
+}
+
+const NotificationItem = ({children, onClick, url, className}: NotificationItemProps) => {
+    return (
+        <NotificationContext.Provider value={{onClick, url}}>
+            <button className={`relative -mx-4 -my-px grid grid-cols-[auto_1fr] gap-3 rounded-lg p-4 text-left hover:bg-grey-75 ${className}`} type='button' onClick={onClick}>
+                {children}
+            </button>
+        </NotificationContext.Provider>
+    );
+};
+
+// Sub-components
+const Icon = ({type}: {type: NotificationType}) => {
+    return (
+        <div className='col-start-1 row-start-1'>
+            <NotificationIcon notificationType={type} />
+        </div>
+    );
+};
+
+const Avatars = ({children}: {children: React.ReactNode}) => {
+    return (
+        <div className='col-start-2 row-start-1 flex gap-2'>
+            {children}
+        </div>
+    );
+};
+
+const Content = ({children}: {children: React.ReactNode}) => {
+    return (
+        <div className='col-start-2 row-start-2'>
+            {children}
+        </div>
+    );
+};
+
+// Attach sub-components to the main component
+NotificationItem.Icon = Icon;
+NotificationItem.Avatars = Avatars;
+NotificationItem.Content = Content;
+
+export default NotificationItem;
