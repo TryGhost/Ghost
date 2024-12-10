a4b5952cb966fd9e8d038edefba73363832c0406
Princi Vershwal <vershwal.princi@gmail.com>
2024-12-10 11:45:17 +0530

    Updated tests

--- ghost/core/test/unit/server/data/importer/import-manager.test.js
+++ ghost/core/test/unit/server/data/importer/import-manager.test.js
@@ -6,16 +6,20 @@ const importManager = require('../../../../../core/server/data/importer/import-m
 
 describe('Import Manager', function () {
     describe('extractZip', function () {
-        it('can extract a zip file', async function () {
+        it('extracts zip file and sets correct file permissions', async function () {
             const zipPath = path.join(__dirname, '/test.zip');
             const extractedPath = await importManager.extractZip(zipPath);
-            const files = glob.sync('**/*', {cwd: extractedPath, nodir: true});
-            files.forEach((file) => {
-                const filePath = path.join(extractedPath, file);
-                const stats = fs.statSync(filePath);
-                const fileMode = stats.mode & 0o777;
-                should.equal(fileMode, 0o644, `File ${file} should have 0644 permissions`);
-            });
+            try {
+                const files = glob.sync('**/*', {cwd: extractedPath, nodir: true});
+                files.forEach((file) => {
+                    const filePath = path.join(extractedPath, file);
+                    const stats = fs.statSync(filePath);
+                    const fileMode = stats.mode & 0o777;
+                    should.equal(fileMode, 0o644, `File ${file} should have 0644 permissions`);
+                });
+            } finally {
+                await fs.remove(extractedPath);
+            }
         });
     });
 });
