9ff81d45f2ac197a69515ceb6305f28a166c370b
Sag <guptazy@gmail.com>
2024-12-10 18:20:18 +0800

    Fixed the bug

--- ghost/stripe/lib/services/webhook/SubscriptionEventService.js
+++ ghost/stripe/lib/services/webhook/SubscriptionEventService.js
@@ -1,5 +1,6 @@
 const errors = require('@tryghost/errors');
 const _ = require('lodash');
+const logging = require('@tryghost/logging');
 module.exports = class SubscriptionEventService {
     constructor(deps) {
         this.deps = deps;
@@ -18,18 +19,34 @@ module.exports = class SubscriptionEventService {
             customer_id: subscription.customer
         });
 
-        if (member) {
-            try {
-                await memberRepository.linkSubscription({
-                    id: member.id,
-                    subscription
-                });
-            } catch (err) {
-                if (err.code !== 'ER_DUP_ENTRY' && err.code !== 'SQLITE_CONSTRAINT') {
-                    throw err;
-                }
-                throw new errors.ConflictError({err});
+        // During checkout, Stripe sends customer.subscription.created and customer.subscription.updated events before checkout.session.completed
+        // We use the checkout.session.completed event to create a member and the related subscription in the database, as checkout.session.completed contains additional information on the subscription (e.g. attribution data)
+        // Therefore, we ignore customer.subscription.* events until a member and its subscription are present in the database
+        if (!member) {
+            logging.info(`Ignoring customer.subscription.* event as member does not exist`);
+            return;
+        }
+
+        const memberSubscriptions = await member.related('stripeSubscriptions').fetch();
+        const memberSubscription = memberSubscriptions.models.find((sub) => {
+            return sub.get('subscription_id') === subscription.id;
+        });
+
+        if (!memberSubscription) {
+            logging.info(`Ignoring customer.subscription.* event as member subscription does not exist`);
+            return;
+        }
+
+        try {
+            await memberRepository.linkSubscription({
+                id: member.id,
+                subscription
+            });
+        } catch (err) {
+            if (err.code !== 'ER_DUP_ENTRY' && err.code !== 'SQLITE_CONSTRAINT') {
+                throw err;
             }
+            throw new errors.ConflictError({err});
         }
     }
 };
