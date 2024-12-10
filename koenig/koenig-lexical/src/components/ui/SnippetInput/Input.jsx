29b4b4eb9209f0b27191b8c63ac3df1c6f3cef7b
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2024-12-10 13:06:59 +0800

    Disabled autofill on snippet name input (TryGhost/Koenig#1402)
    - 1Password autofill gets triggered when creating a new snippet
    - This makes sure the input has no autofill in general and 1Password

--- koenig/koenig-lexical/src/components/ui/SnippetInput/Input.jsx
+++ koenig/koenig-lexical/src/components/ui/SnippetInput/Input.jsx
@@ -5,11 +5,13 @@ export const Input = ({value, onChange, onClear, onKeyDown}) => {
     return (
         <div className="relative m-0 flex items-center justify-evenly gap-1 rounded-lg bg-white font-sans text-md font-normal text-black shadow-md dark:bg-grey-950">
             <input
+                autoComplete="off"
                 autoFocus={true}
                 className={`mb-[1px] h-auto w-full bg-white py-1 pl-3 pr-9 font-normal leading-loose text-grey-900 selection:bg-grey/40 dark:bg-grey-950 dark:text-grey-100 dark:placeholder:text-grey-800 ${value ? 'rounded-b-none rounded-t' : 'rounded'}`}
                 data-testid="snippet-name"
                 placeholder="Snippet name"
                 value={value}
+                data-1p-ignore
                 onChange={onChange}
                 onKeyDown={onKeyDown}
             />
