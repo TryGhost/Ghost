4a0aa9da784785c4df9045ad3f2523c4e534d2b6
Peter Zimon <peter.zimon@gmail.com>
2024-12-11 15:33:09 +0100

    Added docs

--- apps/shade/src/components/ui/button.tsx
+++ apps/shade/src/components/ui/button.tsx
@@ -9,14 +9,10 @@ const buttonVariants = cva(
     {
         variants: {
             variant: {
-                default:
-          'bg-primary text-primary-foreground shadow hover:bg-primary/90',
-                destructive:
-          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
-                outline:
-          'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
-                secondary:
-          'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
+                default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+                destructive: 'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
+                outline: 'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
+                secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
                 ghost: 'hover:bg-accent hover:text-accent-foreground',
                 link: 'text-primary underline-offset-4 hover:underline'
             },
@@ -35,9 +31,9 @@ const buttonVariants = cva(
 );
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+    asChild?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
