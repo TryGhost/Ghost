fca74f15d84610f31116c5f369f2239ab6f36c39
Peter Zimon <peter.zimon@gmail.com>
2024-12-11 19:53:12 +0100

    Added size variant

--- apps/shade/src/assets/icons/icons.ts
+++ apps/shade/src/assets/icons/icons.ts
@@ -1,4 +1,21 @@
 import React from 'react';
+import {cva, type VariantProps} from 'class-variance-authority';
+import {cn} from '@/lib/utils';
+
+// Define the icon styles using CVA
+const iconVariants = cva('', {
+    variants: {
+        size: {
+            sm: 'h-3 w-3',
+            md: 'h-4 w-4',
+            lg: 'h-6 w-6',
+            xl: 'h-8 w-8'
+        }
+    },
+    defaultVariants: {
+        size: 'md'
+    }
+});
 
 // Helper to convert kebab-case to PascalCase with numbers
 const toPascalCase = (str: string): string => {
@@ -8,8 +25,10 @@ const toPascalCase = (str: string): string => {
 };
 
 // Define props interface for icons
-interface IconProps extends React.SVGProps<SVGSVGElement> {
-    className?: string;
+interface IconProps extends
+    React.SVGProps<SVGSVGElement>,
+    VariantProps<typeof iconVariants> {
+        className?: string;
 }
 
 const iconModules = import.meta.glob<{ReactComponent: React.FC<IconProps> }>(
@@ -21,7 +40,12 @@ const Icon = Object.entries(iconModules).reduce((acc, [path, module]) => {
     const kebabName = path.match(/\.\/(.+)\.svg/)?.[1] ?? '';
     const iconName = toPascalCase(kebabName);
 
-    acc[iconName] = module.ReactComponent;
+    acc[iconName] = (props: IconProps) => {
+        const {size, className, ...rest} = props;
+        const iconClassName = cn(iconVariants({size, className}));
+        return React.createElement(module.ReactComponent, {...rest,
+            className: iconClassName});
+    };
     return acc;
 }, {} as Record<string, React.FC<IconProps>>);
 
