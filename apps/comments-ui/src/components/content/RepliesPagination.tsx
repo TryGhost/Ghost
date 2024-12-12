f3305d21f3d1efeb8645653cabfe615f683a9453
Kevin Ansfield <kevin@lookingsideways.co.uk>
2024-12-12 15:41:03 +0000

    Fixed incorrect replies pagination count after deleting reply
    closes https://linear.app/ghost/issue/PLG-303
    
    - when deleting a reply our "replies left" calculation was getting out of sync because the `count.replies` state on the parent comment wasn't being updated, the result was for each comment deleted we were displaying 1 more reply that was still to load
    - updated the `deleteComment` action to also modify the parent comment's `count.replies` value when a reply was deleted ensuring our "replies left" calculation remains correct

--- apps/comments-ui/src/components/content/RepliesPagination.tsx
+++ apps/comments-ui/src/components/content/RepliesPagination.tsx
@@ -12,7 +12,7 @@ const RepliesPagination: React.FC<Props> = ({loadMore, count}) => {
     const shortText = t('{{amount}} more', {amount: formatNumber(count)});
 
     return (
-        <div className="flex w-full items-center justify-start">
+        <div className="flex w-full items-center justify-start" data-testid="replies-pagination">
             <button className="text-md group mb-10 ml-[48px] flex w-auto items-center px-0 pb-2 pt-0 text-left font-sans font-semibold text-neutral-700 sm:mb-12 dark:text-white " data-testid="reply-pagination-button" type="button" onClick={loadMore}>
                 <span className="flex h-[40px] w-auto items-center justify-center whitespace-nowrap rounded-[6px] bg-black/5 px-4 py-2 text-center font-sans text-sm font-semibold text-neutral-700 outline-0 transition-all duration-150 hover:bg-black/10 dark:bg-white/15 dark:text-neutral-300 dark:hover:bg-white/20 dark:hover:text-neutral-100">↓ <span className="ml-1 hidden sm:inline">{longText}</span><span className="ml-1 inline sm:hidden">{shortText}</span>                </span>
             </button>
