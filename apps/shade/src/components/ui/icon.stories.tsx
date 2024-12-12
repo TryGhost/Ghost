21269cec9d83bda5d09fe51d3e60246b5a624f61
Peter Zimon <peter.zimon@gmail.com>
2024-12-12 11:41:15 +0100

    Experimenting with Lucide icons

--- apps/shade/src/components/ui/icon.stories.tsx
+++ apps/shade/src/components/ui/icon.stories.tsx
@@ -3,7 +3,7 @@ import type {Meta, StoryObj} from '@storybook/react';
 import {useState} from 'react';
 
 const meta = {
-    title: 'Components / Icons',
+    title: 'Components / Streamline icons',
     component: Icon.Close,
     tags: ['autodocs'],
     argTypes: {
