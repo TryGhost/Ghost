d1dbbcadbd78d1834d472ba0f8640da7945fbe84
Michael Barrett <mike@ghost.org>
2024-12-13 13:57:08 +0000

    serve auth-frame from public

--- ghost/core/core/frontend/services/assets-minification/index.js
+++ ghost/core/core/frontend/services/assets-minification/index.js
@@ -1,10 +1,7 @@
-const AdminAuthAssets = require('./AdminAuthAssets');
 const CardAssets = require('./CardAssets');
 
-const adminAuthAssets = new AdminAuthAssets();
 const cardAssets = new CardAssets();
 
 module.exports = {
-    adminAuthAssets,
     cardAssets
 };
