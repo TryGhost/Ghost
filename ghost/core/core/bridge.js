cca25b122c5e88706516e5af95615c9292846ab7
Michael Barrett <mike@ghost.org>
2024-12-13 11:09:04 +0000

    build member-attribution.js at build time

--- ghost/core/core/bridge.js
+++ ghost/core/core/bridge.js
@@ -16,7 +16,7 @@ const logging = require('@tryghost/logging');
 const tpl = require('@tryghost/tpl');
 const themeEngine = require('./frontend/services/theme-engine');
 const appService = require('./frontend/services/apps');
-const {adminAuthAssets, cardAssets, memberAttributionAssets} = require('./frontend/services/assets-minification');
+const {adminAuthAssets, cardAssets} = require('./frontend/services/assets-minification');
 const routerManager = require('./frontend/services/routing').routerManager;
 const settingsCache = require('./shared/settings-cache');
 const urlService = require('./server/services/url');
@@ -73,7 +73,6 @@ class Bridge {
             // TODO: is this in the right place?
             // rebuild asset files
             adminAuthAssets.invalidate();
-            memberAttributionAssets.invalidate();
         } catch (err) {
             logging.error(new errors.InternalServerError({
                 message: tpl(messages.activateFailed, {theme: loadedTheme.name}),
--- ghost/core/core/frontend/services/assets-minification/index.js
+++ ghost/core/core/frontend/services/assets-minification/index.js
@@ -1,13 +1,10 @@
 const AdminAuthAssets = require('./AdminAuthAssets');
 const CardAssets = require('./CardAssets');
-const MemberAttributionAssets = require('./MemberAttributionAssets');
 
 const adminAuthAssets = new AdminAuthAssets();
 const cardAssets = new CardAssets();
-const memberAttributionAssets = new MemberAttributionAssets();
 
 module.exports = {
     adminAuthAssets,
-    cardAssets,
-    memberAttributionAssets
+    cardAssets
 };
