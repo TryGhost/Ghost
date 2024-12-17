d921fc4dee7923cc41cc1bbe980d231ddfd49add
Ronald Langeveld <hi@ronaldlangeveld.com>
2024-12-17 10:26:39 +0700

    Added comment regarding Labs & E2E tests
    ref ENG-1862
    
    - Added information regarding Feature Flag behaviour during E2E tests

--- ghost/core/core/shared/labs.js
+++ ghost/core/core/shared/labs.js
@@ -1,3 +1,10 @@
+// Feature flags behavior in tests:
+// By default, all flags listed in GA_FEATURES, BETA_FEATURES, and ALPHA_FEATURES
+// are globally enabled during E2E tests. This ensures flagged code paths are tested
+// automatically.
+// For more details, see the E2E testing documentation:
+// https://ghost.notion.site/End-to-end-Testing-6a2ef073b1754b18aff42e24a632a007?pvs=74
+
 const _ = require('lodash');
 const errors = require('@tryghost/errors');
 const logging = require('@tryghost/logging');
