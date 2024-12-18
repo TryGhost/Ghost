443e624cf031344fe305323c75045b16322aab1e
Steve Larson <9larsons@gmail.com>
2024-12-18 08:31:36 -0600

    Add sentry tracing for redirects endpoint

--- ghost/core/core/shared/sentry.js
+++ ghost/core/core/shared/sentry.js
@@ -76,7 +76,8 @@ const ALLOWED_HTTP_TRANSACTIONS = [
     '/members/api', // any Members API call
     '/:slug', // any frontend post/page
     '/author', // any frontend author page
-    '/tag' // any frontend tag page
+    '/tag', // any frontend tag page
+    '/r' // any email redirect
 ].map((path) => {
     // Sentry names HTTP transactions like: "<HTTP_METHOD> <PATH>" i.e. "GET /ghost/api/content/settings"
     // Match any of the paths above with any HTTP method, and also the homepage "GET /"
