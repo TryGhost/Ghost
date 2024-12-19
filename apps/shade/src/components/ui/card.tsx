b7787ab11a320dd01effbd4d001a06add0999d20
Peter Zimon <peter.zimon@gmail.com>
2024-12-19 11:42:36 +0100

    Fixed all code formatting bugs in Shade

--- apps/shade/src/components/ui/card.tsx
+++ apps/shade/src/components/ui/card.tsx
@@ -3,8 +3,8 @@ import * as React from 'react';
 import {cn} from '@/lib/utils';
 
 const Card = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+    HTMLDivElement,
+    React.HTMLAttributes<HTMLDivElement>
 >(({className, ...props}, ref) => (
     <div
         ref={ref}
@@ -18,8 +18,8 @@ const Card = React.forwardRef<
 Card.displayName = 'Card';
 
 const CardHeader = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+    HTMLDivElement,
+    React.HTMLAttributes<HTMLDivElement>
 >(({className, ...props}, ref) => (
     <div
         ref={ref}
@@ -30,8 +30,8 @@ const CardHeader = React.forwardRef<
 CardHeader.displayName = 'CardHeader';
 
 const CardTitle = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+    HTMLDivElement,
+    React.HTMLAttributes<HTMLDivElement>
 >(({className, ...props}, ref) => (
     <div
         ref={ref}
@@ -42,8 +42,8 @@ const CardTitle = React.forwardRef<
 CardTitle.displayName = 'CardTitle';
 
 const CardDescription = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+    HTMLDivElement,
+    React.HTMLAttributes<HTMLDivElement>
 >(({className, ...props}, ref) => (
     <div
         ref={ref}
@@ -54,16 +54,16 @@ const CardDescription = React.forwardRef<
 CardDescription.displayName = 'CardDescription';
 
 const CardContent = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+    HTMLDivElement,
+    React.HTMLAttributes<HTMLDivElement>
 >(({className, ...props}, ref) => (
     <div ref={ref} className={cn('p-6 pt-0', className)} {...props} />
 ));
 CardContent.displayName = 'CardContent';
 
 const CardFooter = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
+    HTMLDivElement,
+    React.HTMLAttributes<HTMLDivElement>
 >(({className, ...props}, ref) => (
     <div
         ref={ref}
