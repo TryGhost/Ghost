831c5fe4a86ddb40b77427cfbb41f9ff3e45f4e5
Sag <guptazy@gmail.com>
2024-12-20 04:28:42 +0800

    Updated Portal browser test to be less dependent on exact copy
    no issue
    - this Portal browser test was relying on a 1-1 text match with text generated in Portal
    - this makes changing copy in Portal difficult, as publishing a new version of Portal with copy changes can break browser tests

--- ghost/core/test/e2e-browser/admin/site-settings.spec.js
+++ ghost/core/test/e2e-browser/admin/site-settings.spec.js
@@ -51,7 +51,7 @@ test.describe('Site Settings', () => {
             const portalFrame = sharedPage.frameLocator('#ghost-portal-root div iframe');
 
             // Check sign up is disabled and a message is shown
-            await expect(portalFrame.locator('.gh-portal-invite-only-notification')).toHaveText('This site is invite-only, contact the owner for access.');
+            await expect(portalFrame.locator('.gh-portal-section')).toHaveText(/contact the owner for access/);
 
             // Check free trial message is not shown for invite only
             await expect(portalFrame.locator('.gh-portal-free-trial-notification')).not.toBeVisible();
