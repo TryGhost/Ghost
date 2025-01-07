2cf9470ce5a81e585b000ef0892eb2b79eb55876
Chris Raible <chris@ghost.org>
2025-01-07 12:51:18 -0800

    Removed browser test related changes to include them in a separate commit

--- .docker/development.entrypoint.sh
+++ .docker/development.entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Mounting local code into the container overwrites the `node_modules` directories
+# so we need to install dependencies again
 yarn install --frozen-lockfile --prefer-offline
 
 # Execute the CMD
