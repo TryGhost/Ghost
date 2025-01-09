ccc3eebc8f9fe9a815feb6041d65683c5bcdc496
Princi Vershwal <vershwal.princi@gmail.com>
2025-01-09 17:16:07 +0530

    Running query in parallal

--- ghost/core/core/server/services/members/exporter/query.js
+++ ghost/core/core/server/services/members/exporter/query.js
@@ -21,41 +21,51 @@ module.exports = async function (options) {
         //console.timeEnd('filtering_members');
     }
 
-    //console.time('fetching_members');
+    //console.time('fetching_all_data');
+
+    // Define all queries
     const membersQuery = knex('members').select('id', 'email', 'name', 'note', 'status', 'created_at');
     if (hasFilter) {
         membersQuery.whereIn('id', ids);
     }
-    const members = await membersQuery;
-    //console.timeEnd('fetching_members');
 
-    //console.time('fetching_tiers');
-    const tiers = await knex('members_products')
+    const tiersQuery = knex('members_products')
         .select('member_id', knex.raw('GROUP_CONCAT(DISTINCT product_id) as tiers'))
         .groupBy('member_id');
-    const tiersMap = new Map(tiers.map(row => [row.member_id, row.tiers]));
-    //console.timeEnd('fetching_tiers');
 
-    //console.time('fetching_labels');
-    const labels = await knex('members_labels')
+    const labelsQuery = knex('members_labels')
         .select('member_id', knex.raw('GROUP_CONCAT(DISTINCT label_id) as labels'))
         .groupBy('member_id');
-    const labelsMap = new Map(labels.map(row => [row.member_id, row.labels]));
-    //console.timeEnd('fetching_labels');
 
-    //console.time('fetching_stripe_customers');
-    const stripeCustomers = await knex('members_stripe_customers')
+    const stripeCustomersQuery = knex('members_stripe_customers')
         .select('member_id', knex.raw('MIN(customer_id) as stripe_customer_id'))
         .groupBy('member_id');
-    const stripeCustomerMap = new Map(stripeCustomers.map(row => [row.member_id, row.stripe_customer_id]));
-    //console.timeEnd('fetching_stripe_customers');
 
-    //console.time('fetching_newsletter_subscriptions');
-    const newsletterSubscriptions = await knex('members_newsletters').distinct('member_id');
-    const subscribedSet = new Set(newsletterSubscriptions.map(row => row.member_id));
-    //console.timeEnd('fetching_newsletter_subscriptions');
+    const newsletterSubscriptionsQuery = knex('members_newsletters').distinct('member_id');
+
+    // Run queries in parallel
+    const [
+        members,
+        tiers,
+        labels,
+        stripeCustomers,
+        newsletterSubscriptions
+    ] = await Promise.all([
+        membersQuery,
+        tiersQuery,
+        labelsQuery,
+        stripeCustomersQuery,
+        newsletterSubscriptionsQuery
+    ]);
+
+    //console.timeEnd('fetching_all_data');
 
     //console.time('processing_rows');
+    const tiersMap = new Map(tiers.map(row => [row.member_id, row.tiers]));
+    const labelsMap = new Map(labels.map(row => [row.member_id, row.labels]));
+    const stripeCustomerMap = new Map(stripeCustomers.map(row => [row.member_id, row.stripe_customer_id]));
+    const subscribedSet = new Set(newsletterSubscriptions.map(row => row.member_id));
+
     const csvWriterInstance = csvWriter({
         header: [
             {id: 'id', title: 'ID'},
