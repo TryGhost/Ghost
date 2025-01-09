4c3a731c7e7e7c88d9f08553f010ac8b6c3e009e
Princi Vershwal <vershwal.princi@gmail.com>
2025-01-09 17:46:51 +0530

    Fixed query

--- ghost/core/core/server/services/members/exporter/query.js
+++ ghost/core/core/server/services/members/exporter/query.js
@@ -8,7 +8,6 @@ const csvWriter = require('csv-writer').createObjectCsvStringifier;
 module.exports = async function (options) {
     //console.time('total_export_time');
 
-    // Check if filtering is required
     const hasFilter = options.limit !== 'all' || options.filter || options.search;
     let ids = null;
 
@@ -23,27 +22,7 @@ module.exports = async function (options) {
 
     //console.time('fetching_all_data');
 
-    // Define all queries
-    const membersQuery = knex('members').select('id', 'email', 'name', 'note', 'status', 'created_at');
-    if (hasFilter) {
-        membersQuery.whereIn('id', ids);
-    }
-
-    const tiersQuery = knex('members_products')
-        .select('member_id', knex.raw('GROUP_CONCAT(DISTINCT product_id) as tiers'))
-        .groupBy('member_id');
-
-    const labelsQuery = knex('members_labels')
-        .select('member_id', knex.raw('GROUP_CONCAT(DISTINCT label_id) as labels'))
-        .groupBy('member_id');
-
-    const stripeCustomersQuery = knex('members_stripe_customers')
-        .select('member_id', knex.raw('MIN(customer_id) as stripe_customer_id'))
-        .groupBy('member_id');
-
-    const newsletterSubscriptionsQuery = knex('members_newsletters').distinct('member_id');
-
-    // Run queries in parallel
+    // Fetch data in parallel
     const [
         members,
         tiers,
@@ -51,20 +30,26 @@ module.exports = async function (options) {
         stripeCustomers,
         newsletterSubscriptions
     ] = await Promise.all([
-        membersQuery,
-        tiersQuery,
-        labelsQuery,
-        stripeCustomersQuery,
-        newsletterSubscriptionsQuery
+        knex('members').select('id', 'email', 'name', 'note', 'status', 'created_at').modify((query) => {
+            if (hasFilter) {
+                query.whereIn('id', ids);
+            }
+        }),
+        knex('members_products').select('member_id', knex.raw('GROUP_CONCAT(DISTINCT product_id) as tiers')).groupBy('member_id'),
+        knex('members_labels').select('member_id', knex.raw('GROUP_CONCAT(DISTINCT label_id) as labels')).groupBy('member_id'),
+        knex('members_stripe_customers').select('member_id', knex.raw('MIN(customer_id) as stripe_customer_id')).groupBy('member_id'),
+        knex('members_newsletters').distinct('member_id')
     ]);
 
     //console.timeEnd('fetching_all_data');
 
-    //console.time('processing_rows');
+    // Prepare maps for quick lookup
     const tiersMap = new Map(tiers.map(row => [row.member_id, row.tiers]));
     const labelsMap = new Map(labels.map(row => [row.member_id, row.labels]));
     const stripeCustomerMap = new Map(stripeCustomers.map(row => [row.member_id, row.stripe_customer_id]));
     const subscribedSet = new Set(newsletterSubscriptions.map(row => row.member_id));
+
+    //console.time('processing_batches');
 
     const csvWriterInstance = csvWriter({
         header: [
@@ -83,31 +68,47 @@ module.exports = async function (options) {
     });
 
     let csvContent = csvWriterInstance.getHeaderString();
+    const batchSize = 10000;
 
-    for (const member of members) {
-        const tierIds = tiersMap.get(member.id) ? tiersMap.get(member.id).split(',') : [];
-        const labelIds = labelsMap.get(member.id) ? labelsMap.get(member.id).split(',') : [];
-        const subscribed = subscribedSet.has(member.id);
-        const stripeCustomerId = stripeCustomerMap.get(member.id);
+    // Process members in parallel batches
+    const processBatch = async (batch) => {
+        let batchContent = '';
+        for (const member of batch) {
+            const tierIds = tiersMap.get(member.id) ? tiersMap.get(member.id).split(',') : [];
+            const labelIds = labelsMap.get(member.id) ? labelsMap.get(member.id).split(',') : [];
+            const subscribed = subscribedSet.has(member.id);
+            const stripeCustomerId = stripeCustomerMap.get(member.id);
 
-        const processedRow = {
-            ...member,
-            tiers: tierIds.join(', '),
-            labels: labelIds.join(', '),
-            subscribed,
-            comped: member.status === 'comped',
-            created_at: moment(member.created_at).toISOString(),
-            stripe_customer_id: stripeCustomerId || null
-        };
+            const processedRow = {
+                ...member,
+                tiers: tierIds.join(', '),
+                labels: labelIds.join(', '),
+                subscribed,
+                comped: member.status === 'comped',
+                created_at: moment(member.created_at).toISOString(),
+                stripe_customer_id: stripeCustomerId || null
+            };
 
-        csvContent += csvWriterInstance.stringifyRecords([processedRow]);
+            batchContent += csvWriterInstance.stringifyRecords([processedRow]);
+        }
+        return batchContent;
+    };
+
+    // Split members into batches
+    const batches = [];
+    for (let i = 0; i < members.length; i += batchSize) {
+        batches.push(members.slice(i, i + batchSize));
     }
-    //console.timeEnd('processing_rows');
+
+    const batchResults = await Promise.all(batches.map(processBatch));
+    csvContent += batchResults.join('');
+
+    //console.timeEnd('processing_batches');
 
     //console.time('storing_csv');
     const store = storage.getStorage('files');
-    const imageStoredUrl = await store.saveRaw(csvContent, 'members10hk.csv');
-    const fileUrl = urlUtils.urlFor('files', {file: imageStoredUrl}, true) + 'content/files/members10hk.csv';
+    const imageStoredUrl = await store.saveRaw(csvContent, 'members10h.csv');
+    const fileUrl = urlUtils.urlFor('files', {file: imageStoredUrl}, true) + 'content/files/members10h.csv';
     //console.timeEnd('storing_csv');
 
     //console.timeEnd('total_export_time');
