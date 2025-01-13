c5df848e4d03d6d5ecde29518ed922262c1843e9
Peter Zimon <peter.zimon@gmail.com>
2025-01-13 13:22:47 +0100

    Dark mode update

--- apps/admin-x-design-system/src/settings/SettingNavItem.tsx
+++ apps/admin-x-design-system/src/settings/SettingNavItem.tsx
@@ -22,7 +22,7 @@ const SettingNavItem = forwardRef<HTMLLIElement, SettingNavItemProps>(function S
 }, ref) {
     const classNames = clsx(
         'w-100 mt-px flex h-[36px] cursor-pointer items-center rounded-md px-3 py-2 text-left text-[14px] font-medium transition-all hover:bg-grey-200 focus:bg-grey-100 dark:text-grey-600 dark:hover:bg-grey-950 dark:focus:bg-grey-925',
-        isCurrent ? 'bg-grey-200 text-black dark:bg-grey-925 dark:text-white' : 'text-grey-800',
+        isCurrent ? 'bg-grey-200 text-black dark:bg-grey-950 dark:text-white' : 'text-grey-800',
         !isVisible && 'hidden'
     );
 
--- apps/admin-x-settings/src/MainContent.tsx
+++ apps/admin-x-settings/src/MainContent.tsx
@@ -72,7 +72,7 @@ const MainContent: React.FC = () => {
     return (
         <Page>
             {loadingModal && <div className={`fixed inset-0 z-40 h-[calc(100vh-55px)] w-[100vw] tablet:h-[100vh] ${topLevelBackdropClasses}`} />}
-            <div className="no-scrollbar fixed inset-x-0 top-0 z-[35] flex-1 basis-[320px] bg-white p-8 dark:bg-grey-975 tablet:relative tablet:inset-x-auto tablet:top-auto tablet:h-full tablet:overflow-y-scroll tablet:bg-grey-50 tablet:py-0 dark:tablet:bg-black" id="admin-x-settings-sidebar-scroller">
+            <div className="no-scrollbar fixed inset-x-0 top-0 z-[35] flex-1 basis-[320px] bg-white p-8 dark:bg-grey-975 tablet:relative tablet:inset-x-auto tablet:top-auto tablet:h-full tablet:overflow-y-scroll tablet:bg-grey-50 tablet:py-0 dark:tablet:bg-[#101114]" id="admin-x-settings-sidebar-scroller">
                 <div className="relative w-full">
                     <Sidebar />
                 </div>
