b5cd61974216b03df49b67054cc8ee6853b95553
Peter Zimon <peter.zimon@gmail.com>
2025-01-18 13:31:31 +0100

    Added lists

--- apps/posts/src/views/post-analytics/components/newsletter/SentList.tsx
+++ apps/posts/src/views/post-analytics/components/newsletter/SentList.tsx
@@ -59,7 +59,7 @@ const SentList: React.FC<sentListProps> = () => {
     return (
         <Table>
             <TableHeader>
-                <TableRow className='h-[44px] border-t !border-border'>
+                <TableRow className='h-[44px]'>
                     <TableHead className="w-1/3">Member</TableHead>
                     <TableHead className="w-1/3">Tier</TableHead>
                     <TableHead className="w-1/3">Received</TableHead>
