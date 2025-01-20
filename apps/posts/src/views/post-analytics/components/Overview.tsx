e1f5ff1533e441f63ada2a775f62249d73c19391
Peter Zimon <zimo@ghost.org>
2025-01-20 13:56:21 +0100

    Shade updates (#22025)
    ref
    https://linear.app/ghost/issue/DES-1022/overview-tab-for-post-analytics
    
    - A static version of a React-only Post analytics page needed to be
    worked out to learn how Charts, Tabs, Sidebars etc. work in Shade. This
    also is a basis for learning more about React patterns.

--- apps/posts/src/views/post-analytics/components/Overview.tsx
+++ apps/posts/src/views/post-analytics/components/Overview.tsx
@@ -3,9 +3,11 @@ import Conversions from './overview/Conversions';
 import Feedback from './overview/Feedback';
 import NewsletterPerformance from './overview/NewsletterPerformance';
 
-const Overview = () => {
+interface overviewProps {};
+
+const Overview: React.FC<overviewProps> = () => {
     return (
-        <div className="grid w-full grid-cols-3 gap-6 py-4">
+        <div className="grid w-full grid-cols-3 gap-5 py-5">
             <NewsletterPerformance className='col-span-2' />
             <Feedback />
             <ClickPerformance className='col-span-2' />
