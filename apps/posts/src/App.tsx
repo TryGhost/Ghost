35b835f115056780a3492e42887da228ac8c2d61
Peter Zimon <peter.zimon@gmail.com>
2025-01-21 09:45:12 +0100

    Added basic routing

--- apps/posts/src/App.tsx
+++ apps/posts/src/App.tsx
@@ -1,22 +1,12 @@
-import PostAnalytics from './views/post-analytics/PostAnalytics';
 import {FrameworkProvider, TopLevelFrameworkProps} from '@tryghost/admin-x-framework';
-import {RouterProvider, createHashRouter} from 'react-router';
-// import {RoutingProvider} from '@tryghost/admin-x-framework/routing';
+import {RouterProvider} from 'react-router';
 import {ShadeApp, ShadeAppProps, SidebarProvider} from '@tryghost/shade';
+import {router} from './routes';
 
 interface AppProps {
     framework: TopLevelFrameworkProps;
     designSystem: ShadeAppProps;
 }
-
-// TODO: should be in a routing wrapper
-const basePath = 'posts-x';
-const router = createHashRouter([
-    {
-        path: `${basePath}/analytics/:postId`,
-        element: <PostAnalytics />
-    }
-]);
 
 const App: React.FC<AppProps> = ({framework, designSystem}) => {
     return (
--- apps/posts/src/views/post-analytics/PostAnalytics.tsx
+++ apps/posts/src/views/post-analytics/PostAnalytics.tsx
@@ -1,26 +1,40 @@
 import Header from '../../components/Header';
-import Newsletter from './components/Newsletter';
-import Overview from './components/Overview';
-import {LucideIcon, Page, Tabs, TabsContent, TabsList, TabsTrigger} from '@tryghost/shade';
+import {ANALYTICS} from '../../routes';
+import {LucideIcon, Page, Tabs, TabsList, TabsTrigger} from '@tryghost/shade';
+import {Outlet, useLocation, useNavigate, useParams} from 'react-router';
 
 interface postAnalyticsProps {};
 
 const PostAnalytics: React.FC<postAnalyticsProps> = () => {
+    const navigate = useNavigate();
+    const {postId} = useParams();
+    const location = useLocation();
+    const currentTab = location.pathname.split('/').pop() || 'overview';
+
+    const handleTabChange = (value: string) => {
+        if (value === 'overview') {
+            navigate(`${ANALYTICS}/${postId}`);
+        } else {
+            navigate(`${ANALYTICS}/${postId}/${value}`);
+        }
+    };
+
     return (
         <Page>
             <Header />
-            <Tabs className='my-8 flex grow flex-col' defaultValue="overview" variant="button">
+            <Tabs
+                className='my-8 flex grow flex-col'
+                value={currentTab}
+                variant="button"
+                onValueChange={handleTabChange}
+            >
                 <TabsList className='w-full'>
                     <TabsTrigger value="overview"><LucideIcon.Gauge /> Overview</TabsTrigger>
                     <TabsTrigger value="newsletter"><LucideIcon.Mail /> Newsletter</TabsTrigger>
-                    <TabsTrigger value="web"><LucideIcon.Earth /> Web</TabsTrigger>
                 </TabsList>
-                <TabsContent value="overview">
-                    <Overview />
-                </TabsContent>
-                <TabsContent className='mt-0 flex grow flex-col' value="newsletter">
-                    <Newsletter />
-                </TabsContent>
+                <div>
+                    <Outlet />
+                </div>
             </Tabs>
         </Page>
     );
