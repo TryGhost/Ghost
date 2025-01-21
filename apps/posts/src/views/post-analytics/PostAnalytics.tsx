8d47b005abf901c06001d0af97b92bf146f393f4
Peter Zimon <zimo@ghost.org>
2025-01-21 14:54:02 +0100

    Post analytics prototype router (#22034)
    ref https://linear.app/ghost/issue/DES-1082/router-prototype
    
    - The current router from `admin-x-framework` looks super complex. This
    PR is about testing React Router in the Post analytics prototype.

--- apps/posts/src/views/post-analytics/PostAnalytics.tsx
+++ apps/posts/src/views/post-analytics/PostAnalytics.tsx
@@ -1,26 +1,44 @@
 import Header from '../../components/Header';
-import Newsletter from './components/Newsletter';
-import Overview from './components/Overview';
-import {LucideIcon, Page, Tabs, TabsContent, TabsList, TabsTrigger} from '@tryghost/shade';
+import {ANALYTICS} from '../../routes';
+import {Outlet, useLocation, useNavigate, useParams} from 'react-router';
+import {Page, Tabs, TabsList, TabsTrigger} from '@tryghost/shade';
 
 interface postAnalyticsProps {};
 
 const PostAnalytics: React.FC<postAnalyticsProps> = () => {
+    const navigate = useNavigate();
+    const {postId} = useParams();
+    const location = useLocation();
+
+    let currentTab = location.pathname.split('/').pop();
+    if (currentTab === postId || !currentTab) {
+        currentTab = 'overview';
+    }
+
+    const handleTabChange = (value: string) => {
+        if (value === 'overview') {
+            navigate(`${ANALYTICS}/${postId}`);
+        } else {
+            navigate(`${ANALYTICS}/${postId}/${value}`);
+        }
+    };
+
     return (
         <Page>
             <Header />
-            <Tabs className='my-8 flex grow flex-col' defaultValue="overview" variant="button">
+            <Tabs
+                className='my-8 flex grow flex-col'
+                value={currentTab}
+                variant="underline"
+                onValueChange={handleTabChange}
+            >
                 <TabsList className='w-full'>
-                    <TabsTrigger value="overview"><LucideIcon.Gauge /> Overview</TabsTrigger>
-                    <TabsTrigger value="newsletter"><LucideIcon.Mail /> Newsletter</TabsTrigger>
-                    <TabsTrigger value="web"><LucideIcon.Earth /> Web</TabsTrigger>
+                    <TabsTrigger value="overview">Overview</TabsTrigger>
+                    <TabsTrigger value="newsletter">Newsletter</TabsTrigger>
                 </TabsList>
-                <TabsContent value="overview">
-                    <Overview />
-                </TabsContent>
-                <TabsContent className='mt-0 flex grow flex-col' value="newsletter">
-                    <Newsletter />
-                </TabsContent>
+                <div>
+                    <Outlet />
+                </div>
             </Tabs>
         </Page>
     );
