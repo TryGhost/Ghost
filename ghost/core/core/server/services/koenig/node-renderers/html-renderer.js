cea74d95e01a17aec1d82022f3b26a50fd64fced
Kevin Ansfield <kevin@ghost.org>
2025-01-29 08:39:35 +0000

    Expanded visibility controls to cover web segments (#1424)
    ref https://linear.app/ghost/issue/PLG-327
    
    Updated visibility settings to include toggles for controlling anonymous and free/paid web visibility.
    
    - changed the `visibility` object format
      - old: `{showOnWeb: false, showOnEmail: true, memberSegment: 'status:free'}`
      - new: `{web: {nonMembers: false, memberSegment: ''}, email: {memberSegment: 'status:free'}}`
    - added utility for migrating the old format to the new
    - added automatic migration of format in HTML node's `importJSON` method so within the editor we're always dealing with the new format
    - updated HTML node rendering for email to work with the new visibility format
    - updated HTML node rendering for web to output gated block comments
      - e.g.: `<!--kg-gated-block:begin nonMembers:false memberSegment:'status:free'-->...<!--kg-gated-blog:end-->`
    - added utility functions for parsing/serializing the `visibility` object to structures that are more useful when working with UI
    - updated existing beta UI to work with the new visibility format
    - added alpha UI with the additional toggles
    - added `contentVisibilityAlpha` flag support to demo app (disabled by default)

--- ghost/core/core/server/services/koenig/node-renderers/html-renderer.js
+++ ghost/core/core/server/services/koenig/node-renderers/html-renderer.js
@@ -1,5 +1,6 @@
 import {addCreateDocumentOption} from '../../utils/add-create-document-option';
 import {renderEmptyContainer} from '../../utils/render-empty-container';
+import {ALL_MEMBERS_SEGMENT, usesOldVisibilityFormat, migrateOldVisibilityFormat, NO_MEMBERS_SEGMENT} from '../../utils/visibility';
 
 export function renderHtmlNode(node, options = {}) {
     addCreateDocumentOption(options);
@@ -16,64 +17,43 @@ export function renderHtmlNode(node, options = {}) {
     const textarea = document.createElement('textarea');
     textarea.value = wrappedHtml;
 
-    if (!options.feature?.contentVisibility) {
+    if (!options.feature?.contentVisibility || !node.visibility) {
         // `type: 'value'` will render the value of the textarea element
         return {element: textarea, type: 'value'};
     }
 
-    const segment = node.visibility.segment || '';
+    const visibility = node.visibility;
+    const oldVisibilityFormat = usesOldVisibilityFormat(visibility);
 
-    const showOnEmail = node.visibility.showOnEmail;
-    const showOnWeb = node.visibility.showOnWeb;
-
-    if (segment) {
-        textarea.setAttribute('data-gh-segment', segment);
+    if (oldVisibilityFormat) {
+        migrateOldVisibilityFormat(visibility);
     }
 
-    const isEmailOnly = !showOnWeb && showOnEmail;
-    const isWebOnly = showOnWeb && !showOnEmail;
-    const showOnWebAndEmail = showOnEmail && showOnWeb;
-    const showNowhere = !showOnEmail && !showOnWeb;
+    if (options.target === 'email') {
+        if (visibility.email.memberSegment === NO_MEMBERS_SEGMENT) {
+            return renderEmptyContainer(document);
+        }
 
-    if (showNowhere) {
-        return renderEmptyContainer(document);
-    }
+        if (visibility.email.memberSegment === ALL_MEMBERS_SEGMENT) {
+            return {element: textarea, type: 'value'};
+        }
 
-    if (isWebOnly && options.target !== 'email') {
-        return {element: textarea, type: 'value'};
-    }
-
-    if (isWebOnly && options.target === 'email') {
-        return renderEmptyContainer(document);
-    }
-
-    if (isEmailOnly && options.target !== 'email') {
-        return renderEmptyContainer(document);
-    }
-
-    if (isEmailOnly && options.target === 'email') {
         const container = document.createElement('div');
         container.innerHTML = wrappedHtml;
-        if (segment) {
-            container.setAttribute('data-gh-segment', segment);
-        }
+        container.setAttribute('data-gh-segment', visibility.email.memberSegment);
         return {element: container, type: 'html'};
     }
 
-    if (isWebOnly && options.target === 'email') {
+    if (visibility.web.nonMember === false && visibility.web.memberSegment === NO_MEMBERS_SEGMENT) {
         return renderEmptyContainer(document);
     }
 
-    if (showOnWebAndEmail) {
-        if (options.target === 'email') {
-            const container = document.createElement('div');
-            container.innerHTML = wrappedHtml;
-            if (segment) {
-                container.setAttribute('data-gh-segment', segment);
-            }
-            return {element: container, type: 'html'};
-        }
-
-        return {element: textarea, type: 'value'};
+    // If there are restrictions on web visibility, wrap the HTML in a gated block comment
+    if (visibility.web.nonMember !== true || visibility.web.memberSegment !== ALL_MEMBERS_SEGMENT) {
+        const {nonMember, memberSegment} = visibility.web;
+        const visibilityWrappedHtml = `\n<!--kg-gated-block:begin nonMember:${nonMember} memberSegment:"${memberSegment}" -->${textarea.value}<!--kg-gated-block:end-->\n`;
+        textarea.value = visibilityWrappedHtml;
     }
+
+    return {element: textarea, type: 'value'};
 }
