e63d6e24e80bd4d52a95fe1fdf7e62477f77faa1
Daniel Lockyer <hi@daniellockyer.com>
2025-01-30 16:51:28 +0100

    Removed Lodash from several code hotpaths
    - only some small micro-optimizations found whilst browing CPU profiles
    - we can remove lodash from a sanitize plugin method (needs an empty object
      fallback because Object.keys is less forgiving than _.keys and there are some
      tests which don't fully initialize the model)
    - also converts the data-manipulation plugins into for-loops because
      there are more native than using lodash, and it means we can remove
      lodash from this file too
    - lodash is a bit of a perf burn on extreme hotpaths, so it's good to
      clean it up where it's not needed, and native JS is often way faster
    - adds a bit of JSDoc to help with editor autocomplete

--- ghost/core/core/server/api/endpoints/utils/serializers/output/utils/extra-attrs.js
+++ ghost/core/core/server/api/endpoints/utils/serializers/output/utils/extra-attrs.js
@@ -8,11 +8,10 @@ const readingMinutes = require('@tryghost/helpers').utils.readingMinutes;
  * @returns {void} - modifies attrs
  */
 module.exports.forPost = (options, model, attrs) => {
-    const _ = require('lodash');
     // This function is split up in 3 conditions for 3 different purposes:
     // 1. Gets excerpt from post's plaintext. If custom_excerpt exists, it overrides the excerpt but the key remains excerpt.
-    if (Object.prototype.hasOwnProperty.call(options, 'columns') || _.includes(options.columns, 'excerpt') || _.includes(options.columns, 'excerpt') && options.formats && options.formats.includes('plaintext')) {
-        if (_.includes(options.columns, 'excerpt')) {
+    if (Object.prototype.hasOwnProperty.call(options, 'columns') || options.columns?.includes('excerpt') || (options.columns?.includes('excerpt') && options.formats?.includes('plaintext'))) {
+        if (options.columns?.includes('excerpt')) {
             if (!attrs.custom_excerpt || attrs.custom_excerpt === null) {
                 let plaintext = model.get('plaintext');
                 if (plaintext) {
@@ -25,7 +24,7 @@ module.exports.forPost = (options, model, attrs) => {
                 }
             } else {
                 attrs.excerpt = attrs.custom_excerpt;
-                if (!_.includes(options.columns, 'custom_excerpt')) {
+                if (!options.columns?.includes('custom_excerpt')) {
                     delete attrs.custom_excerpt;
                 }
             }
@@ -33,7 +32,7 @@ module.exports.forPost = (options, model, attrs) => {
     }
 
     // 2. Displays plaintext if requested by user as a field. Also works if used as format.
-    if (_.includes(options.columns, 'plaintext') || options.formats && options.formats.includes('plaintext')) {
+    if (options.columns?.includes('plaintext') || options.formats?.includes('plaintext')) {
         let plaintext = model.get('plaintext');
         if (plaintext) {
             attrs.plaintext = plaintext;
@@ -62,7 +61,7 @@ module.exports.forPost = (options, model, attrs) => {
     // reading_time still only works when used along with formats=html.
 
     if (!Object.prototype.hasOwnProperty.call(options, 'columns') ||
-        (options.columns.includes('reading_time'))) {
+        (options.columns?.includes('reading_time'))) {
         if (attrs.html) {
             let additionalImages = 0;
 
