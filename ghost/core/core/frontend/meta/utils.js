5700df9dcc722b555ffb36b56d2aac4b3dd277fe
Cathy Sarisky <catsarisky@gmail.com>
2025-01-31 10:25:38 -0500

    housekeeping

--- ghost/core/core/frontend/meta/utils.js
+++ ghost/core/core/frontend/meta/utils.js
@@ -1,15 +1,5 @@
 const security = require('@tryghost/security');
 const debug = require('@tryghost/debug')('frontend');
-const path = require('path');
-
-function isAbsoluteUrl(url) {
-    try {
-        new URL(url);
-        return true;
-    } catch {
-        return false;
-    }
-}
 
 function fixAnyNonStandardChars(pathOrUrl) {
     let path = pathOrUrl;
