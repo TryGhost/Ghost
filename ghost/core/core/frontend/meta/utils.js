63db8040f961c98895f657e9069f1314f2f3badb
Cathy Sarisky <catsarisky@gmail.com>
2025-01-31 10:19:52 -0500

    deal with absolute urls too!

--- ghost/core/core/frontend/meta/utils.js
+++ ghost/core/core/frontend/meta/utils.js
@@ -1,10 +1,26 @@
 const security = require('@tryghost/security');
 const debug = require('@tryghost/debug')('frontend');
+const path = require('path');
 
-function fixAnyNonStandardChars(path) {
-    let returnString = path;
+function isAbsoluteUrl(url) {
     try {
-        returnString = decodeURIComponent(path)
+        new URL(url);
+        return true;
+    } catch {
+        return false;
+    }
+}
+
+function fixAnyNonStandardChars(pathOrUrl) {
+    let path = pathOrUrl;
+    try { path = new URL(pathOrUrl).pathname; } catch {}
+    let origin = '';
+    try { origin = new URL(pathOrUrl).origin; } catch {}
+
+    let returnString = pathOrUrl;
+
+    try {
+        returnString = origin + decodeURIComponent(path)
             .split('/')
             .map (part => security.string.safe(part))
             .join('/');
