c25f6648b99eca575295b8820e004e564744d6da
Kevin Ansfield <kevin@lookingsideways.co.uk>
2025-02-03 17:07:47 +0000

    Extracted HTML card visibility rendering to utility function
    ref https://linear.app/ghost/issue/PLG-332
    
    - extracted visibility rendering logic and adapted to be more generalised for use in other cards

--- ghost/core/core/server/services/koenig/node-renderers/html-renderer.js
+++ ghost/core/core/server/services/koenig/node-renderers/html-renderer.js
@@ -1,6 +1,6 @@
 import {addCreateDocumentOption} from '../../utils/add-create-document-option';
 import {renderEmptyContainer} from '../../utils/render-empty-container';
-import {ALL_MEMBERS_SEGMENT, usesOldVisibilityFormat, migrateOldVisibilityFormat, NO_MEMBERS_SEGMENT} from '../../utils/visibility';
+import {renderWithVisibility} from '../../utils/visibility';
 
 export function renderHtmlNode(node, options = {}) {
     addCreateDocumentOption(options);
@@ -17,43 +17,11 @@ export function renderHtmlNode(node, options = {}) {
     const textarea = document.createElement('textarea');
     textarea.value = wrappedHtml;
 
-    if (!options.feature?.contentVisibility || !node.visibility) {
-        // `type: 'value'` will render the value of the textarea element
-        return {element: textarea, type: 'value'};
+    if (options.feature?.contentVisibility || node.visibility) {
+        const renderOutput = {element: textarea, type: 'value'};
+        return renderWithVisibility(renderOutput, node.visibility, options);
     }
 
-    const visibility = node.visibility;
-    const oldVisibilityFormat = usesOldVisibilityFormat(visibility);
-
-    if (oldVisibilityFormat) {
-        migrateOldVisibilityFormat(visibility);
-    }
-
-    if (options.target === 'email') {
-        if (visibility.email.memberSegment === NO_MEMBERS_SEGMENT) {
-            return renderEmptyContainer(document);
-        }
-
-        if (visibility.email.memberSegment === ALL_MEMBERS_SEGMENT) {
-            return {element: textarea, type: 'value'};
-        }
-
-        const container = document.createElement('div');
-        container.innerHTML = wrappedHtml;
-        container.setAttribute('data-gh-segment', visibility.email.memberSegment);
-        return {element: container, type: 'html'};
-    }
-
-    if (visibility.web.nonMember === false && visibility.web.memberSegment === NO_MEMBERS_SEGMENT) {
-        return renderEmptyContainer(document);
-    }
-
-    // If there are restrictions on web visibility, wrap the HTML in a gated block comment
-    if (visibility.web.nonMember !== true || visibility.web.memberSegment !== ALL_MEMBERS_SEGMENT) {
-        const {nonMember, memberSegment} = visibility.web;
-        const visibilityWrappedHtml = `\n<!--kg-gated-block:begin nonMember:${nonMember} memberSegment:"${memberSegment}" -->${textarea.value}<!--kg-gated-block:end-->\n`;
-        textarea.value = visibilityWrappedHtml;
-    }
-
+    // `type: 'value'` will render the value of the textarea element
     return {element: textarea, type: 'value'};
 }
