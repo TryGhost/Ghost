009fcec34ede254daf0c574d38950f544dc96caf
Kevin Ansfield <kevin@lookingsideways.co.uk>
2025-02-03 17:07:47 +0000

    Extracted HTML card visibility rendering to utility function
    ref https://linear.app/ghost/issue/PLG-332
    
    - extracted visibility rendering logic and adapted to be more generalised for use in other cards

--- koenig/kg-default-nodes/lib/utils/visibility.js
+++ koenig/kg-default-nodes/lib/utils/visibility.js
@@ -1,3 +1,5 @@
+import {renderEmptyContainer} from './render-empty-container';
+
 export const ALL_MEMBERS_SEGMENT = 'status:free,status:-free';
 export const NO_MEMBERS_SEGMENT = '';
 
@@ -33,4 +35,74 @@ export function migrateOldVisibilityFormat(visibility) {
     } else {
         visibility.email.memberSegment = NO_MEMBERS_SEGMENT;
     }
+}
+
+export function renderWithVisibility(originalRenderOutput, visibility, options) {
+    const document = originalRenderOutput.element.ownerDocument;
+    const content = _getRenderContent(originalRenderOutput);
+
+    if (usesOldVisibilityFormat(visibility)) {
+        migrateOldVisibilityFormat(visibility);
+    }
+
+    if (options.target === 'email') {
+        if (visibility.email.memberSegment === NO_MEMBERS_SEGMENT) {
+            return renderEmptyContainer(document);
+        }
+
+        if (visibility.email.memberSegment === ALL_MEMBERS_SEGMENT) {
+            return originalRenderOutput;
+        }
+
+        return _renderWithEmailVisibility(document, content, visibility.email);
+    }
+
+    const isNotVisibleOnWeb =
+        visibility.web.nonMember === false &&
+        visibility.web.memberSegment === NO_MEMBERS_SEGMENT;
+
+    if (isNotVisibleOnWeb) {
+        return renderEmptyContainer(document);
+    }
+
+    const hasWebVisibilityRestrictions =
+        visibility.web.nonMember !== true ||
+        visibility.web.memberSegment !== ALL_MEMBERS_SEGMENT;
+
+    if (hasWebVisibilityRestrictions) {
+        return _renderWithWebVisibility(document, content, visibility.web);
+    }
+
+    return originalRenderOutput;
+}
+
+/* Private functions -------------------------------------------------------- */
+
+function _getRenderContent({element, type}) {
+    if (type === 'inner') {
+        return element.innerHTML;
+    } else if (type === 'value') {
+        if ('value' in element) {
+            return element.value;
+        }
+        return '';
+    } else {
+        return element.outerHTML;
+    }
+}
+
+function _renderWithEmailVisibility(document, content, emailVisibility) {
+    const {memberSegment} = emailVisibility;
+    const container = document.createElement('div');
+    container.innerHTML = content;
+    container.setAttribute('data-gh-segment', memberSegment);
+    return {element: container, type: 'html'};
+}
+
+function _renderWithWebVisibility(document, content, webVisibility) {
+    const {nonMember, memberSegment} = webVisibility;
+    const wrappedContent = `\n<!--kg-gated-block:begin nonMember:${nonMember} memberSegment:"${memberSegment}" -->${content}<!--kg-gated-block:end-->\n`;
+    const textarea = document.createElement('textarea');
+    textarea.value = wrappedContent;
+    return {element: textarea, type: 'value'};
 }
