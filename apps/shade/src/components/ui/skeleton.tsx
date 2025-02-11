939f181e8c63ebae74ebe55fdeb3b1869bce3e92
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-02-11 15:06:53 +0800

    Added custom props to shadcn skeleton component

--- apps/shade/src/components/ui/skeleton.tsx
+++ apps/shade/src/components/ui/skeleton.tsx
@@ -1,14 +1,29 @@
 import {cn} from '@/lib/utils';
 
+interface SkeletonProps extends React.HTMLAttributes<HTMLSpanElement> {
+    containerClassName?: string;
+    count?: number;
+}
+
 function Skeleton({
+    containerClassName,
+    count = 1,
     className,
     ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+}: SkeletonProps) {
     return (
-        <div
-            className={cn('animate-pulse rounded-md bg-primary/10', className)}
-            {...props}
-        />
+        <span className={containerClassName}>
+            {Array.from({length: count}).map(() => (
+                <>
+                    <span
+                        key={`skeleton-${crypto.randomUUID()}`}
+                        className={cn('inline-flex w-full leading-none animate-pulse rounded-[2px] bg-primary/10', className)}
+                        {...props}
+                    >&zwnj;</span>
+                    <br />
+                </>
+            ))}
+        </span>
     );
 }
 
