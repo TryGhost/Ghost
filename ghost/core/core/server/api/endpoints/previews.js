c7438b9219fa0e3a50ab90631944751efb1e8d84
Kevin Ansfield <kevin@lookingsideways.co.uk>
2025-02-11 18:55:05 +0000

    Added preview-as anonymous/free/paid to previews endpoint
    ref https://linear.app/ghost/issue/PLG-352
    
    - updated `/p/` endpoint to accept an optional `?memberStatus` query parameter with three allowed values: `anonymous/free/paid`
    - when a `memberStatus` query param is provided:
      - `frame.apiType` is set to `content` which triggers the content-gating paths of our output serializer's post mapper
      - where needed, `frame.original.content.member` is set to a minimal member object with a `status` property to allow content-gating NQL queries to match
      - the combination of both changes our API output to match what the Content API does when rendering posts with a particular logged-in member status
    - if no `memberStatus` query param is provided the existing behaviour is preserved of rendering as we do for Admin API requests, showing content for all segments simultaneously

--- ghost/core/core/server/api/endpoints/previews.js
+++ ghost/core/core/server/api/endpoints/previews.js
@@ -2,9 +2,37 @@ const tpl = require('@tryghost/tpl');
 const errors = require('@tryghost/errors');
 const models = require('../../models');
 const ALLOWED_INCLUDES = ['authors', 'tags'];
+const ALLOWED_MEMBER_STATUSES = ['anonymous', 'free', 'paid'];
 
 const messages = {
     postNotFound: 'Post not found.'
+};
+
+// Simulate serving content as different member states by setting the minimal
+// member context needed for content gating to function
+const _addMemberContextToFrame = (frame) => {
+    if (!frame?.options?.memberStatus) {
+        return;
+    }
+
+    // only set apiType when given a memberStatus to preserve backwards compatibility
+    // where we used to serve "Admin API" content with no gating for all previews
+    frame.apiType = 'content';
+
+    frame.original ??= {};
+    frame.original.context ??= {};
+
+    if (frame.options?.memberStatus === 'free') {
+        frame.original.context.member = {
+            status: 'free'
+        };
+    }
+
+    if (frame.options?.memberStatus === 'paid') {
+        frame.original.context.member = {
+            status: 'paid'
+        };
+    }
 };
 
 /** @type {import('@tryghost/api-framework').Controller} */
@@ -17,7 +45,8 @@ const controller = {
         },
         permissions: true,
         options: [
-            'include'
+            'include',
+            'memberStatus'
         ],
         data: [
             'uuid'
@@ -26,6 +55,9 @@ const controller = {
             options: {
                 include: {
                     values: ALLOWED_INCLUDES
+                },
+                memberStatus: {
+                    values: ALLOWED_MEMBER_STATUSES
                 }
             },
             data: {
@@ -35,6 +67,8 @@ const controller = {
             }
         },
         query(frame) {
+            _addMemberContextToFrame(frame);
+
             return models.Post.findOne(Object.assign({status: 'all'}, frame.data), frame.options)
                 .then((model) => {
                     if (!model) {
