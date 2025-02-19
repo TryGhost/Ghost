d984140f6b1feed9e9767afd3be6b79013965bbd
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-02-19 14:29:46 +0800

    Added dark mode support to ActivityPub (#22228)
    ref AP-755

--- apps/admin-x-activitypub/src/components/navigation/MainHeader.tsx
+++ apps/admin-x-activitypub/src/components/navigation/MainHeader.tsx
@@ -6,7 +6,7 @@ interface MainHeaderProps {
 
 const MainHeader: React.FC<MainHeaderProps> = ({children}) => {
     return (
-        <div className='sticky top-0 z-50 border-b border-gray-200 bg-white py-8'>
+        <div className='sticky top-0 z-50 border-b border-gray-200 bg-white py-8 dark:border-gray-950 dark:bg-black'>
             <div className='grid h-8 grid-cols-3'>
                 {children}
             </div>
