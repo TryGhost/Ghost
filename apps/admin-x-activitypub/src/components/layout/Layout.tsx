a6cacf105fee41552f71e95efc454643d2782a15
Peter Zimon <zimo@ghost.org>
2025-02-26 22:09:05 +0100

    Loading message in ActivityPub notifications (#22288)
    closes AP-783
    
    - Notifications load quite slow in ActivityPub. We're working on
    improving it but until it gets faster we'll display a short message
    after 3 seconds of loading to reassure users that the app is not broken.

--- apps/admin-x-activitypub/src/components/layout/Layout.tsx
+++ apps/admin-x-activitypub/src/components/layout/Layout.tsx
@@ -4,7 +4,7 @@ import {Header} from './Header';
 
 const Layout: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({children, ...props}) => {
     return (
-        <div className='mx-auto flex h-screen w-full max-w-page flex-col overflow-y-auto' {...props}>
+        <div className='relative mx-auto flex h-screen w-full max-w-page flex-col overflow-y-auto' {...props}>
             <Header />
             <div className='grid grid-cols-[auto_292px] items-start gap-8 px-8'>
                 <div className='z-0'>
