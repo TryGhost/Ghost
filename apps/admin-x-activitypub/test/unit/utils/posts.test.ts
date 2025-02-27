0893db466b3ff3b37148857879873b6ac6b41a68
Princi Vershwal <vershwal.princi@gmail.com>
2025-02-27 17:17:18 +0530

    Updated mapPostToActivity to add attachments (#22296)
    refs
    https://linear.app/ghost/issue/AP-795/update-feed-service-to-fetch-attachments-for-posts
    
    - Mapped the posts from new feeds API to UI so the we can show the attachments on the feed's UI
    - Added tests to verify the mapping

--- apps/admin-x-activitypub/test/unit/utils/posts.test.ts
+++ apps/admin-x-activitypub/test/unit/utils/posts.test.ts
@@ -18,7 +18,20 @@ describe('mapPostToActivity', function () {
             likedByMe: true,
             replyCount: 3,
             readingTimeMinutes: 4,
-            attachments: [],
+            attachments: [
+                {
+                    type: 'Image',
+                    mediaType: 'image/jpeg',
+                    name: 'test.jpg',
+                    url: 'https://example.com/test.jpg'
+                },
+                {
+                    type: 'Image',
+                    mediaType: 'image/jpeg',
+                    name: 'test1.jpg',
+                    url: 'https://example.com/test1.jpg'
+                }
+            ],
             author: {
                 id: 'https://example.com/users/123',
                 handle: '@testuser@example.com',
@@ -105,5 +118,23 @@ describe('mapPostToActivity', function () {
         expect(object.liked).toBe(true);
         expect(object.reposted).toBe(false);
         expect(object.repostCount).toBe(5);
+    });
+
+    test('it sets the correct attachments', function () {
+        const object = mapPostToActivity(post).object;
+
+        expect(object.attachment).toHaveLength(2);
+        expect(object.attachment[0]).toEqual({
+            type: 'Image',
+            mediaType: 'image/jpeg',
+            name: 'test.jpg',
+            url: 'https://example.com/test.jpg'
+        });
+        expect(object.attachment[1]).toEqual({
+            type: 'Image',
+            mediaType: 'image/jpeg',
+            name: 'test1.jpg',
+            url: 'https://example.com/test1.jpg'
+        });
     });
 });
