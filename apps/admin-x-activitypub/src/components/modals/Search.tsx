ec99fd037de107c59370debfe659aa82e6cd3d9f
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-03-03 16:21:53 +0800

    Refined ActivityPub search (#22297)
    ref AP-769
    
    - displays the search in a modal when the search input is clicked
    - uses the Dialog component from Shade

--- apps/admin-x-activitypub/src/components/modals/Search.tsx
+++ apps/admin-x-activitypub/src/components/modals/Search.tsx
@@ -1,18 +1,13 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {Skeleton} from '@tryghost/shade';
-
-import NiceModal from '@ebay/nice-modal-react';
-import {Button, Icon, LoadingIndicator, NoValueLabel, TextField} from '@tryghost/admin-x-design-system';
-import {useDebounce} from 'use-debounce';
-
 import APAvatar from '@components/global/APAvatar';
 import ActivityItem from '@components/activities/ActivityItem';
 import FollowButton from '@components/global/FollowButton';
-import Separator from '@components/global/Separator';
+import NiceModal from '@ebay/nice-modal-react';
+import React, {useEffect, useRef} from 'react';
 import ViewProfileModal from '@components/modals/ViewProfileModal';
-
-import Layout from '@components/layout';
+import {H4, LucideIcon, Skeleton} from '@tryghost/shade';
+import {LoadingIndicator, NoValueLabel, TextField} from '@tryghost/admin-x-design-system';
 import {type Profile} from '../../api/activitypub';
+import {useDebounce} from 'use-debounce';
 import {useSearchForUser, useSuggestedProfilesForUser} from '@hooks/use-activity-pub-queries';
 
 interface AccountSearchResult {
@@ -29,7 +24,9 @@ interface AccountSearchResultItemProps {
     update: (id: string, updated: Partial<AccountSearchResult>) => void;
 }
 
-const AccountSearchResultItem: React.FC<AccountSearchResultItemProps> = ({account, update}) => {
+const AccountSearchResultItem: React.FC<AccountSearchResultItemProps & {
+    onOpenChange?: (open: boolean) => void;
+}> = ({account, update, onOpenChange}) => {
     const onFollow = () => {
         update(account.id, {
             followedByMe: true,
@@ -48,6 +45,7 @@ const AccountSearchResultItem: React.FC<AccountSearchResultItemProps> = ({accoun
         <ActivityItem
             key={account.id}
             onClick={() => {
+                onOpenChange?.(false);
                 NiceModal.show(ViewProfileModal, {handle: account.handle, onFollow, onUnfollow});
             }}
         >
@@ -59,8 +57,8 @@ const AccountSearchResultItem: React.FC<AccountSearchResultItemProps> = ({accoun
                 handle: account.handle
             }}/>
             <div className='flex flex-col'>
-                <span className='font-semibold text-black'>{account.name}</span>
-                <span className='text-sm text-gray-700'>{account.handle}</span>
+                <span className='font-semibold text-black dark:text-white'>{account.name}</span>
+                <span className='text-sm text-gray-700 dark:text-gray-600'>{account.handle}</span>
             </div>
             <FollowButton
                 className='ml-auto'
@@ -79,17 +77,24 @@ interface SearchResultsProps {
     onUpdate: (id: string, updated: Partial<AccountSearchResult>) => void;
 }
 
-const SearchResults: React.FC<SearchResultsProps> = ({results, onUpdate}) => {
+const SearchResults: React.FC<SearchResultsProps & {
+    onOpenChange?: (open: boolean) => void;
+}> = ({results, onUpdate, onOpenChange}) => {
+    if (!results.length) {
+        return null;
+    }
+
     return (
-        <>
+        <div className='mt-[-7px]'>
             {results.map(account => (
                 <AccountSearchResultItem
                     key={account.id}
                     account={account}
                     update={onUpdate}
+                    onOpenChange={onOpenChange}
                 />
             ))}
-        </>
+        </div>
     );
 };
 
@@ -99,7 +104,9 @@ interface SuggestedProfileProps {
     isLoading: boolean;
 }
 
-const SuggestedProfile: React.FC<SuggestedProfileProps> = ({profile, update, isLoading}) => {
+const SuggestedProfile: React.FC<SuggestedProfileProps & {
+    onOpenChange?: (open: boolean) => void;
+}> = ({profile, update, isLoading, onOpenChange}) => {
     const onFollow = () => {
         update(profile.actor.id, {
             isFollowing: true,
@@ -118,6 +125,7 @@ const SuggestedProfile: React.FC<SuggestedProfileProps> = ({profile, update, isL
         <ActivityItem
             key={profile.actor.id}
             onClick={() => {
+                onOpenChange?.(false);
                 NiceModal.show(ViewProfileModal, {handle: profile.handle, onFollow, onUnfollow});
             }}
         >
@@ -149,31 +157,37 @@ interface SuggestedProfilesProps {
     onUpdate: (id: string, updated: Partial<Profile>) => void;
 }
 
-const SuggestedProfiles: React.FC<SuggestedProfilesProps> = ({profiles, isLoading, onUpdate}) => {
+const SuggestedProfiles: React.FC<SuggestedProfilesProps & {
+    onOpenChange?: (open: boolean) => void;
+}> = ({profiles, isLoading, onUpdate, onOpenChange}) => {
     return (
-        <>
-            <span className='mb-1 flex w-full max-w-[620px] font-semibold'>
-                Suggested accounts
-            </span>
-            {profiles.map((profile, index) => {
-                return (
-                    <React.Fragment key={profile.actor.id}>
-                        <SuggestedProfile
-                            isLoading={isLoading}
-                            profile={profile}
-                            update={onUpdate}
-                        />
-                        {index < profiles.length - 1 && <Separator />}
-                    </React.Fragment>
-                );
-            })}
-        </>
+        <div className='mb-[-15px] flex flex-col gap-2 pt-2'>
+            <H4>Suggested accounts</H4>
+            <div className='flex flex-col'>
+                {profiles.map((profile) => {
+                    return (
+                        <React.Fragment key={profile.actor.id}>
+                            <SuggestedProfile
+                                isLoading={isLoading}
+                                profile={profile}
+                                update={onUpdate}
+                                onOpenChange={onOpenChange}
+                            />
+                        </React.Fragment>
+                    );
+                })}
+            </div>
+        </div>
     );
 };
 
-interface SearchProps {}
+interface SearchProps {
+    onOpenChange?: (open: boolean) => void;
+    query: string;
+    setQuery: (query: string) => void;
+}
 
-const Search: React.FC<SearchProps> = ({}) => {
+const Search: React.FC<SearchProps> = ({onOpenChange, query, setQuery}) => {
     // Initialise suggested profiles
     const {suggestedProfilesQuery, updateSuggestedProfile} = useSuggestedProfilesForUser('index', 6);
     const {data: suggestedProfilesData, isLoading: isLoadingSuggestedProfiles} = suggestedProfilesQuery;
@@ -187,7 +201,6 @@ const Search: React.FC<SearchProps> = ({}) => {
 
     // Initialise search query
     const queryInputRef = useRef<HTMLInputElement>(null);
-    const [query, setQuery] = useState('');
     const [debouncedQuery] = useDebounce(query, 300);
     const {searchQuery, updateAccountSearchResult: updateResult} = useSearchForUser('index', query !== '' ? debouncedQuery : query);
     const {data, isFetching, isFetched} = searchQuery;
@@ -195,7 +208,7 @@ const Search: React.FC<SearchProps> = ({}) => {
     const results = data?.accounts || [];
     const showLoading = isFetching && query.length > 0;
     const showNoResults = !isFetching && isFetched && results.length === 0 && query.length > 0 && debouncedQuery === query;
-    const showSuggested = query === '' || (isFetched && results.length === 0);
+    const showSuggested = query === '';
 
     // Focus the query input on initial render
     useEffect(() => {
@@ -205,62 +218,54 @@ const Search: React.FC<SearchProps> = ({}) => {
     }, []);
 
     return (
-        <Layout>
-            <div className='z-0 mx-auto flex w-full max-w-[560px] flex-col items-center pt-8'>
-                <div className='relative flex w-full items-center'>
-                    <Icon className='absolute left-3 top-3 z-10' colorClass='text-gray-500' name='magnifying-glass' size='sm' />
-                    <TextField
-                        autoComplete='off'
-                        className='mb-6 mr-12 flex h-10 w-full items-center rounded-lg border border-transparent bg-gray-100 px-[33px] py-1.5 transition-colors focus:border-green focus:bg-white focus:outline-2 tablet:mr-0 dark:border-transparent dark:bg-gray-925 dark:text-white dark:placeholder:text-gray-800 dark:focus:border-green dark:focus:bg-gray-950'
-                        containerClassName='w-100'
-                        inputRef={queryInputRef}
-                        placeholder='Enter a handle or account URL...'
-                        title="Search"
-                        type='text'
-                        value={query}
-                        clearBg
-                        hideTitle
-                        unstyled
-                        onChange={e => setQuery(e.target.value)}
-                    />
-                    {query && (
-                        <Button
-                            className='absolute top-3 p-1 sm:right-14 tablet:right-3'
-                            icon='close'
-                            iconColorClass='text-gray-700 !w-[10px] !h-[10px]'
-                            size='sm'
-                            unstyled
-                            onClick={() => {
-                                setQuery('');
-                                queryInputRef.current?.focus();
-                            }}
-                        />
-                    )}
-                </div>
-                {showLoading && <LoadingIndicator size='lg'/>}
-
-                {showNoResults && (
-                    <NoValueLabel icon='user'>
-                        No users matching this handle or account URL
-                    </NoValueLabel>
+        <>
+            <div className='-mx-6 -mt-6 flex items-center gap-2 border-b border-b-gray-150 px-6 pb-[10px] pt-3 dark:border-b-gray-950'>
+                <LucideIcon.Search className='text-gray-600' size={18} strokeWidth={1.5} />
+                <TextField
+                    autoComplete='off'
+                    className='mr-12 flex h-10 w-full items-center rounded-lg border-0 bg-transparent px-0 py-1.5 transition-colors focus:border-0 focus:bg-transparent focus:outline-0 tablet:mr-0 dark:text-white dark:placeholder:text-gray-800'
+                    containerClassName='w-100'
+                    inputRef={queryInputRef}
+                    placeholder='Enter a handle or account URL...'
+                    title="Search"
+                    type='text'
+                    value={query}
+                    clearBg
+                    hideTitle
+                    unstyled
+                    onChange={e => setQuery(e.target.value)}
+                />
+            </div>
+            <div className='min-h-[320px]'>
+                {showLoading && (
+                    <div className='flex h-full items-center justify-center pb-8'>
+                        <LoadingIndicator size='lg' />
+                    </div>
                 )}
-
+                {showNoResults && (
+                    <div className='flex h-full items-center justify-center pb-8'>
+                        <NoValueLabel icon='user'>
+                            No users matching this handle or account URL
+                        </NoValueLabel>
+                    </div>
+                )}
                 {!showLoading && !showNoResults && (
                     <SearchResults
                         results={results}
+                        onOpenChange={onOpenChange}
                         onUpdate={updateResult}
                     />
                 )}
-
                 {showSuggested && (
                     <SuggestedProfiles
                         isLoading={isLoadingSuggestedProfiles}
                         profiles={suggestedProfiles}
+                        onOpenChange={onOpenChange}
                         onUpdate={updateSuggestedProfile}
                     />
                 )}
             </div>
-        </Layout>
+        </>
     );
 };
 
