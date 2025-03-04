446a6a7aef7b95573b71a4ecce4c6565558ced14
Peter Zimon <zimo@ghost.org>
2025-03-04 17:02:12 +0100

    Added ActivityPub soft feature flags (#22361)
    ref AP-832
    
    - In order to be able to ship small, in progress ActivityPub related
    features without exposing them to the public, we need a lightweight and
    fast feature flag subsystem. The current alpha feature flag system is
    too big for some of the tasks we're working on.

--- apps/admin-x-activitypub/src/App.tsx
+++ apps/admin-x-activitypub/src/App.tsx
@@ -1,6 +1,7 @@
 import {APP_ROUTE_PREFIX, routes} from '@src/routes';
 import {DesignSystemApp, DesignSystemAppProps} from '@tryghost/admin-x-design-system';
-import {FrameworkProvider, RouterProvider, TopLevelFrameworkProps} from '@tryghost/admin-x-framework';
+import {FeatureFlagsProvider} from './lib/feature-flags';
+import {FrameworkProvider, Outlet, RouterProvider, TopLevelFrameworkProps} from '@tryghost/admin-x-framework';
 import {ShadeApp} from '@tryghost/shade';
 
 interface AppProps {
@@ -13,7 +14,11 @@ const App: React.FC<AppProps> = ({framework, designSystem}) => {
         <FrameworkProvider {...framework}>
             <DesignSystemApp className='shade' {...designSystem}>
                 <ShadeApp darkMode={designSystem.darkMode} fetchKoenigLexical={null}>
-                    <RouterProvider prefix={APP_ROUTE_PREFIX} routes={routes} />
+                    <RouterProvider prefix={APP_ROUTE_PREFIX} routes={routes}>
+                        <FeatureFlagsProvider>
+                            <Outlet />
+                        </FeatureFlagsProvider>
+                    </RouterProvider>
                 </ShadeApp>
             </DesignSystemApp>
         </FrameworkProvider>
