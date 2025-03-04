45441a352f25da06d541fe2ce265a7f04d2aa75d
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-03-04 18:37:05 +0800

    Added AlertDialog component to Shade

--- apps/shade/src/components/ui/popover.tsx
+++ apps/shade/src/components/ui/popover.tsx
@@ -9,6 +9,8 @@ const PopoverTrigger = PopoverPrimitive.Trigger;
 
 const PopoverAnchor = PopoverPrimitive.Anchor;
 
+const PopoverClose = PopoverPrimitive.Close;
+
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
@@ -26,4 +28,4 @@ const PopoverContent = React.forwardRef<
 ));
 PopoverContent.displayName = PopoverPrimitive.Content.displayName;
 
-export {Popover, PopoverTrigger, PopoverContent, PopoverAnchor};
+export {Popover, PopoverTrigger, PopoverContent, PopoverAnchor, PopoverClose};
