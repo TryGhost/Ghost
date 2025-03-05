8d30fee32a93c4d8fbceb39f27c56cb0f226f04e
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-03-05 15:02:38 +0800

    Fixed reply box focus when reply icon is clicked (#22378)
    ref AP-578
    
    - The reply box makes use of the existing `isFocused` state, but it was
    only used for the initial load
    - This adds `setIsFocused` and uses it in all instances of reply icon
    click
    - Also the textarea was focused only when the value of `isFocused`
    changes, so that means next reply icon clicks won't trigger the focus
    - Added a dedicated hook to force the effect triggered, so that
    that all reply icon clicks trigger the focus

--- apps/admin-x-activitypub/src/components/feed/ArticleModal.tsx
+++ apps/admin-x-activitypub/src/components/feed/ArticleModal.tsx
@@ -11,6 +11,7 @@ import {Activity, ActorProperties, ObjectProperties} from '@tryghost/admin-x-fra
 import {Button, Icon, LoadingIndicator, Modal, Select, SelectOption} from '@tryghost/admin-x-design-system';
 import {renderTimestamp} from '../../utils/render-timestamp';
 import {useBrowseSite} from '@tryghost/admin-x-framework/api/site';
+import {useFocusedState} from '@components/global/APReplyBox';
 import {useModal} from '@ebay/nice-modal-react';
 import {useThreadForUser} from '@hooks/use-activity-pub-queries';
 
@@ -376,7 +377,7 @@ const ArticleModal: React.FC<ArticleModalProps> = ({
 }) => {
     const MODAL_SIZE_SM = 640;
     const MODAL_SIZE_LG = 1420;
-    const [isFocused] = useState(focusReply ? 1 : 0);
+    const [isFocused, setIsFocused] = useFocusedState(focusReply);
 
     const {threadQuery, addToThread} = useThreadForUser('index', activityId);
     const {data: thread, isLoading: isLoadingThread} = threadQuery;
@@ -869,6 +870,7 @@ const ArticleModal: React.FC<ArticleModalProps> = ({
                                             }}
                                             onCommentClick={() => {
                                                 navigateForward(item.id, item.object, item.actor, true);
+                                                setIsFocused(true);
                                             }}
                                         />
                                     </>
@@ -890,6 +892,7 @@ const ArticleModal: React.FC<ArticleModalProps> = ({
                                             behavior: 'smooth',
                                             block: 'center'
                                         });
+                                        setIsFocused(true);
                                     }}
                                 />
                             )}
@@ -919,6 +922,7 @@ const ArticleModal: React.FC<ArticleModalProps> = ({
                                                     behavior: 'smooth',
                                                     block: 'center'
                                                 });
+                                                setIsFocused(true);
                                             }}
                                             onLikeClick={onLikeClick}
                                         />
@@ -956,6 +960,7 @@ const ArticleModal: React.FC<ArticleModalProps> = ({
                                                 }}
                                                 onCommentClick={() => {
                                                     navigateForward(item.id, item.object, item.actor, true);
+                                                    setIsFocused(true);
                                                 }}
                                             />
                                             {showDivider && <FeedItemDivider />}
