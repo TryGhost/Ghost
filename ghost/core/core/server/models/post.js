82f5f248e648d474fd4492f66a58fdd06ee98fd6
Cathy Sarisky <catsarisky@gmail.com>
2025-03-09 08:53:22 -0400

    fix missing space in "super editor" with thanks to coderabbit

--- ghost/core/core/server/models/post.js
+++ ghost/core/core/server/models/post.js
@@ -1500,7 +1500,7 @@ Post = ghostBookshelf.Model.extend({
         isContributor = checkUserPermissionsForRole(loadedPermissions, 'Contributor');
         isOwner = checkUserPermissionsForRole(loadedPermissions, 'Owner');
         isAdmin = checkUserPermissionsForRole(loadedPermissions, 'Administrator');
-        isEditor = checkUserPermissionsForRole(loadedPermissions, 'Editor') || checkUserPermissionsForRole(loadedPermissions, 'SuperEditor');
+        isEditor = checkUserPermissionsForRole(loadedPermissions, 'Editor') || checkUserPermissionsForRole(loadedPermissions, 'Super Editor');
         isIntegration = loadedPermissions.apiKey && _.some(loadedPermissions.apiKey.roles, {name: 'Admin Integration'});
 
         isEdit = (action === 'edit');
