2b5f28f46e011845238b6b9d209a8650afefafd1
Sag <guptazy@gmail.com>
2025-03-10 16:47:54 +0530

    Revert "Moved ActivityPub flag to public beta (#22381)" (#22422)
    ref https://linear.app/ghost/issue/AP-853/move-alpha-flag-to-beta
    
    - This reverts commit a11a04d171370a4935fe698ee6876f2722294860.
    - We'll move the AP flag from alpha to beta early next week instead

--- apps/admin-x-settings/src/components/settings/advanced/labs/BetaFeatures.tsx
+++ apps/admin-x-settings/src/components/settings/advanced/labs/BetaFeatures.tsx
@@ -24,10 +24,6 @@ const BetaFeatures: React.FC = () => {
                 detail={<>Enable support for CashApp, iDEAL, Bancontact, and others. <a className='text-green' href="https://ghost.org/help/payment-methods" rel="noopener noreferrer" target="_blank">Learn more &rarr;</a></>}
                 title='Additional payment methods' />
             <LabItem
-                action={<FeatureToggle flag="ActivityPub" />}
-                detail={<>Federate your Ghost publication over ActivityPub. <a className='text-green' href="https://activitypub.ghost.org/" rel="noopener noreferrer" target="_blank">Learn more &rarr;</a></>}
-                title='ActivityPub' />
-            <LabItem
                 action={<div className='flex flex-col items-end gap-1'>
                     <FileUpload
                         id='upload-redirects'
