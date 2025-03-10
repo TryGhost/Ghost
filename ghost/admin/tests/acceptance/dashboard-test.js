872b19550b90cee9a14c178253d03309d78d8686
Cathy Sarisky <catsarisky@gmail.com>
2025-03-10 16:05:38 -0400

    try again (really it's CI not me!)

--- ghost/admin/tests/acceptance/dashboard-test.js
+++ ghost/admin/tests/acceptance/dashboard-test.js
@@ -7,7 +7,7 @@ import {setupMirage} from 'ember-cli-mirage/test-support';
 import {visit} from '../helpers/visit';
 
 // this whole test suite was tending to fail with claims about not being able to find various imports. 
-// I bumped up the timouts to increase the pass rate, but it isn't consistently enough.
+// I bumped up the timouts to increase the pass rate, but it isn't consistently enough. Not even close.
 
 describe('Acceptance: Dashboard', function () {
     const hooks = setupApplicationTest();
