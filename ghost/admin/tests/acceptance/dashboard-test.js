a8463157723147369aa76179d3378fa59c613f04
Cathy Sarisky <catsarisky@gmail.com>
2025-03-10 15:43:21 -0400

    test cleanup

--- ghost/admin/tests/acceptance/dashboard-test.js
+++ ghost/admin/tests/acceptance/dashboard-test.js
@@ -10,26 +10,6 @@ describe('Acceptance: Dashboard', function () {
     const hooks = setupApplicationTest();
     setupMirage(hooks);
 
-    beforeEach(async function () {
-        this.server.loadFixtures('configs');
-        this.server.loadFixtures('settings');
-
-        let role = this.server.create('role', {name: 'Administrator'});
-        this.server.create('user', {roles: [role]});
-
-        await authenticateSession();
-    });
-
-    it('can visit /dashboard', async function () {
-        await visit('/dashboard');
-        expect(currentURL()).to.equal('/dashboard');
-    });
-
-    it('/ redirects to /dashboard', async function () {
-        await visit('/');
-        expect(currentURL()).to.equal('/dashboard');
-    });
-
     describe('permissions', function () {
         beforeEach(async function () {
             this.server.db.users.remove();
@@ -52,7 +32,6 @@ describe('Acceptance: Dashboard', function () {
         });
 
         it('is not accessible to editors', async function () {
-            await invalidateSession();
             this.server.db.users.remove();
 
             let role = this.server.create('role', {name: 'Editor'});
@@ -65,7 +44,6 @@ describe('Acceptance: Dashboard', function () {
         });
 
         it('is not accessible to authors', async function () {
-            await invalidateSession();
 
             let role = this.server.create('role', {name: 'Author'});
             this.server.create('user', {roles: [role]});
@@ -92,9 +70,9 @@ describe('Acceptance: Dashboard', function () {
 
             let role = this.server.create('role', {name: 'Administrator'});
             this.server.create('user', {roles: [role]});
-    
             await authenticateSession();
         });
+        
         it('can visit /dashboard', async function () {
             await visit('/dashboard');
             expect(currentURL()).to.equal('/dashboard');
