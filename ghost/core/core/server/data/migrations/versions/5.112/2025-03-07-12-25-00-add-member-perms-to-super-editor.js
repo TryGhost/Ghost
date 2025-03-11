bb7fc21dff88cbcc3cbd3231c2dba12c1b5d99cc
Cathy Sarisky <catsarisky@gmail.com>
2025-03-11 14:59:09 -0400

    migration for super editor

---
+++ ghost/core/core/server/data/migrations/versions/5.112/2025-03-07-12-25-00-add-member-perms-to-super-editor.js
@@ -0,0 +1,69 @@
+const {addPermissionToRole, combineTransactionalMigrations} = require('../../utils');
+
+module.exports = combineTransactionalMigrations(
+
+    addPermissionToRole({
+
+        permission: 'Browse Members',
+
+        role: 'Super Editor'
+
+    }),
+
+    addPermissionToRole({
+
+        permission: 'Read Members',
+
+        role: 'Super Editor'
+
+    }),
+
+    addPermissionToRole({
+
+        permission: 'Edit Members',
+
+        role: 'Super Editor'
+
+    }),
+
+    addPermissionToRole({
+
+        permission: 'Add Members',
+
+        role: 'Super Editor'
+
+    }),
+
+    addPermissionToRole({
+
+        permission: 'Delete Members',
+
+        role: 'Super Editor'
+
+    }),
+
+    addPermissionToRole({
+
+        permission: 'Read offers',
+
+        role: 'Super Editor'
+
+    }),
+
+    addPermissionToRole({
+
+        permission: 'Browse offers',
+
+        role: 'Super Editor'
+
+    }),
+
+    addPermissionToRole({
+
+        permission: 'Read member signin urls',
+
+        role: 'Super Editor'
+
+    })
+
+);
