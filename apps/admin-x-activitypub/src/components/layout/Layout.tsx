d940c65c32d18cdeabe790ced01de4e0ebb5f5c5
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-03-11 11:52:56 +0800

    Added onboarding functionality

--- apps/admin-x-activitypub/src/components/layout/Layout.tsx
+++ apps/admin-x-activitypub/src/components/layout/Layout.tsx
@@ -1,17 +1,31 @@
 import * as React from 'react';
 import Header from './Header';
+import Onboarding, {useOnboardingStatus} from './Onboarding';
 import Sidebar from './Sidebar';
+import {useCurrentUser} from '@tryghost/admin-x-framework/api/currentUser';
 
 const Layout: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({children, ...props}) => {
+    const {isOnboarded} = useOnboardingStatus();
+    const {data: currentUser, isLoading} = useCurrentUser();
+
+    if (isLoading || !currentUser) {
+        return null;
+    }
+
     return (
         <div className='relative mx-auto flex h-screen w-full max-w-page flex-col overflow-y-auto' {...props}>
-            <Header />
-            <div className='grid grid-cols-[auto_292px] items-start gap-8 px-8'>
-                <div className='z-0'>
-                    {children}
-                </div>
-                <Sidebar />
-            </div>
+            {isOnboarded ?
+                <>
+                    <Header />
+                    <div className='grid grid-cols-[auto_292px] items-start gap-8 px-8'>
+                        <div className='z-0'>
+                            {children}
+                        </div>
+                        <Sidebar />
+                    </div>
+                </> :
+                <Onboarding />
+            }
         </div>
     );
 };
