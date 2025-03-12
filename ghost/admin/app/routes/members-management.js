60d2880ef965b3e43544483379eb172d44ec0f9a
Cathy Sarisky <catsarisky@gmail.com>
2025-03-12 16:17:20 -0400

    lint.

--- ghost/admin/app/routes/members-management.js
+++ ghost/admin/app/routes/members-management.js
@@ -1,7 +1,6 @@
 import AuthenticatedRoute from './authenticated';
 
 export default class MembersManagementRoute extends AuthenticatedRoute {
-
     beforeModel() {
         super.beforeModel(...arguments);
         if (!this.session.user || !this.session.user.canManageMembers) {
