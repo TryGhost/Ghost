e344881d7eb93148ae180851f2c24f8f41a50952
Cathy Sarisky <catsarisky@gmail.com>
2025-03-12 15:59:50 -0400

    strip out commented out code.

--- ghost/admin/app/routes/members-management.js
+++ ghost/admin/app/routes/members-management.js
@@ -1,17 +1,9 @@
 import AuthenticatedRoute from './authenticated';
-//import {inject as service} from '@ember/service';
-
-/* The commented out code would redirect staff to the home if the site has membership disabled. */
-/* As it is currently possible to disable membership but still have members in the database, this needs more thought. */
 
 export default class MembersManagementRoute extends AuthenticatedRoute {
-    //@service settings;
 
     beforeModel() {
         super.beforeModel(...arguments);
-        /* if (this.settings.get('membersEnabled') === false) {
-            return this.transitionTo('home');
-        } */
         if (!this.session.user || !this.session.user.canManageMembers) {
             return this.transitionTo('home');
         }
