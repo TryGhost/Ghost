a3db88f36c26b6a6c3ef1fd4265555bb2328d443
Cathy Sarisky <catsarisky@gmail.com>
2025-03-11 21:03:57 -0400

    combine permissions changes

--- ghost/core/core/server/data/migrations/versions/5.112/2025-03-07-12-25-00-add-member-perms-to-super-editor.js
+++ ghost/core/core/server/data/migrations/versions/5.112/2025-03-07-12-25-00-add-member-perms-to-super-editor.js
@@ -255,6 +255,41 @@ module.exports = combineTransactionalMigrations(
     addPermissionToRole({
         permission: 'Delete collections',
         role: 'Super Editor'
+    }),
+    addPermissionToRole({
+        permission: 'Moderate comments',
+        role: 'Super Editor'
+    }),
+    addPermissionToRole({
+        permission: 'Like comments',
+        role: 'Super Editor'
+    }),
+    addPermissionToRole({
+        permission: 'Unlike comments',
+        role: 'Super Editor'
+    }),
+    addPermissionToRole({
+        permission: 'Add comments',
+        role: 'Super Editor'
+    }),
+    addPermissionToRole({
+        permission: 'Edit comments',
+        role: 'Super Editor'
+    }),
+    addPermissionToRole({
+        permission: 'Delete comments',
+        role: 'Super Editor'
+    }),
+    addPermissionToRole({
+        permission: 'Read comments',
+        role: 'Super Editor'
+    }),
+    addPermissionToRole({
+        permission: 'Browse comments',
+        role: 'Super Editor'
+    }),
+    addPermissionToRole({
+        permission: 'Report comments',
+        role: 'Super Editor'
     })
 );
-
