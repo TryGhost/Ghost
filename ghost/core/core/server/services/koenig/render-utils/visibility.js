726e660b7d363f57ec3795cc9469b27a55565d9c
Kevin Ansfield <kevin@ghost.org>
2025-03-12 12:16:08 +0000

    Fixed conversion of early visibility formats (#1471)
    ref https://linear.app/ghost/issue/ONC-801/
    
    Early iterations of convent visibility used a `visibility` format that our format conversion did not support. This could result in HTML cards created from these early iterations not rendering in emails.
    
    - changed visibility util functions to always return new objects rather than modify by ref
    - renamed `usesOldVisibilityFormat` to `isOldVisibilityFormat` to better represent fn intent
    - updated `isOldVisibilityFormat` to handle missing `web/email.memberSegment` properties in case there have been previous incorrect migrations
    - extracted "is visibility active" logic to new `isVisibilityRestricted` util function and expanded to cover all old formats
    - updated `migrateOldVisibilityFormat` to handle the older `{emailOnly}` format and to correctly migrate the incorrect NQL filters used in older formats
    - expanded tests to cover all old formats and NQL filters
    - added test to `kg-lexical-html-renderer` to assert HTML card does now get rendered in email when using the old visibility format
    - added `yarn test:no-coverage` to `kg-default-nodes` to get cleaner test output when desired during development

--- ghost/core/core/server/services/koenig/render-utils/visibility.js
+++ ghost/core/core/server/services/koenig/render-utils/visibility.js
@@ -1,49 +1,124 @@
 import {renderEmptyContainer} from './render-empty-container';
 
 export const ALL_MEMBERS_SEGMENT = 'status:free,status:-free';
+export const PAID_MEMBERS_SEGMENT = 'status:-free'; // paid + comped
+export const FREE_MEMBERS_SEGMENT = 'status:free';
 export const NO_MEMBERS_SEGMENT = '';
 
 const DEFAULT_VISIBILITY = {
     web: {
         nonMember: true,
-        memberSegment: 'status:free,status:-free'
+        memberSegment: ALL_MEMBERS_SEGMENT
     },
     email: {
-        memberSegment: 'status:free,status:-free'
+        memberSegment: ALL_MEMBERS_SEGMENT
     }
 };
 
-// ensure we always work with a deep copy to avoid accidental constant mutations
+function isNullish(value) {
+    return value === null || value === undefined;
+}
+
+// ensure we always work with a deep copy to avoid accidental ref mutations
 export function buildDefaultVisibility() {
     return JSON.parse(JSON.stringify(DEFAULT_VISIBILITY));
 }
 
-export function usesOldVisibilityFormat(visibility) {
+export function isOldVisibilityFormat(visibility) {
     return !Object.prototype.hasOwnProperty.call(visibility, 'web')
         || !Object.prototype.hasOwnProperty.call(visibility, 'email')
-        || !Object.prototype.hasOwnProperty.call(visibility.web, 'nonMember');
+        || !Object.prototype.hasOwnProperty.call(visibility.web, 'nonMember')
+        || isNullish(visibility.web.memberSegment)
+        || isNullish(visibility.email.memberSegment);
 }
 
-export function migrateOldVisibilityFormat(visibility) {
-    visibility.web ??= {};
-    visibility.web.nonMember ??= visibility.showOnWeb;
-    visibility.web.memberSegment ??= visibility.showOnWeb ? ALL_MEMBERS_SEGMENT : NO_MEMBERS_SEGMENT;
-
-    visibility.email ??= {};
-    if (visibility.showOnEmail) {
-        visibility.email.memberSegment ??= visibility.segment ? visibility.segment : ALL_MEMBERS_SEGMENT;
+export function isVisibilityRestricted(visibility) {
+    if (isOldVisibilityFormat(visibility)) {
+        return visibility.showOnEmail === false
+            || visibility.showOnWeb === false
+            || visibility.emailOnly === true
+            || visibility.segment !== '';
     } else {
-        visibility.email.memberSegment = NO_MEMBERS_SEGMENT;
+        return visibility.web.nonMember === false
+            || visibility.web.memberSegment !== ALL_MEMBERS_SEGMENT
+            || visibility.email.memberSegment !== ALL_MEMBERS_SEGMENT;
     }
+}
+
+// old formats...
+//
+// "segment" only applies to email visibility
+// {emailOnly: true/false, segment: ''}
+// {showOnWeb: true/false, showOnEmail: true/false, segment: 'status:free,status:-free'}
+//
+// segment: '' = everyone
+// segment: 'status:free' = free members
+// segment: 'status:paid' = paid members (incorrect, misses comped)
+// segment: 'status:-free' = paid members (correct, includes comped)
+// segment: 'status:-free+status:-paid' = no-one (incorrect, misses comped)
+//
+// new format...
+//
+// {
+//     web: {
+//         nonMember: true/false,
+//         memberSegment: 'status:free,status:-free'
+//     },
+//     email: {
+//         memberSegment: 'status:free,status:-free'
+//     }
+// }
+//
+// memberSegment: '' = no-one
+// memberSegment: 'status:free,status:-free' = everyone
+// memberSegment: 'status:free' = free members
+// memberSegment: 'status:-free' = paid + comped members
+export function migrateOldVisibilityFormat(visibility) {
+    if (!visibility || !isOldVisibilityFormat(visibility)) {
+        return visibility;
+    }
+
+    // deep clone to avoid mutating the original object
+    const newVisibility = JSON.parse(JSON.stringify(visibility));
+
+    // ensure we have expected objects ready to populate
+    newVisibility.web ??= {};
+    newVisibility.email ??= {};
+
+    // convert web visibility, old formats only had on/off for web visibility rather than specific segments
+    if (isNullish(visibility.showOnWeb) && isNullish(visibility.emailOnly)) {
+        newVisibility.web = buildDefaultVisibility().web;
+    } else if (!isNullish(visibility.emailOnly)) {
+        newVisibility.web.nonMember = !visibility.emailOnly;
+        newVisibility.web.memberSegment = visibility.emailOnly ? NO_MEMBERS_SEGMENT : ALL_MEMBERS_SEGMENT;
+    } else {
+        newVisibility.web.nonMember = visibility.showOnWeb;
+        newVisibility.web.memberSegment = visibility.showOnWeb ? ALL_MEMBERS_SEGMENT : NO_MEMBERS_SEGMENT;
+    }
+
+    // convert email visibility, taking into account the old (and sometimes incorrect) segment formats
+    if (isNullish(visibility.showOnEmail) && isNullish(visibility.emailOnly)) {
+        newVisibility.email = buildDefaultVisibility().email;
+    } else if (visibility.showOnEmail === false) {
+        newVisibility.email.memberSegment = NO_MEMBERS_SEGMENT;
+    } else if (visibility.segment === 'status:-free+status:-paid') {
+        newVisibility.email.memberSegment = NO_MEMBERS_SEGMENT;
+    } else if (visibility.segment === 'status:free') {
+        newVisibility.email.memberSegment = FREE_MEMBERS_SEGMENT;
+    } else if (visibility.segment === 'status:paid' || visibility.segment === 'status:-free') {
+        newVisibility.email.memberSegment = PAID_MEMBERS_SEGMENT;
+    } else if (!visibility.segment) {
+        newVisibility.email.memberSegment = ALL_MEMBERS_SEGMENT;
+    }
+
+    return newVisibility;
 }
 
 export function renderWithVisibility(originalRenderOutput, visibility, options) {
     const document = originalRenderOutput.element.ownerDocument;
     const content = _getRenderContent(originalRenderOutput);
 
-    if (usesOldVisibilityFormat(visibility)) {
-        migrateOldVisibilityFormat(visibility);
-    }
+    visibility = migrateOldVisibilityFormat(visibility);
 
     if (options.target === 'email') {
         if (visibility.email.memberSegment === NO_MEMBERS_SEGMENT) {
