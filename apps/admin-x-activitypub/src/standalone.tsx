403ab0c23e2e3316b6da1e82f474b62b42fa0f6b
Sag <guptazy@gmail.com>
2025-03-12 15:19:05 +0530

    Updated the client AP app to render nothing when flag is off (#22437)
    ref https://linear.app/ghost/issue/AP-836
    
    - passed the ActivityPub flag to the client AP app
    - if the ActivityPub flag is disabled, skipped rendering the AP client app

--- apps/admin-x-activitypub/src/standalone.tsx
+++ apps/admin-x-activitypub/src/standalone.tsx
@@ -2,4 +2,6 @@ import './styles/index.css';
 import App from './App.tsx';
 import renderStandaloneApp from '@tryghost/admin-x-framework/test/render';
 
-renderStandaloneApp(App, {});
+renderStandaloneApp(App, {
+    activityPubEnabled: false
+});
