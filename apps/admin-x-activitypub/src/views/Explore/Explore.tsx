1a0e18d70c23192304c79523b31b16ebec01febb
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-03-13 16:04:12 +0800

    Updated explainer style

--- apps/admin-x-activitypub/src/views/Explore/Explore.tsx
+++ apps/admin-x-activitypub/src/views/Explore/Explore.tsx
@@ -1,6 +1,6 @@
 import Layout from '@components/layout';
 import React from 'react';
-import {Button, LucideIcon} from '@tryghost/shade';
+import {Button, H4, LucideIcon} from '@tryghost/shade';
 import {SuggestedProfile} from '@src/components/modals/Search';
 import {useOnboardingStatus} from '@src/components/layout/Onboarding';
 import {useSuggestedProfilesForUser} from '@hooks/use-activity-pub-queries';
@@ -20,13 +20,16 @@ const Profile: React.FC = ({ }) => {
     return (
         <Layout>
             {!isExplainerClosed &&
-                <div className='flex flex-col'>
-                    <span>Get started by following</span>
-                    <span>If you want followers then follow others.</span>
-                    <Button variant='ghost' onClick={() => setExplainerClosed(true)}><LucideIcon.X /></Button>
+                <div className='relative mt-6 flex items-center gap-1 rounded-md bg-gradient-to-r from-[#CFB0FF66] to-[#B6E8FF66] p-5'>
+                    <LucideIcon.Sprout className='text-purple' size={42} strokeWidth={0.75} />
+                    <div className='mt-1 flex flex-col gap-[2px]'>
+                        <H4>Get started by following</H4>
+                        <p className='text-sm text-gray-800'>If you want followers then follow others.</p>
+                    </div>
+                    <Button className='absolute right-3 top-3 h-6 w-6 opacity-40' variant='link' onClick={() => setExplainerClosed(true)}><LucideIcon.X size={20} /></Button>
                 </div>
             }
-            <div className='flex flex-col items-center'>
+            <div className='mt-12 flex flex-col items-center'>
                 {
                     suggestedProfiles.map(profile => (
                         <React.Fragment key={profile.actor.id}>
