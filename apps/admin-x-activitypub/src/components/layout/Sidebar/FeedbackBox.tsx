9377a1bff410579a77f628bd962c1e0c4023cfcb
Michael Barrett <mike@ghost.org>
2025-03-13 20:07:15 +0000

    Wired up to post

--- apps/admin-x-activitypub/src/components/layout/Sidebar/FeedbackBox.tsx
+++ apps/admin-x-activitypub/src/components/layout/Sidebar/FeedbackBox.tsx
@@ -1,9 +1,42 @@
+import ArticleModal from '@src/components/feed/ArticleModal';
+import NiceModal from '@ebay/nice-modal-react';
 import React from 'react';
+import {ActorProperties, ObjectProperties} from '@tryghost/admin-x-framework/api/activitypub';
 import {Button, H4, LucideIcon} from '@tryghost/shade';
 import {useFeatureFlags} from '@src/lib/feature-flags';
 
 const FeedbackBox: React.FC = () => {
     const {isEnabled} = useFeatureFlags();
+
+    function showFeedbackReply() {
+        const targetPostId = 'https://main.ghost.org/.ghost/activitypub/note/abfa416f-30ee-4aa9-be3b-b763aa2547fe';
+
+        NiceModal.show(ArticleModal, {
+            activityId: targetPostId,
+            object: {
+                content: 'Mollit laborum Lorem enim est occaecat. Sunt laboris velit proident laborum ad nostrud consequat esse irure fugiat. Anim enim tempor anim do elit mollit in voluptate amet amet qui. Cupidatat velit culpa dolor mollit mollit. Id cillum labore consequat nostrud ullamco eiusmod enim aliquip labore tempor voluptate. Veniam ipsum consectetur velit consectetur dolore aliquip ad commodo id. Consequat incididunt dolor enim.',
+                id: targetPostId,
+                published: '2025-03-10T15:00:58Z',
+                type: 'Note',
+                url: targetPostId,
+                '@context': '',
+                name: ''
+            } as ObjectProperties,
+            actor: {
+                id: 'https://activitypub.ghost.org/.ghost/activitypub/users/index',
+                icon: {
+                    url: 'https://activitypub.ghost.org/content/images/2024/09/ghost-orb-white-squircle-07.png'
+                },
+                name: 'Building ActivityPub',
+                preferredUsername: 'index',
+                summary: 'Ghost is federating over ActivityPub to become part of the world’s largest publishing network',
+                url: 'https://activitypub.ghost.org/'
+            } as ActorProperties,
+            focusReply: true,
+            width: 'narrow',
+            disableStats: true
+        });
+    }
 
     return (
         <div className='sticky bottom-0 z-20 w-full bg-white'>
@@ -16,7 +49,7 @@ const FeedbackBox: React.FC = () => {
                     <span className='text-sm text-gray-700'>
                     Something not working? Let us know
                     </span>
-                    <Button className='mt-2' variant='secondary' onClick={() => {}}>Send feedback</Button>
+                    <Button className='mt-2' variant='secondary' onClick={showFeedbackReply}>Send feedback</Button>
                 </div>
             }
             <div className='-mb-1 ml-3 flex items-center gap-1.5 pt-4 text-xs text-gray-400'>
