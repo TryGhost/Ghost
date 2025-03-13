a7b49de3fe173a2311e263ce8c6a5c3c20ffe6aa
Sag <guptazy@gmail.com>
2025-03-13 17:16:25 +0530

    Passed ActivityPub feature flag to the client app (#22482)
    ref https://linear.app/ghost/issue/AP-836
    
    - the activityPubEnabled flag is used by the client ActivityPub app to
    decide whether to render the app or not

--- ghost/admin/app/components/admin-x/activitypub.js
+++ ghost/admin/app/components/admin-x/activitypub.js
@@ -3,6 +3,11 @@ import {inject as service} from '@ember/service';
 
 export default class AdminXActivityPub extends AdminXComponent {
     @service upgradeStatus;
+    @service feature;
 
     static packageName = '@tryghost/admin-x-activitypub';
+
+    additionalProps = () => ({
+        activityPubEnabled: this.feature.ActivityPub
+    });
 }
