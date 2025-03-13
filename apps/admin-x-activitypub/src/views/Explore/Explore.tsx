c736224d664947b2171317231c08abc5b0bfa9d8
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-03-13 12:05:32 +0800

    Added Explore to the main nav and set up the routes

---
+++ apps/admin-x-activitypub/src/views/Explore/Explore.tsx
@@ -0,0 +1,12 @@
+import Layout from '@components/layout';
+import React from 'react';
+
+const Profile: React.FC = ({ }) => {
+    return (
+        <Layout>
+            Explore
+        </Layout>
+    );
+};
+
+export default Profile;
