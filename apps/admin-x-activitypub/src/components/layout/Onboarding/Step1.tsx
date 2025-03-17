0001e4b60b5faa4e3423147df42148ed2675cdb8
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-03-17 14:33:34 +0800

    Fixed avatar interaction in ActivityPub onboarding step one (#22518)
    no issues
    
    - we wouldn't want any interaction on avatars included on the bottom of
    the onboarding step 1
    - this removes all interactions, so there will be no click and hover
    actions

--- apps/admin-x-activitypub/src/components/layout/Onboarding/Step1.tsx
+++ apps/admin-x-activitypub/src/components/layout/Onboarding/Step1.tsx
@@ -80,9 +80,9 @@ const Step1: React.FC = () => {
                         </div>
                         <p className='leading-tight text-gray-800 dark:text-gray-600'>{account?.bio}</p>
                         <div className='mt-1 flex gap-2 text-sm text-gray-800 dark:text-gray-600'>
-                            <div className='flex [&>*:not(:first-child)]:-ml-2 [&>*:nth-child(1)]:z-30 [&>*:nth-child(2)]:z-20 [&>*:nth-child(3)]:z-10 [&>*]:h-5 [&>*]:w-5 [&>*]:border [&>*]:border-white dark:[&>*]:border-[#101114] [&_img]:h-5 [&_img]:w-5'>
+                            <div className='flex [&>*:not(:first-child)]:-ml-2 [&>*:nth-child(1)]:z-30 [&>*:nth-child(2)]:z-20 [&>*:nth-child(3)]:z-10 [&>*]:pointer-events-none [&>*]:h-5 [&>*]:w-5 [&>*]:border [&>*]:border-white dark:[&>*]:border-[#101114] [&_img]:h-5 [&_img]:w-5'>
                                 {transformedUsers.map(user => (
-                                    <APAvatar key={user.id} author={user} size='xs' />
+                                    <APAvatar key={user.id} author={user} disabled={true} size='xs' />
                                 ))}
                             </div>
                         Authored by {firstThreeUsers[0]?.name}{(meta?.pagination.total ?? 0) > 1 && ` and ${(meta?.pagination.total ?? 1) - 1} others`}
