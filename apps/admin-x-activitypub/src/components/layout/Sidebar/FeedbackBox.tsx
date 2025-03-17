0cfd55a46ab6edf3690f8f269bcf27e1167acd63
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-03-17 14:52:10 +0800

    Removed onboarding and feedback soft flags (#22517)
    ref AP-905
    
    - as the features are about to be shipped, we don't need these soft
    flags anymore
    - released a new version as part of this removal

--- apps/admin-x-activitypub/src/components/layout/Sidebar/FeedbackBox.tsx
+++ apps/admin-x-activitypub/src/components/layout/Sidebar/FeedbackBox.tsx
@@ -3,11 +3,8 @@ import NiceModal from '@ebay/nice-modal-react';
 import React from 'react';
 import {ActorProperties, ObjectProperties} from '@tryghost/admin-x-framework/api/activitypub';
 import {Button, H4, LucideIcon} from '@tryghost/shade';
-import {useFeatureFlags} from '@src/lib/feature-flags';
 
 const FeedbackBox: React.FC = () => {
-    const {isEnabled} = useFeatureFlags();
-
     function showFeedbackReply() {
         const targetPostId = 'https://main.ghost.org/.ghost/activitypub/note/abfa416f-30ee-4aa9-be3b-b763aa2547fe';
 
@@ -40,18 +37,16 @@ const FeedbackBox: React.FC = () => {
 
     return (
         <div className='sticky bottom-0 z-20 w-full bg-white dark:bg-black'>
-            {isEnabled('feedback') &&
-                <div className='flex w-full flex-col gap-0.5 border-t border-gray-200 bg-white px-3 pt-6 dark:border-gray-950 dark:bg-black'>
-                    <div className='flex items-center gap-2'>
-                        <LucideIcon.MessageSquare className='text-purple-500' size={20} strokeWidth={1.5} />
-                        <H4>Beta feedback</H4>
-                    </div>
-                    <span className='text-sm text-gray-700'>
-                    Something not working? Let us know
-                    </span>
-                    <Button className='mt-2 dark:bg-gray-925/70 dark:hover:bg-gray-900' variant='secondary' onClick={showFeedbackReply}>Send feedback</Button>
+            <div className='flex w-full flex-col gap-0.5 border-t border-gray-200 bg-white px-3 pt-6 dark:border-gray-950 dark:bg-black'>
+                <div className='flex items-center gap-2'>
+                    <LucideIcon.MessageSquare className='text-purple-500' size={20} strokeWidth={1.5} />
+                    <H4>Beta feedback</H4>
                 </div>
-            }
+                <span className='text-sm text-gray-700'>
+                Something not working? Let us know
+                </span>
+                <Button className='mt-2 dark:bg-gray-925/70 dark:hover:bg-gray-900' variant='secondary' onClick={showFeedbackReply}>Send feedback</Button>
+            </div>
             <div className='-mb-1 ml-3 flex items-center gap-1.5 pt-4 text-xs text-gray-400'>
                 <a className='text-xs font-medium text-gray-700 hover:text-black dark:text-gray-800 dark:hover:text-white' href="https://ghost.org/help/social-web/" rel="noreferrer" target="_blank">Help</a>
                 &sdot;
