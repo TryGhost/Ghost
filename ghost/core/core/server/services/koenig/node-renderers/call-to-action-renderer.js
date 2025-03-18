25d4af8d50cd164944ea68609e51067c692286e1
Ronald Langeveld <hi@ronaldlangeveld.com>
2025-03-18 13:28:54 +0900

    Revert "Added HTML cleaner for sponsorLabel (#1476)"
    This reverts commit 0246691be70c33acd256334c4e379353ff9fc72d.

--- ghost/core/core/server/services/koenig/node-renderers/call-to-action-renderer.js
+++ ghost/core/core/server/services/koenig/node-renderers/call-to-action-renderer.js
@@ -2,7 +2,6 @@ import {addCreateDocumentOption} from '../../utils/add-create-document-option';
 import {renderWithVisibility} from '../../utils/visibility';
 import {getResizedImageDimensions} from '../../utils/get-resized-image-dimensions';
 import {isLocalContentImage} from '../../utils/is-local-content-image';
-import {buildCleanBasicHtmlForElement} from '../../utils/build-clean-basic-html-for-element';
 
 const showButton = dataset => dataset.showButton && dataset.buttonUrl && dataset.buttonText;
 
@@ -23,7 +22,6 @@ function ctaCardTemplate(dataset) {
     const buttonStyle = dataset.buttonColor === 'accent'
         ? `style="color: ${dataset.buttonTextColor};"`
         : `style="background-color: ${dataset.buttonColor}; color: ${dataset.buttonTextColor};"`;
-
     return `
         <div class="kg-card kg-cta-card kg-cta-bg-${dataset.backgroundColor} kg-cta-${dataset.layout} ${dataset.imageUrl ? 'kg-cta-has-img' : ''} ${dataset.linkColor === 'accent' ? 'kg-cta-link-accent' : ''}" data-layout="${dataset.layout}">
             ${dataset.hasSponsorLabel ? `
@@ -233,15 +231,8 @@ export function renderCallToActionNode(node, options = {}) {
         return renderWithVisibility({element: emailDiv.firstElementChild}, node.visibility, options);
     }
 
-    const element = document.createElement('div');
-
-    if (dataset.hasSponsorLabel) {
-        const cleanBasicHtml = buildCleanBasicHtmlForElement(element);
-        const cleanedHtml = cleanBasicHtml(dataset.sponsorLabel, {firstChildInnerContent: true});
-        dataset.sponsorLabel = cleanedHtml;
-    }
     const htmlString = ctaCardTemplate(dataset);
-
+    const element = document.createElement('div');
     element.innerHTML = htmlString?.trim();
 
     return renderWithVisibility({element: element.firstElementChild}, node.visibility, options);
