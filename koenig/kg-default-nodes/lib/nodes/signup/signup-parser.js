bafaa7c23ffddb35f4f625e8113a62d0ccaffa05
Ronald Langeveld <hi@ronaldlangeveld.com>
2025-03-18 10:50:35 +0900

    Moved rgb-to-hex to util (TryGhost/Koenig#1484)
    no issue
    
    - Moved the rgb to hex function into a util so we can use it later in
      other areas.

--- koenig/kg-default-nodes/lib/nodes/signup/signup-parser.js
+++ koenig/kg-default-nodes/lib/nodes/signup/signup-parser.js
@@ -1,22 +1,4 @@
-function rgbToHex(rgb) {
-    if (rgb === 'transparent') {
-        return rgb;
-    }
-
-    try {
-        // Extract the red, green, and blue values from the RGB string
-        const [r, g, b] = rgb.match(/\d+/g);
-        // Convert each component to hexadecimal
-        const red = parseInt(r, 10).toString(16).padStart(2, '0');
-        const green = parseInt(g, 10).toString(16).padStart(2, '0');
-        const blue = parseInt(b, 10).toString(16).padStart(2, '0');
-        // Concatenate the hexadecimal values
-        const hex = `#${red}${green}${blue}`;
-        return hex;
-    } catch (e) {
-        return null;
-    }
-}
+import {rgbToHex} from '../../utils/rgb-to-hex';
 
 function getLayout(domNode) {
     if (domNode.classList.contains('kg-layout-split')) {
