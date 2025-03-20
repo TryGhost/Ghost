cd90d5ef7792796517d47110319a9ae37812f3ef
Chris Raible <chris@ghost.org>
2025-03-19 18:54:27 -0700

    Refactored tests a bit for improved readability

--- ghost/core/test/e2e-browser/admin/editor.spec.js
+++ ghost/core/test/e2e-browser/admin/editor.spec.js
@@ -3,17 +3,19 @@ const EditorPage = require('../utils/pages/EditorPage');
 
 test.describe('Editor', () => {
     test.describe('Saving behavior', () => {
-        test('should save a draft when only the title is changed for the first time', async ({page}) => {
+        test('should save a draft when only the title is changed and the input is blurred', async ({page}) => {
             const editorPage = new EditorPage(page);
             await editorPage.goto();
-            await editorPage.typeInTitle('Hello world');
+            await editorPage.setTitle('Hello world', {blur: true});
+            await editorPage.waitForCreatePostResponse();
             await editorPage.checkPostStatus('Draft');
         });
         
         test('should save a draft when only the content is changed for the first time', async ({page}) => {
             const editorPage = new EditorPage(page);
             await editorPage.goto();
-            await editorPage.typeInEditor('Hello world');
+            await editorPage.setBody('Hello world', {triggerAutosave: true});
+            await editorPage.waitForCreatePostResponse();
             await editorPage.checkPostStatus('Draft');
         });
     });
--- ghost/core/test/e2e-browser/utils/pages/EditorPage.js
+++ ghost/core/test/e2e-browser/utils/pages/EditorPage.js
@@ -14,8 +14,13 @@ class EditorPage {
         this.postStatus = this.page.locator('[data-test-editor-post-status]').first();
         this.psmButton = this.page.locator('[data-test-editor-psm-trigger]');
         this.breadcrumb = this.page.locator('[data-test-breadcrumb]');
+        this.createPostPromise = this.getCreatePostPromise();
     }
 
+    /**
+     * Navigates to a blank new post or page
+     * @param {'post' | 'page'} type 
+     */
     async goto(type = 'post') {
         await this.page.goto('/ghost');
         if (type === 'post') {
@@ -25,42 +30,97 @@ class EditorPage {
         } else {
             throw new Error(`Invalid type: ${type}`);
         }
-
-        // wait for editor to be ready
         await expect(this.editor).toBeVisible();
     }
 
-    async focusTitle() {
-        await this.titleInput.click();
-    }
-
+    /**
+     * Blurs the title input
+     */
     async blurTitle() {
         await this.titleInput.blur();
     }
 
-    async typeInTitle(text) {
-        await this.focusTitle();
-        await this.page.keyboard.type(text);
-        await this.blurTitle();
+    /**
+     * Fill the title input with the provided text
+     * @param {string} text
+     */
+    async fillTitle(text) {
+        await this.titleInput.fill(text);
     }
 
-    async focusEditor() {
-        await this.editor.click();
+    /**
+     * Sets the title to the provided text, overwriting any existing text, and optionally blurs the input
+     * @param {string} text 
+     * @param {Object} options
+     * @param {boolean} [options.blur] Whether to blur the input after setting the title
+     */
+    async setTitle(text, options = {}) {
+        await this.titleInput.clear();
+        await this.titleInput.fill(text);
+        if (options.blur) {
+            await this.blurTitle();
+        }
     }
 
-    async typeInEditor(text) {
-        await this.focusEditor();
-        await this.page.keyboard.type(text);
+    /**
+     * Fills the editor with the provided text
+     * @param {string} text
+     */
+    async fillBody(text) {
+        await this.editor.fill(text);
     }
 
-    async checkPostStatus(status, hoverStatus) {
+    /**
+     * Sets the editor to the provided text, overwriting any existing text
+     * @param {string} text 
+     * @param {Object} options
+     * @param {boolean} [options.triggerAutosave] Whether to trigger an autosave
+     */
+    async setBody(text, options = {}) {
+        await this.editor.clear();
+        await this.fillBody(text);
+        if (options.triggerAutosave) {
+            await this.triggerAutosave();
+        }
+    }
+
+    /**
+     * Triggers an autosave by fast forwarding the clock by 5 seconds
+     * This saves us 3 seconds of waiting for the autosave to trigger
+     */
+    async triggerAutosave() {
+        await this.page.clock.install();
+        await this.page.clock.fastForward('00:05');
+    }
+
+    /**
+     * Waits for a successful response from the POST /posts endpoint
+     * @returns {Promise<import('@playwright/test').Response>} Playwright response object
+     */
+    async waitForCreatePostResponse() {
+        const response = await this.createPostPromise;
+        return response;
+    }
+
+    /**
+     * Checks that the post status is the provided status
+     * @param {string} status
+     */
+    async checkPostStatus(status) {
         await this.page.waitForLoadState('networkidle');
         await expect(this.postStatus).toContainText(status, {timeout: 10000});
+    }
 
-        if (hoverStatus) {
-            await this.postStatus.hover();
-            await expect(this.postStatus).toContainText(hoverStatus, {timeout: 5000});
-        }
+    /**
+     * Returns a promise that resolves when a successful response from the POST /posts endpoint is received
+     * @returns {Promise<import('@playwright/test').Response>} Playwright response object
+     */
+    getCreatePostPromise() {
+        return this.page.waitForResponse((response) => {
+            return response.request().method() === 'POST' &&
+                response.url().includes('/ghost/api/admin/posts/') &&
+                response.status() === 201;
+        });
     }
 }
 
