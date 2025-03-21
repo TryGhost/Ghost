2c1070c0fd48e5b01cf1b917f14bd20eb673aaa1
Hannah Wolfe <github.erisds@gmail.com>
2025-03-21 17:58:49 +0000

    Added experimental explore ping feature
    - We're experimenting with a new version of explore
    - Eventually, we want to send a ping on a schedule
    - For the first version, we'll just ping on boot
    - This version of the code is behind an alpha flag so that we can test
      getting all the pieces working together

---
+++ ghost/core/core/server/services/explore-ping/ExplorePingService.js
@@ -0,0 +1,62 @@
+module.exports = class ExplorePingService {
+    constructor({PublicConfigService, config, labs, logging, ghostVersion, request}) {
+        this.PublicConfigService = PublicConfigService;
+        this.config = config;
+        this.labs = labs;
+        this.logging = logging;
+        this.ghostVersion = ghostVersion;
+        this.request = request;
+        this.labs = labs;
+    }
+
+    constructPayload() {
+        const {url, title, description, icon, locale, twitter, facebook} = this.PublicConfigService.site;
+        return {
+            ghost: this.ghostVersion.full,
+            url,
+            title,
+            description,
+            icon,
+            locale,
+            twitter,
+            facebook
+        };
+    }
+
+    async makeRequest(exploreUrl, payload) {
+        const json = JSON.stringify(payload);
+        this.logging.info('Pinging Explore with Payload', exploreUrl, json);
+
+        try {
+            const response = await this.request(exploreUrl, {
+                method: 'POST',
+                body: json,
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            this.logging.info('Explore Response', response.statusCode, response.statusMessage);
+
+            return response;
+        } catch (err) {
+            this.logging.warn('Explore Error', err.message);
+        }
+    }
+
+    async ping() {
+        const exploreUrl = this.config.get('explore:url');
+
+        if (!this.labs.isSet('explore')) {
+            return;
+        }
+
+        if (!exploreUrl) {
+            this.logging.warn('Explore URL not set');
+            return;
+        }
+
+        const payload = this.constructPayload();
+        await this.makeRequest(exploreUrl, payload);
+    }
+};
