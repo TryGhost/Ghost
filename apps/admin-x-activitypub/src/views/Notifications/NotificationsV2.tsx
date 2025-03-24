efbbb8f069feec60c5f08001d7e1df7b51495700
Michael Barrett <mike@ghost.org>
2025-03-24 08:56:22 +0000

    Added feature flag for updated notifications component in `admin-x-activitypub`
    refs https://linear.app/ghost/issue/AP-900
    
    Added feature flag for updated notifications component in `admin-x-activitypub`

---
+++ apps/admin-x-activitypub/src/views/Notifications/NotificationsV2.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+import Layout from '@components/layout';
+
+const NotificationsV2: React.FC = () => {
+    return (
+        <Layout>
+            <div className='z-0 flex w-full flex-col items-center justify-center'>
+                <p className='my-20'>Coming soon to an ActivityPub near you!</p>
+            </div>
+        </Layout>
+    );
+};
+
+export default NotificationsV2;
