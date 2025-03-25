c12f6bf8043329aaff72635899d2d34c11b13167
Peter Zimon <zimo@ghost.org>
2025-03-25 14:15:13 +0100

    Updated header links in ActivityPub (#22624)
    ref AP-980
    
    - ATM we only show profiles when clicking on the avatar, not the whole
    heading which makes access of profiles much harder, which eventually
    lowers the chance of people following each other

--- apps/admin-x-activitypub/src/components/feed/ArticleModal.tsx
+++ apps/admin-x-activitypub/src/components/feed/ArticleModal.tsx
@@ -20,6 +20,7 @@ import APReplyBox from '../global/APReplyBox';
 import DeletedFeedItem from './DeletedFeedItem';
 import TableOfContents, {TOCItem} from './TableOfContents';
 import getReadingTime from '../../utils/get-reading-time';
+import {handleProfileClick} from '@src/utils/handle-profile-click';
 import {isPendingActivity} from '../../utils/pending-activity';
 import {openLinksInNewTab} from '@src/utils/content-formatters';
 import {useDebounce} from 'use-debounce';
@@ -754,13 +755,13 @@ const ArticleModal: React.FC<ArticleModalProps> = ({
                             <div className='relative z-10 pt-[3px]'>
                                 <APAvatar author={actor}/>
                             </div>
-                            <div className='relative z-10 flex w-full min-w-0 flex-col overflow-visible text-[1.5rem]'>
+                            <div className='relative z-10 flex w-full min-w-0 cursor-pointer flex-col overflow-visible text-[1.5rem]' onClick={e => handleProfileClick(actor, e)}>
                                 <div className='flex w-full'>
-                                    <span className='min-w-0 truncate whitespace-nowrap font-semibold'>{actor.name}</span>
+                                    <span className='min-w-0 truncate whitespace-nowrap font-semibold hover:underline'>{actor.name}</span>
                                 </div>
                                 <div className='flex w-full'>
                                     <span className='text-gray-700 after:mx-1 after:font-normal after:text-gray-700 after:content-["·"]'>{getUsername(actor)}</span>
-                                    <span className='text-gray-700'>{renderTimestamp(object)}</span>
+                                    <span className='text-gray-700'>{renderTimestamp(object, !object.authored)}</span>
                                 </div>
                             </div>
                         </div>)}
