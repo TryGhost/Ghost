90f4854b25cd5ffedec3ae78372aee1328a3247c
Peter Zimon <zimo@ghost.org>
2025-03-25 10:20:28 +0100

    Added Stats React app (#22613)
    ref ANAL-157
    
    - ATM traffic analytics is a combination of Ember and React components.
    This PR creates a new "Stats" React app that uses Shade

--- apps/admin-x-settings/src/components/settings/advanced/labs/AlphaFeatures.tsx
+++ apps/admin-x-settings/src/components/settings/advanced/labs/AlphaFeatures.tsx
@@ -47,6 +47,10 @@ const features = [{
     title: 'Post analytics redesign',
     description: 'Enables redesigned Post analytics page',
     flag: 'postsX'
+},{
+    title: 'Stats redesign',
+    description: 'Enables redesigned Stats page',
+    flag: 'statsX'
 }, {
     title: 'Sign-up CAPTCHA',
     description: 'Enable CAPTCHA for member sign-up and sign-in',
