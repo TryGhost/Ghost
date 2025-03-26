2329daaada381f6fc11e387c9fd70a1664e31d34
Daniel Lockyer <hi@daniellockyer.com>
2025-03-26 12:06:26 +0100

    Moved `@tryghost/mw-session-from-token` to services folder
    fix https://linear.app/ghost/issue/ENG-2085/mw-session-from-token
    
    - this commit moves the lib code and tests to the core folder so we can
      colocate code in one place rather than splitting it out across
      packages, which increases the cognitive load and overhead
    - this might be classed as a weird place to put the middleware - I'd
      agree, but I'd rather put all the code in one place and then fix it
      afterwards

--- ghost/core/test/unit/server/services/auth/SessionFromToken.test.js
+++ ghost/core/test/unit/server/services/auth/SessionFromToken.test.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const sinon = require('sinon');
 const should = require('should');
-const SessionFromToken = require('../');
+const SessionFromToken = require('../../../../../core/server/services/auth/session/session-from-token');
 
 describe('SessionFromToken', function () {
     it('Parses the request, matches the user to the token, sets the user on req.user and calls createSession', async function () {
