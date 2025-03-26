38d2db0d27b04f63ac6d348a3c31378f5b2e6578
Daniel Lockyer <hi@daniellockyer.com>
2025-03-26 13:09:49 +0100

    Moved @tryghost/bootstrap-socket to core folder
    fix https://linear.app/ghost/issue/ENG-2088/bootstrap-socket
    
    - this commit moves the lib code and tests to the core folder so we can
      colocate code in one place rather than splitting it out across
      packages, which increases the cognitive load and overhead

--- ghost/core/test/unit/server/lib/bootstrap-socket.test.js
+++ ghost/core/test/unit/server/lib/bootstrap-socket.test.js
@@ -1,6 +1,6 @@
 require('should');
 
-const bootstrapSocket = require('../lib/bootstrap-socket');
+const bootstrapSocket = require('../../../../core/server/lib/bootstrap-socket');
 
 describe('Connect and send', function () {
     it('Resolves a promise for a bad call', function () {
