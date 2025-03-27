5d1d8ad87ba5988c0ef4e74f8e7b69bb3daa3a11
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-03-27 15:43:23 +0800

    Added routing support to Feed as an experiment in ActivityPub (#22661)
    no issues
    
    - this adds routing support to only Feed under a soft feature flag called `feed-routes`
    - with this change, the feed items are displayed in the main content view instead of the drawer

--- apps/admin-x-activitypub/src/components/layout/Header/Header.tsx
+++ apps/admin-x-activitypub/src/components/layout/Header/Header.tsx
@@ -3,6 +3,7 @@ import Search from '@src/components/modals/Search';
 import SearchInput from './SearchInput';
 import useActiveRoute from '@src/hooks/use-active-route';
 import {Button, Dialog, DialogContent, DialogTrigger, H1, LucideIcon} from '@tryghost/shade';
+import {useFeatureFlags} from '@src/lib/feature-flags';
 import {useLocation, useNavigate, useNavigationStack} from '@tryghost/admin-x-framework';
 
 interface HeaderTitleProps {
@@ -40,11 +41,13 @@ const Header: React.FC = () => {
     // Get page title from custom route object
     const activeRoute = useActiveRoute();
 
+    const {isEnabled} = useFeatureFlags();
+
     return (
         <div
             className='sticky top-0 z-10 bg-white px-8 dark:bg-black'>
             <div className='flex h-[102px] items-center justify-between gap-5 border-b border-gray-200 dark:border-gray-950'>
-                <HeaderTitle backIcon={location.pathname === '/search'} title={activeRoute?.pageTitle || ''} />
+                <HeaderTitle backIcon={location.pathname === '/search' || (isEnabled('feed-routes') && location.pathname.includes('/feed/'))} title={activeRoute?.pageTitle || ''} />
                 <Dialog open={isSearchOpen} onOpenChange={setIsSearchOpen}>
                     <DialogTrigger>
                         <SearchInput />
