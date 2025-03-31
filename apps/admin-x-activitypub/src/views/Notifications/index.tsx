975aeb02859144c76a7e55bc839afe9cd30607e5
Michael Barrett <mike@ghost.org>
2025-03-31 08:50:41 +0100

    remove notificationsV2 flag

--- apps/admin-x-activitypub/src/views/Notifications/index.tsx
+++ apps/admin-x-activitypub/src/views/Notifications/index.tsx
@@ -1,12 +1,1 @@
-import {useFeatureFlags} from '@src/lib/feature-flags';
-
-import NotificationsV1 from './Notifications';
-import NotificationsV2 from './NotificationsV2';
-
-const Notifications = () => {
-    const {isEnabled} = useFeatureFlags();
-
-    return isEnabled('notificationsV2') ? <NotificationsV2 /> : <NotificationsV1 />;
-};
-
-export default Notifications;
+export {default} from './NotificationsV2';
