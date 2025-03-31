4ba4dc0f99d3d8210c3522542e703a9be1504e67
Daniel Lockyer <hi@daniellockyer.com>
2025-03-31 13:36:12 +0200

    Moved @tryghost/adapter-manager to core folder
    fix https://linear.app/ghost/issue/ENG-2116/adapter-manager
    
    - this commit moves the lib code and tests to the core folder so we can
      colocate code in one place rather than splitting it out across
      packages, which increases the cognitive load and overhead

--- ghost/core/test/unit/server/services/adapter-manager/AdapterManager.test.js
+++ ghost/core/test/unit/server/services/adapter-manager/AdapterManager.test.js
@@ -1,6 +1,6 @@
 const sinon = require('sinon');
 const should = require('should');
-const AdapterManager = require('../');
+const AdapterManager = require('../../../../../core/server/services/adapter-manager/AdapterManager');
 
 class BaseMailAdapter {
     constructor() {
