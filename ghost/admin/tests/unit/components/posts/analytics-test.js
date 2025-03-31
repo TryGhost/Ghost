fac15615e6d3705be7a1df7f67ff1c979fd729a8
Fabien O'Carroll <fabien@allou.is>
2025-03-31 12:19:19 +0700

    🐛 Fixed links showing incorrect counts after refresh
    ref https://linear.app/ghost/issue/ONC-845
    
    Our UI merges links with matching titles to aggregate their counts,
    preserving only the first ID.
    
    During refresh operations, we were incorrectly updating the existing
    `this.links` array by matching on ID and merging with new API data.
    This approach lost count data for subsequent links with matching
    titles since they were already merged and removed from `this.links`.
    
    e.g.
    
     - Original links: [{title: a, count: 1}, {t: b, c: 1}, {t: a, c: 1}]
     - Merged correctly as: [{t: a, c: 2}, {t: b, c: 1}]
     - On refresh, API returns: [{t: a, c: 1}, {t: b, c: 1}, {t: a, c: 2}]
     - But updated incorrectly as: [{t: a, c: 1}, {t: b, c: 1}]
    
    The solution simplifies our approach by removing the special handling
    for existing link data and using the same flow whether it's initial
    load or refresh.
    
    This makes the code smaller, easier to reason about, and fixes counts.

--- ghost/admin/tests/unit/components/posts/analytics-test.js
+++ ghost/admin/tests/unit/components/posts/analytics-test.js
@@ -51,5 +51,81 @@ describe('Unit: Component: posts/analytics', function () {
             expect(instance.links[1].count.clicks).to.equal(3);
             expect(instance.links[1].id).to.equal(1);
         });
+
+        it('Correctly handles updates to `this.links`', function () {
+            const instance = Object.create(Analytics.prototype);
+            instance.utils = {
+                cleanTrackedUrl(url, title) {
+                    if (title) {
+                        return url.split('//')[1];
+                    }
+                    return url;
+                }
+            };
+
+            const originalLinks = [{
+                id: 1,
+                link: {
+                    to: 'https://lone.com'
+                },
+                count: {
+                    clicks: 1
+                }
+            }, {
+                id: 2,
+                link: {
+                    to: 'https://duplicate.com'
+                },
+                count: {
+                    clicks: 1
+                }
+            }, {
+                id: 3,
+                link: {
+                    to: 'https://duplicate.com'
+                },
+                count: {
+                    clicks: 1
+                }
+            }];
+
+            instance.updateLinkData(originalLinks);
+
+            const updatedLinks = [{
+                id: 1,
+                link: {
+                    to: 'https://lone.com'
+                },
+                count: {
+                    clicks: 1
+                }
+            }, {
+                id: 2,
+                link: {
+                    to: 'https://duplicate.com'
+                },
+                count: {
+                    clicks: 1
+                }
+            }, {
+                id: 3,
+                link: {
+                    to: 'https://duplicate.com'
+                },
+                count: {
+                    clicks: 3
+                }
+            }];
+
+            instance.updateLinkData(updatedLinks);
+
+            expect(instance.links.length).to.equal(2);
+
+            expect(instance.links[0].count.clicks).to.equal(4);
+            expect(instance.links[0].id).to.equal(2);
+
+            expect(instance.links[1].count.clicks).to.equal(1);
+            expect(instance.links[1].id).to.equal(1);
+        });
     });
 });
