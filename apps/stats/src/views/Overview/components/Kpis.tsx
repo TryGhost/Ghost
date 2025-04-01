d0438201dce6143c62b8fd61d99a2dbbac4a5f42
Peter Zimon <peter.zimon@gmail.com>
2025-04-01 08:48:56 +0200

    Added first chart

--- apps/stats/src/views/Overview/components/Kpis.tsx
+++ apps/stats/src/views/Overview/components/Kpis.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Card, CardContent, CardHeader, Tabs, TabsContent, TabsList, TabsTrigger} from '@tryghost/shade';
+import {Card, CardContent, CardHeader, ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent, Recharts, Tabs, TabsContent, TabsList, TabsTrigger} from '@tryghost/shade';
+import {useQuery} from '@tinybirdco/charts';
 
 interface KpiTabTriggerProps extends React.ComponentProps<typeof TabsTrigger> {
     children: React.ReactNode;
@@ -28,6 +29,42 @@ const KpiTabValue: React.FC<KpiTabValueProps> = ({label, value}) => {
 };
 
 const Kpis:React.FC = () => {
+    const params = {
+        site_uuid: '',
+        date_from: '2025-03-01',
+        date_to: '2025-03-31'
+        // ...additionalParams
+    };
+
+    const {data, loading} = useQuery({
+        endpoint: 'https://api.tinybird.co/v0/pipes/api_kpis__v7.json',
+        token: '',
+        params
+    });
+
+    // const chartData = [
+    //     {month: 'January', desktop: 186},
+    //     {month: 'February', desktop: 305},
+    //     {month: 'March', desktop: 237},
+    //     {month: 'April', desktop: 73},
+    //     {month: 'May', desktop: 209},
+    //     {month: 'June', desktop: 214}
+    // ];
+
+    const chartData = data?.map(item => ({
+        date: item.date,
+        visits: item.visits
+    }));
+
+    const chartConfig = {
+        visits: {
+            label: 'Visits',
+            color: 'hsl(var(--chart-1))'
+        }
+    } satisfies ChartConfig;
+
+    // console.log('Transformed Data:', JSON.stringify(chartData, null, 2));
+
     return (
         <Card className='col-span-2'>
             <Tabs defaultValue="visits" variant='underline'>
@@ -49,7 +86,38 @@ const Kpis:React.FC = () => {
                 </CardHeader>
                 <CardContent className='min-h-[20vw]'>
                     <TabsContent value="visits">
-                        Visits chart
+                        {loading ? 'Loading' :
+                            <ChartContainer className='max-h-[20vw] w-full' config={chartConfig}>
+                                <Recharts.LineChart
+                                    data={chartData}
+                                    margin={{
+                                        left: 12,
+                                        right: 12
+                                    }}
+                                    accessibilityLayer
+                                >
+                                    <Recharts.CartesianGrid vertical={false} />
+                                    <Recharts.XAxis
+                                        axisLine={false}
+                                        dataKey="date"
+                                        // tickFormatter={value => value.slice(0, 3)}
+                                        tickLine={false}
+                                        tickMargin={8}
+                                    />
+                                    <ChartTooltip
+                                        content={<ChartTooltipContent hideLabel />}
+                                        cursor={false}
+                                    />
+                                    <Recharts.Line
+                                        dataKey="visits"
+                                        dot={false}
+                                        stroke="var(--color-visits)"
+                                        strokeWidth={2}
+                                        type='linear'
+                                    />
+                                </Recharts.LineChart>
+                            </ChartContainer>
+                        }
                     </TabsContent>
                     <TabsContent value="views">
                         Pageviews chart
