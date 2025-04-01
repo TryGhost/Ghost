c5f6530e66230f80cb1717e21d3ee16a862e04b4
Daniel Lockyer <hi@daniellockyer.com>
2025-04-01 14:15:26 +0200

    Moved @tryghost/email-content-generator to service lib folder
    fixes https://linear.app/ghost/issue/ENG-2119/email-content-generator
    
    - this commit moves the lib code and tests to the core folder so we can
      colocate code in one place rather than splitting it out across
      packages, which increases the cognitive load and overhead
    - I'm not entirely happy about the location of this, but it's shared
      between 2 services
    - at the very least, I'd rather get the file moved over to the core
      folder and then we can rejiggle things around afterwards

--- ghost/core/test/unit/server/services/lib/email-content-generator.test.js
+++ ghost/core/test/unit/server/services/lib/email-content-generator.test.js
@@ -1,7 +1,7 @@
 const assert = require('assert/strict');
 const path = require('path');
 
-const EmailContentGenerator = require('../index');
+const EmailContentGenerator = require('../../../../../core/server/services/lib/EmailContentGenerator');
 
 describe('Mail: EmailContentGenerator', function () {
     it('generate welcome', async function () {
