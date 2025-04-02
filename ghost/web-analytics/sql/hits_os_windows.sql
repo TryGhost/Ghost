5e93375e91de70dfdef9a5b6ff8e2bc499e7701d
Chris Raible <chris@ghost.org>
2025-04-02 01:21:16 +0000

    Added tinybird tests of os filter for all endpoints
    ref https://linear.app/ghost/issue/ANAL-194/automated-tests-for-all-queries-including-every-filter-plus-one-other
    
    - This commit adds tests for all Tinybird endpoints with the `os` filter applied, filtering to `windows`
    - This is part of a general effort to add automated tests to our Tinybird endpoints so we can be more confident in making changes

---
+++ ghost/web-analytics/sql/hits_os_windows.sql
@@ -0,0 +1,10 @@
+select
+    row_number() over () as row_num,
+    dense_rank() over (order by session_id) as session_num,
+    *
+from (
+    select timestamp, session_id, browser, os, device, location, pathname
+    from _mv_hits__v${TB_VERSION:-0}
+    where os = 'windows'
+    order by session_id, timestamp
+)
