cba9fcde6708e8502b061fa54fedf7296ced161d
Chris Raible <chris@ghost.org>
2025-04-02 03:33:12 +0000

    Fixed hits_location_gb.sql query

--- ghost/web-analytics/sql/hits_location_gb.sql
+++ ghost/web-analytics/sql/hits_location_gb.sql
@@ -2,5 +2,5 @@ select h.timestamp, h.session_id, h.browser, h.os, h.device, h.location, h.pathn
 from _mv_hits__v${TB_VERSION:-0} h
     join mv_session_data__v${TB_VERSION:-0} s
         on _mv_hits__v${TB_VERSION:-0}.session_id = mv_session_data__v${TB_VERSION:-0}.session_id
-where os = 'windows'
+where location = 'GB'
 order by timestamp
