a8ed20e07d76a29785d31595801a2ed89efa3d58
Chris Raible <chris@ghost.org>
2025-04-01 20:48:13 -0700

    Added tinybird tests for the location filter for all endpoints (#22751)
    ref
    https://linear.app/ghost/issue/ANAL-194/automated-tests-for-all-queries-including-every-filter-plus-one-other
    
    - This commit adds Tinybird fixture tests for filtering by location for
    all endpoints that didn't previously have these tests
    - This is part of an effort to generally improve our test coverage on
    our Tinybird endpoints so that we can more confidently make changes to
    them without breaking other endpoints or filter combinations
    - It also makes some minor updates to the `.sql` queries, to use the
    table aliases more consistently, as suggested by coderabbit.

--- ghost/web-analytics/sql/hits_location_gb.sql
+++ ghost/web-analytics/sql/hits_location_gb.sql
@@ -2,5 +2,5 @@ select h.timestamp, h.session_id, h.browser, h.os, h.device, h.location, h.pathn
 from _mv_hits__v${TB_VERSION:-0} h
     join mv_session_data__v${TB_VERSION:-0} s
         on h.session_id = s.session_id
-where h.browser = 'chrome'
+where h.location = 'GB'
 order by h.timestamp
