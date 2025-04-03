2fa15c2eb80637ef4d73a5bd61c6d1d5487b56c2
Sag <guptazy@gmail.com>
2025-04-03 13:34:06 +0530

    Added site title validation in Ghost settings (#22781)
    ref https://linear.app/ghost/issue/AP-1021
    
    - on signup, we validate the title sitle: it must be present, be between
    3 and 63 characters and should not contain special characters such as
    emojis
    - we have now added the same validation in Ghost settings, to avoid
    empty or invalid site titles

--- apps/admin-x-settings/test/acceptance/general/titleAndDescription.test.ts
+++ apps/admin-x-settings/test/acceptance/general/titleAndDescription.test.ts
@@ -38,4 +38,37 @@ test.describe('Title and description settings', async () => {
             ]
         });
     });
+
+    test('Shows an error if the title is invalid and does not save', async ({page}) => {
+        const {lastApiRequests} = await mockApi({page, requests: {
+            ...globalDataRequests,
+            editSettings: {method: 'PUT', path: /^\/settings\/$/, response: updatedSettingsResponse([
+                {key: 'title', value: 'New Site Title'}
+            ])}
+        }});
+
+        await page.goto('/');
+
+        const section = page.getByTestId('title-and-description');
+        await expect(section.getByText('Test Site')).toHaveCount(1);
+
+        await section.getByRole('button', {name: 'Edit'}).click();
+
+        // Empty title
+        await section.getByLabel('Site title').fill('');
+        await section.getByRole('button', {name: 'Save'}).click();
+        await expect(section.getByText('Please enter a site title.')).toHaveCount(1);
+
+        // Title is too short
+        await section.getByLabel('Site title').fill('ab');
+        await section.getByRole('button', {name: 'Save'}).click();
+        await expect(section.getByText('Please use a site title longer than 3 characters.')).toHaveCount(1);
+
+        // Title has invalid characters
+        await section.getByLabel('Site title').fill('Title with emoji: 🕵️‍♂️');
+        await section.getByRole('button', {name: 'Save'}).click();
+        await expect(section.getByText('Please use a site title without special characters.')).toHaveCount(1);
+
+        expect(lastApiRequests.editSettings).toBeUndefined();
+    });
 });
