ade4ebecea31cfc43e4ba3510e3a5e165dcc1b60
Peter Zimon <peter.zimon@gmail.com>
2025-04-10 16:51:02 +0200

    Update Traffic Analytics app structure
    ref PROD-216

--- apps/stats/src/routes.tsx
+++ apps/stats/src/routes.tsx
@@ -1,4 +1,6 @@
-import Overview from './views/Overview';
+import Locations from './views/Stats/Locations';
+import Sources from './views/Stats/Sources';
+import Web from './views/Stats/Web';
 import {RouteObject} from '@tryghost/admin-x-framework';
 
 export const APP_ROUTE_PREFIX = '/stats-x';
@@ -7,6 +9,18 @@ export const routes: RouteObject[] = [
     {
         path: '',
         index: true,
-        element: <Overview />
+        element: <Web />
+    },
+    {
+        path: '/web/',
+        element: <Web />
+    },
+    {
+        path: '/sources/',
+        element: <Sources />
+    },
+    {
+        path: '/locations/',
+        element: <Locations />
     }
 ];
---
+++ apps/stats/src/views/Stats/Sources.tsx
@@ -0,0 +1,16 @@
+import Header from '@src/components/layout/Header';
+import React from 'react';
+import StatsLayout from './layout/StatsLayout';
+
+const Sources:React.FC = () => {
+    return (
+        <StatsLayout>
+            <Header>Sources</Header>
+            <div>
+                Sources
+            </div>
+        </StatsLayout>
+    );
+};
+
+export default Sources;
