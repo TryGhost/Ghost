871552500fd68dcae8c3ddfeed57d6b458d45ab9
Hannah Wolfe <github.erisds@gmail.com>
2025-04-10 13:45:29 +0100

    Improved handling of TwoFactorAuthentication error (#22856)
    ref
    https://linear.app/ghost/issue/ENG-2074/adjust-verification-copy-if-2fa-is-enabled-for-all-logins
    ref
    https://linear.app/ghost/issue/ENG-2134/editor-reauthentication-flow-doesnt-work-how-i-expect-and-is-untested
    
    - Whilst working towards being able to do better differentiation of what
    2fa flow we are in based on the error response from the server I came
    across the re-authentication unit test file
    - This file has 3 tests, 2 are essentially testing the same thing, and
    all 3 tests stub out too much and don't directly test any other code
    except the stubs
    - The first two tests are verifying the isTwoFactorAuthenticationError
    logic. In moving that to the ajax service integration tests, I found
    issues with the way the payload is handled
    - This change therefore also includes a fix to ensure the payload is
    always JSON that we can inspect
    - I've deleted the re-authentication tests because they don't actually
    test anything
    - I did attempt to replace the intention of testing the modal flow, with
    an integration or acceptance test, but I couldn't find any way to do
    this quickly
    - I've raised
    https://linear.app/ghost/issue/ENG-2134/editor-reauthentication-flow-doesnt-work-how-i-expect-and-is-untested
    to come back to later

--- ghost/admin/app/services/ajax.js
+++ ghost/admin/app/services/ajax.js
@@ -23,6 +23,19 @@ function isJSONContentType(header) {
     return header.indexOf(JSON_CONTENT_TYPE) === 0;
 }
 
+function getJSONPayload(payload) {
+    // ember-simple-auth prevents ember-ajax parsing response as JSON but
+    // we need a JSON object to test against
+    if (typeof payload === 'string') {
+        try {
+            payload = JSON.parse(payload);
+        } catch (e) {
+            // do nothing
+        }
+    }
+    return payload;
+}
+
 /* Version mismatch error */
 
 export class VersionMismatchError extends AjaxError {
@@ -181,6 +194,7 @@ export function isEmailError(errorOrStatus, payload) {
 /* 2FA required error */
 export class TwoFactorTokenRequiredError extends AjaxError {
     constructor(payload) {
+        payload = getJSONPayload(payload);
         super(payload, '2nd factor verification is required to sign in.');
     }
 }
@@ -188,24 +202,13 @@ export class TwoFactorTokenRequiredError extends AjaxError {
 export function isTwoFactorTokenRequiredError(errorOrStatus, payload) {
     const tokenRequiredCode = '2FA_TOKEN_REQUIRED';
 
-    // ember-simple-auth prevents ember-ajax parsing response as JSON but
-    // we need a JSON object to test against
-    if (typeof payload === 'string') {
-        try {
-            payload = JSON.parse(payload);
-        } catch (e) {
-            // do nothing
-        }
-    }
-
     if (isAjaxError(errorOrStatus)) {
         return errorOrStatus instanceof TwoFactorTokenRequiredError || getErrorCode(errorOrStatus) === tokenRequiredCode;
     } else {
+        payload = getJSONPayload(payload);
         return get(payload || {}, 'errors.firstObject.code') === tokenRequiredCode;
     }
 }
-
-/* end: custom error types */
 
 export class AcceptedResponse {
     constructor(data) {
