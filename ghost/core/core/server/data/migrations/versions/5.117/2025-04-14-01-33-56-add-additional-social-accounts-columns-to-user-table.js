f15a3d330ba5ad74be3da32b2097e87b9bb89098
Ronald Langeveld <hi@ronaldlangeveld.com>
2025-04-14 10:40:48 +0900

    Added migration for threads social account

---
+++ ghost/core/core/server/data/migrations/versions/5.117/2025-04-14-01-33-56-add-additional-social-accounts-columns-to-user-table.js
@@ -0,0 +1,44 @@
+const {combineNonTransactionalMigrations, createAddColumnMigration} = require('../../utils');
+
+module.exports = combineNonTransactionalMigrations(
+    createAddColumnMigration('users', 'threads', {
+        type: 'string',
+        maxlength: 1000,
+        nullable: true
+    })
+    // createAddColumnMigration('users', 'bluesky', {
+    //     type: 'string',
+    //     maxlength: 1000,
+    //     nullable: true
+    // }),
+
+    // createAddColumnMigration('users', 'mastodon', {
+    //     type: 'string',
+    //     maxlength: 1000,
+    //     nullable: true
+    // }),
+
+    // createAddColumnMigration('users', 'tiktok', {
+    //     type: 'string',
+    //     maxlength: 1000,
+    //     nullable: true
+    // }),
+
+    // createAddColumnMigration('users', 'youtube', {
+    //     type: 'string',
+    //     maxlength: 1000,
+    //     nullable: true
+    // }),
+
+    // createAddColumnMigration('users', 'instagram', {
+    //     type: 'string',
+    //     maxlength: 1000,
+    //     nullable: true
+    // }),
+
+    // createAddColumnMigration('users', 'linkedin', {
+    //     type: 'string',
+    //     maxlength: 1000,
+    //     nullable: true
+    // })
+);
