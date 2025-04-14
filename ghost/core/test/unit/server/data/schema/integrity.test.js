12b81690bb54d902e324b4b86e75b35ab8bfa20e
Ronald Langeveld <hi@ronaldlangeveld.com>
2025-04-14 12:11:05 +0900

    Only migrate threads for now

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '9ab054b9030af79fdf24e6e9a4270b55';
+    const currentSchemaHash = '-1af9eb207adece2c8d021e2c61b8f587';
     const currentFixturesHash = '0e26f9262320630078b2dcc231db350c';
     const currentSettingsHash = '5eb97fee379c1d0420de58957d29fe26';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
