4db86a63d2feee06faceabe27837511cc4c66e44
Ronald Langeveld <hi@ronaldlangeveld.com>
2025-04-14 12:15:37 +0900

    Revert "Only migrate threads for now"
    This reverts commit 12b81690bb54d902e324b4b86e75b35ab8bfa20e.

--- ghost/core/core/server/data/migrations/versions/5.117/2025-04-14-02-36-30-add-additional-social-accounts-columns-to-user-table.js
+++ ghost/core/core/server/data/migrations/versions/5.117/2025-04-14-02-36-30-add-additional-social-accounts-columns-to-user-table.js
@@ -1,8 +1,38 @@
 const {combineNonTransactionalMigrations, createAddColumnMigration} = require('../../utils');
 module.exports = combineNonTransactionalMigrations(
     createAddColumnMigration('users', 'threads', {
-        type: 'string',
-        maxlength: 2000,
+        type: 'text',
+        maxlength: 65535,
+        nullable: true
+    }),
+    createAddColumnMigration('users', 'bluesky', {
+        type: 'text',
+        maxlength: 65535,
+        nullable: true
+    }),
+    createAddColumnMigration('users', 'mastodon', {
+        type: 'text',
+        maxlength: 65535,
+        nullable: true
+    }),
+    createAddColumnMigration('users', 'tiktok', {
+        type: 'text',
+        maxlength: 65535,
+        nullable: true
+    }),
+    createAddColumnMigration('users', 'youtube', {
+        type: 'text',
+        maxlength: 65535,
+        nullable: true
+    }),
+    createAddColumnMigration('users', 'instagram', {
+        type: 'text',
+        maxlength: 65535,
+        nullable: true
+    }),
+    createAddColumnMigration('users', 'linkedin', {
+        type: 'text',
+        maxlength: 65535,
         nullable: true
     })
 );
