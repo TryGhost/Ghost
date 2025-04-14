b7b2f91a7de0ee529649ca3854fd328bdad084fe
Ronald Langeveld <hi@ronaldlangeveld.com>
2025-04-15 07:53:09 +0900

    Changed maxlength to 191

--- ghost/core/core/server/data/migrations/versions/5.117/2025-04-14-02-36-30-add-additional-social-accounts-columns-to-user-table.js
+++ ghost/core/core/server/data/migrations/versions/5.117/2025-04-14-02-36-30-add-additional-social-accounts-columns-to-user-table.js
@@ -1,38 +1,38 @@
 const {combineNonTransactionalMigrations, createAddColumnMigration} = require('../../utils');
 module.exports = combineNonTransactionalMigrations(
     createAddColumnMigration('users', 'threads', {
-        type: 'text',
-        maxlength: 65535,
+        type: 'string',
+        maxlength: 191,
         nullable: true
     }),
     createAddColumnMigration('users', 'bluesky', {
-        type: 'text',
-        maxlength: 65535,
+        type: 'string',
+        maxlength: 191,
         nullable: true
     }),
     createAddColumnMigration('users', 'mastodon', {
-        type: 'text',
-        maxlength: 65535,
+        type: 'string',
+        maxlength: 191,
         nullable: true
     }),
     createAddColumnMigration('users', 'tiktok', {
-        type: 'text',
-        maxlength: 65535,
+        type: 'string',
+        maxlength: 191,
         nullable: true
     }),
     createAddColumnMigration('users', 'youtube', {
-        type: 'text',
-        maxlength: 65535,
+        type: 'string',
+        maxlength: 191,
         nullable: true
     }),
     createAddColumnMigration('users', 'instagram', {
-        type: 'text',
-        maxlength: 65535,
+        type: 'string',
+        maxlength: 191,
         nullable: true
     }),
     createAddColumnMigration('users', 'linkedin', {
-        type: 'text',
-        maxlength: 65535,
+        type: 'string',
+        maxlength: 191,
         nullable: true
     })
 );
