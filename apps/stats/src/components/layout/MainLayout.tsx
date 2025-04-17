51b20156f4f410f2bd03916599ce0fa04a2ed98f
Peter Zimon <peter.zimon@gmail.com>
2025-04-17 15:47:02 +0200

    Fixed jumping scrollbar in Safari

--- apps/stats/src/components/layout/MainLayout.tsx
+++ apps/stats/src/components/layout/MainLayout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MainLayout: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({children, ...props}) => {
     return (
-        <div className='h-screen w-full overflow-y-auto'>
+        <div className='h-screen w-full overflow-y-scroll'>
             <div className='relative mx-auto flex h-screen max-w-page flex-col' {...props}>
                 {children}
             </div>
