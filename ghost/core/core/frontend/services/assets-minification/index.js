03692a2214ec9772a72c8b5c9aeaf9ee1397b4c9
Steve Larson <9larsons@gmail.com>
2025-04-22 11:27:25 -0500

    Minified ghost-stats script
    ref https://linear.app/ghost/issue/PROD-640/minify-ghost-stats-script

--- ghost/core/core/frontend/services/assets-minification/index.js
+++ ghost/core/core/frontend/services/assets-minification/index.js
@@ -2,15 +2,18 @@ const AdminAuthAssets = require('./AdminAuthAssets');
 const CardAssets = require('./CardAssets');
 const CommentCountsAssets = require('./CommentCountsAssets');
 const MemberAttributionAssets = require('./MemberAttributionAssets');
+const GhostStatsAssets = require('./GhostStatsAssets');
 
 const adminAuthAssets = new AdminAuthAssets();
 const cardAssets = new CardAssets();
 const commentCountsAssets = new CommentCountsAssets();
 const memberAttributionAssets = new MemberAttributionAssets();
+const ghostStatsAssets = new GhostStatsAssets();
 
 module.exports = {
     adminAuthAssets,
     cardAssets,
     commentCountsAssets,
-    memberAttributionAssets
+    memberAttributionAssets,
+    ghostStatsAssets
 };
