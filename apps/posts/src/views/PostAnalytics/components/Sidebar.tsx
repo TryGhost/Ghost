fe5c8d9630ed4fa8e870443a2177abfe66a10a1c
Peter Zimon <peter.zimon@gmail.com>
2025-04-22 11:48:54 +0200

    Added menu handling

--- apps/posts/src/views/PostAnalytics/components/Sidebar.tsx
+++ apps/posts/src/views/PostAnalytics/components/Sidebar.tsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import {LucideIcon, RightSidebarMenu, RightSidebarMenuLink} from '@tryghost/shade';
-import {useLocation, useNavigate} from '@tryghost/admin-x-framework';
+import {useLocation, useNavigate, useParams} from '@tryghost/admin-x-framework';
 
 const Sidebar:React.FC = () => {
     const navigate = useNavigate();
     const location = useLocation();
+    const {postId} = useParams();
 
     return (
         <div className='grow border-l py-8 pl-6 pr-0'>
             <RightSidebarMenu className='sticky top-[134px]'>
-                <RightSidebarMenuLink active={location.pathname === '/' || location.pathname === '/web/'} onClick={() => {
-                    navigate('/');
+                <RightSidebarMenuLink onClick={() => {
+                    navigate(`/posts/analytics/${postId}`, {crossApp: true});
                 }}>
                     <LucideIcon.LayoutTemplate size={16} strokeWidth={1.25} />
                     Overview
                 </RightSidebarMenuLink>
-                <RightSidebarMenuLink active={location.pathname === '/analytics/postid/web'} onClick={() => {
-                    navigate('/sources/');
+                <RightSidebarMenuLink active={location.pathname === `/analytics/${postId}/web`} onClick={() => {
+                    navigate(`/analytics/${postId}/web`);
                 }}>
                     <LucideIcon.MousePointer size={16} strokeWidth={1.25} />
                     Web
