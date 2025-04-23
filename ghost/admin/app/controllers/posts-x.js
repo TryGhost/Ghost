888753d205a73f905391b9827c0c27167876bfc6
Peter Zimon <peter.zimon@gmail.com>
2025-04-23 07:50:06 +0200

    Redirect from Post Analytics/Web if not admin

--- ghost/admin/app/controllers/posts-x.js
+++ ghost/admin/app/controllers/posts-x.js
@@ -1,3 +1,10 @@
 import Controller from '@ember/controller';
+import {inject} from 'ghost-admin/decorators/inject';
+import {inject as service} from '@ember/service';
 
-export default class PostsXController extends Controller {}
+export default class PostsXController extends Controller {
+    @service feature;
+    @service session;
+
+    @inject config;
+}
