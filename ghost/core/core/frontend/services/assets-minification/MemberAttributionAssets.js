5705c6cfa1c0d73fe364cf75170d9b059d235bd0
Steve Larson <9larsons@gmail.com>
2025-04-23 09:30:52 -0500

    Reverted bundled frontend scripts changes (#23005)
    ref https://ghost.slack.com/archives/C07HTEJMR2R/p1745413896875469
    
    Oddly, this passed just fine, but eventually failed in later commits
    with the Canary build with a clear issue in the path: `EACCES:
    permission denied, open
    ‘/home/ghost/core/frontend/src/ghost-stats/ghost-stats.bundled.js’`
    (should be /core/core/).
    
    The minified is what should read the bundler file. At any rate, it's
    been called to my attention that there's an existing issue
    (https://github.com/TryGhost/Ghost/pull/21866) to move these
    runtime-minified assets to build-time minification. The reason some of
    these assets (like card-assets) were built at runtime is so they can be
    rebuilt when applying new themes, as they can be dependent on them.
    
    The best approach here is to resolve the open PR for build-time
    minification, then re-apply these changes in spirit with that same
    approach to the ghost-stats and member-attribution scripts.

--- ghost/core/core/frontend/services/assets-minification/MemberAttributionAssets.js
+++ ghost/core/core/frontend/services/assets-minification/MemberAttributionAssets.js
@@ -2,8 +2,6 @@ const path = require('path');
 const config = require('../../../shared/config');
 const Minifier = require('./Minifier');
 const AssetsMinificationBase = require('./AssetsMinificationBase');
-const {bundleAsset} = require('../assets-bundling/bundle-asset');
-const debug = require('@tryghost/debug')('member-attribution-assets');
 
 module.exports = class MemberAttributionAssets extends AssetsMinificationBase {
     constructor(options = {}) {
@@ -22,7 +20,7 @@ module.exports = class MemberAttributionAssets extends AssetsMinificationBase {
      */
     generateGlobs() {
         return {
-            'member-attribution.min.js': 'member-attribution.bundled.js'
+            'member-attribution.min.js': '*.js'
         };
     }
 
@@ -40,21 +38,8 @@ module.exports = class MemberAttributionAssets extends AssetsMinificationBase {
      * @returns {Promise<void>}
      */
     async load() {
-        try {
-            // Step 1: Bundle the file to resolve imports
-            debug('Bundling member-attribution.js');
-            await bundleAsset({
-                srcFile: path.join('member-attribution', 'member-attribution.js')
-            });
-            
-            // Step 2: Minify the bundled file
-            debug('Minifying bundled member-attribution.js');
-            const globs = this.generateGlobs();
-            const replacements = this.generateReplacements();
-            await this.minify(globs, {replacements});
-        } catch (error) {
-            debug('Error loading member-attribution assets:', error);
-            throw error;
-        }
+        const globs = this.generateGlobs();
+        const replacements = this.generateReplacements();
+        await this.minify(globs, {replacements});
     }
 };
