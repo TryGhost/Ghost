1045c09967ee5628036dfa98a7b79571afb159c8
Steve Larson <9larsons@gmail.com>
2025-04-23 12:30:47 -0500

    Updated file structure, removed minifier assets that were on-demand

--- ghost/core/core/frontend/services/assets-minification/index.js
+++ ghost/core/core/frontend/services/assets-minification/index.js
@@ -1,19 +1,6 @@
-const AdminAuthAssets = require('./AdminAuthAssets');
 const CardAssets = require('./CardAssets');
-const CommentCountsAssets = require('./CommentCountsAssets');
-const MemberAttributionAssets = require('./MemberAttributionAssets');
-const GhostStatsAssets = require('./GhostStatsAssets');
-
-const adminAuthAssets = new AdminAuthAssets();
 const cardAssets = new CardAssets();
-const commentCountsAssets = new CommentCountsAssets();
-const memberAttributionAssets = new MemberAttributionAssets();
-const ghostStatsAssets = new GhostStatsAssets();
 
 module.exports = {
-    adminAuthAssets,
-    cardAssets,
-    commentCountsAssets,
-    memberAttributionAssets,
-    ghostStatsAssets
+    cardAssets
 };
