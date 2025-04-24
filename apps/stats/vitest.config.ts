e9181a972288a68a2213e9673af0c6fe6d9ed13c
Steve Larson <9larsons@gmail.com>
2025-04-24 10:08:50 -0500

    Commented out aliases that are unused currenlty

--- apps/stats/vitest.config.ts
+++ apps/stats/vitest.config.ts
@@ -16,9 +16,9 @@ export default defineConfig({
     resolve: {
         alias: {
             '@src': resolve(__dirname, './src'),
-            '@assets': resolve(__dirname, './src/assets'),
+            // '@assets': resolve(__dirname, './src/assets'),
             '@components': resolve(__dirname, './src/components'),
-            '@hooks': resolve(__dirname, './src/hooks'),
+            // '@hooks': resolve(__dirname, './src/hooks'),
             '@utils': resolve(__dirname, './src/utils'),
             '@views': resolve(__dirname, './src/views')
         }
