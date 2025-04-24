b9c92c4788d21fed9e6618b3e7f71b3b51aa563c
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-04-24 17:27:36 +0800

    Cleaned up file size limitations in ActivityPub

--- apps/admin-x-activitypub/src/utils/image.ts
+++ apps/admin-x-activitypub/src/utils/image.ts
@@ -1,3 +1,9 @@
+export const MAX_FILE_SIZE = 1 * 1024 * 1024; // 1MB limit
+export const FILE_SIZE_ERROR_MESSAGE = 'Image must be less than 1MB in size.';
+
+export const PROFILE_MAX_DIMENSIONS = {width: 400, height: 400};
+export const COVER_MAX_DIMENSIONS = {width: 4000, height: 3000};
+
 /**
  * Checks if an image file's dimensions are within specified maximum limits
  */
