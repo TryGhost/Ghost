d45a3f1418a12a5caafeab21c04761d2905a68fe
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-04-24 20:26:44 +0800

    Updated the copy for Threads and Bluesky share settings (#23024)
    ref AP-1051
    
    - added explainer about the handle change not being supported on those
    platforms

--- apps/admin-x-activitypub/src/views/Preferences/components/BlueskySharing.tsx
+++ apps/admin-x-activitypub/src/views/Preferences/components/BlueskySharing.tsx
@@ -50,6 +50,7 @@ const BlueskySharing: React.FC = () => {
                 {!enabled ?
                     <div className='mt-3 flex flex-col gap-5'>
                         <p className='text-base'>Connect your account to Bluesky to share your content directly to your Bluesky profile. When enabled, new posts you create can be automatically shared to your Bluesky account, expanding your reach across platforms.</p>
+                        <p className='-mt-2 text-base'>Bluesky doesn’t support username changes. Make sure you’re happy with your social web handle before connecting.</p>
                         <Button className='h-10 text-base' disabled={loading} variant='secondary' onClick={handleEnable}>
                             {!loading ?
                                 <><svg height="32" role="img" viewBox="0 0 24 24" width="32" xmlns="http://www.w3.org/2000/svg"><path d="M12 10.8c-1.087 -2.114 -4.046 -6.053 -6.798 -7.995C2.566 0.944 1.561 1.266 0.902 1.565 0.139 1.908 0 3.08 0 3.768c0 0.69 0.378 5.65 0.624 6.479 0.815 2.736 3.713 3.66 6.383 3.364 0.136 -0.02 0.275 -0.039 0.415 -0.056 -0.138 0.022 -0.276 0.04 -0.415 0.056 -3.912 0.58 -7.387 2.005 -2.83 7.078 5.013 5.19 6.87 -1.113 7.823 -4.308 0.953 3.195 2.05 9.271 7.733 4.308 4.267 -4.308 1.172 -6.498 -2.74 -7.078a8.741 8.741 0 0 1 -0.415 -0.056c0.14 0.017 0.279 0.036 0.415 0.056 2.67 0.297 5.568 -0.628 6.383 -3.364 0.246 -0.828 0.624 -5.79 0.624 -6.478 0 -0.69 -0.139 -1.861 -0.902 -2.206 -0.659 -0.298 -1.664 -0.62 -4.3 1.24C16.046 4.748 13.087 8.687 12 10.8Z" fill="#0385FF" strokeWidth="1"></path></svg>
@@ -59,7 +60,7 @@ const BlueskySharing: React.FC = () => {
                         </Button>
                     </div> :
                     <>
-                        <p className='mt-3 text-base'>Your content is now live on Bluesky! We&apos;ve created a dedicated Bluesky account shown below. This account automatically displays everything you publish on Ghost.</p>
+                        <p className='mt-3 text-base'>Your content is now live on Bluesky! We&apos;ve created a dedicated Bluesky profile shown below. Allow a few minutes for your Bluesky profile to activate before posts appear. This profile automatically displays everything you publish on Ghost.</p>
                         <div className='mt-6 flex flex-col items-center gap-4 rounded-lg border border-gray-200 p-8 dark:border-gray-950'>
                             <div className='relative'>
                                 <APAvatar
