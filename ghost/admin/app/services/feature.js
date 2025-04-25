c1b91cb218231e4cf9968a5e244af1c5686827b0
Hannah Wolfe <github.erisds@gmail.com>
2025-04-25 14:19:15 +0100

    Removed deprecated labs flags from admin (#23043)
    ref https://github.com/TryGhost/Ghost/commit/b2d0187eb1472b249efc0d3eb3c5a9bf7fb3473a
    ref https://github.com/TryGhost/Ghost/commit/50db4075d6a9b1e88c147102bc8733e43298c0ba
    
    - These flags were already removed from the labs system
    - This file in admin was missed, so cleaning up

--- ghost/admin/app/services/feature.js
+++ ghost/admin/app/services/feature.js
@@ -65,7 +65,6 @@ export default class FeatureService extends Service {
     @feature('emailCustomization') emailCustomization;
     @feature('i18n') i18n;
     @feature('announcementBar') announcementBar;
-    @feature('signupCard') signupCard;
     @feature('mailEvents') mailEvents;
     @feature('collectionsCard') collectionsCard;
     @feature('importMemberTier') importMemberTier;
@@ -74,7 +73,6 @@ export default class FeatureService extends Service {
     @feature('editorExcerpt') editorExcerpt;
     @feature('contentVisibility') contentVisibility;
     @feature('contentVisibilityAlpha') contentVisibilityAlpha;
-    @feature('postsX') postsX;
     @feature('trafficAnalyticsAlpha') trafficAnalyticsAlpha;
     @feature('trafficAnalytics') trafficAnalytics;
 
