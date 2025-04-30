4a2c608eb67fcbd304fee38d46311c3f4904d951
Chris Raible <chris@ghost.org>
2025-04-30 13:51:12 -0700

    fixup! Moved NX_DAEMON env variable into template

--- apps/admin-x-framework/src/hooks.ts
+++ apps/admin-x-framework/src/hooks.ts
@@ -3,4 +3,3 @@ export {default as useForm} from './hooks/useForm';
 export type {Dirtyable, ErrorMessages, FormHook, OkProps, SaveHandler, SaveState} from './hooks/useForm';
 export {default as useHandleError} from './hooks/useHandleError';
 export {usePermission} from './hooks/usePermissions';
-console.log('hello')
