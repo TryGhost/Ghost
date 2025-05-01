1ec250901052d88498c825380cf4cff6a79e531a
Steve Larson <9larsons@gmail.com>
2025-05-01 13:46:42 -0500

    Wired up member counts to stats growth page (#23139)
    ref https://linear.app/ghost/issue/PROD-658/
    
    The growth tab currently has fixture data for demo purposes. This wires
    up the member counts endpoint used by the Admin Dashboard. We had to
    make a small adjustment to allow for a start date, as it was previously
    always looking back 91 days. We will probably want to make some
    additional adjustments to this down the road.

--- apps/admin-x-framework/src/api/stats.ts
+++ apps/admin-x-framework/src/api/stats.ts
@@ -15,11 +15,37 @@ export type TopContentResponseType = {
     meta: Meta;
 }
 
+export type MemberStatusItem = {
+    date: string;
+    paid: number;
+    free: number;
+    comped: number;
+    paid_subscribed: number;
+    paid_canceled: number;
+}
+
+export type MemberCountHistoryResponseType = {
+    stats: MemberStatusItem[];
+    meta: {
+        totals: {
+            paid: number;
+            free: number;
+            comped: number;
+        }
+    };
+}
+
 // Requests
 
 const dataType = 'TopContentResponseType';
+const memberCountHistoryDataType = 'MemberCountHistoryResponseType';
 
 export const useTopContent = createQuery<TopContentResponseType>({
     dataType,
     path: '/stats/top-content/'
+});
+
+export const useMemberCountHistory = createQuery<MemberCountHistoryResponseType>({
+    dataType: memberCountHistoryDataType,
+    path: '/stats/member_count/'
 });
