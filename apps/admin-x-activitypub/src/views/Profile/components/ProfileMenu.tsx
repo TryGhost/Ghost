fdbdb30649261fb9aab856321f75c3fddb9e970e
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-05-05 12:22:25 +0800

    Implemented UI for the domain block feature in ActivityPub

--- apps/admin-x-activitypub/src/views/Profile/components/ProfileMenu.tsx
+++ apps/admin-x-activitypub/src/views/Profile/components/ProfileMenu.tsx
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import {Account} from '@src/api/activitypub';
 import {
     AlertDialog,
@@ -16,6 +17,7 @@ import {
     PopoverTrigger,
     buttonVariants
 } from '@tryghost/shade';
+import {useFeatureFlags} from '@src/lib/feature-flags';
 
 interface ProfileMenuProps {
     account: Account,
@@ -24,6 +26,7 @@ interface ProfileMenuProps {
     onBlockAccount: () => void;
     disabled?: boolean;
     isBlocked?: boolean;
+    isDomainBlocked?: boolean;
 }
 
 const ProfileMenu: React.FC<ProfileMenuProps> = ({
@@ -32,8 +35,12 @@ const ProfileMenu: React.FC<ProfileMenuProps> = ({
     onCopyHandle,
     onBlockAccount,
     disabled = false,
-    isBlocked = false
+    isBlocked = false,
+    isDomainBlocked = false
 }) => {
+    const {isEnabled} = useFeatureFlags();
+    const [dialogType, setDialogType] = useState<'user' | 'domain' | null>(null);
+
     const handleCopyHandleClick = (e: React.MouseEvent<HTMLElement>) => {
         e.stopPropagation();
         onCopyHandle();
@@ -42,6 +49,10 @@ const ProfileMenu: React.FC<ProfileMenuProps> = ({
     const handleBlockAccountClick = (e: React.MouseEvent<HTMLElement>) => {
         e.stopPropagation();
         onBlockAccount();
+    };
+
+    const handleBlockDomainClick = (e: React.MouseEvent<HTMLElement>) => {
+        e.stopPropagation();
     };
 
     return (
@@ -62,25 +73,54 @@ const ProfileMenu: React.FC<ProfileMenuProps> = ({
                                 <Button
                                     className='justify-start text-red hover:bg-red/5 hover:text-red'
                                     variant='ghost'
-                                    onClick={e => e.stopPropagation()}
+                                    onClick={(e) => {
+                                        e.stopPropagation();
+                                        setDialogType('user');
+                                    }}
                                 >
                                     {isBlocked ? 'Unblock user' : 'Block user'}
                                 </Button>
                             </PopoverClose>
                         </AlertDialogTrigger>
+                        {isEnabled('block-domain') &&
+                            <AlertDialogTrigger asChild>
+                                <PopoverClose asChild>
+                                    <Button
+                                        className='justify-start text-red hover:bg-red/5 hover:text-red'
+                                        variant='ghost'
+                                        onClick={(e) => {
+                                            e.stopPropagation();
+                                            setDialogType('domain');
+                                        }}
+                                    >
+                                        {isDomainBlocked ? 'Unblock domain' : 'Block domain'}
+                                    </Button>
+                                </PopoverClose>
+                            </AlertDialogTrigger>
+                        }
                     </div>
                 </PopoverContent>
             </Popover>
             <AlertDialogContent onClick={e => e.stopPropagation()}>
                 <AlertDialogHeader>
                     <AlertDialogTitle className='mb-1 flex flex-col gap-1'>
-                        {isBlocked ? 'Unblock this user?' : 'Block this user?'}
-                        <span className='text-base font-normal tracking-normal'>{account.handle}</span>
+                        {dialogType === 'user'
+                            ? (isBlocked ? 'Unblock this user?' : 'Block this user?')
+                            : (isDomainBlocked ? 'Unblock this domain?' : 'Block this domain?')}
+                        <span className='text-base font-normal tracking-normal'>
+                            {dialogType === 'user'
+                                ? account.handle
+                                : account.handle.split('@').filter(Boolean)[1]}
+                        </span>
                     </AlertDialogTitle>
                     <AlertDialogDescription>
-                        {isBlocked
-                            ? 'They will be able to follow you and engage with your public posts.'
-                            : 'They will be able to see your public posts, but will no longer be able follow you or interact with your content on the social web.'}
+                        {dialogType === 'user'
+                            ? (isBlocked
+                                ? 'They will be able to follow you and engage with your public posts.'
+                                : 'They will be able to see your public posts, but will no longer be able follow you or interact with your content on the social web.')
+                            : (isDomainBlocked
+                                ? 'Users from this domain will be able to follow you and engage with your public posts.'
+                                : 'All users from this domain will be able to see your public posts, but won\'t be able to follow you or interact with your content.')}
                     </AlertDialogDescription>
                 </AlertDialogHeader>
                 <AlertDialogFooter>
@@ -89,7 +129,7 @@ const ProfileMenu: React.FC<ProfileMenuProps> = ({
                     </AlertDialogCancel>
                     <AlertDialogAction
                         className={isBlocked ? undefined : buttonVariants({variant: 'destructive'})}
-                        onClick={handleBlockAccountClick}
+                        onClick={dialogType === 'user' ? handleBlockAccountClick : handleBlockDomainClick}
                     >
                         {isBlocked ? 'Unblock' : 'Block'}
                     </AlertDialogAction>
