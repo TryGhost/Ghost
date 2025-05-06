83bb760e05ff21629b23fe87c0045e86a9873351
Chris Raible <chris@ghost.org>
2025-05-05 19:05:54 -0700

    fixup! fixup! Reset totals on post analytics to static values

--- apps/posts/src/hooks/usePostReferrers.ts
+++ apps/posts/src/hooks/usePostReferrers.ts
@@ -1,5 +1,5 @@
-import {usePostReferrers as usePostReferrersAPI} from '@tryghost/admin-x-framework/api/stats';
 import {useMemo} from 'react';
+import {usePostReferrers as usePostReferrersAPI} from '@tryghost/admin-x-framework/api/stats';
 
 export const usePostReferrers = (postId: string) => {
     const {data: postReferrerResponse, isLoading} = usePostReferrersAPI(postId);
