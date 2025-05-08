194a8da6923e0fc8cae1030029b0c87513924c1d
Steve Larson <9larsons@gmail.com>
2025-05-07 19:21:58 -0500

    Updated member exports to data stream (#23242)
    ref https://linear.app/ghost/issue/ENG-2135/
    - member exports now utilize data stream for counts >10k
    
    Users with particularly large member counts (somewhere north of 100k)
    could fail to export their members depending on the attached data, with
    very large member counts always failing, because of the limitations with
    Node's memory heap. We've needed to move to a data stream option for a
    while now, and this does it for members.
    
    We should probably repeat this pattern for content exports so we can
    strip out the `disableJsBackups` config option to prevent Ghost from
    bricking.

--- ghost/admin/app/controllers/members.js
+++ ghost/admin/app/controllers/members.js
@@ -3,6 +3,7 @@ import BulkDeleteMembersModal from '../components/members/modals/bulk-delete';
 import BulkRemoveMembersLabelModal from '../components/members/modals/bulk-remove-label';
 import BulkUnsubscribeMembersModal from '../components/members/modals/bulk-unsubscribe';
 import Controller from '@ember/controller';
+import fetch from 'fetch';
 import ghostPaths from 'ghost-admin/utils/ghost-paths';
 import moment from 'moment-timezone';
 import {A} from '@ember/array';
@@ -60,6 +61,7 @@ export default class MembersController extends Controller {
     @tracked showLabelModal = false;
     @tracked filters = A([]);
     @tracked softFilters = A([]);
+    @tracked isExporting = false;
 
     @tracked _availableLabels = A([]);
 
@@ -373,8 +375,37 @@ export default class MembersController extends Controller {
         let exportUrl = ghostPaths().url.api('members/upload');
         let downloadParams = new URLSearchParams(this.getApiQueryObject());
         downloadParams.set('limit', 'all');
-
-        this.utils.downloadFile(`${exportUrl}?${downloadParams.toString()}`);
+        
+        const url = `${exportUrl}?${downloadParams.toString()}`;
+        
+        // Set loading state
+        this.isExporting = true;
+        
+        fetch(url, {method: 'GET'})
+            .then(res => res.blob())
+            .then((blob) => {
+                const blobUrl = window.URL.createObjectURL(blob);
+                const a = document.createElement('a');
+                const datetime = (new Date()).toJSON().substring(0, 10);
+                
+                a.href = blobUrl;
+                a.download = `members.${datetime}.csv`;
+                document.body.appendChild(a);
+                
+                a.click();
+                
+                // Cleanup
+                a.remove();
+                URL.revokeObjectURL(blobUrl);
+            })
+            .catch(() => {
+                // Handle errors silently
+                // A more robust implementation would show an error notification
+            })
+            .finally(() => {
+                // Reset loading state
+                this.isExporting = false;
+            });
     }
 
     @action
