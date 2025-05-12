0247f33adb5349e395e221cfd3c18d5972167a07
Steve Larson <9larsons@gmail.com>
2025-05-12 13:59:44 -0500

    Removed alpha flag for Newsletter tab in Stats and Posts (#23294)
    no ref

--- apps/posts/src/routes.tsx
+++ apps/posts/src/routes.tsx
@@ -3,12 +3,13 @@ import Newsletter from './views/PostAnalytics/Newsletter';
 import Web from './views/PostAnalytics/Web';
 import {ErrorPage} from '@tryghost/shade';
 import {Navigate, RouteObject} from '@tryghost/admin-x-framework';
-import {withFeatureFlag} from '@src/hooks/withFeatureFlag';
+// import {withFeatureFlag} from '@src/hooks/withFeatureFlag';
 
 export const APP_ROUTE_PREFIX = '/posts';
 
-// Wrap the Newsletter component with the feature flag
-const ProtectedNewsletter = withFeatureFlag(Newsletter, 'trafficAnalyticsAlpha', '/analytics/', 'Newsletter');
+// Wrap components with feature flag protection where needed
+//  e.g.
+// const ProtectedNewsletter = withFeatureFlag(Newsletter, 'trafficAnalyticsAlpha', '/analytics/', 'Newsletter');
 
 export const routes: RouteObject[] = [
     {
@@ -34,7 +35,7 @@ export const routes: RouteObject[] = [
             {
                 path: 'analytics/:postId/newsletter',
                 index: true,
-                element: <ProtectedNewsletter />
+                element: <Newsletter />
             },
             {
                 path: '*',
--- apps/stats/src/routes.tsx
+++ apps/stats/src/routes.tsx
@@ -4,12 +4,13 @@ import Newsletters from './views/Stats/Newsletters';
 import Sources from './views/Stats/Sources';
 import Web from './views/Stats/Web';
 import {RouteObject} from '@tryghost/admin-x-framework';
-import {withFeatureFlag} from './hooks/withFeatureFlag';
+// import {withFeatureFlag} from './hooks/withFeatureFlag';
 
 export const APP_ROUTE_PREFIX = '/stats';
 
 // Wrap all components with feature flag protection
-const ProtectedNewsletters = withFeatureFlag(Newsletters, 'trafficAnalyticsAlpha', '/web/', 'Newsletters');
+//  e.g.
+// const ProtectedNewsletters = withFeatureFlag(Newsletters, 'trafficAnalyticsAlpha', '/web/', 'Newsletters');
 
 export const routes: RouteObject[] = [
     {
@@ -36,6 +37,6 @@ export const routes: RouteObject[] = [
     ,
     {
         path: '/newsletters/',
-        element: <ProtectedNewsletters />
+        element: <Newsletters />
     }
 ];
