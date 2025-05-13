24cdc6776974bf5265db622102b70676737e6fa5
Andreas Creten <andreas@madewithlove.com>
2025-05-13 10:47:41 +0200

    Rename EmailSuppressionList to MailgunEmailSuppressionList
    So we change as little code as possible

--- ghost/core/core/server/services/email-suppression-list/service.js
+++ ghost/core/core/server/services/email-suppression-list/service.js
@@ -1,12 +1,12 @@
 const models = require('../../models');
 const configService = require('../../../shared/config');
 const settingsCache = require('../../../shared/settings-cache');
-const EmailSuppressionList = require('./EmailSuppressionList');
+const MailgunEmailSuppressionList = require('./MailgunEmailSuppressionList');
 const EmailService = require('../email-service/');
 
 const mailClient = EmailService.getMailClient(settingsCache, configService);
 
-module.exports = new EmailSuppressionList({
+module.exports = new MailgunEmailSuppressionList({
     Suppression: models.Suppression,
     apiClient: mailClient
 });
