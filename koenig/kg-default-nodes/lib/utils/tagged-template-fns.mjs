f410e7e641c4dda6336e65aa0d60a18738db15e8
Kevin Ansfield <kevin@lookingsideways.co.uk>
2025-05-14 15:03:11 +0100

    Added `html` tagged template fn for syntax highlighting
    no issue
    
    - maps `html` name to our `oneline` tagged template function
    - when `html` is used for a literal string, editors will automatically apply HTML syntax highlighting which makes template strings nicer to work with

--- koenig/kg-default-nodes/lib/utils/tagged-template-fns.mjs
+++ koenig/kg-default-nodes/lib/utils/tagged-template-fns.mjs
@@ -1,7 +1,12 @@
-export function oneline(strings, ...values) {
+const oneline = function (strings, ...values) {
     const result = strings.reduce((acc, str, i) => {
         return acc + str + (values[i] || '');
     }, '');
     // Remove newline+indentation patterns while preserving intentional whitespace
     return result.replace(/\n\s+/g, '').trim();
-}
+};
+
+// Using `html` as a synonym for `oneline` in order to get syntax highlighting in editors
+const html = oneline;
+
+export {oneline, html};
