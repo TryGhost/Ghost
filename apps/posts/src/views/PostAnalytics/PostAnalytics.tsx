f448347a59862762b3906e163c699e7a87fe0ce7
Peter Zimon <peter.zimon@gmail.com>
2025-05-16 17:28:01 +0200

    Refined ranges

--- apps/posts/src/views/PostAnalytics/PostAnalytics.tsx
+++ apps/posts/src/views/PostAnalytics/PostAnalytics.tsx
@@ -1,16 +1,7 @@
 import PostAnalyticsLayout from './components/layout/PostAnalyticsLayout';
 import {Outlet} from '@tryghost/admin-x-framework';
-import {STATS_RANGES} from '@src/utils/constants';
-import {useEffect} from 'react';
-import {useGlobalData} from '@src/providers/PostAnalyticsContext';
 
 const PostAnalytics: React.FC = () => {
-    const {setRange} = useGlobalData();
-
-    useEffect(() => {
-        setRange(STATS_RANGES.ALL_TIME.value);
-    }, [setRange]);
-
     return (
         <PostAnalyticsLayout>
             <Outlet />
