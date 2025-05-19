41d3b884b4aa70bc9c0a3452f9527795afc04ddb
Peter Zimon <peter.zimon@gmail.com>
2025-05-19 15:28:52 +0200

    Using 'weighted' instead of 'ponderated'

--- apps/posts/src/utils/kpi-helpers.ts
+++ apps/posts/src/utils/kpi-helpers.ts
@@ -14,8 +14,8 @@ export const getWebKpiValues = (data: KpiDataItem[] | null | undefined) => {
         return {visits: 0, views: 0, bounceRate: 0, duration: 0};
     }
 
-    // Sum total KPI value from the trend, ponderating using sessions
-    const _ponderatedKPIsTotal = (kpi: keyof KpiDataItem) => {
+    // Sum total KPI value from the trend, weighting using sessions
+    const _weightedKPIsTotal = (kpi: keyof KpiDataItem) => {
         if (totalVisits === 0) {
             return 0;
         }
@@ -28,8 +28,8 @@ export const getWebKpiValues = (data: KpiDataItem[] | null | undefined) => {
 
     const totalVisits = data.reduce((sum, item) => sum + Number(item.visits), 0);
     const totalViews = data.reduce((sum, item) => sum + Number(item.pageviews), 0);
-    const avgBounceRate = _ponderatedKPIsTotal('bounce_rate');
-    const avgDuration = _ponderatedKPIsTotal('avg_session_sec');
+    const avgBounceRate = _weightedKPIsTotal('bounce_rate');
+    const avgDuration = _weightedKPIsTotal('avg_session_sec');
 
     return {
         visits: formatNumber(totalVisits),
