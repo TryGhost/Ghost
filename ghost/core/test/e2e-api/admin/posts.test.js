3ff1921211c51efeff600fc716a2ce7c097c332e
Hannah Wolfe <github.erisds@gmail.com>
2025-05-26 13:54:07 +0100

    Added cacheInvalidation helpers to e2e framework
    - The main thing in this change is adding some assertion helpers
    - I want to make cache invalidation expected behaviour more explicit
    - Especially when it should not be set, which is hard to grok with snapshot tests
    - I can see this is something in some of the tests that haven't yet been migrated
      to the new framework
    - This is a bit experimental and inconsistent I'm not sure it's the right pattern yet

--- ghost/core/test/e2e-api/admin/posts.test.js
+++ ghost/core/test/e2e-api/admin/posts.test.js
@@ -1,5 +1,5 @@
 const should = require('should');
-const {agentProvider, fixtureManager, mockManager, matchers} = require('../../utils/e2e-framework');
+const {agentProvider, fixtureManager, mockManager, matchers, assertions} = require('../../utils/e2e-framework');
 const {anyArray, anyContentVersion, anyEtag, anyErrorId, anyLocationFor, anyObject, anyObjectId, anyISODateTime, anyString, anyStringNumber, anyUuid, stringMatching} = matchers;
 const models = require('../../../core/server/models');
 const escapeRegExp = require('lodash/escapeRegExp');
@@ -465,7 +465,7 @@ describe('Posts API', function () {
                 .matchHeaderSnapshot({
                     'content-version': anyContentVersion,
                     etag: anyEtag,
-                    'x-cache-invalidate': anyString
+                    'x-cache-invalidate': stringMatching(/^\/p\/[a-z0-9-]+\/, \/p\/[a-z0-9-]+\/\?member_status=anonymous, \/p\/[a-z0-9-]+\/\?member_status=free, \/p\/[a-z0-9-]+\/\?member_status=paid$/)
                 });
 
             // mobiledoc revisions are created
@@ -519,7 +519,7 @@ describe('Posts API', function () {
                 .matchHeaderSnapshot({
                     'content-version': anyContentVersion,
                     etag: anyEtag,
-                    'x-cache-invalidate': anyString
+                    'x-cache-invalidate': stringMatching(/^\/p\/[a-z0-9-]+\/, \/p\/[a-z0-9-]+\/\?member_status=anonymous, \/p\/[a-z0-9-]+\/\?member_status=free, \/p\/[a-z0-9-]+\/\?member_status=paid$/)
                 });
 
             // post revisions are created
@@ -614,7 +614,7 @@ describe('Posts API', function () {
                         .matchHeaderSnapshot({
                             'content-version': anyContentVersion,
                             etag: anyEtag,
-                            'x-cache-invalidate': anyString
+                            'x-cache-invalidate': stringMatching(/^\/p\/[a-z0-9-]+\/, \/p\/[a-z0-9-]+\/\?member_status=anonymous, \/p\/[a-z0-9-]+\/\?member_status=free, \/p\/[a-z0-9-]+\/\?member_status=paid$/)
                         })
                         .matchBodySnapshot({
                             posts: [Object.assign({}, matchPostShallowIncludes, {
