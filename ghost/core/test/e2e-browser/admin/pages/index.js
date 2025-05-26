8c63e19438c5e05761a57abdd835d20b35296c4d
Igor Balos <ibalosh@gmail.com>
2025-05-26 10:03:54 +0200

    fix AI suggestion

--- ghost/core/test/e2e-browser/admin/pages/index.js
+++ ghost/core/test/e2e-browser/admin/pages/index.js
@@ -1,6 +1,6 @@
 exports.AdminDashboardPage = require('./admin-dashboard.page');
 exports.AdminLoginPage = require('./admin-login.page');
-exports.AdminPagePage = require('./admin-page');
+exports.AdminPage = require('./admin-page');
 exports.AdminPostsPage = require('./admin-posts.page');
 exports.AdminPostsEmailPreviewPage = require('./admin-posts-email-preview.page');
 exports.AdminPublicationPage = require('./admin-publication.page');
