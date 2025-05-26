05fc2e32f4e010c03a11b647a3e5424f50748bc1
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-05-26 12:06:54 +0800

    Added loading state to new note creation in ActivityPub (#23516)
    PROD-1761
    
    - Implemented loading indicator on post button during note creation
    - Disabled post button while submission is in progress

--- apps/shade/src/components/ui/loading-indicator.tsx
+++ apps/shade/src/components/ui/loading-indicator.tsx
@@ -2,25 +2,37 @@ import React from 'react';
 import {Skeleton} from './skeleton';
 
 type LoadingIndicatorSize = 'sm' | 'md' | 'lg';
+type LoadingIndicatorColor = 'dark' | 'light';
 
 interface LoadingIndicatorProps {
     size: LoadingIndicatorSize;
+    color?: LoadingIndicatorColor;
     className?: string;
 }
 
-const LoadingIndicator: React.FC<LoadingIndicatorProps> = ({size = 'md', className = ''}) => {
-    let styles = `relative mx-0 my-[-0.5] box-border inline-block animate-spin rounded-full border-black/10 before:bg-black dark:border-white/20 dark:before:bg-white before:z-10 before:block before:rounded-full before:content-[''] `;
+const LoadingIndicator: React.FC<LoadingIndicatorProps> = ({size = 'md', color = 'dark', className = ''}) => {
+    let styles = `relative mx-0 my-[-0.5] box-border inline-block animate-spin rounded-full before:z-10 before:block before:rounded-full before:content-['']`;
 
     switch (size) {
     case 'sm':
-        styles += 'h-[16px] w-[16px] border-2 before:mt-[10px] before:h-[3px] before:w-[3px]';
+        styles += ' h-[16px] w-[16px] border-2 before:mt-[10px] before:h-[3px] before:w-[3px]';
         break;
     case 'md':
-        styles += 'h-[20px] w-[20px] border-2 before:mt-[13px] before:h-[3px] before:w-[3px]';
+        styles += ' h-[20px] w-[20px] border-2 before:mt-[13px] before:h-[3px] before:w-[3px]';
         break;
     case 'lg':
     default:
-        styles += 'h-[50px] w-[50px] border before:mt-[7px] before:h-[7px] before:w-[7px]';
+        styles += ' h-[50px] w-[50px] border before:mt-[7px] before:h-[7px] before:w-[7px]';
+        break;
+    }
+
+    switch (color) {
+    case 'light':
+        styles += ' border-white/20 before:bg-white dark:border-black/10 dark:before:bg-black';
+        break;
+    case 'dark':
+    default:
+        styles += ' border-black/10 before:bg-black dark:border-white/20 dark:before:bg-white';
         break;
     }
 
