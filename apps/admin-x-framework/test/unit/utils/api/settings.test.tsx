683c6976dfb21685eb7b901a83519a9ae313b9e8
Steve Larson <9larsons@gmail.com>
2025-05-27 10:41:14 -0500

    Moved all admin apps packages to vitest (#23550)
    no ref
    
    We're trying to unify the testing approaches for the /apps/, in
    particular those using admin-x-framework. Part of the fuzziness here is
    that we had a split mocha+vitest implementation, with some packages
    importing mocha without even using it. This is attempting to clean that
    up.
    
    todo: We need to update vitest. 0.34.3 is almost two years old. Updating
    this might less us remove jest, as there's type issues causing problems
    that seemed to require this as a workaround.

--- apps/admin-x-framework/test/unit/utils/api/settings.test.tsx
+++ apps/admin-x-framework/test/unit/utils/api/settings.test.tsx
@@ -1,8 +1,8 @@
 import {getSettingValue, getSettingValues, isSettingReadOnly} from '../../../../src/api/settings';
 
-describe('settings utils', function () {
-    describe('getSettingValue', function () {
-        it('returns the value of a setting', function () {
+describe('settings utils', () => {
+    describe('getSettingValue', () => {
+        it('returns the value of a setting', () => {
             const settings = [
                 {key: 'test_key', value: 'test_value'}
             ];
@@ -10,15 +10,15 @@ describe('settings utils', function () {
             expect(value).toEqual('test_value');
         });
 
-        it('returns null if settings is null', function () {
+        it('returns null if settings is null', () => {
             const settings = undefined;
             const value = getSettingValue(settings, 'test_key');
             expect(value).toEqual(null);
         });
     });
 
-    describe('getSettingValues', function () {
-        it('returns the values of multiple settings', function () {
+    describe('getSettingValues', () => {
+        it('returns the values of multiple settings', () => {
             const settings = [
                 {key: 'test_key', value: 'test_value'},
                 {key: 'test_key_2', value: 'test_value_2'}
@@ -27,14 +27,14 @@ describe('settings utils', function () {
             expect(values).toEqual(['test_value', 'test_value_2']);
         });
 
-        it('returns undefined for missing keys', function () {
+        it('returns undefined for missing keys', () => {
             const values = getSettingValues([], ['test_key', 'test_key_2']);
             expect(values).toEqual([undefined, undefined]);
         });
     });
 
-    describe('isSettingReadOnly', function () {
-        it('returns true if the setting has an override', function () {
+    describe('isSettingReadOnly', () => {
+        it('returns true if the setting has an override', () => {
             const settings = [
                 {key: 'test_key', is_read_only: true, value: 'test_value'}
             ];
@@ -42,7 +42,7 @@ describe('settings utils', function () {
             expect(value).toEqual(true);
         });
 
-        it('returns false if the setting does not have an override', function () {
+        it('returns false if the setting does not have an override', () => {
             const settings = [
                 {key: 'test_key', is_read_only: false, value: 'test_value'}
             ];
@@ -50,7 +50,7 @@ describe('settings utils', function () {
             expect(value).toEqual(false);
         });
 
-        it('returns undefined if settings is falsy', function () {
+        it('returns undefined if settings is falsy', () => {
             const settings = undefined;
             const value = isSettingReadOnly(settings, 'test_key');
             expect(value).toEqual(undefined);
