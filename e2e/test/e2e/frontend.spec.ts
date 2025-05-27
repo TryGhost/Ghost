495bdeef8264d1077945b7172cb94b12b926d16a
Chris Raible <chris@ghost.org>
2025-05-26 22:21:48 -0700

    Fixed title assertion in frontend test

--- e2e/test/e2e/frontend.spec.ts
+++ e2e/test/e2e/frontend.spec.ts
@@ -2,5 +2,5 @@ import {expect, test} from '../../src/test-fixtures';
 
 test('has correct title', async ({page, appUrls}) => {
     await page.goto(appUrls.baseURL);
-    await expect(page).toHaveTitle('[DEV]');
+    await expect(page).toHaveTitle('Ghost');
 });
