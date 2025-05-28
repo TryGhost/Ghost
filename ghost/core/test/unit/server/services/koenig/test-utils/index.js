aff636952fe7a800c07ef5515a198ac1ecbf6894
Kevin Ansfield <kevin@lookingsideways.co.uk>
2025-05-28 20:06:33 +0100

    Extracted prettifyHTML test util for re-use
    no issue
    
    - allows the prettify aspect of `assertPrettifiesTo` to be used in other test utils

--- ghost/core/test/unit/server/services/koenig/test-utils/index.js
+++ ghost/core/test/unit/server/services/koenig/test-utils/index.js
@@ -6,5 +6,6 @@ module.exports = {
     assertPrettifiesTo: require('./assert-prettifies-to'),
     callRenderer: require('./build-call-renderer')(dom),
     html: require('./html'),
+    prettifyHTML: require('./prettify-html'),
     visibility: require('./visibility')
 };
