7234f772896188c33a4c7ff1e409af086362d604
Steve Larson <9larsons@gmail.com>
2025-05-28 13:55:48 -0500

    f

--- apps/stats/test/unit/hooks/withFeatureFlag.test.tsx
+++ apps/stats/test/unit/hooks/withFeatureFlag.test.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
 import {beforeEach, describe, expect, it, vi} from 'vitest';
 import {render, screen} from '@testing-library/react';
