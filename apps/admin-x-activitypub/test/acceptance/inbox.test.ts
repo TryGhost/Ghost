ae206cc820a6e38144ab8cd9e82219bf771d2ca2
Sag <guptazy@gmail.com>
2025-05-28 15:38:48 +0800

    Removed broken existing test

--- apps/admin-x-activitypub/test/acceptance/inbox.test.ts
+++
@@ -1,9 +0,0 @@
-import {expect, test} from '@playwright/test';
-
-test.describe('Inbox', async () => {
-    test('Renders the inbox page', async ({page}) => {
-        await page.goto('/');
-
-        await expect(page.locator('body')).toContainText('This is your inbox');
-    });
-});
