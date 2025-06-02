d611304f6de4170f560fd06f48b25845b4f7f3b0
Peter Zimon <peter.zimon@gmail.com>
2025-06-02 13:15:18 +0200

    Replaced areacharts to Shade component in post analytics

--- apps/posts/src/views/PostAnalytics/Overview/components/WebOverview.tsx
+++ apps/posts/src/views/PostAnalytics/Overview/components/WebOverview.tsx
@@ -1,5 +1,5 @@
 import React, {useMemo} from 'react';
-import {AlignedAxisTick, BarChartLoadingIndicator, ChartConfig, ChartContainer, ChartTooltip, DateTooltipContent, Recharts, calculateYAxisWidth, formatDisplayDateWithRange, formatNumber, formatQueryDate, getRangeDates, getYRange, sanitizeChartData} from '@tryghost/shade';
+import {BarChartLoadingIndicator, GhAreaChart, formatQueryDate, getRangeDates, sanitizeChartData} from '@tryghost/shade';
 import {KPI_METRICS} from '../../Web/components/Kpis';
 import {KpiDataItem} from '@src/utils/kpi-helpers';
 import {STATS_RANGES} from '@src/utils/constants';
@@ -48,12 +48,6 @@ const WebOverview:React.FC = () => {
         params: params
     });
 
-    const chartConfig = {
-        value: {
-            label: currentMetric.label
-        }
-    } satisfies ChartConfig;
-
     const chartData = sanitizeChartData<KpiDataItem>(data as KpiDataItem[] || [], range, currentMetric.dataKey as keyof KpiDataItem, 'sum')?.map((item: KpiDataItem) => {
         const value = Number(item[currentMetric.dataKey]);
         return {
@@ -66,8 +60,6 @@ const WebOverview:React.FC = () => {
 
     const isLoading = isConfigLoading || loading;
 
-    const yRange = [getYRange(chartData).min, getYRange(chartData).max];
-
     return (
         <div className='my-4 [&_.recharts-cartesian-axis-tick-value]:fill-gray-500'>
             {isLoading ?
@@ -75,71 +67,15 @@ const WebOverview:React.FC = () => {
                     <BarChartLoadingIndicator />
                 </div>
                 :
-                <ChartContainer className='-mb-3 h-[16vw] max-h-[320px] w-full' config={chartConfig}>
-                    <Recharts.AreaChart
-                        data={chartData}
-                        margin={{
-                            left: 0,
-                            right: 20,
-                            top: 12
-                        }}
-                    >
-                        <Recharts.CartesianGrid horizontal={false} vertical={false} />
-                        <Recharts.XAxis
-                            axisLine={false}
-                            dataKey="date"
-                            interval={0}
-                            tick={props => <AlignedAxisTick {...props} formatter={value => formatDisplayDateWithRange(value, range)} />}
-                            tickFormatter={(value) => {
-                                return formatDisplayDateWithRange(value, range);
-                            }}
-                            tickLine={false}
-                            tickMargin={8}
-                            ticks={chartData && chartData.length > 0 ? [chartData[0].date, chartData[chartData.length - 1].date] : []}
-                        />
-                        <Recharts.YAxis
-                            axisLine={false}
-                            domain={yRange}
-                            scale="linear"
-                            tickFormatter={(value) => {
-                                return formatNumber(value);
-                            }}
-                            tickLine={false}
-                            ticks={yRange}
-                            width={calculateYAxisWidth(yRange, currentMetric.formatter)}
-                        />
-                        <ChartTooltip
-                            content={<DateTooltipContent range={range} />}
-                            cursor={true}
-                            isAnimationActive={false}
-                            position={{y: 20}}
-                        />
-                        <defs>
-                            <linearGradient id="fillChart" x1="0" x2="0" y1="0" y2="1">
-                                <stop
-                                    offset="5%"
-                                    stopColor="hsl(var(--chart-blue))"
-                                    stopOpacity={0.8}
-                                />
-                                <stop
-                                    offset="95%"
-                                    stopColor="hsl(var(--chart-blue))"
-                                    stopOpacity={0.1}
-                                />
-                            </linearGradient>
-                        </defs>
-                        <Recharts.Area
-                            dataKey="value"
-                            fill="url(#fillChart)"
-                            fillOpacity={0.2}
-                            isAnimationActive={false}
-                            stackId="a"
-                            stroke="hsl(var(--chart-blue))"
-                            strokeWidth={2}
-                            type="linear"
-                        />
-                    </Recharts.AreaChart>
-                </ChartContainer>
+                <GhAreaChart
+                    className={'-mb-3 h-[16vw] max-h-[320px] w-full'}
+                    color='hsl(var(--chart-blue))'
+                    data={chartData}
+                    id="mrr"
+                    range={range}
+                    showYAxisValues={false}
+                    syncId="overview-charts"
+                />
             }
         </div>
     );
