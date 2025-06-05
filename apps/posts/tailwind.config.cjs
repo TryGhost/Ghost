28c8f2e3a475627a64b2232e6d3b9e50b6079394
Peter Zimon <peter.zimon@gmail.com>
2025-06-05 19:03:10 +0200

    Fixed TailwindCSS build for Shade and React apps
    no ref
    
    - TailwindCSS builds only the classes that are explicitly told to through its config file(s). In the current structure, in each React app we import Shade's Tailwind config and add all files to be included/scanned durint TailwindCSS build. However it can happen that there are classes (e.g. certain media queries, like `xl:grid-cols-2`) that are used in only 'other' apps which will not be built by TailwindCSS when navigating between apps. To solve this for now I added the folders of each React apps 'src' directory to the individual apps' tailwindCSS config in order to force it to build all the classes that appear in all apps. Once we clean up the folder structure, this is going to be much simpler because all react apps will have the same root folder.

--- apps/posts/tailwind.config.cjs
+++ apps/posts/tailwind.config.cjs
@@ -2,5 +2,14 @@ import shadePreset from '@tryghost/shade/tailwind.cjs';
 
 module.exports = {
     presets: [shadePreset('.shade')],
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', '../../node_modules/@tryghost/shade/es/**/*.{js,ts,jsx,tsx}']
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+        '../../node_modules/@tryghost/shade/es/**/*.{js,ts,jsx,tsx}',
+
+        // We need to scan all apps that use shade to force Tailwind build all used classes
+        '../posts/src/**/*.{js,ts,jsx,tsx}',
+        '../stats/src/**/*.{js,ts,jsx,tsx}',
+        '../admin-x-activitypub/src/**/*.{js,ts,jsx,tsx}'
+    ]
 };
