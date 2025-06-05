f1f190d8d1848da73f27c9623accfa457b749bc1
Peter Zimon <zimo@ghost.org>
2025-06-05 09:34:07 +0200

    Improved lists in site-wide analytics (#23684)
    ref
    https://linear.app/ghost/issue/PROD-1849/add-view-all-pattern-to-all-tables
    
    - Created new data list component with extra features like displaying a
    bar in the background and percentage along absolute value
    - Updated Web stats to use new component
    - Updated Locations list to use new component
    - Further minor UI updates such as setting default flag for unknown
    location and unifying KPI tab style (using icons instead of colored
    dots)

--- apps/shade/src/index.ts
+++ apps/shade/src/index.ts
@@ -6,6 +6,7 @@ export * from './components/ui/breadcrumb';
 export * from './components/ui/button';
 export * from './components/ui/card';
 export * from './components/ui/chart';
+export * from './components/ui/data-list';
 export * from './components/ui/dialog';
 export * from './components/ui/dropdown-menu';
 export * from './components/ui/flag';
