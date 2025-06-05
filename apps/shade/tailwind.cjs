107755e1133f59ae6693e51f3131846a33c46fc9
Peter Zimon <peter.zimon@gmail.com>
2025-06-05 20:24:11 +0200

    Added appContent

--- apps/shade/tailwind.cjs
+++ apps/shade/tailwind.cjs
@@ -3,5 +3,12 @@ const config = require('./tailwind.config');
 
 module.exports = selector => ({
     ...config,
+
+    // Add app src content here to force TailwindCSS scan them when building classes
+    appsContent: [
+        '../posts/src/**/*.{js,ts,jsx,tsx}',
+        '../stats/src/**/*.{js,ts,jsx,tsx}',
+        '../admin-x-activitypub/src/**/*.{js,ts,jsx,tsx}'
+    ],
     important: selector
 });
--- apps/stats/tailwind.config.cjs
+++ apps/stats/tailwind.config.cjs
@@ -1,15 +1,10 @@
-const shadePreset = require('@tryghost/shade/tailwind.cjs');
+import shadePreset from '@tryghost/shade/tailwind.cjs';
 
 module.exports = {
     presets: [shadePreset('.shade')],
     content: [
         './index.html',
-        './src/**/*.{js,ts,jsx,tsx}',
         '../../node_modules/@tryghost/shade/es/**/*.{js,ts,jsx,tsx}',
-
-        // We need to scan all apps that use shade to force Tailwind build all used classes
-        '../posts/src/**/*.{js,ts,jsx,tsx}',
-        '../stats/src/**/*.{js,ts,jsx,tsx}',
-        '../admin-x-activitypub/src/**/*.{js,ts,jsx,tsx}'
+        ...shadePreset('.shade').appsContent
     ]
 };
