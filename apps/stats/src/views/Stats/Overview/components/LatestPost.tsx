a8290e436094684c7154eca01eff1c136b2bb613
Peter Zimon <peter.zimon@gmail.com>
2025-06-10 16:39:20 +0200

    Added latest post loading

--- apps/stats/src/views/Stats/Overview/components/LatestPost.tsx
+++ apps/stats/src/views/Stats/Overview/components/LatestPost.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Card, CardContent, CardDescription, CardHeader, CardTitle, LucideIcon, Separator, formatDisplayDate, formatNumber} from '@tryghost/shade';
+import {Card, CardContent, CardDescription, CardHeader, CardTitle, LucideIcon, Separator, Skeleton, formatDisplayDate, formatNumber} from '@tryghost/shade';
 import {LatestPostStats} from '@tryghost/admin-x-framework/api/stats';
 import {useNavigate} from '@tryghost/admin-x-framework';
 
@@ -14,15 +14,9 @@ const LatestPost: React.FC<LatestPostProps> = ({
 }) => {
     const navigate = useNavigate();
 
-    if (isLoading) {
-        return (
-            <div>Loading...</div>
-        );
-    }
-
     return (
-        <Card className={`group/card ${latestPostStats && 'transition-all hover:cursor-pointer hover:bg-accent/50'}`} onClick={() => {
-            if (latestPostStats) {
+        <Card className={`group/card ${latestPostStats && !isLoading && 'transition-all hover:cursor-pointer hover:bg-accent/50'}`} onClick={() => {
+            if (!isLoading && latestPostStats) {
                 navigate(`/posts/analytics/beta/${latestPostStats.id}`, {crossApp: true});
             }
         }}>
@@ -45,48 +39,68 @@ const LatestPost: React.FC<LatestPostProps> = ({
                 {latestPostStats ? (
                     <>
                         <div className='flex flex-col items-stretch'>
-                            {latestPostStats.feature_image ?
-                                <div className='aspect-video w-full rounded-md bg-cover bg-center' style={{
-                                    backgroundImage: `url(${latestPostStats.feature_image})`
-                                }}></div>
+                            {isLoading ?
+                                <>
+                                    <Skeleton className='aspect-video w-full rounded-md' />
+                                    <Skeleton className='mt-4' />
+                                </>
                                 :
-                                <Separator />
+                                <>
+                                    {latestPostStats.feature_image ?
+                                        <div className='aspect-video w-full rounded-md bg-cover bg-center' style={{
+                                            backgroundImage: `url(${latestPostStats.feature_image})`
+                                        }}></div>
+                                        :
+                                        <Separator />
+                                    }
+                                    <div className='mt-4 text-xl font-semibold leading-tighter tracking-tight'>{latestPostStats.title}</div>
+                                    <div className='mt-1 text-sm text-muted-foreground'>Published {formatDisplayDate(latestPostStats.published_at)}</div>
+                                </>
                             }
-                            <div className='mt-4 text-xl font-semibold leading-tight tracking-tight'>{latestPostStats.title}</div>
-                            <div className='mt-0.5 text-sm text-muted-foreground'>Published {formatDisplayDate(latestPostStats.published_at)} {new Date(latestPostStats.published_at).toLocaleDateString()}</div>
                         </div>
                         <div className='flex flex-col items-stretch gap-2 text-sm'>
-                            <div className='flex items-center justify-between'>
-                                <div className='flex items-center gap-1 font-medium text-muted-foreground'>
-                                    <LucideIcon.MousePointer size={16} strokeWidth={1.5} />
-                                    Visitors
+                            {isLoading ?
+                                <>
+                                    <Skeleton className='w-3/4' />
+                                    <Skeleton className='w-3/4' />
+                                    <Skeleton className='w-1/3' />
+                                </>
+                                :
+                                <>
+                                    <div className='flex items-center justify-between'>
+                                        <div className='flex items-center gap-1 font-medium text-muted-foreground'>
+                                            <LucideIcon.MousePointer size={16} strokeWidth={1.5} />
+                                        Visitors
+                                        </div>
+                                        <div className='font-mono'>{formatNumber(latestPostStats.visitors)}</div>
+                                    </div>
+                                    {latestPostStats.open_rate &&
+                                <div className='flex items-center justify-between'>
+                                    <div className='flex items-center gap-1 font-medium text-muted-foreground'>
+                                        <LucideIcon.MailOpen size={16} strokeWidth={1.5} />
+                                        Open rate
+                                    </div>
+                                    <div className='font-mono'>{`${Math.round(latestPostStats.open_rate)}%`}</div>
                                 </div>
-                                <div className='font-mono'>{formatNumber(latestPostStats.visitors)}</div>
-                            </div>
-                            {latestPostStats.open_rate &&
-                            <div className='flex items-center justify-between'>
-                                <div className='flex items-center gap-1 font-medium text-muted-foreground'>
-                                    <LucideIcon.MailOpen size={16} strokeWidth={1.5} />
-                                    Open rate
-                                </div>
-                                <div className='font-mono'>{`${Math.round(latestPostStats.open_rate)}%`}</div>
-                            </div>
+                                    }
+                                    <div className='flex items-center justify-between'>
+                                        <div className='flex items-center gap-1 font-medium text-muted-foreground'>
+                                            <LucideIcon.User size={16} strokeWidth={1.5} />
+                                        Members
+                                        </div>
+                                        <div className='font-mono'>{latestPostStats.member_delta > 0 ? `+${latestPostStats.member_delta}` : latestPostStats.member_delta}</div>
+                                    </div>
+                                </>
                             }
-                            <div className='flex items-center justify-between'>
-                                <div className='flex items-center gap-1 font-medium text-muted-foreground'>
-                                    <LucideIcon.User size={16} strokeWidth={1.5} />
-                                    Members
-                                </div>
-                                <div className='font-mono'>{latestPostStats.member_delta > 0 ? `+${latestPostStats.member_delta}` : latestPostStats.member_delta}</div>
-                            </div>
                         </div>
                     </>
-                ) : (
+                ) :
+                    !isLoading &&
                     <div className='flex flex-col items-center justify-center gap-4 py-8 text-center text-muted-foreground'>
                         <LucideIcon.FileText size={32} strokeWidth={1.5} />
                         <div>No published posts yet</div>
                     </div>
-                )}
+                }
             </CardContent>
         </Card>
     );
