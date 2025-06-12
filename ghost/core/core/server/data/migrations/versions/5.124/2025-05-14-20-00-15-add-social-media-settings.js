c8fa5224dee1a7b05d6d7fb57d869ce3c08726a9
Cathy Sarisky <catsarisky@gmail.com>
2025-06-11 20:58:10 -0400

    database migration for sitewide socials

--- ghost/core/core/server/data/migrations/versions/5.124/2025-05-14-20-00-15-add-social-media-settings.js
+++ ghost/core/core/server/data/migrations/versions/5.124/2025-05-14-20-00-15-add-social-media-settings.js
@@ -1,46 +1,61 @@
 const {combineTransactionalMigrations, addSetting} = require('../../utils');
+const logging = require('@tryghost/logging');
 
 module.exports = combineTransactionalMigrations(
     addSetting({
         key: 'threads',
         value: '',
         type: 'string',
-        group: 'site'
+        group: 'site',
+        onUp: () => logging.info('Adding threads social media setting'),
+        onDown: () => logging.info('Removing threads social media setting')
     }),
     addSetting({
         key: 'bluesky',
         value: '',
         type: 'string',
-        group: 'site'
+        group: 'site',
+        onUp: () => logging.info('Adding bluesky social media setting'),
+        onDown: () => logging.info('Removing bluesky social media setting')
     }),
     addSetting({
         key: 'mastodon',
         value: '',
         type: 'string',
-        group: 'site'
+        group: 'site',
+        onUp: () => logging.info('Adding mastodon social media setting'),
+        onDown: () => logging.info('Removing mastodon social media setting')
     }),
     addSetting({
         key: 'tiktok',
         value: '',
         type: 'string',
-        group: 'site'
+        group: 'site',
+        onUp: () => logging.info('Adding tiktok social media setting'),
+        onDown: () => logging.info('Removing tiktok social media setting')
     }),
     addSetting({
         key: 'youtube',
         value: '',
         type: 'string',
-        group: 'site'
+        group: 'site',
+        onUp: () => logging.info('Adding youtube social media setting'),
+        onDown: () => logging.info('Removing youtube social media setting')
     }),
     addSetting({
         key: 'instagram',
         value: '',
         type: 'string',
-        group: 'site'
+        group: 'site',
+        onUp: () => logging.info('Adding instagram social media setting'),
+        onDown: () => logging.info('Removing instagram social media setting')
     }),
     addSetting({
         key: 'linkedin',
         value: '',
         type: 'string',
-        group: 'site'
+        group: 'site',
+        onUp: () => logging.info('Adding linkedin social media setting'),
+        onDown: () => logging.info('Removing linkedin social media setting')
     })
 ); 
