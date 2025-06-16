be9141b8265c8345517b423ac27230791d4703e5
Peter Zimon <zimo@ghost.org>
2025-06-16 08:38:05 +0200

    Added diff value to Growth charts in analytics (#23846)
    ref
    https://linear.app/ghost/issue/PROD-2060/add-diff-value-to-growth-charts
    
    - Indicating the difference to previous period/day helps users
    understand the trends on charts better.

--- apps/shade/src/components/ui/gh-chart.tsx
+++ apps/shade/src/components/ui/gh-chart.tsx
@@ -2,6 +2,7 @@ import {calculateYAxisWidth, cn, formatDisplayDateWithRange, formatNumber, getYR
 import React from 'react';
 import {AlignedAxisTick, ChartConfig, ChartContainer, ChartTooltip} from './chart';
 import {Area, AreaChart, CartesianGrid, XAxis, YAxis} from 'recharts';
+import {TrendingDown, TrendingUp} from 'lucide-react';
 
 // A Ghost specific charts for analytics re-using ShadCN/UI and Recharts components
 
@@ -11,6 +12,8 @@ interface TooltipPayload {
         date?: string;
         formattedValue?: string;
         label?: string;
+        diffValue?: null | number;
+        formattedDiffValue?: null | string;
     };
 }
 
@@ -26,17 +29,35 @@ const GhCustomTooltipContent = ({active, payload, range, color}: TooltipProps) =
         return null;
     }
 
-    const {date, formattedValue, label} = payload[0].payload;
+    const {date, formattedValue, label, diffValue, formattedDiffValue} = payload[0].payload;
     const displayValue = formattedValue || payload[0].value;
 
     return (
         <div className="min-w-[120px] rounded-lg border bg-background px-3 py-2 shadow-lg">
             {date && <div className="text-sm text-foreground">{formatDisplayDateWithRange(date, range || 0)}</div>}
-            <div className='flex items-center gap-2'>
-                <span className='inline-block size-2 rounded-full opacity-50' style={{backgroundColor: color || 'hsl(var(--chart-blue))'}}></span>
-                <div className='flex grow items-center justify-between gap-3'>
+            <div className='flex items-start gap-2'>
+                <span className='mt-1.5 inline-block size-2 rounded-full opacity-50' style={{backgroundColor: color || 'hsl(var(--chart-blue))'}}></span>
+                <div className='flex grow items-start justify-between gap-5'>
                     {label && <div className="text-sm text-muted-foreground">{label}</div>}
-                    <div className="font-mono font-medium">{displayValue}</div>
+                    <div className="flex flex-col items-end font-mono font-medium">
+                        {displayValue}
+
+                        {diffValue ? diffValue < 0 && (
+                            <div className='flex items-center gap-0.5 text-red-600'>
+                                <TrendingDown size={14} strokeWidth={1.5} />
+                                <span>{formattedDiffValue}</span>
+                            </div>
+                        ) : <></>}
+                        {diffValue ? diffValue > 0 && (
+                            <div className='flex items-center gap-0.5 text-green-600'>
+                                <TrendingUp size={14} strokeWidth={1.5} />
+                                <span>{formattedDiffValue}</span>
+                            </div>
+                        ) : <></>}
+                        {/* {diffValue !== undefined ? diffValue === 0 && (
+                            <span>&mdash;</span>
+                        ) : <></>} */}
+                    </div>
                 </div>
             </div>
         </div>
