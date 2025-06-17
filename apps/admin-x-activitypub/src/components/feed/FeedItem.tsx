c5cb4db79cd6dc9a86a35f7156afca698f5b6285
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-06-17 09:52:32 +0800

    Fixed dark mode display issues in ActivityPub (#23884)
    ref PROD-1766
    
    - Resolved various dark mode styling issues
    - Added correct dark mode selector to Shade tailwind config
    - Fixed pseudo-element styling in dark mode

--- apps/admin-x-activitypub/src/components/feed/FeedItem.tsx
+++ apps/admin-x-activitypub/src/components/feed/FeedItem.tsx
@@ -560,7 +560,7 @@ const FeedItem: React.FC<FeedItemProps> = ({
                             </div>
                         </div>
                         <div className={`absolute -inset-x-3 -inset-y-0 z-0 rounded transition-colors`}></div>
-                        {!last && <div className={`absolute left-[19px] ${isCompact ? 'bottom-[8px] top-[51px]' : isChainContinuation ? 'bottom-[5px] top-[51px]' : isChainParent ? 'bottom-[5px] top-[71px]' : 'bottom-[-7px] top-[71px]'} z-0 w-[2px] rounded-sm bg-gray-200`}></div>}
+                        {!last && <div className={`absolute left-[19px] ${isCompact ? 'bottom-[8px] top-[51px]' : isChainContinuation ? 'bottom-[5px] top-[51px]' : isChainParent ? 'bottom-[5px] top-[71px]' : 'bottom-[-7px] top-[71px]'} z-0 w-[2px] rounded-sm bg-gray-200 dark:bg-gray-950`}></div>}
                     </div>
                 )}
                 <ImageLightbox
