e3e4e89bc700c0095fbbaaa01496e1dba466a192
Peter Zimon <zimo@ghost.org>
2025-06-19 12:48:06 +0200

    Added React app setting params to Posts and Stats (#23925)
    ref https://linear.app/ghost/issue/PROD-2036/state-combinations
    
    - Added a custom app settings object to both Posts and Stats app to
    control various states of the app (e.g. paid memberships are off). With
    this change we keep all settings in Ember instead of handling this logic
    inside each individual React app.

--- apps/stats/src/App.tsx
+++ apps/stats/src/App.tsx
@@ -2,15 +2,43 @@ import GlobalDataProvider from './providers/GlobalDataProvider';
 import {APP_ROUTE_PREFIX, routes} from '@src/routes';
 import {FrameworkProvider, Outlet, RouterProvider, TopLevelFrameworkProps} from '@tryghost/admin-x-framework';
 import {ShadeApp, ShadeAppProps} from '@tryghost/shade';
+import {createContext, useContext} from 'react';
+
+type AppSettingsType = {
+    paidMembersEnabled: boolean;
+}
 
 interface AppProps {
     framework: TopLevelFrameworkProps;
     designSystem: ShadeAppProps;
+    appSettings?: AppSettingsType;
 }
 
-const App: React.FC<AppProps> = ({framework, designSystem}) => {
+interface AppContextType {
+    appSettings?: AppSettingsType;
+    externalNavigate: (url: string) => void;
+}
+
+const AppContext = createContext<AppContextType | undefined>(undefined);
+
+export const useAppContext = () => {
+    const context = useContext(AppContext);
+    if (context === undefined) {
+        throw new Error('useAppContext must be used within an AppProvider');
+    }
+    return context;
+};
+
+const App: React.FC<AppProps> = ({framework, designSystem, appSettings}) => {
+    const appContextValue: AppContextType = {
+        appSettings,
+        externalNavigate: (url: string) => {
+            window.location.href = url;
+        }
+    };
+
     return (
-        <FrameworkProvider 
+        <FrameworkProvider
             {...framework}
             queryClientOptions={{
                 staleTime: 0, // Always consider data stale (matches Ember admin route behavior)
@@ -18,13 +46,15 @@ const App: React.FC<AppProps> = ({framework, designSystem}) => {
                 refetchOnWindowFocus: false // Disable window focus refetch (Ember admin doesn't have this)
             }}
         >
-            <RouterProvider prefix={APP_ROUTE_PREFIX} routes={routes}>
-                <GlobalDataProvider>
-                    <ShadeApp darkMode={designSystem.darkMode} fetchKoenigLexical={null}>
-                        <Outlet />
-                    </ShadeApp>
-                </GlobalDataProvider>
-            </RouterProvider>
+            <AppContext.Provider value={appContextValue}>
+                <RouterProvider prefix={APP_ROUTE_PREFIX} routes={routes}>
+                    <GlobalDataProvider>
+                        <ShadeApp darkMode={designSystem.darkMode} fetchKoenigLexical={null}>
+                            <Outlet />
+                        </ShadeApp>
+                    </GlobalDataProvider>
+                </RouterProvider>
+            </AppContext.Provider>
         </FrameworkProvider>
     );
 };
