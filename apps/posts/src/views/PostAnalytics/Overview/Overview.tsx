694eeab5fe7995ec2bb63b2070edc34f15e9e601
Peter Zimon <peter.zimon@gmail.com>
2025-06-20 14:19:29 +0200

    Refinements

--- apps/posts/src/views/PostAnalytics/Overview/Overview.tsx
+++ apps/posts/src/views/PostAnalytics/Overview/Overview.tsx
@@ -125,54 +125,51 @@ const Overview: React.FC = () => {
                 </div>
             </PostAnalyticsHeader>
             <PostAnalyticsContent>
-                {(showWebSection || showNewsletterSection) && (
-                    <div className={showWebSection && showNewsletterSection ? 'grid grid-cols-2 gap-8' : ''}>
-                        {showWebSection && (
-                            <WebOverview
-                                chartData={processedChartData}
-                                fullWidth={!showNewsletterSection}
-                                isLoading={chartIsLoading || kpiIsLoading || isSourcesLoading}
-                                range={chartRange}
-                                sourcesData={sourcesData}
-                                visitors={kpiValues.visits}
-                            />
-                        )}
-                        {showNewsletterSection && (
-                            <NewsletterOverview isNewsletterStatsLoading={isPostLoading} post={post as Post} />
-                        )}
-                    </div>
-                )}
-                <Card className='group overflow-hidden p-0'>
-                    <div className='relative flex items-center justify-between gap-6'>
-                        <CardHeader>
-                            <CardTitle className='flex items-center gap-1.5 text-lg'>
-                                <LucideIcon.Sprout size={16} strokeWidth={1.5} />
+                <div className='grid grid-cols-2 gap-8'>
+                    {showWebSection && (
+                        <WebOverview
+                            chartData={processedChartData}
+                            fullWidth={!showNewsletterSection}
+                            isLoading={chartIsLoading || kpiIsLoading || isSourcesLoading}
+                            range={chartRange}
+                            sourcesData={sourcesData}
+                            visitors={kpiValues.visits}
+                        />
+                    )}
+                    {showNewsletterSection && (
+                        <NewsletterOverview isNewsletterStatsLoading={isPostLoading} post={post as Post} />
+                    )}
+                    <Card className='group col-span-2 overflow-hidden p-0'>
+                        <div className='relative flex items-center justify-between gap-6'>
+                            <CardHeader>
+                                <CardTitle className='flex items-center gap-1.5 text-lg'>
+                                    <LucideIcon.Sprout size={16} strokeWidth={1.5} />
                                 Growth
-                            </CardTitle>
-                        </CardHeader>
-                        <Button className='absolute right-6 translate-x-10 opacity-0 transition-all duration-200 group-hover:translate-x-0 group-hover:opacity-100' size='sm' variant='outline' onClick={() => {
-                            navigate(`/analytics/beta/${postId}/growth`);
-                        }}>View more</Button>
-                    </div>
-                    <CardContent className='grid grid-cols-3 items-stretch px-0'>
-                        {kpiIsLoading ?
-                            Array.from({length: 3}, (_, i) => (
-                                <div key={i} className='h-[98px] gap-1 border-r px-6 py-5 last:border-r-0'>
-                                    <Skeleton className='w-2/3' />
-                                    <Skeleton className='h-7 w-12' />
-                                </div>
-                            ))
-                            :
-                            <>
-                                <KpiCard className='grow gap-1 py-0'>
-                                    <KpiCardLabel>
+                                </CardTitle>
+                            </CardHeader>
+                            <Button className='absolute right-6 translate-x-10 opacity-0 transition-all duration-200 group-hover:translate-x-0 group-hover:opacity-100' size='sm' variant='outline' onClick={() => {
+                                navigate(`/analytics/beta/${postId}/growth`);
+                            }}>View more</Button>
+                        </div>
+                        <CardContent className='grid grid-cols-3 items-stretch px-0'>
+                            {kpiIsLoading ?
+                                Array.from({length: 3}, (_, i) => (
+                                    <div key={i} className='h-[98px] gap-1 border-r px-6 py-5 last:border-r-0'>
+                                        <Skeleton className='w-2/3' />
+                                        <Skeleton className='h-7 w-12' />
+                                    </div>
+                                ))
+                                :
+                                <>
+                                    <KpiCard className='grow gap-1 py-0'>
+                                        <KpiCardLabel>
                                         Free members
-                                    </KpiCardLabel>
-                                    <KpiCardContent>
-                                        <KpiCardValue className='text-[2.2rem]'>{formatNumber((totals?.free_members || 0))}</KpiCardValue>
-                                    </KpiCardContent>
-                                </KpiCard>
-                                {appSettings?.paidMembersEnabled &&
+                                        </KpiCardLabel>
+                                        <KpiCardContent>
+                                            <KpiCardValue className='text-[2.2rem]'>{formatNumber((totals?.free_members || 0))}</KpiCardValue>
+                                        </KpiCardContent>
+                                    </KpiCard>
+                                    {appSettings?.paidMembersEnabled &&
                                 <>
                                     <KpiCard className='grow gap-1 py-0'>
                                         <KpiCardLabel>
@@ -191,11 +188,12 @@ const Overview: React.FC = () => {
                                         </KpiCardContent>
                                     </KpiCard>
                                 </>
-                                }
-                            </>
-                        }
-                    </CardContent>
-                </Card>
+                                    }
+                                </>
+                            }
+                        </CardContent>
+                    </Card>
+                </div>
             </PostAnalyticsContent>
         </>
     );
