d690ea6dbe7f585a59d6d1bb0eb607335c62b71b
Igor Balos <ibalosh@gmail.com>
2025-06-20 14:52:16 +0200

    Connect trafic analytics tests with TinyBird (#23957)
    ref
    https://linear.app/ghost/issue/PROD-1796/testing-add-acceptance-test-suite-for-stats-and-posts-app
    
    * added basic analytics test for location verifications, that will help
    us verify that dashboard can read data from TinyBird
    * added configuration by environment variables for TinyBird to make it
    configurable
    * provided API helper to get data into TinyBird
    
    - [x] I've read and followed the [Contributor
    Guide](https://github.com/TryGhost/Ghost/blob/main/.github/CONTRIBUTING.md)
    - [x] I've explained my change
    - [x] I've written an automated test to prove my change works

--- apps/stats/test/acceptance/pages/OverviewPage.ts
+++ apps/stats/test/acceptance/pages/OverviewPage.ts
@@ -3,13 +3,11 @@ import {Locator, Page} from '@playwright/test';
 
 class OverviewPage extends AnalyticsPage {
     public readonly header: Locator;
-    public readonly body: Locator;
 
     constructor(page: Page) {
         super(page);
 
         this.header = page.getByRole('heading', {name: 'Analytics'});
-        this.body = page.locator('body');
     }
 }
 
