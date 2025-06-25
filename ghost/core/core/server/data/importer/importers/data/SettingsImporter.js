de902c80af24a5d4a4be3cc3dac0a4c3c5f63e53
Michael Barrett <mike@ghost.org>
2025-06-25 12:09:44 +0100

    🔥 Removed `updated_by` & `created_by` fields (#24053)
    ref https://linear.app/ghost/issue/PROD-1597
    
    Removed `updated_by` & `created_by` fields as they are deprecated and
    should not be being used anywhere meaningful in the codebase. Removal of
    these fields is a prerequisite for the work needed for
    https://linear.app/ghost/issue/PROD-1594 (removal of hardocded owner
    user id `1`)
    
    ---------
    
    Co-authored-by: Hannah Wolfe <github.erisds@gmail.com>

--- ghost/core/core/server/data/importer/importers/data/SettingsImporter.js
+++ ghost/core/core/server/data/importer/importers/data/SettingsImporter.js
@@ -140,9 +140,7 @@ class SettingsImporter extends BaseImporter {
                             group: to.group,
                             type: to.type,
                             flags: to.flags || null,
-                            created_by: deprecatedSetting.created_by || 1,
                             created_at: deprecatedSetting.created_at,
-                            updated_by: deprecatedSetting.updated_by || 1,
                             updated_at: deprecatedSetting.updated_at
                         });
                     });
