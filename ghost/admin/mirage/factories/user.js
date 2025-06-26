b371b3ac00d2a0aba2a498680ff543039b853392
Michael Barrett <mike@ghost.org>
2025-06-26 17:21:50 +0100

    🔥 Removed `updatedBy` & `createdBy` fields in the admin app (#24116)
    ref https://linear.app/ghost/issue/PROD-1597
    ref https://github.com/TryGhost/Ghost/pull/24053
    
    Removed `updatedBy` & `createdBy` fields in the admin app as they are
    deprecated and should not be being used anywhere meaningful in the
    codebase

--- ghost/admin/mirage/factories/user.js
+++ ghost/admin/mirage/factories/user.js
@@ -5,7 +5,6 @@ export default Factory.extend({
     bio: null,
     coverImage: null,
     createdAt: '2015-09-02T13:41:50.000Z',
-    createdBy: null,
     email(i) { return `user-${i}@example.com`; },
     profileImage: '//www.gravatar.com/avatar/3ae045bc198a157401827c8455cd7c99?s=250&d=mm&r=x',
     lastLogin: '2015-11-02T16:12:05.000Z',
@@ -17,7 +16,6 @@ export default Factory.extend({
     status: 'active',
     tour: null,
     updatedAt: '2015-11-02T16:12:05.000Z',
-    updatedBy: '1',
     website: 'http://example.com',
     posts() { return []; },
     roles() { return []; }
