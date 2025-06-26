1baed5a4f3ba3255787c4e33f0681cdd1ddcea0f
Michael Barrett <mike@ghost.org>
2025-06-26 17:21:50 +0100

    🔥 Removed `updatedBy` & `createdBy` fields in the admin app (#24116)
    ref https://linear.app/ghost/issue/PROD-1597
    ref https://github.com/TryGhost/Ghost/pull/24053
    
    Removed `updatedBy` & `createdBy` fields in the admin app as they are
    deprecated and should not be being used anywhere meaningful in the
    codebase

--- ghost/admin/mirage/factories/label.js
+++ ghost/admin/mirage/factories/label.js
@@ -3,11 +3,9 @@ import {Factory} from 'miragejs';
 
 export default Factory.extend({
     createdAt() { return moment.utc().toISOString(); },
-    createdBy: 1,
     name(i) { return `Label ${i}`; },
     slug(i) { return `label-${i}`; },
     updatedAt() { return moment.utc().toISOString(); },
-    updatedBy: 1,
     count() {
         // this gets updated automatically by the label serializer
         return {members: 0};
