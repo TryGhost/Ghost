1af49283f91763affbd3db4c38dcf66e8ba00d2e
Hannah Wolfe <github.erisds@gmail.com>
2025-06-27 15:07:49 +0100

    Removed 4.x migrations, added final error migration  (#24075)
    ref https://github.com/TryGhost/Ghost/commit/145dc4651aa9de71d23b25c27a60acc2c10990db
    ref
    https://github.com/TryGhost/Ghost/commit/b788604e15f676957f8cb603978f26a30bc80b1e
    ref https://ghost.org/docs/update-major-version/. 
    
    - In order to upgrade to 6.x you need to be on: 
      a) the very latest 4.x
      b) any 5.x version
    - We clean out migrations from the previous migration as it allows us to
    keep the codebase a little cleaner
    - And means that we don't have to maintain unmanageable levels of
    backwards compatibility across migration utils etc
    - We add a "final migration" to the very last minor of 4.x with migrations (4.47)
    - This ensures that attempting to update to 6.0 from a too-early version will error
    with a clear message telling you what steps to take instead.
    - Note: we also have to noop the additional migration in 4.47 so that knex-migrator
      doesn't get confused.

--- ghost/core/core/server/data/migrations/versions/4.46/2022-04-13-12-01-add-updated-at-newsletters.js
+++
@@ -1,6 +0,0 @@
-const {createAddColumnMigration} = require('../../utils');
-
-module.exports = createAddColumnMigration('newsletters', 'updated_at', {
-    type: 'dateTime',
-    nullable: true
-});
