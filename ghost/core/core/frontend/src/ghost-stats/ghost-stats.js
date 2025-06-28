514c0f1cfedf5aadc74ef6c85bf6de09c79a513c
Chris Raible <chris@ghost.org>
2025-06-27 22:43:14 -0700

    fixup! Added event_id uuid to ghost-stats event payload

--- ghost/core/core/frontend/src/ghost-stats/ghost-stats.js
+++ ghost/core/core/frontend/src/ghost-stats/ghost-stats.js
@@ -67,6 +67,7 @@ export class GhostStats {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
             const r = Math.random() * 16 | 0;
             const v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
         });
     }
 
