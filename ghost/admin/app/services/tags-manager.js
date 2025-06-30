47da9e0bc35f0ac08d474bc74d3a988df801b7af
Kevin Ansfield <kevin@ghost.org>
2025-06-30 15:14:49 +0100

    🎨 Added search to tags filter dropdown on posts list
    ref https://linear.app/ghost/issue/PROD-1605/
    
    When posts list filtering was first implemented we had no way of filtering tags server-side so we resorted to fetching all tags to populate the tags filter dropdown in one big list. Many things have changed since:
    - sites have grown, some now have 10s of thousands of tags
    - we're removing `?limit=all` support so we can no longer get every tag in one API request
    - our API now supports "contains" queries with `?filter=tags.name:~'query'`
    
    To improve this, we've:
    - removed the all-tags fetch from the `posts` route
    - updated tags filter dropdown behaviour
      - added search field
      - added initial load of 100 entries when filter is opened
      - search is performed via a new `tags-manager` service so the functionality can be re-used in other parts of the app later on
    - added infinite scroll to tags filter dropdown
      - uses `vertical-collection`'s `lastReached` option to call a new `loadMoreTags` action when fired
      - `posts` controller keeps track of the initial tags list and the searched tags list so it can add tags to them as needed
    - updated mirage posts endpoint to filter by tag for easier testing
    - fixed missing logging of mirage requests when logging is enabled

---
+++ ghost/admin/app/services/tags-manager.js
@@ -0,0 +1,26 @@
+import Service, {inject as service} from '@ember/service';
+import {task, timeout} from 'ember-concurrency';
+
+export default class TagsManagerService extends Service {
+    @service store;
+
+    _loadedTags = this.store.peekAll('tag');
+
+    get loadedTags() {
+        return this.sortTags(this._loadedTags);
+    }
+
+    sortTags(tags = []) {
+        return tags
+            .filter(tag => tag.get('id') !== null) // exclude unsaved tags
+            .sort((tagA, tagB) => tagA.name.localeCompare(tagB.name, undefined, {ignorePunctuation: true}));
+    }
+
+    @task({restartable: true})
+    *searchTagsTask(term, {page = 1} = {}) {
+        // debounce the search
+        yield timeout(250);
+        const safeTerm = encodeURIComponent(term).replace(/'/g, '%27'); // encodeURIComponent doesn't escape single quotes
+        return yield this.store.query('tag', {filter: `tags.name:~'${safeTerm}'`, limit: 100, page, order: 'name asc'});
+    }
+}
