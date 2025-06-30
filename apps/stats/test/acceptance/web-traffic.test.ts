8367860ab7f5e223fefcb5ee48106b22036247fc
Igor Balos <ibalosh@gmail.com>
2025-06-30 14:39:52 +0200

    Fixed broken acceptance tests for analytics
    * Tests were not working since, there was an update that includes tinybird token

--- apps/stats/test/acceptance/web-traffic.test.ts
+++ apps/stats/test/acceptance/web-traffic.test.ts
@@ -1,33 +1,13 @@
 import WebTrafficTab from './pages/WebTrafficTab.ts';
-import {addAnalyticsEvent, statsConfig} from '../utils/tinybird-helpers.ts';
+import {addAnalyticsEvent, mockedRequests} from '../utils/tinybird-helpers.ts';
 import {expect, test} from '@playwright/test';
 import {faker} from '@faker-js/faker';
-import {
-    globalDataRequests,
-    mockApi,
-    responseFixtures
-} from '@tryghost/admin-x-framework/test/acceptance';
+import {mockApi} from '@tryghost/admin-x-framework/test/acceptance';
 
 test.describe('Stats App - Web Traffic', () => {
     test('loads tab with correct top sources', async ({page}) => {
         const siteUuid = faker.string.uuid();
-
-        await mockApi({
-            page, requests: {
-                ...globalDataRequests,
-                browseConfig: {
-                    method: 'GET', path: '/config/', response: {
-                        config: {
-                            ...responseFixtures.config.config,
-                            stats: {
-                                ...statsConfig,
-                                id: siteUuid
-                            }
-                        }
-                    }
-                }
-            }
-        });
+        await mockApi({page, requests: mockedRequests(siteUuid)});
 
         const domain = 'example.com';
         await addAnalyticsEvent({siteUuid: siteUuid, referrer: faker.internet.url(), referrerSource: domain});
