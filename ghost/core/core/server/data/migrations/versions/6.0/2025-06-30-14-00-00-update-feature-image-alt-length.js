8aad8f8f340ed14dc6ef106e9a519d938a24a652
Troy Ciesco <tmciesco@gmail.com>
2025-07-01 17:22:39 -0400

    update migration logging

--- ghost/core/core/server/data/migrations/versions/6.0/2025-06-30-14-00-00-update-feature-image-alt-length.js
+++ ghost/core/core/server/data/migrations/versions/6.0/2025-06-30-14-00-00-update-feature-image-alt-length.js
@@ -5,7 +5,7 @@ const DatabaseInfo = require('@tryghost/database-info');
 module.exports = createNonTransactionalMigration(
     async function up(knex) {
         if (DatabaseInfo.isSQLite(knex)) {
-            logging.warn('Skipping migration for SQLite3');
+            logging.warn('Skipping migration for SQLite3 (fixed-length VARCHAR columns are not enforced)');
             return;
         }
         
@@ -20,7 +20,6 @@ module.exports = createNonTransactionalMigration(
         });
     },
     async function down() {
-        logging.warn('Not changing posts_meta.feature_image_alt column');
-        logging.warn('Not changing post_revisions.feature_image_alt column');
+        logging.warn('Reverting posts_meta.feature_image_alt and post_revisions.feature_image_alt column length increases is not supported (major version migrations are not reversible)');
     }
 );
