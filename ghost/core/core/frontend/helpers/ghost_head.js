056685ea513de1a44449b69101c43482777db4ed
Chris Raible <chris@ghost.org>
2025-06-30 17:52:44 -0700

    Disabled Tinybird tracker token in production (#24211)
    ref
    https://linear.app/ghost/issue/PROD-1961/remove-the-tracker-token-from-ghost-config-and-tracker-script
    
    The `ghost-stats.js` script has always required a tracker token to be
    provided via data attribute rendered by ghost_head. When sending events
    directly to Tinybird without the analytics service between them, this is
    a hard requirement. But now that the analytics service has its own
    tracker token, this is no longer a required value for the `ghost-stats`
    script, and shouldn't be used in production at all.
    
    Sometimes it is useful to setup Ghost with a tracker token in local
    development though, so this makes the token optional from the
    perspective of the `ghost-stats` script, and changes `ghost_head` so the
    token is only rendered if `NODE_ENV` is not production.

--- ghost/core/core/frontend/helpers/ghost_head.js
+++ ghost/core/core/frontend/helpers/ghost_head.js
@@ -158,6 +158,8 @@ function getTinybirdTrackerScript(dataRoot) {
 
     const src = getAssetUrl('public/ghost-stats.min.js', false);
 
+    const env = config.get('env');
+
     const statsConfig = config.get('tinybird:tracker');
     const localConfig = config.get('tinybird:tracker:local');
     const localEnabled = localConfig?.enabled ?? false;
@@ -174,7 +176,7 @@ function getTinybirdTrackerScript(dataRoot) {
         member_status: dataRoot.member?.status
     }, (value, key) => `tb_${key}="${value}"`).join(' ');
 
-    return `<script defer src="${src}" data-stringify-payload="false" ${datasource ? `data-datasource="${datasource}"` : ''} data-storage="localStorage" data-host="${endpoint}" data-token="${token}" ${tbParams}></script>`;
+    return `<script defer src="${src}" data-stringify-payload="false" ${datasource ? `data-datasource="${datasource}"` : ''} data-storage="localStorage" data-host="${endpoint}" ${token && env !== 'production' ? `data-token="${token}"` : ''} ${tbParams}></script>`;
 }
 
 /**
