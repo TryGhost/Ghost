68959080a6e70b88d6ec5a911bd2950f74e31ca3
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-07-02 14:57:50 +0800

    Fixed incorrect inline follow button display in ActivityPub

--- apps/admin-x-activitypub/src/views/Feed/Note.tsx
+++ apps/admin-x-activitypub/src/views/Feed/Note.tsx
@@ -202,8 +202,8 @@ const Note = () => {
                         <div className={`mx-auto px-8 pb-10 pt-5`}>
                             {!threadParents.length &&
                             <div className={`col-[2/3] mx-auto flex w-full items-center gap-3 ${canGoBack ? 'pt-10' : 'pt-5'}`}>
-                                <div className='relative z-10 pt-[3px]'>
-                                    <APAvatar author={currentPost.actor} showFollowButton={!currentPost.actor.authored && !currentPost.actor.followedByMe}/>
+                                <div className='relative z-10'>
+                                    <APAvatar author={currentPost.actor} showFollowButton={!currentPost.object.authored && !currentPost.actor.followedByMe}/>
                                 </div>
                                 <div className='relative z-10 flex w-full min-w-0 cursor-pointer flex-col overflow-visible text-[1.5rem]' onClick={(e) => {
                                     handleProfileClick(currentPost.actor, navigate, e);
