8e9603cca17eb0507f63f846d57c53040a684d6e
Steve Larson <9larsons@gmail.com>
2025-07-02 05:02:44 -0500

    Updated Admin homepage to analytics (#24166)
    ref https://linear.app/ghost/issue/PROD-2165/
    - updated default route to /analytics (if enabled by beta flag,
    currently)
    - updated route to show onboarding flow in front of analytics if
    incomplete
    
    ---------
    
    Co-authored-by: Peter Zimon <peter.zimon@gmail.com>

--- ghost/admin/app/routes/home.js
+++ ghost/admin/app/routes/home.js
@@ -1,7 +1,9 @@
 import Route from '@ember/routing/route';
+import {inject} from 'ghost-admin/decorators/inject';
 import {inject as service} from '@ember/service';
 
 export default class HomeRoute extends Route {
+    @inject config;
     @service feature;
     @service modals;
     @service router;
@@ -15,10 +17,19 @@ export default class HomeRoute extends Route {
             return this.router.transitionTo('setup.done');
         }
 
-        if (this.settings.socialWebEnabled && this.session.user.isAdmin) {
+        // Redirect to Analytics if trafficAnalytics is enabled and user has access
+        if (this.config.stats && this.feature.trafficAnalytics && this.session.user?.isAdmin) {
+            return this.router.transitionTo('stats-x');
+        }
+
+        if (this.settings.socialWebEnabled && this.session.user?.isAdmin) {
             this.router.transitionTo('activitypub-x');
         } else {
-            this.router.transitionTo('dashboard');
+            if (this.config.labs?.updatedMainNav) {
+                this.router.transitionTo('stats-x');
+            } else {
+                this.router.transitionTo('dashboard');
+            }
         }
     }
 
