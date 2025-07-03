f3e6560c699cfa407c1bc60e5a40961a1f7127bb
Cathy Sarisky <catsarisky@gmail.com>
2025-07-02 20:13:40 -0400

    fix test, fix missed isEitherEditor

--- ghost/core/test/integration/migrations/migration.test.js
+++ ghost/core/test/integration/migrations/migration.test.js
@@ -188,9 +188,9 @@ describe('Migrations', function () {
 
             permissions.should.havePermission('Browse labels', ['Administrator', 'Editor', 'Author', 'Admin Integration']);
             permissions.should.havePermission('Read labels', ['Administrator', 'Editor', 'Author', 'Admin Integration']);
-            permissions.should.havePermission('Edit labels', ['Administrator', 'Admin Integration', 'Editor']);
-            permissions.should.havePermission('Add labels', ['Administrator', 'Admin Integration', 'Editor']);
-            permissions.should.havePermission('Delete labels', ['Administrator', 'Admin Integration', 'Editor']);
+            permissions.should.havePermission('Edit labels', ['Administrator', 'Editor', 'Admin Integration']);
+            permissions.should.havePermission('Add labels', ['Administrator', 'Editor', 'Admin Integration']);
+            permissions.should.havePermission('Delete labels', ['Administrator', 'Editor', 'Admin Integration']);
 
             permissions.should.havePermission('Read member signin urls');
             permissions.should.havePermission('Read identities');
