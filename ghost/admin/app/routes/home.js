4528923858099c1b23604cfe0bc978233e1c3d66
Steve Larson <9larsons@gmail.com>
2025-07-09 07:17:22 -0500

    Cleaned up Admin's home routing (#24254)
    ref https://linear.app/ghost/issue/PROD-2165/
    - we no longer need to direct to Social/activitypub
    - we should only be directing to Analytics, but need to be behind ui
    flag for the moment
    - cleaned up what appears to be dead code for onboarding, which is in
    the analytics handlebars component now

--- ghost/admin/app/routes/home.js
+++ ghost/admin/app/routes/home.js
@@ -1,41 +1,24 @@
-import Route from '@ember/routing/route';
+import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
 import {inject} from 'ghost-admin/decorators/inject';
 import {inject as service} from '@ember/service';
 
-export default class HomeRoute extends Route {
+export default class HomeRoute extends AuthenticatedRoute {
     @inject config;
     @service feature;
-    @service modals;
     @service router;
-    @service session;
-    @service settings;
 
     beforeModel(transition) {
         super.beforeModel(...arguments);
 
+        // This is needed to initialize the checklist for sites that have been already set up
         if (transition.to?.queryParams?.firstStart === 'true') {
             return this.router.transitionTo('setup.done');
         }
-
-        // Redirect to Analytics if trafficAnalytics is enabled and user has access
-        if (this.config.stats && this.feature.trafficAnalytics && this.session.user?.isAdmin) {
-            return this.router.transitionTo('stats-x');
-        }
-
-        if (this.settings.socialWebEnabled && this.session.user?.isAdmin) {
-            this.router.transitionTo('activitypub-x');
+        
+        if (this.config.labs?.ui60 && this.feature.trafficAnalytics) {
+            this.router.transitionTo('stats-x');
         } else {
-            // stats-x currently redirects back to home if analytics is not enabled
-            // we need to check that here to avoid an infinite loop
-            if (this.config.labs?.ui60 && (this.config.stats && this.feature.trafficAnalytics)) {
-                this.router.transitionTo('stats-x');
-            } else {
-                this.router.transitionTo('dashboard');
-            }
+            this.router.transitionTo('dashboard');
         }
-    }
-
-    resetController(controller) {
-        controller.firstStart = false;
     }
 }
