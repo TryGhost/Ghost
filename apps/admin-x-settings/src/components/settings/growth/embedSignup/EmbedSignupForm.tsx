4e0b7c76587d411f003c027767415eb296e87988
Peter Zimon <peter.zimon@gmail.com>
2025-07-09 16:00:51 +0200

    Removed unnecessary ui60 flag

--- apps/admin-x-settings/src/components/settings/growth/embedSignup/EmbedSignupForm.tsx
+++ apps/admin-x-settings/src/components/settings/growth/embedSignup/EmbedSignupForm.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import TopLevelGroup from '../../../TopLevelGroup';
-import useFeatureFlag from '../../../../hooks/useFeatureFlag';
 import {Button} from '@tryghost/admin-x-design-system';
 import {useRouting} from '@tryghost/admin-x-framework/routing';
 
@@ -9,7 +8,6 @@ const EmbedSignupForm: React.FC<{ keywords: string[] }> = ({keywords}) => {
     const openPreviewModal = () => {
         updateRoute('embed-signup-form/show');
     };
-    const ui60 = useFeatureFlag('ui60');
 
     return (
         <TopLevelGroup
@@ -18,7 +16,7 @@ const EmbedSignupForm: React.FC<{ keywords: string[] }> = ({keywords}) => {
             keywords={keywords}
             navid='embed-signup-form'
             testId='embed-signup-form'
-            title={ui60 ? 'Signup forms' : 'Embeddable signup form'}
+            title='Signup forms'
         />
     );
 };
