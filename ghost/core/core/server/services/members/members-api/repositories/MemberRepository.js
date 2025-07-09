602b4c8b88d30c1d9d3f508eff445c30ffa8e69a
Fabien O'Carroll <fabien@allou.is>
2025-07-09 12:02:48 +0700

    fixup! Removed `FOR UPDATE` when fetching Default Tier

--- ghost/core/core/server/services/members/members-api/repositories/MemberRepository.js
+++ ghost/core/core/server/services/members/members-api/repositories/MemberRepository.js
@@ -947,9 +947,7 @@ module.exports = class MemberRepository {
             ghostProduct = await this._productRepository.get({stripe_product_id: subscriptionPriceData.product}, options);
             // Use first Ghost product as default product in case of missing link
             if (!ghostProduct) {
-                ghostProduct = await this._productRepository.getDefaultProduct({
-                    ...options
-                });
+                ghostProduct = await this._productRepository.getDefaultProduct(options);
             }
 
             // Link Stripe Product & Price to Ghost Product
