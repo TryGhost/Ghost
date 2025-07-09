2aba9dcbf5e4350925762e8a39fd4915018fd0e8
Peter Zimon <zimo@ghost.org>
2025-07-09 18:29:15 +0200

    Updated settings navigation structure for 6.0 (#24299)
    ref https://linear.app/ghost/issue/PROD-2209/settings-ui-changes
    
    - Settings navigation for 6.0 was not reflecting the updated structure
    of the Admin

--- apps/admin-x-settings/src/components/settings/growth/GrowthSettings.tsx
+++ apps/admin-x-settings/src/components/settings/growth/GrowthSettings.tsx
@@ -4,10 +4,21 @@ import React from 'react';
 import Recommendations from './Recommendations';
 import SearchableSection from '../../SearchableSection';
 import TipsAndDonations from './TipsAndDonations';
+import useFeatureFlag from '../../../hooks/useFeatureFlag';
 import {checkStripeEnabled, getSettingValues} from '@tryghost/admin-x-framework/api/settings';
 import {useGlobalData} from '../../providers/GlobalDataProvider';
 
+export const searchKeywords5x = {
+    network: ['network', 'social web', 'social', 'web', 'activitypub', 'activity pub', 'fediverse', 'federated'],
+    tips: ['growth', 'tips', 'donations', 'one time', 'payment'],
+    embedSignupForm: ['growth', 'embeddable signup form', 'embeddable form', 'embeddable sign up form', 'embeddable sign up'],
+    recommendations: ['growth', 'recommendations', 'recommend', 'blogroll'],
+    offers: ['growth', 'offers', 'discounts', 'coupons', 'promotions']
+};
+
 export const searchKeywords = {
+    network: ['network', 'social web', 'social', 'web', 'activitypub', 'activity pub', 'fediverse', 'federated'],
+    explore: ['explore', 'web', 'social', 'web', 'growth', 'list'],
     tips: ['growth', 'tips', 'donations', 'one time', 'payment'],
     embedSignupForm: ['growth', 'embeddable signup form', 'embeddable form', 'embeddable sign up form', 'embeddable sign up'],
     recommendations: ['growth', 'recommendations', 'recommend', 'blogroll'],
@@ -18,13 +29,24 @@ const GrowthSettings: React.FC = () => {
     const {config, settings} = useGlobalData();
     const [hasTipsAndDonations] = getSettingValues(settings, ['donations_enabled']) as [boolean];
     const hasStripeEnabled = checkStripeEnabled(settings || [], config || {});
+    const ui60 = useFeatureFlag('ui60');
+
+    if (!ui60) {
+        return (
+            <SearchableSection keywords={Object.values(searchKeywords5x).flat()} title='Growth'>
+                <Recommendations keywords={searchKeywords5x.recommendations} />
+                <EmbedSignupForm keywords={searchKeywords5x.embedSignupForm} />
+                {hasStripeEnabled && <Offers keywords={searchKeywords5x.offers} />}
+                {hasTipsAndDonations && hasStripeEnabled && <TipsAndDonations keywords={searchKeywords5x.tips} />}
+            </SearchableSection>
+        );
+    }
 
     return (
         <SearchableSection keywords={Object.values(searchKeywords).flat()} title='Growth'>
             <Recommendations keywords={searchKeywords.recommendations} />
             <EmbedSignupForm keywords={searchKeywords.embedSignupForm} />
             {hasStripeEnabled && <Offers keywords={searchKeywords.offers} />}
-            {hasTipsAndDonations && hasStripeEnabled && <TipsAndDonations keywords={searchKeywords.tips} />}
         </SearchableSection>
     );
 };
