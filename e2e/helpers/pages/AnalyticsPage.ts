1e7734e169d2b4e0e30cc1795476443219445428
Chris Raible <chris@ghost.org>
2025-07-08 21:06:12 -0700

    Removed unused page objects for now

--- e2e/helpers/pages/AnalyticsPage.ts
+++
@@ -1,8 +0,0 @@
-import {Page} from '@playwright/test';
-import {AdminPage} from './AdminPage';
-
-export class AnalyticsPage extends AdminPage {
-    constructor(page: Page) {
-        super(page, '/ghost/#/analytics');
-    }
-}
