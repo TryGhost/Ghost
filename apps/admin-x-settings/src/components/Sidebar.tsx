c81687f6be35adfd659eb1a454e882ae685956ed
Peter Zimon <zimo@ghost.org>
2025-07-14 14:36:27 +0200

    Added UI for ActivityPub settings (#24346)
    ref
    https://linear.app/ghost/issue/PROD-2172/add-setting-to-enabledisable-activitypub-in-ga
    
    - In order to allow users to turn ActivityPub (Network) on or off, we
    needed to add a UI for it in settings. This PR adds a barebones version
    to wire up backend and test functionality.

--- apps/admin-x-settings/src/components/Sidebar.tsx
+++ apps/admin-x-settings/src/components/Sidebar.tsx
@@ -242,6 +242,7 @@ const Sidebar: React.FC = () => {
 
                 {ui60 ?
                     <SettingNavSection isVisible={checkVisible(Object.values(growthSearchKeywords).flat())} title="Growth">
+                        <NavItem icon='ap-network' keywords={growthSearchKeywords.network} navid='network' title="Network" onClick={handleSectionClick} />
                         <NavItem icon='heart' keywords={growthSearchKeywords.recommendations} navid='recommendations' title="Recommendations" onClick={handleSectionClick} />
                         <NavItem icon='emailfield' keywords={growthSearchKeywords.embedSignupForm} navid='embed-signup-form' title="Signup forms" onClick={handleSectionClick} />
                         {hasStripeEnabled && <NavItem icon='discount' keywords={growthSearchKeywords.offers} navid='offers' title="Offers" onClick={handleSectionClick} />}
--- apps/admin-x-settings/src/components/settings/growth/GrowthSettings.tsx
+++ apps/admin-x-settings/src/components/settings/growth/GrowthSettings.tsx
@@ -1,4 +1,5 @@
 import EmbedSignupForm from './embedSignup/EmbedSignupForm';
+import Network from './Network';
 import Offers from './Offers';
 import React from 'react';
 import Recommendations from './Recommendations';
@@ -16,6 +17,7 @@ export const searchKeywords5x = {
 };
 
 export const searchKeywords = {
+    network: ['growth', 'network', 'activitypub', 'blog', 'fediverse', 'sharing'],
     recommendations: ['growth', 'recommendations', 'recommend', 'blogroll'],
     embedSignupForm: ['growth', 'embeddable signup form', 'embeddable form', 'embeddable sign up form', 'embeddable sign up'],
     offers: ['growth', 'offers', 'discounts', 'coupons', 'promotions']
@@ -40,6 +42,7 @@ const GrowthSettings: React.FC = () => {
 
     return (
         <SearchableSection keywords={Object.values(searchKeywords).flat()} title='Growth'>
+            <Network keywords={searchKeywords.network} />
             <Recommendations keywords={searchKeywords.recommendations} />
             <EmbedSignupForm keywords={searchKeywords.embedSignupForm} />
             {hasStripeEnabled && <Offers keywords={searchKeywords.offers} />}
