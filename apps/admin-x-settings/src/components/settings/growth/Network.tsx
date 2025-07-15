572a09e89bd27768aef8bef913f3ed16fccf5f6f
Peter Zimon <peter.zimon@gmail.com>
2025-07-15 12:33:43 +0200

    Refined copy

--- apps/admin-x-settings/src/components/settings/growth/Network.tsx
+++ apps/admin-x-settings/src/components/settings/growth/Network.tsx
@@ -57,7 +57,7 @@ const Network: React.FC<{ keywords: string[] }> = ({keywords}) => {
                 onChange={toggleSocialWebSetting}
             />
         }
-        description='Make your content visible to millions across Flipboard, Mastodon, Threads, Bluesky, and WordPress.'
+        description='Distribute posts to the social web, so people can discover and follow your content across BlueSky, Threads, Mastodon, Flipboard, WordPress, and more.'
         keywords={keywords}
         navid='network'
         testId='network'
@@ -71,17 +71,17 @@ const Network: React.FC<{ keywords: string[] }> = ({keywords}) => {
                         key: 'private',
                         value:
                         isDisabled &&
-                            <div className='flex w-full gap-1.5 rounded-md border border-grey-200 bg-grey-75 p-3 text-sm'>
+                            <div className='flex w-full gap-1.5 rounded-md border border-grey-200 bg-grey-75 p-3 text-sm dark:border-grey-900 dark:bg-grey-925'>
                                 <Icon name='info' size={16} />
                                 <div className='-mt-0.5'>
-                                    Network is disabled because your domain isn’t configured correctly. <a className='text-green' href="https://ghost.org/docs" rel="noopener noreferrer" target="_blank">Learn more &rarr;</a>
+                                You need to configure a supported custom domain to use this feature. <a className='text-green' href="https://ghost.org/docs" rel="noopener noreferrer" target="_blank">Help &rarr;</a>
                                 </div>
                             </div>
                     }
                 ]}
             />
-            <div className='-mx-7 -mb-7'>
-                <img src={SettingImg} />
+            <div className='-mx-5 -mb-5 overflow-hidden rounded-b-xl md:-mx-7 md:-mb-7'>
+                <img className='dark:opacity-90' src={SettingImg} />
             </div>
         </>
     </TopLevelGroup>);
