e09d6d72178619caa0a42040447805467d867320
Peter Zimon <zimo@ghost.org>
2025-07-15 13:22:08 +0200

    Improved UI of Network settings (#24368)
    ref https://linear.app/ghost/issue/PROD-2286/settings-ui
    
    - Added an image to the Network setting section to make it stand out and
    be more engaging for users
    - Refined UI copy
    
    ---------
    
    Co-authored-by: Sag <guptazy@gmail.com>

--- apps/admin-x-settings/src/components/settings/growth/Network.tsx
+++ apps/admin-x-settings/src/components/settings/growth/Network.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SettingImg from '../../../assets/images/network.png';
 import TopLevelGroup from '../../TopLevelGroup';
 import validator from 'validator';
 import {Icon, SettingGroupContent, Toggle, withErrorBoundary} from '@tryghost/admin-x-design-system';
@@ -56,28 +57,33 @@ const Network: React.FC<{ keywords: string[] }> = ({keywords}) => {
                 onChange={toggleSocialWebSetting}
             />
         }
-        description='Make your content visible to millions across Flipboard, Mastodon, Threads, Bluesky, and WordPress.'
+        description='Distribute posts to the social web, so people can discover and follow your content across BlueSky, Threads, Mastodon, Flipboard, WordPress, and more.'
         keywords={keywords}
         navid='network'
         testId='network'
         title='Network'
     >
-        <SettingGroupContent
-            columns={1}
-            values={[
-                {
-                    key: 'private',
-                    value:
+        <>
+            <SettingGroupContent
+                columns={1}
+                values={[
+                    {
+                        key: 'private',
+                        value:
                         isDisabled &&
-                            <div className='flex w-full gap-1.5 rounded-md border border-grey-200 bg-grey-75 p-3 text-sm'>
+                            <div className='flex w-full gap-1.5 rounded-md border border-grey-200 bg-grey-75 p-3 text-sm dark:border-grey-900 dark:bg-grey-925'>
                                 <Icon name='info' size={16} />
                                 <div className='-mt-0.5'>
-                                    Network is disabled because your domain isn’t configured correctly. <a className='text-green' href="https://ghost.org/docs" rel="noopener noreferrer" target="_blank">Learn more &rarr;</a>
+                                You need to configure a supported custom domain to use this feature. <a className='text-green' href="https://ghost.org/docs" rel="noopener noreferrer" target="_blank">Help &rarr;</a>
                                 </div>
                             </div>
-                }
-            ]}
-        />
+                    }
+                ]}
+            />
+            <div className='-mx-5 -mb-5 overflow-hidden rounded-b-xl md:-mx-7 md:-mb-7'>
+                <img className='dark:opacity-90' src={SettingImg} />
+            </div>
+        </>
     </TopLevelGroup>);
 };
 
