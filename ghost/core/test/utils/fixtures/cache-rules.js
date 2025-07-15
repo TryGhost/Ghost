2c0020f6735152901a4671b0680f3d5ff7bef66e
Hannah Wolfe <github.erisds@gmail.com>
2025-07-15 12:34:53 +0100

    Added small cache TTLs to frontend and content API
    ref https://linear.app/ghost/issue/PROD-1591/breaking-change-update-our-caching-defaults-dont-default-to-assuming
    
    - The zero cache TTLs on public content were added in a time
      when headless was all the rage, and so it made sense for this to be the default
    - However, for most people these TTLs are too low.
    - Given that headless is a more technical setup, it seems reasonable to expect
      that people choosing headless can configure their cache defaults themselves
    - And that we should optimise for the standard usecase where having a small
      cache TTL makes sense to mitigate load spikes

--- ghost/core/test/utils/fixtures/cache-rules.js
+++ ghost/core/test/utils/fixtures/cache-rules.js
@@ -1,5 +1,5 @@
 module.exports = {
-    public: 'public, max-age=0',
+    public: 'public, max-age=600',
     hour: 'public, max-age=' + 3600,
     day: 'public, max-age=' + 86400,
     year: 'public, max-age=' + 31536000,
