468c259b300901ffb54bd20dc142c6284a6ad261
Chris Raible <chris@ghost.org>
2025-07-16 16:30:11 -0700

    Fixed linter errror

--- apps/stats/src/components/errors/StatsErrorPage.tsx
+++ apps/stats/src/components/errors/StatsErrorPage.tsx
@@ -9,7 +9,7 @@ const StatsErrorPage: React.FC<StatsErrorPageProps> = () => {
         <div className="admin-x-container-error">
             <div className="admin-x-error">
                 <h1>Loading interrupted</h1>
-                <p>They say life is a series of trials and tribulations. This moment right here? It's a tribulation. Our app was supposed to load, and yet here we are. Loadless. Click back to the dashboard to try again.</p>
+                <p>They say life is a series of trials and tribulations. This moment right here? It&apos;s a tribulation. Our app was supposed to load, and yet here we are. Loadless. Click back to the dashboard to try again.</p>
                 <a className="cursor-pointer" onClick={() => window.location.reload()}>&larr; Back to the dashboard</a>
             </div>
         </div>
