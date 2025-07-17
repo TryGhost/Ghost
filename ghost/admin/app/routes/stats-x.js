ad412d18a85a3fafc66a95784d2bd258092691d6
Steve Larson <9larsons@gmail.com>
2025-07-17 09:47:34 -0500

    Updated routes to use home as central logic, remove dashboard

--- ghost/admin/app/routes/stats-x.js
+++ ghost/admin/app/routes/stats-x.js
@@ -1,20 +1,13 @@
 import AuthenticatedRoute from 'ghost-admin/routes/authenticated';
-import {inject} from 'ghost-admin/decorators/inject';
 import {inject as service} from '@ember/service';
+
 export default class StatsXRoute extends AuthenticatedRoute {
-    @inject config;
-    @service feature;
+    @service session;
 
     beforeModel() {
         super.beforeModel(...arguments);
-        // This is based on the logic for the dashboard
-        if (this.session.user.isContributor) {
-            return this.transitionTo('posts');
-        } else if (!this.session.user.isAdmin) {
-            return this.transitionTo('site');
-        }
 
-        if (!this.feature.trafficAnalytics && !this.feature.ui60) {
+        if (!this.session.user.isAdmin) {
             return this.transitionTo('home');
         }
     }
