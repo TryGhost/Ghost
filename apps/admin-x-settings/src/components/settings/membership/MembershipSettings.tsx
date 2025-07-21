61bb45dbebc6b774dc5d5f4d64c8cb0c03acc323
niranjan-uma-shankar <niranjan.u@gmail.com>
2025-07-21 18:21:02 +0530

    Renames incorrect keyword
    ref https://github.com/TryGhost/Ghost/issues/24458
    In 6.0, "tips" is moved under Memberships, so the keyword should reflect that change.

--- apps/admin-x-settings/src/components/settings/membership/MembershipSettings.tsx
+++ apps/admin-x-settings/src/components/settings/membership/MembershipSettings.tsx
@@ -12,7 +12,7 @@ export const searchKeywords = {
     access: ['membership', 'default', 'access', 'subscription', 'post', 'membership', 'comments', 'commenting', 'signup', 'sign up', 'spam', 'filters', 'prevention', 'prevent', 'block', 'domains', 'email'],
     tiers: ['membership', 'tiers', 'payment', 'paid', 'stripe'],
     portal: ['membership', 'portal', 'signup', 'sign up', 'signin', 'sign in', 'login', 'account', 'membership', 'support', 'email', 'address', 'support email address', 'support address'],
-    tips: ['growth', 'tips', 'donations', 'one time', 'payment']
+    tips: ['membership', 'tips', 'donations', 'one time', 'payment']
 };
 
 const MembershipSettings: React.FC = () => {
