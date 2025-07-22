498b50bdf45f845fc3f761417921b877a57c7600
Paul Davis <PaulAdamDavis@users.noreply.github.com>
2025-07-22 11:00:56 +0100

    Change authentication for self-service migrations to send API key instead of JWT (#24462)
    ref
    https://linear.app/ghost/issue/CON-372/change-ghost-self-service-migrations-app-auth
    
    - The self-service migrations app is currently authenticated with a JWT,
    but the expire time for these is capped at 5 minutes in the [Admin API
    key auth
    service](https://github.com/TryGhost/Ghost/blob/main/ghost/core/core/server/services/auth/api-key/admin.js)
    - I wasn't aware of this in #24119, making that change ineffective
    - The API key used to make the JWT is already available to privileged
    users of Ghost Admin
    - This commit changes the migrate service to send the API key rather
    than a JWT (and handling that in the self-service app) yields the same
    result, allowing migrations more time to process, improving the end
    result
    - It also removes the now-unused `jose` dependency

--- ghost/admin/app/services/migrate.js
+++ ghost/admin/app/services/migrate.js
@@ -1,6 +1,5 @@
 import Service, {inject as service} from '@ember/service';
 import config from 'ghost-admin/config/environment';
-import {SignJWT} from 'jose';
 import {tracked} from '@glimmer/tracking';
 
 export default class MigrateService extends Service {
@@ -28,49 +27,26 @@ export default class MigrateService extends Service {
         return url;
     }
 
-    async apiToken() {
-        // TODO: Getting the token can be improved
+    async apiKey() {
         const ghostIntegrationsUrl = this.ghostPaths.url.api('integrations') + '?include=api_keys';
         return this.ajax.request(ghostIntegrationsUrl).then(async (response) => {
             const ssmIntegration = response.integrations.find(r => r.slug === 'self-serve-migration');
 
             const key = ssmIntegration.api_keys[0].secret;
-            const [id, secret] = key.split(':');
 
-            function hexToBytes(hex) {
-                let bytes = [];
-                for (let c = 0; c < hex.length; c += 2) {
-                    bytes.push(parseInt(hex.substr(c, 2), 16));
-                }
-                return new Uint8Array(bytes);
-            }
-
-            const encodedSecret = hexToBytes(secret);
-
-            const token = await new SignJWT({})
-                .setProtectedHeader({
-                    alg: 'HS256',
-                    typ: 'JWT',
-                    kid: id
-                })
-                .setIssuedAt()
-                .setExpirationTime('24h')
-                .setAudience('/admin/')
-                .sign(encodedSecret);
-
-            return token;
+            return key;
         }).catch((error) => {
             throw error;
         });
     }
 
     async postMessagePayload() {
-        const theToken = await this.apiToken();
+        const theKey = await this.apiKey();
         const theOwner = await this.billing.getOwnerUser();
 
         let payload = {
             apiUrl: this.apiUrl,
-            apiToken: theToken,
+            apiKey: theKey,
             stripe: this.isStripeConnected,
             ghostVersion: this.ghostVersion,
             ownerEmail: theOwner.email
@@ -101,13 +77,13 @@ export default class MigrateService extends Service {
         return url;
     }
 
-    // Sends a route update to a child route in the BMA, because we can't control
+    // Sends a route update to a child route in the migrate app, because we can't control
     // navigating to it otherwise
     sendRouteUpdate(route) {
         this.getMigrateIframe().contentWindow.postMessage({
             query: 'routeUpdate',
             response: route
-        }, '*');
+        }, this.migrateUrl);
     }
 
     // Controls migrate window modal visibility and sync of the URL visible in browser
