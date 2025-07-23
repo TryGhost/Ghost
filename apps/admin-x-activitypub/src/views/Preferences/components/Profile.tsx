d7ce56fa66cb4892092ba39222f97f0f367c08a3
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-07-23 19:23:32 +0800

    Removed share feature flag

--- apps/admin-x-activitypub/src/views/Preferences/components/Profile.tsx
+++ apps/admin-x-activitypub/src/views/Preferences/components/Profile.tsx
@@ -272,27 +272,6 @@ const Profile: React.FC<ProfileProps> = ({account, isLoading}) => {
         }
     };
 
-    if (!isEnabled('share')) {
-        return (
-            <div className='flex flex-col items-center'>
-                <APAvatar
-                    author={
-                        {
-                            icon: {
-                                url: account?.avatarUrl || ''
-                            },
-                            name: account?.name || '',
-                            handle: account?.handle
-                        }
-                    }
-                    size='lg'
-                />
-                <H2 className='mb-0.5 mt-4'>{!isLoading ? account?.name : <Skeleton className='w-32' />}</H2>
-                <span className='text-[1.5rem] text-gray-700'>{!isLoading ? account?.handle : <Skeleton className='w-full max-w-56' />}</span>
-            </div>
-        );
-    }
-
     return (
         <TooltipProvider delayDuration={0}>
             <div className='flex flex-col gap-5'>
