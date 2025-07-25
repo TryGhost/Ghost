a233b79fb32362c9a6be25fbe77a6f9bffaeb0af
Filip Chabik <filip@ghost.org>
2025-07-25 16:55:06 +0200

    Added handling for 5.x pipeline builds
    ref https://linear.app/ghost/issue/PRO-1490/prepare-fiveghostis-for-5x-ghost-version
    
    - We want to be able to test out 5.x in staging to be sure this is
    working properly
    - This change makes sure all builds of the 5.x branch get deployed
    correctly

--- .github/scripts/bump-version.js
+++ .github/scripts/bump-version.js
@@ -19,7 +19,7 @@ const semver = require('semver');
 
     let newVersion;
 
-    if (firstArg === 'canary' || firstArg === 'six') {
+    if (firstArg === 'canary' || firstArg === 'five') {
         const bumpedVersion = semver.inc(current_version, 'minor');
         newVersion = `${bumpedVersion}-pre-g${buildString}`;
     } else {
