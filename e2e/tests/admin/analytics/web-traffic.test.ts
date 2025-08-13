add89796d4f70ae8853b7b24a8f640be5441f785
Igor Balos <ibalosh@gmail.com>
2025-08-13 17:57:56 +0200

    fix formatting

--- e2e/tests/admin/analytics/web-traffic.test.ts
+++ e2e/tests/admin/analytics/web-traffic.test.ts
@@ -3,40 +3,40 @@ import {AnalyticsWebTrafficPage} from '../../../helpers/pages/admin';
 
 test.describe('Ghost Admin - Analytics Web Traffic', () => {
     test('empty web traffic general graph', async ({page}) => {
-      const analyticsWebTrafficPage = new AnalyticsWebTrafficPage(page);
-      await analyticsWebTrafficPage.goto();
+        const analyticsWebTrafficPage = new AnalyticsWebTrafficPage(page);
+        await analyticsWebTrafficPage.goto();
 
-      await expect(analyticsWebTrafficPage.totalUniqueVisitorsTab).toContainText('0');
-      await expect(analyticsWebTrafficPage.totalViewsTab).toContainText('0');
-  });
+        await expect(analyticsWebTrafficPage.totalUniqueVisitorsTab).toContainText('0');
+        await expect(analyticsWebTrafficPage.totalViewsTab).toContainText('0');
+    });
 
-  test('empty top content card', async ({page}) => {
-    const analyticsWebTrafficPage = new AnalyticsWebTrafficPage(page);
-    await analyticsWebTrafficPage.goto();
+    test('empty top content card', async ({page}) => {
+        const analyticsWebTrafficPage = new AnalyticsWebTrafficPage(page);
+        await analyticsWebTrafficPage.goto();
 
-    await expect(analyticsWebTrafficPage.topContentCard).toContainText('No visitors');
-  });
+        await expect(analyticsWebTrafficPage.topContentCard).toContainText('No visitors');
+    });
 
-  test('empty top content card - posts', async ({page}) => {
-    const analyticsWebTrafficPage = new AnalyticsWebTrafficPage(page);
-    await analyticsWebTrafficPage.goto();
-    await analyticsWebTrafficPage.postsButton.click();
+    test('empty top content card - posts', async ({page}) => {
+        const analyticsWebTrafficPage = new AnalyticsWebTrafficPage(page);
+        await analyticsWebTrafficPage.goto();
+        await analyticsWebTrafficPage.postsButton.click();
 
-    await expect(analyticsWebTrafficPage.topContentCard).toContainText('No visitors');
-  });
+        await expect(analyticsWebTrafficPage.topContentCard).toContainText('No visitors');
+    });
 
-  test('empty top content card - pages', async ({page}) => {
-    const analyticsWebTrafficPage = new AnalyticsWebTrafficPage(page);
-    await analyticsWebTrafficPage.goto();
-    await analyticsWebTrafficPage.pagesButton.click();
+    test('empty top content card - pages', async ({page}) => {
+        const analyticsWebTrafficPage = new AnalyticsWebTrafficPage(page);
+        await analyticsWebTrafficPage.goto();
+        await analyticsWebTrafficPage.pagesButton.click();
 
-    await expect(analyticsWebTrafficPage.topContentCard).toContainText('No visitors');
-  });
+        await expect(analyticsWebTrafficPage.topContentCard).toContainText('No visitors');
+    });
 
-  test('empty top sources card', async ({page}) => {
-    const analyticsWebTrafficPage = new AnalyticsWebTrafficPage(page);
-    await analyticsWebTrafficPage.goto();
+    test('empty top sources card', async ({page}) => {
+        const analyticsWebTrafficPage = new AnalyticsWebTrafficPage(page);
+        await analyticsWebTrafficPage.goto();
 
-    await expect(analyticsWebTrafficPage.topSourcesCard).toContainText('No visitors');
-  });
+        await expect(analyticsWebTrafficPage.topSourcesCard).toContainText('No visitors');
+    });
 });
