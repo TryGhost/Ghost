ab3e538ebf5fc3701a94e1d7a3132dad1ecd66e4
Igor Balos <ibalosh@gmail.com>
2025-08-13 18:09:28 +0200

    added empty locations verification

--- e2e/helpers/pages/admin/analytics/AnalyticsLocationsPage.ts
+++ e2e/helpers/pages/admin/analytics/AnalyticsLocationsPage.ts
@@ -1,10 +1,14 @@
-import {Page} from '@playwright/test';
+import {Page, Locator} from '@playwright/test';
 import AdminPage from '../AdminPage';
 
 export class AnalyticsLocationsPage extends AdminPage {
+    public readonly visitorsCard: Locator;
+
     constructor(page: Page) {
         super(page);
 
         this.pageUrl = '/ghost/#/analytics/locations';
+
+        this.visitorsCard = page.getByTestId('visitors-card');
     }
 }
