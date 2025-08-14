9052709a07ca90afda9e04105456f8dba41b230c
Troy Ciesco <tmciesco@gmail.com>
2025-08-14 13:48:57 -0400

    cleanup

--- ghost/core/core/server/services/koenig/node-renderers/header-v2-renderer.js
+++ ghost/core/core/server/services/koenig/node-renderers/header-v2-renderer.js
@@ -106,7 +106,6 @@ function emailTemplate(nodeData, options) {
                 </table>
             ` : ''}
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="color:${nodeData.textColor}; ${alignment} ${backgroundImageStyle} ${backgroundAccent}">
-
                 <tr>
                 ${nodeData.layout !== 'split' && nodeData.backgroundImageSrc ? `
                 <!--[if mso]>
