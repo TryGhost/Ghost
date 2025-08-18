c4d79d897761bd798ba3e38d996f3d5052a74fb5
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2025-08-18 15:06:11 +0200

    Remove posts app router basepath
    Refs https://linear.app/ghost/issue/PROD-2504/port-tag-list-to-react
    
    The `/posts` basepath prevents us from adding the root level `/tags` routes

--- apps/posts/src/views/PostAnalytics/Newsletter/Newsletter.tsx
+++ apps/posts/src/views/PostAnalytics/Newsletter/Newsletter.tsx
@@ -83,7 +83,7 @@ const Newsletter: React.FC<postAnalyticsProps> = () => {
     useEffect(() => {
         // Redirect to overview if the post wasn't sent as a newsletter
         if (!isPostLoading && !showNewsletterSection) {
-            navigate(`/analytics/${postId}`);
+            navigate(`/posts/analytics/${postId}`);
         }
     }, [navigate, postId, isPostLoading, showNewsletterSection]);
 
