34cf298a75d61010169207b70ad502afe713e8a9
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2025-08-19 09:36:49 +0200

    Scaffold tag list UI
    Refs https://linear.app/ghost/issue/PROD-2504/port-tag-list-to-react
    
    Replicate old Ember UI using Shade components. Layout and header has largely 
    been copied over from the Analytics views.

--- apps/posts/src/views/Tags/Tags.tsx
+++ apps/posts/src/views/Tags/Tags.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import TagsContent from './components/TagsContent';
+import TagsHeader from './components/TagsHeader';
+import TagsLayout from './components/TagsLayout';
+import TagsList from './components/TagsList';
+import {useLocation} from '@tryghost/admin-x-framework';
 
 const Tags: React.FC = () => {
-    return <h1>Tags in React</h1>;
+    const {search} = useLocation();
+    const qs = new URLSearchParams(search);
+    const type = qs.get('type') ?? 'public';
+
+    return (
+        <TagsLayout>
+            <TagsHeader currentTab={type} />
+            <TagsContent>
+
+                <TagsList items={[
+                    {
+                        id: 1,
+                        name: 'Tag 1',
+                        slug: 'tag-1',
+                        count: 10
+                    },
+                    
+                    {
+                        id: 2,
+                        name: 'Tag 2',
+                        slug: 'tag-2',
+                        count: 20
+                    },
+                    {
+                        id: 3,
+                        name: 'Tag 3',
+                        slug: 'tag-3',
+                        count: 30
+                    }
+                ]} />
+
+            </TagsContent>
+        </TagsLayout>
+    );
 };
 
 export default Tags;
