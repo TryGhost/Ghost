4b0ba05066c3f7748a6846898a3f8d3572f860e7
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2025-08-19 09:03:55 +0200

    Stub out header

--- apps/posts/src/views/Tags/Tags.tsx
+++ apps/posts/src/views/Tags/Tags.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React from "react";
+import TagsHeader from "./components/TagsHeader";
+import TagsContent from "./components/TagsContent";
+import TagsLayout from "./components/TagsLayout";
+import { useLocation } from "@tryghost/admin-x-framework"
 
 const Tags: React.FC = () => {
-    return <h1>Tags in React</h1>;
+    const { search } = useLocation();
+    const qs = new URLSearchParams(search);
+    const type = qs.get("type") ?? "public";
+
+    return (
+        <TagsLayout>
+            <TagsHeader currentTab={type} />
+            <TagsContent />
+        </TagsLayout>
+    );
 };
 
 export default Tags;
