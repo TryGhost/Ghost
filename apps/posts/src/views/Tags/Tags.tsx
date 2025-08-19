92f7093d4d0b8d9dfc2c7f299b4019a603503774
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2025-08-19 09:36:49 +0200

    Stub out table

--- apps/posts/src/views/Tags/Tags.tsx
+++ apps/posts/src/views/Tags/Tags.tsx
@@ -3,6 +3,7 @@ import TagsHeader from "./components/TagsHeader";
 import TagsContent from "./components/TagsContent";
 import TagsLayout from "./components/TagsLayout";
 import { useLocation } from "@tryghost/admin-x-framework"
+import TagsList from "./components/TagsList";
 
 const Tags: React.FC = () => {
     const { search } = useLocation();
@@ -12,7 +13,31 @@ const Tags: React.FC = () => {
     return (
         <TagsLayout>
             <TagsHeader currentTab={type} />
-            <TagsContent />
+            <TagsContent>
+
+                <TagsList items={[
+                    {
+                        id: 1,
+                        name: "Tag 1",
+                        slug: "tag-1",
+                        count: 10
+                    },
+                    
+                    {
+                        id: 2,
+                        name: "Tag 2",
+                        slug: "tag-2",
+                        count: 20
+                    },
+                    {
+                        id: 3,
+                        name: "Tag 3",
+                        slug: "tag-3",
+                        count: 30
+                    },
+                ]} />
+
+            </TagsContent>
         </TagsLayout>
     );
 };
