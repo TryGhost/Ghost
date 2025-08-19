510514a3729e3ec543693b4cc0bb664652d35bd4
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2025-08-19 12:06:48 +0200

    Posts links

--- apps/posts/src/views/Tags/components/TagsList.tsx
+++ apps/posts/src/views/Tags/components/TagsList.tsx
@@ -15,30 +15,38 @@ const TagsList = ({ items }: { items: Tag[] }) => {
         <Table>
             <TableHeader>
                 <TableRow>
-                    <TableHead>Tag</TableHead>
-                    <TableHead>Slug</TableHead>
-                    <TableHead>No. of posts</TableHead>
+                    <TableHead className="px-4">Tag</TableHead>
+                    <TableHead className="px-4">Slug</TableHead>
+                    <TableHead className="px-4">No. of posts</TableHead>
                     <TableHead></TableHead>
                 </TableRow>
             </TableHeader>
-            <TableBody className="border-b-0">
+            <TableBody>
                 {items.map((item) => (
                     <TableRow
                         key={item.id}
-                        className="hover:bg-muted/50 relative"
+                        className="relative"
                     >
-                        <TableCell className="w-1/2 static">
+                        <TableCell className="w-3/5 static p-4">
                             <a
-                                className="before:absolute before:z-50 before:inset-0 before:bg-transparent font-medium block"
+                                className="before:absolute before:z-10 before:inset-0 font-medium text-lg block pb-1"
                                 href={`#/tags/${item.slug}`}
                             >
                                 {item.name}
                             </a>
-                            <span className="text-muted-foreground text-sm block truncate">{item.description}</span>
+                            <span className="text-muted-foreground block truncate">{item.description}</span>
                         </TableCell>
-                        <TableCell className="w-1/4">{item.slug}</TableCell>
-                        <TableCell className="">{item.count?.posts ?? 0}</TableCell>
-                        <TableCell className="w-4 justify-end">
+                        <TableCell className="p-4">{item.slug}</TableCell>
+                        <TableCell className="p-4">
+                            {item.count?.posts ? (
+                                <a href={`#/posts?tag=${item.slug}`} className="relative inline-block z-10 p-4 -m-4 hover:underline">
+                                    {item.count?.posts} posts
+                                </a>
+                            ) : (
+                                <span className="text-muted-foreground">0 posts</span>
+                            )}
+                        </TableCell>
+                        <TableCell className="p-4 w-4 justify-end">
                             <Button
                                 variant="outline"
                                 size="icon"
