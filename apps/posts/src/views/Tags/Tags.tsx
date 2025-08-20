050a0c2e3c0c8aea55a9cdec3cccb3e034bc1d79
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2025-08-20 14:45:57 +0200

    Error and empty state

--- apps/posts/src/views/Tags/Tags.tsx
+++ apps/posts/src/views/Tags/Tags.tsx
@@ -5,7 +5,8 @@ import TagsLayout from "./components/TagsLayout";
 import { useLocation } from "@tryghost/admin-x-framework";
 import TagsList from "./components/TagsList";
 import { useBrowseTags } from "@tryghost/admin-x-framework/api/tags";
-import { LoadingIndicator } from "@tryghost/shade";
+import { Button, LoadingIndicator } from "@tryghost/shade";
+import TagsPlaceholder from "./assets/icons/tags-placeholder.svg";
 
 const Tags: React.FC = () => {
     const { search } = useLocation();
@@ -14,8 +15,7 @@ const Tags: React.FC = () => {
 
     const {
         data,
-        //error,
-        //isFetching,
+        isError,
         isLoading,
         isFetchingNextPage,
         fetchNextPage,
@@ -33,6 +33,33 @@ const Tags: React.FC = () => {
                 {isLoading ? (
                     <div className="flex justify-center items-center h-full">
                         <LoadingIndicator size="lg" />
+                    </div>
+                ) : isError ? (
+                    <div className="flex flex-col items-center justify-center h-full mb-16">
+                        <h2 className="text-xl font-medium mb-2">
+                            Error loading tags
+                        </h2>
+                        <p className="text-muted-foreground mb-4">
+                            Please reload the page to try again
+                        </p>
+                        <Button onClick={() => window.location.reload()}>
+                            Reload page
+                        </Button>
+                    </div>
+                ) : !data?.tags.length ? (
+                    <div className="flex flex-col items-center justify-center h-full gap-8 mb-16">
+                        <img
+                            src={TagsPlaceholder}
+                            alt="Tags placeholder"
+                            width={60}
+                            height={60}
+                        />
+                        <h2 className="text-xl font-medium">
+                            Start organizing your content
+                        </h2>
+                        <Button asChild>
+                            <a href="#/tags/new">Create a new tag</a>
+                        </Button>
                     </div>
                 ) : (
                     <TagsList
