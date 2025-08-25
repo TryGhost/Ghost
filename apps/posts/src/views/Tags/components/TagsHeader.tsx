b08dc0db7ac1fdfb78136c7092eebb2df0192fd5
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2025-08-25 15:21:16 +0200

    Lint format

--- apps/posts/src/views/Tags/components/TagsHeader.tsx
+++ apps/posts/src/views/Tags/components/TagsHeader.tsx
@@ -17,12 +17,12 @@ interface TagsHeaderProps {
 const TagsHeader: React.FC<TagsHeaderProps> = ({currentTab}) => {
     return (
         <>
-            <Navbar className="sticky top-0 z-50 p-4 -mb-4 lg:p-8 lg:-mb-8 flex-col items-start gap-y-2 lg:gap-y-4 border-none bg-gradient-to-b from-background via-background/70 to-background/70 backdrop-blur-md md:flex-row md:items-center dark:bg-black">
-                <H1 className="min-h-[35px] max-w-[920px] indent-0 leading-[1.2em] text-2xl lg:text-3xl">
+            <Navbar className="sticky top-0 z-50 -mb-4 flex-col items-start gap-y-2 border-none bg-gradient-to-b from-background via-background/70 to-background/70 p-4 backdrop-blur-md md:flex-row md:items-center lg:-mb-8 lg:gap-y-4 lg:p-8 dark:bg-black">
+                <H1 className="min-h-[35px] max-w-[920px] indent-0 text-2xl leading-[1.2em] lg:text-3xl">
                     Tags
                 </H1>
 
-                <NavbarActions className="justify-between w-full md:w-auto">
+                <NavbarActions className="w-full justify-between md:w-auto">
                     <PageMenu
                         className="min-h-[34px] pr-2 lg:pr-4"
                         defaultValue={currentTab}
