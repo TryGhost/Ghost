a027fd62a2b34fca2528cc37fb0ddb4ad9b708d2
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2025-08-25 09:54:45 +0200

    Bette header responsivity

--- apps/posts/src/views/Tags/components/TagsHeader.tsx
+++ apps/posts/src/views/Tags/components/TagsHeader.tsx
@@ -17,14 +17,14 @@ interface TagsHeaderProps {
 const TagsHeader: React.FC<TagsHeaderProps> = ({currentTab}) => {
     return (
         <>
-            <Navbar className="sticky top-0 z-50 -mb-8 flex-col items-start gap-y-5 border-none bg-white/70 py-8 backdrop-blur-md md:flex-row md:items-center dark:bg-black">
-                <H1 className="min-h-[35px] max-w-[920px] indent-0 leading-[1.2em]">
+            <Navbar className="sticky top-0 z-50 p-4 -mb-4 lg:p-8 lg:-mb-8 flex-col items-start gap-y-2 lg:gap-y-4 border-none bg-gradient-to-b from-background via-background/70 to-background/70 backdrop-blur-md md:flex-row md:items-center dark:bg-black">
+                <H1 className="min-h-[35px] max-w-[920px] indent-0 leading-[1.2em] text-2xl lg:text-3xl">
                     Tags
                 </H1>
 
-                <NavbarActions>
+                <NavbarActions className="justify-between w-full md:w-auto">
                     <PageMenu
-                        className="min-h-[34px] pr-4"
+                        className="min-h-[34px] pr-2 lg:pr-4"
                         defaultValue={currentTab}
                     >
                         <PageMenuItem value="public" asChild>
