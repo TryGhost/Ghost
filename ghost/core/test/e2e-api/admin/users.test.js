3ef230ef37f6840245ac7fdf1e6930ca4504918d
Troy Ciesco <tmciesco@gmail.com>
2025-08-25 16:01:50 -0400

    make test deterministic, update snapshot

--- ghost/core/test/e2e-api/admin/users.test.js
+++ ghost/core/test/e2e-api/admin/users.test.js
@@ -63,7 +63,7 @@ describe('User API', function () {
             });
     });
     it('Can include user roles', async function () {
-        await agent.get('users/?include=roles')
+        await agent.get('users/?include=roles&order=id%20ASC')
             .expectStatus(200)
             .matchHeaderSnapshot({
                 'content-version': anyContentVersion,
