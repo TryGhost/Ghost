637855621a92aa6f106c7bc91b19fb7b22db36ff
Chris Raible <chris@ghost.org>
2025-09-02 22:29:35 -0400

    Updated all tests to use the ghost-instance fixture

--- e2e/tests/admin/analytics/location.test.ts
+++ e2e/tests/admin/analytics/location.test.ts
@@ -1,4 +1,4 @@
-import {test, expect} from '@playwright/test';
+import {test, expect} from '../../../helpers/fixtures/ghost-instance';
 import {AnalyticsLocationsPage} from '../../../helpers/pages/admin';
 
 test.describe('Ghost Admin - Locations', () => {
