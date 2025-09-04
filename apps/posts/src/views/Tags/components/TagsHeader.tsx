354c551f864ab4de1564ae56df460b2fa6fbc83b
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2025-09-04 13:29:38 +0200

    Use new Header component for tags list
    Refs https://linear.app/ghost/issue/PROD-2506/extract-reusable-header-component-to-shade

--- apps/posts/src/views/Tags/components/TagsHeader.tsx
+++ apps/posts/src/views/Tags/components/TagsHeader.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-    Button,
-    H1,
-    Navbar,
-    NavbarActions,
-    PageMenu,
-    PageMenuItem
-} from '@tryghost/shade';
+import {Button, Header, PageMenu, PageMenuItem} from '@tryghost/shade';
 import {Link} from '@tryghost/admin-x-framework';
 
 interface TagsHeaderProps {
@@ -16,32 +9,28 @@ interface TagsHeaderProps {
 
 const TagsHeader: React.FC<TagsHeaderProps> = ({currentTab}) => {
     return (
-        <>
-            <Navbar className="sticky top-0 z-50 -mb-4 flex-col items-start gap-y-2 border-none bg-gradient-to-b from-background via-background/70 to-background/70 p-4 backdrop-blur-md md:flex-row md:items-center lg:-mb-8 lg:gap-y-4 lg:p-8 dark:bg-black">
-                <H1 className="min-h-[35px] max-w-[920px] indent-0 text-2xl leading-[1.2em] lg:text-3xl">
-                    Tags
-                </H1>
+        <Header variant="inline-nav">
+            <Header.Title>Tags</Header.Title>
 
-                <NavbarActions className="w-full justify-between md:w-auto">
-                    <PageMenu
-                        className="min-h-[34px] pr-2 lg:pr-4"
-                        defaultValue={currentTab}
-                    >
-                        <PageMenuItem value="public" asChild>
-                            <Link to="/tags">Public tags</Link>
-                        </PageMenuItem>
-                        <PageMenuItem value="internal" asChild>
-                            <Link to="/tags?type=internal">Internal tags</Link>
-                        </PageMenuItem>
-                    </PageMenu>
-                    <Button asChild>
-                        <a className="font-bold" href="#/tags/new">
-                            New tag
-                        </a>
-                    </Button>
-                </NavbarActions>
-            </Navbar>
-        </>
+            <Header.Nav>
+                <PageMenu defaultValue={currentTab}>
+                    <PageMenuItem value="public" asChild>
+                        <Link to="/tags">Public tags</Link>
+                    </PageMenuItem>
+                    <PageMenuItem value="internal" asChild>
+                        <Link to="/tags?type=internal">Internal tags</Link>
+                    </PageMenuItem>
+                </PageMenu>
+            </Header.Nav>
+
+            <Header.Actions>
+                <Button asChild>
+                    <a className="font-bold" href="#/tags/new">
+                        New tag
+                    </a>
+                </Button>
+            </Header.Actions>
+        </Header>
     );
 };
 
