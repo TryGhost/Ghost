4291efb32250366e2f6bf1660d05da07551ff8ce
Chris Raible <chris@ghost.org>
2025-09-08 22:18:40 -0700

    Fixed build error

--- e2e/types.d.ts
+++ e2e/types.d.ts
@@ -1,11 +1,11 @@
 declare module '@tryghost/logging' {
-    export function error(...args: any[]): void;
-    export function warn(...args: any[]): void;
-    export function info(...args: any[]): void;
-    export function debug(...args: any[]): void;
+    export function error(...args: unknown[]): void;
+    export function warn(...args: unknown[]): void;
+    export function info(...args: unknown[]): void;
+    export function debug(...args: unknown[]): void;
 }
 
 declare module '@tryghost/debug' {
-    function debug(namespace: string): (...args: any[]) => void;
+    function debug(namespace: string): (...args: unknown[]) => void;
     export = debug;
 }
