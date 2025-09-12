64c620813ad8a0603aaa129c5ae7c0bcf3e30634
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2025-09-12 09:10:21 +0200

    Render ember app alongside react

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+window.__ghost_admin_bridge__ = {};
+console.log('window.__ghost_admin_bridge__', window.__ghost_admin_bridge__);
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
