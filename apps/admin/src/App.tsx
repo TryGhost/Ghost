a6a3103935dd6a1d172a059f1468bbc50e451655
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2025-09-12 15:26:02 +0200

    Add placeholder sidebar
    Ref https://linear.app/ghost/issue/BER-2913/port-sidebar
    
    Adding this placeholder sidebar will let us navigate the admin as we remove the
    Ember sidebar when that app runs within the new admin shell.

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,17 +1,25 @@
-import { Outlet } from "@tryghost/admin-x-framework";
-import { useCurrentUser } from "@tryghost/admin-x-framework/api/currentUser";
-import { EmberProvider, EmberFallback, EmberRoot, EmberAuthSync } from "./ember-bridge";
+import { Outlet } from '@tryghost/admin-x-framework'
+import { useCurrentUser } from '@tryghost/admin-x-framework/api/currentUser'
+import { EmberProvider, EmberFallback, EmberRoot, EmberAuthSync } from './ember-bridge'
+import { AdminLayout } from './layout/AdminLayout.tsx'
 
 function App() {
-    const { data: currentUser } = useCurrentUser();
+    const { data: currentUser } = useCurrentUser()
+    const needsAuthentication = !currentUser
 
     return (
         <EmberProvider>
-            {currentUser ? <Outlet /> : <EmberFallback />}
+            {needsAuthentication ? (
+                <EmberFallback />
+            ) : (
+                <AdminLayout>
+                    <Outlet />
+                </AdminLayout>
+            )}
             <EmberRoot />
             <EmberAuthSync />
         </EmberProvider>
-    );
+    )
 }
 
-export default App;
+export default App
