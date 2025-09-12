82bc2ee005da26ed90e435ed092d5cb9bd385219
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2025-09-12 09:08:27 +0200

    Use proxy to serve Ember assets and admin endpoints
    Refs https://linear.app/ghost/issue/BER-2902/set-up-new-admin-build-that-include-all-relevant-assets-from-existing
    
    The Vite plugin lets us identify the correct entry points for the Ember app so 
    that we can included those assets the generated index.html.

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,6 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { emberAssetsPlugin } from "./vite-ember-assets";
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    plugins: [react(), emberAssetsPlugin()],
+    server: {
+        proxy: {
+            // Proxy requests to the Ghost Admin API. We need to rewrite the
+            // cookies and headers for the existing security middleware not to
+            // reject the requests. 
+            "^/ghost/api/.*": {
+                changeOrigin: true,
+                cookieDomainRewrite: {
+                    "*": "localhost:2368",
+                },
+                headers: {
+                    Referrer: "http://localhost:2368",
+                },
+                target: "http://localhost:2368",
+            },
+
+            // Proxy requests to the Ghost Admin assets
+            "^/ghost/assets/.*": {
+                target: "http://localhost:2368",
+            },
+            "^/ghost/ember-cli-live-reload.js": {
+                target: "http://localhost:2368",
+            },
+        },
+    },
+});
