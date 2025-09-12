a6cf4e3eae96d421d4b2a4d1a3ba26479fee66b7
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2025-09-12 10:06:38 +0200

    Add routes

---
+++ apps/admin/src/routes.tsx
@@ -0,0 +1,28 @@
+import { Outlet, type RouteObject } from "@tryghost/admin-x-framework";
+import { routes as postRoutes } from "@tryghost/posts/src/routes";
+import GlobalDataProvider from "@tryghost/stats/src/providers/GlobalDataProvider";
+import { routes as statsRoutes, APP_ROUTE_PREFIX } from "@tryghost/stats/src/routes";
+//import { routes as activityPubRoutes } from "@tryghost/admin-x-activitypub/src/routes";
+
+export const routes: RouteObject[] = [
+    {
+        path: "/",
+        element: <div>Hello World</div>
+    },
+    ...postRoutes[0].children!,
+    {
+        path: `${APP_ROUTE_PREFIX}`,
+        element: <GlobalDataProvider><Outlet /></GlobalDataProvider>,
+        children: [
+            ...statsRoutes.map(route => ({
+                ...route,
+                path: `${APP_ROUTE_PREFIX}${route.path}`
+            }))
+        ]
+    },
+    //...activityPubRoutes,
+    {
+        path: "*",
+        element: <div>404</div>
+    }
+];
