b829530f8b276fbbee305f36abf275a9c49244f2
Peter Zimon <peter.zimon@gmail.com>
2025-09-16 13:45:46 +0200

    Updated Storybook to v9

--- apps/shade/.storybook/main.tsx
+++ apps/shade/.storybook/main.tsx
@@ -5,18 +5,14 @@ const config: StorybookConfig = {
 	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 	addons: [
 		"@storybook/addon-links",
-		"@storybook/addon-essentials",
-		"@storybook/addon-interactions",
-		{
-			name: '@storybook/addon-styling',
-		},
+		"@storybook/addon-docs",
 	],
 	framework: {
 		name: "@storybook/react-vite",
 		options: {},
 	},
 	docs: {
-		autodocs: "tag",
+		defaultName: 'Docs',
 	},
     async viteFinal(config, options) {
 		config.resolve!.alias = {
