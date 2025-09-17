43d016856f9ca4d8792d555b68c9879827560f97
Peter Zimon <peter.zimon@gmail.com>
2025-09-17 13:29:31 +0200

    Revert using getAbsolutePath

--- apps/shade/.storybook/main.tsx
+++ apps/shade/.storybook/main.tsx
@@ -1,6 +1,6 @@
 import { createRequire } from "node:module";
 import type { StorybookConfig } from "@storybook/react-vite";
-import path from 'path';
+import path, { dirname, join } from 'path';
 
 const require = createRequire(import.meta.url);
 
@@ -8,12 +8,12 @@ const config: StorybookConfig = {
     stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 
     addons: [
-        "@storybook/addon-links",
-        "@storybook/addon-docs"
+        getAbsolutePath("@storybook/addon-links"),
+        getAbsolutePath("@storybook/addon-docs")
     ],
 
     framework: {
-		name: "@storybook/react-vite",
+		name: getAbsolutePath("@storybook/react-vite"),
 		options: {},
 	},
 
@@ -27,3 +27,7 @@ const config: StorybookConfig = {
 	}
 };
 export default config;
+
+function getAbsolutePath(value: string): any {
+    return dirname(require.resolve(join(value, "package.json")));
+}
