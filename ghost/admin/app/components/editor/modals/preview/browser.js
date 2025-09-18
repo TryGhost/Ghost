b9343eb8d3a1d3961e852231db195c8ee8f308e9
Steve Larson <9larsons@gmail.com>
2025-09-18 17:02:51 +0200

    Linter

--- ghost/admin/app/components/editor/modals/preview/browser.js
+++ ghost/admin/app/components/editor/modals/preview/browser.js
@@ -1,8 +1,8 @@
 /* global key */
 import Component from '@glimmer/component';
 import {action} from '@ember/object';
-import {inject as service} from '@ember/service';
 import {run} from '@ember/runloop';
+import {inject as service} from '@ember/service';
 
 export default class ModalPostPreviewBrowserComponent extends Component {
     @service modals;
