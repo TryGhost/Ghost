96868dff146b7b6768a57009e3ac3b9a41573b01
Peter Zimon <zimo@ghost.org>
2025-09-23 15:52:03 +0200

    Upgraded Storybook to v9 in Shade (#24920)
    ref https://linear.app/ghost/issue/PROD-2644/upgrade-storybook-to-v9
    
    A Storybook addon were automatically
    [updated](https://github.com/TryGhost/Ghost/commit/67298abcbca68b13782963037106e591c4eda5e4)
    in Shade by Renovate, which made Storybook break when ran locally. To overcome
    this and future compatible issues in Shade we upgraded Storybook to its
    latest version. Also we want to upgrade to TailwindCSS v4 which is not compatible with the old version of Storybook.
    
    - The upgrade was executed by running the upgrade script `npx
    storybook@latest upgrade`
    ([docs](https://storybook.js.org/docs/releases/upgrading)) which updated
    all storybook components and stories to use the Storybook `react-vite`
    module instead of `react`.
    - Some Storybook addons (essentials, interactions, links, styling,
    blocks) were incompatible with v9 so they were removed from
    `package.json`. We'll add new addons in separate PRs if needed.
    - `glob` v10+ is ESM-only and doesn't provide a default export so the
    import in Vite config was updated accordingly.

--- apps/shade/src/components/ui/icon.stories.tsx
+++ apps/shade/src/components/ui/icon.stories.tsx
@@ -1,5 +1,5 @@
 import Icon, {IconName} from '@/components/ui/icon';
-import type {Meta, StoryObj} from '@storybook/react';
+import type {Meta, StoryObj} from '@storybook/react-vite';
 import {useState} from 'react';
 
 const meta = {
