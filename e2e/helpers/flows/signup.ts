b7aaf21b68e540ef51550726ed6dcd8674b1da4e
Steve Larson <9larsons@gmail.com>
2025-09-24 12:57:24 -0500

    Created sign up flow

---
+++ e2e/helpers/flows/signup.ts
@@ -0,0 +1,25 @@
+import {Page} from '@playwright/test';
+import {HomePage} from '../pages/public';
+import {SignUpPage} from '../pages/portal/SignUpPage';
+import {SignUpSuccessPage} from '../pages/portal/SignUpSuccessPage';
+import {randomEmail, randomName} from '../utils/generators';
+
+/**
+ * Complete the portal signup flow and return the test email used
+ */
+export async function signupViaPortal(page: Page): Promise<{email: string; name: string}> {
+    const homePage = new HomePage(page);
+    await homePage.goto();
+    await homePage.openPortalViaSubscribeButton();
+
+    const signUpPage = new SignUpPage(page);
+    const email = randomEmail();
+    const name = randomName();
+    await signUpPage.fillAndSubmit(email, name);
+
+    const successPage = new SignUpSuccessPage(page);
+    await successPage.waitForSignUpSuccess();
+    await successPage.closePortal();
+
+    return {email, name};
+}
