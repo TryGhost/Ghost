5f8c76e6a70eb00cfe88b7a1fe4ffc5e6e5ec22e
Igor Balos <ibalosh@gmail.com>
2025-09-24 12:42:36 +0200

    remove 'should' format, to keep test stories consistent to our current ones
    fix test decribe function

--- e2e/tests/public/portal-loading.test.ts
+++ e2e/tests/public/portal-loading.test.ts
@@ -4,29 +4,31 @@ import {SignUpPage} from '../../helpers/pages/portal/SignUpPage';
 import {SignInPage} from '../../helpers/pages/portal/SignInPage';
 
 test.describe('Portal Loading', () => {
-    test('can open Portal via Subscribe button', async ({page}) => {
-        const homePage = new HomePage(page);
-        await homePage.goto();
+    test.describe('can open Portal', function () {
+        test('via Subscribe button', async ({page}) => {
+            const homePage = new HomePage(page);
+            await homePage.goto();
 
-        await homePage.openPortalViaSubscribeButton();
+            await homePage.openPortalViaSubscribeButton();
 
-        const signUpPage = new SignUpPage(page);
-        await expect(signUpPage.emailInput).toBeVisible();
-        await expect(signUpPage.signupButton).toBeVisible();
+            const signUpPage = new SignUpPage(page);
+            await expect(signUpPage.emailInput).toBeVisible();
+            await expect(signUpPage.signupButton).toBeVisible();
+        });
+
+        test('via Sign in link', async ({page}) => {
+            const homePage = new HomePage(page);
+            await homePage.goto();
+
+            await homePage.openPortalViaSignInLink();
+
+            const signInPage = new SignInPage(page);
+            await expect(signInPage.emailInput).toBeVisible();
+            await expect(signInPage.continueButton).toBeVisible();
+        });
     });
-    
-    test('can open Portal via Sign in link', async ({page}) => {
-        const homePage = new HomePage(page);
-        await homePage.goto();
 
-        await homePage.openPortalViaSignInLink();
-
-        const signInPage = new SignInPage(page);
-        await expect(signInPage.emailInput).toBeVisible();
-        await expect(signInPage.continueButton).toBeVisible();
-    });
-    
-    test('Portal can switch between signup and signin modes', async ({page}) => {
+    test('Portal can switch between signup and sign in modes', async ({page}) => {
         const homePage = new HomePage(page);
         await homePage.goto();
 
