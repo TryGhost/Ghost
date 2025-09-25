f9dbff15e88032e1386aada98b7c9db5ca01ba0c
Troy Ciesco <tmciesco@gmail.com>
2025-09-25 14:30:38 -0400

    Reverted "Added new OTC input to portal (#24873)" (#24988)
    ref https://linear.app/ghost/issue/PROD-2537
    
    - During testing we found that some of the nice UX in the shadcn version
    of the OTC input component doesn't show up in our version (keyboard
    traversal, etc)
    - This seems to be because of focus issues with the portal modal itself,
    rather than the component implementation. Placing the component
    elsewhere, outside of the portal, doesn't show the same issues
    - Since the current regular input field works fine and the new OTC input
    field has a chance to cause some friction for users, we're reverting the
    change until we fix the portal focus
    - This reverts commit c4a575547507e14ede2581148f7466caedb4dd26.

--- apps/portal/src/components/Frame.styles.js
+++ apps/portal/src/components/Frame.styles.js
@@ -24,7 +24,6 @@ import {TipsAndDonationsSuccessStyle} from './pages/SupportSuccess';
 import {TipsAndDonationsErrorStyle} from './pages/SupportError';
 import {RecommendationsPageStyles} from './pages/RecommendationsPage';
 import NotificationStyle from './Notification.styles';
-import {InputOTCStyles} from './common/InputOTC.styles';
 
 // Global styles
 const FrameStyles = `
@@ -1278,7 +1277,6 @@ export function getFrameStyles({site}) {
         SignupPageStyles +
         OfferPageStyles({site}) +
         NotificationStyle +
-        InputOTCStyles +
         PopupNotificationStyles +
         MobileStyles +
         MultipleProductsGlobalStyles +
