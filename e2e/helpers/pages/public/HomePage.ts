7c85aa148360f24f7fda4c81b1d1645d6e0423ea
Steve Larson <9larsons@gmail.com>
2025-09-30 11:31:12 -0500

    Added mailhog for e2e tests (#24893)
    ref https://linear.app/ghost/issue/PROD-2515/
    - added Mailhog to the e2e package to support email related
    functionality; this is necessary piece of member signups
    - added a member sign up flow test
    
    This set of changes is to enable the ability to do full e2e member
    signup and attribution tests. In order to do that, we need email
    support. Here we add Mailhog, which is used in our development Docker
    setup, in order to accomplish this.
    
    We are fairly tightly coupled to Mailhog here as we explore this option
    and how it works for testing before moving to an adapter pattern like we
    use elsewhere.

--- e2e/helpers/pages/public/HomePage.ts
+++ e2e/helpers/pages/public/HomePage.ts
@@ -4,6 +4,7 @@ import PublicPage from './PublicPage';
 export class HomePage extends PublicPage {
     readonly title: Locator;
     readonly mainSubscribeButton: Locator;
+    readonly accountButton: Locator;
 
     constructor(page: Page) {
         super(page);
@@ -11,5 +12,10 @@ export class HomePage extends PublicPage {
         this.pageUrl = '/';
         this.mainSubscribeButton = page.getByRole('button', {name: 'Subscribe'}).first();
         this.title = page.getByRole('heading', {level: 1});
+        this.accountButton = page.locator('[data-portal="account"]').first();
+    }
+
+    async waitForSignedIn(): Promise<void> {
+        await this.accountButton.waitFor({state: 'visible'});
     }
 }
