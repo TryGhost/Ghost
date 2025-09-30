41199e0f9f42a982e91343c34982a4d08649129a
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2025-09-30 13:45:25 +0200

    Integrate settings app
    ref https://linear.app/ghost/issue/BER-2915/integrate-settings-app-with-admin-shell
    
    - Add the required routes and export the correct entry point
    - Update the Vite config to handle quirks of the settings app
    - Exclude it from the Ember app

--- apps/admin-x-settings/src/App.tsx
+++ apps/admin-x-settings/src/App.tsx
@@ -8,31 +8,34 @@ import {RoutingProvider} from '@tryghost/admin-x-framework/routing';
 import {type ZapierTemplate} from './components/settings/advanced/integrations/ZapierModal';
 
 interface AppProps {
-    framework: TopLevelFrameworkProps;
     designSystem: DesignSystemAppProps;
     officialThemes: OfficialTheme[];
     zapierTemplates: ZapierTemplate[];
     upgradeStatus?: UpgradeStatusType;
 }
 
-function App({framework, designSystem, officialThemes, zapierTemplates, upgradeStatus}: AppProps) {
+export function App({designSystem, officialThemes, zapierTemplates, upgradeStatus}: AppProps) {
     return (
-        <FrameworkProvider {...framework}>
-            <SettingsAppProvider officialThemes={officialThemes} upgradeStatus={upgradeStatus} zapierTemplates={zapierTemplates}>
-                {/* NOTE: we need to have an extra NiceModal.Provider here because the one inside DesignSystemApp
-                    is loaded too late for possible modals in RoutingProvider, and it's quite hard to change it at
-                    this point */}
-                <NiceModal.Provider>
-                    <RoutingProvider basePath='settings' modals={{paths: modalPaths, load: loadModals}}>
-                        <DesignSystemApp className='admin-x-settings' {...designSystem}>
-                            <SettingsRouter />
-                            <MainContent />
-                        </DesignSystemApp>
-                    </RoutingProvider>
-                </NiceModal.Provider>
-            </SettingsAppProvider>
-        </FrameworkProvider>
+        <SettingsAppProvider officialThemes={officialThemes} upgradeStatus={upgradeStatus} zapierTemplates={zapierTemplates}>
+            {/* NOTE: we need to have an extra NiceModal.Provider here because the one inside DesignSystemApp
+                is loaded too late for possible modals in RoutingProvider, and it's quite hard to change it at
+                this point */}
+            <NiceModal.Provider>
+                <RoutingProvider basePath='settings' modals={{paths: modalPaths, load: loadModals}}>
+                    <DesignSystemApp className='admin-x-settings' {...designSystem}>
+                        <SettingsRouter />
+                        <MainContent />
+                    </DesignSystemApp>
+                </RoutingProvider>
+            </NiceModal.Provider>
+        </SettingsAppProvider>
     );
 }
 
-export default App;
+export function StandaloneApp({framework, designSystem, officialThemes, zapierTemplates, upgradeStatus}: AppProps & {framework: TopLevelFrameworkProps}) {
+    return (
+        <FrameworkProvider {...framework}>
+            <App designSystem={designSystem} officialThemes={officialThemes} upgradeStatus={upgradeStatus} zapierTemplates={zapierTemplates} />
+        </FrameworkProvider>
+    );
+}
