e741ad6f4d34ada57109cbc27575e48e073a6984
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2025-10-01 15:28:37 +0200

    Add E2E tests for new tag list (#24816)
    Refs https://linear.app/ghost/issue/PROD-2508/set-up-tests-within-new-e2e-structure
    
    This PR adds some basic tests for the new tags screen to the new E2E
    Playwright setup. The mocks should be removed once we have factories
    for tags and feature flags in place.

--- apps/posts/src/views/Tags/components/TagsLayout.tsx
+++ apps/posts/src/views/Tags/components/TagsLayout.tsx
@@ -4,8 +4,8 @@ import React from 'react';
 const PostAnalyticsLayout: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({children}) => {
     return (
         <MainLayout>
-            <div className='grid w-full grow'>
-                <div className='flex h-full flex-col'>
+            <div className="grid w-full grow">
+                <div className="flex h-full flex-col" data-testid="tags-page">
                     {children}
                 </div>
             </div>
--- e2e/helpers/pages/admin/index.ts
+++ e2e/helpers/pages/admin/index.ts
@@ -11,3 +11,4 @@ export * from './AdminPage';
 export * from './MembersPage';
 export * from './PostEditorPage';
 export * from './settings/SettingsPage';
+export * from './tags/TagsPage';
