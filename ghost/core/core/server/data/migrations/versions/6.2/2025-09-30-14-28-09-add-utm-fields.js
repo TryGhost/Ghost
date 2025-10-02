f36c02e06d9e691c0c4c73647d31f96bec8fa7a2
Steve Larson <9larsons@gmail.com>
2025-10-02 13:46:00 -0500

    Updated migration

--- ghost/core/core/server/data/migrations/versions/6.2/2025-09-30-14-28-09-add-utm-fields.js
+++ ghost/core/core/server/data/migrations/versions/6.2/2025-09-30-14-28-09-add-utm-fields.js
@@ -1,60 +1,24 @@
-const logging = require('@tryghost/logging');
-const {createTransactionalMigration} = require('../../utils');
+const {combineNonTransactionalMigrations, createAddColumnMigration} = require('../../utils');
 
-const tables = [
-    'members_created_events',
-    'members_subscription_created_events',
-    'donation_payment_events'
-];
+module.exports = combineNonTransactionalMigrations(
+    // members_created_events
+    createAddColumnMigration('members_created_events', 'utm_source', {type: 'string', maxlength: 191, nullable: true}),
+    createAddColumnMigration('members_created_events', 'utm_medium', {type: 'string', maxlength: 191, nullable: true}),
+    createAddColumnMigration('members_created_events', 'utm_campaign', {type: 'string', maxlength: 191, nullable: true}),
+    createAddColumnMigration('members_created_events', 'utm_term', {type: 'string', maxlength: 191, nullable: true}),
+    createAddColumnMigration('members_created_events', 'utm_content', {type: 'string', maxlength: 191, nullable: true}),
 
-const utmColumns = [
-    {name: 'utm_source', definition: {type: 'string', maxlength: 191, nullable: true}},
-    {name: 'utm_medium', definition: {type: 'string', maxlength: 191, nullable: true}},
-    {name: 'utm_campaign', definition: {type: 'string', maxlength: 191, nullable: true}},
-    {name: 'utm_term', definition: {type: 'string', maxlength: 191, nullable: true}},
-    {name: 'utm_content', definition: {type: 'string', maxlength: 191, nullable: true}}
-];
+    // members_subscription_created_events
+    createAddColumnMigration('members_subscription_created_events', 'utm_source', {type: 'string', maxlength: 191, nullable: true}),
+    createAddColumnMigration('members_subscription_created_events', 'utm_medium', {type: 'string', maxlength: 191, nullable: true}),
+    createAddColumnMigration('members_subscription_created_events', 'utm_campaign', {type: 'string', maxlength: 191, nullable: true}),
+    createAddColumnMigration('members_subscription_created_events', 'utm_term', {type: 'string', maxlength: 191, nullable: true}),
+    createAddColumnMigration('members_subscription_created_events', 'utm_content', {type: 'string', maxlength: 191, nullable: true}),
 
-module.exports = createTransactionalMigration(
-    async function up(knex) {
-        // we should allow for loops because we want sequential execution
-        // eslint-disable-next-line no-restricted-syntax
-        for (const table of tables) {
-            logging.info(`Adding UTM columns to ${table}`);
-
-            // eslint-disable-next-line no-restricted-syntax
-            for (const column of utmColumns) {
-                const hasColumn = await knex.schema.hasColumn(table, column.name);
-                if (!hasColumn) {
-                    logging.info(`Adding column ${column.name} to ${table}`);
-                    await knex.schema.table(table, function (t) {
-                        t.string(column.name, column.definition.maxlength).nullable();
-                    });
-                } else {
-                    logging.warn(`Column ${column.name} already exists in ${table} - skipping`);
-                }
-            }
-        }
-    },
-
-    async function down(knex) {
-        // we should allow for loops because we want sequential execution
-        // eslint-disable-next-line no-restricted-syntax
-        for (const table of tables) {
-            logging.info(`Removing UTM columns from ${table}`);
-
-            // eslint-disable-next-line no-restricted-syntax
-            for (const column of utmColumns) {
-                const hasColumn = await knex.schema.hasColumn(table, column.name);
-                if (hasColumn) {
-                    logging.info(`Dropping column ${column.name} from ${table}`);
-                    await knex.schema.table(table, function (t) {
-                        t.dropColumn(column.name);
-                    });
-                } else {
-                    logging.warn(`Column ${column.name} does not exist in ${table} - skipping`);
-                }
-            }
-        }
-    }
+    // donation_payment_events
+    createAddColumnMigration('donation_payment_events', 'utm_source', {type: 'string', maxlength: 191, nullable: true}),
+    createAddColumnMigration('donation_payment_events', 'utm_medium', {type: 'string', maxlength: 191, nullable: true}),
+    createAddColumnMigration('donation_payment_events', 'utm_campaign', {type: 'string', maxlength: 191, nullable: true}),
+    createAddColumnMigration('donation_payment_events', 'utm_term', {type: 'string', maxlength: 191, nullable: true}),
+    createAddColumnMigration('donation_payment_events', 'utm_content', {type: 'string', maxlength: 191, nullable: true})
 );
