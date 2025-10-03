64b03cb345675e03d135689d9c1d09fa85abd075
Peter Zimon <zimo@ghost.org>
2025-10-03 12:46:50 +0200

    Improved OTP input styles in Portal (#25040)
    ref https://linear.app/ghost/issue/PROD-2706/
    
    - The current styling of the OTP field in Portal was not following common visual and UX standards.
    
    ---------
    
    Co-authored-by: Kevin Ansfield <kevin@ghost.org>

--- apps/portal/src/components/pages/MagicLinkPage.js
+++ apps/portal/src/components/pages/MagicLinkPage.js
@@ -4,8 +4,6 @@ import CloseButton from '../common/CloseButton';
 import AppContext from '../../AppContext';
 import {ReactComponent as EnvelopeIcon} from '../../images/icons/envelope.svg';
 
-import InputField from '../common/InputField';
-
 export const MagicLinkStyles = `
     .gh-portal-icon-envelope {
         width: 44px;
@@ -144,7 +142,7 @@ export default class MagicLinkPage extends React.Component {
             const code = (state.otc || '').trim();
             return {
                 errors: {
-                    [OTC_FIELD_NAME]: code ? '' : t('Enter code below')
+                    [OTC_FIELD_NAME]: code ? '' : t('Enter code above')
                 }
             };
         }, () => {
@@ -161,9 +159,19 @@ export default class MagicLinkPage extends React.Component {
 
     handleInputChange(e, field) {
         const fieldName = field.name;
-        this.setState({
-            [fieldName]: e.target.value
-        });
+        const value = e.target.value;
+
+        // For OTC field, only allow numeric input
+        if (fieldName === OTC_FIELD_NAME) {
+            const numericValue = value.replace(/[^0-9]/g, '');
+            this.setState({
+                [fieldName]: numericValue
+            });
+        } else {
+            this.setState({
+                [fieldName]: value
+            });
+        }
     }
 
     renderOTCForm() {
@@ -180,20 +188,26 @@ export default class MagicLinkPage extends React.Component {
 
         return (
             <form onSubmit={e => this.handleSubmit(e)}>
-                <section className='gh-portal-section'>
-                    {/* @TODO: create different input component with updated design */}
-                    <InputField
-                        id={`input-${OTC_FIELD_NAME}`}
-                        name={OTC_FIELD_NAME}
-                        type="text"
-                        value={this.state.otc}
-                        placeholder="• • • • • •"
-                        label={t('Code')}
-                        errorMessage={errors.otc || ''}
-                        autoFocus={false}
-                        maxLength={6}
-                        onChange={e => this.handleInputChange(e, {name: OTC_FIELD_NAME})}
-                    />
+                <section className='gh-portal-section gh-portal-otp'>
+                    <div className={`gh-portal-otp-field-container ${errors.otc ? 'error' : ''}`}>
+                        <input
+                            id={`input-${OTC_FIELD_NAME}`}
+                            className={`gh-portal-input ${errors.otc ? 'error' : ''}`}
+                            name={OTC_FIELD_NAME}
+                            type="text"
+                            value={this.state.otc}
+                            inputMode="numeric"
+                            pattern="[0-9]*"
+                            aria-label={t('Code')}
+                            autoFocus={false}
+                            maxLength={6}
+                            onChange={e => this.handleInputChange(e, {name: OTC_FIELD_NAME})}
+                        />
+                    </div>
+                    {errors.otc &&
+                    <div className="gh-portal-otp-error">
+                        {errors.otc}
+                    </div>}
                 </section>
                 <footer className='gh-portal-signin-footer'>
                     <ActionButton
