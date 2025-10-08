1b40d9d7417c9292273198509e87a1edadd9eeb5
Peter Zimon <peter.zimon@gmail.com>
2025-10-08 16:57:16 +0200

    Removed controlled navigation in favor of Link components

--- apps/posts/src/views/Tags/components/TagsHeader.tsx
+++ apps/posts/src/views/Tags/components/TagsHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Button, Header, ToggleGroup, ToggleGroupItem} from '@tryghost/shade';
-import {useNavigate} from '@tryghost/admin-x-framework';
+import {Link} from '@tryghost/admin-x-framework';
 
 interface TagsHeaderProps {
     currentTab?: string;
@@ -8,21 +8,21 @@ interface TagsHeaderProps {
 }
 
 const TagsHeader: React.FC<TagsHeaderProps> = ({currentTab}) => {
-    const navigate = useNavigate();
-
     return (
         <Header variant="inline-nav">
             <Header.Title>Tags</Header.Title>
 
             <Header.Nav>
-                <ToggleGroup data-testid="tags-header-tabs" size='button' type="single" value={currentTab} onValueChange={(newTab) => {
-                    navigate(newTab === 'internal' ? '/tags?type=internal' : '/tags');
-                }}>
-                    <ToggleGroupItem aria-label="Public tags" value="public">
-                        Public tags
+                <ToggleGroup data-testid="tags-header-tabs" size='button' type="single" value={currentTab}>
+                    <ToggleGroupItem aria-label="Public tags" value="public" asChild>
+                        <Link to="/tags">
+                            Public tags
+                        </Link>
                     </ToggleGroupItem>
-                    <ToggleGroupItem aria-label="Internal tags" value="internal">
-                        Internal tags
+                    <ToggleGroupItem aria-label="Internal tags" value="internal" asChild>
+                        <Link to="/tags?type=internal">
+                            Internal tags
+                        </Link>
                     </ToggleGroupItem>
                 </ToggleGroup>
             </Header.Nav>
