505b86190c065a2e21f715deb12a66dbb6dd2755
Chris Raible <chris@ghost.org>
2025-10-08 19:29:32 -0700

    Changed test to guarantee a failure

--- e2e/tests/admin/analytics/overview.test.ts
+++ e2e/tests/admin/analytics/overview.test.ts
@@ -18,7 +18,7 @@ test.describe('Ghost Admin - Analytics Overview', () => {
         await analyticsOverviewPage.goto();
         await analyticsOverviewPage.refreshData();
 
-        expect(await analyticsOverviewPage.uniqueVisitors.count()).toBe(1);
+        expect(await analyticsOverviewPage.uniqueVisitors.count()).toBe(9);
     });
 
     test('latest post', async ({page}) => {
