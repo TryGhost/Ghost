583979f5761dc51182e20f37ee81e77464334cba
Chris Raible <chris@ghost.org>
2025-10-08 20:26:15 -0700

    Fixed intentionally broken test

--- e2e/tests/admin/analytics/overview.test.ts
+++ e2e/tests/admin/analytics/overview.test.ts
@@ -17,7 +17,7 @@ test.describe('Ghost Admin - Analytics Overview', () => {
         await analyticsOverviewPage.goto();
         await analyticsOverviewPage.refreshData();
 
-        expect(await analyticsOverviewPage.uniqueVisitors.count()).toBe(9);
+        expect(await analyticsOverviewPage.uniqueVisitors.count()).toBe(1);
     });
 
     test('latest post', async ({page}) => {
