dcf8967c6b7ac5d67095ba00d7228ff94f7364b0
Igor Balos <ibalosh@gmail.com>
2025-10-13 14:51:19 +0200

    Cleaned up code to make it more aligned to our test suite (#25148)
    * Extracted shared tag form logic into base tag details class containing common form fields
    * Improved test clarity by separating concerns between creation (NewTagsPage) and editing (TagEditorPage) workflows
    * Updated all tag editor tests to use the new page object structure
    * New logic provides better readability of the tests and easier maintenance of the test stories

--- e2e/helpers/pages/admin/tags/TagEditorPage.ts
+++ e2e/helpers/pages/admin/tags/TagEditorPage.ts
@@ -1,14 +1,7 @@
 import {Locator, Page} from '@playwright/test';
-import {AdminPage} from '../AdminPage';
+import {TagDetailsPage} from './TagDetailsPage';
 
-export class TagEditorPage extends AdminPage {
-    readonly nameInput: Locator;
-    readonly slugInput: Locator;
-    readonly descriptionInput: Locator;
-    readonly saveButton: Locator;
-    readonly deleteButton: Locator;
-    readonly backLink: Locator;
-
+export class TagEditorPage extends TagDetailsPage {
     readonly deleteModal: Locator;
     readonly deleteModalPostsCount: Locator;
     readonly deleteModalConfirmButton: Locator;
@@ -19,13 +12,6 @@ export class TagEditorPage extends AdminPage {
         super(page);
 
         this.pageUrl = '/ghost/#/tags';
-        
-        this.nameInput = page.locator('[data-test-input="tag-name"]');
-        this.slugInput = page.locator('[data-test-input="tag-slug"]');
-        this.descriptionInput = page.locator('[data-test-input="tag-description"]');
-        this.saveButton = page.locator('[data-test-button="save"]');
-        this.deleteButton = page.locator('[data-test-button="delete-tag"]');
-        this.backLink = page.locator('[data-test-link="tags-back"]');
 
         this.deleteModal = page.locator('[data-test-modal="confirm-delete-tag"]');
         this.deleteModalPostsCount = this.deleteModal.locator('[data-test-text="posts-count"]');
@@ -34,35 +20,15 @@ export class TagEditorPage extends AdminPage {
         this.navMenuItem = page.locator('[data-test-nav="tags"]');
     }
 
-    async gotoNew() {
-        await this.page.goto('/ghost/#/tags/new');
+    async gotoTagBySlug(slug: string) {
+        this.pageUrl = `/ghost/#/tags/${slug}`;
+        await this.page.goto(this.pageUrl);
     }
 
-    async createTag(name: string, slug: string) {
-        await this.page.goto('/ghost/#/tags/new');
+    async updateTag(name: string, slug: string) {
         await this.fillTagName(name);
         await this.fillTagSlug(slug);
         await this.save();
-    }
-
-    async gotoTagBySlug(slug: string) {
-        await this.page.goto(`/ghost/#/tags/${slug}`);
-    }
-
-    async fillTagName(name: string) {
-        await this.nameInput.fill(name);
-    }
-
-    async fillTagSlug(slug: string) {
-        await this.slugInput.fill(slug);
-    }
-
-    async fillTagDescription(description: string) {
-        await this.descriptionInput.fill(description);
-    }
-
-    async save() {
-        await this.saveButton.click();
     }
 
     async deleteTag() {
@@ -71,10 +37,6 @@ export class TagEditorPage extends AdminPage {
 
     async confirmDelete() {
         await this.deleteModalConfirmButton.click();
-    }
-
-    async goBackToTagsList() {
-        await this.backLink.click();
     }
 }
 
