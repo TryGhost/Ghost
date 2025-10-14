50b6c55b275b19179272b7f99e53c0604ae19cb1
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-10-14 20:14:07 +0800

    Improved loading state of profiles in ActivityPub (#25157)
    ref
    https://linear.app/ghost/issue/PROD-2888/improve-loading-state-of-profile
    
    - hides copy handle button while loading as it was redundant when the
    handle isn't ready
    - displays skeleton loader in posts tab right away instead of waiting
    for the account data is finished loading

--- apps/activitypub/src/views/Profile/components/ProfilePage.tsx
+++ apps/activitypub/src/views/Profile/components/ProfilePage.tsx
@@ -213,12 +213,14 @@ const ProfilePage:React.FC<ProfilePageProps> = ({
                                 <a className='inline-flex max-w-full truncate text-[1.5rem] text-gray-800 hover:text-gray-900 dark:text-gray-600 dark:hover:text-gray-500' href={account?.url} rel='noopener noreferrer' target='_blank'>
                                     <span className='truncate'>{!isLoadingAccount ? account?.handle : <Skeleton className='w-full max-w-56' />}</span>
                                 </a>
-                                <Button className='-ml-1.5 size-6 p-0 text-gray-800 hover:text-gray-900 dark:text-gray-700 dark:hover:text-gray-600' title='Copy handle' variant='link' onClick={handleCopy}>
-                                    {!copied ?
-                                        <LucideIcon.Copy size={16} /> :
-                                        <LucideIcon.Check size={16} />
-                                    }
-                                </Button>
+                                {!isLoadingAccount && (
+                                    <Button className='-ml-1.5 size-6 p-0 text-gray-800 hover:text-gray-900 dark:text-gray-700 dark:hover:text-gray-600' title='Copy handle' variant='link' onClick={handleCopy}>
+                                        {!copied ?
+                                            <LucideIcon.Copy size={16} /> :
+                                            <LucideIcon.Check size={16} />
+                                        }
+                                    </Button>
+                                )}
                                 {account?.followsMe && !isLoadingAccount && (
                                     <Badge className='mt-px whitespace-nowrap' variant='secondary'>Follows you</Badge>
                                 )}
