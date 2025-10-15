029c2f807a5052403df4de2a5b88ff9672eacb2b
Igor Balos <ibalosh@gmail.com>
2025-10-15 10:56:49 +0200

    removed identifier since we are not using it

--- e2e/data-factory/factories/factory.ts
+++ e2e/data-factory/factories/factory.ts
@@ -9,11 +9,6 @@ export abstract class Factory<TOptions extends Record<string, unknown> = Record<
         this.adapter = adapter;
     }
 
-    // name allows you to identify Factory by it's class name
-    get name(): string {
-        return this.constructor.name;
-    }
-
     abstract build(options?: Partial<TOptions>): TResult;
 
     async create(options?: Partial<TOptions>): Promise<TResult> {
