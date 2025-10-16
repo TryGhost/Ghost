a18f6b4f07f5c41faf9ce055f40b027f1148f2b3
Chris Raible <chris@ghost.org>
2025-10-15 19:12:41 -0700

    Added eslint-plugin-playwright to catch common issues

--- e2e/.eslintrc.js
+++ e2e/.eslintrc.js
@@ -1,8 +1,19 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
-    plugins: ['ghost'],
+    plugins: [
+        'ghost',
+        'eslint-plugin-playwright'
+    ],
     extends: [
         'plugin:ghost/ts'
     ],
-    ignorePatterns: ['build/']
+    ignorePatterns: ['build/'],
+    overrides: [
+        {
+            files: ['tests/**', 'helpers/playwright/**', 'helpers/pages/**'],
+            extends: [
+                'plugin:playwright/recommended'
+            ]
+        }
+    ]
 };
