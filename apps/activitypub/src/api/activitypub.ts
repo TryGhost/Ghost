83799f17d515d34fb209c9c34c10867ba0ede671
Michael Barrett <mike@ghost.org>
2025-10-16 16:10:31 +0100

    Updated activitypub Bluesky sharing enablement flow
    ref https://github.com/TryGhost/ActivityPub/pull/1377
    
    We updated the backend implementation of the Bluesky sharing enablement process
    to include a confirmation step in which the client would prompt the backend
    to check what handle has been assigned to the Bluesky account generated by
    Bridgy Fed - This circumvents the need to for us to compute the handle manually
    and potentially getting it wrong if we compute it in a different way to Bridgy
    Fed

--- apps/activitypub/src/api/activitypub.ts
+++ apps/activitypub/src/api/activitypub.ts
@@ -26,7 +26,8 @@ export interface Account {
     domainBlockedByMe: boolean;
     attachment: { name: string; value: string }[];
     blueskyEnabled?: boolean;
-    blueskyHandle?: string;
+    blueskyHandleConfirmed?: boolean;
+    blueskyHandle?: string | null;
 }
 
 export type AccountSearchResult = Pick<
@@ -707,8 +708,20 @@ export class ActivityPubAPI {
         return json.fileUrl;
     }
 
-    async enableBluesky(): Promise<string> {
+    async enableBluesky() {
         const url = new URL('.ghost/activitypub/v1/actions/bluesky/enable', this.apiUrl);
+
+        await this.fetchJSON(url, 'POST');
+    }
+
+    async disableBluesky() {
+        const url = new URL('.ghost/activitypub/v1/actions/bluesky/disable', this.apiUrl);
+
+        await this.fetchJSON(url, 'POST');
+    }
+
+    async confirmBlueskyHandle(): Promise<string> {
+        const url = new URL('.ghost/activitypub/v1/actions/bluesky/confirm-handle', this.apiUrl);
 
         const json = await this.fetchJSON(url, 'POST');
 
@@ -717,11 +730,5 @@ export class ActivityPubAPI {
         }
 
         return String(json.handle);
-    }
-
-    async disableBluesky() {
-        const url = new URL('.ghost/activitypub/v1/actions/bluesky/disable', this.apiUrl);
-
-        await this.fetchJSON(url, 'POST');
     }
 }
