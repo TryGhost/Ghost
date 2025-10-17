d33cf384c9382361073b33999f6d43ab97311e46
Cathy Sarisky <catsarisky@gmail.com>
2025-10-17 07:36:22 -0400

    small refactor / tidy-up

--- ghost/core/core/frontend/helpers/split.js
+++ ghost/core/core/frontend/helpers/split.js
@@ -1,5 +1,12 @@
 const {SafeString} = require('handlebars');
 
+function renderResult(result, options, data) {
+    if (options && typeof options.fn === 'function') {
+        return options.fn(result, {data, blockParams: [result]});
+    }
+    return result;
+}
+
 module.exports = function split(...args) {
     const options = args.pop();
     const data = options.data || {};
@@ -8,15 +15,7 @@ module.exports = function split(...args) {
     
     // Handle undefined and null inputs
     if (string === undefined || string === null) {
-        const emptyResult = [];
-        if (options.fn) {
-            return options.fn(emptyResult, {
-                data: data,
-                blockParams: [emptyResult]
-            });
-        } else {
-            return emptyResult;
-        }
+        return renderResult([], options, data);
     }
     
     // Convert non-string types to string (e.g. numbers)
@@ -26,27 +25,10 @@ module.exports = function split(...args) {
     
     // Handle empty string input
     if (string === '') {
-        const emptyResult = [];
-        if (options.fn) {
-            return options.fn(emptyResult, {
-                data: data,
-                blockParams: [emptyResult]
-            });
-        } else {
-            return emptyResult;
-        }
+        return renderResult([], options, data);
     }
 
     const result = string.split(separator).map(item => new SafeString(item));
-
-    if (options.fn) {
-        // we're in block mode
-        return options.fn(result, {
-            data: data,
-            blockParams: [result]
-        });
-    } else {
-        // we're in inline mode
-        return result;
-    }
+    
+    return renderResult(result, options, data);
 };
