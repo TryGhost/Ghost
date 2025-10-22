ee3ee1190cafc8c6eed341162225bdd7baff82cb
Michael Barrett <mike@ghost.org>
2025-10-22 13:13:17 +0100

    Added feature flagged Global Feed to ActivityPub (#25205)
    ref https://linear.app/ghost/issue/BER-2909
    
    Added a Global Feed section to the ActivityPub app that is behind the
    flag: `global-feed`. This is to allow the ActivityPub team to validate /
    iterate on the concept of a Global Feed within the ActivityPub app to
    help improve discoverability for new accounts
    
    ---------
    
    Co-authored-by: Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
    Co-authored-by: Sag <guptazy@gmail.com>

--- apps/activitypub/src/views/Inbox/Inbox.tsx
+++ apps/activitypub/src/views/Inbox/Inbox.tsx
@@ -2,11 +2,16 @@ import Error from '@components/layout/Error';
 import InboxList from './components/InboxList';
 import React from 'react';
 import {isApiError} from '@src/api/activitypub';
-import {useInboxForUser} from '@hooks/use-activity-pub-queries';
+import {useFeedMode} from '@src/hooks/use-feed-mode';
+import {useGlobalFeedForUser, useInboxForUser} from '@hooks/use-activity-pub-queries';
 
 const Inbox: React.FC = () => {
-    const {inboxQuery} = useInboxForUser({enabled: true});
-    const feedQueryData = inboxQuery;
+    const {feedMode} = useFeedMode();
+
+    const {inboxQuery: followingQuery} = useInboxForUser({enabled: feedMode === 'following'});
+    const {globalFeedQuery: discoverQuery} = useGlobalFeedForUser({enabled: feedMode === 'discover'});
+
+    const feedQueryData = feedMode === 'discover' ? discoverQuery : followingQuery;
     const {data, error, fetchNextPage, hasNextPage, isFetchingNextPage, isLoading} = feedQueryData;
 
     const activities = (data?.pages.flatMap(page => page.posts) ?? Array.from({length: 5}, (_, index) => ({id: `placeholder-${index}`, object: {}})));
