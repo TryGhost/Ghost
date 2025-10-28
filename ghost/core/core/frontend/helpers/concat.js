d7556c3fcfd2c3da856371d2e8af5a8dce27506e
Cathy Sarisky <catsarisky@gmail.com>
2025-10-28 08:50:49 -0400

    adds the ability to concat an array

--- ghost/core/core/frontend/helpers/concat.js
+++ ghost/core/core/frontend/helpers/concat.js
@@ -4,5 +4,13 @@ module.exports = function concat(...args) {
     const options = args.pop();
     const separator = options.hash.separator || '';
 
-    return new SafeString(args.join(separator));
+    // Flatten arrays - if an argument is an array, spread its elements
+    const flattenedArgs = args.reduce((acc, arg) => {
+        if (Array.isArray(arg)) {
+            return acc.concat(arg);
+        }
+        return acc.concat([arg]);
+    }, []);
+
+    return new SafeString(flattenedArgs.join(separator));
 };
