e3d98b6edd0b6bd6562cf75c2390b81414268b3c
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2025-10-29 08:58:13 +0100

    Replace base path with wrapping top-level route

--- apps/activitypub/src/App.tsx
+++ apps/activitypub/src/App.tsx
@@ -1,7 +1,7 @@
-import {APP_ROUTE_PREFIX, routes} from '@src/routes';
 import {FeatureFlagsProvider} from './lib/feature-flags';
 import {FrameworkProvider, Outlet, RouterProvider, TopLevelFrameworkProps} from '@tryghost/admin-x-framework';
 import {ShadeApp} from '@tryghost/shade';
+import {routes} from '@src/routes';
 
 interface AppProps {
     framework: TopLevelFrameworkProps;
@@ -13,14 +13,9 @@ const App: React.FC<AppProps> = ({framework, activityPubEnabled}) => {
         return null;
     }
 
-    // In test mode, we use hash routing to avoid basename issues
-    const routerProps = import.meta.env.VITE_TEST
-        ? {prefix: '', hash: true, routes}
-        : {prefix: APP_ROUTE_PREFIX, routes};
-
     return (
         <FrameworkProvider {...framework}>
-            <RouterProvider {...routerProps}>
+            <RouterProvider prefix={'/'} routes={routes}>
                 <FeatureFlagsProvider>
                     <ShadeApp className="shade-activitypub" darkMode={false} fetchKoenigLexical={null}>
                         <Outlet />
