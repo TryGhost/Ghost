7e3c2751fcfd8ab24cbf0efbb9f073c24d0a9827
Troy Ciesco <tmciesco@gmail.com>
2025-10-29 16:55:51 -0400

    add status column

--- ghost/core/core/server/data/migrations/versions/6.6/2025-10-28-18-29-37-add-outbox-table.js
+++ ghost/core/core/server/data/migrations/versions/6.6/2025-10-28-18-29-37-add-outbox-table.js
@@ -3,6 +3,7 @@ const {addTable} = require('../../utils');
 module.exports = addTable('outbox', {
     id: {type: 'string', maxlength: 24, nullable: false, primary: true},
     event_type: {type: 'string', maxlength: 50, nullable: false},
+    status: {type: 'string', maxlength: 20, nullable: false, defaultTo: 'pending'},
     payload: {type: 'text', maxlength: 65535, nullable: false},
     created_at: {type: 'dateTime', nullable: false},
     retry_count: {type: 'integer', nullable: false, defaultTo: 0},
