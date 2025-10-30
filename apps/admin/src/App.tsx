1c45e592fd3fce5107ea22f82634fcd297df1971
Kevin Ansfield <kevin@lookingsideways.co.uk>
2025-10-30 18:28:56 +0000

    Added Ember to React data syncing
    ref https://linear.app/ghost/issue/BER-2929/
    
    - updated Ember's `state-bridge` service
      - extended from `Evented` to add `.{on,off,trigger}` methods to manage event subscription and triggering
      - added `triggerEmberDataChange` method that fires a `'emberDataChange'` event, containing the operation type, model, id, and response data that will allow React to invalidate or update its query cache
    - updated `EmbeddedRelationAdapter` to call `stateBridge.triggerEmberDataChange` on any create/update/delete calls made on the Ember Data store
      - our `ApplicationAdapter` extends from `EmbeddedRelationAdapter` so this catches all store activity
    - added `useEmberDataSync` hook on the React side
      - polls for `window.EmberBridge` to exist because it will only be available once Ember boots and that's now hosted inside of the React shell
      - once `window.EmberBridge` is available, subscribes to `'emberDataChange'` events
      - when an event occurs, looks up our React Query data type key based on the Ember Data model name and invalidates all queries using that key
      - only does query invalidation for now rather than optimistic query cache updates to keep things simpler, we can add optimistic updates if/when we find a use-case for it
    
    # Conflicts:
    #	ghost/admin/tests/unit/services/state-bridge-test.js

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -2,10 +2,12 @@ import { Outlet } from "@tryghost/admin-x-framework";
 import { useCurrentUser } from "@tryghost/admin-x-framework/api/currentUser";
 import { EmberProvider, EmberFallback, EmberRoot, useEmberAuthSync } from "./ember-bridge";
 import { AdminLayout } from "./layout/AdminLayout";
+import { useEmberDataSync } from "./ember-bridge";
 
 function App() {
     const { data: currentUser } = useCurrentUser();
     useEmberAuthSync();
+    useEmberDataSync();
 
     return (
         <EmberProvider>
