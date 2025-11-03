c540abb98fc1b9b953571095f890d7a74801bd36
Sam Lord <hello@samlord.me>
2025-11-03 15:30:49 +0000

    Added CSD email count to email model
    closes
    [GVA-583](https://linear.app/ghost/issue/GVA-583/create-migration-in-ghost-to-add-csd-member-count-to-emails)
    
    We need to know how many emails were sent using the CSD from the
    previous email in order to ramp up the sending from the CSD.
    
    See [spike PR](https://github.com/TryGhost/Ghost/pull/25303) for our
    test implementation of domain warmup using this field.

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = 'aa00ea8206673b21837fbcc24897f779';
+    const currentSchemaHash = '5d90909b1b8232b45f0174dfa720a051';
     const currentFixturesHash = '0877727032b8beddbaedc086a8acf1a2';
     const currentSettingsHash = 'bb8be7d83407f2b4fa2ad68c19610579';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
