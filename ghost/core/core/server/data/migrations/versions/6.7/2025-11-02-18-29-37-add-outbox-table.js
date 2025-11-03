3457889c2e1ae3e5b6ca9ba831db0a7b24f28d4f
Troy Ciesco <tmciesco@gmail.com>
2025-11-03 12:40:40 -0500

    add message to schema

--- ghost/core/core/server/data/migrations/versions/6.7/2025-11-02-18-29-37-add-outbox-table.js
+++ ghost/core/core/server/data/migrations/versions/6.7/2025-11-02-18-29-37-add-outbox-table.js
@@ -7,5 +7,6 @@ module.exports = addTable('outbox', {
     payload: {type: 'text', maxlength: 65535, nullable: false},
     created_at: {type: 'dateTime', nullable: false},
     retry_count: {type: 'integer', nullable: false, defaultTo: 0},
-    last_retry_at: {type: 'dateTime', nullable: true}
+    last_retry_at: {type: 'dateTime', nullable: true},
+    message: {type: 'string', maxlength: 2000, nullable: true}
 });
--- ghost/core/core/server/data/schema/schema.js
+++ ghost/core/core/server/data/schema/schema.js
@@ -1125,6 +1125,7 @@ module.exports = {
         payload: {type: 'text', maxlength: 65535, nullable: false},
         created_at: {type: 'dateTime', nullable: false},
         retry_count: {type: 'integer', nullable: false, defaultTo: 0},
-        last_retry_at: {type: 'dateTime', nullable: true}
+        last_retry_at: {type: 'dateTime', nullable: true},
+        message: {type: 'string', maxlength: 2000, nullable: true}
     }
 };
