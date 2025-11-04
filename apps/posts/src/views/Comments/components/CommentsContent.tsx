c45ab733d5e3901ab56c7af71b5d99aba92d94f6
Peter Zimon <peter.zimon@gmail.com>
2025-11-04 10:47:10 +0100

    Refinements

--- apps/posts/src/views/Comments/components/CommentsContent.tsx
+++ apps/posts/src/views/Comments/components/CommentsContent.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CommentsContent: React.FC<{children: React.ReactNode}> = ({children}) => {
     return (
-        <div className="flex-1 overflow-auto px-6 py-4">
+        <div className="flex-1 overflow-auto px-8">
             {children}
         </div>
     );
