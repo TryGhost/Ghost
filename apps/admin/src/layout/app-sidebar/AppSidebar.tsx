4e1394ccde3ad907f1816936d67b9e39f61d3925
Peter Zimon <peter.zimon@gmail.com>
2025-11-05 14:22:49 +0100

    Updated footer

--- apps/admin/src/layout/app-sidebar/AppSidebar.tsx
+++ apps/admin/src/layout/app-sidebar/AppSidebar.tsx
@@ -10,15 +10,19 @@ import NavContent from "./NavContent";
 import NavGhostPro from "./NavGhostPro";
 import NavHeader from "./NavHeader";
 import NavFooter from "./NavFooter";
+import NavSettings from "./NavSettings";
 
 function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
     return (
         <Sidebar {...props}>
             <NavHeader className="px-5 pt-6 pb-0" />
-            <SidebarContent className="px-3 pt-4">
-                <NavMain />
-                <NavContent />
-                <NavGhostPro />
+            <SidebarContent className="px-3 pt-4 justify-between">
+                <div className="flex flex-col gap-2 sidebar:gap-4">
+                    <NavMain />
+                    <NavContent />
+                    <NavGhostPro />
+                </div>
+                <NavSettings className="pb-0" />
             </SidebarContent>
             <NavFooter className="p-3 gap-0" />
         </Sidebar>
---
+++ apps/admin/src/layout/app-sidebar/NavSettings.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+
+import {
+    LucideIcon,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarMenu
+} from "@tryghost/shade"
+import { NavMenuItem } from "./NavMenuItem";
+
+function NavSettings({ ...props }: React.ComponentProps<typeof SidebarGroup>) {
+    return (
+        <SidebarGroup {...props}>
+            <SidebarGroupContent>
+                <SidebarMenu>
+                    <NavMenuItem>
+                        <NavMenuItem.Link href="#/settings">
+                            <LucideIcon.Settings />
+                            <NavMenuItem.Label>Settings</NavMenuItem.Label>
+                        </NavMenuItem.Link>
+                    </NavMenuItem>
+
+                    <NavMenuItem>
+                        <NavMenuItem.Link
+                            href="https://ghost.org/help?utm_source=admin&utm_campaign=help"
+                            target="_blank"
+                        >
+                            <LucideIcon.HelpCircle />
+                            <NavMenuItem.Label>Help</NavMenuItem.Label>
+                        </NavMenuItem.Link>
+                    </NavMenuItem>
+                </SidebarMenu>
+            </SidebarGroupContent>
+        </SidebarGroup>
+    );
+}
+
+export default NavSettings;
