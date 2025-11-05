d8c6cc95793dbb97778c7ad2564788c123c51260
Peter Zimon <peter.zimon@gmail.com>
2025-11-05 14:43:23 +0100

    Updated all mobile sidebar breakpoints

--- apps/shade/src/hooks/use-mobile.tsx
+++ apps/shade/src/hooks/use-mobile.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-const MOBILE_BREAKPOINT = 801;
+const MOBILE_BREAKPOINT = 800;
 
 export function useIsMobile() {
     const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
