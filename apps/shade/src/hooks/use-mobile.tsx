eca428cbdf25ad0588ea4be6e2d8f52f396e93c5
Peter Zimon <peter.zimon@gmail.com>
2025-11-05 14:44:46 +0100

    Updated hook mobile size

--- apps/shade/src/hooks/use-mobile.tsx
+++ apps/shade/src/hooks/use-mobile.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-const MOBILE_BREAKPOINT = 800;
+const MOBILE_BREAKPOINT = 801;
 
 export function useIsMobile() {
     const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
