e62650e9b9fb8daed57ec94e59c2adda4ba37c93
Troy Ciesco <tmciesco@gmail.com>
2025-11-06 07:21:57 -0500

    update schema hash

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '4e68fd06f63aab5fef99dddd8dc866bb';
+    const currentSchemaHash = '34938745eb81427846ddba92c31d1b16';
     const currentFixturesHash = '0877727032b8beddbaedc086a8acf1a2';
     const currentSettingsHash = 'bb8be7d83407f2b4fa2ad68c19610579';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
