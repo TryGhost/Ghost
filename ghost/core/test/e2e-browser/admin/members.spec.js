ac18264bb799bf8f792dec2f03fba11aafc57671
Igor Balos <ibalosh@gmail.com>
2025-11-06 17:28:18 +0100

    update Ember components to support new tests

--- ghost/core/test/e2e-browser/admin/members.spec.js
+++ ghost/core/test/e2e-browser/admin/members.spec.js
@@ -46,42 +46,6 @@ test.describe('Admin', () => {
             }
         ];
 
-        test('All members can be exported', async ({sharedPage}) => {
-            // adds 6 members, 3 with the same label
-            for (let member of membersFixture) {
-                await createMember(sharedPage, member);
-            }
-            await sharedPage.goto('/ghost');
-            await sharedPage.locator('.gh-nav a[href="#/members/"]').click();
-            await sharedPage.waitForSelector('button[data-test-button="members-actions"]');
-            await sharedPage.locator('button[data-test-button="members-actions"]').click();
-            await sharedPage.waitForSelector('button[data-test-button="export-members"]');
-            const [download] = await Promise.all([
-                sharedPage.waitForEvent('download'),
-                sharedPage.locator('button[data-test-button="export-members"]').click()
-            ]);
-            const filename = await download.suggestedFilename();
-            expect(filename).toContain('.csv');
-            const csv = await download.path();
-            let csvContents = await fs.readFileSync(csv).toString();
-            expect(csvContents).toMatch(/id,email,name,note,subscribed_to_emails,complimentary_plan,stripe_customer_id,created_at,deleted_at,labels,tiers/);
-            membersFixture.forEach((member) => {
-                expect(csvContents).toMatch(member.name);
-                expect(csvContents).toMatch(member.email);
-                expect(csvContents).toMatch(member.note);
-                expect(csvContents).toMatch(member.label);
-            });
-            // expect(csvContents).toMatch('Test Label'); we deleted the label in a previous test so it's not in this the export
-            const countIds = csvContents.match(/[a-z0-9]{24}/gm).length;
-            expect(countIds).toEqual(6);
-            const countTimestamps = csvContents.match(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/gm).length;
-            expect(countTimestamps).toEqual(6);
-            const countRows = csvContents.match(/(?:"(?:[^"]|"")*"|[^,\n]*)(?:,(?:"(?:[^"]|"")*"|[^,\n]*))*\n/g).length;
-            expect(countRows).toEqual(6);
-            const csvRegex = /^[^",]+((?<=[,\n])|(?=[,\n]))|[^",]+/gm;
-            expect(csvContents).toMatch(csvRegex);
-        });
-
         test('A filtered list of members can be exported', async ({sharedPage}) => {
             await sharedPage.goto('/ghost');
             await sharedPage.locator('.gh-nav a[href="#/members/"]').click();
