803de288702f28f72bed4069d128f325d9de4242
Peter Zimon <peter.zimon@gmail.com>
2025-11-06 13:39:13 +0100

    Updated layout

--- apps/admin-x-settings/src/components/settings/advanced/migrationtools/MigrationToolsExport.tsx
+++ apps/admin-x-settings/src/components/settings/advanced/migrationtools/MigrationToolsExport.tsx
@@ -34,7 +34,7 @@ const MigrationToolsExport: React.FC = () => {
     };
 
     return (
-        <div className='grid grid-cols-3 items-center gap-4 pt-4'>
+        <div className='grid grid-cols-1 gap-4 pt-4 md:grid-cols-2 lg:grid-cols-3'>
             <Button className='!h-9 !font-semibold' color='grey' icon='export' iconColorClass='!h-4 !w-auto' label='Content & settings' onClick={() => downloadAllContent()} />
             <Button className='!h-9 !font-semibold' color='grey' icon='baseline-chart' iconColorClass='!h-4 !w-auto' label='Post analytics' onClick={exportPosts} />
         </div>
