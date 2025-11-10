8bc68f4c6aba3c204ddbba148af9ae57466dcbed
Peter Zimon <peter.zimon@gmail.com>
2025-11-10 16:38:53 +0100

    Moved banner in AppContent

--- apps/admin/src/layout/app-sidebar/AppSidebarContent.tsx
+++ apps/admin/src/layout/app-sidebar/AppSidebarContent.tsx
@@ -1,4 +1,6 @@
 import {
+    Banner,
+    Button,
     SidebarContent,
 } from "@tryghost/shade"
 
@@ -8,6 +10,7 @@ import NavMain from "./NavMain";
 import NavContent from "./NavContent";
 import NavGhostPro from "./NavGhostPro";
 import NavSettings from "./NavSettings";
+import ghostProLogo from "@/assets/images/ghost-pro-logo.png";
 
 function AppSidebarContent() {
     return (
@@ -19,6 +22,16 @@ function AppSidebarContent() {
             </div>
             <div className="flex flex-col gap-2 sidebar:gap-4">
                 <WhatsNewBanner />
+                <Banner variant='gradient' size='lg' className="mx-5 my-2 flex flex-col items-stretch">
+                    <div>
+                        <img src={ghostProLogo} alt="Ghost Pro" className="max-h-[33px]" />
+                    </div>
+                    <div className="text-base mt-3 font-semibold">Unlock every feature</div>
+                    <div className="mt-2 text-gray-700 text-sm mb-4">
+                        Choose a plan to access the full power of Ghost right away, you have <span className="font-semibold text-black">3 days</span> free trial remaining.
+                    </div>
+                    <Button>Upgrade now</Button>
+                </Banner>
                 <NavSettings className="pb-0" />
             </div>
         </SidebarContent>
