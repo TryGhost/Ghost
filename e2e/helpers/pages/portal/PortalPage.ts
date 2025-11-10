4f357f1efb0f546c2d3f895fa67a6609f2aa5a9d
Igor Balos <ibalosh@gmail.com>
2025-11-10 11:03:17 +0100

    remove locator since it's not used yet

--- e2e/helpers/pages/portal/PortalPage.ts
+++ e2e/helpers/pages/portal/PortalPage.ts
@@ -5,7 +5,6 @@ export class PortalPage extends BasePage {
     readonly portalFrame: FrameLocator;
     private readonly frameSelector = '[data-testid="portal-popup-frame"]';
     readonly closeButton: Locator;
-    readonly contactSupportButton: Locator;
     readonly body: Locator;
 
     constructor(page: Page) {
@@ -13,15 +12,11 @@ export class PortalPage extends BasePage {
         this.portalFrame = page.frameLocator(this.frameSelector);
 
         this.closeButton = this.portalFrame.getByRole('button', {name: 'Close'});
-        this.contactSupportButton = this.portalFrame.getByRole('link', {name: 'Contact support'});
         this.body = this.portalFrame.locator('body');
     }
 
-    async closePortal(waitForClose = true): Promise<void> {
+    async closePortal(): Promise<void> {
         await this.closeButton.click();
-
-        if (waitForClose) {
-            await this.page.waitForSelector(this.frameSelector, {state: 'hidden', timeout: 2000});
-        }
+        await this.page.waitForSelector(this.frameSelector, {state: 'hidden', timeout: 2000});
     }
 }
