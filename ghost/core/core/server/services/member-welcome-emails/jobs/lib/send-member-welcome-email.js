83190d7ac01b0318e728ab3aaf777ac5aa490b4b
Troy Ciesco <tmciesco@gmail.com>
2025-11-11 16:35:15 -0500

    guard against missing to email

--- ghost/core/core/server/services/member-welcome-emails/jobs/lib/send-member-welcome-email.js
+++ ghost/core/core/server/services/member-welcome-emails/jobs/lib/send-member-welcome-email.js
@@ -1,4 +1,5 @@
 const logging = require('@tryghost/logging');
+const errors = require('@tryghost/errors');
 const {MEMBER_WELCOME_EMAIL_LOG_KEY} = require('./constants');
 const config = require('../../../../../shared/config');
 const renderWelcomeHtml = require('./email-templates/welcome.html');
@@ -24,7 +25,15 @@ async function sendMemberWelcomeEmail(payload, mailConfig) {
     const html = renderWelcomeHtml(templateData);
     const text = renderWelcomeText(templateData);
 
+    // This is for testing the functionality by sending to a shared inbox instead of the actual member inbox
+    // In the future this will check that payload.email exists instead
     const toEmail = config.get('memberWelcomeEmailTestInbox');
+    if (!toEmail) {
+        throw new errors.IncorrectUsageError({
+            message: 'memberWelcomeEmailTestInbox config is required but not defined'
+        });
+    }
+
     await mailConfig.mailer.send({
         to: toEmail,
         subject: `Welcome to ${mailConfig.siteSettings.title}!`,
