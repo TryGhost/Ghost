13a7ddada75558b693ce92d7e578299c8e932a9b
Igor Balos <ibalosh@gmail.com>
2025-11-11 13:20:37 +0100

    Moved member exports and member filtering flow (#25381)
    ref https://linear.app/ghost/issue/NY-733/port-members-creation-tests
    
    * Migrated member export flow from legacy browser tests to the new suite
    * Migrated member filtering flow
    * Improved factory with option to create and build many
    * Old tests are tightly coupled, dependent on each other, hard to maintain and read

--- e2e/data-factory/factory.ts
+++ e2e/data-factory/factory.ts
@@ -11,11 +11,28 @@ export abstract class Factory<TOptions extends Record<string, unknown> = Record<
 
     abstract build(options?: Partial<TOptions>): TResult;
 
+    buildMany(optionsList: Partial<TOptions>[]): TResult[] {
+        return optionsList.map(options => this.build(options));
+    }
+
     async create(options?: Partial<TOptions>): Promise<TResult> {
         if (!this.adapter) {
             throw new Error('Cannot create without a persistence adapter. Use build() for in-memory objects.');
         }
         const data = this.build(options);
         return await this.adapter.insert(this.entityType, data) as Promise<TResult>;
+    }
+
+    async createMany(optionsList: Partial<TOptions>[]): Promise<TResult[]> {
+        if (!this.adapter) {
+            throw new Error('Cannot create without a persistence adapter. Use buildMany() for in-memory objects.');
+        }
+
+        const results: TResult[] = [];
+        for (const options of optionsList) {
+            const result = await this.create(options);
+            results.push(result);
+        }
+        return results;
     }
 }
