7889b098898be876f2f868d1c067468a5eac610a
Troy Ciesco <tmciesco@gmail.com>
2025-11-13 09:25:54 -0500

    🐛 Fixed growth table loading state showing empty instead of skeleton (#25271)
    closes https://linear.app/ghost/issue/NY-706
    
    - Table loading state was being driven by the `isPageLoading` variable
    that comes from calling `useGrowthStats`, but the data for the growth
    table in particular (instead of the chart) comes from
    `useTopPostsStatsWithRange`
    - This PR updates the table loading state so that it's toggled whenever
    data is fetched for the table itself
    - Makes it so the header row doesn't disappear during loading
    - Adds a bit of padding above the skeleton for both posts/pages and
    source table.
    - Makes the table full width
    - Fixes an invalid DOM by putting the skeleton in valid table-related elements

--- apps/stats/src/views/Stats/Growth/components/GrowthSources.tsx
+++ apps/stats/src/views/Stats/Growth/components/GrowthSources.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import SortButton from '../../components/SortButton';
 import SourceIcon from '../../components/SourceIcon';
-import {Button, EmptyIndicator, LucideIcon, Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger, SkeletonTable, Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow, centsToDollars, formatNumber} from '@tryghost/shade';
+import {Button, EmptyIndicator, LucideIcon, Sheet, SheetContent, SheetDescription, SheetHeader, SheetTitle, SheetTrigger, Skeleton, Table, TableBody, TableCell, TableFooter, TableHead, TableHeader, TableRow, centsToDollars, formatNumber} from '@tryghost/shade';
 import {getFaviconDomain, getSymbol, useAppContext, useNavigate} from '@tryghost/admin-x-framework';
 import {getPeriodText} from '@src/utils/chart-helpers';
 import {useGlobalData} from '@src/providers/GlobalDataProvider';
@@ -188,7 +188,13 @@ export const GrowthSources: React.FC<GrowthSourcesProps> = ({
 
     if (isLoading) {
         return (
-            <SkeletonTable lines={5} />
+            <TableBody>
+                <TableRow className='last:border-none'>
+                    <TableCell className='border-none py-2' colSpan={1}>
+                        <Skeleton containerClassName='space-y-2' count={5} maxWidth={75} randomize />
+                    </TableCell>
+                </TableRow>
+            </TableBody>
         );
     }
 
