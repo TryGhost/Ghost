74e1bbfaa798fdeb84a1943ff830089b3b03e6d3
Rob Lester <rob@ghost.org>
2025-11-20 11:20:12 +0000

    Fixed race conditions in E2E tests with React shell
    ref https://linear.app/ghost/issue/BER-2920

--- e2e/helpers/pages/portal/portal-page.ts
+++ e2e/helpers/pages/portal/portal-page.ts
@@ -4,15 +4,21 @@ import {FrameLocator, Locator, Page} from '@playwright/test';
 export class PortalPage extends BasePage {
     readonly portalFrame: FrameLocator;
     private readonly frameSelector = '[data-testid="portal-popup-frame"]';
+    private readonly portalPopupFrame: Locator;
     readonly closeButton: Locator;
     readonly portalFrameBody: Locator;
 
     constructor(page: Page) {
         super(page);
         this.portalFrame = page.frameLocator(this.frameSelector);
+        this.portalPopupFrame = page.locator(this.frameSelector);
 
         this.closeButton = this.portalFrame.getByRole('button', {name: 'Close'});
         this.portalFrameBody = this.portalFrame.locator('body');
+    }
+
+    async waitForPortalToOpen(): Promise<void> {
+        await this.portalPopupFrame.waitFor({state: 'visible'});
     }
 
     async closePortal(): Promise<void> {
