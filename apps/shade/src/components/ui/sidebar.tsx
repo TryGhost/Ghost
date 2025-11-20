604bd3ab2237e630af9995798fdd927d236998c4
Kevin Ansfield <kevin@lookingsideways.co.uk>
2025-11-20 11:02:46 +0000

    Fixed sidebar not accounting for alerts height
    ref https://linear.app/ghost/issue/BER-2999/
    
    The fixed position of the sidebar means it doesn't adjust for content that is displayed before it, such as upgrade and error alerts, meaning the alert ends up obscuring sidebar.
    
    - updated sidebar styles to use `--sidebar-height-adjustment` when calculating position and height
    - added a wrapper around all alerts/banners and used `on-resize` modifier to set the `--sidebar-height-adjustment` variable based on the rendered alerts height

--- apps/shade/src/components/ui/sidebar.tsx
+++ apps/shade/src/components/ui/sidebar.tsx
@@ -233,7 +233,7 @@ const Sidebar = React.forwardRef<
                 />
                 <div
                     className={cn(
-                        'fixed inset-y-0 z-10 hidden h-svh w-[--sidebar-width] md:flex',
+                        'fixed inset-y-0 top-[var(--sidebar-height-adjustment,0)] z-10 hidden h-[calc(100svh_-_var(--sidebar-height-adjustment,0))] w-[--sidebar-width] md:flex',
                         side === 'left'
                             ? 'left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]'
                             : 'right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]',
