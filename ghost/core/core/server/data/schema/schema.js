a70e776a1c4b4067466a73aa5167337d1b38e1a5
Chris Raible <chris@ghost.org>
2025-11-19 17:17:28 -0800

    Added migration to create automated_emails table

--- ghost/core/core/server/data/schema/schema.js
+++ ghost/core/core/server/data/schema/schema.js
@@ -1131,5 +1131,17 @@ module.exports = {
         '@@INDEXES@@': [
             ['event_type', 'status', 'created_at']
         ]
+    },
+    automated_emails: {
+        id: {type: 'string', maxlength: 24, nullable: false, primary: true},
+        status: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'inactive'},
+        name: {type: 'string', maxlength: 191, nullable: false, unique: true},
+        subject: {type: 'string', maxlength: 300, nullable: false},
+        lexical: {type: 'text', maxlength: 1000000000, fieldtype: 'long', nullable: true},
+        sender_name: {type: 'string', maxlength: 191, nullable: true},
+        sender_email: {type: 'string', maxlength: 191, nullable: true},
+        sender_reply_to: {type: 'string', maxlength: 191, nullable: true},
+        created_at: {type: 'dateTime', nullable: false},
+        updated_at: {type: 'dateTime', nullable: true}
     }
 };
