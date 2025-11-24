71b2860137f6aa92670ba72f43695dce51a8e746
Igor Balos <ibalosh@gmail.com>
2025-11-24 14:42:02 +0100

    improve settings search test

--- e2e/tests/admin/settings/settings-search.test.ts
+++ e2e/tests/admin/settings/settings-search.test.ts
@@ -1,19 +1,32 @@
 import {SettingsPage} from '@/admin-pages';
 import {expect, test} from '@/helpers/playwright';
 
-test.describe('Ghost Admin - settings search - labs auto-open', () => {
-    test('display only Labs component and auto-open when searching for "lab"', async ({page}) => {
+test.describe('Ghost Admin - settings search sidebar', () => {
+    test('displays all section when not searching', async ({page}) => {
         const settingsPage = new SettingsPage(page);
         await settingsPage.goto();
 
         await expect(settingsPage.labsSection.section).toBeVisible();
         await expect(settingsPage.labsSection.openButton).toBeVisible();
+    });
 
+    test('displays only searched section', async ({page}) => {
+        const settingsPage = new SettingsPage(page);
+        await settingsPage.goto();
+        
         await settingsPage.searchByInput('lab');
-
         await expect(settingsPage.labsSidebarLink).toBeVisible();
         await expect(settingsPage.labsSection.section).toBeVisible();
         await expect(settingsPage.labsSection.closeButton).toBeVisible();
+
+        await settingsPage.searchByInput('staff');
+        await expect(settingsPage.staffSidebarLink).toBeVisible();
+        await expect(settingsPage.labsSection.section).toBeHidden();
+    });
+
+    test('displays all searched sections when no sections found', async ({page}) => {
+        const settingsPage = new SettingsPage(page);
+        await settingsPage.goto();
 
         await settingsPage.searchByInput('returnNoResultsInSearch');
         await expect(settingsPage.labsSidebarLink).toBeHidden();
