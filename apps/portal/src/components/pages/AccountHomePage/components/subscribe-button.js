42132c3259c1e7f03a3562607b5dafaa7763114d
Igor Balos <ibalosh@gmail.com>
2025-11-24 12:49:17 +0100

    Moved to kebab-case formatting portal (#25471)
    ref https://linear.app/ghost/issue/NY-722/migrate-ghost-codebase-to-kebab-case-file-naming-convention
    
    * Renamed all files to match to kebab-case format
    * Added ESLint kebab-case,
    * Separated tests from source code, including test utils, to be consistent with our other micro-apps (left only fixtures since they are dependency in src code)
    * Updated to match our working agreement on filenames in Ghost repo

---
+++ apps/portal/src/components/pages/AccountHomePage/components/subscribe-button.js
@@ -0,0 +1,33 @@
+import AppContext from '../../../../app-context';
+import ActionButton from '../../../common/action-button';
+import {isSignupAllowed, hasAvailablePrices} from '../../../../utils/helpers';
+import {useContext} from 'react';
+import {t} from '../../../../utils/i18n';
+
+const SubscribeButton = () => {
+    const {site, action, brandColor, doAction} = useContext(AppContext);
+
+    if (!isSignupAllowed({site}) || !hasAvailablePrices({site})) {
+        return null;
+    }
+    const isRunning = ['checkoutPlan:running'].includes(action);
+
+    const openPlanPage = () => {
+        doAction('switchPage', {
+            page: 'accountPlan',
+            lastPage: 'accountHome'
+        });
+    };
+    return (
+        <ActionButton
+            dataTestId={'view-plans'}
+            isRunning={isRunning}
+            label={t('View plans')}
+            onClick={() => openPlanPage()}
+            brandColor={brandColor}
+            style={{width: '100%'}}
+        />
+    );
+};
+
+export default SubscribeButton;
