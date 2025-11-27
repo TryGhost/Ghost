f809e55097aa112986fdb8ef476160c417fd12dc
Igor Balos <ibalosh@gmail.com>
2025-11-27 14:57:53 +0100

    Moved to kebab case formatting announcement bar (#25546)
    ref https://linear.app/ghost/issue/NY-722/migrate-ghost-codebase-to-kebab-case-file-naming-convention
    
    * Renamed all files to match to kebab-case format
    * Moved tests to match folder structure in other micro-aps
    * Added ESLint kebab-case rule
    * Updated `README` with the package release process 
    * Added to match our working agreement on filenames in Ghost repo

--- apps/announcement-bar/src/components/preview.js
+++ apps/announcement-bar/src/components/preview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {AnnouncementBar} from './AnnouncementBar';
+import {AnnouncementBar} from './announcement-bar';
 
 export function Preview({previewData}) {
     return (
