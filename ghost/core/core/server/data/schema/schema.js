7dd6a247438067fcc4ba258990b9c9ac415fe29f
Chris Raible <chris@ghost.org>
2025-12-02 12:17:56 -0800

    Added validations to status, sender_email and sender_reply_to fields

--- ghost/core/core/server/data/schema/schema.js
+++ ghost/core/core/server/data/schema/schema.js
@@ -1134,14 +1134,14 @@ module.exports = {
     },
     automated_emails: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
-        status: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'inactive'},
+        status: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'inactive', validations: {isIn: [['active', 'inactive']]}},
         name: {type: 'string', maxlength: 191, nullable: false, unique: true},
         slug: {type: 'string', maxlength: 191, nullable: false, unique: true},
         subject: {type: 'string', maxlength: 300, nullable: false},
         lexical: {type: 'text', maxlength: 1000000000, fieldtype: 'long', nullable: true},
         sender_name: {type: 'string', maxlength: 191, nullable: true},
-        sender_email: {type: 'string', maxlength: 191, nullable: true},
-        sender_reply_to: {type: 'string', maxlength: 191, nullable: true},
+        sender_email: {type: 'string', maxlength: 191, nullable: true, validations: {isEmail: true}},
+        sender_reply_to: {type: 'string', maxlength: 191, nullable: true, validations: {isEmail: true}},
         created_at: {type: 'dateTime', nullable: false},
         updated_at: {type: 'dateTime', nullable: true},
         '@@INDEXES@@': [
