d2767a8e07ce6e9bb719d14aa6fb68ee1499726c
Kevin Ansfield <kevin@lookingsideways.co.uk>
2025-12-03 19:19:31 +0000

    Fixed React admin layout to support alerts with reduced scroll containers
    ref https://linear.app/ghost/issue/BER-2999/
    
    Changed the React admin shell to use a flexbox layout that allows Ember alerts to push content down instead of overlaying it. Replaced viewport-based heights (h-svh) with flex-based heights (h-full) throughout the sidebar and layout components to create a proper flex hierarchy from body down to content. This ensures alerts take up space at the top, the main content flexes to fill remaining space, and scrolling works correctly at the SidebarInset level. Also fixed the mobile navbar losing vertical padding by adding flex-shrink-0 to prevent it from being compressed by the flex container.

--- apps/posts/src/components/layout/main-layout.tsx
+++ apps/posts/src/components/layout/main-layout.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const MainLayout: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({children, ...props}) => {
     return (
-        <div className='h-[calc(100svh-var(--mobile-navbar-height))] w-full overflow-y-scroll sidebar:h-screen'>
-            <div className='relative mx-auto flex h-screen max-w-page flex-col' {...props}>
+        <div className='h-full w-full'>
+            <div className='relative mx-auto flex h-full max-w-page flex-col' {...props}>
                 {children}
             </div>
         </div>
