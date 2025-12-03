5ee4606900da90f5ccdbe8d5843e1f7633fd7be8
Chris Raible <chris@ghost.org>
2025-12-03 11:51:49 -0800

    Added permissions for `automated_emails` (#25608)
    ref
    https://linear.app/ghost/issue/NY-772/add-automated-emails-crud-api-endpoints
    
    The new `automated_emails` model should only be accessible by
    Administrators+ in Ghost via API. This adds the appropriate permissions to
    Ghost's DB to enforce these permissions in the API endpoints, which will
    be included in a subsequent commit.

--- ghost/core/test/unit/server/data/schema/fixtures/fixture-manager.test.js
+++ ghost/core/test/unit/server/data/schema/fixtures/fixture-manager.test.js
@@ -409,7 +409,7 @@ describe('Migration Fixture Utils', function () {
             const rolesAllStub = sinon.stub(models.Role, 'findAll').returns(Promise.resolve(dataMethodStub));
 
             fixtureManager.addFixturesForRelation(fixtures.relations[0]).then(function (result) {
-                const FIXTURE_COUNT = 135;
+                const FIXTURE_COUNT = 137;
                 should.exist(result);
                 result.should.be.an.Object();
                 result.should.have.property('expected', FIXTURE_COUNT);
