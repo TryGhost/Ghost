70952a694cc6b60542ebf3cdaf91b49a8a498ffc
Maxine Athos <heyymax@icloud.com>
2025-12-03 15:36:28 -0300

    ✨ Added LOCATION_FILTER to members filter component
    - Introduced LOCATION_FILTER to the filter.js component for enhanced member filtering options.
    - Updated index.js to export LOCATION_FILTER, ensuring it is available for use in the application.

---
+++ ghost/admin/app/components/members/filters/location.js
@@ -0,0 +1,9 @@
+import {CONTAINS_RELATION_OPTIONS} from './relation-options';
+
+export const LOCATION_FILTER = {
+    label: 'Location',
+    name: 'geolocation',
+    valueType: 'string',
+    relationOptions: CONTAINS_RELATION_OPTIONS
+};
+
