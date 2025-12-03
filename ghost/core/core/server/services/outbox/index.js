c038269825a11ed3a489c423bcfcf36e93e2b756
Troy Ciesco <tmciesco@gmail.com>
2025-12-03 14:45:39 -0500

    cleanup

--- ghost/core/core/server/services/outbox/index.js
+++ ghost/core/core/server/services/outbox/index.js
@@ -3,10 +3,6 @@ const jobs = require('./jobs');
 const StartOutboxProcessingEvent = require('./events/StartOutboxProcessingEvent');
 const domainEvents = require('@tryghost/domain-events');
 const processOutbox = require('./jobs/lib/process-outbox');
-const {MemberCreatedEvent} = require('../../../shared/events');
-const config = require('../../../shared/config');
-const {WELCOME_EMAIL_SOURCES} = require('./jobs/lib/constants');
-
 class OutboxServiceWrapper {
     init() {
         if (this.initialized) {
@@ -18,19 +14,6 @@ class OutboxServiceWrapper {
         jobs.scheduleOutboxJob();
 
         domainEvents.subscribe(StartOutboxProcessingEvent, async () => {
-            await this.startProcessing();
-        });
-
-        domainEvents.subscribe(MemberCreatedEvent, async (event) => {
-            if (!config.get('memberWelcomeEmailTestInbox')) {
-                return;
-            }
-
-            if (!WELCOME_EMAIL_SOURCES.includes(event.data.source)) {
-                logging.info('member created but from api so skipping');
-                return;
-            }
-
             await this.startProcessing();
         });
 
