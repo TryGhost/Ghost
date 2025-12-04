5a19d3794349b7c4f118a325a36d949902394301
Troy Ciesco <tmciesco@gmail.com>
2025-12-04 08:57:40 -0500

    Updated member welcome email with automated_email content

--- ghost/core/core/server/services/member-welcome-emails/constants.js
+++ ghost/core/core/server/services/member-welcome-emails/constants.js
@@ -1,5 +1,12 @@
 const MEMBER_WELCOME_EMAIL_LOG_KEY = '[MEMBER-WELCOME-EMAIL]';
 
+const MESSAGES = {
+    NO_EMAIL_TEMPLATE: 'No email template found for member welcome email',
+    EMPTY_LEXICAL_CONTENT: 'Welcome email template has empty content. Please add content to the template in Settings.',
+    INVALID_LEXICAL_STRUCTURE: 'Welcome email template has invalid content structure'
+};
+
 module.exports = {
-    MEMBER_WELCOME_EMAIL_LOG_KEY
+    MEMBER_WELCOME_EMAIL_LOG_KEY,
+    MESSAGES
 };
