1e95da5af4fef1976f3af47dc3579c9d26d624b5
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2025-12-08 16:05:13 +0800

    Fixed linting

--- apps/activitypub/src/views/Feed/components/SuggestedProfiles.tsx
+++ apps/activitypub/src/views/Feed/components/SuggestedProfiles.tsx
@@ -2,7 +2,7 @@ import APAvatar from '@src/components/global/APAvatar';
 import FollowButton from '@src/components/global/FollowButton';
 import ProfilePreviewHoverCard from '@components/global/ProfilePreviewHoverCard';
 import {Account} from '@src/api/activitypub';
-import {Button, H4, LucideIcon, Separator, Skeleton, abbreviateNumber} from '@tryghost/shade';
+import {Button, H4, LucideIcon, Separator, Skeleton} from '@tryghost/shade';
 import {useEffect, useRef, useState} from 'react';
 import {useNavigateWithBasePath} from '@src/hooks/use-navigate-with-base-path';
 import {useSuggestedProfilesForUser} from '@src/hooks/use-activity-pub-queries';
