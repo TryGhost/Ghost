2e7bc66600cf363561d5fb86287c79fc40d55702
Igor Balos <ibalosh@gmail.com>
2025-12-17 13:29:38 +0100

    cleaned up member import service to use factory interfaces

--- e2e/data-factory/factories/member-factory.ts
+++ e2e/data-factory/factories/member-factory.ts
@@ -15,9 +15,9 @@ export interface Member {
     uuid: string;
     name: string | null;
     email: string;
-    note: string | null;
+    note?: string | null;
     geolocation: string | null;
-    labels: string[];
+    labels?: string[];
     email_count: number;
     email_opened_count: number;
     email_open_rate: number | null;
@@ -26,6 +26,10 @@ export interface Member {
     last_commented_at: Date | null;
     newsletters: string[];
     tiers?: Partial<Tier>[];
+    created_at?: string; // ISO 8601 format for backdating
+    complimentary_plan?: boolean;
+    stripe_customer_id?: string;
+    subscribed_to_emails?: string;
 }
 
 export class MemberFactory extends Factory<Partial<Member>, Member> {
@@ -57,7 +61,8 @@ export class MemberFactory extends Factory<Partial<Member>, Member> {
             status: 'free',
             last_seen_at: null,
             last_commented_at: null,
-            newsletters: []
+            newsletters: [],
+            subscribed_to_emails: 'false'
         };
     }
 }
