a983b203f6fcbd9733277cc14d09d82e99fdf0e3
Princi Vershwal <vershwal.princi@gmail.com>
2025-12-17 12:49:00 +0530

    Updated test to make lexical the primary test

--- ghost/core/test/e2e-api/admin/storage-adapter-switching.test.js
+++ ghost/core/test/e2e-api/admin/storage-adapter-switching.test.js
@@ -12,7 +12,7 @@ describe('Ghost Admin - Storage Adapter Switching', function () {
 
     before(async function () {
         agent = await agentProvider.getAdminAPIAgent();
-        await fixtureManager.init('posts', 'snippets', 'api_keys');
+        await fixtureManager.init('posts', 'snippets');
         await agent.loginAsOwner();
         siteUrl = config.get('url');
     });
@@ -23,51 +23,6 @@ describe('Ghost Admin - Storage Adapter Switching', function () {
 
     it('Can switch storage adapter multiple times without data loss', async function () {
         const newCdnUrl = 'https://cdn.example.com/c/site-uuid-2';
-        let res = await agent
-            .get('posts/slug/post-with-all-media-types-mobiledoc/?formats=mobiledoc')
-            .expectStatus(200);
-
-        let post = res.body.posts[0];
-        let mobiledoc = JSON.parse(post.mobiledoc);
-        mobiledoc.cards.find(c => c[0] === 'file')[1].src.should.equal(`${siteUrl}/content/files/document.pdf`);
-
-        urlUtilsHelper.stubUrlUtilsWithCdn({
-            assetBaseUrls: {media: cdnUrl, files: cdnUrl}
-        }, sinon);
-
-        res = await agent
-            .get('posts/slug/post-with-all-media-types-mobiledoc/?formats=mobiledoc')
-            .expectStatus(200);
-
-        post = res.body.posts[0];
-        mobiledoc = JSON.parse(post.mobiledoc);
-        mobiledoc.cards.find(c => c[0] === 'file')[1].src.should.equal(`${cdnUrl}/content/files/document.pdf`);
-
-        sinon.restore();
-
-        res = await agent
-            .get('posts/slug/post-with-all-media-types-mobiledoc/?formats=mobiledoc')
-            .expectStatus(200);
-
-        post = res.body.posts[0];
-        mobiledoc = JSON.parse(post.mobiledoc);
-        mobiledoc.cards.find(c => c[0] === 'file')[1].src.should.equal(`${siteUrl}/content/files/document.pdf`);
-
-        urlUtilsHelper.stubUrlUtilsWithCdn({
-            assetBaseUrls: {media: newCdnUrl, files: newCdnUrl}
-        }, sinon);
-
-        res = await agent
-            .get('posts/slug/post-with-all-media-types-mobiledoc/?formats=mobiledoc')
-            .expectStatus(200);
-
-        post = res.body.posts[0];
-        mobiledoc = JSON.parse(post.mobiledoc);
-        mobiledoc.cards.find(c => c[0] === 'file')[1].src.should.equal(`${newCdnUrl}/content/files/document.pdf`);
-        post.feature_image.should.equal(`${siteUrl}/content/images/feature.jpg`);
-    });
-
-    it('Lexical posts also switch URLs correctly', async function () {
         let res = await agent
             .get('posts/slug/post-with-all-media-types-lexical/?formats=lexical')
             .expectStatus(200);
@@ -89,6 +44,53 @@ describe('Ghost Admin - Storage Adapter Switching', function () {
         post.feature_image.should.equal(`${siteUrl}/content/images/feature.jpg`);
         post.lexical.should.containEql(`${cdnUrl}/content/files/document.pdf`);
         post.lexical.should.containEql(`${cdnUrl}/content/media/video.mp4`);
+
+        sinon.restore();
+
+        res = await agent
+            .get('posts/slug/post-with-all-media-types-lexical/?formats=lexical')
+            .expectStatus(200);
+
+        post = res.body.posts[0];
+        post.feature_image.should.equal(`${siteUrl}/content/images/feature.jpg`);
+        post.lexical.should.containEql(`${siteUrl}/content/files/document.pdf`);
+        post.lexical.should.containEql(`${siteUrl}/content/media/video.mp4`);
+
+        urlUtilsHelper.stubUrlUtilsWithCdn({
+            assetBaseUrls: {media: newCdnUrl, files: newCdnUrl}
+        }, sinon);
+
+        res = await agent
+            .get('posts/slug/post-with-all-media-types-lexical/?formats=lexical')
+            .expectStatus(200);
+
+        post = res.body.posts[0];
+        post.feature_image.should.equal(`${siteUrl}/content/images/feature.jpg`);
+        post.lexical.should.containEql(`${newCdnUrl}/content/files/document.pdf`);
+        post.lexical.should.containEql(`${newCdnUrl}/content/media/video.mp4`);
+    });
+
+    it('Mobiledoc posts also switch URLs correctly', async function () {
+        let res = await agent
+            .get('posts/slug/post-with-all-media-types-mobiledoc/?formats=mobiledoc')
+            .expectStatus(200);
+
+        let post = res.body.posts[0];
+        let mobiledoc = JSON.parse(post.mobiledoc);
+        mobiledoc.cards.find(c => c[0] === 'file')[1].src.should.equal(`${siteUrl}/content/files/document.pdf`);
+
+        urlUtilsHelper.stubUrlUtilsWithCdn({
+            assetBaseUrls: {media: cdnUrl, files: cdnUrl}
+        }, sinon);
+
+        res = await agent
+            .get('posts/slug/post-with-all-media-types-mobiledoc/?formats=mobiledoc')
+            .expectStatus(200);
+
+        post = res.body.posts[0];
+        mobiledoc = JSON.parse(post.mobiledoc);
+        mobiledoc.cards.find(c => c[0] === 'file')[1].src.should.equal(`${cdnUrl}/content/files/document.pdf`);
+        post.feature_image.should.equal(`${siteUrl}/content/images/feature.jpg`);
     });
 
     it('Snippets also switch URLs correctly', async function () {
