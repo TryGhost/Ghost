3b40a690baed61abccfd535e7573d4a8c29d0bd1
Igor Balos <ibalosh@gmail.com>
2025-12-17 16:25:57 +0100

    added comments sorting and show more for normal and reply comments

--- e2e/tests/public/comment-replies.test.ts
+++ e2e/tests/public/comment-replies.test.ts
@@ -8,23 +8,61 @@ import {
 } from '@/data-factory';
 import {PostPage} from '@/public-pages';
 import {SettingsService} from '@/helpers/services/settings/settings-service';
-import {expect, test} from '@/helpers/playwright';
+import {TiersService} from '@/helpers/services/tiers/tiers-service';
+import {expect, signInAsMember, test} from '@/helpers/playwright';
 
-test.describe('Ghost Public - Comments', () => {
+test.describe('Ghost Public - Comments - Replies', () => {
     let commentFactory: CommentFactory;
     let postFactory: PostFactory;
     let memberFactory: MemberFactory;
     let settingsService: SettingsService;
+    let tiersService: TiersService;
 
     test.beforeEach(async ({page}) => {
         postFactory = createPostFactory(page.request);
         memberFactory = createMemberFactory(page.request);
         commentFactory = createCommentFactory(page.request);
         settingsService = new SettingsService(page.request);
+        tiersService = new TiersService(page.request);
     });
 
     test.beforeEach(async () => {
         await settingsService.setCommentsEnabled('all');
+    });
+
+    test('reply to comment', async ({page}) => {
+        const post = await postFactory.create({status: 'published'});
+        const member = await memberFactory.create({status: 'free'});
+        const paidTier = await tiersService.getFirstPaidTier();
+        const paidMember = await memberFactory.create({status: 'comped', tiers: [{id: paidTier.id}]});
+
+        const comment = await commentFactory.create({
+            html: 'Main comment',
+            post_id: post.id,
+            member_id: member.id
+        });
+
+        await commentFactory.create({
+            html: 'Reply to main comment',
+            post_id: post.id,
+            member_id: paidMember.id,
+            parent_id: comment.id
+        });
+
+        await signInAsMember(page, paidMember);
+
+        const postPage = new PostPage(page);
+        await postPage.gotoPost(post.slug);
+        await postPage.comments.waitForCommentsToLoad();
+
+        await expect(postPage.comments.comments).toHaveCount(2);
+        await expect(postPage.comments.comments.first()).toContainText('Main comment');
+        await expect(postPage.comments.comments.last()).toContainText('Reply to main comment');
+
+        await postPage.comments.replyToComment('Main comment', 'Reply to main comment 2');
+        await expect(postPage.comments.comments).toHaveCount(3);
+        await expect(postPage.comments.comments.first()).toContainText('Main comment');
+        await expect(postPage.comments.comments.last()).toContainText('Reply to main comment 2');
     });
 
     test('show replies and load more replies', async ({page}) => {
@@ -52,10 +90,13 @@ test.describe('Ghost Public - Comments', () => {
         await postPage.gotoPost(post.slug);
         await postPage.comments.waitForCommentsToLoad();
 
+        await expect(postPage.comments.comments).toHaveCount(4);
         await expect(postPage.comments.comments.last()).toContainText('reply 3 to comment 1');
         await expect(postPage.comments.showMoreRepliesButton).toBeVisible();
         await expect(postPage.comments.showMoreRepliesButton).toContainText('Show 2 more replies');
+
         await postPage.comments.showMoreRepliesButton.click();
         await expect(postPage.comments.comments.last()).toContainText('reply 5 to comment 1');
+        await expect(postPage.comments.comments).toHaveCount(6);
     });
 });
