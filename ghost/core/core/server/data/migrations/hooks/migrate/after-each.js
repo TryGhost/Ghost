09731348df10316471699ce778d51cfbc3deaa21
Igor Balos <ibalosh@gmail.com>
2026-01-12 10:25:20 +0100

    - added comment to files that have lint filename rule disabled, to explain why - removed lint disable filename check comment from files that do not need it

--- ghost/core/core/server/data/migrations/hooks/migrate/after-each.js
+++ ghost/core/core/server/data/migrations/hooks/migrate/after-each.js
@@ -1,5 +1,3 @@
-/* eslint-disable ghost/filenames/match-regex */
-
 module.exports = function afterEach() {
     return Promise.resolve();
 };
--- ghost/core/core/server/data/migrations/hooks/migrate/before-each.js
+++ ghost/core/core/server/data/migrations/hooks/migrate/before-each.js
@@ -1,5 +1,3 @@
-/* eslint-disable ghost/filenames/match-regex */
-
 module.exports = function beforeEach() {
     return Promise.resolve();
 };
