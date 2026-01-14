31f743ed865f3ec0282ccd319b565e1d4e535530
Chris Raible <chris@ghost.org>
2026-01-14 14:59:46 -0800

    Improved types in koenig utils

--- apps/admin-x-framework/src/utils/koenig.ts
+++ apps/admin-x-framework/src/utils/koenig.ts
@@ -1,39 +1,37 @@
 // Type for the Koenig lexical fetcher function
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type FetchKoenigLexical = () => Promise<any>;
+export type FetchKoenigLexical<T = unknown> = () => Promise<T>;
 
 /**
  * Suspense-compatible resource loader for Koenig lexical editor.
  * Creates a resource that can be used with React Suspense to lazy-load Koenig.
  */
-export const loadKoenig = function (fetchKoenigLexical: FetchKoenigLexical) {
-    let status = 'pending';
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    let response: any;
+export const loadKoenig = function <T = unknown> (fetchKoenigLexical: FetchKoenigLexical<T>) {
+    let status: 'pending' | 'success' | 'error' = 'pending';
+    let response: T | Error;
 
     const suspender = fetchKoenigLexical().then(
         (res) => {
             status = 'success';
             response = res;
         },
-        (err) => {
+        (err: Error) => {
             status = 'error';
             response = err;
         }
     );
 
-    const read = () => {
+    const read = (): T => {
         switch (status) {
         case 'pending':
             throw suspender;
         case 'error':
             throw response;
         default:
-            return response;
+            return response as T;
         }
     };
 
     return {read};
 };
 
-export type EditorResource = ReturnType<typeof loadKoenig>;
+export type EditorResource<T = unknown> = ReturnType<typeof loadKoenig<T>>;
