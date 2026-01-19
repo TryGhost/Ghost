a55befa4056dc31e8a0a7d4d3ae37232c4561350
Evan Hahn <evan@ghost.org>
2026-01-19 14:57:48 -0600

    Converted config utils to TypeScript
    This change should have no user impact.

--- ghost/core/core/shared/config/utils.ts
+++ ghost/core/core/shared/config/utils.ts
@@ -1,6 +1,7 @@
-const path = require('path');
-const fs = require('fs');
-const jsonc = require('jsonc-parser');
+import * as path from 'node:path';
+import * as fs from 'node:fs';
+import * as jsonc from 'jsonc-parser';
+import type {IFormat as NconfFormat, Provider as NconfProvider} from 'nconf';
 
 /**
  * transform all relative paths to absolute paths
@@ -10,7 +11,7 @@ const jsonc = require('jsonc-parser');
  * Path must match minimum one / or \
  * Path can be a "." to re-present current folder
  */
-const makePathsAbsolute = function makePathsAbsolute(nconf, obj, parent) {
+export function makePathsAbsolute(nconf: NconfProvider, obj: object, parent: string) {
     Object.entries(obj).forEach(([pathsKey, configValue]) => {
         if (configValue && typeof configValue === 'object') {
             makePathsAbsolute(nconf, configValue, parent + ':' + pathsKey);
@@ -22,28 +23,28 @@ const makePathsAbsolute = function makePathsAbsolute(nconf, obj, parent) {
             nconf.set(parent + ':' + pathsKey, path.normalize(path.join(__dirname, '../../..', configValue)));
         }
     });
-};
+}
 
-const doesContentPathExist = function doesContentPathExist(contentPath) {
+export function doesContentPathExist(contentPath: string): void {
     if (!fs.existsSync(contentPath)) {
         // new Error is allowed here, as we do not want config to depend on @tryghost/error
         // @TODO: revisit this decision when @tryghost/error is no longer dependent on all of ghost-ignition
         // eslint-disable-next-line ghost/ghost-custom/no-native-error
         throw new Error('Your content path does not exist! Please double check `paths.contentPath` in your custom config file e.g. config.production.json.');
     }
-};
+}
 
 /**
 * Check if the URL in config has a protocol and sanitise it if not including a warning that it should be changed
 */
-const checkUrlProtocol = function checkUrlProtocol(url) {
+export function checkUrlProtocol(url: string): void {
     if (!url.match(/^https?:\/\//i)) {
         // new Error is allowed here, as we do not want config to depend on @tryghost/error
         // @TODO: revisit this decision when @tryghost/error is no longer dependent on all of ghost-ignition
         // eslint-disable-next-line ghost/ghost-custom/no-native-error
         throw new Error('URL in config must be provided with protocol, eg. "http://my-ghost-blog.com"');
     }
-};
+}
 
 /**
  * nconf merges all database keys together and this can be confusing
@@ -52,7 +53,7 @@ const checkUrlProtocol = function checkUrlProtocol(url) {
  * this.clear('key') does not work
  * https://github.com/indexzero/nconf/issues/235#issuecomment-257606507
  */
-const sanitizeDatabaseProperties = function sanitizeDatabaseProperties(nconf) {
+export function sanitizeDatabaseProperties(nconf: NconfProvider): void {
     if (nconf.get('database:client') === 'mysql') {
         nconf.set('database:client', 'mysql2');
     }
@@ -73,26 +74,17 @@ const sanitizeDatabaseProperties = function sanitizeDatabaseProperties(nconf) {
     if (nconf.get('database:client') === 'sqlite3') {
         makePathsAbsolute(nconf, nconf.get('database:connection'), 'database:connection');
     }
-};
+}
 
-const getNodeEnv = () => {
+export const getNodeEnv = (): string => {
     return process.env.NODE_ENV || 'development';
 };
 
-const jsoncFormat = {
+export const jsoncFormat: NconfFormat = {
     parse: function (text) {
         return jsonc.parse(text);
     },
     stringify: function (obj, replacer, spacing) {
         return JSON.stringify(obj, replacer, spacing);
     }
-};
-
-module.exports = {
-    makePathsAbsolute,
-    doesContentPathExist,
-    checkUrlProtocol,
-    sanitizeDatabaseProperties,
-    getNodeEnv,
-    jsoncFormat
 };
