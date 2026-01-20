4367d1a6417df330dfcf3f5e6ad58e13b761b004
Chris Raible <chris@ghost.org>
2026-01-20 14:44:15 -0800

    Added index and fixed nullable fields for automated_email_recipients

--- ghost/core/core/server/data/schema/schema.js
+++ ghost/core/core/server/data/schema/schema.js
@@ -1153,10 +1153,10 @@ module.exports = {
     automated_email_recipients: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
         automated_email_id: {type: 'string', maxlength: 24, nullable: false, references: 'automated_emails.id'},
-        member_id: {type: 'string', maxlength: 24, nullable: false},
+        member_id: {type: 'string', maxlength: 24, nullable: false, index: true},
         processed_at: {type: 'dateTime', nullable: false},
-        member_uuid: {type: 'string', maxlength: 36, nullable: true},
-        member_email: {type: 'string', maxlength: 191, nullable: true},
+        member_uuid: {type: 'string', maxlength: 36, nullable: false},
+        member_email: {type: 'string', maxlength: 191, nullable: false},
         member_name: {type: 'string', maxlength: 191, nullable: true}
     }
 };
