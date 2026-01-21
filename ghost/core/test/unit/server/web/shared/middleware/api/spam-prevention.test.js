e7e41248313d77f171eeabeea2f781278ef61c79
Evan Hahn <evan@ghost.org>
2026-01-21 11:19:36 -0600

    Removed Should.js from middleware tests
    This test-only change should have no user impact.
    
    We want to drop Should.js in favor of Node's assert and Sinon asserts.
    This does that for all of our middleware specs.

--- ghost/core/test/unit/server/web/shared/middleware/api/spam-prevention.test.js
+++ ghost/core/test/unit/server/web/shared/middleware/api/spam-prevention.test.js
@@ -1,9 +1,9 @@
-const should = require('should');
+const assert = require('node:assert/strict');
 const spamPrevention = require('../../../../../../../core/server/web/shared/middleware/api/spam-prevention');
 
 describe('Spam Prevention', function () {
     it('exports a contentApiKey method', function () {
-        should.equal(typeof spamPrevention.contentApiKey, 'function');
+        assert.equal(typeof spamPrevention.contentApiKey, 'function');
     });
 
     describe('contentApiKey method', function () {
@@ -11,7 +11,7 @@ describe('Spam Prevention', function () {
             const ExpressBrute = require('express-brute');
             const result = spamPrevention.contentApiKey();
 
-            should.equal(result instanceof ExpressBrute, true);
+            assert(result instanceof ExpressBrute);
         });
     });
 });
