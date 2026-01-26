433d19cf5827a8424f807c79ae86b0abb70c809e
Michael Barrett <mike@ghost.org>
2026-01-26 08:49:40 +0000

    Updated the offer section in the admin to only display signup offers (#25958)
    ref https://linear.app/ghost/issue/FEA-552
    
    Updated the offer section in the admin to only display signup offers to
    prevent other types of offers from showing in the UI until the UI has
    been updated to accomodate other offer types

--- apps/admin-x-settings/src/components/settings/growth/offers/offers-index.tsx
+++ apps/admin-x-settings/src/components/settings/growth/offers/offers-index.tsx
@@ -104,11 +104,12 @@ export const OffersIndexModal = () => {
     const {updateRoute} = useRouting();
     const {data: {offers: allOffers = []} = {}, isFetching: isFetchingOffers} = useBrowseOffers();
     const {data: {tiers: allTiers} = {}} = useBrowseTiers();
-    const activeOffers = allOffers.filter((offer) => {
+    const signupOffers = allOffers.filter(offer => offer.redemption_type === 'signup');
+    const activeOffers = signupOffers.filter((offer) => {
         const offerTier = allTiers?.find(tier => tier.id === offer?.tier.id);
         return (offer.status === 'active' && offerTier && offerTier.active === true);
     });
-    const archivedOffers = allOffers.filter((offer) => {
+    const archivedOffers = signupOffers.filter((offer) => {
         const offerTier = allTiers?.find(tier => tier.id === offer?.tier.id);
         return (offer.status === 'archived' || (offerTier && offerTier.active === false));
     });
@@ -132,7 +133,7 @@ export const OffersIndexModal = () => {
         updateRoute(`offers/edit/${id}`);
     };
 
-    const sortedOffers = allOffers
+    const sortedOffers = signupOffers
         .sort((offer1, offer2) => {
             const multiplier = sortDirection === 'desc' ? -1 : 1;
             switch (sortOption) {
