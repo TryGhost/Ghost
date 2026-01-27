PandaFisker/Ghost-kapi

Added ability to disable member commenting
ref https://linear.app/ghost/issue/BER-3184

Adds a commenting ability state to members (stored as JSON) and Admin API
endpoints for disabling/enabling commenting. When disabled, the member
cannot post, edit, like, or delete comments until re-enabled or expiry.

ghost/core/core/server/web/comments/routes.js
@@ -3,9 +3,29 @@ const config = require('../../../shared/config');
 const api = require('../../api').endpoints;
 const {http} = require('@tryghost/api-framework');
 const shared = require('../shared');
+const errors = require('@tryghost/errors');
+const tpl = require('@tryghost/tpl');
 
 const bodyParser = require('body-parser');
 const membersService = require('../../../server/services/members');
+
+const messages = {
+    memberCommentingDisabled: 'Your commenting ability has been disabled.'
+};
+
+/**
+ * Middleware to check if the member's commenting ability is disabled.
+ * Uses can_comment property already computed from the session data.
+ */
+function checkMemberCommenting(req, res, next) {
+    if (req.member && req.member.can_comment === false) {
+        return next(new errors.NoPermissionError({
+            message: tpl(messages.memberCommentingDisabled),
+            context: req.member.commenting?.disabled_reason
+        }));
+    }
+    next();
+}
 
 /**
  * @returns {import('express').Router}
@@ -29,14 +49,16 @@ module.exports = function apiRoutes() {
     router.get('/', http(api.commentsMembers.browse));
     router.get('/post/:post_id', http(api.commentsMembers.browse));
     router.get('/:id', http(api.commentsMembers.read));
-    router.post('/', http(api.commentsMembers.add));
-    router.put('/:id', http(api.commentsMembers.edit));
-    router.delete('/:id', http(api.commentsMembers.destroy));
-
-    router.post('/:id/like', http(api.commentsMembers.like));
-    router.delete('/:id/like', http(api.commentsMembers.unlike));
     router.get('/:id/replies', http(api.commentsMembers.replies));
 
+    // Write operations require member to have commenting ability enabled
+    router.post('/', checkMemberCommenting, http(api.commentsMembers.add));
+    router.put('/:id', checkMemberCommenting, http(api.commentsMembers.edit));
+    router.delete('/:id', checkMemberCommenting, http(api.commentsMembers.destroy));
+    router.post('/:id/like', checkMemberCommenting, http(api.commentsMembers.like));
+    router.delete('/:id/like', checkMemberCommenting, http(api.commentsMembers.unlike));
+
+    // Report is allowed even for members with commenting disabled (moderation action)
     router.post('/:id/report', http(api.commentsMembers.report));
 
     return router;

TryGhost/Ghost

ghost/core/core/server/web/comments/routes.js
@@ -3,9 +3,29 @@ const config = require('../../../shared/config');
 const api = require('../../api').endpoints;
 const {http} = require('@tryghost/api-framework');
 const shared = require('../shared');
+const errors = require('@tryghost/errors');
+const tpl = require('@tryghost/tpl');
 
 const bodyParser = require('body-parser');
 const membersService = require('../../../server/services/members');
+
+const messages = {
+    memberCommentingDisabled: 'Your commenting ability has been disabled.'
+};
+
+/**
+ * Middleware to check if the member's commenting ability is disabled.
+ * Uses can_comment property already computed from the session data.
+ */
+function checkMemberCommenting(req, res, next) {
+    if (req.member && req.member.can_comment === false) {
+        return next(new errors.NoPermissionError({
+            message: tpl(messages.memberCommentingDisabled),
+            context: req.member.commenting?.disabledReason
+        }));
+    }
+    next();
+}
 
 /**
  * @returns {import('express').Router}
@@ -29,14 +49,16 @@ module.exports = function apiRoutes() {
     router.get('/', http(api.commentsMembers.browse));
     router.get('/post/:post_id', http(api.commentsMembers.browse));
     router.get('/:id', http(api.commentsMembers.read));
-    router.post('/', http(api.commentsMembers.add));
-    router.put('/:id', http(api.commentsMembers.edit));
-    router.delete('/:id', http(api.commentsMembers.destroy));
-
-    router.post('/:id/like', http(api.commentsMembers.like));
-    router.delete('/:id/like', http(api.commentsMembers.unlike));
     router.get('/:id/replies', http(api.commentsMembers.replies));
 
+    // Write operations require member to have commenting ability enabled
+    router.post('/', checkMemberCommenting, http(api.commentsMembers.add));
+    router.put('/:id', checkMemberCommenting, http(api.commentsMembers.edit));
+    router.delete('/:id', checkMemberCommenting, http(api.commentsMembers.destroy));
+    router.post('/:id/like', checkMemberCommenting, http(api.commentsMembers.like));
+    router.delete('/:id/like', checkMemberCommenting, http(api.commentsMembers.unlike));
+
+    // Report is allowed even for members with commenting disabled (moderation action)
     router.post('/:id/report', http(api.commentsMembers.report));
 
     return router;