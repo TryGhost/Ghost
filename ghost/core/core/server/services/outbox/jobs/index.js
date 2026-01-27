4300df447e74e578494ac5603778f67991fae830
Chris Raible <chris@ghost.org>
2026-01-27 12:53:11 -0800

    Removed memberWelcomeEmailSendInstantly configuration parameter

--- ghost/core/core/server/services/outbox/jobs/index.js
+++ ghost/core/core/server/services/outbox/jobs/index.js
@@ -12,15 +12,8 @@ module.exports = {
             return false;
         }
 
-        const configValue = config.get('memberWelcomeEmailSendInstantly');
-        const testEmailSendInstantly = configValue === true || configValue === 'true';
-
-        // use a random seconds value to avoid spikes to the database on the minute
-        const s = Math.floor(Math.random() * 60); // 0-59
-        // run every 5 minutes, on 1,6,11..., 2,7,12..., 3,8,13..., etc
-        const m = Math.floor(Math.random() * 5); // 0-4
-
-        const cronSchedule = testEmailSendInstantly ? '*/3 * * * * *' : `${s} ${m}/5 * * * *`;
+        // Run every 3 seconds for instant welcome email delivery
+        const cronSchedule = '*/3 * * * * *';
 
         jobsService.addJob({
             at: cronSchedule,
