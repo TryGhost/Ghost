68f9d21ed04fa7d689d52f933cd7ae0781f645d0
Kevin Ansfield <kevin@ghost.org>
2026-01-28 09:13:30 +0000

    🚢 Updated shipping flow to publish from CI (TryGhost/Koenig#1715)
    no issue
    
    - changed `yarn ship` command to only run `lerna version` which runs tests, prompts for updated versions, and pushes a commit with package.json updates
    - added `publish` Github Actions workflow that detects the lerna versions commit on `main` and performs the publish-to-npm steps
      - includes the Sentry version bump, removing the need to have the auth token configured locally
      - uses npm's OIDC trusted publisher flow, all packages on npm must be set up to trust this repo and action

--- koenig/koenig-lexical/check-publish-env.js
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config';
-
-if (!process.env.VITE_SENTRY_AUTH_TOKEN) {
-    throw new Error('VITE_SENTRY_AUTH_TOKEN is not set in your .env file - this is required when publishing');
-}
