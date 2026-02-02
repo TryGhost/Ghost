69eeace36228ac485544487fce35c04ee53f4b7d
Evan Hahn <evan@ghost.org>
2026-02-02 10:43:09 -0600

    Fix i18n

--- apps/portal/src/components/common/sniper-link-button.js
+++ apps/portal/src/components/common/sniper-link-button.js
@@ -1,6 +1,7 @@
 import {ReactComponent as EnvelopeIcon} from '../../images/icons/envelope.svg';
 import {isAndroidChrome} from '../../utils/is-android-chrome';
 import {getOwn} from '../../utils/get-own';
+import {t} from '../../utils/i18n';
 
 const gmailIcon = (
     <svg height="24" width="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 49.4 512 399.4200000000001"><g fill="none" fillRule="evenodd"><g fillRule="nonzero"><path d="M34.91 448.818h81.454V251L0 163.727V413.91c0 19.287 15.622 34.91 34.91 34.91z" fill="#4285f4"/><path d="M395.636 448.818h81.455c19.287 0 34.909-15.622 34.909-34.909V163.727L395.636 251z" fill="#34a853"/><path d="M395.636 99.727V251L512 163.727v-46.545c0-43.142-49.25-67.782-83.782-41.891z" fill="#fbbc04"/></g><path d="M116.364 251V99.727L256 204.455 395.636 99.727V251L256 355.727z" fill="#ea4335"/><path d="M0 117.182v46.545L116.364 251V99.727L83.782 75.291C49.25 49.4 0 74.04 0 117.18z" fill="#c5221f" fillRule="nonzero"/></g></svg>
@@ -36,14 +37,14 @@ const PROVIDER_ICONS = {
 };
 
 const PROVIDER_LABELS = {
-    gmail: 'Open Gmail',
-    outlook: 'Open Outlook',
-    yahoo: 'Open Yahoo Mail',
-    proton: 'Open Proton Mail',
-    icloud: 'Open iCloud Mail',
-    hey: 'Open Hey',
-    aol: 'Open AOL Mail',
-    mailru: 'Open Mail.ru'
+    gmail: t('Open Gmail'),
+    outlook: t('Open Outlook'),
+    yahoo: t('Open Yahoo Mail'),
+    proton: t('Open Proton Mail'),
+    icloud: t('Open iCloud Mail'),
+    hey: t('Open Hey'),
+    aol: t('Open AOL Mail'),
+    mailru: t('Open Mail.ru')
 };
 
 /**
@@ -64,7 +65,7 @@ function SniperLinkButton({
             className='gh-portal-btn gh-portal-btn-sniper-link'
         >
             {getOwn(PROVIDER_ICONS, provider) ?? defaultEmailIcon}
-            <span>{getOwn(PROVIDER_LABELS, provider) ?? 'Open email'}</span>
+            <span>{getOwn(PROVIDER_LABELS, provider) ?? t('Open email')}</span>
         </a>
     );
 }
