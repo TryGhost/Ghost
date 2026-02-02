887ab3566570377e370e2fcdff33a6ba9c7f7e9e
Steve Larson <9larsons@gmail.com>
2026-02-02 08:26:19 -0600

    Minor changes, safeguards

--- ghost/core/core/server/services/koenig/node-renderers/transistor-renderer.js
+++ ghost/core/core/server/services/koenig/node-renderers/transistor-renderer.js
@@ -23,27 +23,23 @@ function frontendTemplate(node, document, options) {
         return null;
     }
 
-    // Build embed URL with member UUID
-    const baseUrl = `https://partner.transistor.fm/ghost/embed/${memberUuid}`;
-    const params = new URLSearchParams();
+    const embedUrl = new URL(`https://partner.transistor.fm/ghost/embed/${memberUuid}`);
 
     if (node.accentColor) {
-        params.set('color', node.accentColor.replace(/^#/, ''));
+        embedUrl.searchParams.set('color', node.accentColor.replace(/^#/, ''));
     }
     if (node.backgroundColor) {
-        params.set('background', node.backgroundColor.replace(/^#/, ''));
+        embedUrl.searchParams.set('background', node.backgroundColor.replace(/^#/, ''));
     }
-
-    const queryString = params.toString();
-    const embedUrl = queryString ? `${baseUrl}?${queryString}` : baseUrl;
 
     const iframe = document.createElement('iframe');
     iframe.setAttribute('width', '100%');
     iframe.setAttribute('height', '400');
+    iframe.setAttribute('title', 'Transistor podcasts');
     iframe.setAttribute('frameborder', 'no');
     iframe.setAttribute('scrolling', 'no');
     iframe.setAttribute('seamless', '');
-    iframe.setAttribute('src', embedUrl);
+    iframe.setAttribute('src', embedUrl.toString());
     iframe.setAttribute('data-kg-transistor-embed', '');
 
     figure.appendChild(iframe);
@@ -52,7 +48,7 @@ function frontendTemplate(node, document, options) {
 }
 
 function emailTemplate(node, document, options) {
-    const accentColor = node.accentColor;
+    const accentColor = node.accentColor || '#15171A';
     const transistorLogo = 'data:image/svg+xml,' + encodeURIComponent(`<svg viewBox="5 0.5 144 144" xmlns="http://www.w3.org/2000/svg"><g fill="${accentColor}"><path d="M77 120.3c-2.6 0-4.8-2.1-4.8-4.8V29.4c0-2.6 2.1-4.8 4.8-4.8s4.8 2.1 4.8 4.8v86.2c0 2.6-2.2 4.7-4.8 4.7z"/><path d="M57 77.3H34c-2.6 0-4.8-2.1-4.8-4.8 0-2.6 2.1-4.8 4.8-4.8h23c2.6 0 4.8 2.1 4.8 4.8 0 2.6-2.1 4.8-4.8 4.8z"/><path d="M120.1 77.3h-23c-2.6 0-4.8-2.1-4.8-4.8 0-2.6 2.1-4.8 4.8-4.8h23c2.6 0 4.8 2.1 4.8 4.8 0 2.6-2.2 4.8-4.8 4.8z"/><path d="M77 144.5c-39.7 0-72-32.3-72-72s32.3-72 72-72 72 32.3 72 72-32.3 72-72 72zM77 10c-34.4 0-62.4 28-62.4 62.4 0 34.4 28 62.4 62.4 62.4 34.4 0 62.4-28 62.4-62.4C139.4 38 111.4 10 77 10z"/></g></svg>`);
 
     // Use {uuid} replacement string - wrapReplacementStrings converts this to %%{uuid}%%
