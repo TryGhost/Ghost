20063669870d67cc66585274cfb1243bb2b53092
Sam Lord <hello@samlord.me>
2026-02-02 13:28:32 +0000

    Revert "Switched back to using card checkout flow (#26039)"
    This reverts commit 0a94c3df12ac30359541c48a322dfff5639a6bd9.

--- apps/portal/src/actions.js
+++ apps/portal/src/actions.js
@@ -335,20 +335,6 @@ async function applyOffer({data, state, api}) {
     }
 }
 
-async function editBilling({data, state, api}) {
-    try {
-        await api.member.editBilling(data);
-    } catch (e) {
-        return {
-            action: 'editBilling:failed',
-            popupNotification: createPopupNotification({
-                type: 'editBilling:failed', autoHide: false, closeable: true, state, status: 'error',
-                message: t('Failed to update billing information, please try again')
-            })
-        };
-    }
-}
-
 async function manageBilling({data, state, api}) {
     try {
         await api.member.manageBilling(data);
@@ -671,7 +657,6 @@ const Actions = {
     updateProfile,
     refreshMemberData,
     clearPopupNotification,
-    editBilling,
     manageBilling,
     checkoutPlan,
     updateNewsletterPreference,
