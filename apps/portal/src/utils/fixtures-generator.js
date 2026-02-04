6424fd1086098204be1ef777c55e75c92d2f7d06
Steve Larson <9larsons@gmail.com>
2026-02-04 17:38:50 -0600

    Fixed naming

--- apps/portal/src/utils/fixtures-generator.js
+++ apps/portal/src/utils/fixtures-generator.js
@@ -13,7 +13,7 @@ export function objectId() {
     }).toLowerCase();
 }
 
-export function uuid() {
+export function generateUuid() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         const r = Math.random() * 16 | 0;
         const v = c === 'x' ? r : (r & 0x3 | 0x8);
@@ -171,7 +171,7 @@ export function getMemberData({
     newsletters = []
 } = {}) {
     return {
-        uuid: uuid(),
+        uuid: generateUuid(),
         email,
         name,
         firstname,
