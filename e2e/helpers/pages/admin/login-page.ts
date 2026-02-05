0221af33699a9f9956c25c07daf61ea174233f1b
Evan Hahn <evan@ghost.org>
2026-02-05 13:22:35 -0600

    Cleaned up login page loading in E2E tests (#26251)
    towards https://linear.app/ghost/issue/NY-1000
    
    *This test-only change should have no user impact.*
    
    This change:
    
    - Logs a more helpful error when visiting the signup page. This seems to
    be failing in CI a lot, and it'd be useful to know more about the error.
    
    - Removes an unnecessary `waitFor` on the email address field.
    `locator.fill()` [automatically waits for this][0].
    
    - Stops an unnecessary check when going to the sign-in page. A user
    should already be available, and the extra wait will probably never
    happen--we visit a URL and then check that we're not at a different URL,
    which it will never be. (And if it *did* redirect, we wouldn't catch it
    this way.)
    
    [0]: https://playwright.dev/docs/actionability

--- e2e/helpers/pages/admin/login-page.ts
+++ e2e/helpers/pages/admin/login-page.ts
@@ -8,8 +8,6 @@ export class LoginPage extends AdminPage {
     readonly forgotButton: Locator;
     readonly passwordResetSuccessMessage: Locator;
 
-    private setupNewUserUrl = 'setup';
-
     constructor(page: Page) {
         super(page);
         this.pageUrl = '/ghost/#/signin';
@@ -22,8 +20,6 @@ export class LoginPage extends AdminPage {
     };
 
     async signIn(email: string, password: string) {
-        await this.emailAddressField.waitFor({state: 'visible'});
-
         await this.emailAddressField.fill(email);
         await this.passwordField.fill(password);
         await this.signInButton.click();
@@ -40,24 +36,12 @@ export class LoginPage extends AdminPage {
     }
 
     async waitForLoginPageAfterUserCreated(): Promise<void> {
-        let counter = 0;
-
-        while (counter < 5) {
-            await this.goto();
-
-            try {
-                await this.page.waitForURL(
-                    url => !url.href.includes(this.setupNewUserUrl),
-                    {timeout: 1000}
-                );
-
-                break;
-            } catch (error) {
-                counter += 1;
-                if (counter >= 5) {
-                    throw error;
-                }
-            }
+        const response = await this.goto();
+        if (!response) {
+            throw new Error('Error going to signin page (no response)');
+        }
+        if (!response.ok) {
+            throw new Error(`Error going to signin page (${response.status})`);
         }
     }
 }
