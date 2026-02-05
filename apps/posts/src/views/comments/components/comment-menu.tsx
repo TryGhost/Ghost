32fb013c4c4760b1a75e99a918b8763b7419b004
Weyland Swart <49831538+weylandswart@users.noreply.github.com>
2026-02-05 09:06:41 +0000

    Tweaked styles for comment moderation (#26259)
    No ref
    
    Tightened up spacing and alignment for disabling/enabling commenting.

--- apps/posts/src/views/comments/components/comment-menu.tsx
+++ apps/posts/src/views/comments/components/comment-menu.tsx
@@ -63,14 +63,14 @@ export function CommentMenu({
                         commentPermalinksEnabled ? (
                             <DropdownMenuItem asChild>
                                 <a href={`${postUrl}#ghost-comments-${commentId}`} rel="noopener noreferrer" target="_blank">
-                                    <LucideIcon.ExternalLink className="mr-2 size-4" />
+                                    <LucideIcon.ExternalLink className="size-4" />
                                     View on post
                                 </a>
                             </DropdownMenuItem>
                         ) : (
                             <DropdownMenuItem asChild>
                                 <a href={postUrl} rel="noopener noreferrer" target="_blank">
-                                    <LucideIcon.ExternalLink className="mr-2 size-4" />
+                                    <LucideIcon.ExternalLink className="size-4" />
                                     View post
                                 </a>
                             </DropdownMenuItem>
@@ -79,7 +79,7 @@ export function CommentMenu({
                     {memberId && (
                         <DropdownMenuItem asChild>
                             <a href={`#/members/${memberId}`}>
-                                <LucideIcon.User className="mr-2 size-4" />
+                                <LucideIcon.User className="size-4" />
                                 View member
                             </a>
                         </DropdownMenuItem>
@@ -88,12 +88,12 @@ export function CommentMenu({
                     {memberId && (
                         canComment !== false ? (
                             <DropdownMenuItem onClick={() => setDisableDialogOpen(true)}>
-                                <LucideIcon.MessageCircleOff className="mr-2 size-4" />
+                                <LucideIcon.MessageCircleOff className="size-4" />
                                 Disable commenting
                             </DropdownMenuItem>
                         ) : (
                             <DropdownMenuItem onClick={handleEnableCommenting}>
-                                <LucideIcon.MessageCircle className="mr-2 size-4" />
+                                <LucideIcon.MessageCircle className="size-4" />
                                 Enable commenting
                             </DropdownMenuItem>
                         )
