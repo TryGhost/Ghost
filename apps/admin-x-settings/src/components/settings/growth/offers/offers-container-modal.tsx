9dbb07ce66b6a8139e7c10badee2ad7722747bdc
Sodbileg Gansukh <sodbileg.gansukh@gmail.com>
2026-02-10 19:33:20 +0800

    Added routing for retention offer modals

--- apps/admin-x-settings/src/components/settings/growth/offers/offers-container-modal.tsx
+++ apps/admin-x-settings/src/components/settings/growth/offers/offers-container-modal.tsx
@@ -1,5 +1,6 @@
 import AddOfferModal from './add-offer-modal';
 import EditOfferModal from './edit-offer-modal';
+import EditRetentionOfferModal from './edit-retention-offer-modal';
 import NiceModal from '@ebay/nice-modal-react';
 import OfferSuccess from './offer-success';
 import {OffersIndexModal} from './offers-index';
@@ -12,6 +13,11 @@ type OffersRouteHandlerProps = {
 const OffersRouteHandler: React.FC<OffersRouteHandlerProps> = ({route}) => {
     if (route === 'offers/new') {
         return <AddOfferModal />;
+    } else if (route.startsWith('offers/edit/retention/') && route.length > 'offers/edit/retention/'.length) {
+        const retentionId = route.split('/').pop();
+        return <EditRetentionOfferModal id={retentionId ? retentionId : ''} />;
+    } else if (route === 'offers/edit/retention') {
+        return <OffersIndexModal defaultTab='retention' />;
     } else if (route.startsWith('offers/edit/') && route.length > 'offers/edit/'.length) {
         const offerId = route.split('/').pop();
         return <EditOfferModal id={offerId ? offerId : ''} />;
