61a2483ceea570ac8f6701b2c57b0d44b6fcdfce
Chris Raible <chris@ghost.org>
2026-02-10 19:00:48 -0800

    ✨ Added welcome emails for free and paid members (#26077)
    ref
    https://linear.app/ghost/issue/NY-969/promote-welcomeemails-labs-flag-to-ga-features-in-ghost-backend
    
    We've been iterating on member welcome emails behind a feature flag for
    a while now, and we're excited to release this in GA for everyone to try
    out.

--- ghost/core/core/shared/labs.js
+++ ghost/core/core/shared/labs.js
@@ -28,7 +28,8 @@ const GA_FEATURES = [
     'inboxlinks',
     'commentModeration',
     'commentPermalinks',
-    'featurebaseFeedback'
+    'featurebaseFeedback',
+    'welcomeEmails'
 ];
 
 // These features are considered publicly available and can be enabled/disabled by users
@@ -49,7 +50,6 @@ const PRIVATE_FEATURES = [
     'emailCustomization',
     'tagsX',
     'emailUniqueid',
-    'welcomeEmails',
     'themeTranslation',
     'indexnow',
     'transistor'
