b012384bfcdf2ee9e2952f6441c3d1b56e393b7e
Evan Hahn <evan@ghost.org>
2026-03-02 10:52:28 -0600

    ✨ Added more card types to the welcome email editor, like images
    closes https://linear.app/ghost/issue/NY-1104
    
    Moved the `welcomeEmailEditor` feature flag to GA.

--- ghost/core/core/shared/labs.js
+++ ghost/core/core/shared/labs.js
@@ -25,7 +25,8 @@ const GA_FEATURES = [
     'customFonts',
     'explore',
     'commentModeration',
-    'featurebaseFeedback'
+    'featurebaseFeedback',
+    'welcomeEmailEditor'
 ];
 
 // These features are considered publicly available and can be enabled/disabled by users
@@ -49,7 +50,6 @@ const PRIVATE_FEATURES = [
     'indexnow',
     'transistor',
     'retentionOffers',
-    'welcomeEmailEditor',
     'membersForward',
     'welcomeEmailsDesignCustomization'
 ];
