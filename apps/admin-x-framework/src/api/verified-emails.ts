48899ea6a4940df4e6fe202e34a2af3ebe50e439
Chris Raible <chris@ghost.org>
2026-03-02 15:45:48 -0800

    Added frontend API hooks for verified emails

---
+++ apps/admin-x-framework/src/api/verified-emails.ts
@@ -0,0 +1,68 @@
+import {createMutation, createQuery} from '../utils/api/hooks';
+
+export type VerifiedEmail = {
+    id: string;
+    email: string;
+    status: 'pending' | 'verified';
+    created_at: string;
+    updated_at: string | null;
+};
+
+export interface VerifiedEmailsResponseType {
+    verified_emails: VerifiedEmail[];
+}
+
+export interface VerifiedEmailsVerifyResponseType {
+    email: string;
+    context?: {
+        type: string;
+        id?: string;
+        property?: string;
+        key?: string;
+    };
+}
+
+export interface VerifiedEmailsAddResponseType {
+    verified: boolean;
+    pending: boolean;
+    email: string;
+}
+
+const dataType = 'VerifiedEmailsResponseType';
+
+export const useBrowseVerifiedEmails = createQuery<VerifiedEmailsResponseType>({
+    dataType,
+    path: '/verified-emails/'
+});
+
+export const useAddVerifiedEmail = createMutation<VerifiedEmailsAddResponseType, {email: string; context?: {type: string; id?: string; property?: string; key?: string}}>({
+    method: 'POST',
+    path: () => '/verified-emails/',
+    body: ({email, context}) => ({email, context}),
+    updateQueries: {
+        dataType,
+        emberUpdateType: 'createOrUpdate',
+        update: (newData) => newData
+    }
+});
+
+export const useVerifyVerifiedEmail = createMutation<VerifiedEmailsVerifyResponseType, {token: string}>({
+    method: 'PUT',
+    path: () => '/verified-emails/verify/',
+    body: ({token}) => ({token}),
+    updateQueries: {
+        dataType,
+        emberUpdateType: 'createOrUpdate',
+        update: (newData) => newData
+    }
+});
+
+export const useDeleteVerifiedEmail = createMutation<unknown, string>({
+    method: 'DELETE',
+    path: (id) => `/verified-emails/${id}/`,
+    updateQueries: {
+        dataType,
+        emberUpdateType: 'delete',
+        update: (newData) => newData
+    }
+});
