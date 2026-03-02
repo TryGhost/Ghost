b5505dde13ae80d110499744995b772388cf1e32
Steve Larson <9larsons@gmail.com>
2026-03-02 17:01:37 -0600

    Refactor for better types/no casting

--- apps/admin-x-settings/src/hooks/use-welcome-email-link-suggestions.ts
+++ apps/admin-x-settings/src/hooks/use-welcome-email-link-suggestions.ts
@@ -1,14 +1,12 @@
-import {getSettingValues} from '@tryghost/admin-x-framework/api/settings';
+import {getSettingValue} from '@tryghost/admin-x-framework/api/settings';
 import {useGlobalData} from '../components/providers/global-data-provider';
 import {useKoenigLinkSuggestions} from '@tryghost/admin-x-framework';
 
 export const useWelcomeEmailLinkSuggestions = () => {
     const {settings, siteData} = useGlobalData();
-    const [membersSignupAccess, donationsEnabled, recommendationsEnabled] = getSettingValues(settings, [
-        'members_signup_access',
-        'donations_enabled',
-        'recommendations_enabled'
-    ]) as [string, boolean, boolean];
+    const membersSignupAccess = getSettingValue<string>(settings, 'members_signup_access') ?? 'none';
+    const donationsEnabled = getSettingValue<boolean>(settings, 'donations_enabled') ?? false;
+    const recommendationsEnabled = getSettingValue<boolean>(settings, 'recommendations_enabled') ?? false;
 
     return useKoenigLinkSuggestions({
         siteUrl: siteData.url,
