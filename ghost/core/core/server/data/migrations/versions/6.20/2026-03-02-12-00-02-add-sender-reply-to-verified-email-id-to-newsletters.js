0209d65fcaca165b59eb14f2d2cf9b75e0659656
Chris Raible <chris@ghost.org>
2026-03-02 15:17:41 -0800

    Added verified email FK columns to newsletters table

---
+++ ghost/core/core/server/data/migrations/versions/6.20/2026-03-02-12-00-02-add-sender-reply-to-verified-email-id-to-newsletters.js
@@ -0,0 +1,5 @@
+const {createAddColumnMigration} = require('../../utils');
+
+module.exports = createAddColumnMigration('newsletters', 'sender_reply_to_verified_email_id', {
+    type: 'string', maxlength: 24, nullable: true, references: 'verified_emails.id', setNullDelete: true
+});
