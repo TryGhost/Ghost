fb7bfca7d5ef5b34248e945e8eb1e188002ea339
Chris Raible <chris@ghost.org>
2026-03-02 15:16:30 -0800

    Added validations to verified_emails schema

--- ghost/core/core/server/data/migrations/versions/6.20/2026-03-02-12-00-00-add-verified-emails-table.js
+++ ghost/core/core/server/data/migrations/versions/6.20/2026-03-02-12-00-00-add-verified-emails-table.js
@@ -2,8 +2,8 @@ const {addTable} = require('../../utils');
 
 module.exports = addTable('verified_emails', {
     id: {type: 'string', maxlength: 24, nullable: false, primary: true},
-    email: {type: 'string', maxlength: 191, nullable: false, unique: true},
-    status: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'pending'},
+    email: {type: 'string', maxlength: 191, nullable: false, unique: true, validations: {isEmail: true}},
+    status: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'pending', validations: {isIn: [['pending', 'verified']]}},
     created_at: {type: 'dateTime', nullable: false},
     updated_at: {type: 'dateTime', nullable: true}
 });
--- ghost/core/core/server/data/schema/schema.js
+++ ghost/core/core/server/data/schema/schema.js
@@ -936,8 +936,8 @@ module.exports = {
     },
     verified_emails: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
-        email: {type: 'string', maxlength: 191, nullable: false, unique: true},
-        status: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'pending'},
+        email: {type: 'string', maxlength: 191, nullable: false, unique: true, validations: {isEmail: true}},
+        status: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'pending', validations: {isIn: [['pending', 'verified']]}},
         created_at: {type: 'dateTime', nullable: false},
         updated_at: {type: 'dateTime', nullable: true}
     },
