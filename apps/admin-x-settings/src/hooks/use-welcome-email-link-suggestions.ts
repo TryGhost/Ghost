0ec9967b24f6aaa6cffa72ca2652199613f4328f
Evan Hahn <evan@ghost.org>
2026-03-02 16:53:29 -0600

    Assert types

--- apps/admin-x-settings/src/hooks/use-welcome-email-link-suggestions.ts
+++ apps/admin-x-settings/src/hooks/use-welcome-email-link-suggestions.ts
@@ -1,12 +1,24 @@
-import {getSettingValue} from '@tryghost/admin-x-framework/api/settings';
+import {getSettingValues} from '@tryghost/admin-x-framework/api/settings';
 import {useGlobalData} from '../components/providers/global-data-provider';
 import {useKoenigLinkSuggestions} from '@tryghost/admin-x-framework';
 
 export const useWelcomeEmailLinkSuggestions = () => {
     const {settings, siteData} = useGlobalData();
-    const membersSignupAccess = getSettingValue<string>(settings, 'members_signup_access') ?? 'none';
-    const donationsEnabled = getSettingValue<boolean>(settings, 'donations_enabled') ?? false;
-    const recommendationsEnabled = getSettingValue<boolean>(settings, 'recommendations_enabled') ?? false;
+    const [membersSignupAccess, donationsEnabled, recommendationsEnabled] = getSettingValues(settings, [
+        'members_signup_access',
+        'donations_enabled',
+        'recommendations_enabled'
+    ]);
+
+    if (typeof membersSignupAccess !== 'string') {
+        throw new TypeError('members_signup_access should be a string');
+    }
+    if (typeof donationsEnabled !== 'string') {
+        throw new TypeError('donations_enabled should be a string');
+    }
+    if (typeof recommendationsEnabled !== 'string') {
+        throw new TypeError('recommendations_enabled should be a string');
+    }
 
     return useKoenigLinkSuggestions({
         siteUrl: siteData.url,
