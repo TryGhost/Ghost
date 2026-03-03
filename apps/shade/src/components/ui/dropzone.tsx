8996fbe22619c8639cf0ee7e206844208e286fd0
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2026-03-03 15:47:11 +0100

    Added reusable Dropzone component in Shade

---
+++ apps/shade/src/components/ui/dropzone.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import {Accept, DropEvent, FileRejection, useDropzone} from 'react-dropzone';
+import {cn} from '@/lib/utils';
+
+type DropzoneRenderProps = {
+    isDragActive: boolean;
+    isDragAccept: boolean;
+    isDragReject: boolean;
+    isFocused: boolean;
+    isFileDialogActive: boolean;
+    open: () => void;
+};
+
+export interface DropzoneProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'children'> {
+    accept?: Accept;
+    multiple?: boolean;
+    maxFiles?: number;
+    disabled?: boolean;
+    onDropAccepted?: (files: File[], event: DropEvent) => void;
+    onDropRejected?: (fileRejections: FileRejection[], event: DropEvent) => void;
+    children?: React.ReactNode | ((props: DropzoneRenderProps) => React.ReactNode);
+}
+
+export const Dropzone = React.forwardRef<HTMLDivElement, DropzoneProps>(({
+    accept,
+    multiple = false,
+    maxFiles = multiple ? 0 : 1,
+    disabled = false,
+    onDropAccepted,
+    onDropRejected,
+    className,
+    children,
+    ...props
+}, ref) => {
+    const {
+        getRootProps,
+        getInputProps,
+        isDragActive,
+        isDragAccept,
+        isDragReject,
+        isFocused,
+        isFileDialogActive,
+        open
+    } = useDropzone({
+        accept,
+        multiple,
+        maxFiles,
+        disabled,
+        onDropAccepted,
+        onDropRejected
+    });
+
+    const content = typeof children === 'function'
+        ? children({isDragActive, isDragAccept, isDragReject, isFocused, isFileDialogActive, open})
+        : children;
+
+    return (
+        <div
+            {...getRootProps({
+                ...props,
+                ref,
+                role: 'button',
+                tabIndex: disabled ? -1 : 0,
+                'aria-disabled': disabled,
+                className: cn(
+                    'flex cursor-pointer flex-col items-center justify-center rounded-md border-2 border-dashed p-10 transition-colors outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none',
+                    disabled && 'cursor-not-allowed opacity-60',
+                    isDragAccept && 'border-green-500 bg-green-50 dark:bg-green-950/20',
+                    isDragReject && 'border-red-500 bg-red-50 dark:bg-red-950/20',
+                    !isDragAccept && !isDragReject && 'border-grey-300 hover:border-grey-400',
+                    className
+                )
+            })}
+        >
+            <input {...getInputProps()} />
+            {content}
+        </div>
+    );
+});
+
+Dropzone.displayName = 'Dropzone';
