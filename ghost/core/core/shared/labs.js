bc692dc8a8339014f3e316a586360d2f9c22a8bc
Evan Hahn <evan@ghost.org>
2026-03-05 09:44:59 -0600

    ✨ Expanded available cards in welcome email editor (#26645)
    closes https://linear.app/ghost/issue/NY-1104
    
    Moved the `welcomeEmailEditor` feature flag to GA.

--- ghost/core/core/shared/labs.js
+++ ghost/core/core/shared/labs.js
@@ -24,7 +24,8 @@ const GA_FEATURES = [
     'customFonts',
     'explore',
     'commentModeration',
-    'featurebaseFeedback'
+    'featurebaseFeedback',
+    'welcomeEmailEditor'
 ];
 
 // These features are considered publicly available and can be enabled/disabled by users
@@ -48,7 +49,6 @@ const PRIVATE_FEATURES = [
     'indexnow',
     'transistor',
     'retentionOffers',
-    'welcomeEmailEditor',
     'membersForward',
     'welcomeEmailsDesignCustomization'
 ];
