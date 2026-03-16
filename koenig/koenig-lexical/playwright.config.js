e9a71e2fa40e44a35577924bcd6417562126791b
Kevin Ansfield <kevin@lookingsideways.co.uk>
2026-03-16 18:54:32 +0000

    Reduced E2E test and expect timeouts
    Set test timeout to 10s and expect timeout to 5s so failing test runs
    complete faster. No tests should take this long so the previous 30s
    default was unnecessarily slow when many tests fail.

--- koenig/koenig-lexical/playwright.config.js
+++ koenig/koenig-lexical/playwright.config.js
@@ -20,6 +20,10 @@ export default defineConfig({
     reporter: process.env.CI ? [['github'], ['html']] :
               process.env.PLAYWRIGHT_HTML_REPORT ? [['html'], ['list']] :
               [['list']],
+    timeout: 10000,
+    expect: {
+        timeout: 5000
+    },
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
