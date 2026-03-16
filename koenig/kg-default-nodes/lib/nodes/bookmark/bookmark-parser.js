b74c37af650132f15d5347ce09e4d8036e83536b
Kevin Ansfield <kevin@lookingsideways.co.uk>
2026-03-16 09:14:45 +0000

    Guard background-image regex match against null
    The .match() call on background-image can return null for non-url()
    values (e.g. "none"), which would throw when indexing [1]. Store
    the match result first and only extract the URL when a match exists.
    
    Addresses review feedback on TryGhost/Koenig#1771.

--- koenig/kg-default-nodes/lib/nodes/bookmark/bookmark-parser.js
+++ koenig/kg-default-nodes/lib/nodes/bookmark/bookmark-parser.js
@@ -70,7 +70,10 @@ export function parseBookmarkNode(BookmarkNode) {
                         // Image is optional,
                         // The element usually still exists with an additional has.mixtapeImage--empty class and has no background image
                         if (imgElement && imgElement.style['background-image']) {
-                            thumbnail = imgElement.style['background-image'].match(/url\(([^)]*?)\)/)[1].replace(/^['"]|['"]$/g, '');
+                            const match = imgElement.style['background-image'].match(/url\(([^)]*?)\)/);
+                            if (match?.[1]) {
+                                thumbnail = match[1].replace(/^['"]|['"]$/g, '');
+                            }
                         }
 
                         let payload = {url,
--- koenig/kg-parser-plugins/lib/cards/embed.js
+++ koenig/kg-parser-plugins/lib/cards/embed.js
@@ -71,7 +71,10 @@ export function fromMixtape(options) {
         // Image is optional,
         // The element usually still exists with an additional has.mixtapeImage--empty class and has no background image
         if (imgElement && imgElement.style['background-image']) {
-            metadata.thumbnail = imgElement.style['background-image'].match(/url\(([^)]*?)\)/)[1].replace(/^['"]|['"]$/g, '');
+            const match = imgElement.style['background-image'].match(/url\(([^)]*?)\)/);
+            if (match?.[1]) {
+                metadata.thumbnail = match[1].replace(/^['"]|['"]$/g, '');
+            }
         }
 
         let payload = {url, metadata};
