738d2077270c0bd14659a330c961d2325d1f1150
Steve Larson <9larsons@gmail.com>
2026-03-17 11:32:54 -0500

    updated - also added linter rules

--- e2e/tests/public/portal-script-loading.test.ts
+++ e2e/tests/public/portal-script-loading.test.ts
@@ -2,18 +2,16 @@ import {HomePage} from '@/helpers/pages';
 import {SettingsService} from '@/helpers/services/settings/settings-service';
 import {expect, test} from '@/helpers/playwright';
 
-const portalScriptSelector = 'script[data-ghost][data-key][data-api]';
-
 test.describe('Portal Script Loading', () => {
     test('memberships enabled - loads portal script', async ({page}) => {
         const settingsService = new SettingsService(page.request);
         await settingsService.setMembersSignupAccess('all');
 
         const homePage = new HomePage(page);
-        await homePage.goto('/#/portal/signup');
+        await homePage.gotoPortalSignup();
 
-        await expect(page.locator(portalScriptSelector)).toHaveAttribute('src', /\/portal\.min\.js$/);
-        await expect(page.locator('#ghost-portal-root div iframe')).toHaveCount(1);
+        await expect(homePage.portalScript).toHaveAttribute('src', /\/portal\.min\.js$/);
+        await expect(homePage.portalIframe).toHaveCount(1);
     });
 
     test.describe('with stripe connected', () => {
@@ -24,10 +22,10 @@ test.describe('Portal Script Loading', () => {
             await settingsService.setMembersSignupAccess('none');
 
             const homePage = new HomePage(page);
-            await homePage.goto('/#/portal/signup');
+            await homePage.gotoPortalSignup();
 
-            await expect(page.locator(portalScriptSelector)).toHaveAttribute('src', /\/portal\.min\.js$/);
-            await expect(page.locator('#ghost-portal-root div iframe')).toHaveCount(1);
+            await expect(homePage.portalScript).toHaveAttribute('src', /\/portal\.min\.js$/);
+            await expect(homePage.portalIframe).toHaveCount(1);
         });
     });
 
@@ -36,9 +34,9 @@ test.describe('Portal Script Loading', () => {
         await settingsService.setMembersSignupAccess('none');
 
         const homePage = new HomePage(page);
-        await homePage.goto('/#/portal/signup');
+        await homePage.gotoPortalSignup();
 
-        await expect(page.locator(portalScriptSelector)).toHaveCount(0);
-        await expect(page.locator('#ghost-portal-root div iframe')).toHaveCount(0);
+        await expect(homePage.portalScript).toHaveCount(0);
+        await expect(homePage.portalIframe).toHaveCount(0);
     });
 });
