f99a8725897a07fd90f095e0ec73367dc9434dac
Kevin Ansfield <kevin@lookingsideways.co.uk>
2026-03-18 07:48:24 +0000

    Migrate kg-card-factory to TypeScript
    - Move lib/ to src/, rename .js to .ts
    - Add tsconfig.json (strict, NodeNext, ESM)
    - Add "type": "module" to package.json
    - Convert source and tests from CJS to ESM
    - Add type annotations throughout
    - Replace .eslintrc.js with eslint.config.js (flat config)
    - Output to build/ via tsc

--- koenig/kg-card-factory/src/CardFactory.ts
+++ koenig/kg-card-factory/src/CardFactory.ts
@@ -1,26 +1,73 @@
-module.exports = class CardFactory {
-    constructor(options) {
+export interface FactoryOptions {
+    siteUrl?: string;
+    [key: string]: unknown;
+}
+
+export interface CardPayload {
+    [key: string]: unknown;
+}
+
+export interface CardTransformOptions {
+    assetsOnly?: boolean;
+    siteUrl?: string;
+    [key: string]: unknown;
+}
+
+export interface DomNode {
+    nodeType?: number;
+    nodeValue?: string;
+    appendChild?(child: unknown): void;
+    [key: string]: unknown;
+}
+
+export interface DomProvider {
+    createComment(text: string): DomNode;
+    createDocumentFragment(): DomNode;
+    createElement?(tag: string): DomNode;
+    createTextNode?(text: string): DomNode;
+    [key: string]: unknown;
+}
+
+export interface CardRenderEnv {
+    dom: DomProvider;
+    [key: string]: unknown;
+}
+
+export interface CardRenderArgs {
+    env: CardRenderEnv;
+    payload: CardPayload;
+    options?: Record<string, unknown>;
+}
+
+export interface CardDefinition {
+    name: string;
+    type: string;
+    config?: { commentWrapper?: boolean };
+    render(args: CardRenderArgs): DomNode;
+    absoluteToRelative?(payload: CardPayload, options: CardTransformOptions): CardPayload;
+    relativeToAbsolute?(payload: CardPayload, options: CardTransformOptions): CardPayload;
+    toTransformReady?(payload: CardPayload, options: CardTransformOptions): CardPayload;
+}
+
+export class CardFactory {
+    factoryOptions: FactoryOptions;
+
+    constructor(options: FactoryOptions = {}) {
         this.factoryOptions = options;
     }
 
-    createCard(card) {
+    createCard(card: CardDefinition) {
         const {factoryOptions} = this;
-
-        const {
-            name,
-            type,
-            config = {}
-        } = card;
+        const {name, type, config = {}} = card;
 
         return {
             name,
             type,
             factoryOptions,
 
-            render({env, payload, options}) {
+            render({env, payload, options}: CardRenderArgs): DomNode {
                 const {dom} = env;
                 const cardOptions = Object.assign({}, factoryOptions, options);
-
                 const cardOutput = card.render({env, payload, options: cardOptions});
 
                 if (cardOutput.nodeType === 3 && cardOutput.nodeValue === '') {
@@ -32,55 +79,41 @@ module.exports = class CardFactory {
                     const beginComment = dom.createComment(`kg-card-begin: ${cleanName}`);
                     const endComment = dom.createComment(`kg-card-end: ${cleanName}`);
                     const fragment = dom.createDocumentFragment();
-
-                    fragment.appendChild(beginComment);
-                    fragment.appendChild(cardOutput);
-                    fragment.appendChild(endComment);
-
+                    fragment.appendChild!(beginComment);
+                    fragment.appendChild!(cardOutput);
+                    fragment.appendChild!(endComment);
                     return fragment;
                 }
 
                 return cardOutput;
             },
 
-            absoluteToRelative(payload, _options) {
+            absoluteToRelative(payload: CardPayload, _options?: CardTransformOptions) {
                 if (card.absoluteToRelative) {
-                    const defaultOptions = {
-                        assetsOnly: false,
-                        siteUrl: factoryOptions.siteUrl
-                    };
+                    const defaultOptions = {assetsOnly: false, siteUrl: factoryOptions.siteUrl};
                     const options = Object.assign({}, defaultOptions, _options);
                     return card.absoluteToRelative(payload, options);
                 }
-
                 return payload;
             },
 
-            relativeToAbsolute(payload, _options) {
+            relativeToAbsolute(payload: CardPayload, _options?: CardTransformOptions) {
                 if (card.relativeToAbsolute) {
-                    const defaultOptions = {
-                        assetsOnly: false,
-                        siteUrl: factoryOptions.siteUrl
-                    };
+                    const defaultOptions = {assetsOnly: false, siteUrl: factoryOptions.siteUrl};
                     const options = Object.assign({}, defaultOptions, _options);
                     return card.relativeToAbsolute(payload, options);
                 }
-
                 return payload;
             },
 
-            toTransformReady(payload, _options) {
+            toTransformReady(payload: CardPayload, _options?: CardTransformOptions) {
                 if (card.toTransformReady) {
-                    const defaultOptions = {
-                        assetsOnly: false,
-                        siteUrl: factoryOptions.siteUrl
-                    };
+                    const defaultOptions = {assetsOnly: false, siteUrl: factoryOptions.siteUrl};
                     const options = Object.assign({}, defaultOptions, _options);
                     return card.toTransformReady(payload, options);
                 }
-
                 return payload;
             }
         };
     }
-};
+}
--- koenig/kg-card-factory/test/CardFactory.test.ts
+++ koenig/kg-card-factory/test/CardFactory.test.ts
@@ -1,88 +1,83 @@
-// Switch these lines once there are useful utils
-// const testUtils = require('./utils');
-require('./utils');
+import './utils/index.js';
+import {CardFactory} from '../src/CardFactory.js';
+import type {DomProvider} from '../src/CardFactory.js';
+import {Document as SimpleDomDocument, HTMLSerializer, voidMap} from 'simple-dom';
 
-const CardFactory = require('../');
-const SimpleDom = require('simple-dom');
-const serializer = new SimpleDom.HTMLSerializer(SimpleDom.voidMap);
+const serializer = new HTMLSerializer(voidMap);
 
 describe('CardFactory', function () {
     describe('render', function () {
         it('adds comment wrapper when configured', function () {
-            let cardDefinition = {
+            const factory = new CardFactory();
+            const card = factory.createCard({
                 name: 'test',
                 type: 'dom',
-                config: {
-                    commentWrapper: true
-                },
+                config: {commentWrapper: true},
                 render({env: {dom}}) {
-                    let div = dom.createElement('div');
-                    div.appendChild(dom.createTextNode('Test!'));
+                    const d = dom as DomProvider & InstanceType<typeof SimpleDomDocument>;
+                    const div = d.createElement('div');
+                    div.appendChild!(d.createTextNode('Test!'));
                     return div;
                 }
-            };
-            let factory = new CardFactory();
-            let card = factory.createCard(cardDefinition);
+            });
 
-            let opts = {
-                env: {
-                    dom: new SimpleDom.Document()
-                }
-            };
+            const opts = {env: {dom: new SimpleDomDocument() as unknown as DomProvider}, payload: {}, options: {}};
 
-            serializer.serialize(card.render(opts))
+            serializer.serialize(card.render(opts) as unknown as Parameters<typeof serializer.serialize>[0])
                 .should.eql('<!--kg-card-begin: test--><div>Test!</div><!--kg-card-end: test-->');
         });
 
         it('skips comment wrapper if card output is blank', function () {
-            let cardDefinition = {
+            const factory = new CardFactory();
+            const card = factory.createCard({
                 name: 'test',
                 type: 'dom',
-                config: {
-                    commentWrapper: true
-                },
+                config: {commentWrapper: true},
                 render({env: {dom}}) {
-                    return dom.createTextNode('');
+                    const d = dom as DomProvider & InstanceType<typeof SimpleDomDocument>;
+                    return d.createTextNode('');
                 }
-            };
-            let factory = new CardFactory();
-            let card = factory.createCard(cardDefinition);
+            });
 
-            let opts = {
-                env: {
-                    dom: new SimpleDom.Document()
-                }
-            };
+            const opts = {env: {dom: new SimpleDomDocument() as unknown as DomProvider}, payload: {}, options: {}};
 
-            serializer.serialize(card.render(opts)).should.eql('');
+            serializer.serialize(card.render(opts) as unknown as Parameters<typeof serializer.serialize>[0]).should.eql('');
         });
     });
 
     describe('absoluteToRelative', function () {
         it('passes siteUrl in from factory options', function () {
-            let cardDefinition = {
-                absoluteToRelative(payload, options) {
-                    return options.siteUrl;
+            const factory = new CardFactory({siteUrl: 'http://127.0.0.1:2368/'});
+            const card = factory.createCard({
+                name: 'test',
+                type: 'dom',
+                render() {
+                    return {};
+                },
+                absoluteToRelative(_payload, options) {
+                    return {receivedSiteUrl: options.siteUrl};
                 }
-            };
-            let factory = new CardFactory({siteUrl: 'http://127.0.0.1:2368/'});
-            let card = factory.createCard(cardDefinition);
+            });
 
-            card.absoluteToRelative().should.equal('http://127.0.0.1:2368/');
+            card.absoluteToRelative({}).should.have.property('receivedSiteUrl', 'http://127.0.0.1:2368/');
         });
     });
 
     describe('relativeToAbsolute', function () {
         it('passes siteUrl in from factory options', function () {
-            let cardDefinition = {
-                relativeToAbsolute(payload, options) {
-                    return options.siteUrl;
+            const factory = new CardFactory({siteUrl: 'http://127.0.0.1:2368/'});
+            const card = factory.createCard({
+                name: 'test',
+                type: 'dom',
+                render() {
+                    return {};
+                },
+                relativeToAbsolute(_payload, options) {
+                    return {receivedSiteUrl: options.siteUrl};
                 }
-            };
-            let factory = new CardFactory({siteUrl: 'http://127.0.0.1:2368/'});
-            let card = factory.createCard(cardDefinition);
+            });
 
-            card.relativeToAbsolute().should.equal('http://127.0.0.1:2368/');
+            card.relativeToAbsolute({}).should.have.property('receivedSiteUrl', 'http://127.0.0.1:2368/');
         });
     });
 });
