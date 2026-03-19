83f71fba153f7224b0fb0bd3b49ee5512658c2e2
Michael Barrett <mike@ghost.org>
2026-03-19 13:25:29 +0000

    add gifted type to schema

--- ghost/core/core/server/data/schema/schema.js
+++ ghost/core/core/server/data/schema/schema.js
@@ -421,7 +421,7 @@ module.exports = {
         email: {type: 'string', maxlength: 191, nullable: false, unique: true, validations: {isEmail: true}},
         status: {
             type: 'string', maxlength: 50, nullable: false, defaultTo: 'free', validations: {
-                isIn: [['free', 'paid', 'comped']]
+                isIn: [['free', 'paid', 'comped', 'gifted']]
             }
         },
         name: {type: 'string', maxlength: 191, nullable: true},
@@ -585,12 +585,12 @@ module.exports = {
         member_id: {type: 'string', maxlength: 24, nullable: false, references: 'members.id', cascadeDelete: true},
         from_status: {
             type: 'string', maxlength: 50, nullable: true, validations: {
-                isIn: [['free', 'paid', 'comped']]
+                isIn: [['free', 'paid', 'comped', 'gifted']]
             }
         },
         to_status: {
             type: 'string', maxlength: 50, nullable: true, validations: {
-                isIn: [['free', 'paid', 'comped']]
+                isIn: [['free', 'paid', 'comped', 'gifted']]
             }
         },
         created_at: {type: 'dateTime', nullable: false}
@@ -650,7 +650,7 @@ module.exports = {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
         type: {
             type: 'string', maxlength: 50, nullable: false, validations: {
-                isIn: [['free', 'comped', 'paid']]
+                isIn: [['free', 'comped', 'paid', 'gifted']]
             }
         },
         status: {
