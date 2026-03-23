0b2f544b642b047ca1028e937a484c6b3c15c485
Kevin Ansfield <kevin@lookingsideways.co.uk>
2026-03-23 08:05:45 +0000

    Export InsertImagePayload and InsertImageFn types from kg-unsplash-selector
    These types are needed by consumers to properly type their onImageInsert
    callbacks but were not exported from the package entry point.

--- koenig/kg-unsplash-selector/src/index.ts
+++ koenig/kg-unsplash-selector/src/index.ts
@@ -1,4 +1,4 @@
 export {UnsplashSearchModal} from './UnsplashSearchModal';
-export type {DefaultHeaderTypes, Photo as PhotoType} from './UnsplashTypes';
+export type {DefaultHeaderTypes, InsertImageFn, InsertImagePayload, Photo as PhotoType} from './UnsplashTypes';
 export {UnsplashProvider} from './api/UnsplashProvider';
 import './styles/index.css';
