705afc56ae7f88c9d6694429e4b99a86fa673013
Steve Larson <9larsons@gmail.com>
2026-03-24 07:57:35 -0500

    Migrated Portal upgrade tests to e2e
    ref https://linear.app/ghost/issue/PLA-9/

--- e2e/helpers/pages/portal/sign-up-page.ts
+++ e2e/helpers/pages/portal/sign-up-page.ts
@@ -2,6 +2,8 @@ import {Locator, Page} from '@playwright/test';
 import {PortalPage} from './portal-page';
 
 export class SignUpPage extends PortalPage {
+    readonly monthlySwitchButton: Locator;
+    readonly yearlySwitchButton: Locator;
     readonly emailInput: Locator;
     readonly nameInput: Locator;
     readonly signupButton: Locator;
@@ -15,6 +17,8 @@ export class SignUpPage extends PortalPage {
     constructor(page: Page) {
         super(page);
 
+        this.monthlySwitchButton = this.portalFrame.locator('[data-test-button="switch-monthly"]');
+        this.yearlySwitchButton = this.portalFrame.locator('[data-test-button="switch-yearly"]');
         this.nameInput = this.portalFrame.getByRole('textbox', {name: 'Name'});
         this.emailInput = this.portalFrame.getByRole('textbox', {name: 'Email'});
         this.signupButton = this.portalFrame.getByRole('button', {name: 'Sign up'});
@@ -53,6 +57,15 @@ export class SignUpPage extends PortalPage {
 
         await paidTierCard.waitFor({state: 'visible'});
         await paidTierSelectButton.click();
+    }
+
+    async switchCadence(cadence: 'monthly' | 'yearly'): Promise<void> {
+        if (cadence === 'monthly') {
+            await this.monthlySwitchButton.click();
+            return;
+        }
+
+        await this.yearlySwitchButton.click();
     }
 
     async continueIfVisible(): Promise<void> {
