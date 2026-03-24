e4c3a7be95e4034ccf26dcc93fb885d0381503c5
Peter Zimon <peter.zimon@gmail.com>
2026-03-24 12:13:29 +0100

    Fixed border sizes

--- apps/admin-x-settings/src/components/settings/email-design/design-fields/button-style-field.tsx
+++ apps/admin-x-settings/src/components/settings/email-design/design-fields/button-style-field.tsx
@@ -8,7 +8,7 @@ export const ButtonStyleField = () => {
             <span>Button style</span>
             <ToggleGroup type="single" value={settings.button_style || 'fill'} onValueChange={(value: string) => value && onSettingsChange({button_style: value})}>
                 <ToggleGroupItem aria-label="Fill" value="fill"><span className="inline-block h-4 w-5 rounded-sm bg-current" /></ToggleGroupItem>
-                <ToggleGroupItem aria-label="Outline" value="outline"><span className="inline-block h-4 w-5 rounded-sm border-2 border-current" /></ToggleGroupItem>
+                <ToggleGroupItem aria-label="Outline" value="outline"><span className="inline-block h-4 w-5 rounded-sm border-[1.5px] border-current" /></ToggleGroupItem>
             </ToggleGroup>
         </div>
     );
