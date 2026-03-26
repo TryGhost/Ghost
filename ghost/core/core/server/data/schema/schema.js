7351b4e9371dabe8674020ff49563c462bfed72c
Chris Raible <chris@ghost.org>
2026-03-26 00:27:55 +0000

    Added email_templates table migration
    ref NY-1137
    Stores design settings for email templates, initially used by welcome
    emails but designed to be reused by newsletters in the future

--- ghost/core/core/server/data/schema/schema.js
+++ ghost/core/core/server/data/schema/schema.js
@@ -1143,6 +1143,28 @@ module.exports = {
             ['event_type', 'status', 'created_at']
         ]
     },
+    email_templates: {
+        id: {type: 'string', maxlength: 24, nullable: false, primary: true},
+        background_color: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'light'},
+        header_background_color: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'transparent'},
+        header_image: {type: 'string', maxlength: 2000, nullable: true},
+        show_header_title: {type: 'boolean', nullable: false, defaultTo: true},
+        footer_content: {type: 'text', maxlength: 1000000000, fieldtype: 'long', nullable: true},
+        button_color: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'accent'},
+        button_corners: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'rounded', validations: {isIn: [['square', 'rounded', 'pill']]}},
+        button_style: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'fill', validations: {isIn: [['fill', 'outline']]}},
+        link_color: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'accent'},
+        link_style: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'underline', validations: {isIn: [['underline', 'regular', 'bold']]}},
+        body_font_category: {type: 'string', maxlength: 191, nullable: false, defaultTo: 'sans_serif', validations: {isIn: [['serif', 'sans_serif']]}},
+        title_font_category: {type: 'string', maxlength: 191, nullable: false, defaultTo: 'sans_serif', validations: {isIn: [['serif', 'sans_serif']]}},
+        title_font_weight: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'bold', validations: {isIn: [['normal', 'medium', 'semibold', 'bold']]}},
+        image_corners: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'square', validations: {isIn: [['square', 'rounded']]}},
+        divider_color: {type: 'string', maxlength: 50, nullable: true},
+        section_title_color: {type: 'string', maxlength: 50, nullable: true},
+        show_badge: {type: 'boolean', nullable: false, defaultTo: true},
+        created_at: {type: 'dateTime', nullable: false},
+        updated_at: {type: 'dateTime', nullable: true}
+    },
     automated_emails: {
         id: {type: 'string', maxlength: 24, nullable: false, primary: true},
         status: {type: 'string', maxlength: 50, nullable: false, defaultTo: 'inactive', validations: {isIn: [['active', 'inactive']]}},
