f7ccf4e9ad282e30e863c7a3190d40bf24d6147b
Chris Raible <chris@ghost.org>
2026-03-30 23:03:36 +0000

    Switched to using the createAddColumnMigration utility function

--- ghost/core/core/server/data/migrations/versions/6.25/2026-03-30-22-16-43-add-email-design-setting-id-to-automated-emails.js
+++ ghost/core/core/server/data/migrations/versions/6.25/2026-03-30-22-16-43-add-email-design-setting-id-to-automated-emails.js
@@ -1,42 +1,8 @@
-const logging = require('@tryghost/logging');
-const {createNonTransactionalMigration} = require('../../utils');
+const {createAddColumnMigration} = require('../../utils');
 
-const TABLE_NAME = 'automated_emails';
-const COLUMN_NAME = 'email_design_setting_id';
-
-module.exports = createNonTransactionalMigration(
-    async function up(knex) {
-        const hasColumn = await knex.schema.hasColumn(TABLE_NAME, COLUMN_NAME);
-
-        if (hasColumn) {
-            logging.warn(`Adding column ${TABLE_NAME}.${COLUMN_NAME} — skipping as column already exists`);
-            return;
-        }
-
-        logging.info(`Adding column ${TABLE_NAME}.${COLUMN_NAME}`);
-
-        await knex.schema.table(TABLE_NAME, function (table) {
-            table.string(COLUMN_NAME, 24)
-                .nullable()
-                .references('email_design_settings.id');
-        });
-    },
-    async function down(knex) {
-        const hasColumn = await knex.schema.hasColumn(TABLE_NAME, COLUMN_NAME);
-
-        if (!hasColumn) {
-            logging.warn(`Removing column ${TABLE_NAME}.${COLUMN_NAME} — skipping as column does not exist`);
-            return;
-        }
-
-        logging.info(`Removing column ${TABLE_NAME}.${COLUMN_NAME}`);
-
-        await knex.schema.table(TABLE_NAME, function (table) {
-            table.dropForeign(COLUMN_NAME);
-        });
-
-        await knex.schema.table(TABLE_NAME, function (table) {
-            table.dropColumn(COLUMN_NAME);
-        });
-    }
-);
+module.exports = createAddColumnMigration('automated_emails', 'email_design_setting_id', {
+    type: 'string',
+    maxlength: 24,
+    nullable: true,
+    references: 'email_design_settings.id'
+});
