8537eff1fba144c471dfdda81868c76f1795d870
Troy Ciesco <tmciesco@gmail.com>
2026-03-29 20:43:29 -0400

    rm extra migration

--- ghost/core/core/server/data/migrations/versions/6.19/2026-03-17-12-00-00-add-newsletter-show-share-button.js
+++
@@ -1,7 +0,0 @@
-const {createAddColumnMigration} = require('../../utils');
-
-module.exports = createAddColumnMigration('newsletters', 'show_share_button', {
-    type: 'boolean',
-    nullable: false,
-    defaultTo: false
-});
