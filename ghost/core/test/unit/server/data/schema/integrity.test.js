90cbaceef9d3794f4cf6a29f485a1de4d0bdc07f
Chris Raible <chris@ghost.org>
2026-03-31 11:41:39 -0700

    Added nullable `email_design_setting_id` FK to automated_emails (#27008)
    closes
    https://linear.app/ghost/issue/NY-1180/add-email-design-setting-id-fk-to-automated-emails
    
    ## Summary
    
    - Added nullable `email_design_setting_id` FK column to
    `automated_emails` table
    - Plan to backfill all existing `automated_emails` rows with the default
    `email_design_settings` row, then make this column non-nullable in two
    follow up migration PRs to come.
    
    Stacked on #27006

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = 'c193998d02d13e8b85162315d938772c';
+    const currentSchemaHash = 'dda821448f20db2f4965d67c7f3d4051';
     const currentFixturesHash = '2f86ab1e3820e86465f9ad738dd0ee93';
     const currentSettingsHash = 'a102b80d2ab0cd92325ed007c94d7da6';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
