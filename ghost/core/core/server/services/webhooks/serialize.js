f8ebf43649037ae0748a052f8b11a76384e135dc
Steve Larson <9larsons@gmail.com>
2026-04-08 14:11:36 -0500

    rm comments

--- ghost/core/core/server/services/webhooks/serialize.js
+++ ghost/core/core/server/services/webhooks/serialize.js
@@ -42,9 +42,6 @@ module.exports = (event, model) => {
                 .then(() => {
                     const result = frame.response[docName][0];
 
-                    // Remove subscriptions from webhook payload — without full
-                    // Stripe relations loaded, it would always be an empty array
-                    // which falsely implies the member has no subscriptions
                     if (docName === 'members') {
                         delete result.subscriptions;
                     }
