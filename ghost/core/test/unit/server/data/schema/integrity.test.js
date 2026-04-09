49501b57acd796b4f780d18ddf0641e54b330568
Michael Barrett <mike@ghost.org>
2026-04-09 14:30:01 +0100

    Added gift subscription notification columns to users table
    ref https://linear.app/ghost/issue/BER-3523
    
    Added gift subscription notification columns to the `users` table so a user
    can configure if they want to recieve these notifications

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '1aedfe928922388669e594aa6b762871';
+    const currentSchemaHash = 'f209300a0c528851f89f2e6302410c85';
     const currentFixturesHash = '2f86ab1e3820e86465f9ad738dd0ee93';
     const currentSettingsHash = 'a102b80d2ab0cd92325ed007c94d7da6';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
