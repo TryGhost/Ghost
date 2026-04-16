f3b5b3324b12345305be7b8508db3ef1105103df
Evan Hahn <evan@ghost.org>
2026-04-16 12:35:49 -0500

    Started scheduling automation polls with scheduler
    closes https://linear.app/ghost/issue/NY-1191
    ref https://github.com/TryGhost/Ghost/pull/27421
    
    Before, we used `setTimeout` to schedule automation polls (with a TODO).
    
    Now, we properly use the scheduler.

---
+++ ghost/core/core/server/api/endpoints/automations.js
@@ -0,0 +1,19 @@
+const domainEvents = require('@tryghost/domain-events');
+const StartAutomationsPollEvent = require('../../services/welcome-email-automations/events/start-automations-poll-event');
+
+/** @type {import('@tryghost/api-framework').Controller} */
+const controller = {
+    docName: 'automations',
+
+    poll: {
+        statusCode: 204,
+        headers: {
+            cacheInvalidate: false
+        },
+        query() {
+            domainEvents.dispatch(StartAutomationsPollEvent.create());
+        }
+    }
+};
+
+module.exports = controller;
