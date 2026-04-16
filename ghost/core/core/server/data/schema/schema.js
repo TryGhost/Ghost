5346a457ccd88eb602be1071eb5429e3a48af785
Michael Barrett <mike@ghost.org>
2026-04-16 11:38:56 +0100

    Added gift subscriptions indexes (#27411)
    no ref
    
    Added 2 new indexes on the gift subscriptions job to make querying for
    pending consumption / expired gifts from the clean up job more efficient

--- ghost/core/core/server/data/schema/schema.js
+++ ghost/core/core/server/data/schema/schema.js
@@ -1251,6 +1251,10 @@ module.exports = {
         redeemed_at: {type: 'dateTime', nullable: true},
         consumed_at: {type: 'dateTime', nullable: true},
         expired_at: {type: 'dateTime', nullable: true},
-        refunded_at: {type: 'dateTime', nullable: true}
+        refunded_at: {type: 'dateTime', nullable: true},
+        '@@INDEXES@@': [
+            ['status', 'consumes_at'],
+            ['status', 'expires_at']
+        ]
     }
 };
