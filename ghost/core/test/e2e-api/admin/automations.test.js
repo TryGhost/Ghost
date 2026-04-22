51cc58f32ec03b4dd7944653c3133d8839a313b6
Evan Hahn <evan@ghost.org>
2026-04-22 16:38:15 -0500

    Add tests for bad /poll token param

--- ghost/core/test/e2e-api/admin/automations.test.js
+++ ghost/core/test/e2e-api/admin/automations.test.js
@@ -5,18 +5,22 @@ const {getSignedAdminToken} = require('../../../core/server/adapters/scheduling/
 const {agentProvider, fixtureManager, matchers, assertions} = require('../../utils/e2e-framework');
 const StartAutomationsPollEvent = require('../../../core/server/services/welcome-email-automations/events/start-automations-poll-event');
 
-const {anyContentVersion, anyEtag} = matchers;
+const {anyContentVersion, anyEtag, anyErrorId} = matchers;
 const {cacheInvalidateHeaderNotSet} = assertions;
 
 describe('Automations API', function () {
     let agent;
+    let schedulerIntegration;
     let schedulerToken;
 
     before(async function () {
         agent = await agentProvider.getAdminAPIAgent();
         await fixtureManager.init('integrations', 'api_keys');
 
-        const schedulerIntegration = await models.Integration.findOne({slug: 'ghost-scheduler'}, {withRelated: 'api_keys'});
+        schedulerIntegration = await models.Integration.findOne(
+            {slug: 'ghost-scheduler'},
+            {withRelated: 'api_keys'}
+        );
 
         schedulerToken = getSignedAdminToken({
             publishedAt: new Date().toISOString(),
@@ -29,19 +33,69 @@ describe('Automations API', function () {
         sinon.restore();
     });
 
-    it('Can poll', async function () {
-        const dispatchStub = sinon.stub(domainEvents, 'dispatch');
+    describe('poll', function () {
+        /** @type {sinon.SinonStub} */
+        let dispatchStub;
 
-        await agent
-            .put(`automations/poll/?token=${schedulerToken}`)
-            .expectStatus(204)
-            .expectEmptyBody()
-            .expect(cacheInvalidateHeaderNotSet())
-            .matchHeaderSnapshot({
-                'content-version': anyContentVersion,
-                etag: anyEtag
+        beforeEach(function () {
+            dispatchStub = sinon.stub(domainEvents, 'dispatch');
+        });
+
+        it('does not poll when request lacks a token', async function () {
+            await agent
+                .put('automations/poll/')
+                .expectStatus(401)
+                .expect(cacheInvalidateHeaderNotSet())
+                .matchHeaderSnapshot({
+                    'content-version': anyContentVersion,
+                    etag: anyEtag
+                })
+                .matchBodySnapshot({
+                    errors: [{
+                        id: anyErrorId,
+                        message: 'Invalid token: No token found in URL'
+                    }]
+                });
+
+            sinon.assert.notCalled(dispatchStub);
+        });
+
+        it('does not poll when request token is invalid', async function () {
+            const invalidSchedulerToken = getSignedAdminToken({
+                publishedAt: new Date().toISOString(),
+                apiUrl: '/members/',
+                integration: schedulerIntegration.toJSON()
             });
 
-        sinon.assert.calledOnceWithExactly(dispatchStub, sinon.match.instanceOf(StartAutomationsPollEvent));
+            await agent
+                .put(`automations/poll/?token=${invalidSchedulerToken}`)
+                .expectStatus(401)
+                .expect(cacheInvalidateHeaderNotSet())
+                .matchHeaderSnapshot({
+                    'content-version': anyContentVersion,
+                    etag: anyEtag
+                })
+                .matchBodySnapshot({
+                    errors: [{
+                        id: anyErrorId
+                    }]
+                });
+
+            sinon.assert.notCalled(dispatchStub);
+        });
+
+        it('triggers a poll with a valid scheduler integration token', async function () {
+            await agent
+                .put(`automations/poll/?token=${schedulerToken}`)
+                .expectStatus(204)
+                .expectEmptyBody()
+                .expect(cacheInvalidateHeaderNotSet())
+                .matchHeaderSnapshot({
+                    'content-version': anyContentVersion,
+                    etag: anyEtag
+                });
+
+            sinon.assert.calledOnceWithExactly(dispatchStub, sinon.match.instanceOf(StartAutomationsPollEvent));
+        });
     });
 });
