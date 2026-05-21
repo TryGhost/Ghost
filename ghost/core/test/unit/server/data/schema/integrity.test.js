0805757b0d6130381af251dccb675fb272f3ea4a
Jonatan Svennberg <jonatan.svennberg@gmail.com>
2026-05-21 08:57:26 +0200

    Added score column to comment likes
    ref https://linear.app/ghost/issue/BER-3549/create-comment-dislikes-private-lab-flag
    
    Stores comment votes in the existing comment_likes table so dislikes can ship without creating a separate table or changing old like inserts.

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -35,7 +35,7 @@ const validateRouteSettings = require('../../../../../core/server/services/route
  */
 describe('DB version integrity', function () {
     // Only these variables should need updating
-    const currentSchemaHash = '022588f95456319debb9a270b24b4564';
+    const currentSchemaHash = 'fafc9d5ad026f5eef11f9229a34994eb';
     const currentFixturesHash = '823aa0e8a8f083e80e271c47836a7e5d';
     const currentSettingsHash = '397be8628c753b1959b8954d5610f83f';
     const currentRoutesHash = '3d180d52c663d173a6be791ef411ed01';
