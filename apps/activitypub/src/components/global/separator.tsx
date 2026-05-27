da6c4d8160d4e8f7ffb60ec71c5c9930ece80dac
Peter Zimon <peter.zimon@gmail.com>
2026-05-27 11:22:43 +0200

    Updated ActivityPub gray token usage
    ref DES-1392
    
    ActivityPub still referenced removed intermediate gray stops and had one dark pseudo-border variant in the wrong order, so these now use supported gray tokens consistently.

--- apps/activitypub/src/components/global/separator.tsx
+++ apps/activitypub/src/components/global/separator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Separator: React.FC = () => {
-    return <div className='bg-gray-150 h-px w-full dark:bg-gray-950' />;
+    return <div className='h-px w-full bg-gray-200 dark:bg-gray-950' />;
 };
 
 export default Separator;
