3832cd12f8d0ab5237d2150cb40444ce9737f74f
Evan Hahn <evan@ghost.org>
2026-06-05 12:07:59 -0500

    Start passing `poll` the things it will need

--- ghost/core/core/server/services/automations/poll.ts
+++ ghost/core/core/server/services/automations/poll.ts
@@ -1,11 +1,42 @@
+import type {AutomationsRepository} from './automations-repository';
+
+type MemberWelcomeEmailService = {
+    init: () => unknown;
+    api: {
+        sendAutomationEmail: (options: {
+            email: {
+                designSettingId: string | null;
+                lexical: string;
+                senderEmail: string | null;
+                senderName: string | null;
+                senderReplyTo: string | null;
+                subject: string;
+            };
+            member: {
+                email: string;
+                name: string | null;
+                uuid: string;
+            };
+            memberStatus: 'free' | 'paid';
+        }) => Promise<unknown>;
+    };
+};
+
 type PollOptions = {
+    automationsApi: Pick<AutomationsRepository,
+        'fetchAndLockSteps' |
+        'finishStepAndEnqueueNext' |
+        'markStepTerminal' |
+        'retryStep'
+    >;
     enqueueAnotherPollAt: (date: Readonly<Date>) => unknown;
+    memberWelcomeEmailService: MemberWelcomeEmailService;
 };
 
 export const poll = async ({
-    // TODO(NY-1286) This ESLint suppression will be removed once we implement polling.
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    enqueueAnotherPollAt
+    automationsApi,
+    enqueueAnotherPollAt,
+    memberWelcomeEmailService
 }: Readonly<PollOptions>): Promise<void> => {
     // TODO(NY-1311) Once we're using real tables, we should remove this conditional.
     if (
