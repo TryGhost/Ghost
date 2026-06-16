1d80b4f7ce352436ac5c3fd557bc6d54793787ed
Evan Hahn <evan@ghost.org>
2026-06-16 12:28:54 -0500

    Started running automations polls outside development (#28636)
    closes https://linear.app/ghost/issue/NY-1311

--- ghost/core/core/server/services/automations/poll.ts
+++ ghost/core/core/server/services/automations/poll.ts
@@ -202,18 +202,6 @@ export const poll = async ({
     enqueueAnotherPollAt,
     memberWelcomeEmailService
 }: Readonly<PollOptions>): Promise<void> => {
-    // TODO(NY-1311) Once we're using real tables, we should remove this conditional.
-    // Note that unlike triggering, where we only continue if the "automations"
-    // flag is enabled, for polling we want to run in all cases. If an
-    // automation was enqueued while the flag was on, we want it to run even if
-    // the feature was turned off.
-    if (
-        process.env.NODE_ENV !== 'development'
-        && !process.env.NODE_ENV?.startsWith('test')
-    ) {
-        return;
-    }
-
     const {steps, nextStepReadyAt} = await automationsApi.fetchAndLockSteps(MAX_STEPS_PER_BATCH);
 
     let nextPollAt = nextStepReadyAt;
