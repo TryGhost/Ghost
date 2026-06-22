14c786fcd86df2df16627336fe1669bcfbaf880c
Evan Hahn <evan@ghost.org>
2026-06-22 12:11:43 -0500

    Logged when member has unsubscribed from automations emails
    no ref
    
    This could be helpful when investigating issues in the future.

--- ghost/core/core/server/services/automations/poll.ts
+++ ghost/core/core/server/services/automations/poll.ts
@@ -166,6 +166,13 @@ const processStep = async ({
             break;
         case 'send_email':
             if (!hasUpdatesAndAnnouncementsEnabled(member)) {
+                logging.warn({
+                    system: {
+                        event: 'automations.poll.member_unsubscribed',
+                        member_id: step.member_id,
+                        step_id: step.id
+                    }
+                }, `[AUTOMATIONS] Member ${step.member_id} for step ${step.id} has unsubscribed from emails. Fast-finishing this step`);
                 break;
             }
             memberWelcomeEmailService.init();
