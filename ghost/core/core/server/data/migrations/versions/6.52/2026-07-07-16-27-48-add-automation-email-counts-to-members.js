bda0376f5fc457fe29dcf23019f35424a9f0ae6a
Chris Raible <chris@ghost.org>
2026-07-07 12:28:41 -0400

    Added automation email count columns
    no ref
    
    Added separate members columns for automation email totals, tracked sends, and opens so automation analytics can be populated independently of newsletter stats.

---
+++ ghost/core/core/server/data/migrations/versions/6.52/2026-07-07-16-27-48-add-automation-email-counts-to-members.js
@@ -0,0 +1,14 @@
+const {combineNonTransactionalMigrations, createAddColumnMigration} = require('../../utils');
+
+const columnDefinition = {
+    type: 'integer',
+    unsigned: true,
+    nullable: false,
+    defaultTo: 0
+};
+
+module.exports = combineNonTransactionalMigrations(
+    createAddColumnMigration('members', 'automation_email_count', columnDefinition),
+    createAddColumnMigration('members', 'automation_tracked_email_count', columnDefinition),
+    createAddColumnMigration('members', 'automation_email_open_count', columnDefinition)
+);
