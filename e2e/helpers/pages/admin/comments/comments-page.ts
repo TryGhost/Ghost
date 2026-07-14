1c7b6e7112a0f545f262c6324ac5ae841fa8fba7
Steve Larson <9larsons@gmail.com>
2026-07-14 09:04:59 -0500

    Moved commenting mutations to acceptance tests
    no ref
    Restores disable-commenting coverage that was gated behind an unused React-shell E2E mode while keeping the checks fast and deterministic.

--- e2e/helpers/pages/admin/comments/comments-page.ts
+++ e2e/helpers/pages/admin/comments/comments-page.ts
@@ -7,14 +7,6 @@ export class CommentsPage extends AdminPage {
     readonly commentRows: Locator;
     readonly viewOnPostMenuItem: Locator;
     readonly viewMemberMenuItem: Locator;
-    readonly disableCommentingMenuItem: Locator;
-    readonly enableCommentingMenuItem: Locator;
-    readonly disableCommentsModal: Locator;
-    readonly disableCommentsModalTitle: Locator;
-    readonly disableCommentsButton: Locator;
-    readonly cancelButton: Locator;
-    readonly commentingDisabledIndicator = (row: Locator) => row.getByTestId('commenting-disabled-indicator');
-    readonly hideCommentsCheckbox: Locator;
 
     constructor(page: Page) {
         super(page);
@@ -24,13 +16,6 @@ export class CommentsPage extends AdminPage {
         this.commentRows = page.getByTestId(commentListRow);
         this.viewOnPostMenuItem = page.getByRole('menuitem', {name: 'View on post'});
         this.viewMemberMenuItem = page.getByRole('menuitem', {name: 'View member'});
-        this.disableCommentingMenuItem = page.getByRole('menuitem', {name: 'Disable commenting'});
-        this.enableCommentingMenuItem = page.getByRole('menuitem', {name: 'Enable commenting'});
-        this.disableCommentsModal = page.getByRole('dialog', {name: 'Disable comments'});
-        this.disableCommentsModalTitle = this.disableCommentsModal.getByRole('heading', {name: 'Disable comments'});
-        this.disableCommentsButton = this.disableCommentsModal.getByRole('button', {name: 'Disable comments'});
-        this.cancelButton = this.disableCommentsModal.getByRole('button', {name: 'Cancel'});
-        this.hideCommentsCheckbox = this.disableCommentsModal.getByRole('checkbox', {name: 'Hide all previous comments'});
     }
 
     async waitForComments(): Promise<void> {
@@ -47,17 +32,5 @@ export class CommentsPage extends AdminPage {
 
     async openMoreMenu(row: Locator): Promise<void> {
         await this.getMoreMenuButton(row).click();
-    }
-
-    async clickDisableCommenting(): Promise<void> {
-        await this.disableCommentingMenuItem.click();
-    }
-
-    async clickEnableCommenting(): Promise<void> {
-        await this.enableCommentingMenuItem.click();
-    }
-
-    async confirmDisableCommenting(): Promise<void> {
-        await this.disableCommentsButton.click();
     }
 }
