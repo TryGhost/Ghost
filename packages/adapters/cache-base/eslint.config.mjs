d1bed8a89a106ac332463079220c8e322c9446d9
Austin Burdine <austin@ghost.org>
2026-07-21 16:36:25 -0400

    Converted adapter base package to Typescript/ESM
    ref https://linear.app/ghost/issue/PLA-231/move-cache-adapter-base-into-ghost-monorepo
    - converts adapter-base-cache package to TypeScript/ESM following standard conventions

--- packages/adapters/cache-base/eslint.config.mjs
+++ packages/adapters/cache-base/eslint.config.mjs
@@ -1,9 +1,3 @@
 import {nodeLibConfig} from '@internal/cfg-eslint';
 
-export default nodeLibConfig({
-    typescript: false,
-    commonjs: true,
-    extraTestRules: {
-        'ghost/ghost-custom/node-assert-strict': 'error'
-    }
-});
+export default nodeLibConfig();
