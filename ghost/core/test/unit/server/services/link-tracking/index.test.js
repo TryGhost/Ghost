adae1ccf8025f11fe78476d788ca6f6d28cfb7da
Chris Raible <chris@ghost.org>
2026-07-28 16:02:16 -0400

    Fixed concurrent link tracking initialization
    ref https://linear.app/ghost/issue/NY-1492/rewrite-links-in-outgoing-automation-emails
    
    Automation steps render concurrently, so callers must await the same setup and a failed initialization must remain safe to retry.

---
+++ ghost/core/test/unit/server/services/link-tracking/index.test.js
@@ -0,0 +1,69 @@
+const assert = require('node:assert/strict');
+const sinon = require('sinon');
+const DomainEvents = require('@tryghost/domain-events');
+
+const linkRedirection = require('../../../../../core/server/services/link-redirection');
+const linkTracking = require('../../../../../core/server/services/link-tracking');
+const LinkClickTrackingService = require('../../../../../core/server/services/link-tracking/link-click-tracking-service');
+
+describe('LinkTrackingServiceWrapper', function () {
+    const originalLinkRedirectionService = linkRedirection.service;
+    const originalLinkRedirectRepository = linkRedirection.linkRedirectRepository;
+
+    afterEach(function () {
+        sinon.restore();
+        linkRedirection.service = originalLinkRedirectionService;
+        linkRedirection.linkRedirectRepository = originalLinkRedirectRepository;
+    });
+
+    it('waits for the same initialization when called concurrently', async function () {
+        linkRedirection.service = {};
+        linkRedirection.linkRedirectRepository = {};
+
+        let finishInitialization;
+        const initialization = new Promise((resolve) => {
+            finishInitialization = resolve;
+        });
+        const originalInit = LinkClickTrackingService.prototype.init;
+        const subscribe = sinon.stub(DomainEvents, 'subscribe');
+        const init = sinon.stub(LinkClickTrackingService.prototype, 'init').callsFake(function () {
+            originalInit.call(this);
+            return initialization;
+        });
+        const wrapper = new linkTracking.LinkTrackingServiceWrapper();
+
+        const firstInit = wrapper.init();
+        const secondInit = wrapper.init();
+        let secondInitFinished = false;
+        secondInit.then(() => {
+            secondInitFinished = true;
+        });
+
+        sinon.assert.calledOnce(init);
+        sinon.assert.calledOnce(subscribe);
+        assert.equal(wrapper.service, undefined);
+        await Promise.resolve();
+        assert.equal(secondInitFinished, false);
+
+        finishInitialization();
+        await Promise.all([firstInit, secondInit]);
+
+        assert.ok(wrapper.service);
+    });
+
+    it('allows initialization to be retried after a failure', async function () {
+        linkRedirection.service = {};
+        linkRedirection.linkRedirectRepository = {};
+
+        const init = sinon.stub(LinkClickTrackingService.prototype, 'init');
+        init.onFirstCall().rejects(new Error('Initialization failed'));
+        init.onSecondCall().resolves();
+        const wrapper = new linkTracking.LinkTrackingServiceWrapper();
+
+        await assert.rejects(wrapper.init(), /Initialization failed/);
+        await wrapper.init();
+
+        sinon.assert.calledTwice(init);
+        assert.ok(wrapper.service);
+    });
+});
