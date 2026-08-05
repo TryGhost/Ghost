3db147f83786f332cd15ebbdc03623116b0fc5b1
Evan Hahn <evan@ghost.org>
2026-08-05 11:19:29 -0500

    Actually move the flag

--- ghost/core/core/shared/labs.js
+++ ghost/core/core/shared/labs.js
@@ -49,7 +49,6 @@ const PUBLIC_BETA_FEATURES = [
 // Which is only visible if the developer experiments flag is enabled
 const PRIVATE_FEATURES = [
     'automations',
-    'automationAnalytics',
     'stripeAutomaticTax',
     'importMemberTier',
     'csvContentImporter',
