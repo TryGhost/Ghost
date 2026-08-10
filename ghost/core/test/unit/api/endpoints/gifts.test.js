af0da3d776b940c6edcb23f2cb57f0532c2d6434
Kevin Ansfield <kevin@ghost.org>
2026-08-10 18:30:38 +0100

    Renamed `gift-reminders` controller to `gifts` (#29848)
    ref https://linear.app/ghost/issue/BER-3851/
    
    - the specificity of the `gift-reminders` controller naming made it
    harder to expand gift subscriptions with additional endpoints, renaming
    to just `gifts` matches the service naming and provides a single file to
    house upcoming gift delivery work

--- ghost/core/test/unit/api/endpoints/gifts.test.js
+++ ghost/core/test/unit/api/endpoints/gifts.test.js
@@ -1,10 +1,10 @@
 const assert = require('node:assert/strict');
 const sinon = require('sinon');
 const domainEvents = require('@tryghost/domain-events');
-const giftRemindersController = require('../../../../core/server/api/endpoints/gift-reminders');
+const giftsController = require('../../../../core/server/api/endpoints/gifts');
 const StartGiftReminderFlushEvent = require('../../../../core/server/services/gifts/events/start-gift-reminder-flush-event');
 
-describe('Gift Reminders controller', function () {
+describe('Gifts controller', function () {
     afterEach(function () {
         sinon.restore();
     });
@@ -13,7 +13,7 @@ describe('Gift Reminders controller', function () {
         it('dispatches a StartGiftReminderFlushEvent', function () {
             const dispatchStub = sinon.stub(domainEvents, 'dispatch');
 
-            const result = giftRemindersController.flushReminders.query({});
+            const result = giftsController.flushReminders.query({});
 
             sinon.assert.calledOnceWithExactly(
                 dispatchStub,
