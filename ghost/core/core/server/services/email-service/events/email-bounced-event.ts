13cb8923e011f6905aee7611ce63629d1e20c03f
Evan Hahn <evan@ghost.org>
2026-08-17 17:10:02 -0500

    TypeScriptify email service events
    no ref

---
+++ ghost/core/core/server/services/email-service/events/email-bounced-event.ts
@@ -0,0 +1,37 @@
+export type EmailFailureData = {
+    id?: string;
+    email: string;
+    memberId: string;
+    emailId: string;
+    error: {
+        message: string;
+        code: number;
+        enhancedCode?: string | null;
+    } | null;
+    emailRecipientId: string;
+    timestamp?: Date;
+};
+
+export class EmailBouncedEvent {
+    readonly id?: string;
+    readonly email: string;
+    readonly memberId: string;
+    readonly emailId: string;
+    readonly error: EmailFailureData['error'];
+    readonly emailRecipientId: string;
+    readonly timestamp: Date;
+
+    private constructor({id, email, memberId, emailId, error, emailRecipientId, timestamp}: EmailFailureData & {timestamp: Date}) {
+        this.id = id;
+        this.memberId = memberId;
+        this.emailId = emailId;
+        this.email = email;
+        this.error = error;
+        this.emailRecipientId = emailRecipientId;
+        this.timestamp = timestamp;
+    }
+
+    static create(data: EmailFailureData): EmailBouncedEvent {
+        return new EmailBouncedEvent({...data, timestamp: data.timestamp || new Date()});
+    }
+}
--- ghost/core/test/unit/server/services/members-events/last-seen-at-updater.test.js
+++ ghost/core/test/unit/server/services/members-events/last-seen-at-updater.test.js
@@ -4,7 +4,7 @@ const LastSeenAtUpdater = require('../../../../../core/server/services/members-e
 const DomainEvents = require('@tryghost/domain-events');
 const {MemberPageViewEvent, MemberCommentEvent, MemberSubscribeEvent, MemberLinkClickEvent} = require('../../../../../core/shared/events');
 const moment = require('moment');
-const EmailOpenedEvent = require('../../../../../core/server/services/email-service/events/email-opened-event');
+const {EmailOpenedEvent} = require('../../../../../core/server/services/email-service/events/email-opened-event');
 const EventEmitter = require('events');
 const logging = require('@tryghost/logging');
 
