aa25788fb317225a87d61ab95c987508a5fa691e
Evan Hahn <evan@ghost.org>
2026-08-17 13:13:40 -0500

    Renamed Mailgun provider ID database column (#30025)
    ref https://github.com/TryGhost/Ghost/pull/29182
    
    This change should have no user impact.
    
    This renames `email_batches.provider_id` to
    `email_batches.mailgun_message_id`.
    
    Two benefits:
    
    - Clarity (several of us have been confused by this name)
    - Matches the `automated_email_recipients` table (see
    https://github.com/TryGhost/Ghost/pull/29182)
    
    This also changes its usage on the frontend. Normally I'd be more
    careful about this change--after all, it's a breaking API change--but it
    only affects a debug view.

---
+++ ghost/core/core/server/data/migrations/versions/6.58/2026-08-11-21-22-48-rename-email-batches-provider-id.js
@@ -0,0 +1,8 @@
+const {createRenameColumnMigration} = require('../../utils');
+
+module.exports = createRenameColumnMigration(
+    'email_batches',
+    'provider_id',
+    'mailgun_message_id',
+    {algorithm: 'instant'}
+);
