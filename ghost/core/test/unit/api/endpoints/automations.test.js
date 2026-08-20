20e8ac1d15f75efaef1b4d1d772cd995aad1eba8
Evan Hahn <evan@ghost.org>
2026-08-20 11:38:14 -0500

    TypeScriptify "start automations poll" event
    no ref

--- ghost/core/test/unit/api/endpoints/automations.test.js
+++ ghost/core/test/unit/api/endpoints/automations.test.js
@@ -2,7 +2,7 @@ const assert = require('node:assert/strict');
 const sinon = require('sinon');
 const domainEvents = require('@tryghost/domain-events');
 const automationsController = require('../../../../core/server/api/endpoints/automations');
-const StartAutomationsPollEvent = require('../../../../core/server/services/automations/events/start-automations-poll-event');
+const {StartAutomationsPollEvent} = require('../../../../core/server/services/automations/events/start-automations-poll-event');
 
 describe('Automations controller', function () {
     // Other endpoints are tested in E2E tests.
