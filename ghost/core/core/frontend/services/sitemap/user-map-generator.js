d0dbe3a0b85c1db9a4191f65c2b72e26d091653a
Steve Larson <9larsons@gmail.com>
2026-08-21 12:12:46 -0500

    Reformatted branch with Oxfmt

--- ghost/core/core/frontend/services/sitemap/user-map-generator.js
+++ ghost/core/core/frontend/services/sitemap/user-map-generator.js
@@ -3,28 +3,26 @@ const validator = require('@tryghost/validator');
 const BaseMapGenerator = require('./base-site-map-generator');
 
 class UserMapGenerator extends BaseMapGenerator {
-    constructor(opts) {
-        super();
+  constructor(opts) {
+    super();
 
-        this.name = 'authors';
-        _.extend(this, opts);
-    }
+    this.name = 'authors';
+    _.extend(this, opts);
+  }
 
-    validateImageUrl(imageUrl) {
-        /** @type {Partial<Parameters<typeof validator.isURL>[0]>} */
-        const isUrlOptions = {
-            protocols: ['http', 'https'],
-            require_protocol: true
-        };
-        return (
-            typeof imageUrl === 'string' &&
-            (
-                // `validator.isURL` doesn't let us express "any TLD or localhost", so we do two checks.
-                validator.isURL(imageUrl, isUrlOptions) ||
-                validator.isURL(imageUrl, {...isUrlOptions, host_whitelist: ['localhost']})
-            )
-        );
-    }
+  validateImageUrl(imageUrl) {
+    /** @type {Partial<Parameters<typeof validator.isURL>[0]>} */
+    const isUrlOptions = {
+      protocols: ['http', 'https'],
+      require_protocol: true,
+    };
+    return (
+      typeof imageUrl === 'string' &&
+      // `validator.isURL` doesn't let us express "any TLD or localhost", so we do two checks.
+      (validator.isURL(imageUrl, isUrlOptions) ||
+        validator.isURL(imageUrl, { ...isUrlOptions, host_whitelist: ['localhost'] }))
+    );
+  }
 }
 
 module.exports = UserMapGenerator;
