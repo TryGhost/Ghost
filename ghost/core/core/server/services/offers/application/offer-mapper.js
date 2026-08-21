c73e1fa945b5d25310a88b701a86e017c7607bdb
Evan Hahn <evan@ghost.org>
2026-08-21 10:32:00 -0500

    Fixed type error in `OfferMapper` code

--- ghost/core/core/server/services/offers/application/offer-mapper.js
+++ ghost/core/core/server/services/offers/application/offer-mapper.js
@@ -17,7 +17,7 @@
  * @prop {number} amount
  *
  * @prop {boolean} currency_restriction
- * @prop {string} currency
+ * @prop {null|string} currency
  *
  * @prop {'once'|'repeating'|'forever'|'trial'} duration
  * @prop {null|number} duration_in_months
@@ -56,58 +56,71 @@
  * @prop {{id: string}|null} tier
  */
 
-class OfferMapper {
-  /**
-   * @param {Offer} offer
-   * @returns {OfferDTO}
-   */
-  static toDTO(offer) {
-    return {
-      id: offer.id,
-      name: offer.name.value,
-      code: offer.code.value,
-      display_title: offer.displayTitle.value,
-      display_description: offer.displayDescription.value,
-      type: offer.type.value,
-      cadence: offer.cadence.value,
-      amount: offer.amount.value,
-      duration: offer.duration.value.type,
-      duration_in_months:
-        offer.duration.value.type === 'repeating' ? offer.duration.value.months : null,
-      currency_restriction: offer.type.value === 'fixed',
-      currency: offer.type.value === 'fixed' ? offer.currency.value : null,
-      status: offer.status.value,
-      redemption_count: offer.redemptionCount,
-      redemption_type: offer.redemptionType.value,
-      tier: offer.tier ? { id: offer.tier.id, name: offer.tier.name } : null,
-      created_at: offer.createdAt,
-      last_redeemed: offer.lastRedeemed,
-    };
-  }
+/**
+ * @param {Readonly<Pick<Offer, 'type' | 'currency'>>} offer
+ * @returns {string | null}
+ */
+const getCurrency = (offer) => {
+    if (offer.type.value === 'fixed') {
+        return offer.currency?.value ?? null;
+    }
+    return null;
+};
 
-  /**
-   * Returns a DTO for a public facing offer (e.g. Portal's retention offer UI)
-   *
-   * @param {Offer} offer
-   * @returns {PublicOfferDTO}
-   */
-  static toPublicDTO(offer) {
-    return {
-      id: offer.id,
-      display_title: offer.displayTitle.value,
-      display_description: offer.displayDescription.value,
-      type: offer.type.value,
-      cadence: offer.cadence.value,
-      amount: offer.amount.value,
-      duration: offer.duration.value.type,
-      duration_in_months:
-        offer.duration.value.type === 'repeating' ? offer.duration.value.months : null,
-      currency: offer.type.value === 'fixed' ? offer.currency.value : null,
-      status: offer.status.value,
-      redemption_type: offer.redemptionType.value,
-      tier: offer.tier ? { id: offer.tier.id } : null,
-    };
-  }
+class OfferMapper {
+    /**
+     * @param {Offer} offer
+     * @returns {OfferDTO}
+     */
+    static toDTO(offer) {
+        return {
+            id: offer.id,
+            name: offer.name.value,
+            code: offer.code.value,
+            display_title: offer.displayTitle.value,
+            display_description: offer.displayDescription.value,
+            type: offer.type.value,
+            cadence: offer.cadence.value,
+            amount: offer.amount.value,
+            duration: offer.duration.value.type,
+            duration_in_months: offer.duration.value.type === 'repeating' ? offer.duration.value.months : null,
+            currency_restriction: offer.type.value === 'fixed',
+            currency: getCurrency(offer),
+            status: offer.status.value,
+            redemption_count: offer.redemptionCount,
+            redemption_type: offer.redemptionType.value,
+            tier: offer.tier
+                ? {id: offer.tier.id, name: offer.tier.name}
+                : null,
+            created_at: offer.createdAt,
+            last_redeemed: offer.lastRedeemed
+        };
+    }
+
+    /**
+     * Returns a DTO for a public facing offer (e.g. Portal's retention offer UI)
+     *
+     * @param {Offer} offer
+     * @returns {PublicOfferDTO}
+     */
+    static toPublicDTO(offer) {
+        return {
+            id: offer.id,
+            display_title: offer.displayTitle.value,
+            display_description: offer.displayDescription.value,
+            type: offer.type.value,
+            cadence: offer.cadence.value,
+            amount: offer.amount.value,
+            duration: offer.duration.value.type,
+            duration_in_months: offer.duration.value.type === 'repeating' ? offer.duration.value.months : null,
+            currency: getCurrency(offer),
+            status: offer.status.value,
+            redemption_type: offer.redemptionType.value,
+            tier: offer.tier
+                ? {id: offer.tier.id}
+                : null
+        };
+    }
 }
 
 module.exports = OfferMapper;
