308c4ad29c8e6f2dbf5de4ec4a92ff46eca3f0c4
Evan Hahn <evan@ghost.org>
2026-08-24 22:37:50 -0500

    TypeScriptified email unsubscribed event test
    no ref

--- ghost/core/test/unit/server/services/email-service/events/email-unsubscribed-event.test.ts
+++ ghost/core/test/unit/server/services/email-service/events/email-unsubscribed-event.test.ts
@@ -1,8 +1,6 @@
-const assert = require('node:assert/strict');
-const ObjectID = require('bson-objectid').default;
-const {
-  EmailUnsubscribedEvent,
-} = require('../../../../../../core/server/services/email-service/events/email-unsubscribed-event');
+import assert from 'node:assert/strict';
+import ObjectID from 'bson-objectid';
+import { EmailUnsubscribedEvent } from '../../../../../../core/server/services/email-service/events/email-unsubscribed-event';
 
 describe('EmailUnsubscribedEvent', function () {
   it('exports a static create method to create instances', function () {
