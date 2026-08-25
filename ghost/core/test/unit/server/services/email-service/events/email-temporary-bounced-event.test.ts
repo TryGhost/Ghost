300bcb3ea87bba2b7959a5d5f495cab40d23c8c9
Evan Hahn <evan@ghost.org>
2026-08-24 22:37:47 -0500

    TypeScriptified temporary email bounce event test
    no ref

--- ghost/core/test/unit/server/services/email-service/events/email-temporary-bounced-event.test.ts
+++ ghost/core/test/unit/server/services/email-service/events/email-temporary-bounced-event.test.ts
@@ -1,8 +1,6 @@
-const assert = require('node:assert/strict');
-const ObjectID = require('bson-objectid').default;
-const {
-  EmailTemporaryBouncedEvent,
-} = require('../../../../../../core/server/services/email-service/events/email-temporary-bounced-event');
+import assert from 'node:assert/strict';
+import ObjectID from 'bson-objectid';
+import { EmailTemporaryBouncedEvent } from '../../../../../../core/server/services/email-service/events/email-temporary-bounced-event';
 
 describe('EmailTemporaryBouncedEvent', function () {
   it('exports a static create method to create instances', function () {
