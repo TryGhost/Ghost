9d5c3f5e1a9159171a4a982d375b494ce0ff0207
Evan Hahn <evan@ghost.org>
2026-08-25 09:10:53 -0500

    TypeScriptified 36 tests (#30275)
    no ref

---
+++ ghost/core/test/unit/server/services/offers/domain/models/offer-cadence.test.ts
@@ -0,0 +1,48 @@
+import assert from 'node:assert/strict';
+// @ts-expect-error This module lacks type definitions.
+import OfferCadence from '../../../../../../../core/server/services/offers/domain/models/offer-cadence';
+
+describe('OfferCadence', function () {
+  describe('OfferCadence.create factory', function () {
+    it('Will only create an OfferCadence containing a string of either "month" or "year"', function () {
+      OfferCadence.create('month');
+      OfferCadence.create('year');
+
+      try {
+        OfferCadence.create();
+        assert.fail();
+      } catch (err) {
+        assert(
+          err instanceof OfferCadence.InvalidOfferCadence,
+          'expected an InvalidOfferCadence error',
+        );
+      }
+
+      try {
+        OfferCadence.create(12);
+        assert.fail();
+      } catch (err) {
+        assert(
+          err instanceof OfferCadence.InvalidOfferCadence,
+          'expected an InvalidOfferCadence error',
+        );
+      }
+
+      try {
+        OfferCadence.create('daily');
+        assert.fail();
+      } catch (err) {
+        assert(
+          err instanceof OfferCadence.InvalidOfferCadence,
+          'expected an InvalidOfferCadence error',
+        );
+      }
+    });
+  });
+
+  it('Exposes a string on the value property', function () {
+    const cadence = OfferCadence.create('month');
+
+    assert(typeof cadence.value === 'string');
+  });
+});
