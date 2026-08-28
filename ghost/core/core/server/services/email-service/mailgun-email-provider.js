0da3b058c0894b85d45c6ae48dacc8afe9987f2e
Evan Hahn <evan@ghost.org>
2026-08-28 09:36:52 -0500

    Removed unused Mailgun error handler callback (#30359)
    no ref

--- ghost/core/core/server/services/email-service/mailgun-email-provider.js
+++ ghost/core/core/server/services/email-service/mailgun-email-provider.js
@@ -35,18 +35,15 @@ const debug = require('@tryghost/debug')('email-service:mailgun-provider-service
 class MailgunEmailProvider {
   #mailgunClient;
   #config;
-  #errorHandler;
 
   /**
    * @param {object} dependencies
    * @param {import('../lib/mailgun-client')} dependencies.mailgunClient - mailgun client to send emails
    * @param {Config} dependencies.config
-   * @param {Function} [dependencies.errorHandler] - custom error handler for logging exceptions
    */
-  constructor({ mailgunClient, config, errorHandler }) {
+  constructor({ mailgunClient, config }) {
     this.#mailgunClient = mailgunClient;
     this.#config = config;
-    this.#errorHandler = errorHandler;
   }
 
   #createRecipientData(replacements) {
