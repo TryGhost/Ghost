e3f0c801ed767375cf11a9c10f7bbe1c61762917
Steve Larson <9larsons@gmail.com>
2026-09-07 15:50:01 -0500

    Added the React editor publishing journeys to the E2E suite (#30557)
    no ref
    
    Added a few E2E user journeys for the React editor.

--- packages/testing/test-data/src/selectors/editor.ts
+++ packages/testing/test-data/src/selectors/editor.ts
@@ -113,3 +113,7 @@ export const editorUpdateButton = 'Update';
 export const editorSaveButton = 'Save';
 export const editorUnpublishButton = 'Unpublish';
 export const editorUnscheduleButton = 'Unschedule';
+export const publishTypePublishAndEmailOption = 'Publish and email';
+export const publishTypePublishOnlyOption = 'Publish only';
+export const publishTypeEmailOnlyOption = 'Email only';
+export const publishAtScheduleOption = 'Schedule for later';
