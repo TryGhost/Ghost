28b71cd81cd6cfb9123e6a583454b8816758bc27
Evan Hahn <evan@ghost.org>
2026-09-07 12:13:51 -0500

    TypeScriptified `defaultIndexName`

--- ghost/core/core/server/data/schema/lib/default-index-name.ts
+++ ghost/core/core/server/data/schema/lib/default-index-name.ts
@@ -4,17 +4,11 @@
  * closely as possible.
  *
  * [0]: https://github.com/knex/knex/blob/e25d54bcb707714a17f5a5744eba5c4246bb4d1d/lib/schema/tablecompiler.js#L401-L415
- *
- * @param {string} tableName
- * @param {string|string[]} columns
- * @returns {string}
  */
-function defaultIndexName(tableName, columns) {
+export function defaultIndexName(tableName: string, columns: string | string[]): string {
   if (!Array.isArray(columns)) {
     columns = columns ? [columns] : [];
   }
   const table = tableName.replace(/\.|-/g, '_');
   return (table + '_' + columns.join('_') + '_index').toLowerCase();
 }
-
-exports.defaultIndexName = defaultIndexName;
