3654bd42f6ceb8c3235d32982ebb661bc4b3144d
Kevin Ansfield <kevin@lookingsideways.co.uk>
2026-09-08 08:09:42 +0100

    Updated donation migration for the current release
    ref https://github.com/TryGhost/Ghost/pull/30118
    
    Move the unshipped migration beyond released versions and refresh the schema integrity hash after rebasing onto main.

--- ghost/core/test/unit/server/data/schema/integrity.test.js
+++ ghost/core/test/unit/server/data/schema/integrity.test.js
@@ -37,7 +37,7 @@ const parseYaml = require('../../../../../core/server/services/route-settings/ya
  */
 describe('DB version integrity', function () {
   // Only these variables should need updating
-  const currentSchemaHash = '563cc592309a7e721995c995c3afa234';
+  const currentSchemaHash = 'b057e3f34a9a4205a6680e02b0c53edb';
   const currentFixturesHash = '5718e0d4eb037f159c312369e949829a';
   const currentSettingsHash = '6ea42a00cca61a1ba87f66eb6e25a78a';
   const currentRoutesHash = 'd8c25fa01bf6d22a2bcb05ba0de70dc1';
